b18ca36919b1c100e68decc693ca4893fd5e8fde
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-07-06 16:52:08 -0400

    More demo targets.

--- demo.py
+++ demo.py
@@ -1,27 +1,36 @@
 from labware import Pipette_P10, Microplate_96_deepwell, Trash, Tiprack_P10
 from protocol import Protocol
 
-pipette_a = Pipette_P10()
-wells = Microplate_96_deepwell()
-
 """ Alt syntax:
-pipette_a = Pipette('p10')
+pipe = Pipette('p10')
 wells = Microplate('96.deepwell')
 """
 
 protocol = Protocol()
 
-protocol.add_instrument(a=pipette_a)
+protocol.add_instrument(a=Pipette_P10())
 protocol.set_modules(
-	a1=wells,
-	a2=Trash(),
-	a3=Tiprack_P10()
+	a1=Tiprack_P10(),
+	a2=Microplate_96_deepwell(),
+	a3=Trash()
 )
 
-pipette_a.get_tip()
-pipette_a.transfer(wells.well('a1'), wells.well('a2'))
-pipette_a.trash_tip()
+well = protocol.get_module('a2')
+pipe = protocol.get_instrument('a')
 
-pipette_a.get_tip()
-pipette_a.transfer(wells.row('b'), wells.row('c'))
-pipetta_a.trash_tip()
+pipe.get_tip()
+pipe.transfer(10, wells.well('a1'), wells.well('a2'))
+pipe.trash_tip()
+
+pipe.get_tip()
+pipe.transfer(10, wells.row('b'), wells.row('c'), reuse_tip=True)
+pipe.trash_tip()
+
+""" Get tip and trash tip should be implicit. """
+pipe.transfer(10, wells.row('c'), wells.row('d'), reuse_tip=True)
+
+""" Get tip; transfer; trash tip """
+pipe.transfer(10, wells.well('a2'), wells.well('a1'))
+
+""" Get tip; transfer; trash tip """
+pipe.transfer(10, wells.well('a1'), wells.well('a2'))
