d613c0b0d9c272519959d4758b86fac9a54f571d
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-07-06 16:42:39 -0400

    D'oh; pipettes aren't deck modules.

--- demo.py
+++ demo.py
@@ -4,13 +4,18 @@ from protocol import Protocol
 pipette_a = Pipette_P10()
 wells = Microplate_96_deepwell()
 
+""" Alt syntax:
+pipette_a = Pipette('p10')
+wells = Microplate('96.deepwell')
+"""
+
 protocol = Protocol()
 
+protocol.add_instrument(a=pipette_a)
 protocol.set_modules(
-	a1=pipette_a,
-	a2=wells,
-	a3=Trash(),
-	a4=Tiprack_P10()
+	a1=wells,
+	a2=Trash(),
+	a3=Tiprack_P10()
 )
 
 pipette_a.get_tip()
