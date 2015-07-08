c1dc515f749e46ce7df050335e891e3f7ff8bb5e
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-07-08 14:25:08 -0400

    Better test names.

--- tests/labware_tests.py
+++ tests/labware_tests.py
@@ -10,26 +10,26 @@ class MicroplateTest(unittest.TestCase):
 		plate.calibrate(x=10, y=10, z=10)
 		self.assertEqual(plate.well('A1').coordinates(), (10, 10, 10))
 
-	def a2_calibration_test(self):
+	def a2_coordinate_test(self):
 		plate = labware.Microplate()
 		plate.calibrate(x=10, y=10, z=10)
 		a2 = plate.well('A2').coordinates()
 		self.assertEqual(a2, (10, 10+self.expected_margin, 10))
 
-	def b1_calibration_test(self):
+	def b1_coordinate_test(self):
 		plate = labware.Microplate()
 		plate.calibrate(x=10, y=10, z=10)
 		b1 = plate.well('B1').coordinates()
 		self.assertEqual(b1, (10+self.expected_margin, 10, 10))
 
-	def b2_calibration_test(self):
+	def b2_coordinate_test(self):
 		plate  = labware.Microplate()
 		plate.calibrate(x=10, y=10, z=10)
 		b2 = plate.well('B2').coordinates()
 		margin = self.expected_margin
 		self.assertEqual(b2, (10+margin, 10+margin, 10))
 
-	def calibration_lowercase_test(self):
+	def coordinate_lowercase_test(self):
 		plate  = labware.Microplate()
 		plate.calibrate(x=10, y=10, z=10)
 		b2 = plate.well('b2').coordinates()
