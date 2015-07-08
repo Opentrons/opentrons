7ff505afe193463f0ef88ea39d2bd42c308fdfd8
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-07-08 16:48:42 -0400

    Tipracks and some refactoring.

--- labware.py
+++ labware.py
@@ -48,9 +48,10 @@ class Labware():
 
 class Deck():
 
-	_slots = {} # Keys are tuples of zero-index positions. A1 = (0, 0)
+	_slots = None # Keys are tuples of zero-index positions. A1 = (0, 0)
 
 	def __init__(self, **kwargs):
+		self._slots = {}
 		self.add_modules(**kwargs)
 
 	def add_modules(self, **kwargs):
@@ -80,7 +81,7 @@ class Deck():
 		calibration = obj['calibration']
 		for pos in calibration:
 			module = self.slot(pos)
-			module.calibrate(calibration[pos])
+			module.calibrate(**calibration[pos])
 
 	@staticmethod
 	def _normalize_position(position):
@@ -134,8 +135,74 @@ class Pipette_P1000(Pipette):
 	min_vol  =  200
 	max_vol  = 1000
 
-class Tiprack(Labware):
+class ContainerGrid(Labware):
+	rows = 0
+	cols = 0
+	spacing = 0
+
+	"""
+	Calibration.
+	"""
+	start_x = 0
+	start_y = 0
+	start_z = 0
+
+	"""
+	A dict containing tuples of zero-indexed child coordinates.
+	We only initialize them when they're accessed because until then, there's
+	no way to mutate their (non-existent) state.
+	"""
+	_children = None #{}
+
+	def __init__(self, parent_deck=None):
+		self.parent_deck = parent_deck
+		self._children = {}
+
+	def calibrate(self, x=None, y=None, z=None, **kwargs):
+		"""
+		Coordinates should represent the center and near-bottom of well
+		A1 with the pipette tip in place.
+		"""
+		self.start_x = x
+		self.start_y = y
+		self.start_z = z
+
+	def get_child_coordinates(self, position):
+		""" Get a well based on a row, col string like "A1". """
+		row, col = Deck._normalize_position(position)
+		offset_x = self.spacing*row
+		offset_y = self.spacing*col
+		print(self.start_x)
+		print(self.start_y)
+		print(self.start_z)
+		return (offset_x+self.start_x, offset_y+self.start_y, self.start_z)
+
+	def get_child(self, position):
+		key = Deck._normalize_position(position)
+		if key not in self._children:
+			child = self.init_child(position)
+			self._children[key] = child
+		return self._children[key]
+
+	def init_child(self, position):
+		return Object()
+
+class Tiprack(ContainerGrid):
+
 	size = None
+
+	"""
+	I'm not even sure there is a standard for spacing on
+	tipracks. I'm just using the microplate spacing, and
+	it works with the rack I have.
+	"""
+	spacing = 9
+
+	def slot(self, position):
+		return self.get_child(position)
+
+	def init_child(self, position):
+		return TiprackSlot(self, position)
 
 class Tiprack_P10(Tiprack):
 	size = 'P10'
@@ -149,7 +216,7 @@ class Tiprack_P200(Tiprack):
 class Tiprack_P1000(Tiprack):
 	size = 'P1000'
 
-class Microplate(Labware):
+class Microplate(ContainerGrid):
 	rows     =   8
 	cols     =  12
 	volume   = 100
@@ -164,39 +231,11 @@ class Microplate(Labware):
 	a1_y     =  11.24
 	spacing  =   9
 
-	"""
-	A dict containing tuples of zero-indexed well coordinates.
-	We only initialize them when they're accessed because until then, there's
-	no way to mutate their (non-existent) state.
-	"""
-	_wells   = {}
-
-	def __init__(self, parent_deck=None):
-		self.parent_deck = parent_deck
-		pass
-
-	def calibrate(self, x=None, y=None, z=None):
-		"""
-		Coordinates should represent the center and near-bottom of well
-		A1 with the pipette tip in place.
-		"""
-		self.start_x    = x
-		self.start_y    = y
-		self.transfer_z = z
-
-	def get_well_coordinates(self, position):
-		""" Get a well based on a row, col string like "A1". """
-		row, col = Deck._normalize_position(position)
-		offset_x = self.spacing*row
-		offset_y = self.spacing*col
-		return (offset_x+self.start_x, offset_y+self.start_y, self.transfer_z)
-
 	def well(self, position):
-		key = Deck._normalize_position(position)
-		if key not in self._wells:
-			well = MicroplateWell(self, position)
-			self._wells[key] = well
-		return self._wells[key]
+		return self.get_child(position)
+
+	def init_child(self, position):
+		return MicroplateWell(self, position)
 
 class Microplate_96(Microplate):
 	pass
@@ -208,11 +247,8 @@ class Microplate_96_deepwell(Microplate_96):
 	height   =  14.6
 	depth    =  10.8
 
-class LiquidContainer():
-	pass
+class GridContainerChild():
 
-class MicroplateWell(LiquidContainer):
-	
 	parent   = None
 	position = None
 
@@ -221,7 +257,13 @@ class MicroplateWell(LiquidContainer):
 		self.position = position
 
 	def coordinates(self):
-		return self.parent.get_well_coordinates(self.position)
+		return self.parent.get_child_coordinates(self.position)
+
+class MicroplateWell(GridContainerChild):
+	pass
+
+class TiprackSlot(GridContainerChild):
+	pass
 
 class Trash(Labware):
 	pass
--- tests/labware_tests.py
+++ tests/labware_tests.py
@@ -58,3 +58,62 @@ class MicroplateTest(unittest.TestCase):
 
 		self.assertEqual(a1, (m_offset, m_offset, m_offset))
 		self.assertEqual(b2, (m_offset+margin, m_offset+margin, m_offset))
+
+
+class TiprackTest(unittest.TestCase):
+
+	expected_margin = 9 # ANSI standard.
+
+	def setUp(self):
+		self.rack = labware.Tiprack()
+		self.rack.calibrate(x=10, y=10, z=10)
+
+	def a1_calibration_test(self):
+		a1 = self.rack.slot('A1').coordinates()
+		self.assertEqual(a1, (10, 10, 10))
+
+	def a2_coordinate_test(self):
+		a2 = self.rack.slot('A2').coordinates()
+		self.assertEqual(a2, (10, 10+self.expected_margin, 10))
+
+	def b1_coordinate_test(self):
+		b1 = self.rack.slot('B1').coordinates()
+		self.assertEqual(b1, (10+self.expected_margin, 10, 10))
+
+	def b2_coordinate_test(self):
+		b2 = self.rack.slot('B2').coordinates()
+		margin = self.expected_margin
+		self.assertEqual(b2, (10+margin, 10+margin, 10))
+
+	def coordinate_lowercase_test(self):
+		b2 = self.rack.slot('b2').coordinates()
+		margin = self.expected_margin
+		self.assertEqual(b2, (10+margin, 10+margin, 10))
+
+	def deck_calibration_test(self):
+
+		m_offset = 10
+
+		config = {
+			'calibration': {
+				'a1': {
+					'type':'tiprack_P2',
+					'x': m_offset,
+					'y': m_offset,
+					'z': m_offset
+				}
+			}
+		}
+
+		deck = labware.Deck(a1=labware.Tiprack())
+		deck.configure(config)
+
+		margin = self.expected_margin
+
+		rack = deck.slot('a1')
+
+		a1 = rack.slot('a1').coordinates()
+		b2 = rack.slot('b2').coordinates()
+
+		self.assertEqual(a1, (m_offset, m_offset, m_offset))
+		self.assertEqual(b2, (m_offset+margin, m_offset+margin, m_offset))
