30884e84c43bb47dd57cf754976e9b7cfe0a0a0b
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-07-08 16:13:51 -0400

    Test code cleanup.

--- tests/labware_tests.py
+++ tests/labware_tests.py
@@ -5,34 +5,29 @@ class MicroplateTest(unittest.TestCase):
 
 	expected_margin = 9 # ANSI standard.
 
+	def setUp(self):
+		self.plate = labware.Microplate()
+		self.plate.calibrate(x=10, y=10, z=10)
+
 	def a1_calibration_test(self):
-		plate = labware.Microplate()
-		plate.calibrate(x=10, y=10, z=10)
-		self.assertEqual(plate.well('A1').coordinates(), (10, 10, 10))
+		a1 = self.plate.well('A1').coordinates()
+		self.assertEqual(a1, (10, 10, 10))
 
 	def a2_coordinate_test(self):
-		plate = labware.Microplate()
-		plate.calibrate(x=10, y=10, z=10)
-		a2 = plate.well('A2').coordinates()
+		a2 = self.plate.well('A2').coordinates()
 		self.assertEqual(a2, (10, 10+self.expected_margin, 10))
 
 	def b1_coordinate_test(self):
-		plate = labware.Microplate()
-		plate.calibrate(x=10, y=10, z=10)
-		b1 = plate.well('B1').coordinates()
+		b1 = self.plate.well('B1').coordinates()
 		self.assertEqual(b1, (10+self.expected_margin, 10, 10))
 
 	def b2_coordinate_test(self):
-		plate  = labware.Microplate()
-		plate.calibrate(x=10, y=10, z=10)
-		b2 = plate.well('B2').coordinates()
+		b2 = self.plate.well('B2').coordinates()
 		margin = self.expected_margin
 		self.assertEqual(b2, (10+margin, 10+margin, 10))
 
 	def coordinate_lowercase_test(self):
-		plate  = labware.Microplate()
-		plate.calibrate(x=10, y=10, z=10)
-		b2 = plate.well('b2').coordinates()
+		b2 = self.plate.well('b2').coordinates()
 		margin = self.expected_margin
 		self.assertEqual(b2, (10+margin, 10+margin, 10))
 
