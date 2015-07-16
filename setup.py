cb5e020bab8f18922431bffaa828ab80cacbefc0
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-07-16 15:24:38 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': "Python implementations of labware.",
+    'author': "OpenTrons",
+    'url': 'http://opentrons.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['labware'],
+    'name': 'opentrons',
+    'test_suite': 'nose.collector'
+}
+
+setup(**config)
