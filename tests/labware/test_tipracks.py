f7f030a7bfbbef5e8901aff5b869461a979a30e9
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-10-19 15:59:02 -0400

    Tipracks: Documented tests; removed obsolete API stuff.

--- tests/labware/test_tipracks.py
+++ tests/labware/test_tipracks.py
@@ -11,29 +11,46 @@ class TiprackTest(unittest.TestCase):
         self.rack.calibrate(x=10, y=11, z=12)
 
     def a1_calibration_test(self):
+        """
+        Calibration included in A1 slot coordinates.
+        """
         a1 = self.rack.slot('A1').coordinates
         self.assertEqual(a1, (10, 11, 12))
 
     def a2_coordinate_test(self):
+        """
+        Calibration included in A2 slot coordinates.
+        """
         a2 = self.rack.slot('A2').coordinates
         self.assertEqual(a2, (10, 11 + self.expected_margin, 12))
 
     def b1_coordinate_test(self):
+        """
+        Coordinates for B1 position.
+        """
         b1 = self.rack.slot('B1').coordinates
         self.assertEqual(b1, (10 + self.expected_margin, 11, 12))
 
     def b2_coordinate_test(self):
+        """
+        Coordinates for B2 position.
+        """
         b2 = self.rack.slot('B2').coordinates
         margin = self.expected_margin
         self.assertEqual(b2, (10 + margin, 11 + margin, 12))
 
     def coordinate_lowercase_test(self):
+        """
+        Accept lowercase coordinates.
+        """
         b2 = self.rack.slot('b2').coordinates
         margin = self.expected_margin
         self.assertEqual(b2, (10 + margin, 11 + margin, 12))
 
     def row_sanity_test(self):
-
+        """
+        Maintain sanity of rowumn values.
+        """
         row = chr(ord('a') + self.rack.rows + 1)
         with self.assertRaises(KeyError):
             self.rack.slot('{}1'.format(row))
@@ -42,7 +59,9 @@ class TiprackTest(unittest.TestCase):
         self.rack.slot('{}1'.format(row))
 
     def col_sanity_test(self):
-
+        """
+        Maintain sanity of col values.
+        """
         col = self.rack.cols + 1
 
         with self.assertRaises(KeyError):
@@ -52,6 +71,9 @@ class TiprackTest(unittest.TestCase):
         self.rack.slot('A{}'.format(col))
 
     def deck_calibration_test(self):
+        """
+        Verify calibration offsets.
+        """
 
         config = {
             'calibration': {
@@ -76,18 +98,3 @@ class TiprackTest(unittest.TestCase):
 
         self.assertEqual(a1, (10, 11, 12))
         self.assertEqual(b2, (10 + margin, 11 + margin, 12))
-
-    def tip_inventory_test(self):
-
-        self.assertEqual(self.rack.slot('a1').get_tip(), True)
-
-        with self.assertRaises(Exception):
-            self.rack.slot('a1').get_tip()
-
-        """ Secondary syntax just in case. """
-
-        slot = self.rack.slot('a2')
-        self.assertEqual(slot.get_tip(), True)
-
-        with self.assertRaises(Exception):
-            slot.get_tip()
