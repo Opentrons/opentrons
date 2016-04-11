319cc3a0dea91d94fc98ce4a8cfdb62fdc51db22
Michelle Steigerwalt <michelle@opentrons.com>
2016-04-11 12:08:11 -0400

    Setup: Author email and exclude tests from the package.

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 config = {
     'description': "A suite of tools for portable automated scientific protocols.",
     'author': "OpenTrons",
+    'author_email': 'info@opentrons.com',
     'url': 'http://opentrons.com',
     'version': '0.4',
     'install_requires': ['pyyaml'],
-    'packages': find_packages(),
+    'packages': find_packages(exclude=["tests"]),
     'package_data': { 
         "labsuite": [
             "config/containers/**/*.yml",
