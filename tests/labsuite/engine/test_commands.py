b6621f9dde2e9d6a4143594c3f706b7fe905b29e
Michelle Steigerwalt <michelle@opentrons.com>
2016-06-14 13:51:44 -0400

    Commands: Named and unnamed tags.

--- tests/labsuite/engine/test_commands.py
+++ tests/labsuite/engine/test_commands.py
@@ -7,14 +7,33 @@ define_argument(
     'A1:A1 or Plate1:A1'
 )
 
+define_argument(
+    'volume',
+    '\d+[umµ]l',
+    '10ul, 10ml, 10l, 10µl, etc.'
+)
+
+
+@syntax('<volume> to <plate>')
+def move_volume(volume=None, plate=None):
+    """ Named tag arguments. """
+    return 'Move volume of {} to {}.'.format(volume, plate)
+
+
 @syntax('<plate:from_plate> to <plate:to_plate>')
 def transfer(from_plate=None, to_plate=None):
-    """ Print a message to the console. """
+    """ Named tag arguments. """
     return 'Transfer from {} to {}.'.format(from_plate, to_plate)
+
 
 class CommandTest(unittest.TestCase):
 
     def test_basic_parsing(self):
         parsed = execute('transfer A2:B1 to D3:A1')
         expected = 'Transfer from A2:B1 to D3:A1.'
+        self.assertEqual(expected, parsed)
+
+    def move_volume(self):
+        parsed = execute('move_volume 100ul to D3:A1')
+        expected = 'Move volume of 100ul to D3:A1.'
         self.assertEqual(expected, parsed)
