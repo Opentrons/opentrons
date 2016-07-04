6e7e64fcba982d3db6f858e2cf3f931d4fb0b863
Michelle Steigerwalt <michelle@opentrons.com>
2016-07-04 14:34:51 -0400

    Logging: set_log_file convenience method.
    If a log file isn't set, and the standard development log file
    doesn't exist (as in the case with an installed library), then
    the log will be available using standard logging module
    configuration called manually.

--- labsuite/util/log.py
+++ labsuite/util/log.py
@@ -2,19 +2,21 @@ import logging
 import labsuite
 import os
 
-filename = os.path.join(
-    os.path.dirname(labsuite.__file__),
-    '..',
-    'logs',
-    'operation_log.log'
-)
 
-logging.basicConfig(
-    filename=filename,
-    level=logging.DEBUG,
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    datefmt='%d-%m-%y %H:%M:%S',
-)
+def set_log_file(filename):
+    """
+    Convenience method to set a log file along with default parameters
+    provided by this module.
+
+    logging.basicConfig can always be used on its own to specify any logging
+    conditions desired.
+    """
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%d-%m-%y %H:%M:%S',
+        filename=filename
+    )
 
 
 def debug(system, message):
@@ -23,3 +25,13 @@ def debug(system, message):
 
 def info(system, message):
     logging.info("[{}] {}".format(system, message))
+
+
+log_path = os.path.join(
+    os.path.dirname(labsuite.__file__),
+    '..',
+    'logs'
+)
+
+if os.path.isdir(log_path):
+    set_log_file(os.path.join(log_path, 'opentrons.log'))
