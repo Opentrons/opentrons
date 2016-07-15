2bc52b682c817616867d4c3ba7c5894fcdd5f02b
Michelle Steigerwalt <michelle@opentrons.com>
2016-07-15 13:43:36 -0400

    Protocol: Change call to internal _run(i) in test with next().
    This still isn't where I want it to be; the first yield to kick
    off the protocol run with a start value of zero and the total
    number of commands is kind of unintuitive.

--- tests/labsuite/protocol/test_protocol.py
+++ tests/labsuite/protocol/test_protocol.py
@@ -180,10 +180,12 @@ class ProtocolTest(unittest.TestCase):
         self.protocol._initialize_context()
         vol1 = self.protocol._context_handler.get_volume('A1:A2')
         self.assertEqual(vol1, 0)
-        self.protocol._run(0)
+        run = self.protocol.run()
+        next(run)  # Yield to set progress.
+        next(run)  # Run first command.
         vol2 = self.protocol._context_handler.get_volume('A1:A2')
         self.assertEqual(vol2, 100)
-        self.protocol._run(1)
+        next(run)  # Run second command.
         vol3 = self.protocol._context_handler.get_volume('A1:A3')
         self.assertEqual(vol3, 80)
 
