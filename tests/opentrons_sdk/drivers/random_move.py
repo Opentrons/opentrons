5d8a4fc706fe3f235e04412e056187841dc6d52c
Andy Sigler <andrewsigler1@gmail.com>
2016-09-13 15:17:49 -0400

    updated random test to include small movements

--- tests/opentrons_sdk/drivers/random_move.py
+++ tests/opentrons_sdk/drivers/random_move.py
@@ -8,27 +8,43 @@ if not motor.connect(sys.argv[1]):
 	print('failed connecting to port {}'.format(sys.argv[1]))
 	sys.exit()
 
+ranges = {
+	'x' : 250,
+	'y' : 200,
+	'z' : 100,
+	'a' : 20,
+	'b' : 20
+}
+
+def rand_move(scale=1.0):
+	if scale > 1.0:
+		scale = 1.0
+	if scale < 0.0:
+		scale = 0.0
+
+	kwargs = {}
+
+	for axis in 'xyzab':
+		if random.random() < 0.3:
+			kwargs[axis] = random.random() * (ranges[axis] * scale)
+			kwargs[axis] += (ranges[axis]/2) * (1.0-scale)
+
+	if bool(kwargs):
+		print('-->  {}'.format(kwargs))
+		motor.move(**kwargs)
+
 try:
 	while True:
 		print('\nhoming')
 		motor.home('abz')
 		motor.home('x','y')
 
-		for i in range(random.randint(5,40)):
-			kwargs = {}
-			if random.random() < 0.3:
-				kwargs['x'] = random.random() * 250
-			if random.random() < 0.3:
-				kwargs['y'] = random.random() * 200
-			if random.random() < 0.3:
-				kwargs['z'] = random.random() * 100
-			if random.random() < 0.3:
-				kwargs['a'] = random.random() * 20
-			if random.random() < 0.3:
-				kwargs['b'] = random.random() * 20
-			if bool(kwargs):
-				print('-->  {}'.format(kwargs))
-				motor.move(**kwargs)
+		motor.move(**ranges)
+
+		for i in range(random.randint(5,20)):
+			rand_move()
+			for i in range(random.randint(5,20)):
+				rand_move(0.025)
 
 		print('waiting for arrival...')
 		motor.wait_for_arrival()
