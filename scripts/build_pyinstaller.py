507a03d96ff5c1377c65e55789ee432b60e88e19
Ahmed Abdalla <ahmad19526@gmail.com>
2016-09-14 11:32:31 -0400

    pyinstallers script generates static css using compass

--- scripts/build_pyinstaller.py
+++ scripts/build_pyinstaller.py
@@ -124,6 +124,20 @@ def move_executable_folder(final_exec_dir):
     return True
 
 
+def generate_static_css():
+    old_cwd = os.getcwd()
+    server_dir = os.path.join(project_root_dir, 'server')
+    os.chdir(server_dir)
+    compass_process = subprocess.Popen(['compass', 'compile'])
+    compass_process.communicate()
+    if compass_process.returncode != 0:
+        print(script_tab + "ERROR: Compass returned with exit code: %s" %
+              compass_process.returncode)
+        return False
+    os.chdir(old_cwd)
+    return True
+
+
 def build_ot_python_backend_executable():
     print(script_tag + "Build procedure started.")
     print(script_tag + "Checking for OS.")
@@ -132,6 +146,11 @@ def build_ot_python_backend_executable():
 
     print(script_tag + "Project directory is:     %s" % project_root_dir)
     print(script_tag + "Script working directory: %s" % os.getcwd())
+
+    print(script_tag + "Generating static CSS for Flask app.")
+    success = generate_static_css()
+    if not success:
+        raise SystemExit(script_tab + "Exiting due to error generating static CSS")
 
 
     print(script_tag + "Removing PyInstaller old temp directories.")
