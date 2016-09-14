ab3bfd1453d3cccb863ece542d7a238d5b299610
Ahmed Abdalla <ahmad19526@gmail.com>
2016-09-14 10:52:55 -0400

    adds tests for helpers

---
+++ server/tests/test_helpers.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+
+from helpers import get_assets
+
+
+class HelpersTestCase(unittest.TestCase):
+
+    def test_get_assets(self):
+        root = os.path.dirname(
+            os.path.dirname(
+                os.path.realpath(__file__)
+            )
+        )
+        self.assertTrue(len(get_assets(root, 'views', 'html')) > 0)
+        self.assertTrue(len(get_assets(root, 'scripts', 'js')) > 0)
+
+if __name__ == '__main__':
+    unittest.main()
