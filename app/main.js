76125101ccbbc2e86dfa60d6f631749d1a0b7875
Ahmed Abdalla <ahmad19526@gmail.com>
2016-09-15 01:12:32 -0400

    creates process manager module for managing the backend

--- app/main.js
+++ app/main.js
@@ -3,18 +3,12 @@ const child_process = require('child_process')
 const electron = require('electron')
 const {app, BrowserWindow} = electron
 
+const startServer = require('./processmanager.js').startServer
+
+typeof startServer
+
 let mainWindow
 
-function startFlaskServer() {
-  child_process.exec("source frontend_env/bin/activate && python3 main.py", function(error, stdout, stderr){
-        console.log(stdout);
-        console.log(stderr);
-        if (error) {
-            throw error;
-        }
-    });
-  setTimeout(createWindow, 5000)
-}
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 1200, height: 900})
@@ -26,5 +20,5 @@ function createWindow () {
   })
 }
 
-app.on('ready', startFlaskServer)
 app.on('ready', createWindow)
+app.on('ready', startServer)
