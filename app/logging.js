0db609aafd6fe9a73880221690090c61f2ac7ba8
Ahmed Abdalla <ahmad19526@gmail.com>
2016-09-15 15:17:38 -0400

    adds logging module

---
+++ app/logging.js
@@ -0,0 +1,43 @@
+const electron = require('electron')
+const fs = require('fs')
+const path = require('path')
+
+const {app} = electron
+const winston = require('winston')
+
+
+function createLogger(path, name) {
+  fs.appendFileSync(path, '');
+
+  return new (winston.Logger)({
+      transports: [
+          new (winston.transports.File)({
+              level: 'verbose',
+              name: name,
+              filename: path,
+              json: false,
+              maxsize: 10*1024*1024,
+              maxFiles: 5,
+              timestamp: function(){
+                const d = new Date();
+                return d.toISOString();
+              }
+          })
+      ]
+  });
+}
+
+function getLogger(name) {
+  const loggerPath = path.join(
+    app.getPath('userData'),
+    'otone_data',
+    name.concat('.log')
+  )
+  return createLogger(loggerPath, name)
+}
+
+
+module.exports = {
+  createLogger,
+  getLogger
+}
