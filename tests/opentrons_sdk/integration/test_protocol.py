600a1a9b11e18e91e7649a379976d5bdd38dffc7
Ahmed Abdalla <ahmad19526@gmail.com>
2016-09-22 09:57:31 -0400

    start protocol integration test

--- tests/opentrons_sdk/integration/test_protocol.py
+++ tests/opentrons_sdk/integration/test_protocol.py
@@ -1,1 +1,15 @@
-__author__ = 'ahmed'
+import unittest
+
+from opentrons_sdk.protocol.protocol import Protocol
+from opentrons_sdk.labware import containers
+
+
+
+class ProtocolTestCase(unittest.TestCase):
+    def setUp(self):
+        self.protocol = Protocol.get_instance()
+
+    def test_protocol_load(self):
+        plate = containers.load('microplate.96', 'A1')
+        # self.protocol
+
