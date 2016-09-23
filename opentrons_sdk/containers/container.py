528f2c270040e9b1bd52f6996291ce886616a52e
astaff <artyom.astafurov@gmail.com>
2016-09-23 17:35:39 -0400

    Adds relative cartesian and polar coordinates calculation for wells relative to decks; more tests

--- opentrons_sdk/containers/container.py
+++ opentrons_sdk/containers/container.py
@@ -1,3 +1,5 @@
+import math
+import numbers
 from collections import OrderedDict
 
 
@@ -99,10 +101,82 @@ class Container(Placeable):
         return self.get_child_by_name(name)
 
     def wells(self):
-        return self.get_children().keys
+        return self.get_children()
 
 
 class Well(Placeable):
     def __init__(self, properties=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        if properties is None:
+            properties = {}
+
         self.properties = properties
+
+        if 'radius' in properties:
+            properties['width'] = properties['radius'] * 2
+            properties['length'] = properties['radius'] * 2
+
+        if 'diameter' in properties:
+            properties['width'] = properties['diameter']
+            properties['length'] = properties['diameter']
+
+        assert 'width' in properties
+        assert 'length' in properties
+
+    def find_parent_deck(self):
+        parent = self.parent
+
+        found = False
+        while not found:
+            if parent is None:
+                break
+            if isinstance(parent, Deck):
+                found = True
+                break
+            parent = parent.parent
+
+        return parent
+
+    # axis_length is here to avoid confision with
+    # height, width, depth
+    def x_length(self):
+        return self.properties['width']
+
+    # axis_length is here to avoid confision with
+    # height, width, depth
+    def y_length(self):
+        return self.properties['length']
+
+    # TODO: add support for H
+    def from_polar(self, r, theta, h):
+        x = self.x_length() / 2.0
+        y = self.y_length() / 2.0
+        r = x
+        return (x + r * math.cos(-theta),
+                y + r * math.sin(-theta),
+                h)
+
+    # TODO: add support for relative Z coordinates
+    def from_cartesian(self, x, y, z):
+        x_center = (self.x_length() / 2.0)
+        y_center = (self.y_length() / 2.0)
+
+        return (x_center + x_center * x,
+                y_center + y_center * y,
+                z)
+
+    def from_center(self, x=None, y=None, z=None, r=None, theta=None, h=None):
+        res = None
+        if all([isinstance(i, numbers.Number) for i in (x, y, z)]):
+            res = self.from_cartesian(x, y, z)
+
+        if all([isinstance(i, numbers.Number) for i in (r, theta, h)]):
+            res = self.from_polar(r, theta, h)
+
+        deck = self.find_parent_deck()
+        res = tuple(
+            map(
+                lambda a: a[0] + a[1], zip(self.coordinates(deck), res)))
+        return res
+
--- tests/opentrons_sdk/containers/test_containers.py
+++ tests/opentrons_sdk/containers/test_containers.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 
 from opentrons_sdk.containers.container import (
     Container,
@@ -8,11 +9,11 @@ from opentrons_sdk.containers.container import (
 
 
 class ContainerTestCase(unittest.TestCase):
-    def generate_plate(self, wells, rows, spacing, offset):
+    def generate_plate(self, wells, rows, spacing, offset, radius):
         c = Container()
 
         for i in range(0, wells):
-            well = Well()
+            well = Well(properties={'radius': radius})
             row, col = divmod(i, rows)
             name = chr(row + ord('A')) + str(1 + col)
             coordinates = (col * spacing[0] + offset[0],
@@ -22,21 +23,21 @@ class ContainerTestCase(unittest.TestCase):
         return c
 
     def test_iterator(self):
-        c = self.generate_plate(4, 2, (5, 5), (0, 0))
+        c = self.generate_plate(4, 2, (5, 5), (0, 0), 5)
         res = [well.coordinates() for well in c]
         expected = [(0, 0, 0), (5, 0, 0), (0, 5, 0), (5, 5, 0)]
 
         self.assertListEqual(res, expected)
 
     def test_next(self):
-        c = self.generate_plate(4, 2, (5, 5), (0, 0))
+        c = self.generate_plate(4, 2, (5, 5), (0, 0), 5)
         well = c['A1']
         expected = c.get_child_by_name('A2')
 
         self.assertEqual(next(well), expected)
 
     def test_int_index(self):
-        c = self.generate_plate(4, 2, (5, 5), (0, 0))
+        c = self.generate_plate(4, 2, (5, 5), (0, 0), 5)
 
         self.assertEqual(c[3], c.get_child_by_name('B2'))
         self.assertEqual(c[1], c.get_child_by_name('A2'))
@@ -46,7 +47,8 @@ class ContainerTestCase(unittest.TestCase):
             wells=96,
             rows=8,
             spacing=(10, 15),
-            offset=(5, 15)
+            offset=(5, 15),
+            radius=5
         )
 
         self.assertEqual(c['A1'].coordinates(), (5, 15, 0))
@@ -59,9 +61,30 @@ class ContainerTestCase(unittest.TestCase):
             wells=96,
             rows=8,
             spacing=(10, 15),
-            offset=(5, 15)
+            offset=(5, 15),
+            radius=5
         )
         deck.add(slot, 'B2', (100, 200, 0))
         slot.add(plate)
 
         self.assertEqual(plate['A1'].coordinates(deck), (105, 215, 0))
+
+    def test_well_from_center(self):
+        deck = Deck()
+        slot = Slot()
+        plate = self.generate_plate(
+            wells=4,
+            rows=2,
+            spacing=(10, 10),
+            offset=(0, 0),
+            radius=5
+        )
+        deck.add(slot, 'A1', (0, 0, 0))
+        slot.add(plate)
+
+        self.assertEqual(
+            plate['B2'].from_center(x=0.0, y=0.0, z=0.0),
+            (15, 15, 0))
+        self.assertEqual(
+            plate['B2'].from_center(r=1.0, theta=math.pi / 2, h=0.0),
+            (15.0, 10.0, 0))
