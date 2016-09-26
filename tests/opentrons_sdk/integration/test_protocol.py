a3c3c536d5ecc3c3bae1bb017cb3fed3b245479f
Ahmed Abdalla <ahmad19526@gmail.com>
2016-09-26 13:29:47 -0400

    fixes tests that were relying on ordered containers

--- tests/opentrons_sdk/integration/test_protocol.py
+++ tests/opentrons_sdk/integration/test_protocol.py
@@ -17,10 +17,9 @@ class ProtocolTestCase(unittest.TestCase):
 
         containers_list = self.protocol.containers()
         self.assertEqual(len(containers_list), 2)
-        print(containers_list)
 
-        self.assertEqual(containers_list[0], plate)
-        self.assertEqual(containers_list[1], tiprack)
+        self.assertTrue(plate in containers_list)
+        self.assertTrue(tiprack in containers_list)
 
     def test_protocol_head(self):
 
