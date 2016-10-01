3356a6dc1deb87e03bb29f3610c2f9bb150a5c31
Andy Sigler <andrewsigler1@gmail.com>
2016-10-01 11:10:30 -0400

    added script for interacting with API

--- tests/opentrons_sdk/integration/move_robot.py
+++ tests/opentrons_sdk/integration/move_robot.py
@@ -11,6 +11,10 @@ plate = containers.load('96-flat', 'A2')
 trash = containers.load('point', 'A1')
 tiprack = containers.load('tiprack-10ul', 'B2')
 
+# tipracks need a Well height equal to the tip length
+for tip in tiprack:
+    tip.properties['height'] = 80
+
 p200 = instruments.Pipette(
     trash_container=trash,
     tip_racks=[tiprack],
@@ -19,49 +23,57 @@ p200 = instruments.Pipette(
     channels=1
 )
 
-p200.calibrate_plunger(top=13, bottom=10, blow_out=11, drop_tip=12)
 p200.set_max_volume(200)
 
-robot.connect('/dev/tty.usbmodem1411')
-# robot.home('z')
-# robot.home()
-
-robot.move_head(b=32)
-p200.calibrate('drop_tip')
-
-robot.move_head(b=30)
-p200.calibrate('blow_out')
-
-robot.move_head(b=28.5)
-p200.calibrate('bottom')
-
-robot.move_head(b=13)
-p200.calibrate('top')
-
-# robot.move_head(z=10)
-# robot.move_head(x=113, y=15)
-# robot.move_head(z=2)
-
-# p200.calibrate_position((tiprack, tiprack[0].center(tiprack)))
+while not robot.connect('/dev/tty.usbmodem1411'):
+    print('attemping connect...')
 
 # robot.home('z')
 # robot.home()
 
-# p200.pick_up_tip(tiprack[0])
-# p200.drop_tip(tiprack[0])
 
-# robot.run()
+def calibrate_plunger():
+    robot.move_head(b=32)
+    p200.calibrate('drop_tip')
 
-# robot.move_head(z=10)
-# robot.move_head(x=20, y=-119.5)
-# robot.move_head(z=-19)
+    robot.move_head(b=30)
+    p200.calibrate('blow_out')
 
-# p200.calibrate_position((plate, plate[0].center(plate)))
+    robot.move_head(b=28.5)
+    p200.calibrate('bottom')
 
-# for well in plate[:-2:2]:
-#     p200.aspirate(10.1, well).dispense(10.1, next(well))
+    robot.move_head(b=13)
+    p200.calibrate('top')
 
-# for well in plate[:-2:2]:
-#     p200.aspirate(10.1, well).dispense(10.1, next(well))
 
-# robot.run()
+def setup_tiprack():
+
+    robot.move_head(z=90)
+    robot.move_head(x=112, y=15)
+    robot.move_head(z=4)
+
+    bottom_pos = tiprack[0].from_center(x=0, y=0, z=1, reference=tiprack)
+    p200.calibrate_position((tiprack, bottom_pos))
+
+
+def setup_plate():
+    robot.move_head(z=90)
+    robot.move_head(x=18, y=-119)
+    robot.move_head(z=-18)
+    p200.calibrate_position((plate, plate[0].center(plate)))
+
+
+def run_test():
+    p200.pick_up_tip(tiprack[0])
+    p200.drop_tip(tiprack[0])
+
+    p200.pick_up_tip(tiprack[4])
+    p200.drop_tip(tiprack[95])
+
+    robot.run()
+
+
+calibrate_plunger()
+setup_tiprack()
+setup_plate()
+run_test()
