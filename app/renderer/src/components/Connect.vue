e86ad0d586bcb08fdd399be81af7fd9105d2d8bb
Ahmed Abdalla <ahmad19526@gmail.com>
2016-10-06 15:48:16 -0400

    setting up vue connect submit

--- app/renderer/src/components/Connect.vue
+++ app/renderer/src/components/Connect.vue
@@ -2,14 +2,53 @@
   <div>
     <nav class="robot">
       <span>connect to robot</span>
-      <input type="text" id="connect_port" value="/dev/tty.usbmodem1421" />
-      <a href="#" class="btn-connect" id="connect_serial">Connect Serial</a>
+      <br>
+       {{ message }}
+      <br>
+      <select v-model="selected_port">
+        <option v-for="p in ports">{{ p }}</option>
+      </select>
+      the selected port is: {{ selected_port}}
+
+      <br>
+      <br>
+      <a @click="connectToRobot" class="btn-connect">Connect!</a>
     </nav>
   </div>
 </template>
 
 
 <script>
+  export default {
+    data:function () {
+      return {
+          message: "Let's connect",
+          selected_port: "",
+          ports: []
+      }
+    },
+    methods: {
+      getPortsList: function () {
+        this.ajaxRequest = true
+        this.$http.get('localhost:8000/ports/list/', function(data, status, request) {
+          console.log('we are getting data...')
+        })
+      },
+      connectToRobot: function() {
+          debugger
+        this.$http.get(
+          'localhost:8000/ports/connect/',
+          {port: this.selected_port},
+          function (data, status, request) {
+            console.log("Tried to connect", data, status, request)
+          })
+      }
+    },
+    beforeMount: function () {
+        this.ports = ["/dev/tty.usbmodem1421", 'B', 'C']
+    }
+
+  }
 </script>
 
 <style lang="sass">
--- app/renderer/src/main.js
+++ app/renderer/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
-import Home from './components/Home.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+import Home from './components/Home.vue'
 import StepList from './components/StepList.vue'
 import Upload from './components/Upload.vue'
 import Connect from './components/Connect.vue'
 
-Vue.use(VueRouter)
+
 Vue.component('StepList', StepList)
 Vue.component('Upload', Upload)
 Vue.component('Connect', Connect)
