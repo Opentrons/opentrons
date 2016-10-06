486bd163cba8ef6bdd5f481a927aea90e67baa77
Ahmed Abdalla <ahmad19526@gmail.com>
2016-10-06 14:55:09 -0400

    setting up Connect component

---
+++ app/renderer/src/components/Connect.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav class="robot">
+      <span>connect to robot</span>
+      <input type="text" id="connect_port" value="/dev/tty.usbmodem1421" />
+      <a href="#" class="btn-connect" id="connect_serial">Connect Serial</a>
+    </nav>
+  </div>
+</template>
+
+
+<script>
+</script>
+
+<style lang="sass">
+</style>
