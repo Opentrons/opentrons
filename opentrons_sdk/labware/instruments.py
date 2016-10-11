b54a5105da0df9755a53ecbb94a4d9fc23b5aeb4
andySigler <andrewsigler1@gmail.com>
2016-10-11 18:18:30 -0400

    removed 'relative' from pick_up_tip

--- opentrons_sdk/labware/instruments.py
+++ opentrons_sdk/labware/instruments.py
@@ -1,3 +1,4 @@
+from opentrons_sdk import containers
 from opentrons_sdk.robot.command import Command
 from opentrons_sdk.robot.robot import Robot
 from opentrons_sdk.containers.calibrator import Calibrator
@@ -208,10 +209,7 @@ class Pipette(object):
 
         return self
 
-    def pick_up_tip(self, location=None):
-
-        if location:
-            self.robot.move_to_bottom(location, instrument=self)
+    def pick_up_tip(self, location):
 
         # TODO: actual plunge depth for picking up a tip
         # varies based on the tip
@@ -219,14 +217,19 @@ class Pipette(object):
         # TODO: Need to talk about containers z positioning
         tip_plunge = 6
 
+        placeable, coordinates = containers.unpack_location(location)
+        if isinstance(location, Placeable):
+            coordinates = placeable.from_center(x=0, y=0, z=-1)
+        pressed_into_tip = coordinates + (0, 0, -tip_plunge)
+
+        self.robot.move_to((placeable, coordinates))
+        for _ in range(3):
+            self.go_to((placeable, pressed_into_tip))
+            self.go_to((placeable, coordinates))
+
         def _do():
             # Dip into tip and pull it up
-            for _ in range(3):
-                self.robot.move_head(z=-tip_plunge, mode='relative')
-                self.robot.move_head(z=tip_plunge, mode='relative')
-
-            self.plunger.wait_for_arrival()
-            self.robot.home('z')
+            pass
 
         description = "Picking up tip from {0}".format(str(location))
         self.robot.add_command(Command(do=_do, description=description))
