171027f84759819528acc1ffb638186e5c018963
Lev Kravinsky <kravinskylev@gmail.com>
2016-10-13 17:25:34 -0400

    add loop to render task data

--- app/renderer/src/components/StepList.vue
+++ app/renderer/src/components/StepList.vue
@@ -1,40 +1,40 @@
 <template>
   <ul>
-    <li :class="{'completed': task.completed}" v-for="task in tasks">
+    <li v-for="task in default_tasks">
       <router-link v-bind:to="task.href">{{task.title}}</router-link>
     </li>
+    <span v-for="instrument in tasks">
+      <li v-for="placeable in instrument.placeables">
+        {{$data.tasks}}
+        <router-link v-bind:to="'instrument/' + placeable.label">Calibrate {{placeable.label}}</router-link>
+      </li>
+    <span/>
   </ul>
 </template>
 
 <script>
-  function tasks() {
-      return [{
-          title: 'connect to robot',
-          completed: true,
-          href: '/connect'
-      }, {
-          title: 'upload a protocol',
-          completed: false,
-          href: '/upload'
-      }, {
-          title: 'calibrate instruments',
-          completed: false,
-          href: '/'
-      }, {
-          title: 'calibrate pipettes',
-          completed: false,
-          href: '/'
-      }, {
-          title: 'review calibration',
-          completed: false,
-          href: '/'
-      }]
-  }
-
   export default {
     name: 'StepList',
-    data() {
-      return { tasks: tasks() }
+    data: function() {
+      return {
+        default_tasks: [
+          {
+            title: 'connect to robot',
+            completed: false,
+            href: '/connect'
+          },
+          {
+            title: 'upload a protocol',
+            completed: false,
+            href: '/upload'
+          }
+        ]
+      }
+    },
+    computed: {
+      tasks() {
+        return this.$store.state.tasks
+      }
     }
   }
 </script>
