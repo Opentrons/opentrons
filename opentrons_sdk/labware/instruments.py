29f3932a08c0e37d77d3a1bbaa81d496d6ef8dab
andySigler <andrewsigler1@gmail.com>
2016-10-17 10:54:56 -0400

    cleaned up imports

--- opentrons_sdk/labware/instruments.py
+++ opentrons_sdk/labware/instruments.py
@@ -1,9 +1,2 @@
-from opentrons_sdk import containers
-from opentrons_sdk.robot.command import Command
-from opentrons_sdk.robot.robot import Robot
-from opentrons_sdk.containers.calibrator import Calibrator
-from opentrons_sdk.containers.placeable import Placeable
-from opentrons_sdk.util.vector import Vector
-
 from opentrons_sdk.labware.magbead import Magbead
 from opentrons_sdk.labware.pipette import Pipette
