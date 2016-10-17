e79a5322d62737ab858a1c15d5721bc00d8e12a7
andySigler <andrewsigler1@gmail.com>
2016-10-17 12:11:39 -0400

    fixed pylama errors

--- tests/opentrons_sdk/protocol/test_command.py
+++ tests/opentrons_sdk/protocol/test_command.py
@@ -1,4 +1,3 @@
-import threading
 import unittest
 
 from opentrons_sdk.robot.command import Command, Macro
@@ -6,31 +5,32 @@ from opentrons_sdk.robot.command import Command, Macro
 
 class CommandTest(unittest.TestCase):
 
-	def setUp(self):
-		pass
+    def setUp(self):
+        pass
 
-	def test_command(self):
-		expected = None
-		def _do():
-			nonlocal expected
-			expected = 'test'
-		description = 'test'
-		command = Command(do=_do, description=description)
-		command()
-		self.assertEquals(expected, command.description)
+    def test_command(self):
+        expected = None
 
-	def test_macro(self):
-		expected = []
+        def _do():
+            nonlocal expected
+            expected = 'test'
+        description = 'test'
+        command = Command(do=_do, description=description)
+        command()
+        self.assertEquals(expected, command.description)
 
-		macro = Macro('macro_test')
+    def test_macro(self):
+        expected = []
 
-		for i in range(3):
-			def _do():
-				nonlocal expected
-				expected.append('test')
-			command = Command(do=_do)
-			macro.add(command)
+        macro = Macro('macro_test')
 
-		macro.do()
+        for i in range(3):
+            def _do():
+                nonlocal expected
+                expected.append('test')
+            command = Command(do=_do)
+            macro.add(command)
 
-		self.assertEquals(expected, ['test', 'test', 'test'])
+        macro.do()
+
+        self.assertEquals(expected, ['test', 'test', 'test'])
