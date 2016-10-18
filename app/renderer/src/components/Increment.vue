ea16d7b67144dae743d01fbd72dd0c512b2e30ef
Kadee80 <kaa328@nyu.edu>
2016-10-18 14:42:59 -0400

    separated increment selection into separate component

---
+++ app/renderer/src/components/Increment.vue
@@ -0,0 +1,32 @@
+<template>
+
+  
+  <div class="increment xyz" >
+      <button   v-for="i in placeable_increments" @click="selectPlaceableIncrement(i)" :class="['btn-full btn-group', { 'active': active(i)} ]">{{i}}</button>
+  </div>
+  
+
+
+</template>
+
+<script>
+export default {
+  name: 'Increment',
+  computed: {
+    placeable_increments(){
+      return [20,10,5,1,0.5,0.1]
+    },
+    pipette_increments(){
+      return [5,1,0.5,0.1]
+    }
+  },
+  methods: {
+    active(i) {
+      return this.$store.state.current_increment_placeable === i
+    },
+    selectPlaceableIncrement(i){
+      this.$store.dispatch("selectPlaceableIncrement", i)
+    }
+  }
+}
+</script>
