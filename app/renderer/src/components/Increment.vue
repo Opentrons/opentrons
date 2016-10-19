0e24cf81e139e4039c103add08ef3a693ecae77b
Kadee80 <kaa328@nyu.edu>
2016-10-19 12:42:31 -0400

    slot buttons now render with proper style

--- app/renderer/src/components/Increment.vue
+++ app/renderer/src/components/Increment.vue
@@ -1,6 +1,8 @@
 <template>
   <span>
-    <button v-for="i in increments" @click="selectPlaceableIncrement(i)" :class="['btn-full btn-group', { 'active': active(i)} ]">{{i}}</button>
+    <button v-for="i in increments" @click="selectPlaceableIncrement(i)" :class="['btn-full btn-group', { 'active': active(i)} , { 'btn-slot': isSlot(i)}  ]">{{i}}</button>
+    <!-- <button @click="selectPlaceableIncrement(91)" :class="['btn-slot', { 'active': active(91)}]">&#8596; slot-x </button>
+    <button @click="selectPlaceableIncrement(135)" :class="['btn-slot', { 'active': active(135)}]">&#8597; slot-y </button> -->
   </span>
 </template>
 
@@ -8,14 +10,12 @@
 export default {
   name: 'Increment',
   props: ['increments'],
-  computed: { 
-    pipette_increments(){
-      return [5,1,0.5,0.1]
-    }
-  },
   methods: {
     active(i) {
       return this.$store.state.current_increment_placeable === i
+    },
+    isSlot(i) {
+      return i === 91 || i === 135
     },
     selectPlaceableIncrement(i){
       this.$store.dispatch("selectPlaceableIncrement", i)
--- app/renderer/src/components/Jog.vue
+++ app/renderer/src/components/Jog.vue
@@ -10,12 +10,14 @@
     <a href="#" data-axis="y" data-direction="pos" class="btn-full btn-y">Y</a>
     <a href="#" data-axis="z" data-direction="pos" class="btn-full btn-z">Z</a>
   </div>
-
   <h3 class="title">Select Increment [mm]</h3>
     <div class="increment" >
       <increment :increments="placeable_increments"></increment>
+      <increment :increments="slot_increments"></increment>
     </div>
   </div> 
+
+  
 </div><!-- End Move -->
   
 </template>
@@ -26,11 +28,19 @@ export default {
   name: 'Jog',
   data: function(){
     return {
-      placeable_increments: [20,10,5,1,0.5,0.1]
-    }
+      placeable_increments: [20,10,5,1,0.5,0.1],
+      pipette_increments: [5,1,0.5,0.1],
+      slot_increments: [91,135],
+
+    }   
   },
   components: {
     Increment
+  },
+  computed: {
+    current_increment_placeable(){
+      return this.$store.state.current_increment_placeable
+    }
   }
 
 }
