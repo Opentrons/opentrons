2d8237e3bfdb693be40656730d79d60c69126bc6
Kadee80 <kaa328@nyu.edu>
2016-10-25 14:01:38 -0400

    added static calibrate placeable component to receive intrument, placeable, and placeable type as props

---
+++ app/renderer/src/components/CalibratePlaceable.vue
@@ -0,0 +1,27 @@
+<template>
+
+<div class="calibration-modal mode-bottom">
+	<!-- Images based on container type. -->
+	<div class="well-img">
+		<!-- <img src="assets/img/well_top.png" class="top" />
+		<img src="assets/img/well_bottom.png" class="bottom" /> -->
+	</div>
+	<div class="update">
+		<a href="#" class="position bottom">Bottom</a>
+		<button class="btn-update save bottom" data-save="bottom">Save </button>
+		<button class="btn-update moveto">Move To </button>
+	</div>
+</div>
+<!-- End Calibration Modal -->
+	
+</template>
+
+<script>
+  export default {
+    name: 'CalibratePlaceable',
+    props: ['instrument', 'placeable', 'type'],
+    data: function () {
+      return {}
+    }
+  }
+</script>
