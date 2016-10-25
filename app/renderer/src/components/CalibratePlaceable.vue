7ece7faa96b490e7124a09e8beaf11a12e6f8004
Kadee80 <kaa328@nyu.edu>
2016-10-25 15:23:58 -0400

    calibrate placeable component toggles static img and button layouts

--- app/renderer/src/components/CalibratePlaceable.vue
+++ app/renderer/src/components/CalibratePlaceable.vue
@@ -1,15 +1,24 @@
 <template>
 
-<div class="calibration-modal mode-bottom">
+<div class="calibration-modal">
 	<!-- Images based on container type. -->
 	<div class="well-img">
-		<!-- <img src="assets/img/well_top.png" class="top" />
-		<img src="assets/img/well_bottom.png" class="bottom" /> -->
+		<img src="../assets/img/well_bottom.png" v-show="placeableType('default')" /> 
+		<img src="../assets/img/tiprack_top.png" v-show="placeableType('tiprack')"/>
+		<img src="../assets/img/point_top.png" v-show="placeableType('point')"/>
 	</div>
-	<div class="update">
-		<a href="#" class="position bottom">Bottom</a>
-		<button class="btn-update save bottom" data-save="bottom">Save </button>
+	<!--  -->
+	<div class="update bottom" v-show="placeableType('default') || placeableType('point')">
+		<span class="position bottom">Bottom</span>
+		<button class="btn-update save bottom" >Save </button>
 		<button class="btn-update moveto">Move To </button>
+	</div>
+
+	<div class="update top" v-show="placeableType('tiprack')">
+		<span class="position top">Tiprack</span>
+		<button class="btn-update save top" >Save </button>
+		<button class="btn-update moveto">Move To </button>
+		<button class="pick-tip">Pick Up Tip</button>
 	</div>
 </div>
 <!-- End Calibration Modal -->
@@ -20,8 +29,16 @@
   export default {
     name: 'CalibratePlaceable',
     props: ['instrument', 'placeable', 'type'],
-    data: function () {
-      return {}
+    methods: {
+			placeableType(type){
+				return this.type === type
+			}
+    },
+    computed: { 
+    	currentType(){
+    		return placeableType(this.type);
+    	}
     }
+
   }
 </script>
