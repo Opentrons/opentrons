576fcf9e2e452d0dbb178fa5402ba4ea9de23110
Kadee80 <kaa328@nyu.edu>
2016-10-28 13:33:05 -0400

    clicking plunger position titles toggles img and avaiable buttons

--- app/renderer/src/components/CalibrateInstrument.vue
+++ app/renderer/src/components/CalibrateInstrument.vue
@@ -1,25 +1,25 @@
 <template>
-	<div class="calibration-modal mode-droptip">
+	<div class="calibration-modal" v-bind:class="currentMode">
 		<div class="pipette-img">
-	<!-- 	  <img src="../assets/img/pipette_top.png" class="top" />
+		  <img src="../assets/img/pipette_top.png" class="top" />
 		  <img src="../assets/img/pipette_bottom.png" class="bottom" />
-		  <img src="../assets/img/pipette_blowout.png" class="blowout" /> -->
+		  <img src="../assets/img/pipette_blowout.png" class="blowout" />
 		  <img src="../assets/img/pipette_droptip.png" class="droptip" />
 		</div>
 		<div class="update">
-		  <span class="position top">Top</span>
+		  <a @click="toggleMode('top')"class="position top">Top</a>
 		  <button class="btn-update save top" @click="calibrateInstrument(instrument, 'top')">Save</button>
-		  <button @click="moveToPlungerPosition(instrument, 'top')" :class="[{'disabled': disabled(instrument, 'top')}, 'btn-update', 'moveto']">Move</button>
+		  <button @click="moveToPlungerPosition(instrument, 'top')" :class="[{'disabled': disabled(instrument, 'top')}, 'btn-update', 'moveto', 'top']">Move</button>
 		  <div class="spacer"></div>
-		  <span class="position bottom">Bottom</span>
+		  <a @click="toggleMode('bottom')" class="position bottom">Bottom</a>
 		  <button class="btn-update save bottom" @click="calibrateInstrument(instrument, 'bottom')">Save</button>
-		  <button @click="moveToPlungerPosition(instrument, 'bottom')" :class="[{'disabled': disabled(instrument, 'bottom')}, 'btn-update', 'moveto']">Move</button>
-		  <span class="position blowout">Blowout</span>
+		  <button @click="moveToPlungerPosition(instrument, 'bottom')" :class="[{'disabled': disabled(instrument, 'bottom')}, 'btn-update', 'moveto', 'bottom']">Move</button>
+		  <a @click="toggleMode('blowout')" class="position blowout">Blowout</a>
 		  <button class="btn-update save blowout" @click="calibrateInstrument(instrument, 'blow_out')">Save</button>
-		  <button @click="moveToPlungerPosition(instrument, 'blow_out')" :class="[{'disabled': disabled(instrument, 'blow_out')}, 'btn-update', 'moveto']">Move</button>
-		  <span class="position droptip">Drop Tip</span>
+		  <button @click="moveToPlungerPosition(instrument, 'blow_out')" :class="[{'disabled': disabled(instrument, 'blow_out')}, 'btn-update', 'moveto', 'blowout']">Move</button>
+		  <a @click="toggleMode('droptip')" class="position droptip">Drop Tip</a>
 		  <button class="btn-update save droptip" @click="calibrateInstrument(instrument, 'drop_tip')">Save</button>
-		  <button @click="moveToPlungerPosition(instrument, 'drop_tip')" :class="[{'disabled': disabled(instrument, 'drop_tip')}, 'btn-update', 'moveto']">Move</button>
+		  <button @click="moveToPlungerPosition(instrument, 'drop_tip')" :class="[{'disabled': disabled(instrument, 'drop_tip')}, 'btn-update', 'moveto', 'droptip']">Move</button>
 		  <button class="btn-update test pick-liquid">Pick Up</button>
 		  <button class="btn-update test eject-liquid">Eject</button>
 		</div>
@@ -30,6 +30,11 @@
   export default {
     name: 'CalibrateInstrument',
     props: ['instrument'],
+    data: function () {
+      return {
+        currentMode : 'droptip'
+      }
+    },
     methods: {
     	calibrateInstrument(instrument, position) {
     		let axis = instrument.axis
@@ -45,7 +50,10 @@
 				if (instrument[position] == undefined) {
 					return true
 				}
-			}
+			},
+      toggleMode(mode){
+        this.currentMode =  mode;
+      }
     }
   }
 </script>
