b388de2170521add4937014f8eae8f833fbd1df0
Lev Kravinsky <kravinskylev@gmail.com>
2016-10-28 12:53:35 -0400

    disable move_to button on uncalibrated positions

--- app/renderer/src/components/CalibrateInstrument.vue
+++ app/renderer/src/components/CalibrateInstrument.vue
@@ -9,17 +9,17 @@
 		<div class="update">
 		  <span class="position top">Top</span>
 		  <button class="btn-update save top" @click="calibrateInstrument(instrument, 'top')">Save</button>
-		  <button class="btn-update moveto " @click="moveToPlungerPosition(instrument, 'top')">Move</button>
+		  <button @click="moveToPlungerPosition(instrument, 'top')" :class="[{'disabled': disabled(instrument, 'top')}, 'btn-update', 'moveto']">Move</button>
 		  <div class="spacer"></div>
 		  <span class="position bottom">Bottom</span>
 		  <button class="btn-update save bottom" @click="calibrateInstrument(instrument, 'bottom')">Save</button>
-		  <button class="btn-update moveto " @click="moveToPlungerPosition(instrument, 'bottom')">Move</button>
+		  <button @click="moveToPlungerPosition(instrument, 'bottom')" :class="[{'disabled': disabled(instrument, 'bottom')}, 'btn-update', 'moveto']">Move</button>
 		  <span class="position blowout">Blowout</span>
 		  <button class="btn-update save blowout" @click="calibrateInstrument(instrument, 'blow_out')">Save</button>
-		  <button class="btn-update moveto" @click="moveToPlungerPosition(instrument, 'blow_out')">Move</button>
+		  <button @click="moveToPlungerPosition(instrument, 'blow_out')" :class="[{'disabled': disabled(instrument, 'blow_out')}, 'btn-update', 'moveto']">Move</button>
 		  <span class="position droptip">Drop Tip</span>
 		  <button class="btn-update save droptip" @click="calibrateInstrument(instrument, 'drop_tip')">Save</button>
-		  <button class="btn-update moveto " @click="moveToPlungerPosition(instrument, 'drop_tip')">Move</button>
+		  <button @click="moveToPlungerPosition(instrument, 'drop_tip')" :class="[{'disabled': disabled(instrument, 'drop_tip')}, 'btn-update', 'moveto']">Move</button>
 		  <button class="btn-update test pick-liquid">Pick Up</button>
 		  <button class="btn-update test eject-liquid">Eject</button>
 		</div>
@@ -30,19 +30,22 @@
   export default {
     name: 'CalibrateInstrument',
     props: ['instrument'],
-    data: function() {
-      return {}
-    },
     methods: {
-    	calibrateInstrument(instrument, position){
+    	calibrateInstrument(instrument, position) {
     		let axis = instrument.axis
     		this.$store.dispatch("calibrateInstrument", {axis: axis, position: position})
     	},
-      moveToPlungerPosition(instrument, position){
+      moveToPlungerPosition(instrument, position) {
+				// TODO - disable this if the class disabled is on the given position
         let axis = instrument.axis
         this.$store.dispatch("moveToPlungerPosition", {axis: axis, position: position})
-      }
-    },
-    computed: {}
+      },
+			disabled(instrument, position) {
+				console.log("***********", instrument[position])
+				if (instrument[position] == undefined) {
+					return true
+				}
+			}
+    }
   }
 </script>
