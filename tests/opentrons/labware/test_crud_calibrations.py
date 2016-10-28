e375d882e35e9d29827e8e65f06e91d9de870ce6
andySigler <andrewsigler1@gmail.com>
2016-10-28 12:16:05 -0400

    added Instrument.delete_calibration_data() method for testing

--- tests/opentrons/labware/test_crud_calibrations.py
+++ tests/opentrons/labware/test_crud_calibrations.py
@@ -44,3 +44,26 @@ class CrudCalibrationsTestCase(unittest.TestCase):
         self.assertTrue('plate' in actual)
         actual = self.p200.calibration_data['A1']['children']['plate']
         self.assertEquals(expected_delta, actual)
+
+    def test_delte_calibrations_data(self):
+
+        self.p200 = instruments.Pipette(name="p200", axis="b")
+
+        expected_delta = {
+            'delta': (1.0, 2.0, 3.0)
+        }
+
+        self.assertTrue('A1' in self.p200.calibration_data)
+        actual = self.p200.calibration_data['A1']['children']
+        self.assertTrue('plate' in actual)
+        actual = self.p200.calibration_data['A1']['children']['plate']
+        self.assertEquals(expected_delta, actual)
+
+        self.p200.delete_calibration_data()
+        self.assertDictEqual(self.p200.calibration_data, {})
+
+        self.p200 = instruments.Pipette(name="p200", axis="b")
+        self.assertDictEqual(self.p200.calibration_data, {})
+        self.assertDictEqual(self.p200.positions, {
+            'top': None, 'bottom': None, 'blow_out': None, 'drop_tip': None
+        })
