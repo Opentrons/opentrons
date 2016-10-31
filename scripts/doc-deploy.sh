4d95e7ce073b34565a7eb2bd71370b22837ec07b
astaff <artyom.astafurov@gmail.com>
2016-10-31 11:56:32 -0400

    updated build script

--- scripts/doc-deploy.sh
+++ scripts/doc-deploy.sh
@@ -15,9 +15,6 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Move to the home level
-cd ..
-
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
 git clone $REPO out
@@ -28,7 +25,7 @@ cd ..
 # Clean out existing contents
 rm -rf out/**/* || exit 0
 
-cp -r docs/build/html/* out/
+cp -r ../docs/build/html/* out/
 
 # Now let's go have some fun with the cloned repo
 cd out
@@ -36,7 +33,7 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
+if [ -z 'git diff --exit-code' ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
