c2e693670c9e64094e9dbdf139147a12c64ee7e6
astaff <artyom.astafurov@gmail.com>
2016-10-31 11:41:14 -0400

    updating deploy script

--- scripts/doc-deploy.sh
+++ scripts/doc-deploy.sh
@@ -18,12 +18,14 @@ SHA=`git rev-parse --verify HEAD`
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
 git clone $REPO out
-cd docs/build
+cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
 rm -rf out/**/* || exit 0
+
+cp -r docs/build/html/* out/
 
 # Now let's go have some fun with the cloned repo
 cd out
