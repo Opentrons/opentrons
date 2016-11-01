7e57fc8983041025e22439c9942a01a7f15f3463
astaff <artyom.astafurov@gmail.com>
2016-10-31 23:35:59 -0400

    switch doc build branch back to master

--- scripts/doc-deploy.sh
+++ scripts/doc-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="171-l-documentation-update"
+SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
