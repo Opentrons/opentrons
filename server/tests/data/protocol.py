8d6628e8850903e0c9b9a15c8d018ca74dd05c95
Lev Kravinsky <kravinskylev@gmail.com>
2016-11-02 10:35:37 -0400

    fix test protocol, sort diagnostics response

--- server/tests/data/protocol.py
+++ server/tests/data/protocol.py
@@ -3,53 +3,61 @@ from opentrons import instruments
 
 plate = containers.load(
     '96-flat',
-    'A2',
-    'plate'
-)
-
-plate2 = containers.load(
-    '96-flat',
-    'A3',
-    'plate2'
+    'B2',
+    'test-plate'
 )
 
 tiprack = containers.load(
-    'tiprack-10ul',
-    'A1',
-    'tiprack'
+    'tiprack-200ul',  # container type from library
+    'A1',             # slot on deck
+    'test-tiprack'
+)
+
+trough = containers.load(
+    'trough-12row',
+    'B1',
+    'test-trough'
 )
 
 trash = containers.load(
     'point',
-    'B2',
-    'trash'
+    'A2',
+    'test-trash'
 )
 
+p1000 = instruments.Pipette(
+    name="p1000",
+    trash_container=trash,
+    tip_racks=[tiprack],
+    min_volume=10,  # These are variable
+    axis="b",
+    channels=1
+)
 p10 = instruments.Pipette(
     name="p10",
     trash_container=trash,
     tip_racks=[tiprack],
-    min_volume=1,
-    axis="b",
-    channels=1
-)
-
-p100 = instruments.Pipette(
-    name="p100",
-    trash_container=trash,
-    tip_racks=[tiprack],
-    min_volume=1,
+    min_volume=1,  # These are variable
     axis="a",
     channels=1
 )
 
+p1000.delete_calibration_data()
+p10.delete_calibration_data()
+
+p1000.set_max_volume(200)
 p10.set_max_volume(10)
-p100.set_max_volume(50)
-p10.pick_up_tip()
-p10.aspirate(5, plate[0]).dispense(5, plate[1]).mix(5)
-p10.aspirate(5, plate[1]).dispense(5, plate[1]).mix(5)
-p10.aspirate(5, plate[2]).dispense(5, plate[1]).mix(5)
-p10.aspirate(5, plate[3]).dispense(5, plate[1]).mix(5)
-p100.aspirate(5, plate2[3]).dispense(5, plate[1]).mix(5)
-p10.aspirate(5, plate2[3]).dispense(5, plate[1]).mix(5)
-p10.drop_tip()
+
+p1000.pick_up_tip(tiprack[0])
+
+p1000.aspirate(10, trough[0])
+p1000.dispense(10, plate[0])
+
+p1000.drop_tip(trash)
+
+p10.pick_up_tip(tiprack[0])
+
+p10.aspirate(5, trough[0])
+p10.dispense(5, plate[0])
+
+p10.drop_tip(trash)
