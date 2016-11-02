1098362467533b593970232aee1ccfe996114881
Lev Kravinsky <kravinskylev@gmail.com>
2016-11-02 11:12:43 -0400

    fix unit test by adding store to vm

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,15 +1,17 @@
 import { expect } from 'chai'
 
 import Vue from 'vue'
+import store from '../../../app/renderer/src/store/store'
 import Home from '../../../app/renderer/src/components/Home.vue'
 
 describe('Home.vue', () => {
   it('should render home page with title', () => {
     const vm = new Vue({
+      store,
       ...Home
     }).$mount('#app')
+    console.log(vm)
     expect(vm.$el.querySelector('#app-title').textContent).to.equal('Logo')
     expect(1).to.equal(1)
   })
 })
-
