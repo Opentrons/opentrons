ca363f318c5e9cacf3f3f1c6494b65c35929eb61
astaff <artyom.astafurov@gmail.com>
2016-11-07 19:24:14 -0500

    initial setup

--- opentrons/__init__.py
+++ opentrons/__init__.py
@@ -2,3 +2,7 @@ from opentrons.robot.robot import Robot
 from opentrons.robot.command import Command
 
 __all__ = [Robot, Command]
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,25 +1,68 @@
+# Inspired by:
+# https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+import codecs
+import versioneer
+import os
 from setuptools import setup, find_packages
 
-config = {
-    'description':
-        "A suite of tools for portable automated scientific protocols.",
-    'author': "Opentrons",
-    'author_email': 'engineering@opentrons.com',
-    'url': 'http://opentrons.com',
-    'version': '2.0',
-    'packages': find_packages(exclude=["tests"]),
-    'install_requires': ['pyserial==3.1.1'],
-    'package_data': {
-        "opentrons": [
-            "config/containers/default-containers.json",
-        ]
-    },
-    'scripts': [
-        'bin/opentrons-compile'
-    ],
-    'name': 'opentrons',
-    'test_suite': 'nose.collector',
-    'zip_safe': False
-}
+VERSION = versioneer.get_version()
 
-setup(**config)
+DISTNAME = 'opentrons'
+LICENSE = 'Apache 2.0'
+AUTHOR = "Opentrons"
+EMAIL = "engineering@opentrons.com"
+URL = "https://github.com/OpenTrons/opentrons-api"
+DOWNLOAD_URL = ''
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Scientific/Engineering',
+]
+KEYWORDS = ["robots", "protocols", "synbio", "pcr", "automation", "lab"]
+DESCRIPTION = (
+    "The Opentrons API is a simple framework designed to make "
+    "writing automated biology lab protocols easy.")
+PACKAGES = find_packages(where='.', exclude=["tests.*", "tests"])
+INSTALL_REQUIRES = ['pyserial==3.1.1']
+TEST_SUITE = 'nose.collector'
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+print(PACKAGES)
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name=DISTNAME,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=EMAIL,
+        maintainer=AUTHOR,
+        maintainer_email=EMAIL,
+        keywords=KEYWORDS,
+        long_description=read("README.rst"),
+        packages=PACKAGES,
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        cmdclass=versioneer.get_cmdclass(),
+        include_package_data=True,
+        test_suite=TEST_SUITE
+    )
