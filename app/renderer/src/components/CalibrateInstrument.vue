bc7cfaae560ebe5d8d6e892dfcace238964108f3
Kadee80 <kaa328@nyu.edu>
2016-11-08 14:33:23 -0500

    added hover toggle logic for images

--- app/renderer/src/components/CalibrateInstrument.vue
+++ app/renderer/src/components/CalibrateInstrument.vue
@@ -1,33 +1,33 @@
 <template>
-	<section id="task-pipette">
+	<section id="task-pipette" >
 		<h1 class="title">Calibrate the p10 pipette</h1>
 		<span>
 		<div class="calibrate-pipette">
-			<section class="calibrate top">
+			<section class="calibrate top" @mouseenter="toggleMode('top')">
 				<span class="title">TOP</span>
 				<button @click="calibrateInstrument(currentAxis(), 'top')" class="btn-calibrate save">SAVE</button>
 				<button @click="moveToPlungerPosition(currentAxis(), 'top')" class="btn-calibrate move-to">MOVE TO</button>
 			</section>
 
-			<section class="calibrate bottom">
+			<section class="calibrate bottom" @mouseenter="toggleMode('bottom')">
 				<span class="title">BOTTOM</span>
 				<button @click="calibrateInstrument(currentAxis(), 'bottom')" class="btn-calibrate save">SAVE</button>
 				<button @click="moveToPlungerPosition(currentAxis(), 'bottom')" class="btn-calibrate move-to">MOVE TO</button>
 			</section>
 
-			<section class="calibrate blowout">
+			<section class="calibrate blowout" @mouseenter="toggleMode('blow-out')">
 				<span class="title">BLOWOUT</span>
 				<button @click="calibrateInstrument(currentAxis(), 'blow_out')" class="btn-calibrate save">SAVE</button>
 				<button @click="moveToPlungerPosition(currentAxis(), 'blow_out')" class="btn-calibrate move-to">MOVE TO</button>
 			</section>
 
-			<section class="calibrate drop-tip" @hover="currentPosition('drop-tip')">
+			<section class="calibrate drop-tip" @mouseenter="toggleMode('drop-tip')">
 				<span class="title">DROP TIP</span>
 				<button @click="calibrateInstrument(currentAxis(), 'drop_tip')" class="btn-calibrate save">SAVE</button>
 				<button @click="moveToPlungerPosition(currentAxis(), 'drop_tip')" class="btn-calibrate move-to">MOVE TO</button>
 			</section>
 		</div>
-		<div class="pipette-diagrams">
+		<div class="pipette-diagrams" v-bind:class="currentMode">
             <img src="../assets/img/pipette_top.png"/>
         </div>
 		</span>
@@ -41,13 +41,19 @@
 <script>
   export default {
     name: 'CalibrateInstrument',
+    data: function() {
+      return {
+        currentMode : 'drop-tip'
+      }
+    },
     methods: {
 			currentAxis() {
 				return this.$route.params.instrument
 			},
-			currentPosition(pos){
-
-			},
+			toggleMode(mode){
+        this.currentMode = mode
+        console.log(mode)
+      },
     	calibrateInstrument(axis, position) {
     		this.$store.dispatch("calibrate", {axis, position})
     	},
