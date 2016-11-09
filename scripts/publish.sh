5d7de1d88a835621c422c7d4bc12d72cfbf96c19
astaff <artyom.astafurov@gmail.com>
2016-11-08 20:52:33 -0500

    moved conda setup to publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-python setup.py sdist bdist_wheel && twine upload dist/*
+# python setup.py sdist bdist_wheel && twine upload dist/*
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda install anaconda-client conda-build
+
 conda config --set anaconda_upload yes
-anaconda login --username opentrons --password '$ANACONDA_PASSWORD'
+anaconda login --username opentrons --password $ANACONDA_PASSWORD
 conda build conda.recipe
