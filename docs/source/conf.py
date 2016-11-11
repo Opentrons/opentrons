963f4a988478735a87517b7d3413215c1d83def1
Kadee80 <kaa328@nyu.edu>
2016-11-11 12:03:35 -0500

    added brand fonts and _static styles

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -135,7 +135,12 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'travis_button': 'True',
+    'font_family': 'Open Sans',
+    'head_font_family': 'AkkoPro-Regular',
+    'sidebar_collapse': 'True'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
