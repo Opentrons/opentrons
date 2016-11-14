c390ef87fd3f634cfb0db631b14ce52e409d7c25
Artyom Astafurov <artyom.astafurov@gmail.com>
2016-11-13 19:32:41 -0500

    Removed checking for changes for now
    Will commit anyway

--- scripts/doc-deploy.sh
+++ scripts/doc-deploy.sh
@@ -34,17 +34,19 @@ cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z $(git diff) ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+git add CNAME
+
+# TODO revisit bash script for checking of there are changes to commit
+# # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+# if [ -z $(git diff) ]; then
+#     echo "No changes to the output on this push; exiting."
+#     exit 0
+# fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A .
 git add .nojekyll
-git add CNAME
 git commit -m "Deploy to GitHub Pages: ${SHA} [ci skip]"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
