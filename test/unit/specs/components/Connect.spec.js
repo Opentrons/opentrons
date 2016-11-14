304624c78d3da9ba9e92f28f76df4f50079914bd
Ahmed Abdalla <ahmad19526@gmail.com>
2016-11-14 12:31:59 -0500

    downgrade inject loader

--- test/unit/specs/components/Connect.spec.js
+++ test/unit/specs/components/Connect.spec.js
@@ -8,18 +8,19 @@ import ConnectComponent from '../../../../app/renderer/src/components/Connect.vu
 const ConnectInjector = require('!!vue?inject!../../../../app/renderer/src/components/Connect.vue')
 const ConnectMock = ConnectInjector({
   '../rest_api_wrapper': {
-    OpenTrons: function () {
+    OpenTrons: (function () {
+      return
       console.log('INJECT MOCK CALLED...')
-    }
+    })()
   }
 })
 
 
 describe('Connect Component', () => {
   // TODO: Figure out how to get inject-loader to work...
-  // it('renders with drop down', () => {
-  //    const vm = new Vue(ConnectMock).$mount()
-  // })
+  it('renders with drop down', () => {
+     const vm = new Vue(ConnectMock).$mount()
+  })
 
   it('has methods for business logic', () => {
     expect(typeof ConnectComponent.methods.getPortsList).be.a.function
