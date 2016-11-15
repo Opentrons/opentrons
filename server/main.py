be6d1d546253e47dab6ab443d9359e827a222ede
andySigler <andrewsigler1@gmail.com>
2016-11-15 09:33:40 -0500

    saving all containers to variable

--- server/main.py
+++ server/main.py
@@ -535,8 +535,9 @@ def update_step_list():
                 'calibrated': _check_if_instrument_calibrated(instrument)
             })
 
+            all_containers = _get_all_containers()
             for placeable_step in step['placeables']:
-                for c in _get_all_containers():
+                for c in all_containers:
                     if c.get_name() == placeable_step['label']:
                         placeable_step.update({
                             'calibrated': _check_if_calibrated(instrument, c)
