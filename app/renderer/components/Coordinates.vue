65db1373f718c5702451917a988a242c977d4586
Ahmed Abdalla <ahmad19526@gmail.com>
2016-11-21 17:37:06 -0500

    fixes indendations

--- app/renderer/components/Coordinates.vue
+++ app/renderer/components/Coordinates.vue
@@ -1,21 +1,21 @@
 <template>
 	<div class="coords">
 		<label class="label">X: </label>
-	  <input type="text" v-bind:value="coords.x" />
-	  <label class="label">Y: </label>
-	  <input type="text" v-bind:value="coords.y" />
-	  <label class="label">Z: </label>
-	  <input type="text" v-bind:value="coords.z" />
-	  <label class="label" v-if="instrument">{{axis}}: </label>
+		<input type="text" v-bind:value="coords.x" />
+		<label class="label">Y: </label>
+		<input type="text" v-bind:value="coords.y" />
+		<label class="label">Z: </label>
+		<input type="text" v-bind:value="coords.z" />
+		<label class="label" v-if="instrument">{{axis}}: </label>
 		<input type="text" v-bind:value="coords[axis.toLowerCase()]" v-if="instrument"/>
 	</div>
 </template>
 
 <script>
 	export default {
-	  name: 'DeckSlot',
-	  props: ['placeable', 'instrument', 'axis'],
-	  computed: {
+		name: 'DeckSlot',
+		props: ['placeable', 'instrument', 'axis'],
+		computed: {
 			coords() {
 				let coords = this.$store.state.coordinates
 				for (var coord in coords) {
@@ -23,6 +23,6 @@
 				}
 				return coords
 			}
-	  }
+		}
 	}
 </script>
