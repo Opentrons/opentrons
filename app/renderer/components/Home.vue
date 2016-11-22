87da354f53f70bdb38bd4daa77e58abd00130872
Lev Kravinsky <kravinskylev@gmail.com>
2016-11-22 16:42:37 -0500

    fix quotation bugs

--- app/renderer/components/Home.vue
+++ app/renderer/components/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <nav class='home' :class='{'disabled': busy}'>
+  <nav :class="[{'disabled': busy}, 'home']">
     <span class='label'>HOME: </span>
-    <span @click='home('all')' class='btn-home'>ALL</span>
-    <span @click='home('x')' class='btn-home'>X</span>
-    <span @click='home('y')' class='btn-home'>Y</span>
-    <span @click='home('z')' class='btn-home'>Z</span>
-    <span @click='home('b')' class='btn-home'>B</span>
-    <span @click='home('a')' class='btn-home'>A</span>
+    <span @click="home('all')" class='btn-home'>ALL</span>
+    <span @click="home('x')" class='btn-home'>X</span>
+    <span @click="home('y')" class='btn-home'>Y</span>
+    <span @click="home('z')" class='btn-home'>Z</span>
+    <span @click="home('b')" class='btn-home'>B</span>
+    <span @click="home('a')" class='btn-home'>A</span>
   </nav>
 </template>
 
@@ -16,7 +16,6 @@
     props: ['busy'],
     methods: {
       home (axis) {
-        console.log(axis)
         this.$store.dispatch('home', {axis: axis})
       }
     }
