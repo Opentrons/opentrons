d96cdb333369764a838dd68e651c3811550cdb40
Lev Kravinsky <kravinskylev@gmail.com>
2016-11-29 12:10:41 -0500

    fix indentation errors, allow global return

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
+    'indent': ['error', 2],
+    'globalReturn': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   },
