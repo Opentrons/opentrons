398be5fadbabd8141db7f49c412d76f55ae15746
Ahmed Abdalla <ahmad19526@gmail.com>
2016-11-29 16:11:25 -0500

    adds tests for configured robot pickle

--- tests/opentrons/protocol/test_pickling_robot.py
+++ tests/opentrons/protocol/test_pickling_robot.py
@@ -15,16 +15,18 @@ class PicklingRobotTestCase(unittest.TestCase):
         self.assertIsInstance(robot_as_bytes, bytes)
         reconstructed_robot = pickle.loads(robot_as_bytes)
 
-    # def test_pickling_configured_robot(self):
-    #     plate = containers.load('96-flat', 'A1')
-    #     p200 = instruments.Pipette(axis='b', max_volume=200)
-    #
-    #     for well in plate:
-    #         p200.aspirate(well).delay(5).dispense(well)
-    #
-    #     cmd_cnts = len(self.robot.commands())
-    #     robot_as_bytes = self.robot.pickle()
-    #     self.assertIsInstance(robot_as_bytes, bytes)
-    #     reconstructed_robot = pickle.loads(robot_as_bytes)
-    #
-    #     reconstructed_robot_cmd_cnts = len(reconstructed_robot.commands())
+    def test_pickling_configured_robot(self):
+        plate = containers.load('96-flat', 'A1')
+        p200 = instruments.Pipette(axis='b', max_volume=200)
+
+        for well in plate:
+            p200.aspirate(well).delay(5).dispense(well)
+
+        cmd_cnts = len(self.robot.commands())
+        print(self.robot._driver)
+        robot_as_bytes = self.robot.pickle()
+        print(self.robot._driver)
+        self.assertIsInstance(robot_as_bytes, bytes)
+        reconstructed_robot = pickle.loads(robot_as_bytes)
+
+        reconstructed_robot_cmd_cnts = len(reconstructed_robot.commands())
