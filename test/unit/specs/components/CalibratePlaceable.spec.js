0fb3f8b8c3cf0043b7dccae5063cadb5c897f23c
Lev Kravinsky <kravinskylev@gmail.com>
2016-12-01 14:41:37 -0500

    add tests for disabling buttons and actions

--- test/unit/specs/components/CalibratePlaceable.spec.js
+++ test/unit/specs/components/CalibratePlaceable.spec.js
@@ -2,25 +2,30 @@
 import { expect } from 'chai'
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import sinon from 'sinon'
+import sinon from 'sinon'
 import CalibratePlaceable from 'renderer/components/CalibratePlaceable.vue'
 
 Vue.use(Vuex)
 
-// function getMockStore () {
-//   return {
-//     actions: {
-//       pickUpTip: sinon.spy(),
-//       dropTip: sinon.spy(),
-//       moveToPosition: sinon.spy(),
-//       calibrate: sinon.spy()
-//     }
-//   }
-// }
+function getMockStore () {
+  return {
+    actions: {
+      pickUpTip: sinon.spy(),
+      dropTip: sinon.spy(),
+      moveToPosition: sinon.spy(),
+      calibrate: sinon.spy()
+    }
+  }
+}
+
+const mockStore = getMockStore()
 
 function getRenderedVm (Component, propsData) {
   const Ctor = Vue.extend(Component)
-  const vm = new Ctor({ propsData }).$mount()
+  const vm = new Ctor({
+    propsData,
+    store: new Vuex.Store(mockStore)
+  }).$mount()
   return vm
 }
 
@@ -28,7 +33,7 @@ let placeable = {
   'slot': 'A1',
   'label': 'tiprack-12ml',
   'sanitizedType': 'tiprack',
-  'calibrated': false
+  'calibrated': true
 }
 let instrument = {'axis': 'a'}
 
@@ -41,12 +46,46 @@ describe('CalibratePlaceable.vue', (done) => {
   })
 
   it('does not render tip buttons if placeable is not tiprack', () => {
-    let plate = placeable
+    let plate = JSON.parse(JSON.stringify(placeable))
     plate['sanitizedType'] = 'default'
 
     expect(getRenderedVm(CalibratePlaceable, {
       placeable: plate,
       instrument
     }).$el.querySelectorAll('button').length).to.equal(2)
+  })
+
+  it('disables all buttons except for save when not calibrated', () => {
+    let uncalibratedPlate = JSON.parse(JSON.stringify(placeable))
+    uncalibratedPlate['calibrated'] = false
+
+    expect(getRenderedVm(CalibratePlaceable, {
+      placeable: uncalibratedPlate,
+      instrument
+    }).$el.querySelectorAll('button.disabled').length).to.equal(3)
+  })
+
+  it('enables all buttons when calibrated', () => {
+    expect(getRenderedVm(CalibratePlaceable, {
+      placeable,
+      instrument
+    }).$el.querySelectorAll('button.disabled').length).to.equal(0)
+  })
+
+  it('calls the correct actions for each button click', () => {
+    let renderedCalibratePlaceable = getRenderedVm(CalibratePlaceable, {
+      placeable,
+      instrument
+    })
+
+    renderedCalibratePlaceable.calibratePlaceable()
+    renderedCalibratePlaceable.moveToPlaceable()
+    renderedCalibratePlaceable.pickUpTip()
+    renderedCalibratePlaceable.dropTip()
+
+    expect(mockStore.actions.calibrate.called).to.be.true
+    expect(mockStore.actions.moveToPosition.called).to.be.true
+    expect(mockStore.actions.pickUpTip.called).to.be.true
+    expect(mockStore.actions.dropTip.called).to.be.true
   })
 })
