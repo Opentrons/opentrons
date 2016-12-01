e9d9263ce212c367cde8910ddf84add4fc0c8071
Lev Kravinsky <kravinskylev@gmail.com>
2016-12-01 13:44:02 -0500

    fixed test based on docs

--- test/unit/specs/components/CalibratePlaceable.spec.js
+++ test/unit/specs/components/CalibratePlaceable.spec.js
@@ -1,57 +1,53 @@
+/* global describe, it */
 import { expect } from 'chai'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import sinon from 'sinon'
+// import sinon from 'sinon'
+import CalibratePlaceable from 'renderer/components/CalibratePlaceable.vue'
+// const CalibratePlaceableInjector = require('!!vue?inject!renderer/components/CalibratePlaceable.vue')
 
 Vue.use(Vuex)
 
-const CalibratePlaceableInjector = require('!!vue?inject!renderer/components/CalibratePlaceable.vue')
-const CalibratePlaceable = CalibratePlaceableInjector({
-  '../rest_api_wrapper': {
-    getPortsList: function () {
-      return {
-        then: function () {
-          return 'hi'
-        }
-      }
-    }
-  }
-})
-
-function getMockStore () {
-  return {
-    state: {
-      is_connected: false,
-      port: null
-    },
-    actions: {
-      connect_robot: sinon.spy(),
-      disconnect_robot: sinon.spy()
-    }
-  }
-}
+// function getMockStore () {
+//   return {
+//     actions: {
+//       pickUpTip: sinon.spy(),
+//       dropTip: sinon.spy(),
+//       moveToPosition: sinon.spy(),
+//       calibrate: sinon.spy()
+//     }
+//   }
+// }
 
 describe('CalibratePlaceable.vue', (done) => {
   it('renders with tiprack based on prop', () => {
-    let mockStore = getMockStore()
+    // let mockStore = getMockStore()
     let placeable = {
       'slot': 'A1',
       'label': 'tiprack-12ml',
-      'sanitizedType': 'tiprack'
+      'sanitizedType': 'tiprack',
+      'calibrated': false
     }
     let instrument = {'axis': 'a'}
-    const vm = new Vue({
-      store: new Vuex.Store(mockStore),
-      el: document.createElement('div'),
-      render: h => h(CalibratePlaceable),
-      props: {placeable, instrument}
-    }).$mount()
-    console.log('************************')
-    console.log(vm)
-    console.log('************************')
-    Vue.nextTick(() => {
-      expect(vm.$el.querySelector('span').length).to.equal(1)
-      done()
-    })
+
+    function getRenderedText (Component, propsData) {
+      const Ctor = Vue.extend(Component)
+      const vm = new Ctor({ propsData }).$mount()
+      return vm.$el.textContent
+    }
+
+    // const vm = new Vue({
+    //   // store: new Vuex.Store(mockStore),
+    //   el: document.createElement('div'),
+    //   render: h => h(CalibratePlaceable)
+    // }).$mount()
+
+    // console.log(vm)
+
+    // expect(vm.$el.querySelector('button').length).to.equal('foo')
+    expect(getRenderedText(CalibratePlaceable, {
+      placeable,
+      instrument
+    })).to.equal('Hello')
   })
 })
