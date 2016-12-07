16be7e37180811e163978feace7169ef91c12f8a
Lev Kravinsky <kravinskylev@gmail.com>
2016-12-07 15:33:50 -0500

    finish test for progress bar

--- test/unit/specs/components/ProgressBar.spec.js
+++ test/unit/specs/components/ProgressBar.spec.js
@@ -22,23 +22,37 @@ function getMockStore () {
 
 const mockStore = getMockStore()
 
-function getRenderedVm (Component, propsData) {
+function getRenderedVm (Component, propsData, store) {
   const Ctor = Vue.extend(Component)
   return new Ctor({
     propsData,
-    store: new Vuex.Store(mockStore)
+    store: new Vuex.Store(store)
   }).$mount()
 }
 
 const propsData = { increments: [1, 2, 5, 10] }
-const progressBar = getRenderedVm(ProgressBar, propsData)
+const progressBar = getRenderedVm(ProgressBar, propsData, mockStore)
 
 describe('ProgressBar.vue', (done) => {
   it('determines run percent correctly, leaving out notifications', () => {
     expect(progressBar.runPercent()).to.equal(33)
   })
-})
 
-// check run percent
-// percentClass
-// running
+  it('determines percent class correctly', () => {
+    expect(progressBar.percentClass()).to.equal('width:33%;')
+  })
+
+  it('determines whether it is running correctly', () => {
+    expect(progressBar.running()).to.be.true
+
+    let finishedStore = getMockStore()
+    finishedStore.state.protocolFinished = false
+    const newProgressBar = getRenderedVm(ProgressBar, propsData, finishedStore)
+    expect(newProgressBar.running()).to.be.false
+
+    let unfinishedStore = getMockStore()
+    unfinishedStore.state.running = true
+    const unfinishedBar = getRenderedVm(ProgressBar, propsData, unfinishedStore)
+    expect(unfinishedBar.running()).to.be.true
+  })
+})
