2936935d590091525257448e6a300336026671b6
andySigler <andrewsigler1@gmail.com>
2017-02-08 12:59:44 -0500

    write created containers to file if name is specified

--- opentrons/containers/__init__.py
+++ opentrons/containers/__init__.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from opentrons.containers.persisted_containers import get_persisted_container
 from opentrons.containers.persisted_containers import list_container_names
 from opentrons.containers.placeable import (
@@ -9,6 +12,7 @@ from opentrons.containers.placeable import (
     unpack_location
 )
 from opentrons.containers.calibrator import apply_calibration
+from opentrons.util import environment
 
 __all__ = [
     get_persisted_container,
@@ -44,22 +48,54 @@ def list():
     return list_container_names()
 
 
-def create(slot, grid, spacing, diameter, depth, name=None):
+def create(slot, grid, spacing, diameter, depth, volume=0, name=None):
     columns, rows = grid
     col_spacing, row_spacing = spacing
     custom_container = Container()
     properties = {
         'type': 'custom',
         'radius': diameter / 2,
-        'height': depth
+        'height': depth,
+        'total-liquid-volume': volume
     }
 
     for r in range(rows):
         for c in range(columns):
             well = Well(properties=properties)
-            name = chr(c + ord('A')) + str(1 + r)
+            well_name = chr(c + ord('A')) + str(1 + r)
             coordinates = (c * col_spacing, r * row_spacing, 0)
-            custom_container.add(well, name, coordinates)
+            custom_container.add(well, well_name, coordinates)
     from opentrons import Robot
+    if name is None:
+        name = 'custom_{0}x{1}'.format(columns, rows)
+    else:
+        json_container = container_to_json(c, name)
+        update_container_create_file(json_container)
     Robot.get_instance().deck[slot].add(custom_container, name)
     return custom_container
+
+
+def container_to_json(c, name):
+    locations = {}
+    for w in c:
+        x, y, z = w.coordinates()
+        locations[w.get_name()] = {
+            'x': x, 'y': y, 'z': z,
+            'depth': w.z_size(),
+            'diameter': w.x_size(),
+            'total-liquid-volume': 0
+        }
+    return {name: {'locations': locations}}
+
+
+def update_container_create_file(data):
+    container_file_path = environment.get_path('CONTAINERS_FILE')
+    if not os.path.isfile(container_file_path):
+        with open(container_file_path, 'w') as f:
+            f.write(json.dumps({'containers': {}}))
+    with open(container_file_path, 'r+') as f:
+        old_data = json.load(f)
+        old_data['containers'].update(data)
+        f.seek(0)
+        f.write(json.dumps(old_data))
+        f.truncate()
