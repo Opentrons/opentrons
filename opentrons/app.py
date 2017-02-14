b73092c78ffb3aab787131ca3b19b72d33dab529
simplyahmazing <ahmad19526@gmail.com>
2017-02-14 13:21:44 -0500

    commit changes

--- opentrons/app.py
+++ opentrons/app.py
@@ -1,5 +1,16 @@
+import threading
+
 from opentrons.server.main import run
 
 
+app_thread = None
+
 def start():
-    run()
+    global app_thread
+    app_thread = threading.Thread(target=run, args=())
+    app_thread.start()
+
+
+def stop():
+    import pdb; pdb.set_trace()
+    app_thread.stop()
