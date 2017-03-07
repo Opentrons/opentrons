72f58052d36ee8aa94440a2c282f4881825427ca
simplyahmazing <ahmad19526@gmail.com>
2017-03-07 16:25:24 -0500

    creates dummy main entry point

--- app/scripts/build_pyinstaller.py
+++ app/scripts/build_pyinstaller.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 
-import functools
-import glob
-import re
 import os
 import shutil
 import platform
 import subprocess
 
-from boto.s3.connection import S3Connection
-from boto.s3.key import Key
 
 import util
 
@@ -22,6 +17,95 @@ script_tab = "                       "
 project_root_dir = \
     os.path.dirname(                                  # going up 1 level
         os.path.dirname(os.path.realpath(__file__)))  # folder dir of this
+
+exec_folder_name = os.path.join(project_root_dir, "app", "backend-dist")
+
+PYINSTALLER_DISTPATH = os.path.join(project_root_dir, "pyinstaller-dist")
+PYINSTALLER_WORKPATH = os.path.join(project_root_dir, "pyinstaller-build")
+
+# verbose_print = print if verbose else lambda *a, **k: None
+
+
+def remove_directory(dir_to_remove):
+    """ :param dir_to_remove: Directory to remove. """
+    if os.path.exists(dir_to_remove):
+        print(script_tab + "Removing directory %s" % dir_to_remove)
+        shutil.rmtree(dir_to_remove)
+    else:
+        print(script_tab + "Directory %s was not found." % dir_to_remove)
+
+
+def get_spec_coll_name():
+    os_type = util.get_os()
+    if os_type == 'win':
+        return "otone_server.exe"
+    elif os_type in ['mac', 'linux']:
+        return "otone_server"
+    raise SystemExit(
+        'Unable to determine pyinstaller.spec COLL name for OS: {}'.format(
+            os_type
+        )
+    )
+
+
+def remove_pyinstaller_temps():
+    """
+    Removes the temporary folders created by PyInstaller (dist and build).
+    """
+    remove_directory(PYINSTALLER_WORKPATH)
+    remove_directory(PYINSTALLER_DISTPATH)
+
+
+def pyinstaller_build():
+    """
+    Launches a subprocess running Python PyInstaller with the spec file from the
+    package folder. Captures the output streams and checks for errors.
+    :return: Boolean indicating the success state of the operation.
+    """
+
+    process_args = [
+        shutil.which("pyinstaller"),
+        "{}".format(os.path.join("scripts", "pyinstaller.spec")),
+        "--workpath", PYINSTALLER_WORKPATH,
+        "--distpath", PYINSTALLER_DISTPATH
+    ]
+    print(script_tab + "Command: %s" % process_args)
+
+    if platform.system().lower() == "windows":
+        pyinstaller_process = subprocess.Popen(process_args, shell=True)
+    else:
+        pyinstaller_process = subprocess.Popen(process_args)
+    std_op, std_err_op = pyinstaller_process.communicate()
+
+    if pyinstaller_process.returncode != 0:
+        print(script_tab + "ERROR: PyInstaller returned with exit code: %s" %
+              pyinstaller_process.returncode)
+        print(script_tab + "PyInstaller STD OUT: {}".format(std_op))
+        print(script_tab + "PyInstaller STD ERR: {}".format(std_err_op))
+        return False
+
+    return True
+
+
+def move_executable_folder(final_exec_dir):
+    """
+    Moves the PyInstaller executable folder from dist to project root.
+    :return: Boolean indicating the success state of the operation.
+    """
+
+    original_exec_dir = os.path.join(
+        PYINSTALLER_DISTPATH, get_spec_coll_name()
+    )
+
+    if os.path.exists(original_exec_dir):
+        print(script_tab + "Moving exec files from %s \n" % original_exec_dir +
+              script_tab + "to %s" % final_exec_dir)
+        shutil.move(original_exec_dir, final_exec_dir)
+    else:
+        print(script_tab + "ERROR: PyInstaller executable output folder '%s' " %
+              original_exec_dir + "not found!")
+        return False
+    return True
 
 
 def generate_static_assets():
@@ -42,46 +126,7 @@ def generate_static_assets():
     return True
 
 
-def upload_to_s3(s3_connection, bucket, key, file_path):
-    """
-    Given an S3 connection object, 'file_path' to the specified 'bucket'
-    at 'key'
-    """
-    bucket_obj = s3_connection.get_bucket(bucket)
-    key_obj = Key(bucket_obj)
-    key_obj.key = key
-    headers = {'Cache-Control':'max-age=61536000'}
-    key_obj.set_contents_from_filename(file_path, headers=headers)
-    key_obj.set_acl('public-read')
-    print(
-        script_tab + "Uploaded {} to {}/{} on S3".format(
-            file_path, bucket, key
-        )
-    )
-
-
-def get_current_branch():
-    """Returns current Travis branch name or branch name of local repo
-    """
-    res = subprocess.check_output(["git", "branch"])
-    # Parses branch name where branch name is a line that starts with an '*'
-    branch = re.search(r'\*\s(.*)\s', res.decode()).group(1)
-    return '{branch}-{username}'.format(
-        branch=branch,
-        username=os.environ.get('USER')
-    )
-
-
-def get_current_commit():
-    """Returns current Travis commit or commit of HEAD ref
-    """
-    return os.environ.get(
-        'TRAVIS_COMMIT',
-        subprocess.check_output(["git", "rev-parse", "HEAD"]).decode()
-    )
-
-
-def build_ot_app_assets():
+def build_ot_python_backend_executable():
     print(script_tag + "Build procedure started.")
     print(script_tag + "Checking for OS.")
     os_type = util.get_os()
@@ -95,31 +140,36 @@ def build_ot_app_assets():
     if not success:
         raise SystemExit(script_tab + "Exiting due to error generating static assets")
 
-    print(script_tag + "Uploading app assets to S3")
-    # assets_dir = os.path.join(project_root_dir, 'release-assets', '*')
-    assets_dir_glob = os.path.join(project_root_dir, 'release-assets', '*')
-    branch = os.environ.get('TRAVIS_BRANCH', get_current_branch())
-    s3_connection = S3Connection(
-        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
-        aws_secret_access_key=os.environ.get('AWS_SECRET_KEY')
-    )
-    upload_build_asset = functools.partial(
-        upload_to_s3, s3_connection, 'ot-app-builds'
-    )
-    def upload(asset_path):
-        """
-        Uploads an asset_path, e.g. 'release-assets/foo.js
-        To S3 in 'ot-app-buids/assets/{branch}/foo.js'
-        """
-        file_name = os.path.basename(asset_path)
-        s3_key = 'assets/{branch}/{file_name}'.format(
-            branch=branch,
-            file_name=file_name
-        )
-        upload_build_asset(s3_key, asset_path)
 
-    list(map(upload, glob.glob(assets_dir_glob)))
+    print(script_tag + "Removing PyInstaller old temp directories.")
+    remove_pyinstaller_temps()
 
+    print(script_tag + "Running PyInstaller process.")
+    success = pyinstaller_build()
+
+    if not success:
+        print(script_tab + "Removing PyInstaller recent temp directories.")
+        remove_pyinstaller_temps()
+        raise SystemExit(script_tab + "Exiting as there was an error in the "
+                                      "PyInstaller execution.")
+
+    print(script_tag + "Removing old OT-App Backend executable directory.")
+    backend_exec_path = os.path.join(
+        exec_folder_name, util.get_os(), get_spec_coll_name()
+    )
+    if os.path.isfile(backend_exec_path):
+        os.remove(backend_exec_path)
+
+    print(script_tag + "Moving executable folder to backend-dist.")
+    success = move_executable_folder(backend_exec_path)
+    if not success:
+        print(script_tab + "Removing PyInstaller recent temp directories.")
+        remove_pyinstaller_temps()
+        raise SystemExit(script_tab + "Exiting now as there was an error in "
+                                      "the PyInstaller execution.")
+
+    print(script_tag + "Removing PyInstaller recent temp directories.")
+    remove_pyinstaller_temps()
 
 if __name__ == "__main__":
-    build_ot_app_assets()
+    build_ot_python_backend_executable()
---
+++ app/server/main.py
@@ -0,0 +1,5 @@
+from opentrons import server
+
+
+if __name__ == '__main__':
+    server.start()
