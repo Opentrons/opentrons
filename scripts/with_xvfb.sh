e2e12af6bb4c2210b55771375647080822cb161b
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 13:34:06 -0500

    comment out nvm alias

--- scripts/with_xvfb.sh
+++ scripts/with_xvfb.sh
@@ -11,7 +11,7 @@ Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1
 # nvm alias default 6.0.0
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm alias default 6.0.0
+# nvm alias default 6.0.0
 
 echo "Exec'ing the cmd: '$1'"
 eval $1
