5c6005a12464d501d15b2d54c2cadad24d26e6ed
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 13:13:34 -0500

    set nvm use alias to v6.0.0

--- scripts/with_xvfb.sh
+++ scripts/with_xvfb.sh
@@ -7,7 +7,8 @@ echo "got here..2"
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1
 
 # Setup npm version
-nvm use 6.0.0
+# nvm use 6.0.0
+nvm alias default 6.0.0
 
 echo "Exec'ing the cmd: '$1'"
 eval $1
