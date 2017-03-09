722babb59689bf5b9e6536d5b057b5ddcfc91dfe
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 16:04:42 -0500

    nvm on same line

--- scripts/with_xvfb.sh
+++ scripts/with_xvfb.sh
@@ -13,8 +13,7 @@ Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm alias default 6.0.0
-nvm use 6.0.0
+nvm use 6.0.0 && make app
 
-echo "Starting app build"
-make app
+# echo "Starting app build"
 
