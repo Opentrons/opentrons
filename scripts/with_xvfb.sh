6429c0f26a2bd753d8a7e5e9201432a0603a6103
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 13:58:37 -0500

    make xvfb run in background

--- scripts/with_xvfb.sh
+++ scripts/with_xvfb.sh
@@ -4,7 +4,7 @@ echo "got here.."
 export DISPLAY=':99.0'
 
 echo "got here..2"
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
 # Setup npm version
 # nvm use 6.0.0
