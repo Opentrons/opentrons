f72ca2c726b31dc4a83c873c02b5f0ec076d7fac
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 16:18:47 -0500

    make script to build app-src

--- scripts/with_xvfb.sh
+++ scripts/with_xvfb.sh
@@ -13,8 +13,8 @@ Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 # nvm alias default 6.0.0
-# nvm use 6.0.0 && 
-make app
+# nvm use 6.0.0 &&
+./build-app-src
 
 # echo "Starting app build"
 
