e2a4dba97d588a265fa12e43ba0fb6c4d5cf9fce
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 13:25:16 -0500

    source linux nvm script

--- scripts/with_xvfb.sh
+++ scripts/with_xvfb.sh
@@ -9,7 +9,9 @@ Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1
 # Setup npm version
 # nvm use 6.0.0
 # nvm alias default 6.0.0
-source $(brew --prefix nvm)/nvm.sh && nvm alias default 6.0.0
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm alias default 6.0.0
 
 echo "Exec'ing the cmd: '$1'"
 eval $1
