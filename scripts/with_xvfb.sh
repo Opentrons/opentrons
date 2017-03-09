75a79277bd625e217ad57ae32856bdaccd235b6a
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 12:11:53 -0500

    make script to explicitly set xvfb in cmd process

---
+++ scripts/with_xvfb.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1
+
+# Setup npm version
+nvm use 6.0.0
+
+echo "Exec'ing the cmd: '$1'"
+eval $1
