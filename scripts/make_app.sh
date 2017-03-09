466c846d94d2036df0a48df2c27df057e7e98487
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 14:29:38 -0500

    make app w/o eval cmd

--- scripts/make_app.sh
+++ scripts/make_app.sh
@@ -14,5 +14,6 @@ Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm alias default 6.0.0
 
-echo "Exec'ing the cmd: '$1'"
-eval $1
+echo "Starting app build"
+make app
+
