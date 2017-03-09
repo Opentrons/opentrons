c6f0b6aeb67bcf79cf2e45e9014a1b422b534ecd
simplyahmazing <ahmad19526@gmail.com>
2017-03-09 16:01:54 -0500

    explicitly call for use of node v6

---
+++ scripts/with_xvfb.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "got here.."
+
+export DISPLAY=':99.0'
+
+echo "got here..2"
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+
+# Setup npm version
+# nvm use 6.0.0
+# nvm alias default 6.0.0
+
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm alias default 6.0.0
+nvm use 6.0.0
+
+echo "Starting app build"
+make app
+
