70ff22bd204015e3517c96c33e752d9ffb10612f
simplyahmazing <ahmad19526@gmail.com>
2017-03-15 11:17:56 -0400

    cleans up scripts dir by removing unneeded code

--- app-shell/scripts/travis.sh
+++ app-shell/scripts/travis.sh
@@ -2,55 +2,6 @@
 
 set -e
 
-run_install ()
-{
-  if [ "$1" == "linux" ]; then
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-  else
-    mkdir -p ~/.nvm && export NVM_DIR=~/.nvm && source $(brew --prefix nvm)/nvm.sh && nvm alias default 6.0.0
-    echo "$(brew --prefix nvm)/nvm.sh" >> ~/.bashrc
-  fi
-
-  if [ "$1" == "linux" ]; then
-    export DISPLAY=':99.0'
-    Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-  fi
-
-  nvm install 6.0.0
-  nvm use 6.0.0
-  node --version
-  npm --version
-
-  pip3 install pip --upgrade
-  echo "Upgrading virtualenv"
-  pip3 install --upgrade virtualenv
-
-  which pip
-  which pip3
-  pip3 --version
-  mkdir -p $HOME/.cache/pip3
-
-  pip3 install -r requirements.txt --cache-dir $HOME/.cache/pip3
-  npm install && cd app && npm install && cd ..  # Hack until instapp-app-deps works on travis
-
-  cd server && python3 -m nose -s --logging-level WARNING && cd ..
-
-  npm i -g mocha
-  npm run unit-renderer
-  npm run unit-main
-  npm run release:posix
-  # TODO(Ahmed): figure out how to get these working w/new structure
-  #  npm run e2e
-  ls dist/*
-  ls releases
-
-  echo "Creating python virtualenv"
-  # TODO(Ahmed): Run create env test script in create_env dir!!!
-  # python3 scripts/create_env.py
-  # cd env-dist && ../scripts/test_env.sh && cd ..
-}
-
 execute_mac ()
 {
   if [ "$1" == "pre-install" ]; then
@@ -102,10 +53,6 @@ execute_linux ()
     ls -la ~/virtualenv/python2.7/bin/
     npm config set python ~/virtualenv/python2.7/bin/python
     echo "npm's python is $(npm config get python)"
-    # if [ "$1" == "linux" ]; then
-      # export DISPLAY=':99.0'
-      # Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-    # fi
 
     # sudo apt-get install g++
     # sudo apt-get install gcc
