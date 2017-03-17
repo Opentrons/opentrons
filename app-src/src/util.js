808938e4cb3aa5fad7d423e6496432d692d04077
andySigler <andrewsigler1@gmail.com>
2017-03-17 13:30:35 -0400

    adds slot to placeable Vue routes, so containers can have same name when in different slots

--- app-src/src/util.js
+++ app-src/src/util.js
@@ -4,7 +4,7 @@ function addHrefs (tasks) {
   tasks.map((instrument) => {
     instrument.href = '/calibrate/' + instrument.axis
     instrument.placeables.map((placeable) => {
-      placeable.href = '/calibrate/' + instrument.axis + '/' + placeable.label
+      placeable.href = '/calibrate/' + instrument.axis + '/' + placeable.slot + '/' + placeable.label
     })
   })
 }
