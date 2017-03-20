05bee637282b4182dcad9852022420d8c75d5e2d
simplyahmazing <ahmad19526@gmail.com>
2017-03-20 12:56:58 -0400

    testing conda deploy script

--- api/scripts/publish.sh
+++ api/scripts/publish.sh
@@ -16,12 +16,12 @@ bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
 conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-conda install anaconda-client conda-build
-
-conda config --set anaconda_upload yes
-# Add opentrons channel to look for dependencies that are not maintained by Continuum
-conda config --add channels opentrons
-anaconda login --username opentrons --password "$ANACONDA_PASSWORD"
-conda build conda.recipe
+# conda update -q conda
+# conda info -a
+# conda install anaconda-client conda-build
+#
+# conda config --set anaconda_upload yes
+# # Add opentrons channel to look for dependencies that are not maintained by Continuum
+# conda config --add channels opentrons
+# anaconda login --username opentrons --password "$ANACONDA_PASSWORD"
+# conda build conda.recipe
