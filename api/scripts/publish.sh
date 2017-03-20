0265378ba06444f7e35d62f06ba571f9a9c59b22
simplyahmazing <ahmad19526@gmail.com>
2017-03-20 17:58:04 -0400

    uncomments publishing script

--- api/scripts/publish.sh
+++ api/scripts/publish.sh
@@ -8,8 +8,7 @@ git checkout .
 git clean -fd
 
 # Publish to PyPI
-echo "echo skipping PyPI deploy"
-# python setup.py sdist bdist_wheel && twine upload dist/*
+python setup.py sdist bdist_wheel && twine upload dist/*
 
 # Publish to https://anaconda.org/opentrons/opentrons
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
@@ -17,12 +16,12 @@ bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
 conda config --set always_yes yes --set changeps1 no
-# conda update -q conda
-# conda info -a
-# conda install anaconda-client conda-build
-#
-# conda config --set anaconda_upload yes
-# # Add opentrons channel to look for dependencies that are not maintained by Continuum
-# conda config --add channels opentrons
-# anaconda login --username opentrons --password "$ANACONDA_PASSWORD"
-# conda build conda.recipe
+conda update -q conda
+conda info -a
+conda install anaconda-client conda-build
+
+conda config --set anaconda_upload yes
+# Add opentrons channel to look for dependencies that are not maintained by Continuum
+conda config --add channels opentrons
+anaconda login --username opentrons --password "$ANACONDA_PASSWORD"
+conda build conda.recipe
