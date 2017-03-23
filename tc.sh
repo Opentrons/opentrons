a6eb9d67f1053cb1cdf1fd80ab32f603e4c2a763
simplyahmazing <ahmad19526@gmail.com>
2017-03-23 16:58:36 -0400

    updates tc script

--- tc.sh
+++ tc.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Got here..."
+
+
+# mkdir -p ~/.nvm && export NVM_DIR=~/.nvm && source $(brew --prefix nvm)/nvm.sh && nvm alias default 6.0.0
+npm config set python /usr/bin/python
+virtualenv -p python3 venv && source venv/bin/activate
+./scripts/with-nodenv-linux "make app"
+make api-exe
+./scripts/with-nodenv-linux "make app-shell"
