3d70bd9022bc0f0c0ffdc53796e45e5481947ad0
simplyahmazing <ahmad19526@gmail.com>
2017-03-23 17:08:48 -0400

    activate nvm; call make directly

--- tc.sh
+++ tc.sh
@@ -2,10 +2,20 @@
 
 echo "Got here..."
 
-
-# mkdir -p ~/.nvm && export NVM_DIR=~/.nvm && source $(brew --prefix nvm)/nvm.sh && nvm alias default 6.0.0
+# Enable nvm
+mkdir -p ~/.nvm && export NVM_DIR=~/.nvm && source $(brew --prefix nvm)/nvm.sh && nvm alias default 6.0.0
 npm config set python /usr/bin/python
+
+# Create python3 env
 virtualenv -p python3 venv && source venv/bin/activate
-./scripts/with-nodenv-linux "make app"
+
+# Build app src
+make app
+# ./scripts/with-nodenv-linux "make app"
+
+# Build app exe
 make api-exe
-./scripts/with-nodenv-linux "make app-shell"
+
+# Build app shell
+make app-shell
+# ./scripts/with-nodenv-linux "make app-shell"
