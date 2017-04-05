501fa15b5d80fea38ba458e37297317648d83393
simplyahmazing <ahmad19526@gmail.com>
2017-04-05 14:26:43 -0400

    adds dataLayer dispatch for DECK_CALIBRATED event

--- app-src/src/components/CalibratePlaceable.vue
+++ app-src/src/components/CalibratePlaceable.vue
@@ -34,7 +34,7 @@
           })
         }
         if (isCalibrated.bind(this)()) {
-          trackEvent('deck-calibrated')
+          trackEvent('DECK_CALIBRATED')
         }
       },
       moveToPosition () {
