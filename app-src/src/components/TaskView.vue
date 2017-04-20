99ffb5105333f08b106e62100989570edcbc0691
Kadee80 <kaa328@nyu.edu>
2017-04-20 15:44:16 -0400

    added pipette navigation and img toggle

--- app-src/src/components/TaskView.vue
+++ app-src/src/components/TaskView.vue
@@ -2,17 +2,34 @@
   
     <section id="task-view">
       <section id="placeable-pane">  
-            <h1 :class="{calibrated : instrument().calibrated,  active: $route.params.placeable }">{{$route.params.placeable}} at slot {{$route.params.slot}}</h1>
+        <h1 :class="{calibrated : instrument().calibrated,  active: $route.params.placeable }">{{$route.params.placeable}} at slot {{$route.params.slot}}</h1>
         <deck-navigation  :instrument='instrument()' :deck='deck()'></deck-navigation>  
       </section>
 
       <section id="instrument-pane">
         <h1>{{instrument().label}} {{channels}} [{{instrumentLocation}}]</h1>
+        <!-- TODO: Refactor into PipetteNavigation Component w/props -->
         <button  v-for="instrument in tasks().instruments" 
         class="tab" :class="{active : activePipette(instrument)}"  
         @click="togglePipette(instrument.axis)"> 
-        {{instrument.axis}} {{instrument.label}}<span v-for="c in instrument.channels">&#9661;</span>
-        </button>
+        {{instrument.axis}} {{instrument.label}}<span v-for="c in instrument.channels">&#9661;</span></button>
+        <div class="pipette-modal" :class="plungerMode">
+        <div class="plunger-nav">
+          <ul>
+            <li @click="modePlunger('top')" class="top">Top</li>
+            <li @click="modePlunger('bottom')" class="bottom">Bottom</li>
+            <li @click="modePlunger('blowout')" class="blowout">Blowout</li>
+            <li @click="modePlunger('droptip')" class="droptip">Drop Tip</li>
+          </ul>
+        </div>
+        <div class="plunger-img">
+          <img src='../assets/img/pipette_top.png' class='top'/>
+          <img src='../assets/img/pipette_bottom.png' class='bottom'/>
+          <img src='../assets/img/pipette_blowout.png' class='blowout'/>
+          <img src='../assets/img/pipette_droptip.png' class='droptip'/>
+        </div>
+        </section>
+
       </section>
 
     </section>
@@ -29,7 +46,8 @@
     props: ['busy'],
     data () {
       return {
-        axis: this.$route.params.instrument
+        axis: this.$route.params.instrument,
+        plungerMode: 'mode-top'
       }
     },
     components: {
@@ -62,6 +80,9 @@
       },
       togglePipette (axis) {
         this.$router.push({ name: 'instrument', params: { instrument: axis, slot: null, placeable: null } })
+      },
+      modePlunger (mode) {
+        this.plungerMode = 'mode-' + mode
       },
       running () {
         return this.$store.state.running || this.$store.state.protocolFinished
