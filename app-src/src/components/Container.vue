40119b490d3592361e2f2fa8bbd5b2fe7c450900
andySigler <andrewsigler1@gmail.com>
2017-05-11 11:04:38 -0400

    only jog-to-slot when not busy

--- app-src/src/components/Container.vue
+++ app-src/src/components/Container.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class='placeable'>
-    <router-link :to="link()" @click='jogToSlot()'>
+  <div class='placeable' @click='jogToSlot()'>
+    <router-link :to="link()">
     <span v-if="wells">
-      <svg v-if="!drawRect" :width="width" viewBox='0 0 85.5 127.75' width='100%' preserveAspectRatio='xMinYMin meet' @click='jogToSlot()'>
+      <svg v-if="!drawRect" :width="width" viewBox='0 0 85.5 127.75' width='100%' preserveAspectRatio='xMinYMin meet'>
         <g>
-          <circle v-for="well in wells"  fill='blue' stroke='white' :cx='well.x' :cy='slotDimY-well.y' :r='well.diameter/2' class="well" @click='jogToSlot()'>
+          <circle v-for="well in wells"  fill='blue' stroke='white' :cx='well.x' :cy='slotDimY-well.y' :r='well.diameter/2' class="well">
           </circle>
         </g>
       </svg>
-      <svg v-else viewBox='0 0 85.5 127.75' width='100%' preserveAspectRatio='xMinYMin' @click='jogToSlot()'>
+      <svg v-else viewBox='0 0 85.5 127.75' width='100%' preserveAspectRatio='xMinYMin'>
         <g>
-          <rect v-for="well in wells"  fill='blue' stroke='white' :x='well.x - well.width/2' :y='well.y - well.length/2' :width='well.width' :height="well.length" class="trough" @click='jogToSlot()'></rect>
+          <rect v-for="well in wells"  fill='blue' stroke='white' :x='well.x - well.width/2' :y='well.y - well.length/2' :width='well.width' :height="well.length" class="trough"></rect>
         </g>
       </svg>
     </span>
@@ -43,7 +43,9 @@ export default {
   },
   methods: {
     jogToSlot (slot) {
-      this.$store.dispatch('jogToSlot', {slot: this.placeable.slot})
+      if (!this.$store.state.busy) {
+        this.$store.dispatch('jogToSlot', {slot: this.placeable.slot})
+      }
     },
     normalize (value) {
       return value * this.scaler
