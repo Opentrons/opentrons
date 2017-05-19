f2cfb24bdae463d66705922c0f0acfe1c9c96071
simplyahmazing <ahmad19526@gmail.com>
2017-05-18 22:13:35 -0400

    cleaning state helpers

--- api/opentrons/util/state.py
+++ api/opentrons/util/state.py
@@ -1,6 +1,31 @@
 from opentrons.instruments import pipette
 from opentrons.containers import placeable
 
+
+def get_state(robot):
+    return [
+        {'axis': inst.axis,
+         'label': inst.name,
+         'channels': inst.channels,
+         'top': inst.positions['top'],
+         'bottom': inst.positions['bottom'],
+         'blow_out': inst.positions['blow_out'],
+         'drop_tip': inst.positions['drop_tip'],
+         'max_volume': inst.max_volume,
+         'calibrated': are_instrument_positions_calibrated(inst),
+         'placeables': [
+             {'type': container.get_type(),
+              'label': container.get_name(),
+              'slot': container.get_parent().get_name(),
+              'calibrated': is_inst_calibrated_to_container(
+                  inst, container
+              )
+            }
+         for container in get_unique_containers(inst)]
+         }
+    for inst in get_all_pipettes(robot)]
+
+
 def get_unique_containers(instrument):
     """
     Returns all associated containers for an instrument
@@ -15,48 +40,8 @@ def get_unique_containers(instrument):
 
     return _sort_containers(list(unique_containers))
 
-def create_step_list(robot):
-    return [{
-            'axis': instrument.axis,
-            'label': instrument.name,
-            'channels': instrument.channels,
-            'placeables': [
-                {
-                    'type': container.get_type(),
-                    'label': container.get_name(),
-                    'slot': container.get_parent().get_name()
-                }
-                for container in get_unique_containers(instrument)
-                ]
-        } for instrument in get_all_pipettes(robot)]
 
-
-def get_state(robot):
-    #if current_protocol_step_list is None:
-     #   create_step_list()
-
-    step_list = create_step_list(robot)
-    for step in step_list:
-        t_axis = str(step['axis']).upper()
-        instrument = robot._instruments[t_axis]
-        step.update({
-            'top': instrument.positions['top'],
-            'bottom': instrument.positions['bottom'],
-            'blow_out': instrument.positions['blow_out'],
-            'drop_tip': instrument.positions['drop_tip'],
-            'max_volume': instrument.max_volume,
-            'calibrated': are_instrument_positions_calibrated(instrument)
-        })
-
-        for placeable_step in step['placeables']:
-            c = get_container_from_step(robot, placeable_step)
-            if c:
-                placeable_step.update({
-                    'calibrated': is_instrument_calibrated_to_container(instrument, c)
-                })
-
-
-def is_instrument_calibrated_to_container(instrument, container):
+def is_inst_calibrated_to_container(instrument, container):
     """
     Returns True if instrument holds calibration data for a Container
     """
@@ -105,23 +90,6 @@ def _get_all_containers(robot):
     return _sort_containers(all_containers)
 
 
-def get_container_from_step(robot, step):
-    """
-    Retruns the matching Container for a given placeable step in the step-list
-    """
-    all_containers = _get_all_containers(robot)
-    for container in all_containers:
-        match = [
-            container.get_name() == step['label'],
-            container.get_parent().get_name() == step['slot'],
-            container.get_type() == step['type']
-
-        ]
-        if all(match):
-            return container
-    return None
-
-
 def _sort_containers(container_list):
     """
     Returns the passed container list, sorted with tipracks first
@@ -145,5 +113,4 @@ def _sort_containers(container_list):
         other,
         key=lambda c: c.get_name().lower()
     )
-
     return tipracks + other
