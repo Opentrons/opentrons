244245d673aea253f130c6897d7c881e85880e51
andySigler <andrewsigler1@gmail.com>
2017-05-24 04:33:05 -0400

    adds more logging to electron main; electron call /main route on window close

--- app-shell/app/main/util.js
+++ app-shell/app/main/util.js
@@ -1,11 +1,16 @@
+const {getLogger} = require('./logging.js')
 const rp = require('request-promise')
 
+const mainLogger = getLogger('electron-main')
+
 function waitUntilServerResponds (createWindow, windowUrl) {
+  mainLogger.info('Pinging ' + windowUrl)
   rp(windowUrl)
     .then((html) => {
       return createWindow()
     })
     .catch((err) => {
+      mainLogger.info(err)
       setTimeout(() => {
         waitUntilServerResponds(createWindow, windowUrl)
       }, 500)
