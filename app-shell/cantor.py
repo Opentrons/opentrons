f4f0a0398881a1b5db6a031974ab75368a7ed57e
simplyahmazing <ahmad19526@gmail.com>
2017-05-29 18:07:34 -0400

    save outstanding edits

---
+++ app-shell/cantor.py
@@ -0,0 +1,21 @@
+import math
+
+
+def cantor_calculate(x, y):
+    """
+    Returns cantor value of 'x' & 'y'
+    https://en.wikipedia.org/wiki/Pairing_function
+    """
+    return (((x + y)*(x + y + 1))/2) + y
+
+
+def cantor_reverse(z):
+    """
+    Returns cantor pair that resulted in 'z'
+    https://en.wikipedia.org/wiki/Pairing_function
+    """
+    w = math.floor((math.sqrt((8*z) + 1) - 1) / 2)
+    t = ((w**2) + w)/2
+    y = z - t
+    x = w - y
+    return (math.floor(x), math.floor(y))
