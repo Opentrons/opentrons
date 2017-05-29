54e07f64556188f004b7254dd2b7de42a9c86eb6
simplyahmazing <ahmad19526@gmail.com>
2017-05-29 19:21:52 -0400

    fixes and refactors for demo

--- app-shell/app/main/util.js
+++ app-shell/app/main/util.js
@@ -1,6 +1,6 @@
 const rp = require('request-promise')
 
-function waitUntilServerResponds (createWindow, windowUrl) {
+const waitUntilServerResponds = (createWindow, windowUrl) => {
   rp(windowUrl)
     .then((html) => {
       return createWindow()
@@ -12,4 +12,21 @@ function waitUntilServerResponds (createWindow, windowUrl) {
     })
 }
 
-module.exports = {waitUntilServerResponds}
+const cantorReverse = (z) => {
+  const w = Math.floor((Math.sqrt((8*z) + 1) - 1)/2)
+  const t = (Math.pow(w, 2) + w)/2
+  const y = z - t
+  const x = w - y
+  return [x, y]
+}
+
+const getAppVersion = (version, channel) => {
+  if (channel !== 'beta') {
+    return version
+  }
+  const [major, minor, fakePatch] = version.split('.')
+  const [patch, buildNumber] = cantorReverse(parseInt(fakePatch))
+  return `${major}.${minor}.${patch}+${buildNumber}`
+}
+
+module.exports = {getAppVersion, waitUntilServerResponds}
