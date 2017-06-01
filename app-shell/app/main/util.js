674a6e647aeaf8c653304f0e4380fc4d885c27cf
Andy Sigler <andrewsigler1@gmail.com>
2017-06-01 14:26:05 -0400

    Run usb detached (#237)
    * copies over SmoothiePlayer related files
    
    * adds commented out test
    
    * works with UI
    
    * pylama
    
    * do no overwrite pipette positions if there are numbers
    
    * catch packet loss during wait_for_arrival(); set UI busy to false on disconnect
    
    * pylama
    
    * adds more logging to electron main; electron call /main route on window close
    
    * get loggers after setting app_data_dir
    
    * do not reset after detached run
    
    * catch errors when running detached protocol
    
    * set virtual smoothie to same position as physical driver
    
    * kill threads on exit, use flask exit function
    
    * fixes pylama and tests
    
    * wait_for_arrival sleeps only if distance is greater than 5x tolerance
    
    * run sys.exit() if server.shutdown is None
    
    * hide detached button, active with shift-click
    
    * do not halt during home, so that previous commands are not cancelled
    
    * set virtual smoothies to same dimensions as connected robot
    
    * requests==2.14.2 in setup.py to avoid build errors
    
    * only run detached if using latest firmware
    
    * set CHANNEL env to branch name if not master
    
    * move opentrons import to top of file
    
    * reverts previous commit
    
    * app-shell build:frontend using python3
    
    * set channel to dev
    
    * replace cp with copy
    
    * build:frontend:win called from Makefile
    
    * only overwrite speeds with defaults if they have not been set
    
    * updates docs for updating firmware

--- app-shell/app/main/util.js
+++ app-shell/app/main/util.js
@@ -1,11 +1,15 @@
+const {getLogger} = require('./logging.js')
 const rp = require('request-promise')
 
-const waitUntilServerResponds = (createWindow, windowUrl) => {
+function waitUntilServerResponds (createWindow, windowUrl) {
+  const mainLogger = getLogger('electron-main')
+  mainLogger.info('Pinging ' + windowUrl)
   rp(windowUrl)
     .then((html) => {
       return createWindow()
     })
     .catch((err) => {
+      mainLogger.info(err)
       setTimeout(() => {
         waitUntilServerResponds(createWindow, windowUrl)
       }, 500)
--- app-shell/scripts/build_electron_app_with_builder.py
+++ app-shell/scripts/build_electron_app_with_builder.py
@@ -163,6 +163,8 @@ def build_electron_app():
         if 'CHANNEL' not in os.environ:
             os.environ['CHANNEL'] = 'beta'
         process_args.extend(["-p", "always"])
+    if 'CHANNEL' not in os.environ:
+        os.environ['CHANNEL'] = 'dev'
 
     print(process_args)
 
