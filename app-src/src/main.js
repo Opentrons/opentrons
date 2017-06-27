a957df40817f81dc9195067a1c5d9dc8064f4cc6
simplyahmazing <ahmad19526@gmail.com>
2017-06-27 14:40:17 -0400

    removes unused import

--- app-src/src/main.js
+++ app-src/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import store from './store/store'
-import { getFakeUserId, getUserId } from './util'
+import { getUserId } from './util'
 import {
   Placeable,
   CalibrateInstrument,
