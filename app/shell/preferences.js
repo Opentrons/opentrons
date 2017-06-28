5de546c23570aed2340e423012cedf0b2c0f8dab
astaff <artyom.astafurov@gmail.com>
2017-06-28 00:29:31 -0400

    commented auto-update prompt altogether

--- app/shell/preferences.js
+++ app/shell/preferences.js
@@ -5,21 +5,21 @@ const settings = require('electron-settings')
 settings.on('create', pathToSettings => {
   // A dialog popping up is preventing 
   // integration tests from running
-  if (process.env.INTEGRATION_TEST === 'true') {
-    settings.setSync('autoUpdate', false)
-    return
-  }
+  // if (process.env.INTEGRATION_TEST === 'true') {
+  //   settings.setSync('autoUpdate', false)
+  //   return
+  // }
 
-  const result = dialog.showMessageBox({
-    message: 'Do you want to turn on auto updating?',
-    buttons: ['Yes', 'No']
-  })
+  // const result = dialog.showMessageBox({
+  //   message: 'Do you want to turn on auto updating?',
+  //   buttons: ['Yes', 'No']
+  // })
 
-  if (result === 0) {
-    settings.setSync('autoUpdate', true)
-  } else if (result === 1) {
-    settings.setSync('autoUpdate', false)
-  }
+  // if (result === 0) {
+  //   settings.setSync('autoUpdate', true)
+  // } else if (result === 1) {
+  //   settings.setSync('autoUpdate', false)
+  // }
 })
 
 function getSetting (setting) {
