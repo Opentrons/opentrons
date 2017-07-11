d6ab15437446af80e9edb0da95cb40853cf71c84
simplyahmazing <ahmad19526@gmail.com>
2017-07-11 11:26:53 -0400

    refactor get_driver to create_driver

--- api/opentrons/drivers/__init__.py
+++ api/opentrons/drivers/__init__.py
@@ -22,7 +22,7 @@ __all__ = [
 ]
 
 
-drivers_by_version = {
+DRIVER_VERSIONS = {
     'v1.0.5': SmoothieDriver_1_2_0,
     'edge-1c222d9NOMSD': SmoothieDriver_2_0_0
 }
@@ -111,24 +111,20 @@ def create_virtual_driver(options=None):
 
     vs = vs_class(default_options)
     c = connection.Connection(vs, port=VIRTUAL_SMOOTHIE_PORT, timeout=0)
-    return get_driver(c)
+    return create_driver(c)
 
 
 def get_serial_driver(port):
     s = serial.Serial()
     c = connection.Connection(s, port=port, baudrate=115200, timeout=0.01)
-    return get_driver(c)
+    return create_driver(c)
 
 
-def get_driver(c):
-    driver_class = drivers_by_version.get(get_version(c))
-    if driver_class:
-        d = driver_class(SMOOTHIE_DEFAULTS)
-        d.connect(c)
-        return d
-    else:
-        raise RuntimeError(
-            'Can not read version from port "{}"'.format(c.name()))
+def create_driver(c):
+    driver_class = DRIVER_VERSIONS[get_version(c)]
+    d = driver_class(SMOOTHIE_DEFAULTS)
+    d.connect(c)
+    return d
 
 
 def get_version(c):
@@ -142,7 +138,7 @@ def get_version(c):
 
     # {"version":v1.0.5}
     v = response.split(':')[-1][:-1]
-    if v in drivers_by_version:
+    if v in DRIVER_VERSIONS:
         return v
 
     # Build version: BRANCH-HASH, Build date: Mar 18 2017 21:15:21, MCU: LPC1769, System Clock: 120MHz  # noqa
@@ -150,5 +146,5 @@ def get_version(c):
     #   6 axis
     # ok
     v = response.split(',')[0].split(' ')[-1]  # BRANCH-HASH
-    if v in drivers_by_version:
+    if v in DRIVER_VERSIONS:
         return v
