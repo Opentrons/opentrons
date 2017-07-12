9d2af67df421a6969ccd513440fa363375cc7c30
simplyahmazing <ahmad19526@gmail.com>
2017-07-12 13:13:33 -0400

    set up rdp server

--- api/tests/opentrons/integration/test_gcode.py
+++ api/tests/opentrons/integration/test_gcode.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-import difflib
-import hashlib
-from pprint import pformat
-import os
-=======
 import os
 import hashlib
->>>>>>> add gcode test
 
 
 def test_gcode():
@@ -27,18 +20,12 @@ def test_gcode():
     os.environ['GCODE_FILE'] = ''
 
     result_hash = hashlib.md5(open(res_gcode, 'rb').read()).hexdigest()
-<<<<<<< HEAD
     expected_hash = hashlib.md5(open(expected_gcode, 'rb').read()).hexdigest()
 
     res = open(res_gcode, 'r').readlines()
     exp = open(expected_gcode, 'r').readlines()
 
     assert result_hash == expected_hash,\
-=======
-    rexpected_hash = hashlib.md5(open(expected_gcode, 'rb').read()).hexdigest()
-
-    assert result_hash == rexpected_hash,\
->>>>>>> add gcode test
         """G-CODE test failed. Compare: {0} and {1}'
 
         Use diff.py for a visual HTML comparison
@@ -47,16 +34,4 @@ def test_gcode():
 
         """.format(
             res_gcode, expected_gcode, os.path.join(this_dir, 'diff.py')
-<<<<<<< HEAD
-        ) + ("""
-        {}
-        """.format(
-            pformat(list(
-                difflib.context_diff(
-                    res[:100], exp[:100], fromfile='res', tofile='exp'
-                )
-            )))
         )
-=======
-        )
->>>>>>> add gcode test
