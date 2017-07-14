b9d1c63265384931a95028c6d6a598aef3c923fe
Kadee80 <kaa328@nyu.edu>
2017-07-14 15:29:07 -0400

    wildcard route

--- app/ui/routes.js
+++ app/ui/routes.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import MainPage from './containers/MainPage';
-import AboutPage from './containers/AboutPage';
+// import AboutPage from './containers/AboutPage';
 
 export default () => (
   <App>
     <Switch>
-      <Route path="/" component={MainPage} />
-      <Route path="/welcome" component={MainPage} />
-      <Route path="/about" component={AboutPage} />
+      <Route path="/*" component={MainPage} />
     </Switch>
   </App>
 );
