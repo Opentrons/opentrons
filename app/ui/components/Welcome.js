531f8562d7e41c4fa601dcb10304754ed4699cdb
Kadee80 <kaa328@nyu.edu>
2017-07-18 10:58:35 -0400

    adds jog pipette component

--- app/ui/components/Welcome.js
+++ app/ui/components/Welcome.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Jog from './Jog'
+import styles from './Welcome.css'
 
 class Welcome extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome!</h1>
+      <div className={styles.welcome}>
+        <Jog className={styles.jog} />
       </div>
-    );
+    )
   }
 }
 
-export default Welcome;
+export default Welcome
