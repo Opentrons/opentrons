a127101e43b38da0944d69eeaf819c26cee61404
Kadee80 <kaa328@nyu.edu>
2017-07-19 15:58:02 -0400

    jog plunger build from data

--- app/ui/components/JogPlunger.js
+++ app/ui/components/JogPlunger.js
@@ -3,18 +3,39 @@ import styles from './Jog.css'
 
 export default class JogPlunger extends Component {
   render() {
+    const axis = [
+      {
+        name: 'b_neg',
+        content: '\u2191',
+        direction: -1
+      },
+      {
+        name: 'b_pos',
+        content: '\u2193',
+        direction: 1
+      },
+      {
+        name: 'a_neg',
+        content: '\u2191',
+        direction: -1
+      },
+      {
+        name: 'a_pos',
+        content: '\u2193',
+        direction: 1
+      },
+    ]
     return (
       <section className={styles.plunger}>
 
         <h2 className={styles.title_plunger}>Plunger Jog</h2>
         <div className={styles.jog_ab}>
           <h2 className={styles.title_b}>B</h2>
-          <button className={styles.b_neg}>&uarr;</button>
-          <button className={styles.b_pos}>&darr;</button>
-
           <h2 className={styles.title_a}>A</h2>
-          <button className={styles.a_neg}>&uarr;</button>
-          <button className={styles.a_pos}>&darr;</button>
+
+          {axis.map((a) =>
+            <button key={a.name} className={styles[a.name]}>{a.content}</button>
+          )}
 
         </div>
         <div className={styles.increment_ab} />
