78db6c8b4b587f091e3bb69de267271b5ecc6947
Kadee80 <kaa328@nyu.edu>
2017-07-19 15:49:08 -0400

    remove blue focus from btn, refactor jogXYZ

--- app/ui/components/JogPipette.js
+++ app/ui/components/JogPipette.js
@@ -3,23 +3,48 @@ import styles from './Jog.css'
 
 export default class JogPipette extends Component {
   render() {
+    const axis = [
+      {
+        name: 'x_neg',
+        content: '\u2190',
+        direction: -1
+      },
+      {
+        name: 'x_pos',
+        content: '\u2192',
+        direction: 1
+      },
+      {
+        name: 'y_neg',
+        content: '\u2191',
+        direction: -1
+      },
+      {
+        name: 'y_pos',
+        content: '\u2193',
+        direction: 1
+      },
+      {
+        name: 'z_neg',
+        content: '\u2191',
+        direction: -1
+      },
+      {
+        name: 'z_pos',
+        content: '\u2193',
+        direction: 1
+      }
+    ]
     return (
       <section className={styles.pipette}>
         <h2 className={styles.title_pipette}>Pipette Jog</h2>
         <div className={styles.jog_xyz}>
           <h2 className={styles.title_xy}>X-Y</h2>
-
-          <button className={styles.x_neg}>&larr;</button>
-          <button className={styles.x_pos}>&rarr;</button>
-
-          <button className={styles.y_neg}>&uarr;</button>
-          <button className={styles.y_pos}>&darr;</button>
-
           <h2 className={styles.title_z}>Z</h2>
 
-          <button className={styles.z_neg}>&uarr;</button>
-          <button className={styles.z_pos}>&darr;</button>
-
+          {axis.map((a) =>
+            <button key={a.name} className={styles[a.name]}>{a.content}</button>
+          )}
         </div>
 
         <div className={styles.increment_xyz} />
