7413186a30708be1921aa3b7bff14624c20d926c
simplyahmazing <ahmad19526@gmail.com>
2017-07-20 13:38:38 -0400

    add name doctest pipette

--- api/opentrons/instruments/pipette.py
+++ api/opentrons/instruments/pipette.py
@@ -717,7 +717,7 @@ class Pipette(Instrument):
         <opentrons.robot.robot.Robot object at ...>
         >>> tiprack = containers.load('tiprack-200ul', 'E1', share=True)
         >>> p200 = instruments.Pipette(axis='a',
-        ...     tip_racks=[tiprack], max_volume=200)
+        ...     tip_racks=[tiprack], max_volume=200, name='p200')
         >>> p200.pick_up_tip() # doctest: +ELLIPSIS
         <opentrons.instruments.pipette.Pipette object at ...>
         >>> p200.aspirate(50, plate[0]) # doctest: +ELLIPSIS
