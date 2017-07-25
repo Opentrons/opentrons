352d304225dfd75e6ecad28f33047fcb9e23b4a0
Kadee80 <kaa328@nyu.edu>
2017-07-25 13:17:45 -0400

    switch dummy input to div, remove linting error

--- app/ui/components/Upload.js
+++ app/ui/components/Upload.js
@@ -5,10 +5,10 @@ export default class Upload extends Component {
   render() {
     return (
       <div className={styles.upload}>
-        <button className={styles.btn}>
+        <div className={styles.btn}>
           <img className={styles.icon} src='../ui/img/plus.png' alt='upload'/>
-          <input className={styles.file} type='file' />
-        </ button>
+          <input className={styles.file} type='file'/>
+        </div>
         <label className={styles.file_label}>Upload Protocol</label>
       </div>
     )
