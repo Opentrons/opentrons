d8a1854256221aaef41421fd7e73143ce9d22463
Mike Cousins <mike@cousins.io>
2017-08-07 17:54:15 -0400

    Rework RPC client to build fully defined remote objects
    * Adapted RPC client to handle new RPC server responses that
      include type information
    * Changed client interface to an EventEmitter
    * Incidental .eslint and package.json edits that will be tackled
      in future cleanup / lint configuration extraction

--- app/test/rpc/integration.spec.js
+++ app/test/rpc/integration.spec.js
@@ -1,21 +1,25 @@
-import log from 'winston'
 import path from 'path'
+import log from 'winston'
+
+import Client from '../../rpc/client'
+
 log.level = 'warning'
 
-const { Connection, Client } = require('../../rpc/client')
+const URL = 'ws://127.0.0.1:31950/'
+const PROTOCOL = path.join(__dirname, '../../../api/opentrons/server/tests/data/dinosaur.py')
 
-describe('Remote protocol run', () => {
-  it('Runs', async () => {
-    const { promise, socket } = Connection(
-      'ws://127.0.0.1:31950/'
-    )
-    const connection = await promise
-    const client = await Client(connection)
-    const robotContainer = await client.get_root()
-    const robot = await robotContainer.load_protocol_file(
-      path.join(__dirname, '../../../api/opentrons/server/tests/data/dinosaur.py'))
+describe('RPC integration test', () => {
+  let client
+
+  beforeEach(async () => (client = await Client(URL)))
+  afterEach(() => client.close())
+
+  it('should be able to connect to server and load a protocol', async () => {
+    client = await Client(URL)
+
+    const robotContainer = await client.control.get_root()
+    const robot = await robotContainer.load_protocol_file(PROTOCOL)
+
     console.log(await robot.commands())
-    console.log(await robot._deck)
-    socket.close()
   })
 })
