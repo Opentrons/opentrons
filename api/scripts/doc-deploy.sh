fee1fe7f4b1b995ce65d68fdbd0795f9c101dbd5
simplyahmazing <ahmad19526@gmail.com>
2017-08-08 17:30:01 -0400

    edit path loc

--- api/scripts/doc-deploy.sh
+++ api/scripts/doc-deploy.sh
@@ -53,7 +53,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in doc-deploy.key.enc -out ../doc-deploy.key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../scripts/doc-deploy.key.enc -out ../doc-deploy.key -d
 chmod 600 ../doc-deploy.key
 eval `ssh-agent -s`
 ssh-add ../doc-deploy.key
