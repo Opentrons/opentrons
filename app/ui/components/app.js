6865d4acf47d5e97c0b1155abd5e3ffb66c29835
Kadee80 <kaa328@nyu.edu>
2017-08-10 15:58:53 -0400

    repurpose button.js from parametric pcr for generic button components, add style as a prop for more control over layout

--- app/ui/components/app.js
+++ app/ui/components/app.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import styles from './app.css'
 
+import Connection from './Connection'
+import Button from './Button'
+
+const running = false;
+
 export default function App () {
   return (
     <div className={styles.run_wrapper}>
-      <header className={styles.menu} />
+      <header className={styles.menu}>
+        <Button onClick= {() => { console.log('run')}} disabled={running} style={styles.run}>Run Job</Button>
+      </header>
       <aside className={styles.sidebar} />
-      <div className={styles.connect} />
+      <div className={styles.connect}>
+        <Connection />
+      </div>
       <section className={styles.run_progress} />
       <main className={styles.task} />
     </div>
