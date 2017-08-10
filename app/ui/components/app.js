62b3cf67c05e3a2f3128259ec852908d75bb4d74
Kadee80 <kaa328@nyu.edu>
2017-08-10 14:59:40 -0400

    initial layout grid template

--- app/ui/components/app.js
+++ app/ui/components/app.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import styles from './app.css'
 
 export default function App () {
   return (
-    <h1>hello robot</h1>
+    <div className={styles.wrapper}>
+      <header className={styles.menu} />
+      <aside className={styles.sidebar} />
+      <div className={styles.connect} />
+      <main className={styles.task} />
+    </div>
   )
 }
