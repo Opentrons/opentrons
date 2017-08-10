2e16e8fcec63af596fe7e220e14d94d6b0582e7e
Kadee80 <kaa328@nyu.edu>
2017-08-10 15:07:39 -0400

    rename wrapper to run_wrapper, add in run controls/progress area

--- app/ui/components/app.js
+++ app/ui/components/app.js
@@ -3,10 +3,11 @@ import styles from './app.css'
 
 export default function App () {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.run_wrapper}>
       <header className={styles.menu} />
       <aside className={styles.sidebar} />
       <div className={styles.connect} />
+      <section className={styles.run_progress} />
       <main className={styles.task} />
     </div>
   )
