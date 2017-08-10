e3f38a69249102fb2594db453945640f041ee0f2
simplyahmazing <ahmad19526@gmail.com>
2017-08-10 17:28:50 -0400

    remove run/simulate refs in pipette

--- api/scripts/doc-deploy.sh
+++ api/scripts/doc-deploy.sh
@@ -6,10 +6,10 @@ TARGET_BRANCH="gh-pages"
 CNAME="docs.opentrons.com"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy."
-    exit 0
-fi
+# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#     echo "Skipping deploy."
+#     exit 0
+# fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -49,14 +49,14 @@ git add .nojekyll
 git commit -m "Deploy to GitHub Pages: ${SHA} [ci skip]"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../scripts/doc-deploy.key.enc -out ../doc-deploy.key -d
-chmod 600 ../doc-deploy.key
-eval `ssh-agent -s`
-ssh-add ../doc-deploy.key
+# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../scripts/doc-deploy.key.enc -out ../doc-deploy.key -d
+# chmod 600 ../doc-deploy.key
+# eval `ssh-agent -s`
+# ssh-add ../doc-deploy.key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
