0e0ba8ddc6136a1a372999d7576981d057467392
Kadee80 <kaa328@nyu.edu>
2017-08-14 13:31:29 -0400

    added mack state

--- app/ui/components/app.js
+++ app/ui/components/app.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './app.css'
 
 import Connection from './Connection'
 import Button from './Button'
+import RunControl from './RunControl'
 
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      running: true,
+      paused: false,
+      errors: ['Lost connection with serial port']
+    }
+  }
+  render(){
+    return (
+      <div className={styles.run_wrapper}>
+        <header className={styles.menu}>
+          <Button onClick= {() => { console.log('run')}} disabled={this.state.running} style={styles.run}>Run Job</Button>
+        </header>
+        <aside className={styles.sidebar} />
+        <div className={styles.connect}>
+          <Connection />
+        </div>
+        
+        <RunControl 
+          style={styles.run_progress} 
+          running={this.state.running} 
+          paused={this.state.paused} 
+          errors={this.state.errors}
+        />
 
-
-export default function App () {
-  const running = false
-  return (
-    <div className={styles.run_wrapper}>
-      <header className={styles.menu}>
-        <Button onClick= {() => { console.log('run')}} disabled={running} style={styles.run}>Run Job</Button>
-      </header>
-      <aside className={styles.sidebar} />
-      <div className={styles.connect}>
-        <Connection />
+        <main className={styles.task} />
       </div>
-      <section className={styles.run_progress} />
-      <main className={styles.task} />
-    </div>
-  )
+    )
+  }
 }
