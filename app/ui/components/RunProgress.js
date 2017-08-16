cb8bdce215ceab5f7d294dc98092378733292a05
Kadee80 <kaa328@nyu.edu>
2017-08-16 10:15:37 -0400

    added prop types tp progress bar, error styles override paused

--- app/ui/components/RunProgress.js
+++ app/ui/components/RunProgress.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import styles from './RunProgress.css'
 
-
-const RunProgress = ({ progress, hasError, paused }) => ( 
+const RunProgress = ({ progress, hasError, paused }) => (
   <div className={styles.bar_wrapper}>
-    <div className={ classnames(styles.bar, {[styles.error_bar] : hasError, [styles.paused_bar] : paused}) } style={{ width: `${progress}%`}}>
-    </div>
+    <div className={classnames(styles.bar, {[styles.error_bar]: hasError, [styles.paused_bar]: paused})} style={{width: `${progress}%`}} />
   </div>
 )
 
 RunProgress.propTypes = {
-  
+  progress: PropTypes.number,
+  hasError: PropTypes.bool,
+  paused: PropTypes.bool
 }
 
 export default RunProgress
