2aa7cbfaa16068d376f634f5b31f411298fc68d2
astaff <artyom.astafurov@gmail.com>
2017-08-24 17:55:17 -0400

    brought back logging

--- api/opentrons/server/main.py
+++ api/opentrons/server/main.py
@@ -8,32 +8,32 @@ from logging.config import dictConfig
 
 # TODO(artyom): might as well use this:
 # https://pypi.python.org/pypi/logging-color-formatter
-# logging_config = dict(
-#     version=1,
-#     formatters={
-#         'basic': {
-#             'format':
-#             '%(asctime)s %(name)s %(levelname)s [Line %(lineno)s] %(message)s'  # noqa: E501
-#         }
-#     },
-#     handlers={
-#         'debug': {
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'basic',
-#         }
-#     },
-#     loggers={
-#         '__main__': {
-#             'handlers': ['debug'],
-#             'level': logging.DEBUG
-#         },
-#         'opentrons.server': {
-#             'handlers': ['debug'],
-#             'level': logging.DEBUG
-#         },
-#     }
-# )
-# dictConfig(logging_config)
+logging_config = dict(
+    version=1,
+    formatters={
+        'basic': {
+            'format':
+            '%(asctime)s %(name)s %(levelname)s [Line %(lineno)s] %(message)s'  # noqa: E501
+        }
+    },
+    handlers={
+        'debug': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'basic',
+        }
+    },
+    loggers={
+        '__main__': {
+            'handlers': ['debug'],
+            'level': logging.DEBUG
+        },
+        'opentrons.server': {
+            'handlers': ['debug'],
+            'level': logging.DEBUG
+        },
+    }
+)
+dictConfig(logging_config)
 
 log = logging.getLogger(__name__)
 
