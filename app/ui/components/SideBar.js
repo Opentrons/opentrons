2baef25514d2ce061e849aeb7285b5b2e7d086e5
Kadee80 <kaa328@nyu.edu>
2017-09-01 15:58:25 -0400

    added placeholder icons into sidebar

--- app/ui/components/SideBar.js
+++ app/ui/components/SideBar.js
@@ -16,6 +16,30 @@ const UploadMenu = props => {
   )
 }
 
+// TODO: (ka) these are just placeholder icons, refactor to reuse same component for upload, design, setup
+// with icon, style, and handler as props
+const DesignMenu = props => {
+  const {onNavClick} = props
+  return (
+    <section className={styles.design_menu} onClick={onNavClick}>
+      <div className={styles.design_icon}>
+        <img src='../ui/img/icon_design.svg' alt='design' />
+      </div>
+    </section>
+  )
+}
+
+const SetupMenu = props => {
+  const {onNavClick} = props
+  return (
+    <section className={styles.setup_menu} onClick={onNavClick}>
+      <div className={styles.setup_icon}>
+        <img src='../ui/img/icon_setup.svg' alt='design' />
+      </div>
+    </section>
+  )
+}
+
 const ConnectionIndicator = props => {
   const {isConnected, onNavClick} = props
   // TODO(mc): handle connection in progress (state is in place for this)
@@ -42,6 +66,8 @@ export default function SideBar (props) {
     <aside className={classnames(grid.nav_panel, { [grid.open]: isNavPanelOpen })}>
       <nav className={styles.nav_icons} >
         <UploadMenu {...props} />
+        <DesignMenu {...props} />
+        <SetupMenu {...props} />
         <ConnectionIndicator {...props} />
       </nav>
       <section className={styles.nav_info}>
