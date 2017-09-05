b3853f47bd35d73e5f79ce0ef754e7744486f205
Kadee80 <kaa328@nyu.edu>
2017-09-05 14:45:17 -0400

    fix run control styles

--- app/ui/components/App.js
+++ app/ui/components/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import grid from './Grid.css'
+import Button from './Button'
 import SideBar from './SideBar'
 import RunControl from './RunControl'
 import RunLog from './RunLog'
+import grid from './Grid.css'
 
 export default function App (props) {
   const {
     // state
-    isReadyToRun
+    isRunning,
+    isReadyToRun,
+    onRunClick
   } = props
   let runControl
   let runLog
@@ -16,7 +19,18 @@ export default function App (props) {
   // mock prop for styling
   const startTime = Date.now()
   const timeRemaining = '00:03:25' // swap out for timer
-
+  let runButton
+  if (!isRunning) {
+    runButton = (
+      <Button
+        onClick={onRunClick}
+        disabled={!isReadyToRun}
+        style={grid.run}
+      >
+        Run Job
+      </Button>
+    )
+  }
   if (isReadyToRun) {
     runControl = (
       <RunControl
@@ -32,7 +46,9 @@ export default function App (props) {
     <div className={grid.wrapper}>
       <SideBar {...props} />
       <main className={grid.task}>
-        <header className={grid.header} />
+        <header className={grid.header}>
+          {runButton}
+        </header>
         {runControl}
         {runLog}
       </main>
@@ -41,5 +57,5 @@ export default function App (props) {
 }
 
 App.propTypes = {
-  onNavClick: PropTypes.func.isRequired
+  onRunClick: PropTypes.func.isRequired
 }
