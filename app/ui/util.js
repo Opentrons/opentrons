24acf04c8718e5f95b60226cb52e7ea19906b876
Mike Cousins <mike@opentrons.com>
2017-09-08 00:15:19 -0400

    Remove remaining code from Vue app

--- app/ui/util.js
+++ app/ui/util.js
@@ -1,25 +1,4 @@
 // utility functions
-
-export function getUserId () {
-  return getUserProfile().user_id || null
-}
-
-export function getUserEmail () {
-  return getUserProfile().email || null
-}
-
-export function getUserProfile () {
-  return JSON.parse(localStorage.getItem('profile') || '{}')
-}
-
-export function isAuthenticated () {
-  const profile = localStorage.getItem('profile')
-  const idToken = localStorage.getItem('id_token')
-  if (profile == null) return false
-  if (idToken == null) return false
-  return true
-}
-
 export function makeActionName (moduleName, actionName) {
   return `${moduleName}:${actionName}`
 }
