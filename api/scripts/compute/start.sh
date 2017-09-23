d0c041005fd7e497bc7dae80a69456e4de3294f3
Jared Greene <jared@opentrons.com>
2017-09-23 11:28:27 -0700

    resin image build

--- api/scripts/compute/start.sh
+++ api/scripts/compute/start.sh
@@ -4,5 +4,5 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 echo "[BOOT] Starting server"
 cd /usr/src/api
-python /usr/src/api/server/main.py
+python /usr/src/api/opentrons/server/main.py
 
