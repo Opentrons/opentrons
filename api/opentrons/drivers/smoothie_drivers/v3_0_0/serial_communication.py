b5d50d2a8c7f99e9522d25fb6af19eeb03dfb405
Jared Greene <jared@opentrons.com>
2017-09-25 17:43:33 -0400

    driver started

---
+++ api/opentrons/drivers/smoothie_drivers/v3_0_0/serial_communication.py
@@ -0,0 +1,87 @@
+import serial
+from serial.tools import list_ports
+import contextlib
+
+DRIVER_ACK = b'ok\r\nok\r\n'
+RECOVERY_TIMEOUT = 10
+DEFAULT_SERIAL_TIMEOUT = 5
+SMOOTHIE_BOARD_NAME = 'FT232R'
+BAUDRATE = 14400
+
+
+def get_ports(device_name):
+    '''Returns all serial devices with a given name'''
+    filtered_devices = filter(
+        lambda device: device_name in device[1],
+        list_ports.comports()
+    )
+    device_ports =  [device[0] for device in filtered_devices]
+    return device_ports
+
+
+@contextlib.contextmanager
+def serial_with_temp_timeout(serial_connection, timeout):
+    '''Implements a temporary timeout for a serial connection'''
+    saved_timeout = serial_connection.timeout
+    if timeout is not None:
+        serial_connection.timeout = timeout
+    yield serial_connection
+    serial_connection.timeout = saved_timeout
+
+
+def _parse_smoothie_response(response):
+    if 'ALARM' in
+    if DRIVER_ACK in response:
+        return response
+    else:
+        return ''
+
+def clear_buffer(serial_connection):
+    serial_connection.reset_input_buffer()
+
+def _write_to_device_and_return(cmd, device_connection):
+    '''Writes to a serial device.
+    - Formats command
+    - Wait for ack return
+    - return parsed response'''
+    command = cmd + '\r\n'
+    # print("writing to smoothie: {}".format(command.encode()))
+    device_connection.write(command.encode())
+    response = device_connection.read_until(DRIVER_ACK)
+    clean_response = _parse_smoothie_response(response)
+    return clean_response
+
+
+def _connect(port_name, baudrate):
+    return serial.Serial(port_name, baudrate=baudrate, timeout=DEFAULT_SERIAL_TIMEOUT)
+
+
+def _attempt_command_recovery(command, serial_conn):
+    '''Recovery after following a failed write_and_return() atempt'''
+    with serial_with_temp_timeout(serial_conn, RECOVERY_TIMEOUT) as device:
+       response =  _write_to_device_and_return(command, device)
+    if not response:
+        raise RuntimeError(
+            "Recovery attempted - no valid smoothie response "
+            "for command: {} in {} seconds".format(command, RECOVERY_TIMEOUT))
+    return response
+
+
+def write_and_return(command, serial_connection, timeout=None):
+    '''Write a command and return the response'''
+    clear_buffer(serial_connection)
+    with serial_with_temp_timeout(serial_connection, timeout) as device_connection:
+        response = _write_to_device_and_return(command, device_connection)
+    if not response: # this could be more robust - maybe a `try` `except`?
+        response = _attempt_command_recovery(command, serial_connection)
+    return response
+
+
+def connect(device_name=SMOOTHIE_BOARD_NAME):
+    '''
+    Creates a serial connection
+    :param device_name: defaults to 'Smoothieboard'
+    :return: serial.Serial connection
+    '''
+    smoothie_port = get_ports(device_name=device_name)[0]
+    return _connect(port_name=smoothie_port, baudrate=BAUDRATE)
