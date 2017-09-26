0210be59a59f3fb224b4312e7813ddbaa4da5bf9
astaff <artyom.astafurov@gmail.com>
2017-09-26 07:02:52 -0400

    rename clibration to clibration_manager in in shell

--- api/opentrons/session/shell.py
+++ api/opentrons/session/shell.py
@@ -5,4 +5,4 @@ from .calibration import CalibrationManager
 class Shell:
     def __init__(self, loop=None):
         session_manager = SessionManager(loop=loop)
-        calibration = CalibrationManager(loop=loop)
+        calibration_manager = CalibrationManager(loop=loop)
