c2485d0b11af21327c92617823023283a343787b
Jared Greene <jared@opentrons.com>
2017-09-26 19:31:03 -0400

    Expand on Probe and Base classes

--- api/opentrons/robot/gantry.py
+++ api/opentrons/robot/gantry.py
@@ -42,3 +42,33 @@ class Gantry:
 
     def add_instrument(self, instrument_slot, instrument):
         instrument.motor = InstrumentMotor(self.driver, instrument_slot)
+
+
+
+
+
+# FIXME: The following should eventually live in the robot
+#
+# def setup_robot(self):
+#     self.setup_pose_tracking()
+#     self.setup_deck()
+#     self.setup_gantry(self._driver)
+#     self.setup_frame_base()
+#
+# def setup_pose_tracking(self):
+#     self.pose_tracker = pose_tracker.PoseTracker()
+#
+# def setup_deck(self):
+#     self._deck = containers.Deck()
+#     # Setup Deck as root object for pose tracker
+#     self.pose_tracker.create_root_object(
+#         self._deck, *self._deck._coordinates
+#     )
+#     self.add_slots_to_deck()
+#
+#
+# def setup_gantry(self):
+#     self._gantry = Gantry()
+#
+# def setup_frame_base(self):
+#     self._frame_base = FrameBase()
