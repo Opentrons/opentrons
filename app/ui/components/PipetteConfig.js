8e427bb5532c3a50e3c80f7e06dcc35f16e93f35
Kadee80 <kaa328@nyu.edu>
2017-09-28 11:24:00 -0400

    static single pipette config component

--- app/ui/components/PipetteConfig.js
+++ app/ui/components/PipetteConfig.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import classnames from 'classnames'
 import styles from './PipetteConfig.css'
 
 export default function PipetteConfig (props) {
   const {side} = props
   return (
     <section className={styles.pipette_group}>
-      <div className={styles.pipette_wrapper}>
-        <button className={styles.pipette_toggle}>{side}</button>
+
+      <div className={classnames(styles.pipette_wrapper, styles[side])}>
+
+        <div className={styles.pipette_info}>
+          <button className={styles.pipette_toggle}>{side}</button>
+
+          <h2 className={styles.title}>Pipette</h2>
+          <h3>Num Channels + Volume</h3>
+          <h2 className={styles.title}>Suggested Tip Type</h2>
+          <h3>Volume ul</h3>
+          <div className={styles.info}>
+            <span className={styles.alert}>!</span>
+            <p>For accuracy, tip dimensions must be defined using the Tip Probe tool.</p>
+          </div>
+          <button className={styles.btn_probe}>Prepare Pipette Tip</button>
+        </div>
+
+        <div className={styles.pipette_icon}>
+          img
+        </div>
+
       </div>
-      <h3 className={styles.title}>Pipette</h3>
+
     </section>
   )
 }
