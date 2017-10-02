803fbff75ac60ca52e4936709bfd64a5613fd3da
astaff <artyom.astafurov@gmail.com>
2017-10-02 17:49:57 -0400

    add test for serializing class object attributes

--- api/tests/opentrons/server/test_server.py
+++ api/tests/opentrons/server/test_server.py
@@ -9,6 +9,8 @@ from uuid import uuid4 as uuid
 
 
 class Foo(object):
+    STATIC = 'static'
+
     def __init__(self, value):
         self.value = value
 
@@ -87,15 +89,18 @@ async def test_call(session, root):
 
 @pytest.mark.parametrize('root', [Foo(0)])
 async def test_init(session, root):
+    serialized_type = session.server.call_and_serialize(lambda: type(root))
     expected = {
         'root': {
             'i': id(root),
             't': type_id(root),
             'v': {'value': 0}
         },
-        'type': session.server.call_and_serialize(lambda: type(root)),
+        'type': serialized_type,
         '$': {'type': rpc.CONTROL_MESSAGE}}
 
+    assert serialized_type['v']['STATIC'] == 'static', \
+        'Class attributes are serialized correctly'
     res = await session.socket.receive_json()
     assert res == expected
 
