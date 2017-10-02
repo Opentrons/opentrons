2c0f275a032f2e645cc1765a8eb6f45a669c19d2
Kadee80 <kaa328@nyu.edu>
2017-10-02 15:52:39 -0400

    list pipette links with optional axis params, list labware with optional params - only show once pipettes are probed

--- app/ui/components/SetupPanel.js
+++ app/ui/components/SetupPanel.js
@@ -1,13 +1,71 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import styles from './NavPanel.css'
+import styles from './SetupPanel.css'
+
+function PipetteLinks (props) {
+  const {axis, volume, channels, isProbed} = props
+  const url = `/setup-instruments/${axis}`
+  const style = isProbed
+    ? styles.confirmed
+    : styles.alert
+  return (
+    <li key={axis}>
+      <Link to={url} className={style}>
+        <span className={styles.axis}>{axis}</span>
+        <span className={styles.type}>{channels}-Channel ({volume}ul)</span>
+      </Link>
+    </li>
+  )
+}
+
+function LabwareLinks (props) {
+  const {name, slot, type, isConfirmed} = props
+  const url = `/setup-deck/${slot}`
+  const style = isConfirmed
+    ? styles.confirmed
+    : styles.alert
+  return (
+    <li key={slot}>
+      <Link to={url} className={style}>
+        [{slot}] {name}
+      </Link>
+    </li>
+  )
+}
 
 export default function SetupPanel (props) {
+  const {instruments, labware, instrumentsConfirmed} = props
+  const instrumentList = instruments.map((i) => PipetteLinks({
+    ...i
+  }))
+  const labwareList = labware.map((l) => LabwareLinks({
+    ...l
+  }))
+  const pipetteSetup =
+  <section className={styles.pipette_group}>
+    <Link to='/setup-instruments'>Pipette Setup</Link>
+    <ul>
+      {instrumentList}
+    </ul>
+  </section>
+
+  let labwareSetup
+  if (instrumentsConfirmed) {
+    labwareSetup =
+    <section className={styles.labware_group}>
+      <Link to='/setup-deck'>Labware Setup</Link>
+      <ul>
+        {labwareList}
+      </ul>
+    </section>
+  }
+
   return (
     <div className={styles.nav_panel}>
+      <h1>Prepare Robot for RUN</h1>
       <section className={styles.links}>
-        <Link to='/setup-instruments'>Set Up Pipettes</Link>
-        <Link to='/setup-deck'>Set Up Labware</Link>
+        {pipetteSetup}
+        {labwareSetup}
         <Link to='/run'>Run</Link>
       </section>
     </div>
--- app/ui/containers/ConnectedSetupPanel.js
+++ app/ui/containers/ConnectedSetupPanel.js
@@ -5,12 +5,13 @@ import SetupPanel from '../components/SetupPanel'
 
 const mapStateToProps = (state) => {
   return {
+    instrumentsConfirmed : true, // all pipettes confirmed ? show labware
     instruments: [
       {
         axis: 'left',
         channels: 'single',
         volume: 200,
-        isProbed: false
+        isProbed: true
       },
       {
         axis: 'right',
@@ -19,6 +20,8 @@ const mapStateToProps = (state) => {
         isProbed: false
       }
     ],
+    tipracksConfirmed: false, // all tipracks confirmed ? unlock other labware
+    labwareConfirmed: false, // all labware confirmed ? show run button
     labware: [
       {
         name: 'tiprack',
@@ -29,7 +32,23 @@ const mapStateToProps = (state) => {
         name: 'tiprack2',
         slot: 'B2',
         type: 'tiprack-200ul',
-        isConfirmed: true
+        isConfirmed: false
+      },
+      {
+        name: 'trough',
+        slot: 'C1',
+        type: 'trough-12row',
+        isConfirmed: false
+      }, {
+        name: 'plate',
+        slot: 'D1',
+        type: '96-PCR-flat',
+        isConfirmed: false
+      }, {
+        name: 'tuberack',
+        slot: 'D2',
+        type: 'tube-rack-2ml',
+        isConfirmed: false
       }
     ]
   }
