8daee56a65e1db007c6d0cca81588bbec7d81e54
Kadee80 <kaa328@nyu.edu>
2017-10-04 09:40:34 -0400

    initial jod modal nested route, container, component. Remove position, replace with numerical slots, add ids for legacy slot names

--- app/ui/pages/SetupDeck.js
+++ app/ui/pages/SetupDeck.js
@@ -1,13 +1,19 @@
 // setup instruments component
 import React from 'react'
+import {Route} from 'react-router'
 import Header from '../components/Header'
 import Page from '../components/Page'
+import ConnectedDeckConfig from '../containers/ConnectedDeckConfig'
+import ConnectedJogModal from '../containers/ConnectedJogModal'
 
-export default function SetupDeckPage () {
+export default function SetupDeckPage (props) {
+  const {match} = props
+  const currentSlot = match.params.slot || '1'
   return (
     <Page>
       <Header />
-      <ConnectedDeckConfig />
+      <ConnectedDeckConfig currentSlot={currentSlot} />
+      <Route path={`${match.url}/jog`} component={ConnectedJogModal} />
     </Page>
   )
 }
