1ab621795e05264bdfb7a173a0c90204476e339c
Kadee80 <kaa328@nyu.edu>
2017-10-05 10:32:05 -0400

    static jog buttons for wriring

--- app/ui/components/JogModal.js
+++ app/ui/components/JogModal.js
@@ -8,7 +8,29 @@ export default function JogModal (props) {
   return (
     <div className={styles.modal_wrapper}>
       <div className={styles.jog_modal}>
-        <div className={styles.jog_controls} />
+        <div className={styles.jog_controls}>
+
+          <button
+            className={styles.btn_x_neg}
+            onClick={() => console.log ('jog x neg')}
+          >
+            &#8592;
+          </button>
+
+          <button
+            className={styles.btn_x_pos}
+            onClick={() => console.log ('jog x neg')}
+          >
+            &#8594;
+          </button>
+
+          <button className={styles.btn_y_neg}>&#8593;</button>
+          <button className={styles.btn_y_pos}>&#8595;</button>
+
+          <button className={styles.btn_z_neg}>&#8593;</button>
+          <button className={styles.btn_z_pos}>&#8595;</button>
+
+        </div>
         <Link to={url} className={styles.close}>X</Link>
         <button className={styles.btn_confirm}>Confirm Position</button>
         <p>Using the arrow buttons, jog the pipette along the x, y and z
