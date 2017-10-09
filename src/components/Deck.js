32ef54502537928feb98c192a0a313063a41f2cf
IanLondon <ianlondondu@gmail.com>
2017-10-09 17:35:11 -0400

    protocol designer: basic styling to deck and container selectors

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,10 +2,55 @@ import React from 'react'
 
 import styles from '../css/style.css'
 
-console.log({styles})
-
-const Deck = () => (
-  <div className={styles.deck}>Xss</div>
+const AddLabware = () => (
+  <div
+    onClick={e => console.log('TODO: addLabware action')}
+    className={styles.addLabware}>
+      Add Labware
+  </div>
 )
+
+const DeckSlot = ({slotName}) => (
+  <div className={styles.deckSlot}>
+    <label>{slotName}</label>
+    <AddLabware />
+    {slotName === 'A1' && <LabwareDropdown />}
+  </div>
+)
+
+const LabwareDropdown = () => (
+  <div className={styles.labwareDropdown}>
+    <label>Labware Type</label>
+    <div className='close'>X</div>
+    <ul>
+      <li>Tip Rack</li>
+      <li>Tube Rack</li>
+      <li>Well Plate ▼</li>
+      <li><ul>
+        <li>96 Deep Well Plate</li>
+        <li>96 Well Plate (Tall)</li>
+        <li>96 Well Plate (Flat)</li>
+      </ul></li>
+      <li>PCR Strip</li>
+      <li>And so</li>
+      <li>on and so on</li>
+    </ul>
+  </div>
+)
+
+const Deck = () => {
+  const slotnames = [
+    'A3', 'B3', 'C3', 'D3', 'E3',
+    'A2', 'B2', 'C2', 'D2', 'E2',
+    'A1', 'B1', 'C1', 'D1', 'E1'
+  ]
+
+  return (
+    <div className={styles.deck}>
+      {slotnames.map((slotName, i) =>
+        <DeckSlot key={i} slotName={slotName} />
+      )}</div>
+  )
+}
 
 export default Deck
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import Deck from './Deck.js'
 
 const Home = () => (
   <div>
-    <h2>Hey. This is adapted from <a href='https://github.com/vebjorni/react-boilerplate-app'>vebjorni/react-boilerplate-app</a></h2>
     <Deck />
+    <h2>Select labware you wish to add ingredients to</h2>
   </div>
 )
 
