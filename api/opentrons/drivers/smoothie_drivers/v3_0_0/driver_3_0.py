d14eaf30b679bf8b0a14cabef4e18af8b8e7b119
Jared Greene <jared@opentrons.com>
2017-10-26 20:49:48 -0400

    remove print statements and fix linting

--- api/opentrons/drivers/smoothie_drivers/v3_0_0/driver_3_0.py
+++ api/opentrons/drivers/smoothie_drivers/v3_0_0/driver_3_0.py
@@ -1,6 +1,5 @@
 from opentrons.drivers.smoothie_drivers.v3_0_0 import serial_communication
 from os import environ
-from functools import wraps
 
 
 '''
@@ -8,22 +7,22 @@ from functools import wraps
 - Driver is the only system component that knows about GCODES or how smoothie
   communications
 
-- Driver is NOT responsible interpreting the motions in any way or knowing anything
-  about what the axes are used for
+- Driver is NOT responsible interpreting the motions in any way or
+  knowing anything about what the axes are used for
 '''
 
-# DEFAULT_STEPS_PER_MM = 'M92 X80 Y80 Z400 A400 B767.38 C767.38' #Avagdro
+# DEFAULT_STEPS_PER_MM = 'M92 X80 Y80 Z400 A400 B767.38 C767.38'  # Avagdro
 
-DEFAULT_STEPS_PER_MM = 'M92 X160 Y160 Z800 A800 B767.38 C767.38' #Ibn
+DEFAULT_STEPS_PER_MM = 'M92 X160 Y160 Z800 A800 B767.38 C767.38'  # Ibn
 
 DEFAULT_MAX_AXIS_SPEEDS = 'M203.1 X300 Y200 Z50 A50 B8 C8'
 DEFAULT_ACCELERATION = 'M204 S1000 X4000 Y3000 Z2000 A2000 B3000 C3000'
 DEFAULT_CURRENT_CONTROL = 'M907 X1.0 Y1.2 Z0.9 A0.9 B0.6 C0.6'
 
 
-MOVEMENT_ERROR_MARGIN = 1/160 # Largest movement in mm for any step
+MOVEMENT_ERROR_MARGIN = 1/160  # Largest movement in mm for any step
 
-AXES_SAFE_TO_HOME = 'XZABC' # Y cannot be homed without homing all
+AXES_SAFE_TO_HOME = 'XZABC'  # Y cannot be homed without homing all
 AXES = 'XYZABC'
 
 SEC_PER_MIN = 60
@@ -41,19 +40,18 @@ GCODES = {'HOME': 'G28.2',
           'SET_POWER': 'M907'}
 
 
-
 def _position_at_expected_thresh(position, cached_position):
     '''Checks parsed position against cache and some threshold'''
-    print('checking read position: {} against cached position {}'.format(position, cached_position))
     for axis in AXES.lower():
         if axis not in position:
             position[axis] = cached_position[axis]
         if abs(position[axis] - cached_position[axis]) > POSITION_THRESH:
             raise RuntimeError(
                 '[DRIVER ERROR] actual position: {}\ncached position: {}'
-                               .format(position, cached_position)
+                .format(position, cached_position)
             )
     return True
+
 
 def _parse_axis_values(raw_axis_values):
     parsed_values = raw_axis_values.split(' ')
@@ -63,6 +61,7 @@ def _parse_axis_values(raw_axis_values):
         for s in parsed_values
     }
     return position
+
 
 class SmoothieDriver_3_0_0:
 
@@ -75,8 +74,11 @@ class SmoothieDriver_3_0_0:
 
     def _update_position_cache(self, target):
         self.cached_position.update({
-            axis.lower(): value for axis, value in target.items() if value is not None
+            axis.lower(): value for axis, value
+            in target.items()
+            if value is not None
         })
+
         self.log += [self.cached_position.copy()]
 
     def _check_position_for_error(self, raw_position):
@@ -85,14 +87,15 @@ class SmoothieDriver_3_0_0:
         return parsed_position
 
     def update_position(self, is_retry=False):
-        print("[Driver] refreshing driver position cache")
         if self.simulating:
             updated_position = self.cached_position
 
         else:
             try:
-                position_response = self._send_command(GCODES['CURRENT_POSITION'])
-                updated_position = self._check_position_for_error(position_response)
+                position_response = \
+                    self._send_command(GCODES['CURRENT_POSITION'])
+                updated_position = \
+                    self._check_position_for_error(position_response)
             except TypeError as e:
                 if is_retry:
                     raise e
@@ -102,9 +105,6 @@ class SmoothieDriver_3_0_0:
         self._update_position_cache(updated_position)
         self._accumulated_position_error = 0
 
-
-    # FIXME (JG 9/28/17): Should have a more thought out
-    # way of simulating vs really running
     def connect(self):
         self.simulating = False
         if environ.get('ENABLE_VIRTUAL_SMOOTHIE', '').lower() == 'true':
@@ -121,21 +121,9 @@ class SmoothieDriver_3_0_0:
     def position(self):
         return self.cached_position
 
-    @property
-    def switch_state(self):
+    def get_switch_state(self):
         '''Returns the state of all SmoothieBoard limit switches'''
         return self._send_command(GCODES['SWITCH_STATUS'])
-
-    @property
-    def power(self):
-        pass
-
-    def set_power(self, power_dict):
-        pass
-
-    @property
-    def speed(self):
-        pass
 
     def set_speed(self, value):
         ''' set total movement speed in mm/second'''
@@ -145,7 +133,9 @@ class SmoothieDriver_3_0_0:
 
     def set_power(self, axis, value):
         ''' set total movement speed in mm/second'''
-        command = '{}{}{}'.format(GCODES['SET_POWER'], axis.upper(), str(value))
+        command = '{}{}{}'.format(
+            GCODES['SET_POWER'], axis.upper(), str(value)
+        )
         self._send_command(command)
 
     # ----------- Private functions --------------- #
@@ -163,7 +153,6 @@ class SmoothieDriver_3_0_0:
         if self.simulating:
             pass
         else:
-            # print('[DRIVER] sending command {}'.format(repr(command_line)))
             return serial_communication.write_and_return(
                 command_line, self.connection, timeout)
 
@@ -204,17 +193,24 @@ class SmoothieDriver_3_0_0:
             self.update_position()
 
     def home(self, axis=None):
-        homed_positions = {'X': 394, 'Y': 344, 'Z': 227, 'A': 227, 'B': 18.9997, 'C': 18.9997}
+        homed_positions = {
+            'X': 394, 'Y': 344, 'Z': 227, 'A': 227, 'B': 18.9997, 'C': 18.9997
+        }
         if not axis:
             self._home_all()
             self._update_position_cache(homed_positions)
 
         else:
-            axes_to_home = [ax for ax in axis.upper() if ax in AXES_SAFE_TO_HOME]
+            axes_to_home = [
+                ax for ax in axis.upper()
+                if ax in AXES_SAFE_TO_HOME
+            ]
             if axes_to_home:
                 command = GCODES['HOME'] + ''.join(axes_to_home)
                 self._send_command(command)
-                self._update_position_cache({axis: homed_positions[axis] for axis in axes_to_home})
+                self._update_position_cache({
+                    axis: homed_positions[axis] for axis in axes_to_home
+                })
 
             else:
                 raise RuntimeError('Cannot home axis: {}'.format(axis))
@@ -222,7 +218,8 @@ class SmoothieDriver_3_0_0:
     def delay(self, seconds):
         seconds = int(seconds)
         milliseconds = (seconds % 1.0) * 1000
-        command = GCODES['DWELL'] + 'S' + str(seconds) + 'P' + str(milliseconds)
+        command = \
+            GCODES['DWELL'] + 'S' + str(seconds) + 'P' + str(milliseconds)
         self._send_command(command)
 
     def probe_axis(self, axis, probing_distance):
@@ -235,7 +232,7 @@ class SmoothieDriver_3_0_0:
         else:
             raise RuntimeError("Cant probe axes {}".format(axis))
 
-    #TODO: Write GPIO low
+    # TODO: Write GPIO low
     def kill(self):
         pass
 
