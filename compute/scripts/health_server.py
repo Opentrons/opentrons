9775ae65756c00e0d58999024878b00f25b52f89
Jared Greene <jared@opentrons.com>
2017-11-12 20:41:16 -0500

    add info to health endpoint

--- compute/scripts/health_server.py
+++ compute/scripts/health_server.py
@@ -1,3 +1,4 @@
+import os
 from aiohttp import web
 
 from avahi.service import AvahiService
@@ -7,13 +8,22 @@ IP_ADDRESS = '0.0.0.0'
 PORT = '31951'
 
 
-async def health(request):
-    return web.Response(text=machine_info())
+async def status(request):
+    robot_status = {
+            'is_api_running': os.environ['API_IS_RUNNING'],
+            'machine_name': os.environ['ROBOT_NAME'],
+            'commit_hash': os.environ['RESIN_APP_RELEASE'],
+            'resin_app': os.environ['RESIN_APP_NAME'],
+            'resin_device_name': os.environ['RESIN_DEVICE_NAME_AT_INIT']
+        }
+
+    return web.json_response(robot_status)
 
 
 
 if __name__ == "__main__":
-    local_service = AvahiService("opentrons", "_http._tcp", PORT)
+    #TODO jmg 11/13/17: advertise an http service
+    local_service = AvahiService("opentrons", "_ssh._tcp", PORT)
     health_server = web.Application()
-    health_server.route.add_get('/', health)
+    health_server.route.add_get('/', status)
     web.run_app(health_server, host=IP_ADDRESS, port=PORT)
--- compute/scripts/main_start.sh
+++ compute/scripts/main_start.sh
@@ -7,6 +7,7 @@ echo "[BOOT] Starting DHCP server"
 . /usr/src/compute/scripts/dhcp_server_init.sh
 
 
+export API_IS_RUNNING=true
 echo "[BOOT] Starting server"
 . /usr/src/compute/scripts/api_server_init.sh
-
+export API_IS_RUNNING=false
