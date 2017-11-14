8602db1e0b2c1f25fc131694db11643707c82996
Jared Greene <jared@opentrons.com>
2017-11-14 11:00:43 -0500

    Use resin device name instead of UUID for MDNS ad

--- compute/scripts/announce_mdns.py
+++ compute/scripts/announce_mdns.py
@@ -3,6 +3,6 @@ import os
 from avahi.service import AvahiService
 
 
-UUID = os.environ.get('RESIN_DEVICE_UUID')
-service_name = 'opentrons-{}'.format(UUID[0:6])
+DEVICE_NAME = os.environ.get('RESIN_DEVICE_NAME_AT_INIT')
+service_name = 'opentrons-{}'.format(DEVICE_NAME)
 AvahiService(service_name, "_http._tcp", port=31951, keep_alive=True)
