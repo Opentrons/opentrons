bb67b65d0329b0d67e92fefb4fec3a7125476ba0
Mike Cousins <mike@opentrons.com>
2017-12-05 13:37:24 -0500

    (app) Update discovery to poll directly connected robots
    - Updated discovery.js to poll /health at fixed local IP every sec
    - Updated MDNS dev script to only advertise if local API is found

--- app/scripts/advertise-local-api.js
+++ app/scripts/advertise-local-api.js
@@ -2,6 +2,12 @@
 // TODO(mc, 2017-10-31): remove this file once API can advertise for itself
 const bonjour = require('bonjour')()
 const randomstring = require('randomstring')
+const request = require('superagent')
+
+const LOCAL_API_POLL_INTERVAL_MS = 5000
+const LOCAL_API_HOST = 'localhost'
+const LOCAL_API_PORT = 31950
+const LOCAL_API_HEALTH_URL = `http://${LOCAL_API_HOST}:${LOCAL_API_PORT}/health`
 
 const id = randomstring.generate({
   length: 6,
@@ -19,10 +25,30 @@ const service = {
   type: 'http'
 }
 
-bonjour.publish(service)
-  .on('up', () => {
-    console.log(`Published MDNS service "${service.name}" on ${service.host}`)
-  })
-  .on('error', (error) => {
-    console.error('Error publishing MDNS service', error)
-  })
+startPolling()
+
+function startPolling () {
+  setTimeout(pollAndPublish, LOCAL_API_POLL_INTERVAL_MS)
+}
+
+function pollAndPublish () {
+  request.get(LOCAL_API_HEALTH_URL)
+    .ok((response) => response.status === 200)
+    .then(() => {
+      console.log(`Found local API at ${LOCAL_API_HOST}:${LOCAL_API_PORT}`)
+      publish()
+    })
+    .catch(startPolling)
+}
+
+function publish () {
+  bonjour.publish(service)
+    .on('up', () => {
+      console.log(`Published MDNS service "${service.name}" on ${service.host}`)
+    })
+    .on('error', (error) => {
+      console.error('Error publishing MDNS service', error)
+      // retry
+      startPolling()
+    })
+}
