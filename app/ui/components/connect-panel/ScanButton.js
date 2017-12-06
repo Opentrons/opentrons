4e76d2458d2d802583ce5cd159d2337c644e6187
Kadee80 <kaa328@nyu.edu>
2017-12-06 13:07:22 -0500

    Position title bar refresh btn
    - temporarily remove position:relative from panel_contents
    - absolute position refresh title button as quickfix.
    (will be revisited with component library work)
    - remove no longer used sidebar.js and style sheet

--- app/ui/components/connect-panel/ScanButton.js
+++ app/ui/components/connect-panel/ScanButton.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import classnames from 'classnames'
 import Button from '../Button'
 import Icon, {REFRESH} from '../Icon'
 import styles from './connect-panel.css'
@@ -7,12 +6,12 @@ import styles from './connect-panel.css'
 export default function ScanButton (props) {
   const {onScanClick, titleBtn} = props
   const style = titleBtn
-  ? classnames(styles.scan_button, styles.title_button)
+  ? styles.title_button
   : styles.scan_button
 
   return (
     <Button
-      style={classnames(styles.scan_button, style)}
+      style={style}
       onClick={onScanClick}
       title='Scan for robots'
     >
