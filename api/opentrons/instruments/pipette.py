ca352fb18da469a3d60cd7d7d001c4da7c8f238c
andySigler <andrewsigler1@gmail.com>
2017-12-08 10:51:21 -0500

    removes fast_home() from pick_up_tip()

--- api/opentrons/instruments/pipette.py
+++ api/opentrons/instruments/pipette.py
@@ -828,8 +828,7 @@ class Pipette:
                 length=self.robot.config.tip_length[self.mount][self.type]
             )
             stall_distance = abs(plunge_depth)
-            self.robot.poses = self.instrument_mover.fast_home(
-                self.robot.poses, safety_margin=stall_distance)
+            self.robot.poses = self.instrument_mover.home(self.robot.poses)
 
             return self
 
@@ -906,9 +905,8 @@ class Pipette:
             d = self._get_plunger_position('drop_tip')
             stall_distance = abs(b - d)
             if home_after:
-                self.robot.poses = self.instrument_actuator.fast_home(
-                    self.robot.poses, safety_margin=stall_distance)
-
+                self.robot.poses = self.instrument_actuator.home(
+                    self.robot.poses)
                 self.robot.poses = self.instrument_actuator.move(
                     self.robot.poses,
                     x=self._get_plunger_position('bottom')
