cb599a1f30516e9a993e994bccc4ab4afd263669
Mike Cousins <mike@opentrons.com>
2017-12-14 18:06:38 -0500

    (complib) Add alert and spinner icons

--- components/src/__tests__/icons.test.js
+++ components/src/__tests__/icons.test.js
@@ -2,9 +2,17 @@
 import React from 'react'
 import Renderer from 'react-test-renderer'
 
-import {Icon, BACK, REFRESH, USB, WIFI} from '..'
+import {Icon, ALERT, BACK, REFRESH, SPINNER, USB, WIFI} from '..'
 
 describe('icons', () => {
+  test('alert icon renders correctly', () => {
+    const tree = Renderer.create(
+      <Icon name={ALERT} className='foo' />
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
   test('back icon renders correctly', () => {
     const tree = Renderer.create(
       <Icon name={BACK} className='foo' />
@@ -16,6 +24,14 @@ describe('icons', () => {
   test('refresh icon renders correctly', () => {
     const tree = Renderer.create(
       <Icon name={REFRESH} className='foo' />
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('spinner icon renders correctly', () => {
+    const tree = Renderer.create(
+      <Icon name={SPINNER} className='foo' />
     ).toJSON()
 
     expect(tree).toMatchSnapshot()
@@ -35,5 +51,13 @@ describe('icons', () => {
     ).toJSON()
 
     expect(tree).toMatchSnapshot()
+  })
+
+  test('spin option adds spin class', () => {
+    const icon = Renderer.create(
+      <Icon name={SPINNER} spin />
+    ).root.findByType('svg')
+
+    expect(icon.props.className).toMatch(/\bspin\b/)
   })
 })
