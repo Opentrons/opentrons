a7b431e0e7dcb6aeef59509e97915f0c27299ec3
IanLondon <ianlondondu@gmail.com>
2017-12-15 18:57:34 -0500

    fix deck imports better; clean up makefile/travis

--- components/src/deck/index.js
+++ components/src/deck/index.js
@@ -1,7 +1,9 @@
-export {ContainerNameOverlay} from './ContainerNameOverlay'
-export {DeckFactory} from './DeckFactory'
-export {EmptyDeckSlot} from './EmptyDeckSlot'
-export {LabwareContainer} from './LabwareContainer.js'
-export {Plate} from './Plate'
-export {SlotOverlay} from './SlotOverlay.js'
-export {Well} from './Well'
+// TODO Ian 2017-12-15 why is this not working?
+//
+// export {DeckFactory} from './DeckFactory.js'
+// export {ContainerNameOverlay} from './ContainerNameOverlay.js'
+// export {EmptyDeckSlot} from './EmptyDeckSlot.js'
+// export {LabwareContainer} from './LabwareContainer.js'
+// export {Plate} from './Plate.js'
+// export {SlotOverlay} from './SlotOverlay.js'
+// export {Well} from './Well.js'
--- components/src/index.js
+++ components/src/index.js
@@ -11,7 +11,16 @@ export * from './utils'
 
 // Components
 export * from './buttons'
-export * from './deck'
+// export * from './deck/index.js'
 export * from './CenteredTextSvg'
 export * from './icons'
 export * from './structure'
+
+// Deck
+export {DeckFactory} from './deck/DeckFactory.js'
+export {ContainerNameOverlay} from './deck/ContainerNameOverlay.js'
+export {EmptyDeckSlot} from './deck/EmptyDeckSlot.js'
+export {LabwareContainer} from './deck/LabwareContainer.js'
+export {Plate} from './deck/Plate.js'
+export {SlotOverlay} from './deck/SlotOverlay.js'
+export {Well} from './deck/Well.js'
