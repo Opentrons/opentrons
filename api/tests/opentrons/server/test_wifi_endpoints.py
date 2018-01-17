7efea2e69bdfc7cec3e50adbc4e99ce588a14726
Benjamin Morris <bmorris458@gmail.com>
2018-01-17 15:58:38 -0500

    Add endpoints for listing, checking status, and configuring wifi networks

---
+++ api/tests/opentrons/server/test_wifi_endpoints.py
@@ -0,0 +1,36 @@
+import json
+from opentrons.server.main import init
+
+
+async def test_wifi_status(virtual_smoothie_env, loop, test_client):
+    app = init(loop)
+    cli = await loop.create_task(test_client(app))
+
+    expected = repr(json.dumps({'status': 'testing'}))
+    resp = await cli.get('/wifi/status')
+    text = await resp.text()
+    assert resp.status == 200
+    assert text == expected
+
+
+async def test_wifi_list(virtual_smoothie_env, loop, test_client):
+    app = init(loop)
+    cli = await loop.create_task(test_client(app))
+
+    expected = repr(json.dumps(['a', 'b', 'c']))
+    resp = await cli.get('/wifi/list')
+    text = await resp.text()
+    assert resp.status == 200
+    assert text == expected
+
+
+async def test_wifi_configure(virtual_smoothie_env, loop, test_client):
+    app = init(loop)
+    cli = await loop.create_task(test_client(app))
+
+    expected = "Configuration successful. SSID: this, PSK: that"
+    resp = await cli.post('/wifi/configure',
+                          json={'ssid': 'this', 'psk': 'that'})
+    text = await resp.text()
+    assert resp.status == 200
+    assert text == expected
