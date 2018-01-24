1524f3eda0d3a8cc1065afe3342ce74f0eda07ef
Benjamin Morris <btmorr@users.noreply.github.com>
2018-01-24 15:20:13 -0500

    Remove  from wifi endpoints (#667)

--- api/tests/opentrons/server/test_wifi_endpoints.py
+++ api/tests/opentrons/server/test_wifi_endpoints.py
@@ -6,7 +6,7 @@ async def test_wifi_status(virtual_smoothie_env, loop, test_client):
     app = init(loop)
     cli = await loop.create_task(test_client(app))
 
-    expected = repr(json.dumps({'status': 'testing'}))
+    expected = json.dumps({'status': 'testing'})
     resp = await cli.get('/wifi/status')
     text = await resp.text()
     assert resp.status == 200
@@ -17,7 +17,7 @@ async def test_wifi_list(virtual_smoothie_env, loop, test_client):
     app = init(loop)
     cli = await loop.create_task(test_client(app))
 
-    expected = repr(json.dumps(['a', 'b', 'c']))
+    expected = json.dumps(['a', 'b', 'c'])
     resp = await cli.get('/wifi/list')
     text = await resp.text()
     assert resp.status == 200
