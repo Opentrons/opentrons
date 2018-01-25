6cb86bcc88ef140458f3b440595f04166c330b67
Kadee80 <kaa328@nyu.edu>
2018-01-25 12:09:24 -0500

    Enable run link after tip probe complete
    - Flowify RunPanel and RunMessage RunButton
    - Conditional RunMesage text based on labwareConfirmed
    - Convert RunLink to RunButton, use PrimaryButton

--- app/src/components/setup-panel/RunMessage.js
+++ app/src/components/setup-panel/RunMessage.js
@@ -1,17 +1,36 @@
+// @flow
 import React from 'react'
 import InfoBox from '../InfoBox'
 import styles from './run-panel.css'
 
-export default function RunMessage () {
-  return (
-    <InfoBox className={styles.run_message}>
-      <p className={styles.run_message_item}>
-        Hurray, your robot is now ready! Click [RUN PROTOCOL] to start your run.
-      </p>
-      <p className={styles.run_message_item}>
-        Tip: Try a dry run prior to adding your samples and re-agents to avoid
-        wasting materials.
-      </p>
-    </InfoBox>
-  )
+type Props = {
+  labwareConfirmed: boolean
+}
+
+export default function RunMessage (props: Props) {
+  const {labwareConfirmed} = props
+  if (labwareConfirmed) {
+    return (
+      <InfoBox className={styles.run_message}>
+        <p className={styles.run_message_item}>
+          Confirming labware calibration is recomended but not required.
+        <p className={styles.run_message_item}>
+          Click [RUN PROTOCOL] to start your run.
+        </p>
+        </p>
+      </InfoBox>
+    )
+  } else {
+    return (
+      <InfoBox className={styles.run_message}>
+        <p className={styles.run_message_item}>
+          You can now proceed to labware calibration or
+          click [RUN PROTOCOL] to start your run.
+        </p>
+        <p className={styles.run_message_item}>
+          Tip: Calibrating labware is recomended, but not required.
+        </p>
+      </InfoBox>
+    )
+  }
 }
