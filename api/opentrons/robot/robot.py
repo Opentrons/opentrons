1d4e6bce242dee66542da035cb9bc05db2520735
Laura-Danielle <laura@opentrons.com>
2018-02-05 18:34:41 -0500

    Adds protections on containers.load() if a container is not found in the db

--- api/opentrons/robot/robot.py
+++ api/opentrons/robot/robot.py
@@ -820,6 +820,7 @@ class Robot(object):
 
     def add_container(self, name, slot, label=None, share=False):
         container = _setup_container(name)
+
         if container is not None:
             location = self._get_placement_location(slot)
             if self._is_available_slot(location, share, slot, name):
