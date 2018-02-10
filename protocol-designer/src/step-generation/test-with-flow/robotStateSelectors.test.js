2f3f6251f0224e09f56264a4730bbfc2054a36aa
IanLondon <ianlondondu@gmail.com>
2018-02-09 19:52:35 -0500

    (pd) next tip selectors fully tested

--- protocol-designer/src/step-generation/test-with-flow/robotStateSelectors.test.js
+++ protocol-designer/src/step-generation/test-with-flow/robotStateSelectors.test.js
@@ -311,19 +311,429 @@ describe('getNextTiprack - single-channel', () => {
   })
 })
 
-describe.skip('getNextTiprack - 8-channel', () => {
+describe('getNextTiprack - 8-channel', () => {
+  test('single tiprack, totally full', () => {
+    const robotState = {
+      instruments: {
+        p300SingleId: p300Single
+      },
+      labware: {
+        tiprack2Id: {
+          slot: '2',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 2'
+        },
+        sourcePlateId: {
+          slot: '10',
+          type: 'trough-12row',
+          name: 'Source (Buffer)'
+        },
+        destPlateId: {
+          slot: '1',
+          type: '96-flat',
+          name: 'Destination Plate'
+        },
+        trashId: {
+          slot: '12',
+          type: 'fixed-trash',
+          name: 'Trash'
+        }
+      },
+      tipState: {
+        tipracks: {
+          tiprack2Id: {...filledTiprackWells}
+        },
+        pipettes: {
+          p300SingleId: false
+        }
+      }
+    }
+
+    const result = getNextTiprack(8, robotState)
+
+    expect(result && result.tiprackId).toEqual('tiprack2Id')
+    expect(result && result.well).toEqual('A1')
+  })
+
   test('single tiprack, partially full', () => {
+    const robotState = {
+      instruments: {
+        p300SingleId: p300Single
+      },
+      labware: {
+        tiprack2Id: {
+          slot: '2',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 2'
+        },
+        sourcePlateId: {
+          slot: '10',
+          type: 'trough-12row',
+          name: 'Source (Buffer)'
+        },
+        destPlateId: {
+          slot: '1',
+          type: '96-flat',
+          name: 'Destination Plate'
+        },
+        trashId: {
+          slot: '12',
+          type: 'fixed-trash',
+          name: 'Trash'
+        }
+      },
+      tipState: {
+        tipracks: {
+          tiprack2Id: {
+            ...filledTiprackWells,
+            A1: false,
+            A2: false,
+            A5: false
+          }
+        },
+        pipettes: {
+          p300SingleId: false
+        }
+      }
+    }
+
+    const result = getNextTiprack(8, robotState)
+
+    expect(result && result.tiprackId).toEqual('tiprack2Id')
+    expect(result && result.well).toEqual('A3')
   })
 
   test('single tiprack, empty, should return null', () => {
+    const robotState = {
+      instruments: {
+        p300SingleId: p300Single
+      },
+      labware: {
+        sourcePlateId: {
+          slot: '10',
+          type: 'trough-12row',
+          name: 'Source (Buffer)'
+        },
+        destPlateId: {
+          slot: '1',
+          type: '96-flat',
+          name: 'Destination Plate'
+        },
+        trashId: {
+          slot: '12',
+          type: 'fixed-trash',
+          name: 'Trash'
+        }
+      },
+      tipState: {
+        tipracks: {},
+        pipettes: {
+          p300SingleId: false
+        }
+      }
+    }
+
+    const result = getNextTiprack(8, robotState)
+
+    expect(result).toEqual(null)
+  })
+
+  test('single tiprack, a well missing from each column, should return null', () => {
+    const robotState = {
+      instruments: {
+        p300SingleId: p300Single
+      },
+      labware: {
+        tiprack2Id: {
+          slot: '2',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 2'
+        },
+        sourcePlateId: {
+          slot: '10',
+          type: 'trough-12row',
+          name: 'Source (Buffer)'
+        },
+        destPlateId: {
+          slot: '1',
+          type: '96-flat',
+          name: 'Destination Plate'
+        },
+        trashId: {
+          slot: '12',
+          type: 'fixed-trash',
+          name: 'Trash'
+        }
+      },
+      tipState: {
+        tipracks: {
+          tiprack2Id: {
+            ...filledTiprackWells,
+            F1: false,
+            B2: false,
+            C3: false,
+            A4: false,
+            H5: false,
+            E6: false,
+            B7: false,
+            A8: false,
+            C9: false,
+            D10: false,
+            G11: false,
+            F12: false
+          }
+        },
+        pipettes: {
+          p300SingleId: false
+        }
+      }
+    }
+
+    const result = getNextTiprack(8, robotState)
+
+    expect(result).toEqual(null)
   })
 
   test('multiple tipracks, all full, should return the filled tiprack in the lowest slot', () => {
+    const robotState = {
+      instruments: {
+        p300SingleId: p300Single
+      },
+      labware: {
+        tiprack2Id: {
+          slot: '2',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 2'
+        },
+        tiprack3Id: {
+          slot: '3',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 3'
+        },
+        tiprack10Id: {
+          slot: '10',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 10'
+        },
+        sourcePlateId: {
+          slot: '9',
+          type: 'trough-12row',
+          name: 'Source (Buffer)'
+        },
+        destPlateId: {
+          slot: '1',
+          type: '96-flat',
+          name: 'Destination Plate'
+        },
+        trashId: {
+          slot: '12',
+          type: 'fixed-trash',
+          name: 'Trash'
+        }
+      },
+      tipState: {
+        tipracks: {
+          tiprack2Id: {
+            ...filledTiprackWells
+          },
+          tiprack3Id: {
+            ...filledTiprackWells
+          },
+          tiprack10Id: {
+            ...filledTiprackWells
+          }
+        },
+        pipettes: {
+          p300SingleId: false
+        }
+      }
+    }
+
+    const result = getNextTiprack(8, robotState)
+
+    expect(result && result.tiprackId).toEqual('tiprack2Id')
+    expect(result && result.well).toEqual('A1')
   })
 
   test('multiple tipracks, some partially full, should return the filled tiprack in the lowest slot', () => {
+    const robotState = {
+      instruments: {
+        p300SingleId: p300Single
+      },
+      labware: {
+        tiprack2Id: {
+          slot: '2',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 2'
+        },
+        tiprack3Id: {
+          slot: '3',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 3'
+        },
+        tiprack10Id: {
+          slot: '10',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 10'
+        },
+        sourcePlateId: {
+          slot: '9',
+          type: 'trough-12row',
+          name: 'Source (Buffer)'
+        },
+        destPlateId: {
+          slot: '1',
+          type: '96-flat',
+          name: 'Destination Plate'
+        },
+        trashId: {
+          slot: '12',
+          type: 'fixed-trash',
+          name: 'Trash'
+        }
+      },
+      tipState: {
+        tipracks: {
+          tiprack2Id: {
+            ...filledTiprackWells,
+            F1: false,
+            B2: false,
+            C3: false,
+            A4: false,
+            H5: false,
+            E6: false,
+            B7: false,
+            A8: false,
+            C9: false,
+            D10: false,
+            G11: false,
+            F12: false
+          },
+          tiprack3Id: {
+            ...filledTiprackWells,
+            A1: false,
+            A2: false,
+            A3: false,
+            A4: false,
+            A5: false,
+            A6: false,
+            A7: false,
+            A8: false,
+            A9: false,
+            A10: false,
+            A11: false,
+            A12: false
+          },
+          tiprack10Id: {
+            ...filledTiprackWells,
+            A1: false
+          }
+        },
+        pipettes: {
+          p300SingleId: false
+        }
+      }
+    }
+
+    const result = getNextTiprack(8, robotState)
+
+    expect(result && result.tiprackId).toEqual('tiprack10Id')
+    expect(result && result.well).toEqual('A2')
   })
 
   test('multiple tipracks, all empty, should return null', () => {
+    const robotState = {
+      instruments: {
+        p300SingleId: p300Single
+      },
+      labware: {
+        tiprack2Id: {
+          slot: '2',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 2'
+        },
+        tiprack3Id: {
+          slot: '3',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 3'
+        },
+        tiprack10Id: {
+          slot: '10',
+          type: 'tiprack-200uL',
+          name: 'Tip rack 10'
+        },
+        sourcePlateId: {
+          slot: '9',
+          type: 'trough-12row',
+          name: 'Source (Buffer)'
+        },
+        destPlateId: {
+          slot: '1',
+          type: '96-flat',
+          name: 'Destination Plate'
+        },
+        trashId: {
+          slot: '12',
+          type: 'fixed-trash',
+          name: 'Trash'
+        }
+      },
+      tipState: {
+        tipracks: {
+          tiprack2Id: {
+            ...filledTiprackWells,
+            F1: false,
+            B2: false,
+            C3: false,
+            A4: false,
+            H5: false,
+            E6: false,
+            B7: false,
+            A8: false,
+            C9: false,
+            D10: false,
+            G11: false,
+            F12: false
+          },
+          tiprack3Id: {
+            ...filledTiprackWells,
+            A1: false,
+            A2: false,
+            A3: false,
+            A4: false,
+            A5: false,
+            A6: false,
+            A7: false,
+            A8: false,
+            A9: false,
+            A10: false,
+            A11: false,
+            A12: false
+          },
+          tiprack10Id: {
+            ...filledTiprackWells,
+            A1: false,
+            A2: false,
+            A3: false,
+            A4: false,
+            A5: false,
+            A6: false,
+            A7: false,
+            A8: false,
+            A9: false,
+            A10: false,
+            A11: false,
+            A12: false
+          }
+        },
+        pipettes: {
+          p300SingleId: false
+        }
+      }
+    }
+
+    const result = getNextTiprack(8, robotState)
+
+    expect(result).toEqual(null)
   })
 })
