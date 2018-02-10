effb350d6cff55d78ff7c9a5200e06b648d74cec
andySigler <andrewsigler1@gmail.com>
2018-02-10 14:09:47 -0500

    default plunger speeds specified in seconds to aspirate/dispense full volume of pipette to mimic human motion

--- api/opentrons/instruments/pipette_config.py
+++ api/opentrons/instruments/pipette_config.py
@@ -1,6 +1,13 @@
 from collections import namedtuple
 
 
+# Default number of seconds to aspirate/dispense a pipette's full volume,
+# and these times were chosen to mimic normal human-pipetting motions.
+# However, accurate speeds are dependent on environment (ex: liquid viscosity),
+# therefore a pipette's flow-rates (ul/sec) should be set by protocol writer
+DEFAULT_ASPIRATE_SECONDS = 2
+DEFAULT_DISPENSE_SECONDS = 1
+
 pipette_config = namedtuple(
     'pipette_config',
     [
@@ -22,8 +29,8 @@ p10_single = pipette_config(
         'drop_tip': -6
     },
     pick_up_current=0.05,
-    aspirate_flow_rate=5,   # default aspirate is 1/2 of total volume
-    dispense_flow_rate=20,  # default dispense speed is 2x of total volume
+    aspirate_flow_rate=10 / DEFAULT_ASPIRATE_SECONDS,
+    dispense_flow_rate=10 / DEFAULT_DISPENSE_SECONDS,
     ul_per_mm=0.617,
     channels=1,
     name='p10_single'
@@ -37,8 +44,8 @@ p10_multi = pipette_config(
         'drop_tip': -6
     },
     pick_up_current=0.2,
-    aspirate_flow_rate=5,
-    dispense_flow_rate=20,
+    aspirate_flow_rate=10 / DEFAULT_ASPIRATE_SECONDS,
+    dispense_flow_rate=10 / DEFAULT_DISPENSE_SECONDS,
     ul_per_mm=0.617,
     channels=8,
     name='p10_multi'
@@ -52,8 +59,8 @@ p300_single = pipette_config(
         'drop_tip': -3.5
     },
     pick_up_current=0.1,
-    aspirate_flow_rate=150,
-    dispense_flow_rate=600,
+    aspirate_flow_rate=300 / DEFAULT_ASPIRATE_SECONDS,
+    dispense_flow_rate=300 / DEFAULT_DISPENSE_SECONDS,
     ul_per_mm=18.51,
     channels=1,
     name='p300_single'
@@ -67,8 +74,8 @@ p300_multi = pipette_config(
         'drop_tip': -5
     },
     pick_up_current=0.3,
-    aspirate_flow_rate=150,
-    dispense_flow_rate=600,
+    aspirate_flow_rate=300 / DEFAULT_ASPIRATE_SECONDS,
+    dispense_flow_rate=300 / DEFAULT_DISPENSE_SECONDS,
     ul_per_mm=18.51,
     channels=8,
     name='p300_multi'
