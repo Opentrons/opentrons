14e6b3074d57d7be625ef7cb8bdb0abcd5c6c770
Ian London <ianlondondu@gmail.com>
2018-03-08 16:41:55 -0500

    (pd) add all possible pipettes to setup; show pipette InstrumentGroup (#987)
    * add all possible pipettes to setup
    * show pipette InstrumentGroup in PD main file page
    * split updatePipettes out of updateFileFields action

--- protocol-designer/src/containers/ConnectedFilePage.js
+++ protocol-designer/src/containers/ConnectedFilePage.js
@@ -9,32 +9,44 @@ import FilePage from '../components/FilePage'
 import {actions, selectors} from '../file-data'
 import type {FilePageFields} from '../file-data'
 import {formConnectorFactory, type FormConnector} from '../utils'
+
 export default connect(mapStateToProps, null, mergeProps)(FilePage)
 
-function mapStateToProps (state: BaseState): FilePageFields {
-  return selectors.fileFormValues(state)
+type Props = React.ElementProps<typeof FilePage>
+type StateProps = {
+  instruments: $PropertyType<Props, 'instruments'>,
+  _values: {[string]: string}
+}
+
+function mapStateToProps (state: BaseState): StateProps {
+  const formValues = selectors.fileFormValues(state)
+  const pipetteData = selectors.pipettesForInstrumentGroup(state)
+
+  return {
+    _values: formValues,
+    instruments: pipetteData
+  }
 }
 
 function mergeProps (
-  stateProps: FilePageFields,
+  stateProps: StateProps,
   dispatchProps: {dispatch: Dispatch<*>}
-): React.ElementProps<typeof FilePage> {
-  const values = stateProps
+): Props {
+  const {instruments, _values} = stateProps
   const {dispatch} = dispatchProps
 
   const onChange = (accessor) => (e: SyntheticInputEvent<*>) => {
     if (accessor === 'name' || accessor === 'description' || accessor === 'author') {
-      dispatch(actions.updateFileFields({
-        [accessor]: e.target.value
-      }))
+      dispatch(actions.updateFileFields({[accessor]: e.target.value}))
     } else {
       console.warn('Invalid accessor in ConnectedFilePage:', accessor)
     }
   }
 
-  const formConnector: FormConnector<FilePageFields> = formConnectorFactory(onChange, values)
+  const formConnector: FormConnector<FilePageFields> = formConnectorFactory(onChange, _values)
 
   return {
-    formConnector
+    formConnector,
+    instruments
   }
 }
