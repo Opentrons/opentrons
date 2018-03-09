7786d92fabebc5f112c1d50ebc2e20837b0f2b2d
Ian London <ianlondondu@gmail.com>
2018-03-09 09:58:14 -0500

    (complib, app, pd) Update InstrumentGroup to take left/right props (#1002)

--- components/src/__tests__/instrument-diagram.test.js
+++ components/src/__tests__/instrument-diagram.test.js
@@ -24,23 +24,23 @@ describe('InstrumentDiagram', () => {
 describe('InstrumentGroup', () => {
   test('Renders correctly', () => {
     const tree = Renderer.create(
-      <InstrumentGroup instruments={[
-        {
+      <InstrumentGroup
+        left={{
           mount: 'left',
           description: 'p300 8-Channel',
           tipType: '150',
           channels: 8,
           className: 'foo'
-        },
-        {
+        }}
+        right={{
           mount: 'right',
           description: 'p10 Single',
           tipType: '10',
           channels: 1,
           isDisabled: true,
           className: 'blah'
-        }
-      ]}/>
+        }}
+      />
     ).toJSON()
 
     expect(tree).toMatchSnapshot()
