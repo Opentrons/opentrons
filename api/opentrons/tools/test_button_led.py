6a2f888fbf2aa74f61f094dfa8e5b44a8945a8eb
andySigler <andrewsigler1@gmail.com>
2018-03-12 13:49:55 -0400

    adds rails lights to led test script

--- api/opentrons/tools/test_button_led.py
+++ api/opentrons/tools/test_button_led.py
@@ -5,6 +5,7 @@ d = robot._driver
 button_state = d.read_button()
 
 button_color_index = 0
+strips_state = 0
 
 while True:
     new_state = d.read_button()
@@ -16,4 +17,9 @@ while True:
             d._set_button_light(g=True)
         elif button_color_index % 3 == 2:
             d._set_button_light(b=True)
+        strips_state = 1 - strips_state
+        if strips_state:
+            d.turn_on_rail_lights()
+        else:
+            d.turn_off_rail_lights()
     button_state = new_state
