0149f71f32aead851bcb9e1d37c153f1f65e525a
andySigler <andrewsigler1@gmail.com>
2018-03-14 16:09:30 -0400

    adds test for invalid config.json

--- api/tests/opentrons/robot/test_robots_config.py
+++ api/tests/opentrons/robot/test_robots_config.py
@@ -15,3 +15,14 @@ def test_default_probe_height():
     cfg = robot_configs.load()
     assert cfg.probe_center[2] == 77.0
     assert cfg.probe_dimensions[2] == 82.0
+
+
+def test_load_corrupt_json():
+    import os
+    from opentrons.robot import robot_configs
+    filename = os.path.join(os.path.dirname(__file__), 'bad_config.json')
+    with open(filename, 'w') as file:
+        file.write('')  # empty config file
+    c = robot_configs.load(filename)
+    assert c.version == 1
+    os.remove(filename)
