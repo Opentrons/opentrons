c3bc13be5a529a2ed7cacc2ba9fb804ff44762c9
andySigler <andrewsigler1@gmail.com>
2018-03-19 13:09:52 -0400

    adds endpoint update_firmware, which flashes HEX file with lpc21isp

--- api/opentrons/server/endpoints/update.py
+++ api/opentrons/server/endpoints/update.py
@@ -20,6 +20,42 @@ async def _install(filename, loop):
     return res
 
 
+async def _update_firmware(filename, loop):
+    # ensure there is a reference to the port
+    log.info('Connecting to robot')
+    from opentrons import robot
+    log.info('Imported Robot')
+    robot.connect()
+    log.info('Connected')
+    driver = robot._driver
+    port = str(driver._connection.port)  # get port name
+    log.info(port)
+    driver._connection.close()
+    log.info('Closed')
+    driver._smoothie_programming_mode()
+    log.info('In ISP mode')
+
+    # run lpc21isp, THIS WILL AROUND 1 MINUTE TO COMPLETE
+    update_cmd = 'lpc21isp -wipe -donotstart {0} {1} {2} 12000'.format(
+        filename, port, robot.config.serial_speed)
+    log.info('Running command: "{}"'.format(update_cmd))
+    proc = await asyncio.create_subprocess_shell(
+        update_cmd,
+        stdout=asyncio.subprocess.PIPE,
+        loop=loop)
+    log.info('Waiting for firmware to flash...')
+    rd = await proc.stdout.read()
+    res = rd.decode().strip()
+    await proc.wait()
+    log.info('Finished flashing firmware')
+
+    # reconnect to port
+    driver._smoothie_reset()
+    driver._connection.open()
+    driver._setup()
+    log.info('Reset Smoothieware')
+
+
 async def install_api(request):
     """
     This handler accepts a POST request with Content-Type: multipart/form-data
@@ -39,6 +75,44 @@ async def install_api(request):
 
         msg = await _install(filename, request.loop)
         log.debug('Install complete')
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
+        log.debug("Result: {}".format(msg))
+        res = web.json_response({
+            'message': msg,
+            'filename': filename})
+    except Exception as e:
+        res = web.json_response(
+            {'error': 'Exception {} raised by update of {}. Trace: {}'.format(
+                type(e), data, e.__traceback__)},
+            status=500)
+    return res
+
+
+async def update_firmware(request):
+    """
+    This handler accepts a POST request with Content-Type: multipart/form-data
+    and a file field in the body named "hex". The file should be a valid HEX
+    image to be flashed to the LPC1769. The received file is flashed using
+    lpc21isp, and then deleted and a success code is returned.
+    """
+    log.debug('Update Firmware request received')
+    data = await request.post()
+    try:
+        filename = data['hex'].filename
+        log.info('Preparing to flash image: {}'.format(filename))
+        content = data['hex'].file.read()
+        log.info('Read in HEX file')
+
+        with open(filename, 'wb') as wf:
+            wf.write(content)
+
+        log.info('Wrote HEX file to local system')
+
+        msg = await _update_firmware(filename, request.loop)
+        log.debug('Firmware Update complete')
         try:
             os.remove(filename)
         except OSError:
