b0302ccbdc426f3642be4e82b80d7639d4b344c1
IanLondon <ianlondondu@gmail.com>
2018-03-21 16:14:18 -0400

    test dispense calls updateLiquidState w args

--- protocol-designer/src/step-generation/test-with-flow/dispense.test.js
+++ protocol-designer/src/step-generation/test-with-flow/dispense.test.js
@@ -2,6 +2,8 @@
 import {createRobotState} from './fixtures'
 import dispense from '../dispense'
 
+import updateLiquidState from '../dispenseUpdateLiquidState'
+
 jest.mock('../dispenseUpdateLiquidState')
 
 describe('dispense', () => {
@@ -26,6 +28,11 @@ describe('dispense', () => {
         }
       }
     }
+
+    // $FlowFixMe: mock methods
+    updateLiquidState.mockClear()
+    // $FlowFixMe: mock methods
+    updateLiquidState.mockReturnValue(initialRobotState.liquidState)
   })
 
   describe('tip tracking & commands:', () => {
@@ -68,5 +75,28 @@ describe('dispense', () => {
     // Is it OK to dispense vol > pipette max vol?
     // LATER: shouldn't dispense > volume of liquid in pipette
     test.skip('dispense with excessive volume should... ?')
+  })
+
+  describe('liquid tracking', () => {
+    test('dispense calls dispenseUpdateLiquidState with specified volume of pipette', () => {
+      dispense({
+        pipette: 'p300SingleId',
+        labware: 'sourcePlateId',
+        well: 'A1',
+        volume: 152
+      })(robotStateWithTip)
+
+      expect(updateLiquidState).toHaveBeenCalledWith(
+        {
+          pipetteId: 'p300SingleId',
+          labwareId: 'sourcePlateId',
+          volume: 152, // TODO IMMEDIATELY
+          well: 'A1',
+          labwareType: 'trough-12row',
+          pipetteData: robotStateWithTip.instruments.p300SingleId
+        },
+        robotStateWithTip.liquidState
+      )
+    })
   })
 })
