c285a151c8b88d7d2c1d2e2a287fb9cc4b3426b3
Mike Cousins <mike@opentrons.com>
2018-03-29 13:00:59 -0400

    feat(app): Drive app update modal content with state from shell module

--- app/src/components/AppSettings/index.js
+++ app/src/components/AppSettings/index.js
@@ -1,15 +1,21 @@
 // @flow
 // app status panel with connect button
 import * as React from 'react'
+
+import type {ShellUpdate} from '../../shell'
 import AppInfoCard from './AppInfoCard'
 import AppUpdateModal from './AppUpdateModal'
 
 import styles from './styles.css'
 
-export default function AppInfo () {
+type Props = ShellUpdate & {
+  checkForUpdates: () => mixed
+}
+
+export default function AppSettings (props: Props) {
   return (
     <div className={styles.app_settings}>
-      <AppInfoCard />
+      <AppInfoCard {...props} />
     </div>
   )
 }
