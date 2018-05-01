072bb4236f03955962256a741f6ed92ab0fc41f6
Mike Cousins <mike@cousins.io>
2018-05-01 15:50:24 -0400

    feat(app): Wire "Calibrate" button to POST /calibration/deck/start (#1333)

--- app/src/components/CalibrateDeck/types.js
+++ app/src/components/CalibrateDeck/types.js
@@ -1,14 +1,25 @@
 // @flow
-import type {RobotMoveState} from '../../http-api-client'
+import type {PipetteConfig} from '@opentrons/labware-definitions'
+import type {RobotService} from '../../robot'
+import type {RobotMoveState, DeckCalStartState} from '../../http-api-client'
 
-export type CalibrateDeckProps = {
+export type OP = {
   title: string,
   subtitle: string,
+  robot: RobotService,
   parentUrl: string,
   baseUrl: string,
-  confirmUrl: string,
   exitUrl: string,
+}
+
+export type SP = {
+  pipette: ?PipetteConfig,
+  startRequest: DeckCalStartState,
   moveRequest: RobotMoveState,
-  moveToFront: () => mixed,
+}
+
+export type DP = {
   back: () => mixed,
 }
+
+export type CalibrateDeckProps = OP & SP & DP
