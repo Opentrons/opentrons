dd69330fe2541966ee2f583b5eebc8f5705df706
Mike Cousins <mike@opentrons.com>
2018-05-02 15:01:37 -0400

    feat(app): Allow user to force take control of robot

--- app/src/components/CalibrateDeck/InUseModal.js
+++ app/src/components/CalibrateDeck/InUseModal.js
@@ -1,13 +1,9 @@
 // @flow
 import * as React from 'react'
-
 import {Link} from 'react-router-dom'
-import {AlertModal, CheckboxField} from '@opentrons/components'
 
-type Props = {
-  parentUrl: string,
-  onBackClick?: () => mixed
-}
+import {AlertModal, CheckboxField} from '@opentrons/components'
+import type {CalibrateDeckProps} from './types'
 
 type State = {
   checkOne: boolean,
@@ -16,9 +12,13 @@ type State = {
 }
 const HEADING = 'Robot is currently in use'
 
-export default class InUseModal extends React.Component <Props, State> {
-  constructor (props: Props) {
+export default class InUseModal extends React.Component<
+  CalibrateDeckProps,
+  State
+> {
+  constructor (props: CalibrateDeckProps) {
     super(props)
+
     this.state = {
       checkOne: false,
       checkTwo: false,
@@ -27,35 +27,36 @@ export default class InUseModal extends React.Component <Props, State> {
   }
 
   render () {
+    const {parentUrl, forceStart} = this.props
     const canContinue = Object.keys(this.state).every(k => this.state[k])
 
     return (
-    <AlertModal
-      heading={HEADING}
-      buttons={[
-        {children: 'cancel', Component: Link, to: this.props.parentUrl},
-        {children: 'continue', disabled: !canContinue}
-      ]}
-    >
-      <p>Are you sure you want to interrupt this robot?</p>
-      <div>
-        <CheckboxField
-          label="It can’t be undone"
-          onChange={() => this.setState({checkOne: !this.state.checkOne})}
-          value={this.state.checkOne}
-        />
-        <CheckboxField
-          label="Any work the robot is doing will be stopped"
-          onChange={() => this.setState({checkTwo: !this.state.checkTwo})}
-          value={this.state.checkTwo}
-        />
-        <CheckboxField
-          label="This is a good idea"
-          onChange={() => this.setState({checkThree: !this.state.checkThree})}
-          value={this.state.checkThree}
-        />
-      </div>
-    </AlertModal>
+      <AlertModal
+        heading={HEADING}
+        buttons={[
+          {children: 'cancel', Component: Link, to: parentUrl},
+          {children: 'continue', onClick: forceStart, disabled: !canContinue}
+        ]}
+      >
+        <p>Are you sure you want to interrupt this robot?</p>
+        <div>
+          <CheckboxField
+            label="It can’t be undone"
+            onChange={() => this.setState({checkOne: !this.state.checkOne})}
+            value={this.state.checkOne}
+          />
+          <CheckboxField
+            label="Any work the robot is doing will be stopped"
+            onChange={() => this.setState({checkTwo: !this.state.checkTwo})}
+            value={this.state.checkTwo}
+          />
+          <CheckboxField
+            label="This is a good idea"
+            onChange={() => this.setState({checkThree: !this.state.checkThree})}
+            value={this.state.checkThree}
+          />
+        </div>
+      </AlertModal>
     )
   }
 }
