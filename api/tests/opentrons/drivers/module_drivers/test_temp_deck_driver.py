f8e8d4f9b638b88483801520960544b7c137a5c7
Andy Sigler <andrewsigler1@gmail.com>
2018-05-02 10:24:49 -0400

    adds more fake tests

--- api/tests/opentrons/drivers/module_drivers/test_temp_deck_driver.py
+++ api/tests/opentrons/drivers/module_drivers/test_temp_deck_driver.py
@@ -1,20 +1,60 @@
 import pytest
 
 from tests.opentrons.conftest import fuzzy_assert
+from opentrons.driver.module_drivers import temp_deck_driver
 
 
-def test_set_temp_deck_temperature(monkeypatch):
+def create_temp_deck_mocks():
     from opentrons.drivers.smoothie_drivers import serial_communication
-    command_log = []
 
     def write_with_log(command, connection, timeout):
-        return 'I am hot: 50'
+        # simulating how the firmware will handle commands and respond
+        if 'C' in command:
+            # setting the temperature
+
+            # find the temperature in the string, then check that it is
+            # a valid temperature
+            # raise an error if it's a bad value
+            if bad_data:
+                return 'ERROR\r\n'
+
+            return 'ok\r\nok\r\n'
+        elif 'X' in command:
+            # turning it off
+            return 'ok\r\nok\r\n'
+        return
 
     monkeypatch.setattr(serial_communication, 'write_and_return',
                         write_with_log)
 
-    # tell the temp-deck to be at 50 degrees C
-    res = serial_communication.write_and_return('Be Hot! 50')
+    return serial_communication
 
-    expected = ['I am hot: 50']
-    fuzzy_assert(result=command_log, expected=expected)
+
+def test_set_temp_deck_temperature(monkeypatch):
+    sc = create_temp_deck_mocks()
+
+    # tell the temp-deck to be at 50 degrees C
+    res = sc.write_and_return('C50')
+
+    expected = 'ok\r\nok\r\n'
+    fuzzy_assert(result=res, expected=expected)
+
+
+def test_set_temp_deck_temperature(monkeypatch):
+    sc = create_temp_deck_mocks()
+
+    # tell the temp-deck to be at 50 degrees C
+    res = sc.write_and_return('C101')
+
+    expected = 'ERROR\r\n'
+    fuzzy_assert(result=res, expected=expected)
+
+
+def test_set_temp_deck_temperature(monkeypatch):
+    sc = create_temp_deck_mocks()
+
+    # tell the temp-deck to be at 50 degrees C
+    res = sc.write_and_return('X')
+
+    expected = 'ok\r\nok\r\n'
+    fuzzy_assert(result=res, expected=expected)
