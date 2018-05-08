f296a0a60a239f7207fab8367bbed765e072af51
Andy Sigler <andrewsigler1@gmail.com>
2018-05-08 11:47:38 -0400

    explicitly return after recursively calling update_position; adds test

--- api/opentrons/drivers/smoothie_drivers/driver_3_0.py
+++ api/opentrons/drivers/smoothie_drivers/driver_3_0.py
@@ -232,11 +232,12 @@ class SmoothieDriver_3_0_0:
                 updated_position = \
                     _parse_axis_values(position_response)
                 # TODO jmg 10/27: log warning rather than an exception
-            except (TypeError, ParseError) as e:
+            except (TypeError, ParseError, ValueError) as e:
                 if is_retry:
                     raise e
                 else:
                     self.update_position(default=default, is_retry=True)
+                    return  # avoid calling `_update_position` after recursion
 
         self._update_position(updated_position)
 
