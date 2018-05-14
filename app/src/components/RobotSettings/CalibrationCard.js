6d0ee73dc8953d890637f83e14ea9f3e03b73ae2
Kadee80 <kaa328@nyu.edu>
2018-05-14 15:50:56 -0400

    fix(app): Hide last calibrated labeled value
    Since this feature is not implemented yet, hide the last calibrated text using visibility: hidden so that layout remains consistent.

--- app/src/components/RobotSettings/CalibrationCard.js
+++ app/src/components/RobotSettings/CalibrationCard.js
@@ -7,6 +7,8 @@ import {push} from 'react-router-redux'
 import type {Robot} from '../../robot'
 import {startDeckCalibration} from '../../http-api-client'
 import {Card, LabeledValue, OutlineButton} from '@opentrons/components'
+// TODO (ka 2018-5-14): temporariliy hide last calibrated labeled value without affecting card layout
+import styles from './styles.css'
 
 type OP = Robot
 
@@ -24,12 +26,12 @@ export default connect(null, mapDispatchToProps)(CalibrationCard)
 
 function CalibrationCard (props: Props) {
   const {start} = props
-
   return (
     <Card title={TITLE} description={CALIBRATION_MESSAGE}>
     <LabeledValue
       label={LAST_RUN_LABEL}
       value='Never'
+      className={styles.hidden_value}
     />
     <OutlineButton onClick={start}>
       Calibrate
