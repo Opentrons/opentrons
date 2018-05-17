cbcb23632f1ce756a506795625798e1a7d585fd0
Ian London <ianlondondu@gmail.com>
2018-05-17 10:38:30 -0400

    Api load json protocols (#1444)
    * api: protocols tests
    
    * api: rpc session handles json protocols
    
    * pd: fix file format 'display-name' field key
    
    * api: change `spam is "somestring"` to `"spam == "somestring"` to fix subtle bugs (like `mount is "left"` failing)

---
+++ api/opentrons/protocols/__init__.py
@@ -0,0 +1,115 @@
+import time
+from itertools import chain
+from opentrons import instruments, labware, robot
+from opentrons.instruments import pipette_config
+
+
+def _sleep(seconds):
+    time.sleep(seconds)
+
+
+def load_pipettes(protocol_data):
+    pipettes = protocol_data.get('pipettes', {})
+    pipettes_by_id = {}
+
+    for pipette_id, props in pipettes.items():
+        model = props.get('model')
+        mount = props.get('mount')
+        config = pipette_config.load(model)
+        pipettes_by_id[pipette_id] = instruments._create_pipette_from_config(
+            config=config,
+            mount=mount)
+
+    return pipettes_by_id
+
+
+def load_labware(protocol_data):
+    data = protocol_data.get('labware', {})
+    loaded_labware = {}
+    for labware_id, props in data.items():
+        slot = props.get('slot')
+        model = props.get('model')
+        display_name = props.get('display-name')
+
+        if slot == '12':
+            if model == 'fixed-trash':
+                # pass in the pre-existing fixed-trash
+                loaded_labware[labware_id] = robot.fixed_trash
+            else:
+                # share the slot with the fixed-trash
+                loaded_labware[labware_id] = labware.load(
+                    model,
+                    slot,
+                    display_name,
+                    share=True
+                )
+        else:
+            loaded_labware[labware_id] = labware.load(
+                model,
+                slot,
+                display_name
+            )
+
+    return loaded_labware
+
+
+def get_location(command_params, loaded_labware):
+    labwareId = command_params.get('labware')
+    well = command_params.get('well')
+    return loaded_labware.get(labwareId, {}).get(well)
+
+
+def get_pipette(command_params, loaded_pipettes):
+    pipetteId = command_params.get('pipette')
+    return loaded_pipettes.get(pipetteId)
+
+
+def dispatch_commands(protocol_data, loaded_pipettes, loaded_labware):
+    subprocedures = [
+        p.get('subprocedure', [])
+        for p in protocol_data.get('procedure', [])]
+
+    flat_subs = chain.from_iterable(subprocedures)
+
+    for command_item in flat_subs:
+        command_type = command_item.get('command')
+        params = command_item.get('params', {})
+
+        pipette = get_pipette(params, loaded_pipettes)
+        location = get_location(params, loaded_labware)
+        volume = params.get('volume')
+
+        if command_type == 'delay':
+            wait = params.get('wait', 0)
+            if type(wait) is str:
+                # TODO Ian 2018-05-14 pass message
+                robot.pause()
+            else:
+                _sleep(wait)
+
+        elif command_type == 'blowout':
+            pipette.blow_out(location)
+
+        elif command_type == 'pick-up-tip':
+            pipette.pick_up_tip(location)
+
+        elif command_type == 'drop-tip':
+            pipette.drop_tip(location)
+
+        elif command_type == 'aspirate':
+            pipette.aspirate(volume, location)
+
+        elif command_type == 'dispense':
+            pipette.dispense(volume, location)
+
+
+def execute_protocol(protocol):
+    loaded_pipettes = load_pipettes(protocol)
+    loaded_labware = load_labware(protocol)
+
+    dispatch_commands(protocol, loaded_pipettes, loaded_labware)
+
+    return {
+        'pipettes': loaded_pipettes,
+        'labware': loaded_labware
+    }
