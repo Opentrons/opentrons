7db1e51553a3b8bffaefd9cc4dd2eb951fb09e9d
Kadee80 <kaa328@nyu.edu>
2018-05-24 11:21:39 -0400

    feat(app): Initiate and confirm run cancel
    Clicking cancel opens modal
    Robot is paused if not already paused
    User sees dialogue box asking them to confirm that they want to cancel
    User has option to confirm and cancel run or cancel and return to the run screen
    
    closes # 1271

---
+++ app/src/components/RunLog/ConfirmCancelModal.js
@@ -0,0 +1,48 @@
+// @flow
+import * as React from 'react'
+import {connect} from 'react-redux'
+import {push} from 'react-router-redux'
+import {AlertModal} from '@opentrons/components'
+
+import {
+  actions as robotActions
+} from '../../robot'
+
+type Props = {
+  back: () => mixed,
+  cancel: () => mixed,
+}
+
+const HEADING = 'Are you sure you want to cancel this run?'
+const CANCEL_TEXT = 'cancel run'
+const BACK_TEXT = 'go back'
+
+const mapDispatchToProps = (dispatch) => ({
+  back: () => {
+    dispatch(robotActions.resume())
+    dispatch(push('/run'))
+  },
+  cancel: () => {
+    dispatch(robotActions.cancel())
+    dispatch(push('/run'))
+  }
+})
+
+function ExitAlertModal (props: Props) {
+  const {back, cancel} = props
+
+  return (
+    <AlertModal
+      heading={HEADING}
+      buttons={[
+        {children: BACK_TEXT, onClick: back},
+        {children: CANCEL_TEXT, onClick: cancel}
+      ]}
+      alertOverlay
+    >
+      <p>Doing so will terminate this run and home your robot.</p>
+    </AlertModal>
+  )
+}
+
+export default connect(null, mapDispatchToProps)(ExitAlertModal)
--- app/src/pages/Run.js
+++ app/src/pages/Run.js
@@ -1,15 +1,20 @@
+// @flow
 // run task component
 import React from 'react'
-
+import {Route} from 'react-router'
 import Page from '../components/Page'
 import SessionHeader from '../components/SessionHeader'
 import RunLog from '../components/RunLog'
+import ConfirmCancelModal from '../components/RunLog/ConfirmCancelModal'
 
 export default function RunPage () {
   return (
     <Page>
       <SessionHeader />
       <RunLog />
+      <Route path='/run/cancel' render={() => (
+        <ConfirmCancelModal />
+      )} />
     </Page>
   )
 }
