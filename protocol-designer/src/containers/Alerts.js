9a7ed2085b0440c9f11297547cd2f117f1118607
Brian Cooper <brian@allegro.com>
2018-06-12 20:51:55 -0400

    feat(protocol-designer): show a clear error when source and destination wells are incompatible
    Depending on the action in a step, certain source and destination well combinations should produce a
    validation error. Display those errors clearly to the user
    
    Closes #1594

--- protocol-designer/src/containers/Alerts.js
+++ protocol-designer/src/containers/Alerts.js
@@ -1,8 +1,10 @@
 // @flow
 import * as React from 'react'
+import _ from 'lodash'
 import type {Dispatch} from 'redux'
 import {connect} from 'react-redux'
 import {selectors} from '../file-data'
+import {selectors as steplistSelectors} from '../steplist'
 import {AlertItem} from '@opentrons/components'
 import type {BaseState} from '../types'
 import type {ErrorType, CommandCreatorError} from '../step-generation'
@@ -44,18 +46,24 @@ function Alerts (props: Props) {
   )
 }
 
+const VISIBLE_FORM_ERRORS = ['_mismatchedWells']
+
 function mapStateToProps (state: BaseState): SP {
   const timelineFull = selectors.robotStateTimeline(state)
-  const errors = timelineFull.timelineErrors
+  const timelineErrors = timelineFull.timelineErrors || []
 
+  // TODO: BC 2018-06-12 deal with all form errors uniformly elsewhere
+  const formErrors = steplistSelectors.currentFormErrors(state)
+  const visibleFormErrors = _.filter(formErrors, (key: string, value: string) => VISIBLE_FORM_ERRORS.includes(value)) || []
+  const formAlerts = visibleFormErrors.map(err => ({message: err, type: 'FORM_ERROR'}))
+
+  const errors = [...timelineErrors, ...formAlerts]
   if (!errors || errors.length === 0) {
-    return {
-      alerts: []
-    }
+    return { alerts: [] }
   }
 
   return {
-    alerts: errors.map(err => ({...err})) // NOTE Flow complains about exact obj types if you don't map & unpack here
+    alerts: [...errors.map(err => ({...err}))]// NOTE Flow complains about exact obj types if you don't map & unpack here
 
     // TODO LATER Ian 2018-05-01 generate warnings somewhere, and merge in here with dismissId's
   }
