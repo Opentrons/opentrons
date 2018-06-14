308d48bbdc924aab2ee688b4d3fe971280def278
Kadee80 <kaa328@nyu.edu>
2018-06-14 14:49:08 -0400

    feat(app): Show connect alert banner on successful connection
    closes #1314

---
+++ app/src/components/RobotSettings/ConnectBanner.js
@@ -0,0 +1,34 @@
+// @flow
+import * as React from 'react'
+import {type Robot} from '../../robot'
+import {AlertItem} from '@opentrons/components'
+
+type State = {
+  dismissed: boolean
+}
+
+export default class ConnectBanner extends React.Component <Robot, State> {
+  constructor (props: Robot) {
+    super(props)
+
+    this.state = {
+      dismissed: false
+    }
+  }
+
+  render () {
+    const {isConnected, name} = this.props
+    const isVisible = isConnected && !this.state.dismissed
+    const TITLE = `${name} successfully connected`
+    return (
+      <div>
+        {isVisible && (
+          <AlertItem
+            type='success'
+            onCloseClick={() => this.setState({dismissed: true})}
+            title={TITLE} />
+        )}
+      </div>
+    )
+  }
+}
