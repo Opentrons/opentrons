a2361052e2fa0a0d0e55bdec5b4623c9fcc59174
Brian Cooper <brian@allegro.com>
2018-06-18 16:17:51 -0400

    move connection into parent form component

--- protocol-designer/src/components/StepEditForm/index.js
+++ protocol-designer/src/components/StepEditForm/index.js
@@ -1,27 +1,22 @@
 // @flow
 import * as React from 'react'
+import {connect} from 'react-redux'
 import get from 'lodash/get'
 import cx from 'classnames'
 import {FlatButton, PrimaryButton} from '@opentrons/components'
 
-import type {FormData} from '../../form-types'
+import {actions, selectors} from '../../steplist' // TODO use steplist/index.js
+import type {FormData, StepType} from '../../form-types'
 import {formConnectorFactory} from '../../utils'
+import type {BaseState, ThunkDispatch} from '../../types'
 import formStyles from '../forms.css'
 import styles from './StepEditForm.css'
 import MixForm from './MixForm'
 import TransferLikeForm from './TransferLikeForm'
 import PauseForm from './PauseForm'
 
-export type Props = {
-  formData: FormData,
-  handleChange: (accessor: string) => (event: SyntheticEvent<HTMLInputElement> | SyntheticEvent<HTMLSelectElement>) => void,
-  onClickMoreOptions: (event: SyntheticEvent<>) => mixed,
-  onCancel: (event: SyntheticEvent<>) => mixed,
-  onSave: (event: SyntheticEvent<>) => mixed,
-  canSave: boolean
-}
-
-const StepFormTypeMap = {
+type StepForm = typeof MixForm | typeof PauseForm | typeof TransferLikeForm
+const STEP_FORM_MAP: {[StepType]: StepForm} = {
   mix: MixForm,
   pause: PauseForm,
   transfer: TransferLikeForm,
@@ -29,9 +24,18 @@ const StepFormTypeMap = {
   distribute: TransferLikeForm
 }
 
-const StepEditForm = ({formData, handleChange, onClickMoreOptions, onCancel, onSave, canSave}: Props) => {
-  const FormComponent: any = get(StepFormTypeMap, formData.stepType)
-  if (!FormComponent) return <div className={formStyles.form}><div>Todo: support {formData.stepType} step</div></div>
+type SP = {formData?: FormData, canSave: boolean}
+type DP = {
+  handleChange: (accessor: string) => (event: SyntheticEvent<HTMLInputElement> | SyntheticEvent<HTMLSelectElement>) => void,
+  onClickMoreOptions: (event: SyntheticEvent<>) => mixed,
+  onCancel: (event: SyntheticEvent<>) => mixed,
+  onSave: (event: SyntheticEvent<>) => mixed,
+}
+
+const StepEditForm = (props: SP & DP) => {
+  const {formData, handleChange, onClickMoreOptions, onCancel, onSave, canSave} = props
+  const FormComponent: any = get(STEP_FORM_MAP, formData.stepType)
+  if (!FormComponent) return <div className={formStyles.form}><div>Todo: support {formData && formData.stepType} step</div></div>
   return (
     <div className={cx(formStyles.form, styles[formData.stepType])}>
       <FormComponent formData={formData} formConnector={formConnectorFactory(handleChange, formData)} />
@@ -44,4 +48,25 @@ const StepEditForm = ({formData, handleChange, onClickMoreOptions, onCancel, onS
   )
 }
 
-export default StepEditForm
+const mapStateToProps = (state: BaseState): SP => ({
+  formData: selectors.formData(state),
+  canSave: selectors.currentFormCanBeSaved(state)
+})
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<*>): DP => ({
+  onCancel: () => dispatch(actions.cancelStepForm()),
+  onSave: () => dispatch(actions.saveStepForm()),
+  onClickMoreOptions: () => dispatch(actions.openMoreOptionsModal()),
+  handleChange: (accessor: string) => (e: SyntheticEvent<HTMLInputElement> | SyntheticEvent<HTMLSelectElement>) => {
+    // TODO Ian 2018-01-26 factor this nasty type handling out
+    const dispatchEvent = value => dispatch(actions.changeFormInput({update: {[accessor]: value}}))
+
+    if (e.target instanceof HTMLInputElement && e.target.type === 'checkbox') {
+      dispatchEvent(e.target.checked)
+    } else if (e.target instanceof HTMLInputElement || e.target instanceof HTMLSelectElement) {
+      dispatchEvent(e.target.value)
+    }
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(StepEditForm)
