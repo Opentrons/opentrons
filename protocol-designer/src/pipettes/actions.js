a94473e0ce4c10d46cc5beaa8fb29ef8353e54bf
IanLondon <ianlondondu@gmail.com>
2018-06-25 16:55:03 -0400

    squash: rewrite type annotation for updatePipettes

--- protocol-designer/src/pipettes/actions.js
+++ protocol-designer/src/pipettes/actions.js
@@ -1,7 +1,7 @@
 // @flow
 import type {PipetteName} from './pipetteData'
 
-export const updatePipettes = (payload: {['left' | 'right']: ?PipetteName}) => ({
+export const updatePipettes = (payload: {'left'?: ?PipetteName, 'right'?: ?PipetteName}) => ({
   type: 'UPDATE_PIPETTES',
   payload
 })
