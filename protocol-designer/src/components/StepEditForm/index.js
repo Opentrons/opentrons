bcfd605408ba5cdda1fed9a27c02aba391362974
Brian Cooper <brian@allegro.com>
2018-06-27 13:45:56 -0400

    clean up form component a bit

--- protocol-designer/src/components/StepEditForm/index.js
+++ protocol-designer/src/components/StepEditForm/index.js
@@ -4,12 +4,8 @@ import {connect} from 'react-redux'
 import get from 'lodash/get'
 import without from 'lodash/without'
 import cx from 'classnames'
-import {
-  PrimaryButton,
-  OutlineButton
-} from '@opentrons/components'
 
-import {actions, selectors} from '../../steplist' // TODO use steplist/index.js
+import {actions, selectors} from '../../steplist'
 import type {StepFieldName} from '../../steplist/fieldLevel'
 import type {FormData, StepType} from '../../form-types'
 import type {BaseState, ThunkDispatch} from '../../types'
@@ -19,6 +15,7 @@ import MixForm from './MixForm'
 import TransferLikeForm from './TransferLikeForm'
 import PauseForm from './PauseForm'
 import ConfirmDeleteModal from './ConfirmDeleteModal'
+import ButtonRow from './ButtonRow'
 
 type StepForm = typeof MixForm | typeof PauseForm | typeof TransferLikeForm
 const STEP_FORM_MAP: {[StepType]: StepForm} = {
@@ -36,14 +33,14 @@ export type FocusHandlers = {
   onFieldBlur: (StepFieldName) => void
 }
 
-type SP = {formData?: ?FormData, canSave?: ?boolean, isNewStep?: boolean}
-type DP = {
-  handleChange: (accessor: string) => (event: SyntheticEvent<HTMLInputElement> | SyntheticEvent<HTMLSelectElement>) => void,
-  onClickMoreOptions: (event: SyntheticEvent<>) => mixed,
-  onDelete: () => mixed,
-  onCancel: (event: SyntheticEvent<>) => mixed,
-  onSave: (event: SyntheticEvent<>) => mixed,
+type SP = {
+  formData?: ?FormData,
+  isNewStep?: boolean
 }
+type DP = {
+  onDelete: () => mixed
+}
+
 type StepEditFormState = {
   showConfirmDeleteModal: boolean,
   focusedField: StepFieldName | null, // TODO: BC make this a real enum of field names
@@ -92,7 +89,7 @@ class StepEditForm extends React.Component<Props, StepEditFormState> {
 
   render () {
     if (!this.props.formData) return null // early-exit if connected formData is absent
-    const {formData, onClickMoreOptions, onDelete, onCancel, onSave, canSave} = this.props
+    const {formData, onDelete} = this.props
     const FormComponent: any = get(STEP_FORM_MAP, formData.stepType)
     if (!FormComponent) { // early-exit if step form doesn't exist
       return <div className={formStyles.form}><div>Todo: support {formData && formData.stepType} step</div></div>
@@ -106,7 +103,6 @@ class StepEditForm extends React.Component<Props, StepEditFormState> {
             onDelete()
           }}
         />}
-
         <div className={cx(formStyles.form, styles[formData.stepType])}>
           { /* TODO: insert form level validation */ }
           <FormComponent
@@ -117,12 +113,7 @@ class StepEditForm extends React.Component<Props, StepEditFormState> {
               onFieldFocus: this.onFieldFocus,
               onFieldBlur: this.onFieldBlur
             }} />
-          <div className={styles.button_row}>
-            <OutlineButton onClick={this.toggleConfirmDeleteModal}>DELETE</OutlineButton>
-            <OutlineButton onClick={onClickMoreOptions}>NOTES</OutlineButton>
-            <PrimaryButton className={styles.cancel_button} onClick={onCancel}>CANCEL</PrimaryButton>
-            <PrimaryButton disabled={!canSave} onClick={onSave}>SAVE</PrimaryButton>
-          </div>
+          <ButtonRow onDelete={this.toggleConfirmDeleteModal} />
         </div>
       </React.Fragment>
     )
@@ -131,25 +122,11 @@ class StepEditForm extends React.Component<Props, StepEditFormState> {
 
 const mapStateToProps = (state: BaseState): SP => ({
   formData: selectors.formData(state),
-  canSave: selectors.currentFormCanBeSaved(state),
   isNewStep: selectors.isNewStepForm(state)
 })
 
 const mapDispatchToProps = (dispatch: ThunkDispatch<*>): DP => ({
-  onDelete: () => dispatch(actions.deleteStep()),
-  onCancel: () => dispatch(actions.cancelStepForm()),
-  onSave: () => dispatch(actions.saveStepForm()),
-  onClickMoreOptions: () => dispatch(actions.openMoreOptionsModal()),
-  handleChange: (accessor: string) => (e: SyntheticEvent<HTMLInputElement> | SyntheticEvent<HTMLSelectElement>) => {
-    // TODO Ian 2018-01-26 factor this nasty type handling out
-    const dispatchEvent = value => dispatch(actions.changeFormInput({update: {[accessor]: value}}))
-
-    if (e.target instanceof HTMLInputElement && e.target.type === 'checkbox') {
-      dispatchEvent(e.target.checked)
-    } else if (e.target instanceof HTMLInputElement || e.target instanceof HTMLSelectElement) {
-      dispatchEvent(e.target.value)
-    }
-  }
+  onDelete: () => dispatch(actions.deleteStep())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(StepEditForm)
