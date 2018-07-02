73f7528950821debc686808fab9859a3e2865ace
Mike Cousins <mike@cousins.io>
2018-07-02 18:00:55 -0400

    feat(app): Add robot settings toggles to Advanced Settings card (#1795)
    Closes #1632

--- app/src/components/RobotSettings/AdvancedSettingsCard.js
+++ app/src/components/RobotSettings/AdvancedSettingsCard.js
@@ -1,24 +1,89 @@
 // @flow
 // app info card with version and updated
 import * as React from 'react'
+import {connect} from 'react-redux'
 
-import {Card} from '@opentrons/components'
-import {LabeledButton} from '../controls'
+import type {State, Dispatch} from '../../types'
+import type {Robot} from '../../robot'
+import type {Setting} from '../../http-api-client'
+import {fetchSettings, setSettings, makeGetRobotSettings} from '../../http-api-client'
+
+import {RefreshCard} from '@opentrons/components'
+import {LabeledButton, LabeledToggle} from '../controls'
+
+type OP = Robot
+
+type SP = {settings: Array<Setting>}
+
+type DP = {
+  fetch: () => mixed,
+  set: (id: string, value: boolean) => mixed,
+}
+
+type Props = OP & SP & DP
+
+type BooleanSettingProps = {
+  id: string,
+  title: string,
+  description: string,
+  value: boolean,
+  set: (id: string, value: boolean) => mixed,
+}
 
 const TITLE = 'Advanced Settings'
 
-export default function AdvancedSettingsCard () {
+export default connect(
+  makeMapStateToProps,
+  mapDispatchToProps
+)(AdvancedSettingsCard)
+
+class BooleanSettingToggle extends React.Component<BooleanSettingProps> {
+  toggle = (value) => this.props.set(this.props.id, !this.props.value)
+
+  render () {
+    return (
+      <LabeledToggle
+        label={this.props.title}
+        onClick={this.toggle}
+        toggledOn={this.props.value}
+      >
+        <p>{this.props.description}</p>
+      </LabeledToggle>
+    )
+  }
+}
+
+function AdvancedSettingsCard (props: Props) {
+  const {name, settings, set, fetch} = props
+
   return (
-    <Card title={TITLE} column>
-    <LabeledButton
-      label='Download Logs'
-      buttonProps={{
-        disabled: true,
-        children: 'Download'
-      }}
-    >
-      <p>Access logs from this robot.</p>
-    </LabeledButton>
-    </Card>
+    <RefreshCard watch={name} refresh={fetch} title={TITLE} column>
+      {settings.map(s => (
+        <BooleanSettingToggle {...s} key={s.id} set={set} />
+      ))}
+      <LabeledButton
+        label='Download Logs'
+        buttonProps={{
+          disabled: true,
+          children: 'Download'
+        }}
+      >
+        <p>Access logs from this robot.</p>
+      </LabeledButton>
+    </RefreshCard>
   )
+}
+
+function makeMapStateToProps (): (state: State, ownProps: OP) => SP {
+  const getRobotSettings = makeGetRobotSettings()
+
+  return (state, ownProps) =>
+    getRobotSettings(state, ownProps).response || {settings: []}
+}
+
+function mapDispatchToProps (dispatch: Dispatch, ownProps: OP): DP {
+  return {
+    fetch: () => dispatch(fetchSettings(ownProps)),
+    set: (id, value) => dispatch(setSettings(ownProps, id, value))
+  }
 }
