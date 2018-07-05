1984e2395e5e22399b3ebce07d2a3653dc23ac7d
IanLondon <ianlondondu@gmail.com>
2018-07-05 11:11:46 -0400

    only show 1 version of each pipette model

--- protocol-designer/src/pipettes/pipetteData.js
+++ protocol-designer/src/pipettes/pipetteData.js
@@ -1,10 +1,22 @@
 // @flow
 import compact from 'lodash/compact'
-import {getPipette, getPipetteModels} from '@opentrons/shared-data'
+import {getPipette} from '@opentrons/shared-data'
+
+// TODO: figure out what pipette versions mean in context of PD,
+// and annotate pipette-config so it's more straightforward to compare models
+const supportedPipetteModels = [
+  'p10_single_v1.3',
+  'p10_multi_v1.3',
+  'p50_single_v1.3',
+  'p50_multi_v1.3',
+  'p300_single_v1.3',
+  'p300_multi_v1.3',
+  'p1000_single_v1.3'
+]
 
 // TODO: should a version of pipetteOptions be moved to shared-data,
 // and used for both PD and Run App?
-export const pipetteOptions = compact(getPipetteModels().map((model: string) => {
+export const pipetteOptions = compact(supportedPipetteModels.map((model: string) => {
   const pipette = getPipette(model)
   return pipette ? {name: pipette.displayName, value: pipette.model} : null
 }))
