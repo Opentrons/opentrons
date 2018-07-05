f740cd6c967ed76884a98c330f0f0b96dcdcda25
IanLondon <ianlondondu@gmail.com>
2018-07-05 14:34:13 -0400

    restore timeline alert dismissibility

--- protocol-designer/src/components/steplist/TimelineAlerts.js
+++ protocol-designer/src/components/steplist/TimelineAlerts.js
@@ -1,56 +1,94 @@
 // @flow
+// TODO: TimelineAlerts.js was replaced by Alerts.js, and Alerts.js is currently not used.
+// See issue #1814. NOTE: for original TimelineAlerts, find an earlier commit
+import * as React from 'react'
 import type {Dispatch} from 'redux'
 import {connect} from 'react-redux'
-import {selectors as fileDataSelectors} from '../../file-data'
+import {
+  actions as dismissActions,
+  selectors as dismissSelectors
+} from '../../dismiss'
 import {selectors as steplistSelectors} from '../../steplist'
-import Alerts from '../Alerts'
+import {selectors as fileDataSelectors} from '../../file-data'
+import {AlertItem} from '@opentrons/components'
 import type {BaseState} from '../../types'
 import type {CommandCreatorError, CommandCreatorWarning} from '../../step-generation'
 
 type SP = {
-  alerts: Array<CommandCreatorError | {
-    ...CommandCreatorWarning,
-    dismissId?: string // presence of dismissId allows alert to be dismissed
-  }>
+  errors: Array<CommandCreatorError>,
+  warnings: Array<CommandCreatorWarning>,
+  _stepId: *
 }
-type DP = { onDismiss: (id: string) => () => mixed }
+
+type DP = {
+  onDismiss: (CommandCreatorWarning) => () => mixed
+}
+
+type Props = SP & DP
 
 // These captions populate the AlertItem body, the title/message
 // comes from the CommandCreatorError / CommandCreatorWarning
-const TIMELINE_ALERT_CAPTIONS: {[warningOrErrorType: string]: string} = {
+const captions: {[warningOrErrorType: string]: string} = {
   'INSUFFICIENT_TIPS': 'Add another tip rack to an empty slot in Deck Setup',
   'ASPIRATE_MORE_THAN_WELL_CONTENTS': 'You are trying to aspirate more than the current volume of one of your well(s). If you intended to add air to your tip, please use the Air Gap advanced setting.'
 }
 
-const mapStateToProps = (state: BaseState): SP => {
-  const timeline = fileDataSelectors.robotStateTimeline(state)
-  const warningsPerStep = fileDataSelectors.warningsPerStep(state)
-  const selectedStepId = steplistSelectors.selectedStepId(state)
+function Alerts (props: Props) {
+  const errors = props.errors.map((error, key) => (
+    <AlertItem
+      type='warning'
+      key={`error:${key}`}
+      title={error.message}
+      onCloseClick={undefined}
+      >
+        {captions[error.type]}
+      </AlertItem>
+    ))
 
-  const {errors} = timeline
-  const rawWarnings = (
-    // hide warnings without explicit FEATURE FLAG
-    process.env.OT_PD_SHOW_WARNINGS === 'true' &&
-    // show warnings only for the selected step
-    selectedStepId &&
-    warningsPerStep[selectedStepId]
-  ) || []
+  const warnings = props.warnings.map((warning, key) => (
+    <AlertItem
+      type='warning'
+      key={`warning:${key}`}
+      title={warning.message}
+      onCloseClick={props.onDismiss(warning)}
+      >
+        {captions[warning.type]}
+      </AlertItem>
+    ))
 
-  const warnings = rawWarnings.map(warning => ({
-    ...warning,
-    // TODO Ian 2018-06-14 once warning dismissal is actually implemented,
-    // the dismiss ID will probably have more info than just the type.
-    // This is a placeholder.
-    dismissId: warning.type
-  }))
-
-  let alerts = errors ? [...errors, ...warnings] : warnings
-
-  return { alerts, captions: TIMELINE_ALERT_CAPTIONS }
+  return (
+    <div>
+      {errors}
+      {warnings}
+    </div>
+  )
 }
 
-const mapDispatchToProps = (dispatch: Dispatch<*>): DP => ({
-  onDismiss: (id: string) => () => console.log('dismiss warning here', id)
-})
+function mapStateToProps (state: BaseState): SP {
+  const timeline = fileDataSelectors.robotStateTimeline(state)
+  const errors = timeline.errors || []
+  const warnings = dismissSelectors.getVisibleWarningsForSelectedStep(state)
+  const _stepId: any = steplistSelectors.selectedStepId(state) // TODO: Ian 2018-07-02 type properly once stepId is always string type
 
-export default connect(mapStateToProps, mapDispatchToProps)(Alerts)
+  return {
+    errors,
+    warnings,
+    _stepId
+  }
+}
+
+function mergeProps (stateProps: SP, dispatchProps: {dispatch: Dispatch<*>}): Props {
+  const {dispatch} = dispatchProps
+  const onDismiss = (warning: CommandCreatorWarning) =>
+    () => dispatch(dismissActions.dismissWarning({
+      warning,
+      stepId: stateProps._stepId
+    }))
+
+  return {
+    ...stateProps,
+    onDismiss
+  }
+}
+
+export default connect(mapStateToProps, null, mergeProps)(Alerts)
