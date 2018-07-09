eba664ca9a13fa2fb3317893242daf148ab46fbc
Mike Cousins <mike@opentrons.com>
2018-07-09 15:48:47 -0400

    feat(app): Add GET /modules to API client

---
+++ app/src/http-api-client/__tests__/modules.test.js
@@ -0,0 +1,92 @@
+// http api /modules tests
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import client from '../client'
+import {fetchModules, makeGetRobotModules} from '..'
+
+jest.mock('../client')
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+const NAME = 'opentrons-dev'
+
+const modules = [
+  {
+    model: 'model',
+    serial: 'serial',
+    fwVersion: 'fwVersion',
+    status: 'status',
+    displayName: 'displayName'
+  }
+]
+
+describe('/modules', () => {
+  let robot
+  let state
+  let store
+
+  beforeEach(() => {
+    client.__clearMock()
+
+    robot = {name: NAME, ip: '1.2.3.4', port: '1234'}
+    state = {api: {api: {}}}
+    store = mockStore(state)
+  })
+
+  describe('fetchModules action creator', () => {
+    const path = 'modules'
+    const response = {modules}
+
+    test('calls GET /modules', () => {
+      client.__setMockResponse(response)
+
+      return store.dispatch(fetchModules(robot))
+        .then(() =>
+          expect(client).toHaveBeenCalledWith(robot, 'GET', path))
+    })
+
+    test('dispatches api:REQUEST and api:SUCCESS', () => {
+      const request = null
+      const expectedActions = [
+        {type: 'api:REQUEST', payload: {robot, request, path}},
+        {type: 'api:SUCCESS', payload: {robot, response, path}}
+      ]
+
+      client.__setMockResponse(response)
+
+      return store.dispatch(fetchModules(robot))
+        .then(() => expect(store.getActions()).toEqual(expectedActions))
+    })
+
+    test('dispatches api:REQUEST and api:FAILURE', () => {
+      const request = null
+      const error = {name: 'ResponseError', status: 500, message: ''}
+      const expectedActions = [
+        {type: 'api:REQUEST', payload: {robot, request, path}},
+        {type: 'api:FAILURE', payload: {robot, error, path}}
+      ]
+
+      client.__setMockError(error)
+
+      return store.dispatch(fetchModules(robot))
+        .then(() => expect(store.getActions()).toEqual(expectedActions))
+    })
+  })
+
+  describe('selectors', () => {
+    beforeEach(() => {
+      state.api.api[NAME] = {
+        modules: {inProgress: true}
+      }
+    })
+
+    test('makeGetRobotModules', () => {
+      const getModules = makeGetRobotModules()
+
+      expect(getModules(state, robot)).toEqual(state.api.api[NAME].modules)
+      expect(getModules(state, {name: 'foo'})).toEqual({inProgress: false})
+    })
+  })
+})
---
+++ app/src/http-api-client/modules.js
@@ -0,0 +1,53 @@
+// @flow
+// API client for modules (the robot kind)
+import {createSelector, type Selector} from 'reselect'
+
+import type {State, ThunkPromiseAction} from '../types'
+import type {BaseRobot, RobotService} from '../robot'
+import type {ApiCall, ApiRequestError} from './types'
+import type {ApiAction} from './actions'
+
+import {apiRequest, apiSuccess, apiFailure} from './actions'
+import {getRobotApiState} from './reducer'
+import client from './client'
+
+type FetchModulesResponse = {
+  model: string,
+  serial: string,
+  fwVersion: string,
+  status: string,
+  displayName: string,
+}
+
+type FetchModulesCall = ApiCall<null, FetchModulesResponse>
+
+export type ModulesAction =
+  | ApiAction<'modules', null, FetchModulesResponse>
+
+export type ModulesState = {
+  modules?: FetchModulesCall
+}
+
+const MODULES: 'modules' = 'modules'
+
+export function fetchModules (robot: RobotService): ThunkPromiseAction {
+  return (dispatch) => {
+    dispatch(apiRequest(robot, MODULES, null))
+
+    return client(robot, 'GET', MODULES)
+      .then(
+        (resp: FetchModulesResponse) => apiSuccess(robot, MODULES, resp),
+        (err: ApiRequestError) => apiFailure(robot, MODULES, err)
+      )
+      .then(dispatch)
+  }
+}
+
+export function makeGetRobotModules () {
+  const selector: Selector<State, BaseRobot, FetchModulesCall> = createSelector(
+    getRobotApiState,
+    (state) => state[MODULES] || {inProgress: false}
+  )
+
+  return selector
+}
