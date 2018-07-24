b279446b54e47476462952f36fd1fee146d1092f
IanLondon <ianlondondu@gmail.com>
2018-07-24 16:58:14 -0400

    feat(protocol-designer): make WellSelectionInput label change for multi-channel pipette
    -WellSelectionInput cleanup & refactor
    -selecting a single-channel pipette uses label "wells",
    multi-channel uses label "columns"
    
    Addresses #1537

---
+++ protocol-designer/src/components/StepEditForm/WellSelectionInput/WellSelectionInput.js
@@ -0,0 +1,31 @@
+// @flow
+import * as React from 'react'
+import {FormGroup, InputField} from '@opentrons/components'
+import type {StepFieldName} from '../../../steplist/fieldLevel'
+import styles from '../StepEditForm.css'
+
+type Props = {
+  name: StepFieldName,
+  primaryWellCount?: number,
+  disabled: boolean,
+  onClick?: (e: SyntheticMouseEvent<*>) => mixed,
+  errorToShow: ?string,
+  isMulti: ?boolean
+}
+
+export default function WellSelectionInput (props: Props) {
+  return (
+    <FormGroup
+      label={props.isMulti ? 'Columns:' : 'Wells:'}
+      disabled={props.disabled}
+      className={styles.well_selection_input}
+      >
+        <InputField
+          readOnly
+          name={props.name}
+          value={props.primaryWellCount ? String(props.primaryWellCount) : null}
+          onClick={props.onClick}
+          error={props.errorToShow} />
+        </FormGroup>
+  )
+}
