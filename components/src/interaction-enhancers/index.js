afef0935dbe24a59accfb5879ca4859091d6eda7
Kadee80 <kaa328@nyu.edu>
2018-07-26 13:41:34 -0400

    feat(comp): Add IntervalWrapper interaction enhancer

--- components/src/interaction-enhancers/index.js
+++ components/src/interaction-enhancers/index.js
@@ -1,10 +1,12 @@
 // @flow
 import clickOutside from './clickOutside'
 import HandleKeypress from './HandleKeypress'
+import IntervalWrapper from './IntervalWrapper'
 
 export type {ClickOutsideInterface} from './clickOutside'
 
 export {
   clickOutside,
-  HandleKeypress
+  HandleKeypress,
+  IntervalWrapper
 }
