16c2a30da62b9faf9ed964b678b6dadc1af45ea1
Ian London <ianlondondu@gmail.com>
2018-07-30 09:24:26 -0400

    fix(protocol-designer): fix styling of pause and mix step items (#1948)
    Closes #1947

--- protocol-designer/src/components/steplist/PauseStepItems.js
+++ protocol-designer/src/components/steplist/PauseStepItems.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react'
 import type {PauseFormData} from '../../step-generation'
+import {PDListItem} from '../lists'
 
 type Props = {
   substeps: PauseFormData
@@ -10,12 +11,18 @@ export default function PauseStepItems (props: Props) {
   const {substeps} = props
   if (substeps.wait === true) {
     // Show message if waiting indefinitely
-    return <li>{substeps.message}</li>
+    return <PDListItem>{substeps.message}</PDListItem>
   }
   if (!substeps.meta) {
     // No message or time, show nothing
     return null
   }
   const {hours, minutes, seconds} = substeps.meta
-  return <li>{hours} hr {minutes} m {seconds} s</li>
+  return <PDListItem>
+    <span>{hours} hr</span>
+    <span>{minutes} m</span>
+    <span>{seconds} s</span>
+    <span/>
+    <span/>
+  </PDListItem>
 }
