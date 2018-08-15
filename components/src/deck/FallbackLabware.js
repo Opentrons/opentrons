7fff8722d21a2d6559f163e14fbf744c3cdfa5d0
IanLondon <ianlondondu@gmail.com>
2018-08-15 15:53:43 -0400

    use CenteredTextSvg in FallbackLabware

--- components/src/deck/FallbackLabware.js
+++ components/src/deck/FallbackLabware.js
@@ -1,15 +1,17 @@
 // @flow
 import * as React from 'react'
 import LabwareOutline from './LabwareOutline'
+import {CenteredTextSvg} from '../CenteredTextSvg'
 import styles from './Labware.css'
 
 export default function FallbackLabware () {
   return (
     <g>
       <LabwareOutline />
-      <text x='50%' y='50%' textAnchor='middle' className={styles.fallback_plate_text}>
-        Custom Container
-      </text>
+      <CenteredTextSvg
+        className={styles.fallback_plate_text}
+        text='Custom Container'
+      />
     </g>
   )
 }
