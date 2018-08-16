5100a100c63337f77f67dc31d3e8d4bcdebb59ed
Kadee80 <kaa328@nyu.edu>
2018-08-16 09:08:15 -0400

    Remove intercom update on route change

--- app/src/support.js
+++ app/src/support.js
@@ -1,17 +1,12 @@
 // @flow
 // user support module
 import noop from 'lodash/noop'
-import {LOCATION_CHANGE} from 'react-router-redux'
 
 import {version} from './../package.json'
 import createLogger from './logger'
 
 import type {ThunkAction, Middleware} from './types'
 import type {Config} from './config'
-
-// import {
-//   selectors as robotSelectors
-// } from './robot'
 
 type SupportConfig = $PropertyType<Config, 'support'>
 
@@ -23,7 +18,7 @@ const INTERCOM_ID = process.env.OT_APP_INTERCOM_ID
 // intercom handler (default noop)
 let intercom = noop
 let userId
-let robot = ''
+let robot
 
 export function initializeSupport (): ThunkAction {
   return (_, getState) => {
@@ -35,12 +30,6 @@ export function initializeSupport (): ThunkAction {
 }
 
 export const supportMiddleware: Middleware = (store) => (next) => (action) => {
-  // update intercom on page change
-  // TODO(mc, 2018-08-02): this is likely to hit intercom throttle limit
-  if (action.type === LOCATION_CHANGE) {
-    intercom('update')
-  }
-
   if (action.type === 'robot:CONNECT_RESPONSE') {
     const state = store.getState()
     robot = state.robot.connection.connectRequest.name
