cfd6dad0f10503ebacfa5927ee964005a828b276
IanLondon <ianlondondu@gmail.com>
2018-08-20 12:52:43 -0400

    remove selector factories

--- protocol-designer/src/top-selectors/tip-contents/index.js
+++ protocol-designer/src/top-selectors/tip-contents/index.js
@@ -5,8 +5,7 @@ import * as StepGeneration from '../../step-generation'
 import {
   selectors as steplistSelectors,
   START_TERMINAL_ITEM_ID,
-  END_TERMINAL_ITEM_ID,
-  type TerminalItemId
+  END_TERMINAL_ITEM_ID
 } from '../../steplist'
 import {selectors as fileDataSelectors} from '../../file-data'
 import {getWellSetForMultichannel} from '../../well-selection/utils'
@@ -15,6 +14,10 @@ import type {Selector} from '../../types'
 import type {ElementProps} from 'react'
 import {typeof Labware} from '@opentrons/components'
 type GetTipProps = $PropertyType<ElementProps<Labware>, 'getTipProps'>
+
+function getLabwareIdProp (state, props: {labwareId: string}) {
+  return props.labwareId
+}
 
 function getTipHighlighted (
   labwareId: string,
@@ -59,58 +62,77 @@ function getTipEmpty (
   )
 }
 
-export const makeGetTerminalTips = (terminalId: TerminalItemId, labwareId: string) => {
-  let robotStateSelector
-  if (terminalId === START_TERMINAL_ITEM_ID) {
-    robotStateSelector = fileDataSelectors.getInitialRobotState
-  } else if (terminalId === END_TERMINAL_ITEM_ID) {
-    robotStateSelector = fileDataSelectors.lastValidRobotState
-  } else {
-    console.error(`Invalid terminalId ${terminalId}, could not makeGetTerminalTips`)
-    return noop
-  }
-  const getInitialTips: Selector<GetTipProps> = createSelector(
-    robotStateSelector,
-    (initialRobotState) => (wellName: string) => ({
+const getInitialTips: Selector<GetTipProps> = createSelector(
+  fileDataSelectors.getInitialRobotState,
+  getLabwareIdProp,
+  (initialRobotState, labwareId) =>
+    (wellName: string) => ({
       empty: getTipEmpty(wellName, labwareId, initialRobotState),
       highlighted: false
     })
-  )
-  return getInitialTips
-}
+)
 
-type TipsPerStep = {[stepId: number]: GetTipProps}
-export const makeGetTipsPerStep = (labwareId: string) => {
-  const getTipsPerStep: Selector<TipsPerStep> = createSelector(
-    steplistSelectors.orderedSteps,
-    fileDataSelectors.robotStateTimeline,
-    (orderedSteps, robotStateTimeline) => {
-      const timeline = robotStateTimeline.timeline
-      return orderedSteps.reduce((acc: TipsPerStep, stepId, timelineIdx) => {
-        const prevFrame: ?* = timeline[timelineIdx - 1]
-        const currentFrame: ?* = timeline[timelineIdx]
+const getLastValidTips: Selector<GetTipProps> = createSelector(
+  fileDataSelectors.lastValidRobotState,
+  getLabwareIdProp,
+  (lastValidRobotState, labwareId) =>
+    (wellName: string) => ({
+      empty: getTipEmpty(wellName, labwareId, lastValidRobotState),
+      highlighted: false
+    })
+)
 
-        return {
-          ...acc,
-          [stepId]: (wellName: string) => {
-            // show empty/present tip state at end of previous frame
-            const empty = (prevFrame)
-              ? getTipEmpty(wellName, labwareId, prevFrame.robotState)
-              : false
-
-            // show highlights of tips used by current frame
-            const highlighted = (currentFrame)
-              ? getTipHighlighted(labwareId, wellName, currentFrame)
-              : false
-
-            return {
-              empty,
-              highlighted
-            }
-          }
-        }
-      }, {})
+export const getTipsForCurrentStep: Selector<GetTipProps> = createSelector(
+  steplistSelectors.orderedSteps,
+  fileDataSelectors.robotStateTimeline,
+  steplistSelectors.getHoveredStepId,
+  steplistSelectors.getActiveItem,
+  getInitialTips,
+  getLastValidTips,
+  getLabwareIdProp,
+  (orderedSteps, robotStateTimeline, hoveredStepId, activeItem, initialTips, lastValidTips, labwareId) => {
+    if (!activeItem.isStep) {
+      const terminalId = activeItem.id
+      if (terminalId === START_TERMINAL_ITEM_ID) {
+        return initialTips
+      } else if (terminalId === END_TERMINAL_ITEM_ID) {
+        return lastValidTips
+      } else {
+        console.error(`Invalid terminalId ${terminalId}, could not getTipsForCurrentStep`)
+        return noop
+      }
     }
-  )
-  return getTipsPerStep
-}
+
+    const stepId = activeItem.id
+    const hovered = stepId === hoveredStepId
+    const timeline = robotStateTimeline.timeline
+    const timelineIdx = orderedSteps.includes(stepId)
+      ? orderedSteps.findIndex(id => id === stepId)
+      : null
+
+    if (timelineIdx == null) {
+      console.error(`Expected non-null timelineIdx for step ${stepId}`)
+      return noop
+    }
+
+    const prevFrame = timeline[timelineIdx - 1]
+    const currentFrame = timeline[timelineIdx]
+
+    return (wellName: string) => {
+      // show empty/present tip state at end of previous frame
+      const empty = (prevFrame)
+        ? getTipEmpty(wellName, labwareId, prevFrame.robotState)
+        : false
+
+      // show highlights of tips used by current frame, if user is hovering
+      const highlighted = (hovered && currentFrame)
+        ? getTipHighlighted(labwareId, wellName, currentFrame)
+        : false
+
+      return {
+        empty,
+        highlighted
+      }
+    }
+  }
+)
