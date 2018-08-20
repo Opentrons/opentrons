87862fcb8277ae0246500166c0783e0604019476
Brian Cooper <brian@allegro.com>
2018-08-20 17:14:47 -0400

    move into values into react state

--- protocol-designer/src/components/StepEditForm/WellOrderInput/WellOrderModal.js
+++ protocol-designer/src/components/StepEditForm/WellOrderInput/WellOrderModal.js
@@ -5,24 +5,49 @@ import {connect} from 'react-redux'
 
 import i18n from '../../../localization'
 import {Portal} from '../../portals/MainPageModalPortal'
-import {Modal, OutlineButton, LabeledValue, FormGroup} from '@opentrons/components'
+import {Modal, OutlineButton, LabeledValue, FormGroup, DropdownField} from '@opentrons/components'
 import modalStyles from '../../modals/modal.css'
-import {actions} from '../../../steplist'
-import { WellOrderField } from '../fields'
+import {actions, selectors} from '../../../steplist'
 import WellOrderViz from './WellOrderViz'
 
 import styles from './WellOrderInput.css'
 
-type DP = { setDefaults: () => mixed }
+export type WellOrderOrdinality = 'first' | 'second'
+export type WellOrderOption = 'l2r' | 'r2l' | 't2b' | 'b2t'
+const VERTICAL_VALUES: Array<WellOrderOption> = ['t2b', 'b2t']
+const HORIZONTAL_VALUES: Array<WellOrderOption> = ['l2r', 'r2l']
+const WELL_ORDER_VALUES: Array<WellOrderOption> = [...VERTICAL_VALUES, ...HORIZONTAL_VALUES]
 
-type Props = {
+type SP = {
+  initialFirstValue: WellOrderOption,
+  initialSecondValue: WellOrderOption
+}
+type DP = {
+  setDefaults: () => mixed,
+  updateValues: (firstValue: WellOrderOption, secondValue: WellOrderOption) => mixed
+}
+
+type OP = {
   isOpen: boolean,
   closeModal: (e: SyntheticEvent<*>) => mixed,
   onSave: () => mixed,
   prefix: 'aspirate' | 'dispense'
 }
 
-class WellOrderModal extends React.Component<Props & DP> {
+type Props = OP & SP & DP
+type State = {
+  firstValue: WellOrderOption,
+  secondValue: WellOrderOption,
+}
+
+class WellOrderModal extends React.Component<Props, State> {
+  constructor (props: Props) {
+    super(props)
+    this.state = {
+      firstValue: props.initialFirstValue,
+      secondValue: props.initialSecondValue
+    }
+  }
   handleReset = () => {
     this.props.setDefaults()
     this.props.closeModal()
@@ -30,8 +55,28 @@ class WellOrderModal extends React.Component<Props & DP> {
   handleClose = () => {
     this.props.closeModal()
   }
+  makeOnChange = (ordinality: 'first' | 'second') => (e: SyntheticEvent<HTMLSelectElement>) => {
+    const {value} = e.currentTarget
+    let nextState = {[`${ordinality}Value`]: value}
+    if (ordinality === 'first') {
+      if (VERTICAL_VALUES.includes(value) && VERTICAL_VALUES.includes(this.state.secondValue)) {
+        nextState = {...nextState, secondValue: HORIZONTAL_VALUES[0]}
+      } else if (HORIZONTAL_VALUES.includes(value) && HORIZONTAL_VALUES.includes(this.state.secondValue)) {
+        nextState = {...nextState, secondValue: VERTICAL_VALUES[0]}
+      }
+    }
+    this.setState(nextState)
+  }
+  isSecondOptionDisabled = (value: WellOrderOption) => {
+    if (VERTICAL_VALUES.includes(this.state.firstValue)) {
+      return VERTICAL_VALUES.includes(value)
+    } else if (HORIZONTAL_VALUES.includes(this.state.firstValue)) {
+      return HORIZONTAL_VALUES.includes(value)
+    }
+  }
   render () {
     if (!this.props.isOpen) return null
+    const {firstValue, secondValue} = this.state
     return (
       <Portal>
         <Modal
@@ -46,13 +91,33 @@ class WellOrderModal extends React.Component<Props & DP> {
           <div className={styles.main_row}>
             <FormGroup label={i18n.t('modal.well_order.field_label')}>
               <div className={styles.field_row}>
-                <WellOrderField prefix={this.props.prefix} ordinality="first" />
+                <DropdownField
+                  value={firstValue}
+                  onChange={this.makeOnChange('first')}
+                  options={
+                    WELL_ORDER_VALUES.map((value) => ({
+                      value,
+                      name: i18n.t(`step_edit_form.field.well_order.option.${value}`)
+                    }))
+                  } />
                 <span className={styles.field_spacer}>{i18n.t('modal.well_order.then')}</span>
-                <WellOrderField prefix={this.props.prefix} ordinality="second" />
+                <DropdownField
+                  value={secondValue}
+                  onChange={this.makeOnChange('second')}
+                  options={
+                    WELL_ORDER_VALUES.map((value) => ({
+                      value,
+                      name: i18n.t(`step_edit_form.field.well_order.option.${value}`),
+                      disabled: this.isSecondOptionDisabled(value)
+                    }))
+                  } />
               </div>
             </FormGroup>
             <FormGroup label={i18n.t('modal.well_order.viz_label')} className={styles.viz_wrapper}>
-              <WellOrderViz prefix={this.props.prefix} />
+              <WellOrderViz
+                prefix={this.props.prefix}
+                firstValue={firstValue}
+                secondValue={secondValue} />
             </FormGroup>
           </div>
           <div className={styles.button_row}>
@@ -69,15 +134,34 @@ class WellOrderModal extends React.Component<Props & DP> {
   }
 }
 
+const mapSTP = (state: BaseState, ownProps: OP): SP => {
+  const formData = selectors.getUnsavedForm(state)
+  const {prefix} = ownProps
+  const initialFirstValue = formData ? formData[`${prefix}_wellOrder_first`] : null
+  const initialSecondValue = formData ? formData[`${prefix}_wellOrder_second`] : null
+  return {
+    initialFirstValue,
+    initialSecondValue
+  }
+}
+
 const mapDTP = (dispatch: Dispatch, ownProps): DP => {
   const {prefix} = ownProps
   return {
     // TODO: BC 2018-08-19 put these defaults in consolidated location
     setDefaults: () => {
-      dispatch(actions.changeFormInput({update: {[`${prefix}_wellOrder_first`]: 'l2r'}}))
-      dispatch(actions.changeFormInput({update: {[`${prefix}_wellOrder_second`]: 't2b'}}))
+      dispatch(actions.changeFormInput({update: {
+        [`${prefix}_wellOrder_first`]: 'l2r',
+        [`${prefix}_wellOrder_second`]: 't2b'
+      }}))
+    },
+    updateValues: (firstValue, secondValue) => {
+      dispatch(actions.changeFormInput({update: {
+        [`${prefix}_wellOrder_first`]: firstValue,
+        [`${prefix}_wellOrder_second`]: secondValue
+      }}))
     }
   }
 }
 
-export default connect(null, mapDTP)(WellOrderModal)
+export default connect(mapSTP, mapDTP)(WellOrderModal)
--- protocol-designer/src/components/StepEditForm/WellOrderInput/WellOrderViz.js
+++ protocol-designer/src/components/StepEditForm/WellOrderInput/WellOrderViz.js
@@ -1,37 +1,27 @@
 // @flow
 import * as React from 'react'
-import {connect} from 'react-redux'
 import cx from 'classnames'
 
 import WELLS_IMAGE from '../../../images/well_order_wells.svg'
 import PATH_IMAGE from '../../../images/well_order_path.svg'
 
-import {selectors} from '../../../steplist'
 import type {WellOrderOption} from '../formFields'
 
 import styles from './WellOrderInput.css'
 
-type OP = {prefix: 'aspirate' | 'dispense'}
-
-type SP = {first: WellOrderOption, second: WellOrderOption}
-const WellOrderViz = (props): WellOrderVizProps => {
-  return (
-    <div className={styles.viz_wrapper}>
-      <img src={WELLS_IMAGE} className={styles.wells_image} />
-      <img
-        src={PATH_IMAGE}
-        className={cx(styles.path_image, styles[`${props.first}_first`], styles[`${props.second}_second`])} />
-    </div>
-  )
+type Props = {
+  prefix: 'aspirate' | 'dispense',
+  firstValue: WellOrderOption,
+  secondValue: WellOrderOption
 }
 
-const mapSTP = (state: BaseState, ownProps: OP): SP => {
-  const formData = selectors.getUnsavedForm(state)
-  const {prefix} = ownProps
-  return {
-    first: formData ? formData[`${prefix}_wellOrder_first`] : null,
-    second: formData ? formData[`${prefix}_wellOrder_second`] : null
-  }
-}
+const WellOrderViz = (props): Props => (
+  <div className={styles.viz_wrapper}>
+    <img src={WELLS_IMAGE} className={styles.wells_image} />
+    <img
+      src={PATH_IMAGE}
+      className={cx(styles.path_image, styles[`${props.firstValue}_first`], styles[`${props.secondValue}_second`])} />
+  </div>
+)
 
-export default connect(mapSTP)(WellOrderViz)
+export default WellOrderViz
