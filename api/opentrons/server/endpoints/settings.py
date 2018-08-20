f42ae1b44b651b764c2f318e0cded6b3827d0365
Seth Foster <sfoster1@users.noreply.github.com>
2018-08-20 17:43:39 -0400

    feat(api): Robot Reset (#2082)
    * chore(api): Move settings endpoints to their own file and add reset
    
    The reset endpoints are just stubs, but now that we’re adding more things under
    the /settings uri we might as well have them somewhere other than endpoints.__init__.
    
    * feat(api): Add robot reset endpoints and implementation to API (#1855)
    
    Add endpoints at GET /settings/reset/options and PUT /settings/reset to, respectively, query what
    reset options are available and reset any subset of those options to their factory configurations.
    This allows us to reset only the parts of the configuration that a user might want, and avoid
    resetting those configurations that are required for operation and difficult to change.

---
+++ api/opentrons/server/endpoints/settings.py
@@ -0,0 +1,103 @@
+import logging
+import shutil
+import os
+from aiohttp import web
+from opentrons.config import advanced_settings as advs
+from opentrons.robot import robot_configs as rc
+from opentrons.data_storage import database as db
+
+log = logging.getLogger(__name__)
+
+_settings_reset_options = [
+    {
+        'id': 'deckCalibration',
+        'name': 'Deck Calibration',
+        'description': 'Reset calibration of pipette to deck'
+    },
+    {
+        'id': 'tipProbe',
+        'name': 'Tip Probe Calibration',
+        'description': 'Erase tip probe data'
+    },
+    {
+        'id': 'labwareCalibration',
+        'name': 'Labware Calibration',
+        'description': 'Erase custom labware calibration'
+    },
+    {
+        'id': 'bootScripts',
+        'name': 'Boot Scripts',
+        'description': 'Erase custom boot scripts'
+    }
+]
+
+
+async def get_advanced_settings(request: web.Request) -> web.Response:
+    """
+    Handles a GET request and returns a json body with the key "settings" and a
+    value that is a list of objects where each object has keys "id", "title",
+    "description", and "value"
+    """
+    res = _get_adv_settings()
+    return web.json_response(res)
+
+
+def _get_adv_settings() -> dict:
+    data = advs.get_all_adv_settings()
+    return {"settings": list(data.values())}
+
+
+async def set_advanced_setting(request: web.Request) -> web.Response:
+    """
+    Handles a POST request with a json body that has keys "id" and "value",
+    where the value of "id" must correspond to an id field of a setting in
+    `opentrons.config.advanced_settings.settings`. Saves the value of "value"
+    for the setting that matches the supplied id.
+    """
+    data = await request.json()
+    key = data.get('id')
+    value = data.get('value')
+    if key and key in advs.settings_by_id.keys():
+        advs.set_adv_setting(key, value)
+        res = _get_adv_settings()
+        status = 200
+    else:
+        res = {'message': 'ID {} not found in settings list'.format(key)}
+        status = 400
+    return web.json_response(res, status=status)
+
+
+async def reset(request: web.Request) -> web.Response:
+    """ Execute a reset of the requested parts of the user configuration.
+    """
+    data = await request.json()
+    for requested_reset in data.keys():
+        if requested_reset not in [opt['id']
+                                   for opt in _settings_reset_options]:
+            log.error('Bad reset option {} requested'.format(requested_reset))
+            return web.json_response(
+                {'message': '{} is not a valid reset option'
+                 .format(requested_reset)},
+                status=400)
+    log.info("Reset requested for {}".format(', '.join(data.keys())))
+    if data.get('deckCalibration'):
+        rc.clear(calibration=True, robot=False)
+    if data.get('tipProbe'):
+        config = rc.load()
+        config.tip_length.clear()
+        rc.save_robot_settings(config)
+    if data.get('labwareCalibration'):
+        db.reset()
+    if data.get('bootScripts'):
+        if os.environ.get('RUNNING_ON_PI'):
+            if os.path.exists('/data/boot.d'):
+                shutil.rmtree('/data/boot.d')
+        else:
+            log.debug('Not on pi, not removing /data/boot.d')
+    return web.json_response({}, status=200)
+
+
+async def available_resets(request: web.Request) -> web.Response:
+    """ Indicate what parts of the user configuration are available for reset.
+    """
+    return web.json_response(_settings_reset_options, status=200)
