e4cf249ce6838f35a6d77fe26b6c4b45d604fced
Ian London <ianlondondu@gmail.com>
2018-09-11 16:30:34 -0400

    feat(protocol-library-kludge): set up OT2 deckmap mini-app kludge (#2210)
    Closes #2145

---
+++ protocol-library-kludge/src/App.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react'
+import URLDeck from './URLDeck'
+import './globals.css'
+
+export default function App () {
+  return (
+    <URLDeck />
+  )
+}
---
+++ protocol-library-kludge/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {AppContainer} from 'react-hot-loader'
+import App from './App'
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App)
+  })
+}
