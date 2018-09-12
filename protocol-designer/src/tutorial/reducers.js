d2982e10d87dde04b250d64f6d2d876cabbc89fc
Brian Arthur Cooper <brian@allegro.com>
2018-09-12 14:10:56 -0400

    feat(protocol-designer): auto dismiss no liquid hint (#2220)
    Upon adding liquid to deck after triggering no liquid hint, remove hint.

--- protocol-designer/src/tutorial/reducers.js
+++ protocol-designer/src/tutorial/reducers.js
@@ -2,6 +2,8 @@
 import {combineReducers} from 'redux'
 import {handleActions} from 'redux-actions'
 import uniq from 'lodash/uniq'
+import without from 'lodash/without'
+import type {EditIngredient} from '../labware-ingred/actions'
 import type {HintKey} from './index'
 import type {AddHintAction, RemoveHintAction} from './actions'
 
@@ -9,6 +11,9 @@ type HintReducerState = Array<HintKey>
 const hints = handleActions({
   ADD_HINT: (state: HintReducerState, action: AddHintAction): HintReducerState => (
     uniq([...state, action.payload.hint])
+  ),
+  EDIT_INGREDIENT: (state: HintReducerState, action: EditIngredient) => (
+    without(state, 'add_liquids_and_labware')
   ),
 }, [])
 
