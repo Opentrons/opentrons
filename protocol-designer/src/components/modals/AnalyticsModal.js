6c75e82064053ced31b7b74e9e09f59dc4e55316
Brian Cooper <brian@allegro.com>
2018-09-26 10:28:15 -0400

    fix(protocol-designer): tweak analytics copy for accuracy
    Update the copy in the analytics disclaimer to accurately portray our data collection in beta.

--- protocol-designer/src/components/modals/AnalyticsModal.js
+++ protocol-designer/src/components/modals/AnalyticsModal.js
@@ -61,7 +61,7 @@ class AnalyticsModal extends React.Component<*, State> {
           <p className={settingsStyles.card_body}>{i18n.t('card.body.reason_for_collecting_data')}</p>
           <ul className={settingsStyles.card_point_list}>
             <li>{i18n.t('card.body.data_collected_is_internal')}</li>
-            <li>{i18n.t('card.body.data_only_from_pd')}</li>
+            {/* TODO: BC 2018-09-26 uncomment when only using fullstory <li>{i18n.t('card.body.data_only_from_pd')}</li> */}
             <li>{i18n.t('card.body.opt_out_of_data_collection')}</li>
           </ul>
         </div>
