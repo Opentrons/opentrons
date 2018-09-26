9d08b623be7e8af76c854509ab4a23b7e81f75a8
IanLondon <ianlondondu@gmail.com>
2018-09-26 11:17:53 -0400

    feat(components): create new tab-styled vertical nav bar
    Closes #1923

---
+++ components/src/tabbedNav/index.js
@@ -0,0 +1,11 @@
+// @flow
+// navigational components
+import TabbedNavBar from './TabbedNavBar'
+import NavTab from './NavTab'
+import OutsideLinkTab from './OutsideLinkTab'
+
+export {
+  TabbedNavBar,
+  NavTab,
+  OutsideLinkTab,
+}
