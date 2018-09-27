35e0003f7398209ede7e78644cd1feef53c74948
Benjamin Morris <btmorr@users.noreply.github.com>
2018-09-27 16:09:20 -0400

    Add docstrings and tests

--- api/opentrons/labware2/__init__.py
+++ api/opentrons/labware2/__init__.py
@@ -1,10 +1,12 @@
 """
 This module will replace Placeable, and should eventually be renamed to
-`labware`
+`labware` and moved to the protocol_api module.
 """
-from typing import List, Tuple, Dict
+from typing import List, Dict
 from enum import Enum, auto
-import math
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y', 'z'])
 
 
 class WellShape(Enum):
@@ -19,11 +21,18 @@ well_shapes = {
 
 
 class Well:
-    def __init__(self, well_props: dict) -> None:
-        self._relative_position = (
-            well_props['x'],
-            well_props['y'],
-            well_props['z'] + well_props['depth'])
+    def __init__(self, well_props: dict, parent: Point) -> None:
+        """
+        Create a well, and track the Point corresponding to the top-center of
+        the well (this Point is in absolute deck coordinates)
+        :param well_props: a dict that conforms to the json-schema for a Well
+        :param parent: a Point representing the absolute position of the parent
+            of the Well (usually the lower-left corner of a labware)
+        """
+        self._position = Point(
+            x=well_props['x'] + parent.x,
+            y=well_props['y'] + parent.y,
+            z=well_props['z'] + well_props['depth'] + parent.z)
 
         self._shape = well_shapes.get(well_props['shape'])
         if self._shape is WellShape.RECTANGULAR:
@@ -41,22 +50,59 @@ class Well:
 
         self._depth = well_props['depth']
 
-    def top(self) -> Tuple[float, float, float]:
-        return self._relative_position
+    def top(self) -> Point:
+        """
+        :return: a Point corresponding to the top-center of the well, relative
+        to the origin of the labware
+        """
+        return self._position
 
-    def bottom(self) -> Tuple[float, float, float]:
+    def bottom(self) -> Point:
+        """
+        :return: a Point corresponding to the bottom-center of the well,
+        relative to the origin of the labware
+        """
         top = self.top()
-        bottom_z = top[2] - self._depth
-        return top[0], top[1], bottom_z
+        bottom_z = top.z - self._depth
+        return Point(x=top.x, y=top.y, z=bottom_z)
 
-    def center(self) -> Tuple[float, float, float]:
+    def center(self) -> Point:
+        """
+        :return: a Point corresponding to the center of the well, relative to
+        the origin of the labware
+        """
         top = self.top()
-        center_z = top[2] - (self._depth / 2.0)
-        return top[0], top[1], center_z
+        center_z = top.z - (self._depth / 2.0)
+        return Point(x=top.x, y=top.y, z=center_z)
 
     def _from_center_cartesian(
-            self, x: float, y: float, z: float) -> Tuple[float, float, float]:
-        c_x, c_y, c_z = self.center()
+            self, x: float, y: float, z: float) -> Point:
+        """
+        Specifies an arbitrary point relative to the center of the well based
+        on percentages of the radius in each axis. For example, to specify the
+        back-right corner of a well at 1/4 of the well depth from the bottom,
+        the call would be `_from_center_cartesian(1, 1, -0.5)`. This point is
+        relative to origin of the labware containing the well, so if the well
+        defintion specifies its top-center as (100, 25, 20) and a depth of 18,
+        and a diameter of 8, then `_from_center_cartesian(0.5, -0.5, 0.1)`
+        would return (102, 23, 12.8).
+
+        Note that the behavior of the `z` parameter is identical to `h` for
+        `_from_center_polar`.
+
+        No checks are performed to ensure that the resulting position will be
+        inside of the well.
+
+        :param x: a float in the range [-1.0, 1.0] for a percentage of half of
+            the radius/width in the X axis
+        :param y: a float in the range [-1.0, 1.0] for a percentage of half of
+            the radius/length in the Y axis
+        :param z: a float in the range [-1.0, 1.0] for a percentage of half of
+            the height above/below the center
+
+        :return: a Point relative to the center of the well
+        """
+        center = self.center()
         if self._shape is WellShape.RECTANGULAR:
             x_size = self._width
             y_size = self._length
@@ -65,31 +111,10 @@ class Well:
             y_size = self._diameter
         z_size = self._depth
 
-        return (
-            c_x + (x * (x_size / 2.0)),
-            c_y + (y * (y_size / 2.0)),
-            c_z + (z * (z_size / 2.0)))
-
-    def _from_center_polar(
-            self,
-            r: float, theta: float, h: float) -> Tuple[float, float, float]:
-        c_x, c_y, c_z = self.center()
-        if self._shape is WellShape.RECTANGULAR:
-            x_size = self._width
-            y_size = self._length
-        else:
-            x_size = self._diameter
-            y_size = self._diameter
-        z_size = self._depth
-
-        delta_x = r * x_size / 2.0 * math.cos(theta)
-        delta_y = r * y_size / 2.0 * math.sin(theta)
-        delta_z = h * z_size
-
-        return (
-            c_x + delta_x,
-            c_y + delta_y,
-            c_z + delta_z)
+        return Point(
+            x=center.x + (x * (x_size / 2.0)),
+            y=center.y + (y * (y_size / 2.0)),
+            z=center.z + (z * (z_size / 2.0)))
 
 
 class Labware:
@@ -98,7 +123,7 @@ class Labware:
     tip rack, etc. It defines the physical geometry of the labware, and
     provides methods for accessing wells within the labware.
     """
-    def __init__(self, definition: dict) -> None:
+    def __init__(self, definition: dict, parent: Point) -> None:
         pass
 
     def wells(self) -> List[Well]:
@@ -120,16 +145,37 @@ class Labware:
         pass
 
 
-def load(displayName: str) -> Labware:
+def _load_definition_by_name(name: str) -> dict:
     """
-    Load a labware definition by name. Definition must have been previously
-    stored locally on the robot.
+    Look up and return a definition by name (name is expected to correspond to
+    the filename of the definition, with the .json extension) and return it or
+    raise an exception
     """
-    pass
+    raise NotImplementedError
 
 
-def load_from_definition(definition: dict) -> Labware:
+def _get_slot_position(slot: str) -> Point:
     """
-    Create a labware object from a labware definition dict
+    :param slot: a string corresponding to a slot on the deck
+    :return: a Point representing the position of the lower-left corner of the
+        slot
     """
-    return Labware(definition)
+    raise NotImplementedError
+
+
+def load(name: str, slot: str) -> Labware:
+    """
+    Return a labware object constructed from a labware definition dict looked
+    up by name (definition must have been previously stored locally on the
+    robot)
+    """
+    definition = _load_definition_by_name(name)
+    return load_from_definition(definition, slot)
+
+
+def load_from_definition(definition: dict, slot: str) -> Labware:
+    """
+    Return a labware object constructed from a provided labware definition dict
+    """
+    point = _get_slot_position(slot)
+    return Labware(definition, point)
--- api/tests/opentrons/labware/test_labware.py
+++ api/tests/opentrons/labware/test_labware.py
@@ -6,7 +6,7 @@ test_data = {
         'depth': 40,
         'totalLiquidVolume': 100,
         'diameter': 30,
-        'x': 10,
+        'x': 40,
         'y': 50,
         'z': 3
     },
@@ -24,30 +24,72 @@ test_data = {
 
 
 def test_well_init():
-    well1 = labware2.Well(test_data['circular_well_json'])
+    slot = labware2.Point(1, 2, 3)
+    well1 = labware2.Well(test_data['circular_well_json'], slot)
     assert well1._diameter == test_data['circular_well_json']['diameter']
     assert well1._length is None
     assert well1._width is None
 
-    well2 = labware2.Well(test_data['rectangular_well_json'])
+    well2 = labware2.Well(test_data['rectangular_well_json'], slot)
     assert well2._diameter is None
     assert well2._length == test_data['rectangular_well_json']['length']
     assert well2._width == test_data['rectangular_well_json']['width']
 
 
 def test_top():
-    well = labware2.Well(test_data['circular_well_json'])
+    slot = labware2.Point(4, 5, 6)
+    well = labware2.Well(test_data['circular_well_json'], slot)
     well_data = test_data['circular_well_json']
-    expected_x = well_data['x']
-    expected_y = well_data['y']
-    expected_z = well_data['z'] + well_data['depth']
+    expected_x = well_data['x'] + slot.x
+    expected_y = well_data['y'] + slot.y
+    expected_z = well_data['z'] + well_data['depth'] + slot.z
     assert well.top() == (expected_x, expected_y, expected_z)
 
 
 def test_bottom():
-    well = labware2.Well(test_data['rectangular_well_json'])
+    slot = labware2.Point(7, 8, 9)
+    well = labware2.Well(test_data['rectangular_well_json'], slot)
     well_data = test_data['rectangular_well_json']
-    expected_x = well_data['x']
-    expected_y = well_data['y']
-    expected_z = well_data['z']
+    expected_x = well_data['x'] + slot.x
+    expected_y = well_data['y'] + slot.y
+    expected_z = well_data['z'] + slot.z
     assert well.bottom() == (expected_x, expected_y, expected_z)
+
+
+def test_from_center_cartesian():
+    slot1 = labware2.Point(10, 11, 12)
+    well1 = labware2.Well(test_data['circular_well_json'], slot1)
+
+    percent1_x = 1
+    percent1_y = 1
+    percent1_z = -0.5
+    point1 = well1._from_center_cartesian(percent1_x, percent1_y, percent1_z)
+
+    # slot.x + well.x + 1 * well.diamter/2
+    expected_x = 10 + 40 + 15
+    # slot.y + well.y + 1 * well.diamter/2
+    expected_y = 11 + 50 + 15
+    # slot.z + well.z + (1 - 0.5) * well.depth/2
+    expected_z = 12 + 3 + 20 - 10
+
+    assert point1.x == expected_x
+    assert point1.y == expected_y
+    assert point1.z == expected_z
+
+    slot2 = labware2.Point(13, 14, 15)
+    well2 = labware2.Well(test_data['rectangular_well_json'], slot2)
+    percent2_x = -0.25
+    percent2_y = 0.1
+    percent2_z = 0.9
+    point2 = well2._from_center_cartesian(percent2_x, percent2_y, percent2_z)
+
+    # slot.x + well.x - 0.25 * well.width/2
+    expected_x = 13 + 45 - 15
+    # slot.y + well.y + 0.1 * well.length/2
+    expected_y = 14 + 10 + 2.5
+    # slot.z + well.z + (1 + 0.9) * well.depth/2
+    expected_z = 15 + 22 + 19
+
+    assert point2.x == expected_x
+    assert point2.y == expected_y
+    assert point2.z == expected_z
