4934c477e02310261c6fb29fa0bf0cc3d5dd156d
Ian London <ianlondondu@gmail.com>
2018-10-05 10:03:40 -0400

    feat(protocol-designer): modify the "name new labware" overlay for new design (#2422)
    Closes #2410

--- protocol-designer/src/components/labware/NameThisLabwareOverlay.js
+++ protocol-designer/src/components/labware/NameThisLabwareOverlay.js
@@ -1,22 +1,21 @@
 // @flow
 import * as React from 'react'
-import ForeignDiv from '../../components/ForeignDiv.js'
 import ClickableText from './ClickableText'
 import styles from './labware.css'
-import type {ClickOutsideInterface} from '@opentrons/components'
+import ForeignDiv from '../../components/ForeignDiv'
+import i18n from '../../localization'
+import {ClickOutside} from '@opentrons/components'
 
 type Props = {
   setLabwareName: (name: ?string) => mixed,
   // TODO Ian 2018-02-16 type these fns elsewhere and import the type
-  deleteLabware: () => mixed,
-} & ClickOutsideInterface
+  editLiquids: () => mixed,
+}
 
 type State = {
   pristine: boolean,
   inputValue: string,
 }
-
-const NICKNAME_PROMPT = 'Add a nickname?'
 
 export default class NameThisLabwareOverlay extends React.Component<Props, State> {
   constructor (props: Props) {
@@ -45,33 +44,37 @@ export default class NameThisLabwareOverlay extends React.Component<Props, State
     this.props.setLabwareName(containerName)
   }
 
+  onAddLiquids = () => {
+    this.onSubmit()
+    this.props.editLiquids()
+  }
+
   render () {
-    const {
-      deleteLabware,
-      passRef,
-    } = this.props
-
     return (
-      <g className={styles.slot_overlay} ref={passRef}>
-        <rect className={styles.overlay_panel} />
-        <g transform='translate(5, 0)'>
-          <ForeignDiv x='0' y='15%' width='90%'>
-            <input
-              className={styles.name_input}
-              onChange={this.handleChange}
-              onKeyUp={this.handleKeyUp}
-              placeholder={NICKNAME_PROMPT}
-              value={this.state.inputValue}
-            />
-          </ForeignDiv>
+      <ClickOutside onClickOutside={this.onSubmit}>
+        {({ref}) => (
+          <g className={styles.slot_overlay} ref={ref}>
+            <rect className={styles.overlay_panel} />
+            <g transform='translate(5, 0)'>
+              <ForeignDiv x='0' y='15%' width='92%'>
+                <input
+                  className={styles.name_input}
+                  onChange={this.handleChange}
+                  onKeyUp={this.handleKeyUp}
+                  placeholder={i18n.t('labware_overlays.name_labware.nickname_placeholder')}
+                  value={this.state.inputValue}
+                />
+              </ForeignDiv>
 
-          <ClickableText onClick={this.onSubmit}
-            iconName='check' y='60%' text='Save' />
+              <ClickableText onClick={this.onAddLiquids}
+                iconName='water' y='50%' text={i18n.t('labware_overlays.name_labware.add_liquids')} />
 
-          <ClickableText onClick={deleteLabware}
-            iconName='close' y='80%' text='Cancel' />
-        </g>
-      </g>
+              <ClickableText onClick={this.onSubmit}
+                iconName='ot-water-outline' y='75%' text={i18n.t('labware_overlays.name_labware.leave_empty')} />
+            </g>
+          </g>
+        )}
+      </ClickOutside>
     )
   }
 }
