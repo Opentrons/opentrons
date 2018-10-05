5a4e68e39a3d234eb4db7dd5877e429780531b09
Mike Cousins <mike@opentrons.com>
2018-10-05 15:42:41 -0400

    wip: Add getUnreachableRobots selector

--- app/src/discovery/__tests__/selectors.test.js
+++ app/src/discovery/__tests__/selectors.test.js
@@ -15,6 +15,55 @@ describe('discovery selectors', () => {
       state: {discovery: {scanning: false}},
       expected: false,
     },
+    {
+      name: 'getUnreachableRobots grabs robots with no ip',
+      selector: discovery.getUnreachableRobots,
+      state: {
+        discovery: {robotsByName: {foo: [{name: 'foo', ip: null}]}},
+      },
+      expected: [{name: 'foo'}],
+    },
+    {
+      name: 'getUnreachableRobots grabs robots with IP but no responses',
+      selector: discovery.getUnreachableRobots,
+      state: {
+        discovery: {
+          robotsByName: {
+            foo: [
+              {ip: '10.0.0.1', advertising: false, ok: false, serverOk: false},
+              {ip: '10.0.0.2', advertising: false, ok: false, serverOk: false},
+            ],
+          },
+        },
+      },
+      expected: [{name: 'foo'}],
+    },
+    {
+      name: 'getUnreachableRobots will not grab reachable robots',
+      selector: discovery.getUnreachableRobots,
+      state: {
+        discovery: {
+          robotsByName: {
+            foo: [
+              {ip: '10.0.0.1', advertising: true, ok: false, serverOk: false},
+              {ip: '10.0.0.2', advertising: false, ok: false, serverOk: false},
+            ],
+            bar: [
+              {ip: '10.0.0.1', advertising: false, ok: true, serverOk: false},
+              {ip: '10.0.0.2', advertising: false, ok: false, serverOk: false},
+            ],
+            baz: [
+              {ip: '10.0.0.1', advertising: false, ok: false, serverOk: true},
+              {ip: '10.0.0.2', advertising: false, ok: false, serverOk: false},
+            ],
+            qux: [
+              {ip: '10.0.0.2', advertising: true, ok: true, serverOk: true},
+            ],
+          },
+        },
+      },
+      expected: [],
+    },
   ]
 
   SPECS.forEach(spec => {
