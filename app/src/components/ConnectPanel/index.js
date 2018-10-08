9b47f2d92adf1ec47aa0c22ad1a502c760b211e4
Katie Adee <kaa328@nyu.edu>
2018-10-08 12:24:28 -0400

    feat(app): Display unreachable robots in list (#2434)
    Closes #2344

--- app/src/components/ConnectPanel/index.js
+++ app/src/components/ConnectPanel/index.js
@@ -6,15 +6,23 @@ import type {State, Dispatch} from '../../types'
 import type {Robot} from '../../robot'
 
 import {selectors as robotSelectors} from '../../robot'
-import {startDiscovery, getScanning} from '../../discovery'
+import {
+  startDiscovery,
+  getScanning,
+  getUnreachableRobots,
+} from '../../discovery'
 
 import {SidePanel} from '@opentrons/components'
 import RobotList from './RobotList'
 import RobotItem from './RobotItem'
 import ScanStatus from './ScanStatus'
+import UnreachableRobotItem from './UnreachableRobotItem'
+
+import type {UnreachableRobot} from '../../discovery'
 
 type StateProps = {
   robots: Array<Robot>,
+  unreachableRobots: Array<UnreachableRobot>,
   found: boolean,
   isScanning: boolean,
 }
@@ -25,16 +33,28 @@ type DispatchProps = {
 
 type Props = StateProps & DispatchProps
 
-export default connect(mapStateToProps, mapDispatchToProps)(ConnectPanel)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ConnectPanel)
 
 function ConnectPanel (props: Props) {
   return (
-    <SidePanel title='Robots'>
+    <SidePanel title="Robots">
       <ScanStatus {...props} />
       <RobotList>
-        {props.robots.map((robot) => (
-          <RobotItem key={robot.name} {...robot} />
+        {props.robots.map(robot => <RobotItem key={robot.name} {...robot} />)}
+        {props.unreachableRobots.map(robot => (
+          <UnreachableRobotItem key={robot.name} {...robot} />
         ))}
+        {/*
+          {props.connectableRobots.map((robot) => (
+            <RobotItem key={robot.name} {...robot} />
+          ))}
+          {props.reachableRobots.map((robot) => (
+            <RobotItem key={robot.name} {...robot} />
+          ))}
+        */}
       </RobotList>
     </SidePanel>
   )
@@ -42,9 +62,9 @@ function ConnectPanel (props: Props) {
 
 function mapStateToProps (state: State): StateProps {
   const robots = robotSelectors.getDiscovered(state)
-
   return {
     robots,
+    unreachableRobots: getUnreachableRobots(state),
     found: robots.length > 0,
     isScanning: getScanning(state),
   }
--- app/src/discovery/index.js
+++ app/src/discovery/index.js
@@ -1,8 +1,11 @@
 // @flow
 // robot discovery state
 import groupBy from 'lodash/groupBy'
+import map from 'lodash/map'
+import {createSelector} from 'reselect'
 import {getShellRobots} from '../shell'
 
+import type {OutputSelector} from 'reselect'
 import type {Service} from '@opentrons/discovery-client'
 import type {State, Action, ThunkAction} from '../types'
 
@@ -27,6 +30,10 @@ type UpdateListAction = {|
   type: 'discovery:UPDATE_LIST',
   payload: {|robots: Array<Service>|},
 |}
+
+export type UnreachableRobot = {
+  name: string,
+}
 
 export type DiscoveryAction = StartAction | FinishAction | UpdateListAction
 
@@ -62,6 +69,23 @@ export function getDiscoveredRobotsByName (state: State) {
 // export function getRobots (state: State) {
 //
 // }
+
+type UnreachableSelector = OutputSelector<State, void, Array<UnreachableRobot>>
+
+const serviceUnreachable = (service: Service) => !service.ip || !service.ok
+// TODO(mc, 2018-10-08): check advertising and serverOk flags
+// !service.ip || (!service.advertising && !service.ok && !service.serverOk)
+
+const robotUnreachable = (robot: Array<Service>) =>
+  robot.every(serviceUnreachable)
+
+const makeUnreachableRobot = (robot: Array<Service>, name: string) =>
+  robotUnreachable(robot) ? {name} : null
+
+export const getUnreachableRobots: UnreachableSelector = createSelector(
+  state => state.discovery.robotsByName,
+  robotsByName => map(robotsByName, makeUnreachableRobot).filter(Boolean)
+)
 
 // getShellRobots makes a sync RPC call, so use sparingly
 const initialState: DiscoveryState = {
