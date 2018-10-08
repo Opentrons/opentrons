ee65c54453ecf4190fba881392ee74c67f0aaa87
Mike Cousins <mike@opentrons.com>
2018-10-08 13:55:26 -0400

    fix(app): Allow portal to re-check for root element
    If the portal modal was used during the first render (e.g. for the analytics opt-in modal), the
    modal root would not be present in the DOM (since the root itself is created by the App component),
    causing the modal to not appear. This change allows the Portal component to a) cache the root
    component instead of getting it from the document on every render and b) retry getting the root
    after mounting, when we're sure (read: pretty sure) the root is also actually mounted in the DOM

--- app/src/components/portal/index.js
+++ app/src/components/portal/index.js
@@ -2,30 +2,38 @@
 import * as React from 'react'
 import ReactDom from 'react-dom'
 
-const PORTAL_ROOT_ID = 'main-modal-portal-root'
+type Props = {children: React.Node}
+
+type State = {hasRoot: boolean}
+
+const PORTAL_ROOT_ID = '__main-modal-portal-root'
+const getPortalRoot = () => global.document.getElementById(PORTAL_ROOT_ID)
 
 export function PortalRoot () {
   return <div id={PORTAL_ROOT_ID} />
 }
 
-export function getPortalElem () {
-  return document.getElementById(PORTAL_ROOT_ID)
-}
+// the children of Portal are rendered into the PortalRoot if it exists in DOM
+export class Portal extends React.Component<Props, State> {
+  $root: ?Element
 
-type Props = {children: React.Node}
-
-/** The children of Portal are rendered into the
-  * PortalRoot, if the PortalRoot exists in the DOM */
-export function Portal (props: Props): React.Node {
-  const modalRootElem = getPortalElem()
-
-  if (!modalRootElem) {
-    console.error('Modal root is not present, could not render modal')
-    return null
+  constructor (props: Props) {
+    super(props)
+    this.$root = getPortalRoot()
+    this.state = {hasRoot: !!this.$root}
   }
 
-  return ReactDom.createPortal(
-    props.children,
-    modalRootElem
-  )
+  // on first launch, $portalRoot isn't in DOM; double check once we're mounted
+  // TODO(mc, 2018-10-08): prerender UI instead
+  componentDidMount () {
+    if (!this.$root) {
+      this.$root = getPortalRoot()
+      this.setState({hasRoot: !!this.$root})
+    }
+  }
+
+  render () {
+    if (!this.$root) return null
+    return ReactDom.createPortal(this.props.children, this.$root)
+  }
 }
