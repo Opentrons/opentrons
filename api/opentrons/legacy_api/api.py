af8b820ad3c0ea69bd9f49075838addfa922d7a2
andySigler <andrewsigler1@gmail.com>
2018-10-11 16:12:04 -0400

    removes call to set_pick_up_current in _create_pipette_from_config and instead passes in as kwarg

--- api/opentrons/legacy_api/api.py
+++ api/opentrons/legacy_api/api.py
@@ -239,10 +239,10 @@ class InstrumentsWrapper(object):
             plunger_current=config.plunger_current,
             drop_tip_current=config.drop_tip_current,
             plunger_positions=config.plunger_positions.copy(),
+            pick_up_current=config.pick_up_current,
             pick_up_distance=config.pick_up_distance,
             fallback_tip_length=config.tip_length)  # TODO move to labware
 
-        p.set_pick_up_current(config.pick_up_current)
         return p
 
     def _retrieve_version_number(self, mount, expected_model_substring):
