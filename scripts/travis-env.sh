8a50de93f24fff0406182fb6396d751736ec1e0d
Mike Cousins <mike@opentrons.com>
2018-10-11 17:41:37 -0400

    fixup: correct branch regex

--- scripts/travis-env.sh
+++ scripts/travis-env.sh
@@ -7,7 +7,7 @@ app_os_target=$TRAVIS_OS_NAME
 if [[
   $TRAVIS_OS_NAME = linux &&
   ! $TRAVIS_TAG &&
-  ! $TRAVIS_BRANCH =~ "^(edge|release_.+)$"
+  ! $TRAVIS_BRANCH =~ ^(edge|release_.+)$
 ]]; then
   # TODO(mc, 2018-03-27): remove posix target in favor of individual
   #   build_$(OS) variables
