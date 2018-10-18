3e7456f43fc8d667042347183084443caeff2510
Ian London <ianlondondu@gmail.com>
2018-10-18 15:15:25 -0400

    feat(protocol-designer): use formik for liquid edit form (#2512)
    * use formik + yup for liquid edit form
    * also add instructional content to liquids page
    
    Closes #2460

--- protocol-designer/src/components/LiquidsPage/index.js
+++ protocol-designer/src/components/LiquidsPage/index.js
@@ -1,7 +1,65 @@
 // @flow
 import * as React from 'react'
-import LiquidEditForm from './LiquidEditForm'
+import {connect} from 'react-redux'
+import assert from 'assert'
 
-export default function LiquidsPage () {
-  return <LiquidEditForm />
+import LiquidEditForm from './LiquidEditForm'
+import LiquidsPageInfo from './LiquidsPageInfo'
+import * as labwareIngredActions from '../../labware-ingred/actions'
+import {selectors as labwareIngredSelectors} from '../../labware-ingred/reducers'
+
+import type {LiquidGroup} from '../../labware-ingred/types'
+import type {BaseState, ThunkDispatch} from '../../types'
+
+type Props = React.ElementProps<typeof LiquidEditForm>
+type WrapperProps = {showForm: boolean, formKey: string, formProps: Props}
+
+type SP = {
+  ...LiquidGroup,
+  _liquidGroupId: ?string,
+  showForm: boolean,
 }
+
+function LiquidEditFormWrapper (props: WrapperProps) {
+  const {showForm, formKey, formProps} = props
+  return showForm
+    ? <LiquidEditForm {...formProps} key={formKey} />
+    : <LiquidsPageInfo />
+}
+
+function mapStateToProps (state: BaseState): SP {
+  const selectedLiquidGroupState = labwareIngredSelectors.getSelectedLiquidGroupState(state)
+  const _liquidGroupId = (selectedLiquidGroupState && selectedLiquidGroupState.liquidGroupId)
+  const allIngredientGroupFields = labwareIngredSelectors.allIngredientGroupFields(state)
+  const selectedIngredFields = _liquidGroupId ? allIngredientGroupFields[_liquidGroupId] : {}
+  const showForm = Boolean(selectedLiquidGroupState.liquidGroupId || selectedLiquidGroupState.newLiquidGroup)
+  assert(!(_liquidGroupId && !selectedIngredFields), `Expected selected liquid group "${String(_liquidGroupId)}" to have fields in allIngredientGroupFields`)
+
+  return {
+    _liquidGroupId,
+    showForm,
+    name: selectedIngredFields.name,
+    description: selectedIngredFields.description,
+    serialize: selectedIngredFields.serialize,
+  }
+}
+
+function mergeProps (stateProps: SP, dispatchProps: {dispatch: ThunkDispatch<*>}): WrapperProps {
+  const {dispatch} = dispatchProps
+  const {showForm, _liquidGroupId, ...passThruFormProps} = stateProps
+  return {
+    showForm,
+    formKey: _liquidGroupId || '__new_form__',
+    formProps: {
+      ...passThruFormProps,
+      deleteLiquidGroup: () => window.alert('Deleting liquids is not yet implemented'), // TODO: Ian 2018-10-12 later ticket
+      cancelForm: () => dispatch(labwareIngredActions.deselectLiquidGroup()),
+      saveForm: (formData: LiquidGroup) => dispatch(labwareIngredActions.editLiquidGroup({
+        ...formData,
+        liquidGroupId: _liquidGroupId,
+      })),
+    },
+  }
+}
+
+export default connect(mapStateToProps, null, mergeProps)(LiquidEditFormWrapper)
