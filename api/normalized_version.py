ddd5454939e5dd5d41a3ae125e07a2c01ab9f2c9
Seth Foster <seth@opentrons.com>
2018-10-18 10:24:26 -0400

    fixup: test dependencies and packaging

--- api/normalized_version.py
+++ api/normalized_version.py
@@ -1,6 +1,15 @@
 import json
 import os
-from setuptools.extern import packaging
+# Pipenv requires setuptools >= 36.2.1. Since 36.2.1, setuptools changed
+# the way they vendor dependencies, like the packaging module that
+# provides the way to normalize version numbers for wheel file names. So
+# we try all the possible ways to find it.
+try:
+    # new way
+    from setuptools.extern import packaging
+except ImportError:
+    # old way
+    from pkg_resources.extern import packaging
 
 pkg_json_path = os.path.join('src', 'opentrons', 'package.json')
 old_ver = json.load(open(pkg_json_path))['version']
