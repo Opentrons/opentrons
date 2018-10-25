d85f6856b555638451b7a43dd7c31781b0fe85f7
Benjamin Morris <btmorr@users.noreply.github.com>
2018-10-25 16:29:15 -0400

    fix mypy error

--- api/src/opentrons/types.py
+++ api/src/opentrons/types.py
@@ -1,22 +1,23 @@
 import enum
 from typing import Union
 from collections import namedtuple
+from typing import Any
 
 _PointTuple = namedtuple('Point', ['x', 'y', 'z'])
 
 
 class Point(_PointTuple):
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Point):
             return False
         return self.x == other.x and self.y == other.y and self.z == other.z
 
-    def __add__(self, other: tuple) -> 'Point':
+    def __add__(self, other: Any) -> 'Point':
         if not isinstance(other, Point):
             return NotImplemented
         return Point(self.x + other.x, self.y + other.y, self.z + other.z)
 
-    def __sub__(self, other: tuple) -> 'Point':
+    def __sub__(self, other: Any) -> 'Point':
         if not isinstance(other, Point):
             return NotImplemented
         return Point(self.x - other.x, self.y - other.y, self.z - other.z)
