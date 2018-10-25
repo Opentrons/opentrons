c5a2434949997d8a821ce39d7c42badda445cd70
Benjamin Morris <btmorr@users.noreply.github.com>
2018-10-25 17:26:45 -0400

    refactor(api): Add a back-compat shim for ways of accessing wells (#2561)
    * refactor(api): Add a back-compat shim for ways of accessing wells in a labware

--- api/src/opentrons/types.py
+++ api/src/opentrons/types.py
@@ -1,8 +1,26 @@
 import enum
 from typing import Union
 from collections import namedtuple
+from typing import Any
 
-Point = namedtuple('Point', ['x', 'y', 'z'])
+_PointTuple = namedtuple('Point', ['x', 'y', 'z'])
+
+
+class Point(_PointTuple):
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Point):
+            return False
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __add__(self, other: Any) -> 'Point':
+        if not isinstance(other, Point):
+            return NotImplemented
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other: Any) -> 'Point':
+        if not isinstance(other, Point):
+            return NotImplemented
+        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
 
 
 class Mount(enum.Enum):
