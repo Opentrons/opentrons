7bcb126e2c766491c327623a898bf6d3f9319e58
Laura-Danielle <laura@opentrons.com>
2018-11-07 17:00:02 -0500

    Add test changes

--- api/tests/opentrons/protocol_api/test_labware_load.py
+++ api/tests/opentrons/protocol_api/test_labware_load.py
@@ -4,23 +4,23 @@ from opentrons import protocol_api as papi, types
 labware_name = 'generic_96_wellPlate_380_uL'
 
 
-def test_load_to_slot():
-    ctx = papi.ProtocolContext()
+def test_load_to_slot(loop):
+    ctx = papi.ProtocolContext(loop=loop)
     labware = ctx.load_labware_by_name(labware_name, '1')
     assert labware._offset == types.Point(0, 0, 0)
     other = ctx.load_labware_by_name(labware_name, 2)
     assert other._offset == types.Point(132.5, 0, 0)
 
 
-def test_loaded():
-    ctx = papi.ProtocolContext()
+def test_loaded(loop):
+    ctx = papi.ProtocolContext(loop=loop)
     labware = ctx.load_labware_by_name(labware_name, '1')
     assert ctx.loaded_labwares[1] == labware
 
 
-def test_from_backcompat():
-    ctx = papi.ProtocolContext()
-    papi.back_compat.reset(ctx)
+def test_from_backcompat(loop):
+    ctx = papi.ProtocolContext(loop=loop)
+    papi.back_compat.reset()
     lw = papi.back_compat.labware.load(labware_name, 3)
     assert lw == ctx.loaded_labwares[3]
 
