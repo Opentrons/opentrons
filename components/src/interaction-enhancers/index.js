17f562e8e87eb5fe54a9eb7dae16c3512de05403
Ian London <ianlondondu@gmail.com>
2018-11-14 11:09:00 -0500

    refactor(components): remove old clickOutside HoC (#2664)
    Closes #2590

--- components/src/interaction-enhancers/index.js
+++ components/src/interaction-enhancers/index.js
@@ -1,12 +1,9 @@
 // @flow
-import clickOutside, {ClickOutside} from './clickOutside'
+import ClickOutside from './ClickOutside'
 import HandleKeypress from './HandleKeypress'
 import IntervalWrapper from './IntervalWrapper'
 
-export type {ClickOutsideInterface} from './clickOutside'
-
 export {
-  clickOutside,
   ClickOutside,
   HandleKeypress,
   IntervalWrapper,
