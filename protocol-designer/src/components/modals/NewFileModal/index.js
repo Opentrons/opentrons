4ddcfa677dcec4ad581d795aac08f5681811bbae
Brian Cooper <brian@allegro.com>
2018-11-29 14:28:06 -0500

    color and size tweaks

--- protocol-designer/src/components/modals/NewFileModal/index.js
+++ protocol-designer/src/components/modals/NewFileModal/index.js
@@ -95,7 +95,7 @@ export default class NewFileModal extends React.Component<Props, State> {
         contentsClassName={styles.new_file_modal_contents}
         className={cx(modalStyles.modal, styles.new_file_modal)}>
         <form onSubmit={() => { canSubmit && this.handleSubmit() }}>
-          <h2>Create New Protocol</h2>
+          <h2 className={styles.new_file_modal_title}>Create New Protocol</h2>
 
           <FormGroup className={formStyles.stacked_row} label='Name:'>
             <InputField
