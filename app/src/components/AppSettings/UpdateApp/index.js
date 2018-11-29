ce4e9da7ab4b44667fa4b0c7e88c4d544927dbf0
Katie Adee <kaa328@nyu.edu>
2018-11-29 09:55:47 -0500

    refactor(app): Add app update message and release notes toggle (#2738)

---
+++ app/src/components/AppSettings/UpdateApp/index.js
@@ -0,0 +1,28 @@
+// @flow
+import * as React from 'react'
+
+import {SpinnerModal} from '@opentrons/components'
+import UpdateAppModal from './UpdateAppModal'
+import RestartAppModal from './RestartAppModal'
+
+import type {ShellUpdateState} from '../../../shell'
+
+type Props = {
+  update: ShellUpdateState,
+  availableVersion: ?string,
+  checkUpdate: () => mixed,
+  downloadUpdate: () => mixed,
+  applyUpdate: () => mixed,
+  closeModal: () => mixed,
+}
+export default function UpdateApp (props: Props) {
+  const {downloaded, downloading} = props.update
+
+  if (downloaded) {
+    return <RestartAppModal {...props} />
+  } else if (downloading) {
+    return <SpinnerModal message="Download in progress" alertOverlay />
+  } else {
+    return <UpdateAppModal {...props} />
+  }
+}
