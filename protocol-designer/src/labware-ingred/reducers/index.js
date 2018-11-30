8c5cce96f80c79047046bd0e848814e42d59af55
Brian Cooper <brian@allegro.com>
2018-11-30 11:04:06 -0500

    feat(protocol-designer): place one tiprack of each type on protocol creation
    By default, when a user creates a protocol, one tip rack for each given mount should be placed on
    the deck.
    
    Closes #1327

--- protocol-designer/src/labware-ingred/reducers/index.js
+++ protocol-designer/src/labware-ingred/reducers/index.js
@@ -32,7 +32,7 @@ import type {
 import * as actions from '../actions'
 import {getPDMetadata} from '../../file-types'
 import type {BaseState, Options} from '../../types'
-import type {LoadFileAction} from '../../load-file'
+import type {LoadFileAction, NewProtocolFields} from '../../load-file'
 import type {
   RemoveWellsContents,
   DeleteLiquidGroup,
@@ -194,6 +194,30 @@ export const containers = handleActions({
         },
       }
     }, {})
+  },
+  CREATE_NEW_PROTOCOL: (
+    state: ContainersState,
+    action: {payload: NewProtocolFields}
+  ): ContainersState => {
+    const initialTipracks = [action.payload.left, action.payload.right].reduce((acc, mount) => {
+      if (mount.tiprackModel) {
+        const id = `${uuid()}:${String(mount.tiprackModel)}`
+        return {
+          ...acc,
+          [id]: {
+            slot: nextEmptySlot(_loadedContainersBySlot({...state, ...acc})),
+            type: mount.tiprackModel,
+            disambiguationNumber: getNextDisambiguationNumber({...state, ...acc}, String(mount.tiprackModel)),
+            id,
+            name: null, // create with null name, so we force explicit naming.
+          },
+        }
+      }
+    }, {})
+    return {
+      ...state,
+      ...initialTipracks,
+    }
   },
 }, initialLabwareState)
 
