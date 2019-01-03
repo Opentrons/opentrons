00b6a96d12559773dc7954774b7621ff68c5e138
Mike Cousins <mike@opentrons.com>
2019-01-03 17:20:14 -0500

    build(babel): Upgrade babel to v7

---
+++ webpack-config/lib/env.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const parseEnvVariable = name => {
+  const value = process.env[name]
+
+  try {
+    return JSON.parse(value)
+  } catch (error) {
+    return value
+  }
+}
+
+module.exports = {
+  DEV_MODE: parseEnvVariable('NODE_ENV') !== 'production',
+  ENABLE_ANALYZER: !!parseEnvVariable('ANALYZER'),
+}
