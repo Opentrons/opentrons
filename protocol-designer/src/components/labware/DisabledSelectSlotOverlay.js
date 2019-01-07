0b4c2495099072e83efa5e23a5526f55fc275e91
Brian Cooper <brian@allegro.com>
2019-01-07 15:01:44 -0500

    tweak copy

--- protocol-designer/src/components/labware/DisabledSelectSlotOverlay.js
+++ protocol-designer/src/components/labware/DisabledSelectSlotOverlay.js
@@ -7,7 +7,7 @@ const DisabledSelectSlotOverlay = () => (
   <g className={cx(styles.slot_overlay, styles.disabled)}>
     <rect className={styles.overlay_panel} />
     <g className={styles.clickable_text}>
-      <text x="0" y="40%">Select a slot</text>
+      <text x="-7%" y="40%">Drag to New Slot</text>
     </g>
   </g>
 )
