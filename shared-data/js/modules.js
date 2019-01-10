8cf7f4acdb4ffa1f64f7b4614a02c675c7e6a81b
Kadee80 <kaa328@nyu.edu>
2019-01-10 09:52:43 -0500

    fixup: Use module type in shared-data

--- shared-data/js/modules.js
+++ shared-data/js/modules.js
@@ -4,7 +4,7 @@ import moduleSpecs from '../robot-data/moduleSpecs.json'
 export type ModuleType = 'magdeck' | 'tempdeck'
 
 // use a name like 'magdeck' to get displayName for app
-export function getModuleDisplayName (name: 'magdeck' | 'tempdeck'): string {
+export function getModuleDisplayName (name: ModuleType): string {
   const displayName = moduleSpecs[name].displayName
 
   return displayName
