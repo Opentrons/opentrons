96ac3cabe285238841a8b969ea997f6cedfb99f2
Brian Cooper <brian@allegro.com>
2019-01-30 14:22:08 -0500

    build up migration function

--- protocol-designer/src/load-file/migration.js
+++ protocol-designer/src/load-file/migration.js
@@ -5,6 +5,7 @@ import omit from 'lodash/omit'
 import {initialDeckSetupStepForm, type SavedStepFormState} from '../step-forms/reducers'
 import {INITIAL_DECK_SETUP_STEP_ID} from '../constants'
 import type {ProtocolFile, FileLabware, FilePipette} from '../file-types'
+import type {FormData} from '../form-types'
 
 function _addDeckSetupStepIfMissing (fileData: ProtocolFile): SavedStepFormState {
   // builds the initial deck setup step for older protocols that didn't have one.
@@ -33,15 +34,37 @@ function _addDeckSetupStepIfMissing (fileData: ProtocolFile): SavedStepFormState
   }
 }
 
+function _consolidateToFlexibleTransfer (formData: FormData): FormData {
+
+}
+
+function _distributeToFlexibleTransfer (formData: FormData): FormData {
+
+}
+
+function _migrateToFlexibleTransfer (savedStepForms: SavedStepFormState): SavedStepFormState {
+  return mapValues(savedStepForms, (formData) => {
+    const {stepType} = formData
+    if (stepType) return formData
+    if (stepType === 'consolidate') {
+      return _consolidateToFlexibleTransfer(formData)
+    } else if (stepType === 'distribute') {
+      return _distributeToFlexibleTransfer(formData)
+    }
+  })
+}
+
 function migrateSavedStepForms (fileData: ProtocolFile): SavedStepFormState {
-  const savedStepForms = _addDeckSetupStepIfMissing(fileData)
+  const savedStepFormsWithDeckSetup = _addDeckSetupStepIfMissing(fileData)
 
   // migrate old kebab-case keys to camelCase
-  return mapValues(savedStepForms, (stepForm) => ({
+  const savedStepFormsWithDeckSetupAndCamelCase = mapValues(savedStepFormsWithDeckSetup, (stepForm) => ({
     ...omit(stepForm, ['step-name', 'step-details']),
     stepName: stepForm.stepName || stepForm['step-name'],
     stepDetails: stepForm.stepDetails || stepForm['step-details'],
   }))
+
+  return _migrateToFlexibleTransfer(savedStepFormsWithDeckSetupAndCamelCase)
 }
 
 export default function migrateFile (file: any): ProtocolFile {
