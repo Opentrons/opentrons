51846d59b33ca6e1866d6cd04222ac8cfe91e5fd
Ian London <ianlondondu@gmail.com>
2019-03-11 17:02:33 -0400

    fix(protocol-designer): fix mix disabled fields (#3192)
    Closes #3049

--- protocol-designer/src/steplist/formLevel/getDisabledFields/getDisabledFieldsMixForm.js
+++ protocol-designer/src/steplist/formLevel/getDisabledFields/getDisabledFieldsMixForm.js
@@ -3,13 +3,13 @@ import type {FormData} from '../../../form-types'
 
 // NOTE: expects that '_checkbox' fields are implemented so that
 // when checkbox is disabled, its dependent fields are hidden
-export default function getDisabledFieldsMoveLiquidForm (
+export default function getDisabledFieldsMixForm (
   rawForm: FormData // TODO IMMEDIATELY use raw form type instead of FormData
 ): Set<string> {
   const disabled = new Set()
 
   if (!rawForm.pipette || !rawForm['labware']) {
-    disabled.add('touchTip') // TODO: BC 2019-1-29 this is actually touchTip_checkbox
+    disabled.add('mix_touchTip_checkbox')
     disabled.add('mix_mmFromBottom')
     disabled.add('wells')
   }
