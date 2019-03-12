f1968baf4d3b60a5cf0fdecc5aa02cf7b66672fb
Mike Cousins <mike@cousins.io>
2019-03-12 16:53:00 -0400

    build(babel): Add optional chaining proposal to babel config (#3201)

--- babel.config.js
+++ babel.config.js
@@ -18,6 +18,7 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
   ],
   presets: ['@babel/preset-flow', '@babel/preset-react', PRESET_ENV_NO_MODULES],
   overrides: [
