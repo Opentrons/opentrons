07e69d0c6e5cb6a32a45aaf12ad24efe9271ca45
IanLondon <ianlondondu@gmail.com>
2019-03-25 15:55:23 -0400

    feat(protocol-designer): update pause form design
    Closes #3142

--- protocol-designer/src/components/StepEditForm/forms/Pause.js
+++ protocol-designer/src/components/StepEditForm/forms/Pause.js
@@ -1,40 +1,63 @@
 // @flow
 import * as React from 'react'
 import {FormGroup} from '@opentrons/components'
+import i18n from '../../../localization'
 
-import formStyles from '../../forms/forms.css'
+import {ConditionalOnField, TextField, RadioGroupField} from '../fields'
+import StepField from '../fields/FieldConnector'
+import styles from '../StepEditForm.css'
 
-import {TextField, RadioGroupField} from '../fields'
 import type {FocusHandlers} from '../types'
 
 type PauseFormProps = {focusHandlers: FocusHandlers}
 function PauseForm (props: PauseFormProps): React.Element<'div'> {
   const {focusHandlers} = props
   return (
-    <div className={formStyles.row_wrapper}>
-      <div className={formStyles.column_1_2}>
-        <RadioGroupField
-          name="pauseForAmountOfTime"
-          options={[{name: 'Pause for an amount of time', value: 'true'}]}
-          {...focusHandlers} />
-        <FormGroup className={formStyles.stacked_row}>
-          <TextField units="hr" name="pauseHour" {...focusHandlers} />
-        </FormGroup>
-        <FormGroup className={formStyles.stacked_row}>
-          <TextField units="m" name="pauseMinute" {...focusHandlers} />
-        </FormGroup>
-        <FormGroup className={formStyles.stacked_row}>
-          <TextField units="s" name="pauseSecond" {...focusHandlers} />
-        </FormGroup>
+    <div className={styles.form_wrapper}>
+      <div className={styles.section_header}>
+        <span className={styles.section_header_text}>
+          {i18n.t('application.stepType.pause')}
+        </span>
       </div>
-      <div className={formStyles.column_1_2}>
-        <RadioGroupField
-          name="pauseForAmountOfTime"
-          options={[{name: 'Pause until told to resume', value: 'false'}]}
-          {...focusHandlers} />
-        <FormGroup label='Message to display'>
-          <TextField name="pauseMessage" {...focusHandlers} />
-        </FormGroup>
+
+      <div className={styles.section_wrapper}>
+        <div className={styles.section_column}>
+          <div className={styles.checkbox_row}>
+            <RadioGroupField
+              name="pauseForAmountOfTime"
+              options={[{name: 'Pause until told to resume', value: 'false'}]}
+              {...focusHandlers} />
+          </div>
+          <div className={styles.checkbox_row}>
+            <RadioGroupField
+              name="pauseForAmountOfTime"
+              options={[{name: 'Delay for an amount of time', value: 'true'}]}
+              {...focusHandlers} />
+          </div>
+          <ConditionalOnField name={'pauseForAmountOfTime'} condition={(val) => val === 'true'}>
+            <div className={styles.form_row}>
+              <TextField {...focusHandlers} className={styles.small_field}
+                units="hr" name="pauseHour" />
+              <TextField {...focusHandlers} className={styles.small_field}
+                units="m" name="pauseMinute" />
+              <TextField {...focusHandlers} className={styles.small_field}
+                units="s" name="pauseSecond" />
+            </div>
+          </ConditionalOnField>
+        </div>
+        <div className={styles.section_column}>
+          <div className={styles.form_row}>
+            {/* TODO: Ian 2019-03-25 consider making this a component eg `TextAreaField.js` if used anywhere else */}
+            <StepField {...focusHandlers} name='pauseMessage' render={({value, updateValue}) => (
+              <FormGroup label='Message to display' className={styles.full_width_field}>
+                <textarea
+                  className={styles.textarea_field}
+                  value={value}
+                  onChange={(e: SyntheticInputEvent<*>) => updateValue(e.currentTarget.value)} />
+              </FormGroup>)
+            }/>
+          </div>
+        </div>
       </div>
     </div>
   )
