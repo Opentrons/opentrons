9d63d6686c870cde068383f0600b12e992621305
IanLondon <ianlondondu@gmail.com>
2019-03-26 09:59:35 -0400

    fixup typo

--- protocol-designer/src/components/StepEditForm/forms/MoveLiquid/SourceDestFields.js
+++ protocol-designer/src/components/StepEditForm/forms/MoveLiquid/SourceDestFields.js
@@ -88,7 +88,7 @@ function SourceDestFields (props: Props) {
 
         <CheckboxRowField
           name={addFieldNamePrefix('touchTip_checkbox')}
-          tooltipComponent={i18n.t(`tooltip.step_fields.${addFieldNamePrefix('touchTip_checkbox')}`)}
+          tooltipComponent={i18n.t(`tooltip.step_fields.defaults.${addFieldNamePrefix('touchTip_checkbox')}`)}
           label={i18n.t('form.step_edit_form.field.touchTip.label')}
           className={styles.small_field}
         >
