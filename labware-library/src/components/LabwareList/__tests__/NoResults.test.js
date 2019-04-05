f6789cb881fc42e77aee970a870a86629660d536
Mike Cousins <mike@cousins.io>
2019-04-05 13:03:33 -0400

    refactor(labware-library): Add "No Results" message if filters hide all labware (#3306)
    Closes #3080

---
+++ labware-library/src/components/LabwareList/__tests__/NoResults.test.js
@@ -0,0 +1,12 @@
+// @flow
+// NoResults tests
+import * as React from 'react'
+import {shallow} from 'enzyme'
+
+import NoResults from '../NoResults'
+
+describe('NoResults', () => {
+  test('component renders', () => {
+    expect(shallow(<NoResults />)).toMatchSnapshot()
+  })
+})
