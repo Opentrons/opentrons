beccdfe1171aef409e364fa2603214772c475507
Brian Cooper <brian@allegro.com>
2019-04-25 12:15:40 -0400

    prettify

--- shared-data/js/scripts/generateDeckLayersFromSVG.js
+++ shared-data/js/scripts/generateDeckLayersFromSVG.js
@@ -2,11 +2,20 @@ const fs = require('fs')
 const path = require('path')
 const JSDOM = require('jsdom').JSDOM
 
+// This script expects two args: the path of the source svg, and the desired
+// destination directory of the output json.
+// NOTE: the expected source SVG is an output SVG from Adobe Illustrator
+// when using `Save as` or `Save as a Copy` and not `Export`
+// Furthermore, the .ai file should contain no groups and all layers
+// will be directly translated to layer groups in the JSON output
+
 const svgPath = process.argv[2]
 const buildDir = process.argv[3]
 
 if (!buildDir) {
-  throw new Error('generateDeckLayersFromSVG requires a build directory given as an argument. eg `node js/scripts/build.js path/to/svg/ path/to/build/`')
+  throw new Error(
+    'generateDeckLayersFromSVG requires a build directory given as an argument. eg `node js/scripts/build.js path/to/svg/ path/to/build/`'
+  )
 }
 
 fs.readFile(svgPath, (err, data) => {
@@ -25,14 +34,11 @@ fs.readFile(svgPath, (err, data) => {
       const paths = features.map(f => ({
         footprint: f.getAttribute('d').replace(/\s|  {2}|\r\n|\n|\r/gm, ''), // clean up whitespaces and newlines
       }))
-      layers = {...layers, [groupId]: paths}
+      layers = { ...layers, [groupId]: paths }
     }
   })
 
   const jsonOutput = JSON.stringify(layers)
 
-  fs.writeFileSync(
-    path.join(buildDir, 'deckLayers.json'),
-    jsonOutput
-  )
+  fs.writeFileSync(path.join(buildDir, 'deckLayers.json'), jsonOutput)
 })
