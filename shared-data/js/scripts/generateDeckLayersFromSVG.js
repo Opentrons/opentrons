020e69e824b9776ef94bd26b1eddb507ac2ffa23
Brian Cooper <brian@allegro.com>
2019-04-29 14:37:15 -0400

    camelCase keys and mike script tweaks

--- shared-data/js/scripts/generateDeckLayersFromSVG.js
+++ shared-data/js/scripts/generateDeckLayersFromSVG.js
@@ -1,44 +1,46 @@
+const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
+const camelCase = require('lodash/camelCase')
 const JSDOM = require('jsdom').JSDOM
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
 
-// This script expects two args: the path of the source svg, and the desired
-// destination directory of the output json.
 // NOTE: the expected source SVG is an output SVG from Adobe Illustrator
 // when using `Save as` or `Save as a Copy` and not `Export`
 // Furthermore, the .ai file should contain no groups and all layers
 // will be directly translated to layer groups in the JSON output
 
+const USAGE =
+  'Expected Params Not Present:   (e.g. node ./scripts/generateDeckLayersFromSVG ./path/to/source/svg ./path/to/output/dir)'
+assert(process.argv[2] && process.argv[3], USAGE)
+
 const svgPath = process.argv[2]
 const buildDir = process.argv[3]
 
-if (!buildDir) {
-  throw new Error(
-    'generateDeckLayersFromSVG requires a build directory given as an argument. eg `node js/scripts/build.js path/to/svg/ path/to/build/`'
-  )
-}
+readFile(svgPath, 'utf8')
+  .then(data => {
+    const doc = new JSDOM(data, 'text/xml')
+    const groups = Array.from(doc.window.document.querySelectorAll('g'))
 
-fs.readFile(svgPath, (err, data) => {
-  if (err || !data) {
-    console.error('SVG file could not be read')
-  }
-  const doc = new JSDOM(data.toString(), 'text/xml')
+    let layers = {}
+    groups.forEach(g => {
+      const groupId = g.getAttribute('id')
+      if (groupId && g.hasChildNodes()) {
+        const features = Array.from(g.querySelectorAll('path'))
+        const paths = features.map(f => ({
+          footprint: f.getAttribute('d').replace(/\s|  {2}|\r\n|\n|\r/gm, ''), // clean up whitespaces and newlines
+        }))
+        layers = { ...layers, [camelCase(groupId)]: paths }
+      }
+    })
 
-  const groups = Array.from(doc.window.document.querySelectorAll('g'))
-
-  let layers = {}
-  groups.forEach(g => {
-    const groupId = g.getAttribute('id')
-    if (groupId && g.hasChildNodes()) {
-      const features = Array.from(g.querySelectorAll('path'))
-      const paths = features.map(f => ({
-        footprint: f.getAttribute('d').replace(/\s|  {2}|\r\n|\n|\r/gm, ''), // clean up whitespaces and newlines
-      }))
-      layers = { ...layers, [groupId]: paths }
-    }
+    const jsonOutput = JSON.stringify(layers)
+    return writeFile(path.join(buildDir, 'deckLayers.json'), jsonOutput)
   })
-
-  const jsonOutput = JSON.stringify(layers)
-
-  fs.writeFileSync(path.join(buildDir, 'deckLayers.json'), jsonOutput)
-})
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
