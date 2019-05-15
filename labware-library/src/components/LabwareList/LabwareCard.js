8441004ce8952333c55a5db5a3f8b702039194c8
Mike Cousins <mike@opentrons.com>
2019-05-15 12:34:03 -0400

    refactor(labware): Remove unnecessary information from labware cards (#3460)
    Closes #3075

--- labware-library/src/components/LabwareList/LabwareCard.js
+++ labware-library/src/components/LabwareList/LabwareCard.js
@@ -4,7 +4,6 @@
 //   many of which will be common to LabwareCard and LabwarePage
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import round from 'lodash/round'
 
 import { getDisplayVolume } from '@opentrons/shared-data'
 import { getPublicPath } from '../../public-path'
@@ -15,34 +14,15 @@ import LoadName from './LoadName'
 import Tags from './Tags'
 import styles from './styles.css'
 
-import {
-  LabeledValueTable,
-  LabelText,
-  Value,
-  TABLE_ROW,
-  LABEL_TOP,
-} from '../ui'
+import { LabelText, Value, LABEL_TOP, LABEL_LEFT } from '../ui'
 
 import {
   CATEGORY_LABELS_BY_CATEGORY,
-  NUM_WELLS_SHORT_BY_CATEGORY,
-  LABWARE_DIMS_BY_CATEGORY,
-  WELL_DIMS_BY_CATEGORY,
-  MM,
-  SHORT_X_DIM,
-  SHORT_Y_DIM,
-  SHORT_Z_DIM,
-  X_DIM,
-  Y_DIM,
-  DIAMETER,
-  DEPTH,
+  NUM_WELLS_BY_CATEGORY,
   MAX_VOLUME,
 } from '../../localization'
 
 import type { LabwareDefinition } from '../../types'
-
-// safe toFixed
-const toFixed = (n: number): string => round(n, 2).toFixed(2)
 
 export type LabwareCardProps = { definition: LabwareDefinition }
 
@@ -53,13 +33,14 @@ export default function LabwareCard(props: LabwareCardProps) {
     <li className={styles.card}>
       <TopBar {...props} />
       <Title {...props} />
-      <div className={styles.gallery_container}>
-        <Gallery {...props} />
-      </div>
-      <div className={styles.stats}>
-        <Dimensions {...props} />
-        <Wells {...props} />
-        <WellProperties {...props} />
+      <div className={styles.card_contents}>
+        <div className={styles.gallery_container}>
+          <Gallery {...props} />
+        </div>
+        <div className={styles.stats}>
+          <WellCount {...props} />
+          <WellProperties {...props} />
+        </div>
       </div>
       <div className={styles.tags_container}>
         <Tags {...props} />
@@ -95,42 +76,15 @@ function Title(props: LabwareCardProps) {
   )
 }
 
-function Dimensions(props: LabwareCardProps) {
-  const { definition } = props
-  const { displayCategory } = definition.metadata
-  const { xDimension, yDimension, zDimension } = definition.dimensions
-  const dimsLabel =
-    LABWARE_DIMS_BY_CATEGORY[displayCategory] || LABWARE_DIMS_BY_CATEGORY.other
-
-  const dimensions = [
-    { label: SHORT_X_DIM, value: toFixed(xDimension) },
-    { label: SHORT_Y_DIM, value: toFixed(yDimension) },
-    { label: SHORT_Z_DIM, value: toFixed(zDimension) },
-  ]
-
-  return (
-    <LabeledValueTable
-      className={styles.dimensions}
-      direction={TABLE_ROW}
-      label={
-        <>
-          {dimsLabel} <span className={styles.units}>({MM})</span>
-        </>
-      }
-      values={dimensions}
-    />
-  )
-}
-
-function Wells(props: LabwareCardProps) {
+function WellCount(props: LabwareCardProps) {
   const { wells, metadata } = props.definition
   const { displayCategory } = metadata
+  const numWellsLabel =
+    NUM_WELLS_BY_CATEGORY[displayCategory] || NUM_WELLS_BY_CATEGORY.other
 
   return (
-    <div className={styles.wells}>
-      <LabelText position={LABEL_TOP}>
-        {`${NUM_WELLS_SHORT_BY_CATEGORY[displayCategory]}`}
-      </LabelText>
+    <div className={styles.well_count}>
+      <LabelText position={LABEL_LEFT}>{numWellsLabel}</LabelText>
       <Value>{Object.keys(wells).length}</Value>
     </div>
   )
@@ -138,45 +92,20 @@ function Wells(props: LabwareCardProps) {
 
 function WellProperties(props: LabwareCardProps) {
   const { definition } = props
-  const { displayCategory, displayVolumeUnits } = definition.metadata
+  const { displayVolumeUnits } = definition.metadata
   const wellProps = getUniqueWellProperties(definition)
-  const wellDimsLabel =
-    WELL_DIMS_BY_CATEGORY[displayCategory] || WELL_DIMS_BY_CATEGORY.other
 
   return (
-    <div className={styles.wells}>
+    <div className={styles.well_properties}>
       {wellProps.map((w, i) => {
         const vol = getDisplayVolume(w.totalLiquidVolume, displayVolumeUnits, 2)
-        const dims = [
-          { label: DEPTH, value: toFixed(w.depth) },
-          w.diameter != null
-            ? { label: DIAMETER, value: toFixed(w.diameter) }
-            : null,
-          w.xDimension != null
-            ? { label: X_DIM, value: toFixed(w.xDimension) }
-            : null,
-          w.yDimension != null
-            ? { label: Y_DIM, value: toFixed(w.yDimension) }
-            : null,
-        ].filter(Boolean)
 
         return (
-          <div key={i} className={styles.well_group}>
-            <LabeledValueTable
-              className={styles.well_dimensions}
-              label={
-                <>
-                  {wellDimsLabel} <span className={styles.units}>({MM})</span>
-                </>
-              }
-              values={dims}
-            />
-            <div className={styles.well_volume}>
-              <LabelText position={LABEL_TOP}>{MAX_VOLUME}</LabelText>
-              <Value>
-                {vol} {displayVolumeUnits}
-              </Value>
-            </div>
+          <div key={i} className={styles.well_group_properties}>
+            <LabelText position={LABEL_TOP}>{MAX_VOLUME}</LabelText>
+            <Value>
+              {vol} {displayVolumeUnits}
+            </Value>
           </div>
         )
       })}
