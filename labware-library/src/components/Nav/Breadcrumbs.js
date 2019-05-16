9185344e0d2864dc77dd2d4a2f98925df82d9fc3
Kadee80 <kaa328@nyu.edu>
2019-05-16 07:10:59 -0400

    refactor: Update individual labware page breadcrumbs

--- labware-library/src/components/Nav/Breadcrumbs.js
+++ labware-library/src/components/Nav/Breadcrumbs.js
@@ -8,7 +8,7 @@ import styles from './styles.css'
 import type { LabwareDefinition } from '../../types'
 
 // TODO(mc, 2019-04-07): i18n
-const EN_LABWARE_LIBRARY = 'Labware Library'
+const EN_LABWARE_LIBRARY = 'Back to Labware Library'
 
 export type BreadcrumbsProps = {
   definition: LabwareDefinition | null,
@@ -21,15 +21,9 @@ export default function Breadcrumbs(props: BreadcrumbsProps) {
   return (
     <div className={styles.breadcrumbs}>
       <div className={styles.breadcrumbs_contents}>
+        <span className={styles.breadcrumbs_separator}>{' < '}</span>
         <Link to={getPublicPath()} className={styles.breadcrumbs_link}>
           {EN_LABWARE_LIBRARY}
-        </Link>
-        <span className={styles.breadcrumbs_separator}>{' > '}</span>
-        <Link
-          to={`${getPublicPath()}${definition.parameters.loadName}`}
-          className={styles.breadcrumbs_link}
-        >
-          {definition.metadata.displayName}
         </Link>
       </div>
     </div>
