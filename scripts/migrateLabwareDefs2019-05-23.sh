901f7f95a299de4a9373d4b0d6c40409910a067d
IanLondon <ianlondondu@gmail.com>
2019-05-24 15:45:26 -0400

    escape vars better

--- scripts/migrateLabwareDefs2019-05-23.sh
+++ scripts/migrateLabwareDefs2019-05-23.sh
@@ -1,14 +1,11 @@
 # !/bin/bash
 # Invoke this with like `./migrateLabwareDefs2019-05-23.sh path/to/shared-data`
 
-for filepath in $(ls $1/definitions2/*.json)
+for filepath in $(ls ${1}/definitions2/*.json)
 do
-  echo $filepath
-  filebasename=$(basename $filepath .json)
-  echo $filebasename
+  filebasename=$(basename "${filepath}" .json)
   # assumes only version is 1
-  dirpath=$1/labware/definitions/2/$filebasename/1
-  echo $dirpath
-  mkdir -p $dirpath
-  cp $filepath $dirpath/$filebasename.json
+  dirpath="${1}/labware/definitions/2/${filebasename}/1"
+  mkdir -p "${dirpath}"
+  cp "${filepath}" "${dirpath}/${filebasename}.json"
 done
