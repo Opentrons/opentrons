316269b8a94f9798f9d74c787ffd44b20ce080af
Kadee80 <kaa328@nyu.edu>
2019-05-29 14:37:57 -0400

    wip: Add measurement diagram files

--- labware-library/src/components/MeasurementGuide/index.js
+++ labware-library/src/components/MeasurementGuide/index.js
@@ -4,8 +4,7 @@ import cx from 'classnames'
 import styles from './styles.css'
 
 export type GuideProps = {|
-  category?: string,
-  guideType?: string,
+  ...DiagramProps,
   guideVisible?: boolean,
 |}
 
@@ -27,34 +26,55 @@ export function MeasurementGuide(props: GuideProps) {
   )
 }
 
-const DIAGRAMS: { [guideType]: { [category]: Array<?string> } } = {
-  footprint: {
-    wellPlate: [
-      require('./images/footprint@3x.png'),
-      require('./images/height-plate-and-reservoir@3x.png'),
-    ],
-    tipRack: [
-      require('./images/footprint@3x.png'),
-      require('./images/height-tip-rack@3x.png'),
-    ],
-    tubeRack: [
-      require('./images/footprint@3x.png'),
-      require('./images/height-tube-rack@3x.png'),
-    ],
-    trough: [
-      require('./images/footprint@3x.png'),
-      require('./images/height-plate-and-reservoir@3x.png'),
-    ],
+const FOOTPRINT_DIAGRAMS: { [category]: Array<?string> } = {
+  wellPlate: [
+    require('./images/dimensions/footprint@3x.png'),
+    require('./images/dimensions/height-plate-and-reservoir@3x.png'),
+  ],
+  tipRack: [
+    require('./images/dimensions/footprint@3x.png'),
+    require('./images/dimensions/height-tip-rack@3x.png'),
+  ],
+  tubeRack: [
+    require('./images/dimensions/footprint@3x.png'),
+    require('./images/dimensions/height-tube-rack@3x.png'),
+  ],
+  reservoir: [
+    require('./images/dimensions/footprint@3x.png'),
+    require('./images/dimensions/height-plate-and-reservoir@3x.png'),
+  ],
+}
+
+const SPACING_DIAGRAMS: { [category]: { [shape]: Array<?string> } } = {
+  wellPlate: {
+    circular: [require('./images/spacing/spacing-well-circular@3x.png')],
+    rectangular: [require('./images/spacing/spacing-well-rectangular@3x.png')],
+  },
+  tiprack: {
+    circular: [require('./images/spacing/spacing-well-circular@3x.png')],
+    rectangular: [require('./images/spacing/spacing-well-rectangular@3x.png')],
+  },
+  tubeRack: {
+    circular: [require('./images/spacing/spacing-well-circular@3x.png')],
+    rectangular: [require('./images/spacing/spacing-well-rectangular@3x.png')],
+  },
+  reservoir: {
+    rectangular: [require('./images/spacing/spacing-reservoir@3x.png')],
   },
 }
 
-type DiagramProps = {
-  category: string,
-  guideType: string,
+export type DiagramProps = {
+  guideType?: string,
+  category?: string,
+  shape?: string,
+  wellBottomShape?: string,
 }
 
 export function getDiagramSrc(props: DiagramProps): string {
-  const { category, guideType } = props
+  const { guideType, category, shape, wellBottomShape } = props
 
-  return DIAGRAMS[guideType][category]
+  if (guideType === 'footprint') return FOOTPRINT_DIAGRAMS[category]
+  else if (guideType === 'spacing') return SPACING_DIAGRAMS[category][shape]
+  else if (guideType === 'measurements')
+    return OFFSET[category][shape][wellBottomShape]
 }
