686abcf5da9ac450817cf1eb06f53523f5eaeb57
IanLondon <ianlondondu@gmail.com>
2019-05-30 11:09:32 -0400

    add "groups" to metadata blacklist; use minified separators

--- api/src/opentrons/protocol_api/labware.py
+++ api/src/opentrons/protocol_api/labware.py
@@ -719,9 +719,10 @@ class ThermocyclerGeometry(ModuleGeometry):
 
 def _hash_labware_def(labware: Dict[str, Any]) -> str:
     # remove keys that do not affect run
-    blacklist = ['metadata', 'brand']
+    blacklist = ['metadata', 'brand', 'groups']
     def_no_metadata = {k: labware[k] for k in labware if k not in blacklist}
-    sorted_def_str = json.dumps(def_no_metadata, sort_keys=True)
+    sorted_def_str = json.dumps(
+        def_no_metadata, sort_keys=True, separators=(',', ':'))
     return sha256(sorted_def_str.encode('utf-8')).hexdigest()
 
 
