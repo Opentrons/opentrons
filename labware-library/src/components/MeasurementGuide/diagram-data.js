cf59b6bd8477504640f426d9527cd98c773b9d5c
Kadee80 <kaa328@nyu.edu>
2019-05-31 14:21:20 -0400

    refactor: Update srs const types

--- labware-library/src/components/MeasurementGuide/diagram-data.js
+++ labware-library/src/components/MeasurementGuide/diagram-data.js
@@ -11,7 +11,7 @@ export type DiagramProps = {|
   isMultiRow?: boolean,
 |}
 
-const FOOTPRINT_DIAGRAMS: { [category: string]: Array<?string> } = {
+const FOOTPRINT_DIAGRAMS: { [category: string]: Array<string> } = {
   wellPlate: [
     require('./images/dimensions/footprint@3x.png'),
     require('./images/dimensions/height-plate-and-reservoir@3x.png'),
@@ -34,7 +34,7 @@ const FOOTPRINT_DIAGRAMS: { [category: string]: Array<?string> } = {
   ],
 }
 
-const ALUM_BLOCK_FOOTPRINTS: { [category: string]: Array<?string> } = {
+const ALUM_BLOCK_FOOTPRINTS: { [category: string]: Array<string> } = {
   tubeRack: [
     require('./images/dimensions/footprint@3x.png'),
     require('./images/dimensions/height-alum-block-tubes@3x.png'),
@@ -46,7 +46,7 @@ const ALUM_BLOCK_FOOTPRINTS: { [category: string]: Array<?string> } = {
 }
 
 const RESERVOIR_SPACING_DIAGRAMS: {
-  [rows: string]: Array<?string>,
+  [rows: string]: Array<string>,
 } = {
   singleRow: [
     require('./images/offset/offset-reservoir@3x.png'),
@@ -59,7 +59,7 @@ const RESERVOIR_SPACING_DIAGRAMS: {
 }
 
 const SPACING_DIAGRAMS: {
-  [shape: string]: Array<?string>,
+  [shape: string]: Array<string>,
 } = {
   circular: [
     require('./images/offset/offset-well-circular@3x.png'),
@@ -71,13 +71,13 @@ const SPACING_DIAGRAMS: {
   ],
 }
 
-const TIPRACK_MEASUREMENT_DIAGRAMS: Array<?string> = [
+const TIPRACK_MEASUREMENT_DIAGRAMS: Array<string> = [
   require('./images/depth/length-tip-rack@3x.png'),
   require('./images/shape/shape-circular@3x.png'),
 ]
 
 const PLATE_MEASUREMENT_DIAGRAMS: {
-  [wellBottomShape: string]: { [shape: string]: Array<?string> },
+  [wellBottomShape: string]: { [shape: string]: Array<string> },
 } = {
   flat: {
     circular: [
@@ -111,7 +111,7 @@ const PLATE_MEASUREMENT_DIAGRAMS: {
   },
 }
 const MEASUREMENT_DIAGRAMS: {
-  [wellBottomShape: string]: { [shape: string]: Array<?string> },
+  [wellBottomShape: string]: { [shape: string]: Array<string> },
 } = {
   flat: {
     circular: [
@@ -148,11 +148,11 @@ const MEASUREMENT_DIAGRAMS: {
 export function getFootprintDiagram(props: DiagramProps) {
   const { category, insertCategory, irregular } = props
   if (category === 'aluminumBlock') {
-    return insertCategory && ALUM_BLOCK_FOOTPRINTS[insertCategory]
+    return insertCategory ? ALUM_BLOCK_FOOTPRINTS[insertCategory] : []
   } else if (category === 'tubeRack' && irregular) {
     return FOOTPRINT_DIAGRAMS['irregular']
   }
-  return category && FOOTPRINT_DIAGRAMS[category]
+  return category ? FOOTPRINT_DIAGRAMS[category] : []
 }
 
 export function getSpacingDiagram(props: DiagramProps) {
@@ -163,20 +163,18 @@ export function getSpacingDiagram(props: DiagramProps) {
       : RESERVOIR_SPACING_DIAGRAMS['singleRow']
   }
 
-  return shape && SPACING_DIAGRAMS[shape]
+  return shape ? SPACING_DIAGRAMS[shape] : []
 }
 
 export function getMeasurementDiagram(props: DiagramProps) {
   const { category, wellBottomShape, shape } = props
   if (category === 'tipRack') return TIPRACK_MEASUREMENT_DIAGRAMS
   else if (category === 'wellPlate') {
-    return (
-      wellBottomShape &&
-      shape &&
-      PLATE_MEASUREMENT_DIAGRAMS[wellBottomShape][shape]
-    )
+    return wellBottomShape && shape
+      ? PLATE_MEASUREMENT_DIAGRAMS[wellBottomShape][shape]
+      : []
   }
-  return (
-    wellBottomShape && shape && MEASUREMENT_DIAGRAMS[wellBottomShape][shape]
-  )
+  return wellBottomShape && shape
+    ? MEASUREMENT_DIAGRAMS[wellBottomShape][shape]
+    : []
 }
