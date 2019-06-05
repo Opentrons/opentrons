3bd5dc12fb769670d99346a1732dced93031ddf1
Brian Cooper <brian@allegro.com>
2019-06-05 17:44:19 -0400

    start to incorporate rws

--- app/src/components/DeckMap/index.js
+++ app/src/components/DeckMap/index.js
@@ -1,14 +1,45 @@
 // @flow
-import React from 'react'
+import React, { useMemo } from 'react'
 
-import { Deck } from '@opentrons/components'
-import ConnectedSlotItem from './ConnectedSlotItem'
+import { RobotWorkSpace, LabwareOnDeck } from '@opentrons/components'
+import { getDeckDefinitions } from '@opentrons/components/src/deck/getDeckDefinitions'
+// import ConnectedSlotItem from './ConnectedSlotItem'
 import LabwareItem from './LabwareItem'
 
-import styles from './styles.css'
+// import styles from './styles.css'
+
+const deckSetupLayerBlacklist = [
+  'calibrationMarkings',
+  'fixedBase',
+  'doorStops',
+  'metalFrame',
+  'removalHandle',
+  'screwHoles',
+]
 
 export default function DeckMap() {
-  return <Deck LabwareComponent={ConnectedSlotItem} className={styles.deck} />
+  const deckDef = useMemo(() => getDeckDefinitions()['ot2_standard'], [])
+  //  <Deck LabwareComponent={ConnectedSlotItem} className={styles.deck} />
+  return (
+    <RobotWorkSpace
+      deckLayerBlacklist={deckSetupLayerBlacklist}
+      deckDef={deckDef}
+      viewBox={`-46 -70 ${488} ${514}`} // TODO: put these in variables
+    >
+      {({ slots, getRobotCoordsFromDOMCoords }) => (
+        <>
+          {map(containedLabware, labwareOnDeck => (
+            <LabwareOnDeck
+              key={labwareOnDeck.id}
+              x={slots[labwareOnDeck.slot].position[0]}
+              y={slots[labwareOnDeck.slot].position[1]}
+              labwareOnDeck={labwareOnDeck}
+            />
+          ))}
+        </>
+      )}
+    </RobotWorkSpace>
+  )
 }
 
 export { LabwareItem }
