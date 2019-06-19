9c79272e5698b06a9aa64a4c90af9db76757c27f
IanLondon <ianlondondu@gmail.com>
2019-06-19 10:53:42 -0400

    add TODOs for missing/unclear mappings

--- protocol-designer/src/load-file/migration/3_0_0.js
+++ protocol-designer/src/load-file/migration/3_0_0.js
@@ -20,16 +20,16 @@ export const SCHEMA_VERSION = 3
 
 // TODO IMMEDIATELY review and confirm all mappings
 const modelToURIMap = {
+  '6-well-plate': 'corning_6_wellplate_16.8ml_flat',
   '12-well-plate': 'corning_12_wellplate_6.9ml_flat',
   '24-well-plate': 'corning_24_wellplate_3.4ml_flat',
-  '384-plate': 'corning_384_wellplate_112ul_flat',
   '48-well-plate': 'corning_48_wellplate_1.6ml_flat',
-  '6-well-plate': 'corning_6_wellplate_16.8ml_flat',
-  '96-PCR-flat': 'biorad_96_wellplate_200ul_pcr',
+  '384-plate': 'corning_384_wellplate_112ul_flat',
+  '96-PCR-flat': 'biorad_96_wellplate_200ul_pcr', // TODO: 96-PCR-flat not in spreadsheet
   '96-flat': 'generic_96_wellplate_340ul_flat',
   'biorad-hardshell-96-PCR': 'biorad_96_wellplate_200ul_pcr',
   'opentrons-aluminum-block-2ml-eppendorf':
-    'opentrons_24_aluminumblock_generic_2ml_screwcap',
+    'opentrons_24_aluminumblock_generic_2ml_screwcap', // TODO: opentrons-aluminum-block-2ml-eppendorf not in spreadsheet
   'opentrons-aluminum-block-2ml-screwcap':
     'opentrons_24_aluminumblock_generic_2ml_screwcap',
   'opentrons-aluminum-block-96-PCR-plate':
@@ -45,11 +45,21 @@ const modelToURIMap = {
   'opentrons-tuberack-2ml-screwcap':
     'opentrons_24_tuberack_generic_2ml_screwcap',
   'opentrons-tuberack-50ml': 'opentrons_6_tuberack_falcon_50ml_conical',
-  'tiprack-1000ul': 'opentrons_96_tiprack_1000ul',
-  'tiprack-10ul': 'opentrons_96_tiprack_10ul',
+  'tiprack-1000ul': 'opentrons_96_tiprack_1000ul', // TODO: spreadsheet says to use GEB: geb_96_tiprack_1000ul
+  'tiprack-10ul': 'opentrons_96_tiprack_10ul', // TODO: spreadsheet says to use GEB: geb_96_tiprack_10ul
   'trough-12row': 'usascientific_12_reservoir_22ml',
-  'fixed-trash': 'opentrons_1_trash_1100ml_fixed',
+  'fixed-trash': 'opentrons_1_trash_1100ml_fixed', // TODO: spreadsheet says to use the smaller one opentrons_1_trash_850ml_fixed but we should use the bigger one, right?
 }
+// TODO: missing mappings for:
+// tiprack-200ul
+// opentrons-aluminum-block-PCR-strips-200ul
+// tube-rack-.75ml
+// tube-rack-2ml
+// tube-rack-15_50ml
+// 96-deep-well
+// 96-PCR-tall
+// PCR-strip-tall
+// trash-box
 
 function v1LabwareModelToV2URI(model: string): string {
   const uri = `opentrons/${modelToURIMap[model]}/1`
