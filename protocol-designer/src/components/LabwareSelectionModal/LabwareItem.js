406b27d8dbf0dcb0f19462474b8faae350f40225
Ian London <ianlondondu@gmail.com>
2019-07-03 11:47:18 -0400

    feat(protocol-designer): add 'view measurements' link (#3665)
    * widen the labware selection modal to accommodate new 'view measurements' button
    
    Closes #3657

--- protocol-designer/src/components/LabwareSelectionModal/LabwareItem.js
+++ protocol-designer/src/components/LabwareSelectionModal/LabwareItem.js
@@ -1,34 +1,51 @@
 // @flow
+import i18n from '../../localization'
 import * as React from 'react'
 import { PDListItem } from '../lists'
 import styles from './styles.css'
+import {
+  getLabwareDefURI,
+  getLabwareDisplayName,
+  type LabwareDefinition2,
+} from '@opentrons/shared-data'
 
-type Props = {
-  selectLabware: (containerType: string) => mixed,
-  containerType: string,
-  displayName: string,
+type Props = {|
+  selectLabware: (labwareLoadName: string) => mixed,
+  labwareDef: LabwareDefinition2,
   onMouseEnter: () => any,
   onMouseLeave: () => any,
-}
+|}
+
+const LABWARE_LIBRARY_PAGE_PATH = 'https://labware.opentrons.com'
 
 export default function LabwareItem(props: Props) {
-  const {
-    selectLabware,
-    onMouseLeave,
-    onMouseEnter,
-    containerType,
-    displayName,
-  } = props
+  const { selectLabware, onMouseLeave, onMouseEnter, labwareDef } = props
+
+  const displayName = getLabwareDisplayName(labwareDef)
+  const labwareURI = getLabwareDefURI(labwareDef)
+  const labwareLoadName = labwareDef.parameters.loadName
+
   return (
     <PDListItem
       border
       hoverable
       className={styles.labware_list_item}
-      onClick={() => selectLabware(containerType)}
+      onClick={() => selectLabware(labwareURI)}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
-      {displayName}
+      <div className={styles.labware_name}>{displayName}</div>
+      {labwareDef.namespace === 'opentrons' ? (
+        <a
+          className={styles.view_measurements_link}
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`${LABWARE_LIBRARY_PAGE_PATH}/${labwareLoadName}`}
+          onClick={e => e.stopPropagation()}
+        >
+          {i18n.t('modal.labware_selection.view_measurements')}
+        </a>
+      ) : null}
     </PDListItem>
   )
 }
