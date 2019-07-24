97d839d0423db4fdab848e1934679b8e74ea07d8
IanLondon <ianlondondu@gmail.com>
2019-07-23 20:19:21 -0400

    add mock for getAllLoadNames

--- labware-library/src/__mocks__/definitions.js
+++ labware-library/src/__mocks__/definitions.js
@@ -3,12 +3,29 @@ import assert from 'assert'
 // replace webpack-specific require.context with Node-based glob in tests
 import path from 'path'
 import glob from 'glob'
+import uniq from 'lodash/uniq'
 
 import type { LabwareList } from '../types'
 
 const LABWARE_FIXTURE_PATTERN = path.join(
   __dirname,
   '../../../shared-data/labware/fixtures/2/*.json'
+)
+
+const allLoadNames = uniq(
+  glob
+    .sync(LABWARE_FIXTURE_PATTERN)
+    .map(require)
+    .map(def => def.parameters.loadName)
+)
+
+assert(
+  allLoadNames.length > 0,
+  `no labware loadNames found, something broke. ${LABWARE_FIXTURE_PATTERN}`
+)
+
+export const getAllLoadNames = jest.fn<Array<void>, Array<string>>(
+  () => allLoadNames
 )
 
 const allLabware = glob
