9f4635d483140b46a3a4708a86ee313697801640
IanLondon <ianlondondu@gmail.com>
2019-07-24 16:25:14 -0400

    oops fix missing type in WIP labwareTestProtocol

--- labware-library/src/labware-creator/labwareTestProtocol.js
+++ labware-library/src/labware-creator/labwareTestProtocol.js
@@ -1,4 +1,12 @@
 // @flow
+import type { LabwareDefinition2 } from '@opentrons/shared-data'
+
+type LabwareTestProtocolArgs = {|
+  pipetteName: string,
+  mount: string,
+  tiprackLoadName: string,
+  definition: LabwareDefinition2,
+|}
 const labwareTestProtocol = (args: LabwareTestProtocolArgs): string => {
   return 'TODO python protocol here'
 }
