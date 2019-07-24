e68c4d05b2d2ec3a5220f94e26d5dc475579cdd8
laura_danielle <laura@opentrons.com>
2019-07-24 11:39:18 +0800

    fix(api): Allow apiv2 to find pipettes by name versus model

--- api/src/opentrons/hardware_control/simulator.py
+++ api/src/opentrons/hardware_control/simulator.py
@@ -5,7 +5,7 @@ from threading import Event
 from typing import Dict, Optional, List, Tuple
 from contextlib import contextmanager
 from opentrons import types
-from opentrons.config.pipette_config import config_models
+from opentrons.config.pipette_config import config_models, configs
 from opentrons.drivers.smoothie_drivers import SimulatingDriver
 from . import modules
 
@@ -15,7 +15,12 @@ MODULE_LOG = logging.getLogger(__name__)
 
 def find_config(prefix: str) -> str:
     """ Find the most recent config matching `prefix` """
-    matches = [conf for conf in config_models if conf.startswith(prefix)]
+    matches = [prefix]
+    try:
+        configs[prefix]
+    except KeyError:
+        matches = [conf for conf in config_models
+                   if configs[conf]['name'].startswith(prefix)]
     if not matches:
         raise KeyError('No match found for prefix {}'.format(prefix))
     if prefix in matches:
