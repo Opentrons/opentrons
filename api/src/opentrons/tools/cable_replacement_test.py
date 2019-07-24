6ab6aeff5379bca8916232c0fe24aa2b5bc4ae8d
laura_danielle <laura@opentrons.com>
2019-07-24 15:21:27 +0800

    fix flake error

--- api/src/opentrons/tools/cable_replacement_test.py
+++ api/src/opentrons/tools/cable_replacement_test.py
@@ -1,11 +1,15 @@
 from opentrons import robot
 
+
 def test_pip_cable(smoothie):
     pipettes_attached = ['left', 'right']
     for mount in pipettes_attached:
-        assert smoothie.read_pipette_model(mount) is not None, f'FAIL, no model {mount}'
-        assert smoothie.read_pipette_id(mount) is not None, f'FAIL, no id {mount}'
+        assert smoothie.read_pipette_model(mount)\
+            is not None, f'FAIL, no model {mount}'
+        assert smoothie.read_pipette_id(mount)\
+            is not None, f'FAIL, no id {mount}'
     print("PASS")
+
 
 def try_homing(smoothie, num):
     try:
@@ -25,6 +29,7 @@ def try_homing(smoothie, num):
         robot.comment(f"FAIL, check wire")
     smoothie.move({'X': 150, 'Y': 150, 'Z': 80,  'A': 80})
     print(f"Homing {num} PASSED")
+
 
 if __name__ == '__main__':
     robot.connect()
