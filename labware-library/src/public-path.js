62a16c0987edf2516768884e6442d250e817a5ca
IanLondon <ianlondondu@gmail.com>
2019-07-24 22:10:17 -0400

    try allowing sandbox public path to be set if "/"

--- labware-library/src/public-path.js
+++ labware-library/src/public-path.js
@@ -2,7 +2,10 @@
 // set the public path at runtime according to deploy location
 // side-effects to make sure public path is set before other imports
 
-if (typeof global.__webpack_public_path__ === 'undefined') {
+if (
+  typeof global.__webpack_public_path__ === 'undefined' ||
+  global.__webpack_public_path__ === '/'
+) {
   if (location.hostname.startsWith('sandbox')) {
     // if we're serving from sandbox, ensure the public path is set to name
     // of the deploy folder
