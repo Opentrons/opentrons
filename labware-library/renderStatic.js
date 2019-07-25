d97d545092ead0b4b6a77f746af10ae6f2501de8
IanLondon <ianlondondu@gmail.com>
2019-07-25 12:47:00 -0400

    crawl / to get labware routes

--- labware-library/renderStatic.js
+++ labware-library/renderStatic.js
@@ -1,10 +1,7 @@
 'use strict'
 const { run } = require('react-snap')
 
-// TODO IMMEDIATELY: add all labware routes
-const allLabwareRoutes = []
-
 run({
   source: 'dist',
-  include: ['create', ...allLabwareRoutes].map(p => `/${p}`),
+  include: ['/', '/create'],
 })
