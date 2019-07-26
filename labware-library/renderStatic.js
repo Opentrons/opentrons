1100cfdc2704a88c79103959f5babb57886e0c6d
IanLondon <ianlondondu@gmail.com>
2019-07-26 13:31:22 -0400

    hacky post-snapshot path manip

--- labware-library/renderStatic.js
+++ labware-library/renderStatic.js
@@ -1,7 +1,30 @@
 'use strict'
 const { run } = require('react-snap')
+const path = require('path')
+const glob = require('glob')
+const fs = require('fs')
+
+const outputPath = 'dist'
+const outputPathAbs = path.join(__dirname, outputPath)
 
 run({
-  source: 'dist',
-  include: ['/', '/create'],
+  source: outputPath,
+  include: ['/', '/create/'],
 })
+  .then(() => {
+    const pages = glob.sync(path.join(outputPathAbs, '**', '*.html'))
+    pages.forEach(pagePath => {
+      const page = fs.readFileSync(pagePath, 'utf8')
+      const relativePath =
+        path
+          .relative(path.dirname(pagePath), outputPathAbs)
+          // if on windows, \ -> / for url paths (untested)
+          .replace('\\', '/') || '.'
+      console.log({ pagePath, relativePath })
+      const newPage = page
+        .replace(/src="\//g, `src="${relativePath}/`)
+        .replace(/link href="\//g, `link href="${relativePath}/`)
+      fs.writeFileSync(pagePath, newPage)
+    })
+  })
+  .catch(console.error)
--- labware-library/src/public-path.js
+++ labware-library/src/public-path.js
@@ -2,20 +2,19 @@
 // set the public path at runtime according to deploy location
 // side-effects to make sure public path is set before other imports
 
-if (
-  typeof global.__webpack_public_path__ === 'undefined' ||
-  global.__webpack_public_path__ === '/'
-) {
-  if (location.hostname.startsWith('sandbox')) {
-    // if we're serving from sandbox, ensure the public path is set to name
-    // of the deploy folder
-    const basePath = location.pathname.slice(1).split('/')[0] || ''
-    global.__webpack_public_path__ = `/${basePath}/`
-  } else {
-    global.__webpack_public_path__ = '/'
-  }
+let _publicPath = '/'
+if (location.hostname.startsWith('sandbox')) {
+  // if we're serving from sandbox, ensure the public path is set to name
+  // of the deploy folder
+  const basePath = location.pathname.slice(1).split('/')[0] || ''
+  _publicPath = `/${basePath}/`
 }
 
+// $FlowFixMe
+__webpack_public_path__ = _publicPath // eslint-disable-line no-undef
+
+console.log('public path', _publicPath)
+
 export function getPublicPath() {
-  return global.__webpack_public_path__
+  return _publicPath
 }
