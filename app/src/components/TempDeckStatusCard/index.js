a1ae895597d89a496910696ae984c70c25aea725
Brian Cooper <brian@allegro.com>
2019-07-31 14:01:10 -0400

    build out module status card

--- app/src/components/TempDeckStatusCard/index.js
+++ app/src/components/TempDeckStatusCard/index.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 
 import { getModuleDisplayName } from '@opentrons/shared-data'
 import { getConnectedRobot } from '../../discovery'
-import { fetchModules, fetchModuleData, getModulesState } from '../../robot-api'
+import { fetchModules, getModulesState } from '../../robot-api'
 
 import { LabeledValue, IntervalWrapper } from '@opentrons/components'
 import StatusCard from './StatusCard'
@@ -15,32 +15,29 @@ import type { State, Dispatch } from '../../types'
 import type { TempDeckModule } from '../../robot-api'
 import type { Robot } from '../../discovery'
 
+import TempDeckCard from './TempDeckCard'
+import ThermocyclerCard from './ThermocyclerCard'
+
 const POLL_TEMPDECK_INTERVAL_MS = 1000
+const LIVE_STATUS_MODULES = ['tempdeck', 'thermocycler']
 
 type SP = {|
   _robot: ?Robot,
-  tempDeck: TempDeckModule | null,
+  liveStatusModules: Array<TempDeckModule | ThermocyclerModule>,
 |}
 
 type DP = {|
   _fetchModules: (_robot: Robot) => mixed,
-  _fetchModuleData: (_robot: Robot, serial: string) => mixed,
 |}
 
 type Props = {|
-  tempDeck: ?TempDeckModule,
+  liveStatusModules: Array<TempDeckModule | ThermocyclerModule>,
   fetchModules: () => mixed,
-  fetchModuleData: () => mixed,
 |}
 
-class TempDeckStatusCard extends React.Component<Props> {
-  componentDidMount() {
-    this.props.fetchModules()
-  }
-
-  render() {
-    const { tempDeck, fetchModuleData } = this.props
-    if (!tempDeck) return null
+const ModuleLiveStatusCards = (props: Props) => {
+    const { liveStatusModules } = this.props
+    if (liveStatusModules.length === 0) return null
 
     const { name, status, data } = tempDeck
     const { currentTemp, targetTemp } = data
@@ -48,23 +45,20 @@ class TempDeckStatusCard extends React.Component<Props> {
 
     return (
       <IntervalWrapper
-        refresh={fetchModuleData}
-        // TODO: BC: 2018-08-13 we should really only be hitting fetchModuleData
-        // again once we've received the response from the last call
+        refresh={fetchModules}
         interval={POLL_TEMPDECK_INTERVAL_MS}
       >
-        <StatusCard title={displayName}>
-          <CardContentRow>
-            <StatusItem status={status} />
-          </CardContentRow>
-          <CardContentRow>
-            <LabeledValue label="Current Temp" value={`${currentTemp} °C`} />
-            <LabeledValue
-              label="Target Temp"
-              value={targetTemp ? `${targetTemp} °C` : 'None'}
-            />
-          </CardContentRow>
-        </StatusCard>
+        {liveStatusModules.map(module => {
+          switch (module.name) {
+            case 'tempdeck':
+              return <TempDeckCard module={module} />
+            case 'thermocycler':
+              return <ThermocyclerCard module={module} />
+            case 'magdeck':
+            default:
+              return null
+          }
+        })}
       </IntervalWrapper>
     )
   }
@@ -75,33 +69,30 @@ function mapStateToProps(state: State): SP {
   const modules = _robot ? getModulesState(state, _robot.name) : []
 
   // TOD0 (ka 2018-7-25): Only supporting 1 temp deck at a time at launch
-  const tempDeck = modules.find(m => m.name === 'tempdeck')
+  const liveStatusModules = modules.filter(m =>
+    LIVE_STATUS_MODULES.includes(m.name)
+  )
 
   // bogus ternary to satisfy flow
   return {
     _robot,
-    tempDeck: tempDeck && tempDeck.name === 'tempdeck' ? tempDeck : null,
+    liveStatusModules,
   }
 }
 
 function mapDispatchToProps(dispatch: Dispatch): DP {
   return {
-    _fetchModuleData: (_robot, _serial) =>
-      dispatch(fetchModuleData(_robot, _serial)),
     _fetchModules: _robot => dispatch(fetchModules(_robot)),
   }
 }
 
 function mergeProps(stateProps: SP, dispatchProps: DP): Props {
-  const { _fetchModules, _fetchModuleData } = dispatchProps
-  const { _robot, tempDeck } = stateProps
-  const _serial = tempDeck?.serial
+  const { _fetchModules } = dispatchProps
+  const { _robot, liveStatusModules} = stateProps
 
   return {
-    tempDeck,
+    liveStatusModules,
     fetchModules: () => _robot && _fetchModules(_robot),
-    fetchModuleData: () =>
-      _robot && _serial && _fetchModuleData(_robot, _serial),
   }
 }
 
@@ -109,4 +100,4 @@ export default connect<Props, {||}, SP, DP, State, Dispatch>(
   mapStateToProps,
   mapDispatchToProps,
   mergeProps
-)(TempDeckStatusCard)
+)(ModuleLiveStatusCards)
