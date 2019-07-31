b6001d03453b0a4a4cc8ca8ccbc0efd3ee0210e7
IanLondon <ianlondondu@gmail.com>
2019-07-31 14:44:34 -0400

    fix y-offset flip

--- labware-library/src/labware-creator/fieldsToLabware.js
+++ labware-library/src/labware-creator/fieldsToLabware.js
@@ -96,7 +96,8 @@ export default function fieldsToLabware(
       version: 1,
       offset: {
         x: fields.gridOffsetX,
-        y: fields.gridOffsetY,
+        // NOTE: offsets are from labware top upper left corner to the well, so Y is inverted
+        y: fields.footprintYDimension - fields.gridOffsetY,
         // NOTE: must give wells a z offset b/c `well.z = offset.z - wellDepth`.
         // We include well lip as part of Z dimension in Labware Creator's fields,
         // so labware's offset.z is the SAME as labwareZDimension.
