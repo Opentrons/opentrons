e1a99e71b33b58e306b059c9791a854c91528cfb
Brian Cooper <brian@allegro.com>
2019-08-16 16:27:12 -0400

    latch modal functionality

--- app/src/components/ReviewDeck/index.js
+++ app/src/components/ReviewDeck/index.js
@@ -1,33 +1,22 @@
 // @flow
 // deck review modal for labware calibration page
-import * as React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import some from 'lodash/some'
+import { PrimaryButton, AlertModal } from '@opentrons/components'
 
-import type { State, Dispatch } from '../../types'
+import type { Dispatch } from '../../types'
 import {
   actions as robotActions,
   selectors as robotSelectors,
-  type Mount,
-  type Labware,
 } from '../../robot'
 
+import { Portal } from '../portal'
 import DeckMap from '../DeckMap'
 import Prompt from './Prompt'
 import styles from './styles.css'
 
-type OP = {| slot: ?string |}
-
-type SP = {| currentLabware: ?Labware, _calibratorMount: ?Mount |}
-
-type DP = {| dispatch: Dispatch |}
-
-type Props = { ...SP, onClick: () => void }
-
-export default connect<Props, OP, SP, {||}, State, Dispatch>(
-  mapStateToProps,
-  null,
-  mergeProps
-)(ReviewDeck)
+type Props = {| slot: ?string |}
 
 function ReviewDeck(props: Props) {
   const { slot } = props
@@ -35,15 +24,26 @@ function ReviewDeck(props: Props) {
   const dispatch = useDispatch<Dispatch>()
   const allLabware = useSelector(robotSelectors.getLabware)
   const calibratorMount = useSelector(robotSelectors.getCalibratorMount)
+  const sessionModules = useSelector(robotSelectors.getModules)
+
+  const [mustPrepNestedLabware, setMustPrepNestedLabware] = useState(
+    some(sessionModules, mod => mod.name === 'thermocycler')
+  )
+  const [isPrepNestedLabwareOpen, setIsPrepNestedLabwareOpen] = useState(false)
 
   const currentLabware = allLabware.find(lw => lw.slot === slot)
 
-  const handleClick = () => {
-    const prepareNestedLabware = true // TODO: get from elsewhere
-    if (prepareNestedLabware) {
-    } else if (currentLabware && calibratorMount) {
+  const continueToCalibrate = () => {
+    if (currentLabware && calibratorMount) {
       const mountToUse = currentLabware.calibratorMount || calibratorMount
       dispatch(robotActions.moveTo(mountToUse, currentLabware.slot))
+    }
+  }
+  const handleClick = () => {
+    if (mustPrepNestedLabware) {
+      setIsPrepNestedLabwareOpen(true)
+    } else {
+      continueToCalibrate()
     }
   }
 
@@ -53,33 +53,59 @@ function ReviewDeck(props: Props) {
       <div className={styles.deck_map_wrapper}>
         <DeckMap className={styles.deck_map} />
       </div>
+      {isPrepNestedLabwareOpen && (
+        <Portal>
+          <AlertModal
+            iconName={null}
+            heading="Position latch to hold down plate"
+          >
+            <p>Push down the latch and lock in place to hold the PCR plate.</p>
+            <p>
+              This helps to ensure your PCR plate stays secure in place when the
+              Thermocycler Module lid opens during a run.
+            </p>
+            <p>TODO IMAGE HERE</p>
+            <PrimaryButton
+              className={styles.open_lid_button}
+              onClick={() => {
+                setMustPrepNestedLabware(false)
+                continueToCalibrate()
+              }}
+            >
+              Confirm PCR Plate Is Latched Down
+            </PrimaryButton>
+          </AlertModal>
+        </Portal>
+      )}
     </div>
   )
 }
 
-function mapStateToProps(state: State, ownProps: OP): SP {
-  // TODO(mc, 2018-02-05): getCurrentLabware selector
-  const labware = robotSelectors.getLabware(state)
-  const currentLabware = labware.find(lw => lw.slot === ownProps.slot)
+export default ReviewDeck
 
-  return {
-    currentLabware,
-    _calibratorMount:
-      currentLabware &&
-      (currentLabware.calibratorMount ||
-        robotSelectors.getCalibratorMount(state)),
-  }
-}
+// function mapStateToProps(state: State, ownProps: OP): SP {
+//   // TODO(mc, 2018-02-05): getCurrentLabware selector
+//   const labware = robotSelectors.getLabware(state)
+//   const currentLabware = labware.find(lw => lw.slot === ownProps.slot)
 
-function mergeProps(stateProps: SP, dispatchProps: DP): Props {
-  const { currentLabware, _calibratorMount } = stateProps
-  const { dispatch } = dispatchProps
+//   return {
+//     currentLabware,
+//     _calibratorMount:
+//       currentLabware &&
+//       (currentLabware.calibratorMount ||
+//         robotSelectors.getCalibratorMount(state)),
+//   }
+// }
 
-  return {
-    ...stateProps,
-    confirmAndMoveTo: () =>
-      currentLabware &&
-      _calibratorMount &&
-      dispatch(robotActions.moveTo(_calibratorMount, currentLabware.slot)),
-  }
-}
+// function mergeProps(stateProps: SP, dispatchProps: DP): Props {
+//   const { currentLabware, _calibratorMount } = stateProps
+//   const { dispatch } = dispatchProps
+
+//   return {
+//     ...stateProps,
+//     confirmAndMoveTo: () =>
+//       currentLabware &&
+//       _calibratorMount &&
+//       dispatch(robotActions.moveTo(_calibratorMount, currentLabware.slot)),
+//   }
+// }
