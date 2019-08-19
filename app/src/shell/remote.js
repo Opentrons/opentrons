86468116f39246772aa5683d20533907e6468c3a
Mike Cousins <mike@cousins.io>
2019-08-19 18:50:10 -0400

    fix(app): make shell remote check lazier to avoid spurious assertions

--- app/src/shell/remote.js
+++ app/src/shell/remote.js
@@ -2,20 +2,23 @@
 // access main process remote modules via attachments to `global`
 import assert from 'assert'
 
-assert(
-  global.APP_SHELL_REMOTE,
-  'Expected APP_SHELL_REMOTE to be attached to global scope; is app-shell/src/preload.js properly configured?'
+const remote = new Proxy(
+  {},
+  {
+    get(target, propName) {
+      assert(
+        global.APP_SHELL_REMOTE,
+        'Expected APP_SHELL_REMOTE to be attached to global scope; is app-shell/src/preload.js properly configured?'
+      )
+
+      assert(
+        propName in global.APP_SHELL_REMOTE,
+        `Expected APP_SHELL_REMOTE.${propName} to exist, is app-shell/src/preload.js properly configured?`
+      )
+
+      return global.APP_SHELL_REMOTE[propName]
+    },
+  }
 )
-
-const remote = new Proxy(global.APP_SHELL_REMOTE, {
-  get(target, propName) {
-    assert(
-      propName in target,
-      `Expected APP_SHELL_REMOTE.${propName} to exist, is app-shell/src/preload.js properly configured?`
-    )
-
-    return target[propName]
-  },
-})
 
 export default remote
