8a0ed3f7760a0a476bc2f6e9c4addf9d4b2519f3
Mike Cousins <mike@cousins.io>
2019-08-19 19:42:15 -0400

    fixup: Type remote now that it isnt an implicit any

--- app/src/shell/remote.js
+++ app/src/shell/remote.js
@@ -2,23 +2,22 @@
 // access main process remote modules via attachments to `global`
 import assert from 'assert'
 
-const remote = new Proxy(
-  {},
-  {
-    get(target, propName) {
-      assert(
-        global.APP_SHELL_REMOTE,
-        'Expected APP_SHELL_REMOTE to be attached to global scope; is app-shell/src/preload.js properly configured?'
-      )
+import type { Remote } from './types'
 
-      assert(
-        propName in global.APP_SHELL_REMOTE,
-        `Expected APP_SHELL_REMOTE.${propName} to exist, is app-shell/src/preload.js properly configured?`
-      )
+const remote = new Proxy((({}: any): Remote), {
+  get(target, propName) {
+    assert(
+      global.APP_SHELL_REMOTE,
+      'Expected APP_SHELL_REMOTE to be attached to global scope; is app-shell/src/preload.js properly configured?'
+    )
 
-      return global.APP_SHELL_REMOTE[propName]
-    },
-  }
-)
+    assert(
+      propName in global.APP_SHELL_REMOTE,
+      `Expected APP_SHELL_REMOTE.${propName} to exist, is app-shell/src/preload.js properly configured?`
+    )
+
+    return global.APP_SHELL_REMOTE[propName]
+  },
+})
 
 export default remote
