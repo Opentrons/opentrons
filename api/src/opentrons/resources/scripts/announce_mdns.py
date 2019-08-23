aa865498c760fccabc67d8ec12f8d72c92f29302
Seth Foster <seth@opentrons.com>
2019-08-23 13:07:58 -0400

    delete more unused files

--- api/src/opentrons/resources/scripts/announce_mdns.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-from avahi_tools.service import AvahiService
-
-
-DEVICE_NAME = os.environ.get('RESIN_DEVICE_NAME_AT_INIT')
-service_name = 'opentrons-{}'.format(DEVICE_NAME)
-AvahiService(service_name, "_http._tcp", port=31950, keep_alive=True)
