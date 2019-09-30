c890897ef696b7e2f283420b7567c2f0228f354e
Seth Foster <seth@opentrons.com>
2019-09-30 11:51:28 -0400

    fix(api): apiv1: cli-dc: fix tip pickup with left mount
    We were getting a little complex with motion commands in _helper_pickup. Relying
    on the smoothie driver to move only in z is ugly, but in line with how we do it
    elsewhere in the module, and doesn't have this issue.
    
    Closes #3980

--- api/src/opentrons/deck_calibration/dc_main.py
+++ api/src/opentrons/deck_calibration/dc_main.py
@@ -325,10 +325,8 @@ class CLITool:
             p.instrument_mover.set_active_current(p._pick_up_current)
             p.instrument_mover.set_speed(p._pick_up_speed)
             dist = (-1 * p._pick_up_distance) + (-1 * p._pick_up_increment * i)
-            location_pickup = Vector(top[0], top[1], dist + top[2])
-            self.hardware.move_to(
-                (self.hardware.deck, location_pickup), p, 'direct')
-            self.hardware.move_to((self.hardware.deck, top), p, 'direct')
+            self.hardware._driver.move({self._current_mount: dist + top[2]})
+            self.hardware._driver.move({self._current_mount: top[2]})
             # move nozzle back up
             p.instrument_mover.pop_active_current()
             p.instrument_mover.pop_speed()
