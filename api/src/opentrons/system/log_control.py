56354f29d4931df53f0b29d7aef6b6d06493fca6
Seth Foster <seth@opentrons.com>
2019-10-01 19:04:36 -0400

    fix(api): Do not run out of memory when dumping large logs (#4157)
    Instead of using the python-systemd bindings for journald to copy data out of the journal, which takes about 2ms per record (!), just shell out to `journalctl` and slurp up the response

--- api/src/opentrons/system/log_control.py
+++ api/src/opentrons/system/log_control.py
@@ -5,18 +5,15 @@ This is the implementation of the endpoints in
 :py:mod:`opentrons.server.endpoints.logs` and friends.
 """
 import asyncio
-import collections
-import datetime
 import logging
+import subprocess
 import syslog
-from typing import Any, Deque, Dict, List, Tuple
-
-import systemd.journal as journal  # type: ignore
+from typing import Tuple
 
 
 LOG = logging.getLogger(__name__)
 
-MAX_RECORDS = 1000000
+MAX_RECORDS = 100000
 
 _SYSLOG_PRIORITY_TO_NAME = {
     syslog.LOG_EMERG: 'emergency',
@@ -30,82 +27,23 @@ _SYSLOG_PRIORITY_TO_NAME = {
 }
 
 
-async def get_records(selector: str, record_count: int)\
-          -> Deque[Dict[str, Any]]:
+async def get_records_dumb(selector: str, records: int,
+                           mode: str) -> bytes:
+    """ Dump the log files.
+
+    :param selector: The syslog selector to limit responses to
+    :param records: The maximum number of records to print
+    :param mode: A journalctl dump mode. Should be either "short" or "json".
     """
-    Get log records up to record count.
-
-    The records are dicts from string keys to the arbitrary data provided
-    by journald's interface (see
-    https://www.freedesktop.org/software/systemd/python-systemd/journal.html ).
-
-    :return: deque[dict[str, Any]]: A deque of records
-    """
-    loop = asyncio.get_event_loop()
-    log_deque: Deque[Dict[str, Any]] = collections.deque(maxlen=record_count)
-    with journal.Reader(journal.SYSTEM_ONLY) as r:
-        r.add_match(SYSLOG_IDENTIFIER=selector)
-        last_time = loop.time()
-        for record in r:
-            log_deque.append(record)
-            now = loop.time()
-            if (now-last_time) > 0.1:
-                last_time = now
-                await asyncio.sleep(0.01)
-    return log_deque
-
-
-def _format_record_text(record: Dict[str, Any]) -> str:
-    dict_rec = _format_record_dict(record)
-    return f'{dict_rec["time"]} {dict_rec["logger"]} '\
-        f'[{dict_rec["level_name"]}]: {dict_rec["message"]}'
-
-
-async def get_records_text(selector: str, record_count: int) -> str:
-    """ Get log records as a newline-separated string of log records.
-
-    Each record is a string of
-
-    ``isotime logger [levelname]: message``
-    """
-    records = await get_records(selector, record_count)
-    return '\n'.join([_format_record_text(record) for record in records])
-
-
-def _format_record_dict(record: Dict[str, Any]) -> Dict[str, Any]:
-    return {
-        'logger': record.get('LOGGER', '<unknown>'),
-        'level': record.get('PRIORITY'),
-        'level_name': _SYSLOG_PRIORITY_TO_NAME.get(  # type: ignore
-            record.get('PRIORITY'), '<unknown>'),
-        'file': record.get('CODE_FILE', '<unknown>'),
-        'line': record.get('CODE_LINE', '<unknown>'),
-        'func': record.get('CODE_FUNC', '<unknown>'),
-        'time': record.get(
-            '__REALTIME_TIMESTAMP',
-            datetime.datetime.fromtimestamp(0)).isoformat(),
-        'boot': str(record.get('_BOOT_ID', '<unknown>')),
-        'message': record.get('MESSAGE', '<unknown>')
-    }
-
-
-async def get_records_serializable(
-        selector: str, record_count: int) -> List[Dict[str, Any]]:
-    """ Get log records in a structured format that is json serializable.
-
-    Retains the syslog fields
-    - 'logger': logger name
-    - 'level': syslog priority, as an int
-    - 'level_name': the name of the priority
-    - 'file': the file from which the record was sourced
-    - 'line': the line from which the record was sourced
-    - 'func': the function from which the record was sourecd
-    - 'time': the time in iso at which the record was sourced
-    - 'boot': the boot id, an opaque string different per boot
-    - 'message': the actual message
-    """
-    records = await get_records(selector, record_count)
-    return [_format_record_dict(rec) for rec in records]
+    proc = await asyncio.create_subprocess_exec(
+        'journalctl', '--no-pager',
+        '-t', selector,
+        '-n', str(records),
+        '-o', mode,
+        '-a',
+        stdout=subprocess.PIPE)
+    stdout, _ = await proc.communicate()
+    return stdout
 
 
 async def set_syslog_level(level: str) -> Tuple[int, str, str]:
