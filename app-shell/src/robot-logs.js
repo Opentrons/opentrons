1442ef86c70c215bc273132011100dc6aeeafcdc
Mike Cousins <mike@opentrons.com>
2019-10-01 17:57:12 -0400

    feat(app): show spinner while robot logs are downloading
    Known limitation: UI will only allow you to be downloading logs from one robot at a time

--- app-shell/src/robot-logs.js
+++ app-shell/src/robot-logs.js
@@ -1,12 +1,24 @@
 // download robot logs manager
 
+import { download } from 'electron-dl'
+import createLogger from './log'
+
+const log = createLogger(__dirname)
+
 export function registerRobotLogs(dispatch, mainWindow) {
   return function handleIncomingAction(action) {
     if (action.type === 'shell:DOWNLOAD_LOGS') {
-      const {
-        payload: { logUrls },
-      } = action
-      logUrls.forEach(url => mainWindow.webContents.downloadURL(url))
+      const { logUrls } = action.payload
+
+      log.debug('Downloading robot logs', { logUrls })
+
+      logUrls
+        .reduce(
+          (result, url) => result.then(() => download(mainWindow, url)),
+          Promise.resolve()
+        )
+        .catch(error => log.error('Error downloading robot logs', { error }))
+        .then(() => dispatch({ type: 'shell:DOWNLOAD_LOGS_DONE' }))
     }
   }
 }
