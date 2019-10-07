732a617a961e5357f06aa2201cb219914228ab13
IanLondon <ianlondondu@gmail.com>
2019-10-07 15:40:56 -0400

    revert PD changes in this PR

--- protocol-designer/src/analytics/integrations.js
+++ protocol-designer/src/analytics/integrations.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import cookie from 'cookie'
 
-export const shutdownFullstory = () => {
+export const shutdownAnalytics = () => {
   if (window[window['_fs_namespace']]) {
     window[window['_fs_namespace']].shutdown()
   }
@@ -25,7 +25,6 @@ const _setAnalyticsTags = () => {
       commit_str,
       version_str,
       buildDate_date,
-      ot_application_name_str: 'protocol-designer', // NOTE: to distinguish from other apps using the org
     })
   }
 }
@@ -46,34 +45,30 @@ export const initializeAnalytics = () => {
       }
       return
     }
-    g = m[e] = function(a, b, s) {
-      g.q ? g.q.push([a, b, s]) : g._api(a, b, s)
+    g = m[e] = function(a, b) {
+      g.q ? g.q.push([a, b]) : g._api(a, b)
     }
     g.q = []
     o = n.createElement(t)
     o.async = 1
-    o.crossOrigin = 'anonymous'
     o.src = 'https://' + _fs_host + '/s/fs.js'
     y = n.getElementsByTagName(t)[0]
     y.parentNode.insertBefore(o, y)
-    g.identify = function(i, v, s) {
-      g(l, { uid: i }, s)
-      if (v) g(l, v, s)
+    g.identify = function(i, v) {
+      g(l, { uid: i })
+      if (v) g(l, v)
     }
-    g.setUserVars = function(v, s) {
-      g(l, v, s)
+    g.setUserVars = function(v) {
+      g(l, v)
     }
-    g.event = function(i, v, s) {
-      g('event', { n: i, p: v }, s)
+    g.event = function(i, v) {
+      g('event', { n: i, p: v })
     }
     g.shutdown = function() {
       g('rec', !1)
     }
     g.restart = function() {
       g('rec', !0)
-    }
-    g.log = function(a, b) {
-      g('log', [a, b])
     }
     g.consent = function(a) {
       g('consent', !arguments.length || a)
