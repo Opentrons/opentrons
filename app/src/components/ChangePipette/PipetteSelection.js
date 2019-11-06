0edc7e49ffc021ffb393beef1534d36ebeddf94a
Brian Arthur Cooper <brian@opentrons.com>
2019-11-06 16:27:39 -0500

    chore(app): create ff for exposing multi GEN2 pipettes (#4364)

--- app/src/components/ChangePipette/PipetteSelection.js
+++ app/src/components/ChangePipette/PipetteSelection.js
@@ -1,7 +1,9 @@
 // @flow
 import * as React from 'react'
+import { useSelector } from 'react-redux'
 
 import { PipetteSelect } from '@opentrons/components'
+import { getConfig } from '../../config'
 import styles from './styles.css'
 
 const LABEL = 'Select the pipette you wish to attach:'
@@ -11,7 +13,10 @@ export type PipetteSelectionProps = {
 }
 
 export default function PipetteSelection(props: PipetteSelectionProps) {
-  const nameBlacklist = ['p20_multi_gen2', 'p300_multi_gen2']
+  const config = useSelector(getConfig)
+  const nameBlacklist = Boolean(config.devInternal?.enableMultiGEN2)
+    ? []
+    : ['p20_multi_gen2', 'p300_multi_gen2']
   return (
     <label className={styles.pipette_selection}>
       <span className={styles.pipette_selection_label}>{LABEL}</span>
