4c3a13fcadebf7f8e20ca418ddef87ac0bca2433
Brian Cooper <brian@allegro.com>
2019-11-06 18:25:18 -0500

    delint

--- api/src/opentrons/protocol_api/contexts.py
+++ api/src/opentrons/protocol_api/contexts.py
@@ -1653,23 +1653,18 @@ class InstrumentContext(CommandPublisher):
                 self._mount, critical_point=cp_override),
             from_lw)
 
-        self._log.debug(f"\n\n\n\nlocation.labware: {location.labware}")
-        if location.labware:
-            self._log.debug(f"location.labware.parent: {location.labware.parent}")
-        self._log.debug(f"ctx modules: {self._ctx._modules}")
-
         to_lw, to_well = geometry.split_loc_labware(location)
         from_lw, from_well = geometry.split_loc_labware(from_loc)
 
-        if (isinstance(to_lw, Labware) and \
+        if (isinstance(to_lw, Labware) and
                 isinstance(to_lw.parent, ThermocyclerGeometry)) or \
-                (isinstance(from_lw, Labware) and \
-                isinstance(from_lw.parent, ThermocyclerGeometry)):
-            tc_context = next(m for m in self._ctx._modules \
-                              if isinstance(m._module, modules.thermocycler.Thermocycler))
+                (isinstance(from_lw, Labware) and
+                    isinstance(from_lw.parent, ThermocyclerGeometry)):
+            tc_context = next(m for m in self._ctx._modules
+                              if isinstance(m, ThermocyclerContext))
             if tc_context and tc_context.lid_position == 'closed':
                 raise RuntimeError(
-                    "Cannot move to labware loaded in Thermocycler"\
+                    "Cannot move to labware loaded in Thermocycler"
                     " when lid is closed")
 
         moves = geometry.plan_moves(from_loc, location, self._ctx.deck,
