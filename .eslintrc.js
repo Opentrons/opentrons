15ee82ce8ccf5b6ff057e3fe77d3840c6ce14573
Brian Cooper <brian@allegro.com>
2019-11-06 15:58:33 -0500

    turn off extra boolean cast lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'react/display-name': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'no-extra-boolean-cast': 'off',
   },
 
   globals: {},
