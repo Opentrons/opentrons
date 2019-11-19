d3e72cf8793df54e8957c43da1b463d1a79f1a73
Brian Cooper <brian@allegro.com>
2019-11-19 15:44:07 -0500

    skeleton of well set helpers in py

--- api/src/opentrons/protocol_api/util.py
+++ api/src/opentrons/protocol_api/util.py
@@ -289,3 +289,78 @@ class ModifiedList(list):
             if name == item.replace("-", "_").lower():
                 return True
         return False
+
+
+# These Well Set utils are pythonized versions of
+# opentrons/shared-data/js/helpers/wellSets.js
+
+# import { getLabwareDefURI } from '@opentrons/shared-data'
+# import uniq from 'lodash/uniq'
+# import { getWellNamePerMultiTip } from './getWellNamePerMultiTip'
+# import type { LabwareDefinition2, PipetteNameSpecs } from '../types'
+
+# type WellSetByPrimaryWell = Array<Array<string>>
+
+# Compute all well sets for a labware def (non-memoized)
+def _getAllWellSetsForLabware(labwareDef)
+#   const allWells: Array<string> = Object.keys(labwareDef.wells)
+#   return allWells.reduce(
+#     (acc: WellSetByPrimaryWell, well: string): WellSetByPrimaryWell => {
+#       const wellSet = getWellNamePerMultiTip(labwareDef, well)
+#       return wellSet === null ? acc : [...acc, wellSet]
+#     },
+#     []
+#   )
+
+# TODO: think on caching
+# creates memoized getAllWellSetsForLabware + getWellSetForMultichannel fns.
+def makeWellSetHelpers()
+#   let cache: {
+#     [labwareDefURI: string]: ?{|
+#       labwareDef: LabwareDefinition2,
+#       wellSetByPrimaryWell: WellSetByPrimaryWell,
+#     |},
+#   } = {}
+
+#   const getAllWellSetsForLabware = (
+#     labwareDef: LabwareDefinition2
+#   ): WellSetByPrimaryWell => {
+#     const labwareDefURI = getLabwareDefURI(labwareDef)
+#     const c = cache[labwareDefURI]
+#     // use cached version only if labwareDef is shallowly equal, in case
+#     // custom labware defs are changed without giving them a new URI
+#     if (c && c.labwareDef === labwareDef) {
+#       return c.wellSetByPrimaryWell
+#     }
+#     const wellSetByPrimaryWell = _getAllWellSetsForLabware(labwareDef)
+#     cache[labwareDefURI] = { labwareDef, wellSetByPrimaryWell }
+#     return wellSetByPrimaryWell
+
+
+def get_well_set_for_multichannel(labwareDef, well: str):
+    # /** Given a well for a labware, returns the well set it belongs to (or null)
+    #  * for 8-channel access.
+    #  * Ie: C2 for 96-flat => ['A2', 'B2', 'C2', ... 'H2']
+    #  * Or A1 for trough => ['A1', 'A1', 'A1', ...]
+    #  **/
+    # const allWellSets = getAllWellSetsForLabware(labwareDef)
+    # return allWellSets.find((wellSet: Array<string>) => wellSet.includes(well))
+
+
+def can_pipette_use_labware(pipetteSpec, labwareDef)
+# if (pipetteSpec.channels === 1) {
+#   // assume all labware can be used by single-channel
+#   return true
+# }
+
+# const allWellSets = getAllWellSetsForLabware(labwareDef)
+# return allWellSets.some(wellSet => {
+#   const uniqueWells = uniq(wellSet)
+#   // if all wells are non-null, and there are either 1 (reservoir-like)
+#   // or 8 (well plate-like) unique wells in the set,
+#   // then assume multi-channel will work
+#   return (
+#     uniqueWells.every(well => well != null) &&
+#     [1, 8].includes(uniqueWells.length)
+#   )
+# })
