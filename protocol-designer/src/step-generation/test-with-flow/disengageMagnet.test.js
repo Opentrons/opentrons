b12687fd574671380759054283a6f8694db39a8e
IanLondon <ianlondondu@gmail.com>
2019-12-03 15:24:33 -0500

    add disengageMagnet test

---
+++ protocol-designer/src/step-generation/test-with-flow/disengageMagnet.test.js
@@ -0,0 +1,35 @@
+// @flow
+import { makeContext, getInitialRobotStateStandard } from './fixtures'
+import { disengageMagnet } from '../commandCreators/atomic/disengageMagnet'
+
+const moduleId = 'magneticModuleId'
+describe('engageMagnet', () => {
+  let invariantContext
+  let robotState
+
+  beforeEach(() => {
+    invariantContext = makeContext()
+    invariantContext.moduleEntities[moduleId] = {
+      id: moduleId,
+      type: 'magdeck',
+      model: 'GEN1',
+    }
+    robotState = getInitialRobotStateStandard(invariantContext)
+    robotState.modules[moduleId] = {
+      slot: '4',
+      moduleState: { type: 'magdeck', engaged: false },
+    }
+  })
+  test('creates engage magnet command', () => {
+    const module = moduleId
+    const result = disengageMagnet({ module }, invariantContext, robotState)
+    expect(result).toEqual({
+      commands: [
+        {
+          command: 'magneticModule/disengageMagnet',
+          params: { module },
+        },
+      ],
+    })
+  })
+})
