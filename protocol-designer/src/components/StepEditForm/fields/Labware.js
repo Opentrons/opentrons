3bdf1d523d55525db59492d2b7a77293a61f35d1
Kadee80 <kaa328@nyu.edu>
2019-12-04 09:21:16 -0500

    feat(protocol-designer): Add set temperature form
    closes #4307

--- protocol-designer/src/components/StepEditForm/fields/Labware.js
+++ protocol-designer/src/components/StepEditForm/fields/Labware.js
@@ -1,14 +1,13 @@
 // @flow
-import * as React from 'react'
 import { connect } from 'react-redux'
-import { DropdownField, type Options } from '@opentrons/components'
-import cx from 'classnames'
+
 import { selectors as uiLabwareSelectors } from '../../../ui/labware'
+import StepFormDropdown from './StepFormDropdown'
+import type { Options } from '@opentrons/components'
 import type { StepFieldName } from '../../../steplist/fieldLevel'
 import type { BaseState } from '../../../types'
 import type { FocusHandlers } from '../types'
-import styles from '../StepEditForm.css'
-import StepField from './FieldConnector'
+import type { StepFormDropdownProps } from './StepFormDropdown'
 
 type OP = {|
   ...$Exact<FocusHandlers>,
@@ -16,58 +15,12 @@ type OP = {|
   className?: string,
 |}
 
-type SP = {| labwareOptions: Options |}
+type SP = {| options: Options |}
 
-type Props = { ...OP, ...SP }
-
-const LabwareFieldSTP = (state: BaseState): SP => ({
-  labwareOptions: uiLabwareSelectors.getLabwareOptions(state),
+const mapSTP = (state: BaseState): SP => ({
+  options: uiLabwareSelectors.getLabwareOptions(state),
 })
 
-const LabwareField = connect<Props, OP, SP, _, _, _>(LabwareFieldSTP)(
-  (props: Props) => {
-    const {
-      labwareOptions,
-      name,
-      className,
-      focusedField,
-      dirtyFields,
-      onFieldBlur,
-      onFieldFocus,
-    } = props
-    return (
-      // TODO: BC abstract e.currentTarget.value inside onChange with fn like onChangeValue of type (value: mixed) => {}
-      <StepField
-        name={name}
-        focusedField={focusedField}
-        dirtyFields={dirtyFields}
-        render={({ value, updateValue, errorToShow }) => {
-          // blank out the dropdown if labware id does not exist
-          const availableLabwareIds = labwareOptions.map(opt => opt.value)
-          const fieldValue = availableLabwareIds.includes(value)
-            ? String(value)
-            : null
-          return (
-            <DropdownField
-              error={errorToShow}
-              className={cx(styles.large_field, className)}
-              options={labwareOptions}
-              onBlur={() => {
-                onFieldBlur(name)
-              }}
-              onFocus={() => {
-                onFieldFocus(name)
-              }}
-              value={fieldValue}
-              onChange={(e: SyntheticEvent<HTMLSelectElement>) => {
-                updateValue(e.currentTarget.value)
-              }}
-            />
-          )
-        }}
-      />
-    )
-  }
+export default connect<StepFormDropdownProps, OP, SP, _, _, _>(mapSTP)(
+  StepFormDropdown
 )
-
-export default LabwareField
