3f3b68978278801a398495a73dc3d7e7a687358f
Brian Cooper <brian@allegro.com>
2020-01-06 15:33:50 -0500

    unlint

--- api/src/opentrons/api/calibration.py
+++ api/src/opentrons/api/calibration.py
@@ -133,10 +133,10 @@ class CalibrationManager:
                 loc = _well0(container._container)
                 instrument._context.location_cache =\
                     Location(self._hardware.gantry_position(
-                        Mount[inst.mount.upper()],
-                        critical_point=CriticalPoint.NOZZLE,
-                        refresh=True),
-                        loc)
+                                Mount[inst.mount.upper()],
+                                critical_point=CriticalPoint.NOZZLE,
+                                refresh=True),
+                             loc)
                 loc_leg = _well0(container._container)
                 inst.pick_up_tip(loc_leg)
         else:
@@ -281,7 +281,7 @@ class CalibrationManager:
             # relative to the old calibration
             container._container.set_calibration(Point(0, 0, 0))
             if ff.calibrate_to_bottom() and not (
-                    container._container.is_tiprack):
+                                            container._container.is_tiprack):
                 orig = _well0(container._container)._bottom().point
             else:
                 orig = _well0(container._container)._top().point
@@ -305,3 +305,4 @@ class CalibrationManager:
         self._hardware._use_safest_height = (self.state in
                                              ['probing', 'moving'])
         self._broker.publish(CalibrationManager.TOPIC, self._snapshot())
+
