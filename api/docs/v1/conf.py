11ef254208b9fc238b7893f7f2bd644e11f4e13a
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2020-01-07 17:54:49 -0500

    docs(api): add favicon to v1 and v2 docs

--- api/docs/v1/conf.py
+++ api/docs/v1/conf.py
@@ -181,8 +181,8 @@ html_logo = '../img/logo.png'
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or
 # 32x32 pixels large.
 #
-# html_favicon = None
-
+html_favicon = '../img/OTfavicon.ico'
+# 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
