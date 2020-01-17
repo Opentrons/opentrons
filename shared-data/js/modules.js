45f140210fcf3ab794effa02366d08050b67477e
Seth Foster <seth@opentrons.com>
2020-01-17 13:34:14 -0500

    feat(api): add module def schema v2 and v2 defs
    This lets us specify gen2 modules along with equivalencies between gen2 and gen1
    of the same model, as well as transforms on a per-slot basis.
    
    Also adds gen2 versions of the temperature and magnetic modules.
    
    Closes #4222

--- shared-data/js/modules.js
+++ shared-data/js/modules.js
@@ -1,6 +1,6 @@
 // @flow
 import type { ModuleType } from './types'
-import moduleSpecs from '../module/definitions/1.json'
+import moduleSpecs from '../module/definitions/2.json'
 
 // use a name like 'magdeck' to get displayName for app
 export function getModuleDisplayName(name: ModuleType): string {
