2ca3958f683eb77985e23a2aaf1c3c237ff6a86e
Ian Solano-Kamaiko <2371418+iansolano@users.noreply.github.com>
2020-01-21 16:22:38 -0500

    style(app): change default exports to named exports
    Change default exports to named exports in app and app-shell

--- app-shell/src/config.js
+++ app-shell/src/config.js
@@ -99,7 +99,7 @@ let _over
 let _log
 const store = () => _store || (_store = new Store({ defaults: DEFAULTS }))
 const overrides = () => _over || (_over = yargsParser(argv, PARSE_ARGS_OPTS))
-const log = () => _log || (_log = createLogger('config'))
+const log = () => _log || (_log = initializeLogger('config'))
 
 // initialize and register the config module with dispatches from the UI
 export function registerConfig(dispatch: Dispatch) {
