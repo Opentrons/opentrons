0fa93a17e975cffd3929cbaecc3f90a2a41da2d1
Ian Solano-Kamaiko <2371418+iansolano@users.noreply.github.com>
2020-01-23 15:21:35 -0500

    style(protocol-designer): change default exports to named exports
    Refactor default exports to named exports to fix linter warnings
    
    #4830

--- protocol-designer/src/containers/ConnectedMainPanel.js
+++ protocol-designer/src/containers/ConnectedMainPanel.js
@@ -25,7 +25,7 @@ type Props = {
   ingredSelectionMode: boolean,
 }
 
-function MainPanelComponent(props: Props) {
+function MainPanel(props: Props) {
   const { page, selectedTerminalItemId, ingredSelectionMode } = props
   switch (page) {
     case 'file-splash':
@@ -68,4 +68,4 @@ function mapStateToProps(state: BaseState): $Exact<Props> {
 
 export const ConnectedMainPanel = connect<Props, {||}, _, _, _, _>(
   mapStateToProps
-)(MainPanelComponent)
+)(MainPanel)
