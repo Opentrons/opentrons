dfe9bdac1a2b218908a05a767f44e58c3a8f9a95
jen-fong <jenny@opentrons.com>
2020-01-29 15:03:25 -0500

    feat(protocol-designer): add labware hover highlighting to modules (#4843)
    * feat(protocol-designer): add labware hover highlighting to magnet and temperature modules
    
    closes #4696
    
    * return labwares instead of module id from highlight selector
    
    * add tests for getHoveredStepLabware selector
    
    * remove unnecessary errors key

---
+++ protocol-designer/src/ui/steps/test/selectors.test.js
@@ -0,0 +1,169 @@
+// @flow
+import selectors from '../selectors'
+import * as utils from '../../modules/utils'
+
+function createArgsForStepId(stepId, stepArgs) {
+  return {
+    [stepId]: {
+      stepArgs,
+    },
+  }
+}
+
+const hoveredStepId = 'hoveredStepId'
+const labware = 'well plate'
+const mixCommand = 'mix'
+describe('getHoveredStepLabware', () => {
+  let initialDeckState
+  beforeEach(() => {
+    initialDeckState = {
+      labware: {},
+      pipettes: {},
+      modules: {},
+    }
+  })
+
+  test('no labware is returned when no hovered step', () => {
+    const stepArgs = {
+      commandCreatorFnName: mixCommand,
+      labware,
+    }
+    const argsByStepId = createArgsForStepId(hoveredStepId, stepArgs)
+    const hoveredStep = null
+
+    const result = selectors.getHoveredStepLabware.resultFunc(
+      argsByStepId,
+      hoveredStep,
+      initialDeckState
+    )
+
+    expect(result).toEqual([])
+  })
+
+  test('no labware is returned when step is not found', () => {
+    const stepArgs = {
+      commandCreatorFnName: mixCommand,
+      labware,
+    }
+    const argsByStepId = createArgsForStepId(hoveredStepId, stepArgs)
+    const hoveredStep = 'another-step'
+
+    const result = selectors.getHoveredStepLabware.resultFunc(
+      argsByStepId,
+      hoveredStep,
+      initialDeckState
+    )
+
+    expect(result).toEqual([])
+  })
+
+  test('no labware is returned when no step arguments', () => {
+    const stepArgs = null
+    const argsByStepId = createArgsForStepId(hoveredStepId, stepArgs)
+
+    const result = selectors.getHoveredStepLabware.resultFunc(
+      argsByStepId,
+      hoveredStepId,
+      initialDeckState
+    )
+
+    expect(result).toEqual([])
+  })
+  ;['consolidate', 'distribute', 'transfer'].forEach(command => {
+    test(`source and destination labware is returned when ${command}`, () => {
+      const sourceLabware = 'test tube'
+      const stepArgs = {
+        commandCreatorFnName: command,
+        destLabware: labware,
+        sourceLabware,
+      }
+      const argsByStepId = createArgsForStepId(hoveredStepId, stepArgs)
+
+      const result = selectors.getHoveredStepLabware.resultFunc(
+        argsByStepId,
+        hoveredStepId,
+        initialDeckState
+      )
+
+      expect(result).toEqual([sourceLabware, labware])
+    })
+  })
+
+  test('labware is returned when command is mix', () => {
+    const stepArgs = {
+      commandCreatorFnName: mixCommand,
+      labware,
+    }
+    const argsByStepId = createArgsForStepId(hoveredStepId, stepArgs)
+
+    const result = selectors.getHoveredStepLabware.resultFunc(
+      argsByStepId,
+      hoveredStepId,
+      initialDeckState
+    )
+
+    expect(result).toEqual([labware])
+  })
+
+  describe('modules', () => {
+    const type = 'tempdeck'
+    const setTempCommand = 'setTemperature'
+    beforeEach(() => {
+      initialDeckState = {
+        labware: {
+          def098: {
+            slot: type,
+          },
+        },
+        pipettes: {},
+        modules: {
+          abc123: {
+            id: 'abc123',
+            type,
+            model: 'GEN1',
+            slot: '1',
+            moduleState: {
+              type,
+              status: 'TEMPERATURE_DEACTIVATED',
+              targetTemperature: null,
+            },
+          },
+        },
+      }
+    })
+
+    test('labware on module is returned when module id exists', () => {
+      utils.getLabwareOnModule = jest.fn().mockReturnValue({ id: labware })
+      const stepArgs = {
+        commandCreatorFnName: setTempCommand,
+        module: type,
+      }
+      const argsByStepId = createArgsForStepId(hoveredStepId, stepArgs)
+
+      const result = selectors.getHoveredStepLabware.resultFunc(
+        argsByStepId,
+        hoveredStepId,
+        initialDeckState
+      )
+
+      expect(result).toEqual([labware])
+    })
+
+    test('no labware is returned when no labware on module', () => {
+      utils.getLabwareOnModule = jest.fn().mockReturnValue(null)
+      const stepArgs = {
+        commandCreatorFnName: setTempCommand,
+        module: type,
+      }
+      const argsByStepId = createArgsForStepId(hoveredStepId, stepArgs)
+
+      const result = selectors.getHoveredStepLabware.resultFunc(
+        argsByStepId,
+        hoveredStepId,
+        initialDeckState
+      )
+
+      expect(result).toEqual([])
+    })
+  })
+})
