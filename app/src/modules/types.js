f220fead698cbb24843d93bc0991895c37b53b6c
Brian Cooper <brian@allegro.com>
2020-02-03 12:16:17 -0500

    add epic update module boilerplate

--- app/src/modules/types.js
+++ app/src/modules/types.js
@@ -152,6 +152,38 @@ export type SendModuleCommandDoneAction =
   | SendModuleCommandSuccessAction
   | SendModuleCommandFailureAction
 
+// fetch modules
+
+export type UpdateModuleAction = {|
+  type: 'modules:UPDATE_MODULE',
+  payload: {| robotName: string, moduleId: string |},
+  meta: RobotApiRequestMeta,
+|}
+
+export type UpdateModuleSuccessAction = {|
+  type: 'modules:UPDATE_MODULE_SUCCESS',
+  payload: {|
+    robotName: string,
+    moduleId: string,
+    message: string,
+  |},
+  meta: RobotApiRequestMeta,
+|}
+
+export type UpdateModuleFailureAction = {|
+  type: 'modules:UPDATE_MODULE_FAILURE',
+  payload: {|
+    robotName: string,
+    moduleId: string,
+    error: {},
+  |},
+  meta: RobotApiRequestMeta,
+|}
+
+export type UpdateModuleDoneAction =
+  | UpdateModuleSuccessAction
+  | UpdateModuleFailureAction
+
 // action union
 
 export type ModulesAction =
@@ -161,6 +193,9 @@ export type ModulesAction =
   | SendModuleCommandAction
   | SendModuleCommandSuccessAction
   | SendModuleCommandFailureAction
+  | UpdateModuleAction
+  | UpdateModuleSuccessAction
+  | UpdateModuleFailureAction
 
 // state types
 
