8e097fa85d73f3dae2fe8e046eb7a45cf867ebe5
Seth Foster <seth@opentrons.com>
2020-02-10 14:57:19 -0500

    fixup: fix comment, use set instead of pytest

--- api/src/opentrons/hardware_control/modules/tempdeck.py
+++ api/src/opentrons/hardware_control/modules/tempdeck.py
@@ -231,11 +231,11 @@ class TempDeck(mod_abc.AbstractModule):
 
     def has_available_update(self) -> bool:
         """ Override of abc implementation to suppress update notifications
-        for v1 and v1.1 temperature modules which cannot be updated """
+        for v1, v1.1, and v2 temperature modules which cannot be updated """
         if not self._device_info:
             model = None
         else:
             model = self._device_info.get('model')
-        if model in ('temp_deck_v1', 'temp_deck_v1.1', 'temp_deck_v2', None):
+        if model in {'temp_deck_v1', 'temp_deck_v1.1', 'temp_deck_v2', None}:
             return False
         return super().has_available_update()
