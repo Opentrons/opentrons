cba9489aa9d0d67c4ad234a5eb95678977ffce1f
Seth Foster <seth@opentrons.com>
2020-02-11 09:57:36 -0500

    fixups: bad tests

--- shared-data/js/__tests__/moduleAccessors.test.js
+++ shared-data/js/__tests__/moduleAccessors.test.js
@@ -8,12 +8,18 @@ import {
   getModuleDisplayName,
 } from '../modules'
 
-const v2DefinitionsGlobPath = path.join('../../module/definitions/2/*.json')
+const v2DefinitionsGlobPath = path.join(
+  __dirname,
+  '../../module/definitions/2/*.json'
+)
 
 describe('all valid models work', () => {
-  const validDefs = glob.sync(v2DefinitionsGlobPath)
-  validDefs.forEach(defPath => {
-    const model = path.parse(defPath).base
+  const validDefPaths = glob.sync(v2DefinitionsGlobPath)
+  test('got at least 1 module def (avoid false positive for tests)', () => {
+    expect(validDefPaths.length).toBeGreaterThan(0)
+  })
+  validDefPaths.forEach(defPath => {
+    const model = path.parse(defPath).name
     const loadedDef = getModuleDef2(model)
     test('ensure valid models load', () => {
       expect(loadedDef).not.toBeNull()
@@ -21,10 +27,10 @@ describe('all valid models work', () => {
       expect(loadedDef.model).toEqual(model)
     })
     test('valid models have module types', () => {
-      expect(getModuleTypeFromModuleModel(model)).toMatch('*')
+      expect(getModuleTypeFromModuleModel(model)).toMatch(/.*/)
     })
     test('valid modules have display names that arent error', () => {
-      expect(getModuleDisplayName(model)).toMatch('*')
+      expect(getModuleDisplayName(model)).toMatch(/.*/)
     })
   })
 })
--- shared-data/js/__tests__/moduleSpecsSchema.test.js
+++ shared-data/js/__tests__/moduleSpecsSchema.test.js
@@ -13,7 +13,10 @@ const ajv = new Ajv({
 const validateModuleSpecsV1 = ajv.compile(moduleSpecsSchemaV1)
 const validateModuleSpecsV2 = ajv.compile(moduleSpecsSchemaV2)
 
-const v2DefinitionsGlobPath = path.join('../../module/definitions/2/*.json')
+const v2DefinitionsGlobPath = path.join(
+  __dirname,
+  '../../module/definitions/2/*.json'
+)
 
 describe('validate all module specs with schema', () => {
   test('ensure V1 module specs match the V1 JSON schema', () => {
@@ -26,26 +29,27 @@ describe('validate all module specs with schema', () => {
     expect(validationErrors).toBe(null)
     expect(valid).toBe(true)
   })
-  test('ensure V2 module specs match the V2 JSON schema', () => {
-    const modulePaths = glob.sync(v2DefinitionsGlobPath)
-    modulePaths.forEach(modulePath => {
-      const filename = path.parse(modulePath).base
-      const moduleDef = require(modulePath)
-      test(`${filename} validates against schema`, () => {
-        const valid = validateModuleSpecsV2(moduleDef)
-        const validationErrors = validateModuleSpecsV2.errors
-        if (validationErrors) {
-          console.log(JSON.stringify(validationErrors, null, 4))
-        }
-        expect(validationErrors).toBe(null)
-        expect(valid).toBe(true)
-      })
+  const modulePaths = glob.sync(v2DefinitionsGlobPath)
+  test('got at least 1 v2 module def (avoid false positive for tests)', () => {
+    expect(modulePaths.length).toBeGreaterThan(0)
+  })
+  modulePaths.forEach(modulePath => {
+    const filename = path.parse(modulePath).name
+    const moduleDef = require(modulePath)
+    test(`${filename} validates against schema`, () => {
+      const valid = validateModuleSpecsV2(moduleDef)
+      const validationErrors = validateModuleSpecsV2.errors
+      if (validationErrors) {
+        console.log(JSON.stringify(validationErrors, null, 4))
+      }
+      expect(validationErrors).toBe(null)
+      expect(valid).toBe(true)
     })
   })
   test('validate each module specs model matches its filename', () => {
-    const modulePaths = glob.sync(v2DefinitionsGlobPath)
     modulePaths.forEach(modulePath => {
-      const filename = path.parse(modulePath).base
+      console.log(`full path is ${modulePath}`)
+      const filename = path.parse(modulePath).name
       const moduleDef = require(modulePath)
       expect(moduleDef.model).toEqual(filename)
     })
