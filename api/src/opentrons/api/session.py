004ead43b17c33ffce0ffe21be147a56c7ef9623
Brian Cooper <brian@allegro.com>
2020-02-12 11:25:25 -0500

    remove commented out del from session

--- api/src/opentrons/api/session.py
+++ api/src/opentrons/api/session.py
@@ -57,11 +57,6 @@ class SessionManager(object):
         self._broker.set_logger(self._command_logger)
         self._motion_lock = lock
 
-    # def __del__(self):
-        # if isinstance(getattr(self, '_hardware', None),
-        #               adapters.SynchronousAdapter):
-        #     self._hardware.join()
-
     def create(
             self,
             name: str,
