26dba90b0a3d56e6679562e7d2d9177ed8533e71
Seth Foster <seth@opentrons.com>
2020-02-20 10:49:58 -0500

    allow legacy models to fix callsites

--- shared-data/js/__tests__/moduleAccessors.test.js
+++ shared-data/js/__tests__/moduleAccessors.test.js
@@ -6,7 +6,16 @@ import {
   getModuleDisplayName,
 } from '../modules'
 
-import { MODULE_MODELS, MODULE_TYPES } from '../constants'
+import {
+  MODULE_MODELS,
+  MODULE_TYPES,
+  TEMPDECK,
+  MAGDECK,
+  THERMOCYCLER,
+  MAGNETIC_MODULE_V1,
+  TEMPERATURE_MODULE_V1,
+  THERMOCYCLER_MODULE_V1,
+} from '../constants'
 
 describe('all valid models work', () => {
   MODULE_MODELS.forEach(model => {
@@ -22,5 +31,17 @@ describe('all valid models work', () => {
     test('valid modules have display names that match the def', () => {
       expect(getModuleDisplayName(model)).toEqual(loadedDef.displayName)
     })
+  })
+})
+
+describe('legacy models work too', () => {
+  const legacyEquivs = [
+    [TEMPDECK, TEMPERATURE_MODULE_V1],
+    [MAGDECK, MAGNETIC_MODULE_V1],
+    [THERMOCYCLER, THERMOCYCLER_MODULE_V1],
+  ]
+  legacyEquivs.forEach(([legacy, modern]) => {
+    const fromLegacy = getModuleDef2(legacy)
+    expect(fromLegacy?.model).toEqual(modern)
   })
 })
--- shared-data/js/modules.js
+++ shared-data/js/modules.js
@@ -6,10 +6,13 @@ import temperatureModuleV2 from '../module/definitions/2/temperatureModuleV2.jso
 import thermocyclerModuleV1 from '../module/definitions/2/thermocyclerModuleV1.json'
 
 import {
+  MAGDECK,
   MAGNETIC_MODULE_V1,
   MAGNETIC_MODULE_V2,
+  TEMPDECK,
   TEMPERATURE_MODULE_V1,
   TEMPERATURE_MODULE_V2,
+  THERMOCYCLER,
   THERMOCYCLER_MODULE_V1,
 } from './constants'
 
@@ -47,15 +50,18 @@ export type ModuleDef2 = {|
 // TODO IMMEDIATELY: Phase out code that uses legacy models
 export const getModuleDef2 = (moduleModel: ModuleModel): ModuleDef2 => {
   switch (moduleModel) {
-    case MAGNETIC_MODULE_V1:
+    case MAGDECK: // TODO IMMEDIATE: Remove when callsites use only new models
+    case MAGNETIC_MODULE_V1: // fallthrough: legacy model
       return magneticModuleV1
     case MAGNETIC_MODULE_V2:
       return magneticModuleV2
-    case TEMPERATURE_MODULE_V1:
+    case TEMPDECK: // TODO IMMEDIATE: Remove when callsites use only new models
+    case TEMPERATURE_MODULE_V1: // fallthrough: legacy model
       return temperatureModuleV1
     case TEMPERATURE_MODULE_V2:
       return temperatureModuleV2
-    case THERMOCYCLER_MODULE_V1:
+    case THERMOCYCLER: // TODO IMMEDIATE: Remove when callsites use only new models
+    case THERMOCYCLER_MODULE_V1: // fallthrough: legacy model
       return thermocyclerModuleV1
     default:
       throw new Error(`Invalid module model ${moduleModel}`)
