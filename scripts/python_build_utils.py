4dc770ed74ad5176d796cd6bc0aa409f9c2c7a00
amit lissack <amit@opentrons.com>
2020-02-20 14:45:23 -0500

    to support the process of splitting up api into two projects add opentrons to python_build_utils.

--- scripts/python_build_utils.py
+++ scripts/python_build_utils.py
@@ -8,26 +8,30 @@ import argparse
 import json
 import os
 import subprocess
+from collections import namedtuple
+
+
+PackageEntry = namedtuple("PackageEntry", ("pkg_json", "br_version_prefix"))
 
 
 HERE = os.path.dirname(__file__)
 
-pkg_jsons = {
-    'api': os.path.join(
-        HERE, '..', 'api', 'src', 'opentrons', 'package.json'),
-    'update-server': os.path.join(
-        HERE, '..', 'update-server', 'otupdate', 'package.json')
-}
 
-
-br_version_prefixes = {
-    'api': 'opentrons_api',
-    'update-server': 'update_server'
+package_entries = {
+    'api': PackageEntry(
+        os.path.join(HERE, '..', 'api', 'src', 'opentrons', 'package.json'),
+        'opentrons_api'),
+    'update-server': PackageEntry(
+        os.path.join(HERE, '..', 'update-server', 'otupdate', 'package.json'),
+        'update_server'),
+    'opentrons': PackageEntry(
+        os.path.join(HERE, '..', 'pyopentrons', 'src', 'opentrons', 'package.json'),
+        'opentrons')
 }
 
 
 def get_version(project):
-    pkg_json_path = pkg_jsons[project]
+    pkg_json_path = package_entries[project].pkg_json
     return json.load(open(pkg_json_path))['version']
 
 
@@ -88,7 +92,7 @@ def dump_br_version(project):
     sha = subprocess.check_output(
         ['git', 'rev-parse', 'HEAD'], cwd=HERE).strip().decode()
     branch = _ref_from_sha(sha)
-    pref = br_version_prefixes[project]
+    pref = package_entries[project].br_version_prefix
     return json.dumps({pref+'_version': normalized,
                        pref+'_sha': sha,
                        pref+'_branch': branch})
@@ -98,7 +102,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Perform one of several build-time tasks')
     parser.add_argument(dest='project', metavar='SUBPROJECT', type=str,
-                        choices=['api', 'update-server'])
+                        choices=package_entries.keys())
     parser.add_argument(dest='task', metavar='TASK', type=str,
                         choices=['normalize_version', 'dump_br_version'])
     args = parser.parse_args()
