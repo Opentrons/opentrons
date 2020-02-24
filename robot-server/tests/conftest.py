2b24a4f42a025b492f183d1a364b8ea93d95cfe7
amit lissack <amit@opentrons.com>
2020-02-24 17:49:29 -0500

    Remove global session from deck_calibration. Create a SessionWrapper that allows access to the single SessionManager instance. This allows cleaning up session when creating a new one.

--- robot-server/tests/conftest.py
+++ robot-server/tests/conftest.py
@@ -224,9 +224,9 @@ async def dc_session(request, async_server, monkeypatch, loop):
         types.Mount.LEFT: None,
         types.Mount.RIGHT: 'p300_multi_v1'})
     ses = endpoints.SessionManager(hw)
-    endpoints.session = ses
-    monkeypatch.setattr(endpoints, 'session', ses)
+    endpoints.session_wrapper.session = ses
     yield ses
+    endpoints.session_wrapper.session = None
 
 
 @pytest.fixture(params=["dinosaur.py"])
@@ -365,6 +365,8 @@ def main_router(loop, virtual_smoothie_env, hardware):
         notifications=router.notifications,
         loop=loop)
     yield router
+
+    router.session_manager._hardware.join()
 
 
 async def wait_until(matcher, notifications, timeout=1, loop=None):
