d705d6849c9f9966fe2384751d7976b54cc76f50
amit lissack <amit@opentrons.com>
2020-02-25 10:46:09 -0500

    buildroot changes for robot-server. service file modified to launch correct entry.

--- robot-server/buildroot.mk
+++ robot-server/buildroot.mk
@@ -1,45 +1,42 @@
 ################################################################################
 #
-# python-opentrons-api
+# python-opentrons-robot-server
 #
 ################################################################################
 
 # Get a key from package.json (like version)
 define get_pkg_json_key
-	$(shell python -c "import json; print(json.load(open('$(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/api/src/opentrons/package.json'))[\"$(1)\"])")
+	$(shell python -c "import json; print(json.load(open('$(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/robot-server/robot_server/package.json'))[\"$(1)\"])")
 endef
 
-PYTHON_OPENTRONS_API_VERSION = $(call get_pkg_json_key,version)
-PYTHON_OPENTRONS_API_LICENSE = $(call get_pkg_json_key,license)
-PYTHON_OPENTRONS_API_LICENSE_FILES = $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/LICENSE
-PYTHON_OPENTRONS_API_SETUP_TYPE = setuptools
-PYTHON_OPENTRONS_API_SITE_METHOD = local
-PYTHON_OPENTRONS_API_SITE = $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)
-PYTHON_OPENTRONS_API_SUBDIR = api
-PYTHON_OPENTRONS_API_POST_INSTALL_TARGET_HOOKS = PYTHON_OPENTRONS_API_INSTALL_VERSION PYTHON_OPENTRONS_API_INSTALL_RELEASE_NOTES
+PYTHON_OPENTRONS_ROBOT_SERVER_VERSION = $(call get_pkg_json_key,version)
+PYTHON_OPENTRONS_ROBOT_SERVER_LICENSE = $(call get_pkg_json_key,license)
+PYTHON_OPENTRONS_ROBOT_SERVER_LICENSE_FILES = $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/LICENSE
+PYTHON_OPENTRONS_ROBOT_SERVER_SETUP_TYPE = setuptools
+PYTHON_OPENTRONS_ROBOT_SERVER_SITE_METHOD = local
+PYTHON_OPENTRONS_ROBOT_SERVER_SITE = $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)
+PYTHON_OPENTRONS_ROBOT_SERVER_SUBDIR = api
+PYTHON_OPENTRONS_ROBOT_SERVER_POST_INSTALL_TARGET_HOOKS = PYTHON_OPENTRONS_ROBOT_SERVER_INSTALL_VERSION
+PYTHON_OPENTRONS_ROBOT_SERVER_SERVICE_FILE_NAME=opentrons-robot-server.service
 
 define OTAPI_DUMP_BR_VERSION
 	$(shell python $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/scripts/python_build_utils.py api dump_br_version)
 endef
 
-define PYTHON_OPENTRONS_API_INSTALL_VERSION
+define PYTHON_OPENTRONS_ROBOT_SERVER_INSTALL_VERSION
 	echo '$(call OTAPI_DUMP_BR_VERSION)' > $(BINARIES_DIR)/opentrons-api-version.json
 endef
 
 ot_api_name := python-opentrons-api
 
-define PYTHON_OPENTRONS_API_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/api/opentrons-api-server.service \
-	  $(TARGET_DIR)/etc/systemd/system/opentrons-api-server.service
+define PYTHON_OPENTRONS_ROBOT_SERVER_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/robot-server/$(PYTHON_OPENTRONS_ROBOT_SERVER_SERVICE_FILE_NAME) \
+	  $(TARGET_DIR)/etc/systemd/system/$(PYTHON_OPENTRONS_ROBOT_SERVER_SERVICE_FILE_NAME)
 
   mkdir -p $(TARGET_DIR)/etc/systemd/system/opentrons.target.wants
 
-  ln -sf ../opentrons-api-server.service \
-    $(TARGET_DIR)/etc/systemd/system/opentrons.target.wants/opentrons-api-server.service
-endef
-
-define PYTHON_OPENTRONS_API_INSTALL_RELEASE_NOTES
-	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/api/release-notes.md $(BINARIES_DIR)/release-notes.md
+  ln -sf ../$(PYTHON_OPENTRONS_ROBOT_SERVER_SERVICE_FILE_NAME) \
+    $(TARGET_DIR)/etc/systemd/system/opentrons.target.wants/$(PYTHON_OPENTRONS_ROBOT_SERVER_SERVICE_FILE_NAME)
 endef
 
 # Calling inner-python-package directly instead of using python-package macro
