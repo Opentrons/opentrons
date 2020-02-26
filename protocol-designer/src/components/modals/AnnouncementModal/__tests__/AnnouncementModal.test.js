898a5f7258d64d38c410fc706b99e6962587bda2
Jenny <jenny@opentrons.com>
2020-02-26 11:49:41 -0500

    add tests

--- protocol-designer/src/components/modals/AnnouncementModal/__tests__/AnnouncementModal.test.js
+++ protocol-designer/src/components/modals/AnnouncementModal/__tests__/AnnouncementModal.test.js
@@ -2,36 +2,90 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
+import { Modal, OutlineButton } from '@opentrons/components'
 import * as persist from '../../../../persist'
-import * as announcements from '../'
+import { AnnouncementModal, localStorageKey } from '../'
+import * as announcements from '../announcements'
+import type { Announcement } from '../announcements'
 
-jest.mock('../')
 jest.mock('../../../../persist.js')
 
 describe('AnnouncementModal', () => {
-  // let AnnouncementModal
-  beforeEach(() => {
-    // persist.getLocalStorageItem = jest.fn()
-    // persist.setLocalStorageItem = jest.fn()
-  })
+  const appVersion = '1.0.0'
+  const getLocalStorageItemMock: JestMockFn<[string], mixed> =
+    persist.getLocalStorageItem
+
+  const announcementsMock: {
+    announcements: Array<Announcement>,
+  } = announcements
 
   test('modal is not shown when announcement has been shown before', () => {
-    // const { AnnouncementModal } = jest.requireActual('../')
-    // persist.getLocalStorageItem.mockReturnValue('1.0.0')
-    // announcements.getLatestAnnouncement = jest.fn().mockReturnValue({
-    //   message: 'a brand spanking new feature exists',
-    //   version: '1.0.0',
-    // })
-    const { AnnouncementModal } = jest.requireActual('../')
-    console.log(AnnouncementModal)
+    getLocalStorageItemMock.mockReturnValue(appVersion)
+    announcementsMock.announcements = [
+      {
+        message: 'test',
+        version: appVersion,
+      },
+    ]
+
     const wrapper = shallow(<AnnouncementModal />)
 
-    // console.log(wrapper.debug())
+    expect(wrapper.find(Modal)).toHaveLength(0)
   })
 
-  test('latest announcement is always shown even if user has not seen all of them', () => {})
+  test('announcement is shown when user has not seen it before', () => {
+    getLocalStorageItemMock.mockReturnValue(appVersion)
+    announcementsMock.announcements = [
+      {
+        message: 'brand new spanking feature',
+        version: '1.1.0',
+      },
+    ]
 
-  test('announcement is shown when user has not seen it before', () => {})
+    const wrapper = shallow(<AnnouncementModal />)
+    const modal = wrapper.find(Modal)
 
-  test('button click saves announcement version to localStorage and closes modal', () => {})
+    expect(modal).toHaveLength(1)
+    expect(modal.html()).toContain('brand new spanking feature')
+  })
+
+  test('latest announcement is always shown', () => {
+    getLocalStorageItemMock.mockReturnValue(appVersion)
+    announcementsMock.announcements = [
+      {
+        message: 'first announcement',
+        version: appVersion,
+      },
+      {
+        message: 'second announcement',
+        version: '1.1.0',
+      },
+    ]
+
+    const wrapper = shallow(<AnnouncementModal />)
+    const modal = wrapper.find(Modal)
+
+    expect(modal).toHaveLength(1)
+    expect(modal.html()).toContain('second announcement')
+  })
+
+  test('button click saves announcement version to localStorage and closes modal', () => {
+    getLocalStorageItemMock.mockReturnValue(appVersion)
+    announcementsMock.announcements = [
+      {
+        message: 'brand new spanking feature',
+        version: '1.1.0',
+      },
+    ]
+
+    const wrapper = shallow(<AnnouncementModal />)
+    const button = wrapper.find(OutlineButton)
+    button.simulate('click')
+
+    expect(persist.setLocalStorageItem).toHaveBeenCalledWith(
+      localStorageKey,
+      '1.1.0'
+    )
+    expect(wrapper.find(Modal)).toHaveLength(0)
+  })
 })
