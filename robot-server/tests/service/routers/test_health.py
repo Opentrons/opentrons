a2eb7cddfb20d9eddaa25da6975a441aed7f2138
amit lissack <amit@opentrons.com>
2020-03-04 10:17:27 -0500

    port aiohttp health endpoint unittests to fastapi

---
+++ robot-server/tests/service/routers/test_health.py
@@ -0,0 +1,23 @@
+from opentrons import __version__
+from opentrons.protocol_api import MAX_SUPPORTED_VERSION
+
+
+def test_health(api_client):
+
+    expected = {
+        'name': 'opentrons-dev',
+        'api_version': __version__,
+        'fw_version': 'Virtual Smoothie',
+        'logs': ['/logs/serial.log', '/logs/api.log'],
+        'system_version': '0.0.0',
+        'protocol_api_version': list(MAX_SUPPORTED_VERSION),
+        "links": {
+            "apiLog": "/logs/api.log",
+            "serialLog": "/logs/serial.log",
+            "apiSpec": "/openapi.json"
+        }
+    }
+    resp = api_client.get('/health')
+    text = resp.json()
+    assert resp.status_code == 200
+    assert text == expected
