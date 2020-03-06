ce151bc844ebc4691e397098b2ed1af2e62a248d
Brian Cooper <brian@allegro.com>
2020-03-06 15:00:40 -0500

    pipette actions wait for is running

--- api/src/opentrons/hardware_control/api.py
+++ api/src/opentrons/hardware_control/api.py
@@ -484,6 +484,7 @@ class API(HardwareAPILike):
         :param axes: A list of axes to home. Default is `None`, which will
                      home everything.
         """
+        await self._execution_manager.wait_for_is_running()
         # Initialize/update current_position
         checked_axes = axes or [ax for ax in Axis]
         gantry = [ax for ax in checked_axes if ax in Axis.gantry_axes()]
@@ -766,6 +767,7 @@ class API(HardwareAPILike):
         at most one of a ZA or BC components. The frame in which to move
         is identified by the presence of (ZA) or (BC).
         """
+        await self._execution_manager.wait_for_is_running()
         # Transform only the x, y, and (z or a) axes specified since this could
         # get the b or c axes as well
         to_transform = tuple((tp
@@ -847,6 +849,7 @@ class API(HardwareAPILike):
 
         Works regardless of critical point or home status.
         """
+        await self._execution_manager.wait_for_is_running()
         smoothie_ax = Axis.by_mount(mount).name.upper()
         async with self._motion_lock:
             smoothie_pos = self._backend.fast_home(smoothie_ax, margin)
