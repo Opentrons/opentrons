769efc26df30bafe2d24ca4777fd17e3d455566b
amitlissack <amit@opentrons.com>
2020-03-16 15:48:05 -0400

    refactor(robot-server, api): implement module endpoints in fastapi (#5229)
    * add hardware fixture
    
    * use mock hardware for health test
    
    * port module unit tests from aiohttp to fastapi
    
    * move module entpoints and modules out of control and into their own python files.
    
    * minor fixes in api
    
    * create find_matching_module function and use it.
    
    * use V1BasicResponse
    
    closes #5141

--- robot-server/tests/conftest.py
+++ robot-server/tests/conftest.py
@@ -1,16 +1,23 @@
+from unittest.mock import MagicMock
+
 import pytest
 from starlette.testclient import TestClient
 from robot_server.service.main import app
 from robot_server.service.dependencies import get_hardware
-from opentrons.hardware_control import ThreadManager, API, HardwareAPILike
-
-
-async def get_hardware_override() -> HardwareAPILike:
-    """Override for get_hardware dependency"""
-    return ThreadManager(API.build_hardware_simulator)
+from opentrons.hardware_control import API, HardwareAPILike
 
 
 @pytest.fixture
-def api_client() -> TestClient:
+def hardware():
+    return MagicMock(spec=API)
+
+
+@pytest.fixture
+def api_client(hardware) -> TestClient:
+
+    async def get_hardware_override() -> HardwareAPILike:
+        """Override for get_hardware dependency"""
+        return hardware
+
     app.dependency_overrides[get_hardware] = get_hardware_override
     return TestClient(app)
