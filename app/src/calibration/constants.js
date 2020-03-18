b197a5f2c3e2ee33344e6412c08defe22600ce23
Brian Cooper <brian@allegro.com>
2020-03-18 18:08:10 -0400

    build up start epic

--- app/src/calibration/constants.js
+++ app/src/calibration/constants.js
@@ -5,3 +5,6 @@ export const START_DECK_CHECK: 'calibration:START_DECK_CHECK' =
 
 export const START_DECK_CHECK_SUCCESS: 'calibration:START_DECK_CHECK_SUCCESS' =
   'calibration:START_DECK_CHECK_SUCCESS'
+
+export const DECK_CHECK_PATH: '/calibration/check/session' =
+  '/calibration/check/session'
