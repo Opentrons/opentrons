0f9aa6b19402d6d84897e8e8eff1112ce83fdf0a
Brian Cooper <brian@allegro.com>
2020-03-20 11:27:42 -0400

    modal bits

--- app/src/components/CheckDeck/index.js
+++ app/src/components/CheckDeck/index.js
@@ -2,19 +2,24 @@
 import * as React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { push } from 'connected-react-router'
-import { ModalPage } from '@opentrons/components'
+import { ModalPage, Icon } from '@opentrons/components'
 import type { State, Dispatch } from '../../types'
 import {
   fetchDeckCheckSession,
   endDeckCheckSession,
   getDeckCheckSession,
 } from '../../calibration'
+import styles from './styles.css'
 
 // import { createLogger } from '../../logger'
 
 // const log = createLogger(__filename)
 
 const DECK_CHECK_SUBTITLE = 'Check deck calibration'
+
+const END_DECK_CHECK_HEADER = 'Calibration check is complete'
+const END_DECK_CHECK_BODY =
+  "You have successfully checked the accuracy of this robot's calibration."
 
 type CheckDeckProps = {|
   parentUrl: string,
@@ -38,11 +43,16 @@ export function CheckDeck(props: CheckDeckProps) {
   return (
     <ModalPage
       titleBar={{
-        subtitle: DECK_CHECK_SUBTITLE,
+        title: DECK_CHECK_SUBTITLE,
         back: { onClick: exit },
       }}
+      contentsClassName={styles.modal_contents}
     >
-      <div>DID IT!</div>
+      <div className={styles.modal_header}>
+        <Icon name="check-circle" className={styles.status_icon} />
+        <h3>{END_DECK_CHECK_HEADER}</h3>
+      </div>
+      <p>{END_DECK_CHECK_BODY}</p>
     </ModalPage>
   )
 }
