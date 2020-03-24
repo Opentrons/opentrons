d34d640b2a3ee26e9d4d473a88d68614a3ca2fb6
Shlok Amin <shlokamin@me.com>
2020-03-24 10:47:57 -0400

    Revert "compute model name constant"
    This reverts commit 9cb494079505e20751fe1ffe6cfeaabdfc780184.

--- api/src/opentrons/protocol_api/module_geometry.py
+++ api/src/opentrons/protocol_api/module_geometry.py
@@ -9,7 +9,6 @@ by :py:mod:`.module_contexts`)
 
 from enum import Enum
 import functools
-import itertools
 import json
 import logging
 import re
@@ -516,12 +515,14 @@ def load_module(
 
 def resolve_module_model(module_name: str) -> ModuleModel:
     """ Turn any of the supported load names into module model names """
-    MODELS_BY_NAME = {
-        e.value: e for e in itertools.chain(
-            list(MagneticModuleModel),
-            list(TemperatureModuleModel),
-            list(ThermocyclerModuleModel))
-        }
+
+    model_map: Mapping[str, ModuleModel] = {
+        'magneticModuleV1': MagneticModuleModel.MAGNETIC_V1,
+        'magneticModuleV2': MagneticModuleModel.MAGNETIC_V2,
+        'temperatureModuleV1': TemperatureModuleModel.TEMPERATURE_V1,
+        'temperatureModuleV2': TemperatureModuleModel.TEMPERATURE_V2,
+        'thermocyclerModuleV1': ThermocyclerModuleModel.THERMOCYCLER_V1,
+    }
 
     alias_map: Mapping[str, ModuleModel] = {
         'magdeck': MagneticModuleModel.MAGNETIC_V1,
@@ -535,7 +536,7 @@ def resolve_module_model(module_name: str) -> ModuleModel:
     }
 
     lower_name = module_name.lower()
-    resolved_name = MODELS_BY_NAME.get(module_name, None) \
+    resolved_name = model_map.get(module_name, None) \
         or alias_map.get(lower_name, None)
     if not resolved_name:
         raise ValueError(
@@ -544,7 +545,7 @@ def resolve_module_model(module_name: str) -> ModuleModel:
             .join(alias_map.keys()) + '"\n' +
             'You can also refer to modules by their ' +
             'exact model: ''"' + '", "'
-            .join(MODELS_BY_NAME.keys()) + '"'
+            .join(model_map.keys()) + '"'
             )
     return resolved_name
 
