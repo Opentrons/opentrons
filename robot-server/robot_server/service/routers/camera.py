a4993460c61c97659a7bcae9d01f43217780cc48
amit lissack <amit@opentrons.com>
2020-03-24 17:06:02 -0400

    StreamingResponse accepts a background param which will be called when sending is done. This is the correct way to clean up captured image file. Not the daft way I was doing it.

--- robot-server/robot_server/service/routers/camera.py
+++ robot-server/robot_server/service/routers/camera.py
@@ -4,6 +4,7 @@ import tempfile
 from http import HTTPStatus
 
 from fastapi import APIRouter, HTTPException
+from starlette.background import BackgroundTask
 from starlette.responses import StreamingResponse
 from opentrons.system import camera
 
@@ -27,17 +28,24 @@ JPG = "image/jpg"
 async def post_picture_capture() -> StreamingResponse:
     """Take a picture"""
     filename = tempfile.mktemp(suffix=".jpg")
+
     try:
         await camera.take_picture(filename)
         log.info("Image taken at %s", filename)
-        return StreamingResponse(open(filename, 'rb'), media_type=JPG)
+        return StreamingResponse(open(filename, 'rb'),
+                                 media_type=JPG,
+                                 background=BackgroundTask(func=_cleanup,
+                                                           filename=filename))
     except camera.CameraException as e:
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
                             detail=str(e))
-    finally:
-        try:
-            if os.path.exists(filename):
-                log.info("Deleting image at %s", filename)
-                os.remove(filename)
-        except OSError:
-            pass
+
+
+def _cleanup(filename: str) -> None:
+    """Clean up after sending the response"""
+    try:
+        if os.path.exists(filename):
+            log.info("Deleting image at %s", filename)
+            os.remove(filename)
+    except OSError:
+        pass
