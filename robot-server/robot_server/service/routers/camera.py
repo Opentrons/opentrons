5e8da4fbdfdb1daaa30d3986400a33ed214288ed
amit lissack <amit@opentrons.com>
2020-03-24 15:12:12 -0400

    implement take picture endpoint with tests

--- robot-server/robot_server/service/routers/camera.py
+++ robot-server/robot_server/service/routers/camera.py
@@ -1,10 +1,18 @@
+import logging
+import os
+import tempfile
 from http import HTTPStatus
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from starlette.responses import StreamingResponse
+from opentrons.system import camera
 
+
+log = logging.getLogger(__name__)
 
 router = APIRouter()
+
+JPG = "image/jpg"
 
 
 @router.post("/camera/picture",
@@ -12,13 +20,24 @@ router = APIRouter()
                          "and return it",
              responses={
                  HTTPStatus.OK: {
-                     "content": {"image/png": {}},
+                     "content": {JPG: {}},
                      "description": "The image"
                  }
              })
 async def post_picture_capture() -> StreamingResponse:
-    return StreamingResponse(
-        content=iter([]),
-        status_code=HTTPStatus.OK,
-        media_type="image/png"
-    )
+    """Take a picture"""
+    filename = tempfile.mktemp(suffix=".jpg")
+    try:
+        await camera.take_picture(filename)
+        log.info("Image taken at %s", filename)
+        return StreamingResponse(open(filename, 'rb'), media_type=JPG)
+    except camera.CameraException as e:
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                            detail=str(e))
+    finally:
+        try:
+            if os.path.exists(filename):
+                log.info("Deleting image at %s", filename)
+                os.remove(filename)
+        except OSError:
+            pass
