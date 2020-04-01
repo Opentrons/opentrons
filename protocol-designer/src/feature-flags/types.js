a7a45add3fa9df08636790bf2748177467635366
Kadee80 <kaa328@nyu.edu>
2020-04-01 13:18:39 -0400

    feat(protocol-designer): Remove multi gen2 feature flag
    closes #5336

--- protocol-designer/src/feature-flags/types.js
+++ protocol-designer/src/feature-flags/types.js
@@ -8,18 +8,17 @@ export const DEPRECATED_FLAGS = [
   'OT_PD_SHOW_UPLOAD_CUSTOM_LABWARE_BUTTON',
   'OT_PD_ENABLE_GEN2_PIPETTES',
   'OT_PD_ENABLE_MODULES',
+  'OT_PD_ENABLE_MULTI_GEN2_PIPETTES',
 ]
 
 // union of feature flag string constant IDs
 export type FlagTypes =
   | 'PRERELEASE_MODE'
   | 'OT_PD_DISABLE_MODULE_RESTRICTIONS'
-  | 'OT_PD_ENABLE_MULTI_GEN2_PIPETTES'
   | 'OT_PD_ENABLE_THERMOCYCLER'
 
 // flags that are not in this list only show in prerelease mode
 export const userFacingFlags: Array<FlagTypes> = [
-  'OT_PD_ENABLE_MULTI_GEN2_PIPETTES',
   'OT_PD_DISABLE_MODULE_RESTRICTIONS',
 ]
 
