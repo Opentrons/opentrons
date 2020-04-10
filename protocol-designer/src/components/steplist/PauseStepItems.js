83ebab9e9e45e51750f769fd4ba009c73ebb1d54
Kadee80 <kaa328@nyu.edu>
2020-04-10 10:29:33 -0400

    fix(protocol-designer): Unify pause step item spacing
    closes #5387

--- protocol-designer/src/components/steplist/PauseStepItems.js
+++ protocol-designer/src/components/steplist/PauseStepItems.js
@@ -23,13 +23,13 @@ export function PauseStepItems(props: Props) {
             <span>Pause for Time</span>
           </li>
           <li className={styles.substep_content}>
-            <span>
+            <span className={styles.substep_time}>
               {hours} {i18n.t('application.units.hours')}
             </span>
-            <span>
+            <span className={styles.substep_time}>
               {minutes} {i18n.t('application.units.minutes')}
             </span>
-            <span>
+            <span className={styles.substep_time}>
               {seconds} {i18n.t('application.units.seconds')}
             </span>
           </li>
