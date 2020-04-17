62bd78f00cc6d4aaf0fc572dea2a7821224b63ff
Shlok Amin <shlokamin@me.com>
2020-04-17 17:12:03 -0400

    remove unused form hook

--- protocol-designer/src/components/modals/EditModulesModal/form-state.js
+++ protocol-designer/src/components/modals/EditModulesModal/form-state.js
@@ -1,11 +1,10 @@
 // @flow
 import { useEffect } from 'react'
-import { useField, useFormikContext } from 'formik'
+import { useFormikContext } from 'formik'
 import { isModuleWithCollisionIssue } from '../../modules/utils'
 import { usePrevious } from '@opentrons/components'
 
 export const useResetSlotOnModelChange = () => {
-  // not sure why flow doesn't like values since I am passing type to the hook
   const { values, setValues } = useFormikContext()
 
   const selectedModel = values.selectedModel
@@ -20,21 +19,4 @@ export const useResetSlotOnModelChange = () => {
       setValues({ selectedModel, selectedSlot })
     }
   })
-}
-
-export const useResetSlotOnModelChangeOld = () => {
-  const [field] = useField('selectedModel')
-  const setSelectedSlot = useField('selectedSlot')[2].setValue
-
-  const selectedModel = field.value
-  const prevSelectedModel = usePrevious(selectedModel)
-  useEffect(() => {
-    if (
-      prevSelectedModel &&
-      prevSelectedModel !== selectedModel &&
-      isModuleWithCollisionIssue(selectedModel)
-    ) {
-      setSelectedSlot('1') // pull from supportedValues
-    }
-  }, [selectedModel, prevSelectedModel, setSelectedSlot])
 }
