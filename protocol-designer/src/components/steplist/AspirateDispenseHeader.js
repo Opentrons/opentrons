f533f98a47071b5695986a7e5173fcbf74df2499
Kadee80 <kaa328@nyu.edu>
2020-04-21 17:18:40 -0400

    refactor(protocol-designer): Update tooltip component in substeps

--- protocol-designer/src/components/steplist/AspirateDispenseHeader.js
+++ protocol-designer/src/components/steplist/AspirateDispenseHeader.js
@@ -1,11 +1,15 @@
 // @flow
 import * as React from 'react'
 import cx from 'classnames'
-import { Icon, HoverTooltip } from '@opentrons/components'
+import {
+  Icon,
+  Tooltip,
+  useHoverTooltip,
+  TOOLTIP_FIXED,
+} from '@opentrons/components'
 import { PDListItem } from '../lists'
 import styles from './StepItem.css'
 import { LabwareTooltipContents } from './LabwareTooltipContents'
-import { Portal } from './TooltipPortal'
 
 type AspirateDispenseHeaderProps = {
   sourceLabwareNickname: ?string,
@@ -22,13 +26,37 @@ export function AspirateDispenseHeader(props: AspirateDispenseHeaderProps) {
     destLabwareDefDisplayName,
   } = props
 
+  const [sourceTargetProps, sourceTooltipProps] = useHoverTooltip({
+    placement: 'bottom-start',
+    strategy: TOOLTIP_FIXED,
+  })
+
+  const [destTargetProps, destTooltipProps] = useHoverTooltip({
+    placement: 'bottom',
+    strategy: TOOLTIP_FIXED,
+  })
+
   return (
-    <React.Fragment>
+    <>
       <li className={styles.aspirate_dispense}>
         <span>ASPIRATE</span>
         <span className={styles.spacer} />
         <span>DISPENSE</span>
       </li>
+
+      <Tooltip {...sourceTooltipProps}>
+        <LabwareTooltipContents
+          labwareNickname={sourceLabwareNickname}
+          labwareDefDisplayName={sourceLabwareDefDisplayName}
+        />
+      </Tooltip>
+
+      <Tooltip {...destTooltipProps}>
+        <LabwareTooltipContents
+          labwareNickname={destLabwareNickname}
+          labwareDefDisplayName={destLabwareDefDisplayName}
+        />
+      </Tooltip>
 
       <PDListItem
         className={cx(
@@ -36,45 +64,17 @@ export function AspirateDispenseHeader(props: AspirateDispenseHeaderProps) {
           styles.emphasized_cell
         )}
       >
-        <HoverTooltip
-          portal={Portal}
-          tooltipComponent={
-            <LabwareTooltipContents
-              labwareNickname={sourceLabwareNickname}
-              labwareDefDisplayName={sourceLabwareDefDisplayName}
-            />
-          }
-        >
-          {hoverTooltipHandlers => (
-            <span
-              {...hoverTooltipHandlers}
-              className={styles.labware_display_name}
-            >
-              {sourceLabwareNickname}
-            </span>
-          )}
-        </HoverTooltip>
+        <span {...sourceTargetProps} className={styles.labware_display_name}>
+          {sourceLabwareNickname}
+        </span>
+
         {/* This is always a "transfer icon" (arrow pointing right) for any step: */}
         <Icon className={styles.step_subitem_spacer} name="ot-transfer" />
-        <HoverTooltip
-          portal={Portal}
-          tooltipComponent={
-            <LabwareTooltipContents
-              labwareNickname={destLabwareNickname}
-              labwareDefDisplayName={destLabwareDefDisplayName}
-            />
-          }
-        >
-          {hoverTooltipHandlers => (
-            <span
-              {...hoverTooltipHandlers}
-              className={styles.labware_display_name}
-            >
-              {destLabwareNickname}
-            </span>
-          )}
-        </HoverTooltip>
+
+        <span {...destTargetProps} className={styles.labware_display_name}>
+          {destLabwareNickname}
+        </span>
       </PDListItem>
-    </React.Fragment>
+    </>
   )
 }
