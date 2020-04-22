1a6493fbbef5d5373801da464f3a93ec96a226a1
laura_danielle <laura@opentrons.com>
2020-04-22 15:12:52 -0400

    refactor(api): Remove diagonal movement from touch tip
    closes #5451

--- api/src/opentrons/protocol_api/instrument_context.py
+++ api/src/opentrons/protocol_api/instrument_context.py
@@ -425,6 +425,36 @@ class InstrumentContext(CommandPublisher):
         else:
             return clamp_value(speed, 80, 1, 'touch_tip:')
 
+    def _build_edges(
+            self, where: Well, offset: float,
+            radius: float = 1.0) -> List[types.Point]:
+        # Determine the touch_tip edges/points
+        offset_pt = types.Point(0, 0, offset)
+        if self._api_version < APIVersion(2, 4):
+            edge_list = [
+                # right edge
+                where._from_center_cartesian(x=radius, y=0, z=1) + offset_pt,
+                # left edge
+                where._from_center_cartesian(x=-radius, y=0, z=1) + offset_pt,
+                # back edge
+                where._from_center_cartesian(x=0, y=radius, z=1) + offset_pt,
+                # front edge
+                where._from_center_cartesian(x=0, y=-radius, z=1) + offset_pt
+            ]
+        else:
+            edge_list = [
+                # right edge
+                where._from_center_cartesian(x=radius, y=0, z=1) + offset_pt,
+                # left edge
+                where._from_center_cartesian(x=-radius, y=0, z=1) + offset_pt,
+                where.center().point + offset_pt,
+                # back edge
+                where._from_center_cartesian(x=0, y=radius, z=1) + offset_pt,
+                # front edge
+                where._from_center_cartesian(x=0, y=-radius, z=1) + offset_pt
+            ]
+        return edge_list
+
     @cmds.publish.both(command=cmds.touch_tip)
     @requires_version(2, 0)
     def touch_tip(self,
@@ -469,20 +499,6 @@ class InstrumentContext(CommandPublisher):
         if not self.hw_pipette['has_tip']:
             raise hc.NoTipAttachedError('Pipette has no tip to touch_tip()')
 
-        def _build_edges(where: Well, offset: float) -> List[types.Point]:
-            # Determine the touch_tip edges/points
-            offset_pt = types.Point(0, 0, offset)
-            return [
-                # right edge
-                where._from_center_cartesian(x=radius, y=0, z=1) + offset_pt,
-                # left edge
-                where._from_center_cartesian(x=-radius, y=0, z=1) + offset_pt,
-                # back edge
-                where._from_center_cartesian(x=0, y=radius, z=1) + offset_pt,
-                # front edge
-                where._from_center_cartesian(x=0, y=-radius, z=1) + offset_pt
-            ]
-
         checked_speed = self._determine_speed(speed)
 
         # If location is a valid well, move to the well first
@@ -505,7 +521,7 @@ class InstrumentContext(CommandPublisher):
             raise TypeError(
                 'location should be a Well, but it is {}'.format(location))
 
-        for edge in _build_edges(location, v_offset):
+        for edge in self._build_edges(location, v_offset, radius):
             self._hw_manager.hardware.move_to(self._mount, edge, checked_speed)
         return self
 
