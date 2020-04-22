65e707b9c24a8770b32bc510cc9e6c2432317932
Katie Adee <kaa328@nyu.edu>
2020-04-22 14:03:38 -0400

    refactor(protocol-designer): Update tooltip component in substeps (#5460)

--- protocol-designer/src/components/steplist/ModuleStepItems.js
+++ protocol-designer/src/components/steplist/ModuleStepItems.js
@@ -1,10 +1,9 @@
 // @flow
 import * as React from 'react'
 import cx from 'classnames'
-import { HoverTooltip } from '@opentrons/components'
+import { Tooltip, useHoverTooltip, TOOLTIP_FIXED } from '@opentrons/components'
 import { i18n } from '../../localization'
 import { PDListItem } from '../lists'
-import { Portal } from './TooltipPortal'
 import { LabwareTooltipContents } from './LabwareTooltipContents'
 import styles from './StepItem.css'
 import type { ModuleRealType } from '@opentrons/shared-data'
@@ -18,39 +17,40 @@ type Props = {|
   message?: ?string,
 |}
 
-export const ModuleStepItems = (props: Props) => (
-  <>
-    <li className={styles.substep_header}>
-      <span>{i18n.t(`modules.module_long_names.${props.moduleType}`)}</span>
-      <span>{props.action}</span>
-    </li>
-    <PDListItem
-      className={cx(styles.step_subitem_column_header, styles.substep_content)}
-    >
-      <HoverTooltip
-        portal={Portal}
-        tooltipComponent={
-          <LabwareTooltipContents
-            labwareNickname={props.labwareNickname}
-            labwareDefDisplayName={props.labwareDisplayName}
-          />
-        }
-      >
-        {hoverTooltipHandlers => (
-          <span
-            {...hoverTooltipHandlers}
-            className={styles.labware_display_name}
-          >
-            {props.labwareNickname}
-          </span>
+export const ModuleStepItems = (props: Props) => {
+  const [targetProps, tooltipProps] = useHoverTooltip({
+    placement: 'bottom-start',
+    strategy: TOOLTIP_FIXED,
+  })
+  return (
+    <>
+      <li className={styles.substep_header}>
+        <span>{i18n.t(`modules.module_long_names.${props.moduleType}`)}</span>
+        <span>{props.action}</span>
+      </li>
+      <Tooltip {...tooltipProps}>
+        <LabwareTooltipContents
+          labwareNickname={props.labwareNickname}
+          labwareDefDisplayName={props.labwareDisplayName}
+        />
+      </Tooltip>
+      <PDListItem
+        className={cx(
+          styles.step_subitem_column_header,
+          styles.substep_content
         )}
-      </HoverTooltip>
-      <span className={styles.module_substep_value}>{props.actionText}</span>
-    </PDListItem>
-    {props.message && (
-      <PDListItem className={cx(styles.substep_content, 'step-item-message')}>
-        &quot;{props.message}&quot;
+      >
+        <span {...targetProps} className={styles.labware_display_name}>
+          {props.labwareNickname}
+        </span>
+
+        <span className={styles.module_substep_value}>{props.actionText}</span>
       </PDListItem>
-    )}
-  </>
-)
+      {props.message && (
+        <PDListItem className={cx(styles.substep_content, 'step-item-message')}>
+          &quot;{props.message}&quot;
+        </PDListItem>
+      )}
+    </>
+  )
+}
