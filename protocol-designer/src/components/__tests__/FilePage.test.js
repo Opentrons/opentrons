86f95fbb10b3bf6b5aaeb5de609e622698c571bb
Shlok Amin <shlokamin@me.com>
2020-04-27 17:37:18 -0400

    add test to make sure edit modules component shows up

--- protocol-designer/src/components/__tests__/FilePage.test.js
+++ protocol-designer/src/components/__tests__/FilePage.test.js
@@ -4,10 +4,14 @@ import { Provider } from 'react-redux'
 import { mount } from 'enzyme'
 import { FilePage } from '../FilePage'
 import { EditModules } from '../EditModules'
+import { EditModulesCard } from '../modules'
 
+jest.mock('../EditModules')
 jest.mock('../../step-forms/utils')
 jest.mock('../../step-forms/selectors')
 jest.mock('../../feature-flags')
+
+const editModulesMock: JestMockFn<any, any> = EditModules
 
 describe('File Page', () => {
   let props
@@ -27,6 +31,7 @@ describe('File Page', () => {
       subscribe: jest.fn(),
       getState: () => ({ mock: 'this is a mocked out getState' }),
     }
+    editModulesMock.mockImplementation(props => <div>mock edit modules</div>)
   })
 
   const render = props =>
@@ -38,5 +43,14 @@ describe('File Page', () => {
   it('renders a file page with Edit Modules closed', () => {
     const wrapper = render(props)
     expect(wrapper.find(EditModules)).toHaveLength(0)
+  })
+  it('renders a file page with Edit Modules open when handle edit modules is called', () => {
+    const wrapper = render(props)
+    wrapper
+      .find(EditModulesCard)
+      .at(0)
+      .prop('openEditModuleModal')()
+    wrapper.update()
+    expect(wrapper.find(EditModules)).toHaveLength(1)
   })
 })
