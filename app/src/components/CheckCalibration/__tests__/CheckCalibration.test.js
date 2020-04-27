e25efbe33fc7c024b43ecb156b6c27be4e0b4e9b
Brian Cooper <brian@allegro.com>
2020-04-27 15:45:49 -0400

    update index component test

--- app/src/components/CheckCalibration/__tests__/CheckCalibration.test.js
+++ app/src/components/CheckCalibration/__tests__/CheckCalibration.test.js
@@ -3,21 +3,33 @@ import * as React from 'react'
 import { Provider } from 'react-redux'
 import { mount } from 'enzyme'
 
+import { getDeckDefinitions } from '@opentrons/components/src/deck/getDeckDefinitions'
+
 import * as Calibration from '../../../calibration'
 import { mockRobotCalibrationCheckSessionData } from '../../../calibration/__fixtures__'
 
 import { CheckCalibration } from '../index'
 import { Introduction } from '../Introduction'
+import { DeckSetup } from '../DeckSetup'
+import { TipPickUp } from '../TipPickUp'
+import { CheckXYPoint } from '../CheckXYPoint'
+import { CheckHeight } from '../CheckHeight'
 import { CompleteConfirmation } from '../CompleteConfirmation'
 
 import type { State } from '../../../types'
 
+jest.mock('@opentrons/components/src/deck/getDeckDefinitions')
 jest.mock('../../../calibration/selectors')
 
 const getRobotCalibrationCheckSession: JestMockFn<
   [State, string],
   $Call<typeof Calibration.getRobotCalibrationCheckSession, State, string>
 > = Calibration.getRobotCalibrationCheckSession
+
+const mockGetDeckDefinitions: JestMockFn<
+  [],
+  $Call<typeof getDeckDefinitions, any>
+> = getDeckDefinitions
 
 describe('CheckCalibration', () => {
   let mockStore
@@ -74,6 +86,116 @@ describe('CheckCalibration', () => {
     const wrapper = render()
 
     expect(wrapper.exists(Introduction)).toBe(true)
+    expect(wrapper.exists(DeckSetup)).toBe(false)
+    expect(wrapper.exists(TipPickUp)).toBe(false)
+    expect(wrapper.exists(CheckXYPoint)).toBe(false)
+    expect(wrapper.exists(CheckHeight)).toBe(false)
+    expect(wrapper.exists(CompleteConfirmation)).toBe(false)
+  })
+
+  it('renders DeckSetup contents when currentStep is labwareLoaded', () => {
+    mockGetDeckDefinitions.mockReturnValue({})
+    getRobotCalibrationCheckSession.mockReturnValue({
+      ...mockRobotCalibrationCheckSessionData,
+      currentStep: 'labwareLoaded',
+    })
+    const wrapper = render()
+
+    expect(wrapper.exists(Introduction)).toBe(false)
+    expect(wrapper.exists(DeckSetup)).toBe(true)
+    expect(wrapper.exists(TipPickUp)).toBe(false)
+    expect(wrapper.exists(CheckXYPoint)).toBe(false)
+    expect(wrapper.exists(CheckHeight)).toBe(false)
+    expect(wrapper.exists(CompleteConfirmation)).toBe(false)
+  })
+
+  it('renders TipPickUp contents when currentStep is preparingPipette', () => {
+    getRobotCalibrationCheckSession.mockReturnValue({
+      ...mockRobotCalibrationCheckSessionData,
+      currentStep: 'preparingPipette',
+    })
+    const wrapper = render()
+
+    expect(wrapper.exists(Introduction)).toBe(false)
+    expect(wrapper.exists(DeckSetup)).toBe(false)
+    expect(wrapper.exists(TipPickUp)).toBe(true)
+    expect(wrapper.exists(CheckXYPoint)).toBe(false)
+    expect(wrapper.exists(CheckHeight)).toBe(false)
+    expect(wrapper.exists(CompleteConfirmation)).toBe(false)
+  })
+
+  it('renders TipPickUp contents when currentStep is inspectingTip', () => {
+    getRobotCalibrationCheckSession.mockReturnValue({
+      ...mockRobotCalibrationCheckSessionData,
+      currentStep: 'inspectingTip',
+    })
+    const wrapper = render()
+
+    expect(wrapper.exists(Introduction)).toBe(false)
+    expect(wrapper.exists(DeckSetup)).toBe(false)
+    expect(wrapper.exists(TipPickUp)).toBe(true)
+    expect(wrapper.exists(CheckXYPoint)).toBe(false)
+    expect(wrapper.exists(CheckHeight)).toBe(false)
+    expect(wrapper.exists(CompleteConfirmation)).toBe(false)
+  })
+
+  it('renders CheckXYPoint contents when currentStep is checkingPointOne', () => {
+    getRobotCalibrationCheckSession.mockReturnValue({
+      ...mockRobotCalibrationCheckSessionData,
+      currentStep: 'checkingPointOne',
+    })
+    const wrapper = render()
+
+    expect(wrapper.exists(Introduction)).toBe(false)
+    expect(wrapper.exists(DeckSetup)).toBe(false)
+    expect(wrapper.exists(TipPickUp)).toBe(false)
+    expect(wrapper.exists(CheckXYPoint)).toBe(true)
+    expect(wrapper.exists(CheckHeight)).toBe(false)
+    expect(wrapper.exists(CompleteConfirmation)).toBe(false)
+  })
+
+  it('renders CheckXYPoint contents when currentStep is checkingPointTwo', () => {
+    getRobotCalibrationCheckSession.mockReturnValue({
+      ...mockRobotCalibrationCheckSessionData,
+      currentStep: 'checkingPointTwo',
+    })
+    const wrapper = render()
+
+    expect(wrapper.exists(Introduction)).toBe(false)
+    expect(wrapper.exists(DeckSetup)).toBe(false)
+    expect(wrapper.exists(TipPickUp)).toBe(false)
+    expect(wrapper.exists(CheckXYPoint)).toBe(true)
+    expect(wrapper.exists(CheckHeight)).toBe(false)
+    expect(wrapper.exists(CompleteConfirmation)).toBe(false)
+  })
+
+  it('renders CheckXYPoint contents when currentStep is checkingPointThree', () => {
+    getRobotCalibrationCheckSession.mockReturnValue({
+      ...mockRobotCalibrationCheckSessionData,
+      currentStep: 'checkingPointThree',
+    })
+    const wrapper = render()
+
+    expect(wrapper.exists(Introduction)).toBe(false)
+    expect(wrapper.exists(DeckSetup)).toBe(false)
+    expect(wrapper.exists(TipPickUp)).toBe(false)
+    expect(wrapper.exists(CheckXYPoint)).toBe(true)
+    expect(wrapper.exists(CheckHeight)).toBe(false)
+    expect(wrapper.exists(CompleteConfirmation)).toBe(false)
+  })
+
+  it('renders CheckHeight contents when currentStep is checkingHeight', () => {
+    getRobotCalibrationCheckSession.mockReturnValue({
+      ...mockRobotCalibrationCheckSessionData,
+      currentStep: 'checkingHeight',
+    })
+    const wrapper = render()
+
+    expect(wrapper.exists(Introduction)).toBe(false)
+    expect(wrapper.exists(DeckSetup)).toBe(false)
+    expect(wrapper.exists(TipPickUp)).toBe(false)
+    expect(wrapper.exists(CheckXYPoint)).toBe(false)
+    expect(wrapper.exists(CheckHeight)).toBe(true)
     expect(wrapper.exists(CompleteConfirmation)).toBe(false)
   })
 
