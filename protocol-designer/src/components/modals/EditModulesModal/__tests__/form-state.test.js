bb17508b2a1324756791f3053c1654a9d3935137
Shlok Amin <shlokamin@me.com>
2020-04-30 11:18:53 -0400

    remove unnecessary act import

--- protocol-designer/src/components/modals/EditModulesModal/__tests__/form-state.test.js
+++ protocol-designer/src/components/modals/EditModulesModal/__tests__/form-state.test.js
@@ -1,6 +1,5 @@
 // @flow
 import * as React from 'react'
-import { act } from 'react-dom/test-utils'
 import { mount } from 'enzyme'
 import * as Formik from 'formik'
 import { isModuleWithCollisionIssue } from '../../../modules/utils'
