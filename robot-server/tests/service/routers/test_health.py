44a570a88fa930c032563db509149b4384a9ed8f
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2020-05-01 11:01:14 -0400

    refactor(api): detect central routing board revision using gpios (#5477)
    * refactor(api): detect central routing board revision using gpios
    
    closes #5288

--- robot-server/tests/service/routers/test_health.py
+++ robot-server/tests/service/routers/test_health.py
@@ -4,11 +4,13 @@ from opentrons.protocol_api import MAX_SUPPORTED_VERSION
 
 def test_health(api_client, hardware):
     hardware.fw_version = "FW111"
+    hardware.board_revision = "BR2.1"
 
     expected = {
         'name': 'opentrons-dev',
         'api_version': __version__,
         'fw_version': 'FW111',
+        'board_revision': 'BR2.1',
         'logs': ['/logs/serial.log', '/logs/api.log'],
         'system_version': '0.0.0',
         'protocol_api_version': list(MAX_SUPPORTED_VERSION),
