e20d64593592ab1d7bba607bd936cb566cfc6984
Shlok Amin <shlokamin@me.com>
2020-05-07 16:34:01 -0400

    fix(protocol-designer): fix multichannel well selection bug (#5607)
    * fix(shared-data): fix _findWellAt and add tests
    
    * remove underscore from exported function
    
    * add test for canPipetteUseLabware helper
    
    * update snapshot test for new fixture
    
    * remove unecessary export
    
    * format fixture
    
    * import pipette fixtures from fixture folder
    
    Co-authored-by: IanLondon <ianlondondu@gmail.com>

--- shared-data/js/helpers/__tests__/wellSets.test.js
+++ shared-data/js/helpers/__tests__/wellSets.test.js
@@ -1,8 +1,88 @@
 // @flow
+import {
+  fixtureP10Single,
+  fixtureP10Multi,
+} from '@opentrons/shared-data/pipette/fixtures/name'
 import fixture_12_trough from '../../../labware/fixtures/2/fixture_12_trough.json'
 import fixture_96_plate from '../../../labware/fixtures/2/fixture_96_plate.json'
 import fixture_384_plate from '../../../labware/fixtures/2/fixture_384_plate.json'
+import fixture_overlappy_wellplate from '../../../labware/fixtures/2/fixture_overlappy_wellplate'
 import { makeWellSetHelpers } from '../wellSets'
+import { findWellAt } from '../getWellNamePerMultiTip'
+
+describe('findWellAt', () => {
+  it('should determine if given (x, y) is within a rectangular well', () => {
+    const def: any = {
+      wells: {
+        A1: {
+          shape: 'rectangular',
+          x: 200,
+          y: 200,
+          xDimension: 10,
+          yDimension: 10,
+        },
+      },
+    }
+    const middle = findWellAt(def, 200, 200)
+    expect(middle).toBe('A1')
+
+    const insideCornerNE = findWellAt(def, 200 - 4, 200 + 4)
+    expect(insideCornerNE).toEqual('A1')
+
+    // exactly at an edge doesn't count
+    const exactlyOnCornerNE = findWellAt(def, 200 - 5, 200 + 5)
+    expect(exactlyOnCornerNE).toBeUndefined()
+
+    const exactlyOnWEdge = findWellAt(def, 200, 200 - 5)
+    expect(exactlyOnWEdge).toBeUndefined()
+
+    const justOutsideToEast = findWellAt(def, 200 + 5.1, 200)
+    expect(justOutsideToEast).toBeUndefined()
+  })
+
+  it('should determine if given (x, y) is within a circular well', () => {
+    const def: any = {
+      wells: {
+        A1: {
+          shape: 'circular',
+          x: 200,
+          y: 200,
+          diameter: 10,
+        },
+      },
+    }
+    const middle = findWellAt(def, 200, 200)
+    expect(middle).toBe('A1')
+
+    const inside = findWellAt(def, 200 - 1, 200 + 1)
+    expect(inside).toEqual('A1')
+
+    // exactly at an edge doesn't count
+    const exactlyOnWEdge = findWellAt(def, 200, 200 - 5)
+    expect(exactlyOnWEdge).toBeUndefined()
+
+    const justOutsideToEast = findWellAt(def, 200 + 5.1, 200)
+    expect(justOutsideToEast).toBeUndefined()
+  })
+})
+
+describe('canPipetteUseLabware', () => {
+  let canPipetteUseLabware
+  beforeEach(() => {
+    const helpers = makeWellSetHelpers()
+    canPipetteUseLabware = helpers.canPipetteUseLabware
+  })
+  it('returns false when wells are too close together for multi channel pipette', () => {
+    const labwareDef = { ...fixture_overlappy_wellplate }
+    const pipette = { ...fixtureP10Multi }
+    expect(canPipetteUseLabware(pipette, labwareDef)).toBe(false)
+  })
+  it('returns true when pipette is single channel', () => {
+    const labwareDef = { ...fixture_overlappy_wellplate }
+    const pipette = { ...fixtureP10Single }
+    expect(canPipetteUseLabware(pipette, labwareDef)).toBe(true)
+  })
+})
 
 describe('getWellSetForMultichannel (integration test)', () => {
   let getWellSetForMultichannel
