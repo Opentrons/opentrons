f11a52f485b8b61f0ce71e73a8d17c84f1e2c804
Kadee80 <kaa328@nyu.edu>
2020-05-07 17:56:14 -0400

    fixup: Add more constants

--- protocol-designer/src/steplist/formLevel/getDefaultsForStepType.js
+++ protocol-designer/src/steplist/formLevel/getDefaultsForStepType.js
@@ -6,6 +6,7 @@ import {
   DEFAULT_WELL_ORDER_FIRST_OPTION,
   DEFAULT_WELL_ORDER_SECOND_OPTION,
   FIXED_TRASH_ID,
+  THERMOCYCLER_STATE,
 } from '../../constants'
 import type { StepType, StepFieldName } from '../../form-types'
 
@@ -94,7 +95,7 @@ export function getDefaultsForStepType(
       }
     case 'thermocycler':
       return {
-        thermocyclerFormType: 'thermocyclerState',
+        thermocyclerFormType: THERMOCYCLER_STATE,
         moduleId: null,
         blockIsActive: false,
         blockTargetTemp: null,
