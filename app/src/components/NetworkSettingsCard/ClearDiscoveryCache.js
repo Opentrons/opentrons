8fb57417cad7a3f4861bd3fa25181fa168417856
Sanniti <sanniti.p@nyu.edu>
2020-05-08 11:54:22 -0400

    fix linter error

--- app/src/components/NetworkSettingsCard/ClearDiscoveryCache.js
+++ app/src/components/NetworkSettingsCard/ClearDiscoveryCache.js
@@ -3,14 +3,13 @@ import * as React from 'react'
 import { useDispatch } from 'react-redux'
 import { clearDiscoveryCache } from '../../discovery'
 import { LabeledButton } from '@opentrons/components'
+import type { Dispatch } from '../../types'
 
-// TODO(mc, 2020-04-27): i18n
+// TODO(spp, 2020-05-08): i18n
 const CLEAR = 'clear'
 const CLEAR_ROBOTS_TITLE = 'Clear Discovered Robots List'
 const CLEAR_ROBOTS_DESCRIPTION =
   'If your app has unused robots in its list, click to clear the cache & remove them.'
-
-import type { Dispatch } from '../../types'
 
 export function ClearDiscoveryCache() {
   const dispatch = useDispatch<Dispatch>()
