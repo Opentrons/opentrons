a9a5fd8e5643db0b896be70ce5fe1d6d4caf95fd
Kadee80 <kaa328@nyu.edu>
2020-05-11 14:11:37 -0400

    fixup: Simplify moduleId getters

--- protocol-designer/src/steplist/formLevel/getNextDefaultModuleId/getNextDefaultTemperatureModuleId.js
+++ protocol-designer/src/steplist/formLevel/getNextDefaultModuleId/getNextDefaultTemperatureModuleId.js
@@ -1,31 +1,18 @@
 // @flow
 import findKey from 'lodash/findKey'
-import last from 'lodash/last'
+
 import { TEMPERATURE_MODULE_TYPE } from '@opentrons/shared-data'
 
 import type { ModuleOnDeck } from '../../../step-forms'
 import type { StepIdType, FormData } from '../../../form-types'
-
-const isLastStepTemp = (lastModuleStep: FormData = {}): boolean =>
-  !!(lastModuleStep.moduleId && lastModuleStep.stepType === 'temperature')
 
 export function getNextDefaultTemperatureModuleId(
   savedForms: { [StepIdType]: FormData },
   orderedStepIds: Array<StepIdType>,
   equippedModulesById: { [moduleId: string]: ModuleOnDeck }
 ): string | null {
-  const prevModuleSteps = orderedStepIds
-    .map(stepId => savedForms[stepId])
-    .filter(form => form && form.moduleId)
-
-  const lastModuleStep = last(prevModuleSteps)
-
-  // TODO (ka 2019-12-20): Since we are hiding the thermocylcer module as an option for now,
-  // should we simplify this to only return temperature modules?
-  const nextDefaultModule: string | null =
-    (isLastStepTemp(lastModuleStep) && lastModuleStep.moduleId) ||
+  return (
     findKey(equippedModulesById, m => m.type === TEMPERATURE_MODULE_TYPE) ||
     null
-
-  return nextDefaultModule || null
+  )
 }
--- protocol-designer/src/steplist/formLevel/getNextDefaultModuleId/getNextDefaultThermocyclerModuleId.js
+++ protocol-designer/src/steplist/formLevel/getNextDefaultModuleId/getNextDefaultThermocyclerModuleId.js
@@ -1,29 +1,17 @@
 // @flow
 import findKey from 'lodash/findKey'
-import last from 'lodash/last'
 import { THERMOCYCLER_MODULE_TYPE } from '@opentrons/shared-data'
 
 import type { ModuleOnDeck } from '../../../step-forms'
 import type { StepIdType, FormData } from '../../../form-types'
-
-const isLastStepTemp = (lastModuleStep: FormData = {}): boolean =>
-  !!(lastModuleStep.moduleId && lastModuleStep.stepType === 'temperature')
 
 export function getNextDefaultThermocyclerModuleId(
   savedForms: { [StepIdType]: FormData },
   orderedStepIds: Array<StepIdType>,
   equippedModulesById: { [moduleId: string]: ModuleOnDeck }
 ): string | null {
-  const prevModuleSteps = orderedStepIds
-    .map(stepId => savedForms[stepId])
-    .filter(form => form && form.moduleId)
-
-  const lastModuleStep = last(prevModuleSteps)
-
-  const nextDefaultModule: string | null =
-    (isLastStepTemp(lastModuleStep) && lastModuleStep.moduleId) ||
+  return (
     findKey(equippedModulesById, m => m.type === THERMOCYCLER_MODULE_TYPE) ||
     null
-
-  return nextDefaultModule || null
+  )
 }
