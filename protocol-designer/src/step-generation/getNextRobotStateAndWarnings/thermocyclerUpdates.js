34ff540bab5b735d59e6b95c55c082ebf8112aa4
Ian London <ianlondondu@gmail.com>
2020-05-12 16:22:01 -0400

    feat(protocol-designer): implement TC robot state updaters (#5657)
    Closes #5611

---
+++ protocol-designer/src/step-generation/getNextRobotStateAndWarnings/thermocyclerUpdates.js
@@ -0,0 +1,119 @@
+// @flow
+import { THERMOCYCLER_MODULE_TYPE } from '@opentrons/shared-data'
+import { getModuleState } from '../robotStateSelectors'
+import type {
+  ModuleOnlyParams,
+  TemperatureParams,
+  ThermocyclerSetTargetBlockTemperatureArgs,
+} from '@opentrons/shared-data/protocol/flowTypes/schemaV4'
+import type {
+  InvariantContext,
+  RobotStateAndWarnings,
+  RobotState,
+} from '../types'
+import type { ThermocyclerModuleState } from '../../step-forms/types'
+
+const _getThermocyclerModuleState = (
+  robotState: RobotState,
+  module: string
+): ThermocyclerModuleState => {
+  const moduleState = getModuleState(robotState, module)
+  if (moduleState.type === THERMOCYCLER_MODULE_TYPE) {
+    return moduleState
+  } else {
+    console.error(
+      `Thermocycler state updater expected ${module} moduleState to be thermocycler, but it was ${moduleState.type}`
+    )
+    // return some object instead of an error :/
+    const fallback: any = {}
+    return fallback
+  }
+}
+
+export const forThermocyclerSetTargetBlockTemperature = (
+  params: ThermocyclerSetTargetBlockTemperatureArgs,
+  invariantContext: InvariantContext,
+  robotStateAndWarnings: RobotStateAndWarnings
+): void => {
+  const { module, temperature } = params
+  const { robotState } = robotStateAndWarnings
+
+  const moduleState = _getThermocyclerModuleState(robotState, module)
+  moduleState.blockTargetTemp = temperature
+}
+
+export const forThermocyclerSetTargetLidTemperature = (
+  params: TemperatureParams,
+  invariantContext: InvariantContext,
+  robotStateAndWarnings: RobotStateAndWarnings
+): void => {
+  const { module, temperature } = params
+  const { robotState } = robotStateAndWarnings
+
+  const moduleState = _getThermocyclerModuleState(robotState, module)
+  moduleState.lidTargetTemp = temperature
+}
+
+export const forThermocyclerAwaitBlockTemperature = (
+  params: TemperatureParams,
+  invariantContext: InvariantContext,
+  robotStateAndWarnings: RobotStateAndWarnings
+): void => {
+  // nothing to be done
+}
+
+export const forThermocyclerAwaitLidTemperature = (
+  params: TemperatureParams,
+  invariantContext: InvariantContext,
+  robotStateAndWarnings: RobotStateAndWarnings
+): void => {
+  // nothing to be done
+}
+
+export const forThermocyclerDeactivateBlock = (
+  params: ModuleOnlyParams,
+  invariantContext: InvariantContext,
+  robotStateAndWarnings: RobotStateAndWarnings
+): void => {
+  const { module } = params
+  const { robotState } = robotStateAndWarnings
+
+  const moduleState = _getThermocyclerModuleState(robotState, module)
+  moduleState.blockTargetTemp = null
+}
+
+export const forThermocyclerDeactivateLid = (
+  params: ModuleOnlyParams,
+  invariantContext: InvariantContext,
+  robotStateAndWarnings: RobotStateAndWarnings
+): void => {
+  const { module } = params
+  const { robotState } = robotStateAndWarnings
+
+  const moduleState = _getThermocyclerModuleState(robotState, module)
+  moduleState.lidTargetTemp = null
+}
+
+export const forThermocyclerCloseLid = (
+  params: ModuleOnlyParams,
+  invariantContext: InvariantContext,
+  robotStateAndWarnings: RobotStateAndWarnings
+): void => {
+  const { module } = params
+  const { robotState } = robotStateAndWarnings
+
+  const moduleState = _getThermocyclerModuleState(robotState, module)
+  moduleState.lidOpen = false
+}
+
+export const forThermocyclerOpenLid = (
+  params: ModuleOnlyParams,
+  invariantContext: InvariantContext,
+  robotStateAndWarnings: RobotStateAndWarnings
+): void => {
+  const { module } = params
+  const { robotState } = robotStateAndWarnings
+
+  const moduleState = _getThermocyclerModuleState(robotState, module)
+  moduleState.lidOpen = true
+}
