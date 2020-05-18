2fc03c9de84265ba95bb5520b7693f6e70daeddf
Brian Cooper <brian@allegro.com>
2020-05-18 17:07:35 -0400

    update check xy copy

--- app/src/components/CheckCalibration/CheckXYPoint.js
+++ app/src/components/CheckCalibration/CheckXYPoint.js
@@ -57,21 +57,21 @@ const assetMap = {
 
 const CHECK_POINT_XY_HEADER = 'Check the X and Y-axis in'
 const SLOT = 'slot'
-const JOG_UNTIL = 'Jog pipette until tip is'
+const JOG_UNTIL = 'Jog the robot until the tip is'
 const JUST_BARELY = 'just barely'
 const TOUCHING_THE_CROSS = 'touching the cross in'
 const THEN = 'Then'
 const CHECK_AXES = 'check x and y-axis'
 const TO_DETERMINE_MATCH =
   'to see if the position matches the calibration co-ordinate.'
-const DROP_TIP_AND_EXIT = 'Drop tip and exit calibration check'
+const EXIT_CHECK = 'Exit robot calibration check'
 
 const BAD_INSPECTING_HEADER = 'Bad calibration data detected'
 const GOOD_INSPECTING_HEADER = 'Good calibration'
 const BAD_INSPECTING_BODY =
-  'The jogged and calibrated x and y-axis co-ordinates do not match, and are out of acceptable bounds.'
+  "Your current pipette tip position does not match your robot's saved calibration data."
 const GOOD_INSPECTING_BODY =
-  'The jogged and calibrated x and y-axis co-ordinates fall within acceptable bounds.'
+  "Your current pipette tip position matches your robot's saved calibration data."
 const DIFFERENCE = 'Difference'
 
 type CheckXYPointProps = {|
@@ -130,6 +130,7 @@ export function CheckXYPoint(props: CheckXYPointProps) {
               <b>&nbsp;{JUST_BARELY}&nbsp;</b>
               {TOUCHING_THE_CROSS}
               <b>&nbsp;{`${SLOT} ${slotNumber || ''}`}.&nbsp;</b>
+              <br />
               {THEN}
               <b>&nbsp;{CHECK_AXES}&nbsp;</b>
               {TO_DETERMINE_MATCH}
@@ -211,7 +212,7 @@ function CompareXY(props: CompareXYProps) {
       </div>
       <div className={styles.button_stack}>
         {exceedsThreshold && (
-          <PrimaryButton onClick={exit}>{DROP_TIP_AND_EXIT}</PrimaryButton>
+          <PrimaryButton onClick={exit}>{EXIT_CHECK}</PrimaryButton>
         )}
         <PrimaryButton onClick={goToNextCheck}>{nextButtonText}</PrimaryButton>
       </div>
