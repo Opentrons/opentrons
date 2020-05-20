2f116525e25da272dfb402f9a2d1fade5ae0fda8
Ian London <ianlondondu@gmail.com>
2020-05-20 18:52:37 -0400

    feat(protocol-designer): add substeps for TC State step (#5695)
    Closes #5694

--- protocol-designer/src/components/steplist/ModuleStepItems.js
+++ protocol-designer/src/components/steplist/ModuleStepItems.js
@@ -1,12 +1,34 @@
 // @flow
 import * as React from 'react'
 import cx from 'classnames'
-import { Tooltip, useHoverTooltip, TOOLTIP_FIXED } from '@opentrons/components'
+import {
+  Tooltip,
+  useHoverTooltip,
+  TOOLTIP_FIXED,
+  type UseHoverTooltipTargetProps,
+} from '@opentrons/components'
 import { i18n } from '../../localization'
 import { PDListItem } from '../lists'
 import { LabwareTooltipContents } from './LabwareTooltipContents'
 import styles from './StepItem.css'
 import type { ModuleRealType } from '@opentrons/shared-data'
+
+export type ModuleStepItemRowProps = {|
+  label?: ?string,
+  value?: ?string,
+  targetProps?: UseHoverTooltipTargetProps,
+|}
+
+export const ModuleStepItemRow = (props: ModuleStepItemRowProps) => (
+  <PDListItem
+    className={cx(styles.step_subitem_column_header, styles.substep_content)}
+  >
+    <span {...props.targetProps} className={styles.labware_display_name}>
+      {props.label}
+    </span>
+    <span className={styles.module_substep_value}>{props.value}</span>
+  </PDListItem>
+)
 
 type Props = {|
   action: string,
@@ -15,6 +37,7 @@ type Props = {|
   labwareDisplayName: ?string,
   labwareNickname: ?string,
   message?: ?string,
+  children?: React.Node,
 |}
 
 export const ModuleStepItems = (props: Props) => {
@@ -34,18 +57,12 @@ export const ModuleStepItems = (props: Props) => {
           labwareDefDisplayName={props.labwareDisplayName}
         />
       </Tooltip>
-      <PDListItem
-        className={cx(
-          styles.step_subitem_column_header,
-          styles.substep_content
-        )}
-      >
-        <span {...targetProps} className={styles.labware_display_name}>
-          {props.labwareNickname}
-        </span>
-
-        <span className={styles.module_substep_value}>{props.actionText}</span>
-      </PDListItem>
+      <ModuleStepItemRow
+        label={props.labwareNickname}
+        targetProps={targetProps}
+        value={props.actionText}
+      />
+      {props.children}
       {props.message && (
         <PDListItem className={cx(styles.substep_content, 'step-item-message')}>
           &quot;{props.message}&quot;
