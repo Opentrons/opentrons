b4d53711ae8fb942350e8f27a5997bca070ece70
Brian Cooper <brian@allegro.com>
2020-05-20 14:43:20 -0400

    allow for meta overwrites in setupEpicTests

--- app/src/calibration/epic/__tests__/fetchRobotCalibrationCheckSessionEpic.test.js
+++ app/src/calibration/epic/__tests__/fetchRobotCalibrationCheckSessionEpic.test.js
@@ -1,7 +1,6 @@
 // @flow
-import { TestScheduler } from 'rxjs/testing'
 import { mockFetchSessionFailureMeta } from '../../../sessions/__fixtures__'
-import { mockRobot } from '../../../robot-api/__fixtures__'
+import { setupEpicTestMocks, runEpicTest } from '../../../robot-api/__utils__'
 import * as Sessions from '../../../sessions'
 import { calibrationEpic } from '..'
 
@@ -15,25 +14,24 @@ describe('fetchRobotCalibrationCheckSessionEpic', () => {
 
   describe('handles fetch calibration check session', () => {
     it('maps cal check session creation conflict response to FETCH_ALL_SESSIONS', () => {
-      const triggerAction = Sessions.createSessionFailure(
-        mockRobot.name,
-        { errors: [{ status: 'theres already someone in here' }] },
-        {
-          requestId: 'abc',
-          response: { ...mockFetchSessionFailureMeta, status: 409 },
-        }
+      const mocks = setupEpicTestMocks(robotName =>
+        Sessions.createSessionFailure(
+          robotName,
+          { errors: [{ status: 'theres already someone in here' }] },
+          {
+            requestId: 'abc',
+            response: { ...mockFetchSessionFailureMeta, status: 409 },
+          }
+        )
       )
 
-      const testScheduler = new TestScheduler((actual, expected) => {
-        expect(actual).toEqual(expected)
-      })
-      testScheduler.run(({ hot, cold, expectObservable, flush }) => {
-        const action$ = hot('--a', { a: triggerAction })
-        const state$ = hot('s-s', { s: {} })
+      runEpicTest(mocks, ({ hot, expectObservable }) => {
+        const action$ = hot('--a', { a: mocks.action })
+        const state$ = hot('s-s', { s: mocks.state })
         const output$ = calibrationEpic(action$, state$)
 
         expectObservable(output$).toBe('--a', {
-          a: Sessions.fetchAllSessions(mockRobot.name),
+          a: Sessions.fetchAllSessions(mocks.robot.name),
         })
       })
     })
