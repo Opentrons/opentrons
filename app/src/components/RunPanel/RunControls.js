92d30c1ffb137111990c48e5d8b9ad33938f6697
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2020-05-27 16:41:39 -0400

    added tooltip

--- app/src/components/RunPanel/RunControls.js
+++ app/src/components/RunPanel/RunControls.js
@@ -9,6 +9,12 @@ import styles from './styles.css'
 const MISSING_MODULES =
   'Please attach all required modules before running this protocol'
 
+const DOOR_OPEN_RUN =
+  'Please close the robot door before running this protocol'
+
+const DOOR_OPEN_RESUME =
+  'Please close the robot door before resuming'
+
 export type RunControlsProps = {|
   disabled: boolean,
   modulesReady: boolean,
@@ -48,10 +54,15 @@ export function RunControls(props: RunControlsProps) {
   if (isReadyToRun && !isRunning) {
     // TODO(mc, 2019-09-03): add same check for pipettes
     const runDisabled = disabled || !modulesReady || isBlocked
-    let tooltip = modulesReady ? null : MISSING_MODULES
+    let runTooltip = null
+    if (!modulesReady) {
+      runTooltip = MISSING_MODULES
+    } else if (isBlocked) {
+      runTooltip = DOOR_OPEN_RUN
+    }
 
     runButton = (
-      <HoverTooltip tooltipComponent={tooltip}>
+      <HoverTooltip tooltipComponent={runTooltip}>
         {hoverTooltipHandlers => (
           <div {...hoverTooltipHandlers}>
             <OutlineButton
@@ -67,14 +78,22 @@ export function RunControls(props: RunControlsProps) {
     )
   } else if (isRunning) {
     const pauseDisabled = disabled || isBlocked
+    let pauseTooltip = isBlocked ? DOOR_OPEN_RESUME : null
+
     pauseResumeButton = (
-      <OutlineButton
-        onClick={onPauseResumeClick}
-        className={styles.run_button}
-        disabled={pauseDisabled}
-      >
-        {pauseResumeText}
-      </OutlineButton>
+      <HoverTooltip tooltipComponent={pauseTooltip}>
+        {hoverTooltipHandlers => (
+          <div {...hoverTooltipHandlers}>
+            <OutlineButton
+              onClick={onPauseResumeClick}
+              className={styles.run_button}
+              disabled={pauseDisabled}
+            >
+              {pauseResumeText}
+            </OutlineButton>
+          </div>
+        )}
+      </HoverTooltip>
     )
     cancelButton = (
       <OutlineButton
