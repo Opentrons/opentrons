bdf049f7639ac3510ad69679ca58cb5270ca633c
Kadee80 <kaa328@nyu.edu>
2020-05-28 10:41:26 -0400

    feat(protocol-designer): Add profiles settings to TC profile form

---
+++ protocol-designer/src/components/StepEditForm/forms/ThermocyclerForm/ProfileSettings.js
@@ -0,0 +1,52 @@
+// @flow
+import * as React from 'react'
+
+import { i18n } from '../../../../localization'
+import { FormGroup } from '@opentrons/components'
+import { TextField } from '../../fields'
+
+import styles from '../../StepEditForm.css'
+
+import type { FocusHandlers } from '../../types'
+
+type Props = {| focusHandlers: FocusHandlers |}
+
+export const ProfileSettings = (props: Props) => {
+  const { focusHandlers } = props
+
+  return (
+    <div className={styles.form_row}>
+      <FormGroup
+        label={i18n.t('form.step_edit_form.field.thermocyclerProfile.volume')}
+        className={styles.profile_settings_group}
+      >
+        <TextField
+          name="profileVolume"
+          className={styles.small_field}
+          units={i18n.t('application.units.microliter')}
+          {...focusHandlers}
+        />
+      </FormGroup>
+      <FormGroup
+        label={i18n.t('form.step_edit_form.field.thermocyclerProfile.lid_temp')}
+        className={styles.profile_settings_group}
+      >
+        <TextField
+          name="profileTargetLidTemp"
+          className={styles.small_field}
+          units={i18n.t('application.units.degrees')}
+          {...focusHandlers}
+        />
+      </FormGroup>
+      <FormGroup
+        label={i18n.t(
+          'form.step_edit_form.field.thermocyclerProfile.lid_position'
+        )}
+      >
+        <p className={styles.profile_settings_lid}>
+          {i18n.t('form.step_edit_form.field.thermocyclerProfile.lid_closed')}
+        </p>
+      </FormGroup>
+    </div>
+  )
+}
