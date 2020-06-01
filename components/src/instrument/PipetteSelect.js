ba8f04e0bb19a52e24b32de4b9645e36266ccdd9
Shlok Amin <shlokamin@me.com>
2020-06-01 17:42:00 -0400

    feat(test): add test id to Pipette Select component
    add test id to Pipette Select component for use in cypress test
    
    #5724

--- components/src/instrument/PipetteSelect.js
+++ components/src/instrument/PipetteSelect.js
@@ -27,6 +27,8 @@ export type PipetteSelectProps = {|
   tabIndex?: number,
   /** classes to apply to the top-level component */
   className?: string,
+  /** custom id to be applied. likely to be used as a data test id for e2e testing */
+  id?: string,
 |}
 
 // TODO(mc, 2019-10-14): i18n
@@ -48,7 +50,13 @@ const specToOption = ({ name, displayName }: PipetteNameSpecs) => ({
 })
 
 export const PipetteSelect = (props: PipetteSelectProps): React.Node => {
-  const { tabIndex, className, enableNoneOption, nameBlacklist = [] } = props
+  const {
+    tabIndex,
+    className,
+    enableNoneOption,
+    id,
+    nameBlacklist = [],
+  } = props
   const whitelist = ({ value }: SelectOption) => {
     return !nameBlacklist.some(n => n === value)
   }
@@ -75,6 +83,7 @@ export const PipetteSelect = (props: PipetteSelectProps): React.Node => {
       value={value}
       defaultValue={defaultValue}
       tabIndex={tabIndex}
+      id={id}
       onChange={option => {
         // TODO(mc, 2020-02-03):  change to `option?.value ?? null`
         // when we enable that babel functionality
