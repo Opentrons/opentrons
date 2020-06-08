55e6621d614f9825caecb443ddcf765e8aacd4be
Shlok Amin <shlokamin@me.com>
2020-06-08 17:04:43 -0400

    use lodash upperFirst to format button name

--- protocol-designer/src/components/modules/ModuleRow.js
+++ protocol-designer/src/components/modules/ModuleRow.js
@@ -1,5 +1,7 @@
 // @flow
 import * as React from 'react'
+import { useDispatch } from 'react-redux'
+import upperFirst from 'lodash/upperFirst'
 import {
   LabeledValue,
   OutlineButton,
@@ -8,14 +10,12 @@ import {
   useHoverTooltip,
 } from '@opentrons/components'
 import { i18n } from '../../localization'
-import { useDispatch } from 'react-redux'
 import { actions as stepFormActions } from '../../step-forms'
-
-import { ModuleDiagram } from './ModuleDiagram'
 import {
   SPAN7_8_10_11_SLOT,
   DEFAULT_MODEL_FOR_MODULE_TYPE,
 } from '../../constants'
+import { ModuleDiagram } from './ModuleDiagram'
 import { isModuleWithCollisionIssue } from './utils'
 import styles from './styles.css'
 
@@ -145,7 +145,7 @@ export function ModuleRow(props: Props) {
             <OutlineButton
               className={styles.module_button}
               onClick={handleEditModule}
-              name={dataIdFormat('edit', type)}
+              name={`edit${upperFirst(type)}`}
             >
               Edit
             </OutlineButton>
@@ -153,7 +153,7 @@ export function ModuleRow(props: Props) {
           <OutlineButton
             className={styles.module_button}
             onClick={handleAddOrRemove}
-            name={dataIdFormat(addRemoveText, type)}
+            name={`${addRemoveText}${upperFirst(type)}`}
           >
             {addRemoveText}
           </OutlineButton>
@@ -162,6 +162,3 @@ export function ModuleRow(props: Props) {
     </div>
   )
 }
-
-const dataIdFormat = (action: string, moduleType: ModuleRealType): string =>
-  `${action}${moduleType.charAt(0).toUpperCase()}${moduleType.slice(1)}`
