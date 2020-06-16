2e3514b93db3d36bb61350e4a4ce81d46b38d1f8
Shlok Amin <shlokamin@me.com>
2020-06-16 10:35:56 -0400

    style(protocol-designer): decrease click target area for TC form radio buttons (#5914)
    * style(protocol-designer): decrease click target area for TC form radio buttons
    
    #5885
    
    * add back step option styling

--- protocol-designer/src/components/StepEditForm/forms/ThermocyclerForm/index.js
+++ protocol-designer/src/components/StepEditForm/forms/ThermocyclerForm/index.js
@@ -31,38 +31,42 @@ export const ThermocyclerForm = (props: TCFormProps): React.Element<'div'> => {
         </span>
       </div>
       <div className={styles.tc_step_group}>
-        <RadioGroupField
-          name="thermocyclerFormType"
-          className={styles.tc_step_option}
-          options={[
-            {
-              name: i18n.t(
-                'form.step_edit_form.field.thermocyclerAction.options.state'
-              ),
-              value: THERMOCYCLER_STATE,
-            },
-          ]}
-          {...focusHandlers}
-        />
+        <div className={styles.checkbox_row}>
+          <RadioGroupField
+            name="thermocyclerFormType"
+            className={styles.tc_step_option}
+            options={[
+              {
+                name: i18n.t(
+                  'form.step_edit_form.field.thermocyclerAction.options.state'
+                ),
+                value: THERMOCYCLER_STATE,
+              },
+            ]}
+            {...focusHandlers}
+          />
+        </div>
         <ConditionalOnField
           name={'thermocyclerFormType'}
           condition={val => val === THERMOCYCLER_STATE}
         >
           <StateFields focusHandlers={focusHandlers} />
         </ConditionalOnField>
-        <RadioGroupField
-          name="thermocyclerFormType"
-          className={styles.tc_step_option}
-          options={[
-            {
-              name: i18n.t(
-                'form.step_edit_form.field.thermocyclerAction.options.profile'
-              ),
-              value: THERMOCYCLER_PROFILE,
-            },
-          ]}
-          {...focusHandlers}
-        />
+        <div className={styles.checkbox_row}>
+          <RadioGroupField
+            name="thermocyclerFormType"
+            className={styles.tc_step_option}
+            options={[
+              {
+                name: i18n.t(
+                  'form.step_edit_form.field.thermocyclerAction.options.profile'
+                ),
+                value: THERMOCYCLER_PROFILE,
+              },
+            ]}
+            {...focusHandlers}
+          />
+        </div>
       </div>
 
       <ConditionalOnField
