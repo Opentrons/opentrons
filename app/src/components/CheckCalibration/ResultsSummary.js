af3ecb8ebbf9f6ebc7dd2dbe20ffd19d739a6274
Brian Arthur Cooper <brian@opentrons.com>
2020-06-18 19:02:28 -0400

    refactor(app): add troubleshooting instructions to cal check summary (#5959)
    Co-authored-by: Seth Foster <seth@opentrons.com>

--- app/src/components/CheckCalibration/ResultsSummary.js
+++ app/src/components/CheckCalibration/ResultsSummary.js
@@ -7,16 +7,21 @@ import partition from 'lodash/partition'
 import * as Sessions from '../../sessions'
 import styles from './styles.css'
 import { PipetteComparisons } from './PipetteComparisons'
+import { BadOutcomeBody } from './BadOutcomeBody'
 import { saveAs } from 'file-saver'
+import { getBadOutcomeHeader } from './utils'
 
 import type {
   RobotCalibrationCheckComparisonsByStep,
+  RobotCalibrationCheckComparison,
   RobotCalibrationCheckInstrument,
 } from '../../sessions/types'
 
 const ROBOT_CALIBRATION_CHECK_SUMMARY_HEADER = 'Calibration check summary:'
 const DROP_TIP_AND_EXIT = 'Drop tip in trash and exit'
 const DOWNLOAD_SUMMARY = 'Download JSON summary'
+const STILL_HAVING_PROBLEMS =
+  'If you are still experiencing issues, please download the JSON summary and share it with our support team who will then follow up with you.'
 
 type ResultsSummaryProps = {|
   deleteSession: () => mixed,
@@ -50,6 +55,18 @@ export function ResultsSummary(props: ResultsSummaryProps): React.Node {
     compStep => Sessions.FIRST_PIPETTE_COMPARISON_STEPS.includes(compStep)
   ).map(stepNames => pick(comparisonsByStep, stepNames))
 
+  const lastFailedComparison = [
+    ...Sessions.FIRST_PIPETTE_COMPARISON_STEPS,
+    ...Sessions.SECOND_PIPETTE_COMPARISON_STEPS,
+  ].reduce((acc, step): RobotCalibrationCheckComparison | null => {
+    const comparison = comparisonsByStep[step]
+    if (comparison && comparison.exceedsThreshold) {
+      return comparison
+    } else {
+      return acc
+    }
+  }, null)
+
   return (
     <>
       <h3 className={styles.summary_page_header}>
@@ -80,7 +97,36 @@ export function ResultsSummary(props: ResultsSummaryProps): React.Node {
       >
         {DOWNLOAD_SUMMARY}
       </OutlineButton>
-      <PrimaryButton onClick={deleteSession}>{DROP_TIP_AND_EXIT}</PrimaryButton>
+      {lastFailedComparison && (
+        <TroubleshootingInstructions comparison={lastFailedComparison} />
+      )}
+
+      <PrimaryButton
+        className={styles.summary_exit_button}
+        onClick={deleteSession}
+      >
+        {DROP_TIP_AND_EXIT}
+      </PrimaryButton>
     </>
+  )
+}
+
+type TroubleshootingInstructionsProps = {
+  comparison: RobotCalibrationCheckComparison,
+}
+function TroubleshootingInstructions(
+  props: TroubleshootingInstructionsProps
+): React.Node {
+  const { comparison } = props
+  return (
+    <div>
+      <p className={styles.summary_bad_outcome_header}>
+        {getBadOutcomeHeader(comparison.transformType)}
+      </p>
+      <p className={styles.summary_bad_outcome_body}>
+        <BadOutcomeBody transform={comparison.transformType} />
+      </p>
+      <p className={styles.summary_bad_outcome_body}>{STILL_HAVING_PROBLEMS}</p>
+    </div>
   )
 }
