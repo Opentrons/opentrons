8f0c70d4f9335e402457b184b8d8ee8205e853ca
amit lissack <amit@opentrons.com>
2020-06-22 10:32:44 -0400

    remove control endpoints

--- api/src/opentrons/server/http.py
+++ api/src/opentrons/server/http.py
@@ -1,19 +1,8 @@
 import logging
 
-from .endpoints import (control)
-
 log = logging.getLogger(__name__)
 
 
 class HTTPServer(object):
     def __init__(self, app):
         self.app = app
-
-        self.app.router.add_get(
-            '/pipettes', control.get_attached_pipettes)
-        self.app.router.add_get(
-            '/robot/positions', control.position_info)
-        self.app.router.add_post(
-            '/robot/move', control.move)
-        self.app.router.add_post(
-            '/robot/home', control.home)
