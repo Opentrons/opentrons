8c9a9d2b33a6919cdce52453c948734f4f17a440
Laura Cox <31892318+Laura-Danielle@users.noreply.github.com>
2020-06-23 14:27:26 -0400

    refactor(robot-server): Utilize temporary directories for I/O operations (#5979)

--- robot-server/tests/conftest.py
+++ robot-server/tests/conftest.py
@@ -1,6 +1,9 @@
 import subprocess
 import time
 import sys
+import tempfile
+import os
+import shutil
 from unittest.mock import MagicMock
 
 import pytest
@@ -8,6 +11,7 @@ from starlette.testclient import TestClient
 from robot_server.service.app import app
 from robot_server.service.dependencies import get_hardware
 from opentrons.hardware_control import API, HardwareAPILike
+from opentrons import config
 
 
 @pytest.fixture
@@ -27,28 +31,45 @@ def api_client(hardware) -> TestClient:
 
 
 @pytest.fixture(scope="session")
-def run_server():
+def server_temp_directory():
+    new_dir = tempfile.mkdtemp()
+    os.environ['OT_API_CONFIG_DIR'] = new_dir
+    config.reload()
+
+    yield new_dir
+    shutil.rmtree(new_dir)
+
+    del os.environ['OT_API_CONFIG_DIR']
+
+
+@pytest.fixture(scope="session")
+def run_server(server_temp_directory):
     with subprocess.Popen([sys.executable, "-m", "robot_server.main"],
-                          env={'OT_ROBOT_SERVER_DOT_ENV_PATH': 'test.env'},
+                          env={'OT_ROBOT_SERVER_DOT_ENV_PATH': "test.env",
+                               'OT_API_CONFIG_DIR': server_temp_directory},
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE) as proc:
         # Wait for a bit to get started
-        time.sleep(2)
+        # TODO (lc, 23-06-2020) We should investigate
+        # using symlinks for the file copy to avoid
+        # having such a long sleep
+        time.sleep(15)
         yield proc
         proc.kill()
 
 
 @pytest.fixture
-def attach_pipettes():
+def attach_pipettes(server_temp_directory):
     import json
-    import os
+
     pipette = {
         "dropTipShake": True,
         "model": "p300_multi_v1"
     }
-    pipette_file_path = os.path.join(
-        os.path.expanduser('~'), '.opentrons/pipettes', 'testpipette01.json'
-    )
+
+    pipette_dir_path = os.path.join(server_temp_directory, 'pipettes')
+    pipette_file_path = os.path.join(pipette_dir_path, 'testpipette01.json')
+
     with open(pipette_file_path, 'w') as pipette_file:
         json.dump(pipette, pipette_file)
     yield
