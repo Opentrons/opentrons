Opentrons/opentrons

commit 2bb54a999d682da246884c87bcf29075058878a3

refactor(app): wire deck cal status API to UI  (#5962)
This PR wires GET /calibration/status to the robot controls card and associated UI.


Note: Tooltips are not working because out Button components still can't display a tooltip if they're disabled, because disabling them sets pointer-events: none in CSS. I will try to follow up and I have written this UI + tests to show tooltips as soon as that is fixed. Give the code a look though for that tooltip copy.

Co-authored-by: Seth Foster <seth@opentrons.com>

app/src/sessions/calibration-check/constants.js
@@ -66,7 +66,7 @@ export const checkCommands = {
 
 export const CHECK_TRANSFORM_TYPE_INSTRUMENT_OFFSET = 'BAD_INSTRUMENT_OFFSET'
 export const CHECK_TRANSFORM_TYPE_UNKNOWN = 'UNKNOWN'
-export const CHECK_TRANSFORM_TYPE_DECK = 'BAD_DECK_TRANSFORM'
+export const CHECK_TRANSFORM_TYPE_DECK = 'BAD_DECK_CAL_STATUS'
 
 export const CHECK_PIPETTE_RANK_FIRST: 'first' = 'first'
 export const CHECK_PIPETTE_RANK_SECOND: 'second' = 'second'

app/src/calibration/constants.js
@@ -1,0 +1,19 @@
+// @flow
+
+export const DECK_CAL_STATUS_OK: 'OK' = 'OK'
+export const DECK_CAL_STATUS_IDENTITY: 'IDENTITY' = 'IDENTITY'
+export const DECK_CAL_STATUS_BAD_CALIBRATION: 'BAD_CALIBRATION' =
+  'BAD_CALIBRATION'
+export const DECK_CAL_STATUS_SINGULARITY: 'SINGULARITY' = 'SINGULARITY'
+
+export const CALIBRATION_STATUS_PATH: '/calibration/status' =
+  '/calibration/status'
+
+export const FETCH_CALIBRATION_STATUS: 'calibration:FETCH_CALIBRATION_STATUS' =
+  'calibration:FETCH_CALIBRATION_STATUS'
+
+export const FETCH_CALIBRATION_STATUS_SUCCESS: 'calibration:FETCH_CALIBRATION_STATUS_SUCCESS' =
+  'calibration:FETCH_CALIBRATION_STATUS_SUCCESS'
+
+export const FETCH_CALIBRATION_STATUS_FAILURE: 'calibration:FETCH_CALIBRATION_STATUS_FAILURE' =
+  'calibration:FETCH_CALIBRATION_STATUS_FAILURE'

app/src/calibration/__fixtures__/calibration-status.js
@@ -1,0 +1,49 @@
+// @flow
+import { GET } from '../../robot-api'
+import {
+  makeResponseFixtures,
+  mockFailureBody,
+} from '../../robot-api/__fixtures__'
+import { CALIBRATION_STATUS_PATH, DECK_CAL_STATUS_IDENTITY } from '../constants'
+
+import type { ResponseFixtures } from '../../robot-api/__fixtures__'
+import type { CalibrationStatus } from '../types'
+
+export const mockCalibrationStatus: CalibrationStatus = {
+  deckCalibration: {
+    status: DECK_CAL_STATUS_IDENTITY,
+    data: [
+      [1.0, 0.0, 0.0, 0.0],
+      [0.0, 1.0, 0.0, 0.0],
+      [0.0, 0.0, 1.0, 0.0],
+      [0.0, 0.0, 0.0, 1.0],
+    ],
+  },
+  instrumentCalibration: {
+    right: {
+      single: [0.0, 0.0, 0.0],
+      multi: [0.0, 0.0, 0.0],
+    },
+    left: {
+      single: [0.0, 0.0, 0.0],
+      multi: [0.0, 0.0, 0.0],
+    },
+  },
+}
+
+export const {
+  successMeta: mockFetchCalibrationStatusSuccessMeta,
+  failureMeta: mockFetchCalibrationStatusFailureMeta,
+  success: mockFetchCalibrationStatusSuccess,
+  failure: mockFetchCalibrationStatusFailure,
+}: ResponseFixtures<
+  CalibrationStatus,
+  {| message: string |}
+> = makeResponseFixtures({
+  method: GET,
+  path: CALIBRATION_STATUS_PATH,
+  successStatus: 200,
+  successBody: mockCalibrationStatus,
+  failureStatus: 500,
+  failureBody: mockFailureBody,
+})

app/src/calibration/__fixtures__/index.js
@@ -1,0 +1,2 @@
+// @flow
+export * from './calibration-status'

app/src/calibration/__tests__/actions.test.js
@@ -1,0 +1,67 @@
+// @flow
+
+import * as Fixtures from '../__fixtures__'
+import * as Actions from '../actions'
+import type { CalibrationAction } from '../types'
+
+type ActionSpec = {|
+  should: string,
+  creator: (...Array<any>) => CalibrationAction,
+  args: Array<mixed>,
+  expected: CalibrationAction,
+|}
+
+const SPECS: Array<ActionSpec> = [
+  {
+    should: 'create a fetchCalibrationStatus action',
+    creator: Actions.fetchCalibrationStatus,
+    args: ['robot-name'],
+    expected: {
+      type: 'calibration:FETCH_CALIBRATION_STATUS',
+      payload: { robotName: 'robot-name' },
+      meta: {},
+    },
+  },
+  {
+    should: 'create a fetchCalibrationStatusSuccess action',
+    creator: Actions.fetchCalibrationStatusSuccess,
+    args: [
+      'robot-name',
+      Fixtures.mockFetchCalibrationStatusSuccess.body,
+      { requestId: '123' },
+    ],
+    expected: {
+      type: 'calibration:FETCH_CALIBRATION_STATUS_SUCCESS',
+      payload: {
+        robotName: 'robot-name',
+        calibrationStatus: Fixtures.mockCalibrationStatus,
+      },
+      meta: { requestId: '123' },
+    },
+  },
+  {
+    should: 'create a fetchCalibrationStatusFailure action',
+    creator: Actions.fetchCalibrationStatusFailure,
+    args: [
+      'robot-name',
+      Fixtures.mockFetchCalibrationStatusFailure.body,
+      { requestId: '123' },
+    ],
+    expected: {
+      type: 'calibration:FETCH_CALIBRATION_STATUS_FAILURE',
+      payload: {
+        robotName: 'robot-name',
+        error: Fixtures.mockFetchCalibrationStatusFailure.body,
+      },
+      meta: { requestId: '123' },
+    },
+  },
+]
+
+describe('calibration actions', () => {
+  SPECS.forEach(({ should, creator, args, expected }) => {
+    it(should, () => {
+      expect(creator(...args)).toEqual(expected)
+    })
+  })
+})

app/src/calibration/__tests__/reducer.test.js
@@ -1,0 +1,19 @@
+// @flow
+
+import * as Fixtures from '../__fixtures__'
+import * as Actions from '../actions'
+import { calibrationReducer } from '../reducer'
+
+describe('calibration reducer', () => {
+  it('should handle a FETCH_CALIBRATION_STATUS_SUCCESS', () => {
+    const action = Actions.fetchCalibrationStatusSuccess(
+      'robot-name',
+      Fixtures.mockCalibrationStatus,
+      {}
+    )
+
+    expect(calibrationReducer({}, action)).toEqual({
+      'robot-name': { calibrationStatus: Fixtures.mockCalibrationStatus },
+    })
+  })
+})

app/src/calibration/__tests__/selectors.test.js
@@ -1,0 +1,44 @@
+// @flow
+
+import * as Fixtures from '../__fixtures__'
+import * as Selectors from '../selectors'
+
+import type { State } from '../../types'
+
+describe('calibration selectors', () => {
+  describe('getCalibrationStatus', () => {
+    it('should return null if no robot in state', () => {
+      const state: $Shape<State> = { calibration: {} }
+      expect(Selectors.getCalibrationStatus(state, 'robotName')).toBe(null)
+    })
+
+    it('should return status if in state', () => {
+      const state: $Shape<State> = {
+        calibration: {
+          robotName: { calibrationStatus: Fixtures.mockCalibrationStatus },
+        },
+      }
+      expect(Selectors.getCalibrationStatus(state, 'robotName')).toEqual(
+        Fixtures.mockCalibrationStatus
+      )
+    })
+  })
+
+  describe('getDeckCalibrationStatus', () => {
+    it('should return null if no robot in state', () => {
+      const state: $Shape<State> = { calibration: {} }
+      expect(Selectors.getDeckCalibrationStatus(state, 'robotName')).toBe(null)
+    })
+
+    it('should return status if in state', () => {
+      const state: $Shape<State> = {
+        calibration: {
+          robotName: { calibrationStatus: Fixtures.mockCalibrationStatus },
+        },
+      }
+      expect(Selectors.getDeckCalibrationStatus(state, 'robotName')).toEqual(
+        Fixtures.mockCalibrationStatus.deckCalibration.status
+      )
+    })
+  })
+})

app/src/calibration/actions.js
@@ -1,0 +1,37 @@
+// @flow
+
+import * as Constants from './constants'
+import * as Types from './types'
+
+import type {
+  RobotApiRequestMeta,
+  RobotApiErrorResponse,
+} from '../robot-api/types'
+
+export const fetchCalibrationStatus = (
+  robotName: string
+): Types.FetchCalibrationStatusAction => ({
+  type: Constants.FETCH_CALIBRATION_STATUS,
+  payload: { robotName },
+  meta: {},
+})
+
+export const fetchCalibrationStatusSuccess = (
+  robotName: string,
+  calibrationStatus: Types.CalibrationStatus,
+  meta: RobotApiRequestMeta
+): Types.FetchCalibrationStatusSuccessAction => ({
+  type: Constants.FETCH_CALIBRATION_STATUS_SUCCESS,
+  payload: { robotName, calibrationStatus },
+  meta,
+})
+
+export const fetchCalibrationStatusFailure = (
+  robotName: string,
+  error: RobotApiErrorResponse,
+  meta: RobotApiRequestMeta
+): Types.FetchCalibrationStatusFailureAction => ({
+  type: Constants.FETCH_CALIBRATION_STATUS_FAILURE,
+  payload: { robotName, error },
+  meta,
+})

commit 7af86a2e635361459d8c488d98e8d6dd6a7a3be5

fix(app): calcheck: fix display of bad deck transform
Looks like 2bb54a999d682da246884c87bcf29075058878a3 accidentally changed
the value for a bad deck transform value from the api, might have been
an overzealous find and replace. This value should match the value in
opentrons.calibration.helper_classes

app/src/sessions/calibration-check/constants.js
@@ -66,7 +66,7 @@ export const checkCommands = {
 
 export const CHECK_TRANSFORM_TYPE_INSTRUMENT_OFFSET = 'BAD_INSTRUMENT_OFFSET'
 export const CHECK_TRANSFORM_TYPE_UNKNOWN = 'UNKNOWN'
-export const CHECK_TRANSFORM_TYPE_DECK = 'BAD_DECK_CAL_STATUS'
+export const CHECK_TRANSFORM_TYPE_DECK = 'BAD_DECK_TRANSFORM'
 
 export const CHECK_PIPETTE_RANK_FIRST: 'first' = 'first'
 export const CHECK_PIPETTE_RANK_SECOND: 'second' = 'second'