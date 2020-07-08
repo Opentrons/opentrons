cd22f6876c8414151cfcb87693bd83538df2395b
Brian Cooper <brian@allegro.com>
2020-07-08 12:49:43 -0400

    update simple state machine test

--- robot-server/tests/robot/calibration/tip_length/test_util.py
+++ robot-server/tests/robot/calibration/tip_length/test_util.py
@@ -16,61 +16,82 @@ def machine(loop):
               'applyingFinish',
               'admiringWork'}
     transitions = {
-      'imaginingTable': {'millingLumber'},
-      'millingLumber': {'sawingToDimensions'},
-      'sawingToDimensions': {'assemblingTable', 'gluingUp'},
-      'assemblingTable': {'gluingUp', 'sandingDown'},
-      'gluingUp': {'assemblingTable', 'sandingDown'},
-      'sandingDown': {'applyingFinish'},
-      'applyingFinish': {'admiringWork'},
-      'rethinkingDesign': {'sawingToDimensions'},
-      util.WILDCARD: {'rethinkingDesign'}
+        'imaginingTable': {
+            'mill': 'millingLumber'
+        },
+        'millingLumber': {
+            'saw': 'sawingToDimensions'
+        },
+        'sawingToDimensions': {
+            'assemble': 'assemblingTable',
+            'glueUp': 'gluingUp'
+        },
+        'assemblingTable': {
+            'glueUp':'gluingUp',
+            'sand': 'sandingDown'
+        },
+        'gluingUp': {
+            'assemble': 'assemblingTable',
+            'sand': 'sandingDown'
+        },
+        'sandingDown': {
+            'applyFinish': 'applyingFinish'
+        },
+        'applyingFinish': {
+            'admire': 'admiringWork'
+        },
+        'rethinkingDesign': {
+            'saw': 'sawingToDimensions'
+        },
+        util.WILDCARD: {
+            'rethink': 'rethinkingDesign'
+        }
     }
 
     return util.SimpleStateMachine(states=states,
                                    transitions=transitions)
 
 
-valid_transitions: List[Tuple[str, str]] = [
-    ('imaginingTable', 'millingLumber'),
-    ('millingLumber', 'sawingToDimensions'),
-    ('sawingToDimensions', 'gluingUp'),
-    ('sawingToDimensions', 'assemblingTable'),
-    ('assemblingTable', 'gluingUp'),
-    ('assemblingTable', 'sandingDown'),
-    ('gluingUp', 'assemblingTable'),
-    ('gluingUp', 'sandingDown'),
-    ('sandingDown', 'applyingFinish'),
-    ('applyingFinish', 'admiringWork'),
-    ('rethinkingDesign', 'sawingToDimensions'),
-    ('imaginingTable', 'rethinkingDesign'),
-    ('millingLumber', 'rethinkingDesign'),
-    ('sawingToDimensions', 'rethinkingDesign'),
-    ('assemblingTable', 'rethinkingDesign'),
-    ('gluingUp', 'rethinkingDesign'),
-    ('rethinkingDesign', 'rethinkingDesign'),
-    ('sandingDown', 'rethinkingDesign'),
-    ('applyingFinish', 'rethinkingDesign'),
-    ('admiringWork', 'rethinkingDesign'),
+valid_transitions: List[Tuple[str, str, str]] = [
+    ('imaginingTable', 'mill', 'millingLumber'),
+    ('millingLumber', 'saw', 'sawingToDimensions'),
+    ('sawingToDimensions', 'glueUp', 'gluingUp'),
+    ('sawingToDimensions', 'assemble', 'assemblingTable'),
+    ('assemblingTable', 'glueUp', 'gluingUp'),
+    ('assemblingTable', 'sand', 'sandingDown'),
+    ('gluingUp', 'assemble', 'assemblingTable'),
+    ('gluingUp', 'sand', 'sandingDown'),
+    ('sandingDown', 'applyFinish', 'applyingFinish'),
+    ('applyingFinish', 'admire', 'admiringWork'),
+    ('rethinkingDesign', 'saw', 'sawingToDimensions'),
+    ('imaginingTable', 'rethink', 'rethinkingDesign'),
+    ('millingLumber', 'rethink', 'rethinkingDesign'),
+    ('sawingToDimensions', 'rethink', 'rethinkingDesign'),
+    ('assemblingTable', 'rethink', 'rethinkingDesign'),
+    ('gluingUp', 'rethink', 'rethinkingDesign'),
+    ('rethinkingDesign', 'rethink', 'rethinkingDesign'),
+    ('sandingDown', 'rethink', 'rethinkingDesign'),
+    ('applyingFinish', 'rethink', 'rethinkingDesign'),
+    ('admiringWork', 'rethink', 'rethinkingDesign'),
 ]
 
 
-@pytest.mark.parametrize('from_state,to_state', valid_transitions)
-async def test_valid_transitions(from_state, to_state, machine):
-    resulting_state = machine.trigger_transition(from_state, to_state)
+@pytest.mark.parametrize('from_state,command,to_state', valid_transitions)
+async def test_valid_transitions(from_state, command, to_state, machine):
+    resulting_state = machine.get_next_state(from_state, command)
     assert resulting_state == to_state
 
 
 invalid_transitions: List[Tuple[str, str]] = [
-    ('imaginingTable', 'gluingUp'),
-    ('millingLumber', 'imaginingTable'),
-    ('gluingUp', 'millingLumber'),
-    ('admiringWork', 'millingLumber'),
+    ('imaginingTable', 'glue'),
+    ('millingLumber', 'assemble'),
+    ('gluingUp', 'doSomethingUnrelatedToWoodworking'),
+    ('admiringWork', 'mill'),
     ('admiringWork', '*'),
 ]
 
 
-@pytest.mark.parametrize('from_state,to_state', invalid_transitions)
-async def test_invalid_transitions(from_state, to_state, machine):
-    resulting_state = machine.trigger_transition(from_state, to_state)
+@pytest.mark.parametrize('from_state,command', invalid_transitions)
+async def test_invalid_transitions(from_state, command, machine):
+    resulting_state = machine.get_next_state(from_state, command)
     assert resulting_state is None
