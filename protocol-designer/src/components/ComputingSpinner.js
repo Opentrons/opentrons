d1aa3c488d69dd82eb4c021d3dbf2f13756cbe52
IanLondon <ianlondondu@gmail.com>
2020-07-16 13:20:09 -0400

    feat(protocol-designer): add 1s delay to computation spinner

--- protocol-designer/src/components/ComputingSpinner.js
+++ protocol-designer/src/components/ComputingSpinner.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react'
 import { useSelector } from 'react-redux'
+import { css, keyframes } from 'styled-components'
 import {
   Flex,
   Icon,
@@ -10,12 +11,28 @@ import {
 } from '@opentrons/components'
 import * as fileDataSelectors from '../file-data/selectors'
 
+const appearWithDelayAnimation = keyframes`
+  100% {
+    opacity: 1.0;
+  }
+`
+
+const appearWithDelayStyle = css`
+  animation-delay: 1s;
+  animation-duration: 0.1s;
+  animation-fill-mode: forwards;
+  animation-name: ${appearWithDelayAnimation};
+  cursor: wait;
+  opacity: 0;
+`
+
 export const ComputingSpinner = (): React.Node => {
   const showSpinner = useSelector(fileDataSelectors.getTimelineIsBeingComputed)
 
   return (
     showSpinner && (
       <Flex
+        css={appearWithDelayStyle}
         alignItems={ALIGN_CENTER}
         justifyContent={JUSTIFY_CENTER}
         backgroundColor="rgba(115, 115, 115, 0.9)"
