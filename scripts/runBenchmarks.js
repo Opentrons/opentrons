7f387f177d0aa6b9ed22c2613d367da2903fcaaa
IanLondon <ianlondondu@gmail.com>
2020-07-27 10:55:09 -0400

    use globby in script

--- scripts/runBenchmarks.js
+++ scripts/runBenchmarks.js
@@ -1,16 +1,25 @@
-#!/usr/bin/env node
+// script to run benchmark tests given a glob path
+'use strict'
 
+const assert = require('assert')
 require('@babel/register')({
   plugins: ['@babel/plugin-transform-modules-commonjs'],
 })
 
-// NOTE: the following was adapted from nanobench/run.js
+const globby = require('globby')
 // eslint-disable-next-line no-unused-vars
 const bench = require('nanobench')
 const path = require('path')
 
+const USAGE =
+  "\nUsage:\n  node ./scripts/runBenchmarks 'path/to/benchmarks/*.js'"
+assert(process.argv.length === 3, USAGE)
+
+const benchmarkFiles = globby.sync(process.argv[2])
+
+// NOTE: adapted from nanobench/run.js
 global.__NANOBENCH__ = require.resolve('nanobench')
 
-for (let i = 2; i < process.argv.length; i++) {
-  require(path.join(process.cwd(), process.argv[i]))
-}
+benchmarkFiles.forEach(f => {
+  require(path.join(process.cwd(), f))
+})
