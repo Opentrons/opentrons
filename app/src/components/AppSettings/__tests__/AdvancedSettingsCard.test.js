516e9e200e7cf5343c1f20e75834a36d04fa22f8
Brian Cooper <brian@allegro.com>
2020-07-28 15:06:47 -0400

    fix test

--- app/src/components/AppSettings/__tests__/AdvancedSettingsCard.test.js
+++ app/src/components/AppSettings/__tests__/AdvancedSettingsCard.test.js
@@ -69,6 +69,7 @@ describe('AdvancedSettingsCard', () => {
     ])
     getFeatureFlags.mockReturnValue({
       enableBundleUpload: false,
+      enableTipLengthCal: true,
     })
 
     render = () => {
