5e970231a98746ae903674d6780211581990a25b
Seth Foster <seth@opentrons.com>
2020-07-30 14:19:44 -0400

    fixup: guess i forgot to run prettier

--- app/src/sessions/__fixtures__/tip-length-calibration.js
+++ app/src/sessions/__fixtures__/tip-length-calibration.js
@@ -1,6 +1,6 @@
 // @flow
-import {fixture_tiprack_300_ul} from '@opentrons/shared-data/labware/fixtures/2/fixture_tiprack_300_ul'
-import {opentrons_calibrationblock_short_side_left} from '@opentrons/shared-data/labware/definitions/2/opentrons_calibrationblock_short_side_left/1'
+import { fixture_tiprack_300_ul } from '@opentrons/shared-data/labware/fixtures/2/fixture_tiprack_300_ul'
+import { opentrons_calibrationblock_short_side_left } from '@opentrons/shared-data/labware/definitions/2/opentrons_calibrationblock_short_side_left/1'
 import type { TipLengthCalibrationSessionDetails } from '../types'
 import type { TipLengthCalibrationSessionParams } from '../tip-length-calibration/types'
 
@@ -30,7 +30,7 @@ export const mockTipLengthCalibrationSessionDetails: TipLengthCalibrationSession
       version: 1,
       isTiprack: false,
       // $FlowFixMe: this is a fixture and i know i'm right
-      definition: opentrons_calibrationblock_short_side_left
+      definition: opentrons_calibrationblock_short_side_left,
     },
   ],
 }
