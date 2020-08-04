47b83496d3336fb9c565d9ed54616b5c404ae29d
Seth Foster <seth@opentrons.com>
2020-08-04 17:14:53 -0400

    refactor(api): add from string for api level
    We'll have to parse these out of the pipette config dict

--- api/src/opentrons/protocols/types.py
+++ api/src/opentrons/protocols/types.py
@@ -13,6 +13,15 @@ class APIVersion(NamedTuple):
     major: int
     minor: int
 
+    @classmethod
+    def from_string(cls, inp: str) -> 'APIVersion':
+        parts = inp.split('.')
+        if len(parts) != 2:
+            raise ValueError(inp)
+        intparts = [int(p) for p in parts]
+
+        return cls(major=intparts[0], minor=intparts[1])
+
     def __str__(self):
         return f'{self.major}.{self.minor}'
 
