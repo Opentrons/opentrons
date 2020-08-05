f0f71091dcb6134792cef5c8016042883809840f
Mike Cousins <mike@opentrons.com>
2020-08-05 17:49:20 -0400

    fix(app): fix data reads from JSON Protocols later than v3 (#6284)
    Fixes #6235

--- shared-data/protocol/index.js
+++ shared-data/protocol/index.js
@@ -1,22 +1,13 @@
 // @flow
-import type { ProtocolFile as SchemaV1ProtocolFile } from './flowTypes/schemaV1'
-import type { ProtocolFile as SchemaV3ProtocolFile } from './flowTypes/schemaV3'
+import type { ProtocolFile as ProtocolFileV1 } from './flowTypes/schemaV1'
+import type { ProtocolFile as ProtocolFileV3 } from './flowTypes/schemaV3'
+import type { ProtocolFile as ProtocolFileV4 } from './flowTypes/schemaV4'
+import type { ProtocolFile as ProtocolFileV5 } from './flowTypes/schemaV5'
 
-type ProtocolData =
-  | $Shape<SchemaV1ProtocolFile<{}>>
-  | $Shape<SchemaV3ProtocolFile<{}>>
+export type { ProtocolFileV1, ProtocolFileV3, ProtocolFileV4, ProtocolFileV5 }
 
-export function getProtocolSchemaVersion(data: ProtocolData): ?number {
-  // $FlowFixMe: (ka, 2019-06-10): cant differentiate which file schema file is needed
-  if (data.schemaVersion > 3) {
-    console.warn('this is protocol schema version is not yet supported')
-  }
-  if (data.schemaVersion === 3) {
-    return 3
-  } else if (data['protocol-schema'] === '2.0.0') {
-    return 2
-  } else if (data['protocol-schema'] === '1.0.0') {
-    return 1
-  }
-  return null
-}
+export type JsonProtocolFile =
+  | $ReadOnly<$Exact<ProtocolFileV1<{ ... }>>>
+  | $ReadOnly<ProtocolFileV3<{ ... }>>
+  | $ReadOnly<ProtocolFileV4<{ ... }>>
+  | $ReadOnly<ProtocolFileV5<{ ... }>>
