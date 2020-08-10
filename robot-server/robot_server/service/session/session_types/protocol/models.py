63b7175af7a240b56efc8cf94149850f9abf6dda
amit lissack <amit@opentrons.com>
2020-08-10 16:39:19 -0400

    rename executedCommands to commands

--- robot-server/robot_server/service/session/session_types/protocol/models.py
+++ robot-server/robot_server/service/session/session_types/protocol/models.py
@@ -6,4 +6,4 @@ class ProtocolSessionDetails(BaseModel):
     protocolId: str
     currentState: typing.Optional[str]
     # TODO: Amit 8/3/2020 - proper schema for command types
-    executedCommands: typing.List[typing.Any]
+    commands: typing.List[typing.Any]
