6e942ee242cb4016d11c08d627809a1d5c9d4196
laura_danielle <laura@opentrons.com>
2020-08-11 14:29:28 -0400

    Assume uri is loadname if not an actual uri

--- api/src/opentrons/calibration_storage/helpers.py
+++ api/src/opentrons/calibration_storage/helpers.py
@@ -42,8 +42,10 @@ def details_from_uri(uri: str, delimiter='/') -> local_types.UriDetails:
         return local_types.UriDetails(
             namespace=info[0], load_name=info[1], version=int(info[2]))
     else:
+        # Here we are assuming that the 'uri' passed in is actually
+        # the loadname, though sometimes it may be an empty string.
         return local_types.UriDetails(
-            namespace='', load_name='', version=1)
+            namespace='', load_name=uri, version=1)
 
 
 def uri_from_details(namespace: str, load_name: str,
