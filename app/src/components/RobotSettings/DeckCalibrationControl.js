63555ed8b0003382d969f75f189b2db5b74f0bb7
Brian Cooper <brian@allegro.com>
2020-08-12 17:55:51 -0400

    copy over styles

--- app/src/components/RobotSettings/DeckCalibrationControl.js
+++ app/src/components/RobotSettings/DeckCalibrationControl.js
@@ -23,6 +23,7 @@ import {
   getLightsOn,
   ROBOT,
 } from '../../robot-controls'
+import { getFeatureFlags } from '../../config'
 import * as RobotApi from '../../robot-api'
 import * as Sessions from '../../sessions'
 import * as Calibration from '../../calibration'
@@ -34,7 +35,9 @@ import type { State, Dispatch } from '../../types'
 import type { ViewableRobot } from '../../discovery/types'
 import type { DeckCalibrationStatus } from '../../calibration/types'
 
+import { Portal } from '../portal'
 import { TitledControl } from '../TitledControl'
+import { CalibrateDeck } from '../CalibrateDeck'
 import { DeckCalibrationWarning } from './DeckCalibrationWarning'
 
 type Props = {|
@@ -69,36 +72,67 @@ export function DeckCalibrationControl(props: Props): React.Node {
   })
   const requestStatus = requestState?.status ?? null
 
-  const ensureSession = () => {
+  const ff = useSelector(getFeatureFlags)
+
+  React.useEffect(() => {
+    if (requestStatus === RobotApi.SUCCESS) setShowWizard(true)
+  }, [requestStatus])
+
+  const handleStartDeckCalSession = () => {
     dispatchRequest(
-      Sessions.ensureSession(robotName, Sessions.SESSION_TYPE_CALIBRATION_CHECK)
+      Sessions.ensureSession(robotName, Sessions.SESSION_TYPE_DECK_CALIBRATION)
     )
   }
 
-  const ff = true
-  const handleButtonClick = ff
-    ? startLegacyDeckCalibration
-    : startDeckCalibration
+  const deckCalibrationSession = useSelector((state: State) => {
+    const session: Sessions.Session | null = Sessions.getRobotSessionOfType(
+      state,
+      robotName,
+      Sessions.SESSION_TYPE_DECK_CALIBRATION
+    )
+    if (
+      session &&
+      session.sessionType === Sessions.SESSION_TYPE_DECK_CALIBRATION
+    ) {
+      return session
+    }
+    return null
+  })
+
+  const handleButtonClick = ff.enableCalibrationOverhaul
+    ? handleStartDeckCalSession
+    : startLegacyDeckCalibration
 
   return (
-    <TitledControl
-      borderBottom={BORDER_SOLID_LIGHT}
-      title={CALIBRATE_TITLE_TEXT}
-      description={<Text>{CALIBRATE_DECK_DESCRIPTION}</Text>}
-      control={
-        <SecondaryBtn
-          width="9rem"
-          onClick={handleButtonClick}
-          disabled={buttonDisabled}
-        >
-          {CALIBRATE_BUTTON_TEXT}
-        </SecondaryBtn>
-      }
-    >
-      <DeckCalibrationWarning
-        deckCalibrationStatus={deckCalStatus}
-        marginTop={SPACING_2}
-      />
-    </TitledControl>
+    <>
+      <TitledControl
+        borderBottom={BORDER_SOLID_LIGHT}
+        title={CALIBRATE_TITLE_TEXT}
+        description={<Text>{CALIBRATE_DECK_DESCRIPTION}</Text>}
+        control={
+          <SecondaryBtn
+            width="9rem"
+            onClick={handleButtonClick}
+            disabled={buttonDisabled}
+          >
+            {CALIBRATE_BUTTON_TEXT}
+          </SecondaryBtn>
+        }
+      >
+        <DeckCalibrationWarning
+          deckCalibrationStatus={deckCalStatus}
+          marginTop={SPACING_2}
+        />
+      </TitledControl>
+      {showWizard && (
+        <Portal>
+          <CalibrateDeck
+            session={deckCalibrationSession}
+            robotName={robotName}
+            closeWizard={() => setShowWizard(false)}
+          />
+        </Portal>
+      )}
+    </>
   )
 }
