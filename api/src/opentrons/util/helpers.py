586fe901bd4c8c05d4a5cb41b7eb18e61aa2dec5
amit lissack <amit@opentrons.com>
2020-08-12 14:21:16 -0400

    simplify deep_get. lose the '.' string.

--- api/src/opentrons/util/helpers.py
+++ api/src/opentrons/util/helpers.py
@@ -1,38 +1,25 @@
 import typing
 
 
-def deep_get(obj: typing.Union[typing.Dict, typing.List, typing.Tuple],
-             key: typing.Union[str, typing.Sequence[typing.Any]],
+def deep_get(obj: typing.Union[typing.Mapping, typing.Sequence],
+             key: typing.Sequence[typing.Union[str, int]],
              default=None):
     """
     Utility to get deeply nested element in a list, tuple or dict without
      resorting to some_dict.get('k1', {}).get('k2', {}).get('k3', {})....etc.
 
     :param obj: A dict, list, or tuple
-    :param key: collection of keys or a string of keys separated by '.'
+    :param key: collection of keys
     :param default: the default to return on error
     :return: value or default
     """
     if not key:
         return default
 
-    if isinstance(key, str):
-        key = key.split('.')
-
     for k in key:
-        if isinstance(obj, dict):
-            try:
-                obj = obj[k]
-            except KeyError:
-                return default
-        elif isinstance(obj, (list, tuple,)):
-            try:
-                # Convert to int only if key is a string.
-                k = int(k) if isinstance(k, str) else k
-                obj = obj[k]
-            except (TypeError, IndexError):
-                return default
-        else:
+        try:
+            obj = obj[k]  # type: ignore
+        except (KeyError, TypeError, IndexError):
             return default
 
     return obj
--- api/tests/opentrons/util/test_helpers.py
+++ api/tests/opentrons/util/test_helpers.py
@@ -23,21 +23,21 @@ def deep_dict():
 @pytest.mark.parametrize(argnames="key,expected_result",
                          argvalues=[
                              # Success cases
-                             ['a', 1],
-                             ['b.2', 3],
-                             ['c.a.1', 5],
-                             ['c.b.d', 4],
-                             ['d.0.a.2', 9],
                              [['a'], 1],
-                             [['b', '2'], 3],
+                             [['b', 2], 3],
                              [['c', 'a', 1], 5],
                              [['c', 'b', 'd'], 4],
-                             [['d', '0', 'a', 2], 9],
+                             [['d', 0, 'a', 2], 9],
+                             [('b',), [1, 2, 3]],
                              # Failure cases
                              [None, None],
                              [[], None],
-                             # look out that float isn't interpreted as index
-                             [['b', 1.3], None],
+                             # Index out of range
+                             [['b', 22], None],
+                             # String index
+                             [['b', "23"], None],
+                             # key error
+                             [["e"], None],
                          ])
 def test_deep_get(deep_dict, key, expected_result):
     assert helpers.deep_get(deep_dict, key) == expected_result
