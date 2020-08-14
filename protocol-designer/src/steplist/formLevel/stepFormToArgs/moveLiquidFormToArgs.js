fb77c53b5063d3746c465aaa84517bfd9020460b
Shlok Amin <shlokamin@me.com>
2020-08-14 09:29:42 -0400

    fix merge conflicts

--- protocol-designer/src/steplist/formLevel/stepFormToArgs/moveLiquidFormToArgs.js
+++ protocol-designer/src/steplist/formLevel/stepFormToArgs/moveLiquidFormToArgs.js
@@ -27,13 +27,8 @@ export function getDelayData(
   checkboxField: 'aspirate_delay_checkbox' | 'dispense_delay_checkbox',
   secondsField: 'aspirate_delay_seconds' | 'dispense_delay_seconds',
   mmFromBottomField:
-<<<<<<< HEAD
     | 'aspirate_delay_mmFromBottom'
     | 'dispense_delay_mmFromBottom'
-=======
-    | 'aspirate_delay_tip_position'
-    | 'dispense_delay_tip_position'
->>>>>>> feat(protocol-designer): implement formToArgs for delays
 ): ?InnerDelayArgs {
   const checkbox = hydratedFormData[checkboxField]
   const seconds = hydratedFormData[secondsField]
@@ -186,22 +181,14 @@ export const moveLiquidFormToArgs = (
     fields,
     'aspirate_delay_checkbox',
     'aspirate_delay_seconds',
-<<<<<<< HEAD
     'aspirate_delay_mmFromBottom'
-=======
-    'aspirate_delay_tip_position'
->>>>>>> feat(protocol-designer): implement formToArgs for delays
   )
 
   const dispenseDelay = getDelayData(
     fields,
     'dispense_delay_checkbox',
     'dispense_delay_seconds',
-<<<<<<< HEAD
     'dispense_delay_mmFromBottom'
-=======
-    'dispense_delay_tip_position'
->>>>>>> feat(protocol-designer): implement formToArgs for delays
   )
 
   const blowoutLocation =
