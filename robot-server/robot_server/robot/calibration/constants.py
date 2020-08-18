c139fd4335669bf7b3e9569ce4806f68e5056562
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2020-08-18 17:08:23 -0400

    make 20ul tiprack as the default for p20 pips

--- robot-server/robot_server/robot/calibration/constants.py
+++ robot-server/robot_server/robot/calibration/constants.py
@@ -35,9 +35,9 @@ TIP_RACK_LOOKUP_BY_MAX_VOL: Dict[str, LabwareLookUp] = {
             FILTERTIPRACK_10,
             FILTERTIPRACK_20}),
     '20': LabwareLookUp(
-        load_name=TIPRACK_10,
+        load_name=TIPRACK_20,
         alternatives={
-            TIPRACK_20,
+            TIPRACK_10,
             FILTERTIPRACK_10,
             FILTERTIPRACK_20}),
     '50': LabwareLookUp(
