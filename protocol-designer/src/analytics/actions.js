19596398b5a140ed12245577730ac3e4bcf60329
Ian London <ianlondondu@gmail.com>
2020-09-15 11:44:45 -0400

    feat(protocol-designer): setup Mixpanel and add initial events (#6511)
    Closes #6057
    
    * Add Mixpanel to PD
    * Add appVersion and appName super properties
    * Add appOpen and saveStep analytics action

--- protocol-designer/src/analytics/actions.js
+++ protocol-designer/src/analytics/actions.js
@@ -1,5 +1,7 @@
 // @flow
 import { initializeFullstory, shutdownFullstory } from './fullstory'
+import { setMixpanelTracking } from './mixpanel'
+import type { AnalyticsEvent } from './mixpanel'
 
 export type SetOptIn = {|
   type: 'SET_OPT_IN',
@@ -10,8 +12,10 @@ const _setOptIn = (payload: $PropertyType<SetOptIn, 'payload'>): SetOptIn => {
   // side effects
   if (payload) {
     initializeFullstory()
+    setMixpanelTracking(true)
   } else {
     shutdownFullstory()
+    setMixpanelTracking(false)
   }
 
   return {
@@ -22,3 +26,22 @@ const _setOptIn = (payload: $PropertyType<SetOptIn, 'payload'>): SetOptIn => {
 
 export const optIn = (): SetOptIn => _setOptIn(true)
 export const optOut = (): SetOptIn => _setOptIn(false)
+
+export type AnalyticsEventAction = {|
+  type: 'ANALYTICS_EVENT',
+  payload: AnalyticsEvent,
+|}
+
+// NOTE: this action creator should only be used for special cases where you want to
+// report an analytics event but you do not have any Redux action that sensibly represents
+// that analytics event.
+//
+// When there *is* a Redux action associated with what you want to report to analytics,
+// use the analytics middleware (usually this means adding a case to
+// `reduxActionToAnalyticsEvent` fn) and don't dispatch ANALYTICS_EVENT.
+//
+// PS: ANALYTICS_EVENT action is effected by the analytics middleware anyway, because
+// we need to read opt-in status from the Redux state.
+export const analyticsEvent = (
+  payload: AnalyticsEvent
+): AnalyticsEventAction => ({ type: 'ANALYTICS_EVENT', payload })
