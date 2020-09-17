c1988804ddeb29bed9d88ed03492c2a89d1c18be
amit lissack <amit@opentrons.com>
2020-09-17 11:35:55 -0400

    fill out unit tests for command routing.

--- robot-server/robot_server/service/session/session_types/live_protocol/command_interface.py
+++ robot-server/robot_server/service/session/session_types/live_protocol/command_interface.py
@@ -1,8 +1,16 @@
+from opentrons import ThreadManager
+
 from robot_server.service.session import models
 from robot_server.service.session.errors import UnsupportedCommandException
+from robot_server.service.session.session_types.live_protocol.state_store import \
+    StateStore
 
 
 class CommandInterface:
+
+    def __init__(self, hardware: ThreadManager, state_store: StateStore):
+        self._hardware = hardware
+        self._state_store = state_store
 
     async def handle_load_labware(self, command: models.LoadLabwareRequest) -> models.LoadLabwareResponse:
         raise UnsupportedCommandException("")
--- robot-server/tests/service/session/session_types/live_protocol/test_command_executor.py
+++ robot-server/tests/service/session/session_types/live_protocol/test_command_executor.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock
 import pytest
 
+from robot_server.service.legacy.models.control import Mount
 from robot_server.service.session.command_execution.command import \
     CommandContent
 from robot_server.service.session.session_types.live_protocol.command_executor\
@@ -35,8 +36,10 @@ def command_executor(mock_command_interface) -> LiveProtocolCommandExecutor:
 
 
 async def test_load_labware(command_executor, mock_command_interface):
+    expected_response = models.LoadLabwareResponse(labwareId="your labware")
+
     async def handler(command):
-        return models.LoadLabwareResponse(labwareId="your labware")
+        return expected_response
 
     mock_command_interface.handle_load_labware.side_effect = handler
 
@@ -51,10 +54,96 @@ async def test_load_labware(command_executor, mock_command_interface):
             data=command))
     )
 
-    assert result.result.data == models.LoadLabwareResponse(
-        labwareId="your labware"
-    )
+    mock_command_interface.handle_load_labware.assert_called_once_with(command)
+
+    assert result.result.data == expected_response
     assert result.content == CommandContent(
             name=models.EquipmentCommand.load_labware,
             data=command)
+
+
+async def test_load_instrument(command_executor, mock_command_interface):
+    expected_response = models.LoadInstrumentResponse(
+        instrumentId="your instrument")
+
+    async def handler(command):
+        return expected_response
+
+    mock_command_interface.handle_load_instrument.side_effect = handler
+
+    command = models.LoadInstrumentRequest(instrumentName="instrument name",
+                                           mount=Mount.left)
+    result = await command_executor.execute(
+        Command(content=CommandContent(
+            name=models.EquipmentCommand.load_instrument,
+            data=command))
+    )
+
+    mock_command_interface.handle_load_instrument.assert_called_once_with(command)
+
+    assert result.result.data == expected_response
+    assert result.content == CommandContent(
+            name=models.EquipmentCommand.load_instrument,
+            data=command)
+
+
+@pytest.mark.parametrize(argnames=['handler_name', 'command_type'],
+                         argvalues=[
+                             ['handle_aspirate', models.PipetteCommand.aspirate],
+                             ['handle_dispense', models.PipetteCommand.dispense]
+                         ])
+async def test_liquid_commands(command_executor, mock_command_interface,
+                               handler_name, command_type):
+    async def handler(command):
+        return None
+
+    command_data = models.LiquidRequest(
+        pipetteId="", labwareId="", wellId="",
+        flowRate=2, volume=1, offsetFromBottom=2)
+
+    mock_command_interface.handle_aspirate.side_effect = handler
+    mock_command_interface.handle_dispense.side_effect = handler
+
+    result = await command_executor.execute(
+        Command(content=CommandContent(
+            name=command_type,
+            data=command_data))
+    )
+
+    getattr(mock_command_interface, handler_name).assert_called_once_with(command_data)
+
+    assert result.result.data is None
+    assert result.content == CommandContent(
+            name=command_type,
+            data=command_data)
+
+
+@pytest.mark.parametrize(argnames=['handler_name', 'command_type'],
+                         argvalues=[
+                             ['handle_pick_up_tip', models.PipetteCommand.pick_up_tip],
+                             ['handle_drop_tip', models.PipetteCommand.drop_tip]
+                         ])
+async def test_tip_commands(command_executor, mock_command_interface,
+                            handler_name, command_type):
+    async def handler(command):
+        return None
+
+    command_data = models.PipetteRequestBase(
+        pipetteId="", labwareId="", wellId="")
+
+    mock_command_interface.handle_pick_up_tip.side_effect = handler
+    mock_command_interface.handle_drop_tip.side_effect = handler
+
+    result = await command_executor.execute(
+        Command(content=CommandContent(
+            name=command_type,
+            data=command_data))
+    )
+
+    getattr(mock_command_interface, handler_name).assert_called_once_with(command_data)
+
+    assert result.result.data is None
+    assert result.content == CommandContent(
+            name=command_type,
+            data=command_data)
 
