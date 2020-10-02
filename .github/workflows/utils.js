b80706afd2a2724904c3f1537abc603cec806a71
Seth Foster <seth@opentrons.com>
2020-10-02 17:46:06 -0400

    chore(build): switch to github workflows for robot stack CI (#6632)
    Add github workflow definitions for all tasks related to robot stack
    checks, tests, builds, and deploys that were not previously configured.
    
    Specifically,
    - The api-lint-test-build and shared-data-lint-test-build workflows now deploy to pypi when necessary (and were renamed)
    - New workflow js-checks run javascript check actions on any javascript
    change, since the checkers do not reasonably work on project subsets
    - New workflow app-test-build-deploy runs js tests for the app and its
    dependencies, cross-platform when necessary; it also builds the app,
    with full signing support, and deploys it as travis does
    
    In addition, appveyor has been fully removed and travis now only handles non-robot-stack workflows.

---
+++ .github/workflows/utils.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { execSync } = require('child_process')
+
+module.exports = {
+  /*
+   * Build the environment variables OT_BRANCH, OT_TAG, and OT_BUILD used in
+   * various other build tasks and set them in the environment for use after
+   * the current step ends.
+   *
+   * param core: The github core package instance made available by actions/github-script
+   * param context: The github context instance made available by actions/github-script
+   */
+  buildComplexEnvVars: (core, context) => {
+    const branch = context.ref.replace(/^refs\/(?:tags|heads)\//, '')
+    const tag = context.ref.startsWith('refs/tags/') ? branch : ''
+    const buildNumber = context.runNumber + 10000
+    console.log(
+      `OT_BRANCH: ${branch}\nOT_TAG: ${tag}\nOT_BUILD: ${buildNumber}\n`
+    )
+    core.exportVariable('OT_BRANCH', branch)
+    core.exportVariable('OT_TAG', tag)
+    core.exportVariable('OT_BUILD', buildNumber)
+  },
+}
