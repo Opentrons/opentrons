4ab2d3a2b0af18e888085ce66c2d71f6b64c05f2
Kadee80 <kaa328@nyu.edu>
2020-10-02 13:46:14 -0400

    fixup: Add text to localization

--- labware-library/src/components/LabwareList/CustomLabwareCard.js
+++ labware-library/src/components/LabwareList/CustomLabwareCard.js
@@ -3,18 +3,25 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { getPublicPath } from '../../public-path'
+import {
+  CUSTOM_LABWARE_PROMPT_NO_RESULTS,
+  CUSTOM_LABWARE_PROMPT_W_RESULTS,
+  CUSTOM_LABWARE_SUPPORT_BTN,
+  LABWARE_CREATOR_BTN,
+} from '../../localization'
 import styles from './styles.css'
 
 type Props = {
   isResultsEmpty?: boolean,
 }
+
 const SUPPORT_URL =
   'https://support.opentrons.com/en/articles/3136504-creating-custom-labware-definitions'
 
 export function CustomLabwareCard(props: Props): React.Node {
   const PROMPT_TEXT = props.isResultsEmpty
-    ? 'No results found'
-    : "Don't see your labware here?"
+    ? CUSTOM_LABWARE_PROMPT_NO_RESULTS
+    : CUSTOM_LABWARE_PROMPT_W_RESULTS
   return (
     <li className={styles.custom_labware_card}>
       <span className={styles.custom_labware_text}>{PROMPT_TEXT}</span>
@@ -25,10 +32,10 @@ export function CustomLabwareCard(props: Props): React.Node {
         target="_blank"
         rel="noopener noreferrer"
       >
-        Learn more about creating custom definitions
+        {CUSTOM_LABWARE_SUPPORT_BTN}
       </a>
       <Link to={`${getPublicPath()}create`} className={styles.btn_blue}>
-        Go to the labware creator
+        {LABWARE_CREATOR_BTN}
       </Link>
     </li>
   )
