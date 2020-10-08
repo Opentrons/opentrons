34dfe7cce0596c1d299bd01961cf6caac9b40827
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2020-10-08 14:13:00 -0400

    refactor(app): tip length calibration data card cleanup (#6702)
    * refactor(app): tip length calibration data card cleanup
    
    closes #6593
    
    * add a todo
    
    * remove calibrated this session prop

---
+++ app/src/components/CalibratePanel/__tests__/TipLengthCalibrationData.test.js
@@ -0,0 +1,45 @@
+// @flow
+import * as React from 'react'
+import { mount } from 'enzyme'
+
+import { TipLengthCalibrationData } from '../TipLengthCalibrationData'
+
+describe('TipLengthCalibrationData', () => {
+  let render
+
+  beforeEach(() => {
+    render = (props = {}) => {
+      const { calibrationData = null, calDataAvailable = true } = props
+      return mount(
+        <TipLengthCalibrationData
+          calibrationData={calibrationData}
+          calDataAvailable={calDataAvailable}
+        />
+      )
+    }
+  })
+
+  it('displays not calibrated if no existing data and not calibrated in this session', () => {
+    const wrapper = render()
+    expect(wrapper.text().includes('Not yet calibrated')).toBe(true)
+  })
+
+  it('displays existing data if present and not calibrated in this session', () => {
+    const wrapper = render({
+      calibrationData: {
+        tipLength: 30,
+        tiprack: 'tiprack',
+        pipette: 'pip',
+        lastModified: 'time',
+      },
+    })
+    expect(wrapper.text().includes('Existing data')).toBe(true)
+  })
+
+  it('displays calibration data n/a when labware is calDataAvailable', () => {
+    const wrapper = render({
+      calDataAvailable: false,
+    })
+    expect(wrapper.text().includes('Calibration Data N/A')).toBe(true)
+  })
+})
