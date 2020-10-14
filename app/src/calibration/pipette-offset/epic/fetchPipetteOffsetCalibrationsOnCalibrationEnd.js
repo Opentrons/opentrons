a3ff5c68f4c70f017c61356c1358bbda64954505
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2020-10-14 00:28:13 -0400

    fetch calibations on session delete requests instead of delete success responses

--- app/src/calibration/pipette-offset/epic/fetchPipetteOffsetCalibrationsOnCalibrationEnd.js
+++ app/src/calibration/pipette-offset/epic/fetchPipetteOffsetCalibrationsOnCalibrationEnd.js
@@ -5,18 +5,19 @@ import { ofType } from 'redux-observable'
 
 import { getConnectedRobotName } from '../../../robot/selectors'
 import * as Actions from '../actions'
-import type { Epic } from '../../../types'
+import type { Epic, State } from '../../../types'
 
 import type { SessionType } from '../../../sessions/types'
 import {
-  DELETE_SESSION_SUCCESS,
+  DELETE_SESSION,
   SESSION_TYPE_CALIBRATION_CHECK,
   SESSION_TYPE_TIP_LENGTH_CALIBRATION,
   SESSION_TYPE_DECK_CALIBRATION,
   SESSION_TYPE_PIPETTE_OFFSET_CALIBRATION,
 } from '../../../sessions/constants'
+import { getRobotSessionById } from '../../../sessions/selectors'
 
-const sessionIncursRefetch: SessionType => boolean = sessionType =>
+const isTargetSessionType: SessionType => boolean = sessionType =>
   [
     SESSION_TYPE_CALIBRATION_CHECK,
     SESSION_TYPE_TIP_LENGTH_CALIBRATION,
@@ -24,17 +25,35 @@ const sessionIncursRefetch: SessionType => boolean = sessionType =>
     SESSION_TYPE_PIPETTE_OFFSET_CALIBRATION,
   ].includes(sessionType)
 
+const sessionIncursRefetch: (
+  state: State,
+  robotName: string,
+  sessionId: string
+) => boolean = (state, robotName, sessionId) => {
+  const sessionType =
+    getRobotSessionById(state, robotName, sessionId)?.sessionType || null
+  if (sessionType) {
+    return isTargetSessionType(sessionType)
+  } else {
+    return false
+  }
+}
+
 export const fetchPipetteOffsetCalibrationsOnCalibrationEndEpic: Epic = (
   action$,
   state$
 ) => {
   return action$.pipe(
-    ofType(DELETE_SESSION_SUCCESS),
-    withLatestFrom(state$, (a, s) => [a, getConnectedRobotName(s)]),
+    ofType(DELETE_SESSION),
+    withLatestFrom(state$, (a, s) => [
+      a,
+      s,
+      getConnectedRobotName(s),
+      a.payload.sessionId,
+    ]),
     filter(
-      ([action, robotName]) =>
-        sessionIncursRefetch(action.payload.data.attributes.sessionType) &&
-        robotName != null
+      ([action, state, robotName, sessionId]) =>
+        robotName != null && sessionIncursRefetch(state, robotName, sessionId)
     ),
     map(robotName => {
       return Actions.fetchPipetteOffsetCalibrations(robotName)
