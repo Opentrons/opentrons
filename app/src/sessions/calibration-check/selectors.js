c6a7134fb793c7f1cf845f496e9898a8cd65cbfc
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2020-10-14 16:04:03 -0400

    refactor(app): fixup: remove react redux mock in calibrate tip length control test

---
+++ app/src/sessions/calibration-check/selectors.js
@@ -0,0 +1,23 @@
+// @flow
+import type { State } from '../../types'
+import { SESSION_TYPE_CALIBRATION_CHECK } from '../constants'
+import type { Session, CalibrationCheckSession } from '../types'
+import { getRobotSessionOfType } from '../selectors'
+
+export const getCalibrationCheckSession: (
+  state: State,
+  robotName: string
+) => CalibrationCheckSession | null = (state, robotName) => {
+  const calCheckSession: Session | null = getRobotSessionOfType(
+    state,
+    robotName,
+    SESSION_TYPE_CALIBRATION_CHECK
+  )
+  if (
+    calCheckSession &&
+    calCheckSession.sessionType === SESSION_TYPE_CALIBRATION_CHECK
+  ) {
+    return calCheckSession
+  }
+  return null
+}
