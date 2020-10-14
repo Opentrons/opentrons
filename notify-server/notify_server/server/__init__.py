9b2249ed3e4c69a3028537950609013665a6bee2
amit lissack <amit@opentrons.com>
2020-10-14 10:07:44 -0400

    add servers

---
+++ notify-server/notify_server/server/__init__.py
@@ -0,0 +1,68 @@
+import logging
+import asyncio
+from asyncio import Queue
+
+from zmq.asyncio import Context
+import zmq
+
+
+log = logging.getLogger(__name__)
+
+
+async def _publisher_server_task(address: str, queue: Queue) -> None:
+    """
+    A task that reads multipart messages: topic, data. Received messages
+    are put into the queue.
+
+    :param address: The address to bind to.
+    :param queue: Queue for received messages.
+    :return: None
+    """
+    log.info("Entering publisher server: %s", address)
+
+    ctx = Context()
+    sock = ctx.socket(zmq.PULL)
+
+    sock.bind(address)
+
+    while True:
+        m = await sock.recv_multipart()
+        log.debug("Event: %s", m)
+        await queue.put(m)
+
+
+async def _subscriber_server_task(address: str, queue: Queue) -> None:
+    """
+    A task that publishes messages to subscribers.
+
+    :param address: The address to bind to
+    :param queue: The queue of multipart messages to send
+    :return: None
+    """
+    log.info("Entering subscriber server: %s", address)
+
+    ctx = Context()
+    sock = ctx.socket(zmq.PUB)
+
+    sock.bind(address)
+
+    while True:
+        s = await queue.get()
+        log.debug("Publishing: %s", s)
+        await sock.send_multipart(s)
+
+
+async def run(settings) -> None:
+    """
+    Run the server tasks. Will not return.
+
+    :param settings: Settings object
+    :return: None
+    """
+    queue = Queue()
+
+    t1 = asyncio.create_task(_subscriber_server_task(settings.subscriber_address, queue))
+    t2 = asyncio.create_task(_publisher_server_task(settings.publisher_address, queue))
+
+    await t1
+    await t2
