b710b926aed0f52e93049111595e3dd01fd66137
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2020-10-15 17:54:23 -0400

    refactor(app): display pipette offset calibration data in file info (#6791)
    * refactor(app): display pipette offset calibration data in file info
    
    closes #6767
    
    * made requested changes
    
    * fix flow error after reabse

--- app/src/components/FileInfo/InstrumentItem.js
+++ app/src/components/FileInfo/InstrumentItem.js
@@ -1,9 +1,29 @@
 // @flow
 import * as React from 'react'
-import { Icon } from '@opentrons/components'
+import {
+  Box,
+  Icon,
+  Flex,
+  Text,
+  ALIGN_CENTER,
+  C_DARK_GRAY,
+  COLOR_ERROR,
+  FONT_BODY_1_DARK,
+  FONT_SIZE_BODY_1,
+  FONT_STYLE_ITALIC,
+  FONT_WEIGHT_SEMIBOLD,
+  SPACING_1,
+  SPACING_2,
+  SPACING_3,
+} from '@opentrons/components'
 import styles from './styles.css'
 
 import type { PipetteCompatibility } from '../../pipettes/types'
+
+const NOT_CALIBRATED = 'Not yet calibrated'
+const NOT_CONNECTED = 'Not connected'
+const CALIBRATION_DATA = 'Calibration data:'
+const AXIS_NAMES = ['x', 'y', 'z']
 
 export type InstrumentItemProps = {|
   compatibility?: PipetteCompatibility,
@@ -11,23 +31,61 @@ export type InstrumentItemProps = {|
   children: React.Node,
   hidden?: boolean,
   needsOffsetCalibration: boolean,
+  pipetteOffsetData?: [number, number, number] | null,
 |}
 
 export function InstrumentItem(props: InstrumentItemProps): React.Node {
-  if (props.hidden) return null
+  const {
+    compatibility,
+    mount,
+    children,
+    hidden,
+    needsOffsetCalibration,
+    pipetteOffsetData = null,
+  } = props
+  if (hidden) return null
+  const match = ['match', 'inexact_match'].includes(compatibility)
   return (
-    <div className={styles.instrument_item}>
-      <StatusIcon
-        match={
-          ['match', 'inexact_match'].includes(props.compatibility) &&
-          !props.needsOffsetCalibration
-        }
-      />
-      {props.mount && (
-        <span className={styles.mount_label}>{props.mount.toUpperCase()}</span>
-      )}
-      {props.children}
-    </div>
+    <>
+      <Flex
+        alignItems={ALIGN_CENTER}
+        marginTop={SPACING_1}
+        marginBottom={SPACING_3}
+      >
+        <StatusIcon match={match && !needsOffsetCalibration} />
+        <Box>
+          <Flex marginBottom={SPACING_1}>
+            {mount && (
+              <Text
+                fontWeight={FONT_WEIGHT_SEMIBOLD}
+                fontSize={FONT_SIZE_BODY_1}
+                minWidth={'3rem'}
+              >
+                {mount.toUpperCase()}
+              </Text>
+            )}
+            <Text css={FONT_BODY_1_DARK}>{children}</Text>
+          </Flex>
+          {!match ? (
+            <Text fontSize={FONT_SIZE_BODY_1} fontStyle={FONT_STYLE_ITALIC}>
+              {NOT_CONNECTED}
+            </Text>
+          ) : !!pipetteOffsetData ? (
+            <Text css={FONT_BODY_1_DARK}>
+              <BuildOffsetText offsetData={pipetteOffsetData} />
+            </Text>
+          ) : (
+            <Text
+              fontSize={FONT_SIZE_BODY_1}
+              fontStyle={FONT_STYLE_ITALIC}
+              color={match ? COLOR_ERROR : C_DARK_GRAY}
+            >
+              {match ? NOT_CALIBRATED : NOT_CONNECTED}
+            </Text>
+          )}
+        </Box>
+      </Flex>
+    </>
   )
 }
 
@@ -37,4 +95,23 @@ function StatusIcon(props: {| match: boolean |}) {
   const iconName = match ? 'check-circle' : 'checkbox-blank-circle-outline'
 
   return <Icon name={iconName} className={styles.status_icon} />
+}
+
+function BuildOffsetText(props: {|
+  offsetData: [number, number, number],
+|}): React.Node {
+  const { offsetData } = props
+  return (
+    <Flex>
+      <Text marginRight={SPACING_2}>{CALIBRATION_DATA}</Text>
+      {AXIS_NAMES.map((key, index) => (
+        <React.Fragment key={key}>
+          <Text fontWeight={FONT_WEIGHT_SEMIBOLD}>{key.toUpperCase()}</Text>
+          <Text marginLeft={SPACING_1} marginRight={SPACING_3}>
+            {offsetData[index] != null ? offsetData[index].toFixed(2) : null}
+          </Text>
+        </React.Fragment>
+      ))}
+    </Flex>
+  )
 }
