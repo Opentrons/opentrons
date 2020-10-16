a0225969ca8aad08a227f8c31917de7c6d756f58
amit lissack <amit@opentrons.com>
2020-10-16 17:07:12 -0400

    fix settings to support ipc as well as tcp.

--- notify-server/notify_server/settings.py
+++ notify-server/notify_server/settings.py
@@ -1,22 +1,31 @@
 """Settings class."""
 
+from typing_extensions import Literal
 from pydantic import BaseSettings, BaseModel, Field
 
 
 class ServerBindAddress(BaseModel):
     """A bind address for server zmq socket."""
 
-    scheme: str
-    port: int
+    scheme: Literal['ipc', 'tcp']
+    host: str = "*"
+    port: int = 5555
+    path: str = "/tmp/notify-server"
+
+    def connection_string(self) -> str:
+        """Create the connection string."""
+        if self.scheme == 'ipc':
+            remainder = self.path
+        else:
+            remainder = f"{self.host}:{self.port}"
+        return f"{self.scheme}://{remainder}"
 
 
 class Settings(BaseSettings):
     """Application Settings."""
 
-    publisher_address: ServerBindAddress = ServerBindAddress(scheme="tcp",
-                                                             port=5555)
-    subscriber_address: ServerBindAddress = ServerBindAddress(scheme="tcp",
-                                                              port=5556)
+    publisher_address: ServerBindAddress = ServerBindAddress(scheme="ipc")
+    subscriber_address: ServerBindAddress = ServerBindAddress(scheme="tcp")
 
     production: bool = Field(
         True,
