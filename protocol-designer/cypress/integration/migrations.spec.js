6d44029d74e8a8cfcc6c702a206e32532a7e6be0
Shlok Amin <shlokamin@me.com>
2020-10-27 15:40:15 -0400

    add new import modal case

--- protocol-designer/cypress/integration/migrations.spec.js
+++ protocol-designer/cypress/integration/migrations.spec.js
@@ -16,59 +16,53 @@ describe('Protocol fixtures migrate and match snapshots', () => {
         '../../fixtures/protocol/1/preFlexGrandfatheredProtocol.json',
       expectedExportFixture:
         '../../fixtures/protocol/5/preFlexGrandfatheredProtocolMigratedFromV1_0_0.json',
-      newLabwareDefsMigrationModal: true,
       unusedPipettes: false,
       exportModalCopy: null,
-      genericMigrationModal: false,
+      migrationModal: 'newLabwareDefs',
     },
     {
       title: 'example_1_1_0 (schema 1, PD version 1.1.1) -> PD 5.2.x, schema 3',
       importFixture: '../../fixtures/protocol/1/example_1_1_0.json',
       expectedExportFixture:
         '../../fixtures/protocol/5/example_1_1_0MigratedFromV1_0_0.json',
-      newLabwareDefsMigrationModal: true,
       unusedPipettes: true,
       exportModalCopy: null,
-      genericMigrationModal: false,
+      migrationModal: 'newLabwareDefs',
     },
     {
       title: 'doItAllV3 (schema 3, PD version 4.0.0) -> PD 5.2.x, schema 3',
       importFixture: '../../fixtures/protocol/4/doItAllV3.json',
       expectedExportFixture: '../../fixtures/protocol/5/doItAllV3.json',
-      newLabwareDefsMigrationModal: false,
       unusedPipettes: false,
       exportModalCopy: null,
-      genericMigrationModal: true,
+      migrationModal: 'noBehaviorChange',
     },
     {
       title: 'doItAllV4 (schema 4, PD version 4.0.0) -> PD 5.2.x, schema 4',
       importFixture: '../../fixtures/protocol/4/doItAllV4.json',
       expectedExportFixture: '../../fixtures/protocol/5/doItAllV4.json',
-      newLabwareDefsMigrationModal: false,
       unusedPipettes: false,
       exportModalCopy:
         'Robot requirements for running module inclusive JSON protocols',
-      genericMigrationModal: true,
+      migrationModal: 'noBehaviorChange',
     },
     {
       title:
         'doItAllV5 (schema 5, PD version 5.2.0) -> import and re-export should preserve data',
       importFixture: '../../fixtures/protocol/5/doItAllV5.json',
       expectedExportFixture: '../../fixtures/protocol/5/doItAllV5.json',
-      newLabwareDefsMigrationModal: false,
       unusedPipettes: false,
       exportModalCopy: 'server version 3.20 or higher',
-      genericMigrationModal: false,
+      migrationModal: null,
     },
     {
       title:
         'mix 5.0.x (schema 3, PD version 5.0.0) -> should migrate to 5.2.x',
       importFixture: '../../fixtures/protocol/5/mix_5_0_x.json',
       expectedExportFixture: '../../fixtures/protocol/5/mix_5_2_0.json',
-      newLabwareDefsMigrationModal: false,
       unusedPipettes: false,
       exportModalCopy: null,
-      genericMigrationModal: true,
+      migrationModal: 'noBehaviorChange',
     },
   ]
 
@@ -77,10 +71,9 @@ describe('Protocol fixtures migrate and match snapshots', () => {
       title,
       importFixture,
       expectedExportFixture,
-      newLabwareDefsMigrationModal,
       unusedPipettes,
       exportModalCopy,
-      genericMigrationModal,
+      migrationModal,
     }) => {
       it(title, () => {
         cy.fixture(importFixture).then(fileContent => {
@@ -102,25 +95,33 @@ describe('Protocol fixtures migrate and match snapshots', () => {
           )
         })
 
-        if (genericMigrationModal) {
-          cy.get('div')
-            .contains(
-              'Your protocol was made in an older version of Protocol Designer'
-            )
-            .should('exist')
-          cy.get('button')
-            .contains('ok', { matchCase: false })
-            .click()
-        }
-
-        if (newLabwareDefsMigrationModal) {
-          // close migration announcement modal
-          cy.get('div')
-            .contains('Update protocol to use new labware definitions')
-            .should('exist')
-          cy.get('button')
-            .contains('update protocol', { matchCase: false })
-            .click()
+        if (migrationModal) {
+          if (migrationModal === 'generic') {
+            cy.get('div')
+              .contains(
+                'Updating the file may make changes to liquid handling actions'
+              )
+              .should('exist')
+            cy.get('button')
+              .contains('ok', { matchCase: false })
+              .click()
+          } else if (migrationModal === 'newLabwareDefs') {
+            cy.get('div')
+              .contains('Update protocol to use new labware definitions')
+              .should('exist')
+            cy.get('button')
+              .contains('update protocol', { matchCase: false })
+              .click()
+          } else if (migrationModal === 'noBehaviorChange') {
+            cy.get('div')
+              .contains(
+                'We have added new features since the last time this protocol was updated, but have not made any changes to existing protocol behavior'
+              )
+              .should('exist')
+            cy.get('button')
+              .contains('ok', { matchCase: false })
+              .click()
+          }
         }
 
         cy.fixture(expectedExportFixture).then(expectedExportProtocol => {
