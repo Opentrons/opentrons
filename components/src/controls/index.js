b6a27c9f668aaba498cee9e69bf92e83ee708234
Seth Foster <seth@opentrons.com>
2020-10-29 17:44:32 -0400

    add a todo about moving components

--- components/src/controls/index.js
+++ components/src/controls/index.js
@@ -1,4 +1,8 @@
 // @flow
+// TODO: All of these are not actually reusable and shouldn't be in the
+// components library. They belong in the app components (and they also
+// belong in the please-refactor-these-to-use-primitives-and-styled-components
+// category). We should move them all.
 export * from './ToggleButton'
 export * from './LabeledToggle'
 export * from './LabeledButton'
