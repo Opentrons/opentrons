126db45413f5943c80f90d2267370c797508b4cf
laura_danielle <laura@opentrons.com>
2020-11-09 10:25:03 -0500

    Fixup tests

--- robot-server/tests/robot/calibration/check/test_user_flow.py
+++ robot-server/tests/robot/calibration/check/test_user_flow.py
@@ -154,7 +154,14 @@ def build_mock_stored_pipette_offset(kind='normal'):
 
 def build_mock_stored_tip_length(kind='normal'):
     if kind == 'normal':
-        return MagicMock(return_value={'tipLength': 30})
+        tip_length = CSTypes.TipLengthCalibration(
+            tip_length=30,
+            pipette='fake id',
+            tiprack='fake_hash',
+            last_modified='some time',
+            source=CSTypes.SourceType.user,
+            status=CSTypes.CalibrationStatus(markedBad=False))
+        return MagicMock(return_value=tip_length)
     else:
         return MagicMock(return_value=None)
 
@@ -375,7 +382,7 @@ async def test_compare_points(mock_user_flow):
     await uf.update_comparison_map()
 
     assert uf.comparison_map.first.pipetteOffset == expected_pip
-    assert uf.comparison_map.first.deck == expected_deck
+    assert uf.comparison_map.first.deck is None
     assert uf.comparison_map.second.pipetteOffset is None
     assert uf.comparison_map.second.deck is None
 
@@ -388,7 +395,7 @@ async def test_compare_points(mock_user_flow):
     await uf.update_comparison_map()
     expected_deck.comparingPointTwo = expected_status
     assert uf.comparison_map.first.pipetteOffset == expected_pip
-    assert uf.comparison_map.first.deck == expected_deck
+    assert uf.comparison_map.first.deck is None
     assert uf.comparison_map.second.pipetteOffset is None
     assert uf.comparison_map.second.deck is None
 
@@ -401,7 +408,7 @@ async def test_compare_points(mock_user_flow):
     await uf.update_comparison_map()
     expected_deck.comparingPointThree = expected_status
     assert uf.comparison_map.first.pipetteOffset == expected_pip
-    assert uf.comparison_map.first.deck == expected_deck
+    assert uf.comparison_map.first.deck is None
     assert uf.comparison_map.second.pipetteOffset is None
     assert uf.comparison_map.second.deck is None
 
@@ -422,21 +429,24 @@ async def test_compare_points(mock_user_flow):
         comparingPointOne=expected_status)
 
     assert uf.comparison_map.first.pipetteOffset == expected_pip
-    assert uf.comparison_map.first.deck == expected_deck
+    assert uf.comparison_map.first.deck is None
     assert uf.comparison_map.second.pipetteOffset == new_pip
     assert uf.comparison_map.second.deck == new_deck
 
 
 async def test_mark_bad_calibration(mock_user_flow_bad_vectors):
     uf = mock_user_flow_bad_vectors
-
-    with patch('opentrons.calibration_storage.modify.mark_bad') as m:
+    storage_path = 'opentrons.calibration_storage.modify'
+    with patch(f'{storage_path}.mark_bad') as m,\
+         patch(f'{storage_path}.create_tip_length_data'),\
+         patch(f'{storage_path}.save_tip_length_calibration'),\
+         patch(f'{storage_path}.save_pipette_calibration'),\
+         patch(f'{storage_path}.save_robot_deck_attitude'):
         uf._current_state = CalibrationCheckState.comparingTip
         await uf.update_comparison_map()
         expected_tip_length_call = [
             uf._tip_lengths[uf.mount],
             CSTypes.SourceType.calibration_check]
-
         m.assert_called_once_with(*expected_tip_length_call)
         m.reset_mock()
 
