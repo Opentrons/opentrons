a498e23995aa3630fc23784c332deefaee61551a
Seth Foster <seth@opentrons.com>
2020-11-10 13:02:44 -0500

    fix(app): Correct text when saving z point for all sessions
    Something here got a little messed up I think by calibration check; we
    now have different (and tested) text for all different sessions.

--- app/src/components/CalibrationPanels/__tests__/SaveZPoint.test.js
+++ app/src/components/CalibrationPanels/__tests__/SaveZPoint.test.js
@@ -156,6 +156,7 @@ describe('SaveZPoint', () => {
     const allText = wrapper.text()
     expect(allText).toContain('save calibration and move to slot 1')
     expect(allText).toContain('calibrate z-axis in slot 5')
+    expect(allText).toContain('calibrate the z offset for this pipette')
   })
 
   it('deck cal session type shows correct text', () => {
@@ -165,6 +166,19 @@ describe('SaveZPoint', () => {
     const allText = wrapper.text()
     expect(allText).toContain('remember z-axis and move to slot 1')
     expect(allText).toContain('z-axis in slot 5')
+    expect(allText).toContain(
+      'use this z position for the rest of deck calibration'
+    )
+  })
+
+  it('health check session type shows correct text', () => {
+    const wrapper = render({
+      sessionType: Sessions.SESSION_TYPE_CALIBRATION_HEALTH_CHECK,
+    })
+    const allText = wrapper.text()
+    expect(allText).toContain('check z-axis')
+    expect(allText).toContain('check z-axis in slot 5')
+    expect(allText).toContain('to determine how this position compares')
   })
 
   it('renders the confirm crash link', () => {
