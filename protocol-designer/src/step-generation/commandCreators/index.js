c1b04fc9775d36c23b987935d7f5b02e087ee195
Shlok Amin <shlokamin@me.com>
2020-11-17 11:38:59 -0800

    feat(protocol-designer): memoize compound command creators
    memoize compound command creators
    
    #7021

--- protocol-designer/src/step-generation/commandCreators/index.js
+++ protocol-designer/src/step-generation/commandCreators/index.js
@@ -1,12 +1,23 @@
 // @flow
-export {
-  transfer,
-  mix,
-  consolidate,
-  distribute,
-  thermocyclerProfileStep,
-  thermocyclerStateStep,
+import { memoize } from 'lodash'
+import {
+  transfer as _transfer,
+  mix as _mix,
+  consolidate as _consolidate,
+  distribute as _distribute,
+  thermocyclerProfileStep as _thermocyclerProfileStep,
+  thermocyclerStateStep as _thermocyclerStateStep,
 } from './compound'
+
+import type {
+  CommandCreator,
+  ConsolidateArgs,
+  DistributeArgs,
+  MixArgs,
+  ThermocyclerProfileStepArgs,
+  ThermocyclerStateStepArgs,
+  TransferArgs,
+} from '../types'
 
 export {
   aspirate,
@@ -23,3 +34,30 @@ export {
   setTemperature,
   touchTip,
 } from './atomic'
+
+const resolver = (args, invariantContext, prevRobotState) =>
+  `${JSON.stringify(args)}${JSON.stringify(invariantContext)}${JSON.stringify(
+    prevRobotState
+  )}`
+
+export const transfer: CommandCreator<TransferArgs> = memoize(
+  _transfer,
+  resolver
+)
+export const mix: CommandCreator<MixArgs> = memoize(_mix, resolver)
+export const consolidate: CommandCreator<ConsolidateArgs> = memoize(
+  _consolidate,
+  resolver
+)
+export const distribute: CommandCreator<DistributeArgs> = memoize(
+  _distribute,
+  resolver
+)
+export const thermocyclerProfileStep: CommandCreator<ThermocyclerProfileStepArgs> = memoize(
+  _thermocyclerProfileStep,
+  resolver
+)
+export const thermocyclerStateStep: CommandCreator<ThermocyclerStateStepArgs> = memoize(
+  _thermocyclerStateStep,
+  resolver
+)
