500ea0814b357769090937e3244b6f5ab5497734
amit lissack <amit@opentrons.com>
2020-12-15 09:51:43 -0500

    slight optimization in piecewise_volume_conversion

--- api/src/opentrons/config/pipette_config.py
+++ api/src/opentrons/config/pipette_config.py
@@ -239,7 +239,12 @@ def piecewise_volume_conversion(
     """
     # pick the first item from the seq for which the target is less than
     # the bracketing element
-    i = list(filter(lambda x: ul <= x[0], sequence))[0]
+    i = None
+    for x in sequence:
+        if ul <= x[0]:
+            i = x
+            break
+    assert i
     # use that element to calculate the movement distance in mm
     return i[1]*ul + i[2]
 
