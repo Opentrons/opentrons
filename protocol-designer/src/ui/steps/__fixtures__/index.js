9b03aa813f7a456147a41de9bd0cd0ea8dde190f
Shlok Amin <shlokamin@me.com>
2021-01-11 22:57:09 -0500

    feat(protocol-designer): add selector for gathering multi select field values
    add selector for gathering multi select field values
    
    closes #7187

---
+++ protocol-designer/src/ui/steps/__fixtures__/index.js
@@ -0,0 +1,1 @@
+export { getMockMoveLiquidStep } from './moveLiquid'
--- protocol-designer/src/ui/steps/test/selectors.test.js
+++ protocol-designer/src/ui/steps/test/selectors.test.js
@@ -15,7 +15,10 @@ import {
   getSelectedStepTitleInfo,
   getActiveItem,
   getMultiSelectLastSelected,
+  getMultiSelectFieldValues,
 } from '../selectors'
+import { getMockMoveLiquidStep } from '../__fixtures__'
+
 import * as utils from '../../modules/utils'
 
 function createArgsForStepId(stepId, stepArgs) {
@@ -316,5 +319,321 @@ describe('getMultiSelectLastSelected', () => {
       lastSelected: 'spam',
     })
     expect(result).toEqual('spam')
+  })
+})
+
+describe('getMultiSelectFieldValues', () => {
+  let mockSavedStepForms
+  let mockmultiSelectItemIds
+
+  beforeEach(() => {
+    mockSavedStepForms = {
+      ...getMockMoveLiquidStep(),
+      // just doing this so the ids are not the exact same
+      another_move_liquid_step_id: {
+        ...getMockMoveLiquidStep().move_liquid_step_id,
+      },
+    }
+    mockmultiSelectItemIds = [
+      'move_liquid_step_id',
+      'another_move_liquid_step_id',
+    ]
+  })
+  afterEach(() => {})
+  it('should return null if any of the forms are not moveLiquid type', () => {
+    const savedStepForms = {
+      ...mockSavedStepForms,
+      another_move_liquid_step_id: {
+        ...mockSavedStepForms.another_move_liquid_step_id,
+        stepType: 'notMoveLiquid',
+      },
+    }
+    expect(
+      getMultiSelectFieldValues.resultFunc(
+        savedStepForms,
+        mockmultiSelectItemIds
+      )
+    ).toBe(null)
+  })
+  describe('when fields are NOT indeterminate', () => {
+    it('should return the fields with the indeterminate boolean', () => {
+      expect(
+        getMultiSelectFieldValues.resultFunc(
+          mockSavedStepForms,
+          mockmultiSelectItemIds
+        )
+      ).toEqual({
+        // aspirate settings
+        aspirate_flowRate: {
+          value: null,
+          isIndeterminate: false,
+        },
+        aspirate_mmFromBottom: {
+          value: 1,
+          isIndeterminate: false,
+        },
+        aspirate_wellOrder_first: {
+          value: 't2b',
+          isIndeterminate: false,
+        },
+        aspirate_wellOrder_second: {
+          value: 'l2r',
+          isIndeterminate: false,
+        },
+        preWetTip: {
+          value: false,
+          isIndeterminate: false,
+        },
+        aspirate_mix_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        aspirate_mix_times: {
+          value: '2',
+          isIndeterminate: false,
+        },
+        aspirate_mix_volume: {
+          value: '5',
+          isIndeterminate: false,
+        },
+        aspirate_delay_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        aspirate_delay_seconds: {
+          value: '2',
+          isIndeterminate: false,
+        },
+        aspirate_delay_mmFromBottom: {
+          value: '1',
+          isIndeterminate: false,
+        },
+        aspirate_airGap_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        aspirate_airGap_volume: {
+          value: '30',
+          isIndeterminate: false,
+        },
+        aspirate_touchTip_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        aspirate_touchTip_mmFromBottom: {
+          value: 1,
+          isIndeterminate: false,
+        },
+        // dispense settings
+        dispense_flowRate: {
+          value: null,
+          isIndeterminate: false,
+        },
+        dispense_mmFromBottom: {
+          value: 0.5,
+          isIndeterminate: false,
+        },
+        dispense_wellOrder_first: {
+          value: 't2b',
+          isIndeterminate: false,
+        },
+        dispense_wellOrder_second: {
+          value: 'l2r',
+          isIndeterminate: false,
+        },
+        dispense_mix_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        dispense_mix_times: {
+          value: null,
+          isIndeterminate: false,
+        },
+        dispense_mix_volume: {
+          value: null,
+          isIndeterminate: false,
+        },
+        dispense_delay_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        dispense_delay_seconds: {
+          value: '1',
+          isIndeterminate: false,
+        },
+        dispense_delay_mmFromBottom: {
+          value: '0.5',
+          isIndeterminate: false,
+        },
+        dispense_airGap_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        dispense_airGap_volume: {
+          value: null,
+          isIndeterminate: false,
+        },
+        dispense_touchTip_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        dispense_touchTip_mmFromBottom: {
+          value: 1,
+          isIndeterminate: false,
+        },
+        blowout_checkbox: {
+          value: true,
+          isIndeterminate: false,
+        },
+        blowout_location: {
+          value: 'trashId',
+          isIndeterminate: false,
+        },
+      })
+    })
+  })
+  describe('when fields are indeterminate', () => {
+    let mockSavedStepFormsIndeterminate
+    beforeEach(() => {
+      mockSavedStepFormsIndeterminate = {
+        ...getMockMoveLiquidStep(),
+        // just doing this so the ids are not the exact same
+        another_move_liquid_step_id: {
+          ...getMockMoveLiquidStep().move_liquid_step_id,
+          aspirate_flowRate: 2,
+          aspirate_mmFromBottom: '2',
+          aspirate_wellOrder_first: 'b2t',
+          aspirate_wellOrder_second: 'r2l',
+          preWetTip: true,
+          aspirate_mix_checkbox: false,
+          // not going to change mix times or mix volumes
+          // if the checkboxes are different the rest of the fields should also be indeterminate
+          aspirate_delay_checkbox: false,
+          // same thing here for delay seconds and mm from bottom
+          aspirate_airGap_checkbox: false,
+          // same thing here with air gap volume
+          aspirate_touchTip_checkbox: false,
+          // same thing with aspirate_touchTip_mmFromBottom
+          dispense_flowRate: 2,
+          dispense_mmFromBottom: '2',
+          dispense_wellOrder_first: 'b2t',
+          dispense_wellOrder_second: 'r2l',
+          dispense_mix_checkbox: false,
+          // same thing here with mix times or mix volumes
+          dispense_delay_checkbox: false,
+          // same thing here for delay seconds and mm from bottom
+          dispense_airGap_checkbox: false,
+          // same thing here with air gap volume
+          dispense_touchTip_checkbox: false,
+          // same thing with dispense_touchTip_mmFromBottom
+          blowout_checkbox: false,
+          // same thing here with blowout location
+        },
+      }
+    })
+    it('should return the fields with the indeterminate boolean', () => {
+      expect(
+        getMultiSelectFieldValues.resultFunc(
+          mockSavedStepFormsIndeterminate,
+          mockmultiSelectItemIds
+        )
+      ).toEqual({
+        // aspirate settings
+        aspirate_flowRate: {
+          isIndeterminate: true,
+        },
+        aspirate_mmFromBottom: {
+          isIndeterminate: true,
+        },
+        aspirate_wellOrder_first: {
+          isIndeterminate: true,
+        },
+        aspirate_wellOrder_second: {
+          isIndeterminate: true,
+        },
+        preWetTip: {
+          isIndeterminate: true,
+        },
+        aspirate_mix_checkbox: {
+          isIndeterminate: true,
+        },
+        aspirate_mix_times: {
+          isIndeterminate: true,
+        },
+        aspirate_mix_volume: {
+          isIndeterminate: true,
+        },
+        aspirate_delay_checkbox: {
+          isIndeterminate: true,
+        },
+        aspirate_delay_seconds: {
+          isIndeterminate: true,
+        },
+        aspirate_delay_mmFromBottom: {
+          isIndeterminate: true,
+        },
+        aspirate_airGap_checkbox: {
+          isIndeterminate: true,
+        },
+        aspirate_airGap_volume: {
+          isIndeterminate: true,
+        },
+        aspirate_touchTip_checkbox: {
+          isIndeterminate: true,
+        },
+        aspirate_touchTip_mmFromBottom: {
+          isIndeterminate: true,
+        },
+        // dispense settings
+        dispense_flowRate: {
+          isIndeterminate: true,
+        },
+        dispense_mmFromBottom: {
+          isIndeterminate: true,
+        },
+        dispense_wellOrder_first: {
+          isIndeterminate: true,
+        },
+        dispense_wellOrder_second: {
+          isIndeterminate: true,
+        },
+        dispense_mix_checkbox: {
+          isIndeterminate: true,
+        },
+        dispense_mix_times: {
+          isIndeterminate: true,
+        },
+        dispense_mix_volume: {
+          isIndeterminate: true,
+        },
+        dispense_delay_checkbox: {
+          isIndeterminate: true,
+        },
+        dispense_delay_seconds: {
+          isIndeterminate: true,
+        },
+        dispense_delay_mmFromBottom: {
+          isIndeterminate: true,
+        },
+        dispense_airGap_checkbox: {
+          isIndeterminate: true,
+        },
+        dispense_airGap_volume: {
+          isIndeterminate: true,
+        },
+        dispense_touchTip_checkbox: {
+          isIndeterminate: true,
+        },
+        dispense_touchTip_mmFromBottom: {
+          isIndeterminate: true,
+        },
+        blowout_checkbox: {
+          isIndeterminate: true,
+        },
+        blowout_location: {
+          isIndeterminate: true,
+        },
+      })
+    })
   })
 })
