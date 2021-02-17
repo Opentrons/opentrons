4f5e7087b019accabe7445e6fee17aa759c9a9d6
IanLondon <ianlondondu@gmail.com>
2021-02-17 16:40:01 -0500

    more placeholders

--- protocol-designer/src/components/BatchEditForm/index.js
+++ protocol-designer/src/components/BatchEditForm/index.js
@@ -1,19 +1,59 @@
 // @flow
 import * as React from 'react'
+import { i18n } from '../../localization'
+import { CheckboxRowField, TextField } from '../StepEditForm/fields'
+import { makeBatchEditFieldProps } from './makeBatchEditFieldProps'
 import type { MultiselectFieldValues } from '../../ui/steps/selectors'
 import type { CountPerStepType } from '../../form-types'
+import type { FieldPropsByName } from '../StepEditForm/types'
+import styles from '../StepEditForm/StepEditForm.css'
 
 export type BatchEditFormProps = {|
   countPerType: CountPerStepType,
-  fieldValues: null | MultiselectFieldValues,
+  fieldValues: MultiselectFieldValues,
+  handleChangeFormInput: (name: string, value: mixed) => void,
 |}
 
-export const BatchEditMoveLiquid = (): React.Node => {
-  return <div>TODO batch edit form for Transfer step goes here</div>
+type BatchEditMoveLiquidProps = {|
+  propsForFields: FieldPropsByName,
+  handleClose: () => mixed,
+  handleSave: () => mixed,
+|}
+export const BatchEditMoveLiquid = (
+  props: BatchEditMoveLiquidProps
+): React.Node => {
+  const { propsForFields, handleClose, handleSave } = props
+  return (
+    // TOOD IMMEDIATELY copied from SourceDestFields. Refactor to be DRY
+    <div>
+      <CheckboxRowField
+        {...propsForFields['aspirate_mix_checkbox']}
+        label={i18n.t('form.step_edit_form.field.mix.label')}
+        className={styles.small_field}
+        tooltipContent={i18n.t(
+          `tooltip.step_fields.defaults.${'aspirate_mix_checkbox'}`
+        )}
+      >
+        <TextField
+          {...propsForFields['aspirate_mix_volume']}
+          className={styles.small_field}
+          units={i18n.t('application.units.microliter')}
+        />
+        <TextField
+          {...propsForFields['aspirate_mix_times']}
+          className={styles.small_field}
+          units={i18n.t('application.units.times')}
+        />
+      </CheckboxRowField>
+      <p>TODO batch edit form for Transfer step goes here</p>
+      <button onClick={handleClose}>Close</button>
+      <button onClick={handleSave}>Save</button>
+    </div>
+  )
 }
 
 export const BatchEditForm = (props: BatchEditFormProps): React.Node => {
-  const { countPerType } = props
+  const { countPerType, fieldValues, handleChangeFormInput } = props
 
   // TODO IMMEDIATELY some unit-tested util for this line
   const selectedStepTypes = Object.keys(countPerType).filter(
@@ -30,7 +70,23 @@ export const BatchEditForm = (props: BatchEditFormProps): React.Node => {
 
   const selectedSingleStepType = selectedStepTypes[0]
   if (selectedSingleStepType === 'moveLiquid') {
-    return <BatchEditMoveLiquid />
+    // Valid state for using makeBatchEditFieldProps
+    const propsForFields = makeBatchEditFieldProps(
+      fieldValues,
+      handleChangeFormInput
+    )
+    return (
+      <BatchEditMoveLiquid
+        propsForFields={propsForFields}
+        handleClose={() => {
+          // TODO(IL, 2021-02-17): implement in #7138
+          console.log('TODO: close')
+        }}
+        handleSave={() => {
+          console.log('TODO: save')
+        }}
+      />
+    )
   }
   return (
     <div>{`${selectedSingleStepType} not supported (TODO real message goes here)`}</div>
--- protocol-designer/src/components/FormManager/index.js
+++ protocol-designer/src/components/FormManager/index.js
@@ -11,15 +11,17 @@ import {
 import { selectors as stepFormSelectors } from '../../step-forms'
 
 export const FormManager = (): React.Node => {
-  const multiStepIds = useSelector(getMultiSelectItemIds)
   const fieldValues = useSelector(getMultiSelectFieldValues)
   const stepIds = useSelector(getMultiSelectItemIds)
   const allSteps = useSelector(stepFormSelectors.getSavedStepForms)
 
-  if (multiStepIds !== null) {
-    // not batch edit mode
-    if (stepIds === null) return null
+  // TODO(IL, 2021-02-17): dispatch changeBatchEditField here in #7222
+  const handleChangeFormInput = (name, value) => {
+    console.log(`TODO: update ${name}: ${String(value)}`)
+  }
 
+  if (fieldValues !== null && stepIds !== null) {
+    // batch edit mode
     const steps = stepIds.map(id => allSteps[id])
     const countPerType = steps.reduce((acc, step) => {
       const { stepType } = step
@@ -31,7 +33,11 @@ export const FormManager = (): React.Node => {
     return (
       <>
         <StepSelectionBanner countPerType={countPerType} />
-        <BatchEditForm countPerType={countPerType} fieldValues={fieldValues} />
+        <BatchEditForm
+          countPerType={countPerType}
+          fieldValues={fieldValues}
+          handleChangeFormInput={handleChangeFormInput}
+        />
       </>
     )
   }
