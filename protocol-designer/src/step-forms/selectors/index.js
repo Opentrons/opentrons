1b9a8106894d962b6d69cc13472de7694b476c21
Shlok Amin <shlokamin@me.com>
2021-02-16 16:32:11 -0800

    implement getBatchEditFormHasUnsavedChanges

--- protocol-designer/src/step-forms/selectors/index.js
+++ protocol-designer/src/step-forms/selectors/index.js
@@ -4,6 +4,7 @@ import assert from 'assert'
 import isEqual from 'lodash/isEqual'
 import mapValues from 'lodash/mapValues'
 import reduce from 'lodash/reduce'
+import isEmpty from 'lodash/isEmpty'
 import { createSelector } from 'reselect'
 import {
   getPipetteNameSpecs,
@@ -419,11 +420,14 @@ export const getCurrentFormHasUnsavedChanges: Selector<boolean> = createSelector
   }
 )
 
-export const getBatchEditFormHasUnsavedChanges = (): boolean => false
-
 export const getBatchEditFieldChanges: Selector<BatchEditFormChangesState> = createSelector(
   rootSelector,
   state => state.batchEditFormChanges
+)
+
+export const getBatchEditFormHasUnsavedChanges: Selector<boolean> = createSelector(
+  getBatchEditFieldChanges,
+  changes => !isEmpty(changes)
 )
 
 const getModuleEntity = (state: InvariantContext, id: string): ModuleEntity => {
