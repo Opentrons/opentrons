2d0b89bcbce0dbc4dd90722ab15a0f3ff5733bf8
Shlok Amin <shlokamin@me.com>
2021-02-18 07:07:41 -0800

    add missing type import

--- protocol-designer/src/step-forms/actions/index.js
+++ protocol-designer/src/step-forms/actions/index.js
@@ -1,5 +1,6 @@
 // @flow
 import { getBatchEditFieldChanges } from '../selectors'
+import type { ThunkAction } from '../../types'
 import type { StepIdType, StepFieldName } from '../../form-types'
 import type { BatchEditFormChangesState } from '../reducers'
 export * from './modules'
@@ -18,18 +19,15 @@ export const changeBatchEditField = (
 export type ResetBatchEditFieldChangesAction = {|
   type: 'RESET_BATCH_EDIT_FIELD_CHANGES',
 |}
-
 export const resetBatchEditFieldChanges = (): ResetBatchEditFieldChangesAction => ({
   type: 'RESET_BATCH_EDIT_FIELD_CHANGES',
 })
 
 type EditedFields = { [StepFieldName]: mixed }
-
 export type SaveStepFormsMultiAction = {|
   type: 'SAVE_STEP_FORMS_MULTI',
   payload: { stepIds: Array<StepIdType>, editedFields: EditedFields },
 |}
-
 export const saveStepFormsMulti: (
   selectedStepIds: Array<StepIdType> | null
 ) => ThunkAction<SaveStepFormsMultiAction> = selectedStepIds => (
