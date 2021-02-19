f86f55af9bfd9b5259ea5f73c06112754adb36d6
Laura Cox <lauracox@Lauras-MacBook-Pro.local>
2021-02-19 08:20:56 -0600

    Fixup: Fix robot server tests

--- api/src/opentrons/protocols/context/protocol_api/protocol_context.py
+++ api/src/opentrons/protocols/context/protocol_api/protocol_context.py
@@ -25,6 +25,7 @@ from opentrons.protocols.labware import load_from_definition, \
     get_labware_definition
 from opentrons.protocols.types import Protocol
 from opentrons_shared_data.labware.dev_types import LabwareDefinition
+from opentrons.drivers.rpi_drivers.types import USBPort
 
 
 MODULE_LOG = logging.getLogger(__name__)
