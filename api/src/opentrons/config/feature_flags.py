faf871ebaa8e359e3d77f1951749b72a3b403fb7
Mike Cousins <mike@opentrons.com>
2021-02-25 12:47:06 -0500

    feat(api): add enableProtocolEngine feature flag to opentrons.config (#7389)
    Closes #7333

--- api/src/opentrons/config/feature_flags.py
+++ api/src/opentrons/config/feature_flags.py
@@ -1,37 +1,43 @@
 from opentrons.config import advanced_settings as advs
 
 
-def short_fixed_trash():
+def short_fixed_trash() -> bool:
     return advs.get_setting_with_env_overload('shortFixedTrash')
 
 
-def calibrate_to_bottom():
+def calibrate_to_bottom() -> bool:
     return advs.get_setting_with_env_overload('calibrateToBottom')
 
 
-def dots_deck_type():
+def dots_deck_type() -> bool:
     return advs.get_setting_with_env_overload('deckCalibrationDots')
 
 
-def disable_home_on_boot():
+def disable_home_on_boot() -> bool:
     return advs.get_setting_with_env_overload('disableHomeOnBoot')
 
 
-def use_protocol_api_v2():
+def use_protocol_api_v2() -> bool:
     return not advs.get_setting_with_env_overload('useLegacyInternals')
 
 
-def use_old_aspiration_functions():
+def use_old_aspiration_functions() -> bool:
     return advs.get_setting_with_env_overload('useOldAspirationFunctions')
 
 
-def enable_door_safety_switch():
+def enable_door_safety_switch() -> bool:
     return advs.get_setting_with_env_overload('enableDoorSafetySwitch')
 
 
-def enable_http_protocol_sessions():
+def enable_http_protocol_sessions() -> bool:
     return advs.get_setting_with_env_overload('enableHttpProtocolSessions')
 
 
-def enable_fast_protocol_upload():
+def enable_fast_protocol_upload() -> bool:
     return advs.get_setting_with_env_overload('enableFastProtocolUpload')
+
+
+def enable_protocol_engine() -> bool:
+    """Get if the ProtocolEngine should be used to run protocol files."""
+
+    return advs.get_setting_with_env_overload("enableProtocolEngine")
