4708a3c2e37fd1b63183838396d171244a31d1ad
amitlissack <amit@opentrons.com>
2021-02-25 16:03:16 -0500

    feat(api): Api protocol engine labware context getters (#7394)
    * implement the very basic getters for engine.LabwareContext
    * lint errors.
    * use proper noqa syntax

--- api/src/opentrons/protocols/implementations/engine/labware_context.py
+++ api/src/opentrons/protocols/implementations/engine/labware_context.py
@@ -1,6 +1,7 @@
 """Module containing labware interface that works with Protocol Engine."""
 from typing import Dict, List
 
+from opentrons.protocol_engine import StateView
 from opentrons.protocols.geometry.labware_geometry import LabwareGeometry
 from opentrons.protocols.implementations.tip_tracker import TipTracker
 from opentrons.protocols.implementations.well import WellImplementation
@@ -16,38 +17,61 @@ from opentrons.protocols.implementations.interfaces.labware\
 class LabwareContext(LabwareInterface):
     """LabwareInterface implementation that works with the Protocol Engine."""
 
+    def __init__(self, labware_id: str, state_view: StateView):
+        self._id = labware_id
+        self._state_view = state_view
+
     def get_uri(self) -> str:
-        raise NotImplementedError()
+        """Get the labware uri."""
+        return self._state_view.labware.get_definition_uri(labware_id=self._id)
 
     def get_display_name(self) -> str:
-        raise NotImplementedError()
+        """Get the display name."""
+        lw = self._state_view.labware.get_labware_data_by_id(labware_id=self._id)
+        # TODO AL 20210225 - this is not consistent with reference
+        #  implementation. It is either the supplied label or displayName in
+        #  definition on the location, which is either a slot or module.
+        return f"{lw.definition['metadata']['displayName']} on {lw.location.slot}"
 
     def get_name(self) -> str:
-        raise NotImplementedError()
+        """Get the name."""
+        # TODO AL 20210225 - this is not consistent with reference
+        #  implementation. It is either the supplied label or loadName in
+        #  definition.
+        return self.get_parameters()['loadName']
 
     def set_name(self, new_name: str) -> None:
         raise NotImplementedError()
 
     def get_definition(self) -> LabwareDefinition:
-        raise NotImplementedError()
+        """Get the labware definition."""
+        return self._state_view.labware.get_labware_data_by_id(
+            labware_id=self._id).definition
 
     def get_parameters(self) -> LabwareParameters:
-        raise NotImplementedError()
+        """Get the labware definition parameters."""
+        return self.get_definition()['parameters']
 
     def get_quirks(self) -> List[str]:
-        raise NotImplementedError()
+        """Get the labware quirks."""
+        return self.get_parameters()['quirks']
 
     def set_calibration(self, delta: Point) -> None:
         raise NotImplementedError()
 
     def get_calibrated_offset(self) -> Point:
-        raise NotImplementedError()
+        """Get the calibrated offset."""
+        x, y, z = self._state_view.labware.get_labware_data_by_id(
+            labware_id=self._id).calibration
+        return Point(x=x, y=y, z=z)
 
     def is_tiprack(self) -> bool:
-        raise NotImplementedError()
+        """Return whether this labware is a tiprack."""
+        return self.get_parameters()['isTiprack']
 
     def get_tip_length(self) -> float:
-        raise NotImplementedError()
+        """Get the tip length."""
+        return self._state_view.labware.get_tip_length(labware_id=self._id)
 
     def set_tip_length(self, length: float):
         raise NotImplementedError()
@@ -80,4 +104,5 @@ class LabwareContext(LabwareInterface):
 
     @property
     def load_name(self) -> str:
-        raise NotImplementedError()
+        """Get the load name."""
+        return self.get_parameters()['loadName']
