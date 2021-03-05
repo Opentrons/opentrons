7fdbdaa781f1318d7312ea184649ea95009ee710
IanLondon <ianlondondu@gmail.com>
2021-03-05 11:45:53 -0500

    DO NOT MERGE: batch edit FF defaults to on for user research

--- protocol-designer/src/feature-flags/reducers.js
+++ protocol-designer/src/feature-flags/reducers.js
@@ -23,7 +23,10 @@ const initialFlags: Flags = {
   PRERELEASE_MODE: process.env.OT_PD_PRERELEASE_MODE === '1' || false,
   OT_PD_DISABLE_MODULE_RESTRICTIONS:
     process.env.OT_PD_DISABLE_MODULE_RESTRICTIONS === '1' || false,
-  BATCH_EDIT_ENABLED: process.env.BATCH_EDIT_ENABLED === '1' || false,
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // DO NOT MERGE!!!
+  BATCH_EDIT_ENABLED: true,
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
 // NOTE(mc, 2020-06-04): `handleActions` cannot be strictly typed
