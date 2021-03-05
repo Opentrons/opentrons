466bd00609355e424709c6a2895aaba8f5afabc5
Ian London <ianlondondu@gmail.com>
2021-03-05 11:40:57 -0500

    feat(protocol-designer): add modal fields (#7440)
    Closes #7239
    
    Co-authored-by: @Kadee80 and @shlokamin

--- protocol-designer/src/components/StepEditForm/fields/WellOrderField/index.js
+++ protocol-designer/src/components/StepEditForm/fields/WellOrderField/index.js
@@ -1,26 +1,39 @@
 // @flow
 import * as React from 'react'
-import { FormGroup, Tooltip, useHoverTooltip } from '@opentrons/components'
+import {
+  FormGroup,
+  Text,
+  Tooltip,
+  useHoverTooltip,
+  FONT_WEIGHT_SEMIBOLD,
+  FONT_SIZE_BODY_1,
+} from '@opentrons/components'
 import cx from 'classnames'
 import { i18n } from '../../../../localization'
 import ZIG_ZAG_IMAGE from '../../../../images/zig_zag_icon.svg'
 import { WellOrderModal } from './WellOrderModal'
 import stepEditStyles from '../../StepEditForm.css'
 import styles from './WellOrderInput.css'
-import type { FormData } from '../../../../form-types'
 import type { FieldProps } from '../../types'
+import type { WellOrderOption } from '../../../../form-types'
 
 type Props = {|
   className?: ?string,
   label?: string,
   prefix: 'aspirate' | 'dispense' | 'mix',
-  formData: FormData,
+  firstValue: ?WellOrderOption,
+  secondValue: ?WellOrderOption,
   updateFirstWellOrder: $PropertyType<FieldProps, 'updateValue'>,
   updateSecondWellOrder: $PropertyType<FieldProps, 'updateValue'>,
 |}
 
 export const WellOrderField = (props: Props): React.Node => {
-  const { formData, updateFirstWellOrder, updateSecondWellOrder } = props
+  const {
+    firstValue,
+    secondValue,
+    updateFirstWellOrder,
+    updateSecondWellOrder,
+  } = props
   const [isModalOpen, setModalOpen] = React.useState(false)
 
   const handleOpen = () => {
@@ -36,11 +49,12 @@ export const WellOrderField = (props: Props): React.Node => {
   }
 
   const getIconClassNames = () => {
-    let iconClassNames = []
-    if (formData) {
-      const first = formData[`${props.prefix}_wellOrder_first`]
-      const second = formData[`${props.prefix}_wellOrder_second`]
-      iconClassNames = [styles[`${first}_first`], styles[`${second}_second`]]
+    const iconClassNames = []
+    if (firstValue) {
+      iconClassNames.push(styles[`${firstValue}_first`])
+    }
+    if (secondValue) {
+      iconClassNames.push(styles[`${secondValue}_second`])
     }
     return iconClassNames
   }
@@ -63,18 +77,31 @@ export const WellOrderField = (props: Props): React.Node => {
             prefix={props.prefix}
             closeModal={handleClose}
             isOpen={isModalOpen}
-            formData={formData}
             updateValues={updateValues}
+            firstValue={firstValue}
+            secondValue={secondValue}
           />
-          <img
-            onClick={handleOpen}
-            src={ZIG_ZAG_IMAGE}
-            className={cx(
-              styles.well_order_icon,
-              { [styles.icon_with_label]: props.label },
-              getIconClassNames()
-            )}
-          />
+          {firstValue != null && secondValue != null ? (
+            <img
+              onClick={handleOpen}
+              src={ZIG_ZAG_IMAGE}
+              className={cx(
+                styles.well_order_icon,
+                { [styles.icon_with_label]: props.label },
+                getIconClassNames()
+              )}
+            />
+          ) : (
+            <Text
+              onClick={handleOpen}
+              fontWeight={FONT_WEIGHT_SEMIBOLD}
+              fontSize={FONT_SIZE_BODY_1}
+              paddingTop="0.5rem"
+              paddingBottom="0.325rem"
+            >
+              {i18n.t('form.step_edit_form.field.well_order.mixed')}
+            </Text>
+          )}
         </FormGroup>
       </div>
     </>
