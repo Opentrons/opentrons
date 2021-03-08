c1cdc36779d729be37b6c8967f65ae1cd19a7baa
neha ojha <nehaojha@nehas-MacBook-Pro.local>
2021-03-08 16:13:24 -0500

    initial e2e

--- protocol-designer/cypress/integration/batchEdit.spec.js
+++ protocol-designer/cypress/integration/batchEdit.spec.js
@@ -1,3 +1,6 @@
+//Enter batch (shift + step) — select multiple steps - flow rate in advance settings -
+//save it in a step - duplicate the selection - exit batch edit via button
+
 describe('Batch Edit Transfo', () => {
   beforeEach(() => {
     cy.visit('/')
@@ -27,13 +30,30 @@ describe('Batch Edit Transfo', () => {
     cy.get('button').contains('ok').click()
 
     // Verify the Design Page
-    // cy.contains('MULTI SELECT BANNER TEST PROTOCOL')
-    // cy.contains('STARTING DECK STATE')
-    // cy.contains('ADD STEP')
+    cy.get('#TitleBar_main > h1').contains('Multi select banner test protocol')
+    cy.get('#TitleBar_main > h2').contains('STARTING DECK STATE')
+    cy.get('button[id=StepCreationButton]').contains('+ Add Step')
 
     // enter into the batch edit mode
     cy.get('[data-test="StepItem_1"]').click({
+      ctrlKey: true,
+    })
+
+    // Range selection with shift
+    cy.get('[data-test="StepItem_3"]').click({
       shiftKey: true,
     })
+
+    //Change the Flowrate to 100 and Save
+    cy.get('[name="aspirate_flowRate"]').click()
+    cy.get('input[name="aspirate_flowRate_customFlowRate"]').type('100')
+    cy.get('button').contains('Done').click()
+    cy.get('button').contains('save').click()
+
+    //Dublicate the selected steps
+    // cy.get
+
+    //Exit batch edit mode
+    cy.get('button').contains('exit batch edit').click()
   })
 })
