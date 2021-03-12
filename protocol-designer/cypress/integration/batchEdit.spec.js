acc1a41540bf23949fc84d24fb586e0c7e56ef9e
neha ojha <nehaojha@nehas-MacBook-Pro.local>
2021-03-11 20:10:15 -0500

    adding the mixed step verification

--- protocol-designer/cypress/integration/batchEdit.spec.js
+++ protocol-designer/cypress/integration/batchEdit.spec.js
@@ -1,7 +1,4 @@
-//Enter batch (shift + step) — select multiple steps - flow rate in advance settings -
-//save it in a step - duplicate the selection - exit batch edit via button
-
-describe('Batch Edit Transfo', () => {
+describe('Batch Edit Transform', () => {
   beforeEach(() => {
     cy.visit('/')
     cy.closeAnnouncementModal()
@@ -9,30 +6,9 @@ describe('Batch Edit Transfo', () => {
   })
 
   // import the batchEdit.json to PD
-  it('title', () => {
-    cy.fixture('../../fixtures/protocol/4/batchEdit.json').then(fileContent => {
-      cy.get('input[type=file]').upload({
-        fileContent: JSON.stringify(fileContent),
-        fileName: 'fixture.json',
-        mimeType: 'application/json',
-        encoding: 'utf8',
-      })
-      cy.get('[data-test="ComputingSpinner"]').should('exist')
-      // wait until computation is done before proceeding, with generous timeout
-      cy.get('[data-test="ComputingSpinner"]', { timeout: 30000 }).should(
-        'not.exist'
-      )
-    })
-
-    // Goto Design tab and verify the page
-
-    cy.get('button[id=NavTab_design]').click()
-    cy.get('button').contains('ok').click()
-
-    // Verify the Design Page
-    cy.get('#TitleBar_main > h1').contains('Multi select banner test protocol')
-    cy.get('#TitleBar_main > h2').contains('STARTING DECK STATE')
-    cy.get('button[id=StepCreationButton]').contains('+ Add Step')
+  it('Verify Flowrate, dublicate, delete functionality in Batch Edit Mode', () => {
+    importProtocol()
+    openDesignTab()
 
     // enter into the batch edit mode
     cy.get('[data-test="StepItem_1"]').click({
@@ -50,10 +26,54 @@ describe('Batch Edit Transfo', () => {
     cy.get('button').contains('Done').click()
     cy.get('button').contains('save').click()
 
+    //Verify that transfer and other step selection does not support multistep editing
+    cy.get('[data-test="StepItem_4"]').click({
+      ctrlKey: true,
+    })
+    cy.get('[id=Text_noSharedSettings]').contains(
+      'No editable settings shared between selected steps',
+      {
+        matchCase: false,
+      }
+    )
+
     //Dublicate the selected steps
-    // cy.get
+    cy.get('#ClickableIcon_duplicate').click()
+
+    //Expand ALL steps
+    cy.get('#ClickableIcon_expand').click()
+
+    //Delete the dublicated steps
+    cy.get('#ClickableIcon_delete').click()
+    cy.get('button').contains('delete steps').click()
 
     //Exit batch edit mode
     cy.get('button').contains('exit batch edit').click()
   })
 })
+
+function importProtocol() {
+  cy.fixture('../../fixtures/protocol/4/batchEdit.json').then(fileContent => {
+    cy.get('input[type=file]').upload({
+      fileContent: JSON.stringify(fileContent),
+      fileName: 'fixture.json',
+      mimeType: 'application/json',
+      encoding: 'utf8',
+    })
+    cy.get('[data-test="ComputingSpinner"]').should('exist')
+    // wait until computation is done before proceeding, with generous timeout
+    cy.get('[data-test="ComputingSpinner"]', { timeout: 30000 }).should(
+      'not.exist'
+    )
+  })
+}
+
+function openDesignTab() {
+  cy.get('button[id=NavTab_design]').click()
+  cy.get('button').contains('ok').click()
+
+  // Verify the Design Page
+  cy.get('#TitleBar_main > h1').contains('Multi select banner test protocol')
+  cy.get('#TitleBar_main > h2').contains('STARTING DECK STATE')
+  cy.get('button[id=StepCreationButton]').contains('+ Add Step')
+}
