7e628d735dc99a1a01d66f48e670df137bac2453
Max Marrone <max@marrone.nyc>
2021-03-16 10:55:05 -0400

    Trivially implement AbstractInstrument.

--- api/src/opentrons/protocols/context/engine/instrument_context.py
+++ api/src/opentrons/protocols/context/engine/instrument_context.py
@@ -1,8 +1,7 @@
-"""The interface that implements InstrumentContext."""
+"""An implementation of AbstractInstrument backed by Protocol Engine."""
 
 from __future__ import annotations
 
-from abc import abstractmethod, ABC
 import typing
 
 from opentrons import types
@@ -11,153 +10,124 @@ from opentrons.protocols.api_support.util import Clearances, PlungerSpeeds, \
     FlowRates
 from opentrons.protocols.context.well import WellImplementation
 
+from opentrons.protocols.context.instrument import AbstractInstrument
 
-class AbstractInstrument(ABC):
 
-    @abstractmethod
+class InstrumentContext(AbstractInstrument):
+
     def get_default_speed(self) -> float:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def set_default_speed(self, speed: float) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def aspirate(self,
                  volume: float,
                  rate: float) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def dispense(self,
                  volume: float,
                  rate: float) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def blow_out(self) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def touch_tip(self,
                   location: WellImplementation,
                   radius: float,
                   v_offset: float,
                   speed: float) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def pick_up_tip(self,
                     well: WellImplementation,
                     tip_length: float,
                     presses: typing.Optional[int],
                     increment: typing.Optional[float]) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def drop_tip(self, home_after: bool) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def home(self) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def home_plunger(self) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def delay(self) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def move_to(self,
                 location: types.Location,
                 force_direct: bool,
                 minimum_z_height: typing.Optional[float],
                 speed: typing.Optional[float]) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_mount(self) -> types.Mount:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_instrument_name(self) -> str:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_pipette_name(self) -> str:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_model(self) -> str:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_min_volume(self) -> float:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_max_volume(self) -> float:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_current_volume(self) -> float:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_available_volume(self) -> float:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_pipette(self) -> PipetteDict:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_channels(self) -> int:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def has_tip(self) -> bool:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def is_ready_to_aspirate(self) -> bool:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def prepare_for_aspirate(self) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_return_height(self) -> float:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_well_bottom_clearance(self) -> Clearances:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_speed(self) -> PlungerSpeeds:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_flow_rate(self) -> FlowRates:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def set_flow_rate(
             self,
             aspirate: typing.Optional[float] = None,
             dispense: typing.Optional[float] = None,
             blow_out: typing.Optional[float] = None) -> None:
-        ...
+        raise NotImplementedError()
 
-    @abstractmethod
     def set_pipette_speed(
             self,
             aspirate: typing.Optional[float] = None,
             dispense: typing.Optional[float] = None,
             blow_out: typing.Optional[float] = None) -> None:
-        ...
+        raise NotImplementedError()
