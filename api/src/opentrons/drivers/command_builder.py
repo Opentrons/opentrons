8bdcefab6bad0d3e16f83f6bb7a22838606a8157
amit lissack <amit@opentrons.com>
2021-03-20 10:56:20 -0400

    add command builder.

---
+++ api/src/opentrons/drivers/command_builder.py
@@ -0,0 +1,94 @@
+from typing import List
+
+
+class CommandBuilder:
+    """Class used to build GCODE commands."""
+
+    def __init__(self, terminator: str) -> None:
+        """
+        Construct a command builder.
+
+        Args:
+            terminator: The command terminator.
+        """
+        self._terminator = terminator
+        self._words: List[str] = []
+
+    def with_float(
+            self, prefix: str, value: float, precision: int = 3) -> 'CommandBuilder':
+        """
+        Add a float value.
+
+        Args:
+            prefix: The value prefix.
+            value: The float value.
+            precision: Rounding precision
+
+        Returns: self
+        """
+        rounded = round(value, precision)
+        return self.add_word(f"{prefix}{rounded}")
+
+    def with_int(self, prefix: str, value: int) -> 'CommandBuilder':
+        """
+        Add an integer value.
+
+        Args:
+            prefix: The value prefix
+            value: The integer value
+
+        Returns: self
+        """
+        return self.add_word(f"{prefix}{value}")
+
+    def with_gcode(self, gcode: str) -> 'CommandBuilder':
+        """
+        Add a GCODE.
+
+        Args:
+            gcode: The gcode
+
+        Returns: self
+        """
+        return self.add_word(gcode)
+
+    def with_builder(self, builder: 'CommandBuilder') -> 'CommandBuilder':
+        """
+        Add all words from builder
+
+        Args:
+            builder: a command builder
+
+        Returns: self
+        """
+        self._words += builder._words
+        return self
+
+    def add_word(self, word: str) -> 'CommandBuilder':
+        """
+        Add a word to the command builder
+
+        Args:
+            word: a word as a string
+
+        Returns: self
+        """
+        self._words.append(word)
+        return self
+
+    def build(self) -> str:
+        """
+        Build command as a string.
+
+        Returns: string
+        """
+        return " ".join(self._words + [self._terminator])
+
+    def __str__(self) -> str:
+        return self.build()
+
+    def __iter__(self):
+        return iter(self._words)
+
+    def __bool__(self) -> bool:
+        return len(self._words) != 0
---
+++ api/tests/opentrons/drivers/test_command_builder.py
@@ -0,0 +1,68 @@
+from opentrons.drivers.command_builder import CommandBuilder
+
+
+def test_builder_create_command_with_terminator() -> None:
+    """It should create a command with terminator."""
+    terminator = "terminator"
+    builder = CommandBuilder(terminator=terminator)
+    assert builder.build() == "terminator"
+
+
+def test_builder_create_command_with_float() -> None:
+    """It should create a command with a floating point value."""
+    terminator = "terminator"
+    builder = CommandBuilder(terminator=terminator)
+    assert builder.with_float(
+        prefix='Z', value=1.2342, precision=3
+    ).build() == "Z1.234 terminator"
+
+
+def test_builder_create_command_with_int() -> None:
+    """It should create a command with an integer point value."""
+    terminator = "terminator"
+    builder = CommandBuilder(terminator=terminator)
+    assert builder.with_int(
+        prefix='Z', value=15
+    ).build() == "Z15 terminator"
+
+
+def test_builder_create_command_with_gcode() -> None:
+    """It should create a command with a GCODE."""
+    terminator = "terminator"
+    builder = CommandBuilder(terminator=terminator)
+    assert builder.with_gcode(
+        gcode='G321'
+    ).build() == "G321 terminator"
+
+
+def test_builder_create_command_with_builder() -> None:
+    """It should create a command words in another builder."""
+    terminator = "terminator"
+    builder = CommandBuilder(terminator=terminator)
+    assert builder.with_gcode(
+        gcode='G321'
+    ).build() == "G321 terminator"
+
+    builder2 = CommandBuilder(terminator=terminator)
+    builder2.with_builder(
+        builder=builder
+    ).with_gcode(
+        gcode="G123"
+    ).with_builder(
+        builder=builder
+    ).build() == " G321 G123 G321 terminator"
+
+
+def test_builder_chain() -> None:
+    """It should create a command using chaining."""
+    terminator = "terminator"
+    builder = CommandBuilder(terminator=terminator)
+    assert builder.with_gcode(
+        gcode='G321'
+    ).with_float(
+        prefix="X", value=321
+    ).with_gcode(
+        gcode="M321"
+    ).with_int(
+        prefix="Z", value=3
+    ).with_gcode("G111").build() == "G321 X321 M321 Z3 G111 terminator"
