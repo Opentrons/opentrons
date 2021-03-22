4322293f792336d51b72e38ded4b79facbc71db6
Brian Cooper <brian@allegro.com>
2021-03-22 14:21:34 -0400

    reverse accidental meaningless code change

--- components/src/tooltips/useTooltip.ts
+++ components/src/tooltips/useTooltip.ts
@@ -43,7 +43,7 @@ const TOOLTIP_ID_PREFIX = 'Tooltip__'
  * }
  * ```
  */
-export function useTooltip(options: UseTooltipOptions = {}): UseTooltipResult {
+export function useTooltip (options: UseTooltipOptions = {}): UseTooltipResult {
   const { placement, strategy, offset = Styles.TOOLTIP_OFFSET_PX } = options
   const tooltipId = useRef(uniqueId(TOOLTIP_ID_PREFIX)).current
   const [target, targetRef] = useState<Element | null>(null)
@@ -84,7 +84,7 @@ export function useTooltip(options: UseTooltipOptions = {}): UseTooltipResult {
     style: tooltipState.tooltipStyle,
     arrowStyle: tooltipState.arrowStyle,
     placement: tooltipState.placement,
-    arrowRef: arrowRef,
+    arrowRef,
   }
 
   return [targetProps, tooltipProps]
