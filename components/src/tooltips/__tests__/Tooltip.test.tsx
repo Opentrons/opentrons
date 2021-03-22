8af29b6f9e70668728f9f19e94e2b80d8d046355
Brian Cooper <brian@allegro.com>
2021-03-22 14:20:37 -0400

    correctly type stubbed children

--- components/src/tooltips/__tests__/Tooltip.test.tsx
+++ components/src/tooltips/__tests__/Tooltip.test.tsx
@@ -15,7 +15,7 @@ const TOOLTIP_SELECTOR = 'div[role="tooltip"]'
 
 const render = (
   visible = true,
-  children = (<></>) as JSX.Element | string
+  children = (<></>) as React.ReactNode
 ): ReturnType<typeof mount> => {
   // NOTE(mc): redundant fragement necessary for enzyme issue with forwardRef
   // https://github.com/enzymejs/enzyme/issues/1852
