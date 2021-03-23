36e4931dff8816da78f2496edb089e985394dedf
Ian London <ianlondondu@gmail.com>
2021-03-23 15:38:37 -0400

    add missing declarations for shared-data (still any-typed though)

--- shared-data/index.d.ts
+++ shared-data/index.d.ts
@@ -13,3 +13,17 @@ export type LabwareWellGroupMetadata = any
 export type LabwareVolumeUnits = any
 export type LabwareDisplayCategory = any
 export type LabwareBrand = any
+export type WellBottomShape = any
+
+export type createRegularLabware = any
+
+export type LABWAREV2_DO_NOT_LIST = any
+
+export type getDisplayVolume = any
+export type getLabwareDefURI = any
+
+export type createRegularLoadName = any
+export type createDefaultDisplayName = any
+
+export type SLOT_LENGTH_MM = any
+export type SLOT_WIDTH_MM = any
