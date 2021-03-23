f7060c960139255e4f1e163d6b40733c700cf402
Ian London <ianlondondu@gmail.com>
2021-03-23 15:32:24 -0400

    any-type some shared-data

---
+++ shared-data/index.d.ts
@@ -0,0 +1,15 @@
+// Temporary defs until shared-data is actually converted
+
+export type LabwareDefinition2 = Record<string, unknown>
+
+// TODO IMMEDIATELY
+export type LabwareParameters = any
+export type LabwareOffset = any
+export type LabwareWell = any
+export type LabwareWellShapeProperties = any
+export type LabwareWellProperties = any
+export type LabwareWellMap = any
+export type LabwareWellGroupMetadata = any
+export type LabwareVolumeUnits = any
+export type LabwareDisplayCategory = any
+export type LabwareBrand = any
