298e046050a4bf77980b7d1fea654fac61d0632b
Brian Cooper <brian@allegro.com>
2021-03-24 14:52:55 -0400

    better ref type in useTimeout

--- components/src/hooks/useTimeout.ts
+++ components/src/hooks/useTimeout.ts
@@ -12,9 +12,7 @@ export function useTimeout(
   callback: () => unknown,
   delay: number | null
 ): void {
-  const savedCallback: React.MutableRefObject<
-    (() => unknown) | undefined
-  > = useRef()
+  const savedCallback = useRef<() => unknown>()
 
   // remember the latest callback
   useEffect(() => {
