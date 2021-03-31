9d0ad834028ff74421f00157bcd546bd781d5756
Shlok Amin <shlokamin@me.com>
2021-03-31 10:35:10 -0400

    feat(protocol-designer): add mix batch edit FF
    add mix batch edit FF
    
    #7559

--- protocol-designer/src/feature-flags/types.js
+++ protocol-designer/src/feature-flags/types.js
@@ -19,11 +19,15 @@ export const DEPRECATED_FLAGS = [
 ]
 
 // union of feature flag string constant IDs
-export type FlagTypes = 'PRERELEASE_MODE' | 'OT_PD_DISABLE_MODULE_RESTRICTIONS'
+export type FlagTypes =
+  | 'PRERELEASE_MODE'
+  | 'OT_PD_DISABLE_MODULE_RESTRICTIONS'
+  | 'OT_PD_ENABLE_BATCH_EDIT_MIX'
 
 // flags that are not in this list only show in prerelease mode
 export const userFacingFlags: Array<FlagTypes> = [
   'OT_PD_DISABLE_MODULE_RESTRICTIONS',
+  'OT_PD_ENABLE_BATCH_EDIT_MIX',
 ]
 
 export const allFlags: Array<FlagTypes> = [
