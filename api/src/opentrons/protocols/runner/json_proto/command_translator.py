4cf1ebbe6c02d1450727542aa19d434e100ba30e
amit lissack <amit@opentrons.com>
2021-04-05 13:20:49 -0400

    remove tests and dispatching.

--- api/src/opentrons/protocols/runner/json_proto/command_translator.py
+++ api/src/opentrons/protocols/runner/json_proto/command_translator.py
@@ -1,4 +1,4 @@
-from typing import Dict, Iterable
+from typing import Iterable
 
 from opentrons.protocol_engine import StateView
 from opentrons.protocol_engine.commands import CommandRequestType
@@ -37,189 +37,128 @@ class CommandTranslator:
             A collection of Protocol Engine command requests.
 
         """
-        try:
-            h = CommandTranslator._COMMAND_TO_NAME[command.command]
-            return getattr(self, h)(command)
-        except KeyError:
-            raise CommandTranslatorError(
-                f"'{command.command}' is not recognized.")
-        except AttributeError:
-            raise CommandTranslatorError(
-                f"Cannot find handler for '{command.command}'.")
+        raise NotImplementedError()
 
     def _aspirate(
             self,
             command: models.LiquidCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _dispense(
             self,
             command: models.LiquidCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _air_gap(
             self,
             command: models.LiquidCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _blowout(
             self,
             command: models.BlowoutCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _touch_tip(
             self,
             command: models.TouchTipCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _pick_up(
             self,
             command: models.PickUpDropTipCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _drop_tip(
             self,
             command: models.PickUpDropTipCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _move_to_slot(
             self,
             command: models.MoveToSlotCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _delay(
             self,
             command: models.DelayCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _magnetic_module_engage(
             self, command: models.MagneticModuleEngageCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _magnetic_module_disengage(
             self,
             command: models.MagneticModuleDisengageCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _temperature_module_set_target(
             self,
             command: models.TemperatureModuleSetTargetCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _temperature_module_await_temperature(
             self,
             command: models.TemperatureModuleAwaitTemperatureCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _temperature_module_deactivate(
             self,
             command: models.TemperatureModuleDeactivateCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_set_target_block_temperature(
             self,
             command: models.ThermocyclerSetTargetBlockTemperatureCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_set_target_lid_temperature(
             self,
             command: models.ThermocyclerSetTargetLidTemperatureCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_await_block_temperature(
             self,
             command: models.ThermocyclerAwaitBlockTemperatureCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_await_lid_temperature(
             self,
             command: models.ThermocyclerAwaitLidTemperatureCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_deactivate_block(
             self,
             command: models.ThermocyclerDeactivateBlockCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_deactivate_lid(
             self,
             command: models.ThermocyclerDeactivateLidCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_open_lid(
             self,
             command: models.ThermocyclerOpenLidCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_close_lid(
             self,
             command: models.ThermocyclerCloseLidCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_run_profile(
             self,
             command: models.ThermocyclerRunProfile) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _thermocycler_await_profile_complete(
             self,
             command: models.ThermocyclerAwaitProfileCompleteCommand) -> ReturnType:
-        pass
+        raise NotImplementedError()
 
     def _move_to_well(
             self,
             command: models.MoveToWellCommand) -> ReturnType:
-        pass
-
-    _COMMAND_TO_NAME: Dict[str, str] = {
-        models.CommandMoveToWell:
-            _move_to_well.__name__,
-        models.CommandThermocyclerAwaitProfile:
-            _thermocycler_await_profile_complete.__name__,
-        models.CommandThermocyclerRunProfile:
-            _thermocycler_run_profile.__name__,
-        models.CommandThermocyclerCloseLid:
-            _thermocycler_close_lid.__name__,
-        models.CommandThermocyclerOpenLid:
-            _thermocycler_open_lid.__name__,
-        models.CommandThermocyclerDeactivateLid:
-            _thermocycler_deactivate_lid.__name__,
-        models.CommandThermocyclerDeactivateBlock:
-            _thermocycler_deactivate_block.__name__,
-        models.CommandThermocyclerSetTargetLid:
-            _thermocycler_set_target_lid_temperature.__name__,
-        models.CommandThermocyclerAwaitBlockTemperature:
-            _thermocycler_await_block_temperature.__name__,
-        models.CommandThermocyclerAwaitLidTemperature:
-            _thermocycler_await_lid_temperature.__name__,
-        models.CommandThermocyclerSetTargetBlock:
-            _thermocycler_set_target_block_temperature.__name__,
-        models.CommandTemperatureModuleDeactivate:
-            _temperature_module_deactivate.__name__,
-        models.CommandTemperatureModuleAwait:
-            _temperature_module_await_temperature.__name__,
-        models.CommandTemperatureModuleSetTarget:
-            _temperature_module_set_target.__name__,
-        models.CommandMagneticModuleDisengage:
-            _magnetic_module_disengage.__name__,
-        models.CommandMagneticModuleEngage:
-            _magnetic_module_engage.__name__,
-        models.CommandDelay:
-            _delay.__name__,
-        models.CommandMoveToSlot:
-            _move_to_slot.__name__,
-        models.CommandDropTip:
-            _drop_tip.__name__,
-        models.CommandPickUpTip:
-            _pick_up.__name__,
-        models.CommandTouchTip:
-            _touch_tip.__name__,
-        models.CommandBlowout:
-            _blowout.__name__,
-        models.CommandAirGap:
-            _air_gap.__name__,
-        models.CommandDispense:
-            _dispense.__name__,
-        models.CommandAspirate:
-            _aspirate.__name__,
-    }
+        raise NotImplementedError()
--- api/tests/opentrons/protocols/runnner/json_proto/test_command_translator.py
+++ api/tests/opentrons/protocols/runnner/json_proto/test_command_translator.py
@@ -1,12 +1,10 @@
 import pytest
 from opentrons.protocol_engine import StateView
-from pytest_lazyfixture import lazy_fixture
 from mock import MagicMock
 
 from opentrons.protocols.runner.json_proto.command_translator import (
-    CommandTranslator, CommandTranslatorError,
+    CommandTranslator
 )
-from opentrons.protocols.runner.json_proto.models import json_protocol as models
 
 
 @pytest.fixture
@@ -17,71 +15,3 @@ def mock_state_view() -> MagicMock:
 @pytest.fixture
 def translator(mock_state_view) -> CommandTranslator:
     return CommandTranslator(state_view=mock_state_view)
-
-
-@pytest.mark.parametrize(
-    argnames=["command", "translator_method_name"],
-    argvalues=[
-        [lazy_fixture("aspirate_command"), "_aspirate"],
-        [lazy_fixture("dispense_command"), "_dispense"],
-        [lazy_fixture("air_gap_command"), "_air_gap"],
-        [lazy_fixture("blowout_command"), "_blowout"],
-        [lazy_fixture("touch_tip_command"), "_touch_tip"],
-        [lazy_fixture("pick_up_command"), "_pick_up"],
-        [lazy_fixture("drop_tip_command"), "_drop_tip"],
-        [lazy_fixture("move_to_slot_command"), "_move_to_slot"],
-        [lazy_fixture("delay_command"), "_delay"],
-        [lazy_fixture("magnetic_module_engage_command"),
-         "_magnetic_module_engage"],
-        [lazy_fixture("magnetic_module_disengage_command"),
-         "_magnetic_module_disengage"],
-        [lazy_fixture("temperature_module_set_target_command"),
-         "_temperature_module_set_target"],
-        [lazy_fixture("temperature_module_await_temperature_command"),
-         "_temperature_module_await_temperature"],
-        [lazy_fixture("temperature_module_deactivate_command"),
-         "_temperature_module_deactivate"],
-        [lazy_fixture("thermocycler_set_target_block_temperature_command"),
-         "_thermocycler_set_target_block_temperature"],
-        [lazy_fixture("thermocycler_set_target_lid_temperature_command"),
-         "_thermocycler_set_target_lid_temperature"],
-        [lazy_fixture("thermocycler_await_block_temperature_command"),
-         "_thermocycler_await_block_temperature"],
-        [lazy_fixture("thermocycler_await_lid_temperature_command"),
-         "_thermocycler_await_lid_temperature"],
-        [lazy_fixture("thermocycler_deactivate_block_command"),
-         "_thermocycler_deactivate_block"],
-        [lazy_fixture("thermocycler_deactivate_lid_command"),
-         "_thermocycler_deactivate_lid"],
-        [lazy_fixture("thermocycler_open_lid_command"),
-         "_thermocycler_open_lid"],
-        [lazy_fixture("thermocycler_close_lid_command"),
-         "_thermocycler_close_lid"],
-        [lazy_fixture("thermocycler_run_profile"),
-         "_thermocycler_run_profile"],
-        [lazy_fixture("thermocycler_await_profile_complete_command"),
-         "_thermocycler_await_profile_complete"],
-        [lazy_fixture("move_to_well_command"), "_move_to_well"],
-    ]
-)
-def test_translate_command_dispatched(
-        translator: CommandTranslator, command: models.AllCommands,
-        translator_method_name: str) -> None:
-    """It should call the correct method based on command type."""
-    # Mock the handler function.
-    m = MagicMock(return_value=[])
-    setattr(translator, translator_method_name, m)
-
-    result = translator.translate(command)
-
-    m.assert_called_once_with(command)
-    assert result == []
-
-
-def test_translate_commands_fail_unknown_command(
-        translator: CommandTranslator, thermocycler_close_lid_command) -> None:
-    """It should raise on unknown command."""
-    thermocycler_close_lid_command.command = "Phony"
-
-    with pytest.raises(CommandTranslatorError):
-        translator.translate(thermocycler_close_lid_command)
