43e40705bbe6d7fd14f52044d1d9d22ad0ae32bc
Ian London <ianlondondu@gmail.com>
2021-04-08 10:39:45 -0400

    cast PD worker to TimelineWorker

--- protocol-designer/src/timelineMiddleware/makeTimelineMiddleware.js
+++ protocol-designer/src/timelineMiddleware/makeTimelineMiddleware.js
@@ -43,7 +43,9 @@ const getSubstepsArgs = (state: BaseState): SubstepsArgsNoTimeline => ({
 
 // TODO(IL, 2020-06-15): once we create an Action union for PD, use that instead of `any` for Middleware<S, A>
 export const makeTimelineMiddleware: () => Middleware<BaseState, any> = () => {
-  const worker: TimelineWorker = new Worker('./worker', { type: 'module' })
+  const worker: TimelineWorker = (new Worker('./worker', {
+    type: 'module',
+  }): any)
 
   let prevTimelineArgs: GenerateRobotStateTimelineArgs | null = null // caches results of dependent selectors, eg {[selectorIndex]: lastCachedSelectorValue}
   let prevSubstepsArgs: SubstepsArgsNoTimeline | null = null
