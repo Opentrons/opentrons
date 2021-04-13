9a583ed24f3b17cd4cac3a5c0e4a1642afa0b782
Mike Cousins <mike@opentrons.com>
2021-04-13 15:20:34 -0400

    fixup: sync changes with edge

--- api/src/opentrons/protocol_api_experimental/protocol_context.py
+++ api/src/opentrons/protocol_api_experimental/protocol_context.py
@@ -6,25 +6,22 @@ from .instrument_context import InstrumentContext
 from .labware import Labware
 
 
-class ProtocolContext:
-    # noqa: D101
+class ProtocolContext:  # noqa: D101
 
-    def load_instrument(
+    def load_instrument(  # noqa: D102
         self,
         instrument_name: str,
         mount: str,
         tip_racks: typing.Sequence[Labware] = tuple(),
         replace: bool = False
     ) -> InstrumentContext:
-        # noqa: D102
         # Changes from APIv2:
         #   * mount must be a str, not types.Mount.
         #   * tip_racks is a Sequence[Labware] defaulting to empty, not an
         #     implicitly optional List[Labware].
         raise NotImplementedError()
 
-    def load_labware(self) -> Labware:
-        # noqa: D102
+    def load_labware(self) -> Labware:  # noqa: D102
         raise NotImplementedError()
 
     # todo(mm, 2021-04-09): Add all other public methods from the APIv2
