264db7007849ab724cd080dd69d1998c989ff6d7
Brian Cooper <brian@allegro.com>
2021-04-20 15:02:01 -0400

    calibration session types

--- app/src/redux/sessions/pipette-offset-calibration/types.ts
+++ app/src/redux/sessions/pipette-offset-calibration/types.ts
@@ -1,5 +1,5 @@
 // pipette offset calibration types
-import typeof {
+import {
   PIP_OFFSET_STEP_SESSION_STARTED,
   PIP_OFFSET_STEP_LABWARE_LOADED,
   PIP_OFFSET_STEP_PREPARING_PIPETTE,
@@ -14,36 +14,36 @@ import type { CalibrationLabware, SessionCommandString } from '../types'
 import type { LabwareDefinition2 } from '@opentrons/shared-data'
 
 export type PipetteOffsetCalibrationStep =
-  | PIP_OFFSET_STEP_SESSION_STARTED
-  | PIP_OFFSET_STEP_LABWARE_LOADED
-  | PIP_OFFSET_STEP_PREPARING_PIPETTE
-  | PIP_OFFSET_STEP_INSPECTING_TIP
-  | PIP_OFFSET_STEP_TIP_LENGTH_COMPLETE
-  | PIP_OFFSET_STEP_JOGGING_TO_DECK
-  | PIP_OFFSET_STEP_SAVING_POINT_ONE
-  | PIP_OFFSET_STEP_CALIBRATION_COMPLETE
-  | PIP_OFFSET_STEP_SESSION_EXITED
+  | typeof PIP_OFFSET_STEP_SESSION_STARTED
+  | typeof PIP_OFFSET_STEP_LABWARE_LOADED
+  | typeof PIP_OFFSET_STEP_PREPARING_PIPETTE
+  | typeof PIP_OFFSET_STEP_INSPECTING_TIP
+  | typeof PIP_OFFSET_STEP_TIP_LENGTH_COMPLETE
+  | typeof PIP_OFFSET_STEP_JOGGING_TO_DECK
+  | typeof PIP_OFFSET_STEP_SAVING_POINT_ONE
+  | typeof PIP_OFFSET_STEP_CALIBRATION_COMPLETE
+  | typeof PIP_OFFSET_STEP_SESSION_EXITED
 
-export type PipetteOffsetCalibrationInstrument = {
-  model: string,
-  name: string,
-  tipLength: number,
-  mount: string,
-  serial: string,
-  defaultTipracks: LabwareDefinition2[],
+export interface PipetteOffsetCalibrationInstrument {
+  model: string
+  name: string
+  tipLength: number
+  mount: string
+  serial: string
+  defaultTipracks: LabwareDefinition2[]
 }
 
-export type PipetteOffsetCalibrationSessionParams = {
-  mount: string,
-  shouldRecalibrateTipLength: boolean,
-  hasCalibrationBlock: boolean,
-  tipRackDefinition: LabwareDefinition2 | null,
+export interface PipetteOffsetCalibrationSessionParams {
+  mount: string
+  shouldRecalibrateTipLength: boolean
+  hasCalibrationBlock: boolean
+  tipRackDefinition: LabwareDefinition2 | null
 }
 
-export type PipetteOffsetCalibrationSessionDetails = {
-  instrument: PipetteOffsetCalibrationInstrument,
-  currentStep: PipetteOffsetCalibrationStep,
-  labware: CalibrationLabware[],
-  shouldPerformTipLength: boolean,
-  supportedCommands: SessionCommandString[],
+export interface PipetteOffsetCalibrationSessionDetails {
+  instrument: PipetteOffsetCalibrationInstrument
+  currentStep: PipetteOffsetCalibrationStep
+  labware: CalibrationLabware[]
+  shouldPerformTipLength: boolean
+  supportedCommands: SessionCommandString[]
 }
--- app/src/redux/sessions/tip-length-calibration/types.ts
+++ app/src/redux/sessions/tip-length-calibration/types.ts
@@ -1,5 +1,5 @@
 // tip length calibration types
-import typeof {
+import {
   TIP_LENGTH_STEP_SESSION_STARTED,
   TIP_LENGTH_STEP_LABWARE_LOADED,
   TIP_LENGTH_STEP_MEASURING_NOZZLE_OFFSET,
@@ -13,31 +13,31 @@ import type { CalibrationLabware } from '../types'
 import type { LabwareDefinition2 } from '@opentrons/shared-data'
 
 export type TipLengthCalibrationStep =
-  | TIP_LENGTH_STEP_SESSION_STARTED
-  | TIP_LENGTH_STEP_LABWARE_LOADED
-  | TIP_LENGTH_STEP_MEASURING_NOZZLE_OFFSET
-  | TIP_LENGTH_STEP_PREPARING_PIPETTE
-  | TIP_LENGTH_STEP_INSPECTING_TIP
-  | TIP_LENGTH_STEP_MEASURING_TIP_OFFSET
-  | TIP_LENGTH_STEP_CALIBRATION_COMPLETE
+  | typeof TIP_LENGTH_STEP_SESSION_STARTED
+  | typeof TIP_LENGTH_STEP_LABWARE_LOADED
+  | typeof TIP_LENGTH_STEP_MEASURING_NOZZLE_OFFSET
+  | typeof TIP_LENGTH_STEP_PREPARING_PIPETTE
+  | typeof TIP_LENGTH_STEP_INSPECTING_TIP
+  | typeof TIP_LENGTH_STEP_MEASURING_TIP_OFFSET
+  | typeof TIP_LENGTH_STEP_CALIBRATION_COMPLETE
 
-export type TipLengthCalibrationInstrument = {
-  model: string,
-  name: string,
-  tipLength: number,
-  mount: string,
-  serial: string,
-  defaultTipracks: LabwareDefinition2[],
+export interface TipLengthCalibrationInstrument {
+  model: string
+  name: string
+  tipLength: number
+  mount: string
+  serial: string
+  defaultTipracks: LabwareDefinition2[]
 }
 
-export type TipLengthCalibrationSessionParams = {
-  mount: string,
-  hasCalibrationBlock: boolean,
-  tipRackDefinition: LabwareDefinition2,
+export interface TipLengthCalibrationSessionParams {
+  mount: string
+  hasCalibrationBlock: boolean
+  tipRackDefinition: LabwareDefinition2
 }
 
-export type TipLengthCalibrationSessionDetails = {
-  instrument: TipLengthCalibrationInstrument,
-  currentStep: TipLengthCalibrationStep,
-  labware: CalibrationLabware[],
+export interface TipLengthCalibrationSessionDetails {
+  instrument: TipLengthCalibrationInstrument
+  currentStep: TipLengthCalibrationStep
+  labware: CalibrationLabware[]
 }
