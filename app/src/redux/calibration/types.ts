ccb18cd62bf35e69b09ed7b46a54d58821288be0
Brian Cooper <brian@allegro.com>
2021-04-22 14:20:25 -0400

    calibration types

--- app/src/redux/calibration/types.ts
+++ app/src/redux/calibration/types.ts
@@ -1,4 +1,3 @@
-
 import type {
   RobotApiRequestMeta,
   RobotApiErrorResponse,
@@ -15,36 +14,36 @@ import type { LabwareCalibrationAction } from './labware/types'
 import type { PipetteOffsetCalibrationsAction } from './pipette-offset/types'
 import type { TipLengthCalibrationsAction } from './tip-length/types'
 
-import typeof {
+import {
   FETCH_CALIBRATION_STATUS,
   FETCH_CALIBRATION_STATUS_SUCCESS,
   FETCH_CALIBRATION_STATUS_FAILURE,
 } from './constants'
 
-export type * from './api-types'
-export type * from './labware/types'
-export type * from './pipette-offset/types'
-export type * from './tip-length/types'
+export * from './api-types'
+export * from './labware/types'
+export * from './pipette-offset/types'
+export * from './tip-length/types'
 
-export type FetchCalibrationStatusAction = {
-  type: FETCH_CALIBRATION_STATUS,
-  payload: { robotName: string },
-  meta: RobotApiRequestMeta,
+export interface FetchCalibrationStatusAction {
+  type: typeof FETCH_CALIBRATION_STATUS
+  payload: { robotName: string }
+  meta: RobotApiRequestMeta
 }
 
-export type FetchCalibrationStatusSuccessAction = {
-  type: FETCH_CALIBRATION_STATUS_SUCCESS,
+export interface FetchCalibrationStatusSuccessAction {
+  type: typeof FETCH_CALIBRATION_STATUS_SUCCESS
   payload: {
-    robotName: string,
-    calibrationStatus: CalibrationStatus,
-  },
-  meta: RobotApiRequestMeta,
+    robotName: string
+    calibrationStatus: CalibrationStatus
+  }
+  meta: RobotApiRequestMeta
 }
 
-export type FetchCalibrationStatusFailureAction = {
-  type: FETCH_CALIBRATION_STATUS_FAILURE,
-  payload: { robotName: string, error: RobotApiErrorResponse },
-  meta: RobotApiRequestMeta,
+export interface FetchCalibrationStatusFailureAction {
+  type: typeof FETCH_CALIBRATION_STATUS_FAILURE
+  payload: { robotName: string; error: RobotApiErrorResponse }
+  meta: RobotApiRequestMeta
 }
 
 export type CalibrationAction =
@@ -55,14 +54,15 @@ export type CalibrationAction =
   | PipetteOffsetCalibrationsAction
   | TipLengthCalibrationsAction
 
-export type PerRobotCalibrationState = $ReadOnly<{
-  calibrationStatus: CalibrationStatus | null,
-  labwareCalibrations: AllLabwareCalibrations | null,
-  pipetteOffsetCalibrations: AllPipetteOffsetCalibrations | null,
-  tipLengthCalibrations: AllTipLengthCalibrations | null,
+export type PerRobotCalibrationState = Readonly<{
+  calibrationStatus: CalibrationStatus | null
+  labwareCalibrations: AllLabwareCalibrations | null
+  pipetteOffsetCalibrations: AllPipetteOffsetCalibrations | null
+  tipLengthCalibrations: AllTipLengthCalibrations | null
 }>
 
-export type CalibrationState = $ReadOnly<{
-  [robotName: string]: PerRobotCalibrationState,
-  ...
-}>
+export type CalibrationState = Readonly<
+  Partial<{
+    [robotName: string]: PerRobotCalibrationState
+  }>
+>
