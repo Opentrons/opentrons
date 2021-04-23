4d3d6b85372e52350b3a86b29114a38145374a06
Brian Cooper <brian@allegro.com>
2021-04-23 12:18:42 -0400

    logger mock types

--- app/src/__mocks__/logger.ts
+++ app/src/__mocks__/logger.ts
@@ -1,20 +1,25 @@
 // mock logger for tests
 import path from 'path'
 
-export function createLogger(filename) {
+export function createLogger(
+  filename: string
+): (message: string, meta: unknown) => void {
   const label = path.relative(path.join(__dirname, '../../..'), filename)
 
+  // @ts-expect-error
   return new Proxy(
     {},
     {
-      get(_, level) {
-        return (message, meta) =>
+      get(_, level: string) {
+        return (message: string, meta: unknown) =>
           console.log(`[${label}] ${level}: ${message} %j`, meta)
       },
     }
   )
 }
 
-export const useLogger = filename => {
+export const useLogger = (
+  filename: string
+): ((message: string, meta: unknown) => void) => {
   return createLogger(filename)
 }
