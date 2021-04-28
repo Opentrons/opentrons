24d6f151a013eb1eb84f86f2a7d9d29e44568eae
amitlissack <amit@opentrons.com>
2021-04-28 11:13:07 -0400

    feat(api): CommandTranslator._aspirate implemented. (#7722)
    closes #7434

--- api/tests/opentrons/protocols/runnner/json_proto/test_command_translator.py
+++ api/tests/opentrons/protocols/runnner/json_proto/test_command_translator.py
@@ -1,6 +1,8 @@
 import pytest
-from opentrons.protocol_engine import StateView
 from mock import MagicMock
+from opentrons.protocol_engine import StateView, WellLocation, WellOrigin
+from opentrons.protocol_engine.commands import AspirateRequest
+from opentrons.protocols.runner.json_proto.models import json_protocol as models
 
 from opentrons.protocols.runner.json_proto.command_translator import (
     CommandTranslator
@@ -13,5 +15,24 @@ def mock_state_view() -> MagicMock:
 
 
 @pytest.fixture
-def translator(mock_state_view) -> CommandTranslator:
+def subject(mock_state_view) -> CommandTranslator:
     return CommandTranslator(state_view=mock_state_view)
+
+
+def test_aspirate(subject, aspirate_command: models.LiquidCommand) -> None:
+    """It should translate a JSON aspirate command to a Protocol Engine
+     aspirate request."""
+    request = subject.translate(aspirate_command)
+
+    assert request == [
+        AspirateRequest(
+            pipetteId=aspirate_command.params.pipette,
+            labwareId=aspirate_command.params.labware,
+            wellName=aspirate_command.params.well,
+            volume=aspirate_command.params.volume,
+            wellLocation=WellLocation(
+                origin=WellOrigin.BOTTOM,
+                offset=(0, 0, aspirate_command.params.offsetFromBottomMm)
+            )
+        )
+    ]
