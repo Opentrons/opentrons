077aa254ef524a957a10c8297afac9f5956617f5
Shlok Amin <shlokamin@me.com>
2021-04-28 14:33:59 -0400

    refactor(labware-creator): split apart sections into separate components (#7735)
    #7707

---
+++ labware-library/src/labware-creator/components/sections/UploadExisting.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import { PrimaryButton } from '@opentrons/components'
+import { ImportLabware } from '../ImportLabware'
+import styles from '../../styles.css'
+
+interface Props {
+  disabled: boolean
+  labwareTypeChildFields: any
+  lastUploaded: any
+  onClick: () => void
+  onUpload: (
+    event:
+      | React.DragEvent<HTMLLabelElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) => void
+}
+
+export const UploadExisting = (props: Props): JSX.Element => {
+  const {
+    disabled,
+    labwareTypeChildFields,
+    lastUploaded,
+    onClick,
+    onUpload,
+  } = props
+  return (
+    <div className={styles.upload_existing_section}>
+      <h2 className={styles.setup_heading}>
+        Edit a file you’ve built with our labware creator omg!
+      </h2>
+      {lastUploaded === null ? (
+        <ImportLabware onUpload={onUpload} />
+      ) : (
+        <div className={styles.labware_type_fields}>
+          {labwareTypeChildFields}
+          <PrimaryButton
+            className={styles.start_creating_btn}
+            onClick={onClick}
+            disabled={disabled}
+          >
+            start editing labware
+          </PrimaryButton>
+        </div>
+      )}
+    </div>
+  )
+}
