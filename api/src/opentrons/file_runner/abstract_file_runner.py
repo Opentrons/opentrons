60df55626054dff24d1a1833a414d22c02ee9293
Mike Cousins <mike@opentrons.com>
2021-05-03 15:33:41 -0400

    refactor(api): add scaffolding for opentrons.file_runner module (#7757)
    Closes #7644

---
+++ api/src/opentrons/file_runner/abstract_file_runner.py
@@ -0,0 +1,21 @@
+"""Abstract interfaces for protocol file running."""
+from abc import ABC, abstractmethod
+
+
+class AbstractFileRunner(ABC):
+    """Abstract interface for an object that can run protocol files."""
+
+    @abstractmethod
+    def play(self) -> None:
+        """Start (or un-pause) running the protocol file."""
+        ...
+
+    @abstractmethod
+    def pause(self) -> None:
+        """Pause the running protocol file's execution."""
+        ...
+
+    @abstractmethod
+    def stop(self) -> None:
+        """Cancel the running protocol file."""
+        ...
---
+++ api/src/opentrons/file_runner/python_file_runner.py
@@ -0,0 +1,18 @@
+"""File runner interfaces for Python protocols."""
+from .abstract_file_runner import AbstractFileRunner
+
+
+class PythonFileRunner(AbstractFileRunner):
+    """Python protocol file runner."""
+
+    def play(self) -> None:
+        """Start (or un-pause) running the Python protocol file."""
+        raise NotImplementedError()
+
+    def pause(self) -> None:
+        """Pause the running Python protocol file's execution."""
+        raise NotImplementedError()
+
+    def stop(self) -> None:
+        """Cancel the running Python protocol file."""
+        raise NotImplementedError()
