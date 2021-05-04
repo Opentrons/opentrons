b6d924e6584be18bfeffca4343854c35f720f753
Mike Cousins <mike@opentrons.com>
2021-05-04 10:47:55 -0400

    refactor(api): move logic from opentrons.protocols.context.engine
    Closes #7699

--- api/src/opentrons/protocol_api_experimental/errors.py
+++ api/src/opentrons/protocol_api_experimental/errors.py
@@ -1,5 +1,8 @@
 """Python Protocol API v3 errors."""
 # TODO(mc, 2021-04-22): assign unique codes to all these errors
+# TODO(mc, 2021-04-22): explore how ProtocolEngine and ProtocolAPI errors
+# interact and combine
+from opentrons.protocol_engine.errors import LabwareIsNotTipRackError
 
 
 class InvalidPipetteNameError(ValueError):
@@ -18,3 +21,12 @@ class InvalidMountError(ValueError):
         """Initialize the error and message with the invalid value."""
         super().__init__(f"{invalid_value} is not a valid mount.")
         self.invalid_value = invalid_value
+
+
+__all__ = [
+    # re-exports from opentrons.protocol_engine
+    "LabwareIsNotTipRackError",
+    # exports specific to the Protocol API layer
+    "InvalidPipetteNameError",
+    "InvalidMountError",
+]
