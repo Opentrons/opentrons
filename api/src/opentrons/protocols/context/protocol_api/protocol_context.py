af1a29aefd25df84fd51a35858f133a37d7c13db
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2021-05-12 13:01:12 -0400

    fix(api): add pause manager to separate pause and delay (#7773)
    * fix(api): add pause mananger to separate pause and delay
    
    * let session handle door pause
    
    * relieve session of safety door duty
    
    * update tests
    
    * make requested changes
    
    * add try finally block
    
    * pull pause mgr out of api
    
    * add should_pause prop

--- api/src/opentrons/protocols/context/protocol_api/protocol_context.py
+++ api/src/opentrons/protocols/context/protocol_api/protocol_context.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 from opentrons import types, API
 from opentrons.protocols.api_support.types import APIVersion
 from opentrons.config import feature_flags as fflags
-from opentrons.hardware_control.types import DoorState
+from opentrons.hardware_control.types import DoorState, PauseType
 from opentrons.hardware_control import SynchronousAdapter
 from opentrons.protocols.api_support.definitions import MAX_SUPPORTED_VERSION
 from opentrons.protocols.geometry.deck import Deck
@@ -259,11 +259,11 @@ class ProtocolContextImplementation(AbstractProtocol):
 
     def pause(self, msg: Optional[str]) -> None:
         """Pause the protocol."""
-        self._hw_manager.hardware.pause()
+        self._hw_manager.hardware.pause(PauseType.PAUSE)
 
     def resume(self) -> None:
         """Result the protocol."""
-        self._hw_manager.hardware.resume()
+        self._hw_manager.hardware.resume(PauseType.PAUSE)
 
     def comment(self, msg: str) -> None:
         """Add comment to run log."""
