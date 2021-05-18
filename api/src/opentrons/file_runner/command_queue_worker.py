763cb618500ba8c506036b6ee817d43d43da5a56
Max Marrone <max@marrone.nyc>
2021-05-18 16:17:44 -0400

    Flesh out docstrings (characterizing existing behavior).

--- api/src/opentrons/file_runner/command_queue_worker.py
+++ api/src/opentrons/file_runner/command_queue_worker.py
@@ -6,7 +6,7 @@ from opentrons.protocol_engine import ProtocolEngine
 
 
 class CommandQueueWorker:
-    """A class that executes the queued commands in a ProtocolEngine."""
+    """Execute a `ProtocolEngine`'s queued commands in the background."""
 
     def __init__(self, protocol_engine: ProtocolEngine) -> None:
         """Construct a CommandQueueWorker.
@@ -19,7 +19,15 @@ class CommandQueueWorker:
         self._terminate = False
 
     def play(self) -> None:
-        """Start/resume queued command execution."""
+        """Start executing the `ProtocolEngine`'s queued commands.
+
+        The commands are executed in order, one by one, in a concurrent background task.
+
+        See `wait_terminated` for when execution may stop.
+
+        If this `CommandQueueWorker` was already executing queued commands,
+        this method is a no-op.
+        """
         if self._task is None:
             self._terminate = False
             self._task = asyncio.create_task(self._loop())
@@ -29,15 +37,41 @@ class CommandQueueWorker:
         self.stop()
 
     def stop(self) -> None:
-        """Stop queued command execution."""
+        """Stop queued command execution.
+
+        If a command is currently executing, it will continue. Further commands will
+        be left unexecuted in the queue.
+
+        If this worker was already stopped, this method is a no-op.
+        """
         if self._task is not None:
             self._terminate = True
+            # fixme(mm, 2021-05-18): I think this is a bug if stop() is called before
+            # wait_terminated(). It discards the background task before it's awaited,
+            # which both leaks the task and throws away any errors raised in the task
+            # that we haven't yet seen.
             self._task = None
 
     async def wait_terminated(self) -> None:
-        """Wait for background task to terminate.
+        """Wait until this `CommandQueueWorker` has stopped executing new commands.
 
-        This can occur either due to a pause, stop, or no more pending requests.
+        Execution will stop when any of the following happen:
+
+          * A command raises an error.
+          * The queue of commands in the `ProtocolEngine` runs empty.
+            (Execution will not automatically resume if more commands are then added.)
+          * You call `stop`.
+
+        If a command is currently executing, this method will return after that command
+        finishes.
+
+        When you're finished with a `CommandQueueWorker`, you must call this method on
+        it. This gives it a chance to clean up its background task, and propagate any
+        errors.
+
+        Raises:
+            ProtocolEngineError: If there was a problem executing one of the commands
+                in the queue.
         """
         if self._task:
             await self._task
