0ce5f67ef6b47f01d00ec429d63b0ce730575615
Brian Arthur Cooper <brian@opentrons.com>
2021-05-27 16:17:56 -0400

    Update components/__utils__/mountWithStore.ts
    correct mock for mountWithStore util
    
    Co-authored-by: Mike Cousins <mike@opentrons.com>

--- components/__utils__/mountWithStore.ts
+++ components/__utils__/mountWithStore.ts
@@ -7,7 +7,7 @@ import type { ReactWrapper } from 'enzyme'
 
 export interface MockStore<State, Action> {
   getState: jest.MockedFunction<() => State>
-  subscribe: jest.MockedFunction<(args: []) => void>
+  subscribe: jest.MockedFunction<() => void>
   dispatch: jest.MockedFunction<(...actions: Action[]) => Action>
 }
 
