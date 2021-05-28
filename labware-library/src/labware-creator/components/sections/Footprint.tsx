7fabbd94d5912f9d9bd6ff4268a6745247cae836
Sarah Breen <sarah.breen@opentrons.com>
2021-05-28 11:52:32 -0400

    feat(labware-creator): add dynamic footprint copy for tip racks
    fix #7715

--- labware-library/src/labware-creator/components/sections/Footprint.tsx
+++ labware-library/src/labware-creator/components/sections/Footprint.tsx
@@ -12,38 +12,51 @@ import styles from '../../styles.css'
 
 const maskTo2Decimal = makeMaskToDecimal(2)
 
-const Content = (): JSX.Element => (
-  <div className={styles.flex_row}>
-    <div className={styles.instructions_column}>
-      <p>
-        Ensure measurement is taken from the <strong>very bottom</strong> of
-        plate.
-      </p>
-      <p>
-        The footprint measurement helps determine if the labware fits firmly
-        into the slots on the OT-2 deck.
-      </p>
+interface ContentProps {
+  values: LabwareFields
+}
+const Content = (props: ContentProps): JSX.Element => {
+  const { values } = props
+
+  return (
+    <div className={styles.flex_row}>
+      <div className={styles.instructions_column}>
+        {values.labwareType === 'tipRack' && (
+          <p>
+            If your Tip Rack has an adapter,{' '}
+            <strong>place it in the adapter.</strong>
+          </p>
+        )}
+        <p>
+          Ensure measurement is taken from the <strong>very bottom</strong> of
+          labware.
+        </p>
+        <p>
+          The footprint measurement helps determine if the labware (in adapter
+          if needed) fits firmly into the slots on the OT-2 deck.
+        </p>
+      </div>
+      <div className={styles.diagram_column}>
+        <img
+          src={require('../../images/footprint.svg')}
+          alt="labware footprint"
+        />
+      </div>
+      <div className={styles.form_fields_column}>
+        <TextField
+          name="footprintXDimension"
+          inputMasks={[maskTo2Decimal]}
+          units="mm"
+        />
+        <TextField
+          name="footprintYDimension"
+          inputMasks={[maskTo2Decimal]}
+          units="mm"
+        />
+      </div>
     </div>
-    <div className={styles.diagram_column}>
-      <img
-        src={require('../../images/footprint.svg')}
-        alt="labware footprint"
-      />
-    </div>
-    <div className={styles.form_fields_column}>
-      <TextField
-        name="footprintXDimension"
-        inputMasks={[maskTo2Decimal]}
-        units="mm"
-      />
-      <TextField
-        name="footprintYDimension"
-        inputMasks={[maskTo2Decimal]}
-        units="mm"
-      />
-    </div>
-  </div>
-)
+  )
+}
 
 export const Footprint = (): JSX.Element | null => {
   const fieldList: Array<keyof LabwareFields> = [
@@ -62,7 +75,7 @@ export const Footprint = (): JSX.Element | null => {
         <>
           <FormAlerts touched={touched} errors={errors} fieldList={fieldList} />
           <XYDimensionAlerts values={values} touched={touched} />
-          <Content />
+          <Content values={values} />
         </>
       </SectionBody>
     </div>
