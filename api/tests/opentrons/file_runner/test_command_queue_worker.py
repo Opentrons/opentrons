a4617d4f1a63d56dfb6154f0c1b76b2d29637ac6
Max Marrone <max@marrone.nyc>
2021-06-09 10:39:00 -0400

    Rename test_stop() to test_pause().

--- api/tests/opentrons/file_runner/test_command_queue_worker.py
+++ api/tests/opentrons/file_runner/test_command_queue_worker.py
@@ -102,7 +102,7 @@ async def test_play(
     assert protocol_engine.execute_command.call_args_list == expected_call_args_list
 
 
-async def test_stop(
+async def test_pause(
         protocol_engine: AsyncMock,
         subject: CommandQueueWorker,
         store_with_commands: AsyncMock,
