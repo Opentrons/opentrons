55e8cb8be55685a732be9bedd417d70c675c900b
curtelsasser <curt.elsasser@opentrons.com>
2021-06-11 12:10:11 -0400

    Addressing some of the feedback. Still need to: - replace css components with basic UI atom components (see PR feedback) - manually and thoroughly test all scenarios - unit test changes

--- api/src/opentrons/hardware_control/api.py
+++ api/src/opentrons/hardware_control/api.py
@@ -339,11 +339,11 @@ class API(HardwareAPILike):
         await self._wait_for_is_running()
         self.pause(PauseType.DELAY)
         try:
-            if not self.is_simulator:
-                async def sleep_for_seconds(seconds: float):
-                    await asyncio.sleep(seconds)
-                delay_task = self._loop.create_task(sleep_for_seconds(duration_s))
-                await self._execution_manager.register_cancellable_task(delay_task)
+            async def sleep_for_seconds(seconds: float):
+                await asyncio.sleep(seconds)
+
+            delay_task = self._loop.create_task(sleep_for_seconds(duration_s))
+            await self._execution_manager.register_cancellable_task(delay_task)
         finally:
             self.resume(PauseType.DELAY)
 
