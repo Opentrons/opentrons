7d58098f543dc53ceb942db43e8ca7e748c0c3a6
Mike Cousins <mike@opentrons.com>
2021-06-14 13:56:04 -0400

    fixup: add xfailed test for race condition

--- robot-server/tests/sessions/test_engine_store.py
+++ robot-server/tests/sessions/test_engine_store.py
@@ -64,6 +64,14 @@ async def test_raise_if_engine_already_exists(subject: EngineStore) -> None:
         await subject.create(protocol=None)
 
 
+@pytest.mark.xfail(raises=NotImplementedError, strict=True)
+async def test_cannot_persist_multiple_engines(subject: EngineStore) -> None:
+    """It should protect against engine creation race conditions."""
+    # TODO(mc, 2021-06-14): figure out how to write a test that actually
+    # fails in practice when race condition is able to be hit
+    raise NotImplementedError("Test not yet implemented")
+
+
 def test_raise_if_engine_does_not_exist(subject: EngineStore) -> None:
     """It should raise if no engine exists when requested."""
     with pytest.raises(EngineMissingError):
