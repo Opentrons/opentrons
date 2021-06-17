e43c80f8f00a0146558345483993bc0c66fdce99
Max Marrone <max@opentrons.com>
2021-06-17 18:05:01 -0400

    feat(protocol-engine): Support loading pipettes and labware from JSON protocols (#7950)

--- api/src/opentrons/protocols/runner/json_proto/command_translator.py
+++ api/src/opentrons/protocols/runner/json_proto/command_translator.py
@@ -1,18 +1,16 @@
-from typing import Dict, Iterable
+from typing import Dict, List
 
-from opentrons.protocol_engine import WellLocation, WellOrigin
-from opentrons.protocol_engine.commands import (
-    CommandRequestType, AspirateRequest, DispenseRequest,
-    PickUpTipRequest, DropTipRequest
-)
-from opentrons.protocols.models import json_protocol as models
+import opentrons.types
+
+from opentrons import protocol_engine as pe
+from opentrons.protocols import models
 
 
 class CommandTranslatorError(Exception):
     pass
 
 
-ReturnType = Iterable[CommandRequestType]
+PECommands = List[pe.commands.CommandRequestType]
 
 
 class CommandTranslator:
@@ -22,10 +20,32 @@ class CommandTranslator:
         """Construct a command translator"""
         pass
 
-    def translate(self, command: models.AllCommands) -> ReturnType:
-        """
-        Translate a PD/JSON protocol to Protocol Engine commands.
+    def translate(self, protocol: models.JsonProtocol) -> PECommands:
+        """Return all Protocol Engine commands required to run the given protocol."""
+        result: List[pe.commands.CommandRequestType] = []
 
+        for pipette_id, pipette in protocol.pipettes.items():
+            result += [self._translate_load_pipette(pipette_id, pipette)]
+
+        for definition_id, definition in protocol.labwareDefinitions.items():
+            result += [self._translate_add_labware_definition(definition)]
+
+        for labware_id, labware in protocol.labware.items():
+            result += [
+                self._translate_load_labware(
+                    labware_id, labware, protocol.labwareDefinitions
+                )
+            ]
+
+        for command in protocol.commands:
+            result += self._translate_command(command)
+
+        return result
+
+    def _translate_command(
+            self,
+            command: models.json_protocol.AllCommands) -> PECommands:
+        """
         Args:
             command: The PD/JSON command to translate
 
@@ -43,9 +63,53 @@ class CommandTranslator:
             raise CommandTranslatorError(
                 f"Cannot find handler for '{command.command}'.")
 
+    def _translate_add_labware_definition(
+            self,
+            labware_definition: models.LabwareDefinition
+            ) -> pe.commands.AddLabwareDefinitionRequest:
+        return pe.commands.AddLabwareDefinitionRequest(definition=labware_definition)
+
+    def _translate_load_labware(
+            self,
+            labware_id: str,
+            labware: models.json_protocol.Labware,
+            labware_definitions: Dict[str, models.LabwareDefinition]
+            ) -> pe.commands.LoadLabwareRequest:
+        """
+        Args:
+            labware_id:
+                The ID that the JSON protocol's commands will use to refer to this
+                labware placement.
+            labware:
+                The JSON protocol's details about this labware placement, including
+                which deck slot it should go in, and a pointer to a labware definition.
+            labware_definitions:
+                The JSON protocol's collection of labware definitions.
+        """
+        definition = labware_definitions[labware.definitionId]
+        return pe.commands.LoadLabwareRequest(
+            location=pe.DeckSlotLocation(
+                slot=opentrons.types.DeckSlotName.from_primitive(labware.slot)
+            ),
+            loadName=definition.parameters.loadName,
+            namespace=definition.namespace,
+            version=definition.version,
+            labwareId=labware_id
+        )
+
+    def _translate_load_pipette(
+            self,
+            pipette_id: str,
+            pipette: models.json_protocol.Pipettes) -> pe.commands.LoadPipetteRequest:
+        return pe.commands.LoadPipetteRequest(
+            pipetteName=pe.PipetteName(pipette.name),
+            mount=opentrons.types.MountType(pipette.mount),
+            pipetteId=pipette_id
+        )
+
     def _aspirate(
             self,
-            command: models.LiquidCommand) -> ReturnType:
+            command: models.json_protocol.LiquidCommand) -> PECommands:
         """
         Translate an aspirate JSON command to a protocol engine aspirate request.
 
@@ -55,16 +119,14 @@ class CommandTranslator:
         Returns: AspirateRequest
 
         """
-        # TODO (al, 2021-04-26): incoming pipette and labware ids are
-        #  assigned by PD. Are they the same as Protocol Engine's?
         return [
-            AspirateRequest(
+            pe.commands.AspirateRequest(
                 pipetteId=command.params.pipette,
                 labwareId=command.params.labware,
                 wellName=command.params.well,
                 volume=command.params.volume,
-                wellLocation=WellLocation(
-                    origin=WellOrigin.BOTTOM,
+                wellLocation=pe.WellLocation(
+                    origin=pe.WellOrigin.BOTTOM,
                     offset=(0, 0, command.params.offsetFromBottomMm)
                 )
             )
@@ -72,7 +134,7 @@ class CommandTranslator:
 
     def _dispense(
             self,
-            command: models.LiquidCommand) -> ReturnType:
+            command: models.json_protocol.LiquidCommand) -> PECommands:
         """
         Translate a dispense JSON command to a protocol engine dispense request.
 
@@ -82,13 +144,13 @@ class CommandTranslator:
         Returns: DispenseRequest
         """
         return [
-            DispenseRequest(
+            pe.commands.DispenseRequest(
                 pipetteId=command.params.pipette,
                 labwareId=command.params.labware,
                 wellName=command.params.well,
                 volume=command.params.volume,
-                wellLocation=WellLocation(
-                    origin=WellOrigin.BOTTOM,
+                wellLocation=pe.WellLocation(
+                    origin=pe.WellOrigin.BOTTOM,
                     offset=(0, 0, command.params.offsetFromBottomMm)
                 )
             )
@@ -96,22 +158,22 @@ class CommandTranslator:
 
     def _air_gap(
             self,
-            command: models.LiquidCommand) -> ReturnType:
+            command: models.json_protocol.LiquidCommand) -> PECommands:
         raise NotImplementedError()
 
     def _blowout(
             self,
-            command: models.BlowoutCommand) -> ReturnType:
+            command: models.json_protocol.BlowoutCommand) -> PECommands:
         raise NotImplementedError()
 
     def _touch_tip(
             self,
-            command: models.TouchTipCommand) -> ReturnType:
+            command: models.json_protocol.TouchTipCommand) -> PECommands:
         raise NotImplementedError()
 
     def _pick_up(
             self,
-            command: models.PickUpDropTipCommand) -> ReturnType:
+            command: models.json_protocol.PickUpDropTipCommand) -> PECommands:
         """
         Translate a pick_up_tip JSON command to a protocol engine pick_up_tip request.
 
@@ -121,7 +183,7 @@ class CommandTranslator:
         Returns: PickUpTipRequest
         """
         return [
-            PickUpTipRequest(
+            pe.commands.PickUpTipRequest(
                 pipetteId=command.params.pipette,
                 labwareId=command.params.labware,
                 wellName=command.params.well
@@ -130,7 +192,7 @@ class CommandTranslator:
 
     def _drop_tip(
             self,
-            command: models.PickUpDropTipCommand) -> ReturnType:
+            command: models.json_protocol.PickUpDropTipCommand) -> PECommands:
         """
         Translate a drop tip JSON command to a protocol engine drop tip request.
 
@@ -141,7 +203,7 @@ class CommandTranslator:
 
         """
         return [
-            DropTipRequest(
+            pe.commands.DropTipRequest(
                 pipetteId=command.params.pipette,
                 labwareId=command.params.labware,
                 wellName=command.params.well
@@ -150,142 +212,153 @@ class CommandTranslator:
 
     def _move_to_slot(
             self,
-            command: models.MoveToSlotCommand) -> ReturnType:
+            command: models.json_protocol.MoveToSlotCommand) -> PECommands:
         raise NotImplementedError()
 
     def _delay(
             self,
-            command: models.DelayCommand) -> ReturnType:
+            command: models.json_protocol.DelayCommand) -> PECommands:
         raise NotImplementedError()
 
     def _magnetic_module_engage(
-            self, command: models.MagneticModuleEngageCommand) -> ReturnType:
+            self,
+            command: models.json_protocol.MagneticModuleEngageCommand) -> PECommands:
         raise NotImplementedError()
 
     def _magnetic_module_disengage(
             self,
-            command: models.MagneticModuleDisengageCommand) -> ReturnType:
+            command: models.json_protocol.MagneticModuleDisengageCommand) -> PECommands:
         raise NotImplementedError()
 
     def _temperature_module_set_target(
             self,
-            command: models.TemperatureModuleSetTargetCommand) -> ReturnType:
+            command: models.json_protocol.TemperatureModuleSetTargetCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _temperature_module_await_temperature(
             self,
-            command: models.TemperatureModuleAwaitTemperatureCommand) -> ReturnType:
+            command: models.json_protocol.TemperatureModuleAwaitTemperatureCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _temperature_module_deactivate(
             self,
-            command: models.TemperatureModuleDeactivateCommand) -> ReturnType:
+            command: models.json_protocol.TemperatureModuleDeactivateCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_set_target_block_temperature(
             self,
-            command: models.ThermocyclerSetTargetBlockTemperatureCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerSetTargetBlockTemperatureCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_set_target_lid_temperature(
             self,
-            command: models.ThermocyclerSetTargetLidTemperatureCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerSetTargetLidTemperatureCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_await_block_temperature(
             self,
-            command: models.ThermocyclerAwaitBlockTemperatureCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerAwaitBlockTemperatureCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_await_lid_temperature(
             self,
-            command: models.ThermocyclerAwaitLidTemperatureCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerAwaitLidTemperatureCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_deactivate_block(
             self,
-            command: models.ThermocyclerDeactivateBlockCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerDeactivateBlockCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_deactivate_lid(
             self,
-            command: models.ThermocyclerDeactivateLidCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerDeactivateLidCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_open_lid(
             self,
-            command: models.ThermocyclerOpenLidCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerOpenLidCommand) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_close_lid(
             self,
-            command: models.ThermocyclerCloseLidCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerCloseLidCommand) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_run_profile(
             self,
-            command: models.ThermocyclerRunProfile) -> ReturnType:
+            command: models.json_protocol.ThermocyclerRunProfile) -> PECommands:
         raise NotImplementedError()
 
     def _thermocycler_await_profile_complete(
             self,
-            command: models.ThermocyclerAwaitProfileCompleteCommand) -> ReturnType:
+            command: models.json_protocol.ThermocyclerAwaitProfileCompleteCommand
+            ) -> PECommands:
         raise NotImplementedError()
 
     def _move_to_well(
             self,
-            command: models.MoveToWellCommand) -> ReturnType:
+            command: models.json_protocol.MoveToWellCommand) -> PECommands:
         raise NotImplementedError()
 
     _COMMAND_TO_NAME: Dict[str, str] = {
-        models.CommandMoveToWell:
+        models.json_protocol.CommandMoveToWell:
             _move_to_well.__name__,
-        models.CommandThermocyclerAwaitProfile:
+        models.json_protocol.CommandThermocyclerAwaitProfile:
             _thermocycler_await_profile_complete.__name__,
-        models.CommandThermocyclerRunProfile:
+        models.json_protocol.CommandThermocyclerRunProfile:
             _thermocycler_run_profile.__name__,
-        models.CommandThermocyclerCloseLid:
+        models.json_protocol.CommandThermocyclerCloseLid:
             _thermocycler_close_lid.__name__,
-        models.CommandThermocyclerOpenLid:
+        models.json_protocol.CommandThermocyclerOpenLid:
             _thermocycler_open_lid.__name__,
-        models.CommandThermocyclerDeactivateLid:
+        models.json_protocol.CommandThermocyclerDeactivateLid:
             _thermocycler_deactivate_lid.__name__,
-        models.CommandThermocyclerDeactivateBlock:
+        models.json_protocol.CommandThermocyclerDeactivateBlock:
             _thermocycler_deactivate_block.__name__,
-        models.CommandThermocyclerSetTargetLid:
+        models.json_protocol.CommandThermocyclerSetTargetLid:
             _thermocycler_set_target_lid_temperature.__name__,
-        models.CommandThermocyclerAwaitBlockTemperature:
+        models.json_protocol.CommandThermocyclerAwaitBlockTemperature:
             _thermocycler_await_block_temperature.__name__,
-        models.CommandThermocyclerAwaitLidTemperature:
+        models.json_protocol.CommandThermocyclerAwaitLidTemperature:
             _thermocycler_await_lid_temperature.__name__,
-        models.CommandThermocyclerSetTargetBlock:
+        models.json_protocol.CommandThermocyclerSetTargetBlock:
             _thermocycler_set_target_block_temperature.__name__,
-        models.CommandTemperatureModuleDeactivate:
+        models.json_protocol.CommandTemperatureModuleDeactivate:
             _temperature_module_deactivate.__name__,
-        models.CommandTemperatureModuleAwait:
+        models.json_protocol.CommandTemperatureModuleAwait:
             _temperature_module_await_temperature.__name__,
-        models.CommandTemperatureModuleSetTarget:
+        models.json_protocol.CommandTemperatureModuleSetTarget:
             _temperature_module_set_target.__name__,
-        models.CommandMagneticModuleDisengage:
+        models.json_protocol.CommandMagneticModuleDisengage:
             _magnetic_module_disengage.__name__,
-        models.CommandMagneticModuleEngage:
+        models.json_protocol.CommandMagneticModuleEngage:
             _magnetic_module_engage.__name__,
-        models.CommandDelay:
+        models.json_protocol.CommandDelay:
             _delay.__name__,
-        models.CommandMoveToSlot:
+        models.json_protocol.CommandMoveToSlot:
             _move_to_slot.__name__,
-        models.CommandDropTip:
+        models.json_protocol.CommandDropTip:
             _drop_tip.__name__,
-        models.CommandPickUpTip:
+        models.json_protocol.CommandPickUpTip:
             _pick_up.__name__,
-        models.CommandTouchTip:
+        models.json_protocol.CommandTouchTip:
             _touch_tip.__name__,
-        models.CommandBlowout:
+        models.json_protocol.CommandBlowout:
             _blowout.__name__,
-        models.CommandAirGap:
+        models.json_protocol.CommandAirGap:
             _air_gap.__name__,
-        models.CommandDispense:
+        models.json_protocol.CommandDispense:
             _dispense.__name__,
-        models.CommandAspirate:
+        models.json_protocol.CommandAspirate:
             _aspirate.__name__,
     }
--- api/tests/opentrons/protocols/runnner/json_proto/test_command_translator.py
+++ api/tests/opentrons/protocols/runnner/json_proto/test_command_translator.py
@@ -1,13 +1,101 @@
-import pytest
-from opentrons.protocol_engine import WellLocation, WellOrigin
-from opentrons.protocol_engine.commands import (
-    AspirateRequest, DispenseRequest, PickUpTipRequest, DropTipRequest
-)
-from opentrons.protocols.models import json_protocol as models
+import typing
 
-from opentrons.protocols.runner.json_proto.command_translator import (
-    CommandTranslator
-)
+import pytest
+
+from opentrons import protocol_engine as pe
+from opentrons.protocols import models
+
+from opentrons.protocols.runner.json_proto.command_translator import CommandTranslator
+
+
+# todo(mc & mm, 2021-06-17):
+#
+# There are two big problems with these tests:
+#
+# 1. I copy-pasted the big JSON protocol fixture from opentrons.file_runner tests.
+# 2. The command translation tests use the internal implementation detail
+#    _translate_command().
+#
+# We should fix both of these by:
+#
+# * Writing a helper function, possibly specific to this test file, to return a minimal
+#   JSON protocol with some given commands, given labware, etc., filling in boring
+#   stuff like metadata.
+# * Making every test here use that helper function with its own hard-coded choice of
+#   commands. (So we don't have to pull expected details out of a fixture to assert
+#   against.)
+# * Consistently testing CommandTranslator as a black box here.
+
+
+@pytest.fixture
+def json_protocol(json_protocol_dict: dict) -> models.JsonProtocol:
+    """Get a parsed JSON protocol model fixture."""
+    return models.JsonProtocol.parse_obj(json_protocol_dict)
+
+
+@pytest.fixture
+def json_protocol_dict(minimal_labware_def: dict, minimal_labware_def2: dict) -> dict:
+    """Get a JSON protocol dictionary fixture."""
+    return {
+        "schemaVersion": 3,
+        "metadata": {},
+        "robot": {"model": "OT-2 Standard"},
+        "pipettes": {"leftPipetteId": {"mount": "left", "name": "p300_single"}},
+        "labware": {
+            "trashId": {
+                "slot": "12",
+                "displayName": "Trash",
+                "definitionId": "opentrons/opentrons_1_trash_1100ml_fixed/1",
+            },
+            "tiprack1Id": {
+                "slot": "1",
+                "displayName": "Opentrons 96 Tip Rack 300 µL",
+                "definitionId": "opentrons/opentrons_96_tiprack_300ul/1",
+            },
+            "wellplate1Id": {
+                "slot": "10",
+                "displayName": "Corning 96 Well Plate 360 µL Flat",
+                "definitionId": "opentrons/corning_96_wellplate_360ul_flat/1",
+            },
+        },
+        "labwareDefinitions": {
+            "opentrons/opentrons_1_trash_1100ml_fixed/1": minimal_labware_def,
+            "opentrons/opentrons_96_tiprack_300ul/1": minimal_labware_def,
+            "opentrons/corning_96_wellplate_360ul_flat/1": minimal_labware_def2,
+        },
+        "commands": [
+            {
+                "command": "pickUpTip",
+                "params": {
+                    "pipette": "leftPipetteId",
+                    "labware": "tiprack1Id",
+                    "well": "A1",
+                },
+            },
+            {
+                "command": "aspirate",
+                "params": {
+                    "pipette": "leftPipetteId",
+                    "volume": 51,
+                    "labware": "wellplate1Id",
+                    "well": "B1",
+                    "offsetFromBottomMm": 10,
+                    "flowRate": 10,
+                },
+            },
+            {
+                "command": "dispense",
+                "params": {
+                    "pipette": "leftPipetteId",
+                    "volume": 50,
+                    "labware": "wellplate1Id",
+                    "well": "H1",
+                    "offsetFromBottomMm": 1,
+                    "flowRate": 50,
+                },
+            },
+        ],
+    }
 
 
 @pytest.fixture
@@ -15,78 +103,152 @@ def subject() -> CommandTranslator:
     return CommandTranslator()
 
 
+def _assert_appear_in_order(elements: typing.Iterable, source: typing.Iterable) -> None:
+    """
+    Assert all elements appear in source, in the given order relative to each other.
+
+    Example:
+
+        _assert_appear_in_order(
+            elements=["a", "c"]
+            source=["a", "b", "c", "d"]
+        )  # Pass.
+
+        _assert_appear_in_order(
+            elements=["c", "a"]
+            source=["a", "b", "c", "d"]
+        )  # Fail.
+    """
+    element_indexes = [source.index(element) for element in elements]
+    assert sorted(element_indexes) == element_indexes
+
+
+def test_labware(
+    subject: CommandTranslator,
+    json_protocol: models.JsonProtocol,
+    minimal_labware_def: dict,
+    minimal_labware_def2: dict,
+) -> None:
+    result = subject.translate(json_protocol)
+
+    expected_add_definition_request_1 = pe.commands.AddLabwareDefinitionRequest(
+        definition=models.LabwareDefinition.parse_obj(minimal_labware_def)
+    )
+    expected_load_request_1 = pe.commands.LoadLabwareRequest(
+        location=pe.DeckSlotLocation(slot=1),
+        loadName=minimal_labware_def["parameters"]["loadName"],
+        namespace=minimal_labware_def["namespace"],
+        version=minimal_labware_def["version"],
+        labwareId="tiprack1Id",
+    )
+
+    expected_add_definition_request_2 = pe.commands.AddLabwareDefinitionRequest(
+        definition=models.LabwareDefinition.parse_obj(minimal_labware_def2)
+    )
+    expected_load_request_2 = pe.commands.LoadLabwareRequest(
+        location=pe.DeckSlotLocation(slot=10),
+        loadName=minimal_labware_def2["parameters"]["loadName"],
+        namespace=minimal_labware_def2["namespace"],
+        version=minimal_labware_def2["version"],
+        labwareId="wellplate1Id",
+    )
+
+    _assert_appear_in_order(
+        elements=[expected_add_definition_request_1, expected_load_request_1],
+        source=result,
+    )
+
+    _assert_appear_in_order(
+        elements=[expected_add_definition_request_2, expected_load_request_2],
+        source=result,
+    )
+
+
+def test_pipettes(
+    subject: CommandTranslator,
+    json_protocol: models.JsonProtocol,
+) -> None:
+    result = subject.translate(json_protocol)
+
+    expected_request = pe.commands.LoadPipetteRequest(
+        pipetteName="p300_single", mount="left", pipetteId="leftPipetteId"
+    )
+
+    assert expected_request in result
+
+
 def test_aspirate(
-        subject: CommandTranslator, aspirate_command: models.LiquidCommand
+    subject: CommandTranslator, aspirate_command: models.json_protocol.LiquidCommand
 ) -> None:
     """It should translate a JSON aspirate command to a Protocol Engine
-     aspirate request."""
-    request = subject.translate(aspirate_command)
+    aspirate request."""
+    request = subject._translate_command(aspirate_command)
 
     assert request == [
-        AspirateRequest(
+        pe.commands.AspirateRequest(
             pipetteId=aspirate_command.params.pipette,
             labwareId=aspirate_command.params.labware,
             wellName=aspirate_command.params.well,
             volume=aspirate_command.params.volume,
-            wellLocation=WellLocation(
-                origin=WellOrigin.BOTTOM,
-                offset=(0, 0, aspirate_command.params.offsetFromBottomMm)
-            )
+            wellLocation=pe.WellLocation(
+                origin=pe.WellOrigin.BOTTOM,
+                offset=(0, 0, aspirate_command.params.offsetFromBottomMm),
+            ),
         )
     ]
 
 
 def test_dispense(
-        subject: CommandTranslator,
-        dispense_command: models.LiquidCommand
+    subject: CommandTranslator, dispense_command: models.json_protocol.LiquidCommand
 ) -> None:
     """It should translate a JSON dispense command to a Protocol Engine
-     dispense request."""
-    request = subject.translate(dispense_command)
+    dispense request."""
+    request = subject._translate_command(dispense_command)
 
     assert request == [
-        DispenseRequest(
+        pe.commands.DispenseRequest(
             pipetteId=dispense_command.params.pipette,
             labwareId=dispense_command.params.labware,
             wellName=dispense_command.params.well,
             volume=dispense_command.params.volume,
-            wellLocation=WellLocation(
-                origin=WellOrigin.BOTTOM,
-                offset=(0, 0, dispense_command.params.offsetFromBottomMm)
-            )
+            wellLocation=pe.WellLocation(
+                origin=pe.WellOrigin.BOTTOM,
+                offset=(0, 0, dispense_command.params.offsetFromBottomMm),
+            ),
         )
     ]
 
 
 def test_drop_tip(
-        subject: CommandTranslator,
-        drop_tip_command: models.PickUpDropTipCommand
+    subject: CommandTranslator,
+    drop_tip_command: models.json_protocol.PickUpDropTipCommand,
 ) -> None:
     """It should translate a JSON drop tip command to a Protocol Engine
-     drop tip request."""
-    request = subject.translate(drop_tip_command)
+    drop tip request."""
+    request = subject._translate_command(drop_tip_command)
 
     assert request == [
-        DropTipRequest(
+        pe.commands.DropTipRequest(
             pipetteId=drop_tip_command.params.pipette,
             labwareId=drop_tip_command.params.labware,
-            wellName=drop_tip_command.params.well
+            wellName=drop_tip_command.params.well,
         )
     ]
 
 
-def test_pick_up_tip(subject, pick_up_command: models.PickUpDropTipCommand) -> None:
+def test_pick_up_tip(
+    subject, pick_up_command: models.json_protocol.PickUpDropTipCommand
+) -> None:
     """
     It should translate a JSON pick up tip command to a Protocol Engine
     PickUpTip request.
     """
-    request = subject.translate(pick_up_command)
+    request = subject._translate_command(pick_up_command)
 
     assert request == [
-        PickUpTipRequest(
+        pe.commands.PickUpTipRequest(
             pipetteId=pick_up_command.params.pipette,
             labwareId=pick_up_command.params.labware,
             wellName=pick_up_command.params.well,
-
         )
     ]
