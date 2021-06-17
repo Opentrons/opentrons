2f5c2d22271723bca5d63c94f8aa3570eacfa51a
Max Marrone <max@marrone.nyc>
2021-06-17 14:48:04 -0400

    Minimize, and document intent of, modules/__init__.py re-exports.
    * Revert some thoughtless re-exports I made earlier in this PR.
    * Say what I think actually should be re-exported here, so I don't make the same mistake again.

--- api/src/opentrons/protocols/models/__init__.py
+++ api/src/opentrons/protocols/models/__init__.py
@@ -1,18 +1,19 @@
+# Convenience re-exports of models that are especially common or important.
+# More detailed sub-models are always available through the underlying
+# submodules.
+#
+# If re-exporting something, its name should still make sense when it's separatred
+# from the name of its parent submodule. e.g. re-exporting models.json_protocol.Labware
+# as models.Labware could be confusing.
+
 from .labware_definition import (
     LabwareDefinition,
     WellDefinition
 )
-from .json_protocol import (
-    Labware,
-    Model as JsonProtocol,
-)
+from .json_protocol import Model as JsonProtocol
 
 __all__ = [
-    # Models for labware definitions:
     "LabwareDefinition",
     "WellDefinition",
-    # Models for JSON protocols:
-    "AllCommands",  # Fix before merge: Why doesn't the linter catch this?
-    "Labware",
     "JsonProtocol",
 ]
