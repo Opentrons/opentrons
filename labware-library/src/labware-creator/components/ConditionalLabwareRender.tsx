9b2fc17843187d36746c10f4ba1ac6e551483ef9
Ian London <ianlondondu@gmail.com>
2021-06-28 14:19:58 -0400

    feat(labware-creator): show whole labware in preview svg (#8013)
    Closes #7164

--- labware-library/src/labware-creator/components/ConditionalLabwareRender.tsx
+++ labware-library/src/labware-creator/components/ConditionalLabwareRender.tsx
@@ -19,7 +19,42 @@ interface Props {
   values: LabwareFields
 }
 
+const calculateViewBox = (args: {
+  bBox: DOMRect | undefined
+  xDim: number
+  yDim: number
+}): string => {
+  const { bBox, xDim, yDim } = args
+
+  // by-eye margin to make sure there is no visual clipping
+  const MARGIN = 5
+
+  // calculate viewBox such that SVG is zoomed and panned with the bBox fully in view,
+  // in a "zoom to fit" manner, plus some visual margin to prevent clipping
+  return `${(bBox?.x ?? 0) - MARGIN} ${(bBox?.y ?? 0) - MARGIN} ${
+    xDim + MARGIN * 2
+  } ${yDim + MARGIN * 2}`
+}
+
 export const ConditionalLabwareRender = (props: Props): JSX.Element => {
+  const gRef = React.useRef<SVGGElement>(null)
+  const [bBox, updateBBox] = React.useState<DOMRect | undefined>(
+    gRef.current ? gRef.current.getBBox() : undefined
+  )
+
+  // In order to implement "zoom to fit", we're calculating the desired viewBox based on getBBox of the child.
+  // So we have to actually render the child to get its bounding box. After that, we re-calculate the viewBox.
+  // Once the viewBox is re-calculated, we use setState to force a re-render.
+  const nextBBox = gRef.current?.getBBox()
+  React.useLayoutEffect((): void => {
+    if (
+      nextBBox != null &&
+      (nextBBox.width !== bBox?.width || nextBBox.height !== bBox?.height)
+    ) {
+      updateBBox(nextBBox)
+    }
+  }, [bBox?.height, bBox?.width, nextBBox])
+
   const definition = React.useMemo(() => {
     const values = cloneDeep(props.values)
 
@@ -65,18 +100,20 @@ export const ConditionalLabwareRender = (props: Props): JSX.Element => {
     return def
   }, [props.values])
 
-  const xDim = definition
-    ? definition.dimensions.xDimension
-    : DEFAULT_X_DIMENSION
-  const yDim = definition
-    ? definition.dimensions.yDimension
-    : DEFAULT_Y_DIMENSION
+  const xDim =
+    definition != null
+      ? bBox?.width ?? definition.dimensions.xDimension
+      : DEFAULT_X_DIMENSION
+  const yDim =
+    definition != null
+      ? bBox?.height ?? definition.dimensions.yDimension
+      : DEFAULT_Y_DIMENSION
 
   return (
-    <RobotWorkSpace viewBox={`0 0 ${xDim} ${yDim}`}>
+    <RobotWorkSpace viewBox={calculateViewBox({ bBox, xDim, yDim })}>
       {() =>
-        definition ? (
-          <LabwareRender definition={definition} />
+        definition != null ? (
+          <LabwareRender definition={definition} gRef={gRef} />
         ) : (
           <>
             <LabwareOutline />
