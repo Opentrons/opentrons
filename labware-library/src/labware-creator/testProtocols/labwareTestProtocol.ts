bf763c2a94d4b7bc57e3977b90e61dd80b201b91
Ian London <ianlondondu@gmail.com>
2021-06-28 15:18:17 -0400

    feat(labware-library): add tip rack test .py protocol

--- labware-library/src/labware-creator/testProtocols/labwareTestProtocol.ts
+++ labware-library/src/labware-creator/testProtocols/labwareTestProtocol.ts
@@ -1,45 +1,5 @@
 import type { LabwareDefinition2 } from '@opentrons/shared-data'
-import type { Options } from './fields'
-
-interface Pipette {
-  displayName: string
-  tiprack: string
-}
-
-const pipettes: Record<string, Pipette> = {
-  p20_single_gen2: {
-    displayName: 'P20 Single GEN2',
-    tiprack: 'opentrons_96_tiprack_20ul',
-  },
-  p300_single_gen2: {
-    displayName: 'P300 Single GEN2',
-    tiprack: 'opentrons_96_tiprack_300ul',
-  },
-  p1000_single_gen2: {
-    displayName: 'P1000 Single GEN2',
-    tiprack: 'opentrons_96_tiprack_1000ul',
-  },
-  p10_single: {
-    displayName: 'P10 Single GEN1',
-    tiprack: 'opentrons_96_tiprack_20ul',
-  },
-  p50_single: {
-    displayName: 'P50 Single GEN1',
-    tiprack: 'opentrons_96_tiprack_300ul',
-  },
-  p300_single: {
-    displayName: 'P300 Single GEN1',
-    tiprack: 'opentrons_96_tiprack_300ul',
-  },
-  p1000_single: {
-    displayName: 'P1000 Single GEN1',
-    tiprack: 'opentrons_96_tiprack_1000ul',
-  },
-}
-
-export const pipetteNameOptions: Options = Object.keys(
-  pipettes
-).map(loadName => ({ name: pipettes[loadName].displayName, value: loadName }))
+import { pipettes } from './constants'
 
 interface LabwareTestProtocolArgs {
   pipetteName: string
