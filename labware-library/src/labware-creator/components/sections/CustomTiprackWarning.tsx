bf8cecda3a9273b8027782bb7116473092527cc4
Ian London <ianlondondu@gmail.com>
2021-06-30 16:17:26 -0400

    feat(labware-creator): update tiprack recommendation text
    Closes #8035

--- labware-library/src/labware-creator/components/sections/CustomTiprackWarning.tsx
+++ labware-library/src/labware-creator/components/sections/CustomTiprackWarning.tsx
@@ -17,9 +17,13 @@ export const CustomTiprackWarning = (): JSX.Element | null => {
         >
           <div className={styles.flex_row}>
             <p className={styles.instructions_text}>
-              Opentrons tip racks are recommended for use with the OT-2. You are
-              welcome to try to use third party tip racks but accuracy is not
-              guaranteed.
+              Opentrons tip racks are recommended for use with the OT-2 because
+              they are specifically designed and verified for automation.
+            </p>
+            <p className={styles.instructions_text}>
+              Third party tips can fit, but not necessarily with a tight seal.
+              You risk tips falling off mid-run as well as pipetting inaccuracy.
+              They may also be more likely to bend or break.
             </p>
           </div>
         </SectionBody>
