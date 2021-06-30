61a73ef198f74827a3959bf4f1607697769289bc
Shlok Amin <shlokamin@me.com>
2021-06-30 16:15:22 -0400

    use util to get labware name

--- labware-library/src/labware-creator/components/sections/Preview.tsx
+++ labware-library/src/labware-creator/components/sections/Preview.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { useFormikContext } from 'formik'
-import { LabwareFields, LabwareType } from '../../fields'
+import { LabwareFields } from '../../fields'
+import { getLabwareName } from '../../utils'
 import { ConditionalLabwareRender } from '../ConditionalLabwareRender'
 import { FormLevelErrorAlerts } from '../FormLevelErrorAlerts'
 
@@ -9,24 +10,13 @@ import { SectionBody } from './SectionBody'
 import styles from '../../styles.css'
 
 export const PreviewInstructions = (props: {
-  labwareType: LabwareType | null | undefined
+  values: LabwareFields
 }): JSX.Element => {
-  const { labwareType } = props
-  let labwarePreviewType = ''
-  switch (labwareType) {
-    case 'tipRack':
-      labwarePreviewType = 'tips'
-      break
-    case 'tubeRack':
-      labwarePreviewType = 'tubes'
-      break
-    default:
-      labwarePreviewType = 'wells'
-  }
+  const { values } = props
   return (
     <p className={styles.preview_instructions}>
-      Check that the size, spacing, and shape of your {labwarePreviewType} looks
-      correct.
+      Check that the size, spacing, and shape of your{' '}
+      {getLabwareName(values, true)} looks correct.
     </p>
   )
 }
@@ -39,7 +29,7 @@ export const Preview = (): JSX.Element => {
       <FormLevelErrorAlerts errors={errors} />
       <div className={styles.preview_labware}>
         <ConditionalLabwareRender values={values} />
-        <PreviewInstructions labwareType={values.labwareType} />
+        <PreviewInstructions values={values} />
       </div>
     </SectionBody>
   )
