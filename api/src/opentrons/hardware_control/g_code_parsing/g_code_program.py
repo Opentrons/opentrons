42058d05dbb5c55ba59e8cfaebeced964a3e25ee
Derek Maggio <derek.maggio@opentrons.com>
2021-07-07 13:11:16 -0700

    feat(api): Refactor framework to use mocking instead of logging

--- api/src/opentrons/hardware_control/g_code_parsing/g_code_program.py
+++ api/src/opentrons/hardware_control/g_code_parsing/g_code_program.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
+from opentrons.hardware_control.emulation.app import \
+    TEMPDECK_PORT, THERMOCYCLER_PORT, SMOOTHIE_PORT, MAGDECK_PORT
 from opentrons.hardware_control.emulation.parser import Parser
 from .g_code import GCode
 from typing import List
+
+from .g_code_watcher import GCodeWatcher
 
 
 class GCodeProgram:
@@ -9,6 +13,13 @@ class GCodeProgram:
     Class for parsing various G-Code files and programs into a
     list of GCode objects
     """
+
+    DEVICE_LOOKUP_BY_PORT = {
+        SMOOTHIE_PORT: 'smoothie',
+        TEMPDECK_PORT: 'tempdeck',
+        THERMOCYCLER_PORT: 'thermocycler',
+        MAGDECK_PORT: 'magdeck',
+    }
 
     @classmethod
     def from_log_file(cls, log_file_path: str) -> GCodeProgram:
@@ -39,6 +50,34 @@ class GCodeProgram:
                         ]
                     )
         return cls(write_matches)
+
+    @classmethod
+    def get_device(cls, serial_connection):
+        serial_port = serial_connection.port
+        device_port = serial_port[serial_port.rfind(':') + 1:]
+        return cls.DEVICE_LOOKUP_BY_PORT[int(device_port)]
+
+    @classmethod
+    def from_g_code_watcher(cls, watcher: GCodeWatcher) -> GCodeProgram:
+        """
+        Function to convert command list collected by GCodeWatcher
+        into GCodeProgram
+        :param watcher: GCodeWatcher object
+        :return: GCodeProgram object
+        """
+        g_codes = []
+        for watcher_data in watcher.get_command_list():
+            device = cls.get_device(watcher_data.serial_connection)
+            g_codes.extend([
+                GCode(
+                    watcher_data.date,
+                    device,
+                    g_code.gcode,
+                    g_code.params
+                )
+                for g_code in Parser().parse(watcher_data.raw_g_code)
+            ])
+        return cls(g_codes)
 
     def __init__(self, g_codes: List[GCode]):
         self._g_codes = g_codes
