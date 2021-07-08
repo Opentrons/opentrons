071c7b25179d167ca965a60111b25059875a7645
Shlok Amin <shlokamin@me.com>
2021-07-08 11:03:05 -0400

    refactor(protocol-library): convert from flow to TS
    convert from flow to TS
    
    #8075

---
+++ protocol-library-kludge/lib/App.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import './globals.css';
+export declare function AppComponent(): JSX.Element;
+export declare const App: typeof AppComponent;
--- protocol-library-kludge/src/App.tsx
+++ protocol-library-kludge/src/App.tsx
@@ -1,11 +1,10 @@
-// @flow
 import * as React from 'react'
 import { hot } from 'react-hot-loader/root'
 import { URLDeck } from './URLDeck'
 import './globals.css'
 
-export function AppComponent(): React.Node {
+export function AppComponent(): JSX.Element {
   return <URLDeck />
 }
 
-export const App: React.AbstractComponent<{||}> = hot(AppComponent)
+export const App = hot(AppComponent)
