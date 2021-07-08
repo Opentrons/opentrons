ef1af58c8aebc229bb416259bdee05ecba742de6
Shlok Amin <shlokamin@me.com>
2021-07-08 14:35:45 -0400

    remove accidentally checked in types (#8078)

--- protocol-library-kludge/lib/App.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import './globals.css';
-export declare function AppComponent(): JSX.Element;
-export declare const App: typeof AppComponent;
--- protocol-library-kludge/lib/getLabware.d.ts
+++
@@ -1,3 +0,0 @@
-import type { LabwareDefinition1, LabwareDefinition2 } from '@opentrons/shared-data';
-export declare function getLegacyLabwareDef(loadName: string | null | undefined): LabwareDefinition1 | null;
-export declare function getLatestLabwareDef(loadName: string | null | undefined): LabwareDefinition2 | null;
