90c5a763ab71674f7bc18a68900c5564525f1c89
Derek Maggio <derek.maggio@opentrons.com>
2021-07-09 10:05:32 -0700

    feat(api): Fix Probe Definition
    Add the possibility of a speed argument being included

--- api/src/opentrons/hardware_control/g_code_parsing/g_code_functionality_defs/probe_g_code_functionality_def.py
+++ api/src/opentrons/hardware_control/g_code_parsing/g_code_functionality_defs/probe_g_code_functionality_def.py
@@ -3,7 +3,8 @@ from .g_code_functionality_def_base import GCodeFunctionalityDefBase
 
 
 class ProbeGCodeFunctionalityDef(GCodeFunctionalityDefBase):
-    EXPECTED_ARGS = ['X', 'Y', 'Z', 'A', 'B', 'C']
+    SPEED_ARG_KEY = 'F'
+    EXPECTED_ARGS = ['X', 'Y', 'Z', 'A', 'B', 'C', 'F']
 
     @classmethod
     def _generate_command_explanation(cls, g_code_args: Dict[str, str]) -> str:
@@ -15,5 +16,18 @@ class ProbeGCodeFunctionalityDef(GCodeFunctionalityDefBase):
             if key in passed_args
         ]
 
+        speed = None
+
+        if cls.SPEED_ARG_KEY in current_keys:
+            speed = g_code_args[cls.SPEED_ARG_KEY]
+            current_keys.remove(cls.SPEED_ARG_KEY)
+
         axis_to_probe = ', '.join(current_keys)
-        return f'PROBE:\n\tProbing the following axes: {axis_to_probe}'
+
+        if speed is None:
+            message = f'PROBE:\n\tProbing the following axes: {axis_to_probe}'
+        else:
+            message = f'PROBE:\n\tProbing the following axes: {axis_to_probe}; ' \
+                      f'at a speed of {speed}'
+
+        return message
