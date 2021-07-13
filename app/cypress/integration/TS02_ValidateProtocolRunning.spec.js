07f90c58ab5a63d91eff5e92e76e758d69471c13
Josh McVey <yersh13@gmail.com>
2021-07-13 16:06:54 -0500

    fix(app electron tests): check all external dependencies for cypress test execution against the app

--- app/cypress/integration/TS02_ValidateProtocolRunning.spec.js
+++ app/cypress/integration/TS02_ValidateProtocolRunning.spec.js
@@ -1,77 +1,115 @@
 // E2E Test Suite 02 for Opentrons App - Validate Protocol Running
-import 'cypress-wait-until';
-import 'cypress-file-upload';
+import 'cypress-wait-until'
+import 'cypress-file-upload'
 
 describe('Test Suite 02 - Validate Protocol Running', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:8090')
-        
-        // Connect to the robot
-        cy.get('.buttons__button_flat__1YVfe > .Svg-sc-1lpozsw-0 > path').click({force:true}).then(() => {
-            cy.waitUntil(() => cy.get('.alerts__title_bar__Pad5-').should('exist').then(() => {
-                
-                // Navigate to the Protocol page once it is confirmed that the robot is successfully connected
-                cy.get('[aria-describedby="Tooltip__2"] > .Svg-sc-1lpozsw-0 > path').click({force:true})
-            }))
-        })
-    })
+  beforeEach(() => {
+    cy.visit('http://localhost:8090')
 
-    // Run JSON protocol named TestAutomationProtocol1.json
-    // Uses 2 modules (magnetic and temperature)
-    it('Run JSON protocol using magnetic and temperature modules', () => {
-        
-        // Load the protocol file and clear the confirmation modal
-        const filePath = 'TestAutomationProtocol1.json'
-        cy.get('.upload-panel__file_input__1Mo7Z').attachFile(filePath).then(() => {
-            cy.get('.modals__alert_modal_buttons__2GQHd > :nth-child(2)').click({force:true}).then(() => {
-                
-                // Wait for simulation to finish before navigating to the Run page
-                cy.waitUntil(() => cy.get('.kIChyh').should('be.visible')).then(() => {
-                    cy.get('[aria-describedby="Tooltip__4"] > .Svg-sc-1lpozsw-0 > path').click().then(() => {
-                        
-                        // Make sure modules populate before hitting the Start Run button
-                        cy.waitUntil(() => cy.get(':nth-child(2) > .lists__title_bar__3n-Et').should('be.visible')).then(() => {
-                            cy.get('.buttons__button_outline__3z7qv').click().then(() => {
-                                
-                                // Confirm the protocol run finished successfully
-                                cy.get('.alerts__title_bar__Pad5-', { timeout: 45000 }).should('have.text', 'Run complete! Reset run to run protocol again.').then(() => {
-                                    
-                                    // Click the Reset Run button to clear it for the next test run
-                                    cy.get('.buttons__button_outline__3z7qv').click()
-                                })
+    // Connect to the robot
+    cy.get('.buttons__button_flat__1YVfe > .Svg-sc-1lpozsw-0 > path')
+      .click({ force: true })
+      .then(() => {
+        cy.waitUntil(() =>
+          cy
+            .get('.alerts__title_bar__Pad5-')
+            .should('exist')
+            .then(() => {
+              // Navigate to the Protocol page once it is confirmed that the robot is successfully connected
+              cy.get(
+                '[aria-describedby="Tooltip__2"] > .Svg-sc-1lpozsw-0 > path'
+              ).click({ force: true })
+            })
+        )
+      })
+  })
+
+  // Run JSON protocol named TestAutomationProtocol1.json
+  // Uses 2 modules (magnetic and temperature)
+  it('Run JSON protocol using magnetic and temperature modules', () => {
+    // Load the protocol file and clear the confirmation modal
+    const filePath = 'TestAutomationProtocol1.json'
+    cy.get('.upload-panel__file_input__1Mo7Z')
+      .attachFile(filePath)
+      .then(() => {
+        cy.get('.modals__alert_modal_buttons__2GQHd > :nth-child(2)')
+          .click({ force: true })
+          .then(() => {
+            // Wait for simulation to finish before navigating to the Run page
+            cy.waitUntil(() => cy.get('.kIChyh').should('be.visible')).then(
+              () => {
+                cy.get(
+                  '[aria-describedby="Tooltip__4"] > .Svg-sc-1lpozsw-0 > path'
+                )
+                  .click()
+                  .then(() => {
+                    // Make sure modules populate before hitting the Start Run button
+                    cy.waitUntil(() =>
+                      cy
+                        .get(':nth-child(2) > .lists__title_bar__3n-Et')
+                        .should('be.visible')
+                    ).then(() => {
+                      cy.get('.buttons__button_outline__3z7qv')
+                        .click()
+                        .then(() => {
+                          // Confirm the protocol run finished successfully
+                          cy.get('.alerts__title_bar__Pad5-', {
+                            timeout: 45000,
+                          })
+                            .should(
+                              'have.text',
+                              'Run complete! Reset run to run protocol again.'
+                            )
+                            .then(() => {
+                              // Click the Reset Run button to clear it for the next test run
+                              cy.get('.buttons__button_outline__3z7qv').click()
                             })
                         })
                     })
-                })
-            })
-        })
-    })
+                  })
+              }
+            )
+          })
+      })
+  })
 
-    
-    // Run JSON protocol named TestAutomationProtocol2.json
-    // Uses NO modules
-    it('Run JSON protocol using no modules', () => {
-        
-        // Load the protocol file and clear the confirmation modal
-        const filePath = 'TestAutomationProtocol2.json'
-        cy.get('.upload-panel__file_input__1Mo7Z').attachFile(filePath).then(() => {
-            cy.get('.modals__alert_modal_buttons__2GQHd > :nth-child(2)').click({force:true}).then(() => {
-                
-                // Wait for simulation to finish before navigating to the Run page
-                cy.waitUntil(() => cy.get('.kIChyh').should('be.visible')).then(() => {
-                    cy.get('[aria-describedby="Tooltip__4"] > .Svg-sc-1lpozsw-0 > path').click().then(() => {
-                        cy.get('.buttons__button_outline__3z7qv').click().then(() => {
-                            
-                            // Confirm the protocol run finished successfully
-                            cy.get('.alerts__title_bar__Pad5-', { timeout: 140000 }).should('have.text', 'Run complete! Reset run to run protocol again.').then(() => {
-                                
-                                // Click the Reset Run button to clear it for the next test run
-                                cy.get('.buttons__button_outline__3z7qv').click()
-                            })
-                        })
-                    })
-                })
-            })
-        })
-    })
+  // Run JSON protocol named TestAutomationProtocol2.json
+  // Uses NO modules
+  it('Run JSON protocol using no modules', () => {
+    // Load the protocol file and clear the confirmation modal
+    const filePath = 'TestAutomationProtocol2.json'
+    cy.get('.upload-panel__file_input__1Mo7Z')
+      .attachFile(filePath)
+      .then(() => {
+        cy.get('.modals__alert_modal_buttons__2GQHd > :nth-child(2)')
+          .click({ force: true })
+          .then(() => {
+            // Wait for simulation to finish before navigating to the Run page
+            cy.waitUntil(() => cy.get('.kIChyh').should('be.visible')).then(
+              () => {
+                cy.get(
+                  '[aria-describedby="Tooltip__4"] > .Svg-sc-1lpozsw-0 > path'
+                )
+                  .click()
+                  .then(() => {
+                    cy.get('.buttons__button_outline__3z7qv')
+                      .click()
+                      .then(() => {
+                        // Confirm the protocol run finished successfully
+                        cy.get('.alerts__title_bar__Pad5-', { timeout: 140000 })
+                          .should(
+                            'have.text',
+                            'Run complete! Reset run to run protocol again.'
+                          )
+                          .then(() => {
+                            // Click the Reset Run button to clear it for the next test run
+                            cy.get('.buttons__button_outline__3z7qv').click()
+                          })
+                      })
+                  })
+              }
+            )
+          })
+      })
+  })
 })
--- app/cypress/integration/TS04_ValidateCustomLabwareTabOnMorePage.spec.js
+++ app/cypress/integration/TS04_ValidateCustomLabwareTabOnMorePage.spec.js
@@ -1,55 +1,90 @@
 // E2E Test Suite 04 for Opentrons App - Validate the Custom Labware Tab on More Page
-import 'cypress-wait-until';
+import 'cypress-wait-until'
 
 // Ensures that the site is opened in the browser before each test case
 describe('Test Suite 04 - Validate the Custom Labware Tab on More Page', () => {
+  before(() => {
+    cy.visit('http://localhost:8090')
 
-    before(() => {
-        cy.visit('http://localhost:8090')
+    // Navigate to More page
+    cy.get('.cOCwJn > .iqXzje').click({ force: true })
 
-        // Navigate to More page
-        cy.get('.cOCwJn > .iqXzje').click({force:true})
+    // Navigate to Custom Labware tab
+    cy.get(':nth-child(2) > .styles__menu_item__11nPC').click()
+  })
 
-        // Navigate to Custom Labware tab
-        cy.get(':nth-child(2) > .styles__menu_item__11nPC').click()
+  // Confirm text on page is correct
+  it('Confirm text on page is correct', () => {
+    cy.get('.structure__title__3nJ-D').should('have.text', 'Custom Labware')
+    cy.get(
+      ':nth-child(1) > .Card__Section-r4iqug-0 > .Card__Title-r4iqug-1'
+    ).should('have.text', 'Labware Management')
+    cy.get('.styles__card_copy__3nzb4').should(
+      'have.text',
+      'Manage custom labware definitions for use in your Python Protocol API Version 2 protocols.'
+    )
+    cy.get(
+      ':nth-child(3) > .styles__labeled_control__WeQhH > .styles__labeled_control_label__3UyR9'
+    ).should('have.text', 'Custom Labware Definitions Folder')
+    cy.get('.forms__input_field__1ppsZ').should('exist')
+    cy.get(
+      ':nth-child(3) > .styles__labeled_control__WeQhH > .buttons__button_outline__3z7qv'
+    ).should('exist')
+    cy.get('[name="change-source"] > span').should('exist')
+    cy.get('[name="reset-source"] > span').should('exist')
+    cy.get(
+      ':nth-child(4) > .styles__labeled_control__WeQhH > .styles__labeled_control_label__3UyR9'
+    ).should('have.text', 'Add New Labware Definitions')
+    cy.get(
+      ':nth-child(4) > .styles__control_info__2Q2Rd > :nth-child(1)'
+    ).should(
+      'have.text',
+      'Add labware definitions to your Custom Labware Definitions folder for use on any OT-2 robot.'
+    )
+    cy.get(
+      ':nth-child(4) > .styles__labeled_control__WeQhH > .buttons__button_outline__3z7qv'
+    ).should('exist')
+    cy.get(
+      ':nth-child(4) > .styles__control_info__2Q2Rd > :nth-child(2)'
+    ).should(
+      'have.text',
+      'Use the Custom Labware Creator to generate new labware definitions'
+    )
+    cy.get(
+      ':nth-child(2) > .Card__Section-r4iqug-0 > .Card__Title-r4iqug-1'
+    ).should('have.text', 'Custom Labware List')
+  })
+
+  // Reset Source for Custom Labware Definitions Folder
+  it('Confirm able to reset source for Custom Labware Definitions Folder', () => {
+    // Click Reset Source but concel before actually doing it
+    cy.get('[name="reset-source"] > span').then(() => {
+      cy.get('[name="reset-source"] > span').click()
+
+      // Confirm contents on the modal
+      cy.get('.modals__alert_modal_heading__hnOW-').should(
+        'have.text',
+        'Reset Custom Labware source directory?'
+      )
+      cy.get('.modals__alert_modal_contents__I7cuT > :nth-child(1)').should(
+        'have.text',
+        'Click "Reset Source" to reset your custom labware directory to its default location.'
+      )
+      cy.get('.modals__alert_modal_contents__I7cuT > :nth-child(2)').should(
+        'have.text',
+        'Labware in your current source directory will not be moved nor deleted.'
+      )
+
+      // Cancel out of the modal
+      cy.get('[name="cancel"]').click()
     })
 
-    // Confirm text on page is correct
-    it('Confirm text on page is correct', () => {
-        cy.get('.structure__title__3nJ-D').should('have.text', 'Custom Labware')
-        cy.get(':nth-child(1) > .Card__Section-r4iqug-0 > .Card__Title-r4iqug-1').should('have.text', 'Labware Management')
-        cy.get('.styles__card_copy__3nzb4').should('have.text', 'Manage custom labware definitions for use in your Python Protocol API Version 2 protocols.')
-        cy.get(':nth-child(3) > .styles__labeled_control__WeQhH > .styles__labeled_control_label__3UyR9').should('have.text', 'Custom Labware Definitions Folder')
-        cy.get('.forms__input_field__1ppsZ').should('exist')
-        cy.get(':nth-child(3) > .styles__labeled_control__WeQhH > .buttons__button_outline__3z7qv').should('exist')
-        cy.get('[name="change-source"] > span').should('exist')
-        cy.get('[name="reset-source"] > span').should('exist')
-        cy.get(':nth-child(4) > .styles__labeled_control__WeQhH > .styles__labeled_control_label__3UyR9').should('have.text', 'Add New Labware Definitions')
-        cy.get(':nth-child(4) > .styles__control_info__2Q2Rd > :nth-child(1)').should('have.text', 'Add labware definitions to your Custom Labware Definitions folder for use on any OT-2 robot.')
-        cy.get(':nth-child(4) > .styles__labeled_control__WeQhH > .buttons__button_outline__3z7qv').should('exist')
-        cy.get(':nth-child(4) > .styles__control_info__2Q2Rd > :nth-child(2)').should('have.text', 'Use the Custom Labware Creator to generate new labware definitions')
-        cy.get(':nth-child(2) > .Card__Section-r4iqug-0 > .Card__Title-r4iqug-1').should('have.text', 'Custom Labware List')
+    // Actually reset source and do not cancel
+    cy.get('[name="reset-source"] > span').then(() => {
+      cy.get('[name="reset-source"] > span').click()
+      cy.get(
+        '.modals__alert_modal_buttons__2GQHd > [name="reset-source"]'
+      ).click()
     })
-
-    // Reset Source for Custom Labware Definitions Folder
-    it('Confirm able to reset source for Custom Labware Definitions Folder', () => {
-        // Click Reset Source but concel before actually doing it
-        cy.get('[name="reset-source"] > span').then(() => {
-            cy.get('[name="reset-source"] > span').click()
-
-            // Confirm contents on the modal
-            cy.get('.modals__alert_modal_heading__hnOW-').should('have.text', 'Reset Custom Labware source directory?')
-            cy.get('.modals__alert_modal_contents__I7cuT > :nth-child(1)').should('have.text', 'Click "Reset Source" to reset your custom labware directory to its default location.')
-            cy.get('.modals__alert_modal_contents__I7cuT > :nth-child(2)').should('have.text', 'Labware in your current source directory will not be moved nor deleted.')
-
-            // Cancel out of the modal
-            cy.get('[name="cancel"]').click()
-        })
-
-        // Actually reset source and do not cancel
-        cy.get('[name="reset-source"] > span').then(() => {
-            cy.get('[name="reset-source"] > span').click()
-            cy.get('.modals__alert_modal_buttons__2GQHd > [name="reset-source"]').click()
-        })
-    })
+  })
 })
