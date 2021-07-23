9eb15675e2c1bdc781267e4e1374a1b08bfc9c60
Mike Cousins <mike@opentrons.com>
2021-07-23 13:24:36 -0400

    wip: run formatter on some robot-server files

--- robot-server/tests/test_app.py
+++ robot-server/tests/test_app.py
@@ -26,9 +26,10 @@ from robot_server.constants import (
             {API_VERSION_HEADER: str(API_VERSION_LATEST)},
             API_VERSION,
         ],
-    ])
+    ],
+)
 def test_api_versioning(api_client, headers, expected_version):
-    resp = api_client.get('/settings', headers=headers)
+    resp = api_client.get("/settings", headers=headers)
     assert resp.headers.get(API_VERSION_HEADER) == str(expected_version)
     assert resp.headers.get(MIN_API_VERSION_HEADER) == str(MIN_API_VERSION)
 
@@ -50,9 +51,9 @@ def mock_log_control():
         "/logs/api.log",
         "/logs/some-random-journald-thing",
         "/",
-    ])
-def test_api_versioning_non_versions_endpoints(
-        api_client, path, mock_log_control):
+    ],
+)
+def test_api_versioning_non_versions_endpoints(api_client, path, mock_log_control):
     del api_client.headers["Opentrons-Version"]
     resp = api_client.get(path)
     assert resp.headers.get(API_VERSION_HEADER) == str(API_VERSION)
@@ -61,7 +62,7 @@ def test_api_versioning_non_versions_endpoints(
 
 def test_api_version_too_low(api_client):
     """It should reject any API version lower than 2."""
-    resp = api_client.get('/settings', headers={API_VERSION_HEADER: "1"})
+    resp = api_client.get("/settings", headers={API_VERSION_HEADER: "1"})
 
     assert resp.status_code == HTTPStatus.BAD_REQUEST
     assert resp.headers.get(API_VERSION_HEADER) == str(API_VERSION)
@@ -72,7 +73,7 @@ def test_api_version_too_low(api_client):
             "detail": (
                 "HTTP API version 1 is no longer supported. Please upgrade "
                 "your Opentrons App or other HTTP API client."
-            )
+            ),
         }
     ]
 
@@ -85,7 +86,8 @@ def test_api_version_too_low(api_client):
         "/system/time",
         "/calibration/pipette_offset",
         "/calibration/tip_length",
-    ])
+    ],
+)
 def test_api_version_missing(api_client, path):
     """It should reject any request without an version header."""
     del api_client.headers["Opentrons-Version"]
