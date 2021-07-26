31856567f5753f1927a6c0328e8ab3699b88a3a9
Derek Maggio <DerekMaggio@users.noreply.github.com>
2021-07-26 08:12:15 -0700

    feat(api): Add Magdeck G-Code Parsing (#8159)

--- api/src/opentrons/hardware_control/g_code_parsing/utils.py
+++ api/src/opentrons/hardware_control/g_code_parsing/utils.py
@@ -1,12 +1,15 @@
 import re
-from typing import Dict, Type
-from opentrons.drivers.smoothie_drivers.driver_3_0 import GCODE as SMOOTHIE_GCODE
+from typing import Dict, Type, Union
+from opentrons.drivers.smoothie_drivers.driver_3_0 import GCODE as SMOOTHIE_G_CODE
+from opentrons.drivers.mag_deck.driver import GCODE as MAGDECK_G_CODE
 
 
 WRITE_REGEX = re.compile(r"(.*?) \| (.*?) \|(.*?)$")
 
 
-def reverse_enum(enum_to_reverse: Type[SMOOTHIE_GCODE]) -> Dict:
+def reverse_enum(
+        enum_to_reverse: Union[Type[SMOOTHIE_G_CODE], Type[MAGDECK_G_CODE]]
+) -> Dict:
     """
     Returns dictionary with keys and values switched from passed Enum
     :param enum_to_reverse: The Enum that you want to reverse
@@ -15,9 +18,9 @@ def reverse_enum(enum_to_reverse: Type[SMOOTHIE_GCODE]) -> Dict:
     # I don't know what is going on with mypy, it is complaining
     # about keys not existing as an attribute. I am not calling it
     # as an attribute. I am calling it as a function.
-    members = enum_to_reverse._member_map_.keys()  # type: ignore[attr-defined]
+    members = enum_to_reverse.__members__.keys()
     values = [
-        enum_to_reverse[member].value
+        enum_to_reverse[member]
         for member in members
     ]
     return dict(zip(values, members))
