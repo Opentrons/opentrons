7d94ae4ce6ad6b2f900ccf40ea16d8f00adecbcd
Mike Cousins <mike@opentrons.com>
2021-08-05 17:55:28 -0400

    fixup: un-reorder actions router tests

--- robot-server/tests/sessions/router/test_actions_router.py
+++ robot-server/tests/sessions/router/test_actions_router.py
@@ -9,7 +9,7 @@ from httpx import AsyncClient
 from tests.helpers import verify_response
 from robot_server.service.task_runner import TaskRunner
 from robot_server.sessions.session_view import SessionView, BasicSessionCreateData
-from robot_server.sessions.engine_store import EngineStore
+from robot_server.sessions.engine_store import EngineStore, EngineMissingError
 
 from robot_server.sessions.session_store import (
     SessionStore,
@@ -92,6 +92,75 @@ def test_create_play_action(
     decoy.verify(engine_store.runner.play())
 
 
+def test_create_session_action_with_missing_id(
+    decoy: Decoy,
+    session_store: SessionStore,
+    unique_id: str,
+    current_time: datetime,
+    client: TestClient,
+) -> None:
+    """It should 404 if the session ID does not exist."""
+    not_found_error = SessionNotFoundError(session_id="session-id")
+
+    decoy.when(session_store.get(session_id="session-id")).then_raise(not_found_error)
+
+    response = client.post(
+        "/sessions/session-id/actions",
+        json={"data": {"actionType": "play"}},
+    )
+
+    verify_response(
+        response,
+        expected_status=404,
+        expected_errors=SessionNotFound(detail=str(not_found_error)),
+    )
+
+
+def test_create_session_action_without_runner(
+    decoy: Decoy,
+    session_view: SessionView,
+    engine_store: EngineStore,
+    unique_id: str,
+    current_time: datetime,
+    client: TestClient,
+) -> None:
+    """It should 400 if the runner is not able to handle the action."""
+    actions = SessionAction(
+        actionType=SessionActionType.PLAY,
+        createdAt=current_time,
+        id=unique_id,
+    )
+
+    next_session = SessionResource(
+        session_id="unique-id",
+        create_data=BasicSessionCreateData(),
+        created_at=datetime(year=2021, month=1, day=1),
+        actions=[actions],
+    )
+
+    decoy.when(
+        session_view.with_action(
+            session=prev_session,
+            action_id=unique_id,
+            action_data=SessionActionCreateData(actionType=SessionActionType.PLAY),
+            created_at=current_time,
+        ),
+    ).then_return((actions, next_session))
+
+    decoy.when(engine_store.runner.play()).then_raise(EngineMissingError("oh no"))
+
+    response = client.post(
+        "/sessions/session-id/actions",
+        json={"data": {"actionType": "play"}},
+    )
+
+    verify_response(
+        response,
+        expected_status=400,
+        expected_errors=SessionActionNotAllowed(detail="oh no"),
+    )
+
+
 def test_create_pause_action(
     decoy: Decoy,
     session_view: SessionView,
@@ -141,7 +210,7 @@ async def test_create_stop_action(
     current_time: datetime,
     async_client: AsyncClient,
 ) -> None:
-    """It should handle a halt action."""
+    """It should handle a stop action."""
     action = SessionAction(
         actionType=SessionActionType.STOP,
         createdAt=current_time,
@@ -171,78 +240,3 @@ async def test_create_stop_action(
 
     verify_response(response, expected_status=201, expected_data=action)
     decoy.verify(await engine_store.runner.stop())
-
-
-def test_create_session_action_with_missing_id(
-    decoy: Decoy,
-    session_store: SessionStore,
-    unique_id: str,
-    current_time: datetime,
-    client: TestClient,
-) -> None:
-    """It should 404 if the session ID does not exist."""
-    not_found_error = SessionNotFoundError(session_id="session-id")
-
-    decoy.when(session_store.get(session_id="session-id")).then_raise(not_found_error)
-
-    response = client.post(
-        "/sessions/session-id/actions",
-        json={"data": {"actionType": "play"}},
-    )
-
-    verify_response(
-        response,
-        expected_status=404,
-        expected_errors=SessionNotFound(detail=str(not_found_error)),
-    )
-
-
-@pytest.mark.xfail(strict=True)
-def test_create_session_action_without_runner(
-    decoy: Decoy,
-    session_view: SessionView,
-    engine_store: EngineStore,
-    unique_id: str,
-    current_time: datetime,
-    client: TestClient,
-) -> None:
-    """It should 400 if the runner is not able to handle the action."""
-    actions = SessionAction(
-        actionType=SessionActionType.PLAY,
-        createdAt=current_time,
-        id=unique_id,
-    )
-
-    next_session = SessionResource(
-        session_id="unique-id",
-        create_data=BasicSessionCreateData(),
-        created_at=datetime(year=2021, month=1, day=1),
-        actions=[actions],
-    )
-
-    decoy.when(
-        session_view.with_action(
-            session=prev_session,
-            action_id=unique_id,
-            action_data=SessionActionCreateData(actionType=SessionActionType.PLAY),
-            created_at=current_time,
-        ),
-    ).then_return((actions, next_session))
-
-    # TODO(mc, 2021-07-06): in reality, it will be the engine_store.runner
-    # property access that triggers this raise. Explore adding property access
-    # rehearsals to decoy
-    # decoy.when(
-    #     await engine_store.runner.run()
-    # ).then_raise(EngineMissingError("oh no"))
-
-    response = client.post(
-        "/sessions/session-id/actions",
-        json={"data": {"actionType": "play"}},
-    )
-
-    verify_response(
-        response,
-        expected_status=400,
-        expected_errors=SessionActionNotAllowed(detail="oh no"),
-    )
