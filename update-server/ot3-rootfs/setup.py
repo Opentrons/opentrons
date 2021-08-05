b41c53f179efc97d1e245e6b60b2279b1404c2dd
sotaoverride <aatrapps@gmail.com>
2021-08-05 09:35:34 -0400

    fix(setup.py): add install requires
    Add INSTALL_REQUIRES

--- update-server/ot3-rootfs/setup.py
+++ update-server/ot3-rootfs/setup.py
@@ -1,4 +1,16 @@
 from distutils.core import setup
+INSTALL_REQUIRES = [
+    'bmap-tools==3.6',
+    'certifi==2021.5.30',
+    'charset-normalizer==2.0.4',
+    'docopt==0.6.2',
+    'idna==3.2',
+    'pipreqs==0.4.10',
+    'requests==2.26.0',
+    'six==1.16.0',
+    'urllib3==1.26.6',
+    'yarg==0.1.9'
+]     
 
 setup(name='RootFS',
       version='1.0',
@@ -6,4 +18,5 @@ setup(name='RootFS',
       author='Aatir Manzur',
       url='https://www.github.com/Opentrons/opentrons/update-server/ot3-rootfs',
       packages=['RootFS'],
+      install_requires=INSTALL_REQUIRES
      )
