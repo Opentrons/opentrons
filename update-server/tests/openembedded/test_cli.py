d661db36f8a32b7d76cd6a6c6df01312bbca3e3e
sotaoverride <aatrapps@gmail.com>
2021-08-13 09:10:02 -0400

    feat(test_cli.py): add cli pytests
    Add cli pytests for RootFS

---
+++ update-server/tests/openembedded/test_cli.py
@@ -0,0 +1,14 @@
+""" tests for otupdate.openembedded.RootFS
+
+Checks functionaly and error casees for the RootFS cli
+"""
+import pytest
+
+from otupdate.openembedded import RootFSCLI
+
+
+def test_parser_bad_arg():
+    with pytest.raises(SystemExit):
+        rfscli = RootFSCLI.RootFSCLI()
+        parser = rfscli.parse_args(['debug', '--tt', 'testting', '--badarg'])
+        print (parser.__dict__)
