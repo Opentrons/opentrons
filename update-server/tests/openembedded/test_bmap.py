73f2f084245ab95c83ddc5cf06ce7734d019de0b
sotaoverride <aatrapps@gmail.com>
2021-08-16 13:49:55 -0400

    feat(test_bmap.py): add simple bmap write test
    Add a simple bmap write test without writiting to an actual disk

---
+++ update-server/tests/openembedded/test_bmap.py
@@ -0,0 +1,22 @@
+""" tests for otupdate.openembedded.RootFS
+
+Checks functionaly and error casees for the bmap operations on RootFS
+"""
+import os
+import subprocess
+from otupdate.openembedded import RootFS
+
+
+def test_disk_image_write(tmp_path):
+    d = tmp_path / "disk_image"
+    d.mkdir()
+    CONTENT = "mock_fs"
+    p = d / "mockFS.wic"
+    p.write_text(CONTENT)
+    subprocess.run(["bmaptool", "create", "-o", "disk_image/image.bmap", "disk_image/mockFS.wic"])
+    rfs = RootFS.RootFS()
+    RootFS.SD_CARD_MOUNT_POINT = os.getcwd()
+    RootFS.BMAP_FILE = 'disk_image/image..bmap'
+    RootFS.DISK = d
+    RootFS.BMAP_IMAGE = 'disk_image/mockFS.wic'
+    rfs.factoryRestore(None)
