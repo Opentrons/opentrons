bf89a39569367b196279540835ba953847578203
reachojhaneha <39049853+reachojhaneha@users.noreply.github.com>
2021-08-24 16:24:48 -0400

    test(protocol-designer): fixing thermocycler failing cypress tests (#8234)
    * fixing the tests
    
    * addressing PR comment
    
    Co-authored-by: neha ojha <nehaojha@nehas-MacBook-Pro.local>

--- protocol-designer/cypress/integration/newProtocolWithTC.spec.js
+++ protocol-designer/cypress/integration/newProtocolWithTC.spec.js
@@ -26,6 +26,8 @@ const moduleModelDropdown =
 const thermocyclerSlot = 'foreignObject[x="12"][y="267"]'
 const slotSeven = 'foreignObject[x="0"][y="181"]'
 
+const thermocyclerWellBlockWithExtraSpace = 'TC Well ' // Note: reason this is needed is because of a cypress race condition. see `https://stackoverflow.com/questions/66257661/how-to-resolve-missing-characters-bug-in-cypress-type-command`
+
 describe('Protocols with Modules', () => {
   beforeEach(() => {
     cy.viewport('macbook-15')
@@ -143,7 +145,7 @@ describe('Protocols with Modules', () => {
         })
       })
       cy.get('input[class*="LabwareOverlays__name_input"]').type(
-        thermocyclerWellBlock
+        thermocyclerWellBlockWithExtraSpace
       )
 
       // Add Thermocycler State Step
@@ -271,7 +273,7 @@ describe('Protocols with Modules', () => {
         })
       })
       cy.get('input[class*="LabwareOverlays__name_input"]').type(
-        thermocyclerWellBlock
+        thermocyclerWellBlockWithExtraSpace
       )
 
       // Add Thermocycler Profile Step
