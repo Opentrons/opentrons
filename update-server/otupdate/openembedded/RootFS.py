35f0226e21dcd15b9504d684b713e4711aa443c7
sotaoverride <aatrapps@gmail.com>
2021-08-24 09:39:08 -0400

    refactor(rootfs.py): add type hints
    add type hints

--- update-server/otupdate/openembedded/RootFS.py
+++ update-server/otupdate/openembedded/RootFS.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import re
 import shlex
@@ -35,9 +36,9 @@ class RootFS:
     def __init__(self):
         self.root_FS_config_ = self.RootFSConfig('' , '', '' , '',
                                                  'mmcblk0',
-                                                 '', '', '/media/mmcblkp1')
+                                                 'root_part', '2', '/media/mmcblkp1')
 
-    def set_partition(self, arg, partition_name):
+    def set_partition(self, arg: argparse.Namespace, partition_name: str) -> None:
         """ Run boot util command here to set partion
          Use the libubootenv utility to set bootargs
          boot.src has a carveout for bootargs, use that """
@@ -61,7 +62,7 @@ class RootFS:
         else:
             raise AssertionError("Unexpected value of partition")
 
-    def swap_partition(self, arg):
+    def swap_partition(self, arg: argparse.Namespace) -> None:
         """swap partitions get current partition
            and swap it with the other available partition"""
         current_partition = self.get_partition()
@@ -71,7 +72,7 @@ class RootFS:
             else:
                 self.set_partition(arg, self.root_FS_config_.ROOTFS_PART1)
 
-    def factory_restore(self, arg):
+    def factory_restore(self, arg: argparse.Namespace) -> None:
         """" bmap to factory reset here"""
         bmap = (self.root_FS_config_.SD_CARD_MOUNT_POINT +
                 self.root_FS_config_.BMAP_FILE)
@@ -83,11 +84,11 @@ class RootFS:
                         self.root_FS_config_.DISK])
 
     """ debug fuctions """
-    def print_rootFS_partition(self, arg) -> str:
+    def print_rootFS_partition(self, arg: argparse.Namespace) -> str:
         tmp = self.get_partition()
         return ('Current RootFS Partition '+tmp.disk+'\n')
 
-    def print_rootFS_config(self, arg) -> str:
+    def print_rootFS_config(self,arg: argparse.Namespace) -> str:
         return(('ROOTFS_TEST_TITLE '+arg.tt+'\n') +
                ('ROOTFS_PART1 '+self.root_FS_config_.ROOTFS_PART1+'\n') +
                ('ROOTFS_PART2 '+self.root_FS_config_.ROOTFS_PART2+'\n') +
@@ -99,6 +100,6 @@ class RootFS:
                ('SD_CARD_MOUNT_POINT '+self.root_FS_config_.SD_CARD_MOUNT_POINT+'\n')
                )
 
-    def debug(self, arg):
+    def debug(self, arg: argparse.Namespace) -> None:
         print(self.print_rootFS_partition(arg))
         print(self.print_rootFS_config(arg))
