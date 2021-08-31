579b04fdce70015ce510d8e1b03b0820afcf7ee0
Max Marrone <max@marrone.nyc>
2021-08-31 11:07:25 -0400

    Flesh out `Metadata` docstring.

--- robot-server/robot_server/protocols/protocol_models.py
+++ robot-server/robot_server/protocols/protocol_models.py
@@ -8,7 +8,33 @@ from typing import Optional
 
 
 class Metadata(BaseModel):
-    """Static info that doesn't affect how the protocol will be run."""
+    """Extra, nonessential information about the protocol.
+
+    This can include things like:
+
+    * A human-readable title and description.
+    * A last-modified date.
+    * A list of authors.
+
+    The exact set of fields is not defined by this API.
+    A protocol can define whatever it likes as its metadata,
+    and this API will pass it along without modification.
+    When reading a protocol's metadata,
+    you should treat it as an arbitrary JSON object from an untrusted source.
+
+    This metadata does *not* include
+    anything material to how the robot will run the protocol.
+    For example, it does not include the protocol's pipette and labware requirements.
+
+    (Exception:
+    Python protocols define their `apiLevel`,
+    which *does* affect how they're run,
+    as part of their metadata.
+    This is for historical reasons;
+    `apiLevel` is not truly metadata and doesn't really belong here.
+    You should not rely on a Python protocol's correct `apiLevel` being present here.
+    If a compelling need arises, this API may expose `apiLevel` some other way.)
+    """
 
     class Config:
         extra = "allow"
@@ -20,7 +46,9 @@ class Protocol(ResourceModel):
     id: str = Field(..., description="A unique identifier for this protocol.")
     createdAt: datetime = Field(
         ...,
-        description="When this protocol was *uploaded.* See also `metadata.createdAt`.",
+        description="When this protocol was *uploaded.*"
+                    " (`metadata` may have information about"
+                    " when this protocol was *authored.*)"
     )
     protocolType: ProtocolFileType = Field(
         ...,
