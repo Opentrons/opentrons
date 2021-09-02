66ecceb4c825bb6c19957594b2dfff72281e78b9
Derek Maggio <derek.maggio@opentrons.com>
2021-09-02 13:49:49 -0700

    Edit setup info

---
+++ g_code_testing/setup.py
@@ -0,0 +1,84 @@
+# Inspired by:
+# https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+import sys
+import codecs
+import os
+import os.path
+from setuptools import setup, find_packages
+
+# make stdout blocking since Travis sets it to nonblocking
+if os.name == "posix":
+    import fcntl
+
+    flags = fcntl.fcntl(sys.stdout, fcntl.F_GETFL)
+    fcntl.fcntl(sys.stdout, fcntl.F_SETFL, flags & ~os.O_NONBLOCK)
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(HERE, "..", "..", "..", "scripts"))
+
+from python_build_utils import normalize_version  # noqa: E402
+
+
+def get_version():
+    buildno = os.getenv("BUILD_NUMBER")
+    if buildno:
+        normalize_opts = {"extra_tag": buildno}
+    else:
+        normalize_opts = {}
+    return normalize_version("g-code-testing", **normalize_opts)
+
+
+VERSION = get_version()
+DISTNAME = 'g-code-testing'
+LICENSE = "Apache 2.0"
+AUTHOR = "Opentrons"
+EMAIL = "engineering@opentrons.com"
+URL = "https://github.com/Opentrons/opentrons"
+DOWNLOAD_URL = ""
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Scientific/Engineering",
+]
+KEYWORDS = ["robots", "protocols", "synbio", "pcr", "automation", "lab"]
+DESCRIPTION = 'Library to run emulated OT-2 and gather it\'s G-Code output'
+PACKAGES = find_packages(where=".", exclude=["tests.*", "tests"])
+INSTALL_REQUIRES = [
+    f"opentrons=={VERSION}",
+    f"robot-server=={VERSION}",
+    f"notify-server=={VERSION}",
+]
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+setup(
+    python_requires=">=3.7",
+    name=DISTNAME,
+    description=DESCRIPTION,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=EMAIL,
+    keywords=KEYWORDS,
+    long_description=__doc__,
+    packages=PACKAGES,
+    zip_safe=False,
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIRES,
+    include_package_data=True,
+
+)
