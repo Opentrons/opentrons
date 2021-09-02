45e6cd9d90cde5c730f7d098cd50a3476ac43124
smb2268 <sarah.breen@opentrons.com>
2021-09-02 15:16:06 -0400

    Completed unattached state

--- app/src/organisms/ProtocolSetup/RunSetupCard/RobotCalibration/CalibrationItem.tsx
+++ app/src/organisms/ProtocolSetup/RunSetupCard/RobotCalibration/CalibrationItem.tsx
@@ -11,58 +11,68 @@ import {
   C_WHITE,
   COLOR_SUCCESS,
   Box,
+  JUSTIFY_SPACE_BETWEEN,
 } from '@opentrons/components'
 import { formatLastModified } from '../../../CalibrationPanels/utils'
 
 interface Props {
-  index?: number
-  title?: string
-  calibratedDate?: string | null
-  calibrated: boolean
   button?: JSX.Element
+  calibrated: boolean
+  calibratedDate?: string | null
+  index?: number
+  subText?: string
+  title?: string
 }
 
 export function CalibrationItem(props: Props): JSX.Element | null {
-  const { index, title, calibratedDate, calibrated, button } = props
+  const { index, title, subText, calibratedDate, calibrated, button } = props
   const { t } = useTranslation(['protocol_setup'])
   const backgroundColor =
     index !== undefined && index % 2 === 0 ? C_NEAR_WHITE : C_WHITE
-
+  const calibratedText =
+    calibratedDate !== null && calibratedDate !== undefined
+      ? t('last_calibrated', {
+          date: formatLastModified(calibratedDate),
+        })
+      : t('not_calibrated')
   return (
     <Box backgroundColor={backgroundColor}>
-      <Flex flexDirection="row" alignItems="center" padding=".5rem">
-        {calibrated ? (
-          <Icon
-            name={'check-circle'}
-            height="1.5rem"
-            width="1.5rem"
-            color={COLOR_SUCCESS}
-            marginRight="0.75rem"
-          />
-        ) : (
-          <Icon
-            height="1.5rem"
-            width="1.5rem"
-            name={'circle'}
-            color={C_WHITE}
-            border="1px solid #9B9B9B"
-            borderRadius="1.5rem"
-            marginRight="0.75rem"
-          />
-        )}
-        <span>
-          {title !== undefined && (
-            <Text fontSize={FONT_SIZE_BODY_2}>{title}</Text>
+      <Flex
+        flexDirection="row"
+        alignItems="center"
+        padding=".5rem"
+        justifyContent={JUSTIFY_SPACE_BETWEEN}
+      >
+        <Flex flexDirection="row" alignItems="center">
+          {calibrated ? (
+            <Icon
+              name={'check-circle'}
+              height="1.5rem"
+              width="1.5rem"
+              color={COLOR_SUCCESS}
+              marginRight="0.75rem"
+            />
+          ) : (
+            <Icon
+              height="1.5rem"
+              width="1.5rem"
+              name={'circle'}
+              color={C_WHITE}
+              border="1px solid #9B9B9B"
+              borderRadius="1.5rem"
+              marginRight="0.75rem"
+            />
           )}
-          <Text fontSize={FONT_SIZE_BODY_1} fontStyle={FONT_STYLE_ITALIC}>
-            {calibratedDate !== null && calibratedDate !== undefined
-              ? t('last_calibrated', {
-                  date: formatLastModified(calibratedDate),
-                })
-              : t('not_calibrated')}
-          </Text>
-        </span>
-        {button}
+          <span>
+            {title !== undefined && (
+              <Text fontSize={FONT_SIZE_BODY_2}>{title}</Text>
+            )}
+            <Text fontSize={FONT_SIZE_BODY_1} fontStyle={FONT_STYLE_ITALIC}>
+              {subText !== undefined ? subText : calibratedText}
+            </Text>
+          </span>
+        </Flex>
+        <div>{button}</div>
       </Flex>
     </Box>
   )
--- app/src/organisms/ProtocolSetup/RunSetupCard/RobotCalibration/index.tsx
+++ app/src/organisms/ProtocolSetup/RunSetupCard/RobotCalibration/index.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
+import { Link as RRDLink } from 'react-router-dom'
 import {
   Text,
+  Flex,
   SPACING_3,
   FONT_WEIGHT_BOLD,
+  FONT_SIZE_BODY_1,
+  FONT_STYLE_ITALIC,
   FONT_HEADER_THIN,
   PrimaryBtn,
+  C_BLUE,
 } from '@opentrons/components'
 import * as PipetteOffset from '../../../../redux/calibration/pipette-offset'
 import * as Pipettes from '../../../../redux/pipettes'
 import * as TipLength from '../../../../redux/calibration/tip-length'
 import * as PipetteConstants from '../../../../redux/pipettes/constants'
+import { formatLastModified } from '../../../CalibrationPanels/utils'
 import { DeckCalibration } from './DeckCalibration'
 import { CalibrationItem } from './CalibrationItem'
 
 import type { Dispatch, State } from '../../../../redux/types'
 import type { ViewableRobot } from '../../../../redux/discovery/types'
+
+const pipettesPageUrl = `/robots/opentrons-dev/instruments`
 
 interface Props {
   robot: ViewableRobot
@@ -51,23 +59,54 @@ export function RobotCalibration(props: Props): JSX.Element {
           if (pipetteTipRackData == null) {
             return null
           } else {
+            let calibrated = false
+            let button
+            let subText
             const attached =
               pipetteTipRackData.exactPipetteMatch ===
                 PipetteConstants.INEXACT_MATCH ||
               pipetteTipRackData.exactPipetteMatch === PipetteConstants.MATCH
 
+            if (
+              pipetteTipRackData.pipetteCalDate !== undefined &&
+              pipetteTipRackData.pipetteCalDate !== null &&
+              attached
+            ) {
+              calibrated = true
+            } else if (!attached) {
+              subText = t('attach_pipette_calibration')
+              button = (
+                <Flex flexDirection="row" alignItems="center">
+                  <Text
+                    fontSize={FONT_SIZE_BODY_1}
+                    fontStyle={FONT_STYLE_ITALIC}
+                    marginRight="2rem"
+                  >
+                    {t('pipette_missing')}
+                  </Text>
+                  <PrimaryBtn
+                    as={RRDLink}
+                    to={pipettesPageUrl}
+                    backgroundColor={C_BLUE}
+                  >
+                    {t('attach_pipette_cta')}
+                  </PrimaryBtn>
+                </Flex>
+              )
+            } else {
+              subText = t('not_calibrated')
+            }
+
             return (
               <CalibrationItem
+                button={button}
+                calibrated={calibrated}
+                calibratedDate={pipetteTipRackData.pipetteCalDate}
                 index={index}
+                subText={subText}
                 title={`${t('mount_title', { mount: mount.toUpperCase() })} ${
                   pipetteTipRackData.pipetteDisplayName
                 }`}
-                calibratedDate={pipetteTipRackData.pipetteCalDate}
-                calibrated={
-                  attached &&
-                  pipetteTipRackData.pipetteCalDate !== undefined &&
-                  pipetteTipRackData.pipetteCalDate !== null
-                }
               />
             )
           }
@@ -90,10 +129,10 @@ export function RobotCalibration(props: Props): JSX.Element {
                 {pipetteTipRackData.tipRacks.map((tipRack, index) => (
                   <CalibrationItem
                     key={index}
+                    calibrated={tipRack.lastModifiedDate !== null}
+                    calibratedDate={tipRack.lastModifiedDate}
                     index={index}
                     title={tipRack.displayName}
-                    calibratedDate={tipRack.lastModifiedDate}
-                    calibrated={tipRack.lastModifiedDate !== null}
                   />
                 ))}
               </div>
