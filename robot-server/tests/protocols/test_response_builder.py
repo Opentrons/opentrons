3afbea2b26d259e2bd614f440a026f316d3a2317
Max Marrone <max@marrone.nyc>
2021-09-03 18:02:39 -0400

    Update `ResponseBuilder` test.

--- robot-server/tests/protocols/test_response_builder.py
+++ robot-server/tests/protocols/test_response_builder.py
@@ -1,18 +1,25 @@
 """Tests for the protocol response model builder."""
 from datetime import datetime
 
-from opentrons.protocol_runner import ProtocolFileType
+from opentrons.protocol_runner import ProtocolFileType, JsonPreAnalysis
 from robot_server.protocols.protocol_store import ProtocolResource
-from robot_server.protocols.protocol_models import Protocol
+from robot_server.protocols.protocol_models import Protocol, Metadata
 from robot_server.protocols.analysis_models import PendingAnalysis
 from robot_server.protocols.response_builder import ResponseBuilder
 
 
 def test_create_single_json_file_response() -> None:
-    """It should create a BasicSession if session_data is None."""
+    metadata_as_dict = {
+        "a_string": "hello",
+        "an_int": 9001,
+        "a_float": 3.14,
+        "a_nested_object": {"a_bool": True}
+    }
+
     protocol_resource = ProtocolResource(
         protocol_id="protocol-id",
         protocol_type=ProtocolFileType.JSON,
+        pre_analysis=JsonPreAnalysis(metadata=metadata_as_dict),
         created_at=datetime(year=2021, month=1, day=1),
         files=[],
     )
@@ -25,6 +32,7 @@ def test_create_single_json_file_response() -> None:
     assert result == Protocol(
         id="protocol-id",
         protocolType=ProtocolFileType.JSON,
+        metadata=Metadata.parse_obj(metadata_as_dict),
         createdAt=datetime(year=2021, month=1, day=1),
         analyses=[protocol_analysis],
     )
