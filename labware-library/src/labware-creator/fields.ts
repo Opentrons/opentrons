Opentrons/opentrons

commit 12a18cb5da24c20ead996f51f1b78f70353f3c7a

refactor(labware-creator): use more const types (#7972)

labware-library/src/labware-creator/fields.ts
@@ -53,7 +53,7 @@ export interface Option {
   disabled?: boolean
   imgSrc?: string
 }
-export type Options = Option[]
+export type Options = readonly Option[]
 
 // NOTE: annoyingly, some components support "rich" `name` values (eg Dropdown)
 // that can be a JSX.Element, and others like RadioField only support string values for `name` :(
@@ -331,22 +331,19 @@ export const aluminumBlockAutofills = {
   },
 } as const
 
-export const labwareTypeAutofills: Record<
-  LabwareType,
-  Partial<LabwareFields>
-> = {
+export const labwareTypeAutofills = {
   tipRack: {
-    homogeneousWells: 'true' as const,
-    wellShape: 'circular' as const,
+    homogeneousWells: 'true',
+    wellShape: 'circular',
     wellBottomShape: null,
   },
   tubeRack: {},
   wellPlate: {},
   reservoir: {},
   aluminumBlock: {},
-}
+} as const
 
-export const aluminumBlockChildTypeOptions: Options = [
+export const aluminumBlockChildTypeOptions = [
   {
     name: 'Tubes',
     value: 'tubes',
@@ -359,14 +356,14 @@ export const aluminumBlockChildTypeOptions: Options = [
     name: 'PCR Plate',
     value: 'pcrPlate',
   },
-]
+] as const
 
 export const getInitialStatus = (): FormStatus => ({
   defaultedDef: null,
   prevValues: null,
 })
 
-export const getDefaultFormState = (): LabwareFields => ({
+export const getDefaultFormState = (): Readonly<LabwareFields> => ({
   labwareType: null,
   tubeRackInsertLoadName: null,
   aluminumBlockType: null,
@@ -413,7 +410,7 @@ export const getDefaultFormState = (): LabwareFields => ({
   pipetteName: null,
 })
 
-export const LABELS: Record<keyof LabwareFields, string> = {
+export const LABELS: Readonly<Record<keyof LabwareFields, string>> = {
   labwareType: 'What type of labware are you creating?',
   tubeRackInsertLoadName: 'Which tube rack?',
   aluminumBlockType: 'Which aluminum block?',

commit decfaa5992caac5170e017f6a31508f9923b2111

fix(labware-creator): fix readonly option type error
fix small typing bug introduced in #7972

labware-library/src/labware-creator/fields.ts
@@ -53,7 +53,7 @@ export interface Option {
   disabled?: boolean
   imgSrc?: string
 }
-export type Options = readonly Option[]
+export type Options = Option[]
 
 // NOTE: annoyingly, some components support "rich" `name` values (eg Dropdown)
 // that can be a JSX.Element, and others like RadioField only support string values for `name` :(
@@ -63,7 +63,7 @@ export interface RichOption {
   disabled?: boolean
   imgSrc?: string
 }
-export type RichOptions = RichOption[]
+export type RichOptions = readonly RichOption[]
 
 export type LabwareType =
   | 'wellPlate'
@@ -363,7 +363,7 @@ export const getInitialStatus = (): FormStatus => ({
   prevValues: null,
 })
 
-export const getDefaultFormState = (): Readonly<LabwareFields> => ({
+export const getDefaultFormState = (): LabwareFields => ({
   labwareType: null,
   tubeRackInsertLoadName: null,
   aluminumBlockType: null,
@@ -410,7 +410,7 @@ export const getDefaultFormState = (): Readonly<LabwareFields> => ({
   pipetteName: null,
 })
 
-export const LABELS: Readonly<Record<keyof LabwareFields, string>> = {
+export const LABELS: Record<keyof LabwareFields, string> = {
   labwareType: 'What type of labware are you creating?',
   tubeRackInsertLoadName: 'Which tube rack?',
   aluminumBlockType: 'Which aluminum block?',