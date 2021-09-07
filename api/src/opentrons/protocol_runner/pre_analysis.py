47f73fb51c0405d822cd718ea8cf3c3b52a6f887
Max Marrone <max@marrone.nyc>
2021-09-07 16:24:15 -0400

    Add skeleton for `.analyze()`.

--- api/src/opentrons/protocol_runner/pre_analysis.py
+++ api/src/opentrons/protocol_runner/pre_analysis.py
@@ -10,17 +10,31 @@ class PreAnalyzer:  # noqa: D101
     def analyze(  # noqa: D102
         protocol_files: List[Path],
     ) -> Union["PythonPreAnalysis", "JsonPreAnalysis"]:
-        raise NotImplementedError()
+        if len(protocol_files) == 0:
+            raise ProtocolNotPreAnalyzableError("Protocol must have at least one file.")
+        elif len(protocol_files) > 1:
+            # TODO(mm, 2021-09-07): add multi-file support
+            raise NotImplementedError("Multi-file protocols not yet supported.")
+
+        main_file = protocol_files[0]
+        suffix = main_file.suffix
+
+        if suffix == ".json":
+            raise NotImplementedError()
+        elif suffix == ".py":
+            raise NotImplementedError()
+        else:
+            raise ProtocolNotPreAnalyzableError(f'Unrecognized file suffix "{suffix}"')
 
 
 Metadata = Dict[str, Any]
-"""A protocol's metadata: non-essential info, like author and title.
+"""A protocol's metadata (non-essential info, like author and title).
 
 Robot software should not change how it handles a protocol depending on anything in
 here.
 
-This must be a dict of simple types serializable to JSON via ``json.dumps()``.
-``Dict[str, Any]`` is an overly-permissive approximation needed because mypy doesn't
+This must be a simple JSON-like dict, serializable to JSON via ``json.dumps()``.
+``Dict[str, Any]`` is an overly-permissive approximation, needed because mypy doesn't
 support recursive types.
 """
 
@@ -34,3 +48,7 @@ class PythonPreAnalysis:  # noqa: D101
 @dataclass(frozen=True)
 class JsonPreAnalysis:  # noqa: D101
     metadata: Metadata
+
+
+class ProtocolNotPreAnalyzableError(Exception):
+    """Raised when an error in the protocol prevents pre-analysis."""
