334f7a2cb52add0b8ed779fec235d3d05afcfe6a
Derek Maggio <derek.maggio@opentrons.com>
2021-09-13 11:53:05 -0700

    Update Makefile

--- g-code-testing/g_code_parsing.mk
+++ g-code-testing/g_code_parsing.mk
@@ -84,9 +84,9 @@ open-diff:
 	google-chrome file://$(diff_file)
 
 
-####################
-### G-CODE TESTS ###
-####################
+##########################################################################
+#                             PROTOCOL TESTS                             #
+##########################################################################
 
 ########################
 # smoothie_protocol.py #
@@ -263,3 +263,260 @@ g-code-2-single-channel-v2-protocol-update-s3:
 	@$(MAKE) --no-print-directory g-code-s3-push \
 		source_file_path=$(file_under_test) \
 		object_name='2-single-channel-v2.txt'
+
+
+##########################################################################
+#                              HTTP TESTS                                #
+##########################################################################
+
+##########################
+# g-code-http-move-left-pipette #
+##########################
+
+# http-move-left-pipette-run
+#	Description - Run http-move-left-pipette and store the output to /tmp/file_under_test.txt
+.PHONY: g-code-http-move-left-pipette-run
+g-code-http-move-left-pipette-run:
+	@$(MAKE) --no-print-directory g-code-run \
+		left_pipette='{"model": "p20_single_v2.0", "id": "P20SV202020070101"}' \
+		right_pipette='{"model": "p300_single_v2.1", "id": "P20SV202020070101"}' \
+		configuration_name='http_move_left_pipette' \
+  		> $(file_under_test)
+
+# g-code-http-move-left-pipette-diff
+#	Description - Run http function to move left pipette and compare it's output to S3 master file.
+# 			      Store the diff to /tmp/diff.html
+.PHONY: g-code-http-move-left-pipette-diff
+g-code-http-move-left-pipette-diff:
+	@$(MAKE) --no-print-directory g-code-http-move-left-pipette-run
+	@$(MAKE) --no-print-directory g-code-s3-pull \
+		object_name='http-move-left-pipette.txt' \
+		> $(master_file)
+	@$(MAKE) --no-print-directory g-code-diff \
+	> /tmp/http-move-left-pipette-diff.html
+
+
+# g-code-http-move-left-pipette-update-s3
+#	Description - Run http-move-left-pipette and override the S3 master file with it's output
+.PHONY: g-code-http-move-left-pipette-update-s3
+g-code-http-move-left-pipette-update-s3:
+	@$(MAKE) --no-print-directory g-code-http-move-left-pipette-run
+	@$(MAKE) --no-print-directory g-code-s3-push \
+		source_file_path=$(file_under_test) \
+		object_name='http-move-left-pipette.txt'
+
+##########################
+# g-code-http-move-right-pipette #
+##########################
+
+# http-move-right-pipette-run
+#	Description - Run http-move-right-pipette and store the output to /tmp/file_under_test.txt
+.PHONY: g-code-http-move-right-pipette-run
+g-code-http-move-right-pipette-run:
+	@$(MAKE) --no-print-directory g-code-run \
+		left_pipette='{"model": "p20_single_v2.0", "id": "P20SV202020070101"}' \
+		right_pipette='{"model": "p300_single_v2.1", "id": "P20SV202020070101"}' \
+		configuration_name='http_move_right_pipette' \
+  		> $(file_under_test)
+
+# g-code-http-move-right-pipette-diff
+#	Description - Run http function to move right pipette and compare it's output to S3 master file.
+# 			      Store the diff to /tmp/diff.html
+.PHONY: g-code-http-move-right-pipette-diff
+g-code-http-move-right-pipette-diff:
+	@$(MAKE) --no-print-directory g-code-http-move-right-pipette-run
+	@$(MAKE) --no-print-directory g-code-s3-pull \
+		object_name='http-move-right-pipette.txt' \
+		> $(master_file)
+	@$(MAKE) --no-print-directory g-code-diff \
+	> /tmp/http-move-right-pipette-diff.html
+
+
+# g-code-http-move-right-pipette-update-s3
+#	Description - Run http-move-right-pipette and override the S3 master file with it's output
+.PHONY: g-code-http-move-right-pipette-update-s3
+g-code-http-move-right-pipette-update-s3:
+	@$(MAKE) --no-print-directory g-code-http-move-right-pipette-run
+	@$(MAKE) --no-print-directory g-code-s3-push \
+		source_file_path=$(file_under_test) \
+		object_name='http-move-right-pipette.txt'
+
+##########################
+# g-code-http-move-left-mount #
+##########################
+
+# http-move-left-mount-run
+#	Description - Run http-move-left-mount and store the output to /tmp/file_under_test.txt
+.PHONY: g-code-http-move-left-mount-run
+g-code-http-move-left-mount-run:
+	@$(MAKE) --no-print-directory g-code-run \
+		left_pipette='{"model": "p20_single_v2.0", "id": "P20SV202020070101"}' \
+		right_pipette='{"model": "p300_single_v2.1", "id": "P20SV202020070101"}' \
+		configuration_name='http_move_left_mount' \
+  		> $(file_under_test)
+
+# g-code-http-move-left-mount-diff
+#	Description - Run http function to move left mount and compare it's output to S3 master file.
+# 			      Store the diff to /tmp/diff.html
+.PHONY: g-code-http-move-left-mount-diff
+g-code-http-move-left-mount-diff:
+	@$(MAKE) --no-print-directory g-code-http-move-left-mount-run
+	@$(MAKE) --no-print-directory g-code-s3-pull \
+		object_name='http-move-left-mount.txt' \
+		> $(master_file)
+	@$(MAKE) --no-print-directory g-code-diff \
+	> /tmp/http-move-left-mount-diff.html
+
+
+# g-code-http-move-left-mount-update-s3
+#	Description - Run http-move-left-mount and override the S3 master file with it's output
+.PHONY: g-code-http-move-left-mount-update-s3
+g-code-http-move-left-mount-update-s3:
+	@$(MAKE) --no-print-directory g-code-http-move-left-mount-run
+	@$(MAKE) --no-print-directory g-code-s3-push \
+		source_file_path=$(file_under_test) \
+		object_name='http-move-left-mount.txt'
+
+##########################
+# g-code-http-move-right-mount #
+##########################
+
+# http-move-right-mount-run
+#	Description - Run http-move-right-mount and store the output to /tmp/file_under_test.txt
+.PHONY: g-code-http-move-right-mount-run
+g-code-http-move-right-mount-run:
+	@$(MAKE) --no-print-directory g-code-run \
+		left_pipette='{"model": "p20_single_v2.0", "id": "P20SV202020070101"}' \
+		right_pipette='{"model": "p300_single_v2.1", "id": "P20SV202020070101"}' \
+		configuration_name='http_move_right_mount' \
+  		> $(file_under_test)
+
+# g-code-http-move-right-mount-diff
+#	Description - Run http function to move right mount and compare it's output to S3 master file.
+# 			      Store the diff to /tmp/diff.html
+.PHONY: g-code-http-move-right-mount-diff
+g-code-http-move-right-mount-diff:
+	@$(MAKE) --no-print-directory g-code-http-move-right-mount-run
+	@$(MAKE) --no-print-directory g-code-s3-pull \
+		object_name='http-move-right-mount.txt' \
+		> $(master_file)
+	@$(MAKE) --no-print-directory g-code-diff \
+	> /tmp/http-move-right-mount-diff.html
+
+
+# g-code-http-move-right-mount-update-s3
+#	Description - Run http-move-right-mount and override the S3 master file with it's output
+.PHONY: g-code-http-move-right-mount-update-s3
+g-code-http-move-right-mount-update-s3:
+	@$(MAKE) --no-print-directory g-code-http-move-right-mount-run
+	@$(MAKE) --no-print-directory g-code-s3-push \
+		source_file_path=$(file_under_test) \
+		object_name='http-move-right-mount.txt'
+
+##########################
+# g-code-http-home-left-pipette #
+##########################
+
+# http-home-left-pipette-run
+#	Description - Run http-home-left-pipette and store the output to /tmp/file_under_test.txt
+.PHONY: g-code-http-home-left-pipette-run
+g-code-http-home-left-pipette-run:
+	@$(MAKE) --no-print-directory g-code-run \
+		left_pipette='{"model": "p20_single_v2.0", "id": "P20SV202020070101"}' \
+		right_pipette='{"model": "p300_single_v2.1", "id": "P20SV202020070101"}' \
+		configuration_name='http_home_left_pipette' \
+  		> $(file_under_test)
+
+# g-code-http-home-left-pipette-diff
+#	Description - Run http function to move left mount and compare it's output to S3 master file.
+# 			      Store the diff to /tmp/diff.html
+.PHONY: g-code-http-home-left-pipette-diff
+g-code-http-home-left-pipette-diff:
+	@$(MAKE) --no-print-directory g-code-http-home-left-pipette-run
+	@$(MAKE) --no-print-directory g-code-s3-pull \
+		object_name='http-home-left-pipette.txt' \
+		> $(master_file)
+	@$(MAKE) --no-print-directory g-code-diff \
+	> /tmp/http-home-left-pipette-diff.html
+
+
+# g-code-http-home-left-pipette-update-s3
+#	Description - Run http-home-left-pipette and override the S3 master file with it's output
+.PHONY: g-code-http-home-left-pipette-update-s3
+g-code-http-home-left-pipette-update-s3:
+	@$(MAKE) --no-print-directory g-code-http-home-left-pipette-run
+	@$(MAKE) --no-print-directory g-code-s3-push \
+		source_file_path=$(file_under_test) \
+		object_name='http-home-left-pipette.txt'
+
+##########################
+# g-code-http-home-right-pipette #
+##########################
+
+# http-home-right-pipette-run
+#	Description - Run http-home-right-pipette and store the output to /tmp/file_under_test.txt
+.PHONY: g-code-http-home-right-pipette-run
+g-code-http-home-right-pipette-run:
+	@$(MAKE) --no-print-directory g-code-run \
+		left_pipette='{"model": "p20_single_v2.0", "id": "P20SV202020070101"}' \
+		right_pipette='{"model": "p300_single_v2.1", "id": "P20SV202020070101"}' \
+		configuration_name='http_home_right_pipette' \
+  		> $(file_under_test)
+
+# g-code-http-home-right-pipette-diff
+#	Description - Run http function to move right mount and compare it's output to S3 master file.
+# 			      Store the diff to /tmp/diff.html
+.PHONY: g-code-http-home-right-pipette-diff
+g-code-http-home-right-pipette-diff:
+	@$(MAKE) --no-print-directory g-code-http-home-right-pipette-run
+	@$(MAKE) --no-print-directory g-code-s3-pull \
+		object_name='http-home-right-pipette.txt' \
+		> $(master_file)
+	@$(MAKE) --no-print-directory g-code-diff \
+	> /tmp/http-home-right-pipette-diff.html
+
+
+# g-code-http-home-right-pipette-update-s3
+#	Description - Run http-home-right-pipette and override the S3 master file with it's output
+.PHONY: g-code-http-home-right-pipette-update-s3
+g-code-http-home-right-pipette-update-s3:
+	@$(MAKE) --no-print-directory g-code-http-home-right-pipette-run
+	@$(MAKE) --no-print-directory g-code-s3-push \
+		source_file_path=$(file_under_test) \
+		object_name='http-home-right-pipette.txt'
+
+##########################
+# g-code-http-home-robot #
+##########################
+
+# http-home-robot-run
+#	Description - Run http-home-robot and store the output to /tmp/file_under_test.txt
+.PHONY: g-code-http-home-robot-run
+g-code-http-home-robot-run:
+	@$(MAKE) --no-print-directory g-code-run \
+		left_pipette='{"model": "p20_single_v2.0", "id": "P20SV202020070101"}' \
+		right_pipette='{"model": "p300_single_v2.1", "id": "P20SV202020070101"}' \
+		configuration_name='http_home_robot' \
+  		> $(file_under_test)
+
+# g-code-http-home-robot-diff
+#	Description - Run http function to move right mount and compare it's output to S3 master file.
+# 			      Store the diff to /tmp/diff.html
+.PHONY: g-code-http-home-robot-diff
+g-code-http-home-robot-diff:
+	@$(MAKE) --no-print-directory g-code-http-home-robot-run
+	@$(MAKE) --no-print-directory g-code-s3-pull \
+		object_name='http-home-robot.txt' \
+		> $(master_file)
+	@$(MAKE) --no-print-directory g-code-diff \
+	> /tmp/http-home-robot-diff.html
+
+
+# g-code-http-home-robot-update-s3
+#	Description - Run http-home-robot and override the S3 master file with it's output
+.PHONY: g-code-http-home-robot-update-s3
+g-code-http-home-robot-update-s3:
+	@$(MAKE) --no-print-directory g-code-http-home-robot-run
+	@$(MAKE) --no-print-directory g-code-s3-push \
+		source_file_path=$(file_under_test) \
+		object_name='http-home-robot.txt'
