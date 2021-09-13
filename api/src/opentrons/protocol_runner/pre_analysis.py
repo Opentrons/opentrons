1d99102d34cdf26415d29bf4d0c69647090c4000
Max Marrone <max@marrone.nyc>
2021-09-13 18:03:55 -0400

    Add missing `pre_analysis` docstrings.

--- api/src/opentrons/protocol_runner/pre_analysis.py
+++ api/src/opentrons/protocol_runner/pre_analysis.py
@@ -1,4 +1,10 @@
-# noqa: D100
+"""Protocol pre-analysis.
+
+Pre-analysis is the first step to reading a protocol. Given an opaque set of files,
+pre-analysis extracts basic information, like whether the files appear to represent a
+protocol, and if so, what kind of protocol it is.
+"""
+
 
 from ast import parse as parse_python
 from dataclasses import dataclass
@@ -12,11 +18,23 @@ from opentrons.protocols.parse import extract_metadata as extract_python_metadat
 from opentrons.protocols.models import JsonProtocol
 
 
+# todo(mm, 2021-09-13): Currently wrapped in a class so dependency-injection and mocking
+# feel less fragile, pending a team discussion about whether we still think this is
+# a good thing to do in Python.
 class PreAnalyzer:  # noqa: D101
     @staticmethod
-    def analyze(  # noqa: D102
+    def analyze(
         protocol_files: List[Path],
     ) -> Union["PythonPreAnalysis", "JsonPreAnalysis"]:
+        """Pre-analyze a set of files that's thought to define a protocol.
+
+        Returns:
+            A `JsonPreAnalysis` or `PythonPreAnalysis`, if the files look like a
+            basically valid JSON or Python protocol, respectively.
+
+        Raises:
+            Any of the subclasses of `NotPreAnalyzableError` defined in this module.
+        """
         if len(protocol_files) == 0:
             raise NoFilesError("Protocol must have at least one file.")
         elif len(protocol_files) > 1:
@@ -98,8 +116,8 @@ Robot software should not change how it handles a protocol depending on anything
 here.
 
 This must be a simple JSON-like dict, serializable to JSON via ``json.dumps()``.
-``Dict[str, Any]`` is an overly-permissive approximation, needed because mypy doesn't
-support recursive types.
+``Dict[str, Any]`` is an overly-permissive approximation,
+needed because mypy doesn't support recursive types.
 """
 
 
@@ -115,11 +133,15 @@ class JsonPreAnalysis:  # noqa: D101
 
 
 class NotPreAnalyzableError(Exception):
-    """Raised when a problem with the provided files prevents pre-analysis."""
+    """Raised when any problem with the provided files prevents pre-analysis."""
 
 
 class FileTypeError(NotPreAnalyzableError):
-    """Raised when a file is provided that's apparently not a protocol file."""
+    """Raised when a file is provided that doesn't look like any kind of protocol."""
+
+
+class NoFilesError(NotPreAnalyzableError):
+    """Raised when an empty list of files is provided."""
 
 
 class JsonParseError(NotPreAnalyzableError):
@@ -131,12 +153,8 @@ class JsonSchemaValidationError(NotPreAnalyzableError):
 
 
 class PythonFileParseError(NotPreAnalyzableError):
-    """Raised when an apparent Python file can't be parsed as Python."""
+    """Raised when an apparent Python protocol can't be parsed as Python."""
 
 
 class PythonMetadataError(NotPreAnalyzableError):
-    """Raised when something's wrong with a Python file's metadata block."""
-
-
-class NoFilesError(NotPreAnalyzableError):
-    """Raised when an empty list of files is provided."""
+    """Raised when something's wrong with a Python protocol's metadata block."""
