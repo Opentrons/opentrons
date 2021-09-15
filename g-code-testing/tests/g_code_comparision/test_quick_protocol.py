39d2d0658c0f2dbcbe6471ba5d337f5968de659e
Derek Maggio <derek.maggio@opentrons.com>
2021-09-15 08:38:42 -0700

    Formatting

--- g-code-testing/tests/g_code_comparision/test_quick_protocol.py
+++ g-code-testing/tests/g_code_comparision/test_quick_protocol.py
@@ -10,7 +10,7 @@ from tests.g_code_comparision.utils import get_master_file
 def protocol_smoothie_setup() -> Settings:
     smoothie = SmoothieSettings(
         left={"model": "p20_single_v2.0", "id": "P20SV202020070101"},
-        right={"model": "p20_single_v2.0", "id": "P20SV202020070101"}
+        right={"model": "p20_single_v2.0", "id": "P20SV202020070101"},
     )
     return Settings(smoothie=smoothie)
 
@@ -25,28 +25,25 @@ def two_single_channel_setup() -> Settings:
 
 TEST_DATA = [
     [
-        'smoothie-protocol-output.txt',
+        "smoothie-protocol-output.txt",
         "protocols/smoothie_protocol.py",
-        protocol_smoothie_setup()
+        protocol_smoothie_setup(),
     ],
     [
-        '2-single-channel-v2.txt',
+        "2-single-channel-v2.txt",
         "protocols/2_single_channel_v2.py",
-        two_single_channel_setup()
+        two_single_channel_setup(),
     ],
 ]
 
 
 @pytest.mark.g_code_confirm
-@pytest.mark.parametrize(
-    "master_file_name,path,settings",
-    TEST_DATA
-)
+@pytest.mark.parametrize("master_file_name,path,settings", TEST_DATA)
 def test_quick_protocols(master_file_name, path, settings):
     expected_output = get_master_file(master_file_name)
     with GCodeEngine(settings).run_protocol(path=path) as program:
         actual_output = program.get_text_explanation(SupportedTextModes.CONCISE)
 
-    assert actual_output == expected_output, \
-        GCodeDiffer(actual_output, expected_output).get_html_diff()
-
+    assert actual_output == expected_output, GCodeDiffer(
+        actual_output, expected_output
+    ).get_html_diff()
--- g-code-testing/tests/g_code_comparision/utils.py
+++ g-code-testing/tests/g_code_comparision/utils.py
@@ -9,12 +9,16 @@ from robot_server.service.legacy.models.control import RobotHomeTarget  # type: 
 from robot_server.service.dependencies import get_motion_lock  # type: ignore
 
 
-BUCKET_NAME = 'g-code-comparison'
+BUCKET_NAME = "g-code-comparison"
 
 
 def get_master_file(master_file_name: str) -> str:
-    s3 = boto3.resource('s3')
-    master_file = s3.Object(
-        BUCKET_NAME, master_file_name
-    ).get().get('Body').read().decode('utf-8')
+    s3 = boto3.resource("s3")
+    master_file = (
+        s3.Object(BUCKET_NAME, master_file_name)
+        .get()
+        .get("Body")
+        .read()
+        .decode("utf-8")
+    )
     return master_file.strip()
