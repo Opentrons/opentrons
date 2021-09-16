b45e05b55b1976f09e29a1adc79d1734115bb568
Derek Maggio <DerekMaggio@users.noreply.github.com>
2021-09-16 12:32:13 -0700

    feat(g-code-testing): HTTP G-Code GitHub Action (RET-174) (#8338)
    * Add Github Action
    
    
    Update Names
    
    
    Run make command in correct project
    
    
    Diff command not run command
    
    
    Add move right pipette
    
    
    Add if: always()
    
    
    Maybe something is being cached because of the name of the job?
    
    * Update Makefile
    
    * Add control actions
    
    * Rename
    
    * Update README
    
    * Fix formatting
    
    * Add aws python package
    
    * Revert GCodeEngine
    
    * Remove configurations
    
    * Update G-Code Engine tests
    
    * Remove GCodeTestData class
    
    * Remove cli
    
    * Add getting S3 Object
    
    * Update http G-Code Tests
    
    * Update Protocol Tests
    
    * Update Makefile
    
    * Add g_code_confirm mark
    
    * Update makefile
    
    * make test - only framework unittests
    * make test-g-code-fast - test fast G-Code confirm tests
    * make test-g-code-2-modules - test 2 modules protocol
    * make test-g-code-swift-smoke - test swift smoke protocol
    * make test-g-code-swift-turbo - test swift turbo protocol
    
    * Consolidate G-Code actions into single action
    
    * Formatting
    
    * Rename file
    
    
    Add running on change of file
    
    * Hardcode boto3 version
    
    * Update type hints
    
    * Use matrix
    
    
    Call the correct path

---
+++ g-code-testing/tests/g_code_comparision/http/test_robot.py
@@ -0,0 +1,114 @@
+from typing import Callable
+
+import pytest
+import asyncio
+from robot_server.service.legacy.routers.control import (
+    post_home_robot,
+    post_move_robot,
+)  # type: ignore
+from robot_server.service.legacy.models.control import (
+    RobotHomeTarget,
+    RobotMoveTarget,
+)  # type: ignore
+from robot_server.service.dependencies import get_motion_lock  # type: ignore
+from g_code_parsing.g_code_engine import GCodeEngine
+from functools import partial
+from opentrons.hardware_control.emulation.settings import Settings, SmoothieSettings
+from g_code_parsing.g_code_program.supported_text_modes import SupportedTextModes
+from g_code_parsing.g_code_differ import GCodeDiffer
+from tests.g_code_comparision.utils import get_master_file
+
+
+@pytest.fixture
+def http_settings() -> Settings:
+    left = {"model": "p20_single_v2.0", "id": "P20SV202020070101"}
+    right = {"model": "p300_single_v2.1", "id": "P20SV202020070101"}
+
+    return Settings(smoothie=SmoothieSettings(left=left, right=right))
+
+
+def robot_home_robot() -> Callable:
+    return partial(
+        post_home_robot,
+        robot_home_target=RobotHomeTarget(target="robot"),
+        motion_lock=asyncio.run(get_motion_lock()),
+    )
+
+
+def robot_home_left_pipette() -> Callable:
+    return partial(
+        post_home_robot,
+        robot_home_target=RobotHomeTarget(target="pipette", mount="left"),
+        motion_lock=asyncio.run(get_motion_lock()),
+    )
+
+
+def robot_home_right_pipette() -> Callable:
+    return partial(
+        post_home_robot,
+        robot_home_target=RobotHomeTarget(target="pipette", mount="right"),
+        motion_lock=asyncio.run(get_motion_lock()),
+    )
+
+
+def robot_move_left_mount() -> Callable:
+    return partial(
+        post_move_robot,
+        robot_move_target=RobotMoveTarget(
+            target="mount", point=[11.0, 55.2, 30.4], mount="left"
+        ),
+        motion_lock=asyncio.run(get_motion_lock()),
+    )
+
+
+def robot_move_left_pipette() -> Callable:
+    return partial(
+        post_move_robot,
+        RobotMoveTarget(target="pipette", point=[100.0, 90.0, 150.0], mount="left"),
+        motion_lock=asyncio.run(get_motion_lock()),
+    )
+
+
+def robot_move_right_mount() -> Callable:
+    return partial(
+        post_move_robot,
+        robot_move_target=RobotMoveTarget(
+            target="mount", point=[300.0, 43.0, 40.0], mount="right"
+        ),
+        motion_lock=asyncio.run(get_motion_lock()),
+    )
+
+
+def robot_move_right_pipette() -> Callable:
+    return partial(
+        post_move_robot,
+        robot_move_target=RobotMoveTarget(
+            target="pipette", point=[10.0, 20.0, 15.0], mount="right"
+        ),
+        motion_lock=asyncio.run(get_motion_lock()),
+    )
+
+
+TEST_DATA = [
+    ["http-home-robot.txt", robot_home_robot()],
+    ["http-home-left-pipette.txt", robot_home_left_pipette()],
+    ["http-home-right-pipette.txt", robot_home_right_pipette()],
+    ["http-move-left-mount.txt", robot_move_left_mount()],
+    ["http-move-left-pipette.txt", robot_move_left_pipette()],
+    ["http-move-right-mount.txt", robot_move_right_mount()],
+    ["http-move-right-pipette.txt", robot_move_right_pipette()],
+]
+
+
+@pytest.mark.g_code_confirm
+@pytest.mark.parametrize("master_file_name,executable", TEST_DATA)
+def test_robot_home_robot(
+    master_file_name: str, executable: Callable, http_settings: Settings
+):
+    expected_output = get_master_file(master_file_name)
+    with GCodeEngine(http_settings).run_http(executable=executable) as program:
+        actual_output = program.get_text_explanation(SupportedTextModes.CONCISE)
+
+    assert actual_output == expected_output, GCodeDiffer(
+        actual_output, expected_output
+    ).get_html_diff()
