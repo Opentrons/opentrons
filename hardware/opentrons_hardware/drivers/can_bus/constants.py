28c3fd16512b624df6faa08202a3d6c664998de1
amit lissack <amit@opentrons.com>
2021-09-16 15:44:03 -0400

    generate constants header.

---
+++ hardware/opentrons_hardware/drivers/can_bus/constants.py
@@ -0,0 +1,51 @@
+"""Constants for can bus."""
+from enum import Enum
+
+
+class NodeId(int, Enum):
+    """Can bus arbitration id node id."""
+
+    broadcast = 0x00
+    host = 0x10
+    pipette = 0x20
+    gantry = 0x40
+
+
+class FunctionCode(int, Enum):
+    """Can bus arbitration id function code."""
+
+    network_management = 0x0
+    sync = 0x2
+    error = 0x4
+    command = 0x10
+    status = 0x12
+    parameters = 0x14
+    bootloader = 0x7C
+    heartbeat = 0x7E
+
+
+class MessageId(int, Enum):
+    """Can bus arbitration id message id."""
+
+    heartbeat_request = 0x3FFF
+    heartbeat_response = 0x3FFE
+
+    device_info_request = 0x3002
+    device_info_response = 0x3003
+
+    stop_request = 0x00
+
+    get_status_request = 0x01
+    get_status_response = 0x05
+
+    move_request = 0x10
+
+    setup_request = 0x02
+
+    set_speed_request = 0x03
+
+    get_speed_request = 0x04
+    get_speed_response = 0x11
+    write_eeprom = 0x2001
+    read_eeprom_request = 0x2002
+    read_eeprom_response = 0x2003
--- hardware/opentrons_hardware/scripts/generate_header.py
+++ hardware/opentrons_hardware/scripts/generate_header.py
@@ -1,16 +1,14 @@
 """Script to generate c++ header file of canbus constants."""
 import argparse
 import io
+from enum import Enum
 from pathlib import Path
+from typing import Type
 
 from opentrons_hardware.drivers.can_bus import (
-    CanDriver,
     MessageId,
     FunctionCode,
     NodeId,
-    CanMessage,
-    ArbitrationId,
-    ArbitrationIdParts,
 )
 
 
@@ -20,22 +18,28 @@ def run(file: Path) -> None:
         output.write("/********************************************\n")
         output.write("* This is a generated file. Do not modify.  *\n")
         output.write("********************************************/\n")
-        output.write("#pragma once\n")
+        output.write("#pragma once\n\n")
 
-        # Function code
-        output.write("/* Function code definitions. */\n")
-        output.write(f"enum class {FunctionCode.__name__} {{\n")
-        for i in FunctionCode:
-            output.write(f"  {i.name} = 0x{i.value:x},\n")
-
-        output.write("}\n")
-
+        write_enum(FunctionCode, output)
+        write_enum(MessageId, output)
+        write_enum(NodeId, output)
 
         file.write_text(output.getvalue())
 
 
+def write_enum(e: Type[Enum], output: io.StringIO) -> None:
+    """Generate enum class from enumeration."""
+    output.write(f"/** {e.__doc__} */\n")
+    output.write(f"enum class {e.__name__} {{\n")
+    for i in e:
+        output.write(f"  {i.name} = 0x{i.value:x},\n")
+    output.write("}\n\n")
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate a C++ header file defining CANBUS constants.")
+    parser = argparse.ArgumentParser(
+        description="Generate a C++ header file defining CANBUS constants."
+    )
     parser.add_argument(
         "--target",
         type=str,
@@ -46,4 +50,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     run(Path(args.target))
-
