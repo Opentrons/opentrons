ee3b7e7d9983b2895bec0b4cb32c11a08422e336
sotaoverride <aatrapps@gmail.com>
2021-09-17 10:01:33 -0400

    decorator for session

--- update-server/otupdate/ot_utils/session.py
+++ update-server/otupdate/ot_utils/session.py
@@ -9,8 +9,7 @@ token assignment, etc
 import functools
 import logging
 from openembedded import (root_fs, oe_server_mode)
-
-# from openembedded_server import update, config
+from openembedded_server import update, config, update_session
 
 from aiohttp import web
 
@@ -22,20 +21,20 @@ def active_session_check(handler):
     """ decorator to check session status
     """
     @functools.wraps(handler)
-    async def decorated(request: web.Request, response: web.Response) -> web.Response:
-        if update.session_from_request(request) is None:
+    async def decorated(request: web.Request) -> web.Response:
+        if update.session_from_request(request) is not None:
             LOG.warning("check_session: active session exists!")
-            response= web.json_response(
+            return web.json_response(
                 data={'message':
                       'An update session is already active on this robot',
                       'error': 'session-already-active'},
                 status=409)
         else:
-            session = UpdateSession(
+            session = update_session.UpdateSession(
                 config.config_from_request(request).download_storage_path)
             request.app[SESSION_VARNAME] = session
-            response = web.json_response(
+            return web.json_response(
             data={'token': session.token},
             status=201)
-        return await handler(request, response)
+        return await handler(request)
     return decorated
