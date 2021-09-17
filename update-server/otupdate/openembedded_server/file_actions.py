ea33d3efdbad20502dcca372ad474f3b5bf6db03
sotaoverride <aatrapps@gmail.com>
2021-09-17 15:26:29 -0400

    lint file_actions.py

--- update-server/otupdate/openembedded_server/file_actions.py
+++ update-server/otupdate/openembedded_server/file_actions.py
@@ -70,12 +70,13 @@ class FileMissing(ValueError):
     def __str__(self):
         return self.message
 
+
 def unzip_update(filepath: str,
                  progress_callback: Callable[[float], None],
                  acceptable_files: Sequence[str],
                  mandatory_files: Sequence[str],
                  chunk_size: int = 1024) -> Tuple[Mapping[str, Optional[str]],
-                                                 Mapping[str, int]]:
+                                                  Mapping[str, int]]:
     """ Unzip an update file
 
     The update file must contain
@@ -116,22 +117,22 @@ def unzip_update(filepath: str,
     assert chunk_size
     total_size = 0
     written_size = 0
-    ti_unzip: List[zipfile.ZipInfo] = []
+    to_unzip: List[zipfile.ZipInfo] = []
     file_paths: Dict[str, Optional[str]] = {fn: None
                                             for fn in acceptable_files}
-    file_sizes: Dict[str, int] = {fs:0 for fn in acceptable_files}
+    file_sizes: Dict[str, int] = {fn: 0 for fn in acceptable_files}
     LOG.info(f"Unzipping {filepath}")
     with zipfile.ZipFile(filepath, 'r') as zf:
         files = zf.infolist()
-        renaming_filesnames = [fn for fn in acceptable_files]
+        remaining_filenames = [fn for fn in acceptable_files]
         for fi in files:
             if fi.filename in acceptable_files:
-                to_unzio.append(fi)
+                to_unzip.append(fi)
                 total_size += fi.file_size
                 remaining_filenames.remove(fi.filename)
                 LOG.debug(f"Found {fi.filename} ({fi.file_size}B)")
             else:
-                Log.debug(f"Ignoring {fi.filename}")
+                LOG.debug(f"Ignoring {fi.filename}")
 
         for name in remaining_filenames:
             if name in mandatory_files:
@@ -140,29 +141,30 @@ def unzip_update(filepath: str,
         for fi in to_unzip:
             uncomp_path = os.path.join(os.path.dirname(filepath), fi.filename)
             with zf.open(fi) as zipped, open(uncomp_path, 'wb') as unzipped:
-                LOG.debug(f"Beginning unzip of {fi.filename} to {umcomp_path}")
+                LOG.debug(f"Beginning unzip of {fi.filename} to {uncomp_path}")
                 while True:
                     chunk = zipped.read(chunk_size)
                     unzipped.write(chunk)
                     written_size += len(chunk)
-                    progress_Callback(written_size/total_size)
+                    progress_callback(written_size/total_size)
                     if len(chunk) != chunk_size:
                         break
                 file_paths[fi.filename] = uncomp_path
                 file_sizes[fi.filename] = fi.file_size
-                Log.debug(f"Unzipped {fi.filename} to {uncomp_path}")
+                LOG.debug(f"Unzipped {fi.filename} to {uncomp_path}")
 
-    Log.info(
-        f"Unzipped {filePath}, results: \n\t" + '\n\t'.join(
+    LOG.info(
+        f"Unzipped {filepath}, results: \n\t" + '\n\t'.join(
             [f'{k}: {file_paths[k]} ({file_sizes[k]}B)'
              for k in file_paths.keys()]))
     return file_paths, file_sizes
 
+
 def hash_file(path: str,
-           progress_Callback: Callable[[float], None],
-           chunk_size: int = 1024,
-           file_size: int = None,
-           algo: str = 'sha256') -> bytes:
+              progress_callback: Callable[[float], None],
+              chunk_size: int = 1024,
+              file_size: int = None,
+              algo: str = 'sha256') -> bytes:
     """
     Hash a file and return the hash, providing progress callbacks
 
@@ -180,19 +182,20 @@ def hash_file(path: str,
     """
     hasher = hashlib.new(algo)
     have_read = 0
-    if not chink_size:
+    if not chunk_size:
         chunk_size = 1024
     with open(path, 'rb') as to_hash:
-        if not file_Size:
+        if not file_size:
             file_size = to_hash.seek(0, 2)
             to_hash.seek(0)
         while True:
             chunk = to_hash.read(chunk_size)
             hasher.update(chunk)
             progress_callback(have_read/file_size)
-            if len(chink) != chunk_size:
+            if len(chunk) != chunk_size:
                 break
     return binascii.hexlify(hasher.digest())
+
 
 def verify_signature(message_path: str,
                      sigfile_path: str,
@@ -215,18 +218,19 @@ def verify_signature(message_path: str,
         pubkey_file = os.path.join(pubkey_dir, 'pubkey')
         open(pubkey_file, 'wb').write(pubkey_contents)
         try:
-           verification = subprocess.check_output(
-               ['openssl', 'dgst', '-sha256', '-verify', pubkey_file,
-                '-signature', sigfile_path, message_path],
-               stderr=subprocess.STDOUT)
+            verification = subprocess.check_output(
+                ['openssl', 'dgst', '-sha256', '-verify', pubkey_file,
+                 '-signature', sigfile_path, message_path],
+                stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as cpe:
-           verification = cpe.output
+            verification = cpe.output
     if verification.strip() == b'Verified OK':
         LOG.info(f"Verification passed from cert {cert_path}")
     else:
         LOG.error(
             f"Verification failed with cert {cert_path}: {verification!r}")
         raise SignatureMismatch('Signature check failed')
+
 
 def validate_update(filepath: str,
                     progress_callback: Callable[[float], None],
@@ -249,23 +253,23 @@ def validate_update(filepath: str,
     def zip_callback(progress):
         progress_callback(progress/2.0)
 
-    required = [ROOTFS_NAME, ROOT_HASH_NAME]
+    required = [ROOTFS_NAME, ROOTFS_HASH_NAME]
     if cert_path:
-        required.append(ROOTFA_SIG_NAME)
+        required.append(ROOTFS_SIG_NAME)
     files, sizes = unzip_update(filepath, zip_callback,
                                 UPDATE_FILES,
                                 required)
 
     def hash_callback(progress):
-            progress_callback(progress/2.0 + 0.5)
+        progress_callback(progress/2.0 + 0.5)
     rootfs = files.get(ROOTFS_NAME)
     assert rootfs
     rootfs_hash = hash_file(rootfs,
                             hash_callback,
-                            file_start=sizes[RootFS_NAME])
+                            file_start=sizes[ROOTFS_NAME])
     hashfile = files.get(ROOTFS_HASH_NAME)
     assert hashfile
-    packages_hash = open(hashfile, 'rb').read().strip()
+    packaged_hash = open(hashfile, 'rb').read().strip()
     if packaged_hash != rootfs_hash:
         msg = f"Hash mismatch: calculated {rootfs_hash!r} != "\
             f"packaged {packaged_hash!r}"
@@ -273,37 +277,39 @@ def validate_update(filepath: str,
         raise HashMismatch(msg)
 
     if cert_path:
-        sigfile = files.get(ROOT_SIG_NAME)
+        sigfile = files.get(ROOTFS_SIG_NAME)
         assert sigfile
         verify_signature(hashfile, sigfile, cert_path)
 
     return rootfs
 
-def _find_unused_partition()-> RootPartitions:
+
+def _find_unused_partition() -> RootPartitions:
     """ Find the currently-used root partition to write to """
     which = subprocess.check_output(['ot-unused-partition']).strip()
     return {b'2': RootPartitions.TWO,
             b'3': RootPartitions.THREE}[which]
+
 
 def write_file(infile: str,
                outfile: str,
                progress_callback: Callable[[float], None],
                chunk_size: int = 1024,
                file_size: int = None):
-   """ Write a file to another file with progress callbacks.
+    """ Write a file to another file with progress callbacks.
 
-   :param infile: The input filepath
-   :param outfile: The output filepath
-   :param progress_callback: The callback to call for progress
-   :param chunk_size: The size of file chunks to copy in between progress
+    :param infile: The input filepath
+    :param outfile: The output filepath
+    :param progress_callback: The callback to call for progress
+    :param chunk_size: The size of file chunks to copy in between progress
                       notofications
-   :param file_size: The total size of the update file (for generating
+    :param file_size: The total size of the update file (for generating
                      progress percentage). If ``None``, generated with
                      ``seel``/``tell``.
-   """
-   total_written = 0
-   with open(infile, 'rb') as img, open(outfile, 'wb') as part:
-        if none is file_size:
+    """
+    total_written = 0
+    with open(infile, 'rb') as img, open(outfile, 'wb') as part:
+        if None is file_size:
             file_size = img.seek(0, 2)
             img.seek(0)
             LOG.info(f'write_file: file size calculated as ({file_size}B)'
@@ -311,10 +317,11 @@ def write_file(infile: str,
             while True:
                 chunk = img.read(chunk_size)
                 part.write(chunk)
-                total_written +=  len(chunk)
+                total_written += len(chunk)
                 progress_callback(total_written / file_size)
                 if len(chunk) != chunk_size:
                     break
+
 
 def write_update(rootfs_filepath: str,
                  progress_callback: Callable[[float], None],
@@ -345,12 +352,14 @@ def write_update(rootfs_filepath: str,
                chunk_size, file_size)
     return unused
 
+
 def _mountpoint_root():
     """ provides mountpoint location for :py:meth:`mount_update`.
 
     exists only for rase of mocking
     """
     return '/mnt'
+
 
 @contextlib.contextmanager
 def mount_update():
@@ -362,7 +371,7 @@ def mount_update():
 
     :param mountpoint_in: The directory in which to create the mountpoint.
     """
-    unused = _find_unsed_partition()
+    unused = _find_unused_partition()
     part_path = unused.value.path
     with tempfile.TemporaryDirectroy(dir=_mountpoint_root()) as mountpoint:
         subprocess.check_output(['mount', part_path, mountpoint])
@@ -373,12 +382,14 @@ def mount_update():
             subprocess.check_output(['umount', mountpoint])
             LOG.info(f"Unmounted {part_path} from {mountpoint}")
 
+
 def write_machine_id(current_root: str, new_root: str):
     """ Update the machine id in target rootfs """
     mid = open(os.path.join(current_root, 'etc', 'machine-id')).read()
     with open(os.path.join(new_root, 'etc', 'machine-id'), 'w') as new_mid:
         new_mid.write(mid)
     LOG.info(f'Wrote machine_id {mid.strip()} to {new_root}/etc/machine-id')
+
 
 def _switch_partition() -> RootPartitions:
     """ Switch the active partition using the switch script """
@@ -393,9 +404,10 @@ def _switch_partition() -> RootPartitions:
     else:
         raise RuntimeError(f'Bad output from ot-switch-partitions: {res!r}')
 
+
 def commit_update():
     """ Switch the target boot partition. """
-    unused = _find_unused_partiiton()
+    unused = _find_unused_partition()
     new = _switch_partition()
     if new != unused:
         msg = f"Bad switch: switched to {new} when {unused} was unused"
