e473e1352e22d35753d8d6a382731a11a00bb297
Derek Maggio <derek.maggio@opentrons.com>
2021-09-20 12:27:51 -0700

    Add tests

---
+++ hardware/tests/firmware_integration/test_echo.py
@@ -0,0 +1,21 @@
+import pytest
+import asyncio
+from typing import AsyncGenerator
+from opentrons_hardware.drivers.can_bus import CanDriver, ArbitrationId, CanMessage
+
+
+@pytest.fixture
+async def driver(
+        loop: asyncio.BaseEventLoop
+) -> AsyncGenerator[CanDriver, None]:
+    driver = await CanDriver.connect_to_emulator('vcan0')
+    yield driver
+    driver.shutdown()
+
+
+async def test_send(driver: CanDriver):
+    message = CanMessage(
+        arbitration_id=ArbitrationId(id=0x1FFFFFFF), data=bytearray([1, 2, 3, 4])
+    )
+
+    await driver.send(message)
