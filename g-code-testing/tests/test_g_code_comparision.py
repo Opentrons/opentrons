baed9fdb3a330cf74f8974a3bbf5ed2405b3a868
Derek Maggio <derek.maggio@opentrons.com>
2021-09-20 09:51:33 -0700

    Liniting

--- g-code-testing/tests/test_g_code_comparision.py
+++ g-code-testing/tests/test_g_code_comparision.py
@@ -1,17 +1,14 @@
 import pytest
-from robot_server.service.dependencies import get_motion_lock  # type: ignore
 from g_code_parsing.g_code_differ import GCodeDiffer
 from g_code_test_data.g_code_configuration import ProtocolGCodeConfirmConfig
-from g_code_test_data.protocol.protocol_configurations import (
-    PROTOCOL_CONFIGURATIONS
-)
+from g_code_test_data.protocol.protocol_configurations import PROTOCOL_CONFIGURATIONS
 from g_code_test_data.g_code_configuration import HTTPGCodeConfirmConfig
 from g_code_test_data.http.http_configurations import HTTP_CONFIGURATIONS
 
 
 @pytest.mark.parametrize(
     "g_code_configuration",
-    [conf.generate_pytest_param() for conf in HTTP_CONFIGURATIONS]
+    [conf.generate_pytest_param() for conf in HTTP_CONFIGURATIONS],
 )
 def test_http(g_code_configuration: HTTPGCodeConfirmConfig):
     print(PROTOCOL_CONFIGURATIONS)
@@ -25,7 +22,7 @@ def test_http(g_code_configuration: HTTPGCodeConfirmConfig):
 
 @pytest.mark.parametrize(
     "g_code_configuration",
-    [conf.generate_pytest_param() for conf in PROTOCOL_CONFIGURATIONS]
+    [conf.generate_pytest_param() for conf in PROTOCOL_CONFIGURATIONS],
 )
 def test_protocols(g_code_configuration: ProtocolGCodeConfirmConfig):
 
