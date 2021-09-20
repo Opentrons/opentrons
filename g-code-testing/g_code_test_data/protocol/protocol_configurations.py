5f63771be05974f059c85cc2d97ce89fc597f4ee
Derek Maggio <derek.maggio@opentrons.com>
2021-09-20 07:05:32 -0700

    Define protocol configurations

---
+++ g-code-testing/g_code_test_data/protocol/protocol_configurations.py
@@ -0,0 +1,71 @@
+from opentrons.hardware_control.emulation.settings import Settings, SmoothieSettings
+from g_code_test_data.g_code_configuration import ProtocolGCodeConfirmConfig
+import pytest
+
+###################
+# Shared Settings #
+###################
+
+SWIFT_SMOOTHIE_SETTINGS = Settings(
+    smoothie=SmoothieSettings(
+        left={"model": "p20_single_v2.0", "id": "P20SV202020070101"},
+        right={"model": "p300_multi_v2.1", "id": "P20SV202020070101"},
+    )
+)
+
+##################
+# Configurations #
+##################
+
+BASIC_SMOOTHIE = ProtocolGCodeConfirmConfig(
+    name='basic_smoothie',
+    path="protocol/protocols/smoothie_protocol.py",
+    settings=Settings(
+        smoothie=SmoothieSettings(
+            left={"model": "p20_single_v2.0", "id": "P20SV202020070101"},
+            right={"model": "p20_single_v2.0", "id": "P20SV202020070101"},
+        )
+    )
+)
+
+TWO_SINGLE_CHANNEL = ProtocolGCodeConfirmConfig(
+    name='2_single_channel',
+    path="protocol/protocols/2_single_channel_v2.py",
+    settings=Settings(
+        smoothie=SmoothieSettings(
+            left={"model": "p20_single_v2.0", "id": "P20SV202020070101"},
+            right={"model": "p300_single_v2.1", "id": "P20SV202020070101"},
+        )
+    )
+)
+
+TWO_MODULES = ProtocolGCodeConfirmConfig(
+    name='2_modules',
+    path="protocol/protocols/2_modules_1s_1m_v2.py",
+    settings=Settings(
+        smoothie=SmoothieSettings(
+            left={"model": "p300_single_v2.1", "id": "P20SV202020070101"},
+            right={"model": "p20_multi_v2.1", "id": "P20SV202020070101"},
+        )
+    )
+)
+
+SWIFT_SMOKE = ProtocolGCodeConfirmConfig(
+    name='swift_smoke',
+    path="protocol/protocols/swift_smoke.py",
+    settings=SWIFT_SMOOTHIE_SETTINGS
+)
+
+SWIFT_TURBO = ProtocolGCodeConfirmConfig(
+    name='swift_turbo',
+    path="protocol/protocols/swift_turbo.py",
+    settings=SWIFT_SMOOTHIE_SETTINGS
+)
+
+PROTOCOL_CONFIGURATIONS = [
+    BASIC_SMOOTHIE.generate_pytest_param(),
+    TWO_SINGLE_CHANNEL.generate_pytest_param(),
+    TWO_MODULES.generate_pytest_param(user_marks=[pytest.mark.slow]),
+    SWIFT_SMOKE.generate_pytest_param(user_marks=[pytest.mark.slow]),
+    SWIFT_TURBO.generate_pytest_param(user_marks=[pytest.mark.slow]),
+]
