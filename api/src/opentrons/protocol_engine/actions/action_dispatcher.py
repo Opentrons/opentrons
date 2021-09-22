8bc987f7f7eab5d36937c96a0dacfb335b88db11
Mike Cousins <mike@opentrons.com>
2021-09-22 13:51:18 -0400

    Update api/src/opentrons/protocol_engine/actions/action_dispatcher.py
    Co-authored-by: Max Marrone <max@opentrons.com>

--- api/src/opentrons/protocol_engine/actions/action_dispatcher.py
+++ api/src/opentrons/protocol_engine/actions/action_dispatcher.py
@@ -4,7 +4,7 @@ from .actions import Action
 
 
 class ActionDispatcher:
-    """A pipeline, with an endpoint, that actions cna be dispatched into."""
+    """A pipeline, with an endpoint, that actions can be dispatched into."""
 
     def __init__(self, sink: ActionHandler) -> None:
         """Intialize the ActionDispatcher and action pipeline.
