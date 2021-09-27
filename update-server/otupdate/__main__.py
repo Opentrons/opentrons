48f54bc8aab15fa9b7c83d64b0cc79b84534e1c1
sotaoverride <aatrapps@gmail.com>
2021-09-27 07:16:18 -0400

    otupdate main compatibale with all buldsystems

--- update-server/otupdate/__main__.py
+++ update-server/otupdate/__main__.py
@@ -1,3 +1,23 @@
-from otupdate.openembedded_server.__main__ import main
+import argparse
+import sys
+from otupdate.openembedded_server.__main__ import main as oe_main
+from otupdate.buildroot.__main__ import main as br_main
 
-main()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='run oe/br'
+                                                 'update server')
+    subparser = parser.add_subparsers(dest='build_system')
+    oe = subparser.add_parser('oe')
+    oe.set_defaults(func=oe_main)
+    args = parser.parse_args()
+    if args.build_system == 'oe':
+        sys.argv = [sys.argv[0]]
+        oe_main()
+    else:
+        sys.argv = [sys.argv[0]]
+        br_main()
+
+
+parse_args()
+# oe_main()
