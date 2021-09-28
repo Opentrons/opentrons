1a6859e1cc6a853ab50ebc5b13d50db3c209ed55
Seth Foster <seth@opentrons.com>
2021-09-28 15:57:26 -0400

    feat(api): Add heater-shaker driver
    Add a basic heater-shaker serial driver to the API.

--- api/tests/opentrons/drivers/test_utils.py
+++ api/tests/opentrons/drivers/test_utils.py
@@ -1,7 +1,12 @@
 from typing import Dict
 import pytest
 from opentrons.drivers import utils
-from opentrons.drivers.types import Temperature, PlateTemperature
+from opentrons.drivers.types import (
+    Temperature,
+    PlateTemperature,
+    RPM,
+    HeaterShakerPlateLockStatus,
+)
 
 
 @pytest.mark.parametrize(
@@ -49,17 +54,66 @@ def test_parse_device_information_failure(input_str: str) -> None:
 @pytest.mark.parametrize(
     argnames=["input_str", "expected_result"],
     argvalues=[
-        ["T:none C:123.4", Temperature(target=None, current=123.4)],
-        ["T:123.566 C:123.446", Temperature(target=123.57, current=123.45)],
-        ["T:-123.566 C:-123.446", Temperature(target=-123.57, current=-123.45)],
-        ["a:3 T:2. C:3 H:0 G:1", Temperature(target=2, current=3)],
+        [
+            "FW:123-2 SerialNo:serial_v HW:m",
+            {"version": "123-2", "serial": "serial_v", "model": "m"},
+        ],
+        [
+            "SerialNo:serial_v HW:m FW:123-2 ",
+            {"version": "123-2", "serial": "serial_v", "model": "m"},
+        ],
+        [
+            "   SerialNo:serial_v HW:m    FW:123-2   ",
+            {"version": "123-2", "serial": "serial_v", "model": "m"},
+        ],
+        [
+            "something:extra SerialNo:serial_v HW:m FW:123-2",
+            {"version": "123-2", "serial": "serial_v", "model": "m"},
+        ],
+    ],
+)
+def test_parse_hs_device_information_success(
+    input_str: str, expected_result: Dict[str, str]
+) -> None:
+    """Test parse device information."""
+    assert utils.parse_hs_device_information(input_str) == expected_result
+
+
+@pytest.mark.parametrize(
+    argnames=["input_str"],
+    argvalues=[
+        ["FW:123 SerialNo:serial_v"],
+        ["FW:123 SerialNog:serial_v HW:123"],
+        [""],
+    ],
+)
+def test_parse_hs_device_information_failure(input_str: str) -> None:
+    """Test parse device information."""
+    with pytest.raises(utils.ParseError):
+        utils.parse_hs_device_information(input_str)
+
+
+@pytest.mark.parametrize(
+    argnames=["input_str", "expected_result", "zero_is_none"],
+    argvalues=[
+        ["T:none C:123.4", Temperature(target=None, current=123.4), False],
+        ["T:123.566 C:123.446", Temperature(target=123.57, current=123.45), False],
+        ["T:-123.566 C:-123.446", Temperature(target=-123.57, current=-123.45), False],
+        ["a:3 T:2. C:3 H:0 G:1", Temperature(target=2, current=3), False],
+        ["T:0 C:124", Temperature(target=0, current=124), False],
+        ["T:0 C:124", Temperature(target=None, current=124), True],
     ],
 )
 def test_parse_temperature_response_success(
-    input_str: str, expected_result: Temperature
+    input_str: str, expected_result: Temperature, zero_is_none: bool
 ) -> None:
     """It should parse temperature response."""
-    assert utils.parse_temperature_response(input_str, 2) == expected_result
+    assert (
+        utils.parse_temperature_response(
+            input_str, 2, zero_target_is_unset=zero_is_none
+        )
+        == expected_result
+    )
 
 
 @pytest.mark.parametrize(
@@ -75,6 +129,64 @@ def test_parse_temperature_response_failure(input_str: str) -> None:
     """It should fail to parse temperature response."""
     with pytest.raises(utils.ParseError):
         utils.parse_temperature_response(input_str, 2)
+
+
+@pytest.mark.parametrize(
+    argnames=["input_str", "expected_result"],
+    argvalues=[
+        ["T:0 C:500", RPM(target=None, current=500)],
+        ["T:25 C:24", RPM(target=25, current=24)],
+        ["a:3 T:2. C:3 H:0 G:1", RPM(target=2, current=3)],
+    ],
+)
+def test_parse_rpm_response_success(input_str: str, expected_result: RPM) -> None:
+    """It should parse temperature response."""
+    assert utils.parse_rpm_response(input_str) == expected_result
+
+
+@pytest.mark.parametrize(
+    argnames=["input_str"],
+    argvalues=[
+        ["T:not_an_int C:123"],
+        ["T:none C:none"],
+        ["C:not_an_int T:123"],
+        [""],
+    ],
+)
+def test_parse_rpm_response_failure(input_str: str) -> None:
+    """It should fail to parse temperature response."""
+    with pytest.raises(utils.ParseError):
+        utils.parse_rpm_response(input_str)
+
+
+@pytest.mark.parametrize(
+    argnames=["input_str", "expected_result"],
+    argvalues=[
+        ["STATUS:IDLE_OPEN", HeaterShakerPlateLockStatus.IDLE_OPEN],
+        ["STATUS:IDLE_CLOSED", HeaterShakerPlateLockStatus.IDLE_CLOSED],
+        ["a:safa STATUS:IDLE_UNKNOWN", HeaterShakerPlateLockStatus.IDLE_UNKNOWN],
+    ],
+)
+def test_parse_plate_lock_status_response_success(
+    input_str: str, expected_result: HeaterShakerPlateLockStatus
+):
+    assert utils.parse_plate_lock_status_response(input_str) == expected_result
+
+
+@pytest.mark.parametrize(
+    argnames=["input_str"],
+    argvalues=[
+        [""],
+        ["SSSSSS:IDLE_OPEN"],
+        ["STATUS:Lord Humongous Sees You"],
+        ["IDLE_OPEN"],
+        ["STATUS"],
+        [":"],
+    ],
+)
+def test_parse_plate_lock_status_response_failure(input_str):
+    with pytest.raises(utils.ParseError):
+        utils.parse_plate_lock_status_response(input_str)
 
 
 @pytest.mark.parametrize(
