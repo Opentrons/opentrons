e2ef58cc23463b6d7d4f7e4210868ed2084db72b
Max Marrone <max@marrone.nyc>
2021-10-01 15:44:24 -0400

    Leave todo for fixing references to `Point.labware` and `Point.point`.

--- api/src/opentrons/types.py
+++ api/src/opentrons/types.py
@@ -78,23 +78,26 @@ class Location:
     :py:meth:`InstrumentContext.aspirate`.
 
     .. warning::
-       The :py:attr:`labware` attribute of this class is used by the protocol
+       The ``.labware`` attribute of this class is used by the protocol
        API internals to, among other things, determine safe heights to retract
        the instruments to when moving between locations. If constructing an
-       instance of this class manually, be sure to either specify `None` as the
+       instance of this class manually, be sure to either specify ``None`` as the
        labware (so the robot does its worst case retraction) or specify the
-       correct labware for the :py:attr:`point` attribute.
+       correct labware for the ``.point`` attribute.
 
 
     .. warning::
-       The `==` operation compares both the position and associated labware.
-       If you only need to compare locations, compare the :py:attr:`point`
+       The ``==`` operation compares both the position and associated labware.
+       If you only need to compare locations, compare the ``.point``
        of each item.
     """
 
     def __init__(self, point: Point, labware: LocationLabware):
         self._point = point
         self._labware = LabwareLike(labware)
+
+    # todo(mm, 2021-10-01): Figure out how to get .point and .labware to show up
+    # in the rendered docs, and then update the class docstring to use cross-references.
 
     @property
     def point(self) -> Point:
