c9eae3c45bab1988c23cb878dad0139ec1c0514c
amit lissack <amit@opentrons.com>
2021-10-07 14:49:34 -0400

    remove sample events.

--- notify-server/notify_server/models/payload_type.py
+++ notify-server/notify_server/models/payload_type.py
@@ -5,7 +5,6 @@ from typing_extensions import Literal
 from pydantic import BaseModel
 
 from notify_server.models.hardware_event import HardwareEventPayload
-from notify_server.models.sample_events import SampleOne, SampleTwo
 
 
 class UserData(BaseModel):
@@ -15,10 +14,7 @@ class UserData(BaseModel):
     data: Dict[Any, Any]
 
 
-# TODO (SPP, 2020-12-4): Remove sample events once there are > 1 real events
 PayloadType = Union[
-    SampleOne,
-    SampleTwo,
     UserData,
     HardwareEventPayload,
 ]
--- notify-server/tests/unit/models/test_event.py
+++ notify-server/tests/unit/models/test_event.py
@@ -5,9 +5,7 @@ from typing import Dict, Any
 import pytest
 
 from notify_server.models.event import Event
-from notify_server.models.sample_events import (
-    SampleOne, SampleOneData, SampleTwo
-)
+from notify_server.models.payload_type import UserData
 
 
 @pytest.mark.parametrize(argnames=["data"],
@@ -15,23 +13,13 @@ from notify_server.models.sample_events import (
                              # Empty
                              [{}],
                              # Unknown type
-                             [{"type": "SampleThree"}],
+                             [{"type": "UserDataBoo"}],
                              # Missing Data
-                             [{"type": "SampleOne"}],
+                             [{"type": "UserData"}],
                              # Wrong Types
-                             [{"type": "SampleOne",
-                               "data": {"val1": "egg", "val2": 123}}],
-                             # Missing Attributes
-                             [{"type": "SampleTwo"}],
-                             # Wrong types
-                             [{"type": "SampleTwo",
-                               "val1": "egg", "val2": 123}],
-                             # Use SampleTwo schema on sample one type
-                             [{"type": "SampleOne", "val1": 123,
-                               "val2": "egg"}],
-                             # Use SampleOne schema on sample two type
-                             [{"type": "SampleTwo",
-                               "data": {"val1": 123, "val2": "egg"}}]]
+                             [{"type": "UserData",
+                               "data": "Hello"}],
+                         ]
                          )
 def test_bad_data_attribute(data: Dict[str, Any]) -> None:
     """Test that invalid data attribute will cause a validation error."""
@@ -47,12 +35,8 @@ def test_bad_data_attribute(data: Dict[str, Any]) -> None:
 @pytest.mark.parametrize(
     argnames=["data", "expected"],
     argvalues=[
-        [{"type": "SampleOne", "data": {"val1": 123, "val2": "egg"}},
-         SampleOne(data=SampleOneData(val1=123, val2="egg"))
-         ],
-        [{"type": "SampleTwo", "val1": 123, "val2": "egg"},
-         SampleTwo(val1=123, val2="egg")
-         ],
+        [{"type": "UserData", "data": {"val1": 123, "val2": "egg"}},
+         UserData(data={"val1": 123, "val2": "egg"})],
     ])
 def test_good_data(data: Dict[str, Any], expected: Event) -> None:
     """Test that the data member is validated correctly."""
