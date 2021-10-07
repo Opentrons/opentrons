16a75819668c9923383c434ad38a6c2a5309792c
amit lissack <amit@opentrons.com>
2021-10-07 14:34:33 -0400

    add UserData payload type.

--- notify-server/notify_server/models/payload_type.py
+++ notify-server/notify_server/models/payload_type.py
@@ -1,13 +1,24 @@
 """The definition of payload types."""
-from typing import Union
+from typing import Union, Dict, Any
+from typing_extensions import Literal
+
+from pydantic import BaseModel
 
 from notify_server.models.hardware_event import HardwareEventPayload
 from notify_server.models.sample_events import SampleOne, SampleTwo
+
+
+class UserData(BaseModel):
+    """Flexible user defined data."""
+
+    type: Literal["UserData"] = "UserData"
+    data: Dict[Any, Any]
 
 
 # TODO (SPP, 2020-12-4): Remove sample events once there are > 1 real events
 PayloadType = Union[
     SampleOne,
     SampleTwo,
+    UserData,
     HardwareEventPayload,
 ]
