f14693d649497ae325fc738a4f99738628202a9b
Sakib Hossain <sakibh97@gmail.com>
2021-10-11 16:26:42 -0400

    Moved cancel run button from old Run page to preProtocolFlowWithoutRPC Run page

--- app/src/organisms/RunDetails/index.tsx
+++ app/src/organisms/RunDetails/index.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Flex, Text, DIRECTION_COLUMN } from '@opentrons/components'
+import {
+  Flex,
+  PrimaryBtn,
+  Text,
+  BORDER_WIDTH_DEFAULT,
+  C_BLUE,
+  C_WHITE,
+  DIRECTION_COLUMN,
+  FONT_WEIGHT_SEMIBOLD,
+  LINE_HEIGHT_SOLID,
+  SPACING_2,
+  SPACING_3,
+} from '@opentrons/components'
 import { Page } from '../../atoms/Page'
 import { useProtocolDetails } from './hooks'
 
@@ -8,9 +20,25 @@ export function RunDetails(): JSX.Element | null {
   const { t } = useTranslation('run_details')
   const { displayName, protocolData } = useProtocolDetails()
   if (protocolData == null) return null
+  const cancelRunButton = (
+    <PrimaryBtn
+      onClick={() => console.log('Cancel Run')}
+      backgroundColor={C_WHITE}
+      color={C_BLUE}
+      borderWidth={BORDER_WIDTH_DEFAULT}
+      lineHeight={LINE_HEIGHT_SOLID}
+      fontWeight={FONT_WEIGHT_SEMIBOLD}
+      marginX={SPACING_3}
+      paddingRight={SPACING_2}
+      paddingLeft={SPACING_2}
+    >
+      Cancel Run
+    </PrimaryBtn>
+  )
 
   const titleBarProps = {
     title: t('protocol_title', { protocol_name: displayName }),
+    rightNode: cancelRunButton,
   }
 
   return (
--- app/src/pages/Run/index.tsx
+++ app/src/pages/Run/index.tsx
@@ -4,39 +4,9 @@ import { SessionHeader } from '../../organisms/SessionHeader'
 import { RunDetails } from '../../organisms/RunDetails'
 import { useFeatureFlag } from '../../redux/config'
 import { RunLog } from './RunLog'
-import {
-  BORDER_WIDTH_DEFAULT,
-  C_BLUE,
-  C_WHITE,
-  Flex,
-  FONT_WEIGHT_SEMIBOLD,
-  JUSTIFY_FLEX_END,
-  LINE_HEIGHT_SOLID,
-  PrimaryBtn,
-  SPACING_1,
-  SPACING_4,
-  SPACING_AUTO,
-} from '@opentrons/components'
 
 export function Run(): JSX.Element {
   const isNewProtocolRunPage = useFeatureFlag('preProtocolFlowWithoutRPC')
-  const cancelRunButton = (
-    <Flex justifyContent={JUSTIFY_FLEX_END} marginLeft={SPACING_AUTO}>
-      <PrimaryBtn
-        onClick={() => console.log('Cancel Run')}
-        backgroundColor={C_WHITE}
-        color={C_BLUE}
-        borderWidth={BORDER_WIDTH_DEFAULT}
-        lineHeight={LINE_HEIGHT_SOLID}
-        fontWeight={FONT_WEIGHT_SEMIBOLD}
-        marginX={SPACING_4}
-        paddingRight={SPACING_1}
-        paddingLeft={SPACING_1}
-      >
-        Cancel Run
-      </PrimaryBtn>
-    </Flex>
-  )
 
   return isNewProtocolRunPage ? (
     <RunDetails />
@@ -44,7 +14,6 @@ export function Run(): JSX.Element {
     <Page
       titleBarProps={{
         title: <SessionHeader />,
-        rightNode: cancelRunButton,
       }}
     >
       <RunLog />
