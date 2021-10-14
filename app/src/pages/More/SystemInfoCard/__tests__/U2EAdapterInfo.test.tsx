6858089eeea199a8b479fe1f33a77f2a7dcafabf
Brent Hagen <brent.timothy.hagen@gmail.com>
2021-10-14 16:57:27 -0400

    refactor(app): remove USB-to-Ethernet adapter serial number and IP info (#8518)
    * refactor(app): remove USB-to-Ethernet adapter serial number and IP info (#8438)
    
    closes #8438

--- app/src/pages/More/SystemInfoCard/__tests__/U2EAdapterInfo.test.tsx
+++ app/src/pages/More/SystemInfoCard/__tests__/U2EAdapterInfo.test.tsx
@@ -1,109 +1,86 @@
 import * as React from 'react'
-import { Provider } from 'react-redux'
-import { mount } from 'enzyme'
-import noop from 'lodash/noop'
+import { when, resetAllWhenMocks } from 'jest-when'
+import { mountWithProviders } from '@opentrons/components'
 
+import { i18n } from '../../../../i18n'
 import * as Fixtures from '../../../../redux/system-info/__fixtures__'
 import * as SystemInfo from '../../../../redux/system-info'
 import { U2EAdapterInfo } from '../U2EAdapterInfo'
 import { U2EDriverWarning } from '../U2EDriverWarning'
 
-import type { State } from '../../../../redux/types'
+import type { State, Action } from '../../../../redux/types'
 
 jest.mock('../../../../redux/system-info/selectors')
 jest.mock('../../../../redux/analytics')
 
 const MOCK_STATE: State = { mockState: true } as any
 
-const MOCK_STORE = {
-  getState: () => MOCK_STATE,
-  dispatch: noop,
-  subscribe: noop,
-}
-
 const getU2EAdapterDevice = SystemInfo.getU2EAdapterDevice as jest.MockedFunction<
   typeof SystemInfo.getU2EAdapterDevice
->
-
-const getU2EInterfacesMap = SystemInfo.getU2EInterfacesMap as jest.MockedFunction<
-  typeof SystemInfo.getU2EInterfacesMap
 >
 
 const getU2EWindowsDriverStatus = SystemInfo.getU2EWindowsDriverStatus as jest.MockedFunction<
   typeof SystemInfo.getU2EWindowsDriverStatus
 >
 
-function stubSelector<R>(
-  mock: jest.MockedFunction<(s: State) => R>,
-  rVal: R
-): void {
-  mock.mockImplementation(state => {
-    expect(state).toBe(MOCK_STATE)
-    return rVal
-  })
-}
-
 describe('U2EAdapterInfo', () => {
-  const render = (): ReturnType<typeof mount> => {
-    return mount(<U2EAdapterInfo />, {
-      wrappingComponent: Provider,
-      wrappingComponentProps: { store: MOCK_STORE },
+  const render = () => {
+    return mountWithProviders<
+      React.ComponentProps<typeof U2EAdapterInfo>,
+      State,
+      Action
+    >(<U2EAdapterInfo />, {
+      initialState: MOCK_STATE,
+      i18n,
     })
   }
 
   beforeEach(() => {
-    stubSelector(getU2EAdapterDevice, null)
-    stubSelector(getU2EWindowsDriverStatus, SystemInfo.NOT_APPLICABLE)
-    stubSelector(getU2EInterfacesMap, {})
+    when(getU2EAdapterDevice).calledWith(MOCK_STATE).mockReturnValue(null)
+
+    when(getU2EWindowsDriverStatus)
+      .calledWith(MOCK_STATE)
+      .mockReturnValue(SystemInfo.NOT_APPLICABLE)
   })
 
   afterEach(() => {
-    jest.resetAllMocks()
+    resetAllWhenMocks()
   })
 
   it('should render a title', () => {
-    const wrapper = render()
-    const title = wrapper.find('h3')
+    const { wrapper } = render()
+    const children = wrapper.children().html()
 
-    expect(title.html()).toMatch(/USB-to-Ethernet Adapter Information/)
-  })
-
-  it('should render a description of the information', () => {
-    const wrapper = render()
-
-    expect(wrapper.children().html()).toMatch(
-      /The OT-2 uses a USB-to-Ethernet adapter for its wired connection/
-    )
+    expect(children).toContain('USB-to-Ethernet Adapter Information')
   })
 
   it('should display a "not found" message if no device is plugged in', () => {
-    const wrapper = render()
+    const { wrapper } = render()
 
     expect(wrapper.children().html()).toMatch(
-      /no OT-2 USB-to-Ethernet adapter/i
+      /No OT-2 USB-to-Ethernet adapter found/i
     )
   })
 
   it('should display device information if present', () => {
     const device = Fixtures.mockRealtekDevice
 
-    stubSelector(getU2EAdapterDevice, device)
+    when(getU2EAdapterDevice).calledWith(MOCK_STATE).mockReturnValue(device)
 
-    const wrapper = render()
+    const { wrapper } = render()
     const children = wrapper.children().html()
 
     expect(getU2EAdapterDevice).toHaveBeenCalledWith(MOCK_STATE)
     expect(children).toContain(device.deviceName)
     expect(children).toContain(device.manufacturer)
-    expect(children).toContain(device.serialNumber)
   })
 
   it('should display Windows driver version if available', () => {
     const device = Fixtures.mockWindowsRealtekDevice
 
-    stubSelector(getU2EAdapterDevice, device)
+    when(getU2EAdapterDevice).calledWith(MOCK_STATE).mockReturnValue(device)
 
-    const wrapper = render()
+    const { wrapper } = render()
     const children = wrapper.children().html()
 
     expect(getU2EAdapterDevice).toHaveBeenCalledWith(MOCK_STATE)
@@ -116,38 +93,40 @@ describe('U2EAdapterInfo', () => {
       windowsDriverVersion: null,
     }
 
-    stubSelector(getU2EAdapterDevice, device)
+    when(getU2EAdapterDevice).calledWith(MOCK_STATE).mockReturnValue(device)
 
-    const wrapper = render()
+    const { wrapper } = render()
     const children = wrapper.children().html()
 
     expect(getU2EAdapterDevice).toHaveBeenCalledWith(MOCK_STATE)
     expect(children).toContain('unknown')
   })
 
-  it('should show an U2EDriverWarning if driver status is OUTDATED', () => {
-    stubSelector(getU2EWindowsDriverStatus, SystemInfo.OUTDATED)
+  it('should NOT show a U2EDriverWarning if driver status is UP_TO_DATE', () => {
+    const device = Fixtures.mockWindowsRealtekDevice
 
-    const wrapper = render()
-    expect(wrapper.exists(U2EDriverWarning)).toBe(true)
+    when(getU2EAdapterDevice).calledWith(MOCK_STATE).mockReturnValue(device)
+
+    when(getU2EWindowsDriverStatus)
+      .calledWith(MOCK_STATE)
+      .mockReturnValue(SystemInfo.UP_TO_DATE)
+
+    const { wrapper } = render()
+
+    expect(wrapper.exists(U2EDriverWarning)).toBe(false)
   })
 
-  it('should display device network adapter information if present', () => {
-    const device = Fixtures.mockRealtekDevice
-    const iface4 = Fixtures.mockNetworkInterface
-    const iface6 = Fixtures.mockNetworkInterfaceV6
+  it('should show a U2EDriverWarning if driver status is OUTDATED', () => {
+    const device = Fixtures.mockWindowsRealtekDevice
 
-    stubSelector(getU2EAdapterDevice, device)
-    stubSelector(getU2EInterfacesMap, {
-      [device.serialNumber]: [iface4, iface6],
-    })
+    when(getU2EAdapterDevice).calledWith(MOCK_STATE).mockReturnValue(device)
 
-    const wrapper = render()
-    const children = wrapper.children().html()
+    when(getU2EWindowsDriverStatus)
+      .calledWith(MOCK_STATE)
+      .mockReturnValue(SystemInfo.OUTDATED)
 
-    expect(getU2EAdapterDevice).toHaveBeenCalledWith(MOCK_STATE)
-    expect(children).toContain(iface4.name)
-    expect(children).toContain(iface4.address)
-    expect(children).toContain(iface6.address)
+    const { wrapper } = render()
+
+    expect(wrapper.exists(U2EDriverWarning)).toBe(true)
   })
 })
