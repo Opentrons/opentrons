a851dfe3e0eed1978dd676186ecbc241768d0bca
Laura Cox <lauracox@Lauras-MacBook-Pro.local>
2021-10-15 11:06:51 -0400

    feat(can): add messages for enabling and disabling motors

--- hardware/opentrons_hardware/drivers/can_bus/messages/message_definitions.py
+++ hardware/opentrons_hardware/drivers/can_bus/messages/message_definitions.py
@@ -46,6 +46,18 @@ class GetStatusRequest:  # noqa: D101
 
 
 @dataclass
+class EnableMotorRequest:  # noqa: D101
+    message_id: Literal[MessageId.enable_motor_request] = MessageId.get_status_request
+    payload_type: Type[BinarySerializable] = payloads.EmptyMessage
+
+
+@dataclass
+class DisableMotorRequest:  # noqa: D101
+    message_id: Literal[MessageId.disable_motor_request] = MessageId.get_status_request
+    payload_type: Type[BinarySerializable] = payloads.EmptyMessage
+
+
+@dataclass
 class GetStatusResponse:  # noqa: D101
     message_id: Literal[MessageId.get_status_response] = MessageId.get_status_response
     payload_type: Type[BinarySerializable] = payloads.EmptyMessage
