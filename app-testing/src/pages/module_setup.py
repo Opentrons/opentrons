83d5f6e9722a8453f773aff80b6c39b3c22853cc
neha ojha <nehaojha@nehas-MacBook-Pro.local>
2021-10-20 16:12:50 -0400

    modifying module_setup.py

--- app-testing/src/pages/module_setup.py
+++ app-testing/src/pages/module_setup.py
@@ -80,3 +80,6 @@ class ModuleSetup:
 
     def click_proceed_to_labware_setup(self) -> None:
         self.get_proceed_to_labware_setup().click()
+
+    def click_proceed_to_module_setup(self) -> None:
+        self.get_proceed_to_module_setup.click()
