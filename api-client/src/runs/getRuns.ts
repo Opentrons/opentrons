66753ffa433df47b2a1fc85e56b3d2ffe553a5cf
Brian Cooper <brian@opentrons.com>
2021-10-25 14:46:24 -0400

    refactor(api-client): split out legacy sessions resource from new runs resource
    Ahead of more additions to the js-api-client, split out the legacy sessions resource utilities from
    the new runs resource utilities.

---
+++ api-client/src/runs/getRuns.ts
@@ -0,0 +1,12 @@
+import { GET, request } from '../request'
+
+import type { ResponsePromise } from '../request'
+import type { HostConfig } from '../types'
+import type { Runs, RunType } from './types'
+
+export function getRuns(
+  config: HostConfig,
+  params?: { session_type: RunType }
+): ResponsePromise<Runs> {
+  return request<Runs>(GET, `/runs`, null, config, params)
+}
---
+++ api-client/src/runs/types.ts
@@ -0,0 +1,35 @@
+export const RUN_TYPE_BASIC: 'basic' = 'basic'
+export const RUN_TYPE_PROTOCOL: 'protocol' = 'protocol'
+
+export type RunType = typeof RUN_TYPE_BASIC | typeof RUN_TYPE_BASIC
+
+interface BasicRun {
+  id: string
+  sessionType: typeof RUN_TYPE_BASIC
+  createParams: Record<string, unknown>
+}
+
+interface ProtocolRun {
+  id: string
+  sessionType: typeof RUN_TYPE_PROTOCOL
+  createParams: Record<string, unknown>
+}
+
+export type RunData = BasicRun | ProtocolRun
+
+interface ResourceLink {
+  href: string
+  meta?: Partial<{ [key: string]: string | null | undefined }>
+}
+
+type ResourceLinks = Record<string, ResourceLink | string | null | undefined>
+
+export interface Run {
+  data: RunData
+  links?: ResourceLinks
+}
+
+export interface Runs {
+  data: RunData[]
+  links?: ResourceLinks
+}
