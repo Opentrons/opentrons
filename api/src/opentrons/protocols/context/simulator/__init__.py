efe6842c6987431a3ff7888e006d64b538e00ebf
Max Marrone <max@opentrons.com>
2021-10-27 12:59:52 -0400

    refactor(protocol-engine): Use "fast simulation" for protocols uploaded over HTTP (#8557)

--- api/src/opentrons/protocols/context/simulator/__init__.py
+++ api/src/opentrons/protocols/context/simulator/__init__.py
@@ -1,1 +1,9 @@
-"""Context simulation."""
+"""Context simulation.
+
+One way of simulating a Protocol API v2 protocol (e.g. to detect equipment requirements)
+is to simulate at the level of the hardware API. However, this is slow.
+
+To speed things up, these classes can additionally take the place of a
+Protocol API v2 context's implementation, to simulate certain things at the level of
+the Protocol API, avoiding certain calls to the underlying hardware API.
+"""
