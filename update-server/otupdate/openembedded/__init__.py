b74798e5e52a8ee14adf347006ca195699033aa4
Seth Foster <seth@opentrons.com>
2021-10-28 10:18:12 -0400

    feat(update-server): add openembedded implementation
    The update server's openembedded (OT3) implementation looks strikingly
    similar to the buildroot implementation, but relies on a command line
    utility called bmap-tools to actually flash images to the hardware.
    Bmap-tools is here: https://github.com/intel/bmap-tools . For all that
    it looks exactly like a Python library, it is a command line tool that
    happens to be written in python and doesn't work as a library, and is
    used as such - it's not a python dep and can't be.

---
+++ update-server/otupdate/openembedded/__init__.py
@@ -0,0 +1,101 @@
+""" update-server implementation for openembedded systems """
+import asyncio
+import logging
+import json
+from aiohttp import web
+from typing import Optional, Mapping
+
+from otupdate.common import (
+    config,
+    constants,
+    control,
+    ssh_key_management,
+    name_management,
+    update,
+)
+from . import update_actions
+
+LOG = logging.getLogger(__name__)
+
+
+@web.middleware
+async def log_error_middleware(request, handler):
+    try:
+        resp = await handler(request)
+    except Exception:
+        LOG.exception(f"Exception serving {request.method} {request.path}")
+        raise
+    return resp
+
+
+def get_version_dict(version_file: Optional[str]) -> Mapping[str, str]:
+    version = {}
+    if version_file:
+        try:
+            version = json.load(open(version_file))
+        except Exception:
+            logging.exception("Could not load version, using defaults")
+    return version
+
+
+def get_app(
+    system_version_file: str = None,
+    config_file_override: str = None,
+    name_override: str = None,
+    boot_id_override: str = None,
+    loop: asyncio.AbstractEventLoop = None,
+) -> web.Application:
+    """Build and return the aiohttp.web.Application that runs the server"""
+
+    version = get_version_dict(system_version_file)
+
+    if not loop:
+        loop = asyncio.get_event_loop()
+
+    config_obj = config.load(config_file_override)
+
+    app = web.Application(middlewares=[log_error_middleware])
+    name = name_override or name_management.get_name()
+    boot_id = boot_id_override or control.get_boot_id()
+    app[config.CONFIG_VARNAME] = config_obj
+    app[constants.RESTART_LOCK_NAME] = asyncio.Lock()
+    app[constants.DEVICE_BOOT_ID_NAME] = boot_id
+    app[constants.DEVICE_NAME_VARNAME] = name
+    update_actions.OT3UpdateActions.build_and_insert(app)
+
+    LOG.info(
+        "Setup: "
+        + "\n\t".join(
+            [
+                f"Device name: {name}",
+                "Buildroot version:         "
+                f'{version.get("buildroot_version", "unknown")}',
+                "\t(from git sha      " f'{version.get("buildroot_sha", "unknown")}',
+                "API version:               "
+                f'{version.get("opentrons_api_version", "unknown")}',
+                "\t(from git sha      "
+                f'{version.get("opentrons_api_sha", "unknown")}',
+                "Update server version:     "
+                f'{version.get("update_server_version", "unknown")}',
+                "\t(from git sha      "
+                f'{version.get("update_server_sha", "unknown")}',
+            ]
+        )
+    )
+    app.router.add_routes(
+        [
+            web.post("/server/update/begin", update.begin),
+            web.post("/server/update/cancel", update.cancel),
+            web.get("/server/update/{session}/status", update.status),
+            web.post("/server/update/{session}/file", update.file_upload),
+            web.post("/server/update/{session}/commit", update.commit),
+            web.post("/server/restart", control.restart),
+            web.get("/server/ssh_keys", ssh_key_management.list_keys),
+            web.post("/server/ssh_keys", ssh_key_management.add),
+            web.delete("/server/ssh_keys", ssh_key_management.clear),
+            web.delete("/server/ssh_keys/{key_md5}", ssh_key_management.remove),
+            web.post("/server/name", name_management.set_name_endpoint),
+            web.get("/server/name", name_management.get_name_endpoint),
+        ]
+    )
+    return app
