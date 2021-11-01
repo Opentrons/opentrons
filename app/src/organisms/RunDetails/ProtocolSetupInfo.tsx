e0f7c9aec335aa1e8198079b86f831bf2fb4a35a
Jethary Rader <jethary@opentrons.com>
2021-11-01 17:02:26 -0400

    refactor protocolSetupinfo to include commands

--- app/src/organisms/RunDetails/ProtocolSetupInfo.tsx
+++ app/src/organisms/RunDetails/ProtocolSetupInfo.tsx
@@ -16,34 +16,86 @@ import {
   TEXT_TRANSFORM_CAPITALIZE,
   TEXT_TRANSFORM_UPPERCASE,
 } from '@opentrons/components'
-import { getModuleDisplayName } from '@opentrons/shared-data'
-
-import { useProtocolDetails } from './hooks'
-import { State } from '../../redux/types'
-import { useSelector } from 'react-redux'
-import { getConnectedRobot } from '../../redux/discovery'
-import { getProtocolPipetteTipRackCalInfo } from '../../redux/pipettes'
+import type { SetupCommand } from '@opentrons/shared-data/protocol/types/schemaV6/command/setup'
 
 interface ProtocolSetupInfoProps {
   onCloseClick: () => unknown
+  //SetupCommand: SetupCommand
 }
 
 export const ProtocolSetupInfo = (
   props: ProtocolSetupInfoProps
 ): JSX.Element | null => {
+  const { SetupCommand } = props
+  const SetupCommand = {
+    commandType: 'loadModule',
+    params: {
+      moduleId: 'temperature_module_gen2',
+      location: { slotName: '9', coordinates: { x: 0, y: 0, z: 0 } },
+    },
+    result: { moduleId: 'thermocycler_gen1' },
+  }
   const { t } = useTranslation('run_details')
-  const { protocolData } = useProtocolDetails()
-  if (protocolData === null) return null
-  const pipetteInfo = Object.values(protocolData.pipettes)
-  const moduleInfo = Object.values(protocolData.modules)
-  const labwareInfo = Object.values(protocolData.labwareDefinitions)
-  const labwareSlot = Object.values(protocolData.labware)
-  const robot = useSelector((state: State) => getConnectedRobot(state))
-  if (robot === null) return null
-  const protocolPipetteData = useSelector((state: State) =>
-    getProtocolPipetteTipRackCalInfo(state, robot.name)
-  )
 
+  let SetupCommandText
+  // if (SetupCommand.commandType === 'loadPipette') {
+  //   SetupCommandText = (
+  //     <Trans
+  //       t={t}
+  //       id={`RunDetails_PipetteSetup`}
+  //       i18nKey={'load_pipette_protocol_setup'}
+  //       values={{
+  //         pipette_name: SetupCommand.result?.pipetteId,
+  //         mount_name: SetupCommand.params.mount,
+  //       }}
+  //       components={{
+  //         span: (
+  //           <Text
+  //             textTransform={TEXT_TRANSFORM_CAPITALIZE}
+  //             marginLeft={SPACING_1}
+  //             marginRight={SPACING_1}
+  //           />
+  //         ),
+  //       }}
+  //     />
+  //   )
+  // } 
+ if (SetupCommand.commandType === 'loadModule') {
+    const moduleSlotNumber = SetupCommand.result?.moduleId.includes(
+      'thermocycler'
+    )
+      ? 4
+      : 1
+    SetupCommandText = (
+      <Trans
+        t={t}
+        id={`RunDetails_ModuleSetup`}
+        i18nKey={'load_modules_protocol_setup'}
+        count={moduleSlotNumber}
+        values={{
+          module: SetupCommand.result?.moduleId,
+          slot_name: Object.values(SetupCommand.params.location)[0],
+        }}
+      />
+    )
+  } 
+  // else if (SetupCommand.commandType === 'loadLabware') {
+  //   SetupCommand.result?.definition.metadata.displayName.includes('trash')
+  //     ? (SetupCommandText = null)
+  //     : (SetupCommandText = (
+  //         <Trans
+  //           t={t}
+  //           id={`RunDetails_LabwareSetup`}
+  //           i18nKey={'load_labware_info_protocol_setup'}
+  //           values={{
+  //             labware_loadname:
+  //               SetupCommand.result?.definition.metadata.displayName,
+  //             labware_version: SetupCommand.result?.definition.version,
+  //             slot_number: Object.values(SetupCommand.params.location)[0],
+  //           }}
+  //         />
+  //       ))
+  // }
   return (
     <Flex
       flexDirection={DIRECTION_COLUMN}
@@ -64,105 +116,7 @@ export const ProtocolSetupInfo = (
           <Icon name="chevron-up" color={C_MED_DARK_GRAY}></Icon>
         </Btn>
       </Flex>
-      {pipetteInfo.map(({ mount, name }) => {
-        const pipetteData = protocolPipetteData[mount]
-        if (pipetteData == null) {
-          return null
-        } else {
-          return (
-            <Flex
-              flexDirection={DIRECTION_ROW}
-              marginTop={SPACING_1}
-              marginBottom={SPACING_1}
-            >
-              <Trans
-                t={t}
-                key={mount}
-                id={`RunDetails_PipetteSetup`}
-                i18nKey={'load_pipette_protocol_setup'}
-                values={{
-                  pipette_name: pipetteData.pipetteDisplayName,
-                  mount_name: mount,
-                }}
-                components={{
-                  span: (
-                    <Text
-                      textTransform={TEXT_TRANSFORM_CAPITALIZE}
-                      marginLeft={SPACING_1}
-                      marginRight={SPACING_1}
-                    />
-                  ),
-                }}
-              />
-            </Flex>
-          )
-        }
-      })}
-
-      {moduleInfo != null &&
-        moduleInfo.map(({ slot, model }, index) => {
-          const slotNumber = slot === 'span7_8_10_11' ? 4 : 1
-          return (
-            <Flex
-              flexDirection={DIRECTION_ROW}
-              marginTop={SPACING_1}
-              marginBottom={SPACING_1}
-            >
-              <Trans
-                t={t}
-                key={index}
-                id={`RunDetails_ModuleSetup`}
-                i18nKey={'load_modules_protocol_setup'}
-                count={slotNumber}
-                values={{
-                  module: getModuleDisplayName(model),
-                  slot_name: slot,
-                }}
-              />
-            </Flex>
-          )
-        })}
-
-      {labwareInfo != null &&
-        labwareInfo.map(({ metadata, version }, index) => {
-          const displayName = metadata.displayName
-          {
-            if (displayName.includes('Opentrons Fixed Trash')) {
-              return null
-            } else {
-              return (
-                <Flex
-                  flexDirection={DIRECTION_ROW}
-                  marginTop={SPACING_1}
-                  marginBottom={SPACING_1}
-                >
-                  <Trans
-                    t={t}
-                    key={index}
-                    id={`RunDetails_LabwareSetup`}
-                    i18nKey={'load_labware_info_protocol_setup'}
-                    values={{
-                      labware_loadname: displayName,
-                      labware_version: version,
-                    }}
-                  />
-                </Flex>
-              )
-            }
-          }
-          // })
-        })}
-
-      {/* <Flex>
-        {labwareSlot != null &&
-          labwareSlot.map(({ slot }, index) => (
-            <Text key={index}>
-              {t('labware_slot_protocol_setup', {
-                slot_number: slot,
-              })}
-            </Text>
-          ))}
-      </Flex> */}
+      <Flex flexDirection={DIRECTION_ROW}>{SetupCommandText}</Flex>
     </Flex>
   )
 }
