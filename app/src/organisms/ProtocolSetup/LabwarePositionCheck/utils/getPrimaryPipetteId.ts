731cba2004d7b821a31cfeff9b336c1f532ea631
Shlok Amin <shlokamin@me.com>
2021-11-02 20:02:33 -0400

    fix getPrimaryPipetteId

--- app/src/organisms/ProtocolSetup/LabwarePositionCheck/utils/getPrimaryPipetteId.ts
+++ app/src/organisms/ProtocolSetup/LabwarePositionCheck/utils/getPrimaryPipetteId.ts
@@ -13,11 +13,11 @@ export const getPrimaryPipetteId = (
   const leftPipetteId = commands.find(
     (command: Command): command is LoadPipetteCommand =>
       command.commandType === 'loadPipette' && command.params.mount === 'left'
-  )?.id
+  )?.params.pipetteId
   const rightPipetteId = commands.find(
     (command: Command): command is LoadPipetteCommand =>
       command.commandType === 'loadPipette' && command.params.mount === 'right'
-  )?.id
+  )?.params.pipetteId
 
   if (leftPipetteId == null || rightPipetteId == null) {
     throw new Error(
