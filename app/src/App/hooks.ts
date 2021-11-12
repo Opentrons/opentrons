c29b2736aa064b62f4cfec0a7b4515277e715ca0
Brian Cooper <brian@opentrons.com>
2021-11-11 20:29:37 -0500

    feat(app): run tab disabled reason hook up to run presence

---
+++ app/src/App/hooks.ts
@@ -0,0 +1,54 @@
+import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import {
+  getNavbarLocations,
+  getConnectedRobotPipettesMatch,
+  getConnectedRobotPipettesCalibrated,
+  getDeckCalibrationOk,
+} from '../redux/nav'
+import { useFeatureFlag } from '../redux/config'
+import { getConnectedRobot } from '../redux/discovery'
+import { useCurrentProtocolRun } from '../organisms/ProtocolUpload/hooks'
+import { NavLocation } from '../redux/nav/types'
+
+function useRunLocation(): NavLocation {
+  const { t } = useTranslation('top_navigation')
+  const robot = useSelector(getConnectedRobot)
+  const pipettesMatch = useSelector(getConnectedRobotPipettesMatch)
+  const pipettesCalibrated = useSelector(getConnectedRobotPipettesCalibrated)
+  const deckCalOk = useSelector(getDeckCalibrationOk)
+
+  const { protocolRecord, runRecord } = useCurrentProtocolRun()
+
+  let disabledReason = null
+  if (!robot) disabledReason = t('please_connect_to_a_robot')
+  else if (protocolRecord == null || runRecord == null)
+    disabledReason = t('please_load_a_protocol')
+  else if (!pipettesMatch) disabledReason = t('attached_pipettes_do_not_match')
+  else if (!pipettesCalibrated) disabledReason = t('pipettes_not_calibrated')
+  else if (!deckCalOk) disabledReason = t('pipettes_not_calibrated')
+
+  return {
+    id: 'run',
+    path: '/run',
+    title: t('run'),
+    iconName: 'ot-run',
+    disabledReason,
+  }
+}
+
+export function useNavLocations() {
+  const isPreProtocolFlowWithoutRPC = useFeatureFlag(
+    'preProtocolFlowWithoutRPC'
+  )
+  const [robots, upload, calibrate, legacyRun, more] = useSelector(
+    getNavbarLocations
+  )
+
+  const runLocation = useRunLocation()
+
+  const legacyLocations = [robots, upload, calibrate, legacyRun, more]
+  const navLocations = [robots, upload, runLocation, more]
+
+  return isPreProtocolFlowWithoutRPC ? navLocations : legacyLocations
+}
