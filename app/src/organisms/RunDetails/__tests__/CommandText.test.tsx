7baf3a8a147ad2085fab3d4fdfa530f983cdc955
Brian Cooper <brian@opentrons.com>
2021-11-16 15:37:02 -0500

    update command text and timer tests

--- app/src/organisms/RunDetails/__tests__/CommandText.test.tsx
+++ app/src/organisms/RunDetails/__tests__/CommandText.test.tsx
@@ -1,7 +1,31 @@
 import * as React from 'react'
+import { when } from 'jest-when'
 import { renderWithProviders } from '@opentrons/components'
 import { i18n } from '../../../i18n'
 import { CommandText } from '../CommandText'
+import { useProtocolDetails } from '../hooks'
+import { useLabwareRenderInfoById } from '../../ProtocolSetup/hooks'
+import { getLabwareLocation } from '../../ProtocolSetup/utils/getLabwareLocation'
+import { getLabwareDisplayName } from '@opentrons/shared-data'
+import type { Command } from '@opentrons/shared-data/protocol/types/schemaV6/command'
+
+jest.mock('../hooks')
+jest.mock('../../ProtocolSetup/hooks')
+jest.mock('../../ProtocolSetup/utils/getLabwareLocation')
+jest.mock('@opentrons/shared-data/js/helpers')
+
+const mockUseProtocolDetails =  useProtocolDetails as jest.MockedFunction<
+  typeof useProtocolDetails
+>
+const mockUseLabwareRenderInfoById =  useLabwareRenderInfoById as jest.MockedFunction<
+  typeof useLabwareRenderInfoById
+>
+const mockGetLabwareDisplayName =  getLabwareDisplayName as jest.MockedFunction<
+  typeof getLabwareDisplayName
+>
+const mockGetLabwareLocation =  getLabwareLocation as jest.MockedFunction<
+  typeof getLabwareLocation
+>
 
 const render = (props: React.ComponentProps<typeof CommandText>) => {
   return renderWithProviders(<CommandText {...props} />, {
@@ -9,18 +33,57 @@ const render = (props: React.ComponentProps<typeof CommandText>) => {
   })[0]
 }
 
-const COMMAND_TEXT = ('COMMAND_TEXT' as unknown) as JSX.Element
+const MOCK_COMMAND_DETAILS = {
+  id: '123',
+  commandType: 'custom',
+  params:  {},
+  status: 'running',
+  result: {},
+  startedAt: 'start timestamp',
+  completedAt: 'end timestamp',
+} as Command
 
 describe('CommandText', () => {
-  let props: React.ComponentProps<typeof CommandText>
-
   beforeEach(() => {
-    props = {
-      commandText: COMMAND_TEXT,
-    }
+    mockUseProtocolDetails.mockReturnValue({protocolData: {} } as any)
+    mockUseLabwareRenderInfoById.mockReturnValue({} as any)
   })
-  it('renders correct command text', () => {
-    const { getByText } = render(props)
-    getByText('COMMAND_TEXT')
+  it('renders correct command text for custom legacy commands', () => {
+    const { getByText } = render({
+      commandOrSummary: {
+        ...MOCK_COMMAND_DETAILS,
+        params: {
+          legacyCommandText: 'legacy command text'
+        }
+      } as Command
+    })
+    getByText('legacy command text')
+  })
+
+  it('renders correct command text for pick up tip', () => {
+    const labwareId = 'labwareId'
+    const wellName = 'wellName'
+    when(mockGetLabwareDisplayName)
+      .calledWith('fake_def' as any)
+      .mockReturnValue('fake_display_name')
+    when(mockGetLabwareLocation)
+      .calledWith(labwareId, [])
+      .mockReturnValue('fake_labware_location')
+    mockUseLabwareRenderInfoById.mockReturnValue({
+      labwareId: {
+        labwareDef: 'fake_def'
+      }
+    } as any)
+    const { getByText } = render({
+      commandOrSummary: {
+        ...MOCK_COMMAND_DETAILS,
+        commandType: 'pickUpTip',
+        params: {
+          wellName,
+          labwareId,
+        }
+      } as Command
+    })
+    getByText('Picking up tip from wellName of fake_display_name in fake_labware_location')
   })
 })
--- app/src/organisms/RunDetails/__tests__/CommandTimer.test.tsx
+++ app/src/organisms/RunDetails/__tests__/CommandTimer.test.tsx
@@ -2,6 +2,16 @@ import * as React from 'react'
 import { renderWithProviders } from '@opentrons/components'
 import { i18n } from '../../../i18n'
 import { CommandTimer } from '../CommandTimer'
+import { useFormatRunTimestamp, useTimeElapsedSincePause } from '../hooks'
+
+jest.mock('../hooks')
+
+const mockUseFormatRunTimestamp =  useFormatRunTimestamp as jest.MockedFunction<
+  typeof useFormatRunTimestamp
+>
+const mockUseTimeElapsedSincePause =  useTimeElapsedSincePause as jest.MockedFunction<
+  typeof useTimeElapsedSincePause
+>
 
 const render = (props: React.ComponentProps<typeof CommandTimer>) => {
   return renderWithProviders(<CommandTimer {...props} />, {
@@ -9,27 +19,29 @@ const render = (props: React.ComponentProps<typeof CommandTimer>) => {
   })[0]
 }
 
+const MOCK_ELAPSED_TIME = '10 minutes'
+
 describe('CommandTimer', () => {
   let props: React.ComponentProps<typeof CommandTimer>
 
   beforeEach(() => {
-    props = {
-      start: '0',
-      timer: '30',
-      end: '1',
-      runStatus: 'paused',
-    }
+    mockUseFormatRunTimestamp.mockReturnValue((timestamp) => timestamp)
+    mockUseTimeElapsedSincePause.mockReturnValue(MOCK_ELAPSED_TIME)
   })
   it('renders correct time when runStatus is paused', () => {
+    props = {
+      commandStartedAt: '0',
+      commandCompletedAt: undefined,
+    }
     const { getByText } = render(props)
     getByText('0')
-    getByText('30')
-    getByText('1')
+    getByText('10 minutes')
+    getByText('-- : -- : --')
   })
   it('renders correct time when runStatus is not paused', () => {
     props = {
-      start: '5',
-      end: '10',
+      commandStartedAt: '5',
+      commandCompletedAt: '10',
     }
     const { getByText } = render(props)
     getByText('5')
