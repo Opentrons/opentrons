6f7acf673b29190dc3699d13d541aa0845dcc7e3
Sakib Hossain <sakibh97@gmail.com>
2021-11-18 15:24:15 -0500

    fix test coverage for cancel run

--- app/src/organisms/RunDetails/index.tsx
+++ app/src/organisms/RunDetails/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
+import { RUN_STATUS_RUNNING } from '@opentrons/api-client'
 import {
   PrimaryBtn,
   BORDER_WIDTH_DEFAULT,
@@ -52,7 +53,7 @@ export function RunDetails(): JSX.Element | null {
   )
 
   const titleBarProps =
-    runStatus === 'running'
+    runStatus === RUN_STATUS_RUNNING
       ? {
           title: t('protocol_title', { protocol_name: displayName }),
           rightNode: cancelRunButton,
