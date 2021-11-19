fe0124b990c005ebcb7af366c91e1b92d100b8c8
amit lissack <amit@opentrons.com>
2021-11-19 09:54:12 -0500

    black.

--- api/src/opentrons/__init__.py
+++ api/src/opentrons/__init__.py
@@ -101,7 +101,7 @@ def should_use_ot3() -> bool:
     """Return true if ot3 hardware controller should be used."""
     if enable_ot3_hardware_controller():
         try:
-            from opentrons_hardware.drivers.can_bus import CanDriver   # noqa: F401
+            from opentrons_hardware.drivers.can_bus import CanDriver  # noqa: F401
 
             return True
         except ModuleNotFoundError:
