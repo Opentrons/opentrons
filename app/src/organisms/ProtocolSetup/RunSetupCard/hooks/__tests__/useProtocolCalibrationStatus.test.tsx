775862813bfea66ebeb59784bb53e6263880e4f5
smb2268 <sarah.breen@opentrons.com>
2021-11-19 14:25:24 -0500

    Updaated test

--- app/src/organisms/ProtocolSetup/RunSetupCard/hooks/__tests__/useProtocolCalibrationStatus.test.tsx
+++ app/src/organisms/ProtocolSetup/RunSetupCard/hooks/__tests__/useProtocolCalibrationStatus.test.tsx
@@ -1,27 +1,175 @@
 import * as React from 'react'
+import { renderHook } from '@testing-library/react-hooks'
 import { when, resetAllWhenMocks } from 'jest-when'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import * as discoverySelectors from '../../../../../redux/discovery/selectors'
-import { useModuleRenderInfoById } from '../../../hooks'
-import { getAttachedModules } from '../../../../../redux/modules'
-import { mockConnectedRobot } from '../../../../../redux/discovery/__fixtures__'
-import { renderHook } from '@testing-library/react-hooks'
-import { useMissingModuleIds } from '..'
+import { getConnectedRobotName } from '../../../../../redux/robot/selectors'
+import { getDeckCalibrationStatus } from '../../../../../redux/calibration/selectors'
+import { mockTipRackDefinition } from '../../../../../redux/custom-labware/__fixtures__'
+import { useCurrentRunPipetteInfoByMount } from '../useCurrentRunPipetteInfoByMount'
+import { useProtocolCalibrationStatus } from '../useProtocolCalibrationStatus'
 import type { Store } from 'redux'
 import type { State } from '../../../../../redux/types'
-import type { ModuleModel, ModuleType } from '@opentrons/shared-data'
 
-jest.mock('../../../hooks')
-jest.mock('../../../../../redux/modules')
-jest.mock('../../../../../redux/discovery/selectors')
+jest.mock('../../../../../redux/robot/selectors')
+jest.mock('../../../../../redux/calibration/selectors')
+jest.mock('../useCurrentRunPipetteInfoByMount')
 
-const mockUseModuleRenderInfoById = useModuleRenderInfoById as jest.MockedFunction<
-  typeof useModuleRenderInfoById
+const mockGetConnectedRobotName = getConnectedRobotName as jest.MockedFunction<
+  typeof getConnectedRobotName
 >
-const mockGetConnectedRobot = discoverySelectors.getConnectedRobot as jest.MockedFunction<
-  typeof discoverySelectors.getConnectedRobot
+const mockGetDeckCalibrationStatus = getDeckCalibrationStatus as jest.MockedFunction<
+  typeof getDeckCalibrationStatus
 >
-const mockGetAttachedModules = getAttachedModules as jest.MockedFunction<
-  typeof getAttachedModules
+const mockUseCurrentRunPipetteInfoByMount = useCurrentRunPipetteInfoByMount as jest.MockedFunction<
+  typeof useCurrentRunPipetteInfoByMount
 >
+
+describe('useProtocolCalibrationStatus hook', () => {
+  const store: Store<State> = createStore(jest.fn(), {})
+
+  beforeEach(() => {
+    store.dispatch = jest.fn()
+
+    when(mockGetConnectedRobotName)
+      .calledWith(undefined as any)
+      .mockReturnValue('robot name')
+
+    when(mockGetDeckCalibrationStatus)
+      .calledWith(undefined as any, 'robot name')
+      .mockReturnValue('OK')
+
+    when(mockUseCurrentRunPipetteInfoByMount).calledWith().mockReturnValue({
+      left: null,
+      right: null,
+    })
+  })
+
+  afterEach(() => {
+    resetAllWhenMocks()
+    jest.restoreAllMocks()
+  })
+  it('should return deck cal failure if not calibrated', () => {
+    const wrapper: React.FunctionComponent<{}> = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+    mockGetDeckCalibrationStatus.mockReturnValue('BAD_CALIBRATION')
+    const { result } = renderHook(useProtocolCalibrationStatus, { wrapper })
+    expect(result.current).toMatchObject({
+      complete: false,
+      reason: 'calibrate_deck_failure_reason',
+    })
+  })
+  it('should return attach pipette if missing', () => {
+    const wrapper: React.FunctionComponent<{}> = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+    mockUseCurrentRunPipetteInfoByMount.mockReturnValue({
+      left: {
+        requestedPipetteMatch: 'incompatible',
+        pipetteCalDate: null,
+        // @ts-expect-error partial type match
+        pipetteSpecs: {
+          displayName: 'pipette 1',
+        },
+        tipRacksForPipette: [
+          {
+            displayName: 'Mock TipRack Definition',
+            lastModifiedDate: '',
+            tipRackDef: mockTipRackDefinition,
+          },
+        ],
+      },
+      right: null,
+    })
+    const { result } = renderHook(useProtocolCalibrationStatus, { wrapper })
+    expect(result.current).toMatchObject({
+      complete: false,
+      reason: 'attach_pipette_failure_reason',
+    })
+  })
+  it('should return calibrate pipette if cal date null', () => {
+    const wrapper: React.FunctionComponent<{}> = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+    mockUseCurrentRunPipetteInfoByMount.mockReturnValue({
+      left: {
+        requestedPipetteMatch: 'match',
+        pipetteCalDate: null,
+        // @ts-expect-error partial type match
+        pipetteSpecs: {
+          displayName: 'pipette 1',
+        },
+        tipRacksForPipette: [
+          {
+            displayName: 'Mock TipRack Definition',
+            lastModifiedDate: '',
+            tipRackDef: mockTipRackDefinition,
+          },
+        ],
+      },
+      right: null,
+    })
+    const { result } = renderHook(useProtocolCalibrationStatus, { wrapper })
+    expect(result.current).toMatchObject({
+      complete: false,
+      reason: 'calibrate_pipette_failure_reason',
+    })
+  })
+  it('should return calibrate tip rack if cal date null', () => {
+    const wrapper: React.FunctionComponent<{}> = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+    mockUseCurrentRunPipetteInfoByMount.mockReturnValue({
+      left: {
+        requestedPipetteMatch: 'match',
+        pipetteCalDate: '2020-08-30T10:02',
+        // @ts-expect-error partial type match
+        pipetteSpecs: {
+          displayName: 'pipette 1',
+        },
+        tipRacksForPipette: [
+          {
+            displayName: 'Mock TipRack Definition',
+            // @ts-expect-error incorrect type
+            lastModifiedDate: null,
+            tipRackDef: mockTipRackDefinition,
+          },
+        ],
+      },
+      right: null,
+    })
+    const { result } = renderHook(useProtocolCalibrationStatus, { wrapper })
+    expect(result.current).toMatchObject({
+      complete: false,
+      reason: 'calibrate_tiprack_failure_reason',
+    })
+  })
+  it('should return complete if everything is calibrated', () => {
+    const wrapper: React.FunctionComponent<{}> = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+    mockUseCurrentRunPipetteInfoByMount.mockReturnValue({
+      left: {
+        requestedPipetteMatch: 'match',
+        pipetteCalDate: '2020-08-30T10:02',
+        // @ts-expect-error partial type match
+        pipetteSpecs: {
+          displayName: 'pipette 1',
+        },
+        tipRacksForPipette: [
+          {
+            displayName: 'Mock TipRack Definition',
+            lastModifiedDate: '2020-08-30T10:02',
+            tipRackDef: mockTipRackDefinition,
+          },
+        ],
+      },
+      right: null,
+    })
+    const { result } = renderHook(useProtocolCalibrationStatus, { wrapper })
+    expect(result.current).toMatchObject({
+      complete: true,
+    })
+  })
+})
