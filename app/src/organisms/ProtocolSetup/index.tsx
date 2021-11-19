03a61aaddb0b44d2fadc1ba3f26a4eb9242b1ce0
Sarah Breen <sarah.breen@opentrons.com>
2021-11-19 18:12:45 -0500

    feat(app): add feedback link to protocol upload page (#8854)
    close #8727

--- app/src/organisms/ProtocolSetup/index.tsx
+++ app/src/organisms/ProtocolSetup/index.tsx
@@ -1,14 +1,39 @@
 import * as React from 'react'
-
-import { DIRECTION_COLUMN, SPACING_3, Flex } from '@opentrons/components'
+import { useTranslation } from 'react-i18next'
+import {
+  DIRECTION_COLUMN,
+  SPACING_3,
+  ALIGN_CENTER,
+  FONT_SIZE_BODY_2,
+  C_DARK_GRAY,
+  Flex,
+  Text,
+  Link,
+} from '@opentrons/components'
 import { RunSetupCard } from './RunSetupCard'
 import { MetadataCard } from './MetadataCard'
 
+const feedbackFormLink =
+  'https://docs.google.com/forms/d/e/1FAIpQLSd6oSV82IfgzSi5t_FP6n_pB_Y8wPGmAgFHsiiFho9qhxr-UQ/viewform'
+
 export function ProtocolSetup(): JSX.Element {
+  const { t } = useTranslation('protocol_setup')
   return (
-    <Flex flexDirection={DIRECTION_COLUMN} padding={SPACING_3}>
+    <Flex
+      flexDirection={DIRECTION_COLUMN}
+      padding={SPACING_3}
+      alignItems={ALIGN_CENTER}
+    >
       <MetadataCard />
       <RunSetupCard />
+      <Text
+        fontSize={FONT_SIZE_BODY_2}
+        paddingTop={SPACING_3}
+        color={C_DARK_GRAY}
+      >
+        {t('protocol_upload_revamp_feedback')}
+        <Link href={feedbackFormLink}> {t('feedback_form_link')}</Link>
+      </Text>
     </Flex>
   )
 }
