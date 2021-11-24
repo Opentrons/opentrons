a2c9d4ffd1b61f887ab4aafcddc9c3863237ef96
Shlok Amin <shlokamin@me.com>
2021-11-24 11:05:16 -0800

    do not count when trash or identity offset

--- app/src/organisms/ProtocolSetup/LabwarePositionCheck/utils/__tests__/getLatestLabwareOffsetCount.test.ts
+++ app/src/organisms/ProtocolSetup/LabwarePositionCheck/utils/__tests__/getLatestLabwareOffsetCount.test.ts
@@ -22,7 +22,7 @@ describe('getLatestLabwareOffsetCount', () => {
       {
         definitionUri: 'some_definitionUri',
         location: { slotName: '1' },
-        vector: { x: 0, y: 0, z: 0 },
+        vector: { x: 1, y: 0, z: 0 },
       },
       {
         definitionUri: 'another_definitionUri',
@@ -32,12 +32,12 @@ describe('getLatestLabwareOffsetCount', () => {
     ]
     expect(getLatestLabwareOffsetCount(labwareOffsets)).toBe(2)
   })
-  it('should return 1 when there is two offset records with the same labware and slot', () => {
+  it('should return 1 when there are two offset records with the same labware and slot', () => {
     const labwareOffsets: LabwareOffset[] = [
       {
         definitionUri: 'some_definitionUri',
         location: { slotName: '1' },
-        vector: { x: 0, y: 0, z: 0 },
+        vector: { x: 1, y: 0, z: 0 },
       },
       {
         definitionUri: 'some_definitionUri',
@@ -47,7 +47,22 @@ describe('getLatestLabwareOffsetCount', () => {
     ]
     expect(getLatestLabwareOffsetCount(labwareOffsets)).toBe(1)
   })
-  it('should return 1 when there is two offset records with the same labware and module id', () => {
+  it('should return 1 when there are two offset records with the same labware and module id', () => {
+    const labwareOffsets: LabwareOffset[] = [
+      {
+        definitionUri: 'some_definitionUri',
+        location: { moduleId: 'some_module' },
+        vector: { x: 1, y: 0, z: 0 },
+      },
+      {
+        definitionUri: 'some_definitionUri',
+        location: { moduleId: 'some_module' },
+        vector: { x: 1, y: 2, z: 3 },
+      },
+    ]
+    expect(getLatestLabwareOffsetCount(labwareOffsets)).toBe(1)
+  })
+  it('should return 0 when all offsets are identity offsets', () => {
     const labwareOffsets: LabwareOffset[] = [
       {
         definitionUri: 'some_definitionUri',
@@ -57,7 +72,22 @@ describe('getLatestLabwareOffsetCount', () => {
       {
         definitionUri: 'some_definitionUri',
         location: { moduleId: 'some_module' },
-        vector: { x: 1, y: 2, z: 3 },
+        vector: { x: 0, y: 0, z: 0 },
+      },
+    ]
+    expect(getLatestLabwareOffsetCount(labwareOffsets)).toBe(0)
+  })
+  it('should not count offset to the trash', () => {
+    const labwareOffsets: LabwareOffset[] = [
+      {
+        definitionUri: 'fixedTrash',
+        location: { slotName: '12' },
+        vector: { x: 0, y: 0, z: 0 },
+      },
+      {
+        definitionUri: 'some_definitionUri',
+        location: { moduleId: 'some_module' },
+        vector: { x: 1, y: 0, z: 0 },
       },
     ]
     expect(getLatestLabwareOffsetCount(labwareOffsets)).toBe(1)
--- app/src/organisms/ProtocolSetup/LabwarePositionCheck/utils/getLatestLabwareOffsetCount.ts
+++ app/src/organisms/ProtocolSetup/LabwarePositionCheck/utils/getLatestLabwareOffsetCount.ts
@@ -1,4 +1,8 @@
+import isEqual from 'lodash/isEqual'
 import type { LabwareOffset } from '@opentrons/api-client'
+
+const IDENTITY_OFFSET = { x: 0, y: 0, z: 0 }
+const TRASH_LOCATION = { slotName: '12' }
 
 export const getLatestLabwareOffsetCount = (
   labwareOffsets: LabwareOffset[]
@@ -15,10 +19,15 @@ export const getLatestLabwareOffsetCount = (
   labwareOffsets.forEach(offset => {
     const location = offset.location
     const definitionUri = offset.definitionUri
+    const vector = offset.vector
     const locationKey =
       'slotName' in location ? location.slotName : location.moduleId
     const id = `${definitionUri}_${locationKey}`
-    if (!uniqueLabwareInstances.has(id)) {
+    if (
+      !uniqueLabwareInstances.has(id) &&
+      !isEqual(vector, IDENTITY_OFFSET) &&
+      !isEqual(location, TRASH_LOCATION)
+    ) {
       count = count + 1
     }
     uniqueLabwareInstances.add(id)
