794b981f5ce8210892d5fff0dc4de3fbba1065cf
smb2268 <sarah.breen@opentrons.com>
2021-11-29 17:55:29 -0500

    Fix logic for clearing offsets

--- app/src/organisms/ProtocolSetup/LabwarePositionCheck/utils/getLatestLabwareOffsetCount.ts
+++ app/src/organisms/ProtocolSetup/LabwarePositionCheck/utils/getLatestLabwareOffsetCount.ts
@@ -1,7 +1,7 @@
 import isEqual from 'lodash/isEqual'
+import uniqBy from 'lodash/uniqBy'
 import { IDENTITY_VECTOR } from '@opentrons/shared-data'
 import type { LabwareOffset } from '@opentrons/api-client'
-
 const TRASH_LOCATION = { slotName: '12' }
 
 // this util counts all "relevant" labware offsets that a user would care about
@@ -11,25 +11,35 @@ const TRASH_LOCATION = { slotName: '12' }
 export const getLatestLabwareOffsetCount = (
   labwareOffsets: LabwareOffset[]
 ): number => {
-  const uniqueLabwareInstances = labwareOffsets.reduce<Set<string>>(
-    (uniqueLabwareInstances, offset) => {
+  // labware offsets are in order of creation, we want to use the most recent ones
+  const orderedLabwareOffsets = labwareOffsets
+    .slice()
+    .sort((a, b) => a.date - b.date)
+
+  const filteredLabwareOffsets = uniqBy(
+    orderedLabwareOffsets,
+    (offset: LabwareOffset) => {
       const location = offset.location
       const definitionUri = offset.definitionUri
-      const vector = offset.vector
       const locationKey =
         'slotName' in location ? location.slotName : location.moduleId
       // key by definition uri AND location, because labware offsets are tied to a combo of both of these attributes
-      const id = `${definitionUri}_${locationKey}`
-      if (
-        !uniqueLabwareInstances.has(id) &&
-        !isEqual(location, TRASH_LOCATION) &&
-        !isEqual(vector, IDENTITY_VECTOR)
-      ) {
-        return uniqueLabwareInstances.add(id)
-      }
-      return uniqueLabwareInstances
-    },
-    new Set()
+      return `${definitionUri}_${locationKey}`
+    }
   )
-  return uniqueLabwareInstances.size
+
+  const uniqueOffsetCount = filteredLabwareOffsets.reduce<
+    LabwareOffset[],
+    number
+  >((acc: number, offset: LabwareOffset) => {
+    const vector = offset.vector
+    if (
+      !isEqual(location, TRASH_LOCATION) &&
+      !isEqual(vector, IDENTITY_VECTOR)
+    ) {
+      return acc + 1
+    }
+    return acc
+  }, 0)
+  return uniqueOffsetCount
 }
