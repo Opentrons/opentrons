6f0904316ce005dfae592ac521d7eaad17a67617
Brian Arthur Cooper <brian@opentrons.com>
2021-11-30 16:41:58 -0500

    fix(app): continue to clone run even after receiving a conflict when run is stopped (#8933)

--- app/src/organisms/ProtocolUpload/hooks/__tests__/useCloneRun.test.tsx
+++ app/src/organisms/ProtocolUpload/hooks/__tests__/useCloneRun.test.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import {
   useRunQuery,
   useHost,
-  useStopRunMutation,
   useCreateRunMutation,
 } from '@opentrons/react-api-client'
 import { useCloneRun } from '../useCloneRun'
@@ -15,9 +14,6 @@ import type { HostConfig } from '@opentrons/api-client'
 jest.mock('@opentrons/react-api-client')
 
 const mockUseHost = useHost as jest.MockedFunction<typeof useHost>
-const mockUseStopRunMutation = useStopRunMutation as jest.MockedFunction<
-  typeof useStopRunMutation
->
 const mockUseRunQuery = useRunQuery as jest.MockedFunction<typeof useRunQuery>
 const mockUseCreateRunMutation = useCreateRunMutation as jest.MockedFunction<
   typeof useCreateRunMutation
@@ -33,7 +29,15 @@ describe('useCloneRun hook', () => {
     when(mockUseHost).calledWith().mockReturnValue(HOST_CONFIG)
     when(mockUseRunQuery)
       .calledWith(RUN_ID)
-      .mockReturnValue({ data: { data: { id: RUN_ID } } } as any)
+      .mockReturnValue({
+        data: {
+          data: {
+            id: RUN_ID,
+            protocolId: 'protocolId',
+            labwareOffsets: 'someOffset',
+          },
+        },
+      } as any)
     when(mockUseCreateRunMutation)
       .calledWith(expect.anything())
       .mockReturnValue({ createRun: jest.fn() } as any)
@@ -49,11 +53,16 @@ describe('useCloneRun hook', () => {
   })
 
   it('should return a function that when called, calls stop run with the run id', async () => {
-    const mockStopRun = jest.fn()
-    mockUseStopRunMutation.mockReturnValue({ stopRun: mockStopRun } as any)
+    const mockCreateRun = jest.fn()
+    mockUseCreateRunMutation.mockReturnValue({
+      createRun: mockCreateRun,
+    } as any)
 
     const { result } = renderHook(() => useCloneRun(RUN_ID), { wrapper })
     result.current && result.current()
-    expect(mockStopRun).toHaveBeenCalledWith(RUN_ID)
+    expect(mockCreateRun).toHaveBeenCalledWith({
+      protocolId: 'protocolId',
+      labwareOffsets: 'someOffset',
+    })
   })
 })
