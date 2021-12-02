902346555b5c26155aed3c882fd6ea8803440c0e
Jethary Rader <jethary@opentrons.com>
2021-12-02 10:25:40 -0500

    feat(app): add Labware Offset Link in LPC
    closes #8963

--- app/src/organisms/ProtocolSetup/RunSetupCard/LabwareSetup/LabwareOffsetModal.tsx
+++ app/src/organisms/ProtocolSetup/RunSetupCard/LabwareSetup/LabwareOffsetModal.tsx
@@ -27,8 +27,8 @@ import styles from '../../styles.css'
 
 const ROBOT_CAL_HELP_ARTICLE =
   'https://support.opentrons.com/en/articles/3499692-how-positional-calibration-works-on-the-ot-2'
-
-const OFFSET_DATA_HELP_ARTICLE = '#' //  TODO IMMEDIATELY: REPLACE WITH ACTUAL LINK
+const OFFSET_DATA_HELP_ARTICLE = 
+  'http://support.opentrons.com/en/articles/5742955-how-labware-offsets-work-on-the-ot-2'
 interface LabwareOffsetModalProps {
   onCloseClick: () => unknown
 }
