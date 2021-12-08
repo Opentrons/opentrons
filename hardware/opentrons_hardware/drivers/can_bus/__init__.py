7cbcbec2bc02b0d257e68e830be8b3a00f15cc0b
Seth Foster <seth@opentrons.com>
2021-12-08 16:05:28 -0500

    refactor(hardware): constants from firmware  (#9016)
    Pipenv can add git repo dependencies, so we can add the
    opentrons_ot3_firmware repo here.
    
    If you want to switch branches you can edit the pipfile (there's not
    really a better way to do this).
    
    This will become a problem if we ever want to depend on this module and
    get its transitive dependencies because that stuff only reads setup.py,
    but we don't really do that yet.

--- hardware/opentrons_hardware/drivers/can_bus/__init__.py
+++ hardware/opentrons_hardware/drivers/can_bus/__init__.py
@@ -1,13 +1,13 @@
 """Can bus drivers package."""
 
 from .driver import CanDriver
-from .message import CanMessage
-from .arbitration_id import (
+from .can_messenger import CanMessenger
+from opentrons_ot3_firmware.message import CanMessage
+from opentrons_ot3_firmware.arbitration_id import (
     ArbitrationId,
     ArbitrationIdParts,
 )
-from .constants import NodeId, FunctionCode, MessageId
-from .can_messenger import CanMessenger
+from opentrons_ot3_firmware.constants import NodeId, FunctionCode, MessageId
 
 
 __all__ = [
