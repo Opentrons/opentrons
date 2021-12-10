e73ede392353bdc7907d177494d486a2dcad8853
Seth Foster <seth@opentrons.com>
2021-12-10 14:07:25 -0500

    remove accidentally committed enum

--- shared-data/python/opentrons_shared_data/deck/__init__.py
+++ shared-data/python/opentrons_shared_data/deck/__init__.py
@@ -2,15 +2,9 @@
 opentrons_shared_data.deck: types and bindings for deck definitions
 """
 from typing import overload, TYPE_CHECKING
-import enum
 import json
 
 from .. import load_shared_data
-
-class RobotModel(enum.Enum):
-    OT2_STANDARD: str = "OT-2 Standard"
-    OT3_STANDARD: str = "OT-3 Standard"
-
 
 if TYPE_CHECKING:
     from .dev_types import (DeckSchema, DeckDefinition,
