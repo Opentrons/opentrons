c30dbfe4d8ed87dcde1fc5418762917741c5ae6a
amit lissack <amit@opentrons.com>
2022-01-10 14:00:12 -0500

    device info tests.

--- hardware/tests/firmware_integration/conftest.py
+++ hardware/tests/firmware_integration/conftest.py
@@ -1,10 +1,15 @@
-"""Test Echo firmware."""
+"""Common fixtures for integration tests."""
 import pytest
 import asyncio
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Iterator
+
+from _pytest.fixtures import FixtureRequest
+
 from opentrons_hardware.drivers.can_bus.settings import DriverSettings
 from opentrons_hardware.drivers.can_bus.build import build_driver
 from opentrons_hardware.drivers.can_bus.abstract_driver import AbstractCanDriver
+from opentrons_hardware.drivers.can_bus import CanMessenger
+from opentrons_ot3_firmware import constants
 
 
 @pytest.fixture
@@ -16,3 +21,25 @@ async def driver(
     driver = await build_driver(settings)
     yield driver
     driver.shutdown()
+
+
+@pytest.fixture
+def can_messenger(
+    driver: AbstractCanDriver,
+) -> CanMessenger:
+    """Create Can messenger."""
+    return CanMessenger(driver)
+
+
+@pytest.fixture(
+    scope="session",
+    params=[
+        constants.NodeId.head,
+        constants.NodeId.pipette,
+        constants.NodeId.gantry_x,
+        constants.NodeId.gantry_y,
+    ],
+)
+def subsystem_node_id(request: FixtureRequest) -> Iterator[constants.NodeId]:
+    """Each subsystem's node id as a fixture"""
+    yield request.param  # type: ignore[attr-defined]
