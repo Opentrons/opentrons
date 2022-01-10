4bbb919196f64746aeb0b3dd65cdbd84e1bf52dc
amit lissack <amit@opentrons.com>
2022-01-10 12:56:14 -0500

    add test-with-opentrons-sock-emulator to run tests against local running emulator using opentrons_sock interface.

---
+++ hardware/tests/firmware_integration/conftest.py
@@ -0,0 +1,18 @@
+"""Test Echo firmware."""
+import pytest
+import asyncio
+from typing import AsyncGenerator
+from opentrons_hardware.drivers.can_bus.settings import DriverSettings
+from opentrons_hardware.drivers.can_bus.build import build_driver
+from opentrons_hardware.drivers.can_bus.abstract_driver import AbstractCanDriver
+
+
+@pytest.fixture
+async def driver(
+    loop: asyncio.BaseEventLoop,
+) -> AsyncGenerator[AbstractCanDriver, None]:
+    """Create CanDriver connected to OT-3 Emulator."""
+    settings = DriverSettings()
+    driver = await build_driver(settings)
+    yield driver
+    driver.shutdown()
