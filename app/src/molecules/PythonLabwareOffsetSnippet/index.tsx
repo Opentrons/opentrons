c96d02776fc40a6bd5dfaa77590a886b2e80de2a
Brian Cooper <brian@opentrons.com>
2022-01-25 18:57:44 -0500

    format

--- app/src/molecules/PythonLabwareOffsetSnippet/index.tsx
+++ app/src/molecules/PythonLabwareOffsetSnippet/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import {
-  FONT_SIZE_CAPTION,
-  SPACING_2,
-} from '@opentrons/components'
+import { FONT_SIZE_CAPTION, SPACING_2 } from '@opentrons/components'
 import { createSnippet } from './createSnippet'
 import type { ProtocolFile } from '@opentrons/shared-data'
 import type { RunData } from '@opentrons/api-client'
