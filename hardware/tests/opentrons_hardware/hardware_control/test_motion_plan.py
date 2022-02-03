5ca9fdb7c1cc29071d390a831907f66722fccb86
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2022-02-03 14:47:58 -0500

    fix test

--- hardware/tests/opentrons_hardware/hardware_control/test_motion_plan.py
+++ hardware/tests/opentrons_hardware/hardware_control/test_motion_plan.py
@@ -17,8 +17,8 @@ from opentrons_hardware.hardware_control.motion_planning.types import (
 def generate_axis_constraint(draw: st.DrawFn) -> AxisConstraints:
     """Create axis constraint using Hypothesis."""
     acc = draw(st.integers(min_value=500, max_value=5000))
-    speed_dist = draw(st.integers(min_value=100, max_value=500))
-    dir_change_dist = draw(st.integers(min_value=10, max_value=100))
+    speed_dist = draw(st.integers(min_value=10, max_value=50))
+    dir_change_dist = draw(st.integers(min_value=5, max_value=10))
     assume(speed_dist > dir_change_dist)
     return AxisConstraints.build(
         max_acceleration=acc,
