657df06a813359d1a4ed519057c687f68b8559b0
Katie Adee <kaa328@nyu.edu>
2022-02-07 18:37:43 -0500

    feat(components): Add design system constants to components library (#9398)
    closes #9389

---
+++ components/src/ui-style-constants/colors.ts
@@ -0,0 +1,43 @@
+// colors fundamentals
+export const background = '#f8f8f8'
+export const white = '#ffffff'
+export const lightBlue = '#f1f8ff'
+export const medGrey = '#e3e3e3'
+
+// colors with states
+export const blueFocus = '#deecff'
+export const blue = '#006fff'
+export const blueHover = '#0061e0'
+export const bluePressed = '#0050b8'
+export const disabled = '#a0a0a0'
+
+export const darkBlack = '#16212D'
+export const darkBlackHover = '#283d52'
+export const darkBlackPressed = '#16212D'
+// note: darkBlackDisabled = greyDisabled
+
+export const darkGrey = '#8a8c8e'
+export const darkGreyHover = '#787a7d'
+export const darkGreyPressed = '#646668'
+// note: darkGreyDisabled = greyDisabled
+
+export const greyHover = '#acacaf'
+export const greyPressed = '#8d8d91'
+export const greyDisabled = '#eaeaeb'
+
+export const yellow = '#f2b53c'
+export const yellowHover = '#eca20f'
+export const yellowPressed = '#eca20f'
+
+export const error = '#bf0000'
+export const errorHover = '#a30000'
+export const errorText = '#850000'
+export const errorBg = '#fff3f3'
+
+export const success = '#04aa65'
+export const successText = '#00854d'
+export const successBg = '#f3fffa'
+
+export const warning = '#f09d20'
+export const warningText = '#7b5b09'
+export const warningBg = '#fffcf5'
