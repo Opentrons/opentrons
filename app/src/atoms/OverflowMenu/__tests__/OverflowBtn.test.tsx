c39303180c19dcbc066e024a6d4a140cb2800e8e
Sakib Hossain <sakib.hossain@opentrons.com>
2022-02-08 17:20:22 -0500

    add test for OverflowBtn

---
+++ app/src/atoms/OverflowMenu/__tests__/OverflowBtn.test.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react'
+import { COLORS, renderWithProviders } from '@opentrons/components'
+import 'jest-styled-components'
+import { OverflowBtn } from '../OverflowBtn'
+import { fireEvent } from '@testing-library/react'
+
+const render = (props: React.ComponentProps<typeof OverflowBtn>) => {
+  return renderWithProviders(<OverflowBtn {...props} />)[0]
+}
+
+describe('OverflowBtn', () => {
+  it('renders a clickable button', () => {
+    const handleClick = jest.fn()
+    const { getByRole } = render({
+      onClick: handleClick,
+    })
+
+    const button = getByRole('button')
+    fireEvent.click(button)
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+
+  it('renders a hover state', () => {
+    const { getByRole } = render({
+      onClick: jest.fn(),
+    })
+
+    expect(getByRole('button')).toHaveStyleRule(
+      'background-color',
+      COLORS.medGrey,
+      {
+        modifier: ':hover',
+      }
+    )
+  })
+
+  it('renders an active state', () => {
+    const { getByRole } = render({
+      onClick: jest.fn(),
+    })
+
+    expect(getByRole('button')).toHaveStyleRule(
+      'background-color',
+      COLORS.lightBlue,
+      {
+        modifier: ':active',
+      }
+    )
+  })
+
+  it('renders a focus state', () => {
+    const { getByRole } = render({
+      onClick: jest.fn(),
+    })
+
+    expect(getByRole('button')).toHaveStyleRule('border', '3px solid #deecff', {
+      modifier: ':focus',
+    })
+  })
+
+  it('renders a disabled state', () => {
+    const { getByRole } = render({
+      onClick: jest.fn(),
+    })
+
+    expect(getByRole('button')).toHaveStyleRule('fill-opacity', '0.5', {
+      modifier: ':disabled',
+    })
+  })
+})
