eea04cf39224ee06e0e4854dfb11b63ebf4c0c11
Jethary Rader <jethary@opentrons.com>
2022-02-09 13:46:13 -0500

    create slideout test

--- app/src/organisms/Devices/ModuleCard/TemperatureModuleSlideout.tsx
+++ app/src/organisms/Devices/ModuleCard/TemperatureModuleSlideout.tsx
@@ -4,49 +4,56 @@ import {
   Flex,
   SPACING_1,
   Text,
-  DIRECTION_ROW,
-  DIRECTION_COLUMN,
-  FONT_WEIGHT_SEMIBOLD,
-  SPACING_3,
   FONT_WEIGHT_REGULAR,
-  JUSTIFY_SPACE_BETWEEN,
-  SPACING_2,
-  TEXT_TRANSFORM_UPPERCASE,
-  C_BRIGHT_GRAY,
+  TYPOGRAPHY,
+  SPACING,
+  COLORS,
   InputField,
   PrimaryBtn,
-  C_BLUE,
   TEXT_TRANSFORM_NONE,
-  JUSTIFY_FLEX_END,
 } from '@opentrons/components'
 import {
+  CELSIUS,
   getModuleDisplayName,
   TEMPERATURE_MODULE_V1,
   TEMPERATURE_MODULE_V2,
 } from '@opentrons/shared-data'
+import { useSendModuleCommand } from '../../../redux/modules'
 import { Slideout } from '../../../atoms/Slideout'
 
-interface MagneticModuleSlideoutProps {
+interface TemperatureModuleSlideoutProps {
   model: typeof TEMPERATURE_MODULE_V1 | typeof TEMPERATURE_MODULE_V2
+  serial: string
   isExpanded: boolean
 }
 
-export const MagneticModuleSlideout = (
-  props: MagneticModuleSlideoutProps
+export const TemperatureModuleSlideout = (
+  props: TemperatureModuleSlideoutProps
 ): JSX.Element | null => {
-  const { model, isExpanded } = props
+  const { model, isExpanded, serial } = props
   const { t } = useTranslation('device_details')
+  const sendModuleCommand = useSendModuleCommand()
   const name = getModuleDisplayName(model)
+  const [temperatureValue, setTemperatureValue] = React.useState<string | null>(
+    null
+  )
+
+  const handleSubmitTemperature = (): void => {
+    if (temperatureValue != null) {
+      sendModuleCommand(serial, 'set_temperature', [Number(temperatureValue)])
+    }
+    setTemperatureValue(null)
+  }
 
   return (
     <Slideout
-      title={t('set_engage_height_slideout', { name: name })}
+      title={t('tempdeck_slideout_title', { name: name })}
       isExpanded={isExpanded}
     >
       <React.Fragment>
         <Text
           fontWeight={FONT_WEIGHT_REGULAR}
-          fontSize="0.6875rem"
+          fontSize={TYPOGRAPHY.fontSizeP}
           paddingTop={SPACING_1}
           data-testid={`Temp_Slideout_body_text_${name}`}
         >
@@ -54,6 +61,39 @@ export const MagneticModuleSlideout = (
             model: name,
           })}
         </Text>
+        <Flex
+          marginTop={SPACING.spacing4}
+          data-testid={`Temp_Slideout_input_field_${name}`}
+        >
+          <Text
+            fontWeight={FONT_WEIGHT_REGULAR}
+            fontSize={TYPOGRAPHY.fontSizeH6}
+            color={COLORS.darkGrey}
+          >
+            {t('temperature')}
+          </Text>
+          {/* TODO Immediately: make sure input field matches final designs */}
+          <InputField
+            units={CELSIUS}
+            value={temperatureValue}
+            onChange={e => setTemperatureValue(e.target.value)}
+          />
+        </Flex>
+        <PrimaryBtn
+          backgroundColor={COLORS.blue}
+          marginTop={'33rem'}
+          textTransform={TEXT_TRANSFORM_NONE}
+          onClick={handleSubmitTemperature}
+          disabled={temperatureValue == null}
+          data-testid={`Temp_Slideout_set_temp_btn_${name}`}
+        >
+          <Text
+            fontWeight={TYPOGRAPHY.fontWeightRegular}
+            fontSize={TYPOGRAPHY.fontSizeP}
+          >
+            {t('set_temp_slideout_btn')}
+          </Text>
+        </PrimaryBtn>
       </React.Fragment>
     </Slideout>
   )
