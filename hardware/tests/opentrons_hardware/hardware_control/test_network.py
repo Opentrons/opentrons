2b684743fd70f8b44b7820bfa6d2b649ffbb239b
Seth Foster <seth@opentrons.com>
2022-02-11 15:35:38 -0500

    fix(hardware,api): can network_probe functionality (#9437)
    Network probe was using get_status_request, which nothing has
    implemented, and therefore nothing responds to. That's bad. Use
    GetDeviceInfo instead which I think was what I was searching for in my
    mind's brain originally.
    
    Additionally, head responds to GetDeviceInfo, not head_l or head_r, so
    handle that in the controller backends.

--- hardware/tests/opentrons_hardware/hardware_control/test_network.py
+++ hardware/tests/opentrons_hardware/hardware_control/test_network.py
@@ -46,9 +46,9 @@ class MockStatusResponder:
         assert self._callbacks
         for callback in self._callbacks:
             for node in self._respond_with_nodes:
-                response = message_definitions.GetStatusResponse(
-                    payload=payloads.GetStatusResponsePayload(
-                        status=utils.UInt8Field(0), data=utils.UInt32Field(0)
+                response = message_definitions.DeviceInfoResponse(
+                    payload=payloads.DeviceInfoResponsePayload(
+                        version=utils.UInt32Field(0),
                     )
                 )
                 asyncio.get_running_loop().call_soon(
@@ -83,7 +83,7 @@ async def test_timeout_fires(mock_can_messenger: AsyncMock) -> None:
     # We should have sent a request
     mock_can_messenger.send.assert_called_once_with(
         node_id=NodeId.broadcast,
-        message=message_definitions.GetStatusRequest(payload=payloads.EmptyPayload()),
+        message=message_definitions.DeviceInfoRequest(payload=payloads.EmptyPayload()),
     )
     # we should have added a listener
     mock_can_messenger.add_listener.assert_called_once()
