5dba53ca0b0864f764cdc12260ba2f5243c9a3a8
Max Marrone <max@marrone.nyc>
2022-02-15 16:25:52 -0500

    Export MagneticModuleStatus.

--- api/src/opentrons/protocol_api_experimental/module_contexts/__init__.py
+++ api/src/opentrons/protocol_api_experimental/module_contexts/__init__.py
@@ -1,11 +1,12 @@
 """Protocol API interfaces for module control."""
 
-from .magnetic_module_context import MagneticModuleContext
+from .magnetic_module_context import MagneticModuleContext, MagneticModuleStatus
 from .temperature_module_context import TemperatureModuleContext
 from .thermocycler_module_context import ThermocyclerModuleContext
 
 __all__ = [
     "MagneticModuleContext",
+    "MagneticModuleStatus",
     "TemperatureModuleContext",
     "ThermocyclerModuleContext",
 ]
