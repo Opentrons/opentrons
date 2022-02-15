b1128481ca9e349df4a8c3425e32ae01307936b5
Max Marrone <max@marrone.nyc>
2022-02-15 12:38:35 -0500

    Add more stub methods to APIv3 `MagneticModuleContext`.

--- api/src/opentrons/protocol_api_experimental/module_contexts/magnetic_module_context.py
+++ api/src/opentrons/protocol_api_experimental/module_contexts/magnetic_module_context.py
@@ -1,5 +1,13 @@
 """Protocol API interfaces for Magnetic Modules."""
 
+from typing import Optional
+from typing_extensions import Literal
+
+from opentrons.protocols.api_support.types import APIVersion
+from opentrons_shared_data.labware.dev_types import LabwareDefinition
+
+from ..labware import Labware
+
 
 class MagneticModuleContext:  # noqa: D101
     # TODO(mc, 2022-02-09): copy or rewrite docstring from
@@ -8,6 +16,82 @@ class MagneticModuleContext:  # noqa: D101
     def __init__(self, module_id: str) -> None:
         self._module_id = module_id
 
+    # todo(mm, 2022-02-15): This public method returns an internal, undocumented type.
+    @property
+    def api_version(self) -> APIVersion:
+        """See APIv2 docstring."""
+        raise NotImplementedError()
+
+    def load_labware(
+        self,
+        name: str,
+        label: Optional[str] = None,
+        namespace: Optional[str] = None,
+        version: int = 1,
+    ) -> Labware:
+        """See APIv2 docstring."""
+        raise NotImplementedError()
+
+    def load_labware_from_definition(
+        self, definition: LabwareDefinition, label: Optional[str] = None
+    ) -> Labware:
+        """See APIv2 docstring."""
+        raise NotImplementedError()
+
+    # todo(mm, 2021-02-15): This looks like a vestigial internal APIv2 thing.
+    # Can we remove it from APIv3?
+    def load_labware_object(self, labware: Labware) -> Labware:
+        """See APIv2 docstring."""
+        raise NotImplementedError()
+
+    @property
+    def labware(self) -> Optional[Labware]:
+        """See APIv2 docstring."""
+        raise NotImplementedError()
+
+    def engage(
+        self,
+        height: Optional[float] = None,
+        *,
+        height_from_base: Optional[float] = None,
+        offset: Optional[float] = None,
+    ) -> None:
+        """See APIv2 docstring.
+
+        .. versionchanged:: 3.0
+            An error is now raised if you provide more than one of
+            ``height``, ``height_from_base``, and ``offset``.
+            Formerly, the behavior was unspecified.
+
+        .. versionchanged:: 3.0
+            You must now specify ``height_from_base`` and ``offset`` as keyword
+            arguments.
+        """
+        if len([a for a in [height, height_from_base, offset] if a is not None]) > 1:
+            raise ValueError(
+                "You may only specify one of"
+                " `height`, `height_from_base`, and `offset`."
+            )
+
+        raise NotImplementedError()
+
+    def disengage(self) -> None:
+        """See APIv2 docstring."""
+        raise NotImplementedError()
+
+    @property
+    def status(self) -> Literal["engaged", "disengaged"]:
+        """See APIv2 docstring."""
+        raise NotImplementedError()
+
+    # todo(mm, 2021-02-15): Does anyone internal or external
+    # use calibrate() in APIv2? Can we remove it from APIv3?
+    def calibrate(self) -> None:
+        """See APIv2 docstring."""
+        raise NotImplementedError()
+
+    # todo(mm, 2021-02-15): Decide whether APIv3 will return the same module object
+    # every time the module is retrieved. If yes, we don't need this method.
     def __eq__(self, other: object) -> bool:
         """Compare for object equality using identifier string."""
         return (
---
+++ api/tests/opentrons/protocol_api_experimental/module_contexts/test_magnetic_module_context.py
@@ -0,0 +1,71 @@
+"""Tests for `magnetic_module_context`."""
+
+
+import pytest
+
+from opentrons.protocol_api_experimental.module_contexts import MagneticModuleContext
+
+
+@pytest.fixture
+def subject() -> MagneticModuleContext:
+    """Return a MagneticModuleContext to test."""
+    return MagneticModuleContext(module_id="module-id")
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_api_version(subject: MagneticModuleContext) -> None:  # noqa: D103
+    _ = subject.api_version
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_load_labware(subject: MagneticModuleContext) -> None:  # noqa: D103
+    subject.load_labware("my_load_name")
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_load_labware_from_definition(  # noqa: D103
+    subject: MagneticModuleContext,
+) -> None:
+    subject.load_labware_from_definition(definition={})  # type: ignore[typeddict-item]
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_load_labware_object(subject: MagneticModuleContext) -> None:  # noqa: D103
+    subject.load_labware_object(labware=None)  # type: ignore[arg-type]
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_labware_property(subject: MagneticModuleContext) -> None:  # noqa: D103
+    _ = subject.labware
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_engage(subject: MagneticModuleContext) -> None:  # noqa: D103
+    subject.engage(10)
+
+
+def test_engage_only_one_height_allowed(subject: MagneticModuleContext) -> None:
+    """It should raise if you provide conflicting height arguments."""
+    with pytest.raises(ValueError):
+        subject.engage(height=1, height_from_base=2, offset=3)
+    with pytest.raises(ValueError):
+        subject.engage(height=1, height_from_base=2)
+    with pytest.raises(ValueError):
+        subject.engage(height=1, offset=3)
+    with pytest.raises(ValueError):
+        subject.engage(height_from_base=2, offset=3)
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_disengage(subject: MagneticModuleContext) -> None:  # noqa: D103
+    subject.disengage()
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_status(subject: MagneticModuleContext) -> None:  # noqa: D103
+    _ = subject.status
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_calibrate(subject: MagneticModuleContext) -> None:  # noqa: D103
+    subject.calibrate()
