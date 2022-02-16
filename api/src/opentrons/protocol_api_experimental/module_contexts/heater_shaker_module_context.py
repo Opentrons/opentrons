50f212f32bf2b800207faab4099f4dfdc9ee3878
Sanniti <sanniti@opentrons.com>
2022-02-16 13:25:51 -0500

    added heater shaker module context with basic methods

---
+++ api/src/opentrons/protocol_api_experimental/module_contexts/heater_shaker_module_context.py
@@ -0,0 +1,80 @@
+"""Protocol API interface for Heater-Shaker Modules."""
+
+from ..errors import (
+    InvalidTargetTemperatureError,
+    InvalidTargetSpeedError,
+)
+
+TEMPERATURE_LOWER_LIMIT = 37
+TEMPERATURE_UPPER_LIMIT = 95
+SPEED_LOWER_LIMIT = 200
+SPEED_UPPER_LIMIT = 3000
+
+
+class HeaterShakerModuleContext:
+    # TODO(spp, 2022-02-15): Revise this when writing other module contexts' docstrings.
+    """Object representing a connected Heater-Shaker Module.
+
+    It should not be instantiated directly; instead, it should be created through
+    :py:meth:`.ProtocolContext.load_module`.
+    """
+
+    def __init__(self, module_id: str) -> None:
+        self._module_id = module_id
+
+    def start_set_temperature(self, temperature: float) -> None:
+        """Set the target temperature of heater-shaker and return immediately.
+
+        This method takes a target temperature between 37C & 95C and tells
+        the heater-shaker to start heating to the target temperature without waiting
+        for the heater-shaker to reach the target.
+        Use :py:meth:`await_temperature` to wait for the temperature set here.
+
+        raises: `InvalidTargetTemperatureError` if temperature out of limits
+        """
+        if not TEMPERATURE_LOWER_LIMIT <= temperature <= TEMPERATURE_UPPER_LIMIT:
+            raise InvalidTargetTemperatureError(
+                f"Temperature should be in range {TEMPERATURE_LOWER_LIMIT} to "
+                f"{TEMPERATURE_UPPER_LIMIT} degree celsius."
+            )
+        raise NotImplementedError()
+
+    def await_temperature(self) -> None:
+        """Wait for the heater-shaker to reach its target temperature.
+
+        Use :py:meth:`start_set_temperature` to set the target temperature first.
+
+        raises: `NoTargetTemperatureError` if heater-shaker has no target temperature.
+        """
+        raise NotImplementedError()
+
+    def stop_heating(self) -> None:
+        """Stop heating."""
+        raise NotImplementedError()
+
+    def set_shake(self, speed: int) -> None:
+        """Set shake speed in RPM and start shaking.
+
+        Latches the labware, starts shaking the plate and returns once the target
+        shake speed has reached.
+        Acceptable shake speed: 200 - 3000 RPM
+
+        raises: `InvalidTargetSpeedError` if target speed out of limits.
+        """
+        if not SPEED_LOWER_LIMIT <= speed <= SPEED_UPPER_LIMIT:
+            raise InvalidTargetSpeedError(
+                f"Speed should be in range {SPEED_LOWER_LIMIT} - {SPEED_UPPER_LIMIT}"
+                f"RPM."
+            )
+        raise NotImplementedError()
+
+    def stop_shake(self) -> None:
+        """Stop shaking."""
+        raise NotImplementedError()
+
+    def __eq__(self, other: object) -> bool:
+        """Compare for object equality using identifier string."""
+        return (
+            isinstance(other, HeaterShakerModuleContext)
+            and self._module_id == other._module_id
+        )
---
+++ api/tests/opentrons/protocol_api_experimental/module_contexts/test_heater_shaker_module_context.py
@@ -0,0 +1,62 @@
+"""Tests for HeaterShakerModuleContext."""
+
+import pytest
+
+from opentrons.protocol_api_experimental import HeaterShakerModuleContext
+from opentrons.protocol_api_experimental.errors import (
+    InvalidTargetTemperatureError,
+    InvalidTargetSpeedError,
+)
+
+
+@pytest.fixture
+def subject() -> HeaterShakerModuleContext:
+    """Get a HeaterShakerModuleContext test subject."""
+    return HeaterShakerModuleContext(module_id="heater-shaker-id")
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_start_set_temperature(subject: HeaterShakerModuleContext) -> None:
+    """It should set the temperature and return immediately."""
+    subject.start_set_temperature(40.8)
+
+
+def test_start_set_invalid_temperature(subject: HeaterShakerModuleContext) -> None:
+    """It should raise if target temperature is invalid."""
+    with pytest.raises(InvalidTargetTemperatureError):
+        subject.start_set_temperature(20)
+    with pytest.raises(InvalidTargetTemperatureError):
+        subject.start_set_temperature(1000)
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_await_temperature(subject: HeaterShakerModuleContext) -> None:
+    """It should await the target temperature."""
+    subject.start_set_temperature(40)
+    subject.await_temperature()
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_stop_heating(subject: HeaterShakerModuleContext) -> None:
+    """It should stop heater shaker heating."""
+    subject.stop_heating()
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_set_speed(subject: HeaterShakerModuleContext) -> None:
+    """It should set heater shaker speed."""
+    subject.set_shake(500)
+
+
+def test_set_invalid_speed(subject: HeaterShakerModuleContext) -> None:
+    """It should raise error when given invalid speed."""
+    with pytest.raises(InvalidTargetSpeedError):
+        subject.set_shake(10)
+    with pytest.raises(InvalidTargetSpeedError):
+        subject.set_shake(10000)
+
+
+@pytest.mark.xfail(strict=True, raises=NotImplementedError)
+def test_stop_shake(subject: HeaterShakerModuleContext) -> None:
+    """It should stop shake."""
+    subject.stop_shake()
