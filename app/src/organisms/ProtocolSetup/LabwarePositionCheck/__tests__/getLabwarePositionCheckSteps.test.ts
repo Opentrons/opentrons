fad47b47a85c4897c72425786257b49c46c87049
Shlok Amin <shlokamin@me.com>
2022-02-17 17:45:19 -0500

    add test case

--- app/src/organisms/ProtocolSetup/LabwarePositionCheck/__tests__/getLabwarePositionCheckSteps.test.ts
+++ app/src/organisms/ProtocolSetup/LabwarePositionCheck/__tests__/getLabwarePositionCheckSteps.test.ts
@@ -1,6 +1,7 @@
 import { when, resetAllWhenMocks } from 'jest-when'
 import _uncasted_protocolWithOnePipette from '@opentrons/shared-data/protocol/fixtures/4/simpleV4.json'
 import _uncasted_protocolWithTwoPipettes from '@opentrons/shared-data/protocol/fixtures/4/transferSettings.json'
+import _uncasted_v6ProtocolWithTwoPipettes from '@opentrons/shared-data/protocol/fixtures/6/multipleTipracks.json'
 import { getLabwarePositionCheckSteps } from '../getLabwarePositionCheckSteps'
 import { getPrimaryPipetteId } from '../utils/getPrimaryPipetteId'
 import { getPipetteWorkflow } from '../utils/getPipetteWorkflow'
@@ -12,6 +13,7 @@ import type { ProtocolFile } from '@opentrons/shared-data'
 // TODO: update these fixtures to be v6 protocols
 const protocolWithOnePipette = (_uncasted_protocolWithOnePipette as unknown) as ProtocolFile<any>
 const protocolWithTwoPipettes = (_uncasted_protocolWithTwoPipettes as unknown) as ProtocolFile<any>
+const v6ProtocolWithTwoPipettes = (_uncasted_v6ProtocolWithTwoPipettes as unknown) as ProtocolFile<any>
 
 jest.mock('../utils/getPrimaryPipetteId')
 jest.mock('../utils/getPipetteWorkflow')
@@ -35,7 +37,7 @@ describe('getLabwarePositionCheckSteps', () => {
   afterEach(() => {
     resetAllWhenMocks()
   })
-  it('should generate commands with the one pipette workflow', () => {
+  it('should generate commands with the one pipette workflow when there is only one pipette in the protocol', () => {
     const mockPipette = protocolWithOnePipette.pipettes.pipetteId
     when(mockGetPrimaryPipetteId)
       .calledWith(
@@ -62,6 +64,56 @@ describe('getLabwarePositionCheckSteps', () => {
       labwareDefinitions: protocolWithOnePipette.labwareDefinitions,
       modules: protocolWithOnePipette.modules,
       commands: protocolWithOnePipette.commands,
+    })
+  })
+  it('should generate commands with the one pipette workflow when there are two pipettes in the protocol but only one is used', () => {
+    const leftPipetteId = '50d23e00-0042-11ec-8258-f7ffdf5ad45a'
+    const rightPipetteId = 'c235a5a0-0042-11ec-8258-f7ffdf5ad45a'
+    const leftPipette = v6ProtocolWithTwoPipettes.pipettes[leftPipetteId]
+    const commandsWithoutLeftPipettePickupTipCommand = v6ProtocolWithTwoPipettes.commands.filter(
+      command =>
+        !(
+          command.commandType === 'pickUpTip' &&
+          command.params.pipetteId === leftPipetteId
+        )
+    )
+
+    const protocolWithTwoPipettesWithOnlyOneBeingUsed = {
+      ...v6ProtocolWithTwoPipettes,
+      commands: commandsWithoutLeftPipettePickupTipCommand,
+    }
+
+    const pipettesBeingUsedInProtocol: ProtocolFile<any>['pipettes'] = {
+      '50d23e00-0042-11ec-8258-f7ffdf5ad45a': { name: 'p300_single_gen2' },
+    }
+
+    when(mockGetPrimaryPipetteId)
+      .calledWith(
+        pipettesBeingUsedInProtocol,
+        protocolWithTwoPipettesWithOnlyOneBeingUsed.commands
+      )
+      .mockReturnValue(rightPipetteId)
+
+    when(mockGetPipetteWorkflow)
+      .calledWith({
+        pipetteNames: [leftPipette.name],
+        primaryPipetteId: rightPipetteId,
+        labware: protocolWithTwoPipettesWithOnlyOneBeingUsed.labware,
+        labwareDefinitions:
+          protocolWithTwoPipettesWithOnlyOneBeingUsed.labwareDefinitions,
+        commands: protocolWithTwoPipettesWithOnlyOneBeingUsed.commands,
+      })
+      .mockReturnValue(1)
+
+    getLabwarePositionCheckSteps(protocolWithTwoPipettesWithOnlyOneBeingUsed)
+
+    expect(mockgetOnePipettePositionCheckSteps).toHaveBeenCalledWith({
+      primaryPipetteId: rightPipetteId,
+      labware: protocolWithTwoPipettesWithOnlyOneBeingUsed.labware,
+      labwareDefinitions:
+        protocolWithTwoPipettesWithOnlyOneBeingUsed.labwareDefinitions,
+      modules: protocolWithTwoPipettesWithOnlyOneBeingUsed.modules,
+      commands: protocolWithTwoPipettesWithOnlyOneBeingUsed.commands,
     })
   })
   it('should generate commands with the two pipette workflow', () => {
