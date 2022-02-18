3960501b0e554785d4d435f4f4914aafde575936
Jethary Rader <jethary@opentrons.com>
2022-02-18 18:02:41 -0500

    fix import

--- app/src/atoms/Interstitial/Interstitial.stories.tsx
+++ app/src/atoms/Interstitial/Interstitial.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Interstitial } from './index'
+import { Interstitial } from './Interstitial'
 import type { Story, Meta } from '@storybook/react'
 
 export default {
