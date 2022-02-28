8111d850180efd1c65acfbaca9adf788a03b2fce
Shlok Amin <shlokamin@me.com>
2022-02-28 17:11:41 -0500

    add correct TS types to response payload from labware def endpoint

--- api-client/src/runs/createLabwareDefinition.ts
+++ api-client/src/runs/createLabwareDefinition.ts
@@ -3,17 +3,18 @@ import { POST, request } from '../request'
 import type { LabwareDefinition2 } from '@opentrons/shared-data'
 import type { ResponsePromise } from '../request'
 import type { HostConfig } from '../types'
-import type { Run } from './types'
+
+export interface CreateLabwareDefinitionResponsePayload {
+  definitionUri: string
+}
 
 export function createLabwareDefinition(
   config: HostConfig,
   runId: string,
   data: LabwareDefinition2
-): ResponsePromise<Run> {
-  return request<Run, { data: LabwareDefinition2 }>(
-    POST,
-    `/runs/${runId}/labware_definitions`,
-    { data },
-    config
-  )
+): ResponsePromise<CreateLabwareDefinitionResponsePayload> {
+  return request<
+    CreateLabwareDefinitionResponsePayload,
+    { data: LabwareDefinition2 }
+  >(POST, `/runs/${runId}/labware_definitions`, { data }, config)
 }
--- react-api-client/src/runs/useCreateLabwareDefinitionMutation.ts
+++ react-api-client/src/runs/useCreateLabwareDefinitionMutation.ts
@@ -3,7 +3,10 @@ import { createLabwareDefinition } from '@opentrons/api-client'
 import { useHost } from '../api'
 import type { UseMutationResult, UseMutateAsyncFunction } from 'react-query'
 import type { LabwareDefinition2 } from '@opentrons/shared-data'
-import type { HostConfig, Run } from '@opentrons/api-client'
+import type {
+  HostConfig,
+  CreateLabwareDefinitionResponsePayload,
+} from '@opentrons/api-client'
 
 interface CreateLabwareDefinitionParams {
   runId: string
@@ -11,12 +14,12 @@ interface CreateLabwareDefinitionParams {
 }
 
 export type UseCreateLabwareDefinitionMutationResult = UseMutationResult<
-  Run,
+  CreateLabwareDefinitionResponsePayload,
   unknown,
   CreateLabwareDefinitionParams
 > & {
   createLabwareDefinition: UseMutateAsyncFunction<
-    Run,
+    CreateLabwareDefinitionResponsePayload,
     unknown,
     CreateLabwareDefinitionParams
   >
@@ -26,21 +29,24 @@ export function useCreateLabwareDefinitionMutation(): UseCreateLabwareDefinition
   const host = useHost()
   const queryClient = useQueryClient()
 
-  const mutation = useMutation<Run, unknown, CreateLabwareDefinitionParams>(
-    ({ runId, data }) =>
-      createLabwareDefinition(host as HostConfig, runId, data)
-        .then(response => {
-          queryClient
-            .invalidateQueries([host, 'runs'])
-            .catch((e: Error) =>
-              console.error(`error invalidating runs query: ${e.message}`)
-            )
-          return response.data
-        })
-        .catch((e: Error) => {
-          console.error(`error creating labware offsets: ${e.message}`)
-          throw e
-        })
+  const mutation = useMutation<
+    CreateLabwareDefinitionResponsePayload,
+    unknown,
+    CreateLabwareDefinitionParams
+  >(({ runId, data }) =>
+    createLabwareDefinition(host as HostConfig, runId, data)
+      .then(response => {
+        queryClient
+          .invalidateQueries([host, 'runs'])
+          .catch((e: Error) =>
+            console.error(`error invalidating runs query: ${e.message}`)
+          )
+        return response.data
+      })
+      .catch((e: Error) => {
+        console.error(`error creating labware offsets: ${e.message}`)
+        throw e
+      })
   )
 
   return {
