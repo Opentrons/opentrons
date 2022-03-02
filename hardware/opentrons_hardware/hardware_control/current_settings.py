23e9fd8342a271d11a0026639242d54aebfda501
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2022-03-02 13:30:42 -0500

    add tests

--- hardware/opentrons_hardware/hardware_control/current_settings.py
+++ hardware/opentrons_hardware/hardware_control/current_settings.py
@@ -1,21 +1,24 @@
 """Utilities for updating the current settings on the OT3."""
-import asyncio
-from lib2to3.pytree import Node
 from typing import Dict, Tuple
 
-from opentrons_ot3_firmware import ArbitrationId
-from opentrons_ot3_firmware.constants import NodeId
+from opentrons_hardware.firmware_bindings.constants import NodeId
 from opentrons_hardware.drivers.can_bus.can_messenger import CanMessenger
-from opentrons_ot3_firmware.messages import payloads, MessageDefinition
-from opentrons_ot3_firmware.messages.message_definitions import (
-    WriteMotorCurrentRequest
+from opentrons_hardware.firmware_bindings.messages import payloads
+from opentrons_hardware.firmware_bindings.messages.message_definitions import (
+    WriteMotorCurrentRequest,
 )
-from opentrons_ot3_firmware.utils import UInt32Field
+from opentrons_hardware.firmware_bindings.utils import UInt32Field
+
+
+CompleteCurrentSettings = Dict[NodeId, Tuple[float, float]]
+PartialCurrentSettings = Dict[NodeId, float]
 
 
 async def set_currents(
-        can_messenger: CanMessenger, current_settings: Dict[NodeId, Tuple[float]],
+    can_messenger: CanMessenger,
+    current_settings: CompleteCurrentSettings,
 ) -> None:
+    """Set hold current and run current for each node."""
     for node, currents in current_settings.items():
         await can_messenger.send(
             node_id=node,
@@ -24,13 +27,14 @@ async def set_currents(
                     hold_current=UInt32Field(int(currents[0] * (2**16))),
                     run_current=UInt32Field(int(currents[1] * (2**16))),
                 )
-            )
+            ),
         )
 
 
 async def set_run_current(
-        can_messenger: CanMessenger, current_settings: Dict[NodeId, float]
+    can_messenger: CanMessenger, current_settings: PartialCurrentSettings
 ) -> None:
+    """Set only the run current for each node."""
     for node, current in current_settings.items():
         await can_messenger.send(
             node_id=node,
@@ -39,13 +43,14 @@ async def set_run_current(
                     hold_current=UInt32Field(0),
                     run_current=UInt32Field(int(current * (2**16))),
                 )
-            )
+            ),
         )
 
 
 async def set_hold_current(
-        can_messenger: CanMessenger, current_settings: Dict[NodeId, float]
+    can_messenger: CanMessenger, current_settings: PartialCurrentSettings
 ) -> None:
+    """Set only the hold current for each node."""
     for node, current in current_settings.items():
         await can_messenger.send(
             node_id=node,
@@ -54,5 +59,5 @@ async def set_hold_current(
                     hold_current=UInt32Field(int(current * (2**16))),
                     run_current=UInt32Field(0),
                 )
-            )
+            ),
         )
