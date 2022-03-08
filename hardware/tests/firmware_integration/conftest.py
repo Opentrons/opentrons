c8161cd503a5a023141e3634d9d9ccf7c6409f52
amitlissack <amit@opentrons.com>
2022-03-08 17:00:55 -0500

    feat(hardware): Hardware integration tests (#9224)
    * add test-with-opentrons-sock-emulator to run tests against local running emulator using opentrons_sock interface.
    
    * device info tests.
    
    * test motion constraint messages.
    
    * test add move group and get move group messages.
    
    * test motor driver messages
    
    * test eeprom messages
    
    * fix device info test.
    
    * node id
    
    * use path.
    
    * fix socket driver and update fw script.
    
    * tests for fw update.
    
    * use SocketDriver in sim_socket script (duh).
    
    * Disable tests with emulation until we're ready for running integration tests.

---
+++ hardware/tests/firmware_integration/conftest.py
@@ -0,0 +1,73 @@
+"""Common fixtures for integration tests."""
+from __future__ import annotations
+import pytest
+import asyncio
+from typing import AsyncGenerator, Iterator, AsyncIterator
+
+from _pytest.fixtures import FixtureRequest
+
+from opentrons_hardware.drivers.can_bus.settings import DriverSettings
+from opentrons_hardware.drivers.can_bus.build import build_driver
+from opentrons_hardware.drivers.can_bus.abstract_driver import AbstractCanDriver
+from opentrons_hardware.drivers.can_bus import CanMessenger, WaitableCallback
+from opentrons_hardware.firmware_bindings import constants
+
+
+@pytest.fixture
+async def driver(
+    loop: asyncio.BaseEventLoop,
+) -> AsyncGenerator[AbstractCanDriver, None]:
+    """Create CanDriver connected to OT-3 Emulator."""
+    settings = DriverSettings()
+    driver = await build_driver(settings)
+    yield driver
+    driver.shutdown()
+
+
+@pytest.fixture
+async def can_messenger(
+    driver: AbstractCanDriver,
+) -> AsyncIterator[CanMessenger]:
+    """Create Can messenger."""
+    messenger = CanMessenger(driver)
+    messenger.start()
+    yield messenger
+    await messenger.stop()
+
+
+@pytest.fixture
+def can_messenger_queue(
+    can_messenger: CanMessenger,
+) -> Iterator[WaitableCallback]:
+    """Create WaitableCallback for the CAN Messenger."""
+    with WaitableCallback(can_messenger) as wc:
+        yield wc
+
+
+@pytest.fixture(
+    scope="session",
+    params=[
+        constants.NodeId.head,
+        constants.NodeId.pipette_left,
+        constants.NodeId.gantry_x,
+        constants.NodeId.gantry_y,
+    ],
+)
+def subsystem_node_id(request: FixtureRequest) -> Iterator[constants.NodeId]:
+    """Each subsystem's node id as a fixture."""
+    yield request.param  # type: ignore[attr-defined]
+
+
+@pytest.fixture(
+    scope="session",
+    params=[
+        constants.NodeId.head_l,
+        constants.NodeId.head_r,
+        constants.NodeId.pipette_left,
+        constants.NodeId.gantry_x,
+        constants.NodeId.gantry_y,
+    ],
+)
+def motor_node_id(request: FixtureRequest) -> Iterator[constants.NodeId]:
+    """Each motor's node id as a fixture."""
+    yield request.param  # type: ignore[attr-defined]
