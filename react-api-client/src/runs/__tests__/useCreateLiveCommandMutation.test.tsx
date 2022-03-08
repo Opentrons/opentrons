8edbc85b0002745d0ccc2f5287863a5731098d45
Sakib Hossain <sakib.hossain@opentrons.com>
2022-03-08 14:09:40 -0500

    feat(api-client): add create live command hook
    This PR adds create live command.
    
    closes #9650

---
+++ react-api-client/src/runs/__tests__/useCreateLiveCommandMutation.test.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react'
+import { when, resetAllWhenMocks } from 'jest-when'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { act, renderHook } from '@testing-library/react-hooks'
+import { createLiveCommand } from '@opentrons/api-client'
+import { useHost } from '../../api'
+import { useCreateLiveCommandMutation } from '../useCreateLiveCommandMutation'
+
+import { mockAnonLoadCommand } from '../__fixtures__'
+
+import type { HostConfig } from '@opentrons/api-client'
+
+jest.mock('@opentrons/api-client')
+jest.mock('../../api/useHost')
+
+const mockCreateCommand = createLiveCommand as jest.MockedFunction<
+  typeof createLiveCommand
+>
+const mockUseHost = useHost as jest.MockedFunction<typeof useHost>
+
+const HOST_CONFIG: HostConfig = { hostname: 'localhost' }
+
+describe('useCreateLiveCommandMutation hook', () => {
+  let wrapper: React.FunctionComponent<{}>
+
+  beforeEach(() => {
+    const queryClient = new QueryClient()
+    const clientProvider: React.FunctionComponent<{}> = ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+    wrapper = clientProvider
+  })
+  afterEach(() => {
+    resetAllWhenMocks()
+  })
+
+  it('should issue the given command to the given run when callback is called', async () => {
+    when(mockUseHost).calledWith().mockReturnValue(HOST_CONFIG)
+    when(mockCreateCommand)
+      .calledWith(HOST_CONFIG, mockAnonLoadCommand, {})
+      .mockResolvedValue({ data: 'something' } as any)
+
+    const { result, waitFor } = renderHook(
+      () => useCreateLiveCommandMutation(),
+      {
+        wrapper,
+      }
+    )
+
+    expect(result.current.data).toBeUndefined()
+    act(() => {
+      result.current.createLiveCommand({
+        command: mockAnonLoadCommand,
+      })
+    })
+    await waitFor(() => {
+      return result.current.data != null
+    })
+    expect(result.current.data).toBe('something')
+  })
+  it('should pass waitUntilComplete and timeout through if given command', async () => {
+    const waitUntilComplete = true
+    const timeout = 2000
+    when(mockUseHost).calledWith().mockReturnValue(HOST_CONFIG)
+    when(mockCreateCommand)
+      .calledWith(HOST_CONFIG, mockAnonLoadCommand, {
+        waitUntilComplete,
+        timeout,
+      })
+      .mockResolvedValue({ data: 'something' } as any)
+
+    const { result, waitFor } = renderHook(
+      () => useCreateLiveCommandMutation(),
+      {
+        wrapper,
+      }
+    )
+
+    expect(result.current.data).toBeUndefined()
+    act(() => {
+      result.current.createLiveCommand({
+        command: mockAnonLoadCommand,
+        waitUntilComplete,
+        timeout,
+      })
+    })
+    await waitFor(() => {
+      return result.current.data != null
+    })
+    expect(result.current.data).toBe('something')
+  })
+})
