1ab08ed10a66b47cbe72d41ffe16a8d37e25d06f
Kadee80 <kaa328@nyu.edu>
2022-03-14 15:23:09 -0400

    refactor: Add note on AlertItem to test deploy

--- labware-library/src/components/labware-ui/NewLabwareAlert.tsx
+++ labware-library/src/components/labware-ui/NewLabwareAlert.tsx
@@ -3,6 +3,7 @@ import { Box, AlertItem } from '@opentrons/components'
 import { NEW_LABWARE_MESSAGE } from '../../localization'
 
 export function NewLabwareAlert(): JSX.Element {
+  // Note: Box is redudant but used here to wrap older alert item that only accepts className for styling
   return (
     <Box marginTop="1px">
       <AlertItem type="info" title={NEW_LABWARE_MESSAGE} />
