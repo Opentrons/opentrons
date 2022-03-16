d7fc0f6359fffa1164617dd241dac42c3ac5479d
Josh McVey <josh.mcvey@opentrons.com>
2022-03-16 12:05:34 -0500

    more exploration

--- api-client/src/tests/api-client.test.ts
+++ api-client/src/tests/api-client.test.ts
@@ -1,13 +1,41 @@
-import 'jest';
-import  { getHealth, Health }   from '../health';
-import { HostConfig } from '../types';
-import { Response } from '../request';
+import 'jest'
+import * as fs from 'fs'
+import { getHealth, Health } from '../health'
+import { HostConfig } from '../types'
+import { Response } from '../request'
+import { createProtocol, Protocol } from '../protocols'
 
-const host: HostConfig = { hostname: process.env.HOST != null ? process.env.HOST : '' } 
+const host: HostConfig = {
+  hostname: process.env.DEV_ROBOT_HOST != null ? process.env.DEV_ROBOT_HOST : '',
+  port: process.env.DEV_ROBOT_PORT != null ? +process.env.DEV_ROBOT_PORT : 0,
+}
+console.log(`host config is: ${host.hostname}:${host.port?.toString()}`)
 
 describe('health tests', () => {
-  it('returns the correct api version', async () => {
+  it.only('getHealth matches the snapshot', async () => {
     const healthResponse: Response<Health> = await getHealth(host)
-    expect(healthResponse.data.api_version).toBe('2.12') // would need to be dynamic in real life
+    expect(healthResponse.data).toMatchSnapshot()
   })
 })
+
+describe('modules', () => {
+  it.only('getModules matches the snapshot', async () => {
+    // cannot getModules with api-client
+  })
+})
+
+
+/* describe('protocols', () => {
+  it('upload a python protocol', async () => {
+    const file = fs.readFileSync(
+      '../../robot-server/tests/integration/protocols/basic_transfer_standalone.py',
+      'utf8'
+    )
+    const files: File[] = [new File([file], 'basic_transfer_standalone.py')]
+    const protocolCreateResponse: Response<Protocol> = await createProtocol(
+      host,
+      files
+    )
+    console.log(protocolCreateResponse)
+  })
+}) */
---
+++ api-client/src/tests/setup-tests.ts
@@ -0,0 +1,3 @@
+
+process.env.DEV_ROBOT_HOST = "localhost"
+process.env.DEV_ROBOT_PORT = "31950"
