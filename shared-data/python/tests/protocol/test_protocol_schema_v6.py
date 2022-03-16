d8f89726849fe2578500e5762c169100aca7aa39
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-03-16 15:04:35 -0400

    rollback all v6 json fixture and fixed v6 parse to model test

--- shared-data/python/tests/protocol/test_protocol_schema_v6.py
+++ shared-data/python/tests/protocol/test_protocol_schema_v6.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+from typing import Any, Dict
 from opentrons_shared_data import load_shared_data
 from opentrons_shared_data.protocol.models import ProtocolSchemaV6
 
@@ -10,12 +11,19 @@ from . import list_fixtures
 def test_v6_types(defpath):
     def_data = load_shared_data(defpath)
     def_model = ProtocolSchemaV6.parse_raw(def_data)
-    def_dict_from_model = def_model.dict(exclude_none=True)
-
-    # pop the schema ID off the dicts so we can compare the full dicts later
+    def_dict_from_model = def_model.dict(
+        exclude_unset=True,
+        # 'schemaVersion' in python is '$schemaVersion' in JSON
+        by_alias=True,
+    )
     expected_def_dict = json.loads(def_data)
-    expected_schema = expected_def_dict.pop("$otSharedSchema")
-    actual_schema = def_dict_from_model.pop("otSharedSchema")
-
-    assert actual_schema == expected_schema
+    delete_unexpected_results(expected_def_dict)
     assert def_dict_from_model == expected_def_dict
+
+
+def delete_unexpected_results(protocol_fixture: Dict[str, Any]) -> None:
+    for command_object_dict in protocol_fixture["commands"]:
+        try:
+            command_object_dict.pop("result")
+        except KeyError:
+            pass
