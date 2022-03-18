89ac6be6fa1084bbb08ccd2f2c75fb088c86d360
Max Marrone <max@marrone.nyc>
2022-03-18 01:43:47 -0400

    Reformat CommandState.

--- api/src/opentrons/protocol_engine/state/commands.py
+++ api/src/opentrons/protocol_engine/state/commands.py
@@ -89,34 +89,38 @@ class CommandEntry:
 
 @dataclass
 class CommandState:
-    """State of all protocol engine command resources.
+    """State of all protocol engine command resources."""
 
-    Attributes:
-        queue_status: Whether the engine is currently pulling new
-            commands off the queue to execute. A command may still be
-            executing, and the robot may still be in motion, even if INACTIVE.
-        is_hardware_stopped: Whether the engine's hardware has ceased
-            motion. Once set, this flag cannot be unset.
-        is_door_blocking: Whether the door is open when
-            enable_door_safety_switch feature flag is ON.
-        run_result: Whether the run is done and succeeded, failed, or stopped.
-            Once set, this status cannot be unset.
-        running_command_id: The ID of the currently running command, if any.
-        queued_command_ids: The IDs of queued commands in FIFO order.
-        commands_by_id: All command resources, in insertion order, mapped
-            by their unique IDs.
-        errors_by_id: All error occurrences, mapped by their unique IDs.
-    """
-
+    # All command IDs, in insertion order.
     all_command_ids: List[str]
-    commands_by_id: Dict[str, CommandEntry]
-    running_command_id: Optional[str]
+
+    # The IDs of queued commands, in FIFO order.
     queued_command_ids: OrderedSet[str]
 
+    # The ID of the currently running command, if any.
+    running_command_id: Optional[str]
+
+    # All command resources, in insertion order, mapped by their unique IDs.
+    commands_by_id: Dict[str, CommandEntry]
+
+    # Whether the engine is currently pulling new commands off the queue to execute.
+    # A command may still be executing, and the robot may still be in motion,
+    # even if INACTIVE.
     queue_status: QueueStatus
+
+    # Whether the engine's hardware has ceased motion.
+    # Once set, this flag cannot be unset.
     is_hardware_stopped: bool
+
+    # Whether the door is open when enable_door_safety_switch
+    # feature flag is ON.
     is_door_blocking: bool
+
+    # Whether the run is done and succeeded, failed, or stopped.
+    # Once set, this status cannot be unset.
     run_result: Optional[RunResult]
+
+    # All error occurrences, mapped by their unique IDs.
     errors_by_id: Dict[str, ErrorOccurrence]
 
 
