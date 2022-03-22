a7d1a451fc07fd48a1013b0d18e5111c845a7cf2
Sanniti <sanniti@opentrons.com>
2022-03-22 03:55:06 -0400

    implemented open latch

--- api/tests/opentrons/protocol_engine/commands/heater_shaker/test_open_latch.py
+++ api/tests/opentrons/protocol_engine/commands/heater_shaker/test_open_latch.py
@@ -1,25 +1,44 @@
 """Test Heater Shaker open latch command implementation."""
-import pytest
 from decoy import Decoy
 
+from opentrons.hardware_control import HardwareControlAPI
+from opentrons.hardware_control.modules import AbstractModule, HeaterShaker
+
+from opentrons.protocol_engine.state import StateView
+from opentrons.protocol_engine.state.modules import HeaterShakerModuleView
 from opentrons.protocol_engine.commands import heater_shaker
 from opentrons.protocol_engine.commands.heater_shaker.open_latch import (
     OpenLatchImpl,
 )
 
 
-@pytest.fixture()
-def subject() -> OpenLatchImpl:
-    """Get the command implementation with mocked out dependencies."""
-    return OpenLatchImpl()
-
-
-# TODO(mc, 2022-02-25): verify hardware interaction
-@pytest.mark.xfail(raises=NotImplementedError, strict=True)
-async def test_open_latch(decoy: Decoy, subject: OpenLatchImpl) -> None:
+async def test_open_latch(
+        decoy: Decoy,
+        state_view: StateView,
+        hardware_api: HardwareControlAPI
+) -> None:
     """It should be able to open the module's labware latch."""
+    subject = OpenLatchImpl(
+        state_view=state_view, hardware_api=hardware_api
+    )
     data = heater_shaker.OpenLatchParams(moduleId="heater-shaker-id")
 
-    result = await subject.execute(data)
+    # Get module view
+    hs_module_view = decoy.mock(cls=HeaterShakerModuleView)
+    decoy.when(
+        state_view.modules.get_heater_shaker_module_view(module_id="heater-shaker-id")
+    ).then_return(hs_module_view)
 
+    # Get attached hardware modules
+    attached = [decoy.mock(cls=AbstractModule), decoy.mock(cls=AbstractModule)]
+    match = decoy.mock(cls=HeaterShaker)
+    decoy.when(hardware_api.attached_modules).then_return(attached)
+
+    # Get stubbed hardware module from hs module view
+    decoy.when(
+        hs_module_view.find_hardware(attached_modules=attached)
+    ).then_return(match)
+
+    result = await subject.execute(data)
+    decoy.verify(await match.open_labware_latch(), times=1)
     assert result == heater_shaker.OpenLatchResult()
