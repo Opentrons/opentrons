d9a04b17aa2131efe307d406db8b71c2445f0263
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-03-22 10:11:26 -0400

    loadLabware command resolved

--- api/src/opentrons/protocol_runner/json_command_translator.py
+++ api/src/opentrons/protocol_runner/json_command_translator.py
@@ -28,9 +28,13 @@ class JsonCommandTranslator:
             elif command.commandType == "loadModule":
                 dict_command["params"].update({"model":protocol.modules[command.params.moduleId].model})
             elif command.commandType == "loadLabware":
+                # TODO define a constructor for LoadLabwareParams that accepts these fields
+                print(command.params.location)
+                definition_id = protocol.labware[command.params.labwareId].definitionId
                 dict_command["params"].update({"displayName": protocol.labware[command.params.labwareId].displayName})
-                dict_command["params"].update({"version": protocol.labwareDefinitions[protocol.labware[command.params.labwareId].definitionId].version})
-                dict_command["params"].update({"namespace": protocol.labwareDefinitions[protocol.labware[command.params.labwareId].definitionId].namespace})
+                dict_command["params"].update({"version": protocol.labwareDefinitions[definition_id].version})
+                dict_command["params"].update({"namespace": protocol.labwareDefinitions[definition_id].namespace})
+                dict_command["params"].update({"loadName": protocol.labwareDefinitions[definition_id].parameters.loadName})
                 print(dict_command)
             translated_obj = parse_obj_as(pe_commands.CommandCreate, dict_command)
             commands_list.append(translated_obj)
