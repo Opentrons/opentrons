a5add0346218e85caf25526abb44624ffbf23681
smb2268 <sarah.breen@opentrons.com>
2022-03-23 12:49:22 -0700

    fix test failure

--- app/src/organisms/Labware/__tests__/AddCustomLabware.test.tsx
+++ app/src/organisms/Labware/__tests__/AddCustomLabware.test.tsx
@@ -2,16 +2,10 @@ import * as React from 'react'
 import { MemoryRouter } from 'react-router-dom'
 import { renderWithProviders } from '@opentrons/components'
 import { i18n } from '../../../i18n'
-import { useAddLabware } from '../hooks'
 import { AddCustomLabware } from '../AddCustomLabware'
-import { fireEvent } from '@testing-library/dom'
 
-jest.mock('../hooks')
+jest.mock('../../../redux/custom-labware')
 jest.mock('../helpers/getAllDefs')
-
-const mockUseAddLabware = useAddLabware as jest.MockedFunction<
-  typeof useAddLabware
->
 
 const render = (props: React.ComponentProps<typeof AddCustomLabware>) => {
   return renderWithProviders(
@@ -37,11 +31,6 @@ describe('AddCustomLabware', () => {
     getByText('Import a Custom Labware Definition')
     getByText('Or choose a file from your computer to upload.')
     getByRole('button', { name: 'Choose File' })
-  })
-  it('calls useAddLabware when choose file is pressed', () => {
-    const [{ getByRole }] = render(props)
-    const chooseFileButton = getByRole('button', { name: 'Choose File' })
-    fireEvent.click(chooseFileButton)
-    expect(mockUseAddLabware).toHaveBeenCalled()
+    getByRole('button', { name: 'browse' })
   })
 })
