f729edb2af4bf5970423d5e216322ab6f331a613
alexjoel42 <alex.copperman@opentrons.com>
2022-03-23 15:00:18 -0400

    we are good to go with API version and longer commands

--- api/src/opentrons/protocols/advanced_control/transfers.py
+++ api/src/opentrons/protocols/advanced_control/transfers.py
@@ -1,4 +1,5 @@
 import enum
+import warnings
 from typing import (
     Any,
     Dict,
@@ -492,29 +493,29 @@ class TransferPlan:
             - self._strategy.disposal_volume
             - self._strategy.air_gap,
         )
-        if self._api_version >= APIVersion(2, 11) :
-            # TODO: @SyntaxColoring, I think that this is following our API policy, might move to 2.12
-            # https://github.com/Opentrons/opentrons/issues/7477#issuecomment-801414763
+        if self._api_version >= APIVersion(2, 12):
             # TODO: update https://github.com/Opentrons/opentrons/blob/edge/api/docs/v2/versioning.rst
+
+
             for step_vol, (src, dest) in plan_iter:
-                if step_vol>0:
-                    if self._strategy.new_tip == types.TransferTipPolicy.ALWAYS:
-                        yield self._format_dict("pick_up_tip", kwargs=self._tip_opts)
-                max_vol = (
-                    self._max_volume
-                    - self._strategy.disposal_volume
-                    - self._strategy.air_gap
-                )
-                xferred_vol = 0.0
-                while xferred_vol < step_vol:
-                    # TODO: account for unequal length sources, dests
-                    # TODO: ensure last transfer is > min_vo
-                    vol = min(max_vol, step_vol - xferred_vol)
-                    yield from self._aspirate_actions(vol, src)
-                    yield from self._dispense_actions(vol=vol, dest=dest, src=src)
-                    xferred_vol += vol
-                if step_vol>0:
-                    yield from self._new_tip_action()
+                    max_vol = (
+                        self._max_volume
+                        - self._strategy.disposal_volume
+                        - self._strategy.air_gap
+                    )
+                    xferred_vol = 0.0
+                    while xferred_vol < step_vol:
+                        if step_vol > 0:
+                            if self._strategy.new_tip == types.TransferTipPolicy.ALWAYS:
+                                yield self._format_dict("pick_up_tip", kwargs=self._tip_opts)
+                        # TODO: account for unequal length sources, dests
+                        # TODO: ensure last transfer is > min_vo
+                        vol = min(max_vol, step_vol - xferred_vol)
+                        yield from self._aspirate_actions(vol, src)
+                        yield from self._dispense_actions(vol=vol, dest=dest, src=src)
+                        xferred_vol += vol
+                        if step_vol > 0:
+                            yield from self._new_tip_action()
         else:
             for step_vol, (src, dest) in plan_iter:
                 if self._strategy.new_tip == types.TransferTipPolicy.ALWAYS:
