f1aa5ffe84e4d009fef7334c3a91d67804a27d7e
Sakib Hossain <sakib.hossain@opentrons.com>
2022-03-23 10:28:13 -0400

    feat(app): add test shake slideout (#9680)
    * feat(app): add test shake slideout
    
    Adds the heater shaker test shake slideout that can be accessed through the module card drop down
    menu.
    
    closes #9317

---
+++ app/src/organisms/Devices/ModuleCard/TestShakeSlideout.tsx
@@ -0,0 +1,312 @@
+import * as React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useCreateLiveCommandMutation } from '@opentrons/react-api-client'
+import {
+  Flex,
+  Text,
+  TYPOGRAPHY,
+  SPACING,
+  COLORS,
+  DIRECTION_COLUMN,
+  Icon,
+  DIRECTION_ROW,
+  TEXT_TRANSFORM_CAPITALIZE,
+  SIZE_AUTO,
+  ALIGN_FLEX_START,
+  Link,
+  Tooltip,
+  useHoverTooltip,
+} from '@opentrons/components'
+import {
+  getModuleDisplayName,
+  HS_RPM_MAX,
+  HS_RPM_MIN,
+  RPM,
+} from '@opentrons/shared-data'
+import { Slideout } from '../../../atoms/Slideout'
+import { PrimaryButton, TertiaryButton } from '../../../atoms/Buttons'
+import { HeaterShakerModuleCard } from '../HeaterShakerWizard/HeaterShakerModuleCard'
+import { Divider } from '../../../atoms/structure'
+import { CollapsibleStep } from '../../ProtocolSetup/RunSetupCard/CollapsibleStep'
+import { InputField } from '../../../atoms/InputField'
+import { HeaterShakerWizard } from '../HeaterShakerWizard'
+
+import type { HeaterShakerModule } from '../../../redux/modules/types'
+import type {
+  HeaterShakerSetTargetShakeSpeedCreateCommand,
+  HeaterShakerStopShakeCreateCommand,
+  HeaterShakerOpenLatchCreateCommand,
+  HeaterShakerCloseLatchCreateCommand,
+} from '@opentrons/shared-data/protocol/types/schemaV6/command/module'
+
+interface TestShakeSlideoutProps {
+  module: HeaterShakerModule
+  onCloseClick: () => unknown
+  isExpanded: boolean
+}
+
+export const TestShakeSlideout = (
+  props: TestShakeSlideoutProps
+): JSX.Element | null => {
+  const { module, onCloseClick, isExpanded } = props
+  const { t } = useTranslation(['device_details', 'shared', 'heater_shaker'])
+  const { createLiveCommand } = useCreateLiveCommandMutation()
+  const name = getModuleDisplayName(module.model)
+  const [targetProps, tooltipProps] = useHoverTooltip()
+
+  const [showCollapsed, setShowCollapsed] = React.useState(false)
+  const [shakeValue, setShakeValue] = React.useState<string | null>(null)
+  const [showWizard, setShowWizard] = React.useState<boolean>(false)
+  const isShaking = module.data.speedStatus !== 'idle'
+  const isLatchOpen =
+    module.data.labwareLatchStatus === 'idle_open' ||
+    module.data.labwareLatchStatus === 'opening'
+
+  const setLatchCommand:
+    | HeaterShakerOpenLatchCreateCommand
+    | HeaterShakerCloseLatchCreateCommand = {
+    commandType: isLatchOpen
+      ? 'heaterShakerModule/closeLatch'
+      : 'heaterShakerModule/openLatch',
+    params: {
+      moduleId: module.id,
+    },
+  }
+
+  const setShakeCommand: HeaterShakerSetTargetShakeSpeedCreateCommand = {
+    commandType: 'heaterShakerModule/setTargetShakeSpeed',
+    params: {
+      moduleId: module.id,
+      rpm: shakeValue !== null ? parseInt(shakeValue) : 0,
+    },
+  }
+
+  const stopShakeCommand: HeaterShakerStopShakeCreateCommand = {
+    commandType: 'heaterShakerModule/stopShake',
+    params: {
+      moduleId: module.id,
+    },
+  }
+
+  const handleLatchCommand = (): void => {
+    createLiveCommand({
+      command: setLatchCommand,
+    }).catch((e: Error) => {
+      console.error(
+        `error setting module status with command type ${setLatchCommand.commandType}: ${e.message}`
+      )
+    })
+  }
+
+  const handleShakeCommand = (): void => {
+    if (shakeValue !== null) {
+      createLiveCommand({
+        command: isShaking ? stopShakeCommand : setShakeCommand,
+      }).catch((e: Error) => {
+        console.error(
+          `error setting module status with command type ${
+            stopShakeCommand.commandType ?? setShakeCommand.commandType
+          }: ${e.message}`
+        )
+      })
+    }
+    setShakeValue(null)
+  }
+
+  const errorMessage =
+    shakeValue != null &&
+    (parseInt(shakeValue) < HS_RPM_MIN || parseInt(shakeValue) > HS_RPM_MAX)
+      ? t('input_out_of_range')
+      : null
+
+  return (
+    <Slideout
+      title={t('test_shake', { ns: 'heater_shaker' })}
+      onCloseClick={onCloseClick}
+      isExpanded={isExpanded}
+      height={`calc(100vh - ${SPACING.spacing4})`} // subtract breadcrumb strip
+      footer={
+        <PrimaryButton
+          textTransform={TEXT_TRANSFORM_CAPITALIZE}
+          width="100%"
+          onClick={onCloseClick}
+          data-testid={`Temp_Slideout_set_temp_btn_${name}`}
+        >
+          {t('close', { ns: 'shared' })}
+        </PrimaryButton>
+      }
+    >
+      <Flex
+        borderRadius={SPACING.spacingS}
+        marginBottom={SPACING.spacing3}
+        backgroundColor={COLORS.background}
+        paddingY={SPACING.spacing4}
+        paddingX={SPACING.spacing4}
+        flexDirection={DIRECTION_ROW}
+        data-testid={'test_shake_slideout_banner_info'}
+      >
+        <Flex color={COLORS.darkGreyEnabled}>
+          <Icon
+            name="information"
+            size={SPACING.spacing6}
+            paddingBottom={SPACING.spacing4}
+            aria-label="information"
+          />
+        </Flex>
+        <Flex flexDirection={DIRECTION_COLUMN} fontSize={TYPOGRAPHY.fontSizeP}>
+          <Text fontWeight={TYPOGRAPHY.fontWeightRegular}>
+            {t('test_shake_slideout_banner_info', { ns: 'heater_shaker' })}
+          </Text>
+        </Flex>
+      </Flex>
+      <Flex
+        border={`${SPACING.spacingXXS} solid ${COLORS.medGrey}`}
+        borderRadius={SPACING.spacing2}
+        flexDirection={DIRECTION_COLUMN}
+        fontWeight={TYPOGRAPHY.fontWeightRegular}
+        padding={`${SPACING.spacing4} ${SPACING.spacingM} ${SPACING.spacingM} ${SPACING.spacing4}`}
+        width="100%"
+        marginBottom={SPACING.spacing3}
+      >
+        <Text
+          fontSize={TYPOGRAPHY.fontSizeP}
+          fontWeight={TYPOGRAPHY.fontWeightSemiBold}
+          color={COLORS.darkBlack}
+        >
+          {t('module_controls')}
+        </Text>
+        <Flex marginTop={SPACING.spacing3}>
+          <HeaterShakerModuleCard module={module} />
+        </Flex>
+        <Flex
+          flexDirection={DIRECTION_ROW}
+          marginY={SPACING.spacingSM}
+          alignItems={ALIGN_FLEX_START}
+        >
+          <Flex flexDirection={DIRECTION_ROW} marginTop={SPACING.spacing3}>
+            <Text
+              fontSize={TYPOGRAPHY.fontSizeP}
+              fontWeight={TYPOGRAPHY.fontWeightSemiBold}
+              color={COLORS.darkBlack}
+            >
+              {t('labware_latch', { ns: 'heater_shaker' })}
+            </Text>
+          </Flex>
+          <TertiaryButton
+            textTransform={TEXT_TRANSFORM_CAPITALIZE}
+            fontSize={TYPOGRAPHY.fontSizeCaption}
+            marginLeft={SIZE_AUTO}
+            paddingX={SPACING.spacing4}
+            onClick={handleLatchCommand}
+            disabled={isShaking}
+            {...targetProps}
+          >
+            {isLatchOpen
+              ? t('close', { ns: 'shared' })
+              : t('open', { ns: 'shared' })}
+          </TertiaryButton>
+          {isShaking ? (
+            <Tooltip {...tooltipProps}>
+              {t('cannot_open_latch', { ns: 'heater_shaker' })}
+            </Tooltip>
+          ) : null}
+        </Flex>
+        <Divider color={COLORS.medGrey} />
+        <Text
+          fontSize={TYPOGRAPHY.fontSizeP}
+          fontWeight={TYPOGRAPHY.fontWeightSemiBold}
+          color={COLORS.darkBlack}
+          marginTop={SPACING.spacing4}
+        >
+          {t('shake_speed', { ns: 'heater_shaker' })}
+        </Text>
+        <Flex flexDirection={DIRECTION_ROW} alignItems={ALIGN_FLEX_START}>
+          <Flex
+            flexDirection={DIRECTION_COLUMN}
+            marginTop={SPACING.spacing3}
+            paddingRight={SPACING.spacing4}
+          >
+            <InputField
+              data-testid={`TestShakeSlideout_shake_input`}
+              units={RPM}
+              value={shakeValue}
+              onChange={e => setShakeValue(e.target.value)}
+              type="number"
+              caption={t('min_max_rpm', {
+                ns: 'heater_shaker',
+                min: HS_RPM_MIN,
+                max: HS_RPM_MAX,
+              })}
+              error={errorMessage}
+            />
+            <Text
+              color={COLORS.darkGreyEnabled}
+              fontSize={TYPOGRAPHY.fontSizeCaption}
+            ></Text>
+          </Flex>
+          <TertiaryButton
+            textTransform={TEXT_TRANSFORM_CAPITALIZE}
+            marginLeft={SIZE_AUTO}
+            marginTop={SPACING.spacing3}
+            paddingX={SPACING.spacing4}
+            onClick={handleShakeCommand}
+            disabled={isLatchOpen}
+            {...targetProps}
+          >
+            {isShaking
+              ? t('stop', { ns: 'shared' })
+              : t('start', { ns: 'shared' })}
+          </TertiaryButton>
+          {isLatchOpen ? (
+            <Tooltip {...tooltipProps}>
+              {t('cannot_shake', { ns: 'heater_shaker' })}
+            </Tooltip>
+          ) : null}
+        </Flex>
+      </Flex>
+      <Flex
+        border={`${SPACING.spacingXXS} solid ${COLORS.medGrey}`}
+        borderRadius={SPACING.spacing2}
+        flexDirection={DIRECTION_COLUMN}
+        fontWeight={TYPOGRAPHY.fontWeightRegular}
+        paddingY={SPACING.spacing4}
+        width="100%"
+      >
+        <CollapsibleStep
+          expanded={showCollapsed}
+          title={
+            <Text
+              textTransform={TEXT_TRANSFORM_CAPITALIZE}
+              fontSize={TYPOGRAPHY.fontSizeP}
+            >
+              {t('troubleshooting', { ns: 'heater_shaker' })}
+            </Text>
+          }
+          expandedIcon="chevron-up"
+          collapsedIcon="chevron-down"
+          toggleExpanded={() =>
+            setShowCollapsed(showCollapsed => !showCollapsed)
+          }
+        >
+          <Text fontSize={TYPOGRAPHY.fontSizeP} marginTop={SPACING.spacing4}>
+            {t('test_shake_troubleshooting_slideout_description', {
+              ns: 'heater_shaker',
+            })}
+          </Text>
+          {showWizard && (
+            <HeaterShakerWizard onCloseClick={() => setShowWizard(false)} />
+          )}
+          <Link
+            fontSize={TYPOGRAPHY.fontSizeP}
+            fontWeight={TYPOGRAPHY.fontWeightSemiBold}
+            color={COLORS.blue}
+            id={'HeaterShaker_Attachment_Instructions'}
+            onClick={() => setShowWizard(true)}
+          >
+            {t('go_to_attachment_instructions', { ns: 'heater_shaker' })}
+          </Link>
+        </CollapsibleStep>
+      </Flex>
+    </Slideout>
+  )
+}
---
+++ app/src/organisms/Devices/ModuleCard/__tests__/TestShakeSlideout.test.tsx
@@ -0,0 +1,225 @@
+import * as React from 'react'
+import { i18n } from '../../../../i18n'
+import { fireEvent } from '@testing-library/react'
+import { useCreateLiveCommandMutation } from '@opentrons/react-api-client'
+import { renderWithProviders } from '@opentrons/components'
+import { TestShakeSlideout } from '../TestShakeSlideout'
+import { HeaterShakerModuleCard } from '../../HeaterShakerWizard/HeaterShakerModuleCard'
+import { mockHeaterShaker } from '../../../../redux/modules/__fixtures__'
+
+jest.mock('@opentrons/react-api-client')
+jest.mock('../../HeaterShakerWizard/HeaterShakerModuleCard')
+
+const mockUseLiveCommandMutation = useCreateLiveCommandMutation as jest.MockedFunction<
+  typeof useCreateLiveCommandMutation
+>
+const mockHeaterShakerModuleCard = HeaterShakerModuleCard as jest.MockedFunction<
+  typeof HeaterShakerModuleCard
+>
+
+const render = (props: React.ComponentProps<typeof TestShakeSlideout>) => {
+  return renderWithProviders(<TestShakeSlideout {...props} />, {
+    i18nInstance: i18n,
+  })[0]
+}
+
+const mockOpenLatchHeaterShaker = {
+  model: 'heaterShakerModuleV1',
+  type: 'heaterShakerModuleType',
+  port: '/dev/ot_module_heatershaker0',
+  serial: 'jkl123',
+  revision: 'heatershaker_v4.0',
+  fwVersion: 'v2.0.0',
+  status: 'idle',
+  hasAvailableUpdate: true,
+  data: {
+    labwareLatchStatus: 'idle_open',
+    speedStatus: 'idle',
+    temperatureStatus: 'idle',
+    currentSpeed: null,
+    currentTemp: null,
+    targetSpeed: null,
+    targetTemp: null,
+    errorDetails: null,
+  },
+  usbPort: { hub: 1, port: 1 },
+} as any
+
+const mockMovingHeaterShaker = {
+  model: 'heaterShakerModuleV1',
+  type: 'heaterShakerModuleType',
+  port: '/dev/ot_module_heatershaker0',
+  serial: 'jkl123',
+  revision: 'heatershaker_v4.0',
+  fwVersion: 'v2.0.0',
+  status: 'idle',
+  hasAvailableUpdate: true,
+  data: {
+    labwareLatchStatus: 'idle_closed',
+    speedStatus: 'speeding up',
+    temperatureStatus: 'idle',
+    currentSpeed: null,
+    currentTemp: null,
+    targetSpeed: null,
+    targetTemp: null,
+    errorDetails: null,
+  },
+  usbPort: { hub: 1, port: 1 },
+} as any
+
+const mockCloseLatchHeaterShaker = {
+  model: 'heaterShakerModuleV1',
+  type: 'heaterShakerModuleType',
+  port: '/dev/ot_module_heatershaker0',
+  serial: 'jkl123',
+  revision: 'heatershaker_v4.0',
+  fwVersion: 'v2.0.0',
+  status: 'idle',
+  hasAvailableUpdate: true,
+  data: {
+    labwareLatchStatus: 'idle_closed',
+    speedStatus: 'idle',
+    temperatureStatus: 'idle',
+    currentSpeed: null,
+    currentTemp: null,
+    targetSpeed: null,
+    targetTemp: null,
+    errorDetails: null,
+  },
+  usbPort: { hub: 1, port: 1 },
+} as any
+
+describe('TestShakeSlideout', () => {
+  let props: React.ComponentProps<typeof TestShakeSlideout>
+  let mockCreateLiveCommand = jest.fn()
+  beforeEach(() => {
+    props = {
+      module: mockHeaterShaker,
+      onCloseClick: jest.fn(),
+      isExpanded: true,
+    }
+    mockCreateLiveCommand = jest.fn()
+    mockCreateLiveCommand.mockResolvedValue(null)
+    mockUseLiveCommandMutation.mockReturnValue({
+      createLiveCommand: mockCreateLiveCommand,
+    } as any)
+    mockHeaterShakerModuleCard.mockReturnValue(
+      <div>Mock Heater Shaker Module Card</div>
+    )
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('renders the slideout information banner icon and description', () => {
+    const { getByText, getByLabelText } = render(props)
+    getByLabelText('information')
+    getByText(
+      'If you want to add labware to the module before doing a test shake, you can use the labware latch controls to hold the latches open.'
+    )
+  })
+
+  it('renders module controls and a heater shaker module card', () => {
+    const { getByText } = render(props)
+
+    getByText('Module Controls')
+    getByText('Mock Heater Shaker Module Card')
+  })
+
+  it('renders the labware latch open button', () => {
+    const { getByRole, getByText } = render(props)
+    getByText('Labware Latch')
+    const button = getByRole('button', { name: /Open/i })
+    expect(button).toBeEnabled()
+  })
+
+  it('renders a start button for speed setting', () => {
+    const { getByText, getByRole } = render(props)
+
+    getByText('Shake speed')
+
+    const button = getByRole('button', { name: /Start/i })
+    expect(button).toBeEnabled()
+  })
+
+  it('renders a troubleshoot accordion and contents when it is clicked', () => {
+    const { getByText } = render(props)
+
+    const troubleshooting = getByText('Troubleshooting')
+    fireEvent.click(troubleshooting)
+
+    getByText(
+      'Revisit instructions for attaching the module to the deck as well as attaching the thermal adapter.'
+    )
+    getByText('Go to attachment instructions')
+  })
+
+  it('start shake button should be disabled if the labware latch is open', () => {
+    props = {
+      module: mockOpenLatchHeaterShaker,
+      onCloseClick: jest.fn(),
+      isExpanded: true,
+    }
+
+    const { getByRole } = render(props)
+    const button = getByRole('button', { name: /Start/i })
+    expect(button).toBeDisabled()
+  })
+
+  it('open latch button should be disabled if the module is shaking', () => {
+    props = {
+      module: mockMovingHeaterShaker,
+      onCloseClick: jest.fn(),
+      isExpanded: true,
+    }
+
+    const { getByRole } = render(props)
+    const button = getByRole('button', { name: /Open/i })
+    expect(button).toBeDisabled()
+  })
+
+  it('renders the open labware latch button and clicking it opens the latch', () => {
+    props = {
+      module: mockCloseLatchHeaterShaker,
+      onCloseClick: jest.fn(),
+      isExpanded: true,
+    }
+
+    const { getByRole } = render(props)
+    const button = getByRole('button', { name: /Open/i })
+    fireEvent.click(button)
+    expect(mockCreateLiveCommand).toHaveBeenCalledWith({
+      command: {
+        commandType: 'heaterShakerModule/openLatch',
+        params: {
+          moduleId: mockCloseLatchHeaterShaker.id,
+        },
+      },
+    })
+  })
+
+  it('entering an input for shake speed and clicking start should begin shaking', () => {
+    props = {
+      module: mockHeaterShaker,
+      onCloseClick: jest.fn(),
+      isExpanded: true,
+    }
+
+    const { getByRole } = render(props)
+    const button = getByRole('button', { name: /Start/i })
+    const input = getByRole('spinbutton')
+    fireEvent.change(input, { target: { value: '300' } })
+    fireEvent.click(button)
+
+    expect(mockCreateLiveCommand).toHaveBeenCalledWith({
+      command: {
+        commandType: 'heaterShakerModule/setTargetShakeSpeed',
+        params: {
+          moduleId: 'heatershaker_id',
+          rpm: 300,
+        },
+      },
+    })
+  })
+})
