9dbaa0f35142903e0d6f2b20f31f738da4e66bed
Max Marrone <max@marrone.nyc>
2022-03-23 18:46:41 -0400

    More WIP

--- robot-server/robot_server/protocols/protocol_store.py
+++ robot-server/robot_server/protocols/protocol_store.py
@@ -2,11 +2,35 @@
 from dataclasses import dataclass
 from datetime import datetime
 from logging import getLogger
-from typing import Dict, List
+from typing import Any, Dict, List
+
+import sqlalchemy
 
 from opentrons.protocol_reader import ProtocolSource
 
+
 log = getLogger(__name__)
+
+
+# TODO: Exposing metadata from this module won't scale when we have multiple store
+# classes each with their own table. We'll need a way of merging every store's
+# Table into a single MetaData, I think?
+metadata = sqlalchemy.MetaData()
+
+_protocol_table = sqlalchemy.Table(
+    "protocol",
+    metadata,
+    sqlalchemy.Column(
+        "id",
+        sqlalchemy.String,
+        primary_key=True,
+    ),
+    sqlalchemy.Column(
+        "created_at",
+        sqlalchemy.DateTime,
+        nullable=False,
+    )
+)
 
 
 @dataclass(frozen=True)
@@ -67,3 +91,61 @@ class ProtocolStore:
             )
 
         return entry
+
+
+@dataclass(frozen=True)
+class ProtocolRecord:
+    protocol_id: str
+    created_at: datetime
+
+
+# TODO: Make all methods async, probably.
+class ProtocolRecordStore:
+    def __init__(self, sql_engine: sqlalchemy.engine.Engine) -> None:
+        self._sql_engine = sql_engine
+
+    def insert(self, protocol_record: ProtocolRecord) -> None:
+        statement = sqlalchemy.insert(_protocol_table).values(
+            id=protocol_record.protocol_id,
+            created_at=protocol_record.created_at
+        )
+        with self._sql_engine.begin() as transaction:
+            # TODO: If I wanted to catch an ID-already-used conflict here,
+            #       how would I do that?
+            # TODO: Will this raise if there was a problem with the insert, or do I
+            #       have to inspect the result somehow?
+            transaction.execute(statement)
+
+    def get(self, protocol_id: str) -> ProtocolRecord:
+        # TODO: Could I use _protocol_table.c.id here instead of _id_column,
+        # or would that lose us some refactor safety?
+        statement = sqlalchemy.select(_protocol_table).where(_protocol_table.c.id==protocol_id)
+        with self._sql_engine.begin() as transaction:
+            try:
+                result = transaction.execute(statement).one()
+            except sqlalchemy.exc.NoResultFound as e:
+                raise ProtocolNotFoundError(protocol_id=protocol_id) from e
+        return self._to_record(sql_result=result)
+
+    def get_all(self, protocol_id: str) -> List[ProtocolRecord]:
+        statement = sqlalchemy.select(_protocol_table)
+        with self._sql_engine.begin() as transaction:
+            results = transaction.execute(statement).all()
+        # TODO: We're iterating over result rows after the transaction has been closed.
+        # Is this legal and safe? Is the results list built eagerly?
+        return [self._to_record(result) for result in results]
+
+    def _to_record(self, sql_result: sqlalchemy.engine.Row) -> ProtocolRecord:
+        # TODO: Any way these types can be inferred? Investigate sqlalchemy-stubs,
+        # reconsider if light ORM use would be good for us.
+        protocol_id = sql_result.id
+        assert isinstance(protocol_id, str)
+        created_at = sql_result.created_at
+        assert isinstance(created_at, datetime)
+        return ProtocolRecord(protocol_id=protocol_id, created_at=created_at)
+
+
+class ProtocolFileStore:
+    pass
+
+
