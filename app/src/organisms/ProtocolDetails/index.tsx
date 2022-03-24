5a07e66175ad469c7f99d9373a1b01f707513933
Brian Cooper <brian@opentrons.com>
2022-03-24 18:11:32 -0400

    add in protocol detail page rounded tabs

--- app/src/organisms/ProtocolDetails/index.tsx
+++ app/src/organisms/ProtocolDetails/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import first from 'lodash/first'
 import { format } from 'date-fns'
+import { css } from 'styled-components'
 import { useTranslation } from 'react-i18next'
 import {
   getModuleType,
@@ -9,11 +10,18 @@ import {
 } from '@opentrons/shared-data'
 import {
   Box,
+  Btn,
   Flex,
   DIRECTION_ROW,
   SPACING,
   TYPOGRAPHY,
+  COLORS,
+  BORDERS,
+  SIZE_1,
   DIRECTION_COLUMN,
+  POSITION_ABSOLUTE,
+  POSITION_RELATIVE,
+  DISPLAY_BLOCK,
   Link,
   Card,
   JUSTIFY_SPACE_BETWEEN,
@@ -33,6 +41,57 @@ import type { ProtocolAnalysisFile } from '@opentrons/shared-data'
 import { OverflowBtn } from '../../atoms/MenuList/OverflowBtn'
 import { Divider } from '../../atoms/structure'
 
+const defaultTabStyle = css`
+  ${TYPOGRAPHY.pSemiBold}
+  border-radius: ${BORDERS.radiusSoftCorners} ${BORDERS.radiusSoftCorners} 0 0;
+  border-top: ${BORDERS.transparentLineBorder};
+  border-left: ${BORDERS.transparentLineBorder};
+  border-right: ${BORDERS.transparentLineBorder};
+  color: ${COLORS.darkGreyEnabled};
+  padding: ${SPACING.spacing3} ${SPACING.spacing4};
+  position: ${POSITION_RELATIVE};
+`
+
+const currentTabStyle = css`
+  background-color: ${COLORS.white};
+  border-top: ${BORDERS.lineBorder};
+  border-left: ${BORDERS.lineBorder};
+  border-right: ${BORDERS.lineBorder};
+  color: ${COLORS.blue};
+
+  /* extend below the tab when active to flow into the content */
+  &:after {
+    position: ${POSITION_ABSOLUTE};
+    display: ${DISPLAY_BLOCK};
+    content: '';
+    background-color: ${COLORS.white};
+    top: 100;
+    left: 0;
+    height: ${SIZE_1};
+    width: 100%;
+  }
+`
+interface RoundTabProps extends React.ComponentProps<typeof Btn> {
+  isCurrent: boolean
+}
+function RoundTab({isCurrent, children, ...restProps}: RoundTabProps): JSX.Element {
+  return (
+    <Btn
+      {...restProps}
+      css={
+        isCurrent
+          ? css`
+            ${defaultTabStyle}
+            ${currentTabStyle}
+          `
+          : defaultTabStyle
+      }
+    >
+      {children}
+    </Btn>
+  )
+}
+
 interface ProtocolDetailsProps extends StoredProtocolData {}
 
 export function ProtocolDetails(
@@ -44,6 +103,9 @@ export function ProtocolDetails(
     protocolData,
     setProtocolData,
   ] = React.useState<ProtocolAnalysisFile<{}> | null>(null)
+  const [currentTab, setCurrentTab] = React.useState<
+    'robot_config' | 'labware'
+  >('robot_config')
 
   React.useEffect(() => {
     if (mostRecentAnalysis != null) {
@@ -71,6 +133,63 @@ export function ProtocolDetails(
   const lastAnalyzed = ''
   const author = ''
   const description = ''
+
+
+  const getTabContents = () => (
+    currentTab === 'labware'
+    ?  <Box>TODO: labware tab contents</Box>
+    : (
+    <Flex flexDirection={DIRECTION_COLUMN}>
+              <Flex
+                flexDirection={DIRECTION_ROW}
+                marginRight={SPACING.spacing4}
+              >
+                <StyledText as="h6">{t('robot')}</StyledText>
+                <StyledText as="p">{robotModel}</StyledText>
+              </Flex>
+              <Flex
+                flexDirection={DIRECTION_ROW}
+                marginRight={SPACING.spacing4}
+              >
+                <StyledText as="h6">{t('left_mount')}</StyledText>
+                <StyledText as="p">
+                  {leftMountPipetteName != null
+                    ? getPipetteNameSpecs(leftMountPipetteName)?.displayName
+                    : t('empty')}
+                </StyledText>
+              </Flex>
+              <Flex
+                flexDirection={DIRECTION_ROW}
+                marginRight={SPACING.spacing4}
+              >
+                <StyledText as="h6">{t('right_mount')}</StyledText>
+                <StyledText as="p">
+                  {rightMountPipetteName != null
+                    ? getPipetteNameSpecs(rightMountPipetteName)?.displayName
+                    : t('empty')}
+                </StyledText>
+              </Flex>
+              {requiredModuleTypes.length > 0 ? (
+                <Flex
+                  flexDirection={DIRECTION_COLUMN}
+                  marginRight={SPACING.spacing4}
+                >
+                  <StyledText as="h6">{t('modules')}</StyledText>
+                  <Flex>
+                    {requiredModuleTypes.map((moduleType, index) => (
+                      <ModuleIcon
+                        key={index}
+                        moduleType={moduleType}
+                        height="1rem"
+                        marginRight={SPACING.spacing3}
+                      />
+                    ))}
+                  </Flex>
+                </Flex>
+              ) : null}
+            </Flex>
+    )
+  )
 
   return (
     <Flex
@@ -164,50 +283,27 @@ export function ProtocolDetails(
         </Card>
         <Box height="100%" width={SPACING.spacing4} />
         <Card flex="1">
-          {t('labware')}
-          {t('liquids')}
-          {t('robot_configuration')}
-
-          <Flex flexDirection={DIRECTION_COLUMN}>
-            <Flex flexDirection={DIRECTION_ROW} marginRight={SPACING.spacing4}>
-              <StyledText as="h6">{t('robot')}</StyledText>
-              <StyledText as="p">{robotModel}</StyledText>
-            </Flex>
-            <Flex flexDirection={DIRECTION_ROW} marginRight={SPACING.spacing4}>
-              <StyledText as="h6">{t('left_mount')}</StyledText>
-              <StyledText as="p">
-                {leftMountPipetteName != null
-                  ? getPipetteNameSpecs(leftMountPipetteName)?.displayName
-                  : t('empty')}
-              </StyledText>
-            </Flex>
-            <Flex flexDirection={DIRECTION_ROW} marginRight={SPACING.spacing4}>
-              <StyledText as="h6">{t('right_mount')}</StyledText>
-              <StyledText as="p">
-                {rightMountPipetteName != null
-                  ? getPipetteNameSpecs(rightMountPipetteName)?.displayName
-                  : t('empty')}
-              </StyledText>
-            </Flex>
-            {requiredModuleTypes.length > 0 ? (
-              <Flex
-                flexDirection={DIRECTION_COLUMN}
-                marginRight={SPACING.spacing4}
-              >
-                <StyledText as="h6">{t('modules')}</StyledText>
-                <Flex>
-                  {requiredModuleTypes.map((moduleType, index) => (
-                    <ModuleIcon
-                      key={index}
-                      moduleType={moduleType}
-                      height="1rem"
-                      marginRight={SPACING.spacing3}
-                    />
-                  ))}
-                </Flex>
-              </Flex>
-            ) : null}
+          <Flex>
+            <RoundTab isCurrent={currentTab === 'robot_config'} onClick={() => setCurrentTab('robot_config')}>
+              {t('robot_configuration')}
+            </RoundTab>
+            <RoundTab isCurrent={currentTab === 'labware'} onClick={() => setCurrentTab('labware')}>
+              {t('labware')}
+            </RoundTab>
           </Flex>
+          <Box
+            backgroundColor={COLORS.white}
+            border={`${SPACING.spacingXXS} ${BORDERS.styleSolid} ${COLORS.medGrey}`}
+            // remove left upper corner border radius when first tab is active
+            borderRadius={`${
+              currentTab === 'robot_config' ? '0' : BORDERS.radiusSoftCorners
+            } ${BORDERS.radiusSoftCorners} ${BORDERS.radiusSoftCorners} ${
+              BORDERS.radiusSoftCorners
+            }`}
+            padding={`${SPACING.spacing5} ${SPACING.spacing4}`}
+          >
+            {getTabContents()}
+          </Box>
         </Card>
       </Flex>
     </Flex>
