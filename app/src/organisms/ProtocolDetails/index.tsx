9ee8d5fe5dd2c9c3fba66b377ed50c503cab1c26
Brian Cooper <brian@opentrons.com>
2022-03-24 17:29:56 -0400

    build out scaffold for protocol details

--- app/src/organisms/ProtocolDetails/index.tsx
+++ app/src/organisms/ProtocolDetails/index.tsx
@@ -8,14 +8,16 @@ import {
   schemaV6Adapter,
 } from '@opentrons/shared-data'
 import {
+  Box,
   Flex,
   DIRECTION_ROW,
-  COLORS,
   SPACING,
-  JUSTIFY_SPACE_BETWEEN,
+  TYPOGRAPHY,
   DIRECTION_COLUMN,
-  JUSTIFY_CENTER,
-  ALIGN_CENTER,
+  Link,
+  Card,
+  JUSTIFY_SPACE_BETWEEN,
+  TEXT_TRANSFORM_CAPITALIZE,
 } from '@opentrons/components'
 import {
   parseInitialPipetteNamesByMount,
@@ -24,15 +26,20 @@ import {
 
 import { StoredProtocolData } from '../../redux/protocol-storage'
 import { StyledText } from '../../atoms/text'
+import { PrimaryButton } from '../../atoms/Buttons'
 import { ModuleIcon } from '../../molecules/ModuleIcon'
 import { DeckThumbnail } from '../../molecules/DeckThumbnail'
 import type { ProtocolAnalysisFile } from '@opentrons/shared-data'
+import { OverflowBtn } from '../../atoms/MenuList/OverflowBtn'
+import { Divider } from '../../atoms/structure'
 
 interface ProtocolDetailsProps extends StoredProtocolData {}
 
-export function ProtocolDetails(props: ProtocolDetailsProps): JSX.Element {
+export function ProtocolDetails(
+  props: ProtocolDetailsProps
+): JSX.Element | null {
   const { protocolKey, srcFileNames, mostRecentAnalysis, modified } = props
-  const { t } = useTranslation('protocol_list')
+  const { t } = useTranslation('protocol_details')
   const [
     protocolData,
     setProtocolData,
@@ -46,6 +53,7 @@ export function ProtocolDetails(props: ProtocolDetailsProps): JSX.Element {
     }
   }, [modified])
 
+  if (protocolData == null) return null
   // TODO: IMMEDIATELY clean up and move these protocol data selectors into api_client as
   // pure functions of RunTimeCommand[]
   const robotModel = protocolData?.robot?.model ?? 'OT-2'
@@ -59,47 +67,113 @@ export function ProtocolDetails(props: ProtocolDetailsProps): JSX.Element {
   const protocolName =
     protocolData?.metadata?.protocolName ?? first(srcFileNames) ?? protocolKey
 
+  const creationMethod = ''
+  const lastAnalyzed = ''
+  const author = ''
+  const description = ''
 
   return (
     <Flex
-      backgroundColor={COLORS.white}
-      border={`1px solid ${COLORS.medGrey}`}
-      borderRadius="4px"
-      flexDirection={DIRECTION_ROW}
-      marginBottom={SPACING.spacing3}
+      flexDirection={DIRECTION_COLUMN}
       padding={SPACING.spacing4}
-      justifyContent={JUSTIFY_SPACE_BETWEEN}
       width="100%"
-      position="relative"
     >
-      <Flex>
+      <Card marginBottom={SPACING.spacing4} padding={SPACING.spacing4}>
         <Flex
-          marginRight={SPACING.spacing4}
-          height="6rem"
-          width="6rem"
-          justifyContent={JUSTIFY_CENTER}
-          alignItems={ALIGN_CENTER}
+          flexDirection={DIRECTION_ROW}
+          justifyContent={JUSTIFY_SPACE_BETWEEN}
         >
-          {protocolData != null ? (
-            <DeckThumbnail analysis={protocolData} />
-          ) : null}
-        </Flex>
-        <Flex flexDirection={DIRECTION_COLUMN} marginRight={SPACING.spacing4}>
           <StyledText as="h3" marginBottom={SPACING.spacing4} height="2.75rem">
             {protocolName}
           </StyledText>
-          <Flex>
-            <Flex
-              flexDirection={DIRECTION_COLUMN}
-              marginRight={SPACING.spacing4}
+          <OverflowBtn
+            onClick={() => console.log('TODO: open overflow menu')}
+          />
+        </Flex>
+        <Flex
+          flexDirection={DIRECTION_ROW}
+          justifyContent={JUSTIFY_SPACE_BETWEEN}
+        >
+          <Flex flexDirection={DIRECTION_COLUMN} marginRight={SPACING.spacing4}>
+            <StyledText as="h6">{t('creation_method')}</StyledText>
+            <StyledText as="p">{creationMethod}</StyledText>
+          </Flex>
+          <Flex flexDirection={DIRECTION_COLUMN} marginRight={SPACING.spacing4}>
+            <StyledText as="h6">{t('last_updated')}</StyledText>
+            <StyledText as="p">
+              {format(new Date(modified), 'MMMM dd, yyyy HH:mm')}
+            </StyledText>
+          </Flex>
+          <Flex flexDirection={DIRECTION_COLUMN} marginRight={SPACING.spacing4}>
+            <StyledText as="h6">{t('last_analyzed')}</StyledText>
+            <StyledText as="p">{lastAnalyzed}</StyledText>
+          </Flex>
+          <PrimaryButton
+            onClick={() => console.log('TODO: open run on a robot slideout')}
+          >
+            {t('run_protocol')}
+          </PrimaryButton>
+        </Flex>
+        <Divider marginY={SPACING.spacing4} />
+        <Flex flexDirection={DIRECTION_ROW}>
+          <Flex
+            flex="1"
+            flexDirection={DIRECTION_COLUMN}
+            marginRight={SPACING.spacing4}
+          >
+            <StyledText as="h6">{t('org_or_author')}</StyledText>
+            <StyledText as="p">{author}</StyledText>
+          </Flex>
+          <Flex
+            flex="1"
+            flexDirection={DIRECTION_COLUMN}
+            marginRight={SPACING.spacing4}
+          >
+            <StyledText as="h6">{t('description')}</StyledText>
+            <StyledText as="p">{description}</StyledText>
+            <Link
+              onClick={() =>
+                console.log(
+                  'TODO: truncate description if more than three lines'
+                )
+              }
             >
+              {t('read_more')}
+            </Link>
+          </Flex>
+        </Flex>
+      </Card>
+
+      <Flex
+        flexDirection={DIRECTION_ROW}
+        justifyContent={JUSTIFY_SPACE_BETWEEN}
+      >
+        <Card flex="1">
+          <StyledText
+            as="h3"
+            fontWeight={TYPOGRAPHY.fontWeightSemiBold}
+            textTransform={TEXT_TRANSFORM_CAPITALIZE}
+            margin={SPACING.spacing4}
+          >
+            {t('deck_setup')}
+          </StyledText>
+          <Divider />
+          <Box padding={SPACING.spacing4}>
+            <DeckThumbnail analysis={protocolData} />
+          </Box>
+        </Card>
+        <Box height="100%" width={SPACING.spacing4} />
+        <Card flex="1">
+          {t('labware')}
+          {t('liquids')}
+          {t('robot_configuration')}
+
+          <Flex flexDirection={DIRECTION_COLUMN}>
+            <Flex flexDirection={DIRECTION_ROW} marginRight={SPACING.spacing4}>
               <StyledText as="h6">{t('robot')}</StyledText>
               <StyledText as="p">{robotModel}</StyledText>
             </Flex>
-            <Flex
-              flexDirection={DIRECTION_COLUMN}
-              marginRight={SPACING.spacing4}
-            >
+            <Flex flexDirection={DIRECTION_ROW} marginRight={SPACING.spacing4}>
               <StyledText as="h6">{t('left_mount')}</StyledText>
               <StyledText as="p">
                 {leftMountPipetteName != null
@@ -107,10 +181,7 @@ export function ProtocolDetails(props: ProtocolDetailsProps): JSX.Element {
                   : t('empty')}
               </StyledText>
             </Flex>
-            <Flex
-              flexDirection={DIRECTION_COLUMN}
-              marginRight={SPACING.spacing4}
-            >
+            <Flex flexDirection={DIRECTION_ROW} marginRight={SPACING.spacing4}>
               <StyledText as="h6">{t('right_mount')}</StyledText>
               <StyledText as="p">
                 {rightMountPipetteName != null
@@ -137,20 +208,7 @@ export function ProtocolDetails(props: ProtocolDetailsProps): JSX.Element {
               </Flex>
             ) : null}
           </Flex>
-        </Flex>
-      </Flex>
-
-      <Flex flexDirection={DIRECTION_COLUMN}>
-        <StyledText
-          as="label"
-          position="absolute"
-          bottom={SPACING.spacing4}
-          right={SPACING.spacing4}
-        >
-          {t('last_updated_at', {
-            date: format(new Date(modified), 'MMMM dd, yyyy HH:mm'),
-          })}
-        </StyledText>
+        </Card>
       </Flex>
     </Flex>
   )
