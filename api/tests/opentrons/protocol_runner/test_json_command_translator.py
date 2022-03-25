1615cfc26336b297fe96ebdbbbe8f98ce50e32d7
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-03-25 09:57:02 -0400

    added labware definition

--- api/tests/opentrons/protocol_runner/test_json_command_translator.py
+++ api/tests/opentrons/protocol_runner/test_json_command_translator.py
@@ -2,7 +2,7 @@
 import pytest
 from typing import Dict, List
 
-from opentrons_shared_data.labware.labware_definition import LabwareDefinition
+from opentrons_shared_data.labware.labware_definition import LabwareDefinition, Parameters, Metadata, DisplayCategory, BrandData, CornerOffsetFromSlot, Dimensions, Group, Metadata1, WellDefinition
 import opentrons_shared_data.protocol.models as json_v6_models
 from opentrons.types import DeckSlotName, MountType
 from opentrons.protocol_runner.json_command_translator import JsonCommandTranslator
@@ -14,6 +14,7 @@ from opentrons.protocol_engine import (
     WellOrigin,
     WellOffset,
     ModuleModel,
+    ModuleLocation
 )
 
 INVALID_TEST_PARAMS = [
@@ -293,6 +294,26 @@ VALID_TEST_PARAMS = [
             )
         ),
     ),
+    (
+        json_v6_models.Command(
+            id="load-labware-command-id-666",
+            commandType="loadLabware",  # used to be delay but is expecting pause
+            params=json_v6_models.Params(
+                labwareId="sourcePlateId",
+                location=json_v6_models.Location(moduleId="temperatureModuleId"),
+            ),
+        ),
+        pe_commands.LoadLabwareCreate(
+            params=pe_commands.LoadLabwareParams(
+                loadName="foo_8_plate_33ul",
+                displayName="Source Plate",
+                labwareId="sourcePlateId",
+                location=ModuleLocation(moduleId="temperatureModuleId"),
+                version=1,
+                namespace="example"
+            )
+        ),
+    ),
 ]
 
 
@@ -303,16 +324,33 @@ def subject() -> JsonCommandTranslator:
 
 
 def _make_json_protocol(
-    *,
-    pipettes: Dict[str, json_v6_models.Pipette] = {
-        "pipetteId": json_v6_models.Pipette(name="p10_single")
-    },
-    labware_definitions: Dict[str, LabwareDefinition] = {},
-    labware: Dict[str, json_v6_models.Labware] = {},
-    commands: List[json_v6_models.Command] = [],
-    modules: Dict[str, json_v6_models.Module] = {
-        "magneticModuleId": json_v6_models.Module(model="magneticModuleV2")
-    }
+        *,
+        pipettes: Dict[str, json_v6_models.Pipette] = {
+            "pipetteId": json_v6_models.Pipette(name="p10_single")
+        },
+        labware_definitions: Dict[str, LabwareDefinition] = {
+            "example/plate/1": json_v6_models.LabwareDefinition(version=1, namespace="example",
+                                                                schemaVersion=2,
+                                                                ordering=[
+                                                                    ["A1", "B1", "C1", "D1"],
+                                                                    ["A2", "B2", "C2", "D2"]
+                                                                  ],
+                                                                groups=[Group(wells=["A1"], metadata=Metadata1())],
+                                                                wells={'A1': WellDefinition(depth=25, x=18.21, y=75.43, z=75, totalLiquidVolume=1100000, shape="rectangular")},
+                                                                dimensions=Dimensions(yDimension=85.5, zDimension=100, xDimension=127.75, depth=25, totalLiquidVolume=33, shape="circular"),
+                                                                cornerOffsetFromSlot=CornerOffsetFromSlot(x=0, y=0, z=0),
+                                                                brand=BrandData(brand="foo"),
+                                                                metadata=Metadata(displayName="Foo 8 Well Plate 33uL",
+                                                                                  displayCategory=DisplayCategory("wellPlate"), displayVolumeUnits="µL"),
+                                                                parameters=Parameters(
+                                                                    loadName="foo_8_plate_33ul", isTiprack=False,
+                                                                    isMagneticModuleCompatible=False, format="irregular"))},
+        labware: Dict[str, json_v6_models.Labware] = {
+            "sourcePlateId": json_v6_models.Labware(displayName="Source Plate", definitionId="example/plate/1")},
+        commands: List[json_v6_models.Command] = [],
+        modules: Dict[str, json_v6_models.Module] = {
+            "magneticModuleId": json_v6_models.Module(model="magneticModuleV2")
+        }
 ) -> json_v6_models.ProtocolSchemaV6:
     """Return a minimal JsonProtocol with the given elements, to use as test input."""
     return json_v6_models.ProtocolSchemaV6(
@@ -332,9 +370,9 @@ def _make_json_protocol(
 
 @pytest.mark.parametrize("test_input, expected_output", VALID_TEST_PARAMS)
 def test_load_command(
-    subject: JsonCommandTranslator,
-    test_input: json_v6_models.Command,
-    expected_output: pe_commands.CommandCreate,
+        subject: JsonCommandTranslator,
+        test_input: json_v6_models.Command,
+        expected_output: pe_commands.CommandCreate,
 ) -> None:
     """Test translating v6 commands to protocol engine commands."""
     output = subject.translate(_make_json_protocol(commands=[test_input]))
@@ -343,9 +381,9 @@ def test_load_command(
 
 @pytest.mark.parametrize("test_input, expected_output", INVALID_TEST_PARAMS)
 def test_invalid_commands(
-    subject: JsonCommandTranslator,
-    test_input: json_v6_models.Command,
-    expected_output: pe_commands.CommandCreate,
+        subject: JsonCommandTranslator,
+        test_input: json_v6_models.Command,
+        expected_output: pe_commands.CommandCreate,
 ) -> None:
     """Test fail invalid payload-translating v6 commands to protocol engine commands."""
     with pytest.raises(AssertionError):
