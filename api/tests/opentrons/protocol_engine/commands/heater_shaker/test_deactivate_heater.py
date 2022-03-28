cab305bed2d5677db3e46985aefb677023a57fad
Mike Cousins <mike@opentrons.com>
2022-03-28 14:46:05 -0400

    wip: refactor deactivate heater and open latch to use equipment handler

--- api/tests/opentrons/protocol_engine/commands/heater_shaker/test_deactivate_heater.py
+++ api/tests/opentrons/protocol_engine/commands/heater_shaker/test_deactivate_heater.py
@@ -1,12 +1,14 @@
 """Test Heater Shaker deactivate heater command implementation."""
-
 from decoy import Decoy
 
-from opentrons.hardware_control import HardwareControlAPI
-from opentrons.hardware_control.modules import AbstractModule, HeaterShaker
+from opentrons.hardware_control.modules import HeaterShaker
 
 from opentrons.protocol_engine.state import StateView
-from opentrons.protocol_engine.state.modules import HeaterShakerModuleView
+from opentrons.protocol_engine.state.modules import (
+    HeaterShakerModuleView,
+    HeaterShakerModuleId,
+)
+from opentrons.protocol_engine.execution import EquipmentHandler
 from opentrons.protocol_engine.commands import heater_shaker
 from opentrons.protocol_engine.commands.heater_shaker.deactivate_heater import (
     DeactivateHeaterImpl,
@@ -14,29 +16,34 @@ from opentrons.protocol_engine.commands.heater_shaker.deactivate_heater import (
 
 
 async def test_deactivate_heater(
-    decoy: Decoy, state_view: StateView, hardware_api: HardwareControlAPI
+    decoy: Decoy,
+    state_view: StateView,
+    equipment: EquipmentHandler,
 ) -> None:
     """It should be able to deactivate the module's heater."""
-    subject = DeactivateHeaterImpl(state_view=state_view, hardware_api=hardware_api)
-
-    data = heater_shaker.DeactivateHeaterParams(moduleId="heater-shaker-id")
+    subject = DeactivateHeaterImpl(state_view=state_view, equipment=equipment)
+    data = heater_shaker.DeactivateHeaterParams(moduleId="input-heater-shaker-id")
 
     # Get module view
+    # Get module view
     hs_module_view = decoy.mock(cls=HeaterShakerModuleView)
+    hs_hardware = decoy.mock(cls=HeaterShaker)
+
     decoy.when(
-        state_view.modules.get_heater_shaker_module_view(module_id="heater-shaker-id")
+        state_view.modules.get_heater_shaker_module_view(
+            module_id="input-heater-shaker-id"
+        )
     ).then_return(hs_module_view)
 
-    # Get attached hardware modules
-    attached = [decoy.mock(cls=AbstractModule), decoy.mock(cls=AbstractModule)]
-    match = decoy.mock(cls=HeaterShaker)
-    decoy.when(hardware_api.attached_modules).then_return(attached)
-
-    # Get stubbed hardware module from hs module view
-    decoy.when(hs_module_view.find_hardware(attached_modules=attached)).then_return(
-        match
+    decoy.when(hs_module_view.module_id).then_return(
+        HeaterShakerModuleId("heater-shaker-id")
     )
 
+    # Get stubbed hardware module
+    decoy.when(
+        equipment.get_module_hardware_api(HeaterShakerModuleId("heater-shaker-id"))
+    ).then_return(hs_hardware)
+
     result = await subject.execute(data)
-    decoy.verify(await match.start_set_temperature(celsius=0), times=1)
+    decoy.verify(await hs_hardware.start_set_temperature(celsius=0), times=1)
     assert result == heater_shaker.DeactivateHeaterResult()
