fa64cd795beb50a3df7c49a4aba6982bcbb5b05f
Neha Ojha <reachojhaneha@gmail.com>
2022-03-30 14:25:47 -0400

    adding the advanced , privacy and ff tabs

--- app-testing/tests/app_settings_v5dot1_test.py
+++ app-testing/tests/app_settings_v5dot1_test.py
@@ -52,6 +52,7 @@ def test_app_settings_v5dot1(
         app_settings: AppSettings = AppSettings(driver, console, request.node.nodeid)
         left_menu: LeftMenu = LeftMenu(driver, console, request.node.nodeid)
 
+        ## General tab verification
         left_menu.click_gear_button()
         assert app_settings.get_app_settings_header().text == "App Settings"
         assert (
@@ -70,8 +71,48 @@ def test_app_settings_v5dot1(
         app_settings.click_close_previous_software_modal()
 
         assert app_settings.get_link_app_robot_sync().is_displayed()
+        assert app_settings.get_software_update_alert_header().is_displayed()
         assert app_settings.get_software_update_alert_toggle().is_displayed()
         app_settings.click_software_update_alert_toggle()
 
+        assert app_settings.get_connect_robot_via_IP_header().is_displayed()
         assert app_settings.get_connect_to_robot_via_IP_address_button().is_displayed()
         app_settings.click_connect_to_robot_via_IP_address_button()
+
+        ## Privacy Tab verification
+        app_settings.click_privacy_tab()
+        assert app_settings.get_robot_app_analytics().is_displayed()
+        assert app_settings.get_robot_app_analytics_toggle().is_displayed()
+        app_settings.click_robot_app_analytics()
+
+        ## Advanced Tab Verification
+        app_settings.click_advanced_tab()
+        assert app_settings.get_advanced_tab().is_displayed()
+        assert app_settings.get_update_channel().is_displayed()
+        assert app_settings.get_update_channel_latest_stable().is_displayed()
+        assert app_settings.get_additional_custom_labware_source_folder().is_displayed()
+        assert app_settings.get_change_labware_source_folder_button().is_displayed()
+        assert app_settings.get_additional_source_folder().is_displayed()
+
+        assert app_settings.get_tip_length_calibration_method().is_displayed()
+        assert app_settings.get_tip_calibration_block_to_calibrate().is_displayed()
+        app_settings.click_tip_calibration_block_to_calibrate()
+        assert app_settings.get_tip_calaibration_trash_bin().is_displayed()
+        app_settings.click_tip_calaibration_trash_bin()
+        assert app_settings.get_tip_calibration_prompt_choose().is_displayed()
+        app_settings.click_tip_calibration_prompt_choose()
+
+        assert app_settings.get_display_unavailable_robots_header().is_displayed()
+        assert app_settings.get_display_unavailable_robots_toggle().is_displayed()
+        app_settings.click_unavailable_robot_toggle()
+
+        assert app_settings.get_clear_unavailable_robots_header().is_displayed()
+        assert app_settings.get_clear_unavailable_robots_list_button().is_displayed()
+        app_settings.click_clear_unavailable_robot_button()
+
+        assert app_settings.get_enable_developer_tool_header().is_displayed()
+        assert app_settings.get_enable_developer_tools_toggle().is_displayed()
+        app_settings.click_enable_developer_tools_toggle()
+
+        app_settings.click_feature_flag_tab()
+        assert app_settings.get_feature_flag_tab().is_displayed()
