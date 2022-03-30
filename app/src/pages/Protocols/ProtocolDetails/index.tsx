195aa6143ec064607b8ffadc67825c200839bbd3
Brian Cooper <brian@opentrons.com>
2022-03-30 13:40:47 -0400

    redirect to protocol landing if protocol key does not exist

--- app/src/pages/Protocols/ProtocolDetails/index.tsx
+++ app/src/pages/Protocols/ProtocolDetails/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Redirect } from 'react-router-dom'
 
 import { useDispatch, useSelector } from 'react-redux'
 import {
   fetchProtocols,
   getStoredProtocol,
 } from '../../../redux/protocol-storage'
-import { ProtocolDetails as ProtocolDetailsContents} from '../../../organisms/ProtocolDetails'
+import { ProtocolDetails as ProtocolDetailsContents } from '../../../organisms/ProtocolDetails'
 
 import type { Dispatch, State } from '../../../redux/types'
 import type { NextGenRouteParams } from '../../../App/NextGenApp'
@@ -15,13 +15,17 @@ export function ProtocolDetails(): JSX.Element {
   const { protocolKey } = useParams<NextGenRouteParams>()
 
   const dispatch = useDispatch<Dispatch>()
-  const storedProtocol = useSelector((state: State) => getStoredProtocol(state, protocolKey))
+  const storedProtocol = useSelector((state: State) =>
+    getStoredProtocol(state, protocolKey)
+  )
 
   React.useEffect(() => {
     dispatch(fetchProtocols())
   }, [])
 
-
-  return storedProtocol != null ? <ProtocolDetailsContents {...storedProtocol}/>:
-  <p>{`TODO:  Details for protocol with key ${protocolKey}`}</p>
+  return storedProtocol != null ? (
+    <ProtocolDetailsContents {...storedProtocol} />
+  ) : (
+    <Redirect to="/protocols" />
+  )
 }
