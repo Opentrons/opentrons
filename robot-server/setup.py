12891ee8afac5f38df6fa2301a08a1632c2d0236
Max Marrone <max@marrone.nyc>
2022-03-30 14:56:01 -0400

    Fix g-code-testing transitive dependency problem?

--- robot-server/setup.py
+++ robot-server/setup.py
@@ -62,9 +62,7 @@ INSTALL_REQUIRES = [
     "uvicorn==0.14.0",
     "wsproto==1.0.0",
     "systemd-python==234; sys_platform=='linux'",
-    # TODO(mc, 2022-03-23): uncomment when buildroot is ready
-    # https://github.com/Opentrons/buildroot/issues/146
-    # "sqlalchemy==1.4.32",
+    "sqlalchemy==1.4.32",
 ]
 
 
