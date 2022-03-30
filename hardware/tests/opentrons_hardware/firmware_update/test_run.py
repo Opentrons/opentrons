fa22262dd59bd139c1a95d822742e8832e461cf2
amit lissack <amit@opentrons.com>
2022-03-30 09:53:34 -0400

    format

--- hardware/tests/opentrons_hardware/firmware_update/test_run.py
+++ hardware/tests/opentrons_hardware/firmware_update/test_run.py
@@ -1,40 +1,50 @@
+"""Tests for run module."""
+from typing import Iterator
+
 import mock
 import pytest
 from mock import AsyncMock, MagicMock
 
-from opentrons_hardware.firmware_bindings.messages.message_definitions import \
-    FirmwareUpdateStartApp
-from opentrons_hardware.firmware_update import FirmwareUpdateInitiator, \
-    FirmwareUpdateDownloader, FirmwareUpdateEraser, run_update, head
+from opentrons_hardware.firmware_bindings.messages.message_definitions import (
+    FirmwareUpdateStartApp,
+)
+from opentrons_hardware.firmware_update import (
+    FirmwareUpdateInitiator,
+    FirmwareUpdateDownloader,
+    FirmwareUpdateEraser,
+    run_update,
+    head,
+)
 
 
 @pytest.fixture
-def mock_initiator_run() -> AsyncMock:
-    """FirmwareInitiator run function."""
+def mock_initiator_run() -> Iterator[AsyncMock]:
+    """Mock run function."""
     with mock.patch.object(FirmwareUpdateInitiator, "run") as p:
         yield p
 
 
 @pytest.fixture
-def mock_downloader_run() -> AsyncMock:
-    """FirmwareUpdateDownloader run function."""
+def mock_downloader_run() -> Iterator[AsyncMock]:
+    """Mock run function."""
     with mock.patch.object(FirmwareUpdateDownloader, "run") as p:
         yield p
 
 
 @pytest.fixture
-def mock_eraser_run() -> AsyncMock:
-    """FirmwareUpdateEraser run function."""
+def mock_eraser_run() -> Iterator[AsyncMock]:
+    """Mock run function."""
     with mock.patch.object(FirmwareUpdateEraser, "run") as p:
         yield p
 
 
-@pytest.mark.parametrize(
-    argnames=["should_erase"],
-    argvalues=[[True], [False]])
+@pytest.mark.parametrize(argnames=["should_erase"], argvalues=[[True], [False]])
 async def test_run_update(
-        mock_initiator_run: AsyncMock, mock_downloader_run: AsyncMock,
-        mock_eraser_run: AsyncMock, should_erase: bool) -> None:
+    mock_initiator_run: AsyncMock,
+    mock_downloader_run: AsyncMock,
+    mock_eraser_run: AsyncMock,
+    should_erase: bool,
+) -> None:
     """It should call all the functions."""
     mock_messenger = AsyncMock()
     mock_hex = MagicMock()
@@ -45,12 +55,20 @@ async def test_run_update(
         hex_processor=mock_hex,
         retry_count=12,
         timeout_seconds=11,
-        erase=should_erase
+        erase=should_erase,
     )
-    mock_initiator_run.assert_called_once_with(target=target, retry_count=12, ready_wait_time_sec=11)
+    mock_initiator_run.assert_called_once_with(
+        target=target, retry_count=12, ready_wait_time_sec=11
+    )
     if should_erase:
-        mock_eraser_run.assert_called_once_with(node_id=target.bootloader_node, timeout_sec=11)
+        mock_eraser_run.assert_called_once_with(
+            node_id=target.bootloader_node, timeout_sec=11
+        )
     else:
         mock_eraser_run.assert_not_called()
-    mock_downloader_run.assert_called_once_with(node_id=target.bootloader_node, hex_processor=mock_hex, ack_wait_seconds=11)
-    mock_messenger.send.assert_called_once_with(node_id=head.bootloader_node, message=FirmwareUpdateStartApp())
+    mock_downloader_run.assert_called_once_with(
+        node_id=target.bootloader_node, hex_processor=mock_hex, ack_wait_seconds=11
+    )
+    mock_messenger.send.assert_called_once_with(
+        node_id=head.bootloader_node, message=FirmwareUpdateStartApp()
+    )
