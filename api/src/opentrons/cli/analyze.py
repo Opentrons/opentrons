ad662364ffc67baec737b064619904a9bc688c79
Mike Cousins <mike@opentrons.com>
2022-04-01 17:19:54 -0400

    fixup: tweak analysis output to add timestamps and protocol meta

--- api/src/opentrons/cli/analyze.py
+++ api/src/opentrons/cli/analyze.py
@@ -1,13 +1,22 @@
 """Opentrons analyze CLI."""
 import click
-import json
-from anyio import run
-from pathlib import Path
-from pydantic.json import pydantic_encoder
-from typing import List, Sequence
 
-from opentrons.protocol_reader import ProtocolReader
+from anyio import run
+from datetime import datetime, timezone
+from pathlib import Path
+from pydantic import BaseModel
+from typing import Any, Dict, List, Sequence, Union
+from typing_extensions import Literal
+
+from opentrons.protocols.api_support.types import APIVersion
+from opentrons.protocol_reader import (
+    ProtocolReader,
+    ProtocolFileRole,
+    ProtocolType,
+    JsonProtocolConfig,
+)
 from opentrons.protocol_runner import create_simulating_runner
+from opentrons.protocol_engine import Command, ErrorOccurrence
 
 
 @click.command()
@@ -48,12 +57,59 @@ async def _analyze(files_and_dirs: Sequence[Path], json_mode: bool) -> None:
 
     protocol_source = await ProtocolReader().read(input_files)
     runner = await create_simulating_runner()
-    results = await runner.run(protocol_source)
+    analysis = await runner.run(protocol_source)
 
     if json_mode:
-        click.echo(json.dumps(results, default=pydantic_encoder))
+        results = AnalyzeResults(
+            createdAt=datetime.now(tz=timezone.utc),
+            files=[
+                ProtocolFile(name=f.path.name, role=f.role)
+                for f in protocol_source.files
+            ],
+            config=(
+                JsonConfig(schemaVersion=protocol_source.config.schema_version)
+                if isinstance(protocol_source.config, JsonProtocolConfig)
+                else PythonConfig(apiVersion=protocol_source.config.api_version)
+            ),
+            metadata=protocol_source.metadata,
+            commands=analysis.commands,
+            errors=analysis.errors,
+        )
+        click.echo(results.json())
         return
 
     raise click.UsageError(
         "Currently, this tool only supports JSON mode. Use `--json`."
     )
+
+
+class ProtocolFile(BaseModel):
+    """A file in a protocol analysis."""
+
+    name: str
+    role: ProtocolFileRole
+
+
+class JsonConfig(BaseModel):
+    """Configuration of a JSON protocol."""
+
+    protocolType: Literal[ProtocolType.JSON] = ProtocolType.JSON
+    schemaVersion: int
+
+
+class PythonConfig(BaseModel):
+    """Configuration of a Python protocol."""
+
+    protocolType: Literal[ProtocolType.PYTHON] = ProtocolType.PYTHON
+    apiVersion: APIVersion
+
+
+class AnalyzeResults(BaseModel):
+    """Results of a protocol analysis."""
+
+    createdAt: datetime
+    files: List[ProtocolFile]
+    config: Union[JsonConfig, PythonConfig]
+    metadata: Dict[str, Any]
+    commands: List[Command]
+    errors: List[ErrorOccurrence]
