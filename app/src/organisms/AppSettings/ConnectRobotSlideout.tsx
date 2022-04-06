590e412827df71967e91ab551a346c5e180452f8
Koji <kk2796@nyu.edu>
2022-04-06 17:38:36 -0400

    Update ConnectRobotSlideout.tsx

--- app/src/organisms/AppSettings/ConnectRobotSlideout.tsx
+++ app/src/organisms/AppSettings/ConnectRobotSlideout.tsx
@@ -69,7 +69,6 @@ export function ConnectRobotSlideout(
       title={t('connect_ip')}
       onCloseClick={onCloseClick}
       isExpanded={isExpanded}
-      zIndex="2"
       height="100%"
       footer={
         <PrimaryButton onClick={onCloseClick} width="100%">
