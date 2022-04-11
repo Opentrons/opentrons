321f8331ce8cea4df5b73b2ac70632096bee4a1e
Shlok Amin <shlokamin@me.com>
2022-04-11 12:26:51 -0400

    fix heater shaker state updater

--- step-generation/src/utils/heaterShakerDiff.ts
+++ step-generation/src/utils/heaterShakerDiff.ts
@@ -2,7 +2,6 @@ import type { HeaterShakerModuleState, HeaterShakerArgs } from '../types'
 
 export interface Diff {
   latchOpen: boolean
-  latchClosed: boolean
   setTemperature: boolean
   deactivateHeater: boolean
   setShakeSpeed: boolean
@@ -11,48 +10,17 @@ export interface Diff {
 
 const getInitialDiff = (): Diff => ({
   latchOpen: false,
-  latchClosed: false,
   setTemperature: false,
   deactivateHeater: false,
   setShakeSpeed: false,
   stopShake: false,
 })
 
-const getLatchOpenDiff = (
-  prevHeaterShakerState: HeaterShakerModuleState,
-  args: HeaterShakerArgs
-): Partial<Diff> => {
-  if (!prevHeaterShakerState.latchOpen && args.latchOpen) {
-    return {
-      latchOpen: true,
-    }
-  }
-
-  return {}
-}
-
-const getLatchClosedDiff = (
-  prevHeaterShakerState: HeaterShakerModuleState,
-  args: HeaterShakerArgs
-): Partial<Diff> => {
-  if (
-    (prevHeaterShakerState.latchOpen && !args.latchOpen) ??
-    (prevHeaterShakerState.latchOpen === null && !args.latchOpen)
-  ) {
-    return {
-      latchOpen: true,
-    }
-  }
-
-  return {}
-}
-
 export const heaterShakerDiff = (
   prevHeaterShakerState: HeaterShakerModuleState,
   args: HeaterShakerArgs
 ): Diff => {
-  const latchOpenDiff = { ...getLatchOpenDiff(prevHeaterShakerState, args) }
-  const lidClosedDiff = { ...getLatchClosedDiff(prevHeaterShakerState, args) }
+  const latchOpen = args.latchOpen
   const tempChanged =
     prevHeaterShakerState.targetTemp !== args.targetTemperature
   const heaterDeactivated = tempChanged && args.targetTemperature === null
@@ -63,8 +31,7 @@ export const heaterShakerDiff = (
 
   return {
     ...getInitialDiff(),
-    ...latchOpenDiff,
-    ...lidClosedDiff,
+    latchOpen,
     setTemperature: heaterActivated,
     deactivateHeater: heaterDeactivated,
     setShakeSpeed: shakerActivated,
