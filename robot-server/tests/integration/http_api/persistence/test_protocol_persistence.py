4cfd135018a9f2aa7973f2b3b4a3f3a5613a0447
Max Marrone <max@marrone.nyc>
2022-04-12 14:47:15 -0400

    Check that more parts of a protocol resource survive rehydration.

--- robot-server/tests/integration/http_api/persistence/test_protocol_persistence.py
+++ robot-server/tests/integration/http_api/persistence/test_protocol_persistence.py
@@ -24,33 +24,39 @@ async def test_protocols_persist(protocol: Callable[[str], IO[bytes]]) -> None:
             assert await robot_client.wait_until_alive(
                 10
             ), "Dev Robot never became available."
+
             protocols_to_create = 13
             for _ in range(protocols_to_create):
                 with protocol(secrets.token_urlsafe(16)) as file:
                     await robot_client.post_protocol([Path(file.name)])
+
             response = await robot_client.get_protocols()
-            uploaded_ids_created_at = [
-                (protocol["id"], protocol["createdAt"])
-                for protocol in response.json()["data"]
-            ]
+            uploaded_protocols = response.json()["data"]
+
             server.stop()
             assert await robot_client.wait_until_dead(12), "Dev Robot did not stop."
+
             server.start()
             assert await robot_client.wait_until_alive(
                 10
             ), "Dev Robot never became available."
+
             response = await robot_client.get_protocols()
-            restarted_ids_created_at = [
-                (protocol["id"], protocol["createdAt"])
-                for protocol in response.json()["data"]
-            ]
+            restarted_protocols = response.json()["data"]
+
+
             # The number of uploaded protocols prior to restart equals the number
             # of protocols in the get protocols response after restart.
-            assert len(response.json()["data"]) == protocols_to_create
-            # The set of (id,createdAt) after restart is the same as prior to restart.
-            # The union of the sets accomplishes this order ignored comparison.
-            assert (
-                len(set(uploaded_ids_created_at) & set(restarted_ids_created_at))
-                == protocols_to_create
-            )
+            assert len(restarted_protocols) == protocols_to_create
+
+            # The protocols after restart are the same as prior to restart,
+            # except we don't care about analyses,
+            # and we don't care about order.
+            for p in uploaded_protocols:
+                del p["analyses"]
+            for p in restarted_protocols:
+                del p["analyses"]
+            uploaded_protocols.sort(key=lambda p: p["id"])
+            restarted_protocols.sort(key=lambda p: p["id"])
+
             server.stop()
