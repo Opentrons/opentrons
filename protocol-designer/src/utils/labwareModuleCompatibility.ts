3fd6623fa5c42bb4075f3be10fff6c79da09661d
Shlok Amin <shlokamin@me.com>
2022-04-18 15:07:45 -0400

    format

--- protocol-designer/src/utils/labwareModuleCompatibility.ts
+++ protocol-designer/src/utils/labwareModuleCompatibility.ts
@@ -55,7 +55,7 @@ const COMPATIBLE_LABWARE_ALLOWLIST_BY_MODULE_TYPE: Record<
     'opentrons_96_deepwell_adapter_nest_wellplate_2ml_deep',
     'opentrons_96_flat_bottom_adapter_nest_wellplate_200ul_flat',
     'opentrons_96_pcr_plate_adapter_nest_wellplate_100ul_pcr_full_skirt',
-    'opentrons_flat_plate_adapter_corning_384_wellplate_112ul_flat'
+    'opentrons_flat_plate_adapter_corning_384_wellplate_112ul_flat',
   ],
 }
 export const getLabwareIsCompatible = (
