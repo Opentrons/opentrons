6be533de62d4616e1375ed600c877f52443ca335
Koji <kk2796@nyu.edu>
2022-04-20 08:46:40 -0400

    Added test cases to RobotSettingAdvanced tab

--- app/src/organisms/Devices/RobotSettings/__tests__/RobotSettingsAdvnaced.test.tsx
+++ app/src/organisms/Devices/RobotSettings/__tests__/RobotSettingsAdvnaced.test.tsx
@@ -99,41 +99,102 @@ const render = () => {
 
 describe('RobotSettings Advanced tab', () => {
   beforeEach(() => {
-    // mockGetShellUpdateState.mockImplementation((state: State) => {
-    //   return {
-    //     info: {
-    //       version: '1.2.3',
-    //       releaseNotes: 'this is a release',
-    //     },
-    //   } as ShellUpdateState
-    // })
     mockGetShellUpdateState.mockReturnValue({
       downloading: true,
     } as ShellUpdateState)
     mockAboutRobotName.mockReturnValue(<div>Mock AboutRobotName Section</div>)
-    mockDisableHoming.mockReturnValue(<div>Mock DisableHoming</div>)
-    mockFactoryReset.mockReturnValue(<div>Mock FactoryReset</div>)
-    mockLegacySettings.mockReturnValue(<div>Mock LegacySettings</div>)
-    mockOpenJupyterControl.mockReturnValue(<div>Mock OpenJupyterControl</div>)
-    mockRobotInformation.mockReturnValue(<div>Mock RobotInformation</div>)
-    mockRobotServerVersion.mockReturnValue(<div>Mock RobotServerVersion</div>)
-    mockShortTrashBin.mockReturnValue(<div>Mock ShortTrashBin</div>)
-    mockTroubleshooting.mockReturnValue(<div>Mock Troubleshooting</div>)
-    mockUpdateRobotSoftware.mockReturnValue(<div>Mock UpdateRobotSoftware</div>)
-    mockUsageSettings.mockReturnValue(<div>Mock UsageSettings</div>)
-    mockUseOlderAspirateBehavior.mockReturnValue(
-      <div>Mock UseOlderAspirateBehavior</div>
+    mockDisableHoming.mockReturnValue(<div>Mock DisableHoming Section</div>)
+    mockFactoryReset.mockReturnValue(<div>Mock FactoryReset Section</div>)
+    mockLegacySettings.mockReturnValue(<div>Mock LegacySettings Section</div>)
+    mockOpenJupyterControl.mockReturnValue(
+      <div>Mock OpenJupyterControl Section</div>
     )
-    mockUseOlderProtocol.mockReturnValue(<div>Mock UseOlderProtocol</div>)
+    mockRobotInformation.mockReturnValue(
+      <div>Mock RobotInformation Section</div>
+    )
+    mockRobotServerVersion.mockReturnValue(
+      <div>Mock RobotServerVersion Section</div>
+    )
+    mockShortTrashBin.mockReturnValue(<div>Mock ShortTrashBin Section</div>)
+    mockTroubleshooting.mockReturnValue(<div>Mock Troubleshooting Section</div>)
+    mockUpdateRobotSoftware.mockReturnValue(
+      <div>Mock UpdateRobotSoftware Section</div>
+    )
+    mockUsageSettings.mockReturnValue(<div>Mock UsageSettings Section</div>)
+    mockUseOlderAspirateBehavior.mockReturnValue(
+      <div>Mock UseOlderAspirateBehavior Section</div>
+    )
+    mockUseOlderProtocol.mockReturnValue(
+      <div>Mock UseOlderProtocol Section</div>
+    )
   })
 
   afterAll(() => {
     jest.resetAllMocks()
   })
 
-  // TODO: test cases
   it('should render AboutRobotName section', () => {
     const [{ getByText }] = render()
     getByText('Mock AboutRobotName Section')
+  })
+
+  it('should render DisableHoming section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock DisableHoming Section')
+  })
+
+  it('should render FactoryReset section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock FactoryReset Section')
+  })
+
+  it('should render LegacySettings section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock LegacySettings Section')
+  })
+
+  it('should render OpenJupyterControl section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock OpenJupyterControl Section')
+  })
+
+  it('should render RobotInformation section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock RobotInformation Section')
+  })
+
+  it('should render RobotServerVersion section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock RobotServerVersion Section')
+  })
+
+  it('should render ShortTrashBin section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock ShortTrashBin Section')
+  })
+
+  it('should render Troubleshooting section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock Troubleshooting Section')
+  })
+
+  it('should render UpdateRobotSoftware section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock UpdateRobotSoftware Section')
+  })
+
+  it('should render UsageSettings section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock UsageSettings Section')
+  })
+
+  it('should render UseOlderAspirateBehavior section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock UseOlderAspirateBehavior Section')
+  })
+
+  it('should render UseOlderProtocol section', () => {
+    const [{ getByText }] = render()
+    getByText('Mock UseOlderProtocol Section')
   })
 })
