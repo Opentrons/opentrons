cf4e9ecf1bc825bf86339fb0867781a8e25f7e3a
Koji(高治) <kk2796@nyu.edu>
2022-04-20 19:11:59 -0400

    feat(app):  App robot settings advanced tab robot update (#10010)
    * Added route for RobotSettings Advanced Tab
    
    * Added items to Advanced tab
    
    * Added items to Advanced Tab
    
    * Created components for each section
    
    * Modified style code
    
    * Added Download logs
    
    * Added robot serialNumber
    
    * Added Browse file system
    
    * Added Disable Homing
    
    * Added updateSettings section functionality
    
    * Added tests for sections that use a toggle button
    
    * Cleanup tests for sections that have a toggle btn
    
    * Added tests for RobotServer and RobotInformation
    
    * Added a base for Rename Robot slideout
    
    * Added size prop to modify icon size on RobotCard
    
    * Added rename functionality
    
    * Added functionality to Factory Reset Modal
    
    - trigger reset
    - connect to a robot
    
    * Added Banner for software update
    
    * Added banner to RobotServerVersion section
    
    * Added the case if a robot serve version is null
    
    * Fixed reloading issues
    
    * Fixed Factory Reset modal props issue
    
    * Removed comments and modify connected prop
    
    * removed unnecessary dependency
    
    * Added software update modal
    
    * Updated the software update modal styling
    
    * fix pipette and module card layout
    
    * Fixed the problems that Brian commented
    
    - Removed comments
    - Updated StyledText props
    - Added modal close function
    - Added tests for OpenJupyterControl
    
    * Added software update modal
    
    * Update RobotServerVersion.tsx
    
    * Fixed test error
    
    - clean up code
    - rename a couple of useState
    - added a couple of tests to `RobotServerVersion`
    - fixed errors on `RobotSettings.test.tsx` under `pages`
    - Added tests to `RobotSettings.test.tsx` under `organisms`
    
    * Added test cases to RobotSettingAdvanced tab
    
    * Remove unused line
    
    * Fixed import statement path error
    
    * Fixes issues for lint-js check
    
    * Added basic test cases to SoftwareUpdate Modal
    
    * Update types.ts
    
    * Fixed js checks issues
    
    * Fixed check-js errors
    
    * Commented out lines for temporary patching
    
    * Fixed lint-js errors
    
    * abstract update robot banner and include in t1 and t2
    
    * fix type
    
    * swap out nested link
    
    * Fixed a couple things on SoftwareUpdateModal
    
    - Added closeModal
    - Updated initial useState for showSoftwareUpdateModal
    
    * mock out update banner in robot card and overview
    
    * Fixed what Mike commented
    
    * Fixed the check js issue
    
    * remove robot card update banner temporarily because of click propagation bug
    
    * remove unused import
    
    Co-authored-by: Brian Cooper <brian@opentrons.com>

---
+++ app/src/organisms/Devices/RobotSettings/AdvancedTab/AdvancedTabSlideouts/FactoryResetModal.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import last from 'lodash/last'
+import {
+  Flex,
+  COLORS,
+  DIRECTION_COLUMN,
+  JUSTIFY_FLEX_END,
+  SPACING,
+} from '@opentrons/components'
+import { StyledText } from '../../../../../atoms/text'
+import { PrimaryButton, SecondaryButton } from '../../../../../atoms/Buttons'
+import { Modal } from '../../../../../atoms/Modal'
+import {
+  useDispatchApiRequest,
+  getRequestById,
+  SUCCESS,
+  PENDING,
+} from '../../../../../redux/robot-api'
+import { resetConfig } from '../../../../../redux/robot-admin'
+import { connect } from '../../../../../redux/robot'
+
+import type { IconProps } from '@opentrons/components'
+import type { State, Dispatch } from '../../../../../redux/types'
+import type { ResetConfigRequest } from '../../../../../redux/robot-admin/types'
+
+interface FactoryResetModalProps {
+  closeModal: () => void
+  isRobotConnected: boolean
+  robotName: string
+  resetOptions?: ResetConfigRequest
+}
+
+export function FactoryResetModal({
+  closeModal,
+  isRobotConnected,
+  robotName,
+  resetOptions,
+}: FactoryResetModalProps): JSX.Element {
+  const reconnectModalIcon: IconProps = {
+    name: 'alert-circle',
+    color: COLORS.blue,
+  }
+  const { t } = useTranslation('device_settings')
+  const [dispatchRequest, requestIds] = useDispatchApiRequest()
+  const dispatch = useDispatch<Dispatch>()
+  const resetRequestStatus = useSelector((state: State) => {
+    const lastId = last(requestIds)
+    return lastId != null ? getRequestById(state, lastId) : null
+  })?.status
+
+  const triggerReset = (): void => {
+    if (resetOptions) dispatchRequest(resetConfig(robotName, resetOptions))
+  }
+
+  const connectRobot = (): void => {
+    dispatch(connect(robotName))
+  }
+
+  React.useEffect(() => {
+    if (resetRequestStatus === SUCCESS) closeModal()
+  }, [resetRequestStatus, closeModal])
+
+  const PENDING_STATUS = resetRequestStatus === PENDING
+
+  return (
+    <>
+      {isRobotConnected ? (
+        <Modal title={t('factory_reset_modal_title')} onClose={closeModal}>
+          <Flex flexDirection={DIRECTION_COLUMN}>
+            <StyledText as="p" marginBottom={SPACING.spacing5}>
+              {t('factory_reset_modal_description')}
+            </StyledText>
+            <Flex justifyContent={JUSTIFY_FLEX_END}>
+              <SecondaryButton
+                onClick={closeModal}
+                marginRight={SPACING.spacing3}
+              >
+                {t('factory_reset_model_cancel_button')}
+              </SecondaryButton>
+              <PrimaryButton
+                backgroundColor={COLORS.error}
+                onClick={triggerReset}
+                disabled={PENDING_STATUS}
+              >
+                {t('factory_reset_modal_confirm_button')}
+              </PrimaryButton>
+            </Flex>
+          </Flex>
+        </Modal>
+      ) : (
+        <Modal
+          title={t('factory_reset_modal_connection_lost_title')}
+          icon={reconnectModalIcon}
+          onClose={closeModal}
+        >
+          <StyledText as="p" marginBottom={SPACING.spacing5}>
+            {t('factory_reset_modal_connection_lost_description')}
+          </StyledText>
+          <Flex justifyContent={JUSTIFY_FLEX_END}>
+            <SecondaryButton
+              marginRight={SPACING.spacing3}
+              onClick={connectRobot}
+            >
+              {t('factory_reset_modal_connection_lost_retry_button')}
+            </SecondaryButton>
+            <PrimaryButton>
+              {t('factory_reset_modal_connection_lost_close_button')}
+            </PrimaryButton>
+          </Flex>
+        </Modal>
+      )}
+    </>
+  )
+}
---
+++ app/src/organisms/Devices/RobotSettings/AdvancedTab/FactoryReset.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import { useTranslation } from 'react-i18next'
+import {
+  Flex,
+  ALIGN_CENTER,
+  JUSTIFY_SPACE_BETWEEN,
+  Box,
+  SPACING,
+  SPACING_AUTO,
+  TYPOGRAPHY,
+} from '@opentrons/components'
+import { StyledText } from '../../../../atoms/text'
+import { TertiaryButton } from '../../../../atoms/Buttons'
+
+interface FactoryResetProps {
+  updateIsExpanded: (
+    isExpanded: boolean,
+    type: 'factoryReset' | 'renameRobot'
+  ) => void
+}
+
+export function FactoryReset({
+  updateIsExpanded,
+}: FactoryResetProps): JSX.Element {
+  const { t } = useTranslation('device_settings')
+
+  return (
+    <Flex alignItems={ALIGN_CENTER} justifyContent={JUSTIFY_SPACE_BETWEEN}>
+      <Box width="70%">
+        <StyledText
+          as="h3"
+          css={TYPOGRAPHY.h3SemiBold}
+          marginBottom={SPACING.spacing4}
+          id="AdvancedSettings_factoryReset"
+        >
+          {t('factory_reset')}
+        </StyledText>
+        <StyledText as="p">{t('factory_reset_description')}</StyledText>
+      </Box>
+      <TertiaryButton
+        marginLeft={SPACING_AUTO}
+        onClick={() => updateIsExpanded(true, 'factoryReset')}
+        id="RobotSettings_FactoryResetChooseButton"
+      >
+        {t('factory_reset_settings_button')}
+      </TertiaryButton>
+    </Flex>
+  )
+}
