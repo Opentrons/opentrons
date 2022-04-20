51804330abb16c1e6db78bb610568ec43b85cd88
Koji <kk2796@nyu.edu>
2022-04-20 10:16:39 -0400

    Update types.ts

--- api-client/src/server/types.ts
+++ api-client/src/server/types.ts
@@ -1,5 +1,3 @@
-// Currently robot-server returns { name: 'newName' }
-// This interface will be updated when the CPX team updates the API
 export interface UpdatedRobotName {
-    name: string;
+  name: string;
 }
