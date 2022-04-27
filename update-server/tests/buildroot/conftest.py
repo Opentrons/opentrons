732e533c67533bb0b2ff123d7f5aa6b8aabf9cfd
sotaoverride <aatrapps@gmail.com>
2022-04-27 09:06:58 -0400

    test_cli fixture for buildroot

--- update-server/tests/buildroot/conftest.py
+++ update-server/tests/buildroot/conftest.py
@@ -24,7 +24,6 @@ async def test_cli(aiohttp_client, loop, otupdate_config, monkeypatch):
     client = await loop.create_task(aiohttp_client(app))
     return client
 
-
 # This can be used to replace update_actions.RootPartitions elements as long
 # as the callee doesn’t actually do identity checking
 FakeRootPartElem = collections.namedtuple("FakeRootPartElem", ("name", "value"))
