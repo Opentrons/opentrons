20472f4a97a92049bf93dd68a15c1e7700a0f391
Jethary Rader <66035149+jerader@users.noreply.github.com>
2022-04-29 16:19:56 -0400

    feat(app): add module icon tooltips to robot card (#10103)
    closes #8672

--- app/src/organisms/Devices/RobotCard.tsx
+++ app/src/organisms/Devices/RobotCard.tsx
@@ -13,17 +13,19 @@ import {
   DIRECTION_ROW,
   SPACING,
   TEXT_TRANSFORM_UPPERCASE,
-  ModuleIcon,
+  BORDERS,
 } from '@opentrons/components'
+import { getModuleDisplayName } from '@opentrons/shared-data'
 
 import OT2_PNG from '../../assets/images/OT2-R_HERO.png'
 import { StyledText } from '../../atoms/text'
+import { UNREACHABLE } from '../../redux/discovery'
+import { ModuleIcon } from '../../molecules/ModuleIcon'
 import { useAttachedModules, useAttachedPipettes } from './hooks'
 import { RobotStatusBanner } from './RobotStatusBanner'
 import { RobotOverflowMenu } from './RobotOverflowMenu'
 
 import type { DiscoveredRobot } from '../../redux/discovery/types'
-import { UNREACHABLE } from '../../redux/discovery'
 // import { UpdateRobotBanner } from '../UpdateRobotBanner'
 
 interface RobotCardProps {
@@ -34,7 +36,6 @@ export function RobotCard(props: RobotCardProps): JSX.Element | null {
   const { robot } = props
   const { name = null, local } = robot
   const { t } = useTranslation('devices_landing')
-
   const attachedModules = useAttachedModules(name)
   const attachedPipettes = useAttachedPipettes(name)
 
@@ -44,7 +45,7 @@ export function RobotCard(props: RobotCardProps): JSX.Element | null {
         alignItems={ALIGN_CENTER}
         backgroundColor={C_WHITE}
         border={`1px solid ${C_MED_LIGHT_GRAY}`}
-        borderRadius="4px"
+        borderRadius={BORDERS.radiusSoftCorners}
         flexDirection={DIRECTION_ROW}
         marginBottom={SPACING.spacing3}
         padding={SPACING.spacing3}
@@ -93,9 +94,15 @@ export function RobotCard(props: RobotCardProps): JSX.Element | null {
               <Flex>
                 {attachedModules.map((module, i) => (
                   <ModuleIcon
-                    key={`${name}_${module.moduleModel}_${i}`}
-                    moduleType={module.moduleType}
-                    size={SPACING.spacing4}
+                    key={`${module.moduleModel}_${i}_${name}`}
+                    tooltipText={t(
+                      'this_robot_has_connected_and_power_on_module',
+                      {
+                        moduleName: getModuleDisplayName(module.moduleModel),
+                      }
+                    )}
+                    index={i}
+                    module={module}
                   />
                 ))}
               </Flex>
