d3b0422f01459d4a074bc8a90706549a3c2c2e77
Ayana Perry <ayana.perry@opentrons.com>
2022-04-29 16:19:49 -0400

    Revised a few minor issues for Task Runner

--- robot-server/robot_server/service/task_runner.py
+++ robot-server/robot_server/service/task_runner.py
@@ -1,12 +1,11 @@
 """
-
-The task runner refactor is now using anyio instead of fastapi.BackgroundTasks.
-Testing will be similar to the previous code base primarily integration and end-to-end tests.
+    This module is mostly a thin wrapper around fastapi.BackgroundTasks
+    that adds logging. It should be tested primarily through integration
+    and end-to-end tests.
 """
 
 from __future__ import annotations
 import asyncio
-from hashlib import new
 from logging import getLogger
 from typing import Any, Awaitable, Callable, Set
 from fastapi import Depends
@@ -16,15 +15,15 @@ log = getLogger(__name__)
 
 TaskFunc = Callable[..., Awaitable[Any]]
 
-_init_taskrunner = AppStateValue["TaskRunner"]("bg_task_runner")
+task_runner = AppStateValue["TaskRunner"]("task_runner")
 
 
 class TaskRunner:
     def __init__(self) -> None:
-        """Initialize the TaskRunner by using Python Set"""
+        """Initialize the TaskRunner"""
 
         self._running_tasks: Set[asyncio.Task[None]] = set()
-        # set of background tasks
+  
 
     def run(self, func: TaskFunc, **kwargs: Any) -> None:
         """Run an async function in the background.
@@ -34,12 +33,11 @@ class TaskRunner:
 
         Arguments:
             func: An async, None-returning function to run in the background.
-            Use **kwargs to pass to func.
+            Kwargs: Keyword arguments to pass straight through to `func`.
         """
         func_name = func.__qualname__
 
         async def wrapper() -> None:
-            # Add the previous logic back
             current_task = asyncio.current_task()
             try:
                 await func(**kwargs)
@@ -53,26 +51,47 @@ class TaskRunner:
         self._running_tasks.add(wrapper_task)
 
     async def cancel_all_and_clean_up(self) -> None:
+        """Cancel any ongoing background tasks and wait for them to stop.
+           Intended to be called just once, when the server shuts down.
+        """
         for task in self._running_tasks:
             task.cancel()
         await asyncio.gather(*self._running_tasks, return_exceptions=True)
+        log.debug(
+            f"Cancelled {len(self._running_tasks)} background tasks."
+            f" Waiting for them to stop."
+        )
+        await asyncio.gather(*self._running_tasks, return_exceptions=True)
+        log.debug("Background tasks stopped.")
 
 
 def initialize_task_runner(app_state: AppState) -> None:
-    """Create a new `TaskRunner` and store it on `app_state`."""
-    _init_taskrunner.set_on(app_state, TaskRunner())
+    """Create a new `TaskRunner` and store it on `app_state`
+       Intended to be called just once, when the server starts up.s
+    """
+    task_runner.set_on(app_state, TaskRunner())
 
 
 async def clean_up_task_runner(app_state: AppState) -> None:
-    """Clean up the `TaskRunner` stored on `app_state`."""
-    task_runner = _init_taskrunner.get_from(app_state)
+    """Clean up the `TaskRunner` stored on `app_state`.
+       Intended to be called just once, when the server shuts down.
+    """
+    task_runner = task_runner.get_from(app_state)
 
     if task_runner is not None:
         await task_runner.cancel_all_and_clean_up()
 
 
-# The get_task_runner method is for the HTTP endpoint functions that need a TaskRunner instance.
 def get_task_runner(app_state: AppState = Depends(get_app_state)) -> TaskRunner:
-    new_task_runner = _init_taskrunner.get_from(app_state)
+    """Intended to be used by endpoint functions as a FastAPI dependency,
+    like `task_runner = fastapi.Depends(get_task_runner)`.
+
+    Args:
+        app_state (AppState, optional): _description_. Defaults to Depends(get_app_state).
+
+    Returns:
+        TaskRunner: _description_
+    """
+    new_task_runner = task_runner.get_from(app_state)
     assert new_task_runner, "Task runner was not initialized"
     return new_task_runner
