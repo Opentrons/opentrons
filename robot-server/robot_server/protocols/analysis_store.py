9f093dddca0c3a11813853888516fb7ec3f452a2
Max Marrone <max@marrone.nyc>
2022-04-29 15:25:25 -0400

    Refactor in-memory storage to its own class and implement .get().

--- robot-server/robot_server/protocols/analysis_store.py
+++ robot-server/robot_server/protocols/analysis_store.py
@@ -63,15 +63,20 @@ class AnalysisStore:
     def __init__(self, sql_engine: SQLEngine) -> None:
         """Initialize the `AnalysisStore`."""
         self._sql_engine = sql_engine
+        self._pending_analysis_store = _PendingAnalysisStore()
 
-        # To make it easier for us to preserve the correct order of analyses
-        # when some are stored in the database and some are stored in-memory,
-        # we disallow more than one pending analysis per protocol at a time.
-        self._pending_analyses_by_protocol: Dict[str, PendingAnalysis] = {}
+        self._pending_analyses_by_id: Dict[str, PendingAnalysis] = {}
 
-    def get(self, analysis_id: str) -> ProtocolAnalysis:
-        """Get a single protocol analysis by its ID."""
-        raise NotImplementedError
+        # We only store at most one pending analysis per protocol.
+        # This makes it easier for us to preserve the correct order of analyses
+        # when some are stored in the database and some are stored in-memory.
+        # The in-memory pending one, if it exists, is always the most recent one.
+        self._pending_analysis_ids_by_protocol: Dict[str, str] = {}
+
+        _log.debug("Debug")
+        _log.info("Info")
+        _log.warning("Warning")
+        _log.error("Error")
 
     def add_pending(self, protocol_id: str, analysis_id: str) -> AnalysisSummary:
         """Add a new pending analysis to the store.
@@ -82,27 +87,21 @@ class AnalysisStore:
             analysis_id: The ID of the new analysis.
                 Must be unique across *all* protocols, not just this one.
 
+                This uniqueness assumption is not checked here. If you violate it,
+                this method will "succeed," but a future call to `update()` may fail
+                unexpectedly because of a SQL uniqueness constraint violation.
+
         Returns:
             A summary of the just-added analysis.
         """
-        assert (
-            protocol_id not in self._pending_analyses_by_protocol
-        ), "Protocol already has a pending analysis."
-
         with self._sql_engine.begin() as transaction:
             self._sql_check_protocol_exists(
                 connection=transaction, protocol_id=protocol_id
             )
-
-        # For implementation simplicity, we don't check our assumption that
-        # the analysis_id must be unique across protocols.
-        # If the caller violates this assumption, it will eventually be a SQL
-        # constraint violation when they promote this pending analysis to a completed one,
-        # and we try inserting it into the database.
-
-        pending_analysis = PendingAnalysis.construct(id=analysis_id)
-        self._pending_analyses_by_protocol[protocol_id] = pending_analysis
-        return _pending_to_summary(pending=pending_analysis)
+        new_pending_analysis = self._pending_analysis_store.add(
+            protocol_id=protocol_id, analysis_id=analysis_id
+        )
+        return _summarize_pending(pending_analysis=new_pending_analysis)
 
     def update(
         self,
@@ -115,17 +114,21 @@ class AnalysisStore:
         """Promote a pending analysis to completed, adding details of its results.
 
         Raises:
-            AnalysisNotFoundError: If `analysis_id` doesn't point to a pending analysis.
+            AnalysisNotPendingOrNotFoundError
         """
+        protocol_id = self._pending_analysis_store.get_protocol_id(
+            analysis_id=analysis_id
+        )
+
+        if protocol_id is None:
+            # No pending analysis with the given ID.
+            # There might be a completed one, but you can't update completed analyses.
+            raise AnalysisNotPendingOrNotFoundError(analysis_id=analysis_id)
+
         if len(errors) > 0:
             result = AnalysisResult.NOT_OK
         else:
             result = AnalysisResult.OK
-
-        # Allow AnalysisNotFoundError to propagate.
-        protocol_id = self._get_protocol_of_pending_analysis(
-            pending_analysis_id=analysis_id
-        )
 
         completed_analysis = CompletedAnalysis.construct(
             id=analysis_id,
@@ -135,7 +138,6 @@ class AnalysisStore:
             pipettes=pipettes,
             errors=errors,
         )
-
         completed_analysis_resource = _CompletedAnalysisResource(
             id=completed_analysis.id,
             protocol_id=protocol_id,
@@ -144,7 +146,17 @@ class AnalysisStore:
         )
         self._sql_add(completed_analysis_resource=completed_analysis_resource)
 
-        del self._pending_analyses_by_protocol[protocol_id]
+        self._pending_analysis_store.remove(analysis_id=analysis_id)
+
+    def get(self, analysis_id: str) -> ProtocolAnalysis:
+        """Get a single protocol analysis by its ID."""
+        pending_analysis = self._pending_analysis_store.get(analysis_id=analysis_id)
+        if pending_analysis is not None:
+            return pending_analysis
+        else:
+            completed_analysis_resource = self._sql_get_by_id(analysis_id=analysis_id)
+            completed_analysis = completed_analysis_resource.completed_analysis
+            return completed_analysis
 
     def get_summaries_by_protocol(self, protocol_id: str) -> List[AnalysisSummary]:
         """Get summaries of all analyses for a protocol, in order from oldest first."""
@@ -154,16 +166,13 @@ class AnalysisStore:
             for analysis_id in completed_analysis_ids
         ]
 
-        pending_analyses: List[AnalysisSummary]
-        try:
-            pending_analyses = [
-                _pending_to_summary(self._pending_analyses_by_protocol[protocol_id])
-            ]
-        except KeyError:
-            pending_analyses = []
-
-        result = completed_analysis_summaries + pending_analyses
-        return result
+        pending_analysis = self._pending_analysis_store.get_by_protocol(
+            protocol_id=protocol_id
+        )
+        if pending_analysis is None:
+            return completed_analysis_summaries
+        else:
+            return completed_analysis_summaries + [_summarize_pending(pending_analysis)]
 
     def get_by_protocol(self, protocol_id: str) -> List[ProtocolAnalysis]:
         """Get all analyses for a protocol, in order from oldest first."""
@@ -174,32 +183,25 @@ class AnalysisStore:
             resource.completed_analysis for resource in completed_analysis_resources
         ]
 
-        pending_analyses: List[ProtocolAnalysis]
-        try:
-            pending_analyses = [self._pending_analyses_by_protocol[protocol_id]]
-        except KeyError:
-            pending_analyses = []
-
-        return completed_analyses + pending_analyses
-
-    def _get_protocol_of_pending_analysis(self, pending_analysis_id: str) -> str:
-        """Return the protocol that owns the given pending analysis.
-
-        Raises:
-            AnalysisNotFoundError: If pending_analysis_id doesn't point to any
-                analysis, or points to analysis that isn't pending.
-        """
-        # This linear search is inefficient,
-        # but we only expect a protocol to have a handful of analyses, at most.
-        for (
-            protocol_id_candidate,
-            pending_analysis,
-        ) in self._pending_analyses_by_protocol.items():
-            if pending_analysis.id == pending_analysis_id:
-                return protocol_id_candidate
-        raise AnalysisNotFoundError(
-            f'Analysis {pending_analysis_id} does not exist or is not pending.'
+        pending_analysis = self._pending_analysis_store.get_by_protocol(
+            protocol_id=protocol_id
         )
+
+        if pending_analysis is None:
+            return completed_analyses
+        else:
+            return completed_analyses + [pending_analysis]
+
+    def _sql_get_by_id(self, analysis_id: str) -> _CompletedAnalysisResource:
+        statement = sqlalchemy.select(analysis_table).where(
+            analysis_table.c.id == analysis_id
+        )
+        with self._sql_engine.begin() as transaction:
+            try:
+                result = transaction.execute(statement).one()
+            except sqlalchemy.exc.NoResultFound as e:
+                raise AnalysisNotFoundError(analysis_id=analysis_id)
+        return _CompletedAnalysisResource.from_sql_row(result)
 
     def _sql_get_by_protocol(
         self, protocol_id: str
@@ -257,6 +259,55 @@ class AnalysisStore:
             raise ProtocolNotFoundError(protocol_id=protocol_id) from e
 
 
+class _PendingAnalysisStore:
+    """An in-memory store of pending analyses."""
+
+    def __init__(self) -> None:
+        self._analyses_by_id: Dict[str, PendingAnalysis] = {}
+        self._analysis_ids_by_protocol_id: Dict[str, str] = {}
+        self._protocol_ids_by_analysis_id: Dict[str, str] = {}
+
+    def add(self, protocol_id: str, analysis_id: str) -> PendingAnalysis:
+        """Add a new pending analysis and associate it with the given protocol."""
+        assert (
+            protocol_id not in self._analysis_ids_by_protocol_id
+        ), "Protocol must not already have a pending analysis."
+
+        new_pending_analysis = PendingAnalysis.construct(id=analysis_id)
+
+        self._analyses_by_id[analysis_id] = new_pending_analysis
+        self._analysis_ids_by_protocol_id[protocol_id] = analysis_id
+        self._protocol_ids_by_analysis_id[analysis_id] = protocol_id
+
+        return new_pending_analysis
+
+    def remove(self, analysis_id: str) -> None:
+        """Remove the pending analysis with the given ID.
+
+        The given analysis must exist.
+        """
+        protocol_id = self._protocol_ids_by_analysis_id[analysis_id]
+
+        del self._analyses_by_id[analysis_id]
+        del self._analysis_ids_by_protocol_id[protocol_id]
+        del self._protocol_ids_by_analysis_id[analysis_id]
+
+    def get(self, analysis_id: str) -> Optional[PendingAnalysis]:
+        return self._analyses_by_id.get(analysis_id, None)
+
+    def get_by_protocol(self, protocol_id: str) -> Optional[PendingAnalysis]:
+        """Return the pending analysis associated with the given protocol, if any."""
+        analysis_id = self._analysis_ids_by_protocol_id.get(protocol_id, None)
+        if analysis_id is None:
+            return None
+        else:
+            return self._analyses_by_id[analysis_id]
+
+    def get_protocol_id(self, analysis_id: str) -> Optional[str]:
+        """Return the ID of the protocol that's associated with the given analysis."""
+        return self._protocol_ids_by_analysis_id.get(analysis_id, None)
+
+
 @dataclass
 class _CompletedAnalysisResource:
     id: str
@@ -301,5 +352,5 @@ class _CompletedAnalysisResource:
         )
 
 
-def _pending_to_summary(pending: PendingAnalysis) -> AnalysisSummary:
-    return AnalysisSummary(id=pending.id, status=pending.status)
+def _summarize_pending(pending_analysis: PendingAnalysis) -> AnalysisSummary:
+    return AnalysisSummary(id=pending_analysis.id, status=pending_analysis.status)
