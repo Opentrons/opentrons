8b50910933ca9e4dd4b5863aadf6ed68b0dddc37
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2022-05-04 17:15:39 -0400

    add gripper class

---
+++ api/src/opentrons/config/gripper_config.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+from dataclasses import dataclass
+import logging
+from typing import Tuple, Literal
+
+from opentrons import config
+from opentrons.config import feature_flags as ff
+
+log = logging.getLogger(__name__)
+
+GripperName = Literal["gripper"]
+GripperModel = Literal["gripper_v1"]
+
+
+@dataclass(frozen=True)
+class GripperConfig:
+    gripper_offset: Tuple[float, float, float]
+    gripper_current: float
+    display_name: str
+    name: GripperName
+    max_travel: float
+    home_position: float
+    steps_per_mm: float
+    idle_current: float
+    model: `GripperModel`
