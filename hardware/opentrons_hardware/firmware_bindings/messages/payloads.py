11c6b4e5ab4361f2f2630b9ed2f1e01992f7d236
Caila Marashaj <caila.marashaj@opentrons.com>
2022-05-09 10:48:05 -0400

    added message to defs, constants, payloads

--- hardware/opentrons_hardware/firmware_bindings/messages/payloads.py
+++ hardware/opentrons_hardware/firmware_bindings/messages/payloads.py
@@ -421,3 +421,17 @@ class TipActionResponsePayload(MoveGroupResponsePayload):
     """A response that sends back whether tip action was successful."""
 
     success: utils.UInt8Field
+
+@dataclass
+class PeripheralInfoRequestPayload(utils.BinarySerializable):
+    """A request to get the initialization status of a peripheral device."""
+
+    sensor: SensorTypeField
+
+@dataclass
+class PeripheralInfoResponsePayload(utils.BinarySerializable):
+    """A response that sends back the initialization status of a peripheral device."""
+
+    sensor: SensorTypeField
+    status: utils.UInt8Field
+
