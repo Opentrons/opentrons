89fab9aae948fb71d172ad9a7caef89f4e158418
Max Marrone <max@marrone.nyc>
2022-05-10 11:42:16 -0400

    Fix silly test mistakes.

--- robot-server/tests/test_deletion_planner.py
+++ robot-server/tests/test_deletion_planner.py
@@ -11,7 +11,7 @@ def test_protocol_deletion() -> None:  # noqa: D103
 
 def test_run_deletion_maximum_of_zero() -> None:  # noqa: D103
     subject = DeletionPlanner()
-    input_runs = [RunSpec(run_id=f"run-{i}", protocol_id=None) for i in range(5)]
+    input_runs = [RunSpec(run_id=f"run-{i+1}", protocol_id=None) for i in range(5)]
 
     # If the maximum runs is 0, everything should be deleted.
     assert subject.plan_deletions_for_new_run(
@@ -26,6 +26,7 @@ def test_run_deletion_maximum_of_zero() -> None:  # noqa: D103
                 "run-2",
                 "run-3",
                 "run-4",
+                "run-5",
             ],
         ),
         protocols_to_delete=set(),
@@ -34,7 +35,7 @@ def test_run_deletion_maximum_of_zero() -> None:  # noqa: D103
 
 def test_run_deletion_several_deleted() -> None:  # noqa: D103
     subject = DeletionPlanner()
-    input_runs = [RunSpec(run_id=f"run-{i}", protocol_id=None) for i in range(5)]
+    input_runs = [RunSpec(run_id=f"run-{i+1}", protocol_id=None) for i in range(5)]
 
     # If the maximum runs is below the number of current runs,
     # enough runs should be deleted that the new count is one fewer than
@@ -59,7 +60,7 @@ def test_run_deletion_several_deleted() -> None:  # noqa: D103
 
 def test_run_deletion_exactly_equal() -> None:  # noqa: D103
     subject = DeletionPlanner()
-    input_runs = [RunSpec(run_id=f"run-{i}", protocol_id=None) for i in range(5)]
+    input_runs = [RunSpec(run_id=f"run-{i+1}", protocol_id=None) for i in range(5)]
 
     # If the maximum runs is exactly equal to the number of current runs,
     # the single oldest run should be deleted to make room for one new one.
@@ -73,7 +74,7 @@ def test_run_deletion_exactly_equal() -> None:  # noqa: D103
 
 def test_run_deletion_no_deletions_needed() -> None:  # noqa: D103
     subject = DeletionPlanner()
-    input_runs = [RunSpec(run_id=f"run-{i}", protocol_id=None) for i in range(5)]
+    input_runs = [RunSpec(run_id=f"run-{i+1}", protocol_id=None) for i in range(5)]
 
     # If the maximum runs is at least one more than the number of current runs,
     # no runs should be deleted.
