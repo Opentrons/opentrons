19de834b37f79be94ed6fde440b5db60afeeba16
amit lissack <amit@opentrons.com>
2022-05-10 11:54:29 -0400

    openembedded health test.

---
+++ update-server/tests/openembedded/test_control.py
@@ -0,0 +1,28 @@
+"""test the openembedded endpoints in otupdate.common.control """
+
+import json
+import os
+
+from aiohttp.test_utils import TestClient
+
+
+async def test_health(test_cli: TestClient):
+    resp = await test_cli.get("/server/update/health")
+    assert resp.status == 200
+    version_dict = json.load(
+        open(os.path.join(os.path.dirname(__file__), "version.json"))
+    )
+    body = await resp.json()
+    assert body == {
+        "name": "opentrons-test",
+        "updateServerVersion": version_dict["update_server_version"],
+        "apiServerVersion": version_dict["opentrons_api_version"],
+        "smoothieVersion": "unimplemented",
+        "systemVersion": version_dict["buildroot_version"],
+        "bootId": "dummy-boot-id-abc123",
+        "capabilities": {
+            "buildrootUpdate": "/server/update/begin",
+            "restart": "/server/restart",
+        },
+        "serialNumber": "unknown",
+    }
