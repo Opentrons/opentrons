f7f9cd7339f28dfade73872663d7033998e86f88
Max Marrone <max@marrone.nyc>
2022-05-11 17:02:28 -0400

    Update test for rename.

--- robot-server/tests/test_deletion_planner.py
+++ robot-server/tests/test_deletion_planner.py
@@ -18,7 +18,7 @@ def test_run_deletion_maximum_of_zero() -> None:  # noqa: D103
     input_runs = [f"run-{i+1}" for i in range(5)]
 
     # If the maximum runs is 0, everything should be deleted.
-    assert subject.plan_deletions_for_new_run(existing_runs=input_runs,) == set(
+    assert subject.plan_for_new_run(existing_runs=input_runs,) == set(
         [
             "run-1",
             "run-2",
@@ -36,7 +36,7 @@ def test_run_deletion_several_deleted() -> None:  # noqa: D103
     # If the maximum runs is below the number of current runs,
     # enough runs should be deleted that the new count is one fewer than
     # the maximum runs. Runs should be deleted oldest-first.
-    assert subject.plan_deletions_for_new_run(existing_runs=input_runs,) == set(
+    assert subject.plan_for_new_run(existing_runs=input_runs,) == set(
         [
             "run-1",
             "run-2",
@@ -52,7 +52,7 @@ def test_run_deletion_exactly_equal() -> None:  # noqa: D103
 
     # If the maximum runs is exactly equal to the number of current runs,
     # the single oldest run should be deleted to make room for one new one.
-    assert subject.plan_deletions_for_new_run(
+    assert subject.plan_for_new_run(
         existing_runs=input_runs,
     ) == set(["run-1"])
 
@@ -64,7 +64,7 @@ def test_run_deletion_no_deletions_needed() -> None:  # noqa: D103
     # If the maximum runs is at least one more than the number of current runs,
     # no runs should be deleted.
     assert (
-        subject.plan_deletions_for_new_run(
+        subject.plan_for_new_run(
             existing_runs=input_runs,
         )
         == set()
