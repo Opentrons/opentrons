f8f257212a415e7ce9cf0a7d73b0d3d81a9ab644
Max Marrone <max@marrone.nyc>
2022-05-12 12:57:42 -0400

    Update deletion planner docstrings.

--- robot-server/robot_server/deletion_planner.py
+++ robot-server/robot_server/deletion_planner.py
@@ -22,33 +22,62 @@ class ProtocolSpec(InterfaceShape):
 
 class ProtocolDeletionPlanner:  # noqa: D101
     def __init__(self, maximum_unused_protocols: int) -> None:
+        """Return a configured protocol deletion planner.
+
+        Args:
+            maximum_unused_protocols: The maximum number of "unused protocols"
+                to allow. An "unused protocol" is one that isn't currently linked
+                to any run. Must be at least 1.
+        """
         self._maximum_unused_protocols = maximum_unused_protocols
 
     def plan_for_new_protocol(
         self,
         existing_protocols: Sequence[ProtocolSpec],
     ) -> Set[str]:
-        """Choose which resources to delete in order to make room for a new protocol.
+        """Choose which protocols to delete in order to make room for a new one.
 
         Args:
-            existing_protocols: Information about all protocols that currently exist
-                the database. Must be in order from oldest first!
-            maximum_protocols: The maximum allowed number of protocols.
+            existing_protocols: Information about all protocols that currently exist.
+                Must be in order from oldest first!
+
+        Returns:
+            IDs of protocols to delete.
+
+            After deleting these protocols, the number of unused protocols
+            will be at least one less than the maximum,
+            allowing an additional unused protocol to be added
+            without going over the limit.
         """
         raise NotImplementedError()
 
 
 class RunDeletionPlanner:  # noqa: D101
     def __init__(self, maximum_runs: int) -> None:
+        """Return a configured run deletion planner.
+
+        Args:
+            maximum_runs: The maximum number of runs to allow.
+                Must be at least 1.
+        """
         self._maximum_runs = maximum_runs
 
     def plan_for_new_run(
         self,
         existing_runs: Sequence[str],
     ) -> Set[str]:
-        """Choose which resources to delete in order to make room for a new run.
+        """Choose which runs to delete in order to make room for a new one.
 
-        Arguments are the same as for`plan_deletions_for_new_protocol()`.
+        Args:
+            existing_runs: The IDs of all runs that currently exist.
+                Must be in order from oldest first!
+
+        Returns:
+            The IDs of runs to delete.
+
+            After deleting these runs, the number of runs
+            will be at least one less than the maximum,
+            allowing an additional run to be added without going over the limit.
         """
         runs_upper_limit = self._maximum_runs - 1
         if runs_upper_limit < 0:
