49f9b1ff415ff5dc598028b5eb1662673101ef81
Sakib Hossain <sakib.hossain@opentrons.com>
2022-05-13 08:12:28 -0400

    feat(app): add protocol card sorting (#10223)
    * feat(app): add protocol card sorting
    
    This PR adds the menu and functionality to sort the protocol cards in different orders.
    
    closes #8817

--- app/src/organisms/ProtocolsLanding/ProtocolList.tsx
+++ app/src/organisms/ProtocolsLanding/ProtocolList.tsx
@@ -6,7 +6,16 @@ import {
   ALIGN_CENTER,
   JUSTIFY_SPACE_BETWEEN,
   SPACING,
+  DIRECTION_ROW,
+  Icon,
+  TYPOGRAPHY,
+  BORDERS,
+  POSITION_ABSOLUTE,
+  COLORS,
+  DIRECTION_COLUMN,
+  Overlay,
 } from '@opentrons/components'
+import { useSortedProtocols } from './hooks'
 import { StyledText } from '../../atoms/text'
 import { SecondaryButton } from '../../atoms/Buttons'
 import { Slideout } from '../../atoms/Slideout'
@@ -14,20 +23,32 @@ import { ChooseRobotSlideout } from '../ChooseRobotSlideout'
 import { UploadInput } from './UploadInput'
 import { ProtocolCard } from './ProtocolCard'
 import { EmptyStateLinks } from './EmptyStateLinks'
+import { MenuItem } from '../../atoms/MenuList/MenuItem'
 
 import type { StoredProtocolData } from '../../redux/protocol-storage'
+import type { ProtocolSort } from './hooks'
 
 interface ProtocolListProps {
   storedProtocols: StoredProtocolData[]
 }
 export function ProtocolList(props: ProtocolListProps): JSX.Element | null {
   const [showSlideout, setShowSlideout] = React.useState(false)
+  const [sortBy, setSortBy] = React.useState<ProtocolSort>('alphabetical')
+  const [showSortByMenu, setShowSortByMenu] = React.useState<boolean>(false)
+  const toggleSetShowSortByMenu = (): void => setShowSortByMenu(!showSortByMenu)
   const { t } = useTranslation('protocol_info')
   const { storedProtocols } = props
   const [
     selectedProtocol,
     setSelectedProtocol,
   ] = React.useState<StoredProtocolData | null>(null)
+
+  const sortedStoredProtocols = useSortedProtocols(sortBy, storedProtocols)
+
+  const handleClickOutside: React.MouseEventHandler<HTMLDivElement> = e => {
+    e.preventDefault()
+    setShowSortByMenu(false)
+  }
 
   return (
     <Box padding={SPACING.spacing4}>
@@ -44,16 +65,83 @@ export function ProtocolList(props: ProtocolListProps): JSX.Element | null {
         marginBottom={SPACING.spacing5}
       >
         <StyledText as="h1">{t('protocols')}</StyledText>
-        {/* TODO - Add text filter dropdown overflow menu component */}
-        <SecondaryButton onClick={() => setShowSlideout(true)}>
-          {t('import')}
-        </SecondaryButton>
+        <Flex flexDirection={DIRECTION_ROW}>
+          <Flex
+            flexDirection={DIRECTION_ROW}
+            alignItems={ALIGN_CENTER}
+            marginRight={SPACING.spacing4}
+            onClick={toggleSetShowSortByMenu}
+          >
+            <StyledText css={TYPOGRAPHY.pSemiBold}>
+              {t('labware_landing:sort_by')}
+            </StyledText>
+            <Icon
+              height={TYPOGRAPHY.lineHeight16}
+              name={showSortByMenu ? 'chevron-up' : 'chevron-down'}
+            />
+          </Flex>
+          {showSortByMenu && (
+            <Flex
+              width="10rem"
+              zIndex={2}
+              borderRadius={BORDERS.radiusSoftCorners}
+              boxShadow={'0px 1px 3px rgba(0, 0, 0, 0.2)'}
+              position={POSITION_ABSOLUTE}
+              backgroundColor={COLORS.white}
+              top="3rem"
+              right={'7rem'}
+              flexDirection={DIRECTION_COLUMN}
+            >
+              <MenuItem
+                onClick={() => {
+                  setSortBy('alphabetical')
+                  setShowSortByMenu(false)
+                }}
+              >
+                {t('labware_landing:alphabetical')}
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  setSortBy('recent')
+                  setShowSortByMenu(false)
+                }}
+              >
+                {t('most_recent_updates')}
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  setSortBy('reverse')
+                  setShowSortByMenu(false)
+                }}
+              >
+                {t('labware_landing:reverse')}
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  setSortBy('oldest')
+                  setShowSortByMenu(false)
+                }}
+              >
+                {t('oldest_updates')}
+              </MenuItem>
+            </Flex>
+          )}
+          {showSortByMenu ? (
+            <Overlay
+              onClick={handleClickOutside}
+              backgroundColor={COLORS.transparent}
+            />
+          ) : null}
+          <SecondaryButton onClick={() => setShowSlideout(true)}>
+            {t('import')}
+          </SecondaryButton>
+        </Flex>
       </Flex>
       <StyledText as="p" paddingBottom={SPACING.spacing4}>
         {t('all_protocols')}
       </StyledText>
       <Flex flexDirection="column">
-        {storedProtocols.map(storedProtocol => (
+        {sortedStoredProtocols.map(storedProtocol => (
           <ProtocolCard
             key={storedProtocol.protocolKey}
             handleRunProtocol={() => setSelectedProtocol(storedProtocol)}
