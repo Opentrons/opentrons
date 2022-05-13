20b1d9444919601cbb9af7e429f8e497be3a6af7
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-05-13 16:52:46 -0400

    fixed failing test

--- robot-server/tests/runs/test_run_data_manager.py
+++ robot-server/tests/runs/test_run_data_manager.py
@@ -589,23 +589,6 @@ def test_get_commands_slice_from_db(
     run_command: commands.Command,
 ) -> None:
     """Should get a sliced command list from run store."""
-    commands_list = [
-        commands.Pause(
-            id="command-id-1",
-            key="command-key",
-            createdAt=datetime(year=2021, month=1, day=1),
-            status=commands.CommandStatus.SUCCEEDED,
-            params=commands.PauseParams(message="Hello"),
-        ),
-        commands.Pause(
-            id="command-id-2",
-            key="command-key",
-            createdAt=datetime(year=2021, month=1, day=1),
-            status=commands.CommandStatus.SUCCEEDED,
-            params=commands.PauseParams(message="Hello"),
-        ),
-        run_command,
-    ]
 
     expected_commands_result = [
         commands.Pause(
@@ -622,8 +605,8 @@ def test_get_commands_slice_from_db(
         commands=expected_commands_result, cursor=1, total_length=3
     )
 
-    decoy.when(mock_run_store.get_run_commands("run_id")).then_return(commands_list)
-    result = subject.get_commands_slice("run_id", 1, 2)
+    decoy.when(mock_run_store.get_commands_slice(run_id="run_id", cursor=1, length=2)).then_return(expected_command_slice)
+    result = subject.get_commands_slice(run_id="run_id", cursor=1, length=2)
 
     assert expected_command_slice == result
 
@@ -635,24 +618,6 @@ def test_get_commands_slice_current_run(
     run_command: commands.Command,
 ) -> None:
     """Should get a sliced command list from engine store."""
-    commands_list = [
-        commands.Pause(
-            id="command-id-1",
-            key="command-key",
-            createdAt=datetime(year=2021, month=1, day=1),
-            status=commands.CommandStatus.SUCCEEDED,
-            params=commands.PauseParams(message="Hello"),
-        ),
-        commands.Pause(
-            id="command-id-2",
-            key="command-key",
-            createdAt=datetime(year=2021, month=1, day=1),
-            status=commands.CommandStatus.SUCCEEDED,
-            params=commands.PauseParams(message="Hello"),
-        ),
-        run_command,
-    ]
-
     expected_commands_result = [
         commands.Pause(
             id="command-id-2",
@@ -668,8 +633,8 @@ def test_get_commands_slice_current_run(
         commands=expected_commands_result, cursor=1, total_length=3
     )
     decoy.when(mock_engine_store.current_run_id).then_return("run-id")
-    decoy.when(mock_engine_store.engine.state_view.commands.get_all()).then_return(
-        commands_list
+    decoy.when(mock_engine_store.engine.state_view.commands.get_slice(1, 2)).then_return(
+        expected_command_slice
     )
 
     result = subject.get_commands_slice("run-id", 1, 2)
