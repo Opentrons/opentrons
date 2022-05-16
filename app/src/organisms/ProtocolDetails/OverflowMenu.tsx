1066ff0238e091c9bcfeac23e729a4b74a063211
Sakib Hossain <sakib.hossain@opentrons.com>
2022-05-16 01:46:06 -0400

    feat(app): add protocol overview section (#10212)
    * feat(app): add protocol overview section
    
    This PR adds the protocol overview section in the protocol details page.
    
    closes #8821

--- app/src/organisms/ProtocolDetails/OverflowMenu.tsx
+++ app/src/organisms/ProtocolDetails/OverflowMenu.tsx
@@ -9,12 +9,14 @@ import {
   DIRECTION_COLUMN,
   POSITION_RELATIVE,
   ALIGN_FLEX_END,
-  SIZE_4,
   Overlay,
+  TYPOGRAPHY,
 } from '@opentrons/components'
 import { Portal } from '../../App/portal'
 import { OverflowBtn } from '../../atoms/MenuList/OverflowBtn'
 import { MenuItem } from '../../atoms/MenuList/MenuItem'
+import { ExternalLink } from '../../atoms/Link/ExternalLink'
+import { Divider } from '../../atoms/structure'
 import {
   analyzeProtocol,
   viewProtocolSourceFolder,
@@ -24,10 +26,11 @@ import type { Dispatch } from '../../redux/types'
 
 interface OverflowMenuProps {
   protocolKey: string
+  protocolType: 'json' | 'python'
 }
 
 export function OverflowMenu(props: OverflowMenuProps): JSX.Element {
-  const { protocolKey } = props
+  const { protocolKey, protocolType } = props
   const { t } = useTranslation('protocol_details')
   const [showOverflowMenu, setShowOverflowMenu] = React.useState<boolean>(false)
   const dispatch = useDispatch<Dispatch>()
@@ -56,7 +59,7 @@ export function OverflowMenu(props: OverflowMenuProps): JSX.Element {
       <OverflowBtn alignSelf={ALIGN_FLEX_END} onClick={handleOverflowClick} />
       {showOverflowMenu ? (
         <Flex
-          width={SIZE_4}
+          width={'12rem'}
           zIndex={10}
           borderRadius={'4px 4px 0px 0px'}
           boxShadow={'0px 1px 3px rgba(0, 0, 0, 0.2)'}
@@ -70,6 +73,20 @@ export function OverflowMenu(props: OverflowMenuProps): JSX.Element {
             {t('show_in_folder')}
           </MenuItem>
           <MenuItem onClick={handleClickReanalyze}>{t('reanalyze')}</MenuItem>
+          {protocolType === 'json' ? (
+            <>
+              <Divider />
+              <MenuItem>
+                <ExternalLink
+                  css={TYPOGRAPHY.linkPSemibold}
+                  href={'https://designer.opentrons.com/'}
+                  id="Overflowmenu_protocol_designer"
+                >
+                  {t('protocol_info:launch_protocol_designer')}
+                </ExternalLink>
+              </MenuItem>
+            </>
+          ) : null}
         </Flex>
       ) : null}
       <Portal level="top">
