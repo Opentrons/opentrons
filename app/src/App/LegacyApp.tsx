82a4631c928747d544000c019c36274ff8882a5e
Ayana Perry <ayana.perry@opentrons.com>
2022-05-20 14:00:36 -0400

    Removed the Upload folder and relevant content

--- app/src/App/LegacyApp.tsx
+++ app/src/App/LegacyApp.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Redirect } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import { Box, POSITION_RELATIVE } from '@opentrons/components'
@@ -7,8 +7,6 @@ import { ApiHostProvider } from '@opentrons/react-api-client'
 
 import { getConnectedRobot } from '../redux/discovery'
 import { Alerts } from '../organisms/Alerts'
-
-import { Upload } from '../pages/Upload'
 
 import { LegacyNavbar } from './LegacyNavbar'
 import { PortalRoot as ModalPortalRoot, TopPortalRoot } from './portal'
@@ -28,9 +26,6 @@ export function LegacyApp(): JSX.Element {
       <Box position={POSITION_RELATIVE} width="100%" height="100%">
         <ModalPortalRoot />
         <Switch>
-          <Route path="/upload">
-            <Upload />
-          </Route>
           <Redirect exact from="/" to="/robots" />
           {/* redirect after next gen app feature flag toggle */}
           <Redirect exact from="/app-settings/feature-flags" to="/more" />
