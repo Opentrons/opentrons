6c075ae100a9d78165fa17e4c8edf06e2f86bcac
Sakib Hossain <sakib.hossain@opentrons.com>
2022-05-25 14:45:13 -0400

    update logic for MoAM support

--- app/src/organisms/Devices/ModuleCard/useModuleIdFromRun.ts
+++ app/src/organisms/Devices/ModuleCard/useModuleIdFromRun.ts
@@ -1,5 +1,6 @@
 import { useAttachedModules, useProtocolDetailsForRun } from '../hooks'
 import type { AttachedModule } from '../../../redux/modules/types'
+import { getModuleDef2 } from '@opentrons/shared-data'
 
 export interface ModuleIdFromRun {
   moduleIdFromRun: string
@@ -13,19 +14,26 @@ export function useModuleIdFromRun(
   const { protocolData } = useProtocolDetailsForRun(runId)
   const attachedModules = useAttachedModules(robotName)
 
+  const { compatibleWith } = getModuleDef2(module.moduleModel)
+
   const filteredModules = attachedModules.filter(
     item =>
-      item.moduleModel === module.moduleModel &&
-      item.serialNumber === module.serialNumber
+      item.moduleModel === module.moduleModel ||
+      compatibleWith.includes(item.moduleModel)
   )
+
   const loadModuleCommands = protocolData?.commands.filter(
     command =>
       command.commandType === 'loadModule' &&
-      command.params.model === module.moduleModel
+      (command.params.model === module.moduleModel ||
+        compatibleWith.includes(command.params.model))
   )
-  const moduleIdFromRun =
-    loadModuleCommands != null &&
-    loadModuleCommands[filteredModules.indexOf(module)].result.moduleId
+
+  const moduleIndex = filteredModules.findIndex(
+    attachedModule => attachedModule.serialNumber === module.serialNumber
+  )
+
+  const moduleIdFromRun = loadModuleCommands?.[moduleIndex].result.moduleId
 
   return { moduleIdFromRun }
 }
