ef59112b914d42dde9ed32b2e91702c9acf3cced
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-05-27 14:24:55 -0400

    changed run_completed_at assigment

--- api/src/opentrons/protocol_engine/state/commands.py
+++ api/src/opentrons/protocol_engine/state/commands.py
@@ -308,9 +308,7 @@ class CommandStore(HasState[CommandState], HandlesActions):
         elif isinstance(action, HardwareStoppedAction):
             self._state.queue_status = QueueStatus.INACTIVE
             self._state.run_result = self._state.run_result or RunResult.STOPPED
-            self._state.run_completed_at = (
-                self._state.run_completed_at or action.completed_at
-            )
+            self._state.run_completed_at = action.completed_at
 
         elif isinstance(action, HardwareEventAction):
             if isinstance(action.event, DoorStateNotification):
