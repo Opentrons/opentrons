7d3997bc22d96cccbadd925eea77cf32207ccc4d
Koji <kk2796@nyu.edu>
2022-05-27 15:31:06 -0400

    Add null check

--- app/src/organisms/CalibrationPanels/ChooseTipRack.tsx
+++ app/src/organisms/CalibrationPanels/ChooseTipRack.tsx
@@ -111,8 +111,9 @@ export function ChooseTipRack(props: ChooseTipRackProps): JSX.Element {
 
   const pipSerial = useSelector(
     (state: State) =>
-      // @ts-expect-error(sa, 2021-05-26): possibly null, can optionally chain. leaving to avoid src code change
-      robotName && getAttachedPipettes(state, robotName)[mount].id
+      robotName != null &&
+      getAttachedPipettes(state, robotName)[mount] != null &&
+      getAttachedPipettes(state, robotName)[mount]?.id
   )
 
   const pipetteOffsetCal = useSelector((state: State) =>
