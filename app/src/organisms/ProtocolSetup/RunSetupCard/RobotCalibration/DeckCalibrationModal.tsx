6cc12085f06652fa9f554c40cdaab36e80f04291
koji <kk2796@nyu.edu>
2022-05-28 00:41:09 -0400

    Update styling

--- app/src/organisms/ProtocolSetup/RunSetupCard/RobotCalibration/DeckCalibrationModal.tsx
+++ app/src/organisms/ProtocolSetup/RunSetupCard/RobotCalibration/DeckCalibrationModal.tsx
@@ -5,18 +5,22 @@ import {
   Link,
   Box,
   Icon,
-  SIZE_4,
   DIRECTION_ROW,
   ALIGN_CENTER,
   JUSTIFY_SPACE_BETWEEN,
+  DIRECTION_COLUMN,
   TYPOGRAPHY,
   SPACING,
+  ALIGN_FLEX_END,
+  TEXT_TRANSFORM_CAPITALIZE,
 } from '@opentrons/components'
 import RobotCalHelpImage from '../../../../assets/images/robot_calibration_help.png'
 import { Portal } from '../../../../App/portal'
 import { Modal } from '../../../../atoms/Modal'
 import { StyledText } from '../../../../atoms/text'
 import { PrimaryButton } from '../../../../atoms/buttons'
+import { Line } from '../../../../atoms/structure'
+import { ExternalLink } from '../../../../atoms/Link/ExternalLink'
 
 const robotCalHelpArticle =
   'https://support.opentrons.com/s/article/How-positional-calibration-works-on-the-OT-2'
@@ -31,48 +35,42 @@ export function DeckCalibrationModal(
   const { onCloseClick } = props
   return (
     <Portal level="top">
-      <Modal>
+      <Modal
+        maxHeight="28.125rem"
+        title={t('robot_cal_help_title')}
+        onClose={onCloseClick}
+      >
         <Box>
           <Flex
-            flexDirection={DIRECTION_ROW}
-            alignItems={ALIGN_CENTER}
-            justifyContent={JUSTIFY_SPACE_BETWEEN}
+            // flexDirection={DIRECTION_ROW}
+            flexDirection={DIRECTION_COLUMN}
+            // alignItems={ALIGN_CENTER}
+            // justifyContent={JUSTIFY_SPACE_BETWEEN}
+          ></Flex>
+          <StyledText
+            as="p"
+            marginTop={SPACING.spacing4}
+            marginBottom={SPACING.spacing4}
           >
-            <StyledText as="h2" fontWeight={TYPOGRAPHY.fontWeightSemiBold}>
-              {t('robot_cal_help_title')}
-            </StyledText>
-            <Box onClick={onCloseClick} id={'RobotCalModal_xButton'}>
-              <Icon name={'close'} size="0.75rem" />
-            </Box>
-          </Flex>
-          <StyledText as="p" marginTop={SPACING.spacing5}>
             {t('robot_cal_description')}
           </StyledText>
-          <Link
-            external
-            css={TYPOGRAPHY.linkPSemiBold}
+          <ExternalLink
             href={robotCalHelpArticle}
             id={'RobotCalModal_helpArticleLink'}
           >
             {t('learn_more_about_robot_cal_link')}
-            <Icon
-              name={'open-in-new'}
-              marginLeft={SPACING.spacing2}
-              size="10px"
-            />
-          </Link>
-          <Box textAlign={ALIGN_CENTER} marginTop={SPACING.spacing5}>
+          </ExternalLink>
+          <Box textAlign={ALIGN_CENTER} marginTop={SPACING.spacing4}>
             <img src={RobotCalHelpImage} width="100%" />
           </Box>
           <StyledText
             as="p"
             fontWeight={TYPOGRAPHY.fontWeightSemiBold}
             marginTop={SPACING.spacing4}
-            role={'heading'}
           >
             {t('deck_calibration_title')}
           </StyledText>
-          <StyledText as="p" marginTop={SPACING.spacing4}>
+          <StyledText as="p" marginTop={SPACING.spacing2}>
             {t('deck_cal_description')}
           </StyledText>
           <StyledText
@@ -83,18 +81,17 @@ export function DeckCalibrationModal(
           >
             {t('tip_length_cal_title')}
           </StyledText>
-          <StyledText as="p" marginTop={SPACING.spacing4}>
+          <StyledText as="p" marginTop={SPACING.spacing2}>
             {t('tip_length_cal_description')}
           </StyledText>
           <StyledText
             as="p"
             fontWeight={TYPOGRAPHY.fontWeightSemiBold}
             marginTop={SPACING.spacing4}
-            role={'heading'}
           >
             {t('pipette_offset_cal')}
           </StyledText>
-          <StyledText as="p" marginTop={SPACING.spacing4}>
+          <StyledText as="p" marginTop={SPACING.spacing2}>
             <Trans
               t={t}
               i18nKey="pipette_offset_cal_description"
@@ -113,24 +110,26 @@ export function DeckCalibrationModal(
             </Flex>
             <Flex flexDirection={DIRECTION_ROW} marginLeft={SPACING.spacing4}>
               <Icon
-                name={'circle'}
+                name="circle"
                 marginRight={SPACING.spacing2}
-                marginTop={'0.35rem'}
+                marginTop="0.35rem"
                 size="4px"
               />
               {t('pipette_offset_cal_description_bullet_2')}
             </Flex>
           </StyledText>
-          <Box textAlign={ALIGN_CENTER} marginTop={SPACING.spacing5}>
-            <PrimaryButton
-              onClick={onCloseClick}
-              width={SIZE_4}
-              name="close"
-              id={'RobotCalModal_closeButton'}
-            >
-              {t('shared:close')}
-            </PrimaryButton>
-          </Box>
+          {/* <Box textAlign={ALIGN_CENTER} marginTop={SPACING.spacing5}> */}
+          {/* <PrimaryButton
+            onClick={onCloseClick}
+            alignSelf={ALIGN_FLEX_END}
+            textTransform={TEXT_TRANSFORM_CAPITALIZE}
+            // width={SIZE_4}
+            name="close"
+            id={'RobotCalModal_closeButton'}
+          >
+            {t('shared:close')}
+          </PrimaryButton> */}
+          {/* </Box> */}
         </Box>
       </Modal>
     </Portal>
