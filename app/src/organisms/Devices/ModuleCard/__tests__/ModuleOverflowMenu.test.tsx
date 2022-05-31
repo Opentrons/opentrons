0b1b15f3d555c961a1c77c54d765406c454c3a41
Jethary Rader <66035149+jerader@users.noreply.github.com>
2022-05-31 11:46:18 -0400

    feat(app): change H-S set shake speed button disabled reason (#10421)
    closes #10418

--- app/src/organisms/Devices/ModuleCard/__tests__/ModuleOverflowMenu.test.tsx
+++ app/src/organisms/Devices/ModuleCard/__tests__/ModuleOverflowMenu.test.tsx
@@ -67,6 +67,28 @@ const mockOpenLatchHeaterShaker = {
   usbPort: { path: '/dev/ot_module_heatershaker0', port: 1 },
 } as any
 
+const mockUnknownLatchHeaterShaker = {
+  id: 'heatershaker_id',
+  moduleModel: 'heaterShakerModuleV1',
+  moduleType: 'heaterShakerModuleType',
+  serialNumber: 'jkl123',
+  hardwareRevision: 'heatershaker_v4.0',
+  firmwareVersion: 'v2.0.0',
+  hasAvailableUpdate: true,
+  data: {
+    labwareLatchStatus: 'idle_unknown',
+    speedStatus: 'idle',
+    temperatureStatus: 'idle',
+    currentSpeed: null,
+    currentTemperature: null,
+    targetSpeed: null,
+    targetTemp: null,
+    errorDetails: null,
+    status: 'idle',
+  },
+  usbPort: { path: '/dev/ot_module_heatershaker0', port: 1 },
+} as any
+
 const mockCloseLatchHeaterShaker = {
   id: 'heatershaker_id',
   moduleModel: 'heaterShakerModuleV1',
@@ -337,6 +359,22 @@ describe('ModuleOverflowMenu', () => {
     })
 
     fireEvent.click(btn)
+  })
+
+  it('renders heater shaker set shake speed button disabled when labware latch status is unknown', () => {
+    props = {
+      module: mockUnknownLatchHeaterShaker,
+      handleSlideoutClick: jest.fn(),
+      handleAboutClick: jest.fn(),
+      handleTestShakeClick: jest.fn(),
+      handleWizardClick: jest.fn(),
+    }
+    const { getByRole } = render(props)
+    expect(
+      getByRole('button', {
+        name: 'Set shake speed',
+      })
+    ).toBeDisabled()
   })
 
   it('renders heater shaker overflow menu and deactivates heater when status changes', () => {
