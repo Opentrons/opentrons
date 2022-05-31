6688ab16e45c9d67b12468523466b5fb9e7a9e1d
Koji <kk2796@nyu.edu>
2022-05-31 18:34:44 -0400

    address pr comment

--- app/src/organisms/Devices/hooks/useIsRobotBusy.ts
+++ app/src/organisms/Devices/hooks/useIsRobotBusy.ts
@@ -1,12 +1,12 @@
 import { useAllSessionsQuery } from '@opentrons/react-api-client'
 
 import { useCurrentRunId } from '../../ProtocolUpload/hooks'
-import { checkIsRobotBusy } from '../RobotSettings/AdvancedTab/utils'
 
 export function useIsRobotBusy(): boolean {
-  const isRobotBusy = useCurrentRunId() !== null
+  const robotHasCurrentRun = useCurrentRunId() !== null
   const allSessionsQueryResponse = useAllSessionsQuery()
-  const isBusy = checkIsRobotBusy(allSessionsQueryResponse, isRobotBusy)
-
-  return isBusy
+  return (
+    robotHasCurrentRun ||
+    Object.keys(allSessionsQueryResponse?.data ?? {}).length !== 0
+  )
 }
