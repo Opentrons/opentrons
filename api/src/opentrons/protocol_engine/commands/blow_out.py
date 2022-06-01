50b4c19bd10ba84662c6241788c34b09586469d7
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-06-01 14:11:26 -0400

    sceleton blow out command

---
+++ api/src/opentrons/protocol_engine/commands/blow_out.py
@@ -0,0 +1,55 @@
+"""Blow-out command request, result, and implementation models."""
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional, Type
+from typing_extensions import Literal
+
+from .pipetting_common import BasePipettingParams
+from .command import AbstractCommandImpl, BaseCommand, BaseCommandCreate
+
+if TYPE_CHECKING:
+    from ..execution import PipettingHandler
+
+
+BlowOutCommandType = Literal["blow-out"]
+
+
+class BlowOutParams(BasePipettingParams):
+    """Payload required to blow-out to a specific well."""
+
+    pass
+
+
+class BlowOutResult(BasePipettingParams):
+    """Result data from the execution of a Blow-out command."""
+
+    pass
+
+
+class BlowOutImplementation(AbstractCommandImpl[BlowOutParams, BlowOutResult]):
+    """Dispense command implementation."""
+
+    def __init__(self, pipetting: PipettingHandler, **kwargs: object) -> None:
+        self._pipetting = pipetting
+
+    async def execute(self, params: BlowOutParams) -> BlowOutResult:
+        """Move to and blow-out to the requested well."""
+        pass
+
+
+class BlowOut(BaseCommand[BlowOutParams, BlowOutResult]):
+    """Dispense command model."""
+
+    commandType: BlowOutCommandType = "blow-out"
+    params: BlowOutParams
+    result: Optional[BlowOutResult]
+
+    _ImplementationCls: Type[BlowOutImplementation] = BlowOutImplementation
+
+
+class BlowOutCreate(BaseCommandCreate[BlowOutParams]):
+    """Create dispense command request model."""
+
+    commandType: BlowOutCommandType = "blow-out"
+    params: BlowOutParams
+
+    _CommandCls: Type[BlowOut] = BlowOut
---
+++ api/tests/opentrons/protocol_engine/commands/test_blow_out.py
@@ -0,0 +1,16 @@
+"""Test blow-out command."""
+
+
+def test_blow_out_request() -> None:
+    """It should be able to create a BlowOutRequest."""
+    pass
+
+
+def test_blow_out_result() -> None:
+    """It should be able to create a BlowOutResult."""
+    pass
+
+
+def test_blow_out_implementation() -> None:
+    """It should test blow out implementation."""
+    pass
