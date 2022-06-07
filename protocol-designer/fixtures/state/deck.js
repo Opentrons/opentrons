3b7100bca3f3939ba9111042dfce7ac52f8fa695
Shlok Amin <shlokamin@me.com>
2022-06-07 16:56:11 -0400

    feat(protocol-designer): disable placing module next to heater shaker
    closes #10449 and #10450

--- protocol-designer/fixtures/state/deck.js
+++ protocol-designer/fixtures/state/deck.js
@@ -5,6 +5,8 @@ import {
   TEMPERATURE_MODULE_V1,
   TEMPERATURE_MODULE_TYPE,
   TEMPERATURE_DEACTIVATED,
+  HEATERSHAKER_MODULE_TYPE,
+  HEATERSHAKER_MODULE_V1,
 } from '@opentrons/shared-data'
 
 const mockMagneticModule = {
@@ -30,6 +32,19 @@ const mockTemperatureModule = {
   model: TEMPERATURE_MODULE_V1,
 }
 
+const mockHeaterShakerModule = {
+  id: 'hs_mod',
+  moduleState: {
+    type: HEATERSHAKER_MODULE_TYPE,
+    targetTemp: null,
+    targetSpeed: null,
+    latchOpen: null,
+  },
+  type: HEATERSHAKER_MODULE_TYPE,
+  slot: '1',
+  model: HEATERSHAKER_MODULE_V1,
+}
+
 const mockDeckSetup = {
   labware: {
     well96Id: {
@@ -46,4 +61,5 @@ const mockDeckSetup = {
 
 export const getMockMagneticModule = () => mockMagneticModule
 export const getMockTemperatureModule = () => mockTemperatureModule
+export const getMockHeaterShakerModule = () => mockHeaterShakerModule
 export const getMockDeckSetup = () => mockDeckSetup
