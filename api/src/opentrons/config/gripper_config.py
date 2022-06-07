212dfc540f29fe05cc56335560fb22a70e7bf9b5
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2022-06-07 14:33:20 -0400

    feat(api, ot3): add gripper to get_attached_instruments  (#10619)

--- api/src/opentrons/config/gripper_config.py
+++ api/src/opentrons/config/gripper_config.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 import logging
-from typing import Tuple
+from typing import Tuple, Optional
 from typing_extensions import Literal
 
 log = logging.getLogger(__name__)
@@ -23,3 +23,22 @@ class GripperConfig:
     steps_per_mm: float
     idle_current: float
     model: GripperModel
+
+
+DUMMY_GRIPPER_CONFIG = GripperConfig(
+    gripper_offset=(0.0, 0.0, 0.0),
+    gripper_current=1.0,
+    display_name="dummy_gripper",
+    name="gripper",
+    max_travel=50.0,
+    home_position=0.0,
+    steps_per_mm=480.0,
+    idle_current=0.2,
+    model="gripper_v1",
+)
+
+
+def load(
+    gripper_model: Optional[int] = None, gripper_id: Optional[int] = None
+) -> GripperConfig:
+    return DUMMY_GRIPPER_CONFIG  # TODO: load actual gripper config
