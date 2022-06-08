3fbb5bbdc3a3f68859f9a650d06f186db23c22ee
Koji <kk2796@nyu.edu>
2022-06-08 17:51:13 -0400

    remove duplicated lines and unused import

--- app/src/organisms/Devices/RobotSettings/RobotSettingsCalibration.tsx
+++ app/src/organisms/Devices/RobotSettings/RobotSettingsCalibration.tsx
@@ -43,8 +43,6 @@ import * as Config from '../../../redux/config'
 import * as Sessions from '../../../redux/sessions'
 import * as Calibration from '../../../redux/calibration'
 import * as Pipettes from '../../../redux/pipettes'
-import * as PipetteOffset from '../../../redux/calibration/pipette-offset'
-import * as TipLength from '../../../redux/calibration/tip-length'
 import {
   useDeckCalibrationData,
   usePipetteOffsetCalibrations,
@@ -466,13 +464,12 @@ export function RobotSettingsCalibration({
     }
   }, [createStatus])
 
+  // Note this
   useInterval(
     () => {
       dispatch(Calibration.fetchCalibrationStatus(robotName))
       dispatch(Calibration.fetchPipetteOffsetCalibrations(robotName))
       dispatch(Calibration.fetchTipLengthCalibrations(robotName))
-      dispatch(PipetteOffset.fetchPipetteOffsetCalibrations(robotName))
-      dispatch(TipLength.fetchTipLengthCalibrations(robotName))
       checkPipetteCalibrationMissing()
     },
     CALS_FETCH_MS,
