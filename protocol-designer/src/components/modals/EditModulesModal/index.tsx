4bc9c305ea7b8449075ce624e56b457e83830d6b
Shlok Amin <shlokamin@me.com>
2022-06-09 12:20:08 -0400

    refactor(protocol-designer): remove too tall labware check in edit modules modal

--- protocol-designer/src/components/modals/EditModulesModal/index.tsx
+++ protocol-designer/src/components/modals/EditModulesModal/index.tsx
@@ -13,7 +13,6 @@ import {
 } from '@opentrons/components'
 import {
   getAreSlotsAdjacent,
-  getIsLabwareAboveHeight,
   THERMOCYCLER_MODULE_TYPE,
   MAGNETIC_MODULE_TYPE,
   HEATERSHAKER_MODULE_TYPE,
@@ -21,7 +20,6 @@ import {
   THERMOCYCLER_MODULE_V1,
   ModuleType,
   ModuleModel,
-  MAX_LABWARE_HEIGHT_EAST_WEST_HEATER_SHAKER_MM,
 } from '@opentrons/shared-data'
 import { i18n } from '../../../localization'
 import {
@@ -149,28 +147,12 @@ export const EditModulesModal = (props: EditModulesModalProps): JSX.Element => {
         { selectedSlot }
       )
     } else if (selectedModel === HEATERSHAKER_MODULE_V1) {
-      const labwareNextToHeaterShaker = getLabwareOnSlot(
-        initialDeckSetup,
-        getSlotNextTo(selectedSlot) ?? ''
-      )
-
       const isHeaterShakerAdjacentToAnotherModule = some(
         initialDeckSetup.modules,
         hwModule => getAreSlotsAdjacent(hwModule.slot, selectedSlot)
       )
 
-      if (
-        labwareNextToHeaterShaker &&
-        getIsLabwareAboveHeight(
-          labwareNextToHeaterShaker.def,
-          MAX_LABWARE_HEIGHT_EAST_WEST_HEATER_SHAKER_MM
-        )
-      ) {
-        errors.selectedSlot = i18n.t(
-          'alert.module_placement.HEATER_SHAKER_ADJACENT_LABWARE_TOO_TALL.body',
-          { selectedSlot }
-        )
-      } else if (isHeaterShakerAdjacentToAnotherModule) {
+      if (isHeaterShakerAdjacentToAnotherModule) {
         errors.selectedSlot = i18n.t(
           'alert.module_placement.HEATER_SHAKER_ADJACENT_TO_ANOTHER_MODULE.body',
           { selectedSlot }
