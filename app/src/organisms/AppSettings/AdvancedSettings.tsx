f8afc7b8669bdc88a23275a3cf4778cacbb810ff
Jethary Rader <jethary@opentrons.com>
2022-06-10 10:17:34 -0400

    change i18n key names

--- app/src/organisms/AppSettings/AdvancedSettings.tsx
+++ app/src/organisms/AppSettings/AdvancedSettings.tsx
@@ -362,13 +362,13 @@ export function AdvancedSettings(): JSX.Element {
               paddingBottom={SPACING.spacing3}
               id="AdvancedSettings_unavailableRobots"
             >
-              {t('display_robot_caching')}
+              {t('disable_robot_caching')}
             </StyledText>
             <StyledText as="p" marginBottom={SPACING.spacing4}>
               {t('note_this_will_clear_caching')}
             </StyledText>
             <StyledText as="p">
-              {t('display_robot_caching_descriptions')}
+              {t('disable_robot_caching_descriptions')}
             </StyledText>
           </Box>
           <ToggleButton
