d3c7612dc359d34f00ee5e43af6b09bcb468f3ca
Ayana Perry <ayana.perry@opentrons.com>
2022-06-13 14:55:23 -0400

    style(app): fixed merge conflict
    fixed merge conflict
    
    #10488

--- app/src/atoms/buttons/index.tsx
+++ app/src/atoms/buttons/index.tsx
@@ -34,10 +34,6 @@ export const TertiaryButton = styled(NewPrimaryBtn)`
     background-color: ${COLORS.bluePressed};
   }
 
-  &:enabled {
-    background-color: ${COLORS.transparent};
-  }
-
   &:focus {
     box-shadow: 0 0 0 3px ${COLORS.warning};
   }
@@ -105,6 +101,10 @@ export const SecondaryButton = styled(NewSecondaryBtn)`
     box-shadow: 0 0 0 3px ${COLORS.warning};
   }
 
+  &:enabled {
+    background-color: ${COLORS.transparent};
+  }
+  
   &:disabled {
     opacity: 50%;
   }
