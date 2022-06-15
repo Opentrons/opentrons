24c9a85f683a08996a7c997e39bca80aa48f9090
Koji <kk2796@nyu.edu>
2022-06-15 16:29:49 -0400

    fix check-js error

--- app/src/organisms/AppSettings/__tests__/ConnectRobotSlideout.test.tsx
+++ app/src/organisms/AppSettings/__tests__/ConnectRobotSlideout.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { fireEvent, queryByText } from '@testing-library/react'
+import { fireEvent } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
 import { renderWithProviders } from '@opentrons/components'
 import { i18n } from '../../../i18n'
