4a8e14fa3a02ea1d0904f0a1b09ffc2df5c19c70
Ayana Perry <ayana.perry@opentrons.com>
2022-06-16 15:05:58 -0400

    style(app): added the paragraph styling to the description section
    added the paragraph styling to the description section
    
    #10682

--- app/src/organisms/ProtocolDetails/index.tsx
+++ app/src/organisms/ProtocolDetails/index.tsx
@@ -140,7 +140,7 @@ const ReadMoreContent = (props: ReadMoreContentProps): JSX.Element => {
           flexDirection={DIRECTION_COLUMN}
           data-testid={`ProtocolDetails_description`}
         >
-          <StyledText>{description}</StyledText>
+          <StyledText as="p">{description}</StyledText>
           {filteredMetaData.map((item, index) => {
             return (
               <React.Fragment key={index}>
