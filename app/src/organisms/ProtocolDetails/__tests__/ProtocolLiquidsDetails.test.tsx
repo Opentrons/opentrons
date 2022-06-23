6ea46dd5090da9fac8f1539422943287b76f2382
Sakib Hossain <sakib.hossain@opentrons.com>
2022-06-23 14:54:32 -0400

    fix test

--- app/src/organisms/ProtocolDetails/__tests__/ProtocolLiquidsDetails.test.tsx
+++ app/src/organisms/ProtocolDetails/__tests__/ProtocolLiquidsDetails.test.tsx
@@ -1,40 +1,26 @@
 import * as React from 'react'
 import { renderWithProviders } from '@opentrons/components'
 import { ProtocolLiquidsDetails } from '../ProtocolLiquidsDetails'
+import { LiquidsListItemDetails } from '../../Devices/ProtocolRun/SetupLiquids/SetupLiquidsList'
 
-const MOCK_LIQUID = [
-  {
-    liquidId: '0',
-    displayName: 'mock liquid 1',
-    description: 'mock sample',
-    displayColor: '#ff4888',
-    locations: [
-      {
-        labwareName: 'Mock Labware',
-        slotName: '5',
-        volumeByWell: {
-          C1: 50,
-          C2: 50,
-        },
-      },
-    ],
-  },
-]
+jest.mock('../../Devices/ProtocolRun/SetupLiquids/SetupLiquidsList')
 
-const render = (props: React.ComponentProps<typeof ProtocolLiquidsDetails>) => {
-  return renderWithProviders(<ProtocolLiquidsDetails {...props} />)
+const mockLiquidsListItemDetails = LiquidsListItemDetails as jest.MockedFunction<
+  typeof LiquidsListItemDetails
+>
+
+const render = () => {
+  return renderWithProviders(<ProtocolLiquidsDetails />)
 }
 
 describe('ProtocolLiquidsDetails', () => {
-  let props: React.ComponentProps<typeof ProtocolLiquidsDetails>
   beforeEach(() => {
-    props = { liquids: MOCK_LIQUID }
+    mockLiquidsListItemDetails.mockReturnValue(
+      <div>mock liquids list item</div>
+    )
   })
-
   it('renders the display name, description and total volume', () => {
-    const [{ getByText }] = render(props)
-    getByText('100 µL')
-    getByText('mock liquid 1')
-    getByText('mock sample')
+    const [{ getAllByText }] = render()
+    getAllByText('mock liquids list item')
   })
 })
