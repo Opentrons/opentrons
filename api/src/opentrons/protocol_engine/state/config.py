14e295f56ace5226977c470731e0f7dac97b365d
Mike Cousins <mike@opentrons.com>
2022-06-28 09:53:46 -0400

    fixup: add docstring

--- api/src/opentrons/protocol_engine/state/config.py
+++ api/src/opentrons/protocol_engine/state/config.py
@@ -4,7 +4,16 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Config:
-    """ProtocolEngine configuration options."""
+    """ProtocolEngine configuration options.
+
+    Params:
+        ignore_pause: The engine should no-op instead of waiting
+            for pauses and delays to complete.
+        use_virtual_modules: The engine should no-op instead of calling
+            modules' hardware control API.
+        block_on_door_open: Protocol execution should pause if the
+            front door is opened.
+    """
 
     ignore_pause: bool = False
     use_virtual_modules: bool = False
