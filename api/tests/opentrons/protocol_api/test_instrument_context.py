565449ed616525ad8aa1dd33db177b62465096e6
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-07-05 17:28:31 -0400

    fixed subject

--- api/tests/opentrons/protocol_api/test_instrument_context.py
+++ api/tests/opentrons/protocol_api/test_instrument_context.py
@@ -2,12 +2,19 @@
 import pytest
 
 from decoy import Decoy
+from opentrons.protocol_api import ProtocolContext
 
 from opentrons.protocol_api.instrument_context import InstrumentContext
 from opentrons.protocols.context.instrument import AbstractInstrument
 from opentrons.hardware_control.instruments.pipette import Pipette
 # from opentrons.config import pipette_config
 # from opentrons.calibration_storage import types as cal_types
+from opentrons.types import Mount, Location, Point
+
+
+@pytest.fixture
+def mock_protocol_context(decoy: Decoy) -> ProtocolContext:
+    return decoy.mock(cls=ProtocolContext)
 
 
 @pytest.fixture
@@ -26,9 +33,17 @@ def mock_pipette_implementation(decoy: Decoy) -> Pipette:
 
 
 @pytest.fixture
-def subject(decoy: Decoy,mock_pipette_implementation: AbstractInstrument) -> InstrumentContext:
-    return decoy.mock(cls=InstrumentContext)
+def subject(decoy: Decoy, mock_pipette_implementation: AbstractInstrument, mock_protocol_context: ProtocolContext) -> InstrumentContext:
+    InstrumentContext(implementation=mock_pipette_implementation, ctx=mock_protocol_context)
 
 
-def test_pick_up_from_location(subject: InstrumentContext) -> None:
+def test_pick_up_from_location(decoy: Decoy, subject: InstrumentContext) -> None:
     """Should pick up tip from supplied location."""
+    # tiprack = mock_protocol_context.load_labware("opentrons_96_tiprack_300ul", 1)
+    #
+    # decoy.when(mock_protocol_context.load_instrument("p300_single", Mount.LEFT, tip_racks=[tiprack]))
+    target = Point(-100, -100, 0)
+
+    subject.pick_up_tip(location=Location(Point(-100, -100, 0), labware="Well"))
+
+    decoy.verify(subject.move_to(target))
