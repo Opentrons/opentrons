246b79d76034262e95c09ced0bd087c2f0f1b7ce
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-07-06 13:46:30 -0400

    fixed test calls. cleanup. added monkeypatch

--- api/tests/opentrons/protocol_api/test_instrument_context.py
+++ api/tests/opentrons/protocol_api/test_instrument_context.py
@@ -1,19 +1,25 @@
 """Tests for the InstrumentContext class."""
 import pytest
+import sys
 
 from decoy import Decoy
+
 from opentrons.protocol_api import ProtocolContext
 
 from opentrons.protocol_api.instrument_context import InstrumentContext
 from opentrons.protocols.context.instrument import AbstractInstrument
-from opentrons.hardware_control.instruments.pipette import Pipette
-# from opentrons.config import pipette_config
-# from opentrons.calibration_storage import types as cal_types
-from opentrons.protocols.context.well import WellImplementation
 from opentrons.types import Mount, Location, Point
 from opentrons.broker import Broker
 from opentrons.protocols.api_support.types import APIVersion
 from opentrons.protocol_api.labware import Well
+from opentrons.protocols.api_support.instrument import validate_tiprack
+
+@pytest.fixture(autouse=True)
+def patch_mock_validate_tiprack(decoy: Decoy, monkeypatch: pytest.MonkeyPatch) -> None:
+    """Replace motion_planning.get_waypoints() with a mock."""
+    mock_validate_tiprack = decoy.mock(func=validate_tiprack)
+    monkeypatch.setattr("opentrons.protocols.api_support.instrument.validate_tiprack", mock_validate_tiprack)
+
 
 
 @pytest.fixture
@@ -22,18 +28,8 @@ def mock_protocol_context(decoy: Decoy) -> ProtocolContext:
 
 
 @pytest.fixture
-def mock_pipette_implementation(decoy: Decoy) -> Pipette:
-    return decoy.mock(cls=Pipette)
-        # Pipette(
-        #     config=pipette_config.load("p10_single_v1"),
-        #     pipette_offset_cal=cal_types.PipetteOffsetByPipetteMount(
-        #         offset=[0, 0, 0],
-        #         source=cal_types.SourceType.user,
-        #         status=cal_types.CalibrationStatus(),
-        #     ),
-        #     pipette_id="test-id",
-        # ),
-    # )
+def mock_pipette_implementation(decoy: Decoy) -> AbstractInstrument:
+    return decoy.mock(cls=AbstractInstrument)
 
 
 @pytest.fixture
@@ -43,13 +39,11 @@ def subject(decoy: Decoy, mock_pipette_implementation: AbstractInstrument, mock_
 
 def test_pick_up_from_location(decoy: Decoy, subject: InstrumentContext) -> None:
     """Should pick up tip from supplied location."""
-    # tiprack = mock_protocol_context.load_labware("opentrons_96_tiprack_300ul", 1)
-    #
-    # decoy.when(mock_protocol_context.load_instrument("p300_single", Mount.LEFT, tip_racks=[tiprack]))
-    mock_well_implementation = decoy.mock(cls=WellImplementation)
+    mock_well = decoy.mock(cls=Well)
+
     target = Point(-100, -100, 0)
-    location = Location(point=Point(-100, -100, 0), labware="opentrons_96_tiprack_300ul")
-    # Well(well_implementation=mock_well_implementation))
+    location = Location(point=Point(-100, -100, 0), labware=mock_well)
+
     subject.pick_up_tip(location=location)
 
     decoy.verify(subject.move_to(target))
