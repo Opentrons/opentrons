f32fcf6e99e824f7fe8c2a8ff3f3bd5dcac64c1c
Mike Cousins <mike@opentrons.com>
2022-07-07 13:02:15 -0400

    refactor(robot-server): remove unused liveProtocol session

--- robot-server/robot_server/service/session/session_types/__init__.py
+++ robot-server/robot_server/service/session/session_types/__init__.py
@@ -3,4 +3,3 @@ from .tip_length_calibration import TipLengthCalibration  # noqa: F401
 from .deck_calibration_session import DeckCalibrationSession  # noqa: F401
 from .pipette_offset_calibration import PipetteOffsetCalibrationSession  # noqa: F401
 from .base_session import BaseSession, SessionMetaData  # noqa: F401
-from .live_protocol.session import LiveProtocolSession  # noqa: F401
