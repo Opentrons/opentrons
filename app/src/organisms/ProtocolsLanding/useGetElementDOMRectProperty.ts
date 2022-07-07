5c21f15100724af722c6ad8a3f8bc1d40fcbc844
Koji <kk2796@nyu.edu>
2022-07-07 12:20:30 -0400

    Add jsdoc

--- app/src/organisms/ProtocolsLanding/useGetElementDOMRectProperty.ts
+++ app/src/organisms/ProtocolsLanding/useGetElementDOMRectProperty.ts
@@ -5,6 +5,23 @@ interface props {
     getElementProperty: (property: DOMRectProperty) => number;
 }
 
+/**
+ *
+ * @param   {React.RefObject} ref(useRef)
+ * @returns {{ getElementProperty: (property: DOMRectProperty) => number }}
+ *          Function returns DOMRectProperty such as x, y, width, height, top, left, right, bottom.
+ * @example
+ * import { useGetElementDOMRectProperty } from './useGetElementDOMRectProperty';
+ * const targetRef = React.useRef(null)
+ * const { getElementProperty } = useGetElementDOMRectProperty<HTMLDivElement>(targetRef)
+ *
+ * const height = getElementProperty('height') // need to specify property
+ *
+ * <Flex ref={targetRef}>
+ *  <StyledText as="p">example</StyledText>
+ * </Flex>
+ *
+ */
 export const useGetElementDOMRectProperty = <T extends HTMLElement>(
 elementRef: React.RefObject<T>): props  => {
     const getElementProperty = React.useCallback(
