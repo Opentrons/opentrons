4725007825997067c29ae0d49a3fa9990a68594b
Vincent Abruzzo <vincent.abruzzo@opentrons.com>
2022-07-11 15:36:36 -0400

    nav hhover

--- app/src/App/Navbar.tsx
+++ app/src/App/Navbar.tsx
@@ -33,7 +33,7 @@ const NavbarLink = styled(NavLink)`
   background-color: ${COLORS.darkBlack};
 
   &:hover {
-    background-color: ${COLORS.darkGreyHover};
+    background-color: ${COLORS.darkBlackHover};
   }
 
   &:focus-visible {
