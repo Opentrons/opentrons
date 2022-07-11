892c5aee4385d9010b1da9a69cefdd91774049f0
andySigler <andrewsigler1@gmail.com>
2022-07-11 16:01:03 -0400

    tests

--- hardware-testing/tests/drivers/radwag/test_driver.py
+++ hardware-testing/tests/drivers/radwag/test_driver.py
@@ -11,7 +11,13 @@ from hardware_testing.drivers import RadwagScale
 @pytest.fixture
 def scale_connection() -> MagicMock:
     """Mock scale connection."""
-    return MagicMock()
+    mm = MagicMock()
+
+    def _write(b: bytes) -> int:
+        return len(b)
+
+    mm.write.side_effect = _write
+    return mm
 
 
 @pytest.fixture
@@ -38,23 +44,23 @@ def create_radwag_result_line(
     # 5 = sign
     # 6-14 = data
     # 16-18 = unit
-    return f"{command:3s}{stability_char[0]} {sign}{str(abs(val)).rjust(9)} {'g':3s}".encode()
+    return f"{command:3s}{stability_char[0]} {sign}{str(abs(val)).rjust(9)} {'g':3s}\r\n".encode()
 
 
 @pytest.mark.parametrize(
     argnames="masses,expected",
     argvalues=[
         # All the same
-        [[5.5, 5.5, 5.5, 5.5], 5.5],
+        [[5.5, 5.5, 5.5, 5.5], [(5.5, True), (5.5, True), (5.5, True), (5.5, True)]],
         # Remove outlier
-        [[1.0, 12.0, 1.0], 1.0],
+        [[1.0, 12.0, 1.0], [(1.0, True), (12.0, True), (1.0, True)]],
     ],
 )
 def test_read_mass(
     subject: RadwagScale,
     scale_connection: MagicMock,
     masses: List[float],
-    expected: float,
+    expected: List[tuple],
 ) -> None:
     """It should read the mass and return the average."""
     scale_connection.readline.side_effect = [
