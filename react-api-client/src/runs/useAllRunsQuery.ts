e8cd3cd05e6a442b0bed35683e60a09af14c7541
Koji <kk2796@nyu.edu>
2022-07-13 16:43:38 -0400

    fix check-js error

--- react-api-client/src/runs/useAllRunsQuery.ts
+++ react-api-client/src/runs/useAllRunsQuery.ts
@@ -5,11 +5,11 @@ import { useHost } from '../api'
 import type { UseQueryOptions } from 'react-query'
 
 export function useAllRunsQuery(
-  options: UseQueryOptions<Runs, Error> = {}
-): UseQueryResult<Runs, Error> {
+  options: UseQueryOptions<Runs, Error, Runs, Array<string|HostConfig>> = {}
+): UseQueryResult<Runs> {
   const host = useHost()
   const query = useQuery(
-    [host, 'runs', 'details'],
+    [host as HostConfig, 'runs', 'details'],
     () => getRuns(host as HostConfig).then(response => response.data),
     { enabled: host !== null, ...options }
   )
