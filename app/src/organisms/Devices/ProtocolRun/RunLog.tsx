5039dea46dde7c35c604a6b27522664037c17316
Brent Hagen <brent.hagen@opentrons.com>
2022-07-18 11:27:08 -0400

    fix(app): add firstNonSetupIndex to historical run commands query cursor
    this omits the non setup commands from the commands query for historical runs, which was the cause
    of some run commands at the end of the run log not being fetched
    
    closes #11135

--- app/src/organisms/Devices/ProtocolRun/RunLog.tsx
+++ app/src/organisms/Devices/ProtocolRun/RunLog.tsx
@@ -162,6 +162,7 @@ export function RunLog({ robotName, runId }: RunLogProps): JSX.Element | null {
         firstPostInitialPlayRunCommandIndex.current =
           lastKnownPrePlayRunCommandIndex.current +
           foundPostPlayRunCommandIndex +
+          firstNonSetupIndex +
           1
       } else {
         lastKnownPrePlayRunCommandIndex.current =
