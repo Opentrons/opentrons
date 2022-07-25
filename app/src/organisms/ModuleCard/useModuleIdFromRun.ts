b29a6b5ee206097278689ec0d526d65e417a69a9
Jethary Rader <jethary@opentrons.com>
2022-07-25 10:12:37 -0400

    fix bug in useModuleIdFromRun

--- app/src/organisms/ModuleCard/useModuleIdFromRun.ts
+++ app/src/organisms/ModuleCard/useModuleIdFromRun.ts
@@ -1,6 +1,6 @@
+import { getModuleDef2 } from '@opentrons/shared-data'
 import { useAttachedModules, useProtocolDetailsForRun } from '../Devices/hooks'
 import type { AttachedModule } from '../../redux/modules/types'
-import { getModuleDef2 } from '@opentrons/shared-data'
 
 export interface ModuleIdFromRun {
   moduleIdFromRun: string
@@ -32,7 +32,12 @@ export function useModuleIdFromRun(
     attachedModule => attachedModule.serialNumber === module.serialNumber
   )
 
-  const moduleIdFromRun = loadModuleCommands?.[moduleIndex].result.moduleId
+  //  result is possibly undefined if you send a module command when run is terminal
+  //  to a module that is not in the protocol but plugged into the robot
+  const moduleIdFromRun =
+    loadModuleCommands?.[moduleIndex] != null
+      ? loadModuleCommands?.[moduleIndex].result.moduleId
+      : ''
 
   return { moduleIdFromRun }
 }
