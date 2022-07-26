ffab6baaae64d0cff381b2abacc83ffc0b64c844
Derek Maggio <derek.maggio@opentrons.com>
2022-07-26 06:58:04 -0700

    Overhaul cli to allow for selection of individual PAPI versions

--- g-code-testing/cli.py
+++ g-code-testing/cli.py
@@ -1,17 +1,32 @@
+"""Command Line Interface for making use of G-Code Parsing Commands."""
+
 import sys
+import re
 import argparse
-from typing import Dict, Any, List, Union
+from typing import (
+    Callable,
+    Dict,
+    Any,
+    List,
+    Optional,
+    Union,
+)
+
+from opentrons import APIVersion
+
 from g_code_parsing.errors import UnparsableCLICommandError
 from g_code_parsing.g_code_differ import GCodeDiffer
-from g_code_test_data.g_code_configuration import HTTPGCodeConfirmConfig, \
-    ProtocolGCodeConfirmConfig
+from g_code_test_data.g_code_configuration import (
+    HTTPGCodeConfirmConfig,
+    ProtocolGCodeConfirmConfig,
+)
 from g_code_test_data.http.http_configurations import HTTP_CONFIGURATIONS
 from g_code_test_data.protocol.protocol_configurations import PROTOCOL_CONFIGURATIONS
 
 
 class GCodeCLI:
-    """
-    CLI for G-Code Parser.
+    """CLI for G-Code Parser.
+
     Takes input from command line, parses it and performs any post-processing.
     The provides run_command method to run passed input.
     """
@@ -29,43 +44,67 @@ class GCodeCLI:
     CONFIGURATION_COMMAND = "configurations"
     CONFIGURATIONS = HTTP_CONFIGURATIONS + PROTOCOL_CONFIGURATIONS
 
-    LOAD_COMPARISON_COMMAND = 'load-comparison'
-    UPDATE_COMPARISON_COMMAND = 'update-comparison'
+    LOAD_COMPARISON_COMMAND = "load-comparison"
+    UPDATE_COMPARISON_COMMAND = "update-comparison"
+    CHECK_MISSING_COMP_FILES = "check-for-missing-comparison-files"
+
+    API_VERSION_REGEX = re.compile(r"\/(\d+\.\d+)\/")
 
     @classmethod
     def parse_args(cls, args: List[str]) -> Dict[str, Any]:
-        """
-        Parse args from arg list.
-        """
+        """Parse args from arg list."""
         parsed_dict = vars(cls.parser().parse_args(args))
         return parsed_dict
 
     @classmethod
-    def _create_configuration_dict(cls) -> Dict[
-        str, Union[HTTPGCodeConfirmConfig, ProtocolGCodeConfirmConfig]
-    ]:
-        """Create lookup dict for configurations"""
+    def _create_configuration_dict(
+        cls,
+    ) -> Dict[str, Union[HTTPGCodeConfirmConfig, ProtocolGCodeConfirmConfig]]:
+        """Create lookup dict for configurations."""
         configurations = {}
         for conf in cls.CONFIGURATIONS:
-            configurations[conf.get_configuration_path()] = conf
+            if isinstance(conf, ProtocolGCodeConfirmConfig):
+                for version in conf.versions:
+                    configurations[conf.get_configuration_paths(version)] = conf
+            else:
+                configurations[conf.get_configuration_paths()] = conf
         return configurations
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Create GCodeCLI object."""
         self._args = self.parse_args(sys.argv[1:])
         self.configurations = self._create_configuration_dict()
         self.respond_with_error_code = False
 
-    def _run(self) -> str:
-        """Execute G-Code Configuration"""
-        return self.configurations[self.args[self.CONFIGURATION_NAME]].execute()
+        self._command_lookup_dict = {
+            self.RUN_COMMAND: self._run,
+            self.DIFF_FILES_COMMAND: self._diff,
+            self.LOAD_COMPARISON_COMMAND: self._pull,
+            self.UPDATE_COMPARISON_COMMAND: self._update_comparison,
+            self.CONFIGURATION_COMMAND: self._configurations,
+            self.CHECK_MISSING_COMP_FILES: self._check_for_missing_comparison_files,
+        }
 
-    def _diff(self) -> str:
-        """Diff G-Code Configuration against stored S3 file"""
+    def _run(self, version: Optional[APIVersion]) -> str:
+        """Execute G-Code Configuration."""
+        configuration = self.configurations[self.args[self.CONFIGURATION_NAME]]
+        return (
+            configuration.execute(version)
+            if version is not None
+            else configuration.execute()
+        )
+
+    def _diff(self, version: Optional[APIVersion]) -> str:
+        """Diff G-Code Configuration against stored comparison file."""
         configuration = self.configurations[self.args[self.CONFIGURATION_NAME]]
         able_to_respond_with_error_code = self.args[self.ERROR_ON_DIFFERENT_FILES]
 
-        actual = configuration.execute()
-        expected = configuration.get_comparison_file()
+        if version is not None:
+            actual = configuration.execute(version)
+            expected = configuration.get_comparison_file(version)
+        else:
+            actual = configuration.execute()
+            expected = configuration.get_comparison_file()
 
         differ = GCodeDiffer(actual, expected)
         strings_equal = differ.strings_are_equal()
@@ -81,50 +120,67 @@ class GCodeCLI:
         return text
 
     def _configurations(self) -> str:
-        """Get a list of runnable G-Code Configurations"""
-        path_string = '\n'.join(
-            [
-                conf.get_configuration_path()
-                for conf in self.configurations.values()
-            ]
-        )
+        """Get a list of runnable G-Code Configurations."""
+        path_string = "\n".join(self.configurations.keys())
         return f"Runnable Configurations:\n{path_string}"
 
-    def _pull(self) -> str:
-        """Load comparison file"""
-        return self.configurations[self.args[self.CONFIGURATION_NAME]].get_comparison_file()
+    def _pull(self, version: Optional[APIVersion]) -> str:
+        """Load comparison file."""
+        config = self.configurations[self.args[self.CONFIGURATION_NAME]]
+        return (
+            config.get_comparison_file(version)
+            if version is not None
+            else config.get_comparison_file()
+        )
 
-    def _update_comparison(self) -> str:
-        """Create/Override comparison file with output of execution"""
-        return self.configurations[self.args[self.CONFIGURATION_NAME]].update_comparison()
+    def _update_comparison(self, version: Optional[APIVersion]) -> str:
+        """Create/Override comparison file with output of execution."""
+        config = self.configurations[self.args[self.CONFIGURATION_NAME]]
+        return (
+            config.update_comparison(version)
+            if version is not None
+            else config.update_comparison()
+        )
+
+    def _check_for_missing_comparison_files(self) -> str:
+        print(self.configurations)
+
+    def _get_command_func(self, passed_command_name: str) -> Callable:
+        try:
+            command_func = self._command_lookup_dict[passed_command_name]
+        except KeyError:
+            raise UnparsableCLICommandError(
+                passed_command_name, list(self._command_lookup_dict.keys())
+            )
+        return command_func
+
+    def _get_version(
+        self, config: Union[ProtocolGCodeConfirmConfig, HTTPGCodeConfirmConfig]
+    ) -> APIVersion:
+        if isinstance(config, ProtocolGCodeConfirmConfig):
+            match = self.API_VERSION_REGEX.search(self.args[self.CONFIGURATION_NAME])
+            version = APIVersion.from_string(match.group(1))
+        else:
+            version = None
+
+        return version
 
     def run_command(self) -> str:
-        """Run command and return it's output"""
+        """Run command and return it's output."""
         passed_command_name = self.args[self.COMMAND_KEY]
 
-        if passed_command_name == self.RUN_COMMAND:
-            command_output = self._run()
-        elif passed_command_name == self.DIFF_FILES_COMMAND:
-            command_output = self._diff()
-        elif passed_command_name == self.CONFIGURATION_COMMAND:
-            command_output = self._configurations()
-        elif passed_command_name == self.LOAD_COMPARISON_COMMAND:
-            command_output = self._pull()
-        elif passed_command_name == self.UPDATE_COMPARISON_COMMAND:
-            command_output = self._update_comparison()
-        else:
-            raise UnparsableCLICommandError(
-                passed_command_name, [self.RUN_COMMAND, self.DIFF_FILES_COMMAND]
-            )
-        return command_output
+        if passed_command_name == self.CONFIGURATION_COMMAND:
+            return self._configurations()
+
+        config = self.configurations[self.args[self.CONFIGURATION_NAME]]
+
+        version = self._get_version(config)
+        command_func = self._get_command_func(passed_command_name)
+        return command_func(version)
 
     @classmethod
     def parser(cls) -> argparse.ArgumentParser:
-        """
-        Method to generate argparse ArgumentParser class for parsing command line
-        input
-        :return: Parser object
-        """
+        """Generates argparse ArgumentParser class for parsing command line input."""
         parser = argparse.ArgumentParser(description="CLI for G-Code Parser")
         subparsers = parser.add_subparsers(
             title="Supported commands",
@@ -135,7 +191,8 @@ class GCodeCLI:
             f"{cls.DIFF_FILES_COMMAND} | "
             f"{cls.CONFIGURATION_COMMAND} | "
             f"{cls.LOAD_COMPARISON_COMMAND} | "
-            f"{cls.UPDATE_COMPARISON_COMMAND}"
+            f"{cls.UPDATE_COMPARISON_COMMAND} | "
+            f"{cls.CHECK_MISSING_COMP_FILES}",
         )
 
         run_parser = subparsers.add_parser(
@@ -154,16 +211,21 @@ class GCodeCLI:
             f"--{cls.ERROR_ON_DIFFERENT_FILES}",
             help="If set, return code 1 on files with different content",
             action="store_true",
-            default=False
+            default=False,
         )
         diff_parser.add_argument(
             "configuration_name",
             type=str,
-            help="Name of configuration you want to diff"
+            help="Name of configuration you want to diff",
         )
 
         subparsers.add_parser(
             cls.CONFIGURATION_COMMAND, help="List of available configurations"
+        )
+
+        subparsers.add_parser(
+            cls.CHECK_MISSING_COMP_FILES,
+            help="Check if there are any comparison files missing.",
         )
 
         load_comparison_parser = subparsers.add_parser(
@@ -174,7 +236,7 @@ class GCodeCLI:
         load_comparison_parser.add_argument(
             "configuration_name",
             type=str,
-            help="Name of configuration you want to pull"
+            help="Name of configuration you want to pull",
         )
 
         update_comparison_parser = subparsers.add_parser(
@@ -185,17 +247,19 @@ class GCodeCLI:
         update_comparison_parser.add_argument(
             "configuration_name",
             type=str,
-            help="Name of configuration you want to push"
+            help="Name of configuration you want to push",
         )
 
         return parser
 
     @property
-    def respond_with_error(self):
+    def respond_with_error(self) -> bool:
+        """Whether ot not CLI should respond with error code on internal error."""
         return self.respond_with_error_code
 
     @property
-    def args(self):
+    def args(self) -> Dict[str, Any]:
+        """Passed CLI args."""
         return self._args
 
 
