0660668e5b753aa04111aa5ecc436a7b1cffb7eb
Sanniti <sanniti@opentrons.com>
2022-07-26 18:08:40 -0400

    update signature too

--- api/src/opentrons/motion_planning/adjacent_slots_getters.py
+++ api/src/opentrons/motion_planning/adjacent_slots_getters.py
@@ -3,45 +3,45 @@
 from typing import Optional, List
 
 
-def _get_north_slot(location: int) -> Optional[int]:
-    if location in [10, 11, 12]:
+def _get_north_slot(slot: int) -> Optional[int]:
+    if slot in [10, 11, 12]:
         return None
     else:
-        return location + 3
+        return slot + 3
 
 
-def get_south_slot(location: int) -> Optional[int]:
-    if location in [1, 2, 3]:
+def get_south_slot(slot: int) -> Optional[int]:
+    if slot in [1, 2, 3]:
         return None
     else:
-        return location - 3
+        return slot - 3
 
 
-def _get_east_slot(location: int) -> Optional[int]:
-    if location in [3, 6, 9, 12]:
+def _get_east_slot(slot: int) -> Optional[int]:
+    if slot in [3, 6, 9, 12]:
         return None
     else:
-        return location + 1
+        return slot + 1
 
 
-def _get_west_slot(location: int) -> Optional[int]:
-    if location in [1, 4, 7, 10]:
+def _get_west_slot(slot: int) -> Optional[int]:
+    if slot in [1, 4, 7, 10]:
         return None
     else:
-        return location - 1
+        return slot - 1
 
 
-def get_east_west_slots(location: int) -> List[int]:
-    east = _get_east_slot(location)
-    west = _get_west_slot(location)
-    return [maybe_loc for maybe_loc in [east, west] if maybe_loc is not None]
+def get_east_west_slots(slot: int) -> List[int]:
+    east = _get_east_slot(slot)
+    west = _get_west_slot(slot)
+    return [maybe_slot for maybe_slot in [east, west] if maybe_slot is not None]
 
 
-def get_north_south_slots(location: int) -> List[int]:
-    north = _get_north_slot(location)
-    south = get_south_slot(location)
-    return [maybe_loc for maybe_loc in [north, south] if maybe_loc is not None]
+def get_north_south_slots(slot: int) -> List[int]:
+    north = _get_north_slot(slot)
+    south = get_south_slot(slot)
+    return [maybe_slot for maybe_slot in [north, south] if maybe_slot is not None]
 
 
-def get_adjacent_slots(location: int) -> List[int]:
-    return get_east_west_slots(location) + get_north_south_slots(location)
+def get_adjacent_slots(slot: int) -> List[int]:
+    return get_east_west_slots(slot) + get_north_south_slots(slot)
