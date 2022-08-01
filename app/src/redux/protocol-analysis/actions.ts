ed66a98dbd3a9b5b283a60209faf015a40eed4f3
Vincent Abruzzo <vincent.abruzzo@opentrons.com>
2022-08-01 16:34:01 -0400

    whitespace fix

--- app/src/redux/protocol-analysis/actions.ts
+++ app/src/redux/protocol-analysis/actions.ts
@@ -3,6 +3,7 @@ export const OPEN_PYTHON_DIRECTORY: 'protocol-analysis:OPEN_PYTHON_DIRECTORY' =
 
 export const CHANGE_PYTHON_PATH_OVERRIDE: 'protocol-analysis:CHANGE_PYTHON_PATH_OVERRIDE' =
   'protocol-analysis:CHANGE_PYTHON_PATH_OVERRIDE'
+
 export interface OpenPythonInterpreterDirectoryAction {
   type: typeof OPEN_PYTHON_DIRECTORY
   meta: { shell: true }
