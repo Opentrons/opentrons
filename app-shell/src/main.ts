d2e277530689a9855e988f9937396f25acbf2d81
Koji <kk2796@nyu.edu>
2022-08-01 14:30:46 -0400

    Update main.ts

--- app-shell/src/main.ts
+++ app-shell/src/main.ts
@@ -65,8 +65,6 @@ function startUp(): void {
 
   mainWindow.once('closed', () => (mainWindow = null))
 
-  // TODO kj: remove {} once https://github.com/sindresorhus/electron-context-menu/issues/153 is solved
-  // After upgrading electron-context-menu to after 0.16.0 remove lookUpSelection() and add searchWithGoogle()
   contextMenu({
     showInspectElement: config.devtools,
   })
