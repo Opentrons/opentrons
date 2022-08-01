97c63e67380ee03d4f34f7ad49873bc322384c73
Koji <kk2796@nyu.edu>
2022-08-01 14:17:51 -0400

    chnage alphaToOpacity to Opacity and remove unused color const

--- components/src/ui-style-constants/colors.ts
+++ components/src/ui-style-constants/colors.ts
@@ -60,8 +60,8 @@ export const errorDisabled = '#8f8f8f'
 
 // others
 export const transparent = 'transparent'
-export const alphaToOpacity35 = '35'
-export const backgroundOverlay = `${darkBlackEnabled}${alphaToOpacity35}`
+export const Opacity35 = '35'
+export const backgroundOverlay = `${darkBlackEnabled}${Opacity35}`
 
 // colors pd liquid
 export const electricPurple = '#b925ff'
@@ -87,73 +87,3 @@ export const liquidColors = [
   tartRed,
   whaleGrey,
 ]
-
-// colors fundamentals
-// export const background = '#f8f8f8'
-// export const white = '#ffffff'
-// export const lightBlue = '#f1f8ff'
-// export const medGrey = '#e3e3e3'
-// export const black = '#000000'
-// export const transparent = 'transparent'
-
-// colors with states
-// export const blueFocus = '#deecff'
-// export const blue = '#006cfa'
-// export const blueHover = '#0061e0'
-// export const bluePressed = '#0050b8'
-// export const medBlue = '#d9e9fe'
-
-// export const disabled = '#a0a0a0'
-// export const medGreyHover = '#b8b8b8'
-// export const darkBlack = '#16212D'
-// export const darkBlackHover = '#283d52'
-// export const darkBlackPressed = '#16212D'
-// export const darkBlackSelected = '#39495B'
-// note: darkBlackDisabled = greyDisabled
-
-// export const darkGrey = '#4a4a4a'
-// export const darkGreyHover = '#646468'
-// export const darkGreyPressed = '#646668'
-// export const darkGreyEnabled = '#707075'
-
-// export const medGreyHover = '#b8b8b8'
-// export const darkBlack = '#16212D'
-// export const darkBlackHover = '#24313F'
-// export const darkBlackPressed = '#16212D'
-// export const darkBlackSelected = '#39495B'
-// note: darkBlackDisabled = greyDisabled
-
-// export const darkGreyHover = '#646668'
-// export const darkGreyPressed = '#5a5a5e'
-// export const darkGreyEnabled = '#707075'
-// note: darkGreyDisabled = greyDisabled
-// note: darkGraySelected = darkBlackSelected
-
-// export const greyHover = '#acacaf'
-// export const greyPressed = '#8d8d91'
-// export const greyDisabled = '#eaeaeb'
-
-// export const lightGrey = '#f8f8f8'
-// export const lightGreyHover = '#16212d26'
-
-// export const yellow = '#f2b53c'
-// export const yellowHover = '#eca20f'
-// export const yellowPressed = '#eca20f'
-
-// export const error = '#bf0000'
-// export const errorHover = '#a30000'
-// export const errorText = '#850000'
-// export const errorBg = '#fff3f3'
-// note: errorDisabled = successDisabled
-
-// export const success = '#04aa65'
-// export const successText = '#00854d'
-// export const successBg = '#f3fffa'
-// export const successDisabled = '#8f8f8f'
-
-// export const warning = '#f09d20'
-// export const warningText = '#7b5b09'
-// export const warningBg = '#fffcf5'
-// note: warningDisabled = successDisabled
-
-// export const alphaToOpacity35 = '35'
