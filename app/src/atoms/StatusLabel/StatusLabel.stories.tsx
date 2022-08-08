7d25a2950eba87dc94fabb15aec097b96aced5bc
Koji <kk2796@nyu.edu>
2022-08-08 19:33:04 -0400

    refactor(app): this will close RAUT-120
    This PR will add statuslabel to storybook

---
+++ app/src/atoms/StatusLabel/StatusLabel.stories.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { COLORS } from '@opentrons/components'
+import { StatusLabel } from './index'
+// import { StyledText } from '../../atoms/text'
+import type { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'App/Atoms/StatusLabel',
+  component: StatusLabel,
+} as Meta
+
+const Template: Story<React.ComponentProps<typeof StatusLabel>> = args => (
+  <StatusLabel {...args} />
+)
+
+export const Active = Template.bind({})
+Active.args = {
+  status: 'Heating',
+  backgroundColor: COLORS.medBlue,
+  iconColor: COLORS.blueEnabled,
+  pulse: true,
+}
+
+export const Holding = Template.bind({})
+Holding.args = {
+  status: 'Holding at target',
+  backgroundColor: COLORS.medBlue,
+  iconColor: COLORS.blueEnabled,
+  pulse: false,
+}
+
+export const Idle = Template.bind({})
+Idle.args = {
+  status: 'Idle',
+  backgroundColor: COLORS.medGreyEnabled,
+  iconColor: COLORS.darkGreyEnabled,
+  pulse: true,
+}
+
+export const Error = Template.bind({})
+Error.args = {
+  status: 'Error',
+  backgroundColor: COLORS.warningBackground,
+  iconColor: COLORS.warningEnabled,
+  pulse: true,
+}
