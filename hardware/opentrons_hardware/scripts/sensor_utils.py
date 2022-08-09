5dc7d319041c34bbfe08a8986a7de99d13d93c31
Laura Cox <31892318+Laura-Danielle@users.noreply.github.com>
2022-08-09 16:51:21 -0400

    refactor(ot3): add support for the new environment sensor (#11315)

--- hardware/opentrons_hardware/scripts/sensor_utils.py
+++ hardware/opentrons_hardware/scripts/sensor_utils.py
@@ -5,12 +5,12 @@ from datetime import datetime, timedelta
 
 from typing import Dict, Any, List
 import csv
-from opentrons_hardware.sensors.utils import SensorDataType
+from opentrons_hardware.sensors.types import SensorDataType, EnvironmentSensorDataType
 
 from opentrons_hardware.drivers.can_bus.can_messenger import CanMessenger
 from opentrons_hardware.drivers.can_bus.abstract_driver import AbstractCanDriver
-from opentrons_hardware.firmware_bindings.constants import NodeId, SensorType
-from opentrons_hardware.sensors import fdc1004, hdc2080, mmr920C04
+from opentrons_hardware.firmware_bindings.constants import NodeId, SensorType, SensorId
+from opentrons_hardware.sensors import sensor_driver, sensor_types
 
 hms = "%H:%M:%S"
 
@@ -86,7 +86,8 @@ async def handle_pressure_sensor(
     """Function to read data from the pressure sensor."""
     start_time = datetime.now()
     csv = None
-    pressure = mmr920C04.PressureSensor()
+    pressure = sensor_types.PressureSensor.build(SensorId.S0, node_id)
+    s_driver = sensor_driver.SensorDriver()
     if include_csv:
         metadata = CSVMetaData(
             command.serial_number,
@@ -100,7 +101,7 @@ async def handle_pressure_sensor(
     while datetime.now() < end_time:
         messenger = CanMessenger(driver=driver)
         messenger.start()
-        data = await pressure.read(messenger, node_id, offset=False, timeout=10)
+        data = await s_driver.read(messenger, pressure, offset=False, timeout=10)
         curr_time = datetime.now().strftime(hms)
         if isinstance(data, SensorDataType):
             log.info(f"Pressure data: {data.to_float()} at: {curr_time}")
@@ -126,7 +127,8 @@ async def handle_capacitive_sensor(
     """Function to read data from the capacitive sensor."""
     start_time = datetime.now()
     csv = None
-    capacitive = fdc1004.CapacitiveSensor()
+    capacitive = sensor_types.CapacitiveSensor.build(SensorId.S0, node_id)
+    s_driver = sensor_driver.SensorDriver()
     messenger = CanMessenger(driver=driver)
     messenger.start()
     if include_csv:
@@ -139,15 +141,12 @@ async def handle_capacitive_sensor(
         )
         csv = CSVFormatter.build(metadata, list(metadata.to_dict().keys()))
     # autozero
-    await capacitive.get_baseline(messenger, node_id, 10, 10, timeout=10)
+    await s_driver.get_baseline(messenger, capacitive, 10, timeout=10)
     end_time = start_time + timedelta(minutes=command.minutes)
     while datetime.now() < end_time:
-        data = await capacitive.read(messenger, node_id, offset=False, timeout=10)
+        data = await s_driver.read(messenger, capacitive, offset=False, timeout=10)
         curr_time = datetime.now().strftime(hms)
 
-        log.info(
-            f"Capacitive data: {data.to_float() if data else 'None'} at: {curr_time}"
-        )
         if isinstance(data, SensorDataType):
             log.info(f"Capacitive data: {data.to_float()} at: {curr_time}")
             if csv:
@@ -156,51 +155,6 @@ async def handle_capacitive_sensor(
             log.info(f"Capacitive data not found at: {curr_time}")
 
     end_time_log = datetime.now().strftime(hms)
-    text_end_time = f"Test ended at: {end_time_log}"
-    log.info(text_end_time)
-    if csv:
-        csv.write(text_end_time)
-
-
-async def read_temp_from_pressure_sensor(
-    command: SensorRun,
-    driver: AbstractCanDriver,
-    node_id: NodeId,
-    include_csv: bool,
-    log: logging.Logger,
-) -> None:
-    """Function to read temperature from the pressure sensor."""
-    start_time = datetime.now()
-    csv = None
-    pressure = mmr920C04.PressureSensor()
-    if include_csv:
-        metadata = CSVMetaData(
-            command.serial_number,
-            command.sensor_type.name,
-            command.minutes,
-            command.auto_zero,
-            start_time.strftime(hms),
-        )
-        csv = CSVFormatter.build(metadata, list(metadata.to_dict().keys()))
-    end_time = start_time + timedelta(minutes=command.minutes)
-    while datetime.now() < end_time:
-        messenger = CanMessenger(driver=driver)
-        messenger.start()
-        data = await pressure.read_temperature(
-            messenger,
-            node_id,
-            offset=False,
-            timeout=10,
-        )
-        curr_time = datetime.now().strftime(hms)
-        if isinstance(data, SensorDataType):
-            log.info(f"Temperature data: {data.to_float()} at: {curr_time}")
-            if csv:
-                csv.write_dict({"time": curr_time, "data": data.to_float()})
-        else:
-            log.info(f"Temperature data not found at: {curr_time}")
-
-    end_time_log = (datetime.now()).strftime(hms)
     text_end_time = f"Test ended at: {end_time_log}"
     log.info(text_end_time)
     if csv:
@@ -217,7 +171,8 @@ async def handle_environment_sensor(
     """Function to read data from the environment sensor."""
     start_time = datetime.now()
     csv = None
-    environment = hdc2080.EnvironmentSensor(command.sensor_type)
+    environment = sensor_types.EnvironmentSensor.build(SensorId.S0, node_id)
+    s_driver = sensor_driver.SensorDriver()
     messenger = CanMessenger(driver=driver)
     messenger.start()
     if include_csv:
@@ -231,12 +186,19 @@ async def handle_environment_sensor(
         csv = CSVFormatter.build(metadata, list(metadata.to_dict().keys()))
     end_time = start_time + timedelta(minutes=command.minutes)
     while datetime.now() < end_time:
-        data = await environment.read(messenger, node_id, timeout=10)
+        data = await s_driver.read(messenger, environment, offset=False, timeout=10)
         curr_time = datetime.now().strftime(hms)
-        if isinstance(data, SensorDataType):
-            log.info(f"Capacitive data: {data.to_float()} at: {curr_time}")
+        if isinstance(data, EnvironmentSensorDataType):
+            log.info(f"Humidity data: {data.humidity.to_float()} at: {curr_time}")
+            log.info(f"Temperature data: {data.temperature.to_float()} at: {curr_time}")
             if csv:
-                csv.write_dict({"time": curr_time, "data": data.to_float()})
+                csv.write_dict(
+                    {
+                        "time": curr_time,
+                        "humidity": data.humidity.to_float(),
+                        "temperature": data.temperature.to_float(),
+                    }
+                )
         else:
             log.info(f"Capacitive data not found at: {curr_time}")
     end_time_log = datetime.now().strftime(hms)
