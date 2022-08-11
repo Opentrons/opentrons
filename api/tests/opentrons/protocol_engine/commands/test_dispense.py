4f0383a49bd6893b3f316a6861047c54d786e2fa
Max Marrone <max@marrone.nyc>
2022-08-11 16:47:00 -0400

    Factor movement out of PipettingHandler.dispense().

--- api/tests/opentrons/protocol_engine/commands/test_dispense.py
+++ api/tests/opentrons/protocol_engine/commands/test_dispense.py
@@ -2,7 +2,7 @@
 from decoy import Decoy
 
 from opentrons.protocol_engine import WellLocation, WellOrigin, WellOffset
-from opentrons.protocol_engine.execution import PipettingHandler
+from opentrons.protocol_engine.execution import MovementHandler, PipettingHandler
 
 from opentrons.protocol_engine.commands.dispense import (
     DispenseParams,
@@ -13,32 +13,42 @@ from opentrons.protocol_engine.commands.dispense import (
 
 async def test_dispense_implementation(
     decoy: Decoy,
+    movement: MovementHandler,
     pipetting: PipettingHandler,
 ) -> None:
-    """A PickUpTipCreate should have an execution implementation."""
-    subject = DispenseImplementation(pipetting=pipetting)
+    """It should move to the target location and then dispense."""
+    subject = DispenseImplementation(movement=movement, pipetting=pipetting)
 
-    location = WellLocation(origin=WellOrigin.BOTTOM, offset=WellOffset(x=0, y=0, z=1))
+    well_location = WellLocation(
+        origin=WellOrigin.BOTTOM, offset=WellOffset(x=0, y=0, z=1)
+    )
 
     data = DispenseParams(
-        pipetteId="abc",
-        labwareId="123",
+        pipetteId="pipette-id-abc123",
+        labwareId="labware-id-abc123",
         wellName="A3",
-        wellLocation=location,
+        wellLocation=well_location,
         volume=50,
         flowRate=1.23,
     )
 
+    async def _rehearse_dispense() -> None:
+        decoy.when(
+            await pipetting.dispense_in_place(
+                pipette_id="pipette-id-abc123",
+                volume=50,
+                flow_rate=1.23,
+            )
+        ).then_return(42)
+
     decoy.when(
-        await pipetting.dispense(
-            pipette_id="abc",
-            labware_id="123",
+        await movement.move_to_well(
+            pipette_id="pipette-id-abc123",
+            labware_id="labware-id-abc123",
             well_name="A3",
-            well_location=location,
-            volume=50,
-            flow_rate=1.23,
+            well_location=well_location,
         )
-    ).then_return(42)
+    ).then_do(_rehearse_dispense)  # TODO: Update Decoy to latest for this to work.
 
     result = await subject.execute(data)
 
