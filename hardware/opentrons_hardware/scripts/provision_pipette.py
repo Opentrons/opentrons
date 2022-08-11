a4b98bd27af34561b389628f64043b924a467473
Seth Foster <seth@opentrons.com>
2022-08-11 17:25:58 -0400

    refactor(hardware): flash pipette serials
    Adds a new module for dealing with pipette serial numbers and a tool for
    flashing them and confirming they're correct.
    
    Closes RET-1168

---
+++ hardware/opentrons_hardware/scripts/provision_pipette.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python3
+"""Provisions pipette EEPROMs.
+
+This can be used either on a production line or locally.
+
+A log of what has been flashed to pipettes can be found at
+provision_pipette.log.
+"""
+
+import asyncio
+import logging
+import logging.config
+import argparse
+import datetime
+from typing import Tuple
+
+from opentrons_hardware.drivers.can_bus import build, CanMessenger, WaitableCallback
+from opentrons_hardware.firmware_bindings.utils import UInt16Field
+from opentrons_hardware.firmware_bindings.messages import (
+    message_definitions,
+    payloads,
+    fields,
+)
+from opentrons_hardware.firmware_bindings.constants import NodeId, PipetteName
+from opentrons_hardware.scripts.can_args import add_can_args, build_settings
+from opentrons_hardware.pipettes import serials
+
+
+async def run(
+    args: argparse.Namespace, base_log: logging.Logger, trace_log: logging.Logger
+) -> None:
+    """Script entrypoint."""
+    which_pipette = {"left": NodeId.pipette_left, "right": NodeId.pipette_right}[
+        args.which
+    ]
+    async with build.driver(build_settings(args)) as driver, CanMessenger(
+        driver
+    ) as messenger:
+        await flash_serials(messenger, which_pipette, base_log, trace_log)
+
+
+async def flash_serials(
+    messenger: CanMessenger,
+    which_pipette: NodeId,
+    base_log: logging.Logger,
+    trace_log: logging.Logger,
+) -> None:
+    """Flash serials in a loop."""
+    while True:
+        should_quit = await get_and_update_serial_once(
+            messenger, which_pipette, base_log, trace_log
+        )
+        if should_quit:
+            return
+
+
+async def get_serial(
+    prompt: str, base_log: logging.Logger
+) -> Tuple[PipetteName, int, bytes]:
+    """Get a serial number that is correct and parseable."""
+    loop = asyncio.get_running_loop()
+    while True:
+        serial = await loop.run_in_executor(None, lambda: input(prompt))
+        try:
+            name, model, data = serials.info_from_serial_string(serial)
+        except Exception as e:
+            base_log.exception("invalid serial")
+            if isinstance(Exception, KeyboardInterrupt):
+                raise
+            print(str(e))
+        else:
+            return name, model, data
+
+
+async def update_serial_and_confirm(
+    messenger: CanMessenger,
+    which_pipette: NodeId,
+    name: PipetteName,
+    model: int,
+    data: bytes,
+    base_log: logging.Logger,
+    trace_log: logging.Logger,
+    attempts: int = 3,
+    attempt_timeout_s: float = 1.0,
+) -> None:
+    """Update and verify the update of serial data."""
+    for attempt in range(attempts):
+        base_log.debug(f"beginning set and confirm attempt {attempt}")
+        set_message = message_definitions.SetSerialNumber(
+            payload=payloads.SerialNumberPayload(
+                serial=fields.SerialField(
+                    serials.serial_val_from_parts(name, model, data)
+                )
+            )
+        )
+        await messenger.send(which_pipette, set_message)
+
+        base_log.debug(f"Sent set-serial: {set_message}")
+        await messenger.send(which_pipette, message_definitions.InstrumentInfoRequest())
+        target = datetime.datetime.now() + datetime.timedelta(seconds=attempt_timeout_s)
+        try:
+            while True:
+                with WaitableCallback(messenger) as wc:
+                    message, arb = await asyncio.wait_for(
+                        wc.read(), (target - datetime.datetime.now()).total_seconds()
+                    )
+                    if (
+                        isinstance(message, message_definitions.PipetteInfoResponse)
+                        and arb.originating_node_id == which_pipette
+                    ):
+                        if (
+                            message.payload.name == fields.PipetteNameField(name.value)
+                            and message.payload.model == UInt16Field(model)
+                            and message.payload.serial == fields.SerialField(data)
+                        ):
+                            base_log.info(f"serial confirmed on attempt {attempt}")
+                            return
+                        else:
+                            base_log.debug("message relevant serial NOT confirmed")
+                    base_log.debug(f"message {type(message)} is not relevant")
+                    base_log.debug(
+                        f"{(target-datetime.datetime.now()).total_seconds()} remaining in attempt {attempt}"
+                    )
+        except asyncio.TimeoutError:
+            continue
+
+
+async def get_and_update_serial_once(
+    messenger: CanMessenger,
+    which_pipette: NodeId,
+    base_log: logging.Logger,
+    trace_log: logging.Logger,
+) -> None:
+    """Read and update a single serial."""
+    name, model, data = await get_serial(
+        f'Enter serial for pipette on {which_pipette.name.split("_")[-1]}: ', base_log
+    )
+    try:
+        await update_serial_and_confirm(
+            messenger, which_pipette, name, model, data, base_log, trace_log
+        )
+        trace_log.info('SUCCESS,{name.name},{model},{data.encode("ascii")}')
+    except Exception:
+        base_log.exception("Update failed")
+        trace_log.info('FAILURE,{name.name},{model},{data.encode("ascii")}')
+        raise
+
+
+def log_config(log_level: int) -> Tuple[logging.Logger, logging.Logger]:
+    """Configure logging."""
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": True,
+            "formatters": {
+                "basic": {"format": "%(asctime)s %(name)s %(levelname)s %(message)s"},
+                "production_trace": {"format": "%(asctime)s %(message)s"},
+            },
+            "handlers": {
+                "main_log_handler": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "formatter": "basic",
+                    "filename": "provision_pipette_debug.log",
+                    "maxBytes": 5000000,
+                    "level": log_level,
+                    "backupCount": 3,
+                },
+                "stream_handler": {
+                    "class": "logging.handlers.StreamHandler",
+                    "formatter": "basic",
+                    "level": log_level,
+                },
+                "trace_log_handler": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "formatter": "basic",
+                    "filename": "provision_pipette.log",
+                    "maxBytes": 5000000,
+                    "level": logging.INFO,
+                    "backupCount": 3,
+                },
+            },
+            "loggers": {
+                "": {
+                    "handlers": (
+                        ["main_log_handler"]
+                        if log_level > logging.INFO
+                        else ["main_log_handler", "stream_handler"]
+                    ),
+                    "level": log_level,
+                },
+                "trace_log": {
+                    "handlers": ["trace_log_handler"],
+                    "level": logging.INFO,
+                },
+            },
+        }
+    )
+    return logging.getLogger(__name__), logging.getLogger("trace_log")
+
+
+def main() -> None:
+    """Entry point."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        help=(
+            "Developer logging level. At DEBUG or below, logs are written "
+            "to console; at INFO or above, logs are only written to "
+            "provision_pipettes_debug.log"
+        ),
+        type=str,
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+        default="WARNING",
+    )
+    parser.add_argument(
+        "--once",
+        action="store_true",
+        help="Run just once and quit instead of staying in a loop",
+    )
+    parser.add_argument(
+        "-w",
+        "--which",
+        type=str,
+        choices=["left", "right"],
+        help="Which pipette to flash",
+    )
+    add_can_args(parser)
+
+    args = parser.parse_args()
+    base_log, trace_log = log_config(getattr(logging, args.log_level))
+    asyncio.run(run(args, base_log, trace_log))
+
+
+if __name__ == "__main__":
+    main()
---
+++ hardware/tests/opentrons_hardware/pipettes/test_serials.py
@@ -0,0 +1,64 @@
+"""Test serial setting."""
+import pytest
+from opentrons_hardware.pipettes import serials
+from opentrons_hardware.firmware_bindings.constants import PipetteName
+
+
+@pytest.mark.parametrize(
+    "scanned_val,name,model,serial",
+    [
+        ("P1KSV0102022022", PipetteName.p1000_single, 1, b"02022022"),
+        ("P50MV29AABBCCDD", PipetteName.p50_multi, 29, b"AABBCCDD"),
+        ("P1KMV1000000000", PipetteName.p1000_multi, 10, b"00000000"),
+        ("P50SV01", PipetteName.p50_single, 1, b"\x00\x00\x00\x00\x00\x00\x00\x00"),
+    ],
+)
+def test_scan_valid_serials(
+    scanned_val: str, name: PipetteName, model: int, serial: bytes
+) -> None:
+    """Various known-good serials."""
+    parsed_name, parsed_model, parsed_serial = serials.info_from_serial_string(
+        scanned_val
+    )
+    assert parsed_name == name
+    assert parsed_model == model
+    assert parsed_serial == serial
+
+
+@pytest.mark.parametrize("scannedval", ["P111V02", "P1ksV22", "P3HSV12"])
+def test_pipette_name_validity(scannedval: str) -> None:
+    """Pipette name lookup matching."""
+    with pytest.raises(ValueError, match="The pipette name part.*"):
+        serials.info_from_serial_string(scannedval)
+
+
+@pytest.mark.parametrize(
+    "scannedval",
+    [
+        "",
+        "P",
+        "P1KS123",
+        "P1KSVVV",
+        "P1KSV0A",
+        "P1KSV0102022022123123123",
+    ],
+)
+def test_serial_validity(scannedval: str) -> None:
+    """Various regex failures."""
+    with pytest.raises(ValueError, match="The serial number.*"):
+        serials.info_from_serial_string(scannedval)
+
+
+@pytest.mark.parametrize("name", [name for name in PipetteName])
+@pytest.mark.parametrize("model", [0, 1, 10, 0xFF])
+@pytest.mark.parametrize(
+    "data",
+    [
+        b"\x00\x00\x00\x00\x00\x00\x00\x00",
+        b"\xff\xff\xff\xff\xff\xff\xff\xff",
+        b"hello  ",
+    ],
+)
+def test_serial_val_from_parts(name: PipetteName, model: int, data: bytes) -> None:
+    """Make sure that valid inputs don't cause exceptions."""
+    serials.serial_val_from_parts(name, model, data)
