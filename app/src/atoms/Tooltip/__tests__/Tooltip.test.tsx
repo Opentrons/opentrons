a50d22d0674c41e11e9647911c9843a77c4cb4f3
Koji <kk2796@nyu.edu>
2022-08-11 18:42:49 -0400

    test(app): add Tooltip test
    This PR will add test cases for Tooltip
    
    fix RAUT-130

---
+++ app/src/atoms/Tooltip/__tests__/Tooltip.test.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import {
+  renderWithProviders,
+  TOOLTIP_TOP,
+  SPACING,
+} from '@opentrons/components'
+import { Tooltip } from '..'
+
+const render = (props: React.ComponentProps<typeof Tooltip>) => {
+  return renderWithProviders(<Tooltip {...props} />)[0]
+}
+
+const placement = TOOLTIP_TOP
+const id = 'Tooltip_123'
+const tooltipRef = jest.fn()
+const tooltipStyle = { position: 'absolute', left: SPACING.spacing2 } as const
+const arrowRef = jest.fn()
+const arrowStyle = { position: 'absolute', left: SPACING.spacing3 } as const
+
+const MockTooltipProps = {
+  id: id,
+  placement: placement,
+  arrowStyle: arrowStyle,
+  arrowRef: arrowRef,
+  visible: true,
+  ref: tooltipRef,
+  style: tooltipStyle,
+}
+
+describe('Tooltip', () => {
+  let props: React.ComponentProps<typeof Tooltip>
+
+  beforeEach(() => {
+    props = {
+      children: 'mock children' as React.ReactNode,
+      tooltipProps: MockTooltipProps,
+      key: 'mock key',
+    }
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('renders correct children when the tooltip is visible', () => {
+    const { getByText } = render(props)
+    const tooltip = getByText('mock children')
+    expect(tooltip).toBeInTheDocument()
+    expect(tooltip).toHaveStyle('position: absolute')
+    expect(tooltip).toHaveStyle('left: 0.25rem')
+    expect(tooltip).toHaveStyle('background: #16212d')
+    expect(tooltip).toHaveStyle('color: #fff')
+    expect(tooltip).toHaveStyle('width: 8.75rem')
+    expect(tooltip).toHaveStyle('font-size: 0.625rem')
+    expect(tooltip).toHaveAttribute('role', 'tooltip')
+  })
+
+  it('renders correct children when the tooltip is visible with a specific with', () => {
+    props = { ...props, width: '3rem' }
+    const { getByText } = render(props)
+    const tooltip = getByText('mock children')
+    expect(tooltip).toHaveStyle('width: 3rem')
+  })
+
+  it('not renders children when the tooltip is invisible', () => {
+    MockTooltipProps.visible = false
+    const { queryByText } = render(props)
+    const tooltip = queryByText('mock children')
+    expect(tooltip).not.toBeInTheDocument()
+  })
+})
