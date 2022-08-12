c5ae003dd6f1520fcd6fb71ccd40ee1e57544f5c
Shlok Amin <shlokamin@me.com>
2022-08-12 13:20:16 -0400

    revert single quote change

--- app/src/organisms/ConfigurePipette/ConfigFormSubmitButton.tsx
+++ app/src/organisms/ConfigurePipette/ConfigFormSubmitButton.tsx
@@ -23,7 +23,7 @@ export function ConfigFormSubmitButton(
       justifyContent={JUSTIFY_CENTER}
       flexDirection={DIRECTION_COLUMN}
       textTransform={TEXT_TRANSFORM_UPPERCASE}
-      boxShadow={'0px -4px 12px rgba(0, 0, 0, 0.15)'}
+      boxShadow="0px -4px 12px rgba(0, 0, 0, 0.15)"
     >
       <PrimaryButton type="submit" form={formId} disabled={disabled}>
         {t('confirm')}
