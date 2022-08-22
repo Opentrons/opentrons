0719684aab99e9d4f3a513b0f2d2b756cef8efbc
Brent Hagen <brent.hagen@opentrons.com>
2022-08-22 16:23:50 -0400

    exclude usb-detection node modules from build

--- app-shell/electron-builder.config.js
+++ app-shell/electron-builder.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 const { OT_APP_DEPLOY_BUCKET, OT_APP_DEPLOY_FOLDER } = process.env
 const DEV_MODE = process.env.NODE_ENV !== 'production'
 const USE_PYTHON = process.env.NO_PYTHON !== 'true'
+const NO_USB_DETECTION = process.env.NO_USB_DETECTION === 'true'
 
 module.exports = {
   appId: 'com.opentrons.app',
@@ -43,6 +44,7 @@ module.exports = {
     target: ['AppImage'],
     executableName: 'opentrons',
     category: 'Science',
+    files: NO_USB_DETECTION ? ['!node_modules/usb-detection'] : [],
   },
   publish:
     OT_APP_DEPLOY_BUCKET && OT_APP_DEPLOY_FOLDER
