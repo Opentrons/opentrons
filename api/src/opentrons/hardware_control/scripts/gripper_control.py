a4daa5c66f293f9d58d2ea6133c61867e4f74abb
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2022-08-22 18:38:12 -0400

    add more  params

--- api/src/opentrons/hardware_control/scripts/gripper_control.py
+++ api/src/opentrons/hardware_control/scripts/gripper_control.py
@@ -1,20 +1,37 @@
 import sys
 sys.path.append('/opt/opentrons-robot-server')
 
-#Opentrons Libraries
 from opentrons.hardware_control.thread_manager import ThreadManager
 from opentrons.hardware_control.ot3api import OT3API
-from opentrons.hardware_control.types import OT3Axis, OT3Mount, OT3AxisKind, CriticalPoint, Axis
-from opentrons.types import Mount, Point
+from opentrons.hardware_control.types import OT3Axis, OT3Mount
+from opentrons.types import Point
 from opentrons_shared_data.deck import load as load_deck_def
 
+VERSION = 0.0
 MOUNT = OT3Mount.GRIPPER
-deck_def = load_deck_def("ot3_standard")
-FROM_SLOT = 8
-TO_SLOT = 3
-GRIP_HEIGHT = 25
 
+deck_def = load_deck_def("ot3_standard")
+
+# Origin slot
+FROM_SLOT = 8
+# Destination slot
+TO_SLOT = 3
+
+# Grip force in newton
+GRIP_FORCE = 20
+
+# Z height in mm from the deck for gripping and safe-traveling
+GRIP_HEIGHT = 25.0
+TRAVEL_HEIGHT = 70.0
+
+# Offset of the center of the gripper jaw to the desired location
 GRIPPER_OFFSET = Point(0.0, 2.65, 0.0)
+
+# If set to true, gripper will move labware back to the origin slot
+RETURN_TO_ORIGIN = False
+
+# Number of times the entire flow is to be repeated
+REPEAT = 0
 
 
 def get_slot_center_in_deck_coord(index: int):
@@ -30,25 +47,47 @@ def build_api():
 
 if __name__ == "__main__":
     api = build_api()
+    print("Homing...")
     api.home()
     homed_pos = api.gantry_position(MOUNT)
 
     from_slot_loc = get_slot_center_in_deck_coord(FROM_SLOT) + GRIPPER_OFFSET
-    print("from:")
-    print(from_slot_loc)
     to_slot_loc = get_slot_center_in_deck_coord(TO_SLOT) + GRIPPER_OFFSET
-    print("to:")
-    print(to_slot_loc)
 
-    api.move_to(MOUNT, from_slot_loc._replace(z=homed_pos.z))
-    api.move_to(MOUNT, from_slot_loc._replace(z=GRIP_HEIGHT))
+    for i in range(REPEAT + 1):
+        print(f"Round: {i} / {REPEAT}")
+        print(f"=========================")
+        api.move_to(MOUNT, from_slot_loc._replace(z=homed_pos.z))
+        api.move_to(MOUNT, from_slot_loc._replace(z=GRIP_HEIGHT))
 
-    api.grip(20)
-    api.move_to(MOUNT, from_slot_loc._replace(z=homed_pos.z))
-    api.move_to(MOUNT, to_slot_loc._replace(z=homed_pos.z))
-    api.move_to(MOUNT, to_slot_loc._replace(z=GRIP_HEIGHT))
+        print("Gripping...")
+        api.grip(GRIP_FORCE)
 
-    api.ungrip()
-    api.move_to(MOUNT, to_slot_loc._replace(z=homed_pos.z))
-    api.home()
-    
+        api.home([OT3Axis.Z_G])
+        api.move_to(MOUNT, to_slot_loc._replace(z=homed_pos.z))
+        api.move_to(MOUNT, to_slot_loc._replace(z=GRIP_HEIGHT))
+
+        print("Releasing...")
+        api.ungrip()
+
+        # Return to safe height
+        api.move_to(MOUNT, to_slot_loc._replace(z=homed_pos.z))
+
+        if RETURN_TO_ORIGIN:
+            api.delay(0.5)
+            api.move_to(MOUNT, to_slot_loc._replace(z=GRIP_HEIGHT))
+
+            print("Gripping...")
+            api.grip(GRIP_FORCE)
+            
+            api.home([OT3Axis.Z_G])
+            api.move_to(MOUNT, from_slot_loc._replace(z=homed_pos.z))
+            api.move_to(MOUNT, from_slot_loc._replace(z=GRIP_HEIGHT))
+
+            print("Releasing...")
+            api.ungrip()
+
+            # Return to safe height
+            api.move_to(MOUNT, to_slot_loc._replace(z=homed_pos.z))
+
+        api.home()
