b0dc9a41a8c721f68e54c008d0f06f9a0e48d674
Laura Cox <laura@opentrons.com>
2022-08-25 13:32:01 -0400

    Correctly filter out labware defs from js side

--- labware-library/src/filters.tsx
+++ labware-library/src/filters.tsx
@@ -62,8 +62,7 @@ export function getFilteredDefinitions(filters: FilterParams): LabwareList {
   return getAllDefinitions().filter(
     def =>
       testCategory(filters.category, def) &&
-      testManufacturer(filters.manufacturer, def) &&
-      testOT3Definition(def)
+      testManufacturer(filters.manufacturer, def)
   )
 }
 
@@ -88,10 +87,4 @@ export function testCategory(
     category === FILTER_OFF ||
     category === definition.metadata.displayCategory
   )
-}
-
-// TODO(lc 8-24-2022) This is a temporary function
-// to filter out the new ot3 tiprack definitions
-export function testOT3Definition(definition: LabwareDefinition): boolean {
-  return !definition.metadata.displayName.includes('OT-3')
 }
