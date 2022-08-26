fbecc9d7c6f7f1075ad7841659ddfa3cdd6e7082
Mike Cousins <mike@opentrons.com>
2022-08-26 12:56:06 -0400

    fixup: more fully deprecate /labware/calibrations endpoints

--- robot-server/robot_server/service/labware/router.py
+++ robot-server/robot_server/service/labware/router.py
@@ -4,10 +4,12 @@ As of the v5 software release, these endpoints do not function.
 All labware offsets are set via `/run` endpoints.
 """
 from typing import Optional
+from typing_extensions import Literal, NoReturn
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 
-from robot_server.errors import ErrorBody
+from robot_server.errors import ErrorDetails, ErrorBody
+from robot_server.versioning import get_requested_version
 from robot_server.service.labware import models as lw_models
 from robot_server.service.errors import RobotServerError, CommonErrorDef
 
@@ -15,47 +17,87 @@ from robot_server.service.errors import RobotServerError, CommonErrorDef
 router = APIRouter()
 
 
+class LabwareCalibrationEndpointsRemoved(ErrorDetails):
+    """An error if you try to use the deprecated Labware Calibration endpoints."""
+
+    id: Literal[
+        "LabwareCalibrationEndpointsRemoved"
+    ] = "LabwareCalibrationEndpointsRemoved"
+    title: str = "Labware Calibration Endpoints Removed"
+    detail: str = "Use the `/runs` endpoints to manage labware offsets."
+
+
 @router.get(
     "/labware/calibrations",
     summary="Fetch all saved labware calibrations from the robot",
-    description="Search the robot for any saved labware offsets "
-    "which allows you to check whether a particular "
-    "labware has been calibrated or not.",
-    response_model=lw_models.MultipleCalibrationsResponse,
+    description=(
+        "This endpoint has been removed."
+        " Use the `/runs` endpoints to manage labware offsets."
+    ),
+    responses={
+        status.HTTP_200_OK: {"model": lw_models.MultipleCalibrationsResponse},
+        status.HTTP_410_GONE: {"model": ErrorBody[LabwareCalibrationEndpointsRemoved]},
+    },
 )
 async def get_all_labware_calibrations(
     loadName: Optional[str] = None,
     namespace: Optional[str] = None,
     version: Optional[int] = None,
     parent: Optional[str] = None,
+    requested_version: int = Depends(get_requested_version),
 ) -> lw_models.MultipleCalibrationsResponse:
-    return lw_models.MultipleCalibrationsResponse(data=[], links=None)
+    if requested_version <= 3:
+        return lw_models.MultipleCalibrationsResponse(data=[], links=None)
+
+    raise LabwareCalibrationEndpointsRemoved().as_error(status.HTTP_410_GONE)
 
 
 @router.get(
     "/labware/calibrations/{calibrationId}",
-    description="Fetch one specific labware offset by ID",
-    response_model=lw_models.SingleCalibrationResponse,
-    responses={status.HTTP_404_NOT_FOUND: {"model": ErrorBody}},
+    summary="Get a saved labware calibration by ID",
+    description=(
+        "This endpoint has been removed."
+        " Use the `/runs` endpoints to manage labware offsets."
+    ),
+    responses={
+        status.HTTP_404_NOT_FOUND: {"model": ErrorBody},
+        status.HTTP_410_GONE: {"model": ErrorBody[LabwareCalibrationEndpointsRemoved]},
+    },
 )
 async def get_specific_labware_calibration(
     calibrationId: str,
-) -> lw_models.SingleCalibrationResponse:
-    raise RobotServerError(
-        definition=CommonErrorDef.RESOURCE_NOT_FOUND,
-        resource="calibration",
-        id=calibrationId,
-    )
+    requested_version: int = Depends(get_requested_version),
+) -> NoReturn:
+    if requested_version <= 3:
+        raise RobotServerError(
+            definition=CommonErrorDef.RESOURCE_NOT_FOUND,
+            resource="calibration",
+            id=calibrationId,
+        )
+
+    raise LabwareCalibrationEndpointsRemoved().as_error(status.HTTP_410_GONE)
 
 
 @router.delete(
     "/labware/calibrations/{calibrationId}",
-    description="Delete one specific labware offset by ID",
-    responses={status.HTTP_404_NOT_FOUND: {"model": ErrorBody}},
+    summary="Remove a saved labware calibration by ID",
+    description=(
+        "This endpoint has been removed."
+        " Use the `/runs` endpoints to manage labware offsets."
+    ),
+    responses={
+        status.HTTP_404_NOT_FOUND: {"model": ErrorBody},
+        status.HTTP_410_GONE: {"model": ErrorBody[LabwareCalibrationEndpointsRemoved]},
+    },
 )
-async def delete_specific_labware_calibration(calibrationId: str):
-    raise RobotServerError(
-        definition=CommonErrorDef.RESOURCE_NOT_FOUND,
-        resource="calibration",
-        id=calibrationId,
-    )
+async def delete_specific_labware_calibration(
+    calibrationId: str, requested_version: int = Depends(get_requested_version)
+) -> NoReturn:
+    if requested_version <= 3:
+        raise RobotServerError(
+            definition=CommonErrorDef.RESOURCE_NOT_FOUND,
+            resource="calibration",
+            id=calibrationId,
+        )
+
+    raise LabwareCalibrationEndpointsRemoved().as_error(status.HTTP_410_GONE)
