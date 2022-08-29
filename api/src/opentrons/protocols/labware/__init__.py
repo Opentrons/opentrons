46457749347a240842f768c40a9d651001fb292f
Mike Cousins <mike@opentrons.com>
2022-08-29 12:44:30 -0400

    refactor(api, robot-server): remove unused labware calibration code (#11401)
    Closes RCORE-109

--- api/src/opentrons/protocols/labware/__init__.py
+++ api/src/opentrons/protocols/labware/__init__.py
@@ -1,11 +1,9 @@
 from .definition import (
     get_labware_definition,
     get_all_labware_definitions,
-    save_calibration,
     save_definition,
     get_labware_hash,
     get_labware_hash_with_parent,
-    delete_all_custom_labware,
     verify_definition,
 )
 from .load import load, load_from_definition
@@ -19,8 +17,6 @@ __all__ = [
     "load_from_definition",
     "save_definition",
     "verify_definition",
-    "delete_all_custom_labware",
-    "save_calibration",
     "get_labware_hash",
     "get_labware_hash_with_parent",
 ]
