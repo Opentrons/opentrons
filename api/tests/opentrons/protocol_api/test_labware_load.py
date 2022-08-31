5bcf1361bf65b4417c8b53c3b12bdb2ec4f90169
Seth Foster <seth@opentrons.com>
2022-08-31 12:41:51 -0400

    refactor(api): use deck def for slot positions
    We have a nice deck definition system where we describe the physical
    geometry of the deck in json. We used that for position in most
    contexts, but when executing APIv2 protocols we used the old-style
    direct calculation of slot offsets for the positions we load labware
    into. This doesn't work for the OT-3.
    
    By changing that logic to also depend on the deck definition, which is
    then selected based on what machine is running, we can get correct
    positioning for both machines.

--- api/tests/opentrons/protocol_api/test_labware_load.py
+++ api/tests/opentrons/protocol_api/test_labware_load.py
@@ -1,38 +1,56 @@
 import pytest
 from opentrons import protocol_api as papi, types
+from opentrons_shared_data.deck.dev_types import DeckDefinitionV3
 
 
 labware_name = "corning_96_wellplate_360ul_flat"
 
 
-def test_load_to_slot(ctx):
+def test_load_to_slot(
+    ctx: papi.ProtocolContext, deck_definition: DeckDefinitionV3
+) -> None:
+    slot_1 = [
+        slot
+        for slot in deck_definition["locations"]["orderedSlots"]
+        if slot["id"] == "1"
+    ][0]
+    slot_2 = [
+        slot
+        for slot in deck_definition["locations"]["orderedSlots"]
+        if slot["id"] == "2"
+    ][0]
     labware = ctx.load_labware(labware_name, "1")
-    assert labware._implementation.get_geometry().offset == types.Point(0, 0, 0)
+
+    assert labware._implementation.get_geometry().offset == types.Point(
+        *slot_1["position"]
+    )
     other = ctx.load_labware(labware_name, 2)
-    assert other._implementation.get_geometry().offset == types.Point(132.5, 0, 0)
+    assert other._implementation.get_geometry().offset == types.Point(
+        *slot_2["position"]
+    )
 
 
-def test_loaded(ctx):
+def test_loaded(ctx: papi.ProtocolContext) -> None:
     labware = ctx.load_labware(labware_name, "1")
     assert ctx.loaded_labwares[1] == labware
 
 
-def test_get_incorrect_definition_by_name():
+def test_get_incorrect_definition_by_name() -> None:
     with pytest.raises(FileNotFoundError):
         papi.labware.get_labware_definition("fake_labware")
 
 
-def test_get_mixed_case_labware_def():
+def test_get_mixed_case_labware_def() -> None:
     dfn = papi.labware.get_labware_definition("COrnIng_96_wElLplaTE_360UL_Flat")
     assert dfn["parameters"]["loadName"] == labware_name
 
 
-def test_load_label(ctx):
+def test_load_label(ctx: papi.ProtocolContext) -> None:
     labware = ctx.load_labware(labware_name, "1", "my cool labware")
     assert "my cool labware" in str(labware)
     assert labware._implementation.get_label() == "my cool labware"
 
 
-def test_deprecated_load(ctx):
+def test_deprecated_load(ctx: papi.ProtocolContext) -> None:
     labware = ctx.load_labware_by_name(labware_name, "1", "my cool labware")
     assert "my cool labware" in str(labware)
