928a214f76608cf544ab68cb90ef96e33ac2f801
Laura Cox <laura@opentrons.com>
2022-09-01 09:05:08 -0400

    add schemas

---
+++ api/src/opentrons/calibration_storage/schemas/ot2/v1.py
@@ -0,0 +1,41 @@
+import typing
+
+from typing_extensions import Literal
+from pydantic import BaseModel
+from datetime import datetime
+
+from opentrons_shared_data.pipette.dev_types import LabwareUri
+
+from opentrons.calibration_storage import types
+
+
+class CalibrationStatus(BaseModel):
+    markedBad: bool = False
+    source: typing.Optional[types.SourceType] = None
+    markedAt: typing.Optional[types.datetime] = None
+
+
+class TipLengthSchema(BaseModel):
+    tipLength: float
+    lastModified: datetime
+    source: types.SourceType
+    status: CalibrationStatus
+    uri: typing.Union[LabwareUri, Literal[""]]
+
+
+class DeckCalibrationSchema(BaseModel):
+    attitude: types.AttitudeMatrix
+    last_modified: datetime
+    source: types.SourceType
+    pipette_calibrated_with: typing.Optional[str]
+    tiprack: typing.Optional[str]
+    status: CalibrationStatus
+
+
+class InstrumentOffsetSchema(BaseModel):
+    offset: types.InstrumentCalOffset
+    tiprack: str
+    uri: str
+    last_modified: datetime
+    source: types.SourceType
+    status: CalibrationStatus
