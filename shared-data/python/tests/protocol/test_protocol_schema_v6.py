37da1ed6125f47a4991060bbeef7ce388152665f
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-09-01 22:49:15 -0400

    wip - added json protocol

--- shared-data/python/tests/protocol/test_protocol_schema_v6.py
+++ shared-data/python/tests/protocol/test_protocol_schema_v6.py
@@ -6,6 +6,74 @@ from opentrons_shared_data.protocol.models import ProtocolSchemaV6
 
 from . import list_fixtures
 
+failed_json_protocol = {
+	"robot": {
+		"model": "OT-2 Standard",
+		"deckId": "ot2_standard"
+	},
+	"pipettes": {
+		"pipetteId": {
+			"name": "p10_single"
+		}
+	},
+	"modules": {
+		"magneticModuleId": {
+			"model": "magneticModuleV2"
+		},
+		"temperatureModuleId": {
+			"model": "temperatureModuleV2"
+		}
+	},
+	"labware": {
+		"fixedTrash": {
+			"displayName": "Trash",
+			"definitionId": "opentrons/opentrons_1_trash_1100ml_fixed/1"
+		},
+		"sourcePlateId": {
+			"displayName": "Source Plate",
+			"definitionId": "example/plate/1"
+		}
+	},
+	"commands": [{
+			"commandType": "loadPipette",
+			"id": "0abc123",
+			"params": {
+				"pipetteId": "pipetteId",
+				"mount": "left"
+			}
+		},
+		{
+			"commandType": "loadModule",
+			"id": "1abc123",
+			"params": {
+				"moduleId": "magneticModuleId",
+				"location": {
+					"slotName": "3"
+				}
+			}
+		},
+		{
+			"commandType": "loadModule",
+			"id": "2abc123",
+			"params": {
+				"moduleId": "temperatureModuleId",
+				"location": {
+					"slotName": "1"
+				}
+			}
+		},
+		{
+			"commandType": "loadLabware",
+			"id": "3abc123",
+			"params": {
+				"labwareId": "sourcePlateId-1",
+				"location": {
+					"moduleId": "temperatureModuleId"
+				}
+			}
+		}
+	]
+}
 
 @pytest.mark.parametrize("defpath", list_fixtures(6))
 def test_v6_types(defpath):
@@ -35,6 +103,5 @@ def delete_unexpected_results(protocol_fixture: Dict[str, Any]) -> None:
 
 def test_schema_validators() -> None:
     """Should raise an error the keys do not match."""
-    def_data = load_shared_data("/Users/tamarzanzouri/Desktop/Protocols/simpleV6_fail.json")
     with pytest.raises(Exception, match="missing loadLabware id in referencing parent data model."):
-        def_model = ProtocolSchemaV6.parse_raw(def_data)
+        def_model = ProtocolSchemaV6.parse_raw(json.dumps(failed_json_protocol, indent=4))
