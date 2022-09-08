3675e264f48ff4a495e0805d3009e7a1cd759a96
Koji <kk2796@nyu.edu>
2022-09-08 15:19:03 -0400

    feat(components): add a hook for a swipe gesture
    This PR will close RCORE-137 partially
    
    fix RCORE-137

---
+++ components/src/hooks/__tests__/useSwipe.test.tsx
@@ -0,0 +1,23 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { useSwipe } from '..'
+
+describe('useSwipe hook', () => {
+  it('useSwipe returns UseSwipeResult object', () => {
+    const { result } = renderHook(() => useSwipe())
+    expect(result.current.style.touchAction).toBe('none')
+    expect(result.current.isEnabled).toBe(true)
+    expect(result.current.swipeType).toBe('')
+  })
+
+  it('When calling enable, isEnabled true', () => {
+    const { result } = renderHook(() => useSwipe())
+    result.current.enable()
+    expect(result.current.isEnabled).toBe(true)
+  })
+
+  it('When calling disable, isEnabled false', () => {
+    const { result } = renderHook(() => useSwipe())
+    result.current.disable()
+    expect(result.current.isEnabled).toBe(false)
+  })
+})
