0a15779c5b9c0e6b295a730eac54d5debe6e27c7
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-09-08 15:25:15 -0400

    use 5 fixtures

--- api/tests/opentrons/cli/test_cli.py
+++ api/tests/opentrons/cli/test_cli.py
@@ -1,24 +1,46 @@
 import subprocess
+import pytest
 from shutil import which
+from typing import List
+from pathlib import Path
 
 from opentrons.cli.analyze import analyze
 from click.testing import CliRunner
 
-async def test_cli_executes_analyze() -> None:
+
+def list_fixtures(version: int) -> List[Path]:
+    base = (
+        Path(__file__).parent
+        / ".."
+        / ".."
+        / ".."
+        / ".."
+        / "shared-data"
+        / "protocol"
+        / "fixtures"
+        / f"{version}"
+    )
+    return list(base.iterdir())
+
+
+@pytest.mark.parametrize("defpath", list_fixtures(5))
+async def test_cli_executes_analyze(defpath: Path) -> None:
     """Should call analyze command and execute with no errors."""
     if which('opentrons') is None:
         subprocess.run('make sdist wheel', shell=True)
         subprocess.run('pip install --find-links=./api/dist opentrons', shell=True)
-    out = subprocess.run(['python', '-m', 'opentrons.cli', 'analyze', '/Users/tamarzanzouri/opentrons/shared-data/protocol/fixtures/5/simpleV5.json', '--json-output', '/dev/stdout'], capture_output=True, text=True)
+    out = subprocess.run(['python', '-m', 'opentrons.cli', 'analyze', str(defpath.resolve()), '--json-output', '/dev/stdout'], capture_output=True, text=True)
 
     assert out.returncode == 0
     # Should I assert that its type StateSummery? should I assert the whole response object?
     assert out.stdout is not None
 
 
-def test_analyze() -> None:
+@pytest.mark.parametrize("defpath", list_fixtures(5))
+def test_analyze(defpath: Path) -> None:
     """Should return a StateSummery object."""
     runner = CliRunner()
-    result = runner.invoke(analyze, ['/Users/tamarzanzouri/opentrons/shared-data/protocol/fixtures/5/simpleV5.json', '--json-output', '/dev/stdout'])
+    result = runner.invoke(analyze, [str(defpath.resolve()), '--json-output', '/dev/stdout'])
+
     assert result.exit_code == 0
     assert result.output is not None
