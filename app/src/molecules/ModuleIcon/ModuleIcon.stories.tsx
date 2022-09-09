dd4fcc22d13e389f4a73161ed4e18ad714874acd
koji <kk2796@nyu.edu>
2022-09-09 02:13:42 -0400

    feat(app): add test and stories for ModuleIcon
    This PR will add test and stories for ModuleIcon component.

---
+++ app/src/molecules/ModuleIcon/ModuleIcon.stories.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react'
+
+import { ModuleIcon } from './index'
+import { StyledText } from '../../atoms/text'
+
+import type { Story, Meta } from '@storybook/react'
+import {
+  Flex,
+  SPACING,
+  DIRECTION_ROW,
+  DIRECTION_COLUMN,
+} from '@opentrons/components'
+
+import type { AttachedModule } from '../../redux/modules/types'
+
+export default {
+  title: 'App/Molecules/ModuleIcon',
+  component: ModuleIcon,
+} as Meta
+
+const temperatureModule = {
+  moduleModel: 'temperatureModuleV1',
+  moduleType: 'temperatureModuleType',
+  data: {},
+} as AttachedModule
+
+const magneticModule = {
+  moduleModel: 'magneticModuleV1',
+  moduleType: 'magneticModuleType',
+  data: {},
+} as AttachedModule
+
+const thermocyclerModule = {
+  moduleModel: 'thermocyclerModuleV1',
+  moduleType: 'thermocyclerModuleType',
+  data: {},
+} as AttachedModule
+
+const heaterShakerModule = {
+  moduleModel: 'heaterShakerModuleV1',
+  moduleType: 'heaterShakerModuleType',
+  data: {},
+} as AttachedModule
+
+const modules = [
+  temperatureModule,
+  magneticModule,
+  thermocyclerModule,
+  heaterShakerModule,
+]
+
+const Template: Story<React.ComponentProps<typeof ModuleIcon>> = args => (
+  <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing3}>
+    <StyledText>Modules</StyledText>
+    <Flex flexDirection={DIRECTION_ROW} gridGap={SPACING.spacing3}>
+      {modules.map((module, i) => (
+        <ModuleIcon
+          key={`module-${i}`}
+          module={module}
+          tooltipText={module.moduleModel}
+        />
+      ))}
+    </Flex>
+  </Flex>
+)
+
+export const Primary = Template.bind({})
+Primary.args = {}
--- app/src/organisms/Devices/RobotCard.tsx
+++ app/src/organisms/Devices/RobotCard.tsx
@@ -159,6 +159,7 @@ function AttachedModules(props: { robotName: string }): JSX.Element | null {
   const { robotName } = props
   const { t } = useTranslation('devices_landing')
   const attachedModules = useAttachedModules()
+  console.log(attachedModules)
   return attachedModules.length > 0 ? (
     <Box
       display="grid"
