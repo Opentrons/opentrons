21d26618544ba6dee8e0b59417af78bb2cba7607
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-09-09 09:47:19 -0400

    removed second option. changed docstring

--- api/tests/opentrons/cli/test_cli.py
+++ api/tests/opentrons/cli/test_cli.py
@@ -25,7 +25,7 @@ def _list_fixtures(version: int) -> List[Path]:
 
 @pytest.mark.parametrize("defpath", _list_fixtures(5))
 def test_analyze(defpath: Path) -> None:
-    """Should return a StateSummery object."""
+    """Should return with no errors and a none empty output."""
     runner = CliRunner()
     result = runner.invoke(
         analyze, [str(defpath.resolve()), "--json-output", "/dev/stdout"]
@@ -34,17 +34,3 @@ def test_analyze(defpath: Path) -> None:
     assert result.exit_code == 0
     # TODO (tz, 9-8-22): should probably assert that expected props are in dict
     assert result.output is not None
-
-
-# another way to test with subprocess
-# @pytest.mark.parametrize("defpath", list_fixtures(5))
-# async def test_cli_executes_analyze(defpath: Path) -> None:
-#     """Should call analyze command and execute with no errors."""
-#     if which('opentrons') is None:
-#         subprocess.run('make sdist wheel', shell=True)
-#         subprocess.run('pip install --find-links=./api/dist opentrons', shell=True)
-#     out = subprocess.run(['python', '-m', 'opentrons.cli', 'analyze', str(defpath.resolve()), '--json-output', '/dev/stdout'], capture_output=True, text=True)
-#
-#     assert out.returncode == 0
-#     # Should I assert that its type StateSummery? should I assert the whole response object?
-#     assert out.stdout is not None
