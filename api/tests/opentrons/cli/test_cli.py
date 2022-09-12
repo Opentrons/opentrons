93216dd0747edb52f4ac144147fca7c4f5945d92
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-09-12 15:04:26 -0400

    write output to file and read from file

--- api/tests/opentrons/cli/test_cli.py
+++ api/tests/opentrons/cli/test_cli.py
@@ -1,4 +1,6 @@
 """Test cli execution."""
+import json
+
 from typing import List
 from pathlib import Path
 
@@ -24,13 +26,21 @@ def _list_fixtures(version: int) -> List[Path]:
 
 
 @pytest.mark.parametrize("defpath", _list_fixtures(5))
-def test_analyze(defpath: Path) -> None:
+def test_analyze(defpath: Path, tmp_path: Path) -> None:
     """Should return with no errors and a none empty output."""
+    analysis_output_path = tmp_path / "analysis_output.json"
+
     runner = CliRunner()
     result = runner.invoke(
-        analyze, [str(defpath.resolve()), "--json-output", "/dev/stdout"]
+        analyze, [str(defpath.resolve()), "--json-output", str(analysis_output_path)]
     )
 
+    analysis_output_json = json.loads(analysis_output_path.read_bytes())
+
     assert result.exit_code == 0
-    # TODO (tz, 9-8-22): should probably assert that expected props are in dict
-    assert result.output is not None
+
+    assert "pipettes" in analysis_output_json
+    assert "commands" in analysis_output_json
+    assert "labware" in analysis_output_json
+    assert "liquids" in analysis_output_json
+    assert "modules" in analysis_output_json
