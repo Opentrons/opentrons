7810f4bd0e67688e9b861ac86d8a77702c6844a9
Brian Cooper <brian@opentrons.com>
2022-09-12 16:21:31 -0400

    add test for complete confirmation

--- app/src/organisms/CalibrationPanels/__tests__/CompleteConfirmation.test.tsx
+++ app/src/organisms/CalibrationPanels/__tests__/CompleteConfirmation.test.tsx
@@ -1,7 +1,55 @@
-// import * as React from 'react'
+import * as React from 'react'
+import { renderWithProviders } from '@opentrons/components'
+import { i18n } from '../../../i18n'
+import { mockDeckCalTipRack } from '../../../redux/sessions/__fixtures__'
+import * as Sessions from '../../../redux/sessions'
 
-// import { CompleteConfirmation } from '../CompleteConfirmation'
+import { CompleteConfirmation } from '../CompleteConfirmation'
 
 describe('CompleteConfirmation', () => {
-  it.todo('add rtl unit test coverage for CompleteConfirmation Panel')
+  let render: (
+    props?: Partial<React.ComponentProps<typeof CompleteConfirmation>>
+  ) => ReturnType<typeof renderWithProviders>
+
+  const mockCleanUpAndExit = jest.fn()
+
+  beforeEach(() => {
+    render = (props = {}) => {
+      const { proceed = mockCleanUpAndExit, flowName, body, visualAid } = props
+      return renderWithProviders(
+        <CompleteConfirmation
+          proceed={proceed}
+          flowName={flowName}
+          body={body}
+          visualAid={visualAid}
+        />,
+        { i18nInstance: i18n }
+      )
+    }
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('clicking continue sends exit command and deletes session', () => {
+    const { getByRole } = render()[0]
+    getByRole('button', { name: 'exit' }).click()
+    expect(mockCleanUpAndExit).toHaveBeenCalled()
+  })
+
+  it('renders heading and body text if present', () => {
+    const { getByRole, getByText } = render({
+      flowName: 'fake flow name',
+      body: 'fake body text',
+    })[0]
+
+    getByRole('heading', { name: 'fake flow name complete!' })
+    getByText('fake body text')
+  })
+
+  it('renders visual aid in place of icon if present', () => {
+    const { getByText } = render({ visualAid: 'fake visual aid' })[0]
+    getByText('fake visual aid')
+  })
 })
--- app/src/organisms/CalibrationPanels/__tests__/ConfirmExit.test.tsx
+++ app/src/organisms/CalibrationPanels/__tests__/ConfirmExit.test.tsx
@@ -1,7 +1,57 @@
-// import * as React from 'react'
+import * as React from 'react'
+import { renderWithProviders } from '@opentrons/components'
+import { i18n } from '../../../i18n'
 
-// import { ConfirmExit } from '../ConfirmExit'
+import { ConfirmExit } from '../ConfirmExit'
 
 describe('ConfirmExit', () => {
-  it.todo('add rtl unit test coverage for ConfirmExit Panel')
+  let render: (
+    props?: Partial<React.ComponentProps<typeof ConfirmExit>>
+  ) => ReturnType<typeof renderWithProviders>
+
+  const mockBack = jest.fn()
+  const mockExit = jest.fn()
+
+  beforeEach(() => {
+    render = (props = {}) => {
+      const {
+        heading,
+        body,
+      } = props
+      return renderWithProviders(
+        <ConfirmExit
+          exit={mockExit}
+          back={mockBack}
+          heading={heading}
+          body={body}
+        />,
+        { i18nInstance: i18n }
+      )
+    }
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('clicking confirm exit calls exit', () => {
+    const { getByRole } = render()[0]
+    getByRole('button', { name: 'exit' }).click()
+    expect(mockExit).toHaveBeenCalled()
+  })
+
+  it('clicking back calls back', () => {
+    const { getByRole } = render()[0]
+    getByRole('button', { name: 'Go back' }).click()
+    expect(mockBack).toHaveBeenCalled()
+  })
+
+  it('renders body and heading text if present', () => {
+    const { getByRole, getByText } = render({
+      heading: 'fake heading',
+      body: 'fake body',
+    })[0]
+    getByRole('heading', { name: 'fake heading' })
+    getByText('fake heading')
+  })
 })
