4a2c89d0592cb9febb6c4ef7ab18db868c8b1a84
koji <kk2796@nyu.edu>
2022-09-14 18:05:43 -0400

    refactor(app): consolidate overflow menu logic (#11293)
    This PR will remove the fixed value for overflow menu width and update the usage of useOnClickOutside
    fix #11009

--- app/src/organisms/Devices/HistoricalProtocolRunOverflowMenu.tsx
+++ app/src/organisms/Devices/HistoricalProtocolRunOverflowMenu.tsx
@@ -151,7 +151,7 @@ function MenuDropdown(props: MenuDropdownProps): JSX.Element {
 
   return (
     <Flex
-      width="11.625rem"
+      whiteSpace="nowrap"
       zIndex={10}
       borderRadius="4px 4px 0px 0px"
       boxShadow="0px 1px 3px rgba(0, 0, 0, 0.2)"
