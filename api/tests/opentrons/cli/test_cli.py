8e74f8cdbffe3da140f66021b041d6fa9a69655b
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-09-14 13:34:47 -0400

    changed list to iter. changed arg name

--- api/tests/opentrons/cli/test_cli.py
+++ api/tests/opentrons/cli/test_cli.py
@@ -1,7 +1,7 @@
 """Test cli execution."""
 import json
 
-from typing import List
+from typing import Iterator
 from pathlib import Path
 
 import pytest
@@ -10,22 +10,21 @@ from click.testing import CliRunner
 from opentrons.cli.analyze import analyze
 
 
-def _list_fixtures(version: int) -> List[Path]:
-    return list(
-        Path(__file__).parent.glob(
-            f"../../../../shared-data/protocol/fixtures/{version}/*.json"
-        )
+def _list_fixtures(version: int) -> Iterator[Path]:
+    return Path(__file__).parent.glob(
+        f"../../../../shared-data/protocol/fixtures/{version}/*.json"
     )
 
 
-@pytest.mark.parametrize("defpath", _list_fixtures(6))
-def test_analyze(defpath: Path, tmp_path: Path) -> None:
+@pytest.mark.parametrize("fixture_path", _list_fixtures(6))
+def test_analyze(fixture_path: Path, tmp_path: Path) -> None:
     """Should return with no errors and a none empty output."""
     analysis_output_path = tmp_path / "analysis_output.json"
 
     runner = CliRunner()
     result = runner.invoke(
-        analyze, [str(defpath.resolve()), "--json-output", str(analysis_output_path)]
+        analyze,
+        [str(fixture_path.resolve()), "--json-output", str(analysis_output_path)],
     )
 
     analysis_output_json = json.loads(analysis_output_path.read_bytes())
