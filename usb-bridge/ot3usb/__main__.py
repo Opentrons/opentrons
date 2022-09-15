5dd09bce0b3d263daf9e8297729cf01f7c420af8
Frank Sinapi <frank.sinapi@opentrons.com>
2022-09-15 16:23:27 -0400

    Added simple CLI module with option to set log level

--- usb-bridge/ot3usb/__main__.py
+++ usb-bridge/ot3usb/__main__.py
@@ -3,13 +3,20 @@ import asyncio
 import logging
 from typing import NoReturn
 import time
+from . import cli
 
 LOG = logging.getLogger(__name__)
 
 
 async def main() -> NoReturn:
     """Entrypoint for USB-TCP bridge."""
-    LOG.info("Starting USB-TCP bridge application")
+    parser = cli.build_root_parser()
+    args = parser.parse_args()
+
+    numeric_level = getattr(logging, args.log_level.upper())
+    logging.basicConfig(level=numeric_level)
+
+    LOG.info("Starting USB-TCP bridge")
 
     while True:
         time.sleep(10)
