582bd8fc7ae07934a569b091a7500b944bef5d4a
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-09-16 09:56:36 -0400

    WIP - removed point field. changed import. cast to OT3API

---
+++ api/tests/opentrons/protocol_engine/commands/calibration/test_calibrate_robot.py
@@ -0,0 +1,49 @@
+"""Test calibrate-robot command."""
+import inspect
+
+import pytest
+from decoy import Decoy
+
+from opentrons.protocol_engine.commands.calibration.calibrate_robot import (
+    CalibrateRobotResult,
+    CalibrateRobotImplementation,
+    CalibrateRobotParams,
+)
+
+from opentrons.hardware_control import (
+    HardwareControlAPI,
+    ot3_calibration as calibration,
+)
+from opentrons.hardware_control.types import OT3Mount
+from opentrons.types import Point
+
+
+@pytest.fixture(autouse=True)
+def _mock_ot3_calibration(decoy: Decoy, monkeypatch: pytest.MonkeyPatch) -> None:
+    for name, func in inspect.getmembers(calibration, inspect.isfunction):
+        monkeypatch.setattr(calibration, name, decoy.mock(func=func))
+
+
+async def test_calibrate_robot_implementation(
+    decoy: Decoy,
+    hardware_api: HardwareControlAPI,
+) -> None:
+    """Test Probe command execution."""
+    subject = CalibrateRobotImplementation(hardware_api=hardware_api)
+
+    params = CalibrateRobotParams(
+        mount=OT3Mount.LEFT,
+    )
+
+    decoy.when(
+        await calibration.find_deck_position(hcapi=hardware_api, mount=params.mount)
+    ).then_return(5.0)
+    decoy.when(
+        await calibration.find_slot_center_binary(
+            hcapi=hardware_api, mount=params.mount, deck_height=5.0
+        )
+    ).then_return((3, 4))
+
+    result = await subject.execute(params)
+
+    assert result == CalibrateRobotResult()
