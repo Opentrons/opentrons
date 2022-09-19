458ef4370d488edf879f05199d5ffbbc68cc6b51
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-09-19 12:26:28 -0400

    changed logic to call calibrate_mount

--- api/tests/opentrons/protocol_engine/commands/calibration/test_calibrate_robot.py
+++ api/tests/opentrons/protocol_engine/commands/calibration/test_calibrate_robot.py
@@ -1,6 +1,5 @@
 """Test calibrate-robot command."""
 import inspect
-from typing import cast
 
 import pytest
 from decoy import Decoy
@@ -17,6 +16,7 @@ from opentrons.hardware_control import (
 )
 from opentrons.hardware_control.ot3api import OT3API
 from opentrons.hardware_control.types import OT3Mount
+from opentrons.types import Point
 
 
 @pytest.fixture(autouse=True)
@@ -27,24 +27,19 @@ def _mock_ot3_calibration(decoy: Decoy, monkeypatch: pytest.MonkeyPatch) -> None
 
 async def test_calibrate_robot_implementation(
     decoy: Decoy,
-    ot3_api: OT3API,
+    hardware_api: HardwareControlAPI,
 ) -> None:
     """Test Probe command execution."""
-    subject = CalibrateRobotImplementation(hardware_api=OT3API)
+    subject = CalibrateRobotImplementation(hardware_api=hardware_api)
 
     params = CalibrateRobotParams(
         mount=OT3Mount.LEFT,
     )
 
     decoy.when(
-        await calibration.find_deck_position(hcapi=ot3_api, mount=params.mount)
-    ).then_return(5.0)
-    decoy.when(
-        await calibration.find_slot_center_binary(
-            hcapi=ot3_api, mount=params.mount, deck_height=5.0
-        )
-    ).then_return((3, 4))
+        await calibration.calibrate_mount(hcapi=hardware_api, mount=params.mount)
+    ).then_return(Point(x=3, y=4, z=6))
 
     result = await subject.execute(params)
 
-    assert result == CalibrateRobotResult()
+    assert result == CalibrateRobotResult(pipetteOffset=Point(x=3, y=4, z=6))
