61c6c7cae7d69e14c4b075ea42da1ee33adb9111
Caila Marashaj <caila.marashaj@opentrons.com>
2022-09-19 13:42:17 -0400

    test passing

--- api/tests/opentrons/protocol_engine/commands/calibration/test_calibration_set_up_position.py
+++ api/tests/opentrons/protocol_engine/commands/calibration/test_calibration_set_up_position.py
@@ -1,79 +1,69 @@
+"""Test for Calibration Set Up Position Implementation."""
 from decoy import Decoy
 import pytest
 import mock
-from typing import Optional, Dict
 
-from opentrons.protocol_engine.commands.calibration.move_to_start_calibration import (
-    CalibrationSetUpPosition,
+from opentrons.protocol_engine.commands.calibration.calibration_set_up_position import (
     CalibrationPositions,
     CalibrationSetUpPositionParams,
     CalibrationSetUpPositionImplementation,
     CalibrationSetUpPositionResult,
 )
-from opentrons.shared_data.python.opentrons
-# if TYPE_CHECKING:
-from opentrons.hardware_control import HardwareControlAPI
 from opentrons.protocol_engine.execution import MovementHandler
 from opentrons.protocol_engine.state import StateView
 from opentrons.protocol_engine.types import DeckPoint
 from opentrons.types import DeckSlotName, Point
 
-#
-# def get_labware_view() -> LabwareView:
-#     """Get a labware view test subject."""
-#     labware_state = LabwareState(
-#         labware_by_id= {},
-#         labware_offsets_by_id= {},
-#         definitions_by_uri= {},
-#         deck_definition= cast(DeckDefinitionV3, {"fake": True}),
-#     )
-#
-#     # labware_view = LabwareView(state=state)
-#     state = State(labware=labware_state)
 
-# fix this
-@pytest.fixture
-def mock_deck_slot_center(state_view: StateView, slot_name: DeckSlotName) -> Point:
-    labware_view = state_view.labware
-    with patch.object(
-        labware_view, "get_slot_center_position",
-    )
+start_calibration = DeckPoint(x=4, y=5, z=6)
+probe_interation = DeckPoint(x=1, y=2, z=3)
 
 
-
-@pytest.fixture
 def mock_deck_slot_center(slot_name: DeckSlotName) -> Point:
+    """Return a point value for each deck slot input."""
     if slot_name == DeckSlotName.SLOT_2:
-        return Point(x=1, y=2, z=3)
+        return Point(1, 2, 3)
     elif slot_name == DeckSlotName.SLOT_5:
-        return Point(x=4, y=5, z=6)
+        return Point(4, 5, 6)
     else:
-        return Point(x=7, y=8, z=9)
+        return Point(7, 8, 9)
 
 
+@pytest.mark.parametrize(
+    argnames=["slot_name", "movement_coordinates"],
+    argvalues=[
+        [CalibrationPositions.start_calibration, start_calibration],
+        [CalibrationPositions.probe_interaction, probe_interation],
+    ],
+)
 async def test_calibration_set_up_position_implementation(
     decoy: Decoy,
     state_view: StateView,
     movement: MovementHandler,
+    slot_name: CalibrationPositions,
+    movement_coordinates: DeckPoint,
 ) -> None:
-
+    """Command should get a Point value for a given deck slot center and \
+        call Movement.move_to_coordinates with the correct input."""
     subject = CalibrationSetUpPositionImplementation(
         state_view=state_view, movement=movement
     )
 
     params = CalibrationSetUpPositionParams(
         pipetteId="pipette-id",
-        slot_name=CalibrationPositions.start_calibration,
+        slot_name=slot_name,
     )
-
-    result = await subject.execute(params=params)
+    with mock.patch.object(
+        state_view.labware, "get_slot_center_position", new=mock_deck_slot_center
+    ):
+        result = await subject.execute(params=params)
 
     assert result == CalibrationSetUpPositionResult()
 
     decoy.verify(
         await movement.move_to_coordinates(
             pipette_id="pipette-id",
-            deck_coordinates=DeckPoint(x=4, y=5, z=6),
+            deck_coordinates=movement_coordinates,
             direct=True,
             additional_min_travel_z=0,
         )
