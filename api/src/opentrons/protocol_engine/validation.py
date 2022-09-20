6d6722b7c141d44d2134416e370450068f9e7d3b
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-09-20 14:51:36 -0400

    added docstring and linting fixes

--- api/src/opentrons/protocol_engine/validation.py
+++ api/src/opentrons/protocol_engine/validation.py
@@ -1,8 +1,10 @@
+"""Validation file for protocol engine."""
 from ..hardware_control.ot3api import OT3API
 from ..hardware_control.protocols import HardwareControlAPI
 from ..protocol_engine.errors.exceptions import HardwareNotSupported
 
 
 def ensure_ot3_hardware(hardware_api: HardwareControlAPI) -> None:
+    """Validate that the HardwareControlAPI is of OT-3 instance."""
     if not isinstance(hardware_api, OT3API):
         raise HardwareNotSupported("This command is supported by OT3 Only.")
