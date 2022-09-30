9e2cb472ad8b1f1ec2ae7290a6fea7a943f4b881
Jethary Rader <jethary@opentrons.com>
2022-09-30 14:04:35 -0400

    refactor(app): thermocycler GEN2 design qa feedback
    closes RCORE-242

--- app/src/atoms/StatusLabel/index.tsx
+++ app/src/atoms/StatusLabel/index.tsx
@@ -15,7 +15,7 @@ import { StyledText } from '../text'
 interface StatusLabelProps {
   status: string
   backgroundColor: string
-  iconColor: string
+  iconColor?: string
   textColor?: string
   fontWeight?: number
   iconSize?: string
@@ -48,23 +48,25 @@ export const StatusLabel = (props: StatusLabelProps): JSX.Element | null => {
         marginBottom={SPACING.spacing2}
         data-testid={`status_label_${status}_${id}`}
       >
-        <Icon
-          name="circle"
-          color={iconColor}
-          size={iconSize ?? '0.25rem'}
-          data-testid="status_circle"
-        >
-          {pulse != null && pulse ? (
-            <animate
-              attributeName="fill"
-              values={`${iconColor}; transparent`}
-              dur="1s"
-              calcMode="discrete"
-              repeatCount="indefinite"
-              data-testid="pulsing_status_circle"
-            />
-          ) : null}
-        </Icon>
+        {iconColor != null ? (
+          <Icon
+            name="circle"
+            color={iconColor}
+            size={iconSize ?? '0.25rem'}
+            data-testid="status_circle"
+          >
+            {pulse != null && pulse ? (
+              <animate
+                attributeName="fill"
+                values={`${iconColor}; transparent`}
+                dur="1s"
+                calcMode="discrete"
+                repeatCount="indefinite"
+                data-testid="pulsing_status_circle"
+              />
+            ) : null}
+          </Icon>
+        ) : null}
         <StyledText
           fontSize={TYPOGRAPHY.fontSizeLabel}
           fontWeight={fontWeight ?? TYPOGRAPHY.fontWeightRegular}
