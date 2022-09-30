a732d7c395c7aee8bad047dda3cdb11139916515
Koji <kk2796@nyu.edu>
2022-09-30 12:18:13 -0400

    fix utils test errors

--- app/src/organisms/ModuleCard/__tests__/utils.test.ts
+++ app/src/organisms/ModuleCard/__tests__/utils.test.ts
@@ -30,31 +30,31 @@ const mockThermocyclerGen1ClosedLid = {
 describe('getModuleCardImage', () => {
   it('should render the correct image string when there is a magnetic module gen 2 attached', () => {
     const result = getModuleCardImage(mockMagneticModuleGen2)
-    expect(result).toEqual('magnetic_module_gen_2_transparent.svg')
+    expect(result).toEqual('magnetic_module_gen_2_transparent.png')
   })
   it('should render the correct image string when there is a magnetic module gen 1 attached', () => {
     const result = getModuleCardImage(mockMagneticModule)
-    expect(result).toEqual('magnetic_module_gen_2_transparent.svg')
+    expect(result).toEqual('magnetic_module_gen_2_transparent.png')
   })
   it('should render the correct image string when there is a temperature module gen 1 attached', () => {
     const result = getModuleCardImage(mockTemperatureModule)
-    expect(result).toEqual('temp_deck_gen_2_transparent.svg')
+    expect(result).toEqual('temp_deck_gen_2_transparent.png')
   })
   it('should render the correct image string when there is a temperature module gen 2 attached', () => {
     const result = getModuleCardImage(mockTemperatureModuleGen2)
-    expect(result).toEqual('temp_deck_gen_2_transparent.svg')
+    expect(result).toEqual('temp_deck_gen_2_transparent.png')
   })
   it('should render the correct image string when there is a heater shaker gen 1 attached', () => {
     const result = getModuleCardImage(mockHeaterShaker)
-    expect(result).toEqual('heatershaker_module_transparent.svg')
+    expect(result).toEqual('heater_shaker_module_transparent.png')
   })
   it('should render the correct image string when there is a thermocycler gen 1 attached with opened lid', () => {
     const result = getModuleCardImage(mockThermocycler)
-    expect(result).toEqual('thermocycler_open_transparent.svg')
+    expect(result).toEqual('thermocycler_open_transparent.png')
   })
   it('should render the correct image string when there is a thermocycler gen 1 attached with closed lid', () => {
     const result = getModuleCardImage(mockThermocyclerGen1ClosedLid)
-    expect(result).toEqual('thermocycler_closed.svg')
+    expect(result).toEqual('thermocycler_closed.png')
   })
   it('should render the correct image string when there is a thermocycler gen 2 with opened lid is attached', () => {
     const result = getModuleCardImage(mockThermocyclerGen2)
