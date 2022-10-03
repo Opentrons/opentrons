8134f04e8e4324b224b521b067724ae397f1e348
Ryan Howard <ryan.howard@opentrons.com>
2022-10-03 13:08:49 -0400

    fix(firmware-bindings): fix formating and liniting issues

--- hardware/tests/opentrons_hardware/firmware_bindings/test_messages/test_message_index.py
+++ hardware/tests/opentrons_hardware/firmware_bindings/test_messages/test_message_index.py
@@ -1,25 +1,13 @@
-import pytest
-import asyncio
-from typing import List, Callable
-from mock import AsyncMock
+"""Tests for can message_index generator."""
+from opentrons_hardware.firmware_bindings import utils
 
-from opentrons_hardware.drivers.can_bus.can_messenger import CanMessenger
-from opentrons_hardware.firmware_bindings import (
-    ArbitrationId,
-    ArbitrationIdParts,
-    utils,
-)
-from opentrons_hardware.firmware_bindings.messages import (
-    MessageDefinition,
-    message_definitions,
-    payloads,
-)
+from opentrons_hardware.firmware_bindings.messages import message_definitions
+
 
 def test_unqiue_index() -> None:
-    last_index = 0;
-    for i in range (1, 20):
-        message=message_definitions.HeartbeatRequest()
+    """Create several messages and make sure they have generate a new index."""
+    last_index = utils.UInt32Field(0)
+    for i in range(1, 20):
+        message = message_definitions.HeartbeatRequest()
         assert message.message_index != last_index
         last_index = message.message_index
-
-
