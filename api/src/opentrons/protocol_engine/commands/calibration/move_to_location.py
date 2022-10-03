a96543b21b8dba29fec499ad5f678d476634a0a9
Caila Marashaj <caila.marashaj@opentrons.com>
2022-10-03 15:18:02 -0400

    removed extended enum

--- api/src/opentrons/protocol_engine/commands/calibration/move_to_location.py
+++ api/src/opentrons/protocol_engine/commands/calibration/move_to_location.py
@@ -13,15 +13,20 @@ from opentrons.protocol_engine.commands.command import (
     BaseCommand,
     BaseCommandCreate,
 )
+<<<<<<< HEAD
+=======
+from opentrons.protocol_engine.types import DeckPoint
+>>>>>>> 055e07927 (removed extended enum)
 
 if TYPE_CHECKING:
-    from opentrons.protocol_engine.execution import MovementHandler, SavedPositionData
+    from opentrons.protocol_engine.execution import MovementHandler
     from opentrons.protocol_engine.state.state import StateView
 
 
 MoveToLocationCommandType = Literal["calibration/moveToLocation"]
 
 
+<<<<<<< HEAD
 class CalibrationPositions(str, Enum):
     """Deck slot to move to."""
 
@@ -35,13 +40,35 @@ class CalibrationPositions(str, Enum):
             return DeckPoint(x=10, y=0, z=3)
         else:
             return DeckPoint(x=0, y=0, z=0)
+=======
+class CalibrationPositions(Enum):
+    """Deck slot to move to."""
+
+    probe_position = 5
+    attach_or_detach = 2
+
+    @property
+    def location(self) -> DeckPoint:
+        """Assign location value to CalibrationPositions with a 3mm safety margin."""
+        if self.value == 5:
+            return DeckPoint(x=float(145), y=float(107), z=float(3))
+        elif self.value == 2:
+            return DeckPoint(x=float(145), y=float(0), z=float(3))
+        else:
+            return DeckPoint(x=float(0), y=float(0), z=float(3))
+>>>>>>> 055e07927 (removed extended enum)
 
 
 class MoveToLocationParams(PipetteIdMixin):
     """Calibration set up position command parameters."""
 
+<<<<<<< HEAD
     deckSlot: CalibrationPositions = Field(
         ...,
+=======
+    deck_slot: CalibrationPositions = Field(
+        CalibrationPositions.probe_position,
+>>>>>>> 055e07927 (removed extended enum)
         description="Slot location to move to before starting calibration.",
     )
 
@@ -49,9 +76,10 @@ class MoveToLocationParams(PipetteIdMixin):
 class MoveToLocationResult(BaseModel):
     """Result data from the execution of a CalibrationSetUpPosition command."""
 
-    position: SavedPositionData = Field(
+    position: DeckPoint = Field(
         ..., description="Deck Point position after the move command has been executed"
     )
+    positionId: str = Field(..., description="Deck Point position id")
 
 
 class MoveToLocationImplementation(
@@ -67,6 +95,7 @@ class MoveToLocationImplementation(
 
     async def execute(self, params: MoveToLocationParams) -> MoveToLocationResult:
         """Move the requested pipette to a given deck slot."""
+<<<<<<< HEAD
         offset = params.deckSlot.offset
         if params.deckSlot == CalibrationPositions.probePosition:
             deck_center = self._state_view.labware.get_slot_center_position(
@@ -88,12 +117,21 @@ class MoveToLocationImplementation(
         await self._movement.move_to_coordinates(
             pipette_id=params.pipetteId,
             deck_coordinates=destination,
+=======
+        await self._movement.move_to_coordinates(
+            pipette_id=params.pipetteId,
+            deck_coordinates=params.deck_slot.location,
+>>>>>>> 055e07927 (removed extended enum)
             direct=True,
             additional_min_travel_z=None,
         )
 
         new_position = await self._movement.save_position(
+<<<<<<< HEAD
             pipette_id=params.pipetteId, position_id=str(params.deckSlot.name)
+=======
+            pipette_id=params.pipetteId, position_id=str(params.deck_slot.name)
+>>>>>>> 055e07927 (removed extended enum)
         )
 
         return MoveToLocationResult(
