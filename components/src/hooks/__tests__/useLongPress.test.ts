1af684f7154f2fd087de950602c4e953aa2deab6
koji <kk2796@nyu.edu>
2022-10-05 00:28:23 -0400

    fix check-js error

--- components/src/hooks/__tests__/useLongPress.test.ts
+++ components/src/hooks/__tests__/useLongPress.test.ts
@@ -2,31 +2,29 @@ import { renderHook } from '@testing-library/react-hooks'
 import { useLongPress } from '../useLongPress'
 
 describe('useLongPress', () => {
-    it('useLongPress returns typeof UseLongPressResult', () => {
-        const { result} = renderHook(() => useLongPress())
-        expect(result.current.style.touchAction).toBe('none')
-        expect(result.current.isEnabled).toBe(true)
-        expect(result.current.isLongPressed).toBe(false)
-        expect(result.current.isTapped).toBe(false)
-    })
+  it('useLongPress returns typeof UseLongPressResult', () => {
+    const { result } = renderHook(() => useLongPress())
+    expect(result.current.style.touchAction).toBe('none')
+    expect(result.current.isEnabled).toBe(true)
+    expect(result.current.isLongPressed).toBe(false)
+    expect(result.current.isTapped).toBe(false)
+  })
 
-    // it('when calling host, isLongPressed true', () => {
-    //     const { result } = renderHook(() => useLongPress())
-        
-    //     expect(result.current.isLongPressed).toBe(true)
-    // })
+  // it('when calling host, isLongPressed true', () => {
+  //     const { result } = renderHook(() => useLongPress())
 
+  //     expect(result.current.isLongPressed).toBe(true)
+  // })
 
-    it('when callling disable, isEnabled true', () => {
-        const { result } = renderHook(() => useLongPress())
-        result.current.enable()
-        expect(result.current.isEnabled).toBe(true)
+  it('when callling disable, isEnabled true', () => {
+    const { result } = renderHook(() => useLongPress())
+    result.current.enable()
+    expect(result.current.isEnabled).toBe(true)
+  })
 
-    })
-
-    it('when callling disable, isEnabled false', () => {
-        const { result } = renderHook(() => useLongPress())
-        result.current.disable()
-        expect(result.current.isEnabled).toBe(false)
-    })
+  it('when callling disable, isEnabled false', () => {
+    const { result } = renderHook(() => useLongPress())
+    result.current.disable()
+    expect(result.current.isEnabled).toBe(false)
+  })
 })
