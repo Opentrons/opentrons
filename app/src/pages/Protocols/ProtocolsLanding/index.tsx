b7a3bad2ffa513970535315278fc4ce4b762bf3b
Koji <kk2796@nyu.edu>
2022-10-11 15:14:47 -0400

    revert the protocols landing page

--- app/src/pages/Protocols/ProtocolsLanding/index.tsx
+++ app/src/pages/Protocols/ProtocolsLanding/index.tsx
@@ -6,15 +6,11 @@ import {
 } from '../../../redux/protocol-storage'
 import { ProtocolsEmptyState } from '../../../organisms/ProtocolsLanding/ProtocolsEmptyState'
 import { ProtocolList } from '../../../organisms/ProtocolsLanding/ProtocolList'
-import { getIsOnDevice } from '../../../redux/config'
-import { getLocalRobot } from '../../../redux/discovery'
 
 import type { Dispatch, State } from '../../../redux/types'
 
 export function ProtocolsLanding(): JSX.Element {
   const dispatch = useDispatch<Dispatch>()
-  const isOnDevice = useSelector(getIsOnDevice)
-  const isLocalRobot = useSelector(getLocalRobot)
   const storedProtocols = useSelector((state: State) =>
     getStoredProtocols(state)
   )
@@ -22,17 +18,9 @@ export function ProtocolsLanding(): JSX.Element {
     dispatch(fetchProtocols())
   }, [dispatch])
 
-  console.log('isOnDevice: ', isOnDevice)
-
-  return (
-    <>
-      {isLocalRobot ? 'local' : 'remote'}
-      {isOnDevice ? 'onDevice' : 'nooooooooo'}
-      {storedProtocols.length > 0 ? (
-        <ProtocolList storedProtocols={storedProtocols} />
-      ) : (
-        <ProtocolsEmptyState />
-      )}
-    </>
+  return storedProtocols.length > 0 ? (
+    <ProtocolList storedProtocols={storedProtocols} />
+  ) : (
+    <ProtocolsEmptyState />
   )
 }
