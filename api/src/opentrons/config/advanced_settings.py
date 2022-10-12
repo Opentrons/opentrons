886321cab379eff8434c54a31e361c16b629fc05
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-10-12 12:36:01 -0400

    added ff and updated settings tests

--- api/src/opentrons/config/advanced_settings.py
+++ api/src/opentrons/config/advanced_settings.py
@@ -184,6 +184,13 @@ settings = [
             " Do not enable."
         ),
     ),
+    SettingDefinition(
+        _id="enableLoadLiquid",
+        title="Enable protocol engine load liquid command",
+        description=(
+            "This is an Opentrons-internal setting to allow load liquid command."
+        ),
+    ),
 ]
 
 if ARCHITECTURE == SystemArchitecture.BUILDROOT:
@@ -480,6 +487,16 @@ def _migrate16to17(previous: SettingsMap) -> SettingsMap:
     return newmap
 
 
+def _migrate17to18(previous: SettingsMap) -> SettingsMap:
+    """Migrate to version 18 of the advanced settings file.
+
+    - Adds enableLoadLiquid option
+    """
+    newmap = {k: v for k, v in previous.items()}
+    newmap["enableLoadLiquid"] = None
+    return newmap
+
+
 _MIGRATIONS = [
     _migrate0to1,
     _migrate1to2,
@@ -498,6 +515,7 @@ _MIGRATIONS = [
     _migrate14to15,
     _migrate15to16,
     _migrate16to17,
+    _migrate17to18
 ]
 """
 List of all migrations to apply, indexed by (version - 1). See _migrate below
