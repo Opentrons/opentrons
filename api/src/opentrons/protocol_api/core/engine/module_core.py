09e44f23e20dada4adae44c4001ca21eae340f8f
tamarzanzouri <tamar.zanzouri@opentrons.com>
2022-10-13 19:20:07 -0400

    added not implemented to abstract methods

--- api/src/opentrons/protocol_api/core/engine/module_core.py
+++ api/src/opentrons/protocol_api/core/engine/module_core.py
@@ -1,6 +1,14 @@
 """Protocol API module implementation logic."""
 from ..module import AbstractModuleCore
 from .labware import LabwareCore
+from ..labware import LabwareCoreType
+
+from opentrons.hardware_control.modules.types import (
+    ModuleModel,
+    ModuleType,
+)
+from opentrons.protocols.geometry.module_geometry import ModuleGeometry
+from opentrons.types import DeckSlotName
 
 
 class ModuleCore(AbstractModuleCore[LabwareCore]):
@@ -17,3 +25,36 @@ class ModuleCore(AbstractModuleCore[LabwareCore]):
     def module_id(self) -> str:
         """The module's unique ProtocolEngine ID."""
         return self._module_id
+
+    @property
+    def geometry(self) -> ModuleGeometry:
+        """Get the module's geometry interface."""
+        raise NotImplementedError("geometry not implemented")
+
+    def get_model(self) -> ModuleModel:
+        """Get the module's model identifier."""
+        raise NotImplementedError("get_model not implemented")
+
+    def get_type(self) -> ModuleType:
+        """Get the module's general type identifier."""
+        raise NotImplementedError("get_type not implemented")
+
+    def get_requested_model(self) -> ModuleModel:
+        """Get the model identifier the module was requested as.
+
+        This may differ from the actual model returned by `get_model`.
+        """
+        raise NotImplementedError("get_requested_model not implemented")
+
+    def get_serial_number(self) -> str:
+        """Get the module's unique hardware serial number."""
+        raise NotImplementedError("get_serial_number not implemented")
+
+    def get_deck_slot(self) -> DeckSlotName:
+        """Get the module's deck slot."""
+        raise NotImplementedError("get_deck_slot not implemented")
+
+    def add_labware_core(self, labware_core: LabwareCoreType) -> None:
+        """Add a labware to the module."""
+        raise NotImplementedError("add_labware_core not implemented")
+
