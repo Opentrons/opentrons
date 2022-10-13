3244ebc1138516d1a1c00c834821c80721f2aa0c
koji <kk2796@nyu.edu>
2022-10-12 20:15:03 -0400

    feat(app): add dummy OT-3 image (#11477)
    add OT-3 image to device landing device detail and slideout
    This PR will close RCORE-74

--- app/src/organisms/Devices/__tests__/RobotOverview.test.tsx
+++ app/src/organisms/Devices/__tests__/RobotOverview.test.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react'
 import { MemoryRouter } from 'react-router-dom'
 import { fireEvent } from '@testing-library/react'
+import { when, resetAllWhenMocks } from 'jest-when'
 
 import { renderWithProviders } from '@opentrons/components'
+import {
+  mockOT2HealthResponse,
+  mockOT2ServerHealthResponse,
+  mockOT3HealthResponse,
+  mockOT3ServerHealthResponse,
+} from '@opentrons/discovery-client/src/__fixtures__'
 
 import { i18n } from '../../../i18n'
 import { useCurrentRunId } from '../../ProtocolUpload/hooks'
@@ -11,6 +18,12 @@ import { mockConnectableRobot } from '../../../redux/discovery/__fixtures__'
 import { useDispatchApiRequest } from '../../../redux/robot-api'
 import { getBuildrootUpdateDisplayInfo } from '../../../redux/buildroot'
 import { fetchLights } from '../../../redux/robot-controls'
+import { getRobotModelByName } from '../../../redux/discovery'
+import {
+  HEALTH_STATUS_OK,
+  ROBOT_MODEL_OT2,
+  ROBOT_MODEL_OT3,
+} from '../../../redux/discovery/constants'
 import { useLights, useRobot, useRunStatuses } from '../hooks'
 import { UpdateRobotBanner } from '../../UpdateRobotBanner'
 import { RobotStatusBanner } from '../RobotStatusBanner'
@@ -18,10 +31,12 @@ import { RobotOverview } from '../RobotOverview'
 import { RobotOverviewOverflowMenu } from '../RobotOverviewOverflowMenu'
 
 import type { DispatchApiRequestType } from '../../../redux/robot-api'
+import type { State } from '../../../redux/types'
 
 jest.mock('../../../redux/robot-api')
 jest.mock('../../../redux/robot-controls')
 jest.mock('../../../redux/buildroot/selectors')
+jest.mock('../../../redux/discovery/selectors')
 jest.mock('../../ProtocolUpload/hooks')
 jest.mock('../hooks')
 jest.mock('../RobotStatusBanner')
@@ -30,6 +45,49 @@ jest.mock('../../ChooseProtocolSlideout')
 jest.mock('../RobotOverviewOverflowMenu')
 
 const OT2_PNG_FILE_NAME = 'OT2-R_HERO.png'
+const OT3_PNG_FILE_NAME = 'OT3.png'
+
+const MOCK_STATE: State = {
+  discovery: {
+    robot: { connection: { connectedTo: null } },
+    robotsByName: {
+      otie: {
+        name: 'otie',
+        health: mockOT2HealthResponse,
+        serverHealth: mockOT2ServerHealthResponse,
+        addresses: [
+          {
+            ip: '10.0.0.3',
+            port: 31950,
+            seen: true,
+            healthStatus: HEALTH_STATUS_OK,
+            serverHealthStatus: HEALTH_STATUS_OK,
+            healthError: null,
+            serverHealthError: null,
+            advertisedModel: ROBOT_MODEL_OT2,
+          },
+        ],
+      },
+      buzz: {
+        name: 'buzz',
+        health: mockOT3HealthResponse,
+        serverHealth: mockOT3ServerHealthResponse,
+        addresses: [
+          {
+            ip: '10.0.0.4',
+            port: 31950,
+            seen: true,
+            healthStatus: HEALTH_STATUS_OK,
+            serverHealthStatus: HEALTH_STATUS_OK,
+            healthError: null,
+            serverHealthError: null,
+            advertisedModel: ROBOT_MODEL_OT3,
+          },
+        ],
+      },
+    },
+  },
+} as any
 
 const mockUseLights = useLights as jest.MockedFunction<typeof useLights>
 const mockUseRobot = useRobot as jest.MockedFunction<typeof useRobot>
@@ -58,24 +116,30 @@ const mockGetBuildrootUpdateDisplayInfo = getBuildrootUpdateDisplayInfo as jest.
   typeof getBuildrootUpdateDisplayInfo
 >
 const mockFetchLights = fetchLights as jest.MockedFunction<typeof fetchLights>
+const mockGetRobotModelByName = getRobotModelByName as jest.MockedFunction<
+  typeof getRobotModelByName
+>
 
 const mockToggleLights = jest.fn()
 
-const render = () => {
+const render = (props: React.ComponentProps<typeof RobotOverview>) => {
   return renderWithProviders(
     <MemoryRouter>
-      <RobotOverview robotName="otie" />
+      <RobotOverview robotName={props.robotName} />
     </MemoryRouter>,
     {
       i18nInstance: i18n,
+      initialState: MOCK_STATE,
     }
   )
 }
 
 describe('RobotOverview', () => {
   let dispatchApiRequest: DispatchApiRequestType
+  let props: React.ComponentProps<typeof RobotOverview>
 
   beforeEach(() => {
+    props = { robotName: 'otie' }
     dispatchApiRequest = jest.fn()
     mockUseRunStatuses.mockReturnValue({
       isRunRunning: false,
@@ -105,34 +169,48 @@ describe('RobotOverview', () => {
     mockRobotOverviewOverflowMenu.mockReturnValue(
       <div>mock RobotOverviewOverflowMenu</div>
     )
+    when(mockGetRobotModelByName)
+      .calledWith(MOCK_STATE, 'otie')
+      .mockReturnValue('OT-2')
   })
   afterEach(() => {
     jest.resetAllMocks()
+    resetAllWhenMocks()
   })
 
-  it('renders an OT image', () => {
-    const [{ getByRole }] = render()
+  it('renders an OT-2 image', () => {
+    const [{ getByRole }] = render(props)
     const image = getByRole('img')
     expect(image.getAttribute('src')).toEqual(OT2_PNG_FILE_NAME)
   })
 
+  it('renders an OT-3 image', () => {
+    props.robotName = 'buzz'
+    when(mockGetRobotModelByName)
+      .calledWith(MOCK_STATE, props.robotName)
+      .mockReturnValue('OT-3')
+    const [{ getByRole }] = render(props)
+    const image = getByRole('img')
+    expect(image.getAttribute('src')).toEqual(OT3_PNG_FILE_NAME)
+  })
+
   it('renders a RobotStatusBanner component', () => {
-    const [{ getByText }] = render()
+    const [{ getByText }] = render(props)
     getByText('Mock RobotStatusBanner')
   })
 
   it('renders a UpdateRobotBanner component', () => {
-    const [{ getByText }] = render()
+    const [{ getByText }] = render(props)
     getByText('Mock UpdateRobotBanner')
   })
 
   it('fetches lights status', () => {
-    render()
+    render(props)
     expect(dispatchApiRequest).toBeCalledWith(mockFetchLights('otie'))
   })
 
   it('renders a lights toggle button', () => {
-    const [{ getByRole, getByText }] = render()
+    const [{ getByRole, getByText }] = render(props)
 
     getByText('Controls')
     getByText('Lights')
@@ -142,13 +220,13 @@ describe('RobotOverview', () => {
   })
 
   it('renders a Run a Protocol button', () => {
-    const [{ getByText }] = render()
+    const [{ getByText }] = render(props)
 
     getByText('Run a protocol')
   })
 
   it('renders a choose protocol slideout hidden by default, expanded after launch', () => {
-    const [{ getByText, getByRole }] = render()
+    const [{ getByText, getByRole }] = render(props)
 
     getByText('Mock Choose Protocol Slideout hidden')
     const runButton = getByRole('button', { name: 'Run a protocol' })
@@ -157,7 +235,7 @@ describe('RobotOverview', () => {
   })
 
   it('renders an overflow menu for the robot overview', () => {
-    const [{ getByText }] = render()
+    const [{ getByText }] = render(props)
 
     getByText('mock RobotOverviewOverflowMenu')
   })
@@ -170,7 +248,7 @@ describe('RobotOverview', () => {
       isRunTerminal: false,
       isRunIdle: false,
     })
-    const [{ getByRole }] = render()
+    const [{ getByRole }] = render(props)
     const runButton = getByRole('button', { name: 'Run a protocol' })
     expect(runButton).toBeDisabled()
   })
@@ -182,7 +260,7 @@ describe('RobotOverview', () => {
       updateFromFileDisabledReason: null,
     })
 
-    const [{ getByRole }] = render()
+    const [{ getByRole }] = render(props)
     const button = getByRole('button', { name: 'Run a protocol' })
     expect(button).toBeDisabled()
   })
@@ -195,7 +273,7 @@ describe('RobotOverview', () => {
       isRunTerminal: true,
       isRunIdle: false,
     })
-    const [{ getByText, getByRole }] = render()
+    const [{ getByText, getByRole }] = render(props)
     const runButton = getByRole('button', { name: 'Run a protocol' })
     fireEvent.click(runButton)
     getByText('Mock Choose Protocol Slideout showing')
