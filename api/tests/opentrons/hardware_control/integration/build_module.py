e17aa21d29edd9860f08d6ecc30449b4508f4fae
Mike Cousins <mike@opentrons.com>
2022-10-20 09:23:12 -0400

    fixup: fix write bug in module emulator proxy

--- api/tests/opentrons/hardware_control/integration/build_module.py
+++ api/tests/opentrons/hardware_control/integration/build_module.py
@@ -1,16 +1,10 @@
 """Build a module emulator for integration tests."""
 import asyncio
-import logging
 from typing import Optional, Type, TypeVar, cast
 
 from opentrons.drivers.rpi_drivers.types import USBPort
 from opentrons.hardware_control import ExecutionManager
 from opentrons.hardware_control.modules import AbstractModule
-
-
-_CONNECT_RETRIES = 3
-
-_log = logging.getLogger(__name__)
 
 ModuleType = TypeVar("ModuleType", bound=AbstractModule)
 
@@ -22,13 +16,7 @@ async def build_module(
     execution_manager: ExecutionManager,
     poll_interval_seconds: Optional[float] = None,
 ) -> ModuleType:
-    """Build a module emulator.
-
-    This factory will retry the build if it fails.
-    This is to account for the fact that the network-based serial connections
-    used by the emulators cannot always close and re-open a socket immediately.
-
-    See https://pyserial.readthedocs.io/en/latest/pyserial_api.html#rfc-2217-network-ports.
+    """Build a module emulator connected to a RFC-2217 network serial port.
 
     Args:
         module_cls: The module to build.
@@ -38,28 +26,13 @@ async def build_module(
 
     Returns:
         The module's hardware control interface.
-
-    Raises:
-        AssertionError: The module was not able to be built, even after retries.
     """
-    module = None
-    retry_attempt = 1
+    module = await module_cls.build(
+        port=f"socket://127.0.0.1:{port}",
+        execution_manager=execution_manager,
+        usb_port=USBPort(name="", port_number=1, device_path="", hub=1),
+        hw_control_loop=asyncio.get_running_loop(),
+        poll_interval_seconds=poll_interval_seconds,
+    )
 
-    # Closing and re-opening network-based serial connections does not always work
-    # Attempt to connect to the driver several times to prevent test flakiness
-    # https://pyserial.readthedocs.io/en/latest/pyserial_api.html#rfc-2217-network-ports
-    while module is None and retry_attempt <= _CONNECT_RETRIES:
-        try:
-            module = await module_cls.build(
-                port=f"socket://127.0.0.1:{port}",
-                execution_manager=execution_manager,
-                usb_port=USBPort(name="", port_number=1, device_path="", hub=1),
-                hw_control_loop=asyncio.get_running_loop(),
-                poll_interval_seconds=poll_interval_seconds,
-            )
-        except Exception as e:
-            _log.warning(f"Module build attempt {retry_attempt} failed.", exc_info=e)
-            retry_attempt += 1
-
-    assert module is not None, f"Failed to build module after {_CONNECT_RETRIES} tries."
     return cast(ModuleType, module)
