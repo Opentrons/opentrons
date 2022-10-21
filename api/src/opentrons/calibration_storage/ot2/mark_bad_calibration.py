0a24285fca87f836b9365322b9a09bf81baaa045
Laura Cox <laura@opentrons.com>
2022-10-21 15:23:09 -0400

    change mark bad typing

--- api/src/opentrons/calibration_storage/ot2/mark_bad_calibration.py
+++ api/src/opentrons/calibration_storage/ot2/mark_bad_calibration.py
@@ -7,41 +7,18 @@ from .. import types as local_types
 from .models import v1
 
 
-@typing.overload
-def mark_bad(
-    calibration: v1.DeckCalibrationModel, source_marked_bad: local_types.SourceType
-) -> v1.DeckCalibrationModel:
-    ...
-
-
-@typing.overload
-def mark_bad(
-    calibration: v1.PipetteOffsetCalibration,
-    source_marked_bad: local_types.SourceType,
-) -> v1.PipetteOffsetCalibration:
-    ...
-
-
-@typing.overload
-def mark_bad(
-    calibration: v1.TipLengthCalibration,
-    source_marked_bad: local_types.SourceType,
-) -> v1.TipLengthCalibration:
-    ...
-
-
-def mark_bad(
-    calibration: typing.Union[
-        v1.DeckCalibrationModel,
-        v1.PipetteOffsetCalibration,
-        v1.TipLengthCalibration,
-    ],
-    source_marked_bad: local_types.SourceType,
-) -> typing.Union[
-    v1.DeckCalibrationModel,
+CalibrationType = typing.TypeVar(
+    "CalibrationType",
     v1.PipetteOffsetCalibration,
     v1.TipLengthCalibration,
-]:
+    v1.DeckCalibrationModel,
+)
+
+
+def mark_bad(
+    calibration: CalibrationType,
+    source_marked_bad: local_types.SourceType,
+) -> CalibrationType:
     calibration.status = v1.CalibrationStatus(
         markedBad=True, source=source_marked_bad, markedAt=utc_now()
     )
