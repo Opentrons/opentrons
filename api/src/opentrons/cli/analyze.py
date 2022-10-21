779e55cea6a793a3acd9087cb757878699bc20b3
Max Marrone <max@marrone.nyc>
2022-10-21 17:04:30 -0400

    Wire up robotType to analysis CLI.

--- api/src/opentrons/cli/analyze.py
+++ api/src/opentrons/cli/analyze.py
@@ -92,6 +92,7 @@ async def _analyze(
                 else PythonConfig(apiVersion=protocol_source.config.api_version)
             ),
             metadata=protocol_source.metadata,
+            robotType=protocol_source.robot_type,
             commands=analysis.commands,
             errors=analysis.state_summary.errors,
             labware=analysis.state_summary.labware,
@@ -139,6 +140,7 @@ class AnalyzeResults(BaseModel):
     files: List[ProtocolFile]
     config: Union[JsonConfig, PythonConfig]
     metadata: Dict[str, Any]
+    robotType: Literal["OT-2 Standard", "OT-3 Standard"]
     commands: List[Command]
     labware: List[LoadedLabware]
     pipettes: List[LoadedPipette]
--- api/tests/opentrons/cli/test_cli.py
+++ api/tests/opentrons/cli/test_cli.py
@@ -31,6 +31,7 @@ def test_analyze(fixture_path: Path, tmp_path: Path) -> None:
 
     assert result.exit_code == 0
 
+    assert "robotType" in analysis_output_json
     assert "pipettes" in analysis_output_json
     assert "commands" in analysis_output_json
     assert "labware" in analysis_output_json
