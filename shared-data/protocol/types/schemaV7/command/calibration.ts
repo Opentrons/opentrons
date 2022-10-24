a7b72606a33f6b1f816feb09a5dcb4a25ab9bc1c
smb2268 <sarah.breen@opentrons.com>
2022-10-24 17:35:15 -0400

    Add calibration/moveToLocation command

--- shared-data/protocol/types/schemaV7/command/calibration.ts
+++ shared-data/protocol/types/schemaV7/command/calibration.ts
@@ -5,14 +5,36 @@ export interface CalibratePipetteCreateCommand extends CommonCommandCreateInfo {
   commandType: 'calibration/calibratePipette'
   params: CalibratePipetteParams
 }
+
+export interface MoveToLocationCreateCommand extends CommonCommandCreateInfo {
+  commandType: 'calibration/moveToLocation'
+  params: MoveToLocationParams
+}
+
 export interface CalibratePipetteRunTimeCommand
   extends CommonCommandRunTimeInfo,
     CalibratePipetteCreateCommand {
   result: CalibratePipetteResult
 }
 
-export type CalibrationRunTimeCommand = CalibratePipetteRunTimeCommand
-export type CalibrationCreateCommand = CalibratePipetteCreateCommand
+export interface MoveToLocationRunTimeCommand
+  extends MoveToLocationCreateCommand {
+  result: MoveToLocationResult
+}
+
+export type CalibrationRunTimeCommand =
+  | CalibratePipetteRunTimeCommand
+  | MoveToLocationRunTimeCommand
+
+export type CalibrationCreateCommand =
+  | CalibratePipetteCreateCommand
+  | MoveToLocationCreateCommand
+
+export const ATTACH_DETACH_POSITION = 'attach_or_detach'
+export const PROBE_POSITION = 'probe_position'
+export type SetupPosition =
+  | typeof ATTACH_DETACH_POSITION
+  | typeof PROBE_POSITION
 
 interface CalibratePipetteParams {
   mount: PipetteMount
@@ -20,4 +42,18 @@ interface CalibratePipetteParams {
 
 interface CalibratePipetteResult {
   pipetteOffset: LabwareOffset
+}
+
+interface MoveToLocationParams {
+  pipetteId: string
+  deckSlot: SetupPosition
+}
+
+interface MoveToLocationResult {
+  deckPoint: {
+    x: number
+    y: number
+    z: number
+  }
+  positionId: string
 }
