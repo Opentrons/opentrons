5d15e0f5e0f014674a45be00f7db9465f80909d9
Ryan howard <ryan.howard@opentrons.com>
2022-10-28 17:11:13 -0400

    fix can_mon for python3.10 (for emulation)

--- hardware/opentrons_hardware/firmware_bindings/messages/message_definitions.py
+++ hardware/opentrons_hardware/firmware_bindings/messages/message_definitions.py
@@ -34,7 +34,7 @@ class BaseMessage(object):
     def __post_init__(self) -> None:
         """Update the message index from the singleton."""
         try:
-            if self.payload.message_index == 0: # type: ignore[attr-defined]
+            if self.payload.message_index == 0:  # type: ignore[attr-defined]
                 index_generator = SingletonMessageIndexGenerator()
                 self.payload.message_index = utils.UInt32Field(  # type: ignore[attr-defined]
                     index_generator.get_next_index()
