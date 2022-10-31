006f4ca5d85cb0e040994991e14c51486ec5c651
Brian Cooper <brian@opentrons.com>
2022-10-31 13:50:23 -0400

    clean up test

--- app/src/organisms/Devices/ProtocolRun/utils/__tests__/getModuleInitialLoadInfo.test.ts
+++ app/src/organisms/Devices/ProtocolRun/utils/__tests__/getModuleInitialLoadInfo.test.ts
@@ -1,9 +1,7 @@
 import _protocolWithMagTempTC from '@opentrons/shared-data/protocol/fixtures/6/transferSettings.json'
-import _protocolWithMultipleTemps from '@opentrons/shared-data/protocol/fixtures/6/multipleTempModules.json'
-import _standardDeckDef from '@opentrons/shared-data/deck/definitions/3/ot2_standard.json'
 import { getModuleInitialLoadInfo } from '../getModuleInitialLoadInfo'
 import { ProtocolAnalysisFile } from '@opentrons/shared-data'
-import { LoadModuleRunTimeCommand } from '@opentrons/shared-data/protocol/types/schemaV6/command/setup'
+import type { LoadModuleRunTimeCommand } from '@opentrons/shared-data/protocol/types/schemaV6/command/setup'
 
 const protocolWithMagTempTC = (_protocolWithMagTempTC as unknown) as ProtocolAnalysisFile
 
@@ -12,24 +10,12 @@ describe('getModuleInitialLoadInfo', () => {
     const TC_ID: keyof typeof _protocolWithMagTempTC.modules =
       '3e039550-3412-11eb-ad93-ed232a2337cf:thermocyclerModuleType'
 
-    const LOAD_TC_COMMAND = {
-      id: '4',
-      commandType: 'loadModule',
-      params: {
-        moduleId: '3e039550-3412-11eb-ad93-ed232a2337cf:thermocyclerModuleType',
-        location: {
-          slotName: '7',
-        },
-      },
-      result: {
-        moduleId: '3e039550-3412-11eb-ad93-ed232a2337cf:thermocyclerModuleType',
-      },
-    }
-
     expect(
       getModuleInitialLoadInfo(TC_ID, protocolWithMagTempTC.commands)
     ).toEqual({
-      location: LOAD_TC_COMMAND.params.location,
+      location: {
+        slotName: '7',
+      },
       protocolLoadOrder: 2,
     })
   })
