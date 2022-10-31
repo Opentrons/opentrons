ad34585b1be24b563018239a18e075996df61492
Thassyo Pinto <thassyo.pinto@gmail.com>
2022-10-31 12:00:26 -0400

    added test setup

--- hardware-testing/hardware_testing/scripts/capacitive_probe_test.py
+++ hardware-testing/hardware_testing/scripts/capacitive_probe_test.py
@@ -9,6 +9,7 @@ from hardware_testing.opentrons_api.types import OT3Mount, OT3Axis, Point
 from hardware_testing.opentrons_api.helpers_ot3 import (
     home_ot3,
     build_async_ot3_hardware_api,
+    wait_for_stable_capacitance_ot3,
 )
 from opentrons.config.types import (
     CapacitivePassSettings,
@@ -23,13 +24,15 @@ def build_arg_parser():
 
 class Capacitive_Probe_Test:
     def __init__(self, simulate: bool, cycles: int) -> None:
+        self.api = None
+        self.mount = None
         self.simulate = simulate
         self.cycles = cycles
-        self.mount = OT3Mount.LEFT if args.mount == "l" else OT3Mount.RIGHT
         self.axes = [OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R]
+        self.PROBE_LENGTH = 34.5
         self.CUTOUT_SIZE = 20
         self.CUTOUT_HALF = self.CUTOUT_SIZE / 2
-        self.CENTER_Z = Point(x=239, y=162, z=0)
+        self.CENTER_Z = Point(x=239, y=162, z=1)
         self.CENTER_XY = Point(x=226.25, y=148, z=self.CENTER_Z.z)
         self.PROBE_SETTINGS_Z_AXIS = CapacitivePassSettings(
             prep_distance_mm=5,
@@ -51,7 +54,13 @@ class Capacitive_Probe_Test:
             "Y Back":None,
             "Y Front":None,
         }
+
+    async def test_setup(self):
         self.file_setup()
+        self.api = await build_async_ot3_hardware_api(is_simulating=self.simulate, use_defaults=True)
+        self.mount = OT3Mount.LEFT if args.mount == "l" else OT3Mount.RIGHT
+        await self.api.add_tip(self.mount, self.PROBE_LENGTH)
+        self.start_time = time.time()
 
     def file_setup(self):
         self.test_name = "capacitive_probe_test"
@@ -77,6 +86,9 @@ class Capacitive_Probe_Test:
             settings = self.PROBE_SETTINGS_Z_AXIS
         else:
             settings = self.PROBE_SETTINGS_XY_AXIS
+        await wait_for_stable_capacitance_ot3(
+            self.api, self.mount, threshold_pf=0.1, duration=1.0
+        )
         point = await self.api.capacitive_probe(
             self.mount, axis, target, settings
         )
@@ -94,13 +106,13 @@ class Capacitive_Probe_Test:
         await api.move_to(mount, above_point)
 
         # Probe deck Z-Axis height
+        # deck_z = 0.25
         deck_z = await self._probe_axis(OT3Axis.by_mount(mount), self.CENTER_Z.z)
         print(f"Deck Z-Axis height = {deck_z} mm")
         self.test_data["Z Height"] = str(round(deck_z, 3))
 
         # Move inside the cutout
         below_z = deck_z - 1
-        below_z = deck_z + 6
         current_position = await api.gantry_position(mount)
         center_xy_above = self.CENTER_XY._replace(z=current_position.z)
         center_xy_below = self.CENTER_XY._replace(z=below_z)
@@ -135,24 +147,32 @@ class Capacitive_Probe_Test:
 
         # Home Z-Axis
         current_position = await api.gantry_position(mount)
-        home_z = home_position._replace(z=current_position.z)
+        safe_z = current_position._replace(z=deck_z + 10)
+        home_z = current_position._replace(z=home_position.z)
+        await api.move_to(mount, safe_z, speed=20)
         await api.move_to(mount, home_z)
 
-        # Move to home
-        await api.move_to(mount, home_position)
+        # Move next to home
+        await api.move_to(mount, home_position + Point(x=-5, y=-5, z=0))
+
+    async def exit(self):
+        if self.api:
+            await self.api.disengage_axes(self.axes)
 
     async def run(self) -> None:
         try:
-            self.api = await build_async_ot3_hardware_api(is_simulating=self.simulate, use_defaults=True)
-            self.start_time = time.time()
+            await self.test_setup()
             for c in range(self.cycles):
                 print(f"\n--> Starting Cycle {c + 1}/{self.cycles}")
                 await self._probe_sequence(self.api, self.mount)
         except Exception as e:
+            await self.exit()
             raise e
         except KeyboardInterrupt:
+            await self.exit()
             print("Test Cancelled!")
         finally:
+            await self.exit()
             print("Test Completed!")
 
 if __name__ == '__main__':
