f791f4768e274d83a5d448fc007d57b8fad94ddc
Brian Arthur Cooper <brian@opentrons.com>
2022-11-02 18:29:28 -0400

    refactor(app): split out robot side feature flags into hidden tab (#11648)
    If the app's devtools are enabled, now not only will the app-side feature flags be revealed in their
    own "feature flags" tab within app settings, but the robot-side feature flags will be revealed in a
    similar new tab within the robot's settings page.

--- app/src/pages/Devices/RobotSettings/index.tsx
+++ app/src/pages/Devices/RobotSettings/index.tsx
@@ -18,6 +18,7 @@ import { useSelector } from 'react-redux'
 
 import { CONNECTABLE, UNREACHABLE, REACHABLE } from '../../../redux/discovery'
 import { getBuildrootSession } from '../../../redux/buildroot'
+import { getDevtoolsEnabled } from '../../../redux/config'
 import { StyledText } from '../../../atoms/text'
 import { Banner } from '../../../atoms/Banner'
 import { useRobot } from '../../../organisms/Devices/hooks'
@@ -26,6 +27,7 @@ import { NavTab } from '../../../molecules/NavTab'
 import { RobotSettingsCalibration } from '../../../organisms/RobotSettingsCalibration'
 import { RobotSettingsAdvanced } from '../../../organisms/Devices/RobotSettings/RobotSettingsAdvanced'
 import { RobotSettingsNetworking } from '../../../organisms/Devices/RobotSettings/RobotSettingsNetworking'
+import { RobotSettingsFeatureFlags } from '../../../organisms/Devices/RobotSettings/RobotSettingsFeatureFlags'
 import { ReachableBanner } from '../../../organisms/Devices/ReachableBanner'
 
 import type { NavRouteParams, RobotSettingsTab } from '../../../App/types'
@@ -66,7 +68,10 @@ export function RobotSettings(): JSX.Element | null {
         updateRobotStatus={updateRobotStatus}
       />
     ),
+    'feature-flags': <RobotSettingsFeatureFlags robotName={robotName} />,
   }
+
+  const devToolsOn = useSelector(getDevtoolsEnabled)
 
   if (
     (robot == null ||
@@ -76,7 +81,11 @@ export function RobotSettings(): JSX.Element | null {
   ) {
     return <Redirect to={`/devices/${robotName}`} />
   }
-  if (robotSettingsTab === 'calibration' && isCalibrationDisabled) {
+  const cannotViewCalibration =
+    robotSettingsTab === 'calibration' && isCalibrationDisabled
+  const cannotViewFeatureFlags =
+    robotSettingsTab === 'feature-flags' && !devToolsOn
+  if (cannotViewCalibration || cannotViewFeatureFlags) {
     return <Redirect to={`/devices/${robotName}/robot-settings/networking`} />
   }
 
@@ -135,6 +144,12 @@ export function RobotSettings(): JSX.Element | null {
               to={`/devices/${robotName}/robot-settings/advanced`}
               tabName={t('advanced')}
             />
+            {devToolsOn ? (
+              <NavTab
+                to={`/devices/${robotName}/robot-settings/feature-flags`}
+                tabName={t('feature_flags')}
+              />
+            ) : null}
           </Flex>
         </Box>
         <Line />
