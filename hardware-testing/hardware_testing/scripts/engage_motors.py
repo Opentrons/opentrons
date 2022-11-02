4669b83688140a84f7c3c72c8eab1c4910259186
David Gomez <david.gomez@opentrons.com>
2022-11-02 10:09:01 -0400

    got engage motors script working

--- hardware-testing/hardware_testing/scripts/engage_motors.py
+++ hardware-testing/hardware_testing/scripts/engage_motors.py
@@ -1,4 +1,4 @@
-"""OT3 Single Axis Movement Test."""
+"""OT3 Thermal Test."""
 import argparse
 import asyncio
 
@@ -16,17 +16,19 @@ from hardware_testing.opentrons_api.helpers_ot3 import (
 MOUNT = OT3Mount.LEFT
 LOAD = GantryLoad.NONE
 CYCLES = 1
-SPEED_X = 500
-SPEED_Z = 200
+SPEED_X = 600
+SPEED_Y = 600
+SPEED_Z = 40
 
-SPEED_Y = 500
-ACCEL_Y = 500
+ACCEL_X = 1000
+ACCEL_Y = 1000
+ACCEL_Z = 400
 
 
 SETTINGS = {
     OT3Axis.X: GantryLoadSettings(
         max_speed=SPEED_X,
-        acceleration=1000,
+        acceleration=ACCEL_X,
         max_start_stop_speed=20,
         max_change_dir_speed=5,
         hold_current=1.5,
@@ -42,20 +44,36 @@ SETTINGS = {
     ),
     OT3Axis.Z_L: GantryLoadSettings(
         max_speed=SPEED_Z,
-        acceleration=50,
-        max_start_stop_speed=1,
+        acceleration=ACCEL_Z,
+        max_start_stop_speed=20,
         max_change_dir_speed=5,
         hold_current=1.5,
         run_current=1.5,
     ),
     OT3Axis.Z_R: GantryLoadSettings(
         max_speed=SPEED_Z,
-        acceleration=400,
+        acceleration=ACCEL_Z,
         max_start_stop_speed=20,
         max_change_dir_speed=5,
         hold_current=1.5,
         run_current=1.5,
     ),
+    OT3Axis.P_L: GantryLoadSettings(
+        max_speed=SPEED_Z,
+        acceleration=ACCEL_Z,
+        max_start_stop_speed=20,
+        max_change_dir_speed=5,
+        hold_current=1.5,
+        run_current=1.5,
+    ),
+    OT3Axis.P_R: GantryLoadSettings(
+        max_speed=SPEED_Z,
+        acceleration=ACCEL_Z,
+        max_start_stop_speed=20,
+        max_change_dir_speed=5,
+        hold_current=1.5,
+        run_current=1.5,
+    )
 }
 
 AXIS_MAP = {'Y': OT3Axis.Y,
@@ -65,27 +83,6 @@ AXIS_MAP = {'Y': OT3Axis.Y,
                 'PL': OT3Axis.P_L,
                 'PR': OT3Axis.P_R}
 
-step_x = 530
-step_y = 400
-step_z = 210
-POINT_MAP = {'Y': Point(y=step_y),
-             'X': Point(x=step_x),
-             'L': Point(z=step_z),
-             'R': Point(z=step_z)}
-
-NEG_POINT_MAP = {'Y': Point(y=-step_y),
-             'X': Point(x=-step_x),
-             'L': Point(z=-step_z),
-             'R': Point(z=-step_z)}
-
-async def _single_axis_move(api: OT3API, cycles: int = 1) -> None:
-    for _ in range(cycles):
-        if(AXIS == 'R'):
-            MOUNT = MOUNT = OT3Mount.RIGHT
-        else:
-            MOUNT = MOUNT = OT3Mount.LEFT
-        await api.move_rel(mount=MOUNT, delta=NEG_POINT_MAP[AXIS], speed=AXIS_SPEED)
-        await api.move_rel(mount=MOUNT, delta=POINT_MAP[AXIS], speed=AXIS_SPEED)
 
 
 async def _main(is_simulating: bool) -> None:
@@ -94,33 +91,33 @@ async def _main(is_simulating: bool) -> None:
         await set_gantry_load_per_axis_settings_ot3(api,
                                         SETTINGS,
                                         load=None)
-        await api.home([AXIS_MAP[AXIS]])
-        await _single_axis_move(api, cycles=CYCLES)
+        await api.engage_axes([OT3Axis.X, OT3Axis.Y,
+                                  OT3Axis.Z_L, OT3Axis.Z_R,
+                                  OT3Axis.P_L, OT3Axis.P_R])
+        input("hit enter to end")
     except KeyboardInterrupt:
-        await api.disengage_axes([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
+        await api.disengage_axes([OT3Axis.X, OT3Axis.Y,
+                                  OT3Axis.Z_L, OT3Axis.Z_R,
+                                  OT3Axis.P_L, OT3Axis.P_R])
     finally:
-        # await api.disengage_axes([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
+        await api.disengage_axes([OT3Axis.X, OT3Axis.Y,
+                                  OT3Axis.Z_L, OT3Axis.Z_R,
+                                  OT3Axis.P_L, OT3Axis.P_R])
         await api.clean_up()
 
 
 if __name__ == "__main__":
-    print('2')
     parser = argparse.ArgumentParser()
     parser.add_argument("--simulate", action="store_true")
-    parser.add_argument("--axis", type=str, default='Y')
-    parser.add_argument("--cycles", type=int, default=CYCLES)
-    parser.add_argument("--speed", type=int, default=100)
-    parser.add_argument("--accel", type=int, default=50)
-    parser.add_argument("--irun", type=float, default=1.5)
-    parser.add_argument("--ihold", type=float, default=0.5)
+    parser.add_argument("--iholdgantry", type=float, default=0.5)
+    parser.add_argument("--iholdpip", type=float, default=0.5)
+
 
     args = parser.parse_args()
-    AXIS = args.axis
-    CYCLES = args.cycles
-    AXIS_SPEED = args.speed
-    SETTINGS[AXIS_MAP[AXIS]].max_speed = AXIS_SPEED
-    print()
-    SETTINGS[AXIS_MAP[AXIS]].acceleration = args.accel
-    SETTINGS[AXIS_MAP[AXIS]].run_current = args.irun
-    SETTINGS[AXIS_MAP[AXIS]].hold_current = args.ihold
+    SETTINGS[AXIS_MAP['X']].hold_current = args.iholdgantry
+    SETTINGS[AXIS_MAP['Y']].hold_current = args.iholdgantry
+    SETTINGS[AXIS_MAP['L']].hold_current = args.iholdgantry
+    SETTINGS[AXIS_MAP['R']].hold_current = args.iholdgantry
+    SETTINGS[AXIS_MAP['PL']].hold_current = args.iholdpip
+    SETTINGS[AXIS_MAP['PR']].hold_current = args.iholdpip
     asyncio.run(_main(args.simulate))
