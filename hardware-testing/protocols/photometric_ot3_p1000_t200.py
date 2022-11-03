cbae2f2c44c15b3046f6a47d7912b74f4cff9d99
andySigler <andrewsigler1@gmail.com>
2022-11-03 15:15:29 -0400

    switch to left mount

--- hardware-testing/protocols/photometric_ot3_p1000_t200.py
+++ hardware-testing/protocols/photometric_ot3_p1000_t200.py
@@ -9,7 +9,7 @@ def run(ctx: ProtocolContext) -> None:
     tiprack_200 = ctx.load_labware("opentrons_ot3_96_tiprack_200uL", "11")
     plate = ctx.load_labware("corning_96_wellplate_360ul_flat", "9")
     trough = ctx.load_labware("nest_12_reservoir_15ml", "8")
-    pipette = ctx.load_instrument("p1000_multi_gen3", "right", tip_racks=[tiprack_200])
+    pipette = ctx.load_instrument("p1000_multi_gen3", "left", tip_racks=[tiprack_200])
 
     row = 1
     wells = [plate[f"{col}{row}"] for col in 'ABCDEFGH']
