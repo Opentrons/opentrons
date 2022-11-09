d4a96001d714c3e36ccf30a50fd19994574bb7bd
Josh McVey <josh.mcvey@opentrons.com>
2022-11-09 16:17:28 -0600

    use cygpath

--- scripts/python.mk
+++ scripts/python.mk
@@ -2,10 +2,15 @@
 # Python executable to use to run pipenv. Note: pipenv will not
 # necessary select this Python to create its virtual environments.
 OT_PYTHON ?= python
-export MSYS_NO_PATHCONV=1
+
 _firstpath := $(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 _possibilities := $(realpath $(_firstpath)/..) $(realpath $(_firstpath)/../..) $(realpath $(_firstpath)/../../..) $(_firstpath)
 monorepo_root := $(firstword $(filter %/opentrons, $(_possibilities)))
+
+ifeq ($(monorepo_root),)
+monorepo_root := $(firstword $(filter %\opentrons, $(_possibilities)))
+monorepo_root :=$(shell cygpath -u $(monorepo_root))
+endif
 
 $(info _possibilities $(_possibilities))
 $(info _firstpath $(_firstpath))
