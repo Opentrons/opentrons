86bf2b0d616fc2bf305b1411daa0a779f6333102
Mehdi Zaidi <55298601+meh-di@users.noreply.github.com>
2022-11-10 17:22:52 -0500

    Update linear accuracy script

--- hardware-testing/hardware_testing/scripts/linear_acc_test.py
+++ hardware-testing/hardware_testing/scripts/linear_acc_test.py
@@ -4,6 +4,7 @@ import asyncio
 import os, time, random
 
 from opentrons.hardware_control.ot3api import OT3API
+from opentrons.hardware_control.types import Axis
 
 from hardware_testing import data
 from hardware_testing.opentrons_api.types import GantryLoad, OT3Mount, OT3Axis, Point
@@ -67,54 +68,85 @@ async def _main(is_simulating: bool) -> None:
     test_name = "linear-acc"
     file_name = data.create_file_name(test_name=test_name, run_id=data.create_run_id(), tag=test_tag)
 
-    await home_ot3(api)
-    await api.move_rel(mount=MOUNT, delta=Point(y=-60), speed=200)
+    await home_ot3(api, [OT3Axis.X])
+    # await api.move_rel(mount=MOUNT, delta=Point(y=-60), speed=200)
     await api.disengage_axes([OT3Axis.Y])
+
+    input("Set Y-Axis\n\n\t>> Continue...")
+    await api.engage_axes([OT3Axis.Y])
 
     print(f"Set digital scale parallel to X-Axis")
     input("\n\t>> Continue...")
 
-    await api.engage_axes([OT3Axis.Y])
+    # await api.engage_axes([OT3Axis.Y])
     starting_read_pos = gauge.read()
 
-    print(f"Set digital scale to 0\n\t>>Current position: {starting_read_pos}")
+    print(f"Attach and set digital scale to 0\n\t>> Current position: {starting_read_pos}")
+    await api.disengage_axes([OT3Axis.Y])
     input("\n\t>> Continue...")
+    await api.engage_axes([OT3Axis.Y])
 
     init_reading = gauge.read()
     print(f"Initial gauge read {test_tag}: {init_reading} mm\n")
     init_pos = 'Initial Position'
     test_axis = test_tag
 
+    init_encoder_pos = await api.encoder_current_position(MOUNT)
+    # for key in init_encoder_pos:
+    #     print(key)
+    #     print(init_encoder_pos[key])
+    # keys = init_encoder_pos.keys()
+    # print(keys)
+    init_encoder_pos = init_encoder_pos[Axis.X]
+    # init_encoder_pos = init_encoder_pos["<Axis.X>"]
+
     if test_axis == "Z":
         test_speed = SPEED_Z
     else:
-        test_speed = SPEED_XY
+        test_speed = 400 # SPEED_XY
 
     input("Press enter to begin test...\n")
 
-    header = ['Cycle', 'Test Axis', 'Position Read (mm)', 'Distance Moved (mm)' 'Speed (mm/s)']
+    header = ['Cycle', 'Test Axis', 'Start Position (mm)', 'Position Read (mm)', 'Distance Moved (mm)', 'Encoder position (mm)','Speed (mm/s)']
     header_str = data.convert_list_to_csv_line(header)
     data.append_data_to_file(test_name=test_name, file_name=file_name, data=header_str)
 
-    init_reading_data = ['0', test_axis, init_reading, init_pos, test_speed]
+    init_reading_data = ['0', test_axis, init_reading, '', init_pos, init_encoder_pos, test_speed]
     init_reading_str = data.convert_list_to_csv_line(init_reading_data)
     data.append_data_to_file(test_name=test_name, file_name=file_name, data=init_reading_str)
 
-    distances = [440*0.25, 440*0.5, 440*0.75, 440]
-
+    # distances = [440*0.25, 440*0.5, 440*0.75, 440]
+    cur_pos = await api.current_position(MOUNT)
+    # print(cur_pos.keys())
+    distances = [cur_pos[Axis.X]*0.25, cur_pos[Axis.X]*0.25, cur_pos[Axis.X]*0.25, cur_pos[Axis.X]*0.25]
+    count = 0
     for cycle in range(CYCLES):
         print(f"Cycle: {cycle+1} out of {CYCLES}")
         # coordinates = await random_move(api)
+        time.sleep(2)
+        start_pos = gauge.read()
+        print(f"\tStart position:\n\t {start_pos} mm")
         for distance in distances:
+            count += 1
+            # if count == 1:
+            #     time.sleep(2)
+            #     start_pos = gauge.read()
             await api.move_rel(mount=MOUNT, delta=Point(x=-distance), speed=400)
             time.sleep(2)
-            pos_reading = gauge.read() - init_reading
+            pos_reading = gauge.read() - start_pos
             print(f"\tCurrent position reading:\n\t {pos_reading} mm")
-            await home_ot3(api, [OT3Axis.X])
+            encoder_pos = await api.encoder_current_position(MOUNT)
+            encoder_pos = init_encoder_pos - encoder_pos[Axis.X]
+            print(f"\tCurrent encoder position reading:\n\t {encoder_pos} mm")
+            # await home_ot3(api, [OT3Axis.X])
             time.sleep(2)
-            cycle_data = [cycle+1, '', pos_reading, distance, test_speed]
+            # if count > 1:
+            #     start_pos = ''
+            cycle_data = [cycle+1, '', start_pos, pos_reading, distance*count, encoder_pos, test_speed]
             cycle_data_str = data.convert_list_to_csv_line(cycle_data)
             data.append_data_to_file(test_name=test_name, file_name=file_name, data=cycle_data_str)
+        await home_ot3(api, [OT3Axis.X])
+        count = 0
 
     await api.disengage_axes([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
 
