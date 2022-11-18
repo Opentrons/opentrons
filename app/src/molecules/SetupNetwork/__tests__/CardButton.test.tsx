5cea5c7c48976511da7c17bd74b49a4ea9619372
koji <koji.kanao@opentrons.com>
2022-11-18 18:56:49 -0500

    rename test file and fix check-js error

--- app/src/molecules/SetupNetwork/__tests__/CardButton.test.tsx
+++ app/src/molecules/SetupNetwork/__tests__/CardButton.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { fireEvent, getByTestId } from '@testing-library/react'
+import { fireEvent } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { renderWithProviders } from '@opentrons/components'
 import { i18n } from '../../../i18n'
