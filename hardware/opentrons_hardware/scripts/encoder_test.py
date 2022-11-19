d7c833e68d341827937526a56a3773bf339bd33f
Carlos <fernandez.carlos40@gmail.com>
2022-11-19 15:37:44 -0500

    grab encoder values without move group

--- hardware/opentrons_hardware/scripts/encoder_test.py
+++ hardware/opentrons_hardware/scripts/encoder_test.py
@@ -2,14 +2,25 @@
 import argparse
 import asyncio
 import logging
+import dataclasses
 from numpy import float64
+import numpy as np
 from logging.config import dictConfig
+from enum import Enum, unique
+from typing import Dict, Set, Callable
 
-from opentrons_hardware.drivers.can_bus import build, CanMessenger
-from opentrons_hardware.firmware_bindings.constants import NodeId
+from opentrons_hardware.drivers.can_bus import build, CanMessenger, WaitableCallback
+from opentrons_hardware.firmware_bindings import ArbitrationId
+from opentrons_hardware.firmware_bindings.constants import MessageId
+from opentrons_hardware.firmware_bindings.messages import MessageDefinition
 
 from opentrons_hardware.firmware_bindings.messages.message_definitions import (
     EncoderPositionRequest,
+)
+
+from opentrons_hardware.firmware_bindings.constants import (
+    MessageId,
+    NodeId,
 )
 
 from opentrons_hardware.hardware_control.move_group_runner import MoveGroupRunner
@@ -17,60 +28,82 @@ from opentrons_hardware.scripts.can_args import add_can_args, build_settings
 
 from opentrons_hardware.drivers.gpio import OT3GPIO
 
-log = logging.getLogger(__name__)
 
-LOG_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "basic": {"format": "%(asctime)s %(name)s %(levelname)s %(message)s"}
-    },
-    "handlers": {
-        "stream_handler": {
-            "class": "logging.StreamHandler",
-            "formatter": "basic",
-            "level": logging.DEBUG,
-        },
-    },
-    "loggers": {
-        "": {
-            "handlers": ["stream_handler"],
-            "level": logging.DEBUG,
-        },
-    },
-}
+class EncoderPosition(int, Enum):
+    encoder_position_um: np.int32
 
+def _create_listener(
+    nodes: Set[NodeId], event: asyncio.Event, responses: Dict[NodeId, int]
+) -> Callable[[MessageDefinition, ArbitrationId], None]:
+    def _listener(message: MessageDefinition, arbitration_id: ArbitrationId) -> None:
+        try:
+            originator = NodeId(arbitration_id.parts.originating_node_id)
+        except ValueError:
+            log.warning(
+                "unknown node id on network: "
+                f"0x{arbitration_id.parts.originating_node_id:x}"
+            )
+            return
+        if originator not in nodes:
+            log.error(
+                "got response from unexpected node id on network: "
+                f"0x{arbitration_id.parts.originating_node_id:x}"
+            )
+            return
+        # if message.message_id != MessageId.limit_sw_response:
+        #     log.warning(f"unexpected message id: 0x{message.message_id:x}")
+        #     return
+        responses[originator] = message.payload.encoder_position
+        if len(responses) == len(nodes):
+            event.set()
 
-async def run_move(messenger: CanMessenger) -> None:
-    """Run the move."""
-    # await messenger.send(node_id=NodeId.broadcast, message=EnableMotorRequest())
-    samples = 1
+    return _listener
+
+async def _get_encoder_position(messenger: CanMessenger):
+    """This function allows us to obtain encoder position from all axes"""
     try:
-        for x in range(1, samples+1):
-            print(await messenger.send(node_id = NodeId.broadcast, message=EncoderPositionRequest()))
+        nodes = [
+            NodeId.gantry_x,
+            NodeId.gantry_y,
+            NodeId.head_l,
+            NodeId.head_r
+        ]
+        event = asyncio.Event()
+        responses: Dict[NodeId, int] = dict()
+        listener = _create_listener(nodes, event, responses)
+        messenger.add_listener(listener)
+
+        await messenger.send(node_id = NodeId.broadcast, message=EncoderPositionRequest())
+        for node in nodes:
+            await messenger.send(
+                node_id=node,
+                message=EncoderPositionRequest(),
+            )
+        await asyncio.wait_for(event.wait(), 1.0)
+        print(responses)
+        for n, r in responses.items():
+            print(f"{n}: {r.value/1000}")
+        messenger.remove_listener(listener)
     except asyncio.CancelledError:
         pass
 
-
-async def run(args: argparse.Namespace) -> None:
+async def get_encoder(args: argparse.Namespace) -> None:
     """Entry point for script."""
     async with build.can_messenger(build_settings(args)) as messenger:
         # build a GPIO handler, which will automatically release estop
-        gpio = OT3GPIO(__name__)
-        gpio.deactivate_estop()
-        await run_move(messenger)
+        # gpio = OT3GPIO(__name__)
+        # gpio.deactivate_estop()
+        await _get_encoder_position(messenger)
 
 
 def main() -> None:
     """Entry point."""
-    dictConfig(LOG_CONFIG)
-
     parser = argparse.ArgumentParser(description="CAN bus move.")
     add_can_args(parser)
 
     args = parser.parse_args()
 
-    asyncio.run(run(args))
+    asyncio.run(get_encoder(args))
 
 
 if __name__ == "__main__":
