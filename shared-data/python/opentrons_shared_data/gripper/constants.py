f8497a9311c14390c39bc940f17b25164ab1d31b
Ryan howard <ryan.howard@opentrons.com>
2022-11-22 12:07:58 -0500

    fix(gripper-def): add extra offsets for dropoff to assist with placement

--- shared-data/python/opentrons_shared_data/gripper/constants.py
+++ shared-data/python/opentrons_shared_data/gripper/constants.py
@@ -1,4 +1,7 @@
 """Gripper constants and default values."""
+from .dev_types import GripperOffset
 
 LABWARE_GRIP_FORCE: float = 20  # Newtons
 IDLE_STATE_GRIP_FORCE: float = 10  # Newtons
+GRIPPER_DECK_DROP_OFFSET: GripperOffset = GripperOffset(-0.5, -0.5, -1.5)
+GRIPPER_MODULE_DROP_OFFSET: GripperOffset = GripperOffset(0.0, 0.0, 1.5)
