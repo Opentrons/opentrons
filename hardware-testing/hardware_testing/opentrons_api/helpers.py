76c54035453e558cc348748ad56eb42f4de19234
Andy Sigler <andrewsigler1@gmail.com>
2022-11-22 10:14:23 -0500

    feat(hardware-testing): Add first multi channel photometric protocols (#11766)
    * adds more protocols for photometric testing an OT3
    
    * some quick testing on a bot
    
    * remove setting flowrate, use default
    
    * wip: adding OT3 protocol execution to run protocols from cli
    
    * linting

--- hardware-testing/hardware_testing/opentrons_api/helpers.py
+++ hardware-testing/hardware_testing/opentrons_api/helpers.py
@@ -1,10 +1,11 @@
 """Opentrons helper methods."""
 from types import MethodType
-from typing import Any, List, Dict
+from typing import Any, List, Dict, cast, Optional
 
 from opentrons import protocol_api, execute, simulate
 from opentrons.protocol_api.labware import Well
 from opentrons.hardware_control.thread_manager import ThreadManagerException
+from opentrons.hardware_control.types import MachineType
 
 from .workarounds import is_running_in_app, store_robot_acceleration
 from .types import Axis, Point
@@ -35,29 +36,34 @@ def _add_fake_comment_pause(
 
 
 def get_api_context(
-    api_level: str, is_simulating: bool = False, connect_to_smoothie: bool = True
+    api_level: str,
+    is_simulating: bool = False,
+    connect_to_hardware: bool = True,
+    machine: Optional[str] = None,
 ) -> protocol_api.ProtocolContext:
     """Create an Opentrons API ProtocolContext instance."""
+    checked_machine = cast(MachineType, machine if machine else "ot3")
     able_to_execute = False
     ctx = None
-    if not is_simulating and connect_to_smoothie:
+    if not is_simulating and connect_to_hardware:
         try:
-            ctx = execute.get_protocol_api(api_level)
+            ctx = execute.get_protocol_api(api_level, machine=checked_machine)
             able_to_execute = True
         except ThreadManagerException:
             # Unable to build non-simulated Protocol Context
             # Probably be running on a non-Linux machine
             # Creating simulated Protocol Context, with .is_simulated() overridden
             pass
-    if not able_to_execute or is_simulating or not connect_to_smoothie:
-        ctx = simulate.get_protocol_api(api_level)
+    if not able_to_execute or is_simulating or not connect_to_hardware:
+        ctx = simulate.get_protocol_api(api_level, machine=checked_machine)
     assert ctx
-    if not able_to_execute or not connect_to_smoothie:
+    if not able_to_execute or not connect_to_hardware:
         _add_fake_simulate(ctx, is_simulating)
     if not is_running_in_app():
         _add_fake_comment_pause(ctx)
-    # NOTE: goshdarnit, all robots will have slower acceleration
-    store_robot_acceleration()
+    if checked_machine == "ot2":
+        # NOTE: goshdarnit, all OT2s should have slower acceleration
+        store_robot_acceleration()
     return ctx
 
 
