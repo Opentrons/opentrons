8ce7fe792ae9e89ad361bea9cd4bd0cdfaf71888
Ryan howard <ryan.howard@opentrons.com>
2022-11-28 12:26:36 -0500

    hook up ensure-send to current settings and current settings test

--- hardware/opentrons_hardware/hardware_control/current_settings.py
+++ hardware/opentrons_hardware/hardware_control/current_settings.py
@@ -1,17 +1,20 @@
 """Utilities for updating the current settings on the OT3."""
 from typing import Tuple
-
+import logging
 from opentrons_hardware.drivers.can_bus.can_messenger import CanMessenger
 from opentrons_hardware.firmware_bindings.messages import payloads
 from opentrons_hardware.firmware_bindings.messages.message_definitions import (
     WriteMotorCurrentRequest,
 )
+from opentrons_hardware.firmware_bindings.constants import ErrorCode
 from opentrons_hardware.firmware_bindings.utils import UInt32Field
 from .types import NodeMap
 
 
 CompleteCurrentSettings = NodeMap[Tuple[float, float]]
 PartialCurrentSettings = NodeMap[float]
+
+log = logging.getLogger(__name__)
 
 
 async def set_currents(
@@ -20,7 +23,7 @@ async def set_currents(
 ) -> None:
     """Set hold current and run current for each node."""
     for node, currents in current_settings.items():
-        await can_messenger.send(
+        error = await can_messenger.ensure_send(
             node_id=node,
             message=WriteMotorCurrentRequest(
                 payload=payloads.MotorCurrentPayload(
@@ -28,7 +31,12 @@ async def set_currents(
                     run_current=UInt32Field(int(currents[1] * (2**16))),
                 )
             ),
+            expected_nodes=[node],
         )
+        if error != ErrorCode.ok:
+            log.error(
+                f"recieved error {str(error)} trying to set currents for {str(node)}"
+            )
 
 
 async def set_run_current(
@@ -36,7 +44,7 @@ async def set_run_current(
 ) -> None:
     """Set only the run current for each node."""
     for node, current in current_settings.items():
-        await can_messenger.send(
+        error = await can_messenger.ensure_send(
             node_id=node,
             message=WriteMotorCurrentRequest(
                 payload=payloads.MotorCurrentPayload(
@@ -44,7 +52,12 @@ async def set_run_current(
                     run_current=UInt32Field(int(current * (2**16))),
                 )
             ),
+            expected_nodes=[node],
         )
+        if error != ErrorCode.ok:
+            log.error(
+                f"recieved error {str(error)} trying to set run current for {str(node)}"
+            )
 
 
 async def set_hold_current(
@@ -52,7 +65,7 @@ async def set_hold_current(
 ) -> None:
     """Set only the hold current for each node."""
     for node, current in current_settings.items():
-        await can_messenger.send(
+        error = await can_messenger.ensure_send(
             node_id=node,
             message=WriteMotorCurrentRequest(
                 payload=payloads.MotorCurrentPayload(
@@ -60,4 +73,9 @@ async def set_hold_current(
                     run_current=UInt32Field(0),
                 )
             ),
+            expected_nodes=[node],
         )
+        if error != ErrorCode.ok:
+            log.error(
+                f"recieved error {str(error)} trying to set run current for {str(node)}"
+            )
