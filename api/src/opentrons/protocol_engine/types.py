531d0328e08b0182ed36b2403e92257a4a04ab37
TamarZanzouri <zanzouritamar@gmail.com>
2022-11-29 13:46:47 -0500

    Update api/src/opentrons/protocol_engine/types.py
    Co-authored-by: Mike Cousins <mike@opentrons.com>

--- api/src/opentrons/protocol_engine/types.py
+++ api/src/opentrons/protocol_engine/types.py
@@ -9,7 +9,7 @@ from typing import Optional, Union, List, Dict, Any, NamedTuple
 from typing_extensions import Literal, TypeGuard
 
 from opentrons_shared_data.pipette.dev_types import PipetteNameType
-from opentrons.types import MountType, DeckSlotName, Point
+from opentrons.types import MountType, DeckSlotName
 from opentrons.hardware_control.modules import ModuleType as ModuleType
 from opentrons.hardware_control.types import CriticalPoint
 
