727c8be5382e6c38c59e17241d0d3103c931eb93
Max Marrone <max@marrone.nyc>
2022-12-01 18:16:50 -0500

    Patch up test_util.py.

--- api/tests/opentrons/protocols/api_support/test_util.py
+++ api/tests/opentrons/protocols/api_support/test_util.py
@@ -1,5 +1,7 @@
 import pytest
 
+from opentrons_shared_data.deck import DefinitionName as DeckDefinitionName
+
 from opentrons.protocol_api.core.protocol_api.labware import LabwareImplementation
 from opentrons.types import Point, Location, Mount
 from opentrons.protocol_api import MAX_SUPPORTED_VERSION
@@ -62,7 +64,7 @@ def test_build_edges():
         api_version=MAX_SUPPORTED_VERSION,
     )
     off = Point(0, 0, 1.0)
-    deck = Deck()
+    deck = Deck(deck_type=DeckDefinitionName.OT2_STANDARD)
     old_correct_edges = [
         test_lw["A1"].from_center_cartesian(x=1.0, y=0, z=1) + off,
         test_lw["A1"].from_center_cartesian(x=-1.0, y=0, z=1) + off,
