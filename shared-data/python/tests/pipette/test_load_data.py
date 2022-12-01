4343d2c2a4435a100e4b724e6df1720d4d4b6629
Laura Cox <laura@opentrons.com>
2022-12-01 14:15:57 -0500

    make load data test attribute specific

--- shared-data/python/tests/pipette/test_load_data.py
+++ shared-data/python/tests/pipette/test_load_data.py
@@ -1,5 +1,5 @@
 import json
-from opentrons_shared_data.pipette import load_data, types, pipette_definition
+from opentrons_shared_data.pipette import load_data, types
 
 from opentrons_shared_data import load_shared_data
 
@@ -10,24 +10,23 @@ def test_load_pipette_definition() -> None:
         types.PipetteChannelType.SINGLE_CHANNEL,
         types.PipetteVersionType(major=1, minor=0),
     )
-    combined_dict = json.loads(
+    liquid = json.loads(
         load_shared_data("pipette/definitions/2/liquid/single_channel/p50/1_0.json")
     )
-    combined_dict.update(
-        json.loads(
-            load_shared_data(
-                "pipette/definitions/2/geometry/single_channel/p50/1_0.json"
-            )
-        )
+    geometry = json.loads(
+        load_shared_data("pipette/definitions/2/geometry/single_channel/p50/1_0.json")
     )
-    combined_dict.update(
-        json.loads(
-            load_shared_data(
-                "pipette/definitions/2/general/single_channel/p50/1_0.json"
-            )
-        )
+    general = json.loads(
+        load_shared_data("pipette/definitions/2/general/single_channel/p50/1_0.json")
     )
-    converted_to_pydantic = pipette_definition.PipetteConfigurations.parse_obj(
-        combined_dict
+
+    assert pipette_config.channels.as_int == general["channels"]
+    assert pipette_config.pipette_type.value == general["model"]
+    assert pipette_config.nozzle_offset == geometry["nozzleOffset"]
+
+    assert (
+        pipette_config.supported_tips[
+            types.PipetteTipType.t50
+        ].default_aspirate_flowrate
+        == liquid["supportedTips"]["t50"]["defaultAspirateFlowRate"]
     )
-    assert pipette_config.dict() == converted_to_pydantic.dict()
