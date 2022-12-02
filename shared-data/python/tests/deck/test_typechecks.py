e5b6cab010ee611aa8327f26ca3903e56d3d5c45
Max Marrone <max@marrone.nyc>
2022-12-02 16:23:20 -0500

    Fix up deck type-checking tests in shared-data.

--- shared-data/python/tests/deck/test_typechecks.py
+++ shared-data/python/tests/deck/test_typechecks.py
@@ -3,11 +3,8 @@ import sys
 import pytest
 import typeguard
 
-from opentrons_shared_data.deck import load as load_deck_definition
+from opentrons_shared_data.deck import DefinitionName, load
 from opentrons_shared_data.deck.dev_types import DeckDefinitionV3
-
-from . import list_deck_def_paths
-
 
 pytestmark = pytest.mark.xfail(
     condition=sys.version_info >= (3, 10),
@@ -15,7 +12,11 @@ pytestmark = pytest.mark.xfail(
 )
 
 
-@pytest.mark.parametrize("defname", list_deck_def_paths(version=3))
-def test_v3_defs(defname):
-    defn = load_deck_definition(name=defname, version=3)
-    typeguard.check_type("defn", defn, DeckDefinitionV3)
+@pytest.mark.parametrize(
+    # Parametrize over all values of the DefinitionName enum.
+    "definition_name",
+    DefinitionName,
+)
+def test_v3_defs(definition_name: DefinitionName) -> None:
+    definition = load(name=definition_name, version=3)
+    typeguard.check_type("definition", definition, DeckDefinitionV3)
