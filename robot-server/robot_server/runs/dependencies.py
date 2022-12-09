4a8ae3b00955d54f9dbb434bd1e5179d06917c4a
Max Marrone <max@opentrons.com>
2022-12-09 14:51:42 -0500

    refactor(api): Add a `robot_type` parameter to ProtocolEngine Config (#11850)

--- robot-server/robot_server/runs/dependencies.py
+++ robot-server/robot_server/runs/dependencies.py
@@ -2,10 +2,12 @@
 from fastapi import Depends
 from sqlalchemy.engine import Engine as SQLEngine
 
+from opentrons_shared_data.robot.dev_types import RobotType
+
 from opentrons.hardware_control import HardwareControlAPI
 
 from robot_server.app_state import AppState, AppStateAccessor, get_app_state
-from robot_server.hardware import get_hardware
+from robot_server.hardware import get_hardware, get_robot_type
 from robot_server.persistence import get_sql_engine
 from robot_server.service.task_runner import get_task_runner, TaskRunner
 from robot_server.deletion_planner import RunDeletionPlanner
@@ -36,12 +38,13 @@ async def get_run_store(
 async def get_engine_store(
     app_state: AppState = Depends(get_app_state),
     hardware_api: HardwareControlAPI = Depends(get_hardware),
+    robot_type: RobotType = Depends(get_robot_type),
 ) -> EngineStore:
     """Get a singleton EngineStore to keep track of created engines / runners."""
     engine_store = _engine_store_accessor.get_from(app_state)
 
     if engine_store is None:
-        engine_store = EngineStore(hardware_api=hardware_api)
+        engine_store = EngineStore(hardware_api=hardware_api, robot_type=robot_type)
         _engine_store_accessor.set_on(app_state, engine_store)
 
     return engine_store
