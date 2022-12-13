a7fe893577a9b164e87cfccd581f9049cbac5384
andySigler <andrewsigler1@gmail.com>
2022-12-13 13:02:59 -0500

    make sure no cases exist where probe misses edge

--- api/src/opentrons/config/defaults_ot3.py
+++ api/src/opentrons/config/defaults_ot3.py
@@ -22,18 +22,18 @@ DEFAULT_CALIBRATION_SETTINGS: Final[OT3CalibrationSettings] = OT3CalibrationSett
     z_offset=ZSenseSettings(
         pass_settings=CapacitivePassSettings(
             prep_distance_mm=3,
-            max_overrun_distance_mm=3,
+            max_overrun_distance_mm=2,
             speed_mm_per_s=1,
-            sensor_threshold_pf=1.0,
+            sensor_threshold_pf=0.5,
         ),
     ),
     edge_sense=EdgeSenseSettings(
-        overrun_tolerance_mm=0.2,
-        early_sense_tolerance_mm=0.2,
+        overrun_tolerance_mm=0.1,
+        early_sense_tolerance_mm=0.1,
         pass_settings=CapacitivePassSettings(
-            prep_distance_mm=0.5,
-            max_overrun_distance_mm=0.5,
-            speed_mm_per_s=2,
+            prep_distance_mm=0.2,
+            max_overrun_distance_mm=0.2,
+            speed_mm_per_s=1,
             sensor_threshold_pf=0.5,
         ),
         search_initial_tolerance_mm=5.0,
