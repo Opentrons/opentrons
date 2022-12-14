3f7d0a5b460df81a16b55f5f0717742e23a9348e
Brian Arthur Cooper <brian@opentrons.com>
2022-12-14 11:17:35 -0500

    Update api/tests/opentrons/commands/test_command_schema_snapshot.py
    Co-authored-by: Max Marrone <max@opentrons.com>

--- api/tests/opentrons/commands/test_command_schema_snapshot.py
+++ api/tests/opentrons/commands/test_command_schema_snapshot.py
@@ -4,10 +4,24 @@ from opentrons_shared_data.command.generate_command_schema import (
 )
 
 
+SCHEMA_CHANGED_MESSAGE = """\
+The Python models for Protocol Engine commands do not match the shared JSON schema.
+
+If this change is accidental, undo the changes to our Python models.
+
+Or, if this change is intentional, update the shared JSON schema by running:
+
+    make -C shared-data/python command-schema COMMAND_SCHEMA_VERSION=<version number>
+
+...and include the updated JSON schema file in your pull request.
+"""
+
+
 def test_command_schema_has_not_changed() -> None:
-    """Confirm that the command schema has not been changed since it was last generated. If this test fails you may want to run `make command_schema version=X` in 'shared-data/python'"""
+    """Confirm that the command schema has not changed."""
     newest_version = get_newest_schema_version()
     existing_schema_string = load_schema_string(newest_version)
     generated_schema_string = generate_command_schema(newest_version)
 
-    assert existing_schema_string == generated_schema_string
+    assert generated_schema_string == existing_schema_string, SCHEMA_CHANGED_MESSAGE
+
