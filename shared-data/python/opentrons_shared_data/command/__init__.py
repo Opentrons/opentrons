115d3c3be381729757d608d73a6165ba12a2464a
Brian Cooper <brian@opentrons.com>
2022-12-15 14:56:27 -0500

    shared-data lint

--- shared-data/python/opentrons_shared_data/command/__init__.py
+++ shared-data/python/opentrons_shared_data/command/__init__.py
@@ -1,4 +1,4 @@
-""" opentrons_shared_data.command: functions command schemas """
+"""opentrons_shared_data.command: functions command schemas."""
 from pathlib import Path
 import json
 import os
@@ -8,6 +8,7 @@ from ..load import load_shared_data, get_shared_data_root
 
 
 def get_newest_schema_version() -> str:
+    """Get the version string of the most modern command schema currently in shared-data."""
     command_schemas_dir = get_shared_data_root() / "command" / "schemas"
     command_schemas = os.listdir(command_schemas_dir)
     all_schema_versions = []
@@ -20,5 +21,6 @@ def get_newest_schema_version() -> str:
 
 
 def load_schema_string(version: str) -> str:
+    """Get the string containing the command JSON schema for the given version string."""
     path = Path("command") / "schemas" / f"{version}.json"
     return json.dumps(json.loads(load_shared_data(path)), indent=2)
