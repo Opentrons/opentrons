2a964e90babf933056a8948742e5b06c11c5d6c2
Max Marrone <max@opentrons.com>
2022-12-14 20:32:14 -0500

    Implement extract_labware_definitions.

---
+++ api/src/opentrons/protocol_reader/_extract_labware_definitions.py
@@ -0,0 +1,51 @@
+import json
+from pathlib import Path
+from typing import AsyncIterable, List
+
+import anyio
+
+from opentrons.protocols.models import LabwareDefinition
+
+from .protocol_source import ProtocolFileRole, ProtocolSource, ProtocolType
+
+
+async def extract_labware_definitions(
+    protocol_source: ProtocolSource,
+) -> AsyncIterable[LabwareDefinition]:
+    """Extract all the labware definitions that are in the given protocol source.
+
+    This accounts for differences between JSON protocols,
+    which embed their labware definitions in the main protocol file,
+    and Python protocols, which have them in separate sidecar files.
+    """
+    if protocol_source.config.protocol_type == ProtocolType.JSON:
+        for labware in await _extract_from_json_protocol_file(
+            path=protocol_source.main_file
+        ):
+            yield labware
+        # Ignore separate labware files.
+
+    else:
+        assert protocol_source.config.protocol_type == ProtocolType.PYTHON
+        for file in protocol_source.files:
+            if file.role == ProtocolFileRole.LABWARE:
+                yield await _extract_from_labware_file(file.path)
+
+
+async def _extract_from_labware_file(path: Path) -> LabwareDefinition:
+    return await anyio.to_thread.run_sync(LabwareDefinition.parse_file, path)
+
+
+async def _extract_from_json_protocol_file(path: Path) -> List[LabwareDefinition]:
+    def extract_sync(path: Path) -> List[LabwareDefinition]:
+        with path.open("rb") as file:
+            json_contents = json.load(file)
+            # Rely on the file conforming to one of our JSON protocol schemas 3 to 7,
+            # which require this labwareDefinitions key.
+            unvalidated_definitions = json_contents["labwareDefinitions"]
+            validated_definitions = [
+                LabwareDefinition.parse_obj(u) for u in unvalidated_definitions
+            ]
+            return validated_definitions
+
+    return await anyio.to_thread.run_sync(extract_sync, path)
