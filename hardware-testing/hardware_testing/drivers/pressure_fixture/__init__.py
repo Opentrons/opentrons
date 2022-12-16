d7861e054a94b5cf6c0f53a90919070503ae028f
Carlos <fernandez.carlos40@gmail.com>
2022-12-16 16:26:38 -0500

    init file for pressure fixture

---
+++ hardware-testing/hardware_testing/drivers/pressure_fixture/__init__.py
@@ -0,0 +1,12 @@
+"""Mark 10 drivers."""
+from .ot3_pressure_fixture import (
+    Ot3PressureFixture,
+    Ot3PressureFixtureBase,
+    SimOt3PressureFixture
+    )
+
+from .types import PressureChannels
+
+__all__ = ["Ot3PressureFixture",
+            "Ot3PressureFixtureBase",
+            "SimOt3PressureFixture"]
