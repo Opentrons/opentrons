732aeee22d0fb408ea6d588e6df3cec812005b37
Max Marrone <max@opentrons.com>
2022-12-21 19:18:00 -0500

    Bring back RoleAnalyzer and update tests.

---
+++ api/src/opentrons/protocol_reader/role_analyzer.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+from typing import Union, List, Sequence
+
+from .basic_info_extractor import (
+    FileInfo,
+    JsonProtocolFileInfo,
+    PythonProtocolFileInfo,
+    LabwareDefinitionFileInfo,
+)
+
+
+# FIX BEFORE MERGE: Rename exception.
+class RoleAnalysisError(ValueError):
+    pass
+
+
+@dataclass(frozen=True)
+class RoleAnalysis:
+    main_file: Union[JsonProtocolFileInfo, PythonProtocolFileInfo]
+    labware_files: List[LabwareDefinitionFileInfo]
+    # todo(mm, 2022-12-19): Add data files like .txt and .csv.
+
+    @property
+    def all_files(self) -> List[FileInfo]:
+        return [self.main_file, *self.labware_files]
+
+
+class RoleAnalyzer:
+    @staticmethod
+    def analyze(files: Sequence[FileInfo]) -> RoleAnalysis:
+        """Analyze a set of input files to determine each of their roles."""
+        if len(files) == 0:
+            raise RoleAnalysisError("No files were provided.")
+
+        main_file_candidates = []
+        labware_files = []
+
+        for f in files:
+            if isinstance(f, (JsonProtocolFileInfo, PythonProtocolFileInfo)):
+                main_file_candidates.append(f)
+            else:
+                # todo(mm, 2022-12-21): Support data files like .txt and .csv.
+                assert isinstance(f, LabwareDefinitionFileInfo)
+                labware_files.append(f)
+
+        if len(main_file_candidates) == 0:
+            if len(files) == 1:
+                raise RoleAnalysisError(
+                    f'"{files[0].original_file.name}" is not a valid protocol file.'
+                )
+            else:
+                file_list = ", ".join(f'"{f.original_file.name}"' for f in files)
+                raise RoleAnalysisError(f"No valid protocol file found in {file_list}.")
+        elif len(main_file_candidates) > 1:
+            file_list = ", ".join(f'"{f.original_file.name}"' for f in main_file_candidates)
+            raise RoleAnalysisError(f"Could not pick single main file from {file_list}.")
+        else:
+            main_file = main_file_candidates[0]
+
+        return RoleAnalysis(
+            main_file=main_file,
+            labware_files=labware_files,
+        )
--- api/tests/opentrons/protocol_reader/test_role_analyzer.py
+++ api/tests/opentrons/protocol_reader/test_role_analyzer.py
@@ -1,211 +1,220 @@
 """Tests for opentrons.protocol_reader.role_analyzer.RoleAnalyzer."""
 import pytest
-from decoy import matchers
-from pathlib import Path
 from typing import List, NamedTuple
 
-from opentrons_shared_data.protocol.models import ProtocolSchemaV6
-from opentrons_shared_data.labware.labware_definition import LabwareDefinition
-from opentrons.protocols.models import JsonProtocol
-from opentrons.protocol_reader.file_reader_writer import BufferedFile
+from opentrons.protocols.api_support.types import APIVersion
 
+from opentrons.protocol_reader.file_reader_writer import BufferedFile
+from opentrons.protocol_reader.basic_info_extractor import (
+    FileInfo,
+    JsonProtocolFileInfo,
+    PythonProtocolFileInfo,
+    LabwareDefinitionFileInfo,
+)
 from opentrons.protocol_reader.role_analyzer import (
     RoleAnalyzer,
     RoleAnalysis,
-    MainFile,
-    LabwareFile,
     RoleAnalysisError,
 )
 
 
-class RoleAnalyzerSpec(NamedTuple):
-    """Spec data for a RoleAnalyzer test."""
-
-    files: List[BufferedFile]
-    expected: RoleAnalysis
-
-
-class RoleAnalyzerErrorSpec(NamedTuple):
-    """Spec data for a RoleAnalyzer test."""
-
-    files: List[BufferedFile]
-    expected_message: str
-
-
-ROLE_ANALYZER_SPECS: List[RoleAnalyzerSpec] = [
-    # Just a Python protocol file:
-    RoleAnalyzerSpec(
-        files=[
-            BufferedFile(name="protocol.py", path=None, contents=b"", data=None),
-        ],
-        expected=RoleAnalysis(
-            main_file=MainFile(name="protocol.py", contents=b"", path=None),
-            labware_files=[],
+@pytest.mark.parametrize(
+    "main_file",
+    [
+        PythonProtocolFileInfo(
+            original_file=BufferedFile(name="foo", path=None, contents=b""),
+            api_level=APIVersion(9001, 9001),
+            metadata={},
         ),
-    ),
-    # Just a JSON protocol file:
-    RoleAnalyzerSpec(
-        files=[
-            BufferedFile(
-                name="protocol.json",
-                contents=b"",
-                path=Path("/dev/null/protocol.json"),
-                data=JsonProtocol.construct(  # type: ignore[call-arg]
-                    labwareDefinitions={
-                        "uri": LabwareDefinition.construct()  # type: ignore[call-arg]
-                    },
-                ),
-            ),
-        ],
-        expected=RoleAnalysis(
-            main_file=MainFile(
-                name="protocol.json",
-                contents=b"",
-                path=Path("/dev/null/protocol.json"),
-                data=JsonProtocol.construct(  # type: ignore[call-arg]
-                    labwareDefinitions=matchers.Anything()
-                ),
-            ),
-            labware_files=[],
-        ),
-    ),
-    # A Python protocol file plus a standalone labware file:
-    RoleAnalyzerSpec(
-        files=[
-            BufferedFile(
-                name="protocol.py",
-                contents=b"",
-                path=Path("/dev/null/protocol.py"),
-                data=None,
-            ),
-            BufferedFile(
-                name="labware.json",
-                contents=b"",
-                path=Path("/dev/null/labware.json"),
-                data=LabwareDefinition.construct(),  # type: ignore[call-arg]
-            ),
-        ],
-        expected=RoleAnalysis(
-            main_file=MainFile(
-                name="protocol.py", contents=b"", path=Path("/dev/null/protocol.py")
-            ),
-            labware_files=[
-                LabwareFile(
-                    name="labware.json",
-                    contents=b"",
-                    path=Path("/dev/null/labware.json"),
-                    data=LabwareDefinition.construct(),  # type: ignore[call-arg]
-                )
-            ],
-        ),
-    ),
-    # A JSON protocol file plus a standalone labware file:
-    RoleAnalyzerSpec(
-        files=[
-            BufferedFile(
-                name="protocol.json",
+        JsonProtocolFileInfo(
+            original_file=BufferedFile(
+                name="foo",
                 contents=b"",
                 path=None,
-                data=JsonProtocol.construct(  # type: ignore[call-arg]
-                    labwareDefinitions={
-                        "uri": LabwareDefinition.construct(  # type: ignore[call-arg]
-                            version=1
-                        )
-                    },
-                ),
             ),
-            BufferedFile(
-                name="labware.json",
+            unvalidated_json={},
+            schema_version=123,
+            metadata={},
+        ),
+    ],
+)
+def test_single_main_file(main_file: FileInfo) -> None:
+    """It should analyze a file list containing only a single main file."""
+    subject = RoleAnalyzer()
+    result = subject.analyze([main_file])
+    assert result.main_file == main_file
+    assert result.labware_files == []
+
+
+@pytest.mark.parametrize(
+    "main_file",
+    [
+        PythonProtocolFileInfo(
+            original_file=BufferedFile(name="foo", path=None, contents=b""),
+            api_level=APIVersion(9001, 9001),
+            metadata={},
+        ),
+        JsonProtocolFileInfo(
+            original_file=BufferedFile(
+                name="foo",
                 contents=b"",
                 path=None,
-                data=LabwareDefinition.construct(version=2),  # type: ignore[call-arg]
             ),
-        ],
-        expected=RoleAnalysis(
-            main_file=MainFile(
-                name="protocol.json",
-                contents=b"",
-                path=None,
-                data=JsonProtocol.construct(  # type: ignore[call-arg]
-                    labwareDefinitions=matchers.Anything()
-                ),
+            unvalidated_json={},
+            schema_version=123,
+            metadata={},
+        ),
+    ],
+)
+def test_single_main_file_and_labware(main_file: FileInfo) -> None:
+    """It should analyze a file list containing a single main file, plus labware."""
+    labware_file_1 = LabwareDefinitionFileInfo(
+        original_file=BufferedFile(
+            name="labware_1",
+            contents=b"",
+            path=None,
+        ),
+        unvalidated_json={},
+    )
+    labware_file_2 = LabwareDefinitionFileInfo(
+        original_file=BufferedFile(
+            name="labware_2",
+            contents=b"",
+            path=None,
+        ),
+        unvalidated_json={},
+    )
+
+    subject = RoleAnalyzer()
+    result = subject.analyze([main_file, labware_file_1, labware_file_2])
+    assert result.main_file == main_file
+    assert result.labware_files == [labware_file_1, labware_file_2]
+
+
+def test_error_if_no_files() -> None:
+    """It should raise if the input file list contains no files."""
+    subject = RoleAnalyzer()
+    with pytest.raises(RoleAnalysisError, match="No files were provided."):
+        subject.analyze([])
+
+
+@pytest.mark.parametrize(
+    "main_file_1, main_file_2",
+    [
+        # Python plus Python:
+        (
+            PythonProtocolFileInfo(
+                original_file=BufferedFile(name="main_file_1", path=None, contents=b""),
+                api_level=APIVersion(9001, 9001),
+                metadata={},
             ),
-            labware_files=[
-                LabwareFile(
-                    name="labware.json",
+            PythonProtocolFileInfo(
+                original_file=BufferedFile(name="main_file_2", path=None, contents=b""),
+                api_level=APIVersion(9001, 9001),
+                metadata={},
+            ),
+        ),
+        # Python plus JSON:
+        (
+            PythonProtocolFileInfo(
+                original_file=BufferedFile(name="main_file_1", path=None, contents=b""),
+                api_level=APIVersion(9001, 9001),
+                metadata={},
+            ),
+            JsonProtocolFileInfo(
+                original_file=BufferedFile(
+                    name="main_file_2",
                     contents=b"",
                     path=None,
-                    data=LabwareDefinition.construct(version=2),  # type: ignore[call-arg]
+                ),
+                unvalidated_json={},
+                schema_version=123,
+                metadata={},
+            ),
+        ),
+        # JSON plus JSON:
+        (
+            JsonProtocolFileInfo(
+                original_file=BufferedFile(
+                    name="main_file_1",
+                    contents=b"",
+                    path=None,
+                ),
+                unvalidated_json={},
+                schema_version=123,
+                metadata={},
+            ),
+            JsonProtocolFileInfo(
+                original_file=BufferedFile(
+                    name="main_file_2",
+                    contents=b"",
+                    path=None,
+                ),
+                unvalidated_json={},
+                schema_version=123,
+                metadata={},
+            ),
+        ),
+    ],
+)
+def test_error_if_multiple_main_files(
+    main_file_1: FileInfo, main_file_2: FileInfo
+) -> None:
+    """It should raise if multiple main files are provided."""
+    subject = RoleAnalyzer()
+    with pytest.raises(
+        RoleAnalysisError,
+        match='Could not pick single main file from "main_file_1", "main_file_2"',
+    ):
+        subject.analyze([main_file_1, main_file_2])
+
+
+@pytest.mark.parametrize(
+    "labware_files, expected_message",
+    [
+        (
+            [
+                LabwareDefinitionFileInfo(
+                    original_file=BufferedFile(
+                        name="labware",
+                        contents=b"",
+                        path=None,
+                    ),
+                    unvalidated_json={},
                 )
             ],
+            '"labware" is not a valid protocol file.',
         ),
-    ),
-    # A protocol file with weird capitalization:
-    RoleAnalyzerSpec(
-        files=[
-            BufferedFile(name="PROTOCOL.PY", contents=b"", data=None, path=None),
-        ],
-        expected=RoleAnalysis(
-            main_file=MainFile(name="PROTOCOL.PY", contents=b"", path=None),
-            labware_files=[],
+        (
+            [
+                LabwareDefinitionFileInfo(
+                    original_file=BufferedFile(
+                        name="labware_1",
+                        contents=b"",
+                        path=None,
+                    ),
+                    unvalidated_json={},
+                ),
+                LabwareDefinitionFileInfo(
+                    original_file=BufferedFile(
+                        name="labware_2",
+                        contents=b"",
+                        path=None,
+                    ),
+                    unvalidated_json={},
+                ),
+            ],
+            'No valid protocol file found in "labware_1", "labware_2"',
         ),
-    ),
-]
-
-
-ROLE_ANALYZER_ERROR_SPECS: List[RoleAnalyzerErrorSpec] = [
-    RoleAnalyzerErrorSpec(
-        files=[],
-        expected_message="No files were provided.",
-    ),
-    RoleAnalyzerErrorSpec(
-        files=[BufferedFile(name="foo.txt", contents=b"", data=None, path=None)],
-        expected_message='"foo.txt" is not a valid protocol file.',
-    ),
-    RoleAnalyzerErrorSpec(
-        files=[
-            BufferedFile(name="hello.py", contents=b"", data=None, path=None),
-            BufferedFile(name="world.py", contents=b"", data=None, path=None),
-        ],
-        expected_message='Could not pick single main file from "hello.py", "world.py"',
-    ),
-    RoleAnalyzerErrorSpec(
-        files=[
-            BufferedFile(name="hello.txt", contents=b"", data=None, path=None),
-            BufferedFile(name="world.txt", contents=b"", data=None, path=None),
-        ],
-        expected_message='No valid protocol file found in "hello.txt", "world.txt"',
-    ),
-    RoleAnalyzerErrorSpec(
-        files=[
-            BufferedFile(name="hello.py", contents=b"", data=None, path=None),
-            BufferedFile(
-                name="world.json",
-                contents=b"",
-                data=JsonProtocol.construct(),  # type: ignore[call-arg]
-                path=None,
-            ),
-        ],
-        expected_message=(
-            'Could not pick single main file from "hello.py", "world.json"'
-        ),
-    ),
-]
-
-
-@pytest.mark.parametrize(RoleAnalyzerSpec._fields, ROLE_ANALYZER_SPECS)
-def test_role_analyzer(files: List[BufferedFile], expected: RoleAnalysis) -> None:
-    """It should analyze a file list properly."""
+    ],
+)
+def test_error_if_files_but_no_main(
+    labware_files: List[FileInfo], expected_message: str
+) -> None:
+    """It should raise if no main files are provided, but other files are."""
     subject = RoleAnalyzer()
-    result = subject.analyze(files)
-
-    assert result == expected
-
-
-@pytest.mark.parametrize(RoleAnalyzerErrorSpec._fields, ROLE_ANALYZER_ERROR_SPECS)
-def test_role_analyzer_error(files: List[BufferedFile], expected_message: str) -> None:
-    """It should raise errors on invalid input lists."""
-    subject = RoleAnalyzer()
-
-    with pytest.raises(RoleAnalysisError, match=expected_message):
-        subject.analyze(files)
+    with pytest.raises(
+        RoleAnalysisError,
+        match=expected_message,
+    ):
+        subject.analyze(labware_files)
