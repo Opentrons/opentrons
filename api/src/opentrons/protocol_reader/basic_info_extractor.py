e854c1495e965f8da44072a5da2eaf1832b6fffa
Max Marrone <max@opentrons.com>
2022-12-21 23:34:37 -0500

    Remove role analysis code from BasicInfoExtractor.

--- api/src/opentrons/protocol_reader/basic_info_extractor.py
+++ api/src/opentrons/protocol_reader/basic_info_extractor.py
@@ -44,31 +44,14 @@ FileInfo = Union[
 ]
 
 
-@dataclass(frozen=True)
-class BasicInfo:
-    main_file: Union[JsonProtocolFileInfo, PythonProtocolFileInfo]
-    labware_files: List[LabwareDefinitionFileInfo]
-    # todo(mm, 2022-12-19): Add data files like .txt and .csv.
-
-    @property
-    def all_files(self) -> List[FileInfo]:
-        return [self.main_file, *self.labware_files]
-
-
 class BasicInfoExtractor:
     @staticmethod
-    async def extract(files: List[BufferedFile]) -> BasicInfo:
-        analyzed_files = [await _analyze(file) for file in files]
-        return _analyze_roles(analyzed_files)
+    async def extract(files: List[BufferedFile]) -> List[FileInfo]:
+        return [await _analyze(file) for file in files]
 
 
 # FIX BEFORE MERGE: Rename exception.
 class ConfigAnalysisError(ValueError):
-    pass
-
-
-# FIX BEFORE MERGE: Rename exception.
-class RoleAnalysisError(ValueError):
     pass
 
 
@@ -202,40 +185,4 @@ def _analyze_python_protocol(
         original_file=py_file,
         metadata=metadata,
         api_level=api_version,
-    )
-
-
-def _analyze_roles(files: Sequence[FileInfo]) -> BasicInfo:
-    """Analyze a set of input files to determine each of their roles."""
-    if len(files) == 0:
-        raise RoleAnalysisError("No files were provided.")
-
-    main_file_candidates = []
-    labware_files = []
-
-    for f in files:
-        if isinstance(f, (JsonProtocolFileInfo, PythonProtocolFileInfo)):
-            main_file_candidates.append(f)
-        else:
-            # todo(mm, 2022-12-21): Support data files like .txt and .csv.
-            assert isinstance(f, LabwareDefinitionFileInfo)
-            labware_files.append(f)
-
-    if len(main_file_candidates) == 0:
-        if len(files) == 1:
-            raise RoleAnalysisError(
-                f'"{files[0].original_file.name}" is not a valid protocol file.'
-            )
-        else:
-            file_list = ", ".join(f'"{f.original_file.name}"' for f in files)
-            raise RoleAnalysisError(f"No valid protocol file found in {file_list}.")
-    elif len(main_file_candidates) > 1:
-        file_list = ", ".join(f'"{f.original_file.name}"' for f in main_file_candidates)
-        raise RoleAnalysisError(f"Could not pick single main file from {file_list}.")
-    else:
-        main_file = main_file_candidates[0]
-
-    return BasicInfo(
-        main_file=main_file,
-        labware_files=labware_files,
     )
