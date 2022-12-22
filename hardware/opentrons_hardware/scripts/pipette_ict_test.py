3bc75862f66c2f29909e19cb15e82615776fc5b2
Carlos <fernandez.carlos40@gmail.com>
2022-12-22 15:42:03 -0500

    working sensor output

--- hardware/opentrons_hardware/scripts/pipette_ict_test.py
+++ hardware/opentrons_hardware/scripts/pipette_ict_test.py
@@ -2,17 +2,19 @@ import argparse
 import asyncio
 from numpy import float64
 import termios
-import sys, tty
+import sys, tty, select, os
 import logging
 import datetime
-from typing import Callable
+from typing import Callable, Dict, Tuple, List, Optional, Set
 from logging.config import dictConfig
 import subprocess
 from enum import Enum, unique
+from opentrons_hardware.hardware_control.types import NodeDict
 
 from opentrons_hardware.drivers.can_bus.build import build_driver
 from opentrons_hardware.drivers.can_bus import build, CanMessenger, WaitableCallback
-from opentrons_hardware.firmware_bindings.constants import NodeId, PipetteName, SensorId
+from opentrons_hardware.firmware_bindings.constants import NodeId, PipetteName, SensorId, SensorType,SensorThresholdMode
+
 from opentrons_hardware.firmware_bindings import ArbitrationId
 from opentrons_hardware.firmware_bindings.messages import (
     message_definitions,
@@ -24,6 +26,8 @@ from opentrons_hardware.firmware_bindings.messages.message_definitions import (
     EnableMotorRequest,
     MotorPositionRequest,
     InstrumentInfoRequest,
+    ReadFromSensorResponse,
+
 )
 
 from opentrons_hardware.hardware_control.motion import (
@@ -32,6 +36,11 @@ from opentrons_hardware.hardware_control.motion import (
     create_home_step,
 )
 
+from opentrons_hardware.firmware_bindings.utils.binary_serializable import Int32Field
+from opentrons_hardware.sensors.types import (
+    SensorDataType,
+    sensor_fixed_point_conversion,
+)
 from opentrons_hardware.hardware_control.limit_switches import get_limit_switches
 from opentrons_hardware.sensors import sensor_driver, sensor_types
 from opentrons_hardware.hardware_control.move_group_runner import MoveGroupRunner
@@ -67,13 +76,13 @@ class PipetteAbbreviation(int, Enum):
     unknown = 0xFFFF
 
 
-def getch():
+def getch() -> str:
     """
     fd: file descriptor stdout, stdin, stderr
     This functions gets a single input keyboard character from the user
     """
 
-    def _getch():
+    def _getch() -> str:
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
@@ -86,12 +95,13 @@ def getch():
     return _getch()
 
 
-async def _jog_axis(messenger: CanMessenger, node, position) -> Dict[NodeId, float]:
+async def _jog_axis(
+    messenger: CanMessenger, node: NodeId, position: Dict[NodeId, float], speed: float
+) -> Dict[NodeId, float]:
     step_size = [0.1, 0.5, 1, 10, 20, 50]
     step_length_index = 3
     step = step_size[step_length_index]
-    pos = 0
-    speed = 10
+    pos = 0.0
     res = {node: (0, 0, 0)}
     information_str = """
         Click  >>   w  << to move up
@@ -102,21 +112,20 @@ async def _jog_axis(messenger: CanMessenger, node, position) -> Dict[NodeId, flo
         Click  >> q << to quit the test script
                     """
     print(information_str)
+    print('\n')
     while True:
         input = getch()
         if input == "w":
-            # plus pipette direction
+            # plus move direction
             sys.stdout.flush()
-            pos = pos + step
-            position["pipette"] = pos
-            res = await move_to(messenger, node, step, -speed)
+            position[node] = pos + step
+            res = await move_to(messenger, node, step, -speed)  # type: ignore[assignment]
 
         elif input == "s":
-            # minus pipette direction
+            # minus move direction
             sys.stdout.flush()
-            pos = pos - step
-            position["pipette"] = pos
-            res = await move_to(messenger, node, step, speed)
+            position[node] = pos - step
+            res = await move_to(messenger, node, step, speed)  # type: ignore[assignment]
 
         elif input == "q":
             sys.stdout.flush()
@@ -142,7 +151,7 @@ async def _jog_axis(messenger: CanMessenger, node, position) -> Dict[NodeId, flo
             return position
         print(
             "Coordinates: ",
-            round(position["pipette"], 2),
+            round(position[node], 2),
             ",",
             "motor position: ",
             res[node][0],
@@ -156,19 +165,24 @@ async def _jog_axis(messenger: CanMessenger, node, position) -> Dict[NodeId, flo
         print("\r", end="")
 
 
-def calc_time(distance, speed):
+def calc_time(distance: float, speed: float) -> float:
     time = abs(distance / speed)
     return time
 
 
-async def home(messenger, node):
+async def home(
+    messenger: CanMessenger, node: NodeId
+) -> NodeDict[Tuple[float, float, bool, bool]]:
     home_runner = MoveGroupRunner(
         move_groups=[[create_home_step({node: float64(100.0)}, {node: float64(-5)})]]
     )
-    await home_runner.run(can_messenger=messenger)
+    axis_dict = await home_runner.run(can_messenger=messenger)
+    return axis_dict
 
 
-async def move_to(messenger: CanMessenger, node, distance, velocity):
+async def move_to(
+    messenger: CanMessenger, node: NodeId, distance: float, velocity: float
+) -> NodeDict[Tuple[float, float, bool, bool]]:
     move_runner = MoveGroupRunner(
         # Group 0
         move_groups=[
@@ -187,54 +201,109 @@ async def move_to(messenger: CanMessenger, node, distance, velocity):
     return axis_dict
 
 
-def determine_abbreviation(pipette_val) -> PipetteAbbreviation:
+def determine_abbreviation(pipette_val: int) -> str:
     return PipetteAbbreviation(pipette_val).name
 
 
-async def read_epprom(messenger: CanMessenger, node):
+async def read_epprom(messenger: CanMessenger, node: NodeId) -> str:
     await messenger.send(node, InstrumentInfoRequest())
     target = datetime.datetime.now()
     try:
         while True:
             with WaitableCallback(messenger) as wc:
                 message, arb = await asyncio.wait_for(wc.read(), 1.0)
-                pipette_val = PipetteName(message.payload.name.value)
-                pipette_version = "V" + str(message.payload.model.value)
+                pipette_val = PipetteName(message.payload.name.value)  # type: ignore[attr-defined]
+                pipette_version = "V" + str(message.payload.model.value)  # type: ignore[attr-defined]
+                sn = str(message.payload.serial.value.decode("ascii").rstrip("\x00"))  # type: ignore[attr-defined]
                 serial_number = (
-                    determine_abbreviation(pipette_val)
-                    + pipette_version
-                    + str(message.payload.serial.value.decode("ascii").rstrip("\x00"))
+                    determine_abbreviation(pipette_val) + pipette_version + sn
                 )
-                return serial_number
+    except asyncio.TimeoutError:
+        pass
+    return serial_number
+
+async def do_run(messenger: CanMessenger,
+                callback: WaitableCallback,
+                target_node: NodeId,
+                target_sensor: SensorType,
+                sensor_id: SensorId,
+                threshold: float,
+                node: NodeId) -> float:
+        """Configure and start the monitoring."""
+        threshold_payload = payloads.SetSensorThresholdRequestPayload(
+            sensor=fields.SensorTypeField(SensorType.capacitive),
+            sensor_id=fields.SensorIdField(sensor_id),
+            threshold=Int32Field(int(threshold * sensor_fixed_point_conversion)),
+            mode=fields.SensorThresholdModeField(SensorThresholdMode.absolute),
+        )
+        threshold_message = message_definitions.SetSensorThresholdRequest(
+            payload=threshold_payload
+        )
+        await messenger.send(target_node, threshold_message)
+        stim_payload = payloads.BindSensorOutputRequestPayload(
+            sensor=fields.SensorTypeField(target_sensor.value),
+            sensor_id=fields.SensorIdField(sensor_id),
+            binding=fields.SensorOutputBindingField(3),
+        )
+        stim_message = message_definitions.BindSensorOutputRequest(payload=stim_payload)
+        reset_payload = payloads.BindSensorOutputRequestPayload(
+            sensor=fields.SensorTypeField(target_sensor.value),
+            sensor_id=fields.SensorIdField(sensor_id),
+            binding=fields.SensorOutputBindingField(0),
+        )
+        reset_message = message_definitions.BindSensorOutputRequest(payload=reset_payload)
+        #print(f"Sending stimulus to {target_node.name} {target_sensor.name}")
+        await messenger.send(target_node, stim_message)
+        start = datetime.datetime.now()
+        sensor_list = []
+        try:
+            async for message, _arbid in callback:
+                if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+                    line = input()
+                    return sensor_list
+                if isinstance(message, ReadFromSensorResponse):
+                    ts = (datetime.datetime.now() - start).total_seconds()
+                    s = SensorType(message.payload.sensor.value).name
+                    d = SensorDataType.build(
+                        message.payload.sensor_data, message.payload.sensor
+                    )
+                    rd = message.payload.sensor_data
+                    sensor_list.append(d.to_float())
+                    print(f"{ts:.3f}: {s} {d.to_float():5.3f}, \traw data: {str(rd)}")
+                    #return d.to_float()
+        finally:
+            await messenger.send(target_node, reset_message)
+
+async def read_sensor(messenger: CanMessenger, node: NodeId, threshold: float, sensor: SensorType, sensor_id: SensorId) -> float:
+    try:
+        with WaitableCallback(messenger) as reader:
+            p =  await do_run(
+                messenger, reader, node, sensor, sensor_id, threshold, node
+            )
+            return p
+    except asyncio.TimeoutError:
+        pass
+
+async def read_pressure_sensor(messenger: CanMessenger, node: NodeId, threshold: float) -> float:
+    sensor = SensorType.pressure
+    sensor_id = SensorId.S0
+    try:
+        with WaitableCallback(messenger) as reader:
+            p =  await do_run(
+                messenger, reader, node, sensor, sensor_id, threshold, node
+            )
+            return p
     except asyncio.TimeoutError:
         pass
 
 
-async def read_capacitive_sensor(messenger: CanMessenger, node):
-    capacitive = sensor_types.CapacitiveSensor.build(SensorId.S0, node)
+async def read_environment_sensor(messenger: CanMessenger, node: NodeId) -> float:
+    environment = sensor_types.EnvironmentSensor.build(SensorId.S0, node)
     s_driver = sensor_driver.SensorDriver()
-    data = await s_driver.read(CanMessenger, capacitive, offset=False, timeout=1)
+    data = await s_driver.read(messenger, environment, offset=False, timeout=1)
     if data is None:
         raise ValueError("Unexpected None value from sensor: {}".format(data))
-    return dataa.to_float()
-
-
-async def read_pressure_sensor(messenger: CanMessenger, node):
-    capacitive = sensor_types.PressureSensor.build(SensorId.S0, node)
-    s_driver = sensor_driver.SensorDriver()
-    data = await s_driver.read(CanMessenger, capacitive, offset=False, timeout=1)
-    if data is None:
-        raise ValueError("Unexpected None value from sensor: {}".format(data))
-    return dataa.to_float()
-
-
-async def read_environment_sensor(messenger: CanMessenger, node):
-    capacitive = sensor_types.EnvironmentSensor.build(SensorId.S0, node)
-    s_driver = sensor_driver.SensorDriver()
-    data = await s_driver.read(CanMessenger, capacitive, offset=False, timeout=1)
-    if data is None:
-        raise ValueError("Unexpected None value from sensor: {}".format(data))
-    return dataa.to_float()
+    return data.to_float()  # type: ignore[union-attr]
 
 
 async def run(args: argparse.Namespace) -> None:
@@ -244,7 +313,7 @@ async def run(args: argparse.Namespace) -> None:
     # gpio.deactivate_estop()
     node = ot3_nodes[args.node]
     subprocess.run(["systemctl", "stop", "opentrons-robot-server"])
-    position = {"pipette": 0}
+    position = {node: 0.0}
     driver = await build_driver(build_settings(args))
     messenger = CanMessenger(driver=driver)
     messenger.start()
@@ -257,15 +326,15 @@ async def run(args: argparse.Namespace) -> None:
     if args.jog:
         print("\n")
         print("----------Read Motor Position and Encoder--------------")
-        await _jog_axis(messenger, node, position)
+        await _jog_axis(messenger, node, position, args.speed)
 
     if args.limit_switch:
         print("\n")
         print("-----------------Read Limit Switch--------------")
-        res = await get_limit_switches(messenger, [node])
+        res = await get_limit_switches(messenger, [node])  # type: ignore[valid-type]
         print(f"Current Limit switch State: {res}")
         input("Block the limit switch and press enter")
-        res = await get_limit_switches(messenger, [node])
+        res = await get_limit_switches(messenger, [node])  # type: ignore[valid-type]
         print(f"Current Limit switch State: {res}")
 
     if args.read_epprom:
@@ -273,6 +342,27 @@ async def run(args: argparse.Namespace) -> None:
         print("-----------------Read EPPROM--------------")
         serial_number = await read_epprom(messenger, node)
         print(f"SN: {serial_number}")
+        await asyncio.sleep(2)
+
+    if args.capacitive:
+        print("\n")
+        print("-----------------Read Capacitive--------------")
+        cap_val = await read_sensor(messenger, node, args.threshold, SensorType.capacitive, SensorId.S0)
+        print(f"Capacitive(uF): {cap_val}")
+        await asyncio.sleep(2)
+
+    if args.pressure:
+        print("\n")
+        print("-----------------Read pressure--------------")
+        p = await read_sensor(messenger, node, args.threshold, SensorType.pressure, SensorId.S0)
+        await asyncio.sleep(2)
+
+    if args.environment:
+        print("\n")
+        print("-----------------Read Environment--------------")
+        en_val = await read_sensor(messenger, node, args.threshold, SensorType.environment, SensorId.S0)
+        print(f"Environment: {en_val}")
+        await asyncio.sleep(2)
 
     print("\n")
 
@@ -340,10 +430,17 @@ def main() -> None:
         "--node", type=str, help="NodeId to use", default="pipette_left"
     )
 
+    parser.add_argument(
+        "-t", "--threshold", type=float, help="sensor threshold", default=50
+    )
+
     parser.add_argument("--limit_switch", action="store_true")
     parser.add_argument("--jog", action="store_true")
     parser.add_argument("--read_epprom", action="store_true")
     parser.add_argument("--home", action="store_true")
+    parser.add_argument("--capacitive", action="store_true")
+    parser.add_argument("--pressure", action="store_true")
+    parser.add_argument("--environment", action="store_true")
 
     args = parser.parse_args()
     asyncio.run(run(args))
