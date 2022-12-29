585fc06d7f3a2d6c59fad3cbb1119dd7693e48a3
Shlok Amin <shlokamin@me.com>
2022-12-29 11:01:09 -0500

    more cleanup

--- app-shell-odd/electron-builder.config.js
+++ app-shell-odd/electron-builder.config.js
@@ -1,12 +1,10 @@
 'use strict'
-const DEV_MODE = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   appId: 'com.opentrons.app',
   electronVersion: '21.3.1',
   files: [
     '**/*',
-    'build/br-premigration-wheels',
     '!Makefile',
     {
       from: '../app/dist',
@@ -18,25 +16,9 @@ module.exports = {
   artifactName: '${productName}-v${version}-${os}-${env.BUILD_ID}.${ext}',
   /* eslint-enable no-template-curly-in-string */
   asar: true,
-  mac: {
-    target: process.platform === 'darwin' ? ['dmg', 'zip'] : ['zip'],
-    category: 'public.app-category.productivity',
-    type: DEV_MODE ? 'development' : 'distribution',
-  },
-  dmg: {
-    icon: null,
-  },
-  win: {
-    target: ['nsis'],
-    publisherName: 'Opentrons Labworks Inc.',
-  },
-  nsis: {
-    oneClick: false,
-  },
   linux: {
-    target: ['AppImage'],
+    target: ['dir'],
     executableName: 'opentrons',
     category: 'Science',
   },
-  generateUpdatesFilesForAllChannels: true,
 }
