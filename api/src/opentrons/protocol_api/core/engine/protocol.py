9264d94e8d9a9c965201e3a959be512772cb7cc7
tamarzanzouri <tamar.zanzouri@opentrons.com>
2023-01-06 09:55:44 -0500

    changed dict key to get

--- api/src/opentrons/protocol_api/core/engine/protocol.py
+++ api/src/opentrons/protocol_api/core/engine/protocol.py
@@ -401,7 +401,4 @@ class ProtocolCore(AbstractProtocol[InstrumentCore, LabwareCore, ModuleCore]):
 
     def _get_module_core_item(self, module_id: str) -> Optional[ModuleCore]:
         """Get Module core for a given module id."""
-        try:
-            return self._module_cores_by_id[module_id]
-        except KeyError:
-            return None
+        return self._module_cores_by_id.get(module_id)
