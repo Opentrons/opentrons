0373f17fc88b5a1488c08e2aea466cd89d25dbee
koji <koji.kanao@opentrons.com>
2023-01-09 17:32:45 -0500

    remove SetWifiCred from FailedToConnect

--- app/src/organisms/SetupNetwork/FailedToConnect.tsx
+++ app/src/organisms/SetupNetwork/FailedToConnect.tsx
@@ -26,6 +26,7 @@ interface FailedToConnectProps {
   requestState?: RequestState
   type: NetworkChangeType | null
   onConnect: () => void
+  handleShowSetWifiCred: (isShow: boolean) => void
 }
 
 export function FailedToConnect({
@@ -33,89 +34,82 @@ export function FailedToConnect({
   requestState,
   type,
   onConnect,
+  handleShowSetWifiCred,
 }: FailedToConnectProps): JSX.Element {
   const { t } = useTranslation(['device_settings', 'shared'])
   const history = useHistory()
   const isInvalidPassword = type === DISCONNECT
-  const [isShowSetWifiCred, setIsShowSetWifiCred] = React.useState<boolean>(
-    false
-  )
 
   const handleClick = (): void => {
-    if (isInvalidPassword) {
-      // reconnect
+    if (!isInvalidPassword) {
+      // Try to reconnect
       onConnect()
     } else {
-      setIsShowSetWifiCred(true)
+      // Display SetWifiCred screen
+      handleShowSetWifiCred(true)
     }
   }
 
   return (
-    <>
-      {isShowSetWifiCred ? (
-        <SetWifiCred ssid={ssid} />
-      ) : (
-        <Flex flexDirection={DIRECTION_COLUMN}>
-          <Flex
-            height="26.5625rem"
-            backgroundColor={COLORS.errorBackgroundMed}
-            justifyContent={JUSTIFY_CENTER}
-            marginBottom={SPACING.spacing6}
+    <Flex flexDirection={DIRECTION_COLUMN}>
+      <Flex
+        height="26.5625rem"
+        backgroundColor={COLORS.errorBackgroundMed}
+        justifyContent={JUSTIFY_CENTER}
+        marginBottom={SPACING.spacing6}
+      >
+        <Flex
+          justifyContent={JUSTIFY_CENTER}
+          alignItems={ALIGN_CENTER}
+          flexDirection={DIRECTION_COLUMN}
+        >
+          <Icon
+            name="ot-alert"
+            size="4.375rem"
+            color={COLORS.errorEnabled}
+            aria-label={
+              !isInvalidPassword
+                ? 'failed_to_connect_invalidPassword'
+                : 'failed_to_connect'
+            }
+          />
+          <StyledText
+            fontSize="2rem"
+            fontWeight={TYPOGRAPHY.fontWeightSemiBold}
+            lineHeight="2.72375rem"
+            marginTop={SPACING.spacingXXL}
           >
-            <Flex
-              justifyContent={JUSTIFY_CENTER}
-              alignItems={ALIGN_CENTER}
-              flexDirection={DIRECTION_COLUMN}
-            >
-              <Icon
-                name="ot-alert"
-                size="4.375rem"
-                color={COLORS.errorEnabled}
-                aria-label={
-                  !isInvalidPassword
-                    ? 'failed_to_connect_invalidPassword'
-                    : 'failed_to_connect'
-                }
-              />
-              <StyledText
-                fontSize="2rem"
-                fontWeight={TYPOGRAPHY.fontWeightSemiBold}
-                lineHeight="2.72375rem"
-                marginTop={SPACING.spacingXXL}
-              >
-                {!isInvalidPassword
-                  ? `Oops! Incorrect password for ${ssid}.`
-                  : t('failed_to_connect_to_ssid', { ssid: ssid })}
+            {!isInvalidPassword
+              ? `Oops! Incorrect password for ${ssid}.`
+              : t('failed_to_connect_to_ssid', { ssid: ssid })}
+          </StyledText>
+          {isInvalidPassword &&
+            requestState != null &&
+            'error' in requestState &&
+            requestState.error != null &&
+            'message' in requestState.error &&
+            requestState.error.message != null && (
+              <StyledText marginTop={SPACING.spacing4}>
+                {requestState.error.message}
               </StyledText>
-              {isInvalidPassword &&
-                requestState != null &&
-                'error' in requestState &&
-                requestState.error != null &&
-                'message' in requestState.error &&
-                requestState.error.message != null && (
-                  <StyledText marginTop={SPACING.spacing4}>
-                    {requestState.error.message}
-                  </StyledText>
-                )}
-            </Flex>
-          </Flex>
-          <Flex gridRow="0.75rem">
-            <SecondaryButton
-              flex="1"
-              onClick={handleClick}
-              textTransform={TYPOGRAPHY.textTransformCapitalize}
-            >
-              {t('shared:try_again')}
-            </SecondaryButton>
-            <PrimaryButton
-              flex="1"
-              onClick={() => history.push('/network-setup/wifi')}
-            >
-              {t('change_network')}
-            </PrimaryButton>
-          </Flex>
+            )}
         </Flex>
-      )}
-    </>
+      </Flex>
+      <Flex gridRow="0.75rem">
+        <SecondaryButton
+          flex="1"
+          onClick={handleClick}
+          textTransform={TYPOGRAPHY.textTransformCapitalize}
+        >
+          {t('shared:try_again')}
+        </SecondaryButton>
+        <PrimaryButton
+          flex="1"
+          onClick={() => history.push('/network-setup/wifi')}
+        >
+          {t('change_network')}
+        </PrimaryButton>
+      </Flex>
+    </Flex>
   )
 }
--- app/src/organisms/SetupNetwork/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ConnectingNetwork'
-export * from './FailedToConnect'
-export * from './SearchNetwork'
-export * from './SetWifiCred'
