b646330364f8d0dc660ff276e8eda16f8dcbc459
koji <koji.kanao@opentrons.com>
2023-01-10 19:25:47 -0500

    add test for software update components

--- app/src/organisms/UpdateRobotSoftware/__tests__/ErrorUpdateSoftware.test.tsx
+++ app/src/organisms/UpdateRobotSoftware/__tests__/ErrorUpdateSoftware.test.tsx
@@ -4,3 +4,50 @@ import { renderWithProviders } from '@opentrons/components'
 import { i18n } from '../../../i18n'
 
 import { ErrorUpdateSoftware } from '../ErrorUpdateSoftware'
+
+const mockPush = jest.fn()
+jest.mock('react-router-dom', () => {
+  const reactRouterDom = jest.requireActual('react-router-dom')
+  return {
+    ...reactRouterDom,
+    useHistory: () => ({ push: mockPush } as any),
+  }
+})
+
+const render = (props: React.ComponentProps<typeof ErrorUpdateSoftware>) => {
+  return renderWithProviders(<ErrorUpdateSoftware {...props} />, {
+    i18nInstance: i18n,
+  })
+}
+
+describe('ErrorUpdateSoftware', () => {
+  let props: React.ComponentProps<typeof ErrorUpdateSoftware>
+
+  beforeEach(() => {
+    props = {
+      errorMessage: 'mock error message',
+    }
+  })
+
+  it('should render text and buttons', () => {
+    const [{ getByText, getByRole }] = render(props)
+    getByText('Software update error')
+    getByText('mock error message')
+    getByRole('button', { name: 'Proceed without updating' })
+    getByRole('button', { name: 'Try again' })
+  })
+
+  it('call mockPush when tapping Proceed without updating', () => {
+    const [{ getByRole }] = render(props)
+    const button = getByRole('button', { name: 'Proceed without updating' })
+    fireEvent.click(button)
+    expect(mockPush).toBeCalledWith('/robot-settings/rename-robot')
+  })
+
+  it('call mock function when tapping Try again', () => {
+    const [{ getByRole }] = render(props)
+    const button = getByRole('button', { name: 'Try again' })
+    fireEvent.click(button)
+    // expect()
+  })
+})
