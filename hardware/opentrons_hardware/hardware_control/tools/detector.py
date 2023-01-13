67aac5c693f9daf5c10b90a5fc5977e3ff43b8ec
Seth Foster <seth@opentrons.com>
2023-01-13 14:46:13 -0500

    hack(hardware)!: hardcode left pipette presence (#12002)
    After https://github.com/Opentrons/ot3-firmware/pull/544, which is
    required because of hardware changes and short time to adapt, the head
    is (temporarily) no longer capable of detecting the presence of
    pipettes. This is a problem for the pipette QC script, which uses the
    mainline pipette detection which relies on the head. Temporarily work
    around this by hardcoding the python side to always return that a
    pipette is present on the left mount.
    
    This will make the script only work for a pipette on the left mount.
    
    This may be merged into the working branch for DVT, but it MUST NOT be
    merged in to edge, ever.

--- hardware/opentrons_hardware/hardware_control/tools/detector.py
+++ hardware/opentrons_hardware/hardware_control/tools/detector.py
@@ -163,11 +163,7 @@ class OneshotToolDetector:
     async def detect(self, timeout_sec: float = 1.0) -> ToolSummary:
         """Run once and detect tools."""
         with WaitableCallback(self._messenger) as wc:
-            attached_status_request = message_definitions.AttachedToolsRequest()
-            await self._messenger.send(
-                node_id=NodeId.head, message=attached_status_request
-            )
-            attached = await _await_one_result(wc)
+            attached = ToolDetectionResult(left=ToolType.pipette, right=ToolType.nothing_attached, gripper=ToolType.nothing_attached)
             should_respond: Set[NodeId] = set()
 
             def _should_query(attach_response: ToolType) -> bool:
