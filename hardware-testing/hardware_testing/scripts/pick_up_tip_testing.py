9d061bf199f5a047b4f5ff1d92aa116fc3fe6f5a
Carlos <fernandez.carlos40@gmail.com>
2023-01-13 14:21:45 -0500

    new added tests

--- hardware-testing/hardware_testing/scripts/pick_up_tip_testing.py
+++ hardware-testing/hardware_testing/scripts/pick_up_tip_testing.py
@@ -130,6 +130,21 @@ async def _jog_axis(api, position) -> None:
                             0,0,-step_size[step_length_index]),
                             speed = za_speed)
 
+        elif input == 'r':
+            sys.stdout.flush()
+            position = await api.current_position_ot3(mount)
+            gauge_reading = gauge.read_stable(timeout=20)
+            test_data["X-Coordinate"] = round(position[OT3Axis.X], 2)
+            test_data["Y-Coordinate"] = round(position[OT3Axis.Y], 2)
+            test_data["Z-Coordinate"] = round(position[OT3Axis.by_mount(mount)], 2)
+            test_data["Deck Height(mm)"] = gauge_reading
+            print(test_data)
+            d_str = f"{round(position[OT3Axis.X], 2)}, \
+                    {round(position[OT3Axis.Y], 2)}, \
+                    {round(position[OT3Axis.by_mount(mount)], 2)}, \
+                    {gauge.read_stable(timeout=20)}, {gauge_reading}\n"
+            data.append_data_to_file(test_n, test_f, d_str)
+
         elif input == 'q':
             sys.stdout.flush()
             print("TEST CANCELLED")
@@ -162,9 +177,6 @@ async def _jog_axis(api, position) -> None:
         print('\r', end='')
 
 async def _main() -> None:
-    mount = OT3Mount.RIGHT
-    # test_n , test_f  = file_setup(test_data)
-    # gauge = dial_indicator_setup()
     hw_api = await build_async_ot3_hardware_api(is_simulating=args.simulate,
                                     use_defaults=True)
     # await set_default_current_settings(hw_api, load=None)
@@ -180,96 +192,166 @@ async def _main() -> None:
     tip_column = 0
     columns_to_use = 12
     try:
-        start_time =  time.time()
-        # await hw_api.move_to(mount, Point(start_loc[OT3Axis.X],
-        #                                     start_loc[OT3Axis.Y],
-        #                                     home_pos[OT3Axis.by_mount(mount)]))
-        await hw_api.move_to(mount, Point(home_pos[OT3Axis.X],
-                                            start_loc[OT3Axis.Y],
-                                            home_pos[OT3Axis.by_mount(mount)]),
-                                            speed = 50)
-        await hw_api.move_to(mount, Point(start_loc[OT3Axis.X],
-                                            start_loc[OT3Axis.Y],
-                                            home_pos[OT3Axis.by_mount(mount)]))
-        for cycle in range(1, columns_to_use+1):
-            if cycle <= 1:
-                print("Move to Tiprack")
-                home_position = await hw_api.current_position_ot3(mount)
-                tiprack_loc = await _jog_axis(hw_api, home_position)
+        if args.test == 'pick_up_tip':
+            start_time =  time.time()
+            await hw_api.move_to(mount, Point(start_loc[OT3Axis.X],
+                                                start_loc[OT3Axis.Y],
+                                                home_pos[OT3Axis.by_mount(mount)]))
+            # await hw_api.move_to(mount, Point(home_pos[OT3Axis.X],
+            #                                     start_loc[OT3Axis.Y],
+            #                                     home_pos[OT3Axis.by_mount(mount)]))
+            # await hw_api.move_to(mount, Point(start_loc[OT3Axis.X],
+            #                                     start_loc[OT3Axis.Y],
+            #                                     home_pos[OT3Axis.by_mount(mount)]))
+            for cycle in range(1, columns_to_use+1):
+                if cycle <= 1:
+                    print("Move to Tiprack")
+                    home_position = await hw_api.current_position_ot3(mount)
+                    tiprack_loc = await _jog_axis(hw_api, home_position)
+                #     Coordinates:  177.0 , 182.1 , 97.5
+                # tiprack_loc = {OT3Axis.X: 177.0, OT3Axis.Y: 182.1, OT3Axis.by_mount(mount): 97.5}
+                # await hw_api.move_to(mount, Point(tiprack_loc[OT3Axis.X],
+                #                                     tiprack_loc[OT3Axis.Y],
+                #                                     home_pos[OT3Axis.by_mount(mount)]))
+                await hw_api.move_to(mount, Point(tiprack_loc[OT3Axis.X] + tip_column,
+                                                    tiprack_loc[OT3Axis.Y],
+                                                    tiprack_loc[OT3Axis.by_mount(mount)]))
+                #await hw_api.pick_up_tip(mount, tip_length = 58.5, presses = 2, increment = 1)
+                current_val = float(input("Enter Current Val: "))
+                print(f"Current Val: {current_val}")
+                await update_pick_up_current(hw_api, mount, current_val)
+                await hw_api.pick_up_tip(mount, tip_length = 58.5)
+                current_pos = await hw_api.current_position_ot3(mount)
+                # Move to Block
+                await hw_api.move_to(mount, Point(207.2,
+                                                    72.7,
+                                                    current_pos[OT3Axis.by_mount(mount)]))
+                current_pos = await hw_api.current_position_ot3(mount)
+                block = await _jog_axis(hw_api, current_pos)
+                await hw_api.home_z(mount, allow_home_other = False)
 
-            await hw_api.move_to(mount, Point(tiprack_loc[OT3Axis.X] + tip_column,
-                                                tiprack_loc[OT3Axis.Y],
-                                                tiprack_loc[OT3Axis.by_mount(mount)]),
-                                                speed = 50)
-            #await hw_api.pick_up_tip(mount, tip_length = 58.5, presses = 2, increment = 1)
-            await update_pick_up_current(hw_api, mount, 0.5)
-            await hw_api.pick_up_tip(mount, tip_length = 58.5)
-            current_pos = await hw_api.current_position_ot3(mount)
-            block = await _jog_axis(hw_api, current_pos)
-            await hw_api.home_z(mount, allow_home_other = False)
+                trash_loc = [432.7 , 393.3 , 100.0]
+                await hw_api.home_z(mount, allow_home_other = False)
+                current_pos = await hw_api.current_position_ot3(mount)
+                await hw_api.move_to(mount, Point(trash_loc[0],
+                                                    trash_loc[1],
+                                                    current_pos[OT3Axis.by_mount(mount)]))
+                await hw_api.move_to(mount, Point(trash_loc[0],
+                                                    trash_loc[1],
+                                                    trash_loc[2]))
+                await hw_api.drop_tip(mount)
+                await hw_api.home_z(mount, allow_home_other = False)
+                current_pos = await hw_api.current_position_ot3(mount)
+                # tip_column += 9
+                await hw_api.move_to(mount, Point(tiprack_loc[OT3Axis.X] + tip_column,
+                                                    tiprack_loc[OT3Axis.Y],
+                                                    current_pos[OT3Axis.by_mount(mount)]))
+        elif args.test == 'tip_height_test':
+            start_time =  time.time()
+            await hw_api.move_to(mount, Point(start_loc[OT3Axis.X],
+                                                start_loc[OT3Axis.Y],
+                                                home_pos[OT3Axis.by_mount(mount)]))
+            # await hw_api.move_to(mount, Point(home_pos[OT3Axis.X],
+            #                                     start_loc[OT3Axis.Y],
+            #                                     home_pos[OT3Axis.by_mount(mount)]))
+            # await hw_api.move_to(mount, Point(start_loc[OT3Axis.X],
+            #                                     start_loc[OT3Axis.Y],
+            #                                     home_pos[OT3Axis.by_mount(mount)]))
+            for cycle in range(1, columns_to_use+1):
+                if cycle <= 1:
+                    print("Move to Tiprack")
+                    home_position = await hw_api.current_position_ot3(mount)
+                    tiprack_loc = await _jog_axis(hw_api, home_position)
+                #     Coordinates:  177.0 , 182.1 , 97.5
+                # tiprack_loc = {OT3Axis.X: 177.0, OT3Axis.Y: 182.1, OT3Axis.by_mount(mount): 97.5}
+                # await hw_api.move_to(mount, Point(tiprack_loc[OT3Axis.X],
+                #                                     tiprack_loc[OT3Axis.Y],
+                #                                     home_pos[OT3Axis.by_mount(mount)]))
+                await hw_api.move_to(mount, Point(tiprack_loc[OT3Axis.X] + tip_column,
+                                                    tiprack_loc[OT3Axis.Y],
+                                                    tiprack_loc[OT3Axis.by_mount(mount)]))
+                #await hw_api.pick_up_tip(mount, tip_length = 58.5, presses = 2, increment = 1)
+                current_val = float(input("Enter Current Val: "))
+                print(f"Current Val: {current_val}")
+                await update_pick_up_current(hw_api, mount, current_val)
+                await hw_api.pick_up_tip(mount, tip_length = 58.5)
+                current_pos = await hw_api.current_position_ot3(mount)
+                # Move to Block
+                await hw_api.move_to(mount, Point(207.2,
+                                                    72.7,
+                                                    current_pos[OT3Axis.by_mount(mount)]))
+                current_pos = await hw_api.current_position_ot3(mount)
+                block = await _jog_axis(hw_api, current_pos)
+                await hw_api.home_z(mount, allow_home_other = False)
 
-            # if cycle <= 1:
-            #     print("Move to Tiprack")
-            #     home_position = await hw_api.current_position_ot3(mount)
-            #     dial_pos = await _jog_axis(hw_api, home_position)
-            # await hw_api.home_z(mount)
-            # current_pos = await hw_api.current_position_ot3(mount)
-            # await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
-            #                                     dial_pos[OT3Axis.Y],
-            #                                     current_pos[OT3Axis.by_mount(mount)]-3))
-            # current_pos = await hw_api.current_position_ot3(mount)
-            # await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
-            #                                     dial_pos[OT3Axis.Y],
-            #                                     current_pos[OT3Axis.by_mount(mount)]))
-            # tip_increment = 0
-            # tips = 8
-            # z_distance_press = 6
-            # for tip in range(1, tips+1):
-            #     # Press onto the dial indicator
-            #     await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
-            #                                         dial_pos[OT3Axis.Y]-tip_increment,
-            #                                         dial_pos[OT3Axis.by_mount(mount)]))
-            #     await asyncio.sleep(2)
-            #     elasped_time = (time.time() - start_time)/60
-            #     test_data["Time"] = round(elasped_time, 3)
-            #     test_data["Tip Height(mm)"] = gauge.read_stable(timeout=20)
-            #     test_data["Tip"] = tip
-            #     print(test_data)
-            #     d_str = f"{elasped_time}, {gauge.read_stable(timeout=20)}, {tip}\n"
-            #     data.append_data_to_file(test_n, test_f, d_str)
-            #     await asyncio.sleep(1)
-            #     # Retract from the dial indicator by 6mm
-            #     await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
-            #                                         dial_pos[OT3Axis.Y]-tip_increment,
-            #                                         dial_pos[OT3Axis.by_mount(mount)]+z_distance_press))
-            #     # backlash compensation
-            #     await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
-            #                                         dial_pos[OT3Axis.Y]-tip_increment,
-            #                                         dial_pos[OT3Axis.by_mount(mount)]+z_distance_press-3))
-            #     # move to the next nozzle
-            #     tip_increment += 9
-            #     current_pos = await hw_api.current_position_ot3(mount)
-            #     await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
-            #                                         dial_pos[OT3Axis.Y]-tip_increment,
-            #                                         current_pos[OT3Axis.by_mount(mount)]))
+                if cycle <= 1:
+                    print("Move to Tiprack")
+                    home_position = await hw_api.current_position_ot3(mount)
+                    dial_pos = await _jog_axis(hw_api, home_position)
+                await hw_api.home_z(mount)
+                current_pos = await hw_api.current_position_ot3(mount)
+                await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
+                                                    dial_pos[OT3Axis.Y],
+                                                    current_pos[OT3Axis.by_mount(mount)]-3))
+                current_pos = await hw_api.current_position_ot3(mount)
+                await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
+                                                    dial_pos[OT3Axis.Y],
+                                                    current_pos[OT3Axis.by_mount(mount)]))
+                tip_increment = 0
+                tips = 8
+                z_distance_press = 6
+                for tip in range(1, tips+1):
+                    # Press onto the dial indicator
+                    await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
+                                                        dial_pos[OT3Axis.Y]-tip_increment,
+                                                        dial_pos[OT3Axis.by_mount(mount)]))
+                    await asyncio.sleep(2)
+                    elasped_time = (time.time() - start_time)/60
+                    test_data["Time"] = round(elasped_time, 3)
+                    test_data["Tip Height(mm)"] = gauge.read_stable(timeout=20)
+                    test_data["Tip"] = tip
+                    print(test_data)
+                    d_str = f"{elasped_time}, {gauge.read_stable(timeout=20)}, {tip}\n"
+                    data.append_data_to_file(test_n, test_f, d_str)
+                    await asyncio.sleep(1)
+                    # Retract from the dial indicator by 6mm
+                    await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
+                                                        dial_pos[OT3Axis.Y]-tip_increment,
+                                                        dial_pos[OT3Axis.by_mount(mount)]+z_distance_press))
+                    # backlash compensation
+                    await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
+                                                        dial_pos[OT3Axis.Y]-tip_increment,
+                                                        dial_pos[OT3Axis.by_mount(mount)]+z_distance_press-3))
+                    # move to the next nozzle
+                    tip_increment += 9
+                    current_pos = await hw_api.current_position_ot3(mount)
+                    await hw_api.move_to(mount, Point(dial_pos[OT3Axis.X],
+                                                        dial_pos[OT3Axis.Y]-tip_increment,
+                                                        current_pos[OT3Axis.by_mount(mount)]))
 
-            trash_loc = [432.7 , 393.3 , 100.0]
-            await hw_api.home_z(mount, allow_home_other = False)
-            current_pos = await hw_api.current_position_ot3(mount)
-            await hw_api.move_to(mount, Point(trash_loc[0],
-                                                trash_loc[1],
-                                                current_pos[OT3Axis.by_mount(mount)]))
-            await hw_api.move_to(mount, Point(trash_loc[0],
-                                                trash_loc[1],
-                                                trash_loc[2]))
-            await hw_api.drop_tip(mount)
-            await hw_api.home_z(mount, allow_home_other = False)
-            current_pos = await hw_api.current_position_ot3(mount)
-            tip_column += 9
-            await hw_api.move_to(mount, Point(tiprack_loc[OT3Axis.X] + tip_column,
-                                                tiprack_loc[OT3Axis.Y],
-                                                current_pos[OT3Axis.by_mount(mount)]))
-
+                trash_loc = [432.7 , 393.3 , 100.0]
+                await hw_api.home_z(mount, allow_home_other = False)
+                current_pos = await hw_api.current_position_ot3(mount)
+                await hw_api.move_to(mount, Point(trash_loc[0],
+                                                    trash_loc[1],
+                                                    current_pos[OT3Axis.by_mount(mount)]))
+                await hw_api.move_to(mount, Point(trash_loc[0],
+                                                    trash_loc[1],
+                                                    trash_loc[2]))
+                await hw_api.drop_tip(mount)
+                await hw_api.home_z(mount, allow_home_other = False)
+                current_pos = await hw_api.current_position_ot3(mount)
+                # tip_column += 9
+                await hw_api.move_to(mount, Point(tiprack_loc[OT3Axis.X] + tip_column,
+                                                    tiprack_loc[OT3Axis.Y],
+                                                    current_pos[OT3Axis.by_mount(mount)]))
+        elif args.test == 'flatness':
+            home_position = await hw_api.current_position_ot3(mount)
+            await hw_api.move_to(mount, Point(start_loc[OT3Axis.X],
+                                                start_loc[OT3Axis.Y],
+                                                home_pos[OT3Axis.by_mount(mount)]))
+            # we don't need this, this is just for a placeholder
+            flatness = await _jog_axis(hw_api, home_position)
         await hw_api.disengage_axes([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
     except KeyboardInterrupt:
         await hw_api.disengage_axes([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
@@ -277,21 +359,44 @@ async def _main() -> None:
         await hw_api.disengage_axes([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
         await hw_api.clean_up()
 
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--simulate", action="store_true")
+    parser.add_argument("--mount", type=str, choices=["left", "right"], default="left")
+    parser.add_argument(
+        "--test",
+        type=str,
+        help="pick_up_tip, tip_height_test, flatness",
+        default="pick_up_tip",
+    )
+    parser.add_argument("--dial_indicator", action="store_true")
+    parser.add_argument("--flatness", action="store_true")
     args = parser.parse_args()
-
-    mount = OT3Mount.RIGHT
+    if args.mount == "left":
+        mount = OT3Mount.LEFT
+    else:
+        mount = OT3Mount.RIGHT
     xy_speed = 250
     speed_z = 60
-
+    if args.dial_indicator:
+        if args.flatness:
+            test_data ={
+                    "X-Coordinate": None,
+                    "Y-Coordinate": None,
+                    "Z-Coordinate": None,
+                    "Deck Height(mm)": None,
+                }
+        else:
+            test_data ={
+                    "Time": None,
+                    "Tip Height(mm)": None,
+                    "Tip": None
+                }
+        gauge = dial_indicator_setup()
+        test_n , test_f  = file_setup(test_data)
     pick_up_speed = 5
     press_distance = 15
     PIPETTE_SPEED = 10
-    test_data ={
-            "Time": None,
-            "Tip Height(mm)": None,
-            "Tip": None
-        }
     asyncio.run(_main())
