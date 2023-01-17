91701b571a78d6e7564b7c41760478d38575b2ba
John Bowser <john.bow7720@gmail.com>
2023-01-17 16:16:38 -0700

    tests

--- app/src/organisms/CalibrationTaskList/__tests__/CalibrationTaskList.test.tsx
+++ app/src/organisms/CalibrationTaskList/__tests__/CalibrationTaskList.test.tsx
@@ -2,33 +2,38 @@ import * as React from 'react'
 import { renderWithProviders } from '@opentrons/components'
 import { i18n } from '../../../i18n'
 import { CalibrationTaskList } from '..'
-import { useCalibrationTaskList } from '../../Devices/hooks'
 import {
-  expectedTaskList,
   mockDeckCalLauncher,
   mockTipLengthCalLauncher,
   mockPipOffsetCalLauncher,
-  expectedIncompleteDeckCalTaskList,
-  expectedIncompleteLeftMountTaskList,
-  expectedIncompleteRightMountTaskList,
+  mockCompleteDeckCalibration,
+  mockAttachedPipettesResponse,
+  mockCompleteTipLengthCalibrations,
+  mockCompletePipetteOffsetCalibrations,
 } from '../../Devices/hooks/__fixtures__/taskListFixtures'
 import { StaticRouter } from 'react-router-dom'
-import { TaskListProps } from '../../TaskList/types'
 
-jest.mock('../../Devices/hooks')
+jest.mock('../../Devices/hooks', () => {
+  const actualHooks = jest.requireActual('../../Devices/hooks')
+  return {
+    ...actualHooks,
+    useAttachedPipettes: jest.fn(() => mockAttachedPipettesResponse),
+    useDeckCalibrationData: jest.fn(() => mockCompleteDeckCalibration),
+    usePipetteOffsetCalibrations: jest.fn(
+      () => mockCompletePipetteOffsetCalibrations
+    ),
+    useTipLengthCalibrations: jest.fn(() => mockCompleteTipLengthCalibrations),
+  }
+})
 
-const mockUseCalibrationTaskList = useCalibrationTaskList as jest.MockedFunction<
-  typeof useCalibrationTaskList
->
-
-const render = () => {
+const render = (robotName: string = 'otie') => {
   return renderWithProviders(
     <StaticRouter>
       <CalibrationTaskList
-        robotName="otie"
-        pipOffsetCalLauncher={jest.fn()}
-        tipLengthCalLauncher={jest.fn()}
-        deckCalLauncher={jest.fn()}
+        robotName={robotName}
+        pipOffsetCalLauncher={mockPipOffsetCalLauncher}
+        tipLengthCalLauncher={mockTipLengthCalLauncher}
+        deckCalLauncher={mockDeckCalLauncher}
       />
     </StaticRouter>,
     {
@@ -38,12 +43,7 @@ const render = () => {
 }
 
 describe('CalibrationTaskList', () => {
-  afterEach(() => {
-    jest.resetAllMocks()
-  })
-
   it('renders the Calibration Task List', () => {
-    mockUseCalibrationTaskList.mockReturnValue(expectedTaskList)
     const [{ getByText }] = render()
     getByText('Deck Calibration')
     getByText('Left Mount')
@@ -51,7 +51,6 @@ describe('CalibrationTaskList', () => {
   })
 
   it('clicking the recalibrate CTAs triggers the calibration launchers', () => {
-    mockUseCalibrationTaskList.mockReturnValue(expectedTaskList)
     const [{ getByText, getAllByText }] = render()
     getByText('Left Mount').click()
     getByText('Right Mount').click()
@@ -67,57 +66,6 @@ describe('CalibrationTaskList', () => {
     recalibrateButtons[3].click()
     expect(mockTipLengthCalLauncher).toHaveBeenCalled()
     recalibrateButtons[4].click()
-    expect(mockPipOffsetCalLauncher).toHaveBeenCalled()
-  })
-
-  it('clicking the deck calibrate CTA triggers the calibration launcher', () => {
-    mockUseCalibrationTaskList.mockReturnValue(
-      expectedIncompleteDeckCalTaskList
-    )
-    const [{ getByText }] = render()
-    getByText('Calibrate').click()
-    expect(mockDeckCalLauncher).toHaveBeenCalled()
-  })
-
-  it('clicking the Left Mount calibrate CTAs triggers the calibration launchers', () => {
-    mockUseCalibrationTaskList.mockReturnValue(
-      expectedIncompleteLeftMountTaskList
-    )
-    const [{ getByText, getAllByText, unmount }] = render()
-    getByText('Left Mount').click()
-    getByText('Calibrate').click()
-    expect(mockTipLengthCalLauncher).toHaveBeenCalled()
-    unmount()
-    const updatedTaskList = {
-      ...expectedIncompleteLeftMountTaskList,
-      activeIndex: [1, 1],
-    } as TaskListProps
-    mockUseCalibrationTaskList.mockReturnValue(updatedTaskList)
-    render()
-    getByText('Left Mount').click()
-    // the previous Calibrate CTA still shows because we just manually changed the activeIndex to make the next button available, so grab the 2nd one
-    getAllByText('Calibrate')[1].click()
-    expect(mockPipOffsetCalLauncher).toHaveBeenCalled()
-  })
-
-  it('clicking the Right Mount calibrate CTAs triggers the calibration launchers', () => {
-    mockUseCalibrationTaskList.mockReturnValue(
-      expectedIncompleteRightMountTaskList
-    )
-    const [{ getByText, getAllByText, unmount }] = render()
-    getByText('Right Mount').click()
-    getByText('Calibrate').click()
-    expect(mockTipLengthCalLauncher).toHaveBeenCalled()
-    unmount()
-    const updatedTaskList = {
-      ...expectedIncompleteRightMountTaskList,
-      activeIndex: [2, 1],
-    } as TaskListProps
-    mockUseCalibrationTaskList.mockReturnValue(updatedTaskList)
-    render()
-    getByText('Right Mount').click()
-    // the previous Calibrate CTA still shows because we just manually changed the activeIndex to make the next button available, so grab the 2nd one
-    getAllByText('Calibrate')[1].click()
     expect(mockPipOffsetCalLauncher).toHaveBeenCalled()
   })
 })
