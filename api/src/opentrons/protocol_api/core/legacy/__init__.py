90a30d3aeb13d4f1540766cb8c47167f658f159e
TamarZanzouri <zanzouritamar@gmail.com>
2023-01-18 14:25:52 -0500

    Update api/src/opentrons/protocol_api/core/legacy/__init__.py

--- api/src/opentrons/protocol_api/core/legacy/__init__.py
+++ api/src/opentrons/protocol_api/core/legacy/__init__.py
@@ -1,8 +1,8 @@
 """The Protocol API v2 context implementation package."""
 # TODO(mc, 2022-08.22): uncomment when import cycles can be resolved
-# from .protocol_context import LegacyProtocolCore
-# from .instrument_context import LegacyInstrumentCore
-# from .labware import LegacyLabwareCore
+# from .legacy_protocol_core import LegacyProtocolCore
+# from .legacy_instrument_core import LegacyInstrumentCore
+# from .legacy_labware_core.py import LegacyLabwareCore
 
 # __all__ = [
 #     "LegacyProtocolCore",
