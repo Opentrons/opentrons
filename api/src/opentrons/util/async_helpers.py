7911a4e275ee165441a3d4ac65cb0f357e2dfbac
Seth Foster <seth@opentrons.com>
2023-01-23 10:16:26 -0500

    Revert "use paramspecs when typechecking"
    This reverts commit 5e783a5d230ac74df2c5453047411a4c8c1a78fe.

--- api/src/opentrons/util/async_helpers.py
+++ api/src/opentrons/util/async_helpers.py
@@ -3,14 +3,9 @@ util.async_helpers - various utilities for asyncio functions and tasks.
 """
 
 from functools import wraps
-from typing import TypeVar, Callable, Awaitable, cast, Any, TYPE_CHECKING
+from typing import TypeVar, Callable, Awaitable, cast
 
 import asyncio
-
-if TYPE_CHECKING:
-    from typing_extensions import ParamSpec
-    P = ParamSpec("P")
-    T = TypeVar("T")
 
 async def asyncio_yield() -> None:
     """
@@ -39,7 +34,11 @@ async def asyncio_yield() -> None:
     """
     await asyncio.sleep(0)
 
-def ensure_yield(async_def_func: "Callable[P, Awaitable[T]]") -> "T":
+
+WrappedReturn = TypeVar("WrappedReturn", contravariant=True)
+WrappedCoro = TypeVar("WrappedCoro", bound=Callable[..., Awaitable[WrappedReturn]])
+
+def ensure_yield(async_def_func: WrappedCoro) -> WrappedCoro:
     """
     A decorator that makes sure that asyncio_yield() is called after the decorated async
     function finishes executing.
@@ -49,11 +48,10 @@ def ensure_yield(async_def_func: "Callable[P, Awaitable[T]]") -> "T":
     # when we either go to python 3.10 or bump typing_extensions to 4.2.0
     # https://github.com/python/typing_extensions/blob/main/CHANGELOG.md#release-420-april-17-2022
     # Until then, we need the cast on line 57
-
     @wraps(async_def_func)
-    async def _wrapper(*args: "P.args", **kwargs: "P.kwargs") -> "T":
+    async def _wrapper(*args, **kwargs):
         ret = await async_def_func(*args, **kwargs)
         await asyncio_yield()
         return ret
 
-    return _wrapper
+    return cast(WrappedCoro, _wrapper)
