6d21259bd029f486254adc3c5663f41eca68df22
John Bowser <john.bow7720@gmail.com>
2023-01-23 11:09:45 -0700

    feat(app): implement calibrations complete dashboard screen
    adds a calibrations complete screen to the calibration dashboard once a user completes all calibrations
    closes RAUT-111

--- app/src/organisms/CalibrationTaskList/index.tsx
+++ app/src/organisms/CalibrationTaskList/index.tsx
@@ -2,7 +2,16 @@ import * as React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useHistory } from 'react-router-dom'
 
-import { COLORS, SPACING } from '@opentrons/components'
+import {
+  ALIGN_CENTER,
+  COLORS,
+  DIRECTION_COLUMN,
+  Flex,
+  Icon,
+  JUSTIFY_CENTER,
+  JUSTIFY_FLEX_END,
+  SPACING,
+} from '@opentrons/components'
 import { Modal } from '../../molecules/Modal'
 import { TaskList } from '../TaskList'
 
@@ -11,6 +20,8 @@ import { useCalibrationTaskList } from '../Devices/hooks'
 import type { DashboardCalOffsetInvoker } from '../../pages/Devices/CalibrationDashboard/hooks/useDashboardCalibratePipOffset'
 import type { DashboardCalTipLengthInvoker } from '../../pages/Devices/CalibrationDashboard/hooks/useDashboardCalibrateTipLength'
 import type { DashboardCalDeckInvoker } from '../../pages/Devices/CalibrationDashboard/hooks/useDashboardCalibrateDeck'
+import { StyledText } from '../../atoms/text'
+import { PrimaryButton } from '../../atoms/buttons'
 
 interface CalibrationTaskListProps {
   robotName: string
@@ -25,7 +36,12 @@ export function CalibrationTaskList({
   tipLengthCalLauncher,
   deckCalLauncher,
 }: CalibrationTaskListProps): JSX.Element {
-  const { t } = useTranslation('robot_calibration')
+  const prevActiveIndex = React.useRef<[number, number] | null>(null)
+  const [
+    showCompletionScreen,
+    setShowCompletionScreen,
+  ] = React.useState<boolean>(false)
+  const { t } = useTranslation(['robot_calibration', 'device_settings'])
   const history = useHistory()
   const { activeIndex, taskList } = useCalibrationTaskList(
     robotName,
@@ -33,6 +49,17 @@ export function CalibrationTaskList({
     tipLengthCalLauncher,
     deckCalLauncher
   )
+
+  React.useEffect(() => {
+    console.log({ prevActiveIndex: prevActiveIndex.current })
+    if (prevActiveIndex.current == null && activeIndex == null) {
+      return
+    }
+    if (prevActiveIndex.current !== null && activeIndex === null) {
+      setShowCompletionScreen(true)
+    }
+    prevActiveIndex.current = activeIndex
+  }, [activeIndex])
 
   return (
     <Modal
@@ -44,9 +71,47 @@ export function CalibrationTaskList({
       backgroundColor={COLORS.fundamentalsBackground}
       childrenPadding={`${String(SPACING.spacing4)} ${String(
         SPACING.spacing5
-      )} ${String(SPACING.spacing5)} ${String(SPACING.spacing2)}`}
+      )} ${String(SPACING.spacing5)} ${
+        showCompletionScreen ? '' : String(SPACING.spacing2)
+      }`}
     >
-      <TaskList activeIndex={activeIndex} taskList={taskList} />
+      {showCompletionScreen ? (
+        <Flex flexDirection={DIRECTION_COLUMN} padding="4.375rem">
+          <Flex
+            flex="1"
+            flexDirection={DIRECTION_COLUMN}
+            justifyContent={JUSTIFY_CENTER}
+            alignItems={ALIGN_CENTER}
+          >
+            <Icon
+              name="ot-check"
+              size="2.775rem"
+              color={COLORS.successEnabled}
+            />
+            <StyledText as="h1" marginTop={SPACING.spacing5}>
+              {t('calibrations_complete')}
+            </StyledText>
+            <Flex
+              flex="0"
+              alignSelf={ALIGN_CENTER}
+              marginTop={SPACING.spacing5}
+              justifyContent={JUSTIFY_FLEX_END}
+            >
+              <PrimaryButton
+                onClick={() =>
+                  history.push(
+                    `/devices/${robotName}/robot-settings/calibration`
+                  )
+                }
+              >
+                {t('device_settings:done')}
+              </PrimaryButton>
+            </Flex>
+          </Flex>
+        </Flex>
+      ) : (
+        <TaskList activeIndex={activeIndex} taskList={taskList} />
+      )}
     </Modal>
   )
 }
