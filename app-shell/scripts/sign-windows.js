8ba0295bdf6a7a4708b45ebaa202156af81f4ab7
Seth Foster <seth@opentrons.com>
2023-01-26 12:27:36 -0500

    add some safety

--- app-shell/scripts/sign-windows.js
+++ app-shell/scripts/sign-windows.js
@@ -1,24 +1,27 @@
 'use strict'
-const path = require('path')
+const {join} = require('path')
 const { mkdtemp, open } = require('fs/promises')
 const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
 
-const certEncodedContent = process.env.WIN_CSC_LINK
-const certPassword = process.env.WIN_CSC_KEY_PASSWORD
-
 exports.default = async function (configuration) {
-  const { toSignPath = path } = configuration
+  if (!Object.hasOwnProperty(process.env, 'WIN_CSC_LINK')) {
+    console.log('No certificate, skipping signing')
+  }
+  const { path } = configuration
+
+  const certEncodedContent = process.env.WIN_CSC_LINK
+  const certPassword = process.env.WIN_CSC_KEY_PASSWORD
   console.log(`Signing ${toSignPath} using powershell commandlets`)
   const certContent = atob(certEncodedContent)
   const tempdir = await mkdtemp('keyfile')
-  const certTempPath = path.join(tempdir, 'key.pfx')
+  const certTempPath = join(tempdir, 'key.pfx')
   const certFile = await open(certTempPath, 'w')
   await certFile.writeFile(certContent)
   await certFile.close()
 
   await exec(
-    `Set-AuthenticodeSignature -Certificate $(Get-PfxCertificate -FilePath ${certTempPath} -Password ${certPassword} ) -FilePath ${toSignPath}`,
+    `Set-AuthenticodeSignature -Certificate $(Get-PfxCertificate -FilePath ${certTempPath} -Password ${certPassword}) -FilePath ${path}`,
     {
       stdio: 'inherit',
       shell: 'powershell.exe'
