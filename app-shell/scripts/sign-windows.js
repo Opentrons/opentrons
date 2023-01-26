dd36dac6b7b8758a69dba8ed492589f24ea0af97
Seth Foster <seth@opentrons.com>
2023-01-26 10:24:00 -0500

    don't use the underspecified config thing

--- app-shell/scripts/sign-windows.js
+++ app-shell/scripts/sign-windows.js
@@ -4,14 +4,13 @@ const { mkdtemp, open } = require('fs/promises')
 const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
 
+const certEncodedContent = process.env('WIN_CSC_LINK')
+const certPassword = process.env('WIN_CSC_KEY_PASSWORD')
+
 exports.default = async function (configuration) {
-  const { toSignPath = path, cscInfo } = configuration
-  if (!Object.hasOwnProperty(configuration, 'cscInfo')) {
-    console.log('No codesign configuration, not signing')
-    return
-  }
-  const { file, password } = cscInfo
-  const certContent = atob(file)
+  const { toSignPath = path } = configuration
+  console.log(`Signing ${toSignPath} using powershell commandlets`)
+  const certContent = atob(certEncodedContent)
   const tempdir = mkdtemp('keyfile')
   const certTempPath = path.join(tempdir, 'key.pfx')
   const certFile = await open(certTempPath)
@@ -19,7 +18,7 @@ exports.default = async function (configuration) {
   await certFile.close()
 
   await exec(
-    `Set-AuthenticodeSignature -Certificate $(Get-PfxCertificate -FilePath ${certTempPath} -Password ${password} ) -FilePath ${toSignPath}`,
+    `Set-AuthenticodeSignature -Certificate $(Get-PfxCertificate -FilePath ${certTempPath} -Password ${certPassword} ) -FilePath ${toSignPath}`,
     {
       stdio: 'inherit',
     }
