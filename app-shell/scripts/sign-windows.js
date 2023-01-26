28de62d31a76a69a3f3e0c7ab973db854997055e
Seth Foster <seth@opentrons.com>
2023-01-26 09:50:31 -0500

    needed some style love

--- app-shell/scripts/sign-windows.js
+++ app-shell/scripts/sign-windows.js
@@ -1,25 +1,25 @@
 'use strict'
 const path = require('path')
-const {mkdtemp, open} = require('fs/promises')
-const {promisify} = require("util")
+const { mkdtemp, open } = require('fs/promises')
+const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
 
 exports.default = async function (configuration) {
-  const {toSignPath = path, cscInfo} = configuration
+  const { toSignPath = path, cscInfo } = configuration
   if (!Object.hasOwnProperty(configuration, 'cscInfo')) {
     console.log('No codesign configuration, not signing')
     return
   }
-  const {file, password} = cscInfo
+  const { file, password } = cscInfo
   const certContent = atob(file)
   const tempdir = mkdtemp('keyfile')
   const certTempPath = path.join(tempdir, 'key.pfx')
   const certFile = await open(certTempPath)
-  await certFile.writeFile(cert)
+  await certFile.writeFile(certContent)
   await certFile.close()
 
   await exec(
-    `Set-AuthenticodeSignature -Certificate \$(Get-PfxCertificate -FilePath ${certTempPath} -Password ${password} ) -FilePath ${toSignPath}`,
+    `Set-AuthenticodeSignature -Certificate $(Get-PfxCertificate -FilePath ${certTempPath} -Password ${password} ) -FilePath ${toSignPath}`,
     {
       stdio: 'inherit',
     }
