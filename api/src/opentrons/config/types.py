ebdb65dd73c28c3ddae7c5833c4689268def9589
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2023-01-25 22:37:57 -0500

    code cleanup

--- api/src/opentrons/config/types.py
+++ api/src/opentrons/config/types.py
@@ -142,6 +142,24 @@ class EdgeSenseSettings:
     search_initial_tolerance_mm: float
     search_iteration_limit: int
 
+    def __init__(
+        self,
+        overrun_tolerance_mm: float,
+        early_sense_tolerance_mm: float,
+        pass_settings: CapacitivePassSettings,
+        search_initial_tolerance_mm: float,
+        search_iteration_limit: int,
+    ) -> None:
+        if not (overrun_tolerance_mm <= pass_settings.max_overrun_distance_mm):
+            raise ValueError("Overrun tolerance and pass setting distance do not match")
+        object.__setattr__(self, "overrun_tolerance_mm", overrun_tolerance_mm)
+        object.__setattr__(self, "early_sense_tolerance_mm", early_sense_tolerance_mm)
+        object.__setattr__(self, "pass_settings", pass_settings)
+        object.__setattr__(
+            self, "search_initial_tolerance_mm", search_initial_tolerance_mm
+        )
+        object.__setattr__(self, "search_iteration_limit", search_iteration_limit)
+
 
 @dataclass(frozen=True)
 class OT3CalibrationSettings:
