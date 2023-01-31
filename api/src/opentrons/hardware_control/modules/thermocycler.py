76dd6899ce8275b46535ba30086b99e221441816
Frank Sinapi <frank.sinapi@opentrons.com>
2023-01-31 15:23:32 -0500

    Added note to Thermocycler HC docstring about plate-lift errors.

--- api/src/opentrons/hardware_control/modules/thermocycler.py
+++ api/src/opentrons/hardware_control/modules/thermocycler.py
@@ -221,7 +221,13 @@ class Thermocycler(mod_abc.AbstractModule):
         return ThermocyclerLidStatus.CLOSED
 
     async def lift_plate(self) -> str:
-        """If the lid is open, lift the plate."""
+        """If the lid is open, lift the plate.
+
+        Note that this should always be preceded by an `open` call. If the
+        lid is not open, the Thermocycler will respond with an error message.
+
+        Returns: Thermocycler lid status after running the command.
+        """
         await self.wait_for_is_running()
         await self._driver.lift_plate()
         await self._wait_for_lid_status(ThermocyclerLidStatus.OPEN)
