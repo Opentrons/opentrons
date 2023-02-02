711d672b2e48a575137fa082d474c172d3899709
Eric Wagoner <ewagoner@gmail.com>
2023-02-02 15:35:05 -0500

    Make deck markedBad optional in useDeckCalibrationData type definition [RAUT-108

--- app/src/organisms/Devices/hooks/useDeckCalibrationData.ts
+++ app/src/organisms/Devices/hooks/useDeckCalibrationData.ts
@@ -24,7 +24,7 @@ export function useDeckCalibrationData(
 ): {
   deckCalibrationData: DeckCalibrationData | null
   isDeckCalibrated: boolean
-  markedBad: boolean
+  markedBad?: boolean
 } {
   const [dispatchRequest] = useDispatchApiRequest()
 
