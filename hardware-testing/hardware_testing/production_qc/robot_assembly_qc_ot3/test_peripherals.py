490f7b4038826ffce7ff233a7c689bfb09d5fda3
andySigler <andrewsigler1@gmail.com>
2023-02-02 07:44:53 -0500

    adds peripherals tests

--- hardware-testing/hardware_testing/production_qc/robot_assembly_qc_ot3/test_peripherals.py
+++ hardware-testing/hardware_testing/production_qc/robot_assembly_qc_ot3/test_peripherals.py
@@ -18,7 +18,7 @@ def build_csv_lines() -> List[Union[CSVLine, CSVLineRepeating]]:
         CSVLine("screen-touch", [CSVResult]),
         CSVLine("deck-lights-on", [CSVResult]),
         CSVLine("deck-lights-off", [CSVResult]),
-        CSVLine("status-lights-on", [CSVResult]),
+        CSVLine("status-light-on", [CSVResult]),
         CSVLine("door-switch", [CSVResult]),
         CSVLine("camera-active", [CSVResult]),
         CSVLine("camera-image", [CSVResult]),
@@ -27,4 +27,33 @@ def build_csv_lines() -> List[Union[CSVLine, CSVLineRepeating]]:
 
 async def run(api: OT3API, report: CSVReport, section: str) -> None:
     """Run."""
-    return
+
+    def _get_user_confirmation(question: str) -> bool:
+        if api.is_simulator:
+            return True
+        return "y" in input(f"{question} (y/n): ").lower()
+
+    # ODD
+    result = _get_user_confirmation("is ODD on?")
+    report(section, "screen-on", [CSVResult.from_bool(result)])
+    result = _get_user_confirmation("is ODD touchscreen working?")
+    report(section, "screen-touch", [CSVResult.from_bool(result)])
+
+    # DECK LIGHTS
+    result = _get_user_confirmation("are the DECK-LIGHTS on?")
+    report(section, "deck-lights-on", [CSVResult.from_bool(result)])
+    # TODO: enable once we are able to turn off the deck lights
+    # result = "y" in input("are the DECK-LIGHTS off? (y/n): ")
+    # report(section, "deck-lights-off", [CSVResult.from_bool(result)])
+
+    # STATUS LIGHTS
+    result = _get_user_confirmation("is the STATUS-LIGHT on?")
+    report(section, "status-light-on", [CSVResult.from_bool(result)])
+    # TODO: do more testing (colors, on/off, etc.) once implemented
+
+    # DOOR SWITCH
+    # TODO: add test once implemented
+
+    # CAMERA:
+    # TODO: read camera image once ffmpeg (or other tool) is implemented in OS
+
