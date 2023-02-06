6394eb0179389669425f016062ab58662448fea1
Frank Sinapi <frank.sinapi@opentrons.com>
2023-02-06 14:46:01 -0500

    feat(server-utils): add server utils to makefile & buildroot

--- external.mk
+++ external.mk
@@ -5,3 +5,4 @@ include $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/robot-server/buildroot.mk
 include $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/shared-data/python/buildroot.mk
 include $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/notify-server/buildroot.mk
 include $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/system-server/buildroot.mk
+include $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/server-utils/buildroot.mk
