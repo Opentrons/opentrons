510f2259a4a759bdce3e7464edfaae297ed961bc
jerome <1458798121@qq.com>
2023-02-06 14:58:46 +0800

    update zstage_qc_ot3.py

--- hardware-testing/hardware_testing/production_qc/zstage_qc_ot3.py
+++ hardware-testing/hardware_testing/production_qc/zstage_qc_ot3.py
@@ -30,7 +30,7 @@ default_run_current = 1.0
 currents = (0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 1.5)
 speeds = (50, 100, 150, 200, 250, 300)
 force_gauge_currents = (0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 1.5)
-force_gauge_speeds = (2, 5, 10, 20)
+force_gauge_speeds = (10, 20)
 Z_STAGE_TOLERANCES_MM = 0.4
 
 data_format = "||{0:^12}|{1:^12}|{2:^12}||"
@@ -197,6 +197,7 @@ async def _currents_speeds_test(messenger: CanMessenger,write_cb: Callable):
                         messenger, 100, default_move_speed, check=True
                     )
                     print(f"motor position: {mot}, encoder position: {enc}")
+                    time.sleep(0.5)
                     mot, enc = await _move_to(
                         messenger, 100, -default_move_speed, check=True
                     )
@@ -265,96 +266,64 @@ async def _force_gauge(messenger: CanMessenger,write_cb: Callable):
                 TH.join()
                 await _home(messenger)
 
-
-async def _force_gauge_stepbystep(messenger: CanMessenger,write_cb: Callable):
-    global thread_sensor
-    global cu_fg, sp_fg, distance_fg
-    mark10 = _connect_to_mark10_fixture(False)
-    for cu_fg in force_gauge_currents:
-        for sp_fg in force_gauge_speeds:
-            await _home(messenger)
-            await _set_pipette_current(messenger, default_run_current)
-            await _move_to(
-                messenger, 100, default_move_speed, check=True
-            )
-            print(f'current = {cu_fg},speed = {sp_fg}')
-            distance_fg = 100
-            for i in range(20):
-                try:
-                    print('Start record force...')
-                    distance_fg += 1
-                    await _set_pipette_current(messenger, cu_fg)
-                    await _move_to(
-                        messenger, distance_fg, sp_fg, check=True
-                    )
-                    print(f'Mark10 force is {mark10.read_force()}')
-                    write_cb([_convert_node_to_str(NODE), cu_fg, sp_fg, mark10.read_force()])
-                except LoseStepError as e:
-                    print(e)
-                    await _home(messenger)
-                    break
-
-
-
 async def _run(messenger: CanMessenger,arguments: argparse.Namespace) -> None:
     if "q" in input("\n\tEnter 'q' to exit"):
         raise KeyboardInterrupt()
     global NODE
-    try:
-        await _homeMount(messenger)
-        # callback function for writing new data to CSV file
-        csv_props, csv_cb = _create_csv_and_get_callbacks(arguments.serial_number)
-        # cache the pressure-data header
-        # add metadata to CSV
-        # FIXME: create a set of CSV helpers, such that you can define a test-report
-        #        schema/format/line-length/etc., before having to fill its contents.
-        #        This would be very helpful, because changes to CVS length/contents
-        #        will break the analysis done in our Sheets
-        csv_cb.write(["--------"])
-        csv_cb.write(["METADATA"])
-        csv_cb.write(["test-name", csv_props.name])
-        csv_cb.write(["operator-name", arguments.operator_name])
-        csv_cb.write(["date", csv_props.id])  # run-id includes a date/time string
+    await _homeMount(messenger)
+    # callback function for writing new data to CSV file
+    csv_props, csv_cb = _create_csv_and_get_callbacks(arguments.serial_number)
+    # cache the pressure-data header
+    # add metadata to CSV
+    # FIXME: create a set of CSV helpers, such that you can define a test-report
+    #        schema/format/line-length/etc., before having to fill its contents.
+    #        This would be very helpful, because changes to CVS length/contents
+    #        will break the analysis done in our Sheets
+    csv_cb.write(["--------"])
+    csv_cb.write(["METADATA"])
+    csv_cb.write(["test-name", csv_props.name])
+    csv_cb.write(["operator-name", arguments.operator_name])
+    csv_cb.write(["date", csv_props.id])  # run-id includes a date/time string
 
-        print('----Test mount left----')
-        csv_cb.write(["----Test mount left----"])
-        NODE = NodeId.head_l
-        await _home(messenger)
-        # run the test
-        csv_cb.write(["----"])
-        csv_cb.write(["Currents_Speeds_Test"])
-        csv_cb.write(["----"])
-        print("Currents_Speeds_Test")
-        csv_cb.write(['Mount', 'Current', 'Speed', 'Result'])
-        await _currents_speeds_test(messenger, csv_cb.write)
+    print('----Test mount left----')
+    csv_cb.write(["----Test mount left----"])
+    NODE = NodeId.head_l
+    await _home(messenger)
+    # run the test
+    csv_cb.write(["----"])
+    csv_cb.write(["Currents_Speeds_Test"])
+    csv_cb.write(["----"])
+    print("Currents_Speeds_Test")
+    csv_cb.write(['Mount', 'Current', 'Speed', 'Result'])
+    await _currents_speeds_test(messenger, csv_cb.write)
 
-        csv_cb.write(["----"])
-        csv_cb.write(["Force_Gauge_Test"])
-        csv_cb.write(["----"])
-        csv_cb.write(['Mount', 'Current', 'Speed', 'Force'])
-        print("Force_Gauge_Test")
-        await _force_gauge_stepbystep(messenger, csv_cb.write)
+    csv_cb.write(["----"])
+    csv_cb.write(["Force_Gauge_Test"])
+    csv_cb.write(["----"])
+    csv_cb.write(['Mount', 'Current', 'Speed', 'Force'])
+    print("Force_Gauge_Test")
+    # await _force_gauge(messenger, csv_cb.write)
 
-        print('----Test mount right----')
-        csv_cb.write(["----Test mount right----"])
-        NODE = NodeId.head_r
-        await _home(messenger)
-        # run the test
-        csv_cb.write(["----"])
-        csv_cb.write(["Currents_Speeds_Test"])
-        csv_cb.write(["----"])
-        print("Currents_Speeds_Test")
-        csv_cb.write(['Mount', 'Current', 'Speed', 'Result'])
-        await _currents_speeds_test(messenger, csv_cb.write)
+    print('----Test mount right----')
+    csv_cb.write(["----Test mount right----"])
+    NODE = NodeId.head_r
+    await _home(messenger)
+    # run the test
+    csv_cb.write(["----"])
+    csv_cb.write(["Currents_Speeds_Test"])
+    csv_cb.write(["----"])
+    print("Currents_Speeds_Test")
+    csv_cb.write(['Mount', 'Current', 'Speed', 'Result'])
+    await _currents_speeds_test(messenger, csv_cb.write)
 
-        csv_cb.write(["----"])
-        csv_cb.write(["Force_Gauge_Test"])
-        csv_cb.write(["----"])
-        csv_cb.write(['Mount', 'Current', 'Speed', 'Force'])
-        print("Force_Gauge_Test")
-        await _force_gauge_stepbystep(messenger, csv_cb.write)
-    except Exception as e:
-        print(e)
+    csv_cb.write(["----"])
+    csv_cb.write(["Force_Gauge_Test"])
+    csv_cb.write(["----"])
+    csv_cb.write(['Mount', 'Current', 'Speed', 'Force'])
+    print("Force_Gauge_Test")
+    # await _force_gauge(messenger, csv_cb.write)
+
+    print('Test Done...')
 
 async def _main(arguments: argparse.Namespace) -> None:
     subprocess.run(["systemctl", "stop", "opentrons-robot-server"])
