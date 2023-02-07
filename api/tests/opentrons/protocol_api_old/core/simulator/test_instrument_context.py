ca8fcc933cc949119b01a82325365a7bd5e6ea0a
tamarzanzouri <tamar.zanzouri@opentrons.com>
2023-02-07 16:46:08 -0500

    fixed legacy simulator test failing

--- api/tests/opentrons/protocol_api_old/core/simulator/test_instrument_context.py
+++ api/tests/opentrons/protocol_api_old/core/simulator/test_instrument_context.py
@@ -65,6 +65,7 @@ def test_drop_tip_no_tip(subject: InstrumentCore, tip_rack: LabwareCore) -> None
 
 def test_blow_out_no_tip(subject: InstrumentCore, labware: LabwareCore) -> None:
     """It should raise an error if a tip is not attached."""
+    subject.home()
     with pytest.raises(NoTipAttachedError, match="Cannot perform BLOWOUT"):
         subject.blow_out(
             location=Location(point=Point(1, 2, 3), labware=None),
