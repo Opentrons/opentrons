d93b65949ad2e3b400e2671fe549bec11fc8da24
Frank Sinapi <frank.sinapi@opentrons.com>
2023-02-08 15:42:53 -0500

    Add an async lock to initialization code

--- system-server/system_server/persistence/__init__.py
+++ system-server/system_server/persistence/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from fastapi import Depends
 from typing_extensions import Final
 import sqlalchemy
-
+from asyncio import Lock
 
 from .database import create_sql_engine
 from .tables import registration_table, migration_table
@@ -22,28 +22,33 @@ _DATABASE_FILE: Final = "system_server.db"
 _log = logging.getLogger(__name__)
 
 
+_persistence_dir_lock = Lock()
+_sql_lock = Lock()
+
+
 async def get_persistence_directory(
     app_state: AppState = Depends(get_app_state),
 ) -> Path:
     """Return the root persistence directory, creating it if necessary."""
-    persistence_dir = _persistence_directory_accessor.get_from(app_state)
+    async with _persistence_dir_lock:
+        persistence_dir = _persistence_directory_accessor.get_from(app_state)
 
-    if persistence_dir is None:
-        setting = get_settings().persistence_directory
+        if persistence_dir is None:
+            setting = get_settings().persistence_directory
 
-        # There is no appropriate default to this setting, so raise an
-        # exception and bail if it isn't specified.
-        if setting is None:
-            raise RuntimeError(
-                "No persistence path was specified.\n"
-                "Configure a persistence path with OT_SYSTEM_SERVER_persistence_directory"
+            # There is no appropriate default to this setting, so raise an
+            # exception and bail if it isn't specified.
+            if setting is None:
+                raise RuntimeError(
+                    "No persistence path was specified.\n"
+                    "Configure a persistence path with OT_SYSTEM_SERVER_persistence_directory"
+                )
+            persistence_dir = await create_persistent_directory(
+                None if setting == "automatically_make_temporary" else Path(setting)
             )
-        persistence_dir = await create_persistent_directory(
-            None if setting == "automatically_make_temporary" else Path(setting)
-        )
-        _persistence_directory_accessor.set_on(app_state, persistence_dir)
+            _persistence_directory_accessor.set_on(app_state, persistence_dir)
 
-    return persistence_dir
+        return persistence_dir
 
 
 async def get_sql_engine(
@@ -51,17 +56,18 @@ async def get_sql_engine(
     persistence_directory: Path = Depends(get_persistence_directory),
 ) -> sqlalchemy.engine.Engine:
     """Return a singleton SQL engine referring to a ready-to-use database."""
-    sql_engine = _sql_engine_accessor.get_from(app_state)
+    async with _sql_lock:
+        sql_engine = _sql_engine_accessor.get_from(app_state)
 
-    if sql_engine is None:
-        sql_engine = create_sql_engine(persistence_directory / _DATABASE_FILE)
-        _sql_engine_accessor.set_on(app_state, sql_engine)
+        if sql_engine is None:
+            sql_engine = create_sql_engine(persistence_directory / _DATABASE_FILE)
+            _sql_engine_accessor.set_on(app_state, sql_engine)
 
-    return sql_engine
-    # Rely on connections being cleaned up automatically when the process dies.
-    # FastAPI doesn't give us a convenient way to properly tie
-    # the lifetime of a dependency to the lifetime of the server app.
-    # https://github.com/tiangolo/fastapi/issues/617
+        return sql_engine
+        # Rely on connections being cleaned up automatically when the process dies.
+        # FastAPI doesn't give us a convenient way to properly tie
+        # the lifetime of a dependency to the lifetime of the server app.
+        # https://github.com/tiangolo/fastapi/issues/617
 
 
 __all__ = [
