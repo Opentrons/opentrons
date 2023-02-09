d622209964f1ace44c539ebc9e3ea53250a5b557
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2023-02-08 19:41:35 -0500

    refactor(api): Improve OT3 instrument calibration process (#11807)

--- api/src/opentrons/hardware_control/backends/ot3controller.py
+++ api/src/opentrons/hardware_control/backends/ot3controller.py
@@ -579,7 +579,7 @@ class OT3Controller:
         serial = attached.serial
         return {
             "config": gripper_config.load(model),
-            "id": f"GRPV{attached.model}{serial}",
+            "id": f"GRPV{attached.model.replace('.', '')}{serial}",
         }
 
     @staticmethod
