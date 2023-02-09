4de20d88580271af86a03cce31e995bd92782458
Seth Foster <seth@opentrons.com>
2023-02-09 10:08:44 -0500

    script takes ios not paths

--- hardware/opentrons_hardware/scripts/update_fws.py
+++ hardware/opentrons_hardware/scripts/update_fws.py
@@ -46,7 +46,8 @@ async def run(args: argparse.Namespace) -> None:
 
     update_dict = json.load(args.dict)
     update_details = {
-        NodeId[node_id]: hex_file for node_id, hex_file in update_dict.items()
+        NodeId[node_id]: open(hex_file, "r")
+        for node_id, hex_file in update_dict.items()
     }
 
     async with build.can_messenger(build_settings(args)) as messenger:
