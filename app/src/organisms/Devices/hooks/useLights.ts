aba15c51e523cae48788fcfec9a808b7b101f49c
Brian Cooper <brian@opentrons.com>
2023-02-13 18:05:44 -0500

    make lights toggle respond to input immediately

--- app/src/organisms/Devices/hooks/useLights.ts
+++ app/src/organisms/Devices/hooks/useLights.ts
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import {
   useLightsQuery,
   useSetLightsMutation,
@@ -8,15 +9,25 @@ export function useLights(): {
   lightsOn: boolean | null
   toggleLights: () => void
 } {
-  const { setLights } = useSetLightsMutation()
+
+  const [lightsOnCache, setLightsOnCache] = React.useState(false)
+  const { setLights, data: setLightsData} = useSetLightsMutation()
   const { data: lightsData } = useLightsQuery({
     refetchInterval: LIGHTS_POLL_MS,
   })
-  const lightsOn = lightsData != null ? lightsData.on : false
+
+  React.useEffect(() => {
+    if (setLightsData != null) {
+      setLightsOnCache(setLightsData.on)
+    } else if (lightsData != null) {
+      setLightsOnCache(lightsData.on)
+    }
+  }, [lightsData, setLightsData])
 
   const toggleLights = (): void => {
-    setLights({ on: !Boolean(lightsOn) })
+    setLightsOnCache(!Boolean(lightsOnCache))
+    setLights({ on: !Boolean(lightsOnCache) })
   }
 
-  return { lightsOn, toggleLights }
+  return { lightsOn: lightsOnCache, toggleLights }
 }
