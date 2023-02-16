1c58229ac049754ad6e8aeef1c379e4b516bc512
Laura Cox <laura@opentrons.com>
2023-02-16 15:53:06 -0500

    Add helper function to convert dict

--- api/src/opentrons/calibration_storage/ot2/tip_length.py
+++ api/src/opentrons/calibration_storage/ot2/tip_length.py
@@ -22,6 +22,17 @@ log = logging.getLogger(__name__)
 # Get Tip Length Calibration
 
 
+def _conver_tip_length_model_to_dict(
+    to_dict: typing.Dict[str, v1.TipLengthModel]
+) -> typing.Dict[str, typing.Any]:
+    # This is a workaround since pydantic doesn't have a nice way to
+    # add encoders when converting to a dict.
+    dict_of_tip_lengths = {}
+    for key, item in to_dict.items():
+        dict_of_tip_lengths[key] = json.loads(item.json())
+    return dict_of_tip_lengths
+
+
 def tip_lengths_for_pipette(
     pipette_id: str,
 ) -> typing.Dict[str, v1.TipLengthModel]:
@@ -133,11 +144,7 @@ def delete_tip_length_calibration(tiprack: str, pipette_id: str) -> None:
         del tip_lengths[tiprack]
         tip_length_dir = config.get_tip_length_cal_path()
         if tip_lengths:
-            # This is a workaround since pydantic doesn't have a nice way to
-            # add encoders when converting to a dict.
-            dict_of_tip_lengths = {}
-            for key, item in tip_lengths.items():
-                dict_of_tip_lengths[key] = json.loads(item.json())
+            dict_of_tip_lengths = _conver_tip_length_model_to_dict(tip_lengths)
             io.save_to_file(tip_length_dir, pipette_id, dict_of_tip_lengths)
         else:
             io.delete_file(tip_length_dir / f"{pipette_id}.json")
@@ -228,9 +235,5 @@ def save_tip_length_calibration(
 
     all_tip_lengths.update(tip_length_cal)
 
-    # This is a workaround since pydantic doesn't have a nice way to
-    # add encoders when converting to a dict.
-    dict_of_tip_lengths = {}
-    for key, item in all_tip_lengths.items():
-        dict_of_tip_lengths[key] = json.loads(item.json())
+    dict_of_tip_lengths = _conver_tip_length_model_to_dict(all_tip_lengths)
     io.save_to_file(tip_length_dir_path, pip_id, dict_of_tip_lengths)
