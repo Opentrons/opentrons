dcdc87f4877654f738867ae76173a23633adb71c
vegano1 <brayan.almonte@gmail.com>
2023-02-20 19:26:27 -0500

    fix(update-server): Check the update size before writting to the unused partition. We can generate updates that are larger than the available space on the rootfs partition, since we do no validation on the size, we can brick the device by writting to the unused partition + whatever other critical files get overwritten.

--- update-server/tests/openembedded/test_updater.py
+++ update-server/tests/openembedded/test_updater.py
@@ -27,6 +27,7 @@ def test_update_valid_part_switch(
         root_FS_intf=mock_root_fs_interface,
         part_mngr=mock_partition_manager_valid_switch,
     )
+    mock_root_fs_interface.write_update = mock.Mock(return_value=(True, ""))
     # lambda just mocks progress callback,
     # 2 here has no significance whatsoever.
     updater.decomp_and_write("/mmc/blk0p1", lambda x: x(2))
@@ -53,6 +54,7 @@ def test_update_invalid_part_switch(
         part_mngr=mock_partition_manager_invalid_switch,
     )
 
+    mock_root_fs_interface.write_update = mock.Mock(return_value=(True, ""))
     updater.decomp_and_write("/mmc/blk0p1", lambda x: x(2))
     mock_partition_manager_invalid_switch.find_unused_partition.assert_called()
     mock_root_fs_interface.write_update.assert_called()
@@ -85,6 +87,7 @@ def test_decomp_and_write(
         part_mngr=mock_partition_manager_valid_switch,
     )
 
+    mock_root_fs_interface.write_update = mock.Mock(return_value=(True, ""))
     updater.decomp_and_write("test_path", lambda x: x(2))
     mock_partition_manager_valid_switch.find_unused_partition.assert_called()
     mock_root_fs_interface.write_update.assert_called()
@@ -127,9 +130,15 @@ def test_lzma(testing_partition, tmpdir):
             total_size += len(chunk)
             if len(chunk) != chunk_size:
                 break
-    root_FS_intf.write_update(rfs_path, p, cb, chunk_size)
-    if total_size % chunk_size != 0:
-        calls = int(total_size / chunk_size) + 1
-    else:
-        calls = total_size / chunk_size
-    assert cb.call_count == calls
+    with mock.patch(
+        "otupdate.openembedded.update_actions.PartitionManager.get_partition_size",
+        mock.Mock(return_value=99999999),
+    ):
+        success, msg = root_FS_intf.write_update(rfs_path, p, cb, chunk_size)
+        if total_size % chunk_size != 0:
+            calls = int(total_size / chunk_size) + 1
+        else:
+            calls = total_size / chunk_size
+        assert cb.call_count == calls
+        assert success
+        assert msg == ""
