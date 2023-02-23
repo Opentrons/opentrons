d5ea206a1480408c815b751c9406769711c01701
Ryan howard <ryan.howard@opentrons.com>
2023-02-23 13:07:24 -0500

    change unit tests to purely python

--- hardware/tests/opentrons_hardware/drivers/binary_usb/test_driver.py
+++ hardware/tests/opentrons_hardware/drivers/binary_usb/test_driver.py
@@ -1,43 +1,79 @@
 """USB Driver tests."""
-import os
-import subprocess
 import serial  # type: ignore[import]
 import time
+from contextlib import ExitStack
+import multiprocessing
+import os
+import pty
+from selectors import DefaultSelector as Selector, EVENT_READ
+import tty
+import io
 from opentrons_hardware.drivers.binary_usb import SerialUsbDriver
 
-from typing import AsyncGenerator
+from typing import AsyncGenerator, List
 import pytest
 import asyncio
 from opentrons_hardware.firmware_bindings.messages.binary_message_definitions import Ack
 
-# this script lets you emulate a serial device
-# the client program should use the serial port file specifed by client_port
 
-# if the port is a location that the user can't access (ex: /dev/ttyUSB0 often),
-# sudo is required
+def run(primary_files_fd: List[int], primary_files_obj: List[io.FileIO]) -> None:
+    """Creates several serial ports.
+
+    When data is received from one port, sends to all the other ports.
+    """
+    print(type(primary_files_obj[0]))
+    primary_files = {}
+
+    for i in range(2):
+        primary_files[primary_files_fd[i]] = primary_files_obj[i]
+
+    with Selector() as selector, ExitStack() as stack:
+        # Context manage all the primary file objects, and add to selector.
+        for fd, f in primary_files.items():
+            stack.enter_context(f)
+            selector.register(fd, EVENT_READ)
+        while True:
+            for key, events in selector.select():
+                if not events & EVENT_READ:
+                    continue
+
+                data = primary_files[int(key.fd)].read()
+                # Write to primary files.
+                for fd, f in primary_files.items():
+                    # Don't write to the sending file.
+                    if fd != key.fileobj:
+                        f.write(data)
 
 
 class SerialEmulator(object):
     """Test fixture for emulating serial connections."""
 
     def __init__(
-        self, device_port: str = "./ttydevice", client_port: str = "./ttyclient"
+        self,
     ) -> None:
         """Open some serial ports to simulate the usb connection."""
-        self.device_port = device_port
-        self.client_port = client_port
-        cmd = [
-            "/usr/bin/socat",
-            "-d",
-            "-d",
-            "PTY,link=%s,raw,echo=0" % self.device_port,
-            "PTY,link=%s,raw,echo=0" % self.client_port,
-        ]
-        self.proc = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        self.primary_files_fd = []  # Dict of primary fd to primary file object.
+        self.primary_files = []
+        self.secondary_names = []  # Dict of primary fd to secondary name.
+        for i in range(2):
+            primary_fd, secondary_fd = pty.openpty()
+            tty.setraw(primary_fd)
+            os.set_blocking(primary_fd, False)
+            secondary_name = os.ttyname(secondary_fd)
+            self.primary_files_fd.append(primary_fd)
+            self.primary_files.append(open(primary_fd, "r+b", buffering=0))
+            self.secondary_names.append(secondary_name)
+
+        self.device_port = self.secondary_names[0]
+        self.client_port = self.secondary_names[1]
+        self.proc = multiprocessing.Process(
+            target=run, args=(self.primary_files_fd, self.primary_files)
         )
+        self.proc.start()
         time.sleep(1)
-        self.serial = serial.Serial(self.device_port, 9600, rtscts=True, dsrdtr=True)
+        self.serial = serial.Serial(
+            self.device_port, 9600, rtscts=True, dsrdtr=True, timeout=1
+        )
         self.err = ""
         self.out = ""
 
@@ -57,13 +93,7 @@ class SerialEmulator(object):
 
     def stop(self) -> None:
         """Disconnect and remove the serial ports."""
-        self.proc.kill()
-        self.proc.communicate()
-        try:
-            os.remove(self.device_port)
-            os.remove(self.client_port)
-        finally:
-            pass
+        self.proc.terminate()
 
 
 @pytest.fixture
@@ -75,7 +105,9 @@ def test_port_host() -> SerialEmulator:
 @pytest.fixture
 def test_port_client(test_port_host: SerialEmulator) -> serial.Serial:
     """Fixture for the serial port that will get passed to the driver."""
-    return serial.Serial(test_port_host.client_port, 9600, rtscts=True, dsrdtr=True)
+    return serial.Serial(
+        test_port_host.client_port, 9600, rtscts=True, dsrdtr=True, timeout=1
+    )
 
 
 @pytest.fixture
@@ -106,7 +138,6 @@ async def test_recv(subject: SerialUsbDriver, test_port_host: SerialEmulator) ->
     length = test_port_host.write(b"\x00\x01\x00\x00")
     assert length == 4
     message = await subject.read()
-    print(message)
     assert type(message) == Ack
 
     assert message == Ack()
