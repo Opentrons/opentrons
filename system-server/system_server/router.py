ddfe79d7ebf288a298dad8c6d99e7e6d5f23f0b8
Frank Sinapi <frank.sinapi@opentrons.com>
2023-02-24 13:33:33 -0500

    feat(system-server): add POST /system/register route (#12153)
    * Added HTTP endpoint to register a new JWT
    * Added database integration for saving/loading/deleting a JWT
    * Removed fake testing endpoint
    * added Tavern to system server dev packages
    * Added a Tavern integration test for /system/register
    * Moved JWT algorithm into a constant
    * Evaluate current time when function is called, not when Python module is loaded
    
    ---------
    
    Co-authored-by: Max Marrone <max@opentrons.com>

--- system-server/system_server/router.py
+++ system-server/system_server/router.py
@@ -1,23 +1,10 @@
 """Application routes."""
-from fastapi import APIRouter, Depends, responses
-from system_server.persistence import get_sql_engine, get_uuid
-import sqlalchemy
-from uuid import UUID
+from fastapi import APIRouter
+from system_server.system.router import system_router
 
 router = APIRouter()
 
-
-# TODO(fs, 02-02-2022): This is a placeholder just to refresh the database for
-# testing before there are any real HTTP requests using it.
-# Delete this as soon as a real HTTP request gets added!!!!!!
-@router.put(
-    "/system/refresh",
-    summary="Refresh the databases",
-    description=("Refresh the database"),
+router.include_router(
+    router=system_router,
+    tags=["System"],
 )
-async def refresh_system_db(
-    sql: sqlalchemy.engine.Engine = Depends(get_sql_engine),
-    uuid: UUID = Depends(get_uuid),
-) -> responses.PlainTextResponse:
-    """Silly fake endpoint to refresh our database."""
-    return responses.PlainTextResponse(f"OK: {uuid}\n")
