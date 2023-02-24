12403d25e01e649aa845ae2b4edc52c6ae24584d
Brent Hagen <brent.hagen@opentrons.com>
2023-02-24 14:17:53 -0500

    add deck map, MoaM detection, placeholder buttons, modals, and module mismatch banner

--- app/src/organisms/ProtocolSetupModules/utils.ts
+++ app/src/organisms/ProtocolSetupModules/utils.ts
@@ -1,16 +1,16 @@
 import { checkModuleCompatibility } from '@opentrons/shared-data'
 
-import type { LoadedModule, RunTimeCommand } from '@opentrons/shared-data'
+import type { ProtocolModuleInfo } from '../../organisms/Devices/ProtocolRun/utils/getProtocolModulesInfo'
 import type { AttachedModule } from '../../redux/modules/types'
 
-export type AttachedProtocolModuleMatch = LoadedModule & {
+export type AttachedProtocolModuleMatch = ProtocolModuleInfo & {
   attachedModuleMatch: AttachedModule | null
 }
 
 // some logic copied from useModuleRenderInfoForProtocolById
 export function getAttachedProtocolModuleMatches(
   attachedModules: AttachedModule[],
-  protocolModulesInLoadOrder: LoadedModule[]
+  protocolModulesInLoadOrder: ProtocolModuleInfo[]
 ): AttachedProtocolModuleMatch[] {
   const matchedAttachedModules: AttachedModule[] = []
   const attachedProtocolModuleMatches = protocolModulesInLoadOrder.map(
@@ -20,7 +20,7 @@ export function getAttachedProtocolModuleMatches(
           attachedModule =>
             checkModuleCompatibility(
               attachedModule.moduleModel,
-              protocolModule.model
+              protocolModule.moduleDef.model
             ) &&
             // check id instead of object reference in useModuleRenderInfoForProtocolById
             matchedAttachedModules.find(
@@ -42,28 +42,4 @@ export function getAttachedProtocolModuleMatches(
     }
   )
   return attachedProtocolModuleMatches
-}
-
-// DONT NEED THIS MODULES ALREADY IN ORDER
-export function getProtocolModulesInLoadOrder(
-  modules: LoadedModule[],
-  commands: RunTimeCommand[]
-): LoadedModule[] {
-  const protocolModulesInLoadOrder = commands
-    .filter(command => command.commandType === 'loadModule')
-    .map(command => {
-      const moduleForCommand = modules.find(
-        // assumption: id from LoadedModule and moduleId from command result are the same entity
-        // existing helper getModuleInitialLoadInfo compares moduleId from LegacySchemaAdapterOutput to moduleId from command result
-        module => command.result.moduleId === module.id
-      )
-      if (moduleForCommand == null) {
-        throw new Error(
-          'expected to be able to find module location, but could not'
-        )
-      }
-      return moduleForCommand
-    })
-
-  return protocolModulesInLoadOrder
 }
