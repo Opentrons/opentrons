e59fa8f9c6d0f2eda72000765f1751cb174ff5ec
Max Marrone <max@opentrons.com>
2023-02-24 14:09:51 -0500

    Test that modules without labware are mapped.

--- api/tests/opentrons/protocol_api/core/engine/test_deck_conflict.py
+++ api/tests/opentrons/protocol_api/core/engine/test_deck_conflict.py
@@ -5,7 +5,8 @@ from opentrons_shared_data.labware.dev_types import LabwareUri
 
 from opentrons.motion_planning import deck_conflict as wrapped_deck_conflict
 from opentrons.protocol_api.core.engine import deck_conflict
-from opentrons.protocol_engine import DeckSlotLocation, StateView
+from opentrons.protocol_engine import DeckSlotLocation, ModuleModel, StateView
+from opentrons.protocol_engine.errors import LabwareNotLoadedOnModuleError
 from opentrons.types import DeckSlotName
 
 
@@ -44,8 +45,7 @@ def test_maps_labware_on_deck(decoy: Decoy, mock_state_view: StateView) -> None:
 
     # Test both ways that the subject's caller can provide a labware:
     # by new_labware_id, and by existing_labware_ids.
-    # We reuse the same labware-id for test convenience;
-    # in the real world, they'd be different.
+    # We reuse the same ID for test convenience; in the real world, they'd be different.
     deck_conflict.check(
         engine_state=mock_state_view,
         existing_labware_ids=["labware-id"],
@@ -74,7 +74,44 @@ def test_maps_labware_on_deck(decoy: Decoy, mock_state_view: StateView) -> None:
 
 
 def test_maps_module_without_labware(decoy: Decoy, mock_state_view: StateView) -> None:
-    raise NotImplementedError
+    decoy.when(mock_state_view.labware.get_id_by_module("module-id")).then_raise(
+        LabwareNotLoadedOnModuleError()
+    )
+    decoy.when(mock_state_view.modules.get_overall_height("module-id")).then_return(
+        3.14159
+    )
+
+    decoy.when(mock_state_view.modules.get_model("module-id")).then_return(
+        ModuleModel.HEATER_SHAKER_MODULE_V1
+    )
+    decoy.when(mock_state_view.modules.get_location("module-id")).then_return(
+        DeckSlotLocation(slotName=DeckSlotName.SLOT_5)
+    )
+
+    # Test both ways that the subject's caller can provide a module:
+    # by new_module_id, and by existing_module_ids.
+    # We reuse the same ID for test convenience; in the real world, they'd be different.
+    deck_conflict.check(
+        engine_state=mock_state_view,
+        existing_labware_ids=[],
+        existing_module_ids=["module-id"],
+        new_module_id="module-id",
+    )
+    decoy.verify(
+        wrapped_deck_conflict.check(
+            existing_items={
+                5: wrapped_deck_conflict.HeaterShakerModule(
+                    name_for_errors="heaterShakerModuleV1",
+                    highest_z_including_labware=3.14159,
+                )
+            },
+            new_item=wrapped_deck_conflict.HeaterShakerModule(
+                name_for_errors="heaterShakerModuleV1",
+                highest_z_including_labware=3.14159,
+            ),
+            new_location=5,
+        )
+    )
 
 
 def test_maps_module_with_labware(decoy: Decoy, mock_state_view: StateView) -> None:
