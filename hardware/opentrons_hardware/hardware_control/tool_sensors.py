1a0153b496a908c4c7cf3df50eda1df43cbbc9d2
Carlos <fernandez.carlos40@gmail.com>
2023-03-01 15:25:48 -0500

    revert this back to acceleration = 0

--- hardware/opentrons_hardware/hardware_control/tool_sensors.py
+++ hardware/opentrons_hardware/hardware_control/tool_sensors.py
@@ -37,7 +37,7 @@ def _build_pass_step(
     movers: List[NodeId],
     distance: Dict[NodeId, float],
     speed: Dict[NodeId, float],
-    acceleration: Dict[NodeId] = {},
+    acceleration: Dict[NodeId, float] = {},
     stop_condition: MoveStopCondition = MoveStopCondition.sync_line,
 ) -> MoveGroupStep:
     return create_step(
@@ -45,7 +45,7 @@ def _build_pass_step(
         velocity={
             ax: float64(speed[ax] * copysign(1.0, distance[ax])) for ax in movers
         },
-        acceleration=acceleration,
+        acceleration={},
         # use any node present to calculate duration of the move, assuming the durations
         #   will be the same
         duration=float64(abs(distance[movers[0]] / speed[movers[0]])),
@@ -80,8 +80,8 @@ async def liquid_probe(
     sensor_group = _build_pass_step(
         movers=[head_node, tool],
         distance={head_node: max_z_distance, tool: max_z_distance},
-        speed={head_node: mount_speed/10, tool: plunger_speed/10},
-        acceleration={head_node: mount_speed/10, tool: plunger_speed/10},
+        speed={head_node: mount_speed, tool: plunger_speed},
+        acceleration={head_node: mount_speed, tool: plunger_speed},
         stop_condition=MoveStopCondition.sync_line,
     )
 
