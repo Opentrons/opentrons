0af83816787f9941f8b3522718bf7904ce7c9b1a
Frank Sinapi <frank.sinapi@opentrons.com>
2023-03-01 16:19:53 -0500

    Added a GET /system/authorize endpoint to check token validitiy

--- system-server/system_server/service/check_jwt_headers.py
+++ system-server/system_server/service/check_jwt_headers.py
@@ -4,7 +4,7 @@ from uuid import UUID
 import logging
 
 from system_server.jwt import jwt_is_valid
-from system_server.constants import REGISTRATION_AUDIENCE
+from system_server.constants import REGISTRATION_AUDIENCE, AUTHORIZATION_AUDIENCE
 from system_server.persistence import get_persistent_uuid
 
 _log = logging.getLogger(__name__)
@@ -39,3 +39,34 @@ async def get_registration_token_header(request: Request) -> str:
     ), "No authentication_bearer in request state; is endpoint properly configured?"
 
     return request.state.authentication_bearer
+
+
+async def check_authorization_token_header(
+    request: Request,
+    authenticationBearer: str = Header(
+        ...,
+        description="An authentication header bearing a token provided by the /system/authorize endpoint.",
+    ),
+    signing_key: UUID = Depends(get_persistent_uuid),
+) -> None:
+    """A header requirement that requests a authorization token from /system/authorize."""
+    _log.info(f"Verifying authorization token: {authenticationBearer}")
+    if not jwt_is_valid(
+        signing_key=str(signing_key),
+        token=authenticationBearer,
+        audience=AUTHORIZATION_AUDIENCE,
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Authorization token invalid",
+        )
+    request.state.authorization_authentication_bearer = authenticationBearer
+
+
+async def get_authorization_token_header(request: Request) -> str:
+    """Gets the authorization token from a request."""
+    assert isinstance(
+        request.state.authorization_authentication_bearer, str
+    ), "No authorization_authentication_bearer in request state; is endpoint properly configured?"
+
+    return request.state.authorization_authentication_bearer
--- system-server/system_server/system/authorize/router.py
+++ system-server/system_server/system/authorize/router.py
@@ -1,11 +1,14 @@
 """Router for all /system/ endpoints."""
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Response, Query
 from uuid import UUID
+from typing import List, Optional
 
 from system_server.persistence import get_persistent_uuid
 from system_server.service.check_jwt_headers import (
     check_registration_token_header,
     get_registration_token_header,
+    check_authorization_token_header,
+    get_authorization_token_header,
 )
 from .models import PostAuthorizeResponse
 from .authorization import authorize_token
@@ -28,3 +31,22 @@ async def authorize(
     """Router for /system/authorize endpoint."""
     authorization = authorize_token(token, str(signing_uuid))
     return PostAuthorizeResponse(token=authorization)
+
+
+@authorize_router.get(
+    "/system/authorize",
+    summary="Verify an authorization token.",
+    dependencies=[Depends(check_authorization_token_header)],
+    responses={
+        status.HTTP_200_OK: {"description": "the authorization is valid"},
+        status.HTTP_403_FORBIDDEN: {"description": "the authorization token is not valid for the given scopes"}
+    }
+)
+async def check_authorization(
+    token: str = Depends(get_authorization_token_header),
+    signing_uuid: UUID = Depends(get_persistent_uuid),
+    scopes: Optional[List[str]] = Query(
+        None, description="List of scopes to verify token access to."
+    ),
+) -> None:
+    return Response(status_code=status.HTTP_200_OK)
