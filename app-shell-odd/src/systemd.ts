ddb0eec4a163fc89317b71c0408f70b2d4cc31ef
Seth Foster <seth@opentrons.com>
2023-03-10 14:26:48 -0500

    further conditional import fixes

--- app-shell-odd/src/systemd.ts
+++ app-shell-odd/src/systemd.ts
@@ -1,24 +1,36 @@
 import { platform } from 'process'
+// Provide systemd when possible and a default mocked instance, used only during
+// dev workflows, when not.
 
-async function provideLinuxExports() {
-  const sdNotify = await import('sd-notify')
-  return { ready: sdNotify.ready, sendStatus: sdNotify.sendStatus }
+interface SDNotify {
+  ready: () => void
+  sendStatus: (text: string) => void
 }
 
-async function provideMockExports() {
-  return {
-    ready: () => console.log('would send sd-notify ready'),
-    sendStatus: (text: string) =>
-      console.log(`would send sd-notify status ${text}`),
+const provideExports = (): SDNotify => {
+  try {
+    // This has to be a require because import is async when used functionally,
+    // and to catch import errors you have to use it functionally, so it can't be
+    // at top level, and we're doing this to put stuff in exports, so let's
+    // refactor this whenever we turn on top-level async
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const systemdNotify = require('sd-notify')
+    return { ready: systemdNotify.ready, sendStatus: systemdNotify.sendStatus }
+  } catch (err) {
+    if (platform === 'linux') {
+      console.error(
+        'Could not import systemd on linux, where it should be present. This is most likely because libsystemd bindings are not available, which hopefully means this is a dev setup.'
+      )
+    }
+    return {
+      ready: () => console.log('would send sd-notify ready'),
+      sendStatus: (text: string) =>
+        console.log(`would send sd-notify status ${text}`),
+    }
   }
 }
 
-async function provideExportsForPlatform(forPlatform: 'linux' | string) {
-  if (forPlatform === 'linux') {
-    return await provideLinuxExports()
-  } else {
-    return await provideMockExports()
-  }
-}
-
-export default await provideExportsForPlatform(platform)
+// Finally, we want this to work just like the actual sd-notify imports, so a default
+// export it is
+// eslint-disable-next-line import/no-default-export
+export default provideExports()
