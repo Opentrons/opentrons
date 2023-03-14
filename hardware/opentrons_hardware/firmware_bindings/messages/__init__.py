8eb555a86fd04a36ac06c5dfe9740bebc9d27ca0
Ryan howard <ryan.howard@opentrons.com>
2023-03-14 12:49:04 -0400

    add a test to network for usb responses

--- hardware/opentrons_hardware/firmware_bindings/messages/__init__.py
+++ hardware/opentrons_hardware/firmware_bindings/messages/__init__.py
@@ -1,4 +1,5 @@
 """Can bus message definitions."""
 from .messages import MessageDefinition, get_definition
+from .binary_message_definitions import BinaryMessageDefinition, get_binary_definition
 
-__all__ = ["MessageDefinition", "get_definition"]
+__all__ = ["MessageDefinition", "get_definition", "BinaryMessageDefinitions", "get_binary_definition"]
