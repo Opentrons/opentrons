34a371da3fcad9dadb6801424dfc2efa236bc936
Jethary Rader <jethary@opentrons.com>
2023-03-14 10:29:12 -0400

    cleanup small button imports, clean up MediumButtonRounded storybook name

---
+++ app/src/atoms/buttons/OnDeviceDisplay/MediumButtonRounded.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { MediumButtonRounded } from '.'
+import type { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'ODD/Atoms/MediumButtonRounded',
+  argTypes: { onClick: { action: 'clicked' } },
+} as Meta
+
+const MediumButtonRoundedTemplate: Story<
+  React.ComponentProps<typeof MediumButtonRounded>
+> = args => <MediumButtonRounded {...args} />
+export const MediumRounded = MediumButtonRoundedTemplate.bind({})
+MediumRounded.args = {
+  children: 'Button text',
+  title: 'medium button rounded',
+}
