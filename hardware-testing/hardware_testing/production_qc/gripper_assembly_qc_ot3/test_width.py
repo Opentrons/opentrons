e95a2a870c5f576409c90900a3a3cd51ff887334
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2023-03-15 13:59:26 -0400

    add test_width script

--- hardware-testing/hardware_testing/production_qc/gripper_assembly_qc_ot3/test_width.py
+++ hardware-testing/hardware_testing/production_qc/gripper_assembly_qc_ot3/test_width.py
@@ -3,18 +3,63 @@ from typing import List, Union
 
 from opentrons.hardware_control.ot3api import OT3API
 
+from hardware_testing.data import ui
 from hardware_testing.data.csv_report import (
     CSVReport,
+    CSVResult,
     CSVLine,
     CSVLineRepeating,
 )
 
+from hardware_testing.opentrons_api import helpers_ot3
+from hardware_testing.opentrons_api.types import OT3Axis, OT3Mount, Point
+
+
+FAILURE_THRESHOLD_MM = 3
+GRIP_HEIGHT = 30
+
+WIDTHS: List[float] = [60, 80]
+GRIP_FORCES: List[float] = [5, 15, 20]
+
+def _get_test_tag(width: float, force: float):
+    return f"{width}mm-{force}N"
+
 
 def build_csv_lines() -> List[Union[CSVLine, CSVLineRepeating]]:
     """Build CSV Lines."""
-    return []
+    lines: List[Union[CSVLine, CSVLineRepeating]] = list()
+    for width in WIDTHS:
+        for force in GRIP_FORCES:
+            tag = _get_test_tag(width, force)
+            lines.append(CSVLine(tag, [float, float, CSVResult]))
+    return lines
 
 
 async def run(api: OT3API, report: CSVReport, section: str) -> None:
     """Run."""
-    return
+    z_ax = OT3Axis.Z_G
+    g_ax = OT3Axis.G
+    mount = OT3Mount.GRIPPER
+    jaw_width = api._gripper_handler.get_gripper().config.geometry.jaw_width["max"]
+
+    async def _save_result(tag: str, expected: float) -> None:
+        await api.refresh_positions()
+        jaw_disp = api._encoder_position[g_ax]
+        actual =  jaw_width - jaw_disp * 2
+        result = CSVResult.from_bool(abs(expected - actual) < FAILURE_THRESHOLD_MM)
+        report(section, tag, [expected, actual, result])
+
+    for width in WIDTHS:
+        for force in GRIP_FORCES:
+            ui.print_header(f"Width(mm): {width}, Force(N): {force}")
+            print("homing z and g...")
+            await api.home([z_ax, g_ax])
+            print("moving down")
+            await api.move_rel(mount, Point(z=GRIP_HEIGHT))
+            print("gripping at {force}N")
+            await api.grip(force)
+            await _save_result(_get_test_tag(width, force), width)
+            print("ungrip")
+            await api.ungrip()
+            print("homing...")
+            await api.home([z_ax])
