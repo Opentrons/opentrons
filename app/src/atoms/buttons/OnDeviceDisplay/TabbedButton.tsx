1c14da3ee2c4958e8970f29bb80a82bfb1a3790b
Eric Wagoner <eric.wagoner@iinteractive.com>
2023-03-16 10:35:26 -0400

    feat(app): create new odd tabbed button component (#12283)
    * Added new pressed purple color [RAUT-362]
    
    * Added new tabbed button odd component [RAUT-362]
    
    * Changed styling to match new SubNavigation button design [RAUT-367]
    
    * Directory re-naming [RAUT-362]
    
    * Styling tweaks to resolve misunderstandings and typos in Firma [RAUT-362]

---
+++ app/src/atoms/buttons/OnDeviceDisplay/TabbedButton.tsx
@@ -0,0 +1,66 @@
+import styled, { css } from 'styled-components'
+import {
+  Btn,
+  BORDERS,
+  COLORS,
+  NewPrimaryBtn,
+  SPACING,
+  styleProps,
+  TYPOGRAPHY,
+} from '@opentrons/components'
+
+const FOREGROUND_STYLES = css`
+  &:focus,
+  &:hover {
+    background-color: ${COLORS.highlightPurple_one};
+  }
+
+  &:active {
+    background-color: ${COLORS.highlightPurple_one_pressed};
+  }
+`
+
+const BACKGROUND_STYLES = css`
+  background-color: ${COLORS.highlightPurple_two};
+  color: ${COLORS.darkBlack_hundred};
+
+  &:focus,
+  &:hover {
+    background-color: ${COLORS.highlightPurple_two};
+  }
+
+  &:active {
+    background-color: ${COLORS.highlightPurple_two_pressed};
+  }
+`
+
+interface TabbedButtonProps extends React.ComponentProps<typeof Btn> {
+  foreground?: boolean
+}
+
+export const TabbedButton = styled(NewPrimaryBtn)<TabbedButtonProps>`
+  ${props =>
+    css`
+      background-color: ${COLORS.highlightPurple_one};
+      border-radius: ${BORDERS.size_four};
+      box-shadow: none;
+      font-size: ${TYPOGRAPHY.fontSize22};
+      font-weight: ${TYPOGRAPHY.fontWeightSemiBold};
+      line-height: ${TYPOGRAPHY.lineHeight28};
+      padding: ${SPACING.spacing4} ${SPACING.spacing5};
+      text-transform: ${TYPOGRAPHY.textTransformNone};
+
+      ${props.foreground === true ? FOREGROUND_STYLES : BACKGROUND_STYLES}
+
+      ${styleProps}
+
+      &:focus,
+      &:focus-visible {
+        box-shadow: 0 0 0 3px ${COLORS.fundamentalsFocus};
+      }
+      &:disabled {
+        background-color: ${COLORS.darkBlack_twenty};
+        color: ${COLORS.darkBlack_sixty};
+      }
+    `}
+`
