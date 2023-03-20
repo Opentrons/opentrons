ea431e1acdf1e14511b097a3033c7b7257ab0119
Jethary Rader <jethary@opentrons.com>
2023-03-20 15:35:54 -0400

    remove console.log

--- api-client/src/protocols/utils.ts
+++ api-client/src/protocols/utils.ts
@@ -256,7 +256,6 @@ export function parseLabwareInfoByLiquidId(
             command.commandType === 'loadLiquid'
         )
       : []
-console.log(loadLiquidCommands)
   return reduce<LoadLiquidRunTimeCommand, LabwareByLiquidId>(
     loadLiquidCommands,
     (acc, command) => {
