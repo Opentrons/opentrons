fa2f8c3e57db58085d54f6ca94809aa8f1fb543d
Jethary Rader <jethary@opentrons.com>
2023-03-23 12:40:00 -0400

    chore(add i18n formatting function): add i18n formatting function to format strings

--- app/src/i18n.ts
+++ app/src/i18n.ts
@@ -1,4 +1,6 @@
 import i18n from 'i18next'
+import startCase from 'lodash/startCase'
+import capitalize from 'lodash/capitalize'
 import { initReactI18next } from 'react-i18next'
 import { resources } from './assets/localization'
 
@@ -20,6 +22,12 @@ i18n.use(initReactI18next).init(
     defaultNS: 'shared',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+      format: function (value, format, lng) {
+        if (format === 'uppercase') return value.toUpperCase()
+        if (format === 'capitalize') return capitalize(value)
+        if (format === 'sentenceCase') return startCase(value)
+        return value
+      },
     },
     keySeparator: false, // use namespaces and context instead
     saveMissing: true,
