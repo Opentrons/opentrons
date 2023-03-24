14cca4cc7e60c2817b849fe3a56440aba3515238
Jethary Rader <jethary@opentrons.com>
2023-03-23 20:15:33 -0400

    clean up titleCase file

--- shared-data/js/titleCase.ts
+++ shared-data/js/titleCase.ts
@@ -8,22 +8,17 @@ export function titleCase(input: string) {
   let result = ''
   let m: RegExpExecArray | null
 
-  // tslint:disable-next-line
   while ((m = TOKENS.exec(input)) !== null) {
     const { 0: token, index } = m
 
     if (
-      // Ignore already capitalized words.
       !IS_MANUAL_CASE.test(token) &&
-      // Ignore small words except at beginning or end.
       (!SMALL_WORDS.test(token) ||
         index === 0 ||
         index + token.length === input.length) &&
-      // Ignore URLs.
       (input.charAt(index + token.length) !== ':' ||
         WHITESPACE.test(input.charAt(index + token.length + 1)))
     ) {
-      // Find and uppercase first word character, skips over *modifiers*.
       result += token.replace(ALPHANUMERIC_PATTERN, m => m.toUpperCase())
       continue
     }
