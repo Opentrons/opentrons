7cd6d93962e07cf345f77527719aa51dcd1dd237
Frank Sinapi <frank.sinapi@opentrons.com>
2023-03-28 10:14:17 -0400

    Updated tests to document how to mock in a response from the binary messenger

--- hardware/tests/opentrons_hardware/hardware_control/test_rear_panel_settings.py
+++ hardware/tests/opentrons_hardware/hardware_control/test_rear_panel_settings.py
@@ -45,24 +45,37 @@ def mock_binary_messenger(mock_usb_driver: AsyncMock) -> BinaryMessenger:
     return msg
 
 
+async def prepare_mock_response(
+    messenger: BinaryMessenger, driver: AsyncMock, value: BinaryMessageDefinition
+) -> None:
+    """Mock getting a response from the binary messenger.
+
+    The messenger has a dedicated reader task that uses the driver as an async
+    iterator. In order to correctly mock getting a message from the driver, we
+    should stop the reader task, load the driver iterator with the value we want,
+    and then restart the async reader task. The next time the readre has a chance
+    to run, it will read the value we specified.
+    """
+    await messenger.stop()
+    driver.__aiter__.return_value = [value]
+    messenger.start()
+
+
 @pytest.mark.parametrize("set", [True, False])
 async def test_set_deck_light(
     mock_binary_messenger: BinaryMessenger, mock_usb_driver: AsyncMock, set: bool
 ) -> None:
     """Test setting the deck light."""
-    # start() MUST be called in the test, not the fixture
-    mock_binary_messenger.start()
-
     expected = SetDeckLightRequest(setting=UInt8Field(set))
 
     # Correct response = good
-    mock_usb_driver.__aiter__.return_value = [Ack()]
+    await prepare_mock_response(mock_binary_messenger, mock_usb_driver, Ack())
     assert await set_deck_light(set, mock_binary_messenger)
     mock_usb_driver.write.assert_called_once_with(message=expected)
     mock_usb_driver.write.reset_mock()
 
     # Incorrect response = bad
-    mock_usb_driver.__aiter__.return_value = [AckFailed()]
+    await prepare_mock_response(mock_binary_messenger, mock_usb_driver, AckFailed())
     assert not await set_deck_light(set, mock_binary_messenger)
     mock_usb_driver.write.assert_called_once_with(message=expected)
 
@@ -72,20 +85,18 @@ async def test_get_deck_light(
     mock_binary_messenger: BinaryMessenger, mock_usb_driver: AsyncMock, set: bool
 ) -> None:
     """Test getting the deck light."""
-    mock_binary_messenger.start()
-
     # Correct response
-    mock_usb_driver.__aiter__.return_value = [
-        GetDeckLightResponse(setting=UInt8Field(set))
-    ]
-
+    await prepare_mock_response(
+        mock_binary_messenger,
+        mock_usb_driver,
+        GetDeckLightResponse(setting=UInt8Field(set)),
+    )
     assert await get_deck_light_state(mock_binary_messenger) == set
 
     mock_usb_driver.write.assert_called_once_with(message=GetDeckLightRequest())
 
     # Error getting a response should default to False
-    mock_usb_driver.__aiter__.return_value = [AckFailed()]
-
+    await prepare_mock_response(mock_binary_messenger, mock_usb_driver, AckFailed())
     assert await get_deck_light_state(mock_binary_messenger) is False
 
 
@@ -94,18 +105,15 @@ async def test_get_door_state(
     mock_binary_messenger: BinaryMessenger, mock_usb_driver: AsyncMock, state: bool
 ) -> None:
     """Test getting the deck light."""
-    mock_binary_messenger.start()
-
     # Correct response
-    mock_usb_driver.__aiter__.return_value = [
-        DoorSwitchStateInfo(door_open=UInt8Field(state))
-    ]
-
+    await prepare_mock_response(
+        mock_binary_messenger,
+        mock_usb_driver,
+        DoorSwitchStateInfo(door_open=UInt8Field(state)),
+    )
     assert await get_door_state(mock_binary_messenger) == state
-
     mock_usb_driver.write.assert_called_once_with(message=DoorSwitchStateRequest())
 
     # Error getting a response should default to False
-    mock_usb_driver.__aiter__.return_value = [AckFailed()]
-
+    await prepare_mock_response(mock_binary_messenger, mock_usb_driver, AckFailed())
     assert await get_door_state(mock_binary_messenger) is False
