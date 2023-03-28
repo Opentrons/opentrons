30cac6f8b0e2f322786efed907b48637f7135cab
Max Marrone <max@opentrons.com>
2023-03-28 16:47:59 -0400

    Just fix quoting.

--- scripts/push.mk
+++ scripts/push.mk
@@ -3,7 +3,7 @@
 find_robot=$(shell yarn run -s discovery find -i 169.254)
 default_ssh_key := ~/.ssh/robot_key
 default_ssh_opts := -o stricthostkeychecking=no -o userknownhostsfile=/dev/null
-is-ot3 = $(shell ssh $(if $(2),"-i $(2)") $(3) root@$(1) systemctl status opentrons-robot-app)
+is-ot3 = $(shell ssh $(if $(2),-i $(2)) $(3) root@$(1) systemctl status opentrons-robot-app)
 # make version less than 4.4 do not use intcmp
 allowed-ssh-versions="1 2 3 4 5 6 7 8"
 # in order to use comma in a string we have to set it to a var
@@ -35,8 +35,8 @@ $(if $(is-windows), echo "when using windows with an openSSH version larger then
 
 define push-python-package
 $(if $(is-ot3), echo "This is an OT-3. Use 'make push-ot3' instead." && exit 1)
-scp $(if $(2),"-i $(2)") $(scp-legacy-option-flag) $(3) "$(4)" root@$(1):/data/$(notdir $(4))
-ssh $(if $(2),"-i $(2)") $(3) root@$(1) \
+scp $(if $(2),-i $(2)) $(scp-legacy-option-flag) $(3) "$(4)" root@$(1):/data/$(notdir $(4))
+ssh $(if $(2),-i $(2)) $(3) root@$(1) \
 "function cleanup () { rm -f /data/$(notdir $(4)) && mount -o remount,ro / ; } ;\
 mount -o remount,rw / &&\
 cd /usr/lib/python3.7/site-packages &&\
@@ -54,8 +54,8 @@ endef
 # argument 8 is either egg or dist (default egg)
 define push-python-sdist
 $(if $(is-ot3), ,echo "This is an OT-2. Use 'make push' instead." && exit 1)
-scp $(if $(2),"-i $(2)") $(scp-legacy-option-flag) $(3) $(4) root@$(1):/var/$(notdir $(4))
-ssh $(if $(2),"-i $(2)") $(3) root@$(1) \
+scp $(if $(2),-i $(2)) $(scp-legacy-option-flag) $(3) $(4) root@$(1):/var/$(notdir $(4))
+ssh $(if $(2),-i $(2)) $(3) root@$(1) \
 "function cleanup () { rm -f /var/$(notdir $(4)) ; rm -rf /var/$(notdir $(4))-unzip; mount -o remount,ro / ; } ;\
  mkdir -p /var/$(notdir $(4))-unzip ; \
  cd /var/$(notdir $(4))-unzip && tar xf ../$(notdir $(4)) ; \
@@ -75,7 +75,7 @@ endef
 # argument 4 is the service name
 
 define restart-service
-ssh $(if $(2),"-i $(2)") $(3)  root@$(1) \
+ssh $(if $(2),-i $(2)) $(3)  root@$(1) \
 "systemctl restart $(4)"
 endef
 
@@ -86,6 +86,6 @@ endef
 # argument 3 is any further ssh options, quoted
 # argument 4 is the unit file path
 define push-systemd-unit
-	scp $(if $(2),"-i $(2)") $(scp-legacy-option-flag) $(3) "$(4)" root@$(1):/data/
-	ssh $(if $(2),"-i $(2)") $(3) root@$(1) "mount -o remount,rw / && mv /data/$(notdir $(4)) /etc/systemd/system/ && systemctl daemon-reload && mount -o remount,ro / || mount -o remount,ro /"
+	scp $(if $(2),-i $(2)) $(scp-legacy-option-flag) $(3) "$(4)" root@$(1):/data/
+	ssh $(if $(2),-i $(2)) $(3) root@$(1) "mount -o remount,rw / && mv /data/$(notdir $(4)) /etc/systemd/system/ && systemctl daemon-reload && mount -o remount,ro / || mount -o remount,ro /"
 endef
