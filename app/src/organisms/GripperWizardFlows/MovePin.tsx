de1b0a14ffad6b59cd6ba517f964dda81ca4ede0
Brian Arthur Cooper <brian@opentrons.com>
2023-03-30 15:36:17 -0400

    refactor(app): wire up gripper attach/detach/recalibrate flows to protocol engine (#12183)
    Replace stubbed out robot commands within the gripper wizard flow logic with real calls to new run
    commands to support calibration, attach, and detach.
    
    Closes RLAB-272

--- app/src/organisms/GripperWizardFlows/MovePin.tsx
+++ app/src/organisms/GripperWizardFlows/MovePin.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
+import { LEFT } from '@opentrons/shared-data'
+import { css } from 'styled-components'
 import { StyledText } from '../../atoms/text'
 import { GenericWizardTile } from '../../molecules/GenericWizardTile'
 import { InProgressModal } from '../../molecules/InProgressModal/InProgressModal'
@@ -8,10 +10,22 @@ import {
   MOVE_PIN_TO_FRONT_JAW,
   REMOVE_PIN_FROM_REAR_JAW,
 } from './constants'
+import { useCreateRunCommandMutation } from '../../resources/runs/hooks'
+import movePinStorageToFront from '../../assets/videos/gripper-wizards/PIN_FROM_STORAGE_TO_FRONT_JAW.webm'
+import movePinFrontToRear from '../../assets/videos/gripper-wizards/PIN_FROM_FRONT_TO_REAR_JAW.webm'
+import movePinRearToStorage from '../../assets/videos/gripper-wizards/PIN_FROM_REAR_TO_STORAGE.webm'
+import calibratingFrontJaw from '../../assets/videos/gripper-wizards/CALIBRATING_FRONT_JAW.webm'
+import calibratingRearJaw from '../../assets/videos/gripper-wizards/CALIBRATING_REAR_JAW.webm'
+
 import type { GripperWizardStepProps, MovePinStep } from './types'
+import type { Coordinates } from '@opentrons/shared-data'
 
 interface MovePinProps extends GripperWizardStepProps, MovePinStep {
-  isExiting: boolean
+  setFrontJawOffset: (offset: Coordinates) => void
+  frontJawOffset: Coordinates | null
+  createRunCommand: ReturnType<
+    typeof useCreateRunCommandMutation
+  >['createRunCommand']
 }
 
 export const MovePin = (props: MovePinProps): JSX.Element | null => {
@@ -21,38 +35,62 @@ export const MovePin = (props: MovePinProps): JSX.Element | null => {
     isRobotMoving,
     goBack,
     movement,
-    // chainRunCommands,
-    // setIsBetweenCommands,
-    // isExiting,
+    setFrontJawOffset,
+    frontJawOffset,
+    createRunCommand,
   } = props
   const { t } = useTranslation(['gripper_wizard_flows', 'shared'])
   if (attachedGripper == null) return null
+
   const handleOnClick = (): void => {
-    // setIsBetweenCommands(true)
-    // chainRunCommands([
-    //   {
-    //     // @ts-expect-error calibration type not yet supported
-    //     commandType: 'calibration/calibrateGripper' as const,
-    //     params: { },
-    //   },
-    //   {
-    //     commandType: 'home' as const,
-    //     params: {
-    //       axes: [], // TODO: use gripper motor axis const here
-    //     },
-    //   },
-    //   {
-    //     // @ts-expect-error calibration type not yet supported
-    //     commandType: 'calibration/moveToLocation' as const,
-    //     params: {
-    //       location: 'attachOrDetach',
-    //     },
-    //   },
-    // ]).then(() => {
-    //   setIsBetweenCommands(false)
-    //   proceed()
-    // })
-    proceed()
+    if (movement === REMOVE_PIN_FROM_REAR_JAW) {
+      proceed()
+    } else {
+      const jaw = movement === MOVE_PIN_TO_FRONT_JAW ? 'front' : 'rear'
+      createRunCommand({
+        command: {
+          commandType: 'home' as const,
+          params: {
+            axes: [], // TODO: use gripper motor axis const here
+          },
+        },
+        waitUntilComplete: true,
+      })
+        .then(() => {
+          createRunCommand({
+            command: {
+              // @ts-expect-error(BC, 2022-03-10): this will pass type checks when we update command types from V6 to V7 in shared-data
+              commandType: 'calibration/calibrateGripper' as const,
+              params:
+                jaw === 'rear' && frontJawOffset != null
+                  ? { jaw, otherJawOffset: frontJawOffset }
+                  : { jaw },
+            },
+            waitUntilComplete: true,
+          })
+            .then(({ data }) => {
+              if (jaw === 'front' && data?.result?.jawOffset != null) {
+                setFrontJawOffset(data.result.jawOffset)
+              }
+              createRunCommand({
+                command: {
+                  // @ts-expect-error(BC, 2022-03-10): this will pass type checks when we update command types from V6 to V7 in shared-data
+                  commandType: 'calibration/moveToMaintenancePosition' as const,
+                  params: {
+                    mount: LEFT, // TODO: update to gripper mount when RLAB-231 is addressed
+                  },
+                },
+                waitUntilComplete: true,
+              })
+                .then(() => {
+                  proceed()
+                })
+                .catch()
+            })
+            .catch()
+        })
+        .catch()
+    }
   }
   const infoByMovement: {
     [m in typeof movement]: {
@@ -60,27 +98,76 @@ export const MovePin = (props: MovePinProps): JSX.Element | null => {
       header: string
       body: React.ReactNode
       buttonText: string
-      image: React.ReactNode
+      prepImage: React.ReactNode
+      inProgressImage?: React.ReactNode
     }
   } = {
     [MOVE_PIN_TO_FRONT_JAW]: {
       inProgressText: t('stand_back_gripper_is_calibrating'),
+      inProgressImage: (
+        <video
+          css={css`
+            max-width: 100%;
+            max-height: 20rem;
+          `}
+          autoPlay={true}
+          loop={true}
+          controls={false}
+          aria-label="calibrating front jaw"
+        >
+          <source src={calibratingFrontJaw} />
+        </video>
+      ),
       header: t('insert_pin_into_front_jaw'),
       body: t('move_pin_from_storage_to_front_jaw'),
       buttonText: t('begin_calibration'),
-      image: (
-        <StyledText>
-          TODO image of moving pin from storage to front jaw
-        </StyledText>
+      prepImage: (
+        <video
+          css={css`
+            max-width: 100%;
+            max-height: 20rem;
+          `}
+          autoPlay={true}
+          loop={true}
+          controls={false}
+          aria-label="move calibration pin from storage location to front jaw"
+        >
+          <source src={movePinStorageToFront} />
+        </video>
       ),
     },
     [MOVE_PIN_FROM_FRONT_JAW_TO_REAR_JAW]: {
       inProgressText: t('stand_back_gripper_is_calibrating'),
+      inProgressImage: (
+        <video
+          css={css`
+            max-width: 100%;
+            max-height: 20rem;
+          `}
+          autoPlay={true}
+          loop={true}
+          controls={false}
+          aria-label="calibrating rear jaw"
+        >
+          <source src={calibratingRearJaw} />
+        </video>
+      ),
       header: t('insert_pin_into_rear_jaw'),
       body: t('move_pin_from_front_to_rear_jaw'),
       buttonText: t('shared:continue'),
-      image: (
-        <StyledText>TODO image of moving pin from front to rear jaw</StyledText>
+      prepImage: (
+        <video
+          css={css`
+            max-width: 100%;
+            max-height: 20rem;
+          `}
+          autoPlay={true}
+          loop={true}
+          controls={false}
+          aria-label="move calibration pin from front jaw to rear jaw"
+        >
+          <source src={movePinFrontToRear} />
+        </video>
       ),
     },
     [REMOVE_PIN_FROM_REAR_JAW]: {
@@ -88,18 +175,42 @@ export const MovePin = (props: MovePinProps): JSX.Element | null => {
       header: t('remove_calibration_pin'),
       body: t('move_pin_from_rear_jaw_to_storage'),
       buttonText: t('complete_calibration'),
-      image: <StyledText>TODO image of storing pin</StyledText>,
+      prepImage: (
+        <video
+          css={css`
+            max-width: 100%;
+            max-height: 20rem;
+          `}
+          autoPlay={true}
+          loop={true}
+          controls={false}
+          aria-label="move calibration rear jaw to storage"
+        >
+          <source src={movePinRearToStorage} />
+        </video>
+      ),
     },
   }
 
-  const { inProgressText, header, body, buttonText, image } = infoByMovement[
-    movement
-  ]
-  if (isRobotMoving) return <InProgressModal description={inProgressText} />
+  const {
+    inProgressText,
+    header,
+    body,
+    buttonText,
+    prepImage,
+    inProgressImage,
+  } = infoByMovement[movement]
+  if (isRobotMoving)
+    return (
+      <InProgressModal
+        description={inProgressText}
+        alternativeSpinner={inProgressImage}
+      />
+    )
   return (
     <GenericWizardTile
       header={header}
-      rightHandBody={image}
+      rightHandBody={prepImage}
       bodyText={<StyledText as="p">{body}</StyledText>}
       proceedButtonText={buttonText}
       proceed={handleOnClick}
