bf326b7b5d1ecd4718af7bbf86203663aeb351a8
Edward Cormany <ecormany@gmail.com>
2023-03-31 18:35:47 -0400

    welcome :og:description:

--- api/docs/v2/conf.py
+++ api/docs/v2/conf.py
@@ -420,3 +420,4 @@ ogp_site_url = "https://docs.opentrons.com/v2/"
 ogp_site_name = "Opentrons Python Protocol API"
 ogp_image = "https://opentrons.com/wp-content/uploads/2023/02/PythonAPI.png"
 ogp_description_length = 170
+ogp_enable_meta_description = False
