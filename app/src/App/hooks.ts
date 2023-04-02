04b5b02a4ddf793ade88d0bed402cf0b97bde75b
koji <koji.kanao@opentrons.com>
2023-04-02 18:19:41 -0400

    add JSDoc

--- app/src/App/hooks.ts
+++ app/src/App/hooks.ts
@@ -6,6 +6,12 @@ import { checkShellUpdate } from '../redux/shell'
 import type { Dispatch } from '../redux/types'
 
 const UPDATE_RECHECK_INTERVAL_MS = 60000
+
+/**
+ * A hook that polls for software updates on a set interval.
+ *
+ * @returns {void}
+ */
 export function useSoftwareUpdatePoll(): void {
   const dispatch = useDispatch<Dispatch>()
   const checkAppUpdate = React.useCallback(() => dispatch(checkShellUpdate()), [
--- app/src/organisms/Devices/hooks/useRobot.ts
+++ app/src/organisms/Devices/hooks/useRobot.ts
@@ -5,6 +5,13 @@ import { getDiscoverableRobotByName } from '../../../redux/discovery'
 import type { DiscoveredRobot } from '../../../redux/discovery/types'
 import type { State } from '../../../redux/types'
 
+/**
+ * A custom hook that returns the DiscoveredRobot object for the specified robot name.
+ *
+ * @param {string | null} robotName - The name of the robot to get.
+ * @returns {DiscoveredRobot | null} The DiscoveredRobot object for the specified robot name, or null if not found.
+ */
+
 export function useRobot(robotName: string | null): DiscoveredRobot | null {
   const robot = useSelector((state: State) =>
     getDiscoverableRobotByName(state, robotName)
