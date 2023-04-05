4567ffa4a79135a53c3448bbabb38da371b3a02b
Mehdi Zaidi <55298601+meh-di@users.noreply.github.com>
2023-04-05 13:06:26 -0400

    Update script, add stall detection

--- hardware-testing/hardware_testing/scripts/gantry_z_lifetime_test.py
+++ hardware-testing/hardware_testing/scripts/gantry_z_lifetime_test.py
@@ -11,65 +11,88 @@ from hardware_testing.opentrons_api.types import GantryLoad, OT3Mount, OT3Axis, 
 
 from opentrons.hardware_control.types import CriticalPoint
 
-async def _bowtie_move(api, homed_position_left: types.Point, homed_position_right: types.Point, _96ch: bool):
+async def _bowtie_move(api, homed_position_left: types.Point, homed_position_right: types.Point, load: GantryLoad, stall_count: int):
 
-    pos_max_left = homed_position_left - types.Point(x=1, y=1, z=1)
-    pos_min_left = types.Point(x=0, y=25, z=pos_max_left.z - 200)  # stay above deck to be safe
-    pos_max_right = homed_position_right - types.Point(x=1, y=1, z=1)
-    pos_min_right = types.Point(x=0, y=25, z=pos_max_right.z - 200)  # stay above deck to be safe
+    pos_max_left = homed_position_left - types.Point(x=1, y=10, z=1)
+    pos_min_left = types.Point(x=0, y=95, z=pos_max_left.z - 150)  # stay above deck to be safe
+    pos_max_right = homed_position_right - types.Point(x=1, y=10, z=1)
+    pos_min_right = types.Point(x=0, y=95, z=pos_max_right.z - 150)  # stay above deck to be safe
+
+    low_tp_points = {
+        '0': [pos_max_left, OT3Axis.LEFT],
+        '1': [pos_max_right - types.Point(x=0, y=0, z=150), OT3Axis.RIGHT],
+        '2': [, OT3Axis.RIGHT],
+        '3': [, OT3Axis.RIGHT],
+        '4': [, OT3Axis.LEFT],
+        '5': [, OT3Axis.LEFT],
+        '6': [, OT3Axis.LEFT],
+        '7': [, OT3Axis.LEFT],
+        '8': [, OT3Axis.RIGHT],
+        '9': [, OT3Axis.RIGHT],
+        '10': [, OT3Axis.RIGHT],
+        '11': [, OT3Axis.LEFT],
+        '12': [, OT3Axis.LEFT],
+        '13': [, OT3Axis.LEFT],
+        '14': [, OT3Axis.LEFT],
+    }
+
+    high_tp_points = []
 
     await api.move_to(OT3Mount.LEFT, pos_max_left) # back-right-up
-    if not _96ch:
+    if load == GantryLoad.LOW_THROUGHPUT:
         print("Moving right mount down...\n")
-        await api.move_rel(OT3Mount.RIGHT, delta=Point(z=-200)) # move right mount down
+        await api.move_rel(OT3Mount.RIGHT, delta=Point(z=-150), _check_stalls=True) # move right mount down
         print("Moving to front-left...\n")
-        await api.move_to(OT3Mount.RIGHT, pos_min_left._replace(z=pos_min_right.z)) # move to front-left with right mount down
+        await api.move_to(OT3Mount.RIGHT, pos_min_left._replace(z=pos_min_right.z), _check_stalls=True) # move to front-left with right mount down
         print("Moving right mount up...\n")
-        await api.move_rel(OT3Mount.RIGHT, delta=Point(z=200)) # move right mount up
+        await api.move_rel(OT3Mount.RIGHT, delta=Point(z=150), _check_stalls=True) # move right mount up
         cur_pos = await api.current_position_ot3(OT3Mount.LEFT)
-        await api.move_to(OT3Mount.LEFT, Point(cur_pos[OT3Axis.X], cur_pos[OT3Axis.Y], cur_pos[OT3Axis.Z_L])) #pos_min_left) # front-left-down
+        print(f"current pos:{cur_pos}\n")
+        await api.move_to(OT3Mount.LEFT, Point(cur_pos[OT3Axis.X], cur_pos[OT3Axis.Y], cur_pos[OT3Axis.Z_L]), _check_stalls=True) #pos_min_left) # front-left-down
         print("Moving left mount down...\n")
-        await api.move_rel(OT3Mount.LEFT, delta=Point(z=-200))
+        await api.move_rel(OT3Mount.LEFT, delta=Point(z=-150), _check_stalls=True)
         print("Moving to back-left...\n")
-        await api.move_to(OT3Mount.LEFT, pos_min_left._replace(x=cur_pos[OT3Axis.X], y=pos_max_left.y))# back-left-down
+        await api.move_to(OT3Mount.LEFT, pos_min_left._replace(x=cur_pos[OT3Axis.X], y=pos_max_left.y), _check_stalls=True)# back-left-down
         print("Moving left mount up...\n")
-        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(x=cur_pos[OT3Axis.X])) #pos_min_left.x)) # back-left-up
+        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(x=cur_pos[OT3Axis.X]), _check_stalls=True) #pos_min_left.x)) # back-left-up
         print("Moving right mount down...\n")
-        await api.move_rel(OT3Mount.RIGHT, delta=Point(z=-200))
+        await api.move_rel(OT3Mount.RIGHT, delta=Point(z=-150), _check_stalls=True)
         print("Moving to front-right...\n")
         # print(f"Point: {pos_max_left._replace(x=517.7, y=pos_min_left.y, z=pos_min_right.z)}\n")
-        await api.move_to(OT3Mount.RIGHT, pos_max_left._replace(x=517.703, y=pos_min_left.y, z=pos_min_right.z)) # front-right-up
+        await api.move_to(OT3Mount.RIGHT, pos_max_left._replace(x=517, y=pos_min_left.y, z=pos_min_right.z), _check_stalls=True) # front-right-up
         print("Moving right mount up...\n")
-        await api.move_rel(OT3Mount.RIGHT, delta=Point(z=200))
+        await api.move_rel(OT3Mount.RIGHT, delta=Point(z=150), _check_stalls=True)
         # print(f"current position of right mount: {await api.gantry_position(OT3Mount.RIGHT)}\n")
         # print(f"current position of left mount: {await api.gantry_position(OT3Mount.LEFT)}\n")
         cur_pos = await api.current_position_ot3(OT3Mount.LEFT)
-        await api.move_to(OT3Mount.LEFT, Point(cur_pos[OT3Axis.X], cur_pos[OT3Axis.Y], cur_pos[OT3Axis.Z_L])) #pos_min_left._replace(x=pos_max_left.x)) # front-right-down
+        print(f"current pos:{cur_pos}\n")
+        await api.move_to(OT3Mount.LEFT, Point(cur_pos[OT3Axis.X], cur_pos[OT3Axis.Y], cur_pos[OT3Axis.Z_L]), _check_stalls=True) #pos_min_left._replace(x=pos_max_left.x)) # front-right-down
         print("Moving left mount down...\n")
-        await api.move_rel(OT3Mount.LEFT, delta=Point(z=-200))
+        await api.move_rel(OT3Mount.LEFT, delta=Point(z=-150), _check_stalls=True)
         print("Moving to back-right...\n")
-        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(x=cur_pos[OT3Axis.X], z=pos_min_left.z)) # back-right-down
+        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(x=cur_pos[OT3Axis.X], z=pos_min_left.z), _check_stalls=True) # back-right-down
     else:
         print("Moving to front-left...\n")
-        await api.move_to(OT3Mount.LEFT, pos_min_left._replace(z=pos_max_left.z)) # front-left-up
+        await api.move_to(OT3Mount.LEFT, pos_min_left._replace(z=pos_max_left.z), _check_stalls=True) # front-left-up
         print("Moving mount down...\n")
-        await api.move_to(OT3Mount.LEFT, pos_min_left) # front-left-down
+        await api.move_to(OT3Mount.LEFT, pos_min_left, _check_stalls=True) # front-left-down
         print("Moving to back-left...\n")
-        await api.move_to(OT3Mount.LEFT, pos_min_left._replace(y=pos_max_left.y)) # back-left-down
+        await api.move_to(OT3Mount.LEFT, pos_min_left._replace(y=pos_max_left.y), _check_stalls=True) # back-left-down
         print("Moving mount up...\n")
-        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(x=pos_min_left.x)) # back-left-up
+        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(x=pos_min_left.x), _check_stalls=True) # back-left-up
         print("Moving to front-right...\n")
-        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(y=pos_min_left.y)) # front-right-up
+        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(y=pos_min_left.y), _check_stalls=True) # front-right-up
         print("Moving mount down...\n")
-        await api.move_to(OT3Mount.LEFT, pos_min_left._replace(x=pos_max_left.x)) # front-right-down
+        await api.move_to(OT3Mount.LEFT, pos_min_left._replace(x=pos_max_left.x), _check_stalls=True) # front-right-down
         print("Moving to back-right...\n")
-        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(z=pos_min_left.z)) # back-right-down
+        await api.move_to(OT3Mount.LEFT, pos_max_left._replace(z=pos_min_left.z), _check_stalls=True) # back-right-down
 
-    await api.move_to(OT3Mount.LEFT, pos_max_left)
+    await api.move_to(OT3Mount.LEFT, pos_max_left, _check_stalls=True)
 
-async def _main(is_simulating: bool, cycles: int, high_load: bool, mount: types.OT3Mount) -> None:
+async def _main(is_simulating: bool, cycles: int, mount: types.OT3Mount) -> None:
     api = await helpers_ot3.build_async_ot3_hardware_api(is_simulating=is_simulating)
     await api.home()
+    load = api.gantry_load
 
     AXES = [OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L]
 
@@ -79,87 +102,28 @@ async def _main(is_simulating: bool, cycles: int, high_load: bool, mount: types.
     RUN_CURRENT = []
     MAX_DISCONTINUITY = []
     DIRECTION_CHANGE_DISCONTINUITY = []
-    # PARAM_LISTS = [MAX_SPEED, ACCEL, HOLD_CURRENT, RUN_CURRENT, MAX_DISCONTINUITY, DIRECTION_CHANGE_DISCONTINUITY]
-    #
-    # PARAM_SETTINGS = [default_max_speed, acceleration, hold_current,
-    #                   run_current, max_speed_discontinuity, direction_change_speed_discontinuity]
 
-    # print(f"96Ch?: {high_load}\n")
     test_robot = input("Enter robot ID:\n\t>> ")
     test_name = "gantry-z-lifetime-test"
-    if high_load:
-        test_config = '96ch + Gripper Config'
-        test_tag = '96ch_Gripper'
-        GANTRY_LOAD = GantryLoad.HIGH_THROUGHPUT
-        # MAX_SPEED_X = api.config.motion_settings.default_max_speed.high_throughput[OT3Axis.X]
-        # MAX_SPEED_Y = api.config.motion_settings.default_max_speed.high_throughput[OT3Axis.Y]
-        # MAX_SPEED_Z = api.config.motion_settings.default_max_speed.high_throughput[OT3Axis.Z]
-        # ACCEL_X = api.config.motion_settings.acceleration.high_throughput[OT3Axis.X]
-        # ACCEL_Y = api.config.motion_settings.acceleration.high_throughput[OT3Axis.Y]
-        # ACCEL_Z = api.config.motion_settings.acceleration.high_throughput[OT3Axis.Z]
-        # HOLD_CURRENT_X = api.config.motion_settings.hold_current.high_throughput[OT3Axis.X]
-        # HOLD_CURRENT_Y = api.config.motion_settings.hold_current.high_throughput[OT3Axis.Y]
-        # HOLD_CURRENT_Z = api.config.motion_settings.hold_current.high_throughput[OT3Axis.Z]
-        # RUN_CURRENT_X = api.config.motion_settings.run_current.high_throughput[OT3Axis.X]
-        # RUN_CURRENT_Y = api.config.motion_settings.run_current.high_throughput[OT3Axis.Y]
-        # RUN_CURRENT_Z = api.config.motion_settings.run_current.high_throughput[OT3Axis.Z]
-    else:
-        test_config = '2 Singles + Gripper Config'
-        test_tag = 'Single_Pipettes_Gripper'
-        GANTRY_LOAD = GantryLoad.LOW_THROUGHPUT
-        # MAX_SPEED_X = api.config.motion_settings.default_max_speed.low_throughput[OT3Axis.X]
-        # MAX_SPEED_Y = api.config.motion_settings.default_max_speed.low_throughput[OT3Axis.Y]
-        # MAX_SPEED_Z = api.config.motion_settings.default_max_speed.low_throughput[OT3Axis.Z]
-        # ACCEL_X = api.config.motion_settings.acceleration.low_throughput[OT3Axis.X]
-        # ACCEL_Y = api.config.motion_settings.acceleration.low_throughput[OT3Axis.Y]
-        # ACCEL_Z = api.config.motion_settings.acceleration.low_throughput[OT3Axis.Z]
-        # HOLD_CURRENT_X = api.config.motion_settings.hold_current.low_throughput[OT3Axis.X]
-        # HOLD_CURRENT_Y = api.config.motion_settings.hold_current.low_throughput[OT3Axis.Y]
-        # HOLD_CURRENT_Z = api.config.motion_settings.hold_current.low_throughput[OT3Axis.Z]
-        # RUN_CURRENT_X = api.config.motion_settings.run_current.low_throughput[OT3Axis.X]
-        # RUN_CURRENT_Y = api.config.motion_settings.run_current.low_throughput[OT3Axis.Y]
-        # RUN_CURRENT_Z = api.config.motion_settings.run_current.low_throughput[OT3Axis.Z]
+    test_config = load.value
+    test_tag = load.value
+    print(f"test_config: {load.value}\n")
+
+    rm = api.get_attached_instrument(OT3Mount.RIGHT).get('name')
+    lm = api.get_attached_instrument(OT3Mount.LEFT).get('name')
+    gripper = api.has_gripper()
 
     for i in range(len(AXES)):
-        MAX_SPEED.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.motion_settings.default_max_speed, AXES[i], GANTRY_LOAD))
-        ACCEL.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.motion_settings.acceleration, AXES[i], GANTRY_LOAD))
-        HOLD_CURRENT.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.current_settings.hold_current, AXES[i], GANTRY_LOAD))
-        RUN_CURRENT.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.current_settings.run_current, AXES[i], GANTRY_LOAD))
-        MAX_DISCONTINUITY.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.motion_settings.max_speed_discontinuity, AXES[i], GANTRY_LOAD))
-        DIRECTION_CHANGE_DISCONTINUITY.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.motion_settings.direction_change_speed_discontinuity, AXES[i], GANTRY_LOAD))
-
-
-    # settings = api.config.motion_settings
-    #
-    # for i in range(len(PARAM_LISTS)):
-    #     for j in range(len(AXES)):
-    #         if high_load:
-    #             PARAM_LISTS[i][j] = settings.PARAM_SETTINGS[i].high_throughput[AXES[j]]
-    #         else:
-    #             PARAM_LISTS[i][j] = settings.PARAM_SETTINGS[i].low_throughput[AXES[j]]
-    #     print(f"PARAM_LISTS: {PARAM_LISTS[i]}\n")
-
-
-    # await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(api,
-    #         OT3Axis.X, GANTRY_LOAD, hold_current=0.7, run_current=1.5)
-    # await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(api,
-    #         OT3Axis.Y, GANTRY_LOAD, hold_current=0.7, run_current=1.5)
-    # await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(api,
-    #         OT3Axis.Z, GANTRY_LOAD, hold_current=1.5, run_current=1.5)
-    #
-    # await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(api,
-    #         OT3Axis.X, GANTRY_LOAD, default_max_speed=, acceleration=,
-    #         max_speed_discontinuity=10, direction_change_speed_discontinuity=5)
-    # await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(api,
-    #         OT3Axis.Y, GANTRY_LOAD, default_max_speed=, acceleration=,
-    #         max_speed_discontinuity=10, direction_change_speed_discontinuity=5)
-    # await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(api,
-    #         OT3Axis.Z, GANTRY_LOAD, default_max_speed=, acceleration=,
-    #         max_speed_discontinuity=10, direction_change_speed_discontinuity=1)
+        MAX_SPEED.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.motion_settings.default_max_speed, AXES[i], load))
+        ACCEL.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.motion_settings.acceleration, AXES[i], load))
+        HOLD_CURRENT.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.current_settings.hold_current, AXES[i], load))
+        RUN_CURRENT.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.current_settings.run_current, AXES[i], load))
+        MAX_DISCONTINUITY.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.motion_settings.max_speed_discontinuity, AXES[i], load))
+        DIRECTION_CHANGE_DISCONTINUITY.append(helpers_ot3.get_gantry_per_axis_setting_ot3(api.config.motion_settings.direction_change_speed_discontinuity, AXES[i], load))
 
     file_name = data.create_file_name(test_name=test_name, run_id=data.create_run_id(), tag=test_tag)
 
-    header = ['Time(s)', 'Test Robot', 'Test Configuration', 'Cycle', '', 'Axis',
+    header = ['Time(s)', 'Test Robot', 'Test Configuration', 'Cycle', '', 'Left Mount', 'Right Mount', 'Gripper Attached', '', 'Axis',
               'Max Speed (mm/s)', 'Acceleration (mm^2/s)', 'Hold Current (A)',
               'Run Current (A)', 'Max Speed Discontinuity', 'Direction Change Speed Discontinuity']
     header_str = data.convert_list_to_csv_line(header)
@@ -170,11 +134,14 @@ async def _main(is_simulating: bool, cycles: int, high_load: bool, mount: types.
     homed_pos_right = await api.gantry_position(OT3Mount.RIGHT)
     for i in range(cycles):
         print(f"========== Cycle {i + 1}/{cycles} ==========\n")
-        await _bowtie_move(api, homed_pos_left, homed_pos_right, high_load)
+        await _bowtie_move(api, homed_pos_left, homed_pos_right, load)
         if (i == 0 or i == 1 or i == 2):
-            cycle_data = [time.perf_counter()-start_time, test_robot, test_config, i+1, '',
-                          AXES[i], MAX_SPEED[i], ACCEL[i], HOLD_CURRENT[i], RUN_CURRENT[i],
+            cycle_data = [time.perf_counter()-start_time, test_robot, test_config, i+1, '', lm, rm, gripper, '',
+                          OT3Axis.to_kind(AXES[i]), MAX_SPEED[i], ACCEL[i], HOLD_CURRENT[i], RUN_CURRENT[i],
                           MAX_DISCONTINUITY[i], DIRECTION_CHANGE_DISCONTINUITY[i]]
+            rm = ''
+            lm = ''
+            gripper = ''
         else:
             cycle_data = [time.perf_counter()-start_time, test_robot, test_config, i+1]
         cycle_data_str = data.convert_list_to_csv_line(cycle_data)
@@ -193,10 +160,10 @@ if __name__ == "__main__":
         "--mount", type=str, choices=list(mount_options.keys()), default="left"
     )
     parser.add_argument("--cycles", type=int, default=5)
-    parser.add_argument("--high_load", action="store_true")
+    # parser.add_argument("--high_load", action="store_true")
     args = parser.parse_args()
     mount = mount_options[args.mount]
 
     if not args.simulate:
         input("Gantry-Z-Lifetime: Is the deck totally empty? (press ENTER to continue)")
-    asyncio.run(_main(args.simulate, args.cycles, args.high_load, mount))
+    asyncio.run(_main(args.simulate, args.cycles, mount))
