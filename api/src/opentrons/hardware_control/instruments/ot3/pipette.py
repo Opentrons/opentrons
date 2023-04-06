578b320b01e33e5befa784854b08d14c9ffed7ae
andySigler <andrewsigler1@gmail.com>
2023-04-06 16:18:20 -0400

    undo changes that we don't want merged

--- api/src/opentrons/hardware_control/instruments/ot3/pipette.py
+++ api/src/opentrons/hardware_control/instruments/ot3/pipette.py
@@ -422,7 +422,7 @@ class Pipette(AbstractInstrument[PipetteConfigurations]):
 
     def set_current_volume(self, new_volume: float) -> None:
         assert new_volume >= 0
-        # assert new_volume <= self.working_volume
+        assert new_volume <= self.working_volume
         self._current_volume = new_volume
 
     def add_current_volume(self, volume_incr: float) -> None:
@@ -434,8 +434,7 @@ class Pipette(AbstractInstrument[PipetteConfigurations]):
         self._current_volume -= volume_incr
 
     def ok_to_add_volume(self, volume_incr: float) -> bool:
-        # return self.current_volume + volume_incr <= self.working_volume
-        return True
+        return self.current_volume + volume_incr <= self.working_volume
 
     def add_tip(self, tip_length: float) -> None:
         """
