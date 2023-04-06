508dfc71eec142b76a08011e7cff096377a1b11a
andySigler <andrewsigler1@gmail.com>
2023-04-06 12:13:49 -0400

    remove unnecessary api modification; linting

--- hardware-testing/hardware_testing/production_qc/gripper_assembly_qc_ot3/test_force.py
+++ hardware-testing/hardware_testing/production_qc/gripper_assembly_qc_ot3/test_force.py
@@ -129,16 +129,18 @@ async def run(api: OT3API, report: CSVReport, section: str) -> None:
     await api.ungrip()
     hover_pos, target_pos = _get_force_gauge_hover_and_grip_positions(api)
     await helpers_ot3.move_to_arched_ot3(api, mount, hover_pos)
-    await api.move_to(mount, target_pos._replace(x=64.0,y=118))
+    await api.move_to(mount, target_pos._replace(x=64.0, y=118))
     if not api.is_simulator:
         ui.get_user_ready("jog gripper to align with gauge")
     pos = await helpers_ot3.jog_mount_ot3(api, mount)
     # MOVE FORCE FIXTURE TO THE MIDDLE OF THE POSITION
     await api.grip(5.0)
-    await api.move_to(mount, Point(x=pos[OT3Axis.X],y=118.0,z=pos[z_ax]))
+    await api.move_to(mount, Point(x=pos[OT3Axis.X], y=118.0, z=pos[z_ax]))
     await api.ungrip()
     if not api.is_simulator:
-        ui.get_user_ready("please check the fixture in the middle position and prepare to grip")
+        ui.get_user_ready(
+            "please check the fixture in the middle position and prepare to grip"
+        )
 
     # LOOP THROUGH FORCES
     ui.print_header("MEASURE NEWTONS")
