3e4077b4a0c6f0c3932ce84d15f7b5aeac60084d
jerome <1458798121@qq.com>
2023-04-07 11:37:43 +0800

    Move to target position directly

--- hardware-testing/hardware_testing/production_qc/gripper_assembly_qc_ot3/test_force.py
+++ hardware-testing/hardware_testing/production_qc/gripper_assembly_qc_ot3/test_force.py
@@ -127,16 +127,13 @@ async def run(api: OT3API, report: CSVReport, section: str) -> None:
     await api.home([z_ax, g_ax])
     # MOVE TO GAUGE
     await api.ungrip()
-    # hover_pos, target_pos = _get_force_gauge_hover_and_grip_positions(api)
-    # await helpers_ot3.move_to_arched_ot3(api, mount, hover_pos)
-    target_pos = Point(x=64.0, y=123.3,z=67.6)
-    await helpers_ot3.move_to_arched_ot3(api, mount, target_pos)
+    target_pos = Point(x=64.0, y=123.3, z=67.6) 
+    await helpers_ot3.move_to_arched_ot3(api, mount, target_pos._replace(z=87.6))
     if not api.is_simulator:
-        ui.get_user_ready("please make sure the gauge in the middle of the gripper")
+        ui.get_user_ready(
+            "please make sure the gauge in the middle of the gripper"
+        )
     await api.move_to(mount, target_pos)
-    if not api.is_simulator:
-        ui.get_user_ready("jog gripper to align with gauge")
-    pos = await helpers_ot3.jog_mount_ot3(api, mount)
     if not api.is_simulator:
         ui.get_user_ready(
             "prepare to grip"
