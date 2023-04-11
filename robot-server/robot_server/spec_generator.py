1965dec154e729f34fea232efb1ccda3d6b15e7d
Edward Cormany <ecormany@gmail.com>
2023-04-11 18:37:38 -0400

    first pass with bootstrapped code

---
+++ robot-server/robot_server/spec_generator.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+import json
+
+from opentrons import __version__
+
+from .app_setup import app
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="Opentrons HTTP API Spec",
+        version=__version__,
+        description=(
+            "This OpenAPI spec describes the HTTP API for Opentrons "
+            "robots. It may be retrieved from a robot on port 31950 at "
+            "/openapi. Some schemas used in requests and responses use "
+            "the `x-patternProperties` key to mean the JSON Schema "
+            "`patternProperties` behavior."
+        ),
+        routes=app.routes,
+    )
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
+
+with open("build/openapi.json", "w") as f:
+    json.dump(custom_openapi(), f)
