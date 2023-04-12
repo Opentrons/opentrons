d7f567a77e07a203d24e5632fdc564eca2e5f076
Jethary Rader <66035149+jerader@users.noreply.github.com>
2023-04-12 17:10:43 -0400

    feat(odd): liquid details modal for ODD  (#12442)
    closes RLIQ-353

--- app/src/molecules/Modal/OnDeviceDisplay/Modal.tsx
+++ app/src/molecules/Modal/OnDeviceDisplay/Modal.tsx
@@ -54,7 +54,7 @@ export function Modal(props: ModalProps): JSX.Element {
     >
       <Flex
         backgroundColor={isError ? COLORS.red_two : COLORS.white}
-        border={isError ? `0.375rem solid ${COLORS.red_two}` : 'none'}
+        border={`0.375rem solid ${isError ? COLORS.red_two : COLORS.white}`}
         width={modalWidth}
         height="max-content"
         maxHeight="32.5rem"
@@ -65,7 +65,6 @@ export function Modal(props: ModalProps): JSX.Element {
         aria-label={`modal_${modalSize}`}
         onClick={e => {
           e.stopPropagation()
-          onOutsideClick(e)
         }}
       >
         {header != null ? (
