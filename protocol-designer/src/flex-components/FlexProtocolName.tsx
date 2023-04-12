df31a44b053c2c957f9d67d0b170851fa9761d0c
Aditya Sancheti <126742727+aditya-sancheti@users.noreply.github.com>
2023-04-12 16:03:54 +0530

    Pd dev rplc 225 rplc 384 protocol designer landing page (#12477)
    * Landing Page Initial Commit
    
    * Round Tabs implementation

---
+++ protocol-designer/src/flex-components/FlexProtocolName.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+
+import { FormGroup, InputField } from '@opentrons/components'
+
+import styles from './FlexComponents.css'
+
+function FlexProtocolNameComponent(formProps: any): JSX.Element {
+  const { formProps: props } = formProps
+
+  return (
+    <>
+      <FormGroup label="Protocol Name">
+        <InputField
+          autoFocus
+          tabIndex={1}
+          type="text"
+          onChange={props.handleChange}
+          onBlur={props.handleBlur}
+          value={props.values.fields.pndName}
+          name="fields.pndName"
+        />
+      </FormGroup>
+      <small>supporting text about any error handling goes here.</small>
+
+      <FormGroup label="Organization/Author">
+        <InputField
+          type="text"
+          onChange={props.handleChange}
+          onBlur={props.handleBlur}
+          value={props.values.fields.pndOrgAuthor}
+          name="fields.pndOrgAuthor"
+        />
+      </FormGroup>
+      <small>supporting text about any error handling goes here.</small>
+
+      <FormGroup label="Description">
+        <textarea
+          rows={4}
+          cols={195}
+          onChange={props.handleChange}
+          onBlur={props.handleBlur}
+          value={props.values.fields.pndDescription}
+          name="fields.pndDescription"
+        />
+      </FormGroup>
+      <small>supporting text about any error handling goes here.</small>
+    </>
+  )
+}
+
+export const FlexProtocolName = FlexProtocolNameComponent
