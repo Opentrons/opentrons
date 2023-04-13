072ac6decaa3f4792ad172685be73cc7de08a3c4
koji <koji.kanao@opentrons.com>
2023-04-13 11:34:53 -0400

    feat(odd): add run failed modal for running protocol (#12483)
    add run failed modal for running protocol
    close RCORE-563

--- app/src/molecules/BackgroundOverlay/index.tsx
+++ app/src/molecules/BackgroundOverlay/index.tsx
@@ -18,7 +18,7 @@ export function BackgroundOverlay(props: BackgroundOverlayProps): JSX.Element {
       right="0"
       top="0"
       bottom="0"
-      zIndex="1"
+      zIndex="3"
       backgroundColor={COLORS.darkBlack_sixty}
       onClick={onClick}
       {...flexProps}
