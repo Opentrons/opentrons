af04dda141962dd2c1cae29020b65c92991e8ae5
koji <koji.kanao@opentrons.com>
2023-04-13 17:30:44 -0400

    fix test cases

--- app/src/organisms/OnDeviceDisplay/RobotDashboard/RecentProtocolRunCard.tsx
+++ app/src/organisms/OnDeviceDisplay/RobotDashboard/RecentProtocolRunCard.tsx
@@ -36,11 +36,6 @@ export function RecentProtocolRunCard({
   const history = useHistory()
   const isSuccess = missingProtocolHardware.length === 0
 
-  console.log(protocolId)
-  console.log('missingProtocolHardware', missingProtocolHardware)
-  console.log(missingProtocolHardware.length)
-  console.log('isSuccess', isSuccess)
-
   const CARD_STYLE = css`
     &:active {
       background-color: ${isSuccess
@@ -59,54 +54,39 @@ export function RecentProtocolRunCard({
     overflow: hidden;
   `
 
-  // switch below
-  // const countHardwareType = (hwType: 'pipette' | 'module'): number => {
-  //   return missingProtocolHardwareType.reduce((acc, hardwareType) => {
-  //     if (hardwareType === hwType) {
-  //       return acc + 1
-  //     }
-  //     return acc
-  //   }, 0)
-  // }
-
-  // const missingPipettes = countHardwareType('pipette')
-  // const missing = countHardwareType('pipette')
-
   const missingProtocolHardwareType = missingProtocolHardware.map(
     hardware => hardware.hardwareType
   )
 
-  const missingProtocolPipetteType = missingProtocolHardwareType.filter(
-    type => type === 'pipette'
-  )
-  const missingProtocolModuleType = missingProtocolHardwareType.filter(
-    type => type === 'module'
-  )
+  // Note(kj:04/13/2023) This component only check the type and count the number
+  // If we need to display any specific information, we will need to use filter
+  const countMissingHardwareType = (hwType: 'pipette' | 'module'): number => {
+    return missingProtocolHardwareType.reduce((acc, hardwareType) => {
+      if (hardwareType === hwType) {
+        return acc + 1
+      }
+      return acc
+    }, 0)
+  }
+
+  const countMissingPipettes = countMissingHardwareType('pipette')
+  const countMissingModules = countMissingHardwareType('module')
 
   let chipText: string = t('ready_to_run')
-  if (
-    missingProtocolPipetteType.length === 0 &&
-    missingProtocolModuleType.length > 0
-  ) {
+  if (countMissingPipettes === 0 && countMissingModules > 0) {
     chipText = t('missing_module', {
-      num: missingProtocolModuleType.length,
-      count: missingProtocolModuleType.length,
+      num: countMissingModules,
+      count: countMissingModules,
     })
-  } else if (
-    missingProtocolPipetteType.length > 0 &&
-    missingProtocolModuleType.length === 0
-  ) {
+  } else if (countMissingPipettes > 0 && countMissingModules === 0) {
     chipText = t('missing_pipette', {
-      num: missingProtocolPipetteType.length,
-      count: missingProtocolPipetteType.length,
+      num: countMissingPipettes,
+      count: countMissingPipettes,
     })
-  } else if (
-    missingProtocolPipetteType.length > 0 &&
-    missingProtocolModuleType.length > 0
-  ) {
+  } else if (countMissingPipettes > 0 && countMissingModules > 0) {
     chipText = t('missing_both', {
-      numMod: missingProtocolModuleType.length,
-      numPip: missingProtocolPipetteType.length,
+      numMod: countMissingModules,
+      numPip: countMissingPipettes,
     })
   }
   const handleCardClick = (): void => {
--- app/src/organisms/OnDeviceDisplay/RobotDashboard/__tests__/RecentProtocolRunCard.test.tsx
+++ app/src/organisms/OnDeviceDisplay/RobotDashboard/__tests__/RecentProtocolRunCard.test.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { when, resetAllWhenMocks } from 'jest-when'
 import { fireEvent } from '@testing-library/react'
 import { formatDistance } from 'date-fns'
 
@@ -18,15 +17,6 @@ const mockProtocolName = 'mockProtocol'
 const mockProtocolId = 'mockProtocolId'
 const mockLastRun = '2023-04-12T21:30:49.124108+00:00'
 
-const mockPipette = [
-  {
-    hardwareType: 'pipette',
-    pipetteName: 'p1000_single_gen3',
-    mount: 'left',
-    connected: true,
-  },
-] as ProtocolHardware[]
-// missing hardware pipette | module
 const mockMissingPipette = [
   {
     hardwareType: 'pipette',
@@ -37,6 +27,21 @@ const mockMissingPipette = [
 ] as ProtocolHardware[]
 
 const mockMissingModule = [
+  {
+    hardwareType: 'module',
+    moduleModel: 'temperatureModuleV2',
+    slot: '1',
+    connected: false,
+  },
+] as ProtocolHardware[]
+
+const missingBoth = [
+  {
+    hardwareType: 'pipette',
+    pipetteName: 'p1000_single_gen3',
+    mount: 'left',
+    connected: false,
+  },
   {
     hardwareType: 'module',
     moduleModel: 'temperatureModuleV2',
@@ -73,12 +78,11 @@ describe('RecentProtocolRunCard', () => {
       protocolId: mockProtocolId,
       lastRun: mockLastRun,
     }
-    mockUseMissingProtocolHardware.mockReturnValue(mockPipette)
+    mockUseMissingProtocolHardware.mockReturnValue([])
   })
 
   afterEach(() => {
     jest.clearAllMocks()
-    resetAllWhenMocks()
   })
 
   it('should render text', () => {
@@ -91,22 +95,28 @@ describe('RecentProtocolRunCard', () => {
     getByText(`Last run ${lastRunTime}`)
   })
 
-  // it('should render missing chip when missing a pipette', () => {
-  //   mockUseMissingProtocolHardware.mockReturnValue(mockMissingPipette)
-  //   const [{ getByText }] = render(props)
-  //   getByText('Missing 1 pipette(s)')
-  // })
+  it('should render missing chip when missing a pipette', () => {
+    mockUseMissingProtocolHardware.mockReturnValue(mockMissingPipette)
+    const [{ getByText }] = render(props)
+    getByText('Missing 1 pipette')
+  })
 
-  // it('should render missing chip when missing a module', () => {
-  //   mockUseMissingProtocolHardware.mockReturnValue(mockMissingModule)
-  //   const [{ getByText }] = render(props)
-  //   getByText('Missing 1 module(s)')
-  // })
+  it('should render missing chip when missing a module', () => {
+    mockUseMissingProtocolHardware.mockReturnValue(mockMissingModule)
+    const [{ getByText }] = render(props)
+    getByText('Missing 1 module')
+  })
 
-  // it('when tapping a card, a mock function is called', () => {
-  //   const [{ getByLabelText }] = render(props)
-  //   const button = getByLabelText('RecentRunCard')
-  //   fireEvent.click(button)
-  //   expect(mockPush).toHaveBeenCalledWith(`protocols/${props.protocolId}`)
-  // })
+  it('should render missing chip (module and pipette) when missing a pipette and a module', () => {
+    mockUseMissingProtocolHardware.mockReturnValue(missingBoth)
+    const [{ getByText }] = render(props)
+    getByText('Missing 1 module(s) and 1 pipette(s)')
+  })
+
+  it('when tapping a card, a mock function is called', () => {
+    const [{ getByLabelText }] = render(props)
+    const button = getByLabelText('RecentRunCard')
+    fireEvent.click(button)
+    expect(mockPush).toHaveBeenCalledWith(`protocols/${props.protocolId}`)
+  })
 })
