d148583ec53bba8a20957d6fe8829e71fdf10516
koji <koji.kanao@opentrons.com>
2023-04-14 23:49:42 -0400

    Update useCurrentRunId.ts

--- app/src/organisms/ProtocolUpload/hooks/useCurrentRunId.ts
+++ app/src/organisms/ProtocolUpload/hooks/useCurrentRunId.ts
@@ -1,5 +1,9 @@
 import { useAllRunsQuery } from '@opentrons/react-api-client'
 
+/**
+ * Retrieves the ID of the current run from the query parameters of the URL.
+ * @returns The ID of the current run, or `null` if no run ID is found.
+ */
 export function useCurrentRunId(): string | null {
   const { data: allRuns } = useAllRunsQuery()
   const currentRunLink = allRuns?.links?.current ?? null
