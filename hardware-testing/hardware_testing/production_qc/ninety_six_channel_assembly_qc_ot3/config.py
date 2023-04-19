bf4accbe1aa052f11869533eefea4e89da8afb04
andySigler <andrewsigler1@gmail.com>
2023-04-19 11:44:23 -0400

    adds more templates for all tests

--- hardware-testing/hardware_testing/production_qc/ninety_six_channel_assembly_qc_ot3/config.py
+++ hardware-testing/hardware_testing/production_qc/ninety_six_channel_assembly_qc_ot3/config.py
@@ -7,7 +7,12 @@ from hardware_testing.data.csv_report import CSVReport, CSVSection
 
 from . import (
     test_plunger,
+    test_jaws,
+    test_pressure,
+    test_tip_sensor,
+    test_environmental_sensor,
     test_droplets,
+    test_partial_pickup,
 )
 
 
@@ -15,7 +20,12 @@ class TestSection(enum.Enum):
     """Test Section."""
 
     PLUNGER = "PLUNGER"
+    JAWS = "JAWS"
+    PRESSURE = "PRESSURE"
+    TIP_SENSOR = "TIP-SENSOR"
+    ENVIRONMENT_SENSOR = "ENVIRONMENT-SENSOR"
     DROPLETS = "DROPLETS"
+    PARTIAL_PICKUP = "PARTIAL_PICKUP"
 
 
 @dataclass
@@ -32,8 +42,28 @@ TESTS = [
         test_plunger.run,
     ),
     (
+        TestSection.JAWS,
+        test_jaws.run,
+    ),
+    (
+        TestSection.PRESSURE,
+        test_pressure.run,
+    ),
+    (
+        TestSection.TIP_SENSOR,
+        test_tip_sensor.run,
+    ),
+    (
+        TestSection.ENVIRONMENT_SENSOR,
+        test_environmental_sensor.run,
+    ),
+    (
         TestSection.DROPLETS,
         test_droplets.run,
+    ),
+    (
+        TestSection.PARTIAL_PICKUP,
+        test_partial_pickup.run,
     ),
 ]
 
@@ -47,7 +77,22 @@ def build_report(test_name: str) -> CSVReport:
                 title=TestSection.PLUNGER.value, lines=test_plunger.build_csv_lines()
             ),
             CSVSection(
+                title=TestSection.JAWS.value, lines=test_jaws.build_csv_lines()
+            ),
+            CSVSection(
+                title=TestSection.PRESSURE.value, lines=test_pressure.build_csv_lines()
+            ),
+            CSVSection(
+                title=TestSection.TIP_SENSOR.value, lines=test_tip_sensor.build_csv_lines()
+            ),
+            CSVSection(
+                title=TestSection.ENVIRONMENT_SENSOR.value, lines=test_environmental_sensor.build_csv_lines()
+            ),
+            CSVSection(
                 title=TestSection.DROPLETS.value, lines=test_droplets.build_csv_lines()
+            ),
+            CSVSection(
+                title=TestSection.PARTIAL_PICKUP.value, lines=test_partial_pickup.build_csv_lines()
             ),
         ],
     )
---
+++ hardware-testing/hardware_testing/production_qc/ninety_six_channel_assembly_qc_ot3/test_environmental_sensor.py
@@ -0,0 +1,24 @@
+"""Test Environmental Sensor."""
+from typing import List, Union, Tuple, Dict
+
+from opentrons.hardware_control.ot3api import OT3API
+
+from hardware_testing.data import ui
+from hardware_testing.data.csv_report import (
+    CSVReport,
+    CSVResult,
+    CSVLine,
+    CSVLineRepeating,
+)
+from hardware_testing.opentrons_api import helpers_ot3
+from hardware_testing.opentrons_api.types import OT3Axis, OT3Mount, Point
+
+
+def build_csv_lines() -> List[Union[CSVLine, CSVLineRepeating]]:
+    """Build CSV Lines."""
+    return []
+
+
+async def run(api: OT3API, report: CSVReport, section: str) -> None:
+    """Run."""
+    return
