63c15501391f4af22f311f4252c6bde832954638
aditya-sancheti <aditya.sancheti@softdel.com>
2023-04-20 15:42:52 +0530

    Modules selection placeholder

--- protocol-designer/src/flex-components/protocol-editor/FlexModules.tsx
+++ protocol-designer/src/flex-components/protocol-editor/FlexModules.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { useFormikContext } from 'formik'
 import { Flex, DIRECTION_COLUMN, SPACING, Box } from '@opentrons/components'
 import { i18n } from '../../localization'
 import { StyledText } from './StyledText'
@@ -13,6 +12,14 @@ import {
   THERMOCYCLER_MODULE_TYPE,
   ModuleType,
   ModuleModel,
+  HEATERSHAKER_MODULE_TYPE,
+  MAGNETIC_MODULE_V1,
+  HEATERSHAKER_MODULE_V1,
+  MAGNETIC_MODULE_TYPE,
+  TEMPERATURE_MODULE_TYPE,
+  TEMPERATURE_MODULE_V2,
+  THERMOCYCLER_MODULE_V2,
+  SPAN7_8_10_11_SLOT,
 } from '@opentrons/shared-data'
 import { ModuleOnDeck } from 'protocol-designer/src/step-forms/types'
 import { ModelModuleInfo } from 'protocol-designer/src/components/EditModules'
@@ -41,51 +48,46 @@ export interface EditModulesFormValues {
 }
 
 function FlexModulesComponent({ formProps }: any): JSX.Element {
-  const { moduleType } = formProps
-
-  const showSlotOption = moduleType !== THERMOCYCLER_MODULE_TYPE
-  const { values } = useFormikContext<EditModulesFormValues>()
-  const { selectedModel } = values
-
-  const disabledModuleRestriction = useSelector(
-    featureFlagSelectors.getDisableModuleRestrictions
-  )
-
-  const noCollisionIssue =
-    selectedModel && !isModuleWithCollisionIssue(selectedModel)
-
-  const slotOptionTooltip = (
-    <div className={styles.slot_tooltip}>
-      {i18n.t('tooltip.edit_module_modal.slot_selection')}
-    </div>
-  )
-
-  const enableSlotSelection = disabledModuleRestriction || noCollisionIssue
-
   const modules = [
     {
+      moduleType: HEATERSHAKER_MODULE_TYPE,
       name: 'Heater-Shaker GEN1',
       supportingCopy: 'supporting copy',
+      selectedModel: HEATERSHAKER_MODULE_V1,
+      selectedSlot: 1,
     },
     {
+      moduleType: THERMOCYCLER_MODULE_TYPE,
       name: 'Thermocycler GEN2',
       supportingCopy: 'supporting copy',
+      selectedModel: THERMOCYCLER_MODULE_V2,
+      selectedSlot: SPAN7_8_10_11_SLOT,
     },
     {
+      moduleType: TEMPERATURE_MODULE_TYPE,
       name: 'Temperature GEN2',
       supportingCopy: 'supporting copy',
+      selectedModel: TEMPERATURE_MODULE_V2,
+      selectedSlot: '3',
     },
     {
+      moduleType: MAGNETIC_MODULE_TYPE,
       name: 'Magnetic Block GEN1',
       supportingCopy: 'supporting copy',
+      selectedModel: MAGNETIC_MODULE_V1,
+      selectedSlot: '9',
     },
     {
+      moduleType: 'gripperModuleType',
       name: 'Gripper',
       supportingCopy: '',
+      selectedModel: 'gripperModuleV1',
     },
     {
+      moduleType: 'trashChuteModuleType',
       name: 'Trash Chute',
       supportingCopy: '',
+      selectedModel: 'trashChuteModuleV1',
     },
   ]
 
@@ -103,6 +105,10 @@ function FlexModulesComponent({ formProps }: any): JSX.Element {
     }
   }
 
+  const disabledModuleRestriction = useSelector(
+    featureFlagSelectors.getDisableModuleRestrictions
+  )
+
   return (
     <>
       <StyledText as="h2">
@@ -111,82 +117,106 @@ function FlexModulesComponent({ formProps }: any): JSX.Element {
 
       <div className={styles.flex_sub_heading}>
         <>
-          {modules.map(({ name, supportingCopy }: any, index: number) => {
-            const isModuleChecked = selectedModules.includes(name)
-
-            return (
-              <>
-                <div className={styles.module_section} key={index}>
-                  <div className={styles.mini_card}>
-                    <MiniCard
-                      isSelected={isModuleChecked}
-                      isError={false}
-                      onClick={() => toggleModuleSelection(name)}
-                    >
-                      <img
-                        src={OT3_PNG}
-                        css={css`
-                          width: 67px;
-                          height: 54px;
-                        `}
-                      />
-                      <Flex
-                        flexDirection={DIRECTION_COLUMN}
-                        marginLeft={SPACING.spacing4}
-                        marginTop={SPACING.spacing3}
-                        marginBottom={SPACING.spacing4}
-                      >
-                        <StyledText as="h4">{name}</StyledText>
-                        <Box>
-                          <StyledText as="h4">{supportingCopy}</StyledText>
-                        </Box>
-                      </Flex>
-                    </MiniCard>
-                  </div>
-                  {isModuleChecked && (
-                    <>
-                      <FormGroup
-                        label="Model"
-                        className={styles.module_options}
-                      >
-                        <ModelDropdown
-                          fieldName={'selectedModel'}
-                          tabIndex={0}
-                          options={MODELS_FOR_MODULE_TYPE[moduleType]}
-                        />
-                      </FormGroup>
-                      {showSlotOption && (
-                        <>
-                          {!enableSlotSelection && (
-                            <Tooltip {...tooltipProps}>
-                              {slotOptionTooltip}
-                            </Tooltip>
-                          )}
-
-                          <div
-                            {...targetProps}
-                            className={styles.module_options}
-                          >
-                            <FormGroup label="Position">
-                              <SlotDropdown
-                                fieldName={'selectedSlot'}
-                                options={getAllModuleSlotsByType(moduleType)}
-                                disabled={!enableSlotSelection}
-                                tabIndex={1}
-                              />
-                            </FormGroup>
-                          </div>
-
-                          <ConnectedSlotMap fieldName={'selectedSlot'} />
-                        </>
-                      )}
-                    </>
-                  )}
+          {modules.map(
+            (
+              {
+                moduleType,
+                name,
+                supportingCopy,
+                selectedModel,
+                selectedSlot,
+              }: any,
+              index: number
+            ) => {
+              const isModuleChecked = selectedModules.includes(name)
+              const showSlotOption = moduleType !== THERMOCYCLER_MODULE_TYPE
+              const showModelOption =
+                moduleType !== ('gripperModuleType' || 'trashChuteModuleType')
+              const noCollisionIssue =
+                selectedModel && !isModuleWithCollisionIssue(selectedModel)
+              const slotOptionTooltip = (
+                <div className={styles.slot_tooltip}>
+                  {i18n.t('tooltip.edit_module_modal.slot_selection')}
                 </div>
-                <div className={styles.line_separator} />
-              </>
-            )
-          })}
+              )
+
+              const enableSlotSelection =
+                disabledModuleRestriction || noCollisionIssue
+
+              return (
+                <>
+                  <div className={styles.module_section} key={index}>
+                    <div className={styles.mini_card}>
+                      <MiniCard
+                        isSelected={isModuleChecked}
+                        isError={false}
+                        onClick={() => toggleModuleSelection(name)}
+                      >
+                        <img
+                          src={OT3_PNG}
+                          css={css`
+                            width: 67px;
+                            height: 54px;
+                          `}
+                        />
+                        <Flex
+                          flexDirection={DIRECTION_COLUMN}
+                          marginLeft={SPACING.spacing4}
+                          marginTop={SPACING.spacing3}
+                          marginBottom={SPACING.spacing4}
+                        >
+                          <StyledText as="h4">{name}</StyledText>
+                          <Box>
+                            <StyledText as="h4">{supportingCopy}</StyledText>
+                          </Box>
+                        </Flex>
+                      </MiniCard>
+                    </div>
+                    {isModuleChecked && showModelOption && (
+                      <>
+                        <FormGroup
+                          label="Model"
+                          className={styles.module_options}
+                        >
+                          <ModelDropdown
+                            fieldName={selectedModel}
+                            tabIndex={0}
+                            options={MODELS_FOR_MODULE_TYPE[moduleType]}
+                          />
+                        </FormGroup>
+                        {showSlotOption && (
+                          <>
+                            {!enableSlotSelection && (
+                              <Tooltip {...tooltipProps}>
+                                {slotOptionTooltip}
+                              </Tooltip>
+                            )}
+
+                            <div
+                              {...targetProps}
+                              className={styles.module_options}
+                            >
+                              <FormGroup label="Position">
+                                <SlotDropdown
+                                  fieldName={selectedSlot}
+                                  options={getAllModuleSlotsByType(moduleType)}
+                                  disabled={!enableSlotSelection}
+                                  tabIndex={1}
+                                />
+                              </FormGroup>
+                            </div>
+
+                            <ConnectedSlotMap fieldName={selectedSlot} />
+                          </>
+                        )}
+                      </>
+                    )}
+                  </div>
+                  <div className={styles.line_separator} />
+                </>
+              )
+            }
+          )}
         </>
       </div>
     </>
