fbea39821e26d6d1067723fa7593b1546441e220
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2023-04-19 20:38:20 -0400

    no need to gate firmware from updating motor pos

--- api/src/opentrons/hardware_control/backends/ot3controller.py
+++ api/src/opentrons/hardware_control/backends/ot3controller.py
@@ -426,12 +426,6 @@ class OT3Controller:
     async def update_motor_estimation(self, axes: Sequence[OT3Axis]) -> None:
         """Update motor position estimation for commanded nodes, and update cache of data."""
         nodes = set([axis_to_node(a) for a in axes])
-        for node in nodes:
-            if not (
-                node in self._motor_status.keys()
-                and self._motor_status[node].encoder_ok
-            ):
-                raise MustHomeError(f"Axis {node} has invalid encoder position")
         response = await update_motor_position_estimation(self._messenger, nodes)
         self._handle_motor_status_response(response)
 
