c9946784984122429a54f5cbb628ff23781ca555
Max Marrone <max@opentrons.com>
2023-04-21 14:13:04 -0400

    Convert to int using the enum's official way, not with int().

--- api/src/opentrons/protocol_engine/state/modules.py
+++ api/src/opentrons/protocol_engine/state/modules.py
@@ -882,7 +882,7 @@ class ModuleView(HasState[ModuleState]):
             HeaterShakerMovementRestrictors(
                 plate_shaking=substate.is_plate_shaking,
                 latch_closed=substate.is_labware_latch_closed,
-                deck_slot=int(self.get_location(substate.module_id).slotName),
+                deck_slot=self.get_location(substate.module_id).slotName.as_int(),
             )
             for substate in hs_substates
         ]
