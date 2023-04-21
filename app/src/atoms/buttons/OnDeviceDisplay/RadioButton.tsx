2d8614f9e448499eae681b4f3b68278c83132678
Eric Wagoner <ewagoner@gmail.com>
2023-04-21 01:24:51 -0400

    Add TODO to make button tabbable and focusable [RAUT-366]

--- app/src/atoms/buttons/OnDeviceDisplay/RadioButton.tsx
+++ app/src/atoms/buttons/OnDeviceDisplay/RadioButton.tsx
@@ -59,6 +59,8 @@ export function RadioButton(props: RadioButtonProps): JSX.Element {
     cursor: not-allowed;
   `
 
+  // TODO: (ew, 2023-04-21): button is not tabbable, so focus state
+  // is not possible on ODD. It's testable in storybook but not in real life.
   const SettingButtonLabel = styled.label`
     border-radius: ${BORDERS.size_four};
     cursor: pointer;
