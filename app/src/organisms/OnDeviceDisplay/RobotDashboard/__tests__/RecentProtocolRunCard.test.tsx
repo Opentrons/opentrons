1add33415931d74e42a28134bc71314a38d22f01
koji <koji.kanao@opentrons.com>
2023-04-24 12:15:47 -0400

    update robot dashboard test

--- app/src/organisms/OnDeviceDisplay/RobotDashboard/__tests__/RecentProtocolRunCard.test.tsx
+++ app/src/organisms/OnDeviceDisplay/RobotDashboard/__tests__/RecentProtocolRunCard.test.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react'
 import { fireEvent } from '@testing-library/react'
 import { formatDistance } from 'date-fns'
-import { when, resetAllWhenMocks } from 'jest-when'
 
 import { renderWithProviders } from '@opentrons/components'
-import { useAllRunsQuery } from '@opentrons/react-api-client'
 
 import { i18n } from '../../../../i18n'
 import { useMissingProtocolHardware } from '../../../../pages/Protocols/hooks'
-import { useTrackEvent } from '../../../../redux/analytics'
-import { useTrackProtocolRunEvent } from '../../../Devices/hooks'
 import { RecentRunProtocolCard } from '../'
 
 import type { ProtocolHardware } from '../../../../pages/Protocols/hooks'
 
-jest.mock('@opentrons/react-api-client')
 jest.mock('../../../../pages/Protocols/hooks')
 jest.mock('../../../../organisms/Devices/hooks')
 
@@ -54,7 +49,6 @@ const missingBoth = [
     connected: false,
   },
 ] as ProtocolHardware[]
-const RUN_ID = '95e67900-bc9f-4fbf-92c6-cc4d7226a51b'
 
 const mockPush = jest.fn()
 jest.mock('react-router-dom', () => {
@@ -65,34 +59,8 @@ jest.mock('react-router-dom', () => {
   }
 })
 
-const mockRun = {
-  actions: [],
-  completedAt: '2023-04-12T15:14:13.811757+00:00',
-  createdAt: '2023-04-12T15:13:52.110602+00:00',
-  current: false,
-  errors: [],
-  // id: '853a3fae-8043-47de-8f03-5d28b3ee3d35',
-  id: mockProtocolId,
-  labware: [],
-  labwareOffsets: [],
-  liquids: [],
-  modules: [],
-  pipettes: [],
-  protocolId: 'mockSortedProtocolID',
-  status: 'stopped',
-}
-
-const mockUseAllRunsQuery = useAllRunsQuery as jest.MockedFunction<
-  typeof useAllRunsQuery
->
 const mockUseMissingProtocolHardware = useMissingProtocolHardware as jest.MockedFunction<
   typeof useMissingProtocolHardware
->
-const mockUseTrackProtocolRunEvent = useTrackProtocolRunEvent as jest.MockedFunction<
-  typeof useTrackProtocolRunEvent
->
-const mockUseTrackEvent = useTrackEvent as jest.MockedFunction<
-  typeof useTrackEvent
 >
 
 const render = (props: React.ComponentProps<typeof RecentRunProtocolCard>) => {
@@ -100,9 +68,6 @@ const render = (props: React.ComponentProps<typeof RecentRunProtocolCard>) => {
     i18nInstance: i18n,
   })
 }
-
-let mockTrackEvent: jest.Mock
-let mockTrackProtocolRunEvent: jest.Mock
 
 describe('RecentRunProtocolCard', () => {
   let props: React.ComponentProps<typeof RecentRunProtocolCard>
@@ -113,20 +78,10 @@ describe('RecentRunProtocolCard', () => {
       protocolId: mockProtocolId,
       lastRun: mockLastRun,
     }
-    mockUseAllRunsQuery.mockReturnValue({ data: { data: [mockRun] } } as any)
-    mockTrackEvent = jest.fn()
-    mockTrackProtocolRunEvent = jest.fn(
-      () => new Promise(resolve => resolve({}))
-    )
-    mockUseTrackEvent.mockReturnValue(mockTrackEvent)
     mockUseMissingProtocolHardware.mockReturnValue([])
-    when(mockUseTrackProtocolRunEvent).calledWith(RUN_ID).mockReturnValue({
-      trackProtocolRunEvent: mockTrackProtocolRunEvent,
-    })
   })
 
   afterEach(() => {
-    resetAllWhenMocks()
     jest.clearAllMocks()
   })
 
@@ -158,10 +113,10 @@ describe('RecentRunProtocolCard', () => {
     getByText('Missing hardware')
   })
 
-  // it('when tapping a card, a mock function is called', () => {
-  //   const [{ getByLabelText }] = render(props)
-  //   const button = getByLabelText('RecentRunProtocolCard')
-  //   fireEvent.click(button)
-  //   expect(mockPush).toHaveBeenCalledWith(`protocols/${props.protocolId}`)
-  // })
+  it('when tapping a card, a mock function is called', () => {
+    const [{ getByLabelText }] = render(props)
+    const button = getByLabelText('RecentRunCard')
+    fireEvent.click(button)
+    expect(mockPush).toHaveBeenCalledWith(`protocols/${props.protocolId}`)
+  })
 })
