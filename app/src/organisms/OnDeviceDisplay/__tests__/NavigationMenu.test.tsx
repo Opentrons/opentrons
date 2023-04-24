416506de6c4a0a0a06e0b25a8e0cf4043cbad1b6
koji <koji.kanao@opentrons.com>
2023-04-24 09:56:21 -0400

    fix test-js errors

--- app/src/organisms/OnDeviceDisplay/__tests__/NavigationMenu.test.tsx
+++ app/src/organisms/OnDeviceDisplay/__tests__/NavigationMenu.test.tsx
@@ -41,7 +41,7 @@ describe('NavigationMenu', () => {
     fireEvent.click(getByLabelText('BackgroundOverlay_ModalShell'))
     expect(props.onClick).toHaveBeenCalled()
     const home = getByText('Home gantry')
-    getByLabelText('home-gantry')
+    getByLabelText('home-gantry_icon')
     fireEvent.click(home)
     expect(mockHome).toHaveBeenCalled()
   })
