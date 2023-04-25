2bba28b9b950736c53c9f8d802ec6305e9e622ce
koji <koji.kanao@opentrons.com>
2023-04-25 16:54:23 -0400

    fix(odd): remove Change Brightness Button from ODD Menu
    remove Change Brightness Button from ODD Menu. This button was added to test the function and there
    is no way to change the brightness except screen/ssh to a robot and editing the file

--- app/src/pages/OnDeviceDisplay/TempODDMenu/index.tsx
+++ app/src/pages/OnDeviceDisplay/TempODDMenu/index.tsx
@@ -39,15 +39,6 @@ export function TempODDMenu(): JSX.Element {
           {`Download latest OT-3 system version ${latestRobotSystemVersion}`}
         </PrimaryButton>
       )}
-      <Flex marginY="1rem">
-        <PrimaryButton
-          onClick={() =>
-            dispatch(updateConfigValue('onDeviceDisplaySettings.brightness', 7))
-          }
-        >
-          {'Change Display Brightness '}
-        </PrimaryButton>
-      </Flex>
       {/* TODO(bh, 2022-12-7): TEMP links to all routes to allow development throughout the app */}
       {onDeviceDisplayRoutes.map((route: RouteProps) => (
         <Flex key={route.path} margin="0.5rem">
