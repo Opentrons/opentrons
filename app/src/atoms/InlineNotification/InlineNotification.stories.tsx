c089b89f2a45186c91e4de66d1b47f2bc41b7690
Eric Wagoner <ewagoner@gmail.com>
2023-04-27 17:13:50 -0400

    Create new InlineNotification component [RCORE-734]

---
+++ app/src/atoms/InlineNotification/InlineNotification.stories.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import { InlineNotification } from '.'
+import type { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'ODD/Atoms/InlineNotification',
+  argTypes: {
+    hug: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: false,
+    },
+    type: {
+      control: {
+        type: 'select',
+        options: ['alert', 'error', 'neutral', 'success'],
+      },
+      defaultValue: 'success',
+    },
+    onClick: { action: 'clicked' },
+    onCloseClick: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: true,
+    },
+  },
+} as Meta
+
+const Template: Story<
+  React.ComponentProps<typeof InlineNotification>
+> = args => <InlineNotification {...args} />
+
+export const InlineNotificationComponent = Template.bind({})
+InlineNotificationComponent.args = {
+  heading: 'awesome',
+  message: 'you did it',
+  type: 'success',
+}
---
+++ app/src/atoms/InlineNotification/index.tsx
@@ -0,0 +1,118 @@
+import * as React from 'react'
+import {
+  Icon,
+  JUSTIFY_SPACE_BETWEEN,
+  IconProps,
+  Flex,
+  DIRECTION_ROW,
+  ALIGN_CENTER,
+  COLORS,
+  SPACING,
+  TYPOGRAPHY,
+  BORDERS,
+  Btn,
+} from '@opentrons/components'
+import { StyledText } from '../text'
+
+import type { StyleProps } from '@opentrons/components'
+
+export type InlineNotificationType = 'alert' | 'error' | 'neutral' | 'success'
+
+export interface InlineNotificationProps extends StyleProps {
+  /** name constant of the icon to display */
+  type: InlineNotificationType
+  /** InlineNotification contents */
+  heading: string
+  message?: string
+  /** Dynamic width based on contents */
+  hug?: boolean
+  /** optional function to run when clicked */
+  onClick?: () => void
+  /** optional handler to show close button/clear alert  */
+  onCloseClick?: (() => void) | React.MouseEventHandler<HTMLButtonElement>
+}
+
+const INLINE_NOTIFICATION_PROPS_BY_TYPE: Record<
+  InlineNotificationType,
+  { icon: IconProps; backgroundColor: string; color: string }
+> = {
+  alert: {
+    icon: { name: 'ot-alert' },
+    backgroundColor: COLORS.yellow_three,
+    color: COLORS.yellow_two,
+  },
+  error: {
+    icon: { name: 'ot-alert' },
+    backgroundColor: COLORS.red_three,
+    color: COLORS.red_two,
+  },
+  neutral: {
+    icon: { name: 'information' },
+    backgroundColor: COLORS.darkBlack_twenty,
+    color: COLORS.darkBlackEnabled,
+  },
+  success: {
+    icon: { name: 'ot-check' },
+    backgroundColor: COLORS.green_three,
+    color: COLORS.green_two,
+  },
+}
+
+export function InlineNotification(
+  props: InlineNotificationProps
+): JSX.Element {
+  const { heading, hug = false, onClick, onCloseClick, message, type } = props
+  const fullHeading = `${heading}${message ? '. ' : ''}`
+  const fullmessage = `${message}.`
+  const inlineNotificationProps = INLINE_NOTIFICATION_PROPS_BY_TYPE[type]
+  const iconProps = {
+    ...inlineNotificationProps.icon,
+    size: '1.75rem',
+    color: INLINE_NOTIFICATION_PROPS_BY_TYPE[type].color,
+  }
+  return (
+    <Flex
+      alignItems={ALIGN_CENTER}
+      backgroundColor={INLINE_NOTIFICATION_PROPS_BY_TYPE[type].backgroundColor}
+      borderRadius={BORDERS.size_three}
+      data-testid={`InlineNotification_${type}`}
+      flexDirection={DIRECTION_ROW}
+      gridGap="0.75rem"
+      justifyContent={JUSTIFY_SPACE_BETWEEN}
+      onClick={onClick}
+      padding={`0.75rem ${SPACING.spacing4}`}
+      width={hug ? 'max-content' : '100%'}
+    >
+      <Icon {...iconProps} aria-label={`icon_${type}`} />
+      <Flex flex="1" alignItems={ALIGN_CENTER}>
+        <StyledText
+          fontSize={TYPOGRAPHY.fontSize22}
+          fontWeight={TYPOGRAPHY.fontWeightRegular}
+          lineHeight={TYPOGRAPHY.lineHeight28}
+        >
+          <span
+            css={`
+              font-weight: ${TYPOGRAPHY.fontWeightSemiBold};
+            `}
+          >
+            {fullHeading}
+          </span>
+          {message && fullmessage}
+        </StyledText>
+      </Flex>
+      {onCloseClick && (
+        <Btn
+          data-testid="InlineNotification_close-button"
+          onClick={onCloseClick}
+        >
+          <Icon
+            width={SPACING.spacing7}
+            height={SPACING.spacing7}
+            name="close"
+            aria-label="close_icon"
+          />
+        </Btn>
+      )}
+    </Flex>
+  )
+}
