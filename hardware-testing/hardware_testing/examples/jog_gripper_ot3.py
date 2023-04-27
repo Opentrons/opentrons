61076d3301e815bb9724d614ce1747cbdf11f6cd
andySigler <andrewsigler1@gmail.com>
2023-04-27 14:35:50 -0400

    set force using user input

--- hardware-testing/hardware_testing/examples/jog_gripper_ot3.py
+++ hardware-testing/hardware_testing/examples/jog_gripper_ot3.py
@@ -6,7 +6,7 @@ from hardware_testing.opentrons_api import types
 from hardware_testing.opentrons_api import helpers_ot3
 
 
-async def _main(is_simulating: bool, force: int) -> None:
+async def _main(is_simulating: bool) -> None:
     mount = types.OT3Mount.GRIPPER
     api = await helpers_ot3.build_async_ot3_hardware_api(is_simulating=is_simulating)
     await api.home()
@@ -14,20 +14,22 @@ async def _main(is_simulating: bool, force: int) -> None:
         await helpers_ot3.jog_mount_ot3(api, mount)
         while True:
             inp = input("\"g\" to grip, \"u\" to ungrip, \"j\" to jog: ").strip()
-            if inp == "g":
-                await api.grip(force)
-            elif inp == "u":
+            if inp[0] == "g":
+                try:
+                    force = float(inp[1:])
+                    await api.grip(force)
+                except Exception as e:
+                    print(e)
+            elif inp[0] == "u":
                 await api.ungrip()
-            elif inp == "j":
+            elif inp[0] == "j":
                 break
             else:
                 print(f"unexpected input: {inp}")
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--simulate", action="store_true")
-    parser.add_argument("--force", type=int, default=15)
     args = parser.parse_args()
-    asyncio.run(_main(args.simulate, args.force))
+    asyncio.run(_main(args.simulate))
