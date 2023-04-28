2d9cce9ef3c885c8810c48cc3a9190582f09a19a
Brian Cooper <brian@opentrons.com>
2023-04-28 16:40:46 -0400

    delete comment and add another comment

--- app/src/organisms/LabwarePositionCheck/JogToWell.tsx
+++ app/src/organisms/LabwarePositionCheck/JogToWell.tsx
@@ -81,6 +81,7 @@ export const JogToWell = (props: JogToWellProps): JSX.Element | null => {
     //  if a user reaches the "confirm exit" modal (unmounting this component)
     //  and clicks "go back" we are able so initialize the live offset to whatever
     //  distance they had already jogged before clicking exit.
+    // the `mounted` variable prevents a possible memory leak (see https://legacy.reactjs.org/docs/hooks-effect.html#example-using-hooks-1)
     let mounted = true
     if (mounted) {
       handleJog('x', 1, 0, setJoggedPosition)
