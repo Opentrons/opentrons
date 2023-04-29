1edb53e565189002439f762ecf957824d9490313
koji <koji.kanao@opentrons.com>
2023-04-29 07:10:24 -0400

    Update OnDeviceDisplayApp.tsx

--- app/src/App/OnDeviceDisplayApp.tsx
+++ app/src/App/OnDeviceDisplayApp.tsx
@@ -188,7 +188,7 @@ export const OnDeviceDisplayApp = (): JSX.Element => {
   const isIdle = useIdle(sleepTime, options)
 
   return (
-    <ApiHostProvider hostname="192.168.0.107">
+    <ApiHostProvider hostname="localhost">
       <Box width="100%">
         {Boolean(isIdle) ? (
           <SleepScreen />
