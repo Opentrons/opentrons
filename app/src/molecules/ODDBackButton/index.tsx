00d5aabfe7e485144fc5a6923620411b6be596b7
Eric Wagoner <ewagoner@gmail.com>
2023-05-02 17:28:14 -0400

    Move ODDBackButton to molecules and add stories and tests [RCORE-734]

--- app/src/molecules/ODDBackButton/index.tsx
+++ app/src/molecules/ODDBackButton/index.tsx
@@ -13,27 +13,31 @@ import {
 export function ODDBackButton(
   props: React.HTMLProps<HTMLButtonElement>
 ): JSX.Element {
+  const { onClick, label } = props
+
   return (
     <Flex
       alignItems={ALIGN_CENTER}
-      gridGap={SPACING.spacing4}
       fontSize={TYPOGRAPHY.fontSize38}
       fontWeight={TYPOGRAPHY.fontWeightLevel2_bold}
+      gridGap={SPACING.spacing4}
       lineHeight={TYPOGRAPHY.lineHeight48}
     >
       <Btn
+        onClick={onClick}
         paddingLeft="0rem"
         paddingRight={SPACING.spacing5}
-        onClick={props.onClick}
         width="3rem"
       >
         <Icon
+
+          color={COLORS.darkBlack_hundred}
+          data-testid="back_icon"
           name="back"
           width={SPACING.spacing5}
-          color={COLORS.darkBlack_hundred}
         />
       </Btn>
-      {props.label}
+      {label}
     </Flex>
   )
 }
