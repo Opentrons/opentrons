cb82e33309e0ee6e1838dd2f20d1f239bf03e159
Andy Sigler <andrewsigler1@gmail.com>
2023-05-03 12:55:23 -0400

    feat(hardware-testing): Volumetric script support multi increment test (#12612)

--- hardware-testing/hardware_testing/protocols/gravimetric_ot3_p1000.py
+++ hardware-testing/hardware_testing/protocols/gravimetric_ot3_p1000.py
@@ -5,12 +5,13 @@ metadata = {"protocolName": "gravimetric-ot3-p1000"}
 # FIXME: bump to v2.14 to utilize protocol engine
 requirements = {"robotType": "OT-3", "apiLevel": "2.13"}
 
-SLOT_VIAL = 4
+SLOT_SCALE = 4
 SLOTS_TIPRACK = {
     50: [3],
     200: [6],
     1000: [9],
 }
+LABWARE_ON_SCALE = "radwag_pipette_calibration_vial"
 
 
 def run(ctx: ProtocolContext) -> None:
@@ -20,7 +21,7 @@ def run(ctx: ProtocolContext) -> None:
         for size, slots in SLOTS_TIPRACK.items()
         for slot in slots
     ]
-    vial = ctx.load_labware("radwag_pipette_calibration_vial", SLOT_VIAL)
+    vial = ctx.load_labware(LABWARE_ON_SCALE, SLOT_SCALE)
     pipette = ctx.load_instrument("p1000_single_gen3", "left")
     for rack in tipracks:
         pipette.pick_up_tip(rack["A1"])
