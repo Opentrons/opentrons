6be95d1beeeddc222a717cc7d6406b9920cee77c
Brian Cooper <brian@opentrons.com>
2023-05-04 15:17:44 -0400

    add new non-redundant spacing sizes

--- components/src/ui-style-constants/spacing.ts
+++ components/src/ui-style-constants/spacing.ts
@@ -1,4 +1,3 @@
-// spacing vars NOTE: this is still flagged to be revisited in Figma
 export const spacing1 = '0.125rem' // 2px
 export const spacing2 = '0.25rem' // 4px
 export const spacing3 = '0.5rem' // 8px
@@ -16,3 +15,21 @@ export const spacingXXS = '1px' // 1px
 export const spacingSM = '0.625rem' //  10px
 export const spacingXXL = '2.5rem' // 40px
 export const spacingAuto = 'auto' // auto
+
+
+
+
+
+
+
+export const spacing8 = '0.5rem' // 8px
+export const spacing12 = '0.75rem' // 12px
+export const spacing16 = '1rem' // 16px
+export const spacing20 = '1.25rem' // 20px
+export const spacing24 = '1.5rem' // 24px
+export const spacing32 = '2rem' // 32px
+export const spacing40 = '2.5rem' // 40px
+export const spacing48 = '3rem' // 48px
+export const spacing68 = '4.25rem' // 68px
+export const spacing80 = '5rem' // 80px
+export const spacing120 = '7.5rem' // 120px
