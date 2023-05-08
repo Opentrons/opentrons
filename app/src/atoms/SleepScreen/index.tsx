5364521417ad0aaffade43ad795995d7ea3b0078
koji <koji.kanao@opentrons.com>
2023-05-08 16:11:17 -0400

    add empty screen instead of bouncing logo screen

--- app/src/atoms/SleepScreen/index.tsx
+++ app/src/atoms/SleepScreen/index.tsx
@@ -4,6 +4,11 @@ import { Flex, COLORS } from '@opentrons/components'
 
 export function SleepScreen(): JSX.Element {
   return (
-    <Flex width="100vw" height="100vh" backgroundColor={COLORS.black}></Flex>
+    <Flex
+      width="100vw"
+      height="100vh"
+      backgroundColor={COLORS.black}
+      data-testid="Touchscreen_SleepScreen"
+    ></Flex>
   )
 }
