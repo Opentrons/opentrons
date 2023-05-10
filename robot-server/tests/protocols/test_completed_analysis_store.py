056f44baf7f1fb422527489848a5e0691ea3af19
Seth Foster <seth@opentrons.com>
2023-05-10 15:57:03 -0400

    fix(robot-server): memory cache analyses (#12648)
    Getting analysis results can really bog down the robot server - in fact,
    it can block the event loop such that new http requests get stalled, and
    that means that apps will mark the server unhealthy.
    
    Recently, the app started asking for analyses much more frequently, which
    would cause the robot server to bog down more frequently, which would
    cause the app to mark the robot unhealthy more frequently. This was an
    unintended change, and reverting it fixes the robot unhealthiness; but
    getting analysis results still threatens to bog down the server, so this
    memcache should fix the problem on the server.
    
    We have similar problems (and solutions) for other heavy resources like
    runs and protocols; but there we do the caching with
    functools.lru_cache. This is not possible for analysis results because
    we get analysis results with async functions, for a very good reason; so
    instead, we need a custom cache implementation.
    
    This cache implementation uses a python dict to hold the result objects;
    since there are limited places to add results, we can add things to the
    cache when they're added to the database and add them to the cache when
    we get things from the database. We use a deque to maintain a FIFO for
    cache eviction.
    
    
    By splitting the implementation to add
    - a public module for CompletedAnalysisStore, which has a public
    interface that can be tested, with a
    - public module for the memory cache, which has a public interface that
    can be tested and injected into CompletedAnalysisStore
    
    we can get back to testing only public interfaces, which is really nice.
    
    At the same time, we can fix the race condition where get_by_protocol_id
    wasn't as reentrant as it could be. Aside from that method, since this
    is async code we should be access safe because the memory cache is synchronous.
    
    ---------
    
    Co-authored-by: Max Marrone <max@opentrons.com>

---
+++ robot-server/tests/protocols/test_completed_analysis_store.py
@@ -0,0 +1,187 @@
+"""Test the CompletedAnalysisStore."""
+from datetime import datetime, timezone
+from pathlib import Path
+
+import pytest
+from sqlalchemy.engine import Engine
+from decoy import Decoy
+
+from robot_server.protocols.completed_analysis_store import (
+    CompletedAnalysisResource,
+    CompletedAnalysisStore,
+)
+from opentrons.protocol_reader import (
+    ProtocolSource,
+    JsonProtocolConfig,
+)
+from robot_server.protocols.analysis_memcache import MemoryCache
+from robot_server.protocols.analysis_models import CompletedAnalysis, AnalysisResult
+from robot_server.protocols.protocol_store import (
+    ProtocolStore,
+    ProtocolResource,
+)
+
+
+@pytest.fixture
+def memcache(decoy: Decoy) -> MemoryCache[str, CompletedAnalysisResource]:
+    """Get a memcache mock."""
+    return decoy.mock(cls=MemoryCache)
+
+
+@pytest.fixture
+def subject(
+    memcache: MemoryCache[str, CompletedAnalysisResource],
+    sql_engine: Engine,
+) -> CompletedAnalysisStore:
+    """Get a subject."""
+    return CompletedAnalysisStore(sql_engine, memcache, "2")
+
+
+@pytest.fixture
+def protocol_store(sql_engine: Engine) -> ProtocolStore:
+    """Return a `ProtocolStore` linked to the same database as the subject under test.
+
+    `ProtocolStore` is tested elsewhere.
+    We only need it here to prepare the database for our `AnalysisStore` tests.
+    An analysis always needs a protocol to link to.
+    """
+    return ProtocolStore.create_empty(sql_engine=sql_engine)
+
+
+def make_dummy_protocol_resource(protocol_id: str) -> ProtocolResource:
+    """Return a placeholder `ProtocolResource` to insert into a `ProtocolStore`.
+
+    Args:
+        protocol_id: The ID to give to the new `ProtocolResource`.
+    """
+    return ProtocolResource(
+        protocol_id=protocol_id,
+        created_at=datetime(year=2021, month=1, day=1, tzinfo=timezone.utc),
+        source=ProtocolSource(
+            directory=Path("/dev/null"),
+            main_file=Path("/dev/null"),
+            config=JsonProtocolConfig(schema_version=123),
+            files=[],
+            metadata={},
+            robot_type="OT-2 Standard",
+            content_hash="abc123",
+        ),
+        protocol_key=None,
+    )
+
+
+def _completed_analysis_resource(
+    analysis_id: str, protocol_id: str
+) -> CompletedAnalysisResource:
+    return CompletedAnalysisResource(
+        analysis_id,
+        protocol_id,
+        "2",
+        CompletedAnalysis(
+            id=analysis_id,
+            result=AnalysisResult.OK,
+            pipettes=[],
+            labware=[],
+            modules=[],
+            commands=[],
+            errors=[],
+            liquids=[],
+        ),
+    )
+
+
+async def test_get_by_analysis_id_prefers_cache(
+    subject: CompletedAnalysisStore,
+    sql_engine: Engine,
+    memcache: MemoryCache[str, CompletedAnalysisResource],
+    protocol_store: ProtocolStore,
+    decoy: Decoy,
+) -> None:
+    """It should return analyses without using SQL the second time the analyses are accessed."""
+    resource = _completed_analysis_resource("analysis-id", "protocol-id")
+    protocol_store.insert(make_dummy_protocol_resource("protocol-id"))
+    # When we retrieve a resource via its id we should see it query the cache, and it should
+    # return the identity-same resource
+    decoy.when(memcache.get("analysis-id")).then_return(resource)
+    assert (await subject.get_by_id("analysis-id")) is resource
+
+
+async def test_get_by_analysis_falls_back_to_sql(
+    subject: CompletedAnalysisStore,
+    sql_engine: Engine,
+    memcache: MemoryCache[str, CompletedAnalysisResource],
+    protocol_store: ProtocolStore,
+    decoy: Decoy,
+) -> None:
+    """It should return analyses from sql if they are not cached."""
+    resource = _completed_analysis_resource("analysis-id", "protocol-id")
+    protocol_store.insert(make_dummy_protocol_resource("protocol-id"))
+    await subject.add(resource)
+    # the analysis is not cached
+    decoy.when(memcache.get("analysis-id")).then_raise(KeyError())
+    analysis_from_sql = await subject.get_by_id("analysis-id")
+    # the cached analysis should be value-equal to what we entered
+    assert analysis_from_sql == resource
+
+
+async def test_get_by_analysis_id_caches_results(
+    subject: CompletedAnalysisStore,
+    sql_engine: Engine,
+    memcache: MemoryCache[str, CompletedAnalysisResource],
+    protocol_store: ProtocolStore,
+    decoy: Decoy,
+) -> None:
+    """It should cache successful fetches from sql."""
+    resource = _completed_analysis_resource("analysis-id", "protocol-id")
+    protocol_store.insert(make_dummy_protocol_resource("protocol-id"))
+    await subject.add(resource)
+    # the analysis is not cached
+    decoy.when(memcache.get("analysis-id")).then_raise(KeyError())
+    from_sql = await subject.get_by_id("analysis-id")
+    assert from_sql == resource
+    decoy.verify(memcache.insert("analysis-id", from_sql))
+
+
+async def test_get_ids_by_protocol(
+    subject: CompletedAnalysisStore, sql_engine: Engine, protocol_store: ProtocolStore
+) -> None:
+    """It should return correct analysis id lists."""
+    resource_1 = _completed_analysis_resource("analysis-id-1", "protocol-id-1")
+    resource_2 = _completed_analysis_resource("analysis-id-2", "protocol-id-1")
+    resource_3 = _completed_analysis_resource("analysis-id-3", "protocol-id-2")
+    protocol_store.insert(make_dummy_protocol_resource("protocol-id-1"))
+    protocol_store.insert(make_dummy_protocol_resource("protocol-id-2"))
+    await subject.add(resource_1)
+    await subject.add(resource_2)
+    await subject.add(resource_3)
+    assert sorted(subject.get_ids_by_protocol("protocol-id-1")) == sorted(
+        ["analysis-id-1", "analysis-id-2"]
+    )
+
+
+async def test_get_by_protocol(
+    subject: CompletedAnalysisStore,
+    sql_engine: Engine,
+    memcache: MemoryCache[str, CompletedAnalysisResource],
+    protocol_store: ProtocolStore,
+    decoy: Decoy,
+) -> None:
+    """It should get analysis by protocol with appropriate caching."""
+    resource_1 = _completed_analysis_resource("analysis-id-1", "protocol-id-1")
+    resource_2 = _completed_analysis_resource("analysis-id-2", "protocol-id-1")
+    resource_3 = _completed_analysis_resource("analysis-id-3", "protocol-id-2")
+    protocol_store.insert(make_dummy_protocol_resource("protocol-id-1"))
+    protocol_store.insert(make_dummy_protocol_resource("protocol-id-2"))
+    decoy.when(memcache.insert("analysis-id-1", resource_1)).then_return(None)
+    decoy.when(memcache.insert("analysis-id-2", resource_2)).then_return(None)
+    decoy.when(memcache.insert("analysis-id-3", resource_3)).then_return(None)
+    await subject.add(resource_1)
+    await subject.add(resource_2)
+    await subject.add(resource_3)
+    decoy.when(memcache.get("analysis-id-1")).then_raise(KeyError())
+    decoy.when(memcache.get("analysis-id-2")).then_return(resource_2)
+    decoy.when(memcache.contains("analysis-id-1")).then_return(False)
+    decoy.when(memcache.contains("analysis-id-2")).then_return(True)
+    decoy.when(memcache.insert("analysis-id-1", resource_1)).then_return(None)
+    resources = await subject.get_by_protocol("protocol-id-1")
+    assert resources == [resource_1, resource_2]
