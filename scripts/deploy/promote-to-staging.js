999cbda75a5a0d22cdcee148caa239674f6b5b70
Jethary <jethary@opentrons.com>
2023-05-10 16:00:35 -0400

    updated script to assume role into prod account

--- scripts/deploy/promote-to-staging.js
+++ scripts/deploy/promote-to-staging.js
@@ -20,35 +20,70 @@ const dryrun = !flags.includes('--deploy')
 
 assert(projectDomain && tag, USAGE)
 
-const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: 'us-east-1' })
+const sts = new AWS.STS({ apiVersion: '2011-06-15' })
 
-const sandboxBucket = `sandbox.${projectDomain}`
-const stagingBucket = `staging.${projectDomain}`
-
-getDeployMetadata(s3, stagingBucket)
-  .then(prevDeployMetadata => {
-    console.log('Previous deploy metadata: %j', prevDeployMetadata)
-
-    return syncBuckets(
-      s3,
-      { bucket: sandboxBucket, path: tag },
-      { bucket: stagingBucket },
-      dryrun
-    ).then(() =>
-      setDeployMetadata(
-        s3,
-        stagingBucket,
-        '',
-        { previous: prevDeployMetadata.current || null, current: tag },
-        dryrun
-      )
+const stagingAssumeRole = () => {
+  return new Promise((resolve, reject) => {
+    sts.assumeRole(
+      {
+        RoleArn: 'arn:aws:iam::043748923082:role/administrator',
+        RoleSessionName: 'promoteToStaging',
+      },
+      (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data.Credentials)
+        }
+      }
     )
   })
-  .then(() => {
-    console.log('Promotion to staging done\n')
-    process.exit(0)
+}
+
+stagingAssumeRole()
+  .then(credentials => {
+    const stagingCredentials = new AWS.Credentials({
+      accessKeyId: credentials.AccessKeyId,
+      secretAccessKey: credentials.SecretAccessKey,
+      sessionToken: credentials.SessionToken,
+    })
+
+    const s3 = new AWS.S3({
+      apiVersion: '2006-03-01',
+      region: 'us-east-1',
+      credentials: stagingCredentials,
+    })
+
+    const sandboxBucket = `sandbox.${projectDomain}`
+    const stagingBucket = `staging.${projectDomain}`
+
+    getDeployMetadata(s3, stagingBucket)
+      .then(prevDeployMetadata => {
+        console.log('Previous deploy metadata: %j', prevDeployMetadata)
+        return syncBuckets(
+          s3,
+          { bucket: sandboxBucket, path: tag },
+          { bucket: stagingBucket },
+          dryrun
+        ).then(() =>
+          setDeployMetadata(
+            s3,
+            stagingBucket,
+            '',
+            { previous: prevDeployMetadata.current || null, current: tag },
+            dryrun
+          )
+        )
+      })
+      .then(() => {
+        console.log('Promotion to staging done\n')
+        process.exit(0)
+      })
+      .catch(error => {
+        console.error(error.message)
+        process.exit(1)
+      })
   })
-  .catch(error => {
-    console.error(error.message)
-    process.exit(1)
+  .catch(err => {
+    console.error(err)
   })
