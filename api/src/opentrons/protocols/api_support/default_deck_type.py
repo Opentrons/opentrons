c20b890ae47eb91b2e993186f33514c1d6362690
Max Marrone <max@opentrons.com>
2023-05-11 14:50:59 -0400

    Clarify deprecation comment.

--- api/src/opentrons/protocols/api_support/default_deck_type.py
+++ api/src/opentrons/protocols/api_support/default_deck_type.py
@@ -12,13 +12,14 @@ def guess_from_global_config() -> str:
     """Return a default deck type based on global environment configuration.
 
     Deprecated:
-        Things that need to know a deck type should request it explicitly.
-
         The notion of "a default deck type" doesn't make sense now that we have:
 
           * Decks that are meaningfully different from each other (OT-2 vs. OT-3).
           * Protocol analysis running off-robot, in environments that cannot be
             permanently configured for any single specific deck type.
+
+        If you need to know the deck type, derive it from explicit sources,
+        like the protocol's declared robot type, instead.
     """
     if feature_flags.enable_ot3_hardware_controller():
         return STANDARD_OT3_DECK
