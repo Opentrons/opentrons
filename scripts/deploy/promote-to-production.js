5c8062fef42c38ef0584418551184be447d9f03a
Jethary <jethary@opentrons.com>
2023-05-11 09:02:16 -0400

    add sts.AssumeRole to promote to production script

--- scripts/deploy/promote-to-production.js
+++ scripts/deploy/promote-to-production.js
@@ -17,30 +17,66 @@ const dryrun = !flags.includes('--deploy')
 
 assert(projectDomain, USAGE)
 
-const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: 'us-east-1' })
+const sts = new AWS.STS({ apiVersion: '2011-06-15' })
 
-const stagingBucket = `staging.${projectDomain}`
-const productionBucket = projectDomain
-
-getDeployMetadata(s3, stagingBucket)
-  .then(deployMetadata => {
-    const { current } = deployMetadata
-    console.log(
-      `Promoting ${projectDomain} ${current} from staging to production\n`
-    )
-
-    return syncBuckets(
-      s3,
-      { bucket: stagingBucket },
-      { bucket: productionBucket },
-      dryrun
+const productionAssumeRole = () => {
+  return new Promise((resolve, reject) => {
+    sts.assumeRole(
+      {
+        RoleArn: 'arn:aws:iam::043748923082:role/administrator',
+        RoleSessionName: 'promoteToProduction',
+      },
+      (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data.Credentials)
+        }
+      }
     )
   })
-  .then(() => {
-    console.log('Promotion to production done\n')
-    process.exit(0)
+}
+
+productionAssumeRole()
+  .then(credentials => {
+    const stagingCredentials = new AWS.Credentials({
+      accessKeyId: credentials.AccessKeyId,
+      secretAccessKey: credentials.SecretAccessKey,
+      sessionToken: credentials.SessionToken,
+    })
+
+    const s3 = new AWS.S3({
+      apiVersion: '2006-03-01',
+      region: 'us-east-1',
+      credentials: stagingCredentials,
+    })
+
+    const stagingBucket = `staging.${projectDomain}`
+    const productionBucket = projectDomain
+
+    getDeployMetadata(s3, stagingBucket)
+      .then(deployMetadata => {
+        const { current } = deployMetadata
+        console.log(
+          `Promoting ${projectDomain} ${current} from staging to production\n`
+        )
+
+        return syncBuckets(
+          s3,
+          { bucket: stagingBucket },
+          { bucket: productionBucket },
+          dryrun
+        )
+      })
+      .then(() => {
+        console.log('Promotion to production done\n')
+        process.exit(0)
+      })
+      .catch(error => {
+        console.error(error.message)
+        process.exit(1)
+      })
   })
-  .catch(error => {
-    console.error(error.message)
-    process.exit(1)
+  .catch(err => {
+    console.error(err)
   })
