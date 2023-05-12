6aead2d35b96019db83655cbe4bf5bc92c757a99
Shlok Amin <shlokamin@me.com>
2023-05-12 17:11:33 -0400

    add hook to render new protocol toasts

--- app/src/App/hooks.ts
+++ app/src/App/hooks.ts
@@ -1,7 +1,10 @@
 import * as React from 'react'
+import difference from 'lodash/difference'
 import { useDispatch } from 'react-redux'
+import { useAllProtocolsQuery } from '@opentrons/react-api-client'
 import { useInterval } from '@opentrons/components'
 import { checkShellUpdate } from '../redux/shell'
+import { useToaster } from '../organisms/ToasterOven'
 
 import type { Dispatch } from '../redux/types'
 
@@ -12,4 +15,25 @@ export function useSoftwareUpdatePoll(): void {
     dispatch,
   ])
   useInterval(checkAppUpdate, UPDATE_RECHECK_INTERVAL_MS)
+}
+
+export function useProtocolReceiptToast(): void {
+  const protocols = useAllProtocolsQuery().data?.data ?? []
+  const protocolIds = protocols.map(({ id }) => id)
+  const { makeToast } = useToaster()
+
+  const protocolIdsRef = React.useRef(protocolIds)
+
+  React.useEffect(() => {
+    if (protocolIds.length > protocolIdsRef.current.length) {
+      const newProtocolIds = difference(protocolIds, protocolIdsRef.current)
+      const protocolNames = newProtocolIds.map(
+        id => protocols.find(p => p.id === id)?.metadata.protocolName ?? ''
+      )
+      protocolNames.forEach(name => {
+        makeToast(`new protocol ${name} added to robot!`, 'info')
+      })
+      protocolIdsRef.current = protocolIds
+    }
+  })
 }
