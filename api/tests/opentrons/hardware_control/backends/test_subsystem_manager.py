f68c5926d0322b55dccd2edb78676433fcd1ef68
Seth Foster <seth@opentrons.com>
2023-05-12 15:45:43 -0400

    test tools and targets

--- api/tests/opentrons/hardware_control/backends/test_subsystem_manager.py
+++ api/tests/opentrons/hardware_control/backends/test_subsystem_manager.py
@@ -1,5 +1,6 @@
 import asyncio
 from typing import Dict, Iterator, Set, Optional, AsyncIterator
+from itertools import chain
 
 import pytest
 
@@ -17,6 +18,7 @@ from opentrons_hardware.firmware_update import FirmwareUpdate
 from opentrons_hardware.drivers import can_bus, binary_usb
 
 from opentrons.hardware_control.backends.subsystem_manager import SubsystemManager
+from opentrons.hardware_control.types import SubSystem
 
 
 def default_subidentifier_for(target: FirmwareTarget) -> int:
@@ -28,7 +30,9 @@ def default_subidentifier_for(target: FirmwareTarget) -> int:
 
 
 def device_info_for(
-    target: FirmwareTarget, subidentifier: Optional[int] = None
+    target: FirmwareTarget,
+    subidentifier: Optional[int] = None,
+    version: int = 1,
 ) -> network.DeviceInfoCache:
     checked_subidentifier = (
         subidentifier
@@ -142,29 +146,38 @@ class ToolDetectionController:
         await self._detection_queue.put(result)
         return result
 
+    def _pipette_info_from_network(
+        self, device: Optional[network.DeviceInfoCache], default_name: PipetteName
+    ) -> Optional[tools.types.PipetteInformation]:
+        if not device:
+            return None
+        if not device.ok:
+            return None
+        pipette_name = (
+            PipetteName(device.subidentifier)
+            if device.subidentifier != 0
+            else default_name
+        )
+        return tools.types.PipetteInformation(
+            pipette_name,
+            pipette_name.value,
+            pipette_name.name,
+            f"dummyserial{pipette_name.name}",
+        )
+
     def _auto_tool_summary(
         self, devices: Dict[FirmwareTarget, network.DeviceInfoCache]
     ) -> tools.types.ToolSummary:
         return tools.types.ToolSummary(
-            left=tools.types.PipetteInformation(
-                PipetteName.p1000_single,
-                PipetteName.p1000_single.value,
-                "p1000s",
-                "P1KSV340101012",
-            )
-            if (NodeId.pipette_left in devices and devices[NodeId.pipette_left].ok)
-            else ToolType.nothing_attached,
-            right=tools.types.PipetteInformation(
-                PipetteName.p50_multi,
-                PipetteName.p50_multi.value,
-                "p50m",
-                "P50MV340202021",
-            )
-            if (NodeId.pipette_right in devices and devices[NodeId.pipette_right].ok)
-            else ToolType.nothing_attached,
+            left=self._pipette_info_from_network(
+                devices.get(NodeId.pipette_left), PipetteName.p1000_single
+            ),
+            right=self._pipette_info_from_network(
+                devices.get(NodeId.pipette_right), PipetteName.p50_multi
+            ),
             gripper=tools.types.GripperInformation(model="1", serial="12131231")
             if (NodeId.gripper in devices and devices[NodeId.gripper].ok)
-            else ToolType.nothing_attached,
+            else None,
         )
 
     async def add_resolution(
@@ -172,7 +185,7 @@ class ToolDetectionController:
         on: tools.types.ToolDetectionResult,
         devices: Dict[FirmwareTarget, network.DeviceInfoCache],
         specific: Optional[tools.types.ToolSummary] = None,
-    ) -> None:
+    ) -> tools.types.ToolSummary:
         summary = specific or self._auto_tool_summary(devices)
 
         arg = tools.types.ToolDetectionResult(
@@ -182,12 +195,13 @@ class ToolDetectionController:
             right=on.right
             if (NodeId.pipette_right in devices and devices[NodeId.pipette_right].ok)
             else ToolType.nothing_attached,
-            gripper=on.right
+            gripper=on.gripper
             if (NodeId.gripper in devices and devices[NodeId.gripper].ok)
             else ToolType.nothing_attached,
         )
 
         self._decoy.when(await self._tool_detector.resolve(arg)).then_return(summary)
+        return summary
 
 
 @pytest.fixture
@@ -313,3 +327,98 @@ async def test_ok(
     ).then_return({target: (1, "/some/path") for target in bad_targets})
     await subject.start()
     assert subject.ok == ok
+
+
+async def test_device_info(
+    subject: SubsystemManager, network_info: network.NetworkInfo, decoy: Decoy
+) -> None:
+    """It should accurately reflect the device info data from the network."""
+    info_value = default_network_info_for(
+        {
+            NodeId.gantry_x,
+            NodeId.gantry_y_bootloader,
+            NodeId.head,
+            NodeId.pipette_right,
+            USBTarget.rear_panel,
+        }
+    )
+    decoy.when(network_info.device_info).then_return(info_value)
+    assert subject.device_info == {
+        SubSystem.gantry_x: info_value[NodeId.gantry_x],
+        SubSystem.gantry_y: info_value[NodeId.gantry_y],
+        SubSystem.head: info_value[NodeId.head],
+        SubSystem.pipette_right: info_value[NodeId.pipette_right],
+        SubSystem.rear_panel: info_value[USBTarget.rear_panel],
+    }
+
+
+async def test_targets(
+    subject: SubsystemManager, network_info: network.NetworkInfo, decoy: Decoy
+) -> None:
+    """It should tell you what targets are connected if you really need to know."""
+    targets = {
+        NodeId.gantry_x,
+        NodeId.gantry_y_bootloader,
+        USBTarget.rear_panel,
+        NodeId.gripper,
+    }
+    decoy.when(network_info.targets).then_return(targets)
+    assert subject.targets == targets
+
+
+@pytest.mark.parametrize(
+    "target_info",
+    [
+        {},
+        {
+            NodeId.pipette_left: device_info_for(NodeId.pipette_left, version=0),
+            NodeId.pipette_right: device_info_for(NodeId.pipette_right),
+        },
+        {NodeId.gripper: device_info_for(NodeId.gripper)},
+        {
+            NodeId.gripper: device_info_for(NodeId.gripper, version=0),
+            NodeId.pipette_left: device_info_for(
+                NodeId.pipette_left, subidentifier=PipetteName.p1000_96.value
+            ),
+        },
+    ],
+)
+async def test_ok_tools_get_mapped(
+    target_info: Dict[FirmwareTarget, network.DeviceInfoCache],
+    subject: SubsystemManager,
+    network_info: network.NetworkInfo,
+    tool_detection_controller: ToolDetectionController,
+    update_bag: FirmwareUpdate,
+    decoy: Decoy,
+) -> None:
+    """When tools are detected no matter their update needs, if they're ok we make them present."""
+    core_targets = {NodeId.gantry_x, NodeId.gantry_y, NodeId.head, USBTarget.rear_panel}
+    all_targets = set(iter(target_info.keys())).union(core_targets)
+    decoy.when(network_info.targets).then_return(all_targets)
+    core_network_info = default_network_info_for(core_targets)
+    all_network_info = dict(chain(core_network_info.items(), target_info.items()))
+    decoy.when(network_info.device_info).then_return(all_network_info)
+
+    tool_struct = await tool_detection_controller.add_detection_on_next_check(
+        set(iter(target_info.keys()))
+    )
+    summary = await tool_detection_controller.add_resolution(tool_struct, target_info)
+
+    decoy.when(
+        update_bag.update_checker(all_network_info, all_targets, False)
+    ).then_return(
+        {
+            target: (1, "/some/path")
+            for target in target_info.keys()
+            if target.is_bootloader()
+        }
+    )
+    decoy.when(
+        update_bag.update_checker(
+            all_network_info,
+            set(),
+            True,
+        )
+    ).then_return({})
+    await subject.start()
+    assert subject.tools == summary
