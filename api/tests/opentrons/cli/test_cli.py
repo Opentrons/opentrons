2df76844995328dbbe1c3ae08c15d18a67218f37
Max Marrone <max@opentrons.com>
2023-05-12 12:40:03 -0400

    Integration-test the analysis CLI to make sure the deck def follows robot type.

--- api/tests/opentrons/cli/test_cli.py
+++ api/tests/opentrons/cli/test_cli.py
@@ -1,6 +1,7 @@
 """Test cli execution."""
 import json
 import tempfile
+import textwrap
 
 from typing import Any, Iterator, List, Tuple
 from pathlib import Path
@@ -52,3 +53,66 @@ def test_analyze(
     assert "labware" in analysis_output_json
     assert "liquids" in analysis_output_json
     assert "modules" in analysis_output_json
+
+
+_DECK_DEFINITION_TEST_SLOT = 2
+_DECK_DEFINITION_TEST_LABWARE = "agilent_1_reservoir_290ml"
+_DECK_DEFINITION_TEST_WELL = "A1"
+
+
+def _get_deck_definition_test_source(robot_type: str) -> str:
+    return textwrap.dedent(
+        f"""\
+        requirements = {{
+            "apiLevel": "2.14",
+            "robotType": "{robot_type}",
+        }}
+        def run(protocol):
+            labware = protocol.load_labware(
+                "{_DECK_DEFINITION_TEST_LABWARE}",
+                "{_DECK_DEFINITION_TEST_SLOT}",
+            )
+            test_point = labware["{_DECK_DEFINITION_TEST_WELL}"].top().point
+            protocol.comment(str(test_point))
+        """
+    )
+
+
+@pytest.mark.parametrize(
+    ("robot_type", "expected_point"),
+    [
+        # These expected_point values were copied from known-good analysis outputs.
+        # The exact values don't matter much for this test, since we're not checking positional
+        # accuracy here. They just need to be clearly different between the OT-2 and OT-3.
+        ("OT-2", "(196.38, 42.785, 44.04)"),
+        ("OT-3", "(227.88, 42.785, 44.04)"),
+    ],
+)
+def test_analysis_deck_definition(
+    robot_type: str,
+    expected_point: str,
+    tmp_path: Path,
+) -> None:
+    """Test that the analysis uses the appropriate deck definition for the protocol's robot type.
+
+    At the time of writing, the only official, public, documented way to observe the deck definition
+    that a protocol uses is for the protocol to load a labware and inspect the deck coordinates of
+    its wells.
+    """
+    protocol_source_file = Path(tmp_path) / "protocol.py"
+    protocol_source_file.write_text(
+        _get_deck_definition_test_source(
+            robot_type=robot_type,
+        )
+    )
+
+    exit_code, analysis_output_json = _get_analysis_result([protocol_source_file])
+
+    assert exit_code == 0
+
+    [load_labware_command, comment_command] = analysis_output_json["commands"]
+    _ = load_labware_command
+
+    # todo(mm, 2023-05-12): When protocols emit true Protocol Engine comment commands instead
+    # of legacy commands, "legacyCommandText" should change to "message".
+    assert comment_command["params"]["legacyCommandText"] == expected_point
