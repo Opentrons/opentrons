3708b525493a03821fa13a08af9b4dbbd1d569fe
Brian Cooper <brian@opentrons.com>
2023-05-15 18:59:28 -0400

    paginated runs query key fix

--- react-api-client/src/runs/useAllRunsQuery.ts
+++ react-api-client/src/runs/useAllRunsQuery.ts
@@ -17,9 +17,14 @@ export function useAllRunsQuery(
   hostOverride?: HostConfig | null
 ): UseQueryResult<Runs> {
   const contextHost = useHost()
-  const host = hostOverride ?? contextHost
+  const host =
+    hostOverride != null ? { ...contextHost, ...hostOverride } : contextHost
+  let queryKey = [host as HostConfig, 'runs', 'details']
+  if (params?.pageLength != null) {
+    queryKey = [...queryKey, String(params.pageLength)]
+  }
   const query = useQuery(
-    [host as HostConfig, 'runs', 'details'],
+    queryKey,
     () => getRuns(host as HostConfig, params).then(response => response.data),
     { enabled: host !== null, ...options }
   )
