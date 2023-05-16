19da185b79b2de06c225e374e49b3819c998f33f
koji <koji.kanao@opentrons.com>
2023-05-16 13:45:10 -0400

    refactor(app): update connect via usb screen
    update connect via usb screen component and test

--- app/src/pages/OnDeviceDisplay/__tests__/ConnectedViaUSB.test.tsx
+++ app/src/pages/OnDeviceDisplay/__tests__/ConnectedViaUSB.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { MemoryRouter } from 'react-router-dom'
-import { fireEvent } from '@testing-library/react'
 import { renderWithProviders } from '@opentrons/components'
 
 import { i18n } from '../../../i18n'
@@ -16,8 +15,6 @@ jest.mock('react-router-dom', () => {
   }
 })
 
-const PNG_FILE_NAME = 'usb@x2.png'
-
 const render = () => {
   return renderWithProviders(
     <MemoryRouter>
@@ -31,20 +28,19 @@ const render = () => {
 
 describe('ConnectViaUSB', () => {
   it('should render text, button, and image', () => {
-    const [{ getByText, getByRole }] = render()
-    getByText('Connect via USB')
-    getByRole('button', { name: 'Back' })
+    const [{ getByText, getByLabelText }] = render()
+    getByText('USB')
+    getByText('No connection found')
+    getByLabelText('Connect_via_usb_back_button')
     getByText(
-      "If you haven't already, download and launch the Opentrons App on your computer. Then connect to your Opentrons Flex with the provided USB cable."
+      'Connect the provided USB A-to-B cable to the robot’s USB-B port and an open port on your computer. (Use a USB B-to-C cable or a USB A-to-C adapter if your computer does not have a USB-A port.) To proceed with setup, the computer must have the Opentrons App installed and running.'
     )
-    const image = getByRole('img')
-    expect(image.getAttribute('src')).toEqual(PNG_FILE_NAME)
   })
 
   it('should call a mock function when tapping back button', () => {
     const [{ getByRole }] = render()
-    const button = getByRole('button', { name: 'Back' })
-    fireEvent.click(button)
+    const button = getByRole('button')
+    button.click()
     expect(mockPush).toHaveBeenCalledWith('/network-setup')
   })
 
