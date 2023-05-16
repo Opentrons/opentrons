6401aaac8e657113c05ec321bfb277395715bcac
David Gomez <david.gomez@opentrons.com>
2023-05-16 16:16:51 -0400

    added all test cases, corrected logic

--- hardware-testing/hardware_testing/scripts/rear_panel_test.py
+++ hardware-testing/hardware_testing/scripts/rear_panel_test.py
@@ -14,7 +14,8 @@ from opentrons_hardware.hardware_control.rear_panel_settings import (
     set_deck_light,
     get_deck_light_state,
     set_ui_color,
-    get_all_pin_state
+    get_all_pin_state,
+    set_sync_pin
 )
 
 from opentrons_hardware.drivers.binary_usb import BinaryMessenger
@@ -34,15 +35,6 @@ from opentrons_hardware.firmware_bindings.messages.binary_message_definitions im
 )
 
 ##START TEST PASSING CONDITIONS
-PRE_TEST_CONDITIONS_d = {"ESTOP_DETECT_1": 0,
-                   "ESTOP_DETECT_2": 0,
-                   "ESTOP": 1,
-                   "AUX_ID_1": 0,
-                   "AUX_ID_2": 1,
-                   "DOOR_SW": 1,
-                   "SYNC": 0,
-                   "AUX_PRES_1": 1,
-                   "AUX_PRES_2": 1}
 
 #needs a sync state
 PRE_TEST_CONDITIONS = RearPinState()
@@ -51,80 +43,64 @@ PRE_TEST_CONDITIONS.aux2_estop_det = False
 PRE_TEST_CONDITIONS.aux1_aux_det = False
 PRE_TEST_CONDITIONS.aux2_aux_det = False
 PRE_TEST_CONDITIONS.aux1_id_active = False
-PRE_TEST_CONDITIONS.aux2_id_active = True
+PRE_TEST_CONDITIONS.aux2_id_active = False
 PRE_TEST_CONDITIONS.etop_active = False
 PRE_TEST_CONDITIONS.door_open = True
 
+#Aux 1 only plugged in, sync active on tester
 AUX_1_CONDITIONS = RearPinState()
 AUX_1_CONDITIONS.aux1_estop_det = True
 AUX_1_CONDITIONS.aux2_estop_det = False
-AUX_1_CONDITIONS.aux1_aux_det = False
-AUX_1_CONDITIONS.aux2_aux_det = True
-AUX_1_CONDITIONS.aux1_id_active = False
+AUX_1_CONDITIONS.aux1_aux_det = True
+AUX_1_CONDITIONS.aux2_aux_det = False
+AUX_1_CONDITIONS.aux1_id_active = True
 AUX_1_CONDITIONS.aux2_id_active = False
 AUX_1_CONDITIONS.etop_active = False
-AUX_1_CONDITIONS.door_open = True
+AUX_1_CONDITIONS.door_open = False
 
-POST_PLUG_CONDITIONS_d = {"ESTOP_DETECT_1": 1,
-                   "ESTOP_DETECT_2": 1,
-                   "ESTOP": 1,
-                   "AUX_ID_1": 0,
-                   "AUX_ID_2": 1,
-                   "DOOR_SW": 1,
-                   "SYNC": 0,
-                   "AUX_PRES_1": 0,
-                   "AUX_PRES_2": 0}
+#Aux 2 only plugged in, sync active on tester
+AUX_2_CONDITIONS = RearPinState()
+AUX_2_CONDITIONS.aux1_estop_det = False
+AUX_2_CONDITIONS.aux2_estop_det = True
+AUX_2_CONDITIONS.aux1_aux_det = False
+AUX_2_CONDITIONS.aux2_aux_det = True
+AUX_2_CONDITIONS.aux1_id_active = False
+AUX_2_CONDITIONS.aux2_id_active = True
+AUX_2_CONDITIONS.etop_active = False
+AUX_2_CONDITIONS.door_open = False
 
+#Aux 1 and 2 plugged in, sync NOT active on tester
 POST_PLUG_CONDITIONS = RearPinState()
 POST_PLUG_CONDITIONS.aux1_estop_det = True
 POST_PLUG_CONDITIONS.aux2_estop_det = True
-POST_PLUG_CONDITIONS.aux1_aux_det = False
+POST_PLUG_CONDITIONS.aux1_aux_det = True
 POST_PLUG_CONDITIONS.aux2_aux_det = True
 POST_PLUG_CONDITIONS.aux1_id_active = False
 POST_PLUG_CONDITIONS.aux2_id_active = False
 POST_PLUG_CONDITIONS.etop_active = False
 POST_PLUG_CONDITIONS.door_open = True
 
-
-ESTOP_CONDITIONS_d = {"ESTOP_DETECT_1": 1,
-                   "ESTOP_DETECT_2": 1,
-                   "ESTOP": 0,
-                   "AUX_ID_1": 0,
-                   "AUX_ID_2": 1,
-                   "DOOR_SW": 1,
-                   "SYNC": 0,
-                   "AUX_PRES_1": 0,
-                   "AUX_PRES_2": 0}
-
+#Aux 1 and 2 plugged in, ESTOP pressed, sync NOT active on tester
 ESTOP_CONDITIONS = RearPinState()
 ESTOP_CONDITIONS.aux1_estop_det = True
 ESTOP_CONDITIONS.aux2_estop_det = True
-ESTOP_CONDITIONS.aux1_aux_det = False
+ESTOP_CONDITIONS.aux1_aux_det = True
 ESTOP_CONDITIONS.aux2_aux_det = True
 ESTOP_CONDITIONS.aux1_id_active = False
 ESTOP_CONDITIONS.aux2_id_active = False
 ESTOP_CONDITIONS.etop_active = True
 ESTOP_CONDITIONS.door_open = True
 
-SYNC_TEST_CONDITIONS_d = {"ESTOP_DETECT_1": 1,
-                   "ESTOP_DETECT_2": 1,
-                   "ESTOP": 0,
-                   "AUX_ID_1": 1,
-                   "AUX_ID_2": 0,
-                   "DOOR_SW": 0,
-                   "SYNC": 1,
-                   "AUX_PRES_1": 0,
-                   "AUX_PRES_2": 0}
-
-SYNC_CONDITIONS = RearPinState()
-SYNC_CONDITIONS.aux1_estop_det = True
-SYNC_CONDITIONS.aux2_estop_det = True
-SYNC_CONDITIONS.aux1_aux_det = False
-SYNC_CONDITIONS.aux2_aux_det = True
-SYNC_CONDITIONS.aux1_id_active = False
-SYNC_CONDITIONS.aux2_id_active = False
-SYNC_CONDITIONS.etop_active = False
-SYNC_CONDITIONS.door_open = True
+#Aux 1 and 2 not plugged in, door closed
+DOOR_CONDITIONS = RearPinState()
+DOOR_CONDITIONS.aux1_estop_det = False
+DOOR_CONDITIONS.aux2_estop_det = False
+DOOR_CONDITIONS.aux1_aux_det = False
+DOOR_CONDITIONS.aux2_aux_det = False
+DOOR_CONDITIONS.aux1_id_active = False
+DOOR_CONDITIONS.aux2_id_active = False
+DOOR_CONDITIONS.etop_active = False
+DOOR_CONDITIONS.door_open = False
 
 ##END TEST PASSING CONDITIONS
 
@@ -132,7 +108,7 @@ SYNC_CONDITIONS.door_open = True
 
 async def get_all_states(usb_messenger):
     state = await get_all_pin_state(usb_messenger)
-    print(str(state))
+    # print(str(state))
     return 1
 
 async def check_all_states(usb_messenger):
@@ -145,9 +121,10 @@ async def run(args: argparse.Namespace) -> None:
         usb_messenger.start()
 
 
+
         #request all states from rp
         #check states match PRE_TEST_CONDITIONS
-        Print("TEST - UNPLUGGED STATE")
+        print("TEST - UNPLUGGED STATE")
         result = await get_all_pin_state(usb_messenger)
         print(result)
         if (result == PRE_TEST_CONDITIONS):
@@ -155,19 +132,19 @@ async def run(args: argparse.Namespace) -> None:
         else:
             print("FAIL")
 
-        print()
 
         #instruct operator to plug in tester
         #start with aux port 1
-        Print("TEST - AUX 1")
-        input("PLUG IN AUX PORT 1 LEFT, PRESS ENTER WHEN COMPLETE")
+        print()
+        print("TEST - AUX 1")
+        input("PLUG IN AUX PORT 1 RIGHT, PRESS ENTER WHEN COMPLETE")
         #set the sync pin so the tester can toggle the DOOR_SW and aux1_id
         #this must be done with only the aux 1 port plugged since the door sw
         #is shared between aux1 and aux 2
         await set_sync_pin(1, usb_messenger)
         result = await get_all_pin_state(usb_messenger)
         print(result)
-        print("POST PLUG COMPARE: ")
+        print("AUX 1 SIGNALS: ")
         if (result == AUX_1_CONDITIONS):
             print("PASS")
         else:
@@ -175,48 +152,81 @@ async def run(args: argparse.Namespace) -> None:
 
         await set_sync_pin(0, usb_messenger)
 
-        Print("TEST - AUX 2")
-        input("PLUG IN AUX PORT 2 RIGHT, PRESS ENTER WHEN COMPLETE")
+        #test the default plugged state after plugging in both aux 1 and 2
+        print()
+        print("TEST - PLUGGED STATE")
+        input("PLUG IN AUX PORT 2 LEFT, PRESS ENTER WHEN COMPLETE")
+        result = await get_all_pin_state(usb_messenger)
+        print(result)
+        if (result == POST_PLUG_CONDITIONS):
+            print("PASS")
+        else:
+            print("FAIL")
 
-
-        response = await usb_messenger.send_and_receive(
-        message=EstopButtonPresentRequest(),
-        response_type=EstopButtonDetectionChange,
-        )
-        print(response)
-        # if response is not None:
-        #     current_state.aux1_estop_det = bool(
-        #         cast(EstopButtonDetectionChange, response).aux1_detected.value
-        #     )
-        #     current_state.aux2_estop_det = bool(
-        #         cast(EstopButtonDetectionChange, response).aux2_detected.value
-        #     )
 
         #test E-Stop
         #make robot move, instruct operator to e-stop
         #do both buttons
         #request RP e-stop state, check it active
         #ask operator if E-stop LED is lit
+        print()
+        print("TEST - ESTOP 1")
+        input("PRESS ESTOP 1")
+        result = await get_all_pin_state(usb_messenger)
+        print(result)
+        if (result == ESTOP_CONDITIONS):
+            print("PASS")
+        else:
+            print("FAIL")
+
+        input("RELEASE ESTOP 1")
+
+        print()
+        print("TEST - ESTOP 2")
+        input("PRESS ESTOP 2")
+        result = await get_all_pin_state(usb_messenger)
+        print(result)
+        if (result == ESTOP_CONDITIONS):
+            print("PASS")
+        else:
+            print("FAIL")
+
 
         #test CAN
 
-        #test AUX ID + Door SW
-        #request RP nSYNC line high
-        #check states against SYNC_TEST_CONDITIONS
-        #AUX_IDs, DOOR_SW, and SYNC should have state changed
-        await get_all_pin_state(usb_messenger)
+        print("TEST - AUX 2")
+        input("UNPLUG AUX PORT 1 RIGHT, PRESS ENTER WHEN COMPLETE")
+        #set the sync pin so the tester can toggle the DOOR_SW and aux1_id
+        #this must be done with only the aux 1 port plugged since the door sw
+        #is shared between aux1 and aux 2
+        await set_sync_pin(1, usb_messenger)
+        result = await get_all_pin_state(usb_messenger)
+        print(result)
+        print("AUX 2 SIGNALS: ")
+        if (result == AUX_2_CONDITIONS):
+            print("PASS")
+        else:
+            print("FAIL")
 
-        input("UNPLUG IN AUX PORT 1")
+        await set_sync_pin(0, usb_messenger)
+
+        input("UNPLUG AUX PORT 2 LEFT, PRESS ENTER WHEN COMPLETE")
 
 
-        input("door open")
+        print("TEST - DOOR")
+        input("CLOSE DOOR")
         door_test_result = await get_door_state(usb_messenger)
         print(door_test_result)
-        input("door closed")
-        door_test_result = await get_door_state(usb_messenger)
-        print(door_test_result)
+        result = await get_all_pin_state(usb_messenger)
+        print(result)
+        print("DOOR TEST: ")
+        if (result == DOOR_CONDITIONS):
+            print("PASS")
+        else:
+            print("FAIL")
 
         #LEDs
+        print("TEST - LEDS")
         input("turn deck light off")
         await set_deck_light(0, usb_messenger)
         input("turn deck light on")
@@ -229,9 +239,6 @@ async def run(args: argparse.Namespace) -> None:
         await set_ui_color(0,0,255,0, usb_messenger)
         input("set ui white")
         await set_ui_color(0,0,0,255, usb_messenger)
-        input("print state")
-        await get_all_states(usb_messenger)
-        input("done")
 
         #check eeprom
 
