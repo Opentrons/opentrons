04399300f2ed98a9b495a2028731b3cf743f4456
koji <koji.kanao@opentrons.com>
2023-05-16 19:39:07 -0400

    fix linting errors

--- app/src/organisms/OnDeviceDisplay/SetupNetwork/__tests__/ConnectingNetwork.test.tsx
+++ app/src/organisms/OnDeviceDisplay/SetupNetwork/__tests__/ConnectingNetwork.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { MemoryRouter } from 'react-router-dom'
 
-import { renderWithProviders, COLORS } from '@opentrons/components'
+import { renderWithProviders } from '@opentrons/components'
 
 import { i18n } from '../../../../i18n'
 import { ConnectingNetwork } from '../ConnectingNetwork'
