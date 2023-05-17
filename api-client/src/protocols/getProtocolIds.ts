35f30617a6a1023787cebea8e4ae996f423bbd7b
Shlok Amin <shlokamin@me.com>
2023-05-17 14:53:00 -0400

    format

--- api-client/src/protocols/getProtocolIds.ts
+++ api-client/src/protocols/getProtocolIds.ts
@@ -4,6 +4,8 @@ import type { ResponsePromise } from '../request'
 import type { HostConfig } from '../types'
 import type { ProtocolsIds } from './types'
 
-export function getProtocolIds(config: HostConfig): ResponsePromise<ProtocolsIds> {
+export function getProtocolIds(
+  config: HostConfig
+): ResponsePromise<ProtocolsIds> {
   return request<ProtocolsIds>(GET, `/protocols/ids`, null, config)
 }
