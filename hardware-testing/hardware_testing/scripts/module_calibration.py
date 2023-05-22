a58eb35add362d5b21af5b1d38c042fcbc5b81b9
Brayan Almonte <vegano1@users.noreply.github.com>
2023-05-22 14:04:32 -0400

    fix(api,hardware-testing): Fix wrong polarity in module offsets + refactor module calibration test script and add verification step. (#12733)
    * fix(api,hardware-testing): Fix the wrong polarity in module offsets + refactor module calibration test script and add verification step.
    
    - Fixed issue with module calibration offset getting generated with the wrong polarity
    - Fixed issue with module calibration where get_nominal_module_offset was applying the calibrated module offset
    - Added calibration_target arg and enums to find_calibration_structure_position method so we can reverse the calibration offset based on whether we are calibrating a deck object (module) or gantry instrument (pipette, gripper, etc)
    - Refactor the module calibration test script and added OpentronsHTTPAPI HTTP wrapper class to consolidate functionality
    - Added a verification step to the module calibration test script so we can confirm the center square with/without offsets.
    - Added urllib3, certifi, and charset-normalizer to Pipfile.lock for hardware_testing module

--- hardware-testing/hardware_testing/scripts/module_calibration.py
+++ hardware-testing/hardware_testing/scripts/module_calibration.py
@@ -1,11 +1,12 @@
-"""OT-3 Module Calibration Script."""
+"""OT-3 Module Calibration test script."""
 import argparse
-from time import sleep
-from typing import Optional, List
-import requests
 
+from hardware_testing.opentrons_api.types import Point
+from hardware_testing.opentrons_api.http_api import (
+    OpentronsHTTPAPI,
+)
 
-MOUNTS = ["left", "right", "extension"]
+PROBE_OFFSET = Point(x=0, y=0, z=44.5)
 
 
 MODELS = [
@@ -19,173 +20,94 @@ MODELS = [
 ]
 
 
-CALIBRATION_ADAPTER = {
-    "temperatureModuleV1": "opentrons_calibration_adapter_temperature_module",
-    "temperatureModuleV2": "opentrons_calibration_adapter_temperature_module",
-    "magneticModuleV1": "opentrons_calibration_adapter_magnetic_module",
-    "magneticModuleV2": "opentrons_calibration_adapter_magnetic_module",
-    "thermocyclerModuleV1": "opentrons_calibration_adapter_thermocycler_module",
-    "thermocyclerModuleV2": "opentrons_calibration_adapter_thermocycler_module",
-    "heaterShakerModuleV1": "opentrons_calibration_adapter_heatershaker_module",
-}
+def _handle_module_case(opentrons_api: OpentronsHTTPAPI, model: str) -> bool:
+    if "heaterShaker" in model:
+        # make sure the clamp is closed
+        opentrons_api.heater_shaker_latch_open(False)
+    elif "thermocycler" in model:
+        # make sure the lid is open
+        opentrons_api.thermocycler_lid_open(True)
+    return True
 
 
-HEADERS = {"opentrons-version": "4"}
-BASE_URL = "http://{}:31950"
-PARAMS = {"waitUntilComplete": "true"}
-
-
-def _home(ip_addr: str, axes: Optional[List[str]] = None) -> None:
-    """Home the z axis for the instrument."""
-    axes = axes or list()
-    print(f"Homing {axes} axes")
-    home = {"data": {"commandType": "home", "params": {"axes": axes}}}
-    url = f"{BASE_URL.format(ip_addr)}/commands"
-    requests.post(headers=HEADERS, url=url, json=home, params=PARAMS)
-
-
-def _get_current_run(ip_addr: str) -> Optional[str]:
-    url = f"{BASE_URL.format(ip_addr)}/runs"
-    res = requests.get(headers=HEADERS, url=url)
-    runs = res.json()["data"]
-    active_run = None
-    for run in runs:
-        if run["current"]:
-            active_run = run["id"]
-            break
-    return active_run
-
-
-def _create_run(ip_addr: str) -> Optional[str]:
-    """Create an empty run."""
-    url = f"{BASE_URL.format(ip_addr)}/runs"
-    res = requests.post(headers=HEADERS, url=url)
-    if res.status_code != 201:
-        return None
-    # get the run id
-    return res.json()["data"].get("id")
-
-
-def _cancel_run(ip_addr: str, run_id: Optional[str] = None) -> bool:
-    run_id = run_id or _get_current_run(ip_addr)
-    print(f"Finishing run {run_id}")
-    stop = {"data": {"actionType": "stop"}}
-    url = f"{BASE_URL.format(ip_addr)}/runs/{run_id}/actions"
-    requests.post(headers=HEADERS, params=PARAMS, url=url, json=stop)
-    sleep(1)  # give some time to cancel the run
-    # delete the run
-    url = f"{BASE_URL.format(ip_addr)}/runs/{run_id}"
-    requests.delete(headers=HEADERS, params=PARAMS, url=url)
-    return _get_current_run(ip_addr) is None
-
-
-def _main(args: argparse.Namespace) -> None:
-    base_url = f"{BASE_URL.format(args.host)}"
-    # check if there is an active run
-    run_id = _get_current_run(args.host)
-    if run_id:
-        choice = input(
-            f"{run_id} There is a run in progress, do you want to cancel it? y/n\n"
-        )
-        if choice.lower() in "no":
-            print("Not canceling existing run, exiting.\n")
-            exit(1)
-        # Cancel the run
-        if not _cancel_run(args.host, run_id):
-            raise RuntimeError("Could not cancel run.")
-
+def _main(args: argparse.Namespace, opentrons_api: OpentronsHTTPAPI) -> None:
     # Create a new run
-    run_id = _create_run(args.host)
+    run_id = opentrons_api.create_run()
     if not run_id:
         raise RuntimeError("Could not create run.")
 
-    url = f"{base_url}/runs/{run_id}/commands"
-
     # Home all axes so we are at a known state
-    _home(args.host)
+    opentrons_api.home()
 
-    # Make sure the module is attached
-    res = requests.get(headers=HEADERS, params=PARAMS, url=f"{base_url}/modules")
-    if res.status_code != 200:
-        raise RuntimeError("Error fetching modules attached.")
-    modules = res.json().get("data", [])
-    for module in modules:
-        if module.get("moduleModel") == args.model:
-            break
-    else:
-        raise RuntimeError(f"Module {args.model} was not detected.")
+    # Load in the instrument
+    pipette_id = opentrons_api.load_pipette(args.mount)
+    if not pipette_id:
+        raise RuntimeError(f"Could not load mount {args.mount}")
 
-    # load the module based on the model
-    print(f"Loading the module {args.model} at slot {args.slot}")
-    load_module = {
-        "data": {
-            "commandType": "loadModule",
-            "params": {"model": args.model, "location": {"slotName": args.slot}},
-        }
-    }
-    res = requests.post(headers=HEADERS, params=PARAMS, url=url, json=load_module)
-    data = res.json().get("data")
-    error = data.get("error")
-    if res.status_code != 201 or error:
-        error_type = error.get("errorType")
-        error_details = error.get("detail")
-        raise RuntimeError(f"Error loading module: {error_type} - {error_details}")
-    module_id = data["result"]["moduleId"]
+    # Load the module
+    module_id = opentrons_api.load_module(args.model, args.slot)
+    if not module_id:
+        raise RuntimeError(f"Could not load module {args.model}")
 
-    # load the calibration labware for the specific module
-    print(f"Loading the calibration adapter at slot {args.slot}")
-    load_labware = {
-        "data": {
-            "commandType": "loadLabware",
-            "params": {
-                "location": {"moduleId": module_id},
-                "loadName": CALIBRATION_ADAPTER[args.model],
-                "namespace": "opentrons",
-                "version": 1,
-            },
-        }
-    }
-    res = requests.post(headers=HEADERS, params=PARAMS, url=url, json=load_labware)
-    data = res.json().get("data")
-    error = data.get("error")
-    if res.status_code != 201 or error:
-        error_type = error.get("errorType")
-        error_details = error.get("detail")
-        msg = f"Error loading labware: {error_type} - {error_details}"
-        raise RuntimeError(msg)
-    labware_id = data["result"]["labwareId"]
+    # Load the calibration labware for the specific module
+    labware_id = opentrons_api.load_labware(module_id, args.model)
+    if not labware_id:
+        raise RuntimeError(f"Could not load labware for {args.model}")
 
-    # calibrate the module
-    print(f"Calibrating {args.model} at slot {args.slot} with mount {args.mount}")
-    calibrate_module = {
-        "data": {
-            "commandType": "calibration/calibrateModule",
-            "params": {
-                "moduleId": module_id,
-                "labwareId": labware_id,
-                "mount": args.mount,
-            },
-        }
-    }
+    # Calibrate the module
+    print(
+        f"Calibrating module {args.model} at slot {args.slot} with mount {args.mount}"
+    )
+    offset = opentrons_api.calibrate_module(args.mount, module_id, labware_id)
+    if not offset:
+        raise RuntimeError(f"Could not calibrate {args.model}")
 
-    res = requests.post(headers=HEADERS, params=PARAMS, url=url, json=calibrate_module)
-    data = res.json().get("data")
-    error = data.get("error")
-    if res.status_code != 201 or error:
-        error_type = error.get("errorType")
-        error_details = error.get("detail")
-        msg = f"Failed to calibrate module {args.model}: {error_type} - {error_details}"
-        raise RuntimeError(msg)
+    # set module pre-conditions (closed lid/latch)
+    _handle_module_case(opentrons_api, args.model)
+    print("Moving to calibrated well center")
+    calibrated_position = opentrons_api.move_to_well(
+        args.mount, "A1", offset=PROBE_OFFSET
+    )
 
     # Finish and print result
-    _cancel_run(args.host, run_id)
-    calibration_offset = res.json()["data"]["result"]["moduleOffset"]
-    print(f"Calibration result {calibration_offset}")
+    print(f"Calibration offset {offset}")
+    print(f"Calibrated center {calibrated_position}")
+
+    # Verify the offset
+    confirm = input("Verify the calibrated offset? y/n\n")
+    if "n" in confirm:
+        return
+
+    # Setup the run
+    opentrons_api.cancel_run(run_id)
+    opentrons_api.create_run()
+    module_id = opentrons_api.load_module(args.model, args.slot)
+    opentrons_api.load_labware(module_id, args.model)
+    opentrons_api.load_pipette(args.mount)
+
+    # set module pre-conditions (closed lid/latch)
+    _handle_module_case(opentrons_api, args.model)
+
+    print("Moving to the uncalibrated well position")
+    # we need to add the probe z offset so we dont crash into the deck
+    inverse_offset = (offset * -1) + PROBE_OFFSET
+    uncalibrated_position = opentrons_api.move_to_well(
+        args.mount, "A1", offset=inverse_offset
+    )
+    print(f"Uncalibrated center position: {uncalibrated_position}")
+
+    input("Press Enter to to move to calibrated position.")
+    calibrated_position = opentrons_api.move_to_well(
+        args.mount, "A1", offset=PROBE_OFFSET
+    )
+    print(f"Calibrated center position: {calibrated_position}")
+
+    input("Press Enter to continue...")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Script to test module calibration over HTTP"
+        description="Script to test and verify module calibration over HTTP"
     )
     parser.add_argument(
         "--host", help="The ip address of the robot", default="localhost"
@@ -205,15 +127,17 @@ if __name__ == "__main__":
     parser.add_argument(
         "--mount",
         help="The mount to use for the calibration",
-        choices=MOUNTS,
+        choices=["left", "right"],
         required=True,
     )
     args = parser.parse_args()
+
     try:
-        _main(args)
+        opentrons_api = OpentronsHTTPAPI(args.host)
+        _main(args, opentrons_api)
     except Exception as e:
         print(e)
-        _cancel_run(args.host)
     finally:
-        # home the gantry
-        _home(args.host, ["leftZ", "rightZ"])
+        # home the gantry and cancel run
+        opentrons_api.home(["leftZ", "rightZ"])
+        opentrons_api.cancel_run()
