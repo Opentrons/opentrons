881b7d4827c8aae5b56f1859d4a5233e987c55fe
Max Marrone <max@opentrons.com>
2023-05-26 14:49:26 -0400

    Generally deduplicate and tidy up integration/conftest.py.

--- robot-server/tests/integration/conftest.py
+++ robot-server/tests/integration/conftest.py
@@ -1,8 +1,9 @@
 import asyncio
+import contextlib
 import json
 import time
 from pathlib import Path
-from typing import Any, Dict, Generator, Iterator
+from typing import Any, Dict, Generator
 
 import pytest
 import requests
@@ -13,9 +14,9 @@ from .dev_server import DevServer
 from .robot_client import RobotClient
 
 
-# Must match our Tavern config in common.yaml.
-_SESSION_SERVER_HOST = "http://localhost"
-_SESSION_SERVER_PORT = "31950"
+_SESSION_SERVER_SCHEME = "http://"
+_SESSION_SERVER_HOST = "localhost"
+_OT2_SESSION_SERVER_PORT = "31950"
 _OT3_SESSION_SERVER_PORT = "31960"
 
 
@@ -37,71 +38,82 @@ def pytest_tavern_beta_after_every_response(
     print(json.dumps(response.json(), indent=4))
 
 
-@pytest.fixture(scope="session")
-def request_session() -> requests.Session:
-    session = requests.Session()
-    session.headers.update({API_VERSION_HEADER: LATEST_API_VERSION_HEADER_VALUE})
-    return session
+@pytest.fixture
+def ot2_server_set_disable_fast_analysis(
+    ot2_server_base_url: str,
+) -> Generator[None, None, None]:
+    """For integration tests that need to set then clear the
+    disableFastProtocolUpload feature flag"""
+    url = f"{ot2_server_base_url}/settings"
+    data = {"id": "disableFastProtocolUpload", "value": True}
+    with _requests_session() as requests_session:
+        requests_session.post(url, json=data)
+        yield None
+        data["value"] = None
+        requests.post(url, json=data)
+
+
+@pytest.fixture
+def ot2_server_base_url(_ot2_session_server: str) -> Generator[str, None, None]:
+    """Return the URL for a running dev server.
+
+    Because it can take several seconds to start up and shut down, one server is shared across all
+    tests in the test session. This fixture softly "resets" it after each test by deleting all
+    runs and protocols, which provides good enough isolation in practice.
+    """
+    yield _ot2_session_server
+    _clean_server_state(_ot2_session_server)
+
+
+@pytest.fixture
+def ot3_server_base_url(_ot3_session_server: str) -> Generator[str, None, None]:
+    """Like `ot2_server_base_url()`, but the server is configured as an OT-3 instead of an OT-2."""
+    yield _ot3_session_server
+    _clean_server_state(_ot3_session_server)
 
 
 @pytest.fixture(scope="session")
-def _run_server(
-    request_session: requests.Session,
-    server_temp_directory: str,
-) -> Iterator[None]:
-    """Run the robot server in a background process."""
+def _ot2_session_server(server_temp_directory: str) -> Generator[str, None, None]:
+    base_url = (
+        f"{_SESSION_SERVER_SCHEME}{_SESSION_SERVER_HOST}:{_OT2_SESSION_SERVER_PORT}"
+    )
     with DevServer(
-        port=_SESSION_SERVER_PORT,
+        port=_OT2_SESSION_SERVER_PORT,
         ot_api_config_dir=Path(server_temp_directory),
     ) as dev_server:
         dev_server.start()
-
-        # Wait for a bit to get started by polling /health
-        from requests.exceptions import ConnectionError
-
-        while True:
-            try:
-                health_response = request_session.get(
-                    f"{_SESSION_SERVER_HOST}:{_SESSION_SERVER_PORT}/health"
-                )
-            except ConnectionError:
-                # The server isn't up yet to accept requests. Keep polling.
-                pass
-            else:
-                if health_response.status_code == 503:
-                    # The server is accepting requests but reporting not ready. Keep polling.
-                    pass
-                else:
-                    # The server's replied with something other than a busy indicator. Stop polling.
-                    break
-
-            time.sleep(0.1)
-
-        yield
+        _wait_until_ready(base_url)
+        yield base_url
 
 
 @pytest.fixture(scope="session")
-def _ot3_run_server(
-    request_session: requests.Session,
-    server_temp_directory: str,
-) -> Iterator[None]:
-    """Run the robot server in a background process."""
+def _ot3_session_server(server_temp_directory: str) -> Generator[str, None, None]:
+    base_url = (
+        f"{_SESSION_SERVER_SCHEME}{_SESSION_SERVER_HOST}:{_OT3_SESSION_SERVER_PORT}"
+    )
     with DevServer(
         port=_OT3_SESSION_SERVER_PORT,
         is_ot3=True,
         ot_api_config_dir=Path(server_temp_directory),
     ) as dev_server:
         dev_server.start()
+        _wait_until_ready(base_url)
+        yield base_url
 
-        # Wait for a bit to get started by polling /health
-        from requests.exceptions import ConnectionError
 
+@contextlib.contextmanager
+def _requests_session() -> Generator[requests.Session, None, None]:
+    with requests.Session() as session:
+        session.headers.update({API_VERSION_HEADER: LATEST_API_VERSION_HEADER_VALUE})
+        yield session
+
+
+def _wait_until_ready(base_url: str) -> None:
+    with _requests_session() as requests_session:
         while True:
             try:
-                health_response = request_session.get(
-                    f"{_SESSION_SERVER_HOST}:{_OT3_SESSION_SERVER_PORT}/health"
-                )
-            except ConnectionError:
+                health_response = requests_session.get(f"{base_url}/health")
+            except requests.ConnectionError:
                 # The server isn't up yet to accept requests. Keep polling.
                 pass
             else:
@@ -110,49 +122,15 @@ def _ot3_run_server(
                     pass
                 else:
                     # The server's replied with something other than a busy indicator. Stop polling.
-                    break
+                    return
 
             time.sleep(0.1)
 
-        yield
 
-
-@pytest.fixture
-def set_disable_fast_analysis(
-    request_session: requests.Session,
-) -> Iterator[None]:
-    """For integration tests that need to set then clear the
-    disableFastProtocolUpload feature flag"""
-    url = "http://localhost:31950/settings"
-    data = {"id": "disableFastProtocolUpload", "value": True}
-    request_session.post(url, json=data)
-    yield None
-    data["value"] = None
-    request_session.post(url, json=data)
-
-
-def _clean_server_state() -> None:
+def _clean_server_state(base_url: str) -> None:
     async def _clean_server_state_async() -> None:
-        port = "31950"
-        async with RobotClient.make(
-            base_url=f"http://localhost:{port}", version="*"
-        ) as robot_client:
-            await _delete_all_runs(robot_client)
-            await _delete_all_protocols(robot_client)
-
-    asyncio.run(_clean_server_state_async())
-
-
-# TODO(jbl 2023-05-01) merge this with ot3_run_server, along with clean_server_state and run_server
-def _ot3_clean_server_state() -> None:
-    # async fn that does the things below
-    # make a robot client
-    # delete protocols
-    async def _clean_server_state_async() -> None:
-        port = "31960"
-        async with RobotClient.make(
-            base_url=f"http://localhost:{port}", version="*"
-        ) as robot_client:
+        async with RobotClient.make(base_url=base_url, version="*") as robot_client:
+            # Delete runs first because protocols can't be deleted if a run refers to them.
             await _delete_all_runs(robot_client)
             await _delete_all_protocols(robot_client)
 
@@ -173,15 +151,3 @@ async def _delete_all_protocols(robot_client: RobotClient) -> None:
     protocol_ids = [p["id"] for p in response.json()["data"]]
     for protocol_id in protocol_ids:
         await robot_client.delete_protocol(protocol_id)
-
-
-@pytest.fixture
-def ot2_server_base_url(_run_server: None) -> Generator[str, None, None]:
-    yield f"{_SESSION_SERVER_HOST}:{_SESSION_SERVER_PORT}"
-    _clean_server_state()
-
-
-@pytest.fixture
-def ot3_server_base_url(_ot3_run_server: None) -> Generator[str, None, None]:
-    yield f"{_SESSION_SERVER_HOST}:{_OT3_SESSION_SERVER_PORT}"
-    _ot3_clean_server_state()
