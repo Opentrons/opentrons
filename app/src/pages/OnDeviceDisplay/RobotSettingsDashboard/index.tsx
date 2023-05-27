056c3cc6b851738fb789fa944a43fd851f073769
koji <koji.kanao@opentrons.com>
2023-05-26 21:37:34 -0400

    fix check-js errors

--- app/src/pages/OnDeviceDisplay/RobotSettingsDashboard/index.tsx
+++ app/src/pages/OnDeviceDisplay/RobotSettingsDashboard/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
 
 import { Flex, DIRECTION_COLUMN, SPACING } from '@opentrons/components'
 
