289c79161377b506dbc5248d89e37b3fc13b6a83
Brent Hagen <brent.hagen@opentrons.com>
2023-05-31 17:31:02 -0400

    monitor ODD usb connection by authorization token in setup flow

--- app/src/pages/OnDeviceDisplay/ConnectViaUSB.tsx
+++ app/src/pages/OnDeviceDisplay/ConnectViaUSB.tsx
@@ -16,6 +16,7 @@ import {
   POSITION_ABSOLUTE,
   BORDERS,
 } from '@opentrons/components'
+import { useConnectionsQuery } from '@opentrons/react-api-client'
 import { StyledText } from '../../atoms/text'
 import { StepMeter } from '../../atoms/StepMeter'
 import { MediumButton } from '../../atoms/buttons'
@@ -23,8 +24,13 @@ import { MediumButton } from '../../atoms/buttons'
 export function ConnectViaUSB(): JSX.Element {
   const { i18n, t } = useTranslation(['device_settings', 'shared'])
   const history = useHistory()
-  // ToDo (kj:05/16/2023) isConnected part will be implemented later
-  const isConnected = false
+  // TODO(bh, 2023-5-31): active connections from /system/connected isn't exactly the right way to monitor for a usb connection -
+  // the system-server tracks active connections by authorization token, which is valid for 2 hours
+  // another option is to report an active usb connection by monitoring usb port traffic (discovery-client polls health from the desktop app)
+  const activeConnections = useConnectionsQuery().data?.connections ?? []
+  const isConnected = activeConnections.some(
+    connection => connection.agent === 'com.opentrons.app.usb'
+  )
 
   return (
     <>
@@ -90,7 +96,7 @@ export function ConnectViaUSB(): JSX.Element {
             </Flex>
             <MediumButton
               buttonText={i18n.format(t('shared:continue'), 'capitalize')}
-              onClick={() => history.push('/robot-settings/update-robot')}
+              onClick={() => history.push('/robot-settings/rename-robot')}
             />
           </Flex>
         ) : (
