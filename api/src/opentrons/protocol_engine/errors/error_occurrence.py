fb2800fe308750acb6ed3923f524559fade0d14f
Frank Sinapi <frank.sinapi@opentrons.com>
2023-06-05 14:05:44 -0400

    Formatting...

--- api/src/opentrons/protocol_engine/errors/error_occurrence.py
+++ api/src/opentrons/protocol_engine/errors/error_occurrence.py
@@ -12,4 +12,6 @@ class ErrorOccurrence(BaseModel):
     errorType: str = Field(..., description="Specific error type that occurred.")
     createdAt: datetime = Field(..., description="When the error occurred.")
     detail: str = Field(..., description="A human-readable message about the error.")
-    errorCode: int = Field(..., description="An enumerated error code for the error type.")
+    errorCode: int = Field(
+        ..., description="An enumerated error code for the error type."
+    )
