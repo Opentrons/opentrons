57db4de272f272b5f3598d977885924b21b90881
Frank Sinapi <frank.sinapi@opentrons.com>
2023-06-05 14:25:28 -0400

    Added a test that the command store will grab the correct error code

--- api/tests/opentrons/protocol_engine/state/test_command_store.py
+++ api/tests/opentrons/protocol_engine/state/test_command_store.py
@@ -830,6 +830,48 @@ def test_command_store_saves_unknown_finish_error() -> None:
     )
 
 
+def test_command_store_saves_correct_error_code() -> None:
+    """If an error is derived from ProtocolEngineError, its ErrorCode should be used."""
+
+    TEST_CODE = 1234
+
+    class MyCustomError(errors.ProtocolEngineError):
+        ERROR_CODE = TEST_CODE
+
+    subject = CommandStore(is_door_open=False, config=_make_config())
+
+    error_details = FinishErrorDetails(
+        error=MyCustomError("oh no"),
+        error_id="error-id",
+        created_at=datetime(year=2021, month=1, day=1),
+    )
+    subject.handle_action(FinishAction(error_details=error_details))
+
+    assert subject.state == CommandState(
+        queue_status=QueueStatus.PAUSED,
+        run_result=RunResult.FAILED,
+        run_completed_at=None,
+        is_door_blocking=False,
+        running_command_id=None,
+        all_command_ids=[],
+        queued_command_ids=OrderedSet(),
+        queued_setup_command_ids=OrderedSet(),
+        commands_by_id=OrderedDict(),
+        errors_by_id={
+            "error-id": errors.ErrorOccurrence(
+                id="error-id",
+                createdAt=datetime(year=2021, month=1, day=1),
+                errorType="MyCustomError",
+                detail="oh no",
+                # Unknown errors use the default error code
+                errorCode=TEST_CODE,
+            )
+        },
+        run_started_at=None,
+        latest_command_hash=None,
+    )
+
+
 def test_command_store_ignores_stop_after_graceful_finish() -> None:
     """It should no-op on stop if already gracefully finished."""
     subject = CommandStore(is_door_open=False, config=_make_config())
