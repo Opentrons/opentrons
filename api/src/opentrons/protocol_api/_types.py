e2796f1b02e9601b06d8732d33c342f53810b974
Max Marrone <max@opentrons.com>
2023-06-06 12:19:49 -0400

    OFF_DECK tangent.

--- api/src/opentrons/protocol_api/_types.py
+++ api/src/opentrons/protocol_api/_types.py
@@ -8,3 +8,11 @@ class OffDeckType(enum.Enum):
 
 
 OFF_DECK: Final = OffDeckType.OFF_DECK
+
+# Set __doc__ manually as a workaround. When this docstring is written the normal way, right after
+# the constant definition, Sphinx has trouble picking it up.
+OFF_DECK.__doc__ = """\
+A special placeholder value for a labware's location, indicating that it's not currently on the robot's deck.
+
+See :py:obj:`ProtocolContext.move_labware()`.
+"""
