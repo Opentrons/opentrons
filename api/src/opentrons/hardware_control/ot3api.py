4445d808e5401c9283c8af780bb7e1ddee65a162
TamarZanzouri <zanzouritamar@gmail.com>
2023-06-06 16:38:35 -0400

    feature(api, hardware-controller): allow hardware to move both mounts at the same time (#12844)

--- api/src/opentrons/hardware_control/ot3api.py
+++ api/src/opentrons/hardware_control/ot3api.py
@@ -18,6 +18,7 @@ from typing import (
     Any,
     TypeVar,
     Tuple,
+    Mapping,
 )
 from opentrons.hardware_control.modules.module_calibration import (
     ModuleCalibrationOffset,
@@ -96,7 +97,7 @@ from .types import (
     UpdateStatus,
     StatusBarState,
 )
-from .errors import MustHomeError, GripperNotAttachedError
+from .errors import MustHomeError, GripperNotAttachedError, AxisNotPresentError
 from . import modules
 from .ot3_calibration import OT3Transforms, OT3RobotCalibrationProvider
 
@@ -152,7 +153,7 @@ class OT3API(
     # of methods that are present in the protocol will call the (empty,
     # do-nothing) methods in the protocol. This will happily make all the
     # tests fail.
-    HardwareControlInterface[OT3Transforms],
+    HardwareControlInterface[OT3Transforms, OT3Axis],
 ):
     """This API is the primary interface to the hardware controller.
 
@@ -978,6 +979,69 @@ class OT3API(
             max_speeds=checked_max,
             expect_stalls=_expect_stalls,
         )
+
+    async def move_axes(  # noqa: C901
+        self,
+        position: Mapping[OT3Axis, float],
+        speed: Optional[float] = None,
+        max_speeds: Optional[Dict[OT3Axis, float]] = None,
+    ) -> None:
+        """Moves the effectors of the specified axis to the specified position.
+        The effector of the x,y axis is the center of the carriage.
+        The effector of the pipette mount axis are the mount critical points but only in z.
+        """
+        if not self._current_position:
+            await self.refresh_positions()
+
+        for axis in position.keys():
+            if not self._backend.axis_is_present(axis):
+                raise AxisNotPresentError(f"{axis} is not present")
+
+        if not self._backend.check_encoder_status(list(position.keys())):
+            await self.home()
+
+        valid_motor = self._current_position and self._backend.check_motor_status(
+            list(position.keys())
+        )
+        if not valid_motor:
+            raise MustHomeError("Current position is invalid; please home motors.")
+
+        absolute_positions: "OrderedDict[OT3Axis, float]" = OrderedDict()
+        current_position = self._current_position
+        if OT3Axis.X in position:
+            absolute_positions[OT3Axis.X] = position[OT3Axis.X]
+        else:
+            absolute_positions[OT3Axis.X] = current_position[OT3Axis.X]
+        if OT3Axis.Y in position:
+            absolute_positions[OT3Axis.Y] = position[OT3Axis.Y]
+        else:
+            absolute_positions[OT3Axis.Y] = current_position[OT3Axis.Y]
+
+        have_z = False
+        for axis in [OT3Axis.Z_L, OT3Axis.Z_R, OT3Axis.Z_G]:
+            if axis in position:
+                have_z = True
+                if OT3Axis.Z_L:
+                    carriage_effectors_offset = (
+                        self._robot_calibration.left_mount_offset
+                    )
+                elif OT3Axis.Z_R:
+                    carriage_effectors_offset = (
+                        self._robot_calibration.right_mount_offset
+                    )
+                else:
+                    carriage_effectors_offset = (
+                        self._robot_calibration.gripper_mount_offset
+                    )
+                absolute_positions[axis] = position[axis] - carriage_effectors_offset.z
+
+        if not have_z:
+            absolute_positions[OT3Axis.Z_L] = current_position[OT3Axis.Z_L]
+        for axis, position_value in position.items():
+            if axis not in absolute_positions:
+                absolute_positions[axis] = position_value
+
+        await self._move(target_position=absolute_positions, speed=speed)
 
     async def move_rel(
         self,
