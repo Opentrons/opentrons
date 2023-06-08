8b71abc0f49601caee79d8dc8e4ba53d40d398ca
carlos-fernandez <fernandez.carlos40@gmail.com>
2023-06-08 12:14:45 -0400

    update pick up speed

--- hardware-testing/hardware_testing/opentrons_api/helpers_ot3.py
+++ hardware-testing/hardware_testing/opentrons_api/helpers_ot3.py
@@ -394,15 +394,6 @@ async def update_pick_up_current(
     config_model.current = current
     pipette.pick_up_configurations = config_model
 
-async def update_96_pick_up_current(
-    api: OT3API, mount: OT3Mount, current: float = 0.125
-) -> None:
-    """Update pick-up-tip current."""
-    pipette = _get_pipette_from_mount(api, mount)
-    config_model = pipette.pick_up_configurations
-    config_model.pick_up_motor_actions = current
-    pipette.pick_up_configurations = config_model
-
 async def update_drop_tip_current(
     api: OT3API, mount: OT3Mount, current: float = 1.0
 ) -> None:
@@ -422,6 +413,14 @@ async def update_pick_up_distance(
     config_model.distance = distance
     pipette.pick_up_configurations = config_model
 
+async def update_pick_up_speed(
+    api: OT3API, mount: OT3Mount, speed: float = 5.0
+) -> None:
+    """Update pick-up-tip current."""
+    pipette = _get_pipette_from_mount(api, mount)
+    config_model = pipette.pick_up_configurations
+    config_model.speed = speed
+    pipette.pick_up_configurations = config_model
 
 async def move_plunger_absolute_ot3(
     api: OT3API,
