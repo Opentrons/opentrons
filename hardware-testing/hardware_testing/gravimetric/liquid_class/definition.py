af699be6469799b05426bb2e90c68d64085ae14f
andySigler <andrewsigler1@gmail.com>
2023-06-08 09:45:28 -0400

    updates liquid-class definition to include accelerations

--- hardware-testing/hardware_testing/gravimetric/liquid_class/definition.py
+++ hardware-testing/hardware_testing/gravimetric/liquid_class/definition.py
@@ -6,33 +6,26 @@ from dataclasses import dataclass
 class LiquidSettings:
     """Liquid Settings for both aspirate and dispense."""
 
+    submerge_depth: float  # millimeters below meniscus
+    acceleration: float  # ul/sec^2
     flow_rate: float  # ul/sec
     delay: float  # seconds
-    submerge: float  # millimeters below meniscus
-    retract: float  # millimeters above meniscus
-
-
-@dataclass
-class AirGapSettings:
-    """Air Gap Settings."""
-
-    leading_air_gap: float  # microliters
-    trailing_air_gap: float  # microliters
+    retract_acceleration: float  # mm/sec/sec
+    retract_height: float  # millimeters above meniscus
 
 
 @dataclass
 class AspirateSettings(LiquidSettings):
     """Aspirate Settings."""
 
-    air_gap: AirGapSettings
+    trailing_air_gap: float  # microliters
 
 
 @dataclass
 class DispenseSettings(LiquidSettings):
     """Dispense Settings."""
 
-    acceleration: float  # ul/sec^2 FIXME: get working within protocol
-    deceleration: float  # ul/sec^2 FIXME: get working within protocol
+    leading_air_gap: float  # microliters
 
 
 @dataclass
@@ -53,27 +46,21 @@ def interpolate(
 
     return LiquidClassSettings(
         aspirate=AspirateSettings(
+            submerge_depth=_interp(a.aspirate.submerge_depth, b.aspirate.submerge_depth),
+            acceleration=_interp(a.aspirate.acceleration, b.aspirate.acceleration),
             flow_rate=_interp(a.aspirate.flow_rate, b.aspirate.flow_rate),
             delay=_interp(a.aspirate.delay, b.aspirate.delay),
-            submerge=_interp(a.aspirate.submerge, b.aspirate.submerge),
-            retract=_interp(a.aspirate.retract, b.aspirate.retract),
-            air_gap=AirGapSettings(
-                leading_air_gap=_interp(
-                    a.aspirate.air_gap.leading_air_gap,
-                    b.aspirate.air_gap.leading_air_gap,
-                ),
-                trailing_air_gap=_interp(
-                    a.aspirate.air_gap.trailing_air_gap,
-                    b.aspirate.air_gap.trailing_air_gap,
-                ),
-            ),
+            retract_acceleration=_interp(a.aspirate.retract_acceleration, b.aspirate.retract_acceleration),
+            retract_height=_interp(a.aspirate.retract_height, b.aspirate.retract_height),
+            trailing_air_gap=_interp(a.aspirate.trailing_air_gap, b.aspirate.trailing_air_gap),
         ),
         dispense=DispenseSettings(
+            submerge_depth=_interp(a.dispense.submerge_depth, b.dispense.submerge_depth),
+            acceleration=_interp(a.dispense.acceleration, b.dispense.acceleration),
             flow_rate=_interp(a.dispense.flow_rate, b.dispense.flow_rate),
             delay=_interp(a.dispense.delay, b.dispense.delay),
-            submerge=_interp(a.dispense.submerge, b.dispense.submerge),
-            retract=_interp(a.dispense.retract, b.dispense.retract),
-            acceleration=_interp(a.dispense.acceleration, b.dispense.acceleration),
-            deceleration=_interp(a.dispense.deceleration, b.dispense.deceleration),
+            retract_acceleration=_interp(a.dispense.retract_acceleration, b.dispense.retract_acceleration),
+            retract_height=_interp(a.dispense.retract_height, b.dispense.retract_height),
+            leading_air_gap=_interp(a.dispense.leading_air_gap, b.dispense.leading_air_gap),
         ),
     )
