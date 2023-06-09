596aff9b3380bc6b6d2371bb4e949d886adbe6e8
jbleon95 <jeremybenleon@gmail.com>
2023-06-09 12:57:34 -0400

    tests for error checking and labware on labware

--- api/tests/opentrons/protocol_engine/commands/test_load_adapter.py
+++ api/tests/opentrons/protocol_engine/commands/test_load_adapter.py
@@ -6,6 +6,7 @@ from decoy import Decoy
 
 from opentrons.types import DeckSlotName
 from opentrons.protocols.models import LabwareDefinition
+from opentrons.protocol_engine.errors import LabwareDefinitionIsNotAdapterError
 from opentrons.protocol_engine.types import DeckSlotLocation
 from opentrons.protocol_engine.execution import LoadedLabwareData, EquipmentHandler
 from opentrons.protocol_engine.resources import labware_validation
@@ -71,3 +72,45 @@ async def test_load_adapter_implementation(
         definition=labware_definition,
         offsetId="adapter-offset-id",
     )
+
+
+async def test_load_adapter_raises_not_adapter(
+    decoy: Decoy,
+    equipment: EquipmentHandler,
+) -> None:
+    """A LoadAdapter command should raise if the definition is not validated as an adapter."""
+    subject = LoadAdapterImplementation(equipment=equipment)
+
+    data = LoadAdapterParams(
+        location=DeckSlotLocation(slotName=DeckSlotName.SLOT_3),
+        loadName="some-load-name",
+        namespace="opentrons-test",
+        version=1,
+        displayName="My custom display name",
+        adapterId="abc-123",
+    )
+
+    labware_definition = LabwareDefinition.construct()  # type: ignore[call-arg]
+
+    decoy.when(
+        await equipment.load_labware(
+            location=DeckSlotLocation(slotName=DeckSlotName.SLOT_3),
+            load_name="some-load-name",
+            namespace="opentrons-test",
+            version=1,
+            labware_id="abc-123",
+        )
+    ).then_return(
+        LoadedLabwareData(
+            labware_id="adapter-id",
+            definition=labware_definition,
+            offsetId="adapter-offset-id",
+        )
+    )
+
+    decoy.when(
+        labware_validation.validate_definition_is_adapter(labware_definition)
+    ).then_return(False)
+
+    with pytest.raises(LabwareDefinitionIsNotAdapterError):
+        await subject.execute(data)
--- api/tests/opentrons/protocol_engine/commands/test_load_labware.py
+++ api/tests/opentrons/protocol_engine/commands/test_load_labware.py
@@ -6,7 +6,15 @@ from decoy import Decoy
 
 from opentrons.types import DeckSlotName
 from opentrons.protocols.models import LabwareDefinition
-from opentrons.protocol_engine.types import DeckSlotLocation
+from opentrons.protocol_engine.errors import (
+    LabwareDefinitionIsNotLabwareError,
+    LabwareCannotBeStackedError,
+)
+from opentrons.protocol_engine.types import (
+    DeckSlotLocation,
+    OnLabwareLocation,
+    LoadedLabware,
+)
 from opentrons.protocol_engine.execution import LoadedLabwareData, EquipmentHandler
 from opentrons.protocol_engine.resources import labware_validation
 from opentrons.protocol_engine.state import StateView
@@ -71,3 +79,151 @@ async def test_load_labware_implementation(
         definition=well_plate_def,
         offsetId="labware-offset-id",
     )
+
+
+async def test_load_labware_raises_not_labware(
+    decoy: Decoy,
+    well_plate_def: LabwareDefinition,
+    equipment: EquipmentHandler,
+    state_view: StateView,
+) -> None:
+    """A LoadLabware command should raise if the definition is not validated as a labware."""
+    subject = LoadLabwareImplementation(equipment=equipment, state_view=state_view)
+
+    data = LoadLabwareParams(
+        location=DeckSlotLocation(slotName=DeckSlotName.SLOT_3),
+        loadName="some-load-name",
+        namespace="opentrons-test",
+        version=1,
+        displayName="My custom display name",
+    )
+
+    decoy.when(
+        await equipment.load_labware(
+            location=DeckSlotLocation(slotName=DeckSlotName.SLOT_3),
+            load_name="some-load-name",
+            namespace="opentrons-test",
+            version=1,
+            labware_id=None,
+        )
+    ).then_return(
+        LoadedLabwareData(
+            labware_id="labware-id",
+            definition=well_plate_def,
+            offsetId="labware-offset-id",
+        )
+    )
+
+    decoy.when(
+        labware_validation.validate_definition_is_labware(well_plate_def)
+    ).then_return(False)
+
+    with pytest.raises(LabwareDefinitionIsNotLabwareError):
+        await subject.execute(data)
+
+
+async def test_load_labware_on_labware(
+    decoy: Decoy,
+    well_plate_def: LabwareDefinition,
+    equipment: EquipmentHandler,
+    state_view: StateView,
+) -> None:
+    """A LoadLabware command should raise if the definition is not validated as a labware."""
+    subject = LoadLabwareImplementation(equipment=equipment, state_view=state_view)
+
+    data = LoadLabwareParams(
+        location=OnLabwareLocation(labwareId="other-labware-id"),
+        loadName="some-load-name",
+        namespace="opentrons-test",
+        version=1,
+        displayName="My custom display name",
+    )
+
+    decoy.when(
+        await equipment.load_labware(
+            location=OnLabwareLocation(labwareId="other-labware-id"),
+            load_name="some-load-name",
+            namespace="opentrons-test",
+            version=1,
+            labware_id=None,
+        )
+    ).then_return(
+        LoadedLabwareData(
+            labware_id="labware-id",
+            definition=well_plate_def,
+            offsetId="labware-offset-id",
+        )
+    )
+
+    decoy.when(
+        labware_validation.validate_definition_is_labware(well_plate_def)
+    ).then_return(True)
+
+    decoy.when(state_view.labware.get("other-labware-id")).then_return(
+        LoadedLabware.construct(loadName="other-load-name")  # type: ignore[call-arg]
+    )
+
+    decoy.when(
+        labware_validation.validate_labware_can_be_stacked(
+            well_plate_def, "other-load-name"
+        )
+    ).then_return(True)
+
+    result = await subject.execute(data)
+
+    assert result == LoadLabwareResult(
+        labwareId="labware-id",
+        definition=well_plate_def,
+        offsetId="labware-offset-id",
+    )
+
+
+async def test_load_labware_raises_cannot_stack(
+    decoy: Decoy,
+    well_plate_def: LabwareDefinition,
+    equipment: EquipmentHandler,
+    state_view: StateView,
+) -> None:
+    """A LoadLabware command should raise if the definition is not validated as a labware."""
+    subject = LoadLabwareImplementation(equipment=equipment, state_view=state_view)
+
+    data = LoadLabwareParams(
+        location=OnLabwareLocation(labwareId="other-labware-id"),
+        loadName="some-load-name",
+        namespace="opentrons-test",
+        version=1,
+        displayName="My custom display name",
+    )
+
+    decoy.when(
+        await equipment.load_labware(
+            location=OnLabwareLocation(labwareId="other-labware-id"),
+            load_name="some-load-name",
+            namespace="opentrons-test",
+            version=1,
+            labware_id=None,
+        )
+    ).then_return(
+        LoadedLabwareData(
+            labware_id="labware-id",
+            definition=well_plate_def,
+            offsetId="labware-offset-id",
+        )
+    )
+
+    decoy.when(
+        labware_validation.validate_definition_is_labware(well_plate_def)
+    ).then_return(True)
+
+    decoy.when(state_view.labware.get("other-labware-id")).then_return(
+        LoadedLabware.construct(loadName="other-load-name")  # type: ignore[call-arg]
+    )
+
+    decoy.when(
+        labware_validation.validate_labware_can_be_stacked(
+            well_plate_def, "other-load-name"
+        )
+    ).then_return(False)
+
+    with pytest.raises(LabwareCannotBeStackedError):
+        await subject.execute(data)
