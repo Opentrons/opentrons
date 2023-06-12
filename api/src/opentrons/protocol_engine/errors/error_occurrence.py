676c6d92c643b4f4d72b1dd43a8ae319bbc7788b
Frank Sinapi <frank.sinapi@opentrons.com>
2023-06-12 10:43:16 -0400

    ErrorOccurrence changes - Made the errorCode field a string instead of an int - Added an override for the schema for ErrorOccurrence so that the code appears to be required

--- api/src/opentrons/protocol_engine/errors/error_occurrence.py
+++ api/src/opentrons/protocol_engine/errors/error_occurrence.py
@@ -1,5 +1,6 @@
 """Models for concrete occurrences of specific errors."""
 from datetime import datetime
+from typing import Any, Dict
 from pydantic import BaseModel, Field
 from .exceptions import ErrorCode
 
@@ -13,7 +14,22 @@ class ErrorOccurrence(BaseModel):
     errorType: str = Field(..., description="Specific error type that occurred.")
     createdAt: datetime = Field(..., description="When the error occurred.")
     detail: str = Field(..., description="A human-readable message about the error.")
-    errorCode: int = Field(
+    errorCode: str = Field(
         default=ErrorCode.UNKNOWN,
         description="An enumerated error code for the error type.",
     )
+
+    class Config:
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any], model: object) -> None:
+            """Append the schema to make the errorCode appear required.
+
+            `errorCode` has a default because it is not included in earlier
+            versions of this model, _and_ this model is loaded directly from
+            the on-robot store. That means that, without a default, it will
+            fail to parse. Once a default is defined, the automated schema will
+            mark this as a non-required field, which is misleading as this is
+            a response from the server to the client and it will always have an
+            errorCode defined. This hack is required because it informs the client
+            that it does not, in fact, have to account for a missing errorCode."""
+            schema["required"].append("errorCode")
