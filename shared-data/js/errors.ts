593fc93489c6d3adca3fcad78c3c1d18fc2f0f02
Seth Foster <seth@opentrons.com>
2023-06-12 13:54:24 -0400

    shared-data: add js tests and format and lint

--- shared-data/js/errors.ts
+++ shared-data/js/errors.ts
@@ -3,29 +3,33 @@ import errorDefinitions from '../errors/definitions/1/errors.json'
 export type ErrorCodes = keyof typeof errorDefinitions.codes
 export type ErrorCategories = keyof typeof errorDefinitions.categories
 
-
-export type ErrorCategory = {
-    detail: string
-    codePrefix: string
+export interface ErrorCategory {
+  detail: string
+  codePrefix: string
 }
 
-export type ErrorBody = {
-    detail: string
-    category: ErrorCategory
-    code: ErrorCodes
+export interface ErrorBody {
+  detail: string
+  category: ErrorCategory
+  code: ErrorCodes
 }
 
-type JSONErrorBody = {
-    detail: string
-    category: ErrorCategories
+interface JSONErrorBody {
+  detail: string
+  category: ErrorCategories
 }
 
 export type ErrorDefinitions = Map<ErrorCodes, ErrorBody>
 
-export function getError(code: ErrorCodes): ErrorBody | null{
-    return errorDefinitions.codes[code] ? {
+export function getError(code: ErrorCodes): ErrorBody | null {
+  return errorDefinitions.codes[code]
+    ? {
         code: code,
         detail: errorDefinitions.codes[code].detail,
-        category: errorDefinitions.categories[(errorDefinitions.codes[code] as JSONErrorBody).category]
-    } : null
+        category:
+          errorDefinitions.categories[
+            (errorDefinitions.codes[code] as JSONErrorBody).category
+          ],
+      }
+    : null
 }
