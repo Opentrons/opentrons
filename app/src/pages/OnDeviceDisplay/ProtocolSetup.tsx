6abf7ecfc0364a4c8ff260e90e6f71e7a9253e48
Shlok Amin <shlokamin@me.com>
2023-06-12 16:52:18 -0400

    fix(app): do not render maintenance modal when starting LPC from ODD
    RQA-951

--- app/src/pages/OnDeviceDisplay/ProtocolSetup.tsx
+++ app/src/pages/OnDeviceDisplay/ProtocolSetup.tsx
@@ -34,6 +34,7 @@ import {
 
 import { StyledText } from '../../atoms/text'
 import { Skeleton } from '../../atoms/Skeleton'
+import { useMaintenanceRunTakeover } from '../../organisms/TakeoverModal'
 import {
   useAttachedModules,
   useLPCDisabledReason,
@@ -45,7 +46,6 @@ import { ProtocolSetupModules } from '../../organisms/ProtocolSetupModules'
 import { ProtocolSetupLiquids } from '../../organisms/ProtocolSetupLiquids'
 import { ProtocolSetupInstruments } from '../../organisms/ProtocolSetupInstruments'
 import { useLaunchLPC } from '../../organisms/LabwarePositionCheck/useLaunchLPC'
-import { ProtocolSetupLabwarePositionCheck } from '../../organisms/ProtocolSetupLabwarePositionCheck'
 import { getUnmatchedModulesForProtocol } from '../../organisms/ProtocolSetupModules/utils'
 import { ConfirmCancelRunModal } from '../../organisms/OnDeviceDisplay/RunningProtocol'
 import {
@@ -223,6 +223,7 @@ function PrepareToRun({
     protocolRecord?.data.files[0].name
   const mostRecentAnalysis = useMostRecentCompletedAnalysis(runId)
   const { launchLPC, LPCWizard } = useLaunchLPC(runId)
+  const { setODDMaintenanceFlowInProgress } = useMaintenanceRunTakeover()
 
   const { play } = useRunControls(runId)
 
@@ -405,7 +406,10 @@ function PrepareToRun({
           disabled={protocolModulesInfo.length === 0}
         />
         <ProtocolSetupStep
-          onClickSetupStep={launchLPC}
+          onClickSetupStep={() => {
+            setODDMaintenanceFlowInProgress()
+            launchLPC()
+          }}
           title={t('labware_position_check')}
           detail={t(
             lpcDisabledReason != null ? 'currently_unavailable' : 'recommended'
@@ -473,12 +477,6 @@ export function ProtocolSetup(): JSX.Element {
     ),
     labware: (
       <ProtocolSetupLabware runId={runId} setSetupScreen={setSetupScreen} />
-    ),
-    lpc: (
-      <ProtocolSetupLabwarePositionCheck
-        runId={runId}
-        setSetupScreen={setSetupScreen}
-      />
     ),
     liquids: (
       <ProtocolSetupLiquids runId={runId} setSetupScreen={setSetupScreen} />
