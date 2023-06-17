a5c49d8d53f6dd1d6649f793bc7e40c049139fa1
andySigler <andrewsigler1@gmail.com>
2023-06-16 21:40:14 -0400

    set correct deck version

--- hardware-testing/hardware_testing/gravimetric/helpers.py
+++ hardware-testing/hardware_testing/gravimetric/helpers.py
@@ -73,7 +73,7 @@ def get_api_context(
         hardware_api=ThreadManager(_thread_manager_build_hw_api),  # type: ignore[arg-type]
         deck_type="ot3_standard",
         extra_labware=extra_labware,
-        deck_version=int(deck_version),
+        deck_version=2,
     )
 
 
