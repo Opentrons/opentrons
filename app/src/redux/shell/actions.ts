2025ab3927913ae1b3215a40efa1fc17dbc827d3
koji <koji.kanao@opentrons.com>
2023-06-21 18:25:29 -0400

    feat(app): add ErrorBoundary component (#12902)
    Add ErrorBoundary component to provide restart function with users when the touchscreen app shows the
    white screen

--- app/src/redux/shell/actions.ts
+++ app/src/redux/shell/actions.ts
@@ -1,10 +1,15 @@
-import type { UiInitializedAction, UsbRequestsAction } from './types'
+import type {
+  UiInitializedAction,
+  UsbRequestsAction,
+  AppRestartAction,
+} from './types'
 
 export const UI_INITIALIZED: 'shell:UI_INITIALIZED' = 'shell:UI_INITIALIZED'
 export const USB_HTTP_REQUESTS_START: 'shell:USB_HTTP_REQUESTS_START' =
   'shell:USB_HTTP_REQUESTS_START'
 export const USB_HTTP_REQUESTS_STOP: 'shell:USB_HTTP_REQUESTS_STOP' =
   'shell:USB_HTTP_REQUESTS_STOP'
+export const APP_RESTART: 'shell:APP_RESTART' = 'shell:APP_RESTART'
 
 export const uiInitialized = (): UiInitializedAction => ({
   type: UI_INITIALIZED,
@@ -18,5 +23,10 @@ export const usbRequestsStart = (): UsbRequestsAction => ({
 
 export const usbRequestsStop = (): UsbRequestsAction => ({
   type: USB_HTTP_REQUESTS_STOP,
+  meta: { shell: true },
+})
+
+export const appRestart = (): AppRestartAction => ({
+  type: APP_RESTART,
   meta: { shell: true },
 })
