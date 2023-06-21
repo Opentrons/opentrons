15d3fcea067f504f3f36e8f66fc64ac1df487142
Brian Cooper <brian@opentrons.com>
2023-06-21 15:27:59 -0400

    find and replace all usage

--- hardware-testing/hardware_testing/gravimetric/execute.py
+++ hardware-testing/hardware_testing/gravimetric/execute.py
@@ -213,7 +213,7 @@ def _load_labware(
     tiprack_load_settings: List[Tuple[int, str]] = [
         (
             slot,
-            f"opentrons_ot3_96_tiprack_{cfg.tip_volume}ul",
+            f"opentrons_flex_96_tiprack_{cfg.tip_volume}ul",
         )
         for slot in cfg.slots_tiprack
     ]
