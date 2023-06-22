d16d419c7d99322c7009dc379ae6ed2583de60de
tamarzanzouri <tamar.zanzouri@opentrons.com>
2023-06-22 16:54:51 -0400

    raise an estop error when caught

--- hardware/opentrons_hardware/hardware_control/move_group_runner.py
+++ hardware/opentrons_hardware/hardware_control/move_group_runner.py
@@ -65,6 +65,11 @@ from opentrons_hardware.hardware_control.motion import MoveStopCondition
 from opentrons_hardware.hardware_control.motion_planning.move_utils import (
     MoveConditionNotMet,
 )
+from opentrons_shared_data.errors.exceptions import (
+    CanbusCommunicationError,
+    EStopActivatedError,
+    EnumeratedError,
+)
 from .types import NodeDict
 
 log = logging.getLogger(__name__)
@@ -450,6 +455,24 @@ class MoveScheduler:
                 nodes.append(NodeId(node_id))
         return nodes
 
+    def _raise_exception_from_error(
+        self, group_id: int, error: ErrorMessage
+    ) -> EnumeratedError:
+        if error.payload.error_code.value == ErrorCode.estop_detected:
+            raise EStopActivatedError(
+                message=f"estop detected for move group {group_id}",
+                # detail={error.message_id.value: error},
+                # should be an EnumeratedError
+                # wrapping=[error],
+            )
+        else:
+            raise CanbusCommunicationError(
+                message=f"Unrecoverable firmware error during move group {group_id}: {error}",
+                # detail={error.message_id.value: error},
+                # should be an EnumeratedError
+                # wrapping=[error],
+            )
+
     async def _send_stop_if_necessary(
         self, can_messenger: CanMessenger, group_id: int
     ) -> None:
@@ -462,9 +485,7 @@ class MoveScheduler:
             if err != ErrorCode.stop_requested:
                 log.warning("Stop request failed")
             if self._error:
-                raise RuntimeError(
-                    f"Unrecoverable firmware error during move group {group_id}: {self._error}"
-                )
+                self._raise_exception_from_error(group_id=group_id, error=self._error)
             else:
                 # This happens when the move completed without stop condition
                 raise MoveConditionNotMet
