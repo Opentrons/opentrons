be25f5acbf91711a81ee580bdf9bc44181b36dac
Laura Cox <laura@opentrons.com>
2023-06-22 12:06:06 +0300

    use gantry load instead, add test

--- api/src/opentrons/hardware_control/ot3api.py
+++ api/src/opentrons/hardware_control/ot3api.py
@@ -1661,7 +1661,8 @@ class OT3API(
 
         # TODO: implement tip-detection sequence during pick-up-tip for 96ch,
         #       but not with DVT pipettes because those can only detect drops
-        if self._pipette_handler.get_pipette(realmount).channels.value != 96:
+
+        if self.gantry_load != GantryLoad.HIGH_THROUGHPUT:
             await self._backend.get_tip_present(realmount, TipStateType.PRESENT)
 
         _add_tip_to_instrs()
@@ -1743,7 +1744,7 @@ class OT3API(
         )
 
         # TODO: implement tip-detection sequence during drop-tip for 96ch
-        if self._pipette_handler.get_pipette(realmount).channels.value != 96:
+        if self.gantry_load != GantryLoad.HIGH_THROUGHPUT:
             await self._backend.get_tip_present(realmount, TipStateType.ABSENT)
         _remove()
 
