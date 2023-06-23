0e716c3e909b8892e2d93e1bec175f4384841b6e
vegano1 <brayan.almonte@gmail.com>
2023-06-23 16:55:16 -0400

    move down and add comment for clarity

--- shared-data/js/getLabware.ts
+++ shared-data/js/getLabware.ts
@@ -37,10 +37,6 @@ export const LABWAREV2_DO_NOT_LIST = [
   'eppendorf_96_tiprack_10ul_eptips',
   'opentrons_calibrationblock_short_side_left',
   'opentrons_calibrationblock_short_side_right',
-  'opentrons_calibration_adapter_heatershaker_module',
-  'opentrons_calibration_adapter_magnetic_module',
-  'opentrons_calibration_adapter_temperature_module',
-  'opentrons_calibration_adapter_thermocycler_module',
   // TODO(lc 8-24-2022) We are temporarily filtering
   // out ot-3 labware definitions right now. We should
   // have a way to filter these in the future to display
@@ -49,6 +45,11 @@ export const LABWAREV2_DO_NOT_LIST = [
   'opentrons_ot3_96_tiprack_200ul',
   'opentrons_ot3_96_tiprack_1000ul',
   'opentrons_ot3_96_tiprack_50ul',
+  // TODO(ba, 2023-06-23): exclude module calibration adapters
+  'opentrons_calibration_adapter_heatershaker_module',
+  'opentrons_calibration_adapter_magnetic_module',
+  'opentrons_calibration_adapter_temperature_module',
+  'opentrons_calibration_adapter_thermocycler_module',
 ]
 // NOTE(sa, 2020-7-14): in PD we do not want to list calibration blocks
 // but we still might want the rest of the labware in LABWAREV2_DO_NOT_LIST
