df2fb6288047fb6c45dd39dd8d5d88035ab45af3
Brian Cooper <brian@opentrons.com>
2023-06-23 10:55:16 -0400

    proof of concept deck map anim

--- components/src/hardware-sim/Deck/RobotWorkSpace.stories.tsx
+++ components/src/hardware-sim/Deck/RobotWorkSpace.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { animated, useSpring, useSpringValue } from '@react-spring/web'
 import { RobotWorkSpace } from './RobotWorkSpace'
 import { RobotCoordsForeignDiv, Module } from '@opentrons/components'
 import { getModuleDef2 } from '@opentrons/shared-data'
@@ -57,13 +58,6 @@ Deck.args = {
     const rectSlot = deckSlotsById['11']
     return (
       <>
-        <rect
-          x={rectSlot.position[0]}
-          y={rectSlot.position[1]}
-          width={rectSlot.boundingBox.xDimension}
-          height={rectSlot.boundingBox.yDimension}
-          fill="#0075ff33"
-        />
         <RobotCoordsForeignDiv
           x={divSlot.position[0] - 30}
           y={divSlot.position[1]}
@@ -85,7 +79,43 @@ Deck.args = {
           x={moduleSlot.position[0]}
           y={moduleSlot.position[1]}
         />
+        <Wrapper
+          x={rectSlot.position[0]}
+          y={rectSlot.position[1]}
+          width={rectSlot.boundingBox.xDimension}
+          height={rectSlot.boundingBox.yDimension}
+          fill="#0075ff33"
+        />
       </>
     )
   },
+}
+
+function Wrapper(props): JSX.Element {
+  const [isOn, setIsOn] = React.useState(false)
+  return isOn ? (
+    <MyRect {...props} onClick={() => { setIsOn(!isOn) }}/>
+  ) : (
+    <rect onClick={() => { setIsOn(!isOn) }} {...props} /> 
+  )
+}
+
+function MyRect(props): JSX.Element {
+  const springProps = useSpring({
+    config: {
+      duration: 2000,
+    },
+    from: { y: 0, x: 0, fill: '#ff0000' },
+    to: async (next, cancel) => {
+      await next({ x: -130, fill: '#00ff00' })
+      await next({ y: 88, fill: '#0000ff' })
+      await next({ x: 0, fill: '#ff0000' })
+      await next({ y: 0, fill: '#00ff00' })
+    },
+    loop: true,
+  })
+
+  return (
+    <animated.rect style={springProps} {...props}  />
+  )
 }
