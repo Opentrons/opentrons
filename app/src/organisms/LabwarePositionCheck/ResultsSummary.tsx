471e572fd0f99c3496e807983c0908cefd4121ff
Brian Cooper <brian@opentrons.com>
2023-06-27 16:36:48 -0400

    patch up tests and lint

--- app/src/organisms/LabwarePositionCheck/ResultsSummary.tsx
+++ app/src/organisms/LabwarePositionCheck/ResultsSummary.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import isEqual from 'lodash/isEqual'
 import { useTranslation } from 'react-i18next'
