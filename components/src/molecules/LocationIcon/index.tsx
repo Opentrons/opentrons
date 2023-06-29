842a76e402ae6724866968a2f871477632d73e96
Brent Hagen <brent.hagen@opentrons.com>
2023-06-29 19:38:17 -0400

    add responsive styles to LocationIcon

--- components/src/molecules/LocationIcon/index.tsx
+++ components/src/molecules/LocationIcon/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react'
+import { css } from 'styled-components'
 
 import { Icon } from '../../icons'
 import { Flex, Text } from '../../primitives'
 import { ALIGN_CENTER } from '../../styles'
-import { BORDERS, COLORS, SPACING, TYPOGRAPHY } from '../../ui-style-constants'
+import {
+  BORDERS,
+  COLORS,
+  RESPONSIVENESS,
+  SPACING,
+  TYPOGRAPHY,
+} from '../../ui-style-constants'
 
 import type { IconName } from '../../icons'
 import type { StyleProps } from '../../primitives'
@@ -23,6 +30,30 @@ interface HardwareIconProps extends StyleProps {
 // type union requires one of slotName or iconName, but not both
 type LocationIconProps = SlotLocationProps | HardwareIconProps
 
+const LOCATION_ICON_STYLE = css<{
+  slotName?: string
+  height?: string
+  width?: string
+}>`
+  align-items: ${ALIGN_CENTER};
+  border: 2px solid ${COLORS.darkBlack100};
+  border-radius: ${BORDERS.borderRadiusSize3};
+  height: ${props => props.height ?? SPACING.spacing32};
+  padding: ${SPACING.spacing2} 0.375rem;
+  width: ${props => props.width ?? 'max-content'};
+  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+    padding: ${SPACING.spacing4}
+      ${props => (props.slotName != null ? SPACING.spacing8 : '0.375rem')};
+  }
+`
+
+const SLOT_NAME_TEXT_STYLE = css`
+  ${TYPOGRAPHY.pSemiBold}
+  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+    ${TYPOGRAPHY.smallBodyTextBold}
+  }
+`
+
 export function LocationIcon({
   slotName,
   iconName,
@@ -30,17 +61,12 @@ export function LocationIcon({
 }: LocationIconProps): JSX.Element {
   return (
     <Flex
-      alignItems={ALIGN_CENTER}
-      height="2rem"
-      width="max-content"
-      padding={`${SPACING.spacing4} ${SPACING.spacing8}`}
-      border={`2px solid ${COLORS.darkBlack100}`}
-      borderRadius={BORDERS.borderRadiusSize3}
       data-testid={
         slotName != null
           ? `LocationIcon_${slotName}`
           : `LocationIcon_${String(iconName)}`
       }
+      css={LOCATION_ICON_STYLE}
       {...styleProps}
     >
       {iconName != null ? (
@@ -51,7 +77,7 @@ export function LocationIcon({
           aria-label={iconName}
         />
       ) : (
-        <Text css={TYPOGRAPHY.smallBodyTextBold}>{slotName}</Text>
+        <Text css={SLOT_NAME_TEXT_STYLE}>{slotName}</Text>
       )}
     </Flex>
   )
