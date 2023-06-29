5b02a370b5e4e2720245e3af92f6c226d62ae7e1
David Gomez <david.gomez@opentrons.com>
2023-06-29 13:47:33 -0400

    added 96ch compliance test

---
+++ hardware-testing/hardware_testing/scripts/96ch_test_compliance.py
@@ -0,0 +1,67 @@
+import asyncio
+
+from hardware_testing.opentrons_api.helpers_ot3 import (
+    build_async_ot3_hardware_api,
+    get_slot_calibration_square_position_ot3)
+from hardware_testing.opentrons_api.types import GantryLoad, OT3Mount, OT3Axis, Point
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+async def _main() -> None:
+    api = await build_async_ot3_hardware_api(
+        pipette_left="p1000_96_v3.4",
+        stall_detection_enable=False,
+        gripper="GRPV1120230323A01"
+    )
+    await api.reset()
+
+    #home and record home positions
+    await api.home()
+    home_pos = await api.gantry_position(OT3Mount.LEFT)
+    home_pos_grip = await api.gantry_position(OT3Mount.GRIPPER)
+
+    for i in range(1000):
+        #move to slot 5
+        slot_5 = get_slot_calibration_square_position_ot3(5)
+        await api.move_to(OT3Mount.LEFT, slot_5._replace(z=home_pos.z))
+        slot_5_pos = await api.gantry_position(OT3Mount.LEFT)
+        slot_5_pos_g = await api.gantry_position(OT3Mount.GRIPPER)
+        await api.move_to(OT3Mount.LEFT, slot_5._replace(z=100))
+        await api.move_to(OT3Mount.GRIPPER, slot_5_pos_g._replace(z=50))
+
+        #pickup tip and aspirate
+        await api.pick_up_tip(mount = OT3Mount.LEFT, tip_length = 10)
+        await api.aspirate(mount = OT3Mount.LEFT, volume = 10)
+        await api.dispense(mount = OT3Mount.LEFT, volume = 10)
+        await api.drop_tip(mount = OT3Mount.LEFT, home_after = False)
+
+        #grip plate
+        await api.home_gripper_jaw()
+
+        #move up
+        await api.move_to(OT3Mount.LEFT, slot_5_pos)
+        await api.move_to(OT3Mount.GRIPPER, slot_5_pos_g)
+
+        #move to slot 12
+        slot_10 = get_slot_calibration_square_position_ot3(10)
+        await api.move_to(OT3Mount.LEFT, slot_10._replace(z=home_pos.z))
+        slot_10_pos = await api.gantry_position(OT3Mount.LEFT)
+        slot_10_pos_g = await api.gantry_position(OT3Mount.GRIPPER)
+
+        await api.move_to(OT3Mount.LEFT, slot_10_pos._replace(z=100))
+        await api.move_to(OT3Mount.GRIPPER, slot_10_pos._replace(z=50))
+
+        await api.pick_up_tip(mount = OT3Mount.LEFT, tip_length = 10)
+        await api.aspirate(mount = OT3Mount.LEFT, volume = 10)
+        await api.dispense(mount = OT3Mount.LEFT, volume = 10)
+        await api.drop_tip(mount = OT3Mount.LEFT, home_after = False)
+
+        #grip plate
+        await api.home_gripper_jaw()
+
+
+
+if __name__ == "__main__":
+    asyncio.run(_main())
