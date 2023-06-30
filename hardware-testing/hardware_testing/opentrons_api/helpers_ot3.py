f40e09e73dc9118beb5b90410b562d827d6eef60
Andy Sigler <andrewsigler1@gmail.com>
2023-06-29 21:12:18 -0400

    feat(hardware-testing): 96ch working on scale (#13019)

--- hardware-testing/hardware_testing/opentrons_api/helpers_ot3.py
+++ hardware-testing/hardware_testing/opentrons_api/helpers_ot3.py
@@ -548,17 +548,21 @@ def _jog_read_user_input(terminator: str, home_key: str) -> Tuple[str, float, bo
 
 
 async def _jog_axis_some_distance(
-    api: OT3API, mount: OT3Mount, axis: str, distance: float
+    api: OT3API,
+    mount: OT3Mount,
+    axis: str,
+    distance: float,
+    speed: Optional[float],
 ) -> None:
     if not axis or distance == 0.0:
         return
     elif axis == "G":
         await move_gripper_jaw_relative_ot3(api, distance)
     elif axis == "P":
-        await move_plunger_relative_ot3(api, mount, distance)
+        await move_plunger_relative_ot3(api, mount, distance, speed=speed)
     else:
         delta = Point(**{axis.lower(): distance})
-        await api.move_rel(mount=mount, delta=delta)
+        await api.move_rel(mount=mount, delta=delta, speed=speed)
 
 
 async def _jog_print_current_position(
@@ -590,6 +594,7 @@ async def _jog_do_print_then_input_then_move(
     distance: float,
     do_home: bool,
     display: Optional[bool] = True,
+    speed: Optional[float] = None,
 ) -> Tuple[str, float, bool]:
     try:
         if display:
@@ -610,7 +615,7 @@ async def _jog_do_print_then_input_then_move(
         }
         await api.home([str_to_axes[axis]])
     else:
-        await _jog_axis_some_distance(api, mount, axis, distance)
+        await _jog_axis_some_distance(api, mount, axis, distance, speed)
     return axis, distance, do_home
 
 
@@ -619,6 +624,7 @@ async def jog_mount_ot3(
     mount: OT3Mount,
     critical_point: Optional[CriticalPoint] = None,
     display: Optional[bool] = True,
+    speed: Optional[float] = None,
 ) -> Dict[OT3Axis, float]:
     """Jog an OT3 mount's gantry XYZ and pipettes axes."""
     if api.is_simulator:
@@ -632,7 +638,14 @@ async def jog_mount_ot3(
     while True:
         try:
             axis, distance, do_home = await _jog_do_print_then_input_then_move(
-                api, mount, critical_point, axis, distance, do_home, display=display
+                api,
+                mount,
+                critical_point,
+                axis,
+                distance,
+                do_home,
+                display=display,
+                speed=speed,
             )
         except ValueError as e:
             print(e)
