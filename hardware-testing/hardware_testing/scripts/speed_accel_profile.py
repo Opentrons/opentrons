0d26f903ed469ecd3a28fde9e6b633cf1fb5d90a
David Gomez <david.gomez@opentrons.com>
2023-07-07 16:15:44 -0400

    added encoder value stability check, refactored movement check, added output of raw errors

--- hardware-testing/hardware_testing/scripts/speed_accel_profile.py
+++ hardware-testing/hardware_testing/scripts/speed_accel_profile.py
@@ -44,14 +44,14 @@ FAKE_GRIPPER = {NodeId.gripper_g: MotorStatus(motor_ok=True, encoder_ok=True)}
 TEST_PARAMETERS: Dict[GantryLoad, Dict[str, Dict[str, Dict[str, float]]]] = {
     GantryLoad.LOW_THROUGHPUT: {
         "X": {
-            "SPEED": {"MIN": 400, "MAX": 600, "INC": 200},
-            "ACCEL": {"MIN": 800, "MAX": 1200, "INC": 400},
-            "CURRENT": {"MIN": 1.25, "MAX": 1.4, "INC": 0.15}
+            "SPEED": {"MIN": 250, "MAX": 450, "INC": 100},
+            "ACCEL": {"MIN": 700, "MAX": 900, "INC": 100},
+            "CURRENT": {"MIN": 1, "MAX": 1.5, "INC": 0.25}
         },
         "Y": {
-            "SPEED": {"MIN": 325, "MAX": 500, "INC": 175},
-            "ACCEL": {"MIN": 600, "MAX": 1000, "INC": 400},
-            "CURRENT": {"MIN": 1.25, "MAX": 1.4, "INC": 0.15}
+            "SPEED": {"MIN": 225, "MAX": 375, "INC": 75},
+            "ACCEL": {"MIN": 500, "MAX": 700, "INC": 100},
+            "CURRENT": {"MIN": 1, "MAX": 1.4, "INC": 0.2}
         },
         "L": {
             "SPEED": {"MIN": 80, "MAX": 120, "INC": 20},
@@ -202,6 +202,15 @@ NEG_POINT_MAP = {
     "G": Point(z=-step_g)
 }
 
+#find real value for G
+ENCODER_RESOLUTION = {
+    "Y": 0.0399,
+    "X": 0.0399,
+    "L": 0.00293,
+    "R": 0.00293,
+    "G": 0.05
+}
+
 
 def get_pos_delta(
     final_pos: Dict[OT3Axis, float], inital_pos: Dict[OT3Axis, float]
@@ -222,13 +231,18 @@ def check_move_error(delta_move: float, point_delta: Point, axis: str) -> float:
 
     # compare with point based on axis
     if axis == "X":
-        move_error = abs(delta_move) - abs(point_delta[0])
+        #move_error = abs(delta_move) - abs(point_delta[0])
+        move_error = delta_move - point_delta[0]
+        #print("Actual Move Error: " + str(delta_move - point_delta[0]))
     elif axis == "Y":
-        move_error = abs(delta_move) - abs(point_delta[1])
+        #move_error = abs(delta_move) - abs(point_delta[1])
+        move_error = delta_move - point_delta[1]
+        #print("Actual Move Error: " + str(delta_move - point_delta[1]))
     else:
-        move_error = abs(delta_move) - abs(point_delta[2])
+        #move_error = abs(delta_move) - abs(point_delta[2])
+        move_error = delta_move - point_delta[2]
 
-    return abs(move_error)
+    return move_error
 
 
 def get_move_correction(actual_move: float, axis: str, direction: int) -> Point:
@@ -243,17 +257,60 @@ def get_move_correction(actual_move: float, axis: str, direction: int) -> Point:
 
     return move_correction
 
+async def get_stable_encoder(axis: str, api: OT3API, MOUNT: OT3Mount) -> Dict[OT3Axis, float]:
+    """Keep reading the encoder until it's value is stable"""
+    # Keep reading until we have 3 stable reads
+    stable_value_count = 0
+    while stable_value_count < 2:
+        read0 = await api.encoder_current_position_ot3(mount=MOUNT,
+                                                            refresh=True)
+        read1 = await api.encoder_current_position_ot3(mount=MOUNT,
+                                                            refresh=True)
+
+        jitter = get_pos_delta(read1, read0)[AXIS_MAP[axis]]
+        #check if difference in subsequent reads is less than an encoder tic
+        if jitter < ENCODER_RESOLUTION[axis]:
+            stable_value_count = stable_value_count + 1
+
+    # Return the stable reading
+    return await api.encoder_current_position_ot3(mount=MOUNT,
+                                                        refresh=True)
+
+
+async def move_and_check(axis: str, api: OT3API, MOUNT: OT3Mount,
+                         move_dist: Point, move_speed: int) -> list:
+    """Move the Gantry a set distance, check the movement positional error"""
+
+    inital_pos = await get_stable_encoder(axis, api, MOUNT)
+    await api.move_rel(mount=MOUNT, delta=move_dist, speed=move_speed)
+
+    final_pos = await get_stable_encoder(axis, api, MOUNT)
+
+    print("inital_pos: " + str(inital_pos))
+    print("final_pos: " + str(final_pos))
+
+    delta_move_pos = get_pos_delta(final_pos, inital_pos)
+    delta_move_axis = delta_move_pos[AXIS_MAP[axis]]
+    move_error = check_move_error(delta_move_axis, move_dist, axis)
+
+    print(axis + " Error: " + str(move_error))
+
+    return [move_error, delta_move_axis]
+
 
 async def _single_axis_move(
     axis: str, api: OT3API, cycles: int = 1
 ) -> list:
     """Move and check error of single axis."""
     avg_error = []
+    neg_errors = []
+    pos_errors = []
     MOUNT = MOUNT_MAP[axis]
 
     # move away from the limit switch before cycling
     await api.move_rel(mount=MOUNT, delta=HOME_POINT_MAP[axis], speed=80)
-    time.sleep(ENCODER_DELAY*2) #let postition settle
+    #time.sleep(ENCODER_DELAY*2) #let postition settle
+
     # if axis == "G":
     #     await api.home_gripper_jaw()
     #     api._backend._motor_status.update(FAKE_GRIPPER)
@@ -276,8 +333,10 @@ async def _single_axis_move(
         #     print("must home error should be raised")
         # else:
         #     print("must home error should NOT be raised")
-        inital_pos = await api.encoder_current_position_ot3(mount=MOUNT,
-                                                            refresh=ENCODER_REFRESH)
+
+
+        # inital_pos = await api.encoder_current_position_ot3(mount=MOUNT,
+        #                                                     refresh=ENCODER_REFRESH)
         cur_speed = SETTINGS[AXIS_MAP[axis]].max_speed
 
         print("Executing Move: " + str(c))
@@ -285,25 +344,16 @@ async def _single_axis_move(
         print(" Speed - " + str(cur_speed))
         print(" Acceleration - " + str(SETTINGS[AXIS_MAP[axis]].acceleration))
 
-        await api.move_rel(mount=MOUNT, delta=NEG_POINT_MAP[axis], speed=cur_speed)
+        neg_move = await move_and_check(axis, api, MOUNT, NEG_POINT_MAP[axis], cur_speed)
+        neg_move_error = neg_move[0]
+        neg_move_dist = neg_move[1]
 
-        # check if we moved to correct position with encoder
-        time.sleep(ENCODER_DELAY) #let postition settle
-        move_pos = await api.encoder_current_position_ot3(mount=MOUNT,
-                                                            refresh=ENCODER_REFRESH)
-        print("inital_pos: " + str(inital_pos))
-        print("mov_pos: " + str(move_pos))
-        delta_move_pos = get_pos_delta(move_pos, inital_pos)
-        delta_move_axis = delta_move_pos[AXIS_MAP[axis]]
-        move_error = check_move_error(delta_move_axis, NEG_POINT_MAP[axis], axis)
-        # if we had error in the move stop move
-        print(axis + " Error: " + str(move_error))
+        avg_error.append(abs(neg_move_error))
+        neg_errors.append(neg_move_error)
 
-        avg_error.append(abs(move_error))
-        output_list = [sum(avg_error) / len(avg_error), c, max(avg_error)]
-        if move_error >= ERROR_THRESHOLD:
+        if abs(neg_move_error) >= ERROR_THRESHOLD:
             # attempt to move closer to the home position quickly
-            move_error_correction = get_move_correction(delta_move_axis, axis, -1)
+            move_error_correction = get_move_correction(neg_move_dist, axis, -1)
             print()
             print("**************************************")
             print("ERROR IN NEG MOVE, CORRECTING: " + str(move_error_correction))
@@ -330,26 +380,18 @@ async def _single_axis_move(
                 await api.home([OT3Axis.Z_G])
             if not BENCH:
                 await api.home([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
+
+            output_list = [sum(avg_error) / len(avg_error), c, max(avg_error), neg_errors, pos_errors]
             return output_list
 
-        # record the current position
-        inital_pos = await api.encoder_current_position_ot3(mount=MOUNT,
-                                                            refresh=ENCODER_REFRESH)
+        pos_move = await move_and_check(axis, api, MOUNT, POINT_MAP[axis], cur_speed)
+        pos_move_error = pos_move[0]
+        pos_move_dist = pos_move[1]
 
-        # move back to near homed position
-        await api.move_rel(mount=MOUNT, delta=POINT_MAP[axis], speed=cur_speed)
+        avg_error.append(abs(pos_move_error))
+        pos_errors.append(pos_move_error)
 
-        time.sleep(ENCODER_DELAY) #let postition settle
-        final_pos = await api.encoder_current_position_ot3(mount=MOUNT,
-                                                            refresh=ENCODER_REFRESH)
-        delta_pos = get_pos_delta(final_pos, inital_pos)
-        delta_pos_axis = delta_pos[AXIS_MAP[axis]]
-        move_error = check_move_error(delta_pos_axis, POINT_MAP[axis], axis)
-        print(axis + " Error: " + str(move_error))
-
-        avg_error.append(abs(move_error))
-        output_list = [sum(avg_error) / len(avg_error), c, max(avg_error)]
-        if abs(move_error) >= ERROR_THRESHOLD:
+        if abs(pos_move_error) >= ERROR_THRESHOLD:
             print()
             print("**************************************")
             print("ERROR IN POS MOVE")
@@ -361,6 +403,7 @@ async def _single_axis_move(
                 await api.home([OT3Axis.Z_G])
             if not BENCH:
                 await api.home([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
+            output_list = [sum(avg_error) / len(avg_error), c, max(avg_error), neg_errors, pos_errors]
             return output_list
 
         # home every 50 cycles in case we have drifted
@@ -371,12 +414,12 @@ async def _single_axis_move(
                 await api.home([OT3Axis.X, OT3Axis.Y, OT3Axis.Z_L, OT3Axis.Z_R])
             # move away from the limit switch before cycling
             await api.move_rel(mount=MOUNT, delta=HOME_POINT_MAP[axis], speed=80)
-            time.sleep(ENCODER_DELAY*2) #let postition settle
+            # time.sleep(ENCODER_DELAY*2) #let postition settle
 
     if DELAY > 0:
         time.sleep(DELAY)
 
-    output_list = [sum(avg_error) / len(avg_error), c + 1, max(avg_error)]
+    output_list = [sum(avg_error) / len(avg_error), c + 1, max(avg_error), neg_errors, pos_errors]
     return output_list
 
 
@@ -456,7 +499,8 @@ async def _main(is_simulating: bool) -> None:
 
         test_axis_list = list(AXIS)
         with open(BASE_DIRECTORY + SAVE_NAME + AXIS + ".csv", mode="w") as csv_file:
-            fieldnames = ["axis", "current", "speed", "acceleration", "error", "cycles"]
+            fieldnames = ["test_name", "axis", "current", "speed",
+                          "acceleration", "neg_error", "pos_error"]
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
 
@@ -502,6 +546,8 @@ async def _main(is_simulating: bool) -> None:
                     run_avg_error = move_output_list[0]
                     cycles_completed = move_output_list[1]
                     max_error = move_output_list[2]
+                    neg_errors = move_output_list[3]
+                    pos_errors = move_output_list[4]
 
                     # #record results to dictionary for neat formatting later
                     c_i = TABLE_RESULTS_KEY[test_axis][p["CURRENT"]]
@@ -517,16 +563,28 @@ async def _main(is_simulating: bool) -> None:
                     print("Speed: " + str(p["SPEED"]))
                     print("Acceleration: " + str(p["ACCEL"]))
                     print("Error: " + str(run_avg_error))
-                    writer.writerow(
-                        {
-                            "axis": test_axis,
-                            "current": p["CURRENT"],
-                            "speed": p["SPEED"],
-                            "acceleration": p["ACCEL"],
-                            "error": run_avg_error,
-                            "cycles": cycles_completed,
-                        }
-                    )
+                    for row in neg_errors:
+                        writer.writerow(
+                            {
+                                "test_name" : TEST_NAME,
+                                "axis": test_axis,
+                                "current": p["CURRENT"],
+                                "speed": p["SPEED"],
+                                "acceleration": p["ACCEL"],
+                                "neg_error": round(row, 5)
+                            }
+                        )
+                    for row in pos_errors:
+                        writer.writerow(
+                            {
+                                "test_name" : TEST_NAME,
+                                "axis": test_axis,
+                                "current": p["CURRENT"],
+                                "speed": p["SPEED"],
+                                "acceleration": p["ACCEL"],
+                                "pos_error": round(row, 5)
+                            }
+                        )
 
                     # create tableized output csv for neat presentation
                     # output for cycles completed
@@ -537,16 +595,6 @@ async def _main(is_simulating: bool) -> None:
 
                     # #output for average error
                     save_table([test_axis], "max_error", results_max_error, 3)
-
-        # create tableized output csv for neat presentation
-        # output for cycles completed
-        #save_table(test_axis_list, "cycles", results_cycles, 0)
-
-        # #output for average error
-        #save_table(test_axis_list, "error", results_error, 3)
-
-        # #output for average error
-        #save_table(test_axis_list, "max_error", results_max_error, 3)
 
 
     except KeyboardInterrupt:
