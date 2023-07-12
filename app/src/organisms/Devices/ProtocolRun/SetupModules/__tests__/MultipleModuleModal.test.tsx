f5e81a74ffb2de7d7aa4a413b023ae1b5cf6da3f
Jethary <jethary@opentrons.com>
2023-07-12 14:51:49 -0400

    feat(app): multiple modules modal on device display
    closes RAUT-529

--- app/src/organisms/Devices/ProtocolRun/SetupModules/__tests__/MultipleModuleModal.test.tsx
+++ app/src/organisms/Devices/ProtocolRun/SetupModules/__tests__/MultipleModuleModal.test.tsx
@@ -2,8 +2,14 @@ import * as React from 'react'
 import { fireEvent } from '@testing-library/react'
 import { renderWithProviders } from '@opentrons/components'
 import { i18n } from '../../../../../i18n'
+import { getIsOnDevice } from '../../../../../redux/config'
 import { MultipleModulesModal } from '../MultipleModulesModal'
 
+jest.mock('../../../../../redux/config')
+
+const mockGetIsOnDevice = getIsOnDevice as jest.MockedFunction<
+  typeof getIsOnDevice
+>
 const render = (props: React.ComponentProps<typeof MultipleModulesModal>) => {
   return renderWithProviders(<MultipleModulesModal {...props} />, {
     i18nInstance: i18n,
@@ -14,6 +20,7 @@ describe('MultipleModulesModal', () => {
   let props: React.ComponentProps<typeof MultipleModulesModal>
   beforeEach(() => {
     props = { onCloseClick: jest.fn() }
+    mockGetIsOnDevice.mockReturnValue(false)
   })
 
   it('should render the correct header', () => {
@@ -46,5 +53,14 @@ describe('MultipleModulesModal', () => {
     const closeButton = getByRole('button', { name: 'close' })
     fireEvent.click(closeButton)
     expect(props.onCloseClick).toHaveBeenCalled()
+  })
+  it('should render the correct text and img for on device display', () => {
+    mockGetIsOnDevice.mockReturnValue(true)
+    const { getByText, getByRole } = render(props)
+    getByText(
+      'You can use multiples of most module types within a single Python protocol by connecting and loading the modules in a specific order. The robot will initialize the matching module attached to the lowest numbered port first, regardless of what deck slot it occupies.'
+    )
+    const img = getByRole('img')
+    expect(img.getAttribute('src')).toBe('multiple_modules_modal_odd.png')
   })
 })
