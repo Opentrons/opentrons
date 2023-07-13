3b743c0d5eac7964f0665716f9a7a7d1e001ed7c
Brian Arthur Cooper <brian@opentrons.com>
2023-07-13 14:29:04 -0400

    fix(labware-library): fix ssr hydration bug, module product links, nest lw links, armadillo clone (#13083)
    Fix bug where in the rehydration step of conditionally rendered react children would replace server
    rendered elements with the wrong client rendered elements. Fix product links to opentrons module
    pages in nav. Replace all broken nest labware productlinks . duplicate armadillo pcr plate
    definition in favor of opentrons branded and retire old def
    
    Closes RLAB-334, RLAB-330, RLAB-277, RLAB-230

--- labware-library/src/components/Nav/Breadcrumbs.tsx
+++ labware-library/src/components/Nav/Breadcrumbs.tsx
@@ -6,9 +6,16 @@ import { Link } from '../ui'
 
 import styles from './styles.css'
 
-export function Breadcrumbs(): JSX.Element {
+interface BreadcrumbsProps {
+  show?: boolean
+}
+export function Breadcrumbs(props: BreadcrumbsProps): JSX.Element {
+  const { show = true } = props
   return (
-    <div className={styles.breadcrumbs}>
+    <div
+      className={styles.breadcrumbs}
+      style={{ display: show ? 'block' : 'none' }}
+    >
       <div className={styles.breadcrumbs_contents}>
         <span className={styles.breadcrumbs_separator}>{' < '}</span>
         <Link to={getPublicPath()} className={styles.breadcrumbs_link}>
