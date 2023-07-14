dae22693b7e961c88ae6345740cd347af109782a
Laura Cox <laura@opentrons.com>
2023-07-14 17:36:02 +0300

    fixup update key func in pip config

--- shared-data/python/opentrons_shared_data/pipette/load_data.py
+++ shared-data/python/opentrons_shared_data/pipette/load_data.py
@@ -116,18 +116,20 @@ def load_liquid_model(
 
 
 def _change_to_camel_case(c: str) -> str:
+    # Tiny helper function to convert to camelCase.
     config_name = c.split("_")
     if len(config_name) == 1:
         return config_name[0]
-    return f"{config_name[0]}".join(s.capitalize() for s in config_name[1::])
+    return f"{config_name[0]}" + "".join(s.capitalize() for s in config_name[1::])
 
 
 def update_pipette_configuration(
     base_configurations: PipetteConfigurations,
     v1_configuration_changes: Dict[str, Any],
 ) -> PipetteConfigurations:
-    """Helper function to migration V1 Configs to the V2 format.
+    """Helper function to update 'V1' format configurations (left over from PipetteDict).
 
+    #TODO (lc 7-14-2023) Remove once the pipette config dict is eliminated.
     Given an input of v1 mutable configs, look up the equivalent keyed
     value of that configuration."""
     quirks_list = []
@@ -138,18 +140,23 @@ def update_pipette_configuration(
         if c == "quirks" and isinstance(v, dict):
             quirks_list.extend([b.name for b in v.values() if b.value])
         else:
-            new_names = _MAP_KEY_TO_V2[lookup_key]
-            top_name = new_names["top_level_name"]
-            nested_name = new_names["nested_name"]
-            if c == "tipLength":
-                # This is only a concern for OT-2 configs and I think we can
-                # be less smart about handling multiple tip types by updating
-                # all tips.
-                for k in dict_of_base_model[new_names["top_level_name"]].keys():
-                    dict_of_base_model[top_name][k][nested_name] = v
-            else:
-                # isinstances are needed for type checking.
-                dict_of_base_model[top_name][nested_name] = v
+            try:
+                dict_of_base_model[lookup_key] = v
+            except KeyError:
+                # The name is not the same format as previous so
+                # we need to look it up from the V2 key map
+                new_names = _MAP_KEY_TO_V2[lookup_key]
+                top_name = new_names["top_level_name"]
+                nested_name = new_names["nested_name"]
+                if c == "tipLength":
+                    # This is only a concern for OT-2 configs and I think we can
+                    # be less smart about handling multiple tip types by updating
+                    # all tips.
+                    for k in dict_of_base_model[new_names["top_level_name"]].keys():
+                        dict_of_base_model[top_name][k][nested_name] = v
+                else:
+                    # isinstances are needed for type checking.
+                    dict_of_base_model[top_name][nested_name] = v
     dict_of_base_model["quirks"] = list(
         set(dict_of_base_model["quirks"]) - set(quirks_list)
     )
