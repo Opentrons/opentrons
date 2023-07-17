ed3496a1a588a8db39f740b564e74a1d67029198
Brian Cooper <brian@opentrons.com>
2023-07-17 16:27:37 -0400

    refactor(components): add higher and lower-level deck abstraction for Flex configuration
    Create a RobotCoordinateSpace component that manages only scaling/reflection for the coordinate
    system anchored in the bottom left. Create DeckSlotLocation component to rener a specific deck slot
    location and bake path information into the component itself instead of deserialzing svgson. Create
    a BaseDeck asbstraction that handles the basic rendering of a deck with labware, modules, and deck
    labels

--- components/src/hardware-sim/BaseDeck/BaseDeck.stories.tsx
+++ components/src/hardware-sim/BaseDeck/BaseDeck.stories.tsx
@@ -5,7 +5,12 @@ import { FLEX_ROBOT_TYPE, LabwareDefinition2 } from '@opentrons/shared-data'
 
 import type { Meta, StoryObj } from '@storybook/react'
 import { BaseDeck as BaseDeckComponent } from './'
-import { HEATERSHAKER_MODULE_V1, MAGNETIC_BLOCK_V1, TEMPERATURE_MODULE_V2, THERMOCYCLER_MODULE_V2 } from '@opentrons/shared-data/js'
+import {
+  HEATERSHAKER_MODULE_V1,
+  MAGNETIC_BLOCK_V1,
+  TEMPERATURE_MODULE_V2,
+  THERMOCYCLER_MODULE_V2,
+} from '@opentrons/shared-data/js'
 
 const meta: Meta<React.ComponentProps<typeof BaseDeckComponent>> = {
   component: BaseDeckComponent,
@@ -16,12 +21,10 @@ export default meta
 type Story = StoryObj<React.ComponentProps<typeof BaseDeckComponent>>
 
 export const BaseDeck: Story = {
-  render: args => (
-    <BaseDeckComponent {...args} />
-  ),
+  render: args => <BaseDeckComponent {...args} />,
   args: {
     robotType: FLEX_ROBOT_TYPE,
-    trashSlotName: "A3",
+    trashSlotName: 'A3',
     labwareLocations: [
       {
         labwareLocation: { slotName: 'C2' },
@@ -37,7 +40,7 @@ export const BaseDeck: Story = {
         moduleLocation: { slotName: 'B1' },
         moduleModel: THERMOCYCLER_MODULE_V2,
         nestedLabwareDef: fixture_96_plate as LabwareDefinition2,
-        innerProps: { lidMotorState: 'open' }
+        innerProps: { lidMotorState: 'open' },
       },
       {
         moduleLocation: { slotName: 'D1' },
@@ -53,9 +56,7 @@ export const BaseDeck: Story = {
         moduleLocation: { slotName: 'D2' },
         moduleModel: MAGNETIC_BLOCK_V1,
         nestedLabwareDef: fixture_96_plate as LabwareDefinition2,
-      }
-    ]
+      },
+    ],
   },
 }
-
-
