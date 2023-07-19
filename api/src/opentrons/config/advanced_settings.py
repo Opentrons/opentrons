0452b8d3affdf8340744408c6cad9dc922af7f9f
Brian Cooper <brian@opentrons.com>
2023-07-19 11:47:33 -0400

    fix(api): add yocto to architectures that observe the disabledLogAggregation setting
    Add the advanced setting for disabled log aggregation for yocto system architecture.
    
    Closes RSS-289

--- api/src/opentrons/config/advanced_settings.py
+++ api/src/opentrons/config/advanced_settings.py
@@ -193,7 +193,7 @@ settings = [
     ),
 ]
 
-if ARCHITECTURE == SystemArchitecture.BUILDROOT:
+if ARCHITECTURE == SystemArchitecture.BUILDROOT or ARCHITECTURE == SystemArchitecture.YOCTO:
     settings.append(DisableLogIntegrationSettingDefinition())
 
 
