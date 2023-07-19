52bb75e625ed3d9211e871d6dc7febbe99f2432e
jbleon95 <jeremybenleon@gmail.com>
2023-07-19 13:09:28 -0400

    refactor to avoid repeated code

--- api/src/opentrons/protocol_engine/execution/equipment.py
+++ api/src/opentrons/protocol_engine/execution/equipment.py
@@ -388,16 +388,26 @@ class EquipmentHandler:
             The ID of the labware offset that will apply,
             or None if no labware offset will apply.
         """
-        if isinstance(labware_location, DeckSlotLocation):
-            offset = self._state_store.labware.find_applicable_labware_offset(
-                definition_uri=labware_definition_uri,
-                location=LabwareOffsetLocation(
-                    slotName=labware_location.slotName,
-                    moduleModel=None,
-                ),
-            )
-            return self._get_id_from_offset(offset)
+        labware_offset_location = (
+            self._get_labware_offset_location_from_labware_location(labware_location)
+        )
+        if labware_offset_location is None:
+            # No offset for off-deck location.
+            # Returning None instead of raising an exception allows loading a labware
+            # with 'offDeck' as valid location.
+            # Also allows using `moveLabware` with 'offDeck' location.
+            return None
+        offset = self._state_store.labware.find_applicable_labware_offset(
+            definition_uri=labware_definition_uri,
+            location=labware_offset_location,
+        )
+        return self._get_id_from_offset(offset)
 
+    def _get_labware_offset_location_from_labware_location(
+        self, labware_location: LabwareLocation
+    ) -> Optional[LabwareOffsetLocation]:
+        if isinstance(labware_location, DeckSlotLocation):
+            return LabwareOffsetLocation(slotName=labware_location.slotName)
         elif isinstance(labware_location, ModuleLocation):
             module_id = labware_location.moduleId
             # Allow ModuleNotLoadedError to propagate.
@@ -431,15 +441,7 @@ class EquipmentHandler:
                 module_id=module_id
             )
             slot_name = module_location.slotName
-            offset = self._state_store.labware.find_applicable_labware_offset(
-                definition_uri=labware_definition_uri,
-                location=LabwareOffsetLocation(
-                    slotName=slot_name,
-                    moduleModel=module_model,
-                ),
-            )
-            return self._get_id_from_offset(offset)
-
+            return LabwareOffsetLocation(slotName=slot_name, moduleModel=module_model)
         elif isinstance(labware_location, OnLabwareLocation):
             parent_labware_id = labware_location.labwareId
             parent_labware_uri = self._state_store.labware.get_definition_uri(
@@ -449,42 +451,18 @@ class EquipmentHandler:
             base_location = self._state_store.labware.get_parent_location(
                 parent_labware_id
             )
-
-            if isinstance(base_location, ModuleLocation):
-                module_id = base_location.moduleId
-                module_model = self._state_store.modules.get_requested_model(
-                    module_id=module_id
-                )
-                assert module_model is not None, (
-                    "Can't find offsets for labware"
-                    " that are loaded on modules"
-                    " that were loaded with ProtocolEngine.use_attached_modules()."
-                )
-                module_location = self._state_store.modules.get_location(
-                    module_id=module_id
-                )
-                slot_name = module_location.slotName
-            elif isinstance(base_location, DeckSlotLocation):
-                slot_name = base_location.slotName
-                module_model = None
-            else:  # No offset for labware sitting on labware off-deck
-                return None
-
-            offset = self._state_store.labware.find_applicable_labware_offset(
-                definition_uri=labware_definition_uri,
-                location=LabwareOffsetLocation(
-                    slotName=slot_name,
-                    moduleModel=module_model,
-                    definitionUri=parent_labware_uri,
-                ),
+            base_labware_offset_location = (
+                self._get_labware_offset_location_from_labware_location(base_location)
             )
-            return self._get_id_from_offset(offset)
-
-        else:
-            # No offset for off-deck location.
-            # Returning None instead of raising an exception allows loading a labware
-            # with 'offDeck' as valid location.
-            # Also allows using `moveLabware` with 'offDeck' location.
+            if base_labware_offset_location is None:
+                # No offset for labware sitting on labware off-deck
+                return None
+            return LabwareOffsetLocation(
+                slotName=base_labware_offset_location.slotName,
+                moduleModel=base_labware_offset_location.moduleModel,
+                definitionUri=parent_labware_uri,
+            )
+        else:  # Off deck
             return None
 
     @staticmethod
