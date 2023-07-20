b7a5b815425f187910e54ed27d0a9dc862be5714
andySigler <andrewsigler1@gmail.com>
2023-07-19 23:40:40 -0400

    adds firmware-check script to integration test

--- hardware-testing/hardware_testing/production_qc/firmware_check.py
+++ hardware-testing/hardware_testing/production_qc/firmware_check.py
@@ -4,16 +4,24 @@ from asyncio import run
 from hardware_testing.opentrons_api import helpers_ot3
 
 
-async def _main() -> None:
-    api = await helpers_ot3.build_async_ot3_hardware_api(is_simulating=False)
+async def _main(simulate: bool) -> None:
+    api = await helpers_ot3.build_async_ot3_hardware_api(is_simulating=simulate)
     while True:
-        input("\n\npress ENTER to check firmware:")
+        if not api.is_simulator:
+            input("\n\npress ENTER to check firmware:")
         await api.cache_instruments()
         for subsys, state in api.attached_subsystems.items():
             print(f" - v{state.current_fw_version}: {subsys.name}")
         await helpers_ot3.update_firmware(api)
         print("done")
+        if api.is_simulator:
+            break
 
 
 if __name__ == "__main__":
-    run(_main())
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--simulate", action="store_true")
+    args = parser.parse_args()
+    run(_main(args.simulate))
