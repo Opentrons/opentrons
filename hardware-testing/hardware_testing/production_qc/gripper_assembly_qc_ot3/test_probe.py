e40a53f426459f2856d763306c0f5e462a573f1a
Andy Sigler <andrewsigler1@gmail.com>
2023-07-25 16:02:06 -0400

    adds full probing sequence

--- hardware-testing/hardware_testing/production_qc/gripper_assembly_qc_ot3/test_probe.py
+++ hardware-testing/hardware_testing/production_qc/gripper_assembly_qc_ot3/test_probe.py
@@ -7,6 +7,10 @@ from opentrons_hardware.sensors import sensor_driver, sensor_types
 
 from opentrons.config.types import CapacitivePassSettings
 from opentrons.hardware_control.ot3api import OT3API
+from opentrons.hardware_control.ot3_calibration import (
+    calibrate_gripper,
+    calibrate_gripper_jaw,
+)
 from opentrons.hardware_control.backends.ot3controller import OT3Controller
 from opentrons.hardware_control.backends.ot3utils import sensor_id_for_instrument
 
@@ -26,6 +30,8 @@ from hardware_testing.opentrons_api.types import Axis, OT3Mount, Point, GripperP
 TEST_SLOT = 5
 PROBE_PREP_HEIGHT_MM = 5
 PROBE_POS_OFFSET = Point(13, 13, 0)
+JAW_ALIGNMENT_MM_X = 0.1
+JAW_ALIGNMENT_MM_Z = 0.1
 
 
 def _get_test_tag(probe: GripperProbe) -> str:
@@ -37,7 +43,16 @@ def build_csv_lines() -> List[Union[CSVLine, CSVLineRepeating]]:
     lines: List[Union[CSVLine, CSVLineRepeating]] = list()
     for p in GripperProbe:
         tag = _get_test_tag(p)
-        lines.append(CSVLine(tag, [float, float, float, float, float, CSVResult]))
+        lines.append(CSVLine(f"{tag}-open-air-pf", [float]))
+        lines.append(CSVLine(f"{tag}-probe-pf", [float]))
+        lines.append(CSVLine(f"{tag}-deck-pf", [float]))
+        lines.append(CSVLine(f"{tag}-result", [CSVResult]))
+    for p in GripperProbe:
+        lines.append(CSVLine(f"jaw-probe-{p.name.lower()}-xyz", [float, float, float]))
+    for axis in ["x", "z"]:
+        lines.append(CSVLine(f"jaw-alignment-{axis}-spec", [float]))
+        lines.append(CSVLine(f"jaw-alignment-{axis}-actual", [float]))
+        lines.append(CSVLine(f"jaw-alignment-{axis}-result", [CSVResult]))
     return lines
 
 
@@ -92,21 +107,9 @@ async def run(api: OT3API, report: CSVReport, section: str) -> None:
     hover_pos, probe_pos = _get_hover_and_probe_pos(api)
     z_limit = probe_pos.z - pass_settings.max_overrun_distance_mm
 
-    async def _save_result(
-        tag: str,
-        no_probe: float,
-        probe: float,
-        found: float,
-        z_limit: float,
-        deck: float,
-        valid: bool,
-    ) -> None:
-        result = CSVResult.from_bool(valid)
-        report(section, tag, [no_probe, probe, found, z_limit, deck, result])
-
     for probe in GripperProbe:
         sensor_id = sensor_id_for_instrument(GripperProbe.to_type(probe))
-        ui.print_header(f"Probe: {probe}")
+        ui.print_header(f"Capacitive: {probe.name}")
         cap_sensor = sensor_types.CapacitiveSensor.build(sensor_id, NodeId.gripper)
         print("homing and grip...")
         await api.home([z_ax, g_ax])
@@ -118,19 +121,19 @@ async def run(api: OT3API, report: CSVReport, section: str) -> None:
         await api.grip(15)
 
         # take reading for baseline (1)
-        no_probe_baseline = 0.0
+        open_air_pf = 0.0
         if not api.is_simulator:
-            no_probe_baseline = await _read_from_sensor(api, s_driver, cap_sensor, 10)
-        print(f"baseline without probe: {no_probe_baseline}")
+            open_air_pf = await _read_from_sensor(api, s_driver, cap_sensor, 10)
+        print(f"baseline without probe: {open_air_pf}")
 
         # take reading for baseline with pin attached (2)
         if not api.is_simulator:
             ui.get_user_ready(f"place calibration pin in the {probe.name}")
         # add pin to update critical point
-        probe_baseline = 0.0
+        probe_pf = 0.0
         if not api.is_simulator:
-            probe_baseline = await _read_from_sensor(api, s_driver, cap_sensor, 10)
-        print(f"baseline with probe: {probe_baseline}")
+            probe_pf = await _read_from_sensor(api, s_driver, cap_sensor, 10)
+        print(f"baseline with probe: {probe_pf}")
 
         # begins probing
         if not api.is_simulator:
@@ -159,29 +162,67 @@ async def run(api: OT3API, report: CSVReport, section: str) -> None:
 
         # check against max overrun
         valid_height = found_pos >= z_limit
-        reading_on_deck = 0.0
+        deck_pf = 0.0
         if valid_height:
             if not api.is_simulator:
                 ui.get_user_ready("about to press into the deck")
             await api.move_to(mount, probe_pos._replace(z=found_pos))
             if not api.is_simulator:
-                reading_on_deck = await _read_from_sensor(api, s_driver, cap_sensor, 10)
-        print(f"Reading on deck: {reading_on_deck}")
+                deck_pf = await _read_from_sensor(api, s_driver, cap_sensor, 10)
+        print(f"Reading on deck: {deck_pf}")
 
-        result = valid_height and reading_on_deck > probe_baseline
+        result = deck_pf > probe_pf
 
-        await _save_result(
-            _get_test_tag(probe),
-            no_probe_baseline,
-            probe_baseline,
-            found_pos,
-            z_limit,
-            reading_on_deck,
-            result,
-        )
+        _tag = _get_test_tag(probe)
+        report(section, f"{_tag}-open-air-pf", [open_air_pf])
+        report(section, f"{_tag}-probe-pf", [probe_pf])
+        report(section, f"{_tag}-deck-pf", [deck_pf])
+        report(section, f"{_tag}-result", [CSVResult.from_bool(result)])
         await api.home_z()
         await api.ungrip()
 
         if not api.is_simulator:
             ui.get_user_ready(f"remove calibration pin in the {probe.name}")
         api.remove_gripper_probe()
+
+    async def _calibrate_jaw(_p: GripperProbe) -> Point:
+        ui.print_header(f"Probe Deck: {_p.name}")
+        await api.retract(OT3Mount.GRIPPER)
+        if not api.is_simulator:
+            ui.get_user_ready(f"attach probe to {_p.name}")
+        if api.is_simulator:
+            ret = Point(x=0, y=0, z=0)
+        else:
+            ret = await calibrate_gripper_jaw(api, _p)
+        await api.retract(OT3Mount.GRIPPER)
+        if not api.is_simulator:
+            ui.get_user_ready(f"remove probe from {_p.name}")
+        report(section, f"jaw-probe-{_p.name.lower()}-xyz", [ret.x, ret.y, ret.z])
+        return ret
+
+    _offsets = {probe: await _calibrate_jaw(probe) for probe in GripperProbe}
+    _diff_x = abs(_offsets[GripperProbe.FRONT].x - _offsets[GripperProbe.REAR].x)
+    _diff_z = abs(_offsets[GripperProbe.FRONT].z - _offsets[GripperProbe.REAR].z)
+    _offset = await calibrate_gripper(
+        api,
+        offset_front=_offsets[GripperProbe.FRONT],
+        offset_rear=_offsets[GripperProbe.REAR],
+    )
+    print(f"front offset: {_offsets[GripperProbe.FRONT]}")
+    print(f"rear offset: {_offsets[GripperProbe.REAR]}")
+    print(f"average offset: {_offset}")
+    report(section, "jaw-alignment-x-spec", [JAW_ALIGNMENT_MM_X])
+    report(section, "jaw-alignment-x-actual", [_diff_x])
+    report(
+        section,
+        "jaw-alignment-x-result",
+        [CSVResult.from_bool(_diff_x <= JAW_ALIGNMENT_MM_X)],
+    )
+    report(section, "jaw-alignment-z-spec", [JAW_ALIGNMENT_MM_Z])
+    report(section, "jaw-alignment-z-actual", [_diff_z])
+    report(
+        section,
+        "jaw-alignment-z-result",
+        [CSVResult.from_bool(_diff_z <= JAW_ALIGNMENT_MM_Z)],
+    )
+    await api.retract(OT3Mount.GRIPPER)
