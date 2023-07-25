759181b71cb50e5613cbe000bcd52851d03c548b
andySigler <andrewsigler1@gmail.com>
2023-07-25 10:04:24 -0400

    adds pipette SN to printout during firmware check

--- hardware-testing/hardware_testing/production_qc/firmware_check.py
+++ hardware-testing/hardware_testing/production_qc/firmware_check.py
@@ -2,19 +2,42 @@
 from asyncio import run
 from typing import List
 
+from opentrons.hardware_control.ot3api import OT3API
+
 from hardware_testing.opentrons_api import helpers_ot3
+from hardware_testing.opentrons_api.types import OT3Mount
 
 from opentrons.hardware_control.types import SubSystem
+
+
+def _get_instrument_serial_number(api: OT3API, subsystem: SubSystem) -> str:
+    if subsystem == SubSystem.pipette_right:
+        _pip = api.hardware_pipettes[OT3Mount.RIGHT.to_mount()]
+        _pip_id = helpers_ot3.get_pipette_serial_ot3(_pip)
+        _id = f" ({_pip_id})"
+    elif subsystem == SubSystem.pipette_left:
+        _pip = api.hardware_pipettes[OT3Mount.LEFT.to_mount()]
+        _pip_id = helpers_ot3.get_pipette_serial_ot3(_pip)
+        _id = f" ({_pip_id})"
+    elif subsystem == SubSystem.gripper:
+        gripper = api.attached_gripper
+        assert gripper
+        gripper_id = str(gripper["gripper_id"])
+        _id = f" ({gripper_id})"
+    else:
+        _id = ""
+    return _id
 
 
 async def _main(simulate: bool, subsystems: List[SubSystem]) -> None:
     api = await helpers_ot3.build_async_ot3_hardware_api(is_simulating=simulate)
     while True:
-        if not api.is_simulator:
-            input("\n\npress ENTER to check firmware:")
         await api.cache_instruments()
         for subsys, state in api.attached_subsystems.items():
-            print(f" - v{state.current_fw_version}: {subsys.name}")
+            _id = _get_instrument_serial_number(api, subsys)
+            print(f" - v{state.current_fw_version}: {subsys.name}{_id}")
+        if not api.is_simulator:
+            input("\n\npress ENTER to check/update firmware:")
         await helpers_ot3.update_firmware(api, subsystems=subsystems)
         print("done")
         if api.is_simulator:
