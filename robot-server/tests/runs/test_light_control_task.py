8e5d8b1750c3426bdef72bb0df8700061561bc4d
Frank Sinapi <frank.sinapi@opentrons.com>
2023-07-26 14:35:38 -0400

    Fixed tests and added some more tests

--- robot-server/tests/runs/test_light_control_task.py
+++ robot-server/tests/runs/test_light_control_task.py
@@ -50,7 +50,7 @@ async def test_get_current_status(
     """Test LightController.get_current_status."""
     decoy.when(engine_store.current_run_id).then_return("fake_id" if active else None)
     decoy.when(engine_store.engine.state_view.commands.get_status()).then_return(status)
-    decoy.when(hardware_api.get_estop_state()).then_return()
+    decoy.when(hardware_api.get_estop_state()).then_return(estop)
 
     expected = Status(
         estop_status=estop,
@@ -104,4 +104,56 @@ async def test_light_controller_update(
 
     decoy.verify(
         await hardware_api.set_status_bar_state(state=expected), times=call_count
+    )
+
+
+async def test_provide_engine_store(
+    decoy: Decoy, hardware_api: HardwareControlAPI, engine_store: EngineStore
+) -> None:
+    """Test providing an engine store after initialization."""
+    subject = LightController(api=hardware_api, engine_store=None)
+    decoy.when(hardware_api.get_estop_state()).then_return(EstopState.DISENGAGED)
+    assert subject.get_current_status() == Status(
+        estop_status=EstopState.DISENGAGED,
+        engine_status=None,
+    )
+
+    decoy.when(engine_store.current_run_id).then_return("fake_id")
+    decoy.when(engine_store.engine.state_view.commands.get_status()).then_return(
+        EngineStatus.RUNNING
+    )
+
+    subject.update_engine_store(engine_store=engine_store)
+    assert subject.get_current_status() == Status(
+        estop_status=EstopState.DISENGAGED,
+        engine_status=EngineStatus.RUNNING,
+    )
+
+
+async def test_estop_precedence(
+    decoy: Decoy,
+    hardware_api: HardwareControlAPI,
+    subject: LightController,
+) -> None:
+    """Test that the estop is prioritized."""
+    # Software error
+    await subject.update(
+        prev_status=Status(EstopState.PHYSICALLY_ENGAGED, None),
+        new_status=Status(EstopState.PHYSICALLY_ENGAGED, EngineStatus.RUNNING),
+    )
+    # Running
+    await subject.update(
+        prev_status=Status(EstopState.PHYSICALLY_ENGAGED, EngineStatus.RUNNING),
+        new_status=Status(EstopState.LOGICALLY_ENGAGED, EngineStatus.RUNNING),
+    )
+    # Software error
+    await subject.update(
+        prev_status=Status(EstopState.LOGICALLY_ENGAGED, EngineStatus.RUNNING),
+        new_status=Status(EstopState.PHYSICALLY_ENGAGED, EngineStatus.IDLE),
+    )
+
+    decoy.verify(
+        await hardware_api.set_status_bar_state(state=StatusBarState.SOFTWARE_ERROR),
+        await hardware_api.set_status_bar_state(state=StatusBarState.RUNNING),
+        await hardware_api.set_status_bar_state(state=StatusBarState.SOFTWARE_ERROR),
     )
