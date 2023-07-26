108594beb70fb82260bd0bd207d74cd7906e3106
Frank Sinapi <frank.sinapi@opentrons.com>
2023-07-26 12:22:44 -0400

    set up light control task to accept no engine store & base status on both estop and engine store

--- robot-server/tests/runs/test_light_control_task.py
+++ robot-server/tests/runs/test_light_control_task.py
@@ -5,10 +5,10 @@ from typing import Optional
 from decoy import Decoy
 
 from opentrons.hardware_control import HardwareControlAPI
-from opentrons.hardware_control.types import StatusBarState
+from opentrons.hardware_control.types import StatusBarState, EstopState
 from opentrons.protocol_engine.types import EngineStatus
 from robot_server.runs.engine_store import EngineStore
-from robot_server.runs.light_control_task import LightController
+from robot_server.runs.light_control_task import LightController, Status
 
 
 @pytest.fixture
@@ -26,12 +26,16 @@ def subject(
 
 
 @pytest.mark.parametrize(
-    ["active", "status"],
     [
-        [False, EngineStatus.IDLE],
-        [True, EngineStatus.IDLE],
-        [True, EngineStatus.RUNNING],
-        [False, EngineStatus.FAILED],
+        "active",
+        "status",
+        "estop",
+    ],
+    [
+        [False, EngineStatus.IDLE, EstopState.DISENGAGED],
+        [True, EngineStatus.IDLE, EstopState.PHYSICALLY_ENGAGED],
+        [True, EngineStatus.RUNNING, EstopState.LOGICALLY_ENGAGED],
+        [False, EngineStatus.FAILED, EstopState.NOT_PRESENT],
     ],
 )
 async def test_get_current_status(
@@ -40,12 +44,18 @@ async def test_get_current_status(
     subject: LightController,
     active: bool,
     status: EngineStatus,
+    estop: EstopState,
+    hardware_api: HardwareControlAPI,
 ) -> None:
     """Test LightController.get_current_status."""
     decoy.when(engine_store.current_run_id).then_return("fake_id" if active else None)
     decoy.when(engine_store.engine.state_view.commands.get_status()).then_return(status)
+    decoy.when(hardware_api.get_estop_state()).then_return()
 
-    expected = status if active else None
+    expected = Status(
+        estop_status=estop,
+        engine_status=status if active else None,
+    )
 
     assert subject.get_current_status() == expected
 
@@ -83,7 +93,12 @@ async def test_light_controller_update(
     Verifies that the status bar is NOT updated if the state is the same, and
     checks that state mapping is correct.
     """
-    await subject.update(prev_status=prev_state, new_status=new_state)
+    await subject.update(
+        prev_status=Status(
+            estop_status=EstopState.DISENGAGED, engine_status=prev_state
+        ),
+        new_status=Status(estop_status=EstopState.DISENGAGED, engine_status=new_state),
+    )
 
     call_count = 0 if prev_state == new_state else 1
 
