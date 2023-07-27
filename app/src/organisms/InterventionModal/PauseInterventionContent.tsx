a536c53d892460b71ff363f402ce175d6c9de48a
Brent Hagen <brent.hagen@opentrons.com>
2023-07-27 10:46:18 -0400

    feat(app,components): add intervention modal to ODD (#13135)
    makes necessary changes to the intervention modal and base modal components to display
    different layout/styling on ODD and desktop. adds the intervention modal to the RunningProtocol ODD
    page.
    
    re RLAB-323, closes RLAB-321, RLAB-324, RLAB-325

--- app/src/organisms/InterventionModal/PauseInterventionContent.tsx
+++ app/src/organisms/InterventionModal/PauseInterventionContent.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
+import { css } from 'styled-components'
 
 import {
   ALIGN_CENTER,
+  BORDERS,
   COLORS,
   DIRECTION_COLUMN,
   Flex,
-  Icon,
+  RESPONSIVENESS,
   SPACING,
+  TYPOGRAPHY,
   useInterval,
 } from '@opentrons/components'
 
@@ -15,6 +18,15 @@ import { StyledText } from '../../atoms/text'
 import { EMPTY_TIMESTAMP } from '../Devices/constants'
 import { formatInterval } from '../RunTimeControl/utils'
 import { InterventionCommandMessage } from './InterventionCommandMessage'
+
+const PAUSE_INTERVENTION_CONTENT_STYLE = css`
+  flex-direction: ${DIRECTION_COLUMN};
+  grid-gap: ${SPACING.spacing12};
+  width: 100%;
+  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+    grid-gap: ${SPACING.spacing20};
+  }
+`
 
 export interface PauseContentProps {
   startedAt: string | null
@@ -26,14 +38,42 @@ export function PauseInterventionContent({
   message,
 }: PauseContentProps): JSX.Element {
   return (
-    <Flex flexDirection={DIRECTION_COLUMN} gridGap="0.75rem">
+    <Flex css={PAUSE_INTERVENTION_CONTENT_STYLE}>
       <PauseHeader startedAt={startedAt} />
-      <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing4}>
-        <InterventionCommandMessage commandMessage={message} />
-      </Flex>
+      <InterventionCommandMessage commandMessage={message} />
     </Flex>
   )
 }
+
+const PAUSE_HEADER_STYLE = css`
+  align-items: ${ALIGN_CENTER};
+  background-color: ${COLORS.fundamentalsBackground};
+  border-radius: ${BORDERS.radiusSoftCorners};
+  grid-gap: ${SPACING.spacing6};
+  padding: ${SPACING.spacing16};
+  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+    align-self: ${ALIGN_CENTER};
+    background-color: ${COLORS.light1};
+    border-radius: ${BORDERS.borderRadiusSize3};
+    grid-gap: ${SPACING.spacing32};
+    padding: ${SPACING.spacing24};
+    min-width: 36.5rem;
+  }
+`
+
+const PAUSE_TEXT_STYLE = css`
+  ${TYPOGRAPHY.h1Default}
+  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+    ${TYPOGRAPHY.level4HeaderSemiBold}
+  }
+`
+
+const PAUSE_TIME_STYLE = css`
+  ${TYPOGRAPHY.h1Default}
+  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+    ${TYPOGRAPHY.level1Header}
+  }
+`
 
 interface PauseHeaderProps {
   startedAt: string | null
@@ -48,16 +88,11 @@ function PauseHeader({ startedAt }: PauseHeaderProps): JSX.Element {
     startedAt != null ? formatInterval(startedAt, now) : EMPTY_TIMESTAMP
 
   return (
-    <Flex alignItems={ALIGN_CENTER} gridGap="0.75rem">
-      <Icon
-        name="pause-circle"
-        size={SPACING.spacing32}
-        flex="0 0 auto"
-        color={COLORS.darkGreyEnabled}
-      />
-      <StyledText as="h1">
-        {i18n.format(t('paused_for'), 'capitalize')} {runTime}
+    <Flex css={PAUSE_HEADER_STYLE}>
+      <StyledText css={PAUSE_TEXT_STYLE}>
+        {i18n.format(t('paused_for'), 'capitalize')}
       </StyledText>
+      <StyledText css={PAUSE_TIME_STYLE}>{runTime}</StyledText>
     </Flex>
   )
 }
