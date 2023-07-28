d31d307484414b846d573d699c8cc4504aed9863
koji <koji.kanao@opentrons.com>
2023-07-27 20:37:27 -0400

    add test for useEstopContext

---
+++ app/src/organisms/EmergencyStop/__tests__/hooks.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+import { renderHook } from '@testing-library/react-hooks'
+
+import { useEstopContext } from '../hooks'
+
+describe('useEstopContext', () => {
+  it('should return the correct context values', () => {
+    const { result } = renderHook(() => useEstopContext())
+    expect(result.current.isDismissedModal).toBe(false)
+    expect(typeof result.current.setIsDismissedModal).toBe('function')
+  })
+})
