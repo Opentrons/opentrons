2921ff4f552b332b1b6de9d694502ea7fafe786c
Derek Maggio <derek.maggio@opentrons.com>
2023-07-28 13:28:29 -0700

    Upload fixed protocol

--- app-testing/files/protocols/py/OT3_P100_96_HS_TM_2_15_Quick_Zymo_RNA_Bacteria.py
+++ app-testing/files/protocols/py/OT3_P100_96_HS_TM_2_15_Quick_Zymo_RNA_Bacteria.py
@@ -31,7 +31,6 @@ requirements = {
 
 """
 
-
 HS_SLOT = 1
 dry_run = False
 USE_GRIPPER = True
@@ -44,36 +43,35 @@ def run(ctx):
     (note that this is the recommended configuration)
     """
     # Protocol Parameters
-    MAG_PLATE_SLOT = ctx.load_module("heaterShakerModuleV1", "1")
     deepwell_type = "nest_96_wellplate_2ml_deep"
     if not dry_run:
         settling_time = 3
     else:
         settling_time = 0.25
     # Volumes Defined
-    lysis_vol = 200
-    binding_buffer_vol = 430  # Beads+Binding
-    wash_vol = stop_vol = 500
+    lysis_vol = 100
+    binding_buffer_vol = 215  # Beads+Binding
+    wash_vol = stop_vol = 250
     dnase_vol = 50
-    elution_vol = 110
-    starting_vol = 500  # This is sample volume (300 in shield) + lysis volume
+    elution_vol = 50
+    starting_vol = 250  # This is sample volume (300 in shield) + lysis volume
 
     h_s = ctx.load_module("heaterShakerModuleV1", HS_SLOT)
     sample_plate = h_s.load_labware("opentrons_96_deep_well_adapter_nest_wellplate_2ml_deep")
     samples_m = sample_plate.wells()[0]
     h_s.close_labware_latch()
+    MAG_PLATE_SLOT = ctx.load_module("magneticBlockV1", "4")
 
     tempdeck = ctx.load_module("Temperature Module Gen2", "3")
     # Keep elution warm during protocol
     elutionplate = tempdeck.load_labware("opentrons_96_pcr_adapter_armadillo_wellplate_200ul")
 
     # Load Reservoir Plates
-    lysis_reservoir = ctx.load_labware(deepwell_type, "2")  # deleted after use- replaced (by gripper) with wash2 res
+    wash2_reservoir = lysis_reservoir = ctx.load_labware(
+        deepwell_type, "2"
+    )  # deleted after use- replaced (by gripper) with wash2 res
     bind_reservoir = ctx.load_labware(deepwell_type, "6")
     wash1_reservoir = ctx.load_labware(deepwell_type, "5")
-    wash2_reservoir = ctx.load_labware(
-        deepwell_type, "4"
-    )  # loaded on magplate- move to lysis location after lysis is used
     wash3_reservoir = wash4_reservoir = wash5_reservoir = ctx.load_labware(deepwell_type, "7")
     dnase_reservoir = ctx.load_labware("armadillo_96_wellplate_200ul_pcr_full_skirt", "9")
     stop_reservoir = ctx.load_labware(deepwell_type, "8")
@@ -100,16 +98,15 @@ def run(ctx):
     pip.flow_rate.blow_out = 300
 
     def grip_offset(action, item, slot=None):
-        """Grip offset."""
         from opentrons.types import Point
 
         # EDIT these values
         # NOTE: we are still testing to determine our software's defaults
         #       but we also expect users will want to edit these
         _pick_up_offsets = {
-            "deck": Point(x=-0.2),
-            "mag-plate": Point(x=-0.5, y=3),
-            "heater-shaker": Point(x=-2.5, y=1.5),
+            "deck": Point(),
+            "mag-plate": Point(),
+            "heater-shaker": Point(),
             "temp-module": Point(),
             "thermo-cycler": Point(),
         }
@@ -117,21 +114,21 @@ def run(ctx):
         # NOTE: we are still testing to determine our software's defaults
         #       but we also expect users will want to edit these
         _drop_offsets = {
-            "deck": Point(z=-2),
-            "mag-plate": Point(y=3, z=9.5),
-            "heater-shaker": Point(x=-2.5, y=2, z=-2),
-            "temp-module": Point(z=-2),
-            "thermo-cycler": Point(),
+            "deck": Point(z=0.5),
+            "mag-plate": Point(z=0.5),
+            "heater-shaker": Point(z=0.5),
+            "temp-module": Point(z=0.5),
+            "thermo-cycler": Point(z=0.5),
         }
         # do NOT edit these values
         # NOTE: these values will eventually be in our software
         #       and will not need to be inside a protocol
         _hw_offsets = {
             "deck": Point(),
-            "mag-plate": Point(z=2.5),
-            "heater-shaker": Point(z=3),
-            "temp-module": Point(),
-            "thermo-cycler": Point(),
+            "mag-plate": Point(),
+            "heater-shaker": Point(z=2.5),
+            "temp-module": Point(z=5),
+            "thermo-cycler": Point(z=2.5),
         }
         # make sure arguments are correct
         action_options = ["pick-up", "drop"]
@@ -269,18 +266,18 @@ def run(ctx):
         blink()
         ctx.pause('Please remove lysis reservoir (slot 2 or D2) from the deck.')
         del ctx.deck['2']
-        """
+        
 
         ctx._core._engine_client.move_labware(
             labware_id=lysis_reservoir._core.labware_id,
             new_location="offDeck",
             strategy="manualMoveWithPause",
-            use_pick_up_location_lpc_offset=False,
+            use_pick_up_location_lpc_offset=False, 
             use_drop_location_lpc_offset=False,
             pick_up_offset=None,
-            drop_offset=None,
+            drop_offset=None
         )
-        """
+        
         **********
 
 
@@ -290,17 +287,18 @@ def run(ctx):
         
         ***********
 
-        """
+        
 
-        # Transfer wash2 res from magnet to deck slot
+
+        #Transfer wash2 res from magnet to deck slot
         ctx.move_labware(
-            wash2_reservoir,
-            2,
+            wash2_reservoir, 
+            2, 
             use_gripper=USE_GRIPPER,
-            pick_up_offset=grip_offset("pick-up", "mag-plate"),
-            drop_offset=grip_offset("drop", "deck"),
+            pick_up_offset=grip_offset("pick-up","mag-plate"),
+            drop_offset=grip_offset("drop","deck")
         )
-
+        """
         ctx.delay(
             minutes=1 if not dry_run else 0.25,
             msg="Please wait 2 minutes while the lysis buffer mixes with the sample.",
@@ -487,28 +485,40 @@ def run(ctx):
         pip.dispense(vol, source)
         pip.return_tip()
 
+        h_s.open_labware_latch()
+        # Transfer plate to magnet
+        ctx.move_labware(
+            sample_plate,
+            h_s,
+            use_gripper=USE_GRIPPER,
+            pick_up_offset=grip_offset("pick-up", "mag-plate"),
+            drop_offset=grip_offset("drop", "heater-shaker", slot=HS_SLOT),
+        )
+        h_s.close_labware_latch()
+
     """
     Here is where you can call the methods defined above to fit your specific
     protocol. The normal sequence is:
     """
     # Start Protocol
-    lysis(lysis_vol, lysis_res)
-    bind(binding_buffer_vol, bind_res)
-    wash(wash_vol, wash1)
-    if not dry_run:
-        wash(wash_vol, wash2)
-        wash(wash_vol, wash3)
-    # dnase1 treatment
-    dnase(dnase_vol, dnase_res)
-    stop_reaction(stop_vol, stop_res)
-    # Resume washes
-    if not dry_run:
-        wash(wash_vol, wash4)
-        wash(wash_vol, wash5)
-        tempdeck.set_temperature(55)
-        drybeads = 9  # Number of minutes you want to dry for
-    else:
-        drybeads = 0.5
-    for beaddry in np.arange(drybeads, 0, -0.5):
-        ctx.delay(minutes=0.5, msg="There are " + str(beaddry) + " minutes left in the drying step.")
-    elute(elution_vol, elution_res)
+    for x in range(2):
+        lysis(lysis_vol, lysis_res)
+        bind(binding_buffer_vol, bind_res)
+        wash(wash_vol, wash1)
+        if not dry_run:
+            wash(wash_vol, wash2)
+            wash(wash_vol, wash3)
+        # dnase1 treatment
+        dnase(dnase_vol, dnase_res)
+        stop_reaction(stop_vol, stop_res)
+        # Resume washes
+        if not dry_run:
+            wash(wash_vol, wash4)
+            wash(wash_vol, wash5)
+            tempdeck.set_temperature(55)
+            drybeads = 9  # Number of minutes you want to dry for
+        else:
+            drybeads = 0.5
+        for beaddry in np.arange(drybeads, 0, -0.5):
+            ctx.delay(minutes=0.5, msg="There are " + str(beaddry) + " minutes left in the drying step.")
+        elute(elution_vol, elution_res)
