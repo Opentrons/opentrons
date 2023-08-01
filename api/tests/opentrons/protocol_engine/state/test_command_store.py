7bfba275aa86bc09f76e9983896e11a83fc78439
Max Marrone <max@opentrons.com>
2023-08-01 17:11:09 -0400

    Add tests for CommandStore.

--- api/tests/opentrons/protocol_engine/state/test_command_store.py
+++ api/tests/opentrons/protocol_engine/state/test_command_store.py
@@ -809,21 +809,43 @@ def test_timestamps_are_latched() -> None:
     assert subject.state.run_completed_at == stop_time_1
 
 
-def test_command_store_saves_unknown_finish_error() -> None:
-    """It not store a ProtocolEngineError that comes in with the stop action."""
+def test_command_store_wraps_unknown_errors() -> None:
+    """Fatal errors that are unknown should be wrapped in EnumeratedErrors.
+
+    Fatal errors can come in through FinishActions and HardwareStoppedActions.
+    If these are not descendants of EnumeratedError already, they should be
+    wrapped in an EnumeratedError before being converted to an ErrorOccurrence.
+
+    The wrapping EnumeratedError should be an UnexpectedProtocolError for errors that happened
+    in the main part of the protocol run, or a PythonException for errors that happened elsewhere.
+    """
     subject = CommandStore(is_door_open=False, config=_make_config())
 
-    error_details = FinishErrorDetails(
-        error=RuntimeError("oh no"),
-        error_id="error-id",
-        created_at=datetime(year=2021, month=1, day=1),
+    subject.handle_action(
+        FinishAction(
+            error_details=FinishErrorDetails(
+                error=RuntimeError("oh no"),
+                error_id="error-id-1",
+                created_at=datetime(year=2021, month=1, day=1),
+            )
+        )
     )
-    subject.handle_action(FinishAction(error_details=error_details))
+
+    subject.handle_action(
+        HardwareStoppedAction(
+            completed_at=datetime(year=2022, month=2, day=2),
+            finish_error_details=FinishErrorDetails(
+                error=RuntimeError("yikes"),
+                error_id="error-id-2",
+                created_at=datetime(year=2023, month=3, day=3),
+            ),
+        )
+    )
 
     assert subject.state == CommandState(
         queue_status=QueueStatus.PAUSED,
         run_result=RunResult.FAILED,
-        run_completed_at=None,
+        run_completed_at=datetime(year=2022, month=2, day=2),
         is_door_blocking=False,
         running_command_id=None,
         all_command_ids=[],
@@ -831,24 +853,22 @@ def test_command_store_saves_unknown_finish_error() -> None:
         queued_setup_command_ids=OrderedSet(),
         commands_by_id=OrderedDict(),
         run_error=errors.ErrorOccurrence(
-            id="error-id",
+            id="error-id-1",
             createdAt=datetime(year=2021, month=1, day=1),
-            # this is wrapped into an UnexpectedProtocolError because it's not
-            # enumerated
+            # This is wrapped into an UnexpectedProtocolError because it's not
+            # enumerated, and it happened in the main part of the run.
             errorType="UnexpectedProtocolError",
-            # but it has the information about what created it
-            detail="oh no",
-            # Unknown errors use the default error code
+            # Unknown errors use the default error code.
             errorCode=ErrorCodes.GENERAL_ERROR.value.code,
-            # and they wrap
+            # And it has information about what created it.
+            detail="oh no",
             wrappedErrors=[
                 errors.ErrorOccurrence(
-                    id="error-id",
+                    id="error-id-1",
                     createdAt=datetime(year=2021, month=1, day=1),
                     errorType="PythonException",
                     detail="RuntimeError: oh no",
                     errorCode="4000",
-                    # and we get some fun extra info if this wraps a normal exception
                     errorInfo={
                         "class": "RuntimeError",
                         "args": "('oh no',)",
@@ -857,14 +877,28 @@ def test_command_store_saves_unknown_finish_error() -> None:
                 )
             ],
         ),
-        finish_error=None,
+        finish_error=errors.ErrorOccurrence(
+            id="error-id-2",
+            createdAt=datetime(year=2023, month=3, day=3),
+            # This is wrapped into a PythonException because it's not
+            # enumerated, and it happened during the post-run cleanup steps.
+            errorType="PythonException",
+            # Unknown errors use the default error code.
+            errorCode=ErrorCodes.GENERAL_ERROR.value.code,
+            # And it has information about what created it.
+            detail="RuntimeError: yikes",
+            errorInfo={
+                "class": "RuntimeError",
+                "args": "('yikes',)",
+            },
+        ),
         run_started_at=None,
         latest_command_hash=None,
     )
 
 
-def test_command_store_saves_correct_error_code() -> None:
-    """If an error is derived from ProtocolEngineError, its ErrorCode should be used."""
+def test_command_store_preserves_enumerated_errors() -> None:
+    """If an error is derived from EnumeratedError, it should be stored as-is."""
 
     class MyCustomError(errors.ProtocolEngineError):
         def __init__(self, message: str) -> None:
@@ -872,17 +906,31 @@ def test_command_store_saves_correct_error_code() -> None:
 
     subject = CommandStore(is_door_open=False, config=_make_config())
 
-    error_details = FinishErrorDetails(
-        error=MyCustomError(message="oh no"),
-        error_id="error-id",
-        created_at=datetime(year=2021, month=1, day=1),
+    subject.handle_action(
+        FinishAction(
+            error_details=FinishErrorDetails(
+                error=MyCustomError(message="oh no"),
+                error_id="error-id-1",
+                created_at=datetime(year=2021, month=1, day=1),
+            )
+        )
     )
-    subject.handle_action(FinishAction(error_details=error_details))
+
+    subject.handle_action(
+        HardwareStoppedAction(
+            completed_at=datetime(year=2022, month=2, day=2),
+            finish_error_details=FinishErrorDetails(
+                error=MyCustomError(message="yikes"),
+                error_id="error-id-2",
+                created_at=datetime(year=2023, month=3, day=3),
+            ),
+        )
+    )
 
     assert subject.state == CommandState(
         queue_status=QueueStatus.PAUSED,
         run_result=RunResult.FAILED,
-        run_completed_at=None,
+        run_completed_at=datetime(year=2022, month=2, day=2),
         is_door_blocking=False,
         running_command_id=None,
         all_command_ids=[],
@@ -890,19 +938,22 @@ def test_command_store_saves_correct_error_code() -> None:
         queued_setup_command_ids=OrderedSet(),
         commands_by_id=OrderedDict(),
         run_error=errors.ErrorOccurrence(
-            id="error-id",
+            id="error-id-1",
             createdAt=datetime(year=2021, month=1, day=1),
             errorType="MyCustomError",
             detail="oh no",
             errorCode=ErrorCodes.PIPETTE_NOT_PRESENT.value.code,
         ),
-        finish_error=None,
+        finish_error=errors.ErrorOccurrence(
+            id="error-id-2",
+            createdAt=datetime(year=2023, month=3, day=3),
+            errorType="MyCustomError",
+            detail="yikes",
+            errorCode=ErrorCodes.PIPETTE_NOT_PRESENT.value.code,
+        ),
         run_started_at=None,
         latest_command_hash=None,
     )
-
-
-# TODO: Add finish error tests.
 
 
 def test_command_store_ignores_stop_after_graceful_finish() -> None:
