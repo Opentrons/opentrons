8d5d2a123c7aef531b040c26a6202ffa83024246
Thassyo Pinto <thassyo.pinto@gmail.com>
2023-08-02 17:23:23 -0400

    disabled save instrument offset

--- api/src/opentrons/hardware_control/ot3_calibration.py
+++ api/src/opentrons/hardware_control/ot3_calibration.py
@@ -737,7 +737,7 @@ async def calibrate_gripper_jaw(
         return offset
     finally:
         hcapi.remove_gripper_probe()
-        await hcapi.ungrip()
+        # await hcapi.ungrip()
 
 
 async def calibrate_gripper(
@@ -746,7 +746,7 @@ async def calibrate_gripper(
     """Calibrate gripper."""
     offset = gripper_pin_offsets_mean(front=offset_front, rear=offset_rear)
     LOG.info(f"Gripper calibration offset: {offset}")
-    await hcapi.save_instrument_offset(OT3Mount.GRIPPER, offset)
+    # await hcapi.save_instrument_offset(OT3Mount.GRIPPER, offset)
     return offset
 
 
@@ -769,7 +769,7 @@ async def calibrate_pipette(
         await hcapi.reset_instrument_offset(mount)
         await hcapi.add_tip(mount, hcapi.config.calibration.probe_length)
         offset = await _calibrate_mount(hcapi, mount, slot, method, raise_verify_error)
-        await hcapi.save_instrument_offset(mount, offset)
+        # await hcapi.save_instrument_offset(mount, offset)
         return offset
     finally:
         await hcapi.remove_tip(mount)
