58fae9f63e0c68a9324e005678f4949101dc4704
Max Marrone <max@opentrons.com>
2023-08-04 12:40:03 -0400

    Comments.

--- robot-server/robot_server/service/legacy/reset_odd.py
+++ robot-server/robot_server/service/legacy/reset_odd.py
@@ -8,7 +8,8 @@ _RESET_MARKER_PATH: Final = AsyncPath("/data/ODD/_CONFIG_TO_BE_DELETED_ON_REBOOT
 
 _RESET_MARKER_FILE_CONTENTS: Final = """\
 This file was placed here by robot-server.
-It tells the on-device display process to clear its configuration on the next boot.
+It tells the on-device display process to clear its configuration on the next boot,
+after which it will delete this file.
 """
 
 
@@ -17,7 +18,14 @@ async def mark_odd_for_reset_next_boot(
 ) -> None:
     """Mark the configuration of the Flex's on-device display so it gets reset on the next boot.
 
-    This assumes you're running on a real Flex, as opposed to an OT-2 or a local dev machine.
+    We can't just delete the config files from this Python process, because the on-device display
+    process actively reads and writes to them, so it would introduce race conditions.
+    We could `systemctl stop` the other process to fix that, but that would introduce other
+    problems: the factory-reset flow involves a separate HTTP request to reboot the machine,
+    and if we stopped the other process, it wouldn't be around to send it.
+
+    This implementation assumes you're running on a real Flex, as opposed to an OT-2 or a
+    local dev machine.
 
     Params:
         reset_marker_path: The path of the marker file to create. This should only be overridden
