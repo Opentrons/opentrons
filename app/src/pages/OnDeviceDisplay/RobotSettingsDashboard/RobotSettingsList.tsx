8ec1acdc7d66136b100492a464a164484407e6e9
koji <koji.kanao@opentrons.com>
2023-08-07 10:41:19 -0400

    fix(app): fix software update InlineNotification layout
    fix software update InlineNotification layout
    
    close RQA-1143

--- app/src/pages/OnDeviceDisplay/RobotSettingsDashboard/RobotSettingsList.tsx
+++ app/src/pages/OnDeviceDisplay/RobotSettingsDashboard/RobotSettingsList.tsx
@@ -102,7 +102,7 @@ export function RobotSettingsList(props: RobotSettingsListProps): JSX.Element {
           settingName={t('apply_historic_offsets')}
           settingInfo={t('historic_offsets_description')}
           iconName="reticle"
-          enabledHistoricOffests
+          enabledHistoricOffsets
           historicOffsetsOn={historicOffsetsOn}
         />
         <RobotSettingButton
