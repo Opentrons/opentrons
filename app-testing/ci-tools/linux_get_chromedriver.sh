38222ef51067412eb64b989746ddd2953dbfbc5c
Derek Maggio <derek.maggio@opentrons.com>
2023-08-08 08:30:04 -0700

    I HATE EVERYTHING

--- app-testing/ci-tools/linux_get_chromedriver.sh
+++ app-testing/ci-tools/linux_get_chromedriver.sh
@@ -4,11 +4,11 @@ set -eo pipefail
 
 VERSION=$1
 
-CHROMEAPP=google-chrome
-if ! type -a google-chrome >/dev/null 2>&1; then
-    sudo apt-get update
-    sudo apt-get install -y google-chrome
-fi
+# CHROMEAPP=google-chrome
+# if ! type -a google-chrome >/dev/null 2>&1; then
+#     sudo apt-get update
+#     sudo apt-get install -y google-chrome
+# fi
 wget -q -c -nc --retry-connrefused --tries=0 https://github.com/electron/electron/releases/download/v${VERSION}/chromedriver-v${VERSION}-linux-x64.zip
 unzip -o -q chromedriver-v${VERSION}-linux-x64.zip
 sudo mv chromedriver /usr/local/bin/chromedriver
