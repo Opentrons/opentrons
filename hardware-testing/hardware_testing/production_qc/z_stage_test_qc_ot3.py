c6efe837569c6dd380b93f93b2b2114f7c812312
David Gomez <david.gomez@opentrons.com>
2023-08-08 17:40:47 -0400

    z stage test in working state

--- hardware-testing/hardware_testing/production_qc/z_stage_test_qc_ot3.py
+++ hardware-testing/hardware_testing/production_qc/z_stage_test_qc_ot3.py
@@ -9,20 +9,6 @@ from dataclasses import dataclass, fields
 from threading import Thread
 from pathlib import Path
 
-# from opentrons_hardware.drivers.can_bus.build import build_driver
-# from opentrons_hardware.drivers.can_bus import CanMessenger, WaitableCallback
-# from opentrons_hardware.firmware_bindings.constants import NodeId, PipetteName
-# from opentrons_hardware.hardware_control.current_settings import set_currents
-# from opentrons_hardware.hardware_control.motion import (
-#     MoveGroupSingleAxisStep,
-#     create_home_step,
-# )
-# from opentrons_hardware.firmware_bindings.messages.message_definitions import (
-#     InstrumentInfoRequest,
-# )
-# from opentrons_hardware.hardware_control.move_group_runner import MoveGroupRunner
-# from opentrons_hardware.scripts.can_args import add_can_args, build_settings
-
 from opentrons.config.defaults_ot3 import (
     DEFAULT_MAX_SPEEDS,
     DEFAULT_ACCELERATIONS,
@@ -49,39 +35,18 @@ LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 
 
-default_move_speed = 15
-default_run_current = 1.4
-currents = (1.0, 1.5)
-speeds = (20, 25)
-force_gauge_currents = (0.5, 1.0, 1.5)
-force_gauge_speeds = (2, 5, 10, 20)
-Z_STAGE_TOLERANCES_MM = 0.4
-
 FORCE_SPEED = 10
 
-data_format = "||{0:^12}|{1:^12}|{2:^12}||"
+FORCE_TEST_SETTINGS = [{"CURRENT":0.15,
+                        "F_AVG": 20},
+                       {"CURRENT":0.5,
+                        "F_AVG": 40},
+                       {"CURRENT":1.5,
+                        "F_AVG": 55}]
 
-CYCLES = 1
-sus_str = "----_----"
+
 thread_sensor = False
-cu_fg = 0.0
-sp_fg = 0.0
-distance_fg = 0.0
 
-
-# NODE = NodeId.head_l
-
-# save test results, to be saved and displayed at the end
-CURRENTS_SPEEDS_TEST_RESULTS = []
-FORCE_GAUGE_TEST_RESULTS = []
-
-
-
-# def _convert_node_to_str(Node: NodeId) -> str:
-#     if Node == NodeId.head_l:
-#         return 'Mount_left'
-#     else:
-#         return 'Mount_right'
 def _connect_to_mark10_fixture(simulate: bool) -> Mark10:
     if not simulate:
         # _port = list_ports_and_select('mark10')
@@ -143,73 +108,16 @@ def _connect_to_mark10_fixture(simulate: bool) -> Mark10:
 #     )
 
 
-
-# async def _home(messenger: CanMessenger) -> None:
-#     await _set_current(messenger, default_run_current)
-#     home_runner = MoveGroupRunner(
-#         move_groups=[[create_home_step({NODE: float64(100.0)}, {NODE: float64(-5)})]]
-#     )
-#     await home_runner.run(can_messenger=messenger)
-#
-# async def _homeMount(messenger: CanMessenger) -> None:
-#     await _set_current(messenger, default_run_current)
-#     # Home mount Left
-#     home_runner = MoveGroupRunner(
-#         move_groups=[[create_home_step({NodeId.head_l: float64(100.0)}, {NodeId.head_l: float64(-5)})]]
-#     )
-#     await home_runner.run(can_messenger=messenger)
-#     # Home Mount right
-#     home_runner = MoveGroupRunner(
-#         move_groups=[[create_home_step({NodeId.head_r: float64(100.0)}, {NodeId.head_r: float64(-5)})]]
-#     )
-#     await home_runner.run(can_messenger=messenger)
-#
-# async def _home_check(messenger: CanMessenger) -> None:
-#     check = True
-#     count = 0
-#     while check:
-#         count += 1
-#         mot, enc = await _move_to(
-#                     messenger, 1, default_move_speed, check=True
-#                 )
-#         print("enc",enc)
-#         if abs(enc) <= 1.1:
-#             check = False
-#         elif count > 5:
-#             #raise OverHomeCountError
-#             print(233333)
-#         else:
-#             await _home(messenger)
-
-
-
-
-# async def _set_current(messenger: CanMessenger, run_current: float) -> None:
-#     currents: Dict[NodeId, Tuple[float, float]] = dict()
-#     currents[NODE] = (float(0.1), float(run_current))
-#     try:
-#         await set_currents(messenger, currents)
-#     except asyncio.CancelledError:
-#         pass
-
-
-# class LoseStepError(Exception):
-#     """Lost Step Error."""
-#
-#     pass
-#
-# class OverHomeCountError(Exception):
-#     print('Homed over the max count, please check the z-stage')
-
-
 def _record_force(mark10:Mark10):
     global thread_sensor
-    global cu_fg
+    global force_output
     # mark10 = _connect_to_mark10_fixture(False)
     try:
         while thread_sensor:
-            time.sleep(0.2)
+            time.sleep(0.1)
             force = mark10.read_force()
+            force_output.append(force)
+            LOG.info(f"Force: {force}")
             # write_cb([_convert_node_to_str(NODE), cu_fg, sp_fg, force])
             # FORCE_GAUGE_TEST_RESULTS.append([_convert_node_to_str(NODE), cu_fg, sp_fg, force])
             # print(_convert_node_to_str(NODE), cu_fg, sp_fg, force)
@@ -219,60 +127,84 @@ def _record_force(mark10:Mark10):
     except KeyboardInterrupt:
         thread_sensor = False
 
+def analyze_force(force_output: List, avg_pass: float) -> bool:
+    """Analyze the Force output from the test to check for pass/fail."""
+    # Only average forces higher than half of the max value
+    max_force = max(force_output)
+    print(f"Max Force: {max_force}")
 
-async def _force_gauge(api: OT3API, mount: OT3Mount, simulate: bool) -> None:
+    if max_force <= 0:
+        return False
+
+    count = 0
+    sum = 0
+    for force in force_output:
+        if force > max_force/2:
+            count += 1
+            sum += force
+
+    average_force = sum/count
+    print(f"Average Force: {average_force}")
+
+    if average_force > avg_pass:
+        return True
+    else:
+        return False
+
+
+
+async def _force_gauge(api: OT3API, mount: OT3Mount, simulate: bool) -> bool:
     global thread_sensor
-    global cu_fg
+    global force_output
+
     LOG.info("_force_gauge")
     mark10 = _connect_to_mark10_fixture(simulate)
     LOG.info("connected to mark 10")
+    ui.print_header(f'Testing {mount.name} Mount')
     z_ax = Axis.by_mount(mount)
     LOG.info(f"Home Axis: {z_ax}")
     await api.home([z_ax])
     home_pos = await api.gantry_position(mount)
     LOG.info(f"Home Position: {home_pos}")
-    pre_test_pos = home_pos._replace(z=home_pos.z - 15)
+    pre_test_pos = home_pos._replace(z=home_pos.z - 10)
     LOG.info(f"Pre-Test Position: {pre_test_pos}")
-    press_pos = home_pos._replace(z=pre_test_pos.z - 7)
+    press_pos = home_pos._replace(z=pre_test_pos.z - 35)
     LOG.info(f"Press Position: {press_pos}")
 
-    for cu_fg in force_gauge_currents:
+    qc_pass = True
+    for test in FORCE_TEST_SETTINGS:
+        test_current = test["CURRENT"]
         # Move to just above force gauge
         await api.move_to(mount=mount, abs_position=pre_test_pos)
 
-        ui.print_header(f'Testing Current = {cu_fg}')
+        ui.print_header(f'Testing Current = {test_current}')
         TH = Thread(target=_record_force, args=(mark10,))
         thread_sensor = True
+        force_output = []
         TH.start()
+        try:
+            async with api._backend.restore_current():
+                await api._backend.set_active_current({z_ax:test_current})
+                LOG.info(f"Current: {api._backend._current_settings[z_ax].run_current}")
+                await api.move_to(mount=mount, abs_position=press_pos,
+                                  speed=FORCE_SPEED, _expect_stalls=True)
+        finally:
+            thread_sensor = False
+            TH.join()
 
-        # await api._backend.set_active_current({z_ax: cu_fg})
-        await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(
-            api,
-            z_ax,
-            api.gantry_load,
-            run_current=cu_fg
-        )
-        LOG.info(f"Current: {api._backend._current_settings[z_ax].run_current}")
-        await api.move_to(mount=mount, abs_position=press_pos,
-                          speed=FORCE_SPEED, _expect_stalls=True)
-        thread_sensor = False
-        TH.join()
-
-        # await api._backend.set_active_current({z_ax: 1})
-        await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(
-            api,
-            z_ax,
-            api.gantry_load,
-            run_current=1.0
-        )
+        force_pass = analyze_force(force_output, test["F_AVG"])
+        if force_pass:
+            print("PASS")
+        else:
+            print("FAIL")
+        qc_pass = qc_pass and force_pass
         # we expect a stall has happened during pick up, so we want to
         # update the motor estimation
         await api._update_position_estimation([Axis.by_mount(mount)])
 
         await api.move_to(mount=mount, abs_position=pre_test_pos)
 
-        # await _set_current(messenger, default_run_current)
-        # await _home(messenger)
+    return qc_pass
 
 # def _results_check() -> None:
 #     c_left_count = 0
@@ -296,29 +228,27 @@ async def _force_gauge(api: OT3API, mount: OT3Mount, simulate: bool) -> None:
 #     print(f'Mount_left Force Guage Test: --PASS') if f_left_count >= 10 else print(f'Mount_left Force Guage Test: --FAIL')
 #     print(f'Mount_right Force Guage Test: --PASS') if f_right_count >= 10 else print(f'Mount_right Force Guage Test: --FAIL')
 
-async def _run(api: OT3API, arguments: argparse.Namespace) -> None:
-    # if "q" in input("\n\tEnter 'q' to exit"):
-    #     raise KeyboardInterrupt()
-
+async def _run(api: OT3API, arguments: argparse.Namespace) -> bool:
+    qc_pass = True
 
     if not arguments.skip_left:
         ui.print_header("Test mount left")
 
         if not arguments.skip_force_left:
             ui.print_header("Force_Gauge_Test")
-            await _force_gauge(api, OT3Mount.LEFT, arguments.simulate)
+            res = await _force_gauge(api, OT3Mount.LEFT, arguments.simulate)
+            qc_pass = res and qc_pass
+
 
     if not arguments.skip_right:
         print('----Test mount right----')
 
         if not arguments.skip_force_right:
             print("Force_Gauge_Test")
-            await _force_gauge(api, OT3Mount.RIGHT, arguments.simulate)
+            res = await _force_gauge(api, OT3Mount.RIGHT, arguments.simulate)
+            qc_pass = res and qc_pass
 
-    # if not arguments.skip_results_check:
-    #     _results_check()
-
-    print('Test Done...')
+    return qc_pass
 
 
 
@@ -327,6 +257,8 @@ async def _main(arguments: argparse.Namespace) -> None:
     api = await helpers_ot3.build_async_ot3_hardware_api(
         is_simulating=arguments.simulate
     )
+    await api.set_gantry_load(api.gantry_load)
+
     try:
         await api.home([Axis.X,
                         Axis.Y,
@@ -338,37 +270,22 @@ async def _main(arguments: argparse.Namespace) -> None:
                         Axis.by_mount(OT3Mount.LEFT),
                         Axis.by_mount(OT3Mount.RIGHT)])
 
-    # await api.home([Axis.X])
-    # await api.home([Axis.Y])
-    # await api.home([Axis.by_mount(OT3Mount.LEFT)])
-    # await api.home([Axis.by_mount(OT3Mount.RIGHT)])
-    qc_pass = True
+    qc_pass = False
 
 
 
     try:
-        await _run(api, arguments)
+        qc_pass = await _run(api, arguments)
     except KeyboardInterrupt:
         print("Cancelled")
     except Exception as e:
+        raise e
+    finally:
         await api.clean_up()
         if qc_pass:
             ui.print_title("Test Done - PASSED")
         else:
             ui.print_title("Test Done - FAILED")
-        raise e
-
-    # subprocess.run(["systemctl", "stop", "opentrons-robot-server"])
-    # driver = await build_driver(build_settings(arguments))
-    # messenger = CanMessenger(driver=driver)
-    # messenger.start()
-    # while True:
-    #     try:
-    #         await _run(messenger,arguments)
-    #     except KeyboardInterrupt:
-    #         break
-    #     except Exception as e:
-    #         print(e)
 
 
 if __name__ == "__main__":
@@ -377,11 +294,8 @@ if __name__ == "__main__":
     arg_parser.add_argument("--simulate", action="store_true")
     arg_parser.add_argument("--operator", type=str, required=True)
     arg_parser.add_argument("--sn", type=str, required=True)
-    arg_parser.add_argument("--skip_current_left", action="store_true")
-    arg_parser.add_argument("--skip_current_right", action="store_true")
     arg_parser.add_argument("--skip_left", action="store_true")
     arg_parser.add_argument("--skip_right", action="store_true")
     arg_parser.add_argument("--skip_force_left", action="store_true")
     arg_parser.add_argument("--skip_force_right", action="store_true")
-    arg_parser.add_argument("--skip_results_check", action="store_true")
     asyncio.run(_main(arg_parser.parse_args()))
