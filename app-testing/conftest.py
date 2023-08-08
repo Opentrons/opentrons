c9973fa6db66cdfcb241ee61f5404b0c6a071b05
Derek Maggio <derek.maggio@opentrons.com>
2023-08-08 08:53:05 -0700

    Try specifying specifically where chromedriver is

--- app-testing/conftest.py
+++ app-testing/conftest.py
@@ -12,6 +12,7 @@ from rich.console import Console
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service
 
 collect_ignore_glob = ["files/**/*.py"]
 
@@ -108,7 +109,8 @@ def driver(request: pytest.FixtureRequest) -> Generator[WebDriver, None, None]:
     os.environ["OT_APP_UPDATE__CHANNEL"] = update_channel
     os.environ["OT_APP_LOG__LEVEL__CONSOLE"] = "error"
     os.environ["OT_APP_DISCOVERY__CANDIDATES"] = "localhost"  # fixed in 6.2
-    with webdriver.Chrome(options=options) as driver:
+    
+    with webdriver.Chrome(options=options, service=Service(os.environ["CHROMEWEBDRIVER"])) as driver:
         _console.print("Driver Capabilities.", style="bright_yellow on blue")
         _console.print(driver.capabilities)
         localhost: Optional[str] = os.getenv("LOCALHOST")
