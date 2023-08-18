cac8f2fa0d76f7e1c7f52429fa7e18af943c62cf
Edward Cormany <ecormany@gmail.com>
2023-08-18 12:17:12 -0400

    lint is voodoo

--- api/src/opentrons/protocol_api/protocol_context.py
+++ api/src/opentrons/protocol_api/protocol_context.py
@@ -924,16 +924,16 @@ class ProtocolContext(CommandPublisher):
         and :py:attr:`loaded_modules` to get a dict of modules.
 
         For :ref:`advanced-control` *only*, you can delete an element of the ``deck`` dict.
-        This only works for deck slots that contain labware objects. For example, if slot 
-        1 contains a labware, ``del protocol.deck['1']`` will free the slot so you can 
+        This only works for deck slots that contain labware objects. For example, if slot
+        1 contains a labware, ``del protocol.deck['1']`` will free the slot so you can
         load another labware there.
-        
+
         .. warning::
-            Deleting labware from a deck slot does not pause the protocol. Subsequent 
-            commands continue immediately. If you need to physically move the labware to 
-            reflect the new deck state, add a :py:meth:`.pause` or use 
-            :py:meth:`.move_labware` instead. 
-        
+            Deleting labware from a deck slot does not pause the protocol. Subsequent
+            commands continue immediately. If you need to physically move the labware to
+            reflect the new deck state, add a :py:meth:`.pause` or use
+            :py:meth:`.move_labware` instead.
+
         .. versionchanged:: 2.15
            ``del`` sets the corresponding labware's location to ``OFF_DECK``.
         """
