48a67c833376b72780334ca6f6212ebddb0b5c58
Shlok Amin <shlokamin@me.com>
2023-08-22 12:06:35 -0400

    add some logs

--- labware-library/src/public-path.ts
+++ labware-library/src/public-path.ts
@@ -1,11 +1,14 @@
 // set the public path at runtime according to deploy location
 // side-effects to make sure public path is set before other imports
-
+console.log('in public path')
+console.log({ hostname: location.hostname })
 let _publicPath = '/'
 if (location.hostname.startsWith('sandbox')) {
+  console.log('sandbox environment')
   // if we're serving from sandbox, ensure the public path is set to name
   // of the deploy folder
   const basePath = location.pathname.slice(1).split('/')[0] || ''
+  console.log({ basePath })
   _publicPath = `/${basePath}/`
 }
 
