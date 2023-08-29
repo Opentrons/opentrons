65ae1ae0c0b42b64e02e41509263cae04109e80d
koji <koji.kanao@opentrons.com>
2023-08-29 10:57:57 -0400

    Update constants.ts

--- app/src/DesignTokens/constants.ts
+++ app/src/DesignTokens/constants.ts
@@ -1,4 +1,14 @@
-import { customViewports } from '../../../.storybook/preview'
+// Note (kk:08/29/2023) Needed this in this ts file to avoid check-js errors on CI
+const customViewports = {
+  onDeviceDisplay: {
+    name: 'Touchscreen',
+    type: 'tablet',
+    styles: {
+      width: '1024px',
+      height: '600px',
+    },
+  },
+}
 
 export const touchScreenViewport = {
   viewport: {
