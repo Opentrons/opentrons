f7256e560dbf7b53ec4df6647cf0b900bfa2cfa6
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2023-08-29 11:55:23 -0400

    feat(app): update instrument and module calibration wizard flows (#13378)
    Closes RAUT-586 closes RAUT-647

--- app/src/organisms/ModuleWizardFlows/constants.ts
+++ app/src/organisms/ModuleWizardFlows/constants.ts
@@ -1,11 +1,30 @@
+import { css } from 'styled-components'
+import { RESPONSIVENESS, TYPOGRAPHY } from '@opentrons/components'
+
 export const SECTIONS = {
   BEFORE_BEGINNING: 'BEFORE_BEGINNING',
   FIRMWARE_UPDATE: 'FIRMWARE_UPDATE',
   SELECT_LOCATION: 'SELECT_LOCATION',
   PLACE_ADAPTER: 'PLACE_ADAPTER',
   ATTACH_PROBE: 'ATTACH_PROBE',
+  DETACH_PROBE: 'DETACH_PROBE',
   SUCCESS: 'SUCCESS',
 } as const
 
+export const FLOWS = {
+  CALIBRATE: 'CALIBRATE',
+}
+
 export const CAL_PIN_LOADNAME = 'calibration_pin' as const
 export const SCREWDRIVER_LOADNAME = 'hex_screwdriver' as const
+
+export const LEFT_SLOTS: string[] = ['A1', 'B1', 'C1', 'D1']
+
+export const BODY_STYLE = css`
+  ${TYPOGRAPHY.pRegular};
+
+  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+    font-size: 1.275rem;
+    line-height: 1.75rem;
+  }
+`
