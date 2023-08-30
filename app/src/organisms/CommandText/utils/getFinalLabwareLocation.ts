1aeed817583bd999d4739c7d7dcc9af6a2418b63
Brian Cooper <brian@opentrons.com>
2023-08-30 16:33:16 -0400

    update docstring of getFinalLabwareLocation

--- app/src/organisms/CommandText/utils/getFinalLabwareLocation.ts
+++ app/src/organisms/CommandText/utils/getFinalLabwareLocation.ts
@@ -1,11 +1,11 @@
-import type { LabwareLocation, RunTimeCommand } from '@opentrons/shared-data/'
+import type { LabwareLocation, RunTimeCommand } from '@opentrons/shared-data'
 
 /**
  * given a list of commands and a labwareId, calculate the resulting location
  * of the corresponding labware after all given commands are executed
  * @param labwareId target labware
  * @param commands list of commands to search within
- * @returns string in format hh:mm:ss, e.g. 03:15:45
+ * @returns LabwareLocation object of the resulting location of the target labware after all commands execute
  */
 export function getFinalLabwareLocation(
   labwareId: string,
