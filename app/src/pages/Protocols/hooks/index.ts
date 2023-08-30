11c534fbdc46a38041982c24ada8c3afa07f51cd
Brian Cooper <brian@opentrons.com>
2023-08-30 16:30:36 -0400

    update all instances of analyses query to as doc

--- app/src/pages/Protocols/hooks/index.ts
+++ app/src/pages/Protocols/hooks/index.ts
@@ -133,10 +133,12 @@ export const useRequiredProtocolHardware = (
 export const useRequiredProtocolLabware = (
   protocolId: string
 ): LabwareSetupItem[] => {
-  const { data: protocolAnalyses } = useProtocolAnalysesQuery(protocolId, {
-    staleTime: Infinity,
-  })
-  const mostRecentAnalysis = last(protocolAnalyses?.data ?? []) ?? null
+  const { data: protocolData } = useProtocolQuery(protocolId)
+  const { data: mostRecentAnalysis } = useProtocolAnalysisAsDocumentQuery(
+    protocolId,
+    last(protocolData?.data.analysisSummaries)?.id ?? null,
+    { enabled: protocolData != null }
+  )
   const commands =
     (mostRecentAnalysis as CompletedProtocolAnalysis)?.commands ?? []
   const { onDeckItems, offDeckItems } = getLabwareSetupItemGroups(commands)
