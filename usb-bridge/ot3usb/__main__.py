62ccaffb4ac17c0a587a4a3cbe069440e9a65ce1
Frank Sinapi <frank.sinapi@opentrons.com>
2023-08-31 09:53:18 -0400

    Fixes to USB gadget config - Grab USB serial number from the filesystem if it exists - user-facing name should be Flex, not OT3

--- usb-bridge/ot3usb/__main__.py
+++ usb-bridge/ot3usb/__main__.py
@@ -4,7 +4,8 @@ import logging
 import time
 from typing import NoReturn
 
-from . import cli, usb_config, default_config, usb_monitor, tcp_conn, listener
+from . import cli, usb_config, usb_monitor, tcp_conn, listener
+from .default_config import get_gadget_config, PHY_NAME
 
 LOG = logging.getLogger(__name__)
 
@@ -20,7 +21,7 @@ async def main() -> NoReturn:
     LOG.info("Starting USB-TCP bridge")
 
     config = usb_config.SerialGadget(
-        driver=usb_config.OSDriver(), config=default_config.default_gadget
+        driver=usb_config.OSDriver(), config=get_gadget_config()
     )
 
     try:
@@ -43,7 +44,7 @@ async def main() -> NoReturn:
     ser = None
 
     monitor = usb_monitor.USBConnectionMonitorFactory.create(
-        phy_udev_name=default_config.PHY_NAME, udc_folder=config.udc_folder()
+        phy_udev_name=PHY_NAME, udc_folder=config.udc_folder()
     )
 
     # Create a tcp connection that will be managed by `listen`
