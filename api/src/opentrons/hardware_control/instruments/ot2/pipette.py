27689ea4862e1145380dc6674f983359deb70152
Seth Foster <seth@opentrons.com>
2023-09-01 14:02:12 -0400

    this too must be updated

--- api/src/opentrons/hardware_control/instruments/ot2/pipette.py
+++ api/src/opentrons/hardware_control/instruments/ot2/pipette.py
@@ -449,7 +449,7 @@ class Pipette(AbstractInstrument[PipetteConfigurations]):
 
     @property
     def minimum_volume(self) -> float:
-        """The smallest controllable volume the pipette can handle."""
+        """The smallest controllable volume the pipette can handle with the current liquid class.."""
         return self.liquid_class.min_volume
 
     @property
