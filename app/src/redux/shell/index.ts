f81ea9675572cd0025026dca1a7acdf0e4a948f7
Brent Hagen <brent.hagen@opentrons.com>
2023-09-01 14:34:13 -0400

    fix(app,app-shell): download robot logs from from app not app-shell (#13388)
    makes api requests to download robot logs within the browser instead of using electron-dl package
    within app-shell
    
    Closes RQA-1225

--- app/src/redux/shell/index.ts
+++ app/src/redux/shell/index.ts
@@ -2,8 +2,6 @@
 
 export * from './actions'
 export * from './update'
-export * from './robot-logs/actions'
-export * from './robot-logs/selectors'
 export * from './is-ready/actions'
 export * from './is-ready/selectors'
 
