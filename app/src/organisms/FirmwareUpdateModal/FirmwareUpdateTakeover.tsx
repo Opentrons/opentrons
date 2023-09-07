a71d233345026519866aefef0abc250dfed95316
smb2268 <sarah.breen@opentrons.com>
2023-09-07 08:08:52 -0400

    fix(app): reflect firmware updates not initiated on ODD
    fix RQA-1252

--- app/src/organisms/FirmwareUpdateModal/FirmwareUpdateTakeover.tsx
+++ app/src/organisms/FirmwareUpdateModal/FirmwareUpdateTakeover.tsx
@@ -3,41 +3,90 @@ import * as React from 'react'
 import {
   useInstrumentsQuery,
   useCurrentMaintenanceRun,
+  useCurrentAllSubsystemUpdatesQuery,
+  useSubsystemUpdateQuery,
 } from '@opentrons/react-api-client'
 
 import { useIsUnboxingFlowOngoing } from '../RobotSettingsDashboard/NetworkSettings/hooks'
-import { UpdateNeededModal } from './UpdateNeededModal'
+import { UpdateInProgressModal } from './UpdateInProgressModal'
 
-const INSTRUMENT_POLL_INTERVAL = 5000
+import { UpdateNeededModal } from './UpdateNeededModal'
+import type { Subsystem } from '@opentrons/api-client'
+const POLL_INTERVAL_5000_MS = 5000
 
 export function FirmwareUpdateTakeover(): JSX.Element {
   const [
     showUpdateNeededModal,
     setShowUpdateNeededModal,
   ] = React.useState<boolean>(false)
+  const [
+    initiatedSubsystemUpdate,
+    setInitiatedSubsystemUpdate,
+  ] = React.useState<Subsystem | null>(null)
+
   const instrumentsData = useInstrumentsQuery({
-    refetchInterval: INSTRUMENT_POLL_INTERVAL,
+    refetchInterval: POLL_INTERVAL_5000_MS,
   }).data?.data
-  const { data: maintenanceRunData } = useCurrentMaintenanceRun()
   const subsystemUpdateInstrument = instrumentsData?.find(
     instrument => instrument.ok === false
   )
+
+  const { data: maintenanceRunData } = useCurrentMaintenanceRun({
+    refetchInterval: POLL_INTERVAL_5000_MS,
+  })
   const isUnboxingFlowOngoing = useIsUnboxingFlowOngoing()
 
+  const {
+    data: currentSubsystemsUpdatesData,
+  } = useCurrentAllSubsystemUpdatesQuery({
+    refetchInterval: POLL_INTERVAL_5000_MS,
+  })
+  const externalSubsystemUpdateExists =
+    currentSubsystemsUpdatesData?.data.some(
+      update =>
+        (update.updateStatus === 'queued' ||
+          update.updateStatus === 'updating') &&
+        update.subsystem !== initiatedSubsystemUpdate
+    ) ?? false
+  const externalSubsystemUpdate = currentSubsystemsUpdatesData?.data.find(
+    update =>
+      update.updateStatus === 'updating' &&
+      update.subsystem !== initiatedSubsystemUpdate
+  )
+  const { data: externalsubsystemUpdateData } = useSubsystemUpdateQuery(
+    externalSubsystemUpdate?.id ?? null
+  )
+
   React.useEffect(() => {
-    if (subsystemUpdateInstrument != null && maintenanceRunData == null) {
+    if (
+      subsystemUpdateInstrument != null &&
+      maintenanceRunData == null &&
+      !isUnboxingFlowOngoing &&
+      !externalSubsystemUpdateExists
+    ) {
       setShowUpdateNeededModal(true)
     }
-  }, [subsystemUpdateInstrument, maintenanceRunData])
+  }, [
+    subsystemUpdateInstrument,
+    maintenanceRunData,
+    isUnboxingFlowOngoing,
+    externalSubsystemUpdateExists,
+  ])
 
   return (
     <>
-      {subsystemUpdateInstrument != null &&
-      showUpdateNeededModal &&
-      !isUnboxingFlowOngoing ? (
+      {subsystemUpdateInstrument != null && showUpdateNeededModal ? (
         <UpdateNeededModal
           subsystem={subsystemUpdateInstrument.subsystem}
           setShowUpdateModal={setShowUpdateNeededModal}
+          setInitiatedSubsystemUpdate={setInitiatedSubsystemUpdate}
+        />
+      ) : null}
+      {externalSubsystemUpdate != null ? (
+        <UpdateInProgressModal
+          percentComplete={
+            externalsubsystemUpdateData?.data.updateProgress ?? 0
+          }
         />
       ) : null}
     </>
