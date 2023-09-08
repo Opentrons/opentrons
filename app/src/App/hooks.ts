4734f18dfac60c739fad529e7ae235ed6b9d5c77
Jamey H <jamey.huffnagle@opentrons.com>
2023-09-08 08:50:47 -0400

    fix(app): ODD redirect to ProtocolDashboard if cancel protocol setup on desktop app

--- app/src/App/hooks.ts
+++ app/src/App/hooks.ts
@@ -3,6 +3,7 @@ import difference from 'lodash/difference'
 import { useTranslation } from 'react-i18next'
 import { useQueryClient } from 'react-query'
 import { useDispatch } from 'react-redux'
+
 import { useInterval } from '@opentrons/components'
 import {
   useAllProtocolIdsQuery,
@@ -20,10 +21,12 @@ import {
   RUN_STATUS_FAILED,
   RUN_STATUS_SUCCEEDED,
 } from '@opentrons/api-client'
+
 import { checkShellUpdate } from '../redux/shell'
 import { useToaster } from '../organisms/ToasterOven'
-import type { SetStatusBarCreateCommand } from '@opentrons/shared-data/protocol/types/schemaV7/command/incidental'
 
+import type { SetStatusBarCreateCommand } from '@opentrons/shared-data/protocol/types/schemaV7/command/incidental'
+import type { Run, Runs } from '@opentrons/api-client'
 import type { Dispatch } from '../redux/types'
 
 const CURRENT_RUN_POLL = 5000
@@ -127,49 +130,71 @@ export function useCurrentRunRoute(): string | null {
     { pageLength: 1 },
     { refetchInterval: CURRENT_RUN_POLL }
   )
-  const currentRunLink = allRuns?.links?.current ?? null
-  const currentRun =
-    currentRunLink != null &&
-    typeof currentRunLink !== 'string' &&
-    'href' in currentRunLink
-      ? allRuns?.data.find(
-          run => run.id === currentRunLink.href.replace('/runs/', '')
-        ) // trim link path down to only runId
-      : null
+  const runRecord = useGetRunRecord(allRuns)
+  return useRouteBasedOnRunRecord(runRecord)
 
-  const currentRunId = currentRun?.id ?? null
+  function useGetRunRecord(allRuns: Runs | undefined): Run | undefined {
+    const currentRunLink = allRuns?.links?.current ?? null
+    const currentRun =
+      currentRunLink != null &&
+      typeof currentRunLink !== 'string' &&
+      'href' in currentRunLink
+        ? allRuns?.data.find(
+            run => run.id === currentRunLink.href.replace('/runs/', '')
+          ) // trim link path down to only runId
+        : null
+    const currentRunId = currentRun?.id ?? null
+    const { data: runRecord } = useRunQuery(currentRunId, {
+      staleTime: Infinity,
+      enabled: currentRunId != null,
+    })
+    return runRecord
+  }
 
-  const { data: runRecord } = useRunQuery(currentRunId, {
-    staleTime: Infinity,
-    enabled: currentRunId != null,
-  })
+  function useRouteBasedOnRunRecord(runRecord: Run | undefined): string | null {
+    if (useHasDesktopCancelledProtocolSetup(runRecord)) return '/protocols'
+    else if (runRecord == null) return null
+    else return routeBasedOnRunStatusAndActions(runRecord)
+  }
 
-  const runStatus = runRecord?.data.status
-  const runActions = runRecord?.data.actions
+  function useHasDesktopCancelledProtocolSetup(
+    runRecord: Run | undefined
+  ): boolean {
+    const previouslyStoredRunRecord = React.useRef<Run | null>(null)
+    if (previouslyStoredRunRecord != null && runRecord == null) {
+      previouslyStoredRunRecord.current = null
+      return true
+    } else {
+      previouslyStoredRunRecord.current = runRecord ?? null
+      return false
+    }
+  }
 
-  if (runRecord == null || runStatus == null || runActions == null) return null
-  // grabbing run id off of the run query to have all routing info come from one source of truth
-  const runId = runRecord.data.id
-
-  const hasBeenStarted = runActions?.some(
-    action => action.actionType === RUN_ACTION_TYPE_PLAY
-  )
-  if (
-    runStatus === RUN_STATUS_SUCCEEDED ||
-    // don't want to route to the run summary page if the run has been cancelled before starting
-    (runStatus === RUN_STATUS_STOPPED && hasBeenStarted) ||
-    runStatus === RUN_STATUS_FAILED
-  ) {
-    return `/runs/${runId}/summary`
-  } else if (
-    runStatus === RUN_STATUS_IDLE ||
-    (!hasBeenStarted && runStatus === RUN_STATUS_BLOCKED_BY_OPEN_DOOR)
-  ) {
-    return `/runs/${runId}/setup`
-    // don't want to route to the run page if the run hasn't started
-  } else if (hasBeenStarted) {
-    return `/runs/${runId}/run`
-  } else {
-    return null
+  function routeBasedOnRunStatusAndActions(runRecord: Run): string | null {
+    const runStatus = runRecord.data.status
+    const runActions = runRecord.data.actions
+    if (runStatus == null || runActions == null) return null
+    // grabbing run id off of the run query to have all routing info come from one source of truth
+    const runId = runRecord?.data.id
+    const hasRunBeenStarted = runActions?.some(
+      action => action.actionType === RUN_ACTION_TYPE_PLAY
+    )
+    if (
+      runStatus === RUN_STATUS_SUCCEEDED ||
+      (runStatus === RUN_STATUS_STOPPED && hasRunBeenStarted) ||
+      runStatus === RUN_STATUS_FAILED
+    ) {
+      return `/runs/${runId}/summary`
+    } else if (
+      runStatus === RUN_STATUS_IDLE ||
+      (!hasRunBeenStarted && runStatus === RUN_STATUS_BLOCKED_BY_OPEN_DOOR)
+    ) {
+      return `/runs/${runId}/setup`
+    } else if (hasRunBeenStarted) {
+      return `/runs/${runId}/run`
+    } else {
+      // includes runs cancelled before starting and runs not yet started
+      return null
+    }
   }
 }
