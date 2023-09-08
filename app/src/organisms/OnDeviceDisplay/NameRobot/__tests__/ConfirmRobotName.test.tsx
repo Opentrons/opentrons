058fadc406afdfb8c2f0c551b3368dc005762858
Frank Sinapi <frank.sinapi@opentrons.com>
2023-09-08 15:06:43 -0400

    App sends "setStatusBar" command to run "disco" animation after first-time setup completes

--- app/src/organisms/OnDeviceDisplay/NameRobot/__tests__/ConfirmRobotName.test.tsx
+++ app/src/organisms/OnDeviceDisplay/NameRobot/__tests__/ConfirmRobotName.test.tsx
@@ -5,6 +5,9 @@ import { renderWithProviders } from '@opentrons/components'
 
 import { i18n } from '../../../../i18n'
 import { ConfirmRobotName } from '../ConfirmRobotName'
+import { useCreateLiveCommandMutation } from '@opentrons/react-api-client'
+
+import type { SetStatusBarCreateCommand } from '@opentrons/shared-data/protocol/types/schemaV7/command/incidental'
 
 const mockPush = jest.fn()
 
@@ -15,6 +18,13 @@ jest.mock('react-router-dom', () => {
     useHistory: () => ({ push: mockPush } as any),
   }
 })
+
+jest.mock('@opentrons/react-api-client')
+jest.mock('@opentrons/shared-data/protocol/types/schemaV7/command/incidental')
+
+const mockUseCreateLiveCommandMutation = useCreateLiveCommandMutation as jest.MockedFunction<
+  typeof useCreateLiveCommandMutation
+>
 
 const render = (props: React.ComponentProps<typeof ConfirmRobotName>) => {
   return renderWithProviders(
@@ -29,10 +39,16 @@ const render = (props: React.ComponentProps<typeof ConfirmRobotName>) => {
 
 describe('ConfirmRobotName', () => {
   let props: React.ComponentProps<typeof ConfirmRobotName>
+  let mockCreateLiveCommand = jest.fn()
   beforeEach(() => {
+    mockCreateLiveCommand = jest.fn()
+    mockCreateLiveCommand.mockResolvedValue(null)
     props = {
       robotName: 'otie',
     }
+    mockUseCreateLiveCommandMutation.mockReturnValue({
+      createLiveCommand: mockCreateLiveCommand,
+    } as any)
   })
 
   it('should render text, an image and a button', () => {
@@ -45,7 +61,16 @@ describe('ConfirmRobotName', () => {
   it('when tapping a button, call a mock function', () => {
     const [{ getByText }] = render(props)
     const button = getByText('Finish setup')
+    const animationCommand: SetStatusBarCreateCommand = {
+      commandType: 'setStatusBar',
+      params: { animation: 'disco' },
+    }
     button.click()
     expect(mockPush).toBeCalledWith('/dashboard')
+    expect(mockUseCreateLiveCommandMutation).toBeCalledWith()
+    expect(mockCreateLiveCommand).toBeCalledWith({
+      command: animationCommand,
+      waitUntilComplete: false,
+    })
   })
 })
