456628db3aa81756c88ab2b7ce542450fda0a4bd
Andy Sigler <andrewsigler1@gmail.com>
2023-09-11 20:25:17 -0400

    increase open-air and sealed pressure thresholds

--- hardware-testing/hardware_testing/production_qc/pipette_assembly_qc_ot3/__main__.py
+++ hardware-testing/hardware_testing/production_qc/pipette_assembly_qc_ot3/__main__.py
@@ -188,12 +188,12 @@ PRESSURE_ASPIRATE_VOL = {
     }
 }
 PRESSURE_THRESH_OPEN_AIR = {
-    1: [-15, 15],
-    8: [-15, 15]
+    1: [-150, 150],
+    8: [-150, 150]
 }
 PRESSURE_THRESH_SEALED = {
-    1: [-50, 50],
-    8: [0, 200]
+    1: [-500, 500],
+    8: [-200, 400]
 }
 PRESSURE_THRESH_COMPRESS = {
     1: [-2600, 1600],
