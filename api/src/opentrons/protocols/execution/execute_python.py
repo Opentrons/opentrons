aa7fea2b31728d24a4bda5dbec63df7d5684c447
Max Marrone <max@opentrons.com>
2023-09-12 17:13:34 -0400

    Fix the bug.

--- api/src/opentrons/protocols/execution/execute_python.py
+++ api/src/opentrons/protocols/execution/execute_python.py
@@ -63,7 +63,6 @@ def run_python(proto: PythonProtocol, context: ProtocolContext):
         SmoothieAlarm,
         asyncio.CancelledError,
         ExecutionCancelledError,
-        ProtocolCommandFailedError,
     ):
         # this is a protocol cancel and shouldn't have special logging
         raise
