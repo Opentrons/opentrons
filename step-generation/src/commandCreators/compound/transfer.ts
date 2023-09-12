5054a4f7eb4e6bc98998dd11f8a5548b59bacf87
Jethary <jethary@opentrons.com>
2023-09-12 12:20:31 -0400

    feat(app, shared-data, protocol-designer, step-generation): frontend low volume support
    closes RAUT-714

--- step-generation/src/commandCreators/compound/transfer.ts
+++ step-generation/src/commandCreators/compound/transfer.ts
@@ -164,7 +164,9 @@ export const transfer: CommandCreator<TransferArgs> = (
           } else if (args.changeTip === 'perDest') {
             changeTipNow = isInitialSubtransfer || destWell !== prevDestWell
           }
-
+          
+          //  todo(jr, 9/12/23): figure out a way to only emit this command if the the previous pipetting volume
+          //  with the same pipette is the same 1uL volume
           const configureForVolumeCommand: CurriedCommandCreator[] =
             args.volume <= 1 &&
             (invariantContext.pipetteEntities[args.pipette].name ===
