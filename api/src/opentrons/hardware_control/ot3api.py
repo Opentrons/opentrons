2d819532673d99e104e77090c01a3a2ad07944eb
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2023-09-13 06:14:40 +0800

    fix(api): OT3 use retract target in pick up tip function (#13538)
    * retract pip after pick up tip

--- api/src/opentrons/hardware_control/ot3api.py
+++ api/src/opentrons/hardware_control/ot3api.py
@@ -1837,6 +1837,10 @@ class OT3API(
         for rel_point, speed in spec.shake_off_list:
             await self.move_rel(realmount, rel_point, speed=speed)
 
+        # fixme: really only need this during labware position check so user
+        # can verify if a tip is properly attached
+        await self.move_rel(realmount, top_types.Point(z=spec.retract_target))
+
         # TODO: implement tip-detection sequence during pick-up-tip for 96ch,
         #       but not with DVT pipettes because those can only detect drops
 
