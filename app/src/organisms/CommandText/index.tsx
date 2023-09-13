69decb1473af87d2250dcc27b95d8cc68a5c563f
Jamey H <jamey.huffnagle@opentrons.com>
2023-09-13 09:58:23 -0400

    fix(app): thermocycler run preview style fix (#13539)
    Closes RQA-747
    
    Fixes run preview commands involving the thermocycler profile to have styles consistent with other
    run preview steps.

--- app/src/organisms/CommandText/index.tsx
+++ app/src/organisms/CommandText/index.tsx
@@ -85,18 +85,18 @@ export function CommandText(props: Props): JSX.Element | null {
       )
       return (
         <Flex flexDirection={DIRECTION_COLUMN} {...styleProps}>
-          <StyledText marginBottom={SPACING.spacing4} {...styleProps}>
+          <StyledText as="p" marginBottom={SPACING.spacing4} {...styleProps}>
             {t('tc_starting_profile', {
               repetitions: Object.keys(steps).length,
             })}
           </StyledText>
-          <Flex marginLeft={SPACING.spacing16}>
+          <StyledText as="p" marginLeft={SPACING.spacing16}>
             <ul>
               {steps.map((step: string, index: number) => (
                 <li key={index}> {step}</li>
               ))}
             </ul>
-          </Flex>
+          </StyledText>
         </Flex>
       )
     }
