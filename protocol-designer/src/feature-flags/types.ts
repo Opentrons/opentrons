02fa678c482afdd75b3428e22c1979991be4f75d
Jethary Rader <66035149+jerader@users.noreply.github.com>
2023-09-13 11:07:30 -0400

    feat(protocol-designer): create 96-channel feature flag (#13540)
    closese RAUT-691

--- protocol-designer/src/feature-flags/types.ts
+++ protocol-designer/src/feature-flags/types.ts
@@ -26,6 +26,7 @@ export type FlagTypes =
   | 'PRERELEASE_MODE'
   | 'OT_PD_DISABLE_MODULE_RESTRICTIONS'
   | 'OT_PD_ALLOW_ALL_TIPRACKS'
+  | 'OT_PD_ALLOW_96_CHANNEL'
 // flags that are not in this list only show in prerelease mode
 export const userFacingFlags: FlagTypes[] = [
   'OT_PD_DISABLE_MODULE_RESTRICTIONS',
