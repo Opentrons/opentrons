03dec76c121ab512167ff36ee919bda0f0d62984
koji <koji.kanao@opentrons.com>
2023-09-13 17:40:24 -0400

    fix(component, shared-data): fix select location on deck for tc
    fix select location on deck for tc
    
    RQA-1494

--- components/src/hooks/useSelectDeckLocation/index.tsx
+++ components/src/hooks/useSelectDeckLocation/index.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react'
 import isEqual from 'lodash/isEqual'
 import { DeckDefinition, getDeckDefFromRobotType } from '@opentrons/shared-data'
-import { RobotCoordinateSpace } from '../../hardware-sim/RobotCoordinateSpace'
 
-import type { ModuleLocation, RobotType } from '@opentrons/shared-data'
+import { RobotCoordinateSpace } from '../../hardware-sim/RobotCoordinateSpace'
 import { COLORS, SPACING } from '../../ui-style-constants'
 import { RobotCoordsForeignDiv, SlotLabels } from '../../hardware-sim'
 import { Icon } from '../../icons'
 import { Text } from '../../primitives'
 import { ALIGN_CENTER, JUSTIFY_CENTER } from '../../styles'
 import { DeckSlotLocation } from '../../hardware-sim/DeckSlotLocation'
+
+import type { ModuleLocation, RobotType } from '@opentrons/shared-data'
 
 const X_CROP_MM = 60
 export function useDeckLocationSelect(
@@ -57,6 +58,7 @@ export function DeckLocationSelect({
             typeof l === 'object' && 'slotName' in l && l.slotName === slot.id
         )
         const isSelected = isEqual(selectedLocation, slotLocation)
+        console.log('isSelected', isSelected)
         let fill = COLORS.highlightPurple2
         if (isSelected) fill = COLORS.highlightPurple1
         if (isDisabled) fill = COLORS.darkGreyDisabled
@@ -72,7 +74,7 @@ export function DeckLocationSelect({
             />
             {isSelected ? (
               <RobotCoordsForeignDiv
-                x={slot.position[0]}
+                x={slot.id === 'Z1' ? '-65' : slot.position[0]}
                 y={slot.position[1]}
                 width={slot.boundingBox.xDimension}
                 height={slot.boundingBox.yDimension}
@@ -83,6 +85,9 @@ export function DeckLocationSelect({
                   height: '100%',
                   gridGap: SPACING.spacing4,
                 }}
+                extraTransform={
+                  slot.id === 'Z1' ? 'translate(-5px, 40px)' : undefined
+                }
               >
                 <Icon name="check-circle" size="1.5rem" color={COLORS.white} />
                 <Text color={COLORS.white} fontSize="1.5rem">
