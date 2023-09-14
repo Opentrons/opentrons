4c014ed6a316ff55ae4407a335868306db272a19
Jethary <jethary@opentrons.com>
2023-09-14 09:22:01 -0400

    remove unused import

--- app/src/organisms/RobotSettingsCalibration/CalibrationDetails/__tests__/ModuleCalibrationOverflowMenu.test.tsx
+++ app/src/organisms/RobotSettingsCalibration/CalibrationDetails/__tests__/ModuleCalibrationOverflowMenu.test.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { resetAllWhenMocks } from 'jest-when'
 import { waitFor } from '@testing-library/react'
 import { renderWithProviders } from '@opentrons/components'
 import { useCreateLiveCommandMutation } from '@opentrons/react-api-client'
