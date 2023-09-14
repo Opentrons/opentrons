642e16b774b62e800c6bf8665f80a343ab29127b
Frank Sinapi <frank.sinapi@opentrons.com>
2023-09-14 12:17:02 -0400

    Added parameter to enable optional homing when axis position is bad

--- api/tests/opentrons/protocol_engine/commands/test_home.py
+++ api/tests/opentrons/protocol_engine/commands/test_home.py
@@ -2,6 +2,7 @@
 from decoy import Decoy
 
 from opentrons.protocol_engine.types import MotorAxis
+from opentrons.types import MountType
 from opentrons.protocol_engine.execution import MovementHandler
 
 from opentrons.protocol_engine.commands.home import (
@@ -33,3 +34,32 @@ async def test_home_all_implementation(decoy: Decoy, movement: MovementHandler) 
 
     assert result == HomeResult()
     decoy.verify(await movement.home(axes=None))
+
+
+async def test_home_with_invalid_position(
+    decoy: Decoy, movement: MovementHandler
+) -> None:
+    """Test handling when the homing is conditional (only move if position invalid)."""
+    subject = HomeImplementation(movement=movement)
+
+    decoy.when(
+        await movement.check_for_valid_position(mount=MountType.LEFT)
+    ).then_return(False)
+    decoy.when(
+        await movement.check_for_valid_position(mount=MountType.RIGHT)
+    ).then_return(True)
+
+    data_should_home = HomeParams(
+        axes=[MotorAxis.X, MotorAxis.Y], skipIfMountPositionOk=MountType.LEFT
+    )
+    data_should_not_home = HomeParams(
+        axes=[MotorAxis.X, MotorAxis.Y], skipIfMountPositionOk=MountType.RIGHT
+    )
+
+    result = await subject.execute(data_should_home)
+    assert result == HomeResult()
+
+    result = await subject.execute(data_should_not_home)
+    assert result == HomeResult()
+
+    decoy.verify(await movement.home(axes=[MotorAxis.X, MotorAxis.Y]), times=1)
