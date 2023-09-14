a11b82d144b1954c07c32a5e74ec0794f9b8d121
andySigler <andrewsigler1@gmail.com>
2023-09-14 00:30:59 -0400

    simulation works

--- hardware-testing/hardware_testing/protocols/installation_qualification/flex_iq_p1000_multi_200ul.py
+++ hardware-testing/hardware_testing/protocols/installation_qualification/flex_iq_p1000_multi_200ul.py
@@ -2,14 +2,13 @@
 from math import pi
 from typing import List
 
-from opentrons.protocol_api import ProtocolContext
+from opentrons.protocol_api import ProtocolContext, InstrumentContext, Labware
 
 metadata = {"protocolName": "Flex IQ: P1000 Multi 200ul"}
 requirements = {"robotType": "Flex", "apiLevel": "2.15"}
 
 TEST_VOLUME = 200
 DUT = "p1000_multi_gen3"
-SOURCE = "nest_12_reservoir_15ml"
 TEST_SOURCES = [
     {
         "source": "A1",
@@ -23,6 +22,12 @@ TEST_SOURCES = [
 
 SUBMERGE_MM = 1.5
 RETRACT_MM = 5.0
+
+SRC_LABWARE_BY_CHANNELS = {
+    1: "nest_96_wellplate_2ml_deep",
+    8: "nest_12_reservoir_15ml",
+    96: "nest_1_reservoir_195ml"
+}
 
 MIN_VOL_SRC = {
     "nest_96_wellplate_2ml_deep": 500,
@@ -131,40 +136,140 @@ def _ul_to_mm(load_name: str, ul: float) -> float:
     raise ValueError(f"unable to find height of {ul:.1} ul in {load_name}")
 
 
-def _dye_heights_per_trial(load_name: str, channels: int, trials: int) -> List[float]:
-    ul_per_aspirate = TEST_VOLUME * channels
+def _start_volumes_per_trial(volume: float, load_name: str, channels: int, trials: int) -> List[float]:
+    ul_per_aspirate = volume * channels
     ul_per_run = ul_per_aspirate * trials
     ul_at_start = ul_per_run + MIN_VOL_SRC[load_name]
-    ul_per_trial = [
+    return [
         ul_at_start - (ul_per_aspirate * i)
         for i in range(trials)
     ]
-    mm_per_trial = [
-        _ul_to_mm(load_name, ul)
-        for ul in ul_per_trial
-    ]
-    return mm_per_trial
+
+
+def _dye_start_volumes_per_trial(load_name: str, channels: int, trials: int) -> List[float]:
+    return _start_volumes_per_trial(TEST_VOLUME, load_name, channels, trials)
+
+
+def _diluent_start_volumes_per_trial(load_name: str, channels: int, trials: int) -> List[float]:
+    return _start_volumes_per_trial(max(200 - TEST_VOLUME, 0), load_name, channels, trials)
+
+
+def _assign_starting_volumes_dye(
+        ctx: ProtocolContext,
+        pipette: InstrumentContext,
+        reservoir: Labware,
+) -> None:
+    dye = ctx.define_liquid(
+        name="Dye",
+        description="Dye range for test volume",
+        display_color="#FF0000",
+    )
+    for test in TEST_SOURCES:
+        src_ul_per_trial = _dye_start_volumes_per_trial(
+            reservoir.load_name, pipette.channels, len(test["destinations"])
+        )
+        first_trial_ul = src_ul_per_trial[0]
+        reservoir[test["source"]].load_liquid(dye, first_trial_ul)
+
+
+def _assign_starting_volumes_diluent(
+        ctx: ProtocolContext,
+        pipette: InstrumentContext,
+        reservoir: Labware,
+) -> None:
+    if TEST_VOLUME >= 200:
+        return
+    diluent = ctx.define_liquid(
+        name="Diluent",
+        description="Diluent",
+        display_color="#0000FF",
+    )
+    if pipette.channels == 1:
+        num_transfers_per_source_well = 1
+    else:
+        num_transfers_per_source_well = 6
+    source_wells = ["A11", "A12"]
+    for well in source_wells:
+        src_ul_per_trial = _diluent_start_volumes_per_trial(
+            reservoir.load_name, pipette.channels, num_transfers_per_source_well
+        )
+        first_trial_ul = src_ul_per_trial[0]
+        reservoir[well].load_liquid(diluent, first_trial_ul)
+
+
+def _transfer(
+        volume: float,
+        pipette: InstrumentContext,
+        reservoir: Labware,
+        plate: Labware,
+        source: str, destinations: List[str],
+        same_tip: bool = False
+) -> None:
+    src_ul_per_trial = _diluent_start_volumes_per_trial(
+        reservoir.load_name, pipette.channels, len(destinations)
+    )
+    src_heights = [_ul_to_mm(reservoir.load_name, ul) for ul in src_ul_per_trial]
+    dst_height = _ul_to_mm(plate.load_name, volume)
+    if same_tip and not pipette.has_tip:
+        pipette.pick_up_tip()
+    for dst_name, height_src in zip(destinations, src_heights):
+        # calculate pipetting positions
+        aspirate_pos = reservoir[source].bottom(height_src - SUBMERGE_MM)
+        dispense_pos = plate[dst_name].bottom(dst_height - SUBMERGE_MM)
+        blow_out_pos = plate[dst_name].bottom(dst_height + RETRACT_MM)
+        # transfer
+        if not same_tip:
+            pipette.pick_up_tip()
+        pipette.aspirate(TEST_VOLUME, aspirate_pos)
+        pipette.dispense(TEST_VOLUME, dispense_pos)
+        pipette.blow_out(blow_out_pos)
+        if not same_tip:
+            pipette.drop_tip()
+
+
+def _transfer_diluent(pipette: InstrumentContext, reservoir: Labware, plate: Labware) -> None:
+    diluent_vol = 200 - TEST_VOLUME
+    if diluent_vol <= 0:
+        return
+    target_cols = set([
+        dst[1:]
+        for test in TEST_SOURCES
+        for dst in test["destinations"]
+    ])
+    destinations = [f"A{col}" for col in target_cols]
+    pipette.pick_up_tip()
+    for i, dst in enumerate(destinations):
+        if i < len(destinations) / 2:
+            src = "A11"
+        else:
+            src = "A12"
+        _transfer(diluent_vol, pipette, reservoir, plate, src, destinations, same_tip=True)
+    pipette.drop_tip()
+
+
+def _transfer_dye(pipette: InstrumentContext, reservoir: Labware, plate: Labware) -> None:
+    for test in TEST_SOURCES:
+        _transfer(TEST_VOLUME, pipette, reservoir, plate, test["source"], test["destinations"])
 
 
 def run(ctx: ProtocolContext) -> None:
-    reservoir = ctx.load_labware(SOURCE, "D2")
+    dye_pipette = ctx.load_instrument(
+        DUT,
+        "left",
+        tip_racks=[ctx.load_labware("opentrons_flex_96_tiprack_200uL", "D1")]
+    )
     plate = ctx.load_labware("corning_96_wellplate_360ul_flat", "D3")
-    tip_rack_200 = ctx.load_labware("opentrons_flex_96_tiprack_200uL", "D1")
-    dut = ctx.load_instrument(DUT, "left", tip_racks=[tip_rack_200])
+    reservoir = ctx.load_labware(SRC_LABWARE_BY_CHANNELS[dye_pipette.channels], "D2")
 
-    for test in TEST_SOURCES:
-        src_heights = _dye_heights_per_trial(
-            reservoir.load_name, dut.channels, len(test["destinations"])
+    _assign_starting_volumes_dye(ctx, dye_pipette, reservoir)
+    if TEST_VOLUME < 200:
+        diluent_pipette = ctx.load_instrument(
+            "p1000_multi_gen3",
+            "right",
+            tip_racks=[ctx.load_labware("opentrons_flex_96_tiprack_200uL", "C1")]
         )
-        dst_height = _ul_to_mm(plate.load_name, 200)  # always ends at 200ul
-        for dst_name, height_src in zip(test["destinations"], src_heights):
-            # calculate pipetting positions
-            aspirate_pos = reservoir[test["source"]].bottom(height_src - SUBMERGE_MM)
-            dispense_pos = plate[dst_name].bottom(dst_height - SUBMERGE_MM)
-            blow_out_pos = plate[dst_name].bottom(dst_height + RETRACT_MM)
-            # transfer
-            dut.pick_up_tip()
-            dut.aspirate(TEST_VOLUME, aspirate_pos)
-            dut.dispense(TEST_VOLUME, dispense_pos)
-            dut.blow_out(blow_out_pos)
-            dut.drop_tip()
+        _assign_starting_volumes_diluent(ctx, dye_pipette, reservoir)
+        _transfer_diluent(diluent_pipette, reservoir, plate)
+    _transfer_dye(dye_pipette, reservoir, plate)
+
+
