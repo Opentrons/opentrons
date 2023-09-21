790ae9d36ecdbd209fd3c15a5bd3b4e40923dd6f
Seth Foster <seth@opentrons.com>
2023-09-21 10:49:28 -0400

    chore: get the ymls in there

--- scripts/update-releases-json.js
+++ scripts/update-releases-json.js
@@ -7,7 +7,7 @@ const fs = require('fs/promises')
 const versionFinder = require('./git-version')
 
 const parseArgs = require('./deploy/lib/parseArgs')
-const USAGE = '\nUsage:\n node ./scripts/update-releases-json <releases-json-path> <project> <windows-path> <mac-path> <linux-path>'
+const USAGE = '\nUsage:\n node ./scripts/update-releases-json <releases-json-path> <project> <artifact-dir>'
 
 async function readOrDefaultReleases(releasesPath) {
   try {
@@ -19,20 +19,46 @@ async function readOrDefaultReleases(releasesPath) {
   }
 }
 
+const FILES_IN_RELEASE_JSON = [
+  /Opentrons.*\.exe$/,
+  /Opentrons.*\.dmg$/,
+  /Opentrons.*\.AppImage$/,
+  /latest.*yml$/,
+  /alpha.*yml$/,
+  /beta.*yml$/
+]
+
+function artifactNameToObj(artifactName) {
+  if (artifactName.search(/Opentrons.*\.exe$/) !== -1) {
+    return {'Opentrons.exe': artifactName}
+  } else if (artifactName.search(/Opentrons.*\.dmg$/) !== -1) {
+    return {'Opentrons.dmg': artifactName}
+  } else if (artifactName.search(/Opentrons.*\.AppImage$/) !== -1) {
+    return {'Opentrons.AppImage': artifactName}
+  } else if (artifactName.search(/(latest|alpha|beta).*yml$/) !== -1) {
+    return {[artifactName]: artifactName}
+  } else {
+    throw new Error(`Unmatched artifact ${artifactName}`)
+  }
+}
+
+async function artifactsFromDir(artifactDirPath) {
+  const files = await fs.readdir(artifactDirPath, {withFileTypes: true})
+  return files.filter(dirent => dirent.isFile() && FILES_IN_RELEASE_JSON.some(re => dirent.name.search(re) !== -1))
+      .map(dirent => artifactNameToObj(dirent.name))
+      .reduce((prev, current) => ({...prev, ...current}))
+}
+
 async function main() {
   const {args}= parseArgs(process.argv.slice(2))
-  const [releasesPath, project, windowsPath, macPath, linuxPath] = args
-  if (!releasesPath || !project || !windowsPath || !macPath || !linuxPath) {
+  const [releasesPath, project, artifactDirPath] = args
+  if (!releasesPath || !project || !artifactDirPath) {
     throw new Error(USAGE)
   }
   const releasesData = await readOrDefaultReleases(releasesPath)
   const version = await versionFinder.versionForProject(project)
-  releasesData.production[version] = {
-    'Opentrons.exe': windowsPath,
-    'Opentrons.dmg': macPath,
-    'Opentrons.AppImage': linuxPath,
-    revoked: false
-  } ;
+  releasesData.production[version] = {...(await artifactsFromDir(artifactDirPath)), revoked: false} ;
+
   (await fs.open(releasesPath, 'w')).writeFile(JSON.stringify(releasesData))
 }
 
