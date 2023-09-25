e3ebcd868500d5229ae06354f52202c43da95313
Jethary <jethary@opentrons.com>
2023-09-25 11:07:26 -0400

    fix(app): oT-2 pipette flow success screen img resize
    closes RQA-1214

--- app/src/molecules/SimpleWizardBody/index.tsx
+++ app/src/molecules/SimpleWizardBody/index.tsx
@@ -20,6 +20,7 @@ import SuccessIcon from '../../assets/images/icon_success.png'
 import { getIsOnDevice } from '../../redux/config'
 import { StyledText } from '../../atoms/text'
 import { Skeleton } from '../../atoms/Skeleton'
+import { FLEX_ROBOT_TYPE, RobotType } from '@opentrons/shared-data'
 
 interface Props extends StyleProps {
   iconColor: string
@@ -28,6 +29,7 @@ interface Props extends StyleProps {
   children?: React.ReactNode
   subHeader?: string | JSX.Element
   isPending?: boolean
+  robotType?: RobotType
   /**
    *  this prop is to change justifyContent of OnDeviceDisplay buttons
    *  TODO(jr, 8/9/23): this SHOULD be refactored so the
@@ -87,6 +89,7 @@ export function SimpleWizardBody(props: Props): JSX.Element {
     subHeader,
     isSuccess,
     isPending,
+    robotType = FLEX_ROBOT_TYPE,
     ...styleProps
   } = props
   const isOnDevice = useSelector(getIsOnDevice)
@@ -144,8 +147,8 @@ export function SimpleWizardBody(props: Props): JSX.Element {
           <>
             {isSuccess ? (
               <img
-                width="250px"
-                height="208px"
+                width={robotType === FLEX_ROBOT_TYPE ? '250px' : '160px'}
+                height={robotType === FLEX_ROBOT_TYPE ? '208px' : '120px'}
                 src={SuccessIcon}
                 alt="Success Icon"
               />
