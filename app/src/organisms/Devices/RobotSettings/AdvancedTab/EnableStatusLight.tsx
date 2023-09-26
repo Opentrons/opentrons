1496e25705f44fa22c216e0de40334ab25d60116
koji <koji.kanao@opentrons.com>
2023-09-26 14:14:10 -0400

    feat(app): add enable status light section to robotsettings
    add enable status light section to robotsettings advanced tab and this is for Flex.
    
    close RQA-1637

---
+++ app/src/organisms/Devices/RobotSettings/AdvancedTab/EnableStatusLight.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import {
+  ALIGN_CENTER,
+  Box,
+  DIRECTION_COLUMN,
+  Flex,
+  JUSTIFY_SPACE_BETWEEN,
+  SPACING,
+  TYPOGRAPHY,
+} from '@opentrons/components'
+import { StyledText } from '../../../../atoms/text'
+import { ToggleButton } from '../../../../atoms/buttons'
+import { useLEDLights } from '../../hooks'
+
+interface EnableStatusLightProps {
+  robotName: string
+}
+export function EnableStatusLight({
+  robotName,
+}: EnableStatusLightProps): JSX.Element {
+  const { t } = useTranslation('device_settings')
+  const { lightsEnabled, toggleLights } = useLEDLights(robotName)
+
+  return (
+    <Flex
+      alignItems={ALIGN_CENTER}
+      justifyContent={JUSTIFY_SPACE_BETWEEN}
+      marginTop={SPACING.spacing24}
+    >
+      <Box width="70%">
+        <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing4}>
+          <StyledText
+            as="p"
+            fontWeight={TYPOGRAPHY.fontWeightSemiBold}
+            id="AdvancedSettings_Enable_Status_Light"
+          >
+            {t('enable_status_light')}
+          </StyledText>
+          <StyledText as="p">{t('enable_status_light_description')}</StyledText>
+        </Flex>
+      </Box>
+      <ToggleButton
+        label="enable_status_light"
+        toggledOn={lightsEnabled}
+        onClick={toggleLights}
+        id="RobotSettings_enableStatusLightToggleButton"
+      />
+    </Flex>
+  )
+}
---
+++ app/src/organisms/Devices/RobotSettings/AdvancedTab/__tests__/EnableStatusLight.test.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+
+import { renderWithProviders } from '@opentrons/components'
+
+import { i18n } from '../../../../../i18n'
+import { useLEDLights } from '../../../hooks'
+import { EnableStatusLight } from '../EnableStatusLight'
+
+jest.mock('../../../hooks')
+
+const mockUseLEDLights = useLEDLights as jest.MockedFunction<
+  typeof useLEDLights
+>
+
+const ROBOT_NAME = 'otie'
+const mockToggleLights = jest.fn()
+const render = (props: React.ComponentProps<typeof EnableStatusLight>) => {
+  return renderWithProviders(<EnableStatusLight {...props} />, {
+    i18nInstance: i18n,
+  })
+}
+
+describe('EnableStatusLight', () => {
+  let props: React.ComponentProps<typeof EnableStatusLight>
+
+  beforeEach(() => {
+    props = {
+      robotName: ROBOT_NAME,
+    }
+    mockUseLEDLights.mockReturnValue({
+      lightsEnabled: false,
+      toggleLights: mockToggleLights,
+    })
+  })
+
+  it('should render text and toggle button', () => {
+    const [{ getByText, getByLabelText }] = render(props)
+    getByText('Enable status light')
+    getByText(
+      'Turn on or off the strip of color LEDs on the front of the robot.'
+    )
+    expect(getByLabelText('enable_status_light')).toBeInTheDocument()
+  })
+
+  it('should call a mock function when clicking toggle button', () => {
+    const [{ getByLabelText }] = render(props)
+    getByLabelText('enable_status_light').click()
+    expect(mockToggleLights).toHaveBeenCalled()
+  })
+})
