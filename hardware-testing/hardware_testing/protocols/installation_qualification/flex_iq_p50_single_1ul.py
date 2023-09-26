506ba52e2910cc9f31fcb6edcd7a1a9f4f14ac84
andySigler <andrewsigler1@gmail.com>
2023-09-25 21:12:33 -0400

    import ceil

--- hardware-testing/hardware_testing/protocols/installation_qualification/flex_iq_p50_single_1ul.py
+++ hardware-testing/hardware_testing/protocols/installation_qualification/flex_iq_p50_single_1ul.py
@@ -1,5 +1,5 @@
 """Flex IQ: P50 Single 1ul."""
-from math import pi
+from math import pi, ceil
 from typing import List, Optional, Dict, Tuple
 
 from opentrons.protocol_api import ProtocolContext, InstrumentContext, Labware
