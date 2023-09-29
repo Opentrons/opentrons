fa7d8916b8fb6acf0950b65df4262503f5144f68
Jamey H <jamey.huffnagle@opentrons.com>
2023-09-29 16:04:59 -0400

    refactor(app): safely start robot update
    Adds a wrapper to clear existing robot update session before starting a new one.

---
+++ app/src/redux/robot-update/__tests__/hooks.test.ts
@@ -0,0 +1,35 @@
+import { useDispatch } from 'react-redux'
+import { renderHook } from '@testing-library/react-hooks'
+
+import { useDispatchStartRobotUpdate } from '../hooks'
+import { startRobotUpdate, clearRobotUpdateSession } from '../actions'
+
+jest.mock('react-redux')
+
+const mockUseDispatch = useDispatch as jest.MockedFunction<typeof useDispatch>
+
+describe('useDispatchStartRobotUpdate', () => {
+  let mockDispatch: jest.Mock
+  const mockRobotName = 'robotName'
+  const mockSystemFile = 'systemFile'
+
+  beforeEach(() => {
+    mockDispatch = jest.fn()
+    mockUseDispatch.mockReturnValue(mockDispatch)
+  })
+
+  afterEach(() => {
+    mockUseDispatch.mockClear()
+    jest.clearAllMocks()
+  })
+
+  it('clears the robot update session before dispatching a new session with the given robotName and systemFile', () => {
+    const { result } = renderHook(useDispatchStartRobotUpdate)
+
+    result.current(mockRobotName, mockSystemFile)
+    expect(mockDispatch).toHaveBeenCalledWith(clearRobotUpdateSession())
+    expect(mockDispatch).toHaveBeenCalledWith(
+      startRobotUpdate(mockRobotName, mockSystemFile)
+    )
+  })
+})
---
+++ app/src/redux/robot-update/hooks.ts
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux'
+import { startRobotUpdate, clearRobotUpdateSession } from './actions'
+import type { Action } from '../types'
+
+type DispatchStartRobotUpdate = (
+  robotName: string,
+  systemFile?: string | undefined
+) => void
+
+export function useDispatchStartRobotUpdate(): DispatchStartRobotUpdate {
+  const dispatch = useDispatch<(a: Action) => void>()
+
+  function dispatchStartRobotUpdate(
+    robotName: string,
+    systemFile?: string
+  ): void {
+    dispatch(clearRobotUpdateSession())
+    dispatch(startRobotUpdate(robotName, systemFile))
+  }
+
+  return dispatchStartRobotUpdate
+}
