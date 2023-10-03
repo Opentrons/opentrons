8cc25707e614d18d82a8eb3afb170c63e07619ff
Jethary <jethary@opentrons.com>
2023-10-03 13:29:09 -0400

    create loadFixture

--- protocol-designer/typings/reselect.d.ts
+++ protocol-designer/typings/reselect.d.ts
@@ -1,6 +1,6 @@
 import { OutputSelector, Selector } from 'reselect'
 declare module 'reselect' {
-  // declaring type for createSelector with 14 selectors because the reselect types only support up to 12 selectors
+  // declaring type for createSelector with 15 selectors because the reselect types only support up to 12 selectors
   export function createSelector<
     S,
     R1,
@@ -17,6 +17,7 @@ declare module 'reselect' {
     R12,
     R13,
     R14,
+    R15,
     T
   >(
     selector1: Selector<S, R1>,
@@ -33,6 +34,7 @@ declare module 'reselect' {
     selector12: Selector<S, R12>,
     selector13: Selector<S, R13>,
     selector14: Selector<S, R14>,
+    selector15: Selector<S, R15>,
     combiner: (
       res1: R1,
       res2: R2,
@@ -47,7 +49,8 @@ declare module 'reselect' {
       res11: R11,
       res12: R12,
       res13: R13,
-      res14: R14
+      res14: R14,
+      res15: R15
     ) => T
   ): OutputSelector<
     S,
@@ -66,7 +69,8 @@ declare module 'reselect' {
       res11: R11,
       res12: R12,
       res13: R13,
-      res14: R14
+      res14: R14,
+      res15: R15
     ) => T
   >
 }
