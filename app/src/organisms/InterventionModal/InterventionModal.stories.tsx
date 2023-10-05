685f04a2b109889ad45931f1341b2a8b145f9605
Brian Arthur Cooper <brian@opentrons.com>
2023-10-05 13:00:47 -0400

    fix(app): add support link for manual intervention steps (#13715)
    Reintroduce the link to the desktop manual intervention modal that leads to a page explaining manual
    intervention steps.

--- app/src/organisms/InterventionModal/InterventionModal.stories.tsx
+++ app/src/organisms/InterventionModal/InterventionModal.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import fixture_96_plate from '@opentrons/shared-data/labware/fixtures/2/fixture_96_plate.json'
 import { configReducer } from '../../redux/config/reducer'
 import { mockRunData } from './__fixtures__'
 import { InterventionModal as InterventionModalComponent } from './'
@@ -45,4 +46,49 @@ PauseIntervention.args = {
   robotName: 'Otie',
   command: pauseCommand,
   run: mockRunData,
+}
+
+export const MoveLabwareIntervention = Template.bind({})
+MoveLabwareIntervention.args = {
+  robotName: 'Otie',
+  command: {
+    commandType: 'moveLabware',
+    params: {
+      labwareId: 'fake_labware_id',
+      newLocation: {
+        slotName: '1',
+      },
+    },
+  },
+  run: {
+    ...mockRunData,
+    labware: [
+      {
+        id: 'fake_labware_id',
+        loadName: fixture_96_plate.parameters.loadName,
+        definitionUri: 'fixture/fixture_96_plate/1',
+        location: {
+          slotName: '9',
+        },
+      },
+    ],
+  },
+  analysis: {
+    commands: [
+      {
+        commandType: 'loadLabware',
+        params: {
+          displayName: 'fake display name',
+          labwareId: 'fake_labware_id',
+          loadName: fixture_96_plate.parameters.loadName,
+          namespace: 'fixture',
+          version: 1,
+          location: { slotName: '9' },
+        },
+        result: {
+          definition: fixture_96_plate,
+        },
+      },
+    ],
+  },
 }
