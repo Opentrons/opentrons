86c9d14c3e8fccf504149765735d8cf65c103b07
koji <koji.kanao@opentrons.com>
2023-10-05 15:56:55 -0400

    feat(app, components): add deck configuration section to device details
    Add add fixture modal for Desktop and setup instructions modal for Desktop app
    
    close RAUT-683

--- app/src/organisms/DeviceDetailsDeckConfiguration/TouchScreenDeckFixtureSetupInstructionModal.stories.tsx
+++ app/src/organisms/DeviceDetailsDeckConfiguration/TouchScreenDeckFixtureSetupInstructionModal.stories.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import { touchScreenViewport } from '../../DesignTokens/constants'
-import { DeckFixtureSetupInstructionModal } from './DeckFixtureSetupInstructionModal'
+import { DeckFixtureSetupInstructionsModal } from './DeckFixtureSetupInstructionsModal'
 import type { Story, Meta } from '@storybook/react'
 
 export default {
-  title: 'ODD/Organisms/DeckFixtureSetupInstructionModal',
+  title: 'ODD/Organisms/DeckFixtureSetupInstructionsModal',
   argTypes: {
     modalSize: {
       options: ['small', 'medium', 'large'],
@@ -16,9 +16,10 @@ export default {
 } as Meta
 
 const Template: Story<
-  React.ComponentProps<typeof DeckFixtureSetupInstructionModal>
-> = args => <DeckFixtureSetupInstructionModal {...args} />
+  React.ComponentProps<typeof DeckFixtureSetupInstructionsModal>
+> = args => <DeckFixtureSetupInstructionsModal {...args} />
 export const Default = Template.bind({})
 Default.args = {
   setShowSetupInstructionsModal: () => {},
+  isOnDevice: true,
 }
