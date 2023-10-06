b6b2cd229ca56473ac1b6d2ce7ca766c9481c4aa
Max Marrone <max@opentrons.com>
2023-10-06 15:24:14 -0400

    Import fixup.

--- api/src/opentrons/simulate.py
+++ api/src/opentrons/simulate.py
@@ -35,7 +35,7 @@ from opentrons.protocol_api import MAX_SUPPORTED_VERSION
 from opentrons.protocols.duration import DurationEstimator
 from opentrons.protocols.execution import execute
 from opentrons.legacy_broker import LegacyBroker
-from opentrons.config import IS_ROBOT, JUPYTER_NOTEBOOK_LABWARE_DIR
+from opentrons.config import IS_ROBOT
 from opentrons import protocol_api
 from opentrons.commands import types as command_types
 
