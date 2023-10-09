bf1be27d559be090ee79707ed9610606da5e508c
koji <koji.kanao@opentrons.com>
2023-10-09 10:00:56 -0400

    fix(app): fix module calibration slot selection rendering for tc
    modify svg rendering part for tc and modify the text position.
    
    RQA-1494

--- components/src/hooks/useSelectDeckLocation/index.tsx
+++ components/src/hooks/useSelectDeckLocation/index.tsx
@@ -37,12 +37,14 @@ interface DeckLocationSelectProps {
   selectedLocation: ModuleLocation
   setSelectedLocation: (loc: ModuleLocation) => void
   disabledLocations?: ModuleLocation[]
+  isThermocycler?: boolean
 }
 export function DeckLocationSelect({
   deckDef,
   selectedLocation,
   setSelectedLocation,
   disabledLocations = [],
+  isThermocycler = false,
 }: DeckLocationSelectProps): JSX.Element {
   return (
     <RobotCoordinateSpace
@@ -60,22 +62,18 @@ export function DeckLocationSelect({
         let fill = COLORS.highlightPurple2
         if (isSelected) fill = COLORS.highlightPurple1
         if (isDisabled) fill = COLORS.darkGreyDisabled
-        return (
-          <React.Fragment key={slot.id}>
-            <DeckSlotLocation
-              slotName={slot.id}
-              slotBaseColor={fill}
-              slotClipColor={COLORS.white}
-              onClick={() => !isDisabled && setSelectedLocation(slotLocation)}
-              cursor={isDisabled || isSelected ? 'default' : 'pointer'}
-              deckDefinition={deckDef}
-            />
-            {isSelected ? (
+        if (isSelected && slot.id === 'B1' && isThermocycler) {
+          return (
+            <g>
+              <path
+                fill={fill}
+                d="M-97.8,496.6h239c2.3,0,4.2-1.9,4.2-4.2v-282c0-2.3-1.9-4.2-4.2-4.2h-239c-2.3,0-4.2,1.9-4.2,4.2v282 C-102,494.7-100.1,496.6-97.8,496.6z"
+              />
               <RobotCoordsForeignDiv
-                x={slot.position[0]}
-                y={slot.position[1]}
+                x="-50"
+                y="214"
                 width={slot.boundingBox.xDimension}
-                height={slot.boundingBox.yDimension}
+                height="282"
                 innerDivProps={{
                   display: 'flex',
                   alignItems: ALIGN_CENTER,
@@ -89,6 +87,51 @@ export function DeckLocationSelect({
                   Selected
                 </Text>
               </RobotCoordsForeignDiv>
+            </g>
+          )
+        } else if (slot.id === 'A1' && isThermocycler) {
+          return null
+        }
+        return (
+          <React.Fragment key={slot.id}>
+            <DeckSlotLocation
+              slotName={slot.id}
+              slotBaseColor={fill}
+              slotClipColor={COLORS.white}
+              onClick={() => !isDisabled && setSelectedLocation(slotLocation)}
+              cursor={isDisabled || isSelected ? 'default' : 'pointer'}
+              deckDefinition={deckDef}
+              showExtensions={isThermocycler}
+            />
+            {isSelected ? (
+              <g>
+                <path
+                  fill={fill}
+                  d="M-97.8,496.6h239c2.3,0,4.2-1.9,4.2-4.2v-70c0-2.3-1.9-4.2-4.2-4.2h-239c-2.3,0-4.2,1.9-4.2,4.2v70 C-102,494.7-100.1,496.6-97.8,496.6z"
+                />
+                <RobotCoordsForeignDiv
+                  x={isThermocycler ? '-65' : slot.position[0]}
+                  y={slot.position[1]}
+                  width={slot.boundingBox.xDimension}
+                  height={slot.boundingBox.yDimension}
+                  innerDivProps={{
+                    display: 'flex',
+                    alignItems: ALIGN_CENTER,
+                    justifyContent: JUSTIFY_CENTER,
+                    height: '100%',
+                    gridGap: SPACING.spacing4,
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    size="1.5rem"
+                    color={COLORS.white}
+                  />
+                  <Text color={COLORS.white} fontSize="1.5rem">
+                    Selected
+                  </Text>
+                </RobotCoordsForeignDiv>
+              </g>
             ) : null}
           </React.Fragment>
         )
