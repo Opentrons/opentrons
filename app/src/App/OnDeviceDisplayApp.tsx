7d25d2d1d195c89bdea4b52bfde34b49486da38b
Seth Foster <seth@opentrons.com>
2023-10-09 17:28:00 -0400

    add a comment

--- app/src/App/OnDeviceDisplayApp.tsx
+++ app/src/App/OnDeviceDisplayApp.tsx
@@ -50,11 +50,17 @@ import { OnDeviceDisplayAppFallback } from './OnDeviceDisplayAppFallback'
 import type { Dispatch } from '../redux/types'
 import type { RouteProps } from './types'
 
+// If the system boots while no network connection is available, then some requests to localhost
+// hang eternally while connecting (so no request timeouts work either) and things get
+// generally weird. Overriding the browser API to pretend to always be "online" fixes this.
+// It makes sense; if "onLine" is false, that means that any network call is _guaranteed_ to fail
+// so middlewares probably elide them; but we really want it to be true basically always because
+// most of what we do is via localhost.
+
 Object.defineProperty(window.navigator, 'onLine', {
-  get: () => true
+  get: () => true,
 })
 window.dispatchEvent(new Event('online'))
-
 
 export const onDeviceDisplayRoutes: RouteProps[] = [
   {
