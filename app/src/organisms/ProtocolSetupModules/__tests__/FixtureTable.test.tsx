ac3ca5166198e80d843912be8af8d9e964134709
koji <koji.kanao@opentrons.com>
2023-10-10 19:04:16 -0400

    add trash bin and waste chute as mock data

--- app/src/organisms/ProtocolSetupModules/__tests__/FixtureTable.test.tsx
+++ app/src/organisms/ProtocolSetupModules/__tests__/FixtureTable.test.tsx
@@ -3,7 +3,11 @@ import { UseQueryResult } from 'react-query'
 
 import { renderWithProviders } from '@opentrons/components'
 import { useDeckConfigurationQuery } from '@opentrons/react-api-client'
-import { STAGING_AREA_LOAD_NAME } from '@opentrons/shared-data'
+import {
+  STAGING_AREA_LOAD_NAME,
+  TRASH_BIN_LOAD_NAME,
+  WASTE_CHUTE_LOAD_NAME,
+} from '@opentrons/shared-data'
 
 import { i18n } from '../../../i18n'
 import { FixtureTable } from '../FixtureTable'
@@ -12,11 +16,23 @@ import type { DeckConfiguration, Fixture } from '@opentrons/shared-data'
 
 jest.mock('@opentrons/react-api-client')
 
-const mockFixture = {
-  fixtureId: 'mockId',
-  fixtureLocation: 'B3',
-  loadName: STAGING_AREA_LOAD_NAME,
-} as Fixture
+const mockFixture = [
+  {
+    fixtureId: 'mockId',
+    fixtureLocation: 'B3',
+    loadName: STAGING_AREA_LOAD_NAME,
+  },
+  {
+    fixtureId: 'mockId',
+    fixtureLocation: 'A1',
+    loadName: TRASH_BIN_LOAD_NAME,
+  },
+  {
+    fixtureId: 'mockId',
+    fixtureLocation: 'D3',
+    loadName: WASTE_CHUTE_LOAD_NAME,
+  },
+] as Fixture[]
 
 const mockUseDeckConfigurationQuery = useDeckConfigurationQuery as jest.MockedFunction<
   typeof useDeckConfigurationQuery
@@ -29,15 +45,19 @@ const render = () => {
 describe('FixtureTable', () => {
   beforeEach(() => {
     mockUseDeckConfigurationQuery.mockReturnValue({
-      data: [mockFixture],
+      data: mockFixture,
     } as UseQueryResult<DeckConfiguration>)
   })
-  it('should render table header and content', () => {
+  it('should render table header and contents', () => {
     const [{ getByText }] = render()
     getByText('Fixture')
     getByText('Location')
     getByText('Status')
     getByText('Staging Area Slot')
     getByText('B3')
+    getByText('Trash Bin')
+    getByText('A1')
+    getByText('Waste Chute')
+    getByText('D3')
   })
 })
