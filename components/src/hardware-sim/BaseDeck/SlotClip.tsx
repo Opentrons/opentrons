aca2cc8e95ca934b87aedb50de760f7da311be17
Brent Hagen <brent.hagen@opentrons.com>
2023-10-11 17:11:22 -0400

    update SlotClip stroke default constant

--- components/src/hardware-sim/BaseDeck/SlotClip.tsx
+++ components/src/hardware-sim/BaseDeck/SlotClip.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 
+import { COLORS } from '../../ui-style-constants'
+
 export function SlotClip(props: React.SVGProps<SVGPathElement>): JSX.Element {
   return (
     <path
       fill="none"
-      stroke="#16212D"
+      stroke={COLORS.darkBlackEnabled}
       strokeWidth={3}
       strokeOpacity={0.7}
       {...props}
