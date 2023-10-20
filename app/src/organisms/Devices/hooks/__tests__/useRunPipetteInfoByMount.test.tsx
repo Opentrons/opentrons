3b7c7aeba4c6941e1bd7dded62ea7fbc922f1822
Brian Cooper <brian@opentrons.com>
2023-10-20 10:56:19 -0400

    fix up final test fixture

--- app/src/organisms/Devices/hooks/__tests__/useRunPipetteInfoByMount.test.tsx
+++ app/src/organisms/Devices/hooks/__tests__/useRunPipetteInfoByMount.test.tsx
@@ -108,7 +108,7 @@ const modifiedSimpleV6Protocol = ({
     },
     {
       id: 'tipRackId',
-      displayName: 'Opentrons 96 Tip Rack 10 µL',
+      displayName: 'Opentrons OT-2 96 Tip Rack 10 µL',
       definitionUri: 'opentrons/opentrons_96_tiprack_10ul/1',
       loadName: 'opentrons_96_tiprack_10ul',
     },
