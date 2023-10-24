d03b1855688d41fadbbedb5b38943de1ddbda972
Frank Sinapi <frank.sinapi@opentrons.com>
2023-10-24 10:47:27 -0400

    Fix calibration functions ot2 test

--- api/tests/opentrons/hardware_control/test_calibration_functions.py
+++ api/tests/opentrons/hardware_control/test_calibration_functions.py
@@ -12,14 +12,6 @@ from opentrons.util.helpers import utc_now
 from opentrons.types import Mount, Point
 
 
-# TODO(mc, 2023-02-17): this reload resolves test polution from repeated
-# reloads of this module in tests/opentrons/calibration_storage.
-# module reloads to be removed in https://github.com/Opentrons/opentrons/pull/12049
-@pytest.fixture(autouse=True, scope="module")
-def reload_module() -> None:
-    importlib.reload(calibration_storage)
-
-
 def test_migrate_affine_xy_to_attitude():
     affine = [
         [1.0, 2.0, 3.0, 4.0],
@@ -97,7 +89,7 @@ def test_load_pipette_offset(ot_config_tempdir):
     mount = Mount.LEFT
     offset = Point(1, 2, 3)
 
-    calibration_storage.save_pipette_calibration(
+    calibration_storage.ot2.save_pipette_calibration(
         offset, pip_id, mount, "hash", "opentrons/opentrons_96_tiprack_10ul/1"
     )
     obj = instrument_calibration.load_pipette_offset(pip_id, mount)
