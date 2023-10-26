aa73b8d617c9f94f6f7d22016ff871f3e86fc21b
Brent Hagen <brent.hagen@opentrons.com>
2023-10-26 11:27:41 -0400

    feat(app,components): render modules and labware in BaseDeck (#13829)
    updates BaseDeck to render modules and labware in the app. uses an instance of BaseDeck on the
    protocol run setup modules & deck map tab.
    
    closes RAUT-793

--- app/src/organisms/Devices/ProtocolRun/SetupLabware/SetupLabwareMap.tsx
+++ app/src/organisms/Devices/ProtocolRun/SetupLabware/SetupLabwareMap.tsx
@@ -1,163 +1,163 @@
 import * as React from 'react'
 import map from 'lodash/map'
 
+import { parseInitialLoadedLabwareByAdapter } from '@opentrons/api-client'
 import {
+  BaseDeck,
   Flex,
   Box,
-  LabwareRender,
-  Module,
-  RobotWorkSpace,
-  SlotLabels,
   DIRECTION_COLUMN,
   SPACING,
+  EXTENDED_DECK_CONFIG_FIXTURE,
 } from '@opentrons/components'
 import {
   FLEX_ROBOT_TYPE,
   getDeckDefFromRobotType,
-  inferModuleOrientationFromXCoordinate,
-  RunTimeCommand,
+  getRobotTypeFromLoadedLabware,
   THERMOCYCLER_MODULE_V1,
 } from '@opentrons/shared-data'
-import {
-  useLabwareRenderInfoForRunById,
-  useModuleRenderInfoForProtocolById,
-  useProtocolDetailsForRun,
-} from '../../hooks'
-import { LabwareInfoOverlay } from '../LabwareInfoOverlay'
-import { getStandardDeckViewLayerBlockList } from '../utils/getStandardDeckViewLayerBlockList'
+
 import { getLabwareSetupItemGroups } from '../../../../pages/Protocols/utils'
+import { useFeatureFlag } from '../../../../redux/config'
+import { getDeckConfigFromProtocolCommands } from '../../../../resources/deck_configuration/utils'
+import { getAttachedProtocolModuleMatches } from '../../../ProtocolSetupModulesAndDeck/utils'
+import { useAttachedModules } from '../../hooks'
+import { LabwareInfoOverlay } from '../LabwareInfoOverlay'
+import { getLabwareRenderInfo } from '../utils/getLabwareRenderInfo'
+import { getProtocolModulesInfo } from '../utils/getProtocolModulesInfo'
+import { getStandardDeckViewLayerBlockList } from '../utils/getStandardDeckViewLayerBlockList'
 import { OffDeckLabwareList } from './OffDeckLabwareList'
-import { parseInitialLoadedLabwareByAdapter } from '@opentrons/api-client'
+
+import type {
+  CompletedProtocolAnalysis,
+  ProtocolAnalysisOutput,
+} from '@opentrons/shared-data'
+
+const ATTACHED_MODULE_POLL_MS = 5000
 
 interface SetupLabwareMapProps {
-  robotName: string
   runId: string
-  commands: RunTimeCommand[]
+  protocolAnalysis: CompletedProtocolAnalysis | ProtocolAnalysisOutput | null
 }
 
 export function SetupLabwareMap({
-  robotName,
   runId,
-  commands,
-}: SetupLabwareMapProps): JSX.Element {
-  const moduleRenderInfoById = useModuleRenderInfoForProtocolById(
-    robotName,
-    runId
-  )
-  const { robotType } = useProtocolDetailsForRun(runId)
-  const labwareRenderInfoById = useLabwareRenderInfoForRunById(runId)
+  protocolAnalysis,
+}: SetupLabwareMapProps): JSX.Element | null {
+  const attachedModules =
+    useAttachedModules({
+      refetchInterval: ATTACHED_MODULE_POLL_MS,
+    }) ?? []
+
+  const enableDeckConfig = useFeatureFlag('enableDeckConfiguration')
+
+  // early return null if no protocol analysis
+  if (protocolAnalysis == null) return null
+
+  const commands = protocolAnalysis.commands
+
+  const robotType = getRobotTypeFromLoadedLabware(protocolAnalysis.labware)
   const deckDef = getDeckDefFromRobotType(robotType)
-  const { offDeckItems } = getLabwareSetupItemGroups(commands)
+
+  const protocolModulesInfo = getProtocolModulesInfo(protocolAnalysis, deckDef)
+
+  const attachedProtocolModuleMatches = getAttachedProtocolModuleMatches(
+    attachedModules,
+    protocolModulesInfo
+  )
+
   const initialLoadedLabwareByAdapter = parseInitialLoadedLabwareByAdapter(
     commands
   )
+
+  const moduleLocations = attachedProtocolModuleMatches.map(module => {
+    const labwareInAdapterInMod =
+      module.nestedLabwareId != null
+        ? initialLoadedLabwareByAdapter[module.nestedLabwareId]
+        : null
+    //  only rendering the labware on top most layer so
+    //  either the adapter or the labware are rendered but not both
+    const topLabwareDefinition =
+      labwareInAdapterInMod?.result?.definition ?? module.nestedLabwareDef
+    const topLabwareId =
+      labwareInAdapterInMod?.result?.labwareId ?? module.nestedLabwareId
+    const topLabwareDisplayName =
+      labwareInAdapterInMod?.params.displayName ??
+      module.nestedLabwareDisplayName
+
+    return {
+      moduleModel: module.moduleDef.model,
+      moduleLocation: { slotName: module.slotName },
+      innerProps:
+        module.moduleDef.model === THERMOCYCLER_MODULE_V1
+          ? { lidMotorState: 'open' }
+          : {},
+
+      nestedLabwareDef: topLabwareDefinition,
+      moduleChildren: (
+        <>
+          {topLabwareDefinition != null && topLabwareId != null ? (
+            <LabwareInfoOverlay
+              definition={topLabwareDefinition}
+              labwareId={topLabwareId}
+              displayName={topLabwareDisplayName}
+              runId={runId}
+            />
+          ) : null}
+        </>
+      ),
+    }
+  })
+
+  const { offDeckItems } = getLabwareSetupItemGroups(commands)
+
+  // hide the extended deck behind feature flag to avoid confustion
+  const deckConfig = enableDeckConfig
+    ? EXTENDED_DECK_CONFIG_FIXTURE
+    : getDeckConfigFromProtocolCommands(protocolAnalysis.commands)
+
+  const labwareRenderInfo = getLabwareRenderInfo(protocolAnalysis, deckDef)
+
+  const labwareLocations = map(
+    labwareRenderInfo,
+    ({ x, y, labwareDef, displayName, slotName }, labwareId) => {
+      const labwareInAdapter = initialLoadedLabwareByAdapter[labwareId]
+      //  only rendering the labware on top most layer so
+      //  either the adapter or the labware are rendered but not both
+      const topLabwareDefinition =
+        labwareInAdapter?.result?.definition ?? labwareDef
+      const topLabwareId = labwareInAdapter?.result?.labwareId ?? labwareId
+      const topLabwareDisplayName =
+        labwareInAdapter?.params.displayName ?? displayName
+
+      return {
+        labwareLocation: { slotName },
+        definition: topLabwareDefinition,
+        topLabwareId,
+        topLabwareDisplayName,
+        labwareChildren: (
+          <LabwareInfoOverlay
+            definition={topLabwareDefinition}
+            labwareId={topLabwareId}
+            displayName={topLabwareDisplayName}
+            runId={runId}
+          />
+        ),
+      }
+    }
+  )
+
   return (
     <Flex flex="1" maxHeight="180vh" flexDirection={DIRECTION_COLUMN}>
       <Flex flexDirection={DIRECTION_COLUMN} marginY={SPACING.spacing16}>
         <Box margin="0 auto" maxWidth="46.25rem" width="100%">
-          <RobotWorkSpace
-            deckDef={deckDef}
+          <BaseDeck
+            deckConfig={deckConfig}
             deckLayerBlocklist={getStandardDeckViewLayerBlockList(robotType)}
-            deckFill="#e6e6e6"
-            trashSlotName="A3"
-            id="LabwareSetup_deckMap"
-          >
-            {() => (
-              <>
-                {map(
-                  moduleRenderInfoById,
-                  ({
-                    x,
-                    y,
-                    moduleDef,
-                    nestedLabwareDef,
-                    nestedLabwareId,
-                    nestedLabwareDisplayName,
-                    moduleId,
-                  }) => {
-                    const labwareInAdapterInMod =
-                      nestedLabwareId != null
-                        ? initialLoadedLabwareByAdapter[nestedLabwareId]
-                        : null
-                    //  only rendering the labware on top most layer so
-                    //  either the adapter or the labware are rendered but not both
-                    const topLabwareDefinition =
-                      labwareInAdapterInMod?.result?.definition ??
-                      nestedLabwareDef
-                    const topLabwareId =
-                      labwareInAdapterInMod?.result?.labwareId ??
-                      nestedLabwareId
-                    const topLabwareDisplayName =
-                      labwareInAdapterInMod?.params.displayName ??
-                      nestedLabwareDisplayName
-
-                    return (
-                      <Module
-                        key={`LabwareSetup_Module_${moduleId}_${x}${y}`}
-                        x={x}
-                        y={y}
-                        orientation={inferModuleOrientationFromXCoordinate(x)}
-                        def={moduleDef}
-                        innerProps={
-                          moduleDef.model === THERMOCYCLER_MODULE_V1
-                            ? { lidMotorState: 'open' }
-                            : {}
-                        }
-                      >
-                        {topLabwareDefinition != null &&
-                        topLabwareId != null ? (
-                          <React.Fragment
-                            key={`LabwareSetup_Labware_${topLabwareId}_${x}${y}`}
-                          >
-                            <LabwareRender definition={topLabwareDefinition} />
-                            <LabwareInfoOverlay
-                              definition={topLabwareDefinition}
-                              labwareId={topLabwareId}
-                              displayName={topLabwareDisplayName}
-                              runId={runId}
-                            />
-                          </React.Fragment>
-                        ) : null}
-                      </Module>
-                    )
-                  }
-                )}
-                {map(
-                  labwareRenderInfoById,
-                  ({ x, y, labwareDef, displayName }, labwareId) => {
-                    const labwareInAdapter =
-                      initialLoadedLabwareByAdapter[labwareId]
-                    //  only rendering the labware on top most layer so
-                    //  either the adapter or the labware are rendered but not both
-                    const topLabwareDefinition =
-                      labwareInAdapter?.result?.definition ?? labwareDef
-                    const topLabwareId =
-                      labwareInAdapter?.result?.labwareId ?? labwareId
-                    const topLabwareDisplayName =
-                      labwareInAdapter?.params.displayName ?? displayName
-
-                    return (
-                      <React.Fragment
-                        key={`LabwareSetup_Labware_${topLabwareId}_${x}${y}`}
-                      >
-                        <g transform={`translate(${x},${y})`}>
-                          <LabwareRender definition={topLabwareDefinition} />
-                          <LabwareInfoOverlay
-                            definition={topLabwareDefinition}
-                            labwareId={topLabwareId}
-                            displayName={topLabwareDisplayName}
-                            runId={runId}
-                          />
-                        </g>
-                      </React.Fragment>
-                    )
-                  }
-                )}
-
-                <SlotLabels robotType={robotType} />
-              </>
-            )}
-          </RobotWorkSpace>
+            robotType={robotType}
+            labwareLocations={labwareLocations}
+            moduleLocations={moduleLocations}
+          />
         </Box>
         <OffDeckLabwareList
           labwareItems={offDeckItems}
