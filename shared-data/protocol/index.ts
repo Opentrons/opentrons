fecc2ba110c2a863d7741ff2f5e7ae1660ae1890
Brian Cooper <brian@opentrons.com>
2023-10-26 11:24:44 -0400

    fix up ts config for protocol types

--- shared-data/protocol/index.ts
+++ shared-data/protocol/index.ts
@@ -5,6 +5,14 @@ import type { ProtocolFile as ProtocolFileV5 } from './types/schemaV5'
 import type { ProtocolFile as ProtocolFileV6 } from './types/schemaV6'
 import type { ProtocolFile as ProtocolFileV7 } from './types/schemaV7'
 
+export {
+  ProtocolFileV1,
+  ProtocolFileV3,
+  ProtocolFileV4,
+  ProtocolFileV5,
+  ProtocolFileV6,
+  ProtocolFileV7
+}
 export type JsonProtocolFile =
   | Readonly<ProtocolFileV1<{}>>
   | Readonly<ProtocolFileV3<{}>>
