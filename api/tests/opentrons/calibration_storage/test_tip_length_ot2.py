2a06a3a914e50531e8269460a728fe195d88b82f
Frank Sinapi <frank.sinapi@opentrons.com>
2023-10-31 11:26:12 -0400

    refactor(api): remove feature-flag dependency from calibration_storage (#13833)
    * Removed FF dependency when importing calibration_storage, moved around where imports are publicized
    * robot server import changes to match new calibration_storage import methodology
    * Split some tests into ot2/ot3 variants
    * Modified settings reset to have to be specified as OT2 or OT3 by caller
    * removed tip length cal utils on OT3 because they are not used ever
    * Fixed robot server calibration flows to avoid importing OT-3 utilities that are irrelevant to OT-2 workflows

--- api/tests/opentrons/calibration_storage/test_tip_length_ot2.py
+++ api/tests/opentrons/calibration_storage/test_tip_length_ot2.py
@@ -1,6 +1,4 @@
 import pytest
-import importlib
-import opentrons
 from typing import cast, Any, TYPE_CHECKING
 
 from opentrons.calibration_storage import (
@@ -8,15 +6,19 @@ from opentrons.calibration_storage import (
     helpers,
 )
 
+from opentrons.calibration_storage.ot2 import (
+    create_tip_length_data,
+    save_tip_length_calibration,
+    tip_lengths_for_pipette,
+    load_tip_length_calibration,
+    delete_tip_length_calibration,
+    clear_tip_length_calibration,
+    models,
+)
+
 if TYPE_CHECKING:
     from opentrons_shared_data.labware.dev_types import LabwareDefinition
     from opentrons_shared_data.pipette.dev_types import LabwareUri
-    from opentrons_shared_data.deck.dev_types import RobotModel
-
-
-@pytest.fixture(autouse=True)
-def reload_module(robot_model: "RobotModel") -> None:
-    importlib.reload(opentrons.calibration_storage)
 
 
 @pytest.fixture
@@ -30,11 +32,6 @@ def starting_calibration_data(
 
     Adds dummy data to a temporary directory to test delete commands against.
     """
-    from opentrons.calibration_storage import (
-        create_tip_length_data,
-        save_tip_length_calibration,
-    )
-
     tip_length1 = create_tip_length_data(minimal_labware_def, 22.0)
     tip_length2 = create_tip_length_data(minimal_labware_def, 31.0)
     tip_length3 = create_tip_length_data(minimal_labware_def2, 31.0)
@@ -49,12 +46,6 @@ def test_save_tip_length_calibration(
     """
     Test saving tip length calibrations.
     """
-    from opentrons.calibration_storage import (
-        tip_lengths_for_pipette,
-        create_tip_length_data,
-        save_tip_length_calibration,
-    )
-
     assert tip_lengths_for_pipette("pip1") == {}
     assert tip_lengths_for_pipette("pip2") == {}
     tip_rack_hash = helpers.hash_labware_def(minimal_labware_def)
@@ -72,8 +63,6 @@ def test_get_tip_length_calibration(
     """
     Test ability to get a tip length calibration model.
     """
-    from opentrons.calibration_storage import load_tip_length_calibration, models
-
     tip_length_data = load_tip_length_calibration("pip1", minimal_labware_def)
     assert tip_length_data == models.v1.TipLengthModel(
         tipLength=22.0,
@@ -92,11 +81,6 @@ def test_delete_specific_tip_calibration(
     """
     Test delete a specific tip length calibration.
     """
-    from opentrons.calibration_storage import (
-        tip_lengths_for_pipette,
-        delete_tip_length_calibration,
-    )
-
     assert len(tip_lengths_for_pipette("pip1").keys()) == 2
     assert tip_lengths_for_pipette("pip2") != {}
     tip_rack_hash = helpers.hash_labware_def(minimal_labware_def)
@@ -109,11 +93,6 @@ def test_delete_all_tip_calibration(starting_calibration_data: Any) -> None:
     """
     Test delete all tip length calibration.
     """
-    from opentrons.calibration_storage import (
-        tip_lengths_for_pipette,
-        clear_tip_length_calibration,
-    )
-
     assert tip_lengths_for_pipette("pip1") != {}
     assert tip_lengths_for_pipette("pip2") != {}
     clear_tip_length_calibration()
