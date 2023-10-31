3eb7438029113f3c257d3859465addb016a86955
koji <koji.kanao@opentrons.com>
2023-10-31 11:44:11 -0400

    feat(app): update deck map modal in protocol setup (#13860)
    * feat(app): update deck map in protocol setup

---
+++ app/src/organisms/ProtocolSetupModulesAndDeck/ModulesAndDeckMapViewModal.tsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import {
+  BaseDeck,
+  RobotWorkSpace,
+  COLORS,
+  Module,
+  SlotLabels,
+} from '@opentrons/components'
+import {
+  inferModuleOrientationFromXCoordinate,
+  FLEX_ROBOT_TYPE,
+} from '@opentrons/shared-data'
+
+import { Modal } from '../../molecules/Modal'
+import { ModuleInfo } from '../Devices/ModuleInfo'
+import { ROBOT_MODEL_OT3 } from '../../redux/discovery' // need to switch if needed to use this
+import { useFeatureFlag } from '../../redux/config'
+import { getDeckConfigFromProtocolCommands } from '../../resources/deck_configuration/utils'
+import { getStandardDeckViewLayerBlockList } from '../Devices/ProtocolRun/utils/getStandardDeckViewLayerBlockList'
+
+import type {
+  CompletedProtocolAnalysis,
+  DeckDefinition,
+} from '@opentrons/shared-data'
+import type { ModalHeaderBaseProps } from '../../molecules/Modal/types'
+import type { AttachedProtocolModuleMatch } from './utils'
+
+const OT3_STANDARD_DECK_VIEW_LAYER_BLOCK_LIST: string[] = [
+  'DECK_BASE',
+  'BARCODE_COVERS',
+  'SLOT_SCREWS',
+  'SLOT_10_EXPANSION',
+  'CALIBRATION_CUTOUTS',
+]
+
+// Note (kk:10/26/2023) once we are ready for removing ff, we will be able to update props
+interface ModulesAndDeckMapViewModalProps {
+  setShowDeckMapModal: (showDeckMapModal: boolean) => void
+  attachedProtocolModuleMatches: AttachedProtocolModuleMatch[]
+  runId: string
+  deckDef: DeckDefinition
+  mostRecentAnalysis?: CompletedProtocolAnalysis | null
+}
+
+export function ModulesAndDeckMapViewModal({
+  setShowDeckMapModal,
+  attachedProtocolModuleMatches,
+  runId,
+  deckDef,
+  mostRecentAnalysis,
+}: ModulesAndDeckMapViewModalProps): JSX.Element | null {
+  const { t } = useTranslation('protocol_setup')
+  const enableDeckConfig = useFeatureFlag('enableDeckConfiguration')
+
+  const modalHeader: ModalHeaderBaseProps = {
+    title: t('map_view'),
+    hasExitIcon: true,
+  }
+
+  const protocolAnalysis = mostRecentAnalysis
+
+  if (protocolAnalysis == null) return null
+
+  const deckConfig = getDeckConfigFromProtocolCommands(
+    protocolAnalysis.commands
+  )
+
+  const moduleLocations = attachedProtocolModuleMatches.map(module => ({
+    moduleModel: module.moduleDef.model,
+    moduleLocation: { slotName: module.slotName },
+    moduleChildren: (
+      <ModuleInfo
+        moduleModel={module.moduleDef.model}
+        isAttached={module.attachedModuleMatch != null}
+        physicalPort={module.attachedModuleMatch?.usbPort ?? null}
+        runId={runId}
+      />
+    ),
+  }))
+
+  return (
+    <Modal
+      header={modalHeader}
+      modalSize="large"
+      onOutsideClick={() => setShowDeckMapModal(false)}
+    >
+      {/* we will be able to remove this once we are ready for removing the ff */}
+      {enableDeckConfig ? (
+        <BaseDeck
+          deckConfig={deckConfig}
+          deckLayerBlocklist={getStandardDeckViewLayerBlockList(
+            FLEX_ROBOT_TYPE
+          )}
+          robotType={FLEX_ROBOT_TYPE}
+          labwareLocations={[]}
+          moduleLocations={moduleLocations}
+        />
+      ) : (
+        <RobotWorkSpace
+          deckDef={deckDef}
+          deckLayerBlocklist={OT3_STANDARD_DECK_VIEW_LAYER_BLOCK_LIST}
+          deckFill={COLORS.light1}
+          trashSlotName="A3"
+          id="ModuleSetup_deckMap"
+          trashColor={COLORS.darkGreyEnabled}
+        >
+          {() => (
+            <>
+              {attachedProtocolModuleMatches.map(module => (
+                <Module
+                  key={module.moduleId}
+                  x={module.x}
+                  y={module.y}
+                  orientation={inferModuleOrientationFromXCoordinate(module.x)}
+                  def={module.moduleDef}
+                >
+                  <ModuleInfo
+                    moduleModel={module.moduleDef.model}
+                    isAttached={module.attachedModuleMatch != null}
+                    physicalPort={module.attachedModuleMatch?.usbPort ?? null}
+                    runId={runId}
+                  />
+                </Module>
+              ))}
+              <SlotLabels robotType={ROBOT_MODEL_OT3} />
+            </>
+          )}
+        </RobotWorkSpace>
+      )}
+    </Modal>
+  )
+}
---
+++ app/src/organisms/ProtocolSetupModulesAndDeck/__tests__/ModulesAndDeckMapViewModal.test.tsx
@@ -0,0 +1,148 @@
+import * as React from 'react'
+import { when, resetAllWhenMocks } from 'jest-when'
+
+import {
+  renderWithProviders,
+  BaseDeck,
+  RobotWorkSpace,
+  EXTENDED_DECK_CONFIG_FIXTURE,
+} from '@opentrons/components'
+
+import { i18n } from '../../../i18n'
+import { useFeatureFlag } from '../../../redux/config'
+import { getDeckConfigFromProtocolCommands } from '../../../resources/deck_configuration/utils'
+import { ModulesAndDeckMapViewModal } from '../ModulesAndDeckMapViewModal'
+
+jest.mock('@opentrons/components/src/hardware-sim/BaseDeck')
+jest.mock('@opentrons/components/src/hardware-sim/Deck/RobotWorkSpace')
+jest.mock('@opentrons/api-client')
+jest.mock('../../../redux/config')
+jest.mock('../../Devices/hooks')
+jest.mock('../../../resources/deck_configuration/utils')
+jest.mock('../../Devices/ModuleInfo')
+jest.mock('../../Devices/ProtocolRun/utils/getLabwareRenderInfo')
+
+const mockRunId = 'mockRunId'
+const mockSetShowDeckMapModal = jest.fn()
+const PROTOCOL_ANALYSIS = {
+  id: 'fake analysis',
+  status: 'completed',
+  labware: [],
+} as any
+
+const mockAttachedProtocolModuleMatches = [
+  {
+    moduleId: 'mockModuleId',
+    x: 328,
+    y: 107,
+    z: 0,
+    moduleDef: {
+      $otSharedSchema: 'module/schemas/3',
+      moduleType: 'magneticBlockType',
+      model: 'magneticBlockV1',
+      labwareOffset: {
+        x: 0,
+        y: 0,
+        z: 38,
+      },
+      dimensions: {
+        bareOverallHeight: 45,
+        overLabwareHeight: 0,
+        xDimension: 136,
+        yDimension: 94,
+        footprintXDimension: 127.75,
+        footprintYDimension: 85.75,
+        labwareInterfaceXDimension: 128,
+        labwareInterfaceYDimension: 86,
+      },
+      cornerOffsetFromSlot: {
+        x: -4.125,
+        y: -4.125,
+        z: 0,
+      },
+      calibrationPoint: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      config: {},
+      gripperOffsets: {},
+      displayName: 'Magnetic Block GEN1',
+      quirks: [],
+      slotTransforms: {
+        ot2_standard: {},
+        ot2_short_trash: {},
+        ot3_standard: {},
+      },
+      compatibleWith: [],
+      twoDimensionalRendering: {},
+    },
+    nestedLabwareDef: null,
+    nestedLabwareDisplayName: null,
+    nestedLabwareId: null,
+    protocolLoadOrder: 0,
+    slotName: 'C3',
+    attachedModuleMatch: null,
+  },
+] as any
+
+const render = (
+  props: React.ComponentProps<typeof ModulesAndDeckMapViewModal>
+) => {
+  return renderWithProviders(<ModulesAndDeckMapViewModal {...props} />, {
+    i18nInstance: i18n,
+  })[0]
+}
+
+const mockUseFeatureFlag = useFeatureFlag as jest.MockedFunction<
+  typeof useFeatureFlag
+>
+const mockBaseDeck = BaseDeck as jest.MockedFunction<typeof BaseDeck>
+const mockRobotWorkSpace = RobotWorkSpace as jest.MockedFunction<
+  typeof RobotWorkSpace
+>
+const mockGetDeckConfigFromProtocolCommands = getDeckConfigFromProtocolCommands as jest.MockedFunction<
+  typeof getDeckConfigFromProtocolCommands
+>
+
+describe('ModulesAndDeckMapViewModal', () => {
+  let props: React.ComponentProps<typeof ModulesAndDeckMapViewModal>
+
+  beforeEach(() => {
+    props = {
+      setShowDeckMapModal: mockSetShowDeckMapModal,
+      attachedProtocolModuleMatches: mockAttachedProtocolModuleMatches,
+      deckDef: {} as any,
+      runId: mockRunId,
+      mostRecentAnalysis: PROTOCOL_ANALYSIS,
+    }
+    when(mockUseFeatureFlag)
+      .calledWith('enableDeckConfiguration')
+      .mockReturnValue(true)
+    when(mockGetDeckConfigFromProtocolCommands).mockReturnValue(
+      EXTENDED_DECK_CONFIG_FIXTURE
+    )
+    mockBaseDeck.mockReturnValue(<div>mock BaseDeck</div>)
+    mockRobotWorkSpace.mockReturnValue(<div>mock RobotWorkSpace</div>)
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+    resetAllWhenMocks()
+  })
+
+  it('should render map view when ff is on', () => {
+    const { getByText } = render(props)
+    getByText('Map View')
+    getByText('mock BaseDeck')
+  })
+
+  it('should render map view when ff is off', () => {
+    when(mockUseFeatureFlag)
+      .calledWith('enableDeckConfiguration')
+      .mockReturnValue(false)
+    const { getByText } = render(props)
+    getByText('Map View')
+    getByText('mock RobotWorkSpace')
+  })
+})
