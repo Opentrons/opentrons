39372b1ad2e583cf9c8d8d10bf883cdfcbf21aaf
koji <koji.kanao@opentrons.com>
2023-11-07 15:43:02 -0500

    feat(app): add robot is busy banner to device details (#13932)
    * feat(app): add robot is busy banner to device details

--- app/src/organisms/DeviceDetailsDeckConfiguration/index.tsx
+++ app/src/organisms/DeviceDetailsDeckConfiguration/index.tsx
@@ -17,6 +17,7 @@ import {
   TYPOGRAPHY,
 } from '@opentrons/components'
 import {
+  useCurrentMaintenanceRun,
   useDeckConfigurationQuery,
   useUpdateDeckConfigurationMutation,
 } from '@opentrons/react-api-client'
@@ -32,6 +33,8 @@ import { AddFixtureModal } from './AddFixtureModal'
 import { useRunStatuses } from '../Devices/hooks'
 
 import type { Cutout } from '@opentrons/shared-data'
+
+const RUN_REFETCH_INTERVAL = 5000
 
 interface DeviceDetailsDeckConfigurationProps {
   robotName: string
@@ -56,6 +59,10 @@ export function DeviceDetailsDeckConfiguration({
   const deckConfig = useDeckConfigurationQuery().data ?? []
   const { updateDeckConfiguration } = useUpdateDeckConfigurationMutation()
   const { isRunRunning } = useRunStatuses()
+  const { data: maintenanceRunData } = useCurrentMaintenanceRun({
+    refetchInterval: RUN_REFETCH_INTERVAL,
+  })
+  const isMaintenanceRunExisting = maintenanceRunData?.data?.id != null
 
   const handleClickAdd = (fixtureLocation: Cutout): void => {
     setTargetFixtureLocation(fixtureLocation)
@@ -124,13 +131,22 @@ export function DeviceDetailsDeckConfiguration({
           gridGap={SPACING.spacing16}
           paddingX={SPACING.spacing16}
           paddingBottom={SPACING.spacing32}
-          paddingTop={isRunRunning ? undefined : SPACING.spacing32}
+          paddingTop={
+            isRunRunning || isMaintenanceRunExisting
+              ? undefined
+              : SPACING.spacing32
+          }
           width="100%"
           flexDirection={DIRECTION_COLUMN}
         >
           {isRunRunning ? (
             <Banner type="warning">
-              {t('deck_configuration_is_not_available')}
+              {t('deck_configuration_is_not_available_when_run_is_in_progress')}
+            </Banner>
+          ) : null}
+          {isMaintenanceRunExisting ? (
+            <Banner type="warning">
+              {t('deck_configuration_is_not_available_when_robot_is_busy')}
             </Banner>
           ) : null}
           <Flex gridGap={SPACING.spacing40}>
@@ -142,7 +158,7 @@ export function DeviceDetailsDeckConfiguration({
               flexDirection={DIRECTION_COLUMN}
             >
               <DeckConfigurator
-                readOnly={isRunRunning}
+                readOnly={isRunRunning || isMaintenanceRunExisting}
                 deckConfig={deckConfig}
                 handleClickAdd={handleClickAdd}
                 handleClickRemove={handleClickRemove}
