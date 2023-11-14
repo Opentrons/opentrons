cf9b4885887c6e450ee3d886e06d054627f380e9
Brent Hagen <brent.hagen@opentrons.com>
2023-11-14 18:14:03 -0500

    fix ProtocolSetupDeckConfiguration test

--- app/src/organisms/ProtocolSetupDeckConfiguration/__tests__/ProtocolSetupDeckConfiguration.test.tsx
+++ app/src/organisms/ProtocolSetupDeckConfiguration/__tests__/ProtocolSetupDeckConfiguration.test.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react'
 import { when, resetAllWhenMocks } from 'jest-when'
 
-import { renderWithProviders, DeckConfigurator } from '@opentrons/components'
+import { renderWithProviders, BaseDeck } from '@opentrons/components'
 import { useUpdateDeckConfigurationMutation } from '@opentrons/react-api-client'
 
 import { i18n } from '../../../i18n'
 import { useMostRecentCompletedAnalysis } from '../../LabwarePositionCheck/useMostRecentCompletedAnalysis'
 import { ProtocolSetupDeckConfiguration } from '..'
 
-jest.mock('@opentrons/components/src/hardware-sim/DeckConfigurator/index')
+jest.mock('@opentrons/components/src/hardware-sim/BaseDeck/index')
 jest.mock('@opentrons/react-api-client')
 jest.mock('../../LabwarePositionCheck/useMostRecentCompletedAnalysis')
 
 const mockSetSetupScreen = jest.fn()
 const mockUpdateDeckConfiguration = jest.fn()
-const mockCreateDeckConfiguration = jest.fn()
 const PROTOCOL_DETAILS = {
   displayName: 'fake protocol',
   protocolData: [],
@@ -22,15 +21,13 @@ const PROTOCOL_DETAILS = {
   robotType: 'OT-3 Standard' as const,
 }
 
-const mockDeckConfigurator = DeckConfigurator as jest.MockedFunction<
-  typeof DeckConfigurator
->
 const mockUseMostRecentCompletedAnalysis = useMostRecentCompletedAnalysis as jest.MockedFunction<
   typeof useMostRecentCompletedAnalysis
 >
 const mockUseUpdateDeckConfigurationMutation = useUpdateDeckConfigurationMutation as jest.MockedFunction<
   typeof useUpdateDeckConfigurationMutation
 >
+const mockBaseDeck = BaseDeck as jest.MockedFunction<typeof BaseDeck>
 
 const render = (
   props: React.ComponentProps<typeof ProtocolSetupDeckConfiguration>
@@ -50,7 +47,7 @@ describe('ProtocolSetupDeckConfiguration', () => {
       setSetupScreen: mockSetSetupScreen,
       providedFixtureOptions: [],
     }
-    mockDeckConfigurator.mockReturnValue(<div>mock DeckConfigurator</div>)
+    mockBaseDeck.mockReturnValue(<div>mock BaseDeck</div>)
     when(mockUseMostRecentCompletedAnalysis)
       .calledWith('mockRunId')
       .mockReturnValue(PROTOCOL_DETAILS.protocolData as any)
@@ -66,7 +63,7 @@ describe('ProtocolSetupDeckConfiguration', () => {
   it('should render text, button, and DeckConfigurator', () => {
     const [{ getByText }] = render(props)
     getByText('Deck configuration')
-    getByText('mock DeckConfigurator')
+    getByText('mock BaseDeck')
     getByText('Confirm')
   })
 
@@ -79,6 +76,6 @@ describe('ProtocolSetupDeckConfiguration', () => {
   it('should call a mock function when tapping confirm button', () => {
     const [{ getByText }] = render(props)
     getByText('Confirm').click()
-    expect(mockCreateDeckConfiguration).toHaveBeenCalled()
+    expect(mockUpdateDeckConfiguration).toHaveBeenCalled()
   })
 })
