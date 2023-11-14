c8ca7042b2739e34dc4627d9ce591a7696782efa
Max Marrone <max@opentrons.com>
2023-11-14 15:46:39 -0500

    Wire up storage to PUT/GET /deck_configuration endpoints.

--- robot-server/robot_server/deck_configuration/store.py
+++ robot-server/robot_server/deck_configuration/store.py
@@ -1,7 +1,13 @@
 # noqa: D100
 
+import asyncio
 from datetime import datetime
-from typing import List, Optional
+from typing import List
+
+import anyio
+
+from opentrons import calibration_storage
+from opentrons.calibration_storage import types as calibration_storage_types
 
 from . import models
 
@@ -10,19 +16,51 @@ class DeckConfigurationStore:
     """An in-memory stand-in for a persistent store of the robot's deck configuration."""
 
     def __init__(self) -> None:
-        self._cutoutFixtures: List[models.CutoutFixture] = []
-        self._last_updated_at: Optional[datetime] = None
+        # opentrons.calibration_storage is not generally safe for concurrent access.
+        #
+        # TODO(mm, 2023-11-20): The POST /settings/reset endpoint circumvents this lock by directly
+        # interacting with opentrons.calibration_storage.
+        self._calibration_storage_lock = asyncio.Lock()
 
     async def set(
         self, request: models.DeckConfigurationRequest, last_updated_at: datetime
     ) -> models.DeckConfigurationResponse:
         """Set the robot's current deck configuration."""
-        self._cutoutFixtures = request.cutoutFixtures
-        self._last_updated_at = last_updated_at
-        return await self.get()
+        async with self._calibration_storage_lock:
+            calibration_storage.save_robot_deck_configuration(
+                cutout_fixture_placements=[
+                    calibration_storage_types.CutoutFixturePlacement(
+                        cutout_fixture_id=e.cutoutFixtureId, cutout_id=e.cutoutId
+                    )
+                    for e in request.cutoutFixtures
+                ],
+                last_updated_at=last_updated_at,
+            )
+            return await self._get_assuming_locked()
 
     async def get(self) -> models.DeckConfigurationResponse:
         """Get the robot's current deck configuration."""
+        async with self._calibration_storage_lock:
+            return await self._get_assuming_locked()
+
+    async def _get_assuming_locked(self) -> models.DeckConfigurationResponse:
+        from_storage = await anyio.to_thread.run_sync(
+            calibration_storage.get_robot_deck_configuration
+        )
+        if from_storage is None:
+            # TODO: Figure out where and how to populate the default deck configuration.
+            cutout_fixtures: List[models.CutoutFixture] = []
+            last_updated_at = None
+        else:
+            cutout_fixtures_from_storage, last_updated_at = from_storage
+            cutout_fixtures = [
+                models.CutoutFixture.construct(
+                    cutoutFixtureId=e.cutout_fixture_id,
+                    cutoutId=e.cutout_id,
+                )
+                for e in cutout_fixtures_from_storage
+            ]
         return models.DeckConfigurationResponse.construct(
-            cutoutFixtures=self._cutoutFixtures, lastUpdatedAt=self._last_updated_at
+            cutoutFixtures=cutout_fixtures,
+            lastUpdatedAt=last_updated_at,
         )
