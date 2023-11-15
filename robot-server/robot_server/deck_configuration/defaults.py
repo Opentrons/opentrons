033b65f14d7806c48bf64d19f9443a99e0b27624
Max Marrone <max@opentrons.com>
2023-11-15 15:08:08 -0500

    Add the default configuration for the Flex.

--- robot-server/robot_server/deck_configuration/defaults.py
+++ robot-server/robot_server/deck_configuration/defaults.py
@@ -12,7 +12,46 @@ from opentrons_shared_data.robot.dev_types import RobotTypeEnum
 from . import models
 
 
-_for_flex = models.DeckConfigurationRequest.construct(cutoutFixtures=[])
+_for_flex = models.DeckConfigurationRequest.construct(
+    cutoutFixtures=[
+        models.CutoutFixture.construct(
+            cutoutId="cutoutA1", cutoutFixtureId="singleLeftSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutB1", cutoutFixtureId="singleLeftSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutC1", cutoutFixtureId="singleLeftSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutD1", cutoutFixtureId="singleLeftSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutA2", cutoutFixtureId="singleCenterSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutB2", cutoutFixtureId="singleCenterSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutC2", cutoutFixtureId="singleCenterSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutD2", cutoutFixtureId="singleCenterSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutA3", cutoutFixtureId="trashBinAdapter"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutB3", cutoutFixtureId="singleRightSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutC3", cutoutFixtureId="singleRightSlot"
+        ),
+        models.CutoutFixture.construct(
+            cutoutId="cutoutD3", cutoutFixtureId="singleRightSlot"
+        ),
+    ]
+)
 
 
 _for_ot2 = models.DeckConfigurationRequest.construct(cutoutFixtures=[])
