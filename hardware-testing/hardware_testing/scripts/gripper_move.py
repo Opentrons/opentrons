e2243df75a7ba28b2949714b1a37c052f0ba0b0d
Ryan howard <ryan.howard@opentrons.com>
2023-11-15 13:51:01 -0500

    format lint

--- hardware-testing/hardware_testing/scripts/gripper_move.py
+++ hardware-testing/hardware_testing/scripts/gripper_move.py
@@ -3,9 +3,6 @@
 import argparse
 import asyncio
 import time
-from typing import Tuple, Dict, Optional
-import datetime
-import os, sys
 
 from hardware_testing.opentrons_api.types import (
     OT3Mount,
@@ -16,15 +13,15 @@ from hardware_testing.opentrons_api.helpers_ot3 import (
     build_async_ot3_hardware_api,
 )
 
+
 async def _main() -> None:
-    today = datetime.date.today()
     hw_api = await build_async_ot3_hardware_api(
         is_simulating=args.simulate, use_defaults=True
     )
     await asyncio.sleep(1)
     await hw_api.cache_instruments()
     timeout_start = time.time()
-    timeout = 60*60*3
+    timeout = 60 * 60 * 3
     count = 0
     x_offset = 80
     y_offset = 44
@@ -33,18 +30,16 @@ async def _main() -> None:
         await asyncio.sleep(1)
         await hw_api.set_lights(rails=True)
         home_position = await hw_api.current_position_ot3(mount)
-        await hw_api.grip(force_newtons = None, stay_engaged = True)
+        await hw_api.grip(force_newtons=None, stay_engaged=True)
         print(f"home: {home_position}")
         x_home = home_position[Axis.X] - x_offset
         y_home = home_position[Axis.Y] - y_offset
         z_home = home_position[Axis.Z_G]
         while time.time() < timeout_start + timeout:
-        #while True:
+            # while True:
             print(f"time: {time.time()-timeout_start}")
-            await hw_api.move_to(mount,
-                                    Point(x_home, y_home, z_home))
-            await hw_api.move_to(mount,
-                                    Point(x_home, y_home, z_home - 190))
+            await hw_api.move_to(mount, Point(x_home, y_home, z_home))
+            await hw_api.move_to(mount, Point(x_home, y_home, z_home - 190))
             count += 1
             print(f"cycle: {count}")
         await hw_api.home()
@@ -53,7 +48,6 @@ async def _main() -> None:
     finally:
         await hw_api.disengage_axes([Axis.X, Axis.Y, Axis.G])
         await hw_api.clean_up()
-    
 
 
 if __name__ == "__main__":
@@ -75,7 +69,9 @@ if __name__ == "__main__":
     parser.add_argument("--simulate", action="store_true")
     parser.add_argument("--trough", action="store_true")
     parser.add_argument("--tiprack", action="store_true")
-    parser.add_argument("--mount", type=str, choices=["left", "right", "gripper"], default="gripper")
+    parser.add_argument(
+        "--mount", type=str, choices=["left", "right", "gripper"], default="gripper"
+    )
     args = parser.parse_args()
     if args.mount == "left":
         mount = OT3Mount.LEFT
