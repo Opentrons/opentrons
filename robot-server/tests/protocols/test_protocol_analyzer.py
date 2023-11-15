24e6e6738a950fa1ddf671d1d12470c171ef5425
Max Marrone <max@opentrons.com>
2023-11-15 17:02:19 -0500

    Update tests.

--- robot-server/tests/protocols/test_protocol_analyzer.py
+++ robot-server/tests/protocols/test_protocol_analyzer.py
@@ -53,6 +53,8 @@ async def test_analyze(
     subject: ProtocolAnalyzer,
 ) -> None:
     """It should be able to analyze a protocol."""
+    robot_type = "OT-3 Standard"
+
     protocol_resource = ProtocolResource(
         protocol_id="protocol-id",
         created_at=datetime(year=2021, month=1, day=1),
@@ -62,7 +64,7 @@ async def test_analyze(
             config=JsonProtocolConfig(schema_version=123),
             files=[],
             metadata={},
-            robot_type="OT-3 Standard",
+            robot_type=robot_type,
             content_hash="abc123",
         ),
         protocol_key="dummy-data-111",
@@ -101,7 +103,7 @@ async def test_analyze(
 
     decoy.when(
         await protocol_runner.create_simulating_runner(
-            robot_type="OT-3 Standard",
+            robot_type=robot_type,
             protocol_config=JsonProtocolConfig(schema_version=123),
         )
     ).then_return(json_runner)
@@ -130,6 +132,7 @@ async def test_analyze(
     decoy.verify(
         await analysis_store.update(
             analysis_id="analysis-id",
+            robot_type=robot_type,
             commands=[analysis_command],
             labware=[analysis_labware],
             modules=[],
