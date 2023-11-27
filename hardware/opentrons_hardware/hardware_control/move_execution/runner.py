b07e66f4a125f23cbd1916e9e49ffd6d27c1c20b
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2023-11-27 13:37:00 -0500

    draft

--- hardware/opentrons_hardware/hardware_control/move_execution/runner.py
+++ hardware/opentrons_hardware/hardware_control/move_execution/runner.py
@@ -90,33 +90,14 @@ log = logging.getLogger(__name__)
 
 class MoveGroupRunner:
     """A Move command runner."""
-    # def __init__(
-    #     self,
-    #     move_groups: MoveGroups,
-    #     start_index: int = 0,
-    #     ignore_stalls: bool = False,
-    # ) -> None:
-    #     self._scheduler = MoveDispatcher(move_groups, start_index, ignore_stalls)
-    #     self._is_prepped: bool = False
-
     def __init__(
         self,
         move_groups: MoveGroups,
-        start_at_index: int = 0,
+        start_index: int = 0,
         ignore_stalls: bool = False,
     ) -> None:
-        """Constructor.
-
-        Args:
-            move_groups: The move groups to run.
-            start_at_index: The index the MoveGroupManager will start at
-            ignore_stalls: Depends on the disableStallDetection feature flag
-        """
-        self._move_groups = move_groups
-        self._start_at_index = start_at_index
-        self._ignore_stalls = ignore_stalls
+        self._scheduler = MoveDispatcher(move_groups, start_index, ignore_stalls)
         self._is_prepped: bool = False
-
 
     @staticmethod
     def _has_moves(move_groups: MoveGroups) -> bool:
@@ -133,7 +114,7 @@ class MoveGroupRunner:
         ensure tighter timing, if you want something else to start as soon as
         possible to the actual execution of the move.
         """
-        if not self._has_moves(self._move_groups):
+        if not self._scheduler.has_moves():
             log.debug("No moves. Nothing to do.")
             return
         await self._clear_groups(can_messenger)
@@ -149,7 +130,7 @@ class MoveGroupRunner:
         ensure tighter timing, if you want something else to start as soon as
         possible to the actual execution of the move.
         """
-        if not self._has_moves(self._move_groups):
+        if not self._scheduler.has_moves():
             log.debug("No moves. Nothing to do.")
             return {}
         if not self._is_prepped:
@@ -262,107 +243,6 @@ class MoveGroupRunner:
                             step, group_i + self._start_at_index, seq_i
                         ),
                     )
-
-    def _convert_velocity(
-        self, velocity: Union[float, np.float64], interrupts: int
-    ) -> Int32Field:
-        return Int32Field(int((velocity / interrupts) * (2**31)))
-
-    def _get_message_type(
-        self, step: SingleMoveStep, group: int, seq: int
-    ) -> MessageDefinition:
-        """Return the correct payload type."""
-        if isinstance(step, MoveGroupSingleAxisStep):
-            return self._get_stepper_motor_message(step, group, seq)
-        elif isinstance(step, MoveGroupTipActionStep):
-            return self._get_tip_action_motor_message(step, group, seq)
-        else:
-            return self._get_brushed_motor_message(step, group, seq)
-
-    def _get_brushed_motor_message(
-        self, step: MoveGroupSingleGripperStep, group: int, seq: int
-    ) -> MessageDefinition:
-        payload = GripperMoveRequestPayload(
-            group_id=UInt8Field(group),
-            seq_id=UInt8Field(seq),
-            duration=UInt32Field(
-                int(step.duration_sec * brushed_motor_interrupts_per_sec)
-            ),
-            duty_cycle=UInt32Field(int(step.pwm_duty_cycle)),
-            encoder_position_um=Int32Field(int(step.encoder_position_um)),
-            stay_engaged=UInt8Field(int(step.stay_engaged)),
-        )
-        if step.move_type == MoveType.home:
-            return GripperHomeRequest(payload=payload)
-        elif step.move_type == MoveType.grip:
-            return GripperGripRequest(payload=payload)
-        else:
-            return AddBrushedLinearMoveRequest(payload=payload)
-
-    def _get_stepper_motor_message(
-        self, step: MoveGroupSingleAxisStep, group: int, seq: int
-    ) -> MessageDefinition:
-        if step.move_type == MoveType.home:
-            home_payload = HomeRequestPayload(
-                group_id=UInt8Field(group),
-                seq_id=UInt8Field(seq),
-                duration=UInt32Field(int(step.duration_sec * interrupts_per_sec)),
-                velocity_mm=self._convert_velocity(
-                    step.velocity_mm_sec, interrupts_per_sec
-                ),
-            )
-            return HomeRequest(payload=home_payload)
-        else:
-            stop_cond = step.stop_condition.value
-            if self._ignore_stalls:
-                stop_cond += MoveStopCondition.ignore_stalls.value
-            linear_payload = AddLinearMoveRequestPayload(
-                request_stop_condition=MoveStopConditionField(stop_cond),
-                group_id=UInt8Field(group),
-                seq_id=UInt8Field(seq),
-                duration=UInt32Field(int(step.duration_sec * interrupts_per_sec)),
-                acceleration_um=Int32Field(
-                    int(
-                        (
-                            step.acceleration_mm_sec_sq
-                            * 1000.0
-                            / interrupts_per_sec
-                            / interrupts_per_sec
-                        )
-                        * (2**31)
-                    )
-                ),
-                velocity_mm=Int32Field(
-                    int((step.velocity_mm_sec / interrupts_per_sec) * (2**31))
-                ),
-            )
-            return AddLinearMoveRequest(payload=linear_payload)
-
-    def _get_tip_action_motor_message(
-        self, step: MoveGroupTipActionStep, group: int, seq: int
-    ) -> TipActionRequest:
-        tip_action_payload = TipActionRequestPayload(
-            group_id=UInt8Field(group),
-            seq_id=UInt8Field(seq),
-            duration=UInt32Field(int(step.duration_sec * tip_interrupts_per_sec)),
-            velocity=self._convert_velocity(
-                step.velocity_mm_sec, tip_interrupts_per_sec
-            ),
-            action=PipetteTipActionTypeField(step.action),
-            request_stop_condition=MoveStopConditionField(step.stop_condition),
-            acceleration=Int32Field(
-                int(
-                    (
-                        step.acceleration_mm_sec_sq
-                        * 1000.0
-                        / tip_interrupts_per_sec
-                        / tip_interrupts_per_sec
-                    )
-                    * (2**31)
-                )
-            ),
-        )
-        return TipActionRequest(payload=tip_action_payload)
 
     async def _move(
         self, can_messenger: CanMessenger, start_at_index: int
--- hardware/opentrons_hardware/hardware_control/move_execution/types.py
+++ hardware/opentrons_hardware/hardware_control/move_execution/types.py
@@ -1,11 +1,58 @@
+from dataclasses import dataclass, field
 from typing import Union, Tuple, List
 
 from opentrons_hardware.firmware_bindings import ArbitrationId
 from opentrons_hardware.firmware_bindings.messages.message_definitions import (
+    AddLinearMoveRequest,
+    HomeRequest,
+    GripperGripRequest,
+    GripperHomeRequest,
     MoveCompleted,
     TipActionResponse,
 )
 
-AcceptableMoves = Union[MoveCompleted, TipActionResponse]
-CompletionPacket = Tuple[ArbitrationId, AcceptableMoves]
+SchedulableMoves = Union[AddLinearMoveRequest, HomeRequest, GripperGripRequest, GripperHomeRequest, AddBrushedLinearMoveRequest, TipActionRequest]
+AcceptableMoveResponses = Union[MoveCompleted, TipActionResponse]
+CompletionPacket = Tuple[ArbitrationId, AcceptableMoveResponses]
 Completions = List[CompletionPacket]
+
+
+@dataclass(order=True)
+class ScheduledMove:
+
+    message_index: UInt32Field
+    seq_id: int
+    node_id: int
+    stop_condition: MoveStopCondition
+    duration: float
+
+    def __post_init__(self) -> None:
+        self.acceptable_acks = MoveStopCondition.acceptable_ack(self.stop_condition)
+
+    def accept_ack(self, ack: MoveAckId) -> bool:
+        return True if ack in self.acceptable_acks else False
+
+
+@dataclass(order=True)
+class ScheduledTipActionMove(ScheduledMove):
+
+    tip_action_motors: List[GearMotorId] = field(default_factory=list)
+
+    def all_motors_repsonded(self) -> bool:
+        return not self.tip_action_motors
+
+
+@dataclass
+class ScheduledGroupInfo:
+
+    group_id: int
+    moves: List[SchedulableMoves]
+    duration: float = field(init=False)
+    expected_nodes: List[NodeId] = field(init=False)
+
+    def __post_init__(self) -> Moves:
+        self.duration = sum(m.duration for m in self.moves)
+        self.expected_nodes = list(set(NodeId(m.node_id) for m in self.moves))
+
+    def all_moves_completed(self) -> bool:
+        reutrn self.moves
