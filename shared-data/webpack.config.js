cdf3366577f284fde737e4d9745fd16282f6c494
Brian Cooper <brian@opentrons.com>
2023-12-01 16:09:45 -0500

    add build step for shared data as lib

---
+++ shared-data/webpack.config.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const path = require('path')
+const webpackMerge = require('webpack-merge')
+const { baseConfig } = require('@opentrons/webpack-config')
+
+const ENTRY_INDEX = path.join(__dirname, 'src/index.ts')
+const OUTPUT_PATH = path.join(__dirname, 'lib')
+
+module.exports = async () =>
+  webpackMerge(baseConfig, {
+    entry: { index: ENTRY_INDEX },
+    output: {
+      path: OUTPUT_PATH,
+      filename: 'opentrons-shared-data.js',
+      library: '@opentrons/shared-data',
+      libraryTarget: 'umd'
+    },
+  })
