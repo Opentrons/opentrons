d9c0a0390c1e551bc73482c41a66672555f73528
Jamey H <jamey.huffnagle@opentrons.com>
2023-12-04 13:01:19 -0500

    feedback

--- app/src/organisms/CommandText/utils/getAddressableAreaDisplayName.ts
+++ app/src/organisms/CommandText/utils/getAddressableAreaDisplayName.ts
@@ -22,13 +22,35 @@ export function getAddressableAreaDisplayName(
 
   const addressableAreaName: MoveToAddressableAreaParams['addressableAreaName'] =
     addressableAreaCommand.params.addressableAreaName
-  const movableTrashSubstr = 'movableTrash'
 
-  if (addressableAreaName.includes(movableTrashSubstr)) {
-    const slotName = addressableAreaName.split(movableTrashSubstr)[1]
+  if (addressableAreaName.includes('movableTrash')) {
+    const slotName = getMovableTrashSlot(addressableAreaName)
     return t('trash_bin_in_slot', { slot_name: slotName })
   } else if (addressableAreaName.includes('WasteChute')) {
     return t('waste_chute')
   } else if (addressableAreaName === 'fixedTrash') return t('fixed_trash')
   else return addressableAreaName
+}
+
+const getMovableTrashSlot = (addressableAreaName: string): string => {
+  switch (addressableAreaName) {
+    case 'movableTrashA1':
+      return 'A1'
+    case 'movableTrashA3':
+      return 'A3'
+    case 'movableTrashB1':
+      return 'B1'
+    case 'movableTrashB3':
+      return 'B3'
+    case 'movableTrashC1':
+      return 'C1'
+    case 'movableTrashC3':
+      return 'C3'
+    case 'movableTrashD1':
+      return 'D1'
+    case 'movableTrashD3':
+      return 'D3'
+    default:
+      return ''
+  }
 }
