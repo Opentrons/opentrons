e0fd061acc2f3b37d79b846a2560653b7a06d0f1
Max Marrone <max@opentrons.com>
2023-12-06 16:08:59 -0500

    Delete unused stuff in _waste_chute_dimensions.

--- api/src/opentrons/protocol_api/_waste_chute_dimensions.py
+++ api/src/opentrons/protocol_api/_waste_chute_dimensions.py
@@ -1,18 +1,11 @@
 """Constants for the dimensions of the Flex waste chute.
 
-TODO: These should be moved into shared-data and interpreted by Protocol Engine.
+TODO: Delete this when we resolve https://opentrons.atlassian.net/browse/RSS-418.
 """
 
 
 from opentrons.types import Point
 
-
-SLOT_ORIGIN_TO_1_OR_8_TIP_A1 = Point(64, 21.91, 115)
-SLOT_ORIGIN_TO_96_TIP_A1 = Point(14.445, 42.085, 115)
-
 # TODO: This z-coord is misleading. We need to account for the labware height and the paddle height;
 # we can't define this as a single coordinate.
 SLOT_ORIGIN_TO_GRIPPER_JAW_CENTER = Point(64, 29, 136.5)
-
-# This is the same with or without the cover attached, since the cover is flush with the rim.
-ENVELOPE_HEIGHT = 125
