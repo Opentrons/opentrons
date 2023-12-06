87610ae2e208b3b994e46373147dfc868ab2b481
Joe Wojak <jwojak@gmail.com>
2023-12-06 09:16:41 -0500

    Update api/src/opentrons/protocol_api/labware.py
    Co-authored-by: Ed Cormany <edward.cormany@opentrons.com>

--- api/src/opentrons/protocol_api/labware.py
+++ api/src/opentrons/protocol_api/labware.py
@@ -611,7 +611,7 @@ class Labware:
         """
         Accessor function that generates a list of wells in a top down,
         left to right order. This is representative of moving down rows and
-        across columns (e.g. ``'A1'``, ``'B1'``, ``'C1'...'A2'``, ``'B2'``, ``'C2'``)
+        across columns (i.e., A1, B1, C1…A2, B2, C2…)
 
         With indexing one can treat it as a typical python
         list. To access well A1 for example, write ``labware.wells()[0]``.
