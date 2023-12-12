44e4c2e4fbe7aec36f49ace730167f81bdbfbbe1
Jamey H <jamey.huffnagle@opentrons.com>
2023-12-12 13:07:03 -0500

    style(app): add t10 torx screwdriver to hs required equipment

--- app/src/organisms/ModuleWizardFlows/BeforeBeginning.tsx
+++ app/src/organisms/ModuleWizardFlows/BeforeBeginning.tsx
@@ -1,21 +1,31 @@
 import * as React from 'react'
 import { UseMutateFunction } from 'react-query'
 import { Trans, useTranslation } from 'react-i18next'
+
 import {
   HEATERSHAKER_MODULE_MODELS,
   TEMPERATURE_MODULE_MODELS,
   THERMOCYCLER_MODULE_MODELS,
 } from '@opentrons/shared-data/js/constants'
 import { getModuleDisplayName } from '@opentrons/shared-data'
+
 import { StyledText } from '../../atoms/text'
 import { GenericWizardTile } from '../../molecules/GenericWizardTile'
 import { WizardRequiredEquipmentList } from '../../molecules/WizardRequiredEquipmentList'
+
 import type {
   CreateMaintenanceRunData,
   MaintenanceRun,
+  AttachedModule,
 } from '@opentrons/api-client'
 import type { AxiosError } from 'axios'
 import type { ModuleCalibrationWizardStepProps } from './types'
+
+interface EqipmentItem {
+  loadName: string
+  displayName: string
+  subtitle?: string
+}
 
 interface BeforeBeginningProps extends ModuleCalibrationWizardStepProps {
   createMaintenanceRun: UseMutateFunction<
@@ -47,8 +57,9 @@ export const BeforeBeginning = (
   }, [])
   const moduleDisplayName = getModuleDisplayName(attachedModule.moduleModel)
 
-  let adapterLoadname
-  let adapterDisplaynameKey
+  let adapterLoadname: string
+  let adapterDisplaynameKey: string
+  const equipmentList = useAddEquipmentToSpecificModules([], attachedModule)
   if (
     THERMOCYCLER_MODULE_MODELS.some(
       model => model === attachedModule.moduleModel
@@ -77,10 +88,12 @@ export const BeforeBeginning = (
     )
     return null
   }
-  const equipmentList = [
-    { loadName: 'calibration_probe', displayName: t('pipette_probe') },
-    { loadName: adapterLoadname, displayName: t(adapterDisplaynameKey) },
-  ]
+  equipmentList.push(
+    ...[
+      { loadName: 'calibration_probe', displayName: t('pipette_probe') },
+      { loadName: adapterLoadname, displayName: t(adapterDisplaynameKey) },
+    ]
+  )
 
   return (
     <GenericWizardTile
@@ -101,4 +114,23 @@ export const BeforeBeginning = (
       proceed={proceed}
     />
   )
+}
+
+const useAddEquipmentToSpecificModules = (
+  equipmentList: EqipmentItem[],
+  attachedModule: AttachedModule
+): EqipmentItem[] => {
+  const { t } = useTranslation('heater_shaker')
+  if (
+    HEATERSHAKER_MODULE_MODELS.some(
+      model => model === attachedModule.moduleModel
+    )
+  ) {
+    equipmentList.unshift({
+      loadName: 't10_torx_screwdriver',
+      displayName: t('t10_torx_screwdriver'),
+      subtitle: t('t10_torx_screwdriver_subtitle'),
+    })
+  }
+  return equipmentList
 }
