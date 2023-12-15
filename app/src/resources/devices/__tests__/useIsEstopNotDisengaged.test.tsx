62a7465a8d13680cda0d8da61e4bad408beccc9e
koji <koji.kanao@opentrons.com>
2023-12-15 16:53:34 -0500

    fix(app): fix buttons disabled conditions when estop is not disengaged
    add a new hook to check estop status to control buttons' disabled on Desktop app
    
    close RQA-2110

---
+++ app/src/resources/devices/__tests__/useIsEstopNotDisengaged.test.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react'
+import { when, resetAllWhenMocks } from 'jest-when'
+import { renderHook } from '@testing-library/react-hooks'
+
+import { useIsFlex } from '../../../organisms/Devices/hooks'
+import { useEstopQuery } from '@opentrons/react-api-client'
+import {
+  DISENGAGED,
+  NOT_PRESENT,
+  PHYSICALLY_ENGAGED,
+  LOGICALLY_ENGAGED,
+} from '../../../organisms/EmergencyStop'
+import { useIsEstopNotDisengaged } from '../hooks/useIsEstopNotDisengaged'
+
+jest.mock('@opentrons/react-api-client')
+jest.mock('../../../organisms/Devices/hooks')
+
+const mockUseIsFlex = useIsFlex as jest.MockedFunction<typeof useIsFlex>
+const mockUseEstopQuery = useEstopQuery as jest.MockedFunction<
+  typeof useEstopQuery
+>
+
+const ROBOT_NAME = 'mockRobot'
+const mockEstopStatus = {
+  data: {
+    status: DISENGAGED,
+    leftEstopPhysicalStatus: DISENGAGED,
+    rightEstopPhysicalStatus: NOT_PRESENT,
+  },
+}
+const mockPhysicallyEngagedStatus = {
+  data: {
+    ...mockEstopStatus.data,
+    status: PHYSICALLY_ENGAGED,
+  },
+}
+const mockLogicallyEngagedStatus = {
+  data: {
+    ...mockEstopStatus.data,
+    status: LOGICALLY_ENGAGED,
+  },
+}
+const mockNotPresentStatus = {
+  data: {
+    ...mockEstopStatus.data,
+    status: NOT_PRESENT,
+  },
+}
+
+describe('useIsEstopNotDisengaged', () => {
+  beforeEach(() => {
+    when(mockUseIsFlex).calledWith(ROBOT_NAME).mockReturnValue(true)
+    mockUseEstopQuery.mockReturnValue({ data: mockEstopStatus } as any)
+  })
+
+  afterAll(() => {
+    resetAllWhenMocks()
+    jest.clearAllMocks()
+  })
+
+  it('should return false when e-stop status is disengaged', () => {
+    const isEstopNotDisengaged = useIsEstopNotDisengaged(ROBOT_NAME)
+    expect(isEstopNotDisengaged).toBe(false)
+  })
+
+  it('should return true when e-stop status is physically engaged', () => {
+    mockUseEstopQuery.mockReturnValue({
+      data: mockPhysicallyEngagedStatus,
+    } as any)
+    const isEstopNotDisengaged = useIsEstopNotDisengaged(ROBOT_NAME)
+    expect(isEstopNotDisengaged).toBe(true)
+  })
+
+  it('should return true when e-stop status is logically engaged', () => {
+    mockUseEstopQuery.mockReturnValue({
+      data: mockLogicallyEngagedStatus,
+    } as any)
+    const isEstopNotDisengaged = useIsEstopNotDisengaged(ROBOT_NAME)
+    expect(isEstopNotDisengaged).toBe(true)
+  })
+  it('should return true when e-stop status is not present', () => {
+    mockUseEstopQuery.mockReturnValue({ data: mockNotPresentStatus } as any)
+    const isEstopNotDisengaged = useIsEstopNotDisengaged(ROBOT_NAME)
+    expect(isEstopNotDisengaged).toBe(true)
+  })
+})
---
+++ app/src/resources/devices/hooks/useIsEstopNotDisengaged.ts
@@ -0,0 +1,23 @@
+import { useEstopQuery } from '@opentrons/react-api-client'
+import { useIsFlex } from '../../../organisms/Devices/hooks'
+import { DISENGAGED } from '../../../organisms/EmergencyStop'
+
+/**
+ * Checks if the emergency stop is not disengaged.
+ *
+ * @param {string} robotName - The name of the robot.
+ * @returns {boolean} Returns true if the emergency stop is not disengaged, false otherwise.
+ * not disengaged cases are physically engaged, logically engaged, and not present(unplugged)
+ *
+ * @example
+ * const isNotDisengaged = useIsEstopNotDisengaged('Robot1');
+ */
+
+export const useIsEstopNotDisengaged = (robotName: string): boolean => {
+  const isFlex = useIsFlex(robotName)
+  const { data: estopStatus } = useEstopQuery({
+    enabled: isFlex,
+  })
+
+  return estopStatus?.data.status !== DISENGAGED
+}
