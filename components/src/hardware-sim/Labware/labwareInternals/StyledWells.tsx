85a059323d01c87d4b15e5fcbd4c657dff6fd52b
Brian Cooper <brian@opentrons.com>
2023-12-18 13:18:33 -0500

    clean up well and remove css modules

--- components/src/hardware-sim/Labware/labwareInternals/StyledWells.tsx
+++ components/src/hardware-sim/Labware/labwareInternals/StyledWells.tsx
@@ -2,15 +2,61 @@ import * as React from 'react'
 import { Well } from './Well'
 import type { LabwareDefinition2 } from '@opentrons/shared-data'
 import type { WellGroup } from './types'
+import { COLORS } from '../../../ui-style-constants'
 
+type WellContents =
+  | 'tipPresent'
+  | 'tipMissing'
+  | 'defaultWell'
+  | 'disabledWell'
+  | 'highlightedWell'
+  | 'selectedWell'
 export interface StyledWellProps {
-  className: string
+  wellContents: WellContents
   definition: LabwareDefinition2
   wells: WellGroup
 }
 
+const STYLE_BY_WELL_CONTENTS: {
+  [wellContents in WellContents]: {
+    stroke: React.CSSProperties['stroke']
+    fill: React.CSSProperties['fill']
+    strokeWidth: React.CSSProperties['strokeWidth']
+  } } = {
+  highlightedWell: {
+    stroke: COLORS.blueEnabled,
+    fill: `${COLORS.blueEnabled}33`, // 20% opacity
+    strokeWidth: 0.6,
+  },
+  disabledWell: {
+    stroke: '#C6C6C6', // LEGACY --light-grey-hover
+    fill: '#EDEDEDCC', // LEGACY --lightest-gray + 80% opacity 
+    strokeWidth: 0.6,
+  },
+  selectedWell: {
+    stroke: COLORS.blueEnabled,
+    fill: COLORS.transparent,
+    strokeWidth: 0.6,
+  },
+  tipMissing: {
+    stroke: '#A4A4A4', // LEGACY --c-near-black
+    fill: '#E5E2E2',
+    strokeWidth: 0.6,
+  },
+  tipPresent: {
+    fill: COLORS.white,
+    stroke: '#A4A4A4', // LEGACY --c-near-black
+    strokeWidth: 0.6,
+  },
+  defaultWell: {
+    fill: COLORS.white,
+    stroke: '#A4A4A4', // LEGACY --c-near-black
+    strokeWidth: 0.6,
+  }
+}
+
 function StyledWellsComponent(props: StyledWellProps): JSX.Element {
-  const { className, definition, wells } = props
+  const { definition, wells, wellContents } = props
   return (
     <>
       {Object.keys(wells).map((wellName: string) => (
@@ -18,7 +64,7 @@ function StyledWellsComponent(props: StyledWellProps): JSX.Element {
           key={wellName}
           wellName={wellName}
           well={definition.wells[wellName]}
-          className={className}
+          {...STYLE_BY_WELL_CONTENTS[wellContents]}
         />
       ))}
     </>
--- components/src/hardware-sim/Labware/labwareInternals/Well.tsx
+++ components/src/hardware-sim/Labware/labwareInternals/Well.tsx
@@ -1,94 +1,84 @@
-import assert from 'assert'
 import * as React from 'react'
-import cx from 'classnames'
-import styles from './Well.css'
+
+import { COLORS } from '../../../ui-style-constants'
 
 import type { LabwareWell } from '@opentrons/shared-data'
-import type { CSSProperties } from 'styled-components'
 import type { WellMouseEvent } from './types'
+import type { StyleProps } from '../../../primitives'
 
-export interface WellProps {
-  /** if included, overrides the default classname */
-  className?: string | null | undefined
-  /** fill inline style */
-  fill?: CSSProperties['fill']
-  /** stroke inline style */
-  stroke?: CSSProperties['stroke']
+export interface WellProps extends StyleProps {
   /** Well Name (eg 'A1') */
   wellName: string
   /** well object from labware definition */
   well: LabwareWell
-  /** special class used for drag-to-select functionality. Should not be used for styling */
-  selectableWellClass?: string
+  stroke: React.CSSProperties['stroke']
+  strokeWidth: React.CSSProperties['strokeWidth']
+  fill: React.CSSProperties['fill']
   /** Optional callback, called with WellMouseEvent args onMouseOver */
   onMouseEnterWell?: (e: WellMouseEvent) => unknown
   onMouseLeaveWell?: (e: WellMouseEvent) => unknown
 }
 
-export function WellComponent(props: WellProps): JSX.Element | null {
+
+
+export function WellComponent(props: WellProps): JSX.Element {
   const {
     well,
     wellName,
-    fill,
     stroke,
+    strokeWidth,
+    fill,
     onMouseEnterWell,
     onMouseLeaveWell,
   } = props
-  assert(well, `expected 'well' prop for well "${wellName}"`)
-  if (!well) return null
   const { x, y } = well
 
-  const baseClassName = props.className || styles.default_well
-  const className = cx(baseClassName, props.selectableWellClass)
+  // const mouseInteractionProps = {
+  //   className: cx(baseClassName, props.selectableWellClass),
+  //   'data-wellname': wellName,
+  // }
+  // const noMouseProps = {
+  //   className: props.className ?? styles.default_well,
+  //   style: { pointerEvents: 'none' },
+  // }
+  // // exclude all mouse interactivity props if no event handler props provided
+  // const commonProps =
+  //   onMouseEnterWell != null || onMouseLeaveWell != null
+  //     ? mouseInteractionProps
+  //     : noMouseProps
 
-  const _mouseInteractionProps = {
-    className,
-    style: { fill, stroke },
-    'data-wellname': wellName,
-    onMouseEnter: onMouseEnterWell
-      ? ((event =>
-          onMouseEnterWell({ wellName, event })) as React.MouseEventHandler)
-      : undefined,
-    onMouseLeave: onMouseLeaveWell
-      ? ((event =>
-          onMouseLeaveWell({ wellName, event })) as React.MouseEventHandler)
-      : undefined,
+  const isInteractive = onMouseEnterWell != null || onMouseLeaveWell != null 
+  const pointerEvents: React.CSSProperties['pointerEvents'] = isInteractive ? 'auto' : 'none' 
+  const commonProps = {
+    'data-wellname': isInteractive ? wellName : undefined,
+    onMouseEnter: onMouseEnterWell != null ? (event: React.MouseEvent) => onMouseEnterWell({ wellName, event }) : undefined,
+    onMouseLeave: onMouseLeaveWell != null ? (event: React.MouseEvent) => onMouseLeaveWell({ wellName, event }) : undefined,
+    style: { pointerEvents, stroke, strokeWidth, fill }
   }
-  const _noMouseProps = {
-    className: baseClassName,
-    style: {
-      fill,
-      stroke,
-      pointerEvents: 'none' as CSSProperties['pointerEvents'],
-    },
-  }
-  // exclude all mouse interactivity props if no event handler props provided
-  const commonProps =
-    onMouseEnterWell || onMouseLeaveWell
-      ? _mouseInteractionProps
-      : _noMouseProps
+
 
   if (well.shape === 'circular') {
     const { diameter } = well
     const radius = diameter / 2
-    return <circle {...commonProps} cx={x} cy={y} r={radius} />
-  }
-
-  if (well.shape === 'rectangular') {
-    const { xDimension, yDimension } = well
     return (
-      <rect
+      <circle
         {...commonProps}
-        x={x - xDimension / 2}
-        y={y - yDimension / 2}
-        width={xDimension}
-        height={yDimension}
-      />
+        cx={x}
+        cy={y}
+        r={radius} />
     )
   }
 
-  console.warn('Invalid well', well)
-  return null
+  const { xDimension, yDimension } = well
+  return (
+    <rect
+      {...commonProps}
+      x={x - xDimension / 2}
+      y={y - yDimension / 2}
+      width={xDimension}
+      height={yDimension}
+    />
+  )
 }
 
 export const Well: React.MemoExoticComponent<typeof WellComponent> = React.memo(
