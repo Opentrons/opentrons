87b299390530fb5b134c569508b9c4c9db6bf882
Jamey H <jamey.huffnagle@opentrons.com>
2023-12-18 14:47:57 -0500

    fix(app): Don't render ProtocolRunHeader TerminalBanner on CurrentRun (#14233)
    Closes RQA-2013
    
    * fix(app): fix viewing error modal dismissing error and drop tip banners
    
    * refactor(app): refactor useMostRunRecentId to be safer

--- app/src/organisms/Devices/ProtocolRun/ProtocolRunHeader.tsx
+++ app/src/organisms/Devices/ProtocolRun/ProtocolRunHeader.tsx
@@ -364,7 +364,7 @@ export function ProtocolRunHeader({
         CANCELLABLE_STATUSES.includes(runStatus) ? (
           <Banner type="warning">{t('shared:close_robot_door')}</Banner>
         ) : null}
-        {isRunCurrent ? (
+        {mostRecentRunId === runId ? (
           <TerminalRunBanner
             {...{
               runStatus,
@@ -455,7 +455,7 @@ export function ProtocolRunHeader({
         ) : null}
         {showDropTipWizard &&
         pipettesWithTip[0]?.specs != null &&
-        isRunCurrent ? (
+        mostRecentRunId === runId ? (
           <DropTipWizard
             robotType={isFlex ? FLEX_ROBOT_TYPE : OT2_ROBOT_TYPE}
             mount={pipettesWithTip[0].mount}
--- app/src/organisms/ProtocolUpload/hooks/useMostRecentRunId.ts
+++ app/src/organisms/ProtocolUpload/hooks/useMostRecentRunId.ts
@@ -1,8 +1,9 @@
 import { useAllRunsQuery } from '@opentrons/react-api-client'
+import last from 'lodash/last'
 
 export function useMostRecentRunId(): string | null {
   const { data: allRuns } = useAllRunsQuery()
-  return allRuns != null && allRuns.data.length > 0
-    ? allRuns.data[allRuns.data.length - 1].id
+  return allRuns != null && allRuns.data?.length > 0
+    ? last(allRuns.data)?.id ?? null
     : null
 }
