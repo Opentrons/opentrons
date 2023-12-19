c991bf12aa2fb4023c7487461fc220797051e39d
smb2268 <sarah.breen@opentrons.com>
2023-12-19 13:15:44 -0500

    Fix instrument name in firmware update results modal

--- app/src/organisms/FirmwareUpdateModal/UpdateResultsModal.tsx
+++ app/src/organisms/FirmwareUpdateModal/UpdateResultsModal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { useTranslation, Trans } from 'react-i18next'
 import capitalize from 'lodash/capitalize'
+import { getPipetteModelSpecs } from '@opentrons/shared-data'
 import {
   ALIGN_CENTER,
   BORDERS,
@@ -40,8 +41,9 @@ export function UpdateResultsModal(
   if (instrument?.ok) {
     instrumentName =
       instrument?.instrumentType === 'pipette'
-        ? instrument?.instrumentName
-        : instrument.instrumentType
+        ? getPipetteModelSpecs(instrument.instrumentModel)?.displayName ??
+          'pipette'
+        : 'Flex Gripper'
   }
   return (
     <>
