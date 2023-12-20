60d40afc227e4828a486af24f90beb31e1015c9d
David Gomez <david.gomez@opentrons.com>
2023-12-20 14:23:46 -0500

    increased acceleration to pipetting default, added blow out every 1k cycles, added z movement to aspirate at top

--- hardware-testing/hardware_testing/scripts/ultima_lifetest.py
+++ hardware-testing/hardware_testing/scripts/ultima_lifetest.py
@@ -19,6 +19,7 @@ from opentrons.hardware_control.backends.ot3utils import sensor_id_for_instrumen
 from hardware_testing.gravimetric.config import _get_liquid_probe_settings
 from opentrons.hardware_control.types import InstrumentProbeType
 
+from hardware_testing.data import get_git_description
 from hardware_testing.data.csv_report import (
     CSVReport,
     CSVResult,
@@ -30,13 +31,20 @@ from hardware_testing.opentrons_api.types import (
     Axis,
     CriticalPoint,
     Point,
+    OT3AxisKind,
 )
 from hardware_testing.data import ui
 from hardware_testing.opentrons_api import helpers_ot3
 
+from opentrons.config.advanced_settings import set_adv_setting
+
+asyncio.run(set_adv_setting('disableOverpressureDetection', True))
+
 FLOW_TO_SPEED = 1.0/15.9 # mm/uL
 DEFAULT_FLOW = 700
 DEFAULT_TRIALS = 10
+
+VOLUME_OFFSET = 300/15.9 #300uL in mm of plunger travel
 
 TIP_RACK_LABWARE = f"opentrons_flex_96_tiprack_1000ul"
 TIP_RACK_SLOT = 6
@@ -49,10 +57,19 @@ OFFSET_FOR_1_WELL_LABWARE = Point(x=9 * -11 * 0.5, y=9 * 7 * 0.5)
 
 TEMP_DECK_LABWARE = "nest_12_reservoir_15ml"
 TEMP_DECK_SLOT = 1
-OFFSET_FOR_TEMP_DECK = Point(x=1, y=9 * 7 * 0.5, z=-15)
+# OFFSET_FOR_TEMP_DECK = Point(x=23, y=9 * 7 * 0.5, z=-4.5) #ultima
+# OFFSET_FOR_TEMP_DECK = Point(x=1, y=9 * 7 * 0.5, z=-9) #nest 12 well
+OFFSET_FOR_TEMP_DECK = Point(x=1, y=9 * 7 * 0.5, z=-25) #nest 12 well no temp
+# Temp deck temp of 36C keeps fluid at ~32
+
+# DISPENSE_OFFSET = 20 #ultima
+DISPENSE_OFFSET = 8 #nest 12 well
 
 NUM_PRESSURE_READINGS = 10
 SECONDS_BETWEEN_READINGS = 0.25
+
+DEFAULT_SPEED = DEFAULT_MAX_SPEEDS.low_throughput[OT3AxisKind.P]
+FLOW_ACCELERATION = 24000.0/15.9
 
 def _get_test_tag(flow: float, trial: int) -> str:
     return f"flow-{flow}-trial-{trial}"
@@ -152,20 +169,36 @@ async def move_twin_plunger_absolute_ot3(
             await _move_coro
 
 
+async def twin_z_move(
+    api: OT3API,
+    position: List[float]
+) -> None:
+    await helpers_ot3.move_to_arched_ot3(
+        api, OT3Mount.LEFT, position
+    )
+
+    gantry_z = await api.gantry_position(OT3Mount.LEFT)
+    await api._move({Axis.X:api._current_position[Axis.X],
+                     Axis.Y:api._current_position[Axis.Y],
+                     Axis.Z_R:api._current_position[Axis.Z_L]})
+
+
 async def test_cycle(
     api: OT3API,
     positions: List[List[float]],
     flow: float,
     starting_cycle: int,
-    cycles: int
+    cycles: int,
+    bottom_position: List[float],
 ) -> None:
 
     speed = flow * FLOW_TO_SPEED
 
     for i in range(cycles):
-        print(starting_cycle)
         await move_twin_plunger_absolute_ot3(api, positions[0], speed=speed)
+        await twin_z_move(api, bottom_position + Point(z=DISPENSE_OFFSET))
         await move_twin_plunger_absolute_ot3(api, positions[1], speed=speed)
+        await twin_z_move(api, bottom_position)
         starting_cycle += 1
 
 
@@ -202,10 +235,6 @@ async def _main(is_simulating: bool, trials: int, flow: float,
         pipette_right="p50_single_v3.4",
     )
 
-    # home and move to a safe position
-    await _reset_gantry(api)
-    await api.home([Axis.P_L, Axis.P_R])
-
     # test each attached pipette
     mount = await _get_next_pipette_mount(api)
     print(mount)
@@ -214,148 +243,196 @@ async def _main(is_simulating: bool, trials: int, flow: float,
     for i in range(trials):
         trial_list.append(i)
 
+
     report = _build_csv_report(flow=flow, trials=trial_list)
     dut = helpers_ot3.DeviceUnderTest.by_mount(mount)
-    helpers_ot3.set_csv_report_meta_data_ot3(api, report, dut)
+    # helpers_ot3.set_csv_report_meta_data_ot3(api, report, dut)
+    report.set_operator("Ultima")
+    robot_serial = helpers_ot3.get_robot_serial_ot3(api)
+    dut_str = helpers_ot3._get_serial_for_dut(api, dut)
+    report.set_tag(dut_str)
+    report.set_device_id(dut_str, dut_str)
+    report.set_robot_id(robot_serial)
+    report.set_firmware(api.fw_version)
+    report.set_version(get_git_description())
+
     sn = helpers_ot3.get_pipette_serial_ot3(api.hardware_pipettes[OT3Mount.LEFT.to_mount()])
     print(f"Serial Left: {sn}")
     sn = helpers_ot3.get_pipette_serial_ot3(api.hardware_pipettes[OT3Mount.RIGHT.to_mount()])
     print(f"Serial Right: {sn}")
 
-    # Pick up tips
-    tip_len = helpers_ot3.get_default_tip_length(TIP_VOLUME)
-    left_tips = get_tiprack_nominal()
-    await helpers_ot3.move_to_arched_ot3(api, OT3Mount.LEFT, left_tips)
-    input("Pick up tip? Press Enter" )
-    await api.pick_up_tip(OT3Mount.LEFT, tip_length=tip_len)
-
-    right_tips = left_tips + Point(x=9)
-    await helpers_ot3.move_to_arched_ot3(api, OT3Mount.RIGHT, right_tips)
-    input("Pick up tip? Press Enter" )
-    await api.pick_up_tip(OT3Mount.RIGHT, tip_length=tip_len)
-
-    # Move to reservoir
-    await api.home([Axis.Z_L, Axis.Z_R])
-    z_home = await api.gantry_position(OT3Mount.LEFT)
+    try:
+        # home and move to a safe position
+        await _reset_gantry(api)
+        await api.home([Axis.P_L, Axis.P_R])
 
 
-    mounts = [OT3Mount.LEFT, OT3Mount.RIGHT]
-    pip_top = [helpers_ot3.get_plunger_positions_ot3(api, mounts[0])[0],
-               helpers_ot3.get_plunger_positions_ot3(api, mounts[1])[0]]
-    pip_bot = [helpers_ot3.get_plunger_positions_ot3(api, mounts[0])[1],
-               helpers_ot3.get_plunger_positions_ot3(api, mounts[1])[1]]
-    pip_blow = [helpers_ot3.get_plunger_positions_ot3(api, mounts[0])[2],
-               helpers_ot3.get_plunger_positions_ot3(api, mounts[1])[2]]
-    pip_drop = [helpers_ot3.get_plunger_positions_ot3(api, mounts[0])[3],
-               helpers_ot3.get_plunger_positions_ot3(api, mounts[1])[3]]
 
-    # Liquid probe
-    if liquid_probe:
-        reservoir_a1_nominal = get_reservoir_nominal()
-        reservoir_a1_home = reservoir_a1_nominal._replace(z=z_home.z)
+        # Pick up tips
+        tip_len = helpers_ot3.get_default_tip_length(TIP_VOLUME)
+        left_tips = get_tiprack_nominal()
+        await helpers_ot3.move_to_arched_ot3(api, OT3Mount.LEFT, left_tips)
+        # input("Pick up tip? Press Enter" )
+        await api.pick_up_tip(OT3Mount.LEFT, tip_length=tip_len)
+
+        right_tips = left_tips + Point(x=9)
+        await helpers_ot3.move_to_arched_ot3(api, OT3Mount.RIGHT, right_tips)
+        # input("Pick up tip? Press Enter" )
+        await api.pick_up_tip(OT3Mount.RIGHT, tip_length=tip_len)
+
+        # Move to reservoir
+        await api.home([Axis.Z_L, Axis.Z_R])
+        z_home = await api.gantry_position(OT3Mount.LEFT)
+
+
+        mounts = [OT3Mount.LEFT, OT3Mount.RIGHT]
+        pip_top = [helpers_ot3.get_plunger_positions_ot3(api, mounts[0])[0] + VOLUME_OFFSET,
+                   helpers_ot3.get_plunger_positions_ot3(api, mounts[1])[0] + VOLUME_OFFSET]
+        pip_bot = [helpers_ot3.get_plunger_positions_ot3(api, mounts[0])[1],
+                   helpers_ot3.get_plunger_positions_ot3(api, mounts[1])[1]]
+        pip_blow = [helpers_ot3.get_plunger_positions_ot3(api, mounts[0])[2],
+                   helpers_ot3.get_plunger_positions_ot3(api, mounts[1])[2]]
+        pip_drop = [helpers_ot3.get_plunger_positions_ot3(api, mounts[0])[3],
+                   helpers_ot3.get_plunger_positions_ot3(api, mounts[1])[3]]
+
+        # print(pip_top)
+        # print(pip_bot)
+
+        # Liquid probe
+        if liquid_probe:
+            reservoir_a1_nominal = get_reservoir_nominal()
+            reservoir_a1_home = reservoir_a1_nominal._replace(z=z_home.z)
+            await helpers_ot3.move_to_arched_ot3(
+                api, OT3Mount.LEFT, reservoir_a1_home
+            )
+
+            for m in mounts:
+                for _probe in InstrumentProbeType:
+                    sensor_id = sensor_id_for_instrument(_probe)
+                    print(f"Mount: {m}")
+                    sn = helpers_ot3.get_pipette_serial_ot3(api.hardware_pipettes[m.to_mount()])
+                    print(f"Serial: {sn}")
+                    print(f"Sensor: {_probe}")
+                    open_pa = 0.0
+                    try:
+                        open_pa = await _read_from_sensor(api, sensor_id, NUM_PRESSURE_READINGS, m)
+                    except helpers_ot3.SensorResponseBad:
+                        ui.print_error(f"{_probe} pressure sensor not working, skipping")
+                        continue
+                    print(f"open-pa: {open_pa}")
+            # input("move to pre position...")
+            # await helpers_ot3.move_to_arched_ot3(
+            #     api, OT3Mount.LEFT, reservoir_a1_nominal + Point(z=8)
+            # )
+            probe_settings = api.config.liquid_sense
+            probe_settings.starting_mount_height = (reservoir_a1_nominal + Point(z=8)).z
+            print(probe_settings)
+
+            select = input("Primary (p) or Secondary (s): p/s")
+            if select == 'p':
+                print("sense liquid left primary...")
+                sensed_z = round(
+                    await api.liquid_probe(OT3Mount.LEFT, probe_settings, InstrumentProbeType.PRIMARY), 3
+                )
+                print(f"SENSED LIQUID Z: {sensed_z}")
+
+                print("sense liquid right primary...")
+                sensed_z = round(
+                    await api.liquid_probe(OT3Mount.RIGHT, probe_settings, InstrumentProbeType.PRIMARY), 3
+                )
+                print(f"SENSED LIQUID Z: {sensed_z}")
+            else:
+                print("sense liquid left secondary...")
+                sensed_z = round(
+                    await api.liquid_probe(OT3Mount.LEFT, probe_settings, InstrumentProbeType.SECONDARY), 3
+                )
+                print(f"SENSED LIQUID Z: {sensed_z}")
+
+                print("sense liquid right secondary...")
+                sensed_z = round(
+                    await api.liquid_probe(OT3Mount.RIGHT, probe_settings, InstrumentProbeType.SECONDARY), 3
+                )
+                print(f"SENSED LIQUID Z: {sensed_z}")
+
+            await helpers_ot3.move_to_arched_ot3(
+                api, OT3Mount.LEFT, reservoir_a1_home
+            )
+
+        # Descend into temp deck reservoir
+        temp_reservoir_a1_nominal = get_temp_reservoir_nominal()
+        temp_reservoir_a1_nominal_home = temp_reservoir_a1_nominal + Point(z=30)
         await helpers_ot3.move_to_arched_ot3(
-            api, OT3Mount.LEFT, reservoir_a1_home
+            api, OT3Mount.LEFT, temp_reservoir_a1_nominal_home
         )
 
-        for m in mounts:
-            for _probe in InstrumentProbeType:
-                sensor_id = sensor_id_for_instrument(_probe)
-                print(f"Mount: {m}")
-                sn = helpers_ot3.get_pipette_serial_ot3(api.hardware_pipettes[m.to_mount()])
-                print(f"Serial: {sn}")
-                print(f"Sensor: {_probe}")
-                open_pa = 0.0
-                try:
-                    open_pa = await _read_from_sensor(api, sensor_id, NUM_PRESSURE_READINGS, m)
-                except helpers_ot3.SensorResponseBad:
-                    ui.print_error(f"{_probe} pressure sensor not working, skipping")
-                    continue
-                print(f"open-pa: {open_pa}")
-        # input("move to pre position...")
+        # input("Descend?")
+
+        # await api.home([Axis.P_L, Axis.P_R])
+        await move_twin_plunger_absolute_ot3(api, pip_bot)
+
         # await helpers_ot3.move_to_arched_ot3(
-        #     api, OT3Mount.LEFT, reservoir_a1_nominal + Point(z=8)
+        #     api, OT3Mount.LEFT, temp_reservoir_a1_nominal
         # )
-        probe_settings = api.config.liquid_sense
-        probe_settings.starting_mount_height = (reservoir_a1_nominal + Point(z=8)).z
-        print(probe_settings)
+        #
+        # gantry_z = await api.gantry_position(OT3Mount.LEFT)
+        # await api._move({Axis.X:api._current_position[Axis.X],
+        #                  Axis.Y:api._current_position[Axis.Y],
+        #                  Axis.Z_R:api._current_position[Axis.Z_L]})
+        await twin_z_move(api, temp_reservoir_a1_nominal)
 
-        select = input("Primary (p) or Secondary (s): p/s")
-        if select == 'p':
-            print("sense liquid left primary...")
-            sensed_z = round(
-                await api.liquid_probe(OT3Mount.LEFT, probe_settings, InstrumentProbeType.PRIMARY), 3
+
+        total_cycles = 0
+        cycles_per_trial = 100
+        blow_inc = 10
+        # cycles_per_trial = 2
+        # blow_inc = 10
+        for t in range(trials):
+            await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(
+                api,
+                Axis.P_L,
+                default_max_speed=DEFAULT_SPEED,
+                acceleration=FLOW_ACCELERATION,
             )
-            print(f"SENSED LIQUID Z: {sensed_z}")
-
-            print("sense liquid right primary...")
-            sensed_z = round(
-                await api.liquid_probe(OT3Mount.RIGHT, probe_settings, InstrumentProbeType.PRIMARY), 3
+            await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(
+                api,
+                Axis.P_R,
+                default_max_speed=DEFAULT_SPEED,
+                acceleration=FLOW_ACCELERATION,
             )
-            print(f"SENSED LIQUID Z: {sensed_z}")
-        else:
-            print("sense liquid left secondary...")
-            sensed_z = round(
-                await api.liquid_probe(OT3Mount.LEFT, probe_settings, InstrumentProbeType.SECONDARY), 3
-            )
-            print(f"SENSED LIQUID Z: {sensed_z}")
+            await test_cycle(api, [pip_top, pip_bot], flow,
+                             total_cycles, cycles_per_trial,
+                             temp_reservoir_a1_nominal)
+            if t%blow_inc == 0:
+                print("Blow Out")
+                await test_cycle(api, [pip_blow, pip_bot], flow,
+                                 0, 1,
+                                 temp_reservoir_a1_nominal)
+            total_cycles += cycles_per_trial
+            print(total_cycles)
+            report(OT3Mount.LEFT.name, _get_test_tag(flow, t), [total_cycles])
+            report(OT3Mount.RIGHT.name, _get_test_tag(flow, t), [total_cycles])
 
-            print("sense liquid right secondary...")
-            sensed_z = round(
-                await api.liquid_probe(OT3Mount.RIGHT, probe_settings, InstrumentProbeType.SECONDARY), 3
-            )
-            print(f"SENSED LIQUID Z: {sensed_z}")
+        report.save_to_disk()
+        # report.print_results()
 
-        await helpers_ot3.move_to_arched_ot3(
-            api, OT3Mount.LEFT, reservoir_a1_home
-        )
+        # Drop Tips
+        await api.home([Axis.Z_L, Axis.Z_R])
+        await helpers_ot3.move_to_arched_ot3(api, OT3Mount.LEFT,
+                                             left_tips + Point(z=10))
+        await helpers_ot3.move_to_arched_ot3(api, OT3Mount.LEFT,
+                                             left_tips - Point(z=tip_len/3))
+        await api.drop_tip(OT3Mount.LEFT)
 
-    # Descend into temp deck reservoir
-    temp_reservoir_a1_nominal = get_temp_reservoir_nominal()
-    temp_reservoir_a1_nominal_home = temp_reservoir_a1_nominal + Point(z=30)
-    await helpers_ot3.move_to_arched_ot3(
-        api, OT3Mount.LEFT, temp_reservoir_a1_nominal_home
-    )
+        await helpers_ot3.move_to_arched_ot3(api, OT3Mount.RIGHT,
+                                             right_tips - Point(z=tip_len/3))
+        await api.drop_tip(OT3Mount.RIGHT)
 
-    input("Descend?")
-
-    await api.home([Axis.P_L, Axis.P_R])
-    await move_twin_plunger_absolute_ot3(api, pip_bot)
-
-    await helpers_ot3.move_to_arched_ot3(
-        api, OT3Mount.LEFT, temp_reservoir_a1_nominal
-    )
-
-    gantry_z = await api.gantry_position(OT3Mount.LEFT)
-    await api._move({Axis.X:api._current_position[Axis.X],
-                     Axis.Y:api._current_position[Axis.Y],
-                     Axis.Z_R:api._current_position[Axis.Z_L]})
-
-
-    total_cycles = 0
-    cycles_per_trial = 100
-    for t in range(trials):
-        await test_cycle(api, [pip_top,pip_bot], flow,
-                         total_cycles, cycles_per_trial)
-        total_cycles += cycles_per_trial
-        report(OT3Mount.LEFT.name, _get_test_tag(flow, t), [total_cycles])
-        report(OT3Mount.RIGHT.name, _get_test_tag(flow, t), [total_cycles])
-
-    report.save_to_disk()
-    # report.print_results()
-
-    # Drop Tips
-    await api.home([Axis.Z_L, Axis.Z_R])
-    await helpers_ot3.move_to_arched_ot3(api, OT3Mount.LEFT,
-                                         left_tips + Point(z=10))
-    await helpers_ot3.move_to_arched_ot3(api, OT3Mount.LEFT,
-                                         left_tips - Point(z=tip_len/3))
-    await api.drop_tip(OT3Mount.LEFT)
-
-    await helpers_ot3.move_to_arched_ot3(api, OT3Mount.RIGHT,
-                                         right_tips - Point(z=tip_len/3))
-    await api.drop_tip(OT3Mount.RIGHT)
-
-    ui.print_title("DONE")
+        ui.print_title("DONE")
+    except KeyboardInterrupt:
+        print("Cancelled")
+    finally:
+        await api.clean_up()
+        report.save_to_disk()
 
 
 if __name__ == "__main__":
