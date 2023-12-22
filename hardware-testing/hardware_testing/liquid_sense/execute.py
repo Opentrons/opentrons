5649811e7ff338025c0fe43d0920647dea3444f5
Ryan howard <ryan.howard@opentrons.com>
2023-12-22 11:12:48 -0500

    flesh out the stat calclation

--- hardware-testing/hardware_testing/liquid_sense/execute.py
+++ hardware-testing/hardware_testing/liquid_sense/execute.py
@@ -8,17 +8,27 @@ from hardware_testing.gravimetric.workarounds import get_sync_hw_api
 from hardware_testing.gravimetric.config import LIQUID_PROBE_SETTINGS
 from hardware_testing.opentrons_api.types import OT3Mount
 from opentrons.hardware_control.types import InstrumentProbeType
-
+from statistics import stdev
 
 def run(tip: int, run_args: RunArgs) -> None:
     """Run a liquid probe test."""
+    well: Well = Well()
+    results : List[float] = []
     for trial in range(run_args.trials):
         print(f"Running liquid probe test with tip {tip}")
-        store_trial(run_args.test_report, trial, tip, 40.0)
-    store_tip_results(run_args.test_report, tip, 40, 0.05, 0.05)
+        height = _run_trial(run_args, tip, well, trial)
+        results.append(height)
+        store_trial(run_args.test_report, trial, tip, height)
+
+    average:float = sum(results) / runargs.trials
+    cv: float = stdev(results) / average
+    #fake this for now
+    expected_height = 40.0
+    d: float = (average - expected_height) / expected_height
+    store_tip_results(run_args.test_report, tip, average, cv, d)
 
 
-def _run_trial(run_args: RunArgs, tip: int, well: Well) -> float:
+def _run_trial(run_args: RunArgs, tip: int, well: Well, trial: int) -> float:
     hw_api = get_sync_hw_api(run_args.ctx)
     lqid_cfg: Dict[str, int] = LIQUID_PROBE_SETTINGS[run_args.pipette_volume][
         run_args.pipette_channels
@@ -40,6 +50,8 @@ def _run_trial(run_args: RunArgs, tip: int, well: Well) -> float:
     run_args.pipette.move_to(well.top())
 
     hw_mount = OT3Mount.LEFT if run_args.pipette.mount == "left" else OT3Mount.RIGHT
-
+    run_args.recorder.set_sample_tag(f"trial-{trial}-{tip}ul")
     # TODO add in stuff for secondary probe
-    return hw_api.liquid_probe(hw_mount, lps, InstrumentProbeType.PRIMARY)
+    height = hw_api.liquid_probe(hw_mount, lps, InstrumentProbeType.PRIMARY)
+    run_args.recorder.clear_sample_tag()
+    return
