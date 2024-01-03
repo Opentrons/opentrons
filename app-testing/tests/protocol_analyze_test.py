6a99448e40d3967f8680c02ab6ee1f8db02dd2ed
Josh McVey <josh.mcvey@opentrons.com>
2024-01-03 14:53:15 -0600

    fix(app-testing): 7.1 snapshots

--- app-testing/tests/protocol_analyze_test.py
+++ app-testing/tests/protocol_analyze_test.py
@@ -49,8 +49,7 @@ def get_error_text(protocol_landing: ProtocolLanding, error_link: WebElement) ->
 
 @pytest.mark.parametrize(
     "protocol",
-    _what_protocols(),
-    ids=[x.file_name for x in _what_protocols()],
+    _what_protocols()
 )
 def test_analyses(
     driver: WebDriver,
