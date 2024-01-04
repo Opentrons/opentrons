7ad9891d8d06a621d989f4175da4165a108a7372
Ryan howard <ryan.howard@opentrons.com>
2024-01-04 11:21:20 -0500

    fix the retract in PAPI due to mismatched method names

--- api/src/opentrons/protocol_api/core/instrument.py
+++ api/src/opentrons/protocol_api/core/instrument.py
@@ -290,7 +290,7 @@ class AbstractInstrument(ABC, Generic[WellCoreType]):
     def is_tip_tracking_available(self) -> bool:
         """Return whether auto tip tracking is available for the pipette's current nozzle configuration."""
 
-    def _retract(self) -> None:
+    def retract(self) -> None:
         """Retract this instrument to the top of the gantry."""
         ...
 
