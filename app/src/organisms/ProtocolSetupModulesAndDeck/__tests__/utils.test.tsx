fd4b942bdfcc20a1fe3b449afb87898957fb6782
Brent Hagen <brent.hagen@opentrons.com>
2024-01-05 14:24:46 -0500

    add more tests

--- app/src/organisms/ProtocolSetupModulesAndDeck/__tests__/utils.test.tsx
+++ app/src/organisms/ProtocolSetupModulesAndDeck/__tests__/utils.test.tsx
@@ -1,15 +1,72 @@
+import { getModuleDef2 } from '@opentrons/shared-data'
+
+import { mockTemperatureModule } from '../../../redux/modules/__fixtures__'
 import {
   getAttachedProtocolModuleMatches,
   getUnmatchedModulesForProtocol,
 } from '../utils'
 
+const temperatureProtocolModule = {
+  moduleId: 'mockTempModuleId',
+  x: 0,
+  y: 0,
+  z: 0,
+  moduleDef: getModuleDef2('temperatureModuleV1'),
+  nestedLabwareDef: null,
+  nestedLabwareId: null,
+  nestedLabwareDisplayName: null,
+  protocolLoadOrder: 0,
+  slotName: 'D1',
+}
+
+const magneticProtocolModule = {
+  moduleId: 'mockMagneticModuleId',
+  x: 0,
+  y: 0,
+  z: 0,
+  moduleDef: getModuleDef2('magneticModuleV2'),
+  nestedLabwareDef: null,
+  nestedLabwareId: null,
+  nestedLabwareDisplayName: null,
+  protocolLoadOrder: 0,
+  slotName: 'D1',
+}
+
 describe('getAttachedProtocolModuleMatches', () => {
   it('returns no module matches when no modules attached', () => {
-    const result = getAttachedProtocolModuleMatches([], [])
-    expect(result).toEqual([])
+    const result = getAttachedProtocolModuleMatches(
+      [],
+      [temperatureProtocolModule, magneticProtocolModule]
+    )
+    expect(result).toEqual([
+      { ...temperatureProtocolModule, attachedModuleMatch: null },
+      { ...magneticProtocolModule, attachedModuleMatch: null },
+    ])
   })
 
-  // TODO(bh, 2023-02-27): additional test coverage
+  it('returns no module matches when no modules match', () => {
+    const result = getAttachedProtocolModuleMatches(
+      [mockTemperatureModule],
+      [magneticProtocolModule]
+    )
+    expect(result).toEqual([
+      { ...magneticProtocolModule, attachedModuleMatch: null },
+    ])
+  })
+
+  it('returns module match when modules match', () => {
+    const result = getAttachedProtocolModuleMatches(
+      [mockTemperatureModule],
+      [temperatureProtocolModule, magneticProtocolModule]
+    )
+    expect(result).toEqual([
+      {
+        ...temperatureProtocolModule,
+        attachedModuleMatch: mockTemperatureModule,
+      },
+      { ...magneticProtocolModule, attachedModuleMatch: null },
+    ])
+  })
 })
 
 describe('getUnmatchedModulesForProtocol', () => {
@@ -21,5 +78,36 @@ describe('getUnmatchedModulesForProtocol', () => {
     })
   })
 
-  // TODO(bh, 2023-02-27): additional test coverage
+  it('returns no missing module ids or remaining attached modules when attached modules match', () => {
+    const result = getUnmatchedModulesForProtocol(
+      [mockTemperatureModule],
+      [temperatureProtocolModule]
+    )
+    expect(result).toEqual({
+      missingModuleIds: [],
+      remainingAttachedModules: [],
+    })
+  })
+
+  it('returns missing module ids when protocol modules missing', () => {
+    const result = getUnmatchedModulesForProtocol(
+      [],
+      [temperatureProtocolModule, magneticProtocolModule]
+    )
+    expect(result).toEqual({
+      missingModuleIds: ['mockTempModuleId', 'mockMagneticModuleId'],
+      remainingAttachedModules: [],
+    })
+  })
+
+  it('returns remaining attached modules when protocol modules and attached modules do not match', () => {
+    const result = getUnmatchedModulesForProtocol(
+      [mockTemperatureModule],
+      [magneticProtocolModule]
+    )
+    expect(result).toEqual({
+      missingModuleIds: ['mockMagneticModuleId'],
+      remainingAttachedModules: [mockTemperatureModule],
+    })
+  })
 })
