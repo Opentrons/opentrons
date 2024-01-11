d360c1f3ac1e9ef4427974af459987faf527afc2
koji <koji.kanao@opentrons.com>
2024-01-11 15:36:56 -0500

    add core colors

--- components/src/helix-design-system/colors.ts
+++ components/src/helix-design-system/colors.ts
@@ -7,7 +7,7 @@
  */
 
 /**
- * yellow 
+ * yellow
  */
 
 /**
@@ -25,6 +25,13 @@
 /**
  * core
  */
+export const black90 = '#16212D'
+export const black80 = '#24313F'
+export const black70 = '#39495B'
+export const white = '#FFFFFF'
+export const blue50 = '#006CFA'
+export const transparentBlack60 = '#16212D99'
+export const transparentBlack40 = '#16212D66'
 
 /**
  * extras
