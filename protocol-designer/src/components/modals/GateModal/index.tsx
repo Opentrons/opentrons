f7b989d9d76883e96eb157a17ffafaa67d10dc30
Jethary <jethary@opentrons.com>
2024-01-12 13:15:29 -0500

    fix rest of modal compoennts

--- protocol-designer/src/components/modals/GateModal/index.tsx
+++ protocol-designer/src/components/modals/GateModal/index.tsx
@@ -1,73 +1,51 @@
 import * as React from 'react'
-import { connect } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import cx from 'classnames'
 import { AlertModal } from '@opentrons/components'
 import {
   actions as analyticsActions,
   selectors as analyticsSelectors,
 } from '../../../analytics'
-import { BaseState, ThunkDispatch } from '../../../types'
 import settingsStyles from '../../SettingsPage/SettingsPage.css'
 import modalStyles from '../modal.css'
-interface Props {
-  hasOptedIn: boolean | null
-  optIn: () => unknown
-  optOut: () => unknown
-}
 
-interface SP {
-  hasOptedIn: Props['hasOptedIn']
-}
+export function GateModal(): JSX.Element | null {
+  const { t } = useTranslation(['card', 'button'])
+  const hasOptedIn = useSelector(analyticsSelectors.getHasOptedIn)
+  const dispatch = useDispatch()
 
-interface DP {
-  optIn: Props['optIn']
-  optOut: Props['optOut']
-}
-
-class GateModalComponent extends React.Component<Props> {
-  render(): JSX.Element | null {
-    const { optIn, optOut } = this.props
-
-    if (this.props.hasOptedIn == null) {
-      return (
-        <AlertModal
-          alertOverlay
-          className={cx(modalStyles.modal, modalStyles.blocking)}
-          buttons={[
-            { onClick: optOut, children: i18n.t('button.no') },
-            { onClick: optIn, children: i18n.t('button.yes') },
-          ]}
-        >
-          <h3>{i18n.t('card.toggle.share_session')}</h3>
-          <div className={settingsStyles.body_wrapper}>
-            <p className={settingsStyles.card_body}>
-              {i18n.t('card.body.reason_for_collecting_data')}
-            </p>
-            <ul className={settingsStyles.card_point_list}>
-              <li>{i18n.t('card.body.data_collected_is_internal')}</li>
-              <li>{i18n.t('card.body.data_only_from_pd')}</li>
-              <li>{i18n.t('card.body.opt_out_of_data_collection')}</li>
-            </ul>
-          </div>
-        </AlertModal>
-      )
-    }
+  if (hasOptedIn == null) {
+    return (
+      <AlertModal
+        alertOverlay
+        className={cx(modalStyles.modal, modalStyles.blocking)}
+        buttons={[
+          {
+            onClick: () => dispatch(analyticsActions.optOut()),
+            children: t('button:no'),
+          },
+          {
+            onClick: () => dispatch(analyticsActions.optIn()),
+            children: t('button:yes'),
+          },
+        ]}
+      >
+        <h3>{t('toggle.share_session')}</h3>
+        <div className={settingsStyles.body_wrapper}>
+          <p className={settingsStyles.card_body}>
+            {t('body.reason_for_collecting_data')}
+          </p>
+          <ul className={settingsStyles.card_point_list}>
+            <li>{t('body.data_collected_is_internal')}</li>
+            <li>{t('body.data_only_from_pd')}</li>
+            <li>{t('body.opt_out_of_data_collection')}</li>
+          </ul>
+        </div>
+      </AlertModal>
+    )
+  } else {
     return null
   }
 }
-
-function mapStateToProps(state: BaseState): SP {
-  return { hasOptedIn: analyticsSelectors.getHasOptedIn(state) }
-}
-
-function mapDispatchToProps(dispatch: ThunkDispatch<any>): DP {
-  return {
-    optIn: () => dispatch(analyticsActions.optIn()),
-    optOut: () => dispatch(analyticsActions.optOut()),
-  }
-}
-
-export const GateModal = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(GateModalComponent)
