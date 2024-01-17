574754994107c1541588923f1a5b1e2e39949f84
Brian Cooper <brian@opentrons.com>
2024-01-17 13:42:58 -0500

    remove --font-body-2-dark

--- components/src/primitives/Text.tsx
+++ components/src/primitives/Text.tsx
@@ -4,9 +4,6 @@ import { styleProps, isntStyleProp } from './style-props'
 
 import type { PrimitiveComponent } from './types'
 
-// TODO(mc, 2020-05-08): add variants (--font-body-2-dark, etc) as variant prop
-// or as components that compose the base Text component
-
 /**
  * Text primitive
  *
