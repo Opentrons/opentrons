3ecc315e7add03f7c8f951954d8584e31c7fc613
Jethary <jethary@opentrons.com>
2024-01-18 14:40:06 -0500

    being to refactor out connect fns

--- protocol-designer/src/components/SettingsPage/index.tsx
+++ protocol-designer/src/components/SettingsPage/index.tsx
@@ -1,27 +1,17 @@
 import * as React from 'react'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-import { BaseState } from '../../types'
-import { selectors, Page } from '../../navigation'
+import { selectors } from '../../navigation'
 import { SettingsApp } from './SettingsApp'
 
 export { SettingsSidebar } from './SettingsSidebar'
 
-interface Props {
-  currentPage: Page
-}
-
-const SettingsPageComponent = (props: Props): JSX.Element => {
-  switch (props.currentPage) {
+export const SettingsPage = (): JSX.Element => {
+  const currentPage = useSelector(selectors.getCurrentPage)
+  switch (currentPage) {
     // TODO: Ian 2019-08-21 when we have other pages, put them here
     case 'settings-app':
     default:
       return <SettingsApp />
   }
 }
-
-const STP = (state: BaseState): Props => ({
-  currentPage: selectors.getCurrentPage(state),
-})
-
-export const SettingsPage = connect(STP)(SettingsPageComponent)
