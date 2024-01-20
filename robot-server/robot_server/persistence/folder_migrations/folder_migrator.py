776d6c680b06e9275a2ce3ee727420a5317fc086
Max Marrone <max@opentrons.com>
2024-01-20 00:53:52 -0500

    Comment and docstring clarifications.

--- robot-server/robot_server/persistence/folder_migrations/folder_migrator.py
+++ robot-server/robot_server/persistence/folder_migrations/folder_migrator.py
@@ -1,3 +1,20 @@
+"""A versioning and migration system for data stored on the filesystem.
+
+This helps you maintain a file tree like this:
+
+    our_data/
+        v1/
+            foo.json
+            bar.db
+        v2/
+            foo.json
+            bar.db
+        ...
+
+Where each version has its own isolated subdirectory.
+"""
+
+
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
@@ -14,6 +31,8 @@ _log = logging.getLogger(__name__)
 
 
 class MigrationOrchestrator:
+    """Sequences migrations and performs them in order."""
+
     def __init__(
         self,
         *,
@@ -21,12 +40,41 @@ class MigrationOrchestrator:
         migrations: List[Migration],
         temp_file_prefix: str,
     ) -> None:
-        """Sequence migrations and perform them in order."""
+        """Configure the `MigrationOrchestrator`.
+
+        Args:
+            root: Where to hold all the version subdirectories.
+                For example, if you want a file tree like this:
+
+                    foo/
+                        v1/
+                        v2/
+                        ...
+
+                Then `foo/` is the root.
+
+            migrations: All known current and historical versions. This defines the
+                set of known version subdirectories, and how to migrate between them.
+
+            temp_file_prefix: A file name prefix for when the migration code needs to
+                place temporary files or directories in the root. You must ensure that
+                this doesn't overlap with any legitimate files.
+        """
         self._root = root
         self._migrations = migrations
         self._temp_file_prefix = temp_file_prefix
 
     def migrate_to_latest(self) -> Path:
+        """Perform any required migrations to bring us up to the latest version.
+
+        This inspects the filesystem to figure out what version we're on now,
+        then runs all the necessary migrations, in sequence, to bring us to the latest
+        version that was configured in `__init__()`.
+
+        The migration is performed atomically.
+
+        :returns: The path to the latest version subdirectory.
+        """
         current = self._get_current()
         if current is None:
             sequence = self._migrations
@@ -56,11 +104,11 @@ class MigrationOrchestrator:
                         )
                     )
                 else:
-                    # For intermediate migrations, use normal system temporary
-                    # directories, and never commit them to persistent storage.
-                    # This is inherently safer in case we crash and leave anything
-                    # behind, and it's also possibly faster (tmpfs instead of flash
-                    # storage).
+                    # Unlike the final migration, for intermediate migrations,
+                    # we use normal temporary directories in the default system
+                    # location. This is inherently safer in case we crash and leave
+                    # anything behind, and it's also possibly faster (tmpfs instead of
+                    # flash storage).
                     #
                     # TODO: Consider freeing each directory when we're done with it.
                     # e.g. if we're migrating from v1 to v10, we can free the temporary
@@ -111,15 +159,25 @@ class MigrationOrchestrator:
 
 
 class Migration(ABC):
+    """Represents a single migration step (e.g. v1->v2, not v1->v2->v3).
+
+    Subclass this and override `migrate()` to implement the step's actual migration
+    logic.
+    """
+
     def __init__(self, subdirectory: str) -> None:
-        """Represents a single migration, e.g. from v1 to v2.
+        """Configure the migration step.
 
-        Subclass this and override `migrate()` to implement your actual migration logic.
+        :param subdirectory: The subdirectory name for this version. For example, if
+            you want a file tree like this:
 
-        :param subdirectory: Where, under the `MigrationOrchestrator`'s root
-            directory, this migration should output. e.g. if you have
-            `foo/bar/storage/v1` and `foo/bar/storage/v2`, the instance of this
-            class that creates `foo/bar/storage/v2` should have `v2` for this argument.
+                our_data/
+                    v1/
+                    v2/
+                    ...
+
+            There would be one `Migration` where `subdirectory` is "v1", one where
+            `subdirectory` is "v2", and so on.
         """
         _validate_bare_name(subdirectory)
         self.subdirectory: Final = subdirectory
@@ -128,17 +186,18 @@ class Migration(ABC):
     def migrate(self, source_dir: Path, dest_dir: Path) -> None:
         """Perform the migration.
 
-        `source_dir` is the directory that was output by the previous migration.
-        If this is the first migration in the sequence, it will be the
-        `MigrationOrchestrator`'s root.
+        `source_dir` is the directory of the version before this one, the version that
+        we're migrating from. Or, if this is the first migration in the sequence,
+        it will be the `MigrationOrchestrator`'s root.
 
-        `dest_dir` is where this method should output. It will be a temporary directory;
-        if everything goes well, it will be moved into place atomically.
+        `dest_dir` is where this method should output its new, migrated files.
+        It will be a temporary directory; if everything goes well, it will be moved
+        into place atomically.
 
         A typical implementation should:
 
         1. Open known files from `source_dir`.
-        2. Optionally transform their contents.
+        2. Transform their contents, as needed.
         3. Write the new transformed files to `dest_dir`.
 
         If something goes wrong, this method should signal it by raising an exception.
@@ -148,13 +207,15 @@ class Migration(ABC):
 
 @contextlib.contextmanager
 def _atomic_dir(destination: Path, temp_prefix: str) -> Generator[Path, None, None]:
-    """Return a temporary directory.
+    """Atomically create a directory and its contained files.
 
-    If the code inside the `with`-block succeeds, promote the directory to be
-    non-temporary by atomically moving it to be at `destination`.
+    This returns a temporary directory. While the `with`-block is open, you can fill
+    it with files. Then:
 
-    If the code inside the `with`-block raises an exception, delete the temporary
-    directory.
+    * If the code inside the `with`-block succeeds, the directory is promoted to be
+      non-temporary, by atomically moving it to be at `destination`.
+    * Or, if the code inside the `with`-block raises an exception, the temporary
+      directory is deleted.
 
     Crashes or shutdowns may leave the temporary directory behind, adjacent to
     `destination`. Choose a `temp_prefix` that's easily identifiable so you can
@@ -162,9 +223,7 @@ def _atomic_dir(destination: Path, temp_prefix: str) -> Generator[Path, None, No
     """
     temp_dir = tempfile.mkdtemp(
         # Manually specify `dir` to keep the temporary directory on the same filesystem
-        # as our target. Otherwise, the rename won't be atomic. e.g. if `dir` were
-        # automatically chosen to be /tmp, on tmpfs, our "rename" would just be a full
-        # recursive copy followed by a full recursive delete.
+        # as our target. Otherwise, the rename won't be atomic.
         dir=destination.parent,
         prefix=temp_prefix,
     )
