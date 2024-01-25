aedbbe49d3047b3716967eff52418a72bf955d73
Max Marrone <max@opentrons.com>
2024-01-25 11:47:04 -0500

    Consolidate loops.

--- robot-server/robot_server/persistence/_migrations/up_to_3.py
+++ robot-server/robot_server/persistence/_migrations/up_to_3.py
@@ -4,11 +4,7 @@ from typing import Any, Dict, List
 
 import sqlalchemy
 
-from .._database import (
-    create_schema_2_sql_engine,
-    create_schema_3_sql_engine,
-    sqlite_rowid,
-)
+from .._database import create_schema_2_sql_engine, create_schema_3_sql_engine
 from .._folder_migrator import Migration
 from .._tables import schema_2, schema_3
 from ._util import copy_rows_unmodified, copy_if_exists, copytree_if_exists
@@ -80,44 +76,34 @@ def _migrate_run_table(
     source_transaction: sqlalchemy.engine.Connection,
     dest_transaction: sqlalchemy.engine.Connection,
 ) -> None:
-    select_everything_except_commands = sqlalchemy.select(
-        schema_2.run_table.c.id,
-        schema_2.run_table.c.created_at,
-        schema_2.run_table.c.protocol_id,
-        schema_2.run_table.c.state_summary,
-        schema_2.run_table.c.engine_status,
-        schema_2.run_table.c._updated_at,
-    ).order_by(sqlite_rowid)
+    select_old_run = sqlalchemy.select(schema_2.run_table)
     insert_new_run = sqlalchemy.insert(schema_3.run_table)
-
-    for old_run_row in (
-        source_transaction.execute(select_everything_except_commands).mappings().all()
-    ):
-        # Insert one at a time to retain sqlite rowid ordering.
-        # Providing many rows at once to execute() may not preserve order.
-        # https://www.mail-archive.com/db-sig@python.org/msg02071.html
-        dest_transaction.execute(insert_new_run, old_run_row)
-
-    select_commands = sqlalchemy.select(
-        schema_2.run_table.c.id, schema_2.run_table.c.commands
-    )
     insert_new_command = sqlalchemy.insert(schema_3.run_command_table)
 
-    for old_command_row in source_transaction.execute(select_commands).all():
-        run_id = old_command_row.id
-        commands: List[Dict[str, Any]] = old_command_row.commands or []
+    for old_run_row in source_transaction.execute(select_old_run).all():
+        dest_transaction.execute(
+            insert_new_run,
+            id=old_run_row.id,
+            created_at=old_run_row.created_at,
+            protocol_id=old_run_row.protocol_id,
+            state_summary=old_run_row.state_summary,
+            engine_status=old_run_row.engine_status,
+            _updated_at=old_run_row._updated_at,
+        )
+
+        commands: List[Dict[str, Any]] = old_run_row.commands or []
         new_command_rows = [
             {
-                "run_id": run_id,
+                "run_id": old_run_row.id,
                 "index_in_run": index_in_run,
                 "command_id": command["id"],
                 "command": command,
             }
             for index_in_run, command in enumerate(commands)
         ]
-        # Insert all the commands in one go, to avoid the overhead of separate
-        # statements, and since we had to bring them all into memory at once in order to
-        # parse them anyway.
+        # Insert all the commands for this run in one go, to avoid the overhead of
+        # separate statements, and since we had to bring them all into memory at once
+        # in order to parse them anyway.
         if len(new_command_rows) > 0:
             # This needs to be guarded by a len>0 check because if the list is empty,
             # SQLAlchemy misinterprets this as inserting a single row with all default
