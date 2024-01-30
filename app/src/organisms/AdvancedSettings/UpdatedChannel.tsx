ee4811818365b57238f1ebc57f68e8f28dc820de
koji <koji.kanao@opentrons.com>
2024-01-31 08:07:23 +0900

    refactor(app): refactor Updated channel section (#14379)
    * refactor(app): refactor Updated channel section

---
+++ app/src/organisms/AdvancedSettings/UpdatedChannel.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+
+import {
+  Flex,
+  ALIGN_CENTER,
+  JUSTIFY_SPACE_BETWEEN,
+  DIRECTION_COLUMN,
+  TYPOGRAPHY,
+  SPACING,
+} from '@opentrons/components'
+
+import { StyledText } from '../../atoms/text'
+import { SelectField } from '../../atoms/SelectField'
+import {
+  getUpdateChannel,
+  getUpdateChannelOptions,
+  updateConfigValue,
+} from '../../redux/config'
+
+import type { SelectOption } from '../../atoms/SelectField/Select'
+import type { Dispatch } from '../../redux/types'
+
+export function UpdatedChannel(): JSX.Element {
+  const { t } = useTranslation('app_settings')
+  const dispatch = useDispatch<Dispatch>()
+  const channel = useSelector(getUpdateChannel)
+  const channelOptions: SelectOption[] = useSelector(getUpdateChannelOptions)
+  const handleChannel = (_: string, value: string): void => {
+    dispatch(updateConfigValue('update.channel', value))
+  }
+
+  const formatOptionLabel: React.ComponentProps<
+    typeof SelectField
+  >['formatOptionLabel'] = (option, index): JSX.Element => {
+    const { label, value } = option
+    return (
+      <StyledText
+        as="p"
+        textTransform={TYPOGRAPHY.textTransformCapitalize}
+        id={index}
+      >
+        {value === 'latest' ? label : value}
+      </StyledText>
+    )
+  }
+
+  return (
+    <Flex
+      alignItems={ALIGN_CENTER}
+      justifyContent={JUSTIFY_SPACE_BETWEEN}
+      gridGap={SPACING.spacing40}
+    >
+      <Flex flexDirection={DIRECTION_COLUMN}>
+        <StyledText
+          css={TYPOGRAPHY.h3SemiBold}
+          paddingBottom={SPACING.spacing8}
+          id="AdvancedSettings_updatedChannel"
+        >
+          {t('update_channel')}
+        </StyledText>
+        <StyledText as="p" paddingBottom={SPACING.spacing8}>
+          {t('update_description')}
+        </StyledText>
+      </Flex>
+      <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing8}>
+        <StyledText css={TYPOGRAPHY.labelSemiBold}>{t('channel')}</StyledText>
+        <SelectField
+          name={'__UpdateChannel__'}
+          options={channelOptions}
+          onValueChange={handleChannel}
+          value={channel}
+          placeholder={channel}
+          formatOptionLabel={formatOptionLabel}
+          isSearchable={false}
+          width="10rem"
+        />
+      </Flex>
+    </Flex>
+  )
+}
--- app/src/pages/AppSettings/AdvancedSettings.tsx
+++ app/src/pages/AppSettings/AdvancedSettings.tsx
@@ -1,22 +1,6 @@
 import * as React from 'react'
-import { useTranslation } from 'react-i18next'
-import { useSelector, useDispatch } from 'react-redux'
-
-import {
-  ALIGN_CENTER,
-  Box,
-  DIRECTION_COLUMN,
-  Flex,
-  JUSTIFY_SPACE_BETWEEN,
-  SPACING,
-  TYPOGRAPHY,
-} from '@opentrons/components'
-
-import * as Config from '../../redux/config'
-import { SelectOption } from '../../atoms/SelectField/Select'
-import { SelectField } from '../../atoms/SelectField'
+import { Box, SPACING } from '@opentrons/components'
 import { Divider } from '../../atoms/structure'
-import { StyledText } from '../../atoms/text'
 import {
   ClearUnavailableRobots,
   EnableDevTools,
@@ -24,75 +8,17 @@ import {
   OverridePathToPython,
   PreventRobotCaching,
   ShowHeaterShakerAttachmentModal,
-  U2EInformation,
   ShowLabwareOffsetSnippets,
+  U2EInformation,
+  UpdatedChannel,
   AdditionalCustomLabwareSourceFolder,
 } from '../../organisms/AdvancedSettings'
 
-import type { Dispatch } from '../../redux/types'
-
 export function AdvancedSettings(): JSX.Element {
-  const { t } = useTranslation(['app_settings', 'shared'])
-  const channel = useSelector(Config.getUpdateChannel)
-  const channelOptions: SelectOption[] = useSelector(
-    Config.getUpdateChannelOptions
-  )
-  const dispatch = useDispatch<Dispatch>()
-  const handleChannel = (_: string, value: string): void => {
-    dispatch(Config.updateConfigValue('update.channel', value))
-  }
-
-  const formatOptionLabel: React.ComponentProps<
-    typeof SelectField
-  >['formatOptionLabel'] = (option, index): JSX.Element => {
-    const { label, value } = option
-    return (
-      <StyledText
-        as="p"
-        textTransform={TYPOGRAPHY.textTransformCapitalize}
-        id={index}
-      >
-        {value === 'latest' ? label : value}
-      </StyledText>
-    )
-  }
-
   return (
     <>
       <Box paddingX={SPACING.spacing16} paddingY={SPACING.spacing24}>
-        <Flex
-          alignItems={ALIGN_CENTER}
-          justifyContent={JUSTIFY_SPACE_BETWEEN}
-          gridGap={SPACING.spacing40}
-        >
-          <Flex flexDirection={DIRECTION_COLUMN}>
-            <StyledText
-              css={TYPOGRAPHY.h3SemiBold}
-              paddingBottom={SPACING.spacing8}
-              id="AdvancedSettings_updatedChannel"
-            >
-              {t('update_channel')}
-            </StyledText>
-            <StyledText as="p" paddingBottom={SPACING.spacing8}>
-              {t('update_description')}
-            </StyledText>
-          </Flex>
-          <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing8}>
-            <StyledText css={TYPOGRAPHY.labelSemiBold}>
-              {t('channel')}
-            </StyledText>
-            <SelectField
-              name={'__UpdateChannel__'}
-              options={channelOptions}
-              onValueChange={handleChannel}
-              value={channel}
-              placeholder={channel}
-              formatOptionLabel={formatOptionLabel}
-              isSearchable={false}
-              width="10rem"
-            />
-          </Flex>
-        </Flex>
+        <UpdatedChannel />
         <Divider marginY={SPACING.spacing24} />
         <AdditionalCustomLabwareSourceFolder />
         <Divider marginY={SPACING.spacing24} />
