56fe384fc8d09166c86394338d2745bfcd9daa35
caila-marashaj <caila.marashaj@opentrons.com>
2024-01-30 12:13:35 -0500

    change liquid_probe to support synced reading

--- hardware/opentrons_hardware/hardware_control/move_group_runner.py
+++ hardware/opentrons_hardware/hardware_control/move_group_runner.py
@@ -307,8 +307,7 @@ class MoveGroupRunner:
             )
             return HomeRequest(payload=home_payload)
         elif step.move_type == MoveType.sensor:
-            # stop_condition = step.stop_condition.value
-            stop_condition = MoveStopCondition.sync_line
+            stop_condition = step.stop_condition.value
             sensor_move_payload = AddSensorLinearMoveBasePayload(
                 request_stop_condition=MoveStopConditionField(stop_condition),
                 group_id=UInt8Field(group),
