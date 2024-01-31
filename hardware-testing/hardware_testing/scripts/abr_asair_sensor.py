e8a7d136919a4ec0d36d1fa1956c99ba2cf8145f
rclarke0 <rhyann.clarke@opentrons.com>
2024-01-31 14:37:56 -0500

    commented out temp humidity abr sensor script

--- hardware-testing/hardware_testing/scripts/abr_asair_sensor.py
+++ hardware-testing/hardware_testing/scripts/abr_asair_sensor.py
@@ -1,8 +1,6 @@
-from hardware_testing import data
-from hardware_testing.drivers import asair_sensor
+# from hardware_testing import data
+# from hardware_testing.drivers import asair_sensor
 
 
-if __name__ == "__main__":
-    asair_sensor.connect()
-    reading = asair_sensor.get_reading()
-    print(reading)
+# if __name__ == "__main__":
+    
