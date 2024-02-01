1df37faf2e30ece01f68779a2f8853c7fc75d113
Shlok Amin <shlokamin@me.com>
2024-02-01 15:33:41 -0500

    remove require.context call from the app

--- app/src/assets/labware/getLabware.ts
+++ app/src/assets/labware/getLabware.ts
@@ -1,76 +1,23 @@
-import groupBy from 'lodash/groupBy'
-import type {
-  LabwareDefinition1,
-  LabwareDefinition2,
+import {
+  getAllLegacyDefinitions,
+  getAllDefinitions as getLatestDefinitions,
+  type LabwareDefinition1,
+  type LabwareDefinition2,
+  type LabwareDefByDefURI,
 } from '@opentrons/shared-data'
-
-// require all definitions in the labware/definitions/1 directory
-// require.context is webpack-specific method
-const labwareSchemaV1DefsContext = require.context(
-  '@opentrons/shared-data/labware/definitions/1',
-  true, // traverse subdirectories
-  /\.json$/, // import filter
-  'sync' // load every definition into one synchronous chunk
-)
-let labwareSchemaV1Defs: Readonly<LabwareDefinition1[]> | null = null
-function getLegacyLabwareDefs(): Readonly<LabwareDefinition1[]> {
-  if (!labwareSchemaV1Defs) {
-    labwareSchemaV1Defs = labwareSchemaV1DefsContext
-      .keys()
-      .map((name: string) => labwareSchemaV1DefsContext(name))
-  }
-
-  return labwareSchemaV1Defs as Readonly<LabwareDefinition1[]>
-}
 
 export function getLegacyLabwareDef(
   loadName: string | null | undefined
 ): LabwareDefinition1 | null {
-  const def = getLegacyLabwareDefs().find(d => d.metadata.name === loadName)
-  return def || null
-}
-
-// require all definitions in the labware/definitions/2 directory
-// require.context is webpack-specific method
-const labwareSchemaV2DefsContext = (require as any).context(
-  '@opentrons/shared-data/labware/definitions/2',
-  true, // traverse subdirectories
-  /\.json$/, // import filter
-  'sync' // load every definition into one synchronous chunk
-)
-
-let labwareSchemaV2Defs: LabwareDefinition2[] | null = null
-function getLatestLabwareDefs(): LabwareDefinition2[] {
-  // NOTE: unlike labware-library, no filtering out "do not list labware"
-  // also, more convenient & performant to make a map {loadName: def} not an array
-  if (!labwareSchemaV2Defs) {
-    const allDefs = labwareSchemaV2DefsContext
-      .keys()
-      .map((name: string) => labwareSchemaV2DefsContext(name))
-    // group by namespace + loadName
-    const labwareDefGroups: {
-      [groupKey: string]: LabwareDefinition2[]
-    } = groupBy(allDefs, d => `${d.namespace}/${d.parameters.loadName}`)
-
-    labwareSchemaV2Defs = Object.keys(labwareDefGroups).map(
-      (groupKey: string) => {
-        const group = labwareDefGroups[groupKey]
-        const allVersions = group.map(d => d.version)
-        const highestVersionNum = Math.max(...allVersions)
-        const resultIdx = group.findIndex(d => d.version === highestVersionNum)
-        return group[resultIdx]
-      }
-    )
+  if (loadName != null) {
+    return getAllLegacyDefinitions()[loadName]
   }
-
-  return labwareSchemaV2Defs
+  return null
 }
 
 export function getLatestLabwareDef(
   loadName: string | null | undefined
 ): LabwareDefinition2 | null {
-  const def = getLatestLabwareDefs().find(
-    d => d.parameters.loadName === loadName
-  )
+  const def = Object.values(getLatestDefinitions()).find(d => d.parameters.loadName === loadName)
   return def || null
 }
