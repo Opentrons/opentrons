a0fe77a46797a9d01c9c17174dd0196286788ea5
Jethary <jethary@opentrons.com>
2024-02-05 17:36:33 -0500

    remove portal

--- app/src/organisms/AnalyticsSettingsModal/index.tsx
+++ app/src/organisms/AnalyticsSettingsModal/index.tsx
@@ -21,7 +21,6 @@ import {
 } from '../../redux/analytics'
 import { LegacyModal } from '../../molecules/LegacyModal'
 import { StyledText } from '../../atoms/text'
-import { Portal } from '../../App/portal'
 import type { Dispatch } from '../../redux/types'
 
 const PRIVACY_POLICY_LINK = 'https://opentrons.com/privacy-policy'
@@ -37,57 +36,55 @@ export function AnalyticsSettingsModal(): JSX.Element | null {
     dispatch(toggleAnalyticsOptedIn())
   }
 
-  return !seen ? (
-    <Portal>
-      <LegacyModal
-        title={
-          <StyledText css={TYPOGRAPHY.h3SemiBold}>
-            {t('acknowledge_privacy')}
-          </StyledText>
-        }
-        footer={
-          <Flex
-            justifyContent={JUSTIFY_FLEX_END}
-            paddingRight={SPACING.spacing16}
-            paddingBottom={SPACING.spacing16}
-          >
-            <PrimaryButton onClick={handleClick}>
-              <StyledText as="p">{t('agree')}</StyledText>
-            </PrimaryButton>
-          </Flex>
-        }
-      >
-        <Flex flexDirection={DIRECTION_COLUMN}>
+  return seen ? (
+    <LegacyModal
+      title={
+        <StyledText css={TYPOGRAPHY.h3SemiBold}>
+          {t('acknowledge_privacy')}
+        </StyledText>
+      }
+      footer={
+        <Flex
+          justifyContent={JUSTIFY_FLEX_END}
+          paddingRight={SPACING.spacing16}
+          paddingBottom={SPACING.spacing16}
+        >
+          <PrimaryButton onClick={handleClick}>
+            <StyledText as="p">{t('agree')}</StyledText>
+          </PrimaryButton>
+        </Flex>
+      }
+    >
+      <Flex flexDirection={DIRECTION_COLUMN}>
+        <Flex gridGap={SPACING.spacing10} flexDirection={DIRECTION_COLUMN}>
           <Flex gridGap={SPACING.spacing10} flexDirection={DIRECTION_COLUMN}>
-            <Flex gridGap={SPACING.spacing10} flexDirection={DIRECTION_COLUMN}>
-              <Trans
-                t={t}
-                i18nKey={t('privacy_body')}
-                components={{ block: <StyledText as="p" /> }}
+            <Trans
+              t={t}
+              i18nKey={t('privacy_body')}
+              components={{ block: <StyledText as="p" /> }}
+            />
+          </Flex>
+          <Link
+            external
+            fontSize={TYPOGRAPHY.fontSizeP}
+            css={TYPOGRAPHY.darkLinkLabelSemiBold}
+            href={PRIVACY_POLICY_LINK}
+          >
+            <Flex
+              flexDirection={DIRECTION_ROW}
+              alignItems={ALIGN_CENTER}
+              color={COLORS.blue50}
+            >
+              {t('opentrons_privacy_policy')}
+              <Icon
+                marginLeft={SPACING.spacing4}
+                name="external-link"
+                size="0.75rem"
               />
             </Flex>
-            <Link
-              external
-              fontSize={TYPOGRAPHY.fontSizeP}
-              css={TYPOGRAPHY.darkLinkLabelSemiBold}
-              href={PRIVACY_POLICY_LINK}
-            >
-              <Flex
-                flexDirection={DIRECTION_ROW}
-                alignItems={ALIGN_CENTER}
-                color={COLORS.blue50}
-              >
-                {t('opentrons_privacy_policy')}
-                <Icon
-                  marginLeft={SPACING.spacing4}
-                  name="external-link"
-                  size="0.75rem"
-                />
-              </Flex>
-            </Link>
-          </Flex>
+          </Link>
         </Flex>
-      </LegacyModal>
-    </Portal>
+      </Flex>
+    </LegacyModal>
   ) : null
 }
