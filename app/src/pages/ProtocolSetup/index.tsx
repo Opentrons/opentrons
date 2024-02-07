93b65534cb36a98a632a1742fc7761b39010ee60
koji <koji.kanao@opentrons.com>
2024-02-07 18:24:41 +0900

    update colors

--- app/src/pages/ProtocolSetup/index.tsx
+++ app/src/pages/ProtocolSetup/index.tsx
@@ -138,7 +138,7 @@ export function ProtocolSetupStep({
   if (!disabled) {
     switch (status) {
       case 'general':
-        backgroundColor = COLORS.blue35
+        backgroundColor = COLORS.grey40
         break
       case 'ready':
         backgroundColor = COLORS.green40
