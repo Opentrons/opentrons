36758863862d41c0a8c42b553ff0ed88c58fcf72
Ryan howard <ryan.howard@opentrons.com>
2024-02-09 14:33:34 -0500

    add the dial slot to the other liquid sense lpc protocols

--- hardware-testing/hardware_testing/protocols/liquid_sense_lpc/liquid_sense_ot3_p1000_96.py
+++ hardware-testing/hardware_testing/protocols/liquid_sense_lpc/liquid_sense_ot3_p1000_96.py
@@ -5,11 +5,12 @@ metadata = {"protocolName": "liquid-sense-ot3-p1000-96"}
 requirements = {"robotType": "Flex", "apiLevel": "2.15"}
 
 SLOT_SCALE = 4
+SLOT_DIAL = 5
 SLOTS_TIPRACK = {
     # TODO: add slot 12 when tipracks are disposable
-    50: [2, 3, 5, 6, 7, 8, 9, 10, 11],
-    200: [2, 3, 5, 6, 7, 8, 9, 10, 11],  # NOTE: ignored during calibration
-    1000: [2, 3, 5, 6, 7, 8, 9, 10, 11],  # NOTE: ignored during calibration
+    50: [2, 3, 6, 7, 8, 9, 10, 11],
+    200: [2, 3, 6, 7, 8, 9, 10, 11],  # NOTE: ignored during calibration
+    1000: [2, 3, 6, 7, 8, 9, 10, 11],  # NOTE: ignored during calibration
 }
 
 LABWARE_ON_SCALE = "nest_1_reservoir_195ml"
