a21fcf9c3c8e4467bb040cd153f882cb64fd0d77
Seth Foster <seth@opentrons.com>
2024-02-12 18:00:54 -0500

    fix(app-shell): detect usb devices after update
    When we updated to electron 27, our old package for detecting devices
    didn't work anymore. Instead we now use the npm package `usb`, which is
    a reimplementation of node-usb that also provides a WebUSB frontend.
    
    The integration here had a couple small problems; the webusb just straight
    up doesn't seem to work with these devices, and we were doing the
    callback glue slightly wrong.
    
    With both those problems fixed, we should once again detected device add
    and removal basically instantly, which fixes some issues with robot
    detection and removal.
    
    Closes RQA-2324

--- app-shell/src/system-info/usb-devices.ts
+++ app-shell/src/system-info/usb-devices.ts
@@ -1,6 +1,6 @@
 import assert from 'assert'
 import execa from 'execa'
-import { usb, WebUSB } from 'usb'
+import { usb } from 'usb'
 import { isWindows } from '../os'
 import { createLogger } from '../log'
 
@@ -12,14 +12,69 @@ export type UsbDeviceMonitorOptions = Partial<{
 }>
 
 export interface UsbDeviceMonitor {
-  getAllDevices: () => Promise<USBDevice[]>
+  getAllDevices: () => Promise<UsbDevice[]>
   stop: () => void
 }
 
 const log = createLogger('usb-devices')
-const webusb = new WebUSB({
-  allowAllDevices: true,
+
+const descriptorToDevice = (
+  descriptors: usb.Device,
+  manufacturerName?: string,
+  serialNumber?: string
+): UsbDevice => ({
+  vendorId: descriptors.deviceDescriptor.idVendor,
+  productId: descriptors.deviceDescriptor.idProduct,
+  serialNumber,
+  manufacturerName,
 })
+
+const getStringDescriptorPromise = (
+  device: usb.Device,
+  index: number
+): Promise<string> =>
+  new Promise((resolve, reject) =>
+    device.getStringDescriptor(index, (error?, value?) =>
+      !!error || !!!value ? reject(error) : resolve(value)
+    )
+  )
+
+const orDefault = <T, U>(
+  promise: Promise<T>,
+  defaulter: (err: any) => U
+): Promise<T | U> =>
+  promise.catch((err: any) => {
+    return new Promise(resolve => resolve(defaulter(err)))
+  })
+
+function upstreamDeviceFromUsbDevice(device: usb.Device): Promise<UsbDevice> {
+  return Promise.all([
+    orDefault(
+      getStringDescriptorPromise(device, device.deviceDescriptor.iManufacturer),
+      (err: any) => {
+        log.error(
+          `Failed to get manufacturer for vid=${device.deviceDescriptor.idVendor.toString(
+            16
+          )} pid=${device.deviceDescriptor.idProduct.toString(16)}: ${err}`
+        )
+        return undefined
+      }
+    ),
+    orDefault(
+      getStringDescriptorPromise(device, device.deviceDescriptor.iSerialNumber),
+      (err: any) => {
+        log.error(
+          `Failed to get serial for vid=${device.deviceDescriptor.idVendor.toString(
+            16
+          )} pid=${device.deviceDescriptor.idProduct.toString(16)}: ${err}`
+        )
+        return undefined
+      }
+    ),
+  ]).then(([manufacturer, serialNumber]) =>
+    descriptorToDevice(device, manufacturer, serialNumber)
+  )
+}
 
 export function createUsbDeviceMonitor(
   options: UsbDeviceMonitorOptions = {}
@@ -27,15 +82,22 @@ export function createUsbDeviceMonitor(
   const { onDeviceAdd, onDeviceRemove } = options
 
   if (typeof onDeviceAdd === 'function') {
-    usb.on('attach', device => onDeviceAdd)
+    usb.on('attach', device =>
+      upstreamDeviceFromUsbDevice(device).then(onDeviceAdd)
+    )
   }
 
   if (typeof onDeviceRemove === 'function') {
-    usb.on('detach', device => onDeviceRemove)
+    usb.on('detach', device => onDeviceRemove(descriptorToDevice(device)))
   }
 
   return {
-    getAllDevices: () => Promise.resolve(webusb.getDevices()),
+    getAllDevices: () =>
+      new Promise<usb.Device[]>((resolve, reject) => {
+        resolve(usb.getDeviceList())
+      }).then(deviceList =>
+        Promise.all(deviceList.map(upstreamDeviceFromUsbDevice))
+      ),
     stop: () => {
       if (typeof onDeviceAdd === 'function') {
         usb.removeAllListeners('attach')
