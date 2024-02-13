394a0c7b5ab38b09c63602677775e71bbfc69fd1
Ryan howard <ryan.howard@opentrons.com>
2024-02-13 14:16:43 -0500

    align up the start z of the of each trial in the final report

--- hardware-testing/hardware_testing/liquid_sense/post_process.py
+++ hardware-testing/hardware_testing/liquid_sense/post_process.py
@@ -9,34 +9,43 @@ COL_TRIAL_CONVERSION = {
     3: "J",
     4: "M",
     5: "P",
-    7: "S",
-    6: "V",
-    7: "Y",
-    8: "AB",
-    9: "AE",
-    10: "AH",
-    11: "AK",
-    12: "AN"}
-def process_csv_directory(data_directory: str, tips: List[int], trials: int) -> None:
+    6: "S",
+    7: "V",
+    8: "Y",
+    9: "AB",
+    10: "AE",
+    11: "AH",
+    12: "AK",
+    13: "AN",
+}
+
+
+def process_csv_directory(  # noqa: C901
+    data_directory: str, tips: List[int], trials: int
+) -> None:
     """Post process script csvs."""
     csv_files: List[str] = os.listdir(data_directory)
     summary: str = [f for f in csv_files if "CSVReport" in f][0]
     final_report_file: str = f"{data_directory}/final_report.csv"
-
+    # initalize our data structs
     pressure_csvs = [f for f in csv_files if "pressure_sensor_data" in f]
     pressure_results_files: Dict[int, List[str]] = {}
     pressure_results: Dict[int, Dict[int, List[float]]] = {}
     results_settings: Dict[int, Dict[int, Tuple[float, float, float]]] = {}
     tip_offsets: Dict[int, List[float]] = {}
+    p_offsets: Dict[int, List[float]] = {}
     for tip in tips:
         pressure_results_files[tip] = [f for f in pressure_csvs if f"tip{tip}" in f]
         pressure_results[tip] = {}
         results_settings[tip] = {}
         tip_offsets[tip] = []
+        p_offsets[tip] = []
         for trial in range(trials):
             pressure_results[tip][trial] = []
             results_settings[tip][trial] = (0.0, 0.0, 0.0)
     max_results_len = 0
+
+    # read in all of the pressure csvs into one big struct so we can process them
     for tip in tips:
         for trial in range(trials):
             with open(
@@ -47,15 +56,15 @@ def process_csv_directory(data_directory: str, tips: List[int], trials: int) -> 
                 for row in trial_reader:
                     if i == 1:
                         results_settings[tip][trial] = (
-                                float(row[2]),
-                                float(row[3]),
-                                float(row[4]),
-                            )
+                            float(row[2]),
+                            float(row[3]),
+                            float(row[4]),
+                        )
                     if i > 1:
                         pressure_results[tip][trial].append(float(row[1]))
                     i += 1
                 max_results_len = max([i - 2, max_results_len])
-
+    # start writing the final report csv
     with open(f"{data_directory}/{summary}", newline="") as summary_csv:
         summary_reader = csv.reader(summary_csv)
         with open(final_report_file, "w", newline="") as final_report:
@@ -64,26 +73,58 @@ def process_csv_directory(data_directory: str, tips: List[int], trials: int) -> 
             s = 0
             for row in summary_reader:
                 final_report_writer.writerow(row)
-                s+=1
-                if (s >= 46 and s < 46+(trials*len(tips))):
-                    tip_offsets[tips[int((s-46)/trials)]].append(float(row[8]))
+                s += 1
+                if s >= 46 and s < 46 + (trials * len(tips)):
+                    # while processing this grab the tip offsets from the summary
+                    tip_offsets[tips[int((s - 46) / trials)]].append(float(row[8]))
+            # summary_reader.line_num is the last line in the summary that has text
             pressures_start_line = summary_reader.line_num + 3
+            # calculate where the start and end of each block of data we want to graph
             final_report_writer.writerow(
                 [
                     "50ul",
-                    f"A{pressures_start_line}",
+                    f"A{pressures_start_line-1}",
                     f"{COL_TRIAL_CONVERSION[trials]}{pressures_start_line + max_results_len -1}",
                     "200ul",
-                    f"A{pressures_start_line+max_results_len}",
+                    f"A{pressures_start_line+max_results_len-1}",
                     f"{COL_TRIAL_CONVERSION[trials]}{pressures_start_line +(2*max_results_len)-1}",
                     "10000ul",
-                    f"A{pressures_start_line+(2*max_results_len)}",
+                    f"A{pressures_start_line+(2*max_results_len-1)}",
                     f"{COL_TRIAL_CONVERSION[trials]}{pressures_start_line + (3*max_results_len)-1}",
                 ]
             )
+
+            # build a header row
             pressure_header_row = ["time"]
             for i in range(trials):
-                pressure_header_row.extend(["pressure", "z_travel", "p_travel"])
+                pressure_header_row.extend(
+                    [f"pressure T{i+1}", f"z_travel T{i+1}", f"p_travel T{i+1}"]
+                )
+
+            # we want to line up the z height's of each trial at time==0
+            # to do this we drop the results at the beginning of each of the trials
+            # except for one with the longest tip (lower tip offset are longer tips)
+            for tip in tips:
+                min_tip_offset = min(tip_offsets[tip])
+                for trial in range(trials):
+                    for i in range(max_results_len):
+                        if tip_offsets[tip][trial] > min_tip_offset:
+                            # drop this pressure result
+                            pressure_results[tip][trial].pop(0)
+                            # we don't want to change the length of this array so just stretch out
+                            # the last value
+                            pressure_results[tip][trial].append(
+                                pressure_results[tip][trial][-1]
+                            )
+                            # decrement the offset while this is true so we can account for it later
+                            tip_offsets[tip][trial] -= i * 0.001
+                            # keep track of how this effects the plunger start position
+                            p_offsets[tip][trial] = i * 0.001
+                        else:
+                            # we've lined up this trial so move to the next
+                            break
+
+            # write the processed test data
             for tip in tips:
                 time = 0.0
                 final_report_writer.writerow(pressure_header_row)
@@ -94,7 +135,15 @@ def process_csv_directory(data_directory: str, tips: List[int], trials: int) -> 
                             pressure_row.append(f"{pressure_results[tip][trial][i]}")
                         else:
                             pressure_row.append("")
-                        pressure_row.append(f"{results_settings[tip][trial][0] * time - tip_offsets[tip][trial]}")
-                        pressure_row.append(f"{abs(results_settings[tip][trial][1]) * time}")
+                        pressure_row.append(
+                            f"{results_settings[tip][trial][0] * time - tip_offsets[tip][trial]}"
+                        )
+                        pressure_row.append(
+                            f"{abs(results_settings[tip][trial][1]) * time + p_offsets[tip][trial]}"
+                        )
                     final_report_writer.writerow(pressure_row)
                     time += 0.001
+
+
+if __name__ == "__main__":
+    process_csv_directory("/home/ryan/testdata", [50], 3)
