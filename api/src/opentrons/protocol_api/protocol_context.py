678d5c1f03a9a7172b0e32514c17b5404e9974fb
Max Marrone <max@opentrons.com>
2024-02-15 13:41:26 -0500

    docs(api): Update reference docs for `ProtocolContext.pause()` (#14500)
    Co-authored-by: Ed Cormany <edward.cormany@opentrons.com>

--- api/src/opentrons/protocol_api/protocol_context.py
+++ api/src/opentrons/protocol_api/protocol_context.py
@@ -979,9 +979,9 @@ class ProtocolContext(CommandPublisher):
 
         A human can resume the protocol in the Opentrons App or on the touchscreen.
 
-        This function returns immediately, but the next function call that
-        is blocked by a paused robot (anything that involves moving) will
-        not return until the protocol is resumed.
+        .. note::
+            In Python Protocol API version 2.13 and earlier, the pause will only
+            take effect on the next function call that involves moving the robot.
 
         :param str msg: An optional message to show in the run log entry for the pause step.
         """
