2280817e09e2bd11427e60637656c81c7be0a827
Shlok Amin <shlokamin@me.com>
2024-02-16 15:20:28 -0500

    update storybook

--- shared-data/vite.config.ts
+++ shared-data/vite.config.ts
@@ -1,39 +1,23 @@
 /* eslint-disable */
-import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
     // Relative to the root
-    outDir: 'dist',
+    ssr: 'src/js/index.ts',
+    outDir: 'lib',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      esmExternals: true,
+    },
   },
-  plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
       target: 'es2020',
     },
   },
-  css: {
-    postcss: {
-      plugins: [],
-    },
-  },
   define: {
     'process.env': process.env,
-    global: 'window',
-  },
-  resolve: {
-    alias: {
-      '@opentrons/components/styles': `@opentrons/components/src/index.module.css`,
-      '@opentrons/components': `@opentrons/components/src/index.ts`,
-      '@opentrons/shared-data': `@opentrons/shared-data/js/index.ts`,
-      '@opentrons/step-generation': `@opentrons/step-generation/src/index.ts`,
-      '@opentrons/api-client': `${path.resolve(__dirname, 'src')}/index.ts`,
-      '@opentrons/react-api-client': `${path.resolve(
-        __dirname,
-        'src'
-      )}/index.ts`,
-    },
+    global: 'globalThis',
   },
 })
