c19f26c99262e6d40f8d4e46fa1a49c592bb27bd
Ryan howard <ryan.howard@opentrons.com>
2024-02-26 15:34:42 -0500

    some updates to the script that let us change algorythms easily

--- hardware/opentrons_hardware/scripts/lld_data_script.py
+++ hardware/opentrons_hardware/scripts/lld_data_script.py
@@ -6,12 +6,98 @@ import matplotlib.pyplot as plot
 import numpy as np
 
 
+#----present day threshold based----
+def tick(pressure: float) -> bool:
+    return pressure < -150
+def reset() -> None:
+    pass
+
+
+"""
+#-----Simple moving average derivative---
+impossible_pressure = 9001.0
+samples_n = 3
+running_samples : List[float] = [impossible_pressure] * samples_n
+derivative_threshold = -1
+
+def reset() -> None:
+    running_samples = [impossible_pressure] * samples_n
+
+def tick(pressure: float) -> bool:
+    try:
+        next_ind = running_samples.index(impossible_pressure)
+        # if no exception we're still filling the minimum samples
+        running_samples[next_ind] = pressure
+        return False
+    except ValueError: # the array has been filled
+        pass
+    # store old running average
+    prev_running_avg = sum(running_samples)/samples_n
+    # left shift old samples
+    for i in range(samples_n-1):
+        running_samples[i] = running_samples[i+1]
+    running_samples[samples_n-1] = pressure
+    new_running_avg = sum(running_samples)/samples_n
+    return (new_running_avg - prev_running_avg) < derivative_threshold
+"""
+
+"""
+#-----weighted moving average derivative---
+import numpy
+impossible_pressure = 9001.0
+samples_n = 4
+weights = numpy.array([0.4, 0.3, 0.2, 0.1])
+running_samples = numpy.full(samples_n, impossible_pressure)
+derivative_threshold = -1
+
+def reset() -> None:
+    global running_samples
+    running_samples = numpy.full(samples_n, impossible_pressure)
+
+def tick(pressure: float) -> bool:
+    global running_samples
+    if numpy.isin(impossible_pressure, running_samples):
+        next_ind = numpy.where(running_samples==impossible_pressure)[0][0]
+        # if no exception we're still filling the minimum samples
+        running_samples[next_ind] = pressure
+        return False
+    # store old running average
+    prev_running_avg = numpy.sum(numpy.multiply(running_samples, weights))
+    # left shift old samples
+    for i in range(samples_n-1):
+        running_samples[i] = running_samples[i+1]
+    running_samples[samples_n-1] = pressure
+    new_running_avg = numpy.sum(numpy.multiply(running_samples, weights))
+    return (new_running_avg - prev_running_avg) < derivative_threshold
+"""
+
+"""
+#-----exponential moving average derivative---
+impossible_pressure: float = 9001.0
+current_average : float = impossible_pressure
+derivative_threshold = -1
+
+def reset() -> None:
+    global current_average
+    current_average = impossible_pressure
+
+def tick(pressure: float) -> bool:
+    global current_average
+    if current_average == impossible_pressure:
+        current_average = pressure
+        return False
+    else:
+        new_average = (pressure + current_average) / 2
+        derivative = new_average - current_average
+        print(derivative)
+        current_average = new_average
+        return derivative < derivative_threshold
+"""
+
 def running_avg(
     time: List, pressure: List, z_travel: List, p_travel: List
 ) -> Optional[Tuple[str, str, str]]:
-    pressure_derivative_threshold = 2
-    running_avg_threshold = 2
-
+    reset()
     average = float(pressure[0])
     running_time = []
     running_derivative = []
@@ -29,12 +115,14 @@ def running_avg(
         # print(running_avg_derivative)
 
         # there are kinda drastic changes in avg derivative in the very beginning
-        if i > 10 and abs(running_avg_derivative) > pressure_derivative_threshold:
+        if tick(float(pressure[i])):
         # if average < running_avg_threshold:
             # breakpoint()
-            print(f"found z height = {z_travel[i]}")
-            print(f"at time = {time[i]}")
+            #print(f"found z height = {z_travel[i]}")
+            #print(f"at time = {time[i]}")
             return_val = time[i], z_travel[i], p_travel[i]
+            # once we find it we don't need to keep going
+            break
     time_array = np.array(running_time)
     derivative_array = np.array(running_derivative)
     avg_array = np.array(running_avg)
@@ -65,11 +153,11 @@ def main() -> None:
             reader = csv.reader(file)
             reader_list = list(reader)
 
-        number_of_trials = int((len(reader_list[0]) - 1) / 3)
+        number_of_trials = int(reader_list[34][2])
 
+        expected_height = reader_list[44][6]
         # have a time list for each trial so the list lengths can all be equal
         for trial in range(number_of_trials):
-            expected_height = reader_list[44 + trial][2]
             print(f"Trial {trial}:")
             print(f"Expected z height: {expected_height}")
 
@@ -80,11 +168,11 @@ def main() -> None:
             pressure = []
             z_travel = []
             p_travel = []
-            for row in range(63, len(reader_list)):
+            for row in range(69, len(reader_list)):
                 current_time = reader_list[row][0]
-                current_pressure = reader_list[row][3 * trial + 1]
-                current_z_pos = reader_list[row][3 * trial + 2]
-                current_p_pos = reader_list[row][3 * trial + 3]
+                current_pressure = reader_list[row][3 * trial + 2]
+                current_z_pos = reader_list[row][3 * trial + 3]
+                current_p_pos = reader_list[row][3 * trial + 4]
 
                 if any(
                     [
