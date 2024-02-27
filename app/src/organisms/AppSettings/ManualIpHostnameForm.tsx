7a8836a46ddca310204f3dcc7f152284711f9de1
ncdiehl11 <ndiehl@opentrons.com>
2024-02-27 17:15:41 -0500

    lint

--- app/src/organisms/AppSettings/ManualIpHostnameForm.tsx
+++ app/src/organisms/AppSettings/ManualIpHostnameForm.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { useDispatch } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { useFormik } from 'formik'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 import {
   Flex,
