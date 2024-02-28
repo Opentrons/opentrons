5ac953a8dbe9eebb85bb2bfd79f289ebca99baba
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2024-02-28 12:38:55 -0500

    refactor(app): refactor notify hooks

--- app/src/resources/runs/useNotifyAllRunsQuery.ts
+++ app/src/resources/runs/useNotifyAllRunsQuery.ts
@@ -8,39 +8,37 @@ import type { UseQueryResult } from 'react-query'
 import type { AxiosError } from 'axios'
 import type { HostConfig, GetRunsParams, Runs } from '@opentrons/api-client'
 import type { UseAllRunsQueryOptions } from '@opentrons/react-api-client/src/runs/useAllRunsQuery'
-import type { QueryOptionsWithPolling } from '../useNotifyService'
+import type {
+  QueryOptionsWithPolling,
+  HTTPRefetchFrequency,
+} from '../useNotifyService'
 
 export function useNotifyAllRunsQuery(
   params: GetRunsParams = {},
   options: QueryOptionsWithPolling<UseAllRunsQueryOptions, AxiosError> = {},
   hostOverride?: HostConfig | null
 ): UseQueryResult<Runs, AxiosError> {
-  const [refetchUsingHTTP, setRefetchUsingHTTP] = React.useState(false)
+  const [
+    refetchUsingHTTP,
+    setRefetchUsingHTTP,
+  ] = React.useState<HTTPRefetchFrequency>(null)
 
-  const { isNotifyError } = useNotifyService<
-    UseAllRunsQueryOptions,
-    AxiosError
-  >({
+  useNotifyService<UseAllRunsQueryOptions, AxiosError>({
     topic: 'robot-server/runs',
-    refetchUsingHTTP: () => setRefetchUsingHTTP(true),
+    setRefetchUsingHTTP,
     options,
     hostOverride,
   })
-
-  const isNotifyEnabled =
-    !isNotifyError &&
-    !options?.forceHttpPolling &&
-    options?.staleTime !== Infinity
-
-  if (!isNotifyEnabled && !refetchUsingHTTP) setRefetchUsingHTTP(true)
-  const isHTTPEnabled = options?.enabled !== false && refetchUsingHTTP
 
   const httpResponse = useAllRunsQuery(
     params,
     {
       ...(options as UseAllRunsQueryOptions),
-      enabled: isHTTPEnabled,
-      onSettled: isNotifyEnabled ? () => setRefetchUsingHTTP(false) : undefined,
+      enabled: options?.enabled !== false && refetchUsingHTTP != null,
+      onSettled:
+        refetchUsingHTTP === 'once'
+          ? () => setRefetchUsingHTTP(null)
+          : () => null,
     },
     hostOverride
   )
