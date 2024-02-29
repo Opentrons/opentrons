d219837b4df3132305f9f0301b1c89025cf20856
Shlok Amin <shlokamin@me.com>
2024-02-29 13:04:09 -0500

    exclude node modules in common js options

--- app-shell/vite.config.ts
+++ app-shell/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(
         commonjsOptions: {
           transformMixedEsModules: true,
           esmExternals: true,
+          exclude: [/node_modules/],
         },
         lib: {
           entry: {
