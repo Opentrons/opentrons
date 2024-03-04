beaed06be38caa08cc3327547be02cbff23028fd
Derek Maggio <derek.j.maggio@gmail.com>
2024-03-04 08:58:41 -0800

    Remove smoke test comment

--- app-testing/files/protocols/py/OT2_P300M_P20S_TC_HS_TM_2_15_dispense_changes.py
+++ app-testing/files/protocols/py/OT2_P300M_P20S_TC_HS_TM_2_15_dispense_changes.py
@@ -1,5 +1,3 @@
-"""Smoke Test v3.0 """
-# https://opentrons.atlassian.net/projects/RQA?selectedItem=com.atlassian.plugins.atlassian-connect-plugin:com.kanoah.test-manager__main-project-page#!/testCase/QB-T497
 from opentrons import protocol_api
 
 metadata = {
