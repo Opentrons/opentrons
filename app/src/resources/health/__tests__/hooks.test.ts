df5aae3e3d2e506e7ce2d18c03a0e834aa793e48
koji <koji.kanao@opentrons.com>
2024-03-04 18:44:58 -0500

    Update hooks.test.ts

--- app/src/resources/health/__tests__/hooks.test.ts
+++ app/src/resources/health/__tests__/hooks.test.ts
@@ -1,18 +1,14 @@
 import { renderHook } from '@testing-library/react'
-
+import { describe, it, expect, vi } from 'vitest'
 import { useHealthQuery } from '@opentrons/react-api-client'
 
 import { useRobotInitializationStatus, INIT_STATUS } from '../hooks'
 
-jest.mock('@opentrons/react-api-client')
-
-const mockUseHealthQuery = (useHealthQuery as jest.MockedFunction<
-  typeof useHealthQuery
->) as jest.Mock
+vi.mock('@opentrons/react-api-client')
 
 describe('useRobotInitializationStatus', () => {
   it('should return "INITIALIZING" when response status code is 503', () => {
-    mockUseHealthQuery.mockImplementation(({ onSuccess }) => {
+    vi.mocked(useHealthQuery).mockImplementation(({ onSuccess }) => {
       onSuccess({ status: 503 })
     })
     const { result } = renderHook(() => useRobotInitializationStatus())
@@ -20,7 +16,7 @@ describe('useRobotInitializationStatus', () => {
   })
 
   it('should return "SUCCEEDED" when response status code is 200', () => {
-    mockUseHealthQuery.mockImplementation(({ onSuccess }) => {
+    vi.mocked(useHealthQuery).mockImplementation(({ onSuccess }) => {
       onSuccess({ status: 200 })
     })
     const { result } = renderHook(() => useRobotInitializationStatus())
@@ -28,7 +24,7 @@ describe('useRobotInitializationStatus', () => {
   })
 
   it('should return "FAILED" when response status code is 500', () => {
-    mockUseHealthQuery.mockImplementation(({ onSuccess }) => {
+    vi.mocked(useHealthQuery).mockImplementation(({ onSuccess }) => {
       onSuccess({ status: 500 })
     })
     const { result } = renderHook(() => useRobotInitializationStatus())
@@ -36,7 +32,7 @@ describe('useRobotInitializationStatus', () => {
   })
 
   it('should return null when response status code is not 200, 500, or 503.', () => {
-    mockUseHealthQuery.mockImplementation(({ onSuccess }) => {
+    vi.mocked(useHealthQuery).mockImplementation(({ onSuccess }) => {
       onSuccess({ status: 404 })
     })
     const { result } = renderHook(() => useRobotInitializationStatus())
