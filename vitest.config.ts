1ebd2cee6e1fb88b0db569999878e4443bff7c02
koji <koji.kanao@opentrons.com>
2024-03-05 00:58:57 -0500

    add discovery-client

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,29 +4,42 @@ import path from 'path'
 import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(viteConfig, defineConfig({
-  test: {
-    environment: 'jsdom',
-    allowOnly: true,
-    exclude: [
-      ...configDefaults.exclude,
-      '**/node_modules/**',
-      '**/dist/**'
-    ],
-    setupFiles: ['./setup-vitest.js'] 
-  },
-  resolve: {
-    alias: {
-      '@opentrons/components/styles': path.resolve('./components/src/index.module.css'),
-      '@opentrons/components': path.resolve('./components/src/index.ts'),
-      '@opentrons/shared-data/pipette/fixtures/name': path.resolve('./shared-data/pipette/fixtures/name/index.ts'),
-      '@opentrons/shared-data/labware/fixtures/1': path.resolve('./shared-data/labware/fixtures/1/index.ts'),
-      '@opentrons/shared-data/labware/fixtures/2': path.resolve('./shared-data/labware/fixtures/2/index.ts'),
-      '@opentrons/shared-data': path.resolve('./shared-data/js/index.ts'),
-      '@opentrons/step-generation': path.resolve('./step-generation/src/index.ts'),
-      '@opentrons/api-client': path.resolve('./api-client/src/index.ts'),
-      '@opentrons/react-api-client': path.resolve( './react-api-client/src/index.ts'),
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      allowOnly: true,
+      exclude: [...configDefaults.exclude, '**/node_modules/**', '**/dist/**'],
+      setupFiles: ['./setup-vitest.js'],
     },
-  },
-}))
-
+    resolve: {
+      alias: {
+        '@opentrons/components/styles': path.resolve(
+          './components/src/index.module.css'
+        ),
+        '@opentrons/components': path.resolve('./components/src/index.ts'),
+        '@opentrons/shared-data/pipette/fixtures/name': path.resolve(
+          './shared-data/pipette/fixtures/name/index.ts'
+        ),
+        '@opentrons/shared-data/labware/fixtures/1': path.resolve(
+          './shared-data/labware/fixtures/1/index.ts'
+        ),
+        '@opentrons/shared-data/labware/fixtures/2': path.resolve(
+          './shared-data/labware/fixtures/2/index.ts'
+        ),
+        '@opentrons/shared-data': path.resolve('./shared-data/js/index.ts'),
+        '@opentrons/step-generation': path.resolve(
+          './step-generation/src/index.ts'
+        ),
+        '@opentrons/api-client': path.resolve('./api-client/src/index.ts'),
+        '@opentrons/react-api-client': path.resolve(
+          './react-api-client/src/index.ts'
+        ),
+        '@opentrons/discovery-client': path.resolve(
+          './discovery-client/src/index.ts'
+        ),
+      },
+    },
+  })
+)
