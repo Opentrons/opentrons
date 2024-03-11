4b036613360affd2532e14f15b6441375c07abd0
Shlok Amin <shlokamin@me.com>
2024-03-11 17:15:39 -0400

    fix(protocol-designer): fix custom labware uploads afrer vite migration (#14626)

--- protocol-designer/src/timelineMiddleware/makeTimelineMiddleware.ts
+++ protocol-designer/src/timelineMiddleware/makeTimelineMiddleware.ts
@@ -102,7 +102,6 @@ export const makeTimelineMiddleware: () => Middleware<BaseState, any> = () => {
       if (prevTimelineArgs !== null && prevSubstepsArgs !== null) {
         const timelineArgs: GenerateRobotStateTimelineArgs = prevTimelineArgs
         const substepsArgs: SubstepsArgsNoTimeline = prevSubstepsArgs
-        console.log('about to post worker message')
         worker.postMessage({
           needsTimeline: true,
           timelineArgs,
