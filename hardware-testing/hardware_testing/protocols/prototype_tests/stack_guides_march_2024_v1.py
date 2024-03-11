31dceb737c89e879222b2e1193824266f37651f9
Andy Sigler <andrewsigler1@gmail.com>
2024-03-11 09:45:30 -0400

    lid protocol looks good

--- hardware-testing/hardware_testing/protocols/prototype_tests/stack_guides_march_2024_v1.py
+++ hardware-testing/hardware_testing/protocols/prototype_tests/stack_guides_march_2024_v1.py
@@ -9,15 +9,13 @@ requirements = {"robotType": "Flex", "apiLevel": "2.16"}
 """
 Gripper picks up a tip-rack from a deck-slot (anywhere)
  - then, drop tip-rack into tip-rack guide
- - pause, wait for tester to press continue
+   - PAUSE, wait for tester to press continue
  - then, drop a 2nd tip-rack on top of that previous tip-rack
- - pause, wait for tester to press continue
-Repeat step 2, but with purposeful XYZ offsets,
+   - PAUSE, wait for tester to press continue
+Repeat above, but with purposeful XYZ offsets
  - Each time, make the offset larger
  - Eventually a collision will occur (or the tester will Cancel the protocol)
  - Include the current offset in the PAUSE messages, so we can then record the failing offset
-Protocol is now complete
- - Test can repeated, and Tester can modify the Protocol
 """
 
 START_UNSTACKED = True
--- hardware-testing/hardware_testing/protocols/prototype_tests/tc_lid_march_2024_v1.py
+++ hardware-testing/hardware_testing/protocols/prototype_tests/tc_lid_march_2024_v1.py
@@ -5,29 +5,28 @@ from opentrons.protocol_api import ProtocolContext, Labware
 # NOTE: branch used for previous testing >>> `thermocycler_lid_testing_for_hardware`
 
 metadata = {"protocolName": "tc-lid-march-2024-v1"}
-requirements = {"robotType": "Flex", "apiLevel": "2.16"}
+requirements = {"robotType": "Flex", "apiLevel": "2.16"}  # TODO: confirm which version to use
 
 
 """
-Protocol starts:
+Setup:
  - 1-5x lids are stacked in deck D2
  - Thermocycler installed
-For each lid on the stack:
- - Gripper picks up top-most lid
- - Lid placed in TC
- - pause, wait for tester to press continue
- - Lid removed from TC, placed into deck-slot C2
-   - Stack onto previously placed lids
- - pause, wait for tester to press continue
-Protocol is now complete
- - Repeat same Protocol, but incorporate the same XYZ offset test as described above for the tip-rack guides
- - Include the current offset in the PAUSE messages, so we can then record the failing offset
+
+Run:
+ - For each lid in the stack (1-5x)
+   - Move lid in D2 to Thermocycler
+     - Remove top-most lid
+     - PAUSE, wait for tester to press continue
+   - Move lid from Thermocycler to new slot C2
+     - Stacked onto any previously placed lids
+     - PAUSE, wait for tester to press continue
 """
 
-ADAPTERS_DEFINITION = "tc_lid_march_2024_v1"
-ADAPTERS_STARTING_SLOT = "D2"
-ADAPTERS_ENDING_SLOT = "C2"
-ADAPTERS_COUNT = 5
+LID_DEFINITION = "tc_lid_march_2024_v1"
+LID_STARTING_SLOT = "D2"
+LID_ENDING_SLOT = "C2"
+LID_COUNT = 5
 
 USING_THERMOCYCLER = True
 
@@ -55,22 +54,22 @@ def _move_labware_with_offset_and_pause(
 
 
 def run(protocol: ProtocolContext):
-    # load stacked adapters
-    adapters: List[Labware] = []
-    for i in range(ADAPTERS_COUNT):
-        parent = adapters[-1] if adapters else protocol
-        adapters.append(parent.load_adapter(ADAPTERS_DEFINITION))  # FIXME: cannot load adapter using adapter
-
+    # SETUP
+    lids: List[Labware] = [protocol.load_labware(LID_DEFINITION, LID_STARTING_SLOT)]
+    for i in range(LID_COUNT - 1):
+        lids.append(lids[-1].load_labware(LID_DEFINITION))
+    lids.reverse()  # NOTE: reversing to more easily loop through lids from top-to-bottom
     if USING_THERMOCYCLER:
-        # load thermocycler
+        # TODO: confirm if we need to load 96-well adapter onto Thermocycler
         thermocycler = protocol.load_module("thermocyclerModuleV2")
         thermocycler.open_lid()
         plate_in_cycler = thermocycler.load_labware("armadillo_96_wellplate_200ul_pcr_full_skirt")
+    else:
+        plate_in_cycler = None
 
-    # pickup lids from stack, place in cycler, then finally re-stack on new-slot
-    for i in range(ADAPTERS_COUNT - 1, -1, -1):
-        lid = adapters[i]
-        destination = adapters[i + 1] if i + 1 < len(adapters) else ADAPTERS_ENDING_SLOT
+    # RUN
+    prev_moved_lid: Optional[Labware] = None
+    for lid in lids:
         if USING_THERMOCYCLER:
             _move_labware_with_offset_and_pause(
                 protocol,
@@ -81,8 +80,9 @@ def run(protocol: ProtocolContext):
             )
         _move_labware_with_offset_and_pause(
             protocol,
-            destination,
-            plate_in_cycler,
+            lid,
+            prev_moved_lid if prev_moved_lid else LID_ENDING_SLOT,
             pick_up_offset={"x": 0, "y": 0, "z": 0},  # PICK-UP from THERMOCYCLER
             drop_offset={"x": 0, "y": 0, "z": 0},  # DROP-OFF in DECK-SLOT
         )
+        prev_moved_lid = lid
