8a4baca552f98011e8427047f3f8c13d03150a96
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2024-03-14 11:58:25 -0400

    get rid of tabBorder

--- components/src/helix-design-system/borders.ts
+++ components/src/helix-design-system/borders.ts
@@ -2,6 +2,9 @@ import { css } from 'styled-components'
 
 import { COLORS } from '.'
 
+/**
+ * Constants
+ */
 export const borderRadius2 = '2px' // WEB ONLY!
 export const borderRadius4 = '4px'
 export const borderRadius8 = '8px'
@@ -10,14 +13,14 @@ export const borderRadius16 = '16px'
 export const borderRadius40 = '40px'
 export const borderRadiusFull = '200px'
 
+/**
+ * Styles
+ */
 export const styleSolid = 'solid'
 
-export const tabBorder = css`
-  border-bottom-style: ${styleSolid};
-  border-bottom-width: 2px;
-  border-bottom-color: ${COLORS.purple50};
-`
-
+/**
+ * Special-case borders
+ */
 export const activeLineBorder = `1px ${styleSolid} ${COLORS.blue50}`
 export const lineBorder = `1px ${styleSolid} ${COLORS.grey30}`
 export const cardOutlineBorder = css`
@@ -30,8 +33,10 @@ export const cardOutlineBorder = css`
   }
 `
 
+/**
+ * Shadows
+ */
 export const smallDropShadow = '0px 3px 6px rgba(0, 0, 0, 0.23)'
-
 // touch screen
 export const shadowBig = '0px 3px 6px rgba(0,0,0,0.23)'
 export const shadowSmall = '0px 0px 40px rgba(0,0,0,0.4)'
