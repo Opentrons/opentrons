487ed535fcd4f5207be66141a9cd5529811b200a
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2024-03-14 10:45:36 -0400

    shiny new borders file

---
+++ components/src/helix-design-system/borders.ts
@@ -0,0 +1,7 @@
+export const borderRadius2 = '2px' // WEB ONLY!
+export const borderRadius4 = '4px'
+export const borderRadius8 = '8px'
+export const borderRadius12 = '12px'
+export const borderRadius16 = '16px'
+export const borderRadius40 = '40px'
+export const borderRadiusFull = '200px'
