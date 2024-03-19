88b674f167db4e94b63851e0a993bd0a7a291363
jbleon95 <jeremybenleon@gmail.com>
2024-03-19 13:39:09 -0400

    refactor parameter dynamic attribute setting to not set class level

--- api/src/opentrons/protocol_api/_parameters.py
+++ api/src/opentrons/protocol_api/_parameters.py
@@ -5,21 +5,21 @@ from opentrons.protocols.parameters.types import AllowedTypes, ParameterNameErro
 
 class Parameters:
     def __init__(self, parameters: Optional[Dict[str, AllowedTypes]] = None) -> None:
+        super().__setattr__("_values", {})
         self._values: Dict[str, AllowedTypes] = {}
         if parameters is not None:
             for name, value in parameters.items():
                 self._initialize_parameter(name, value)
 
-    def _getparam(self, variable_name: str) -> Any:
-        return self._values[variable_name]
+    def __setattr__(self, key: str, value: Any) -> None:
+        if key in self._values:
+            raise AttributeError(f"Cannot overwrite protocol defined parameter {key}")
+        super().__setattr__(key, value)
 
     def _initialize_parameter(self, variable_name: str, value: AllowedTypes) -> None:
         if not hasattr(self, variable_name):
+            setattr(self, variable_name, value)
             self._values[variable_name] = value
-            prop = property(
-                fget=lambda s, v=variable_name: Parameters._getparam(s, v)  # type: ignore[misc]
-            )
-            setattr(Parameters, variable_name, prop)
         else:
             raise ParameterNameError(
                 f"Cannot use {variable_name} as a variable name, either duplicates another"
