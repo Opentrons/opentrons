72498bb93c001f37d4ac71be49d06c3ee3abead1
koji <kk2796@nyu.edu>
2024-03-19 17:49:12 -0400

    fix export name from index.ts

--- app/src/atoms/SoftwareKeyboard/index.ts
+++ app/src/atoms/SoftwareKeyboard/index.ts
@@ -1,3 +1,3 @@
 export { CustomKeyboard } from './CustomKeyboard'
 export { NormalKeyboard } from './NormalKeyboard'
-export { Numpad } from './NumericalKeyboard'
+export { NumericalKeyboard } from './NumericalKeyboard'
