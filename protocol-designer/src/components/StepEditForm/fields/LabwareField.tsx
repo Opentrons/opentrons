e6f226022fec3defc21603e859728f2e34bc5176
Jethary <jethary@opentrons.com>
2024-03-19 16:26:57 -0400

    remove lines

--- protocol-designer/src/components/StepEditForm/fields/LabwareField.tsx
+++ protocol-designer/src/components/StepEditForm/fields/LabwareField.tsx
@@ -14,5 +14,6 @@ export const LabwareField = (props: FieldProps): JSX.Element => {
     props.name === 'dispense_labware'
       ? [...options, ...disposalOptions]
       : [...options]
+
   return <StepFormDropdown {...props} options={allOptions} />
 }
