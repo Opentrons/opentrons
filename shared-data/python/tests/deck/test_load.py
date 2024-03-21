8e19d9d8dee1af099a421503a8c84b43f1ff5b8d
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2024-03-21 16:17:42 -0400

    use typeguard

--- shared-data/python/tests/deck/test_load.py
+++ shared-data/python/tests/deck/test_load.py
@@ -1,13 +1,28 @@
 import pytest
+import typeguard
 from opentrons_shared_data.deck.load import load
-from opentrons_shared_data.deck.constants import AVAILBLE_VERSIONS, DeckVersionType
+from opentrons_shared_data.deck.constants import DeckVersionType
 from opentrons_shared_data.deck.types import DeckType
-from opentrons_shared_data.deck.models import DeckDefinitionABC
+from opentrons_shared_data.deck.models import (
+    DeckDefinitionABC,
+    DeckDefinitionV3,
+    DeckDefinitionV4,
+    DeckDefinitionV5,
+)
 
 
-@pytest.mark.parametrize("deck_type", DeckType)
-@pytest.mark.parametrize("version", AVAILBLE_VERSIONS)
-def test_load_definition(deck_type: DeckType, version: DeckVersionType) -> None:
-    deck_def = load(deck_type, version)
-    assert isinstance(deck_def, DeckDefinitionABC)
+@pytest.mark.parametrize("deck", DeckType)
+@pytest.mark.parametrize(
+    "version,def_type",
+    [
+        (3, DeckDefinitionV3),
+        (4, DeckDefinitionV4),
+        (5, DeckDefinitionV5),
+    ],
+)
+def test_load_definition(
+    deck: DeckType, version: DeckVersionType, def_type: DeckDefinitionABC
+) -> None:
+    deck_def = load(deck, version)
+    typeguard.check_type(deck_def, def_type)
     assert deck_def.schemaVersion == version
