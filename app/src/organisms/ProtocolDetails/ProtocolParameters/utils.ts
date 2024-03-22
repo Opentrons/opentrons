c3e2edd68e01951c1c16ec8672ff473b084e2776
koji <kk2796@nyu.edu>
2024-03-22 19:52:38 -0400

    refactor(app): create utils to format value for rtp
    create formatRunTimeParameterValue to format rtp value and replacelocal function with that
    
    close AUTH-236

---
+++ app/src/organisms/ProtocolDetails/ProtocolParameters/utils.ts
@@ -0,0 +1,31 @@
+import type { RunTimeParameter } from '@opentrons/shared-data'
+
+export const formatRunTimeParameterValue = (
+  runTimeParameter: RunTimeParameter
+): string => {
+  const { type, default: defaultValue } = runTimeParameter
+  const suffix =
+    'suffix' in runTimeParameter && runTimeParameter.suffix != null
+      ? runTimeParameter.suffix
+      : null
+  switch (type) {
+    case 'int':
+    case 'float':
+      return suffix !== null
+        ? `${defaultValue.toString()} ${suffix}`
+        : defaultValue.toString()
+    case 'boolean':
+      return Boolean(defaultValue) ? 'On' : 'Off'
+    case 'str':
+      if ('choices' in runTimeParameter && runTimeParameter.choices != null) {
+        const choice = runTimeParameter.choices.find(
+          choice => choice.value === defaultValue
+        )
+        if (choice != null) {
+          return choice.displayName
+        }
+      }
+      break
+  }
+  return ''
+}
