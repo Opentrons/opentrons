34ed9fcbc47ea7a3ddc01097c25fc043bfdb48f8
ncdiehl11 <ncdiehl11@gmail.com>
2024-03-28 15:07:38 -0400

    get RTPs from analysis at ProtocolRunRuntimeParameters

--- app/src/organisms/Devices/ProtocolRun/ProtocolRunRunTimeParameters.tsx
+++ app/src/organisms/Devices/ProtocolRun/ProtocolRunRunTimeParameters.tsx
@@ -26,149 +26,6 @@ import { formatRunTimeParameterValue } from '@opentrons/shared-data'
 import type { RunTimeParameter } from '@opentrons/shared-data'
 import { Tooltip } from '../../../atoms/Tooltip'
 
-const mockData: RunTimeParameter[] = [
-  {
-    value: true,
-    displayName: 'Dry Run',
-    variableName: 'DRYRUN',
-    description: 'Is this a dry or wet run? Wet is true, dry is false',
-    type: 'boolean',
-    default: false,
-  },
-  {
-    value: true,
-    displayName: 'Use Gripper',
-    variableName: 'USE_GRIPPER',
-    description: 'For using the gripper.',
-    type: 'boolean',
-    default: true,
-  },
-  {
-    value: true,
-    displayName: 'Trash Tips',
-    variableName: 'TIP_TRASH',
-    description:
-      'to throw tip into the trash or to not throw tip into the trash',
-    type: 'boolean',
-    default: true,
-  },
-  {
-    value: true,
-    displayName: 'Deactivate Temperatures',
-    variableName: 'DEACTIVATE_TEMP',
-    description: 'deactivate temperature on the module',
-    type: 'boolean',
-    default: true,
-  },
-  {
-    value: 4,
-    displayName: 'Columns of Samples',
-    variableName: 'COLUMNS',
-    description: 'How many columns do you want?',
-    type: 'int',
-    min: 1,
-    max: 14,
-    default: 4,
-  },
-  {
-    value: 6,
-    displayName: 'PCR Cycles',
-    variableName: 'PCR_CYCLES',
-    description: 'number of PCR cycles on a thermocycler',
-    type: 'int',
-    min: 1,
-    max: 10,
-    default: 6,
-  },
-  {
-    value: 6.5,
-    displayName: 'EtoH Volume',
-    variableName: 'ETOH_VOLUME',
-    description: '70% ethanol volume',
-    type: 'float',
-    suffix: 'mL',
-    min: 1.5,
-    max: 10.0,
-    default: 6.5,
-  },
-  {
-    value: 'none',
-    displayName: 'Default Module Offsets',
-    variableName: 'DEFAULT_OFFSETS',
-    description: 'default module offsets for temp, H-S, and none',
-    type: 'str',
-    choices: [
-      {
-        displayName: 'No offsets',
-        value: 'none',
-      },
-      {
-        displayName: 'temp offset',
-        value: '1',
-      },
-      {
-        displayName: 'heater-shaker offset',
-        value: '2',
-      },
-    ],
-    default: 'none',
-  },
-  {
-    value: 'left',
-    displayName: 'pipette mount',
-    variableName: 'mont',
-    description: 'pipette mount',
-    type: 'str',
-    choices: [
-      {
-        displayName: 'Left',
-        value: 'left',
-      },
-      {
-        displayName: 'Right',
-        value: 'right',
-      },
-    ],
-    default: 'left',
-  },
-  {
-    value: 'flex',
-    displayName: 'short test case',
-    variableName: 'short 2 options',
-    description: 'this play 2 short options',
-    type: 'str',
-    choices: [
-      {
-        displayName: 'OT-2',
-        value: 'ot2',
-      },
-      {
-        displayName: 'Flex',
-        value: 'flex',
-      },
-    ],
-    default: 'flex',
-  },
-  {
-    value: 'flex',
-    displayName: 'long test case',
-    variableName: 'long 2 options',
-    description: 'this play 2 long options',
-    type: 'str',
-    choices: [
-      {
-        displayName: 'I am kind of long text version',
-        value: 'ot2',
-      },
-      {
-        displayName: 'I am kind of long text version. Today is 3/15',
-        value: 'flex',
-      },
-    ],
-    default: 'flex',
-  },
-]
-
 interface ProtocolRunRuntimeParametersProps {
   runId: string
 }
@@ -177,18 +34,13 @@ export function ProtocolRunRuntimeParameters({
 }: ProtocolRunRuntimeParametersProps): JSX.Element {
   const { t } = useTranslation('protocol_setup')
   const mostRecentAnalysis = useMostRecentCompletedAnalysis(runId)
-  // todo (nd:03/27/2024) resolve runTimeParameters to analysis's runTimeParameters object once available
   const runTimeParameters = mostRecentAnalysis?.runTimeParameters ?? []
-  // const runTimeParameters = mockData
   const hasParameter = runTimeParameters.length > 0
 
-  // ToDo (kk:03/19/2024) this will be replaced with the boolean from values check result
   const hasCustomValues = runTimeParameters.some(
     parameter => parameter.value !== parameter.default
   )
 
-  // ToDO (kk:03/18/2024) Need to add Chip to updated runTime parameter value
-  // This part will be implemented in a following PR since need to runTime parameter slideout
   return (
     <>
       <Flex
@@ -242,6 +94,7 @@ export function ProtocolRunRuntimeParameters({
                 {runTimeParameters.map(
                   (parameter: RunTimeParameter, index: number) => (
                     <StyledTableRowComponent
+                      key={`${index}_${parameter.variableName}`}
                       parameter={parameter}
                       index={index}
                       runTimeParametersLength={runTimeParameters.length}
