62f6db9e6e333024e473bc20e349e9d5f07d95a6
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2024-04-01 15:29:40 -0400

    feat(app): range error handling for number RTPs (#14765)
    closes [AUTH-104](https://opentrons.atlassian.net/browse/AUTH-104)

--- app/src/organisms/ChooseRobotSlideout/index.tsx
+++ app/src/organisms/ChooseRobotSlideout/index.tsx
@@ -379,8 +379,30 @@ export function ChooseRobotSlideout(
             value={value}
             title={runtimeParam.displayName}
             tooltipText={runtimeParam.description}
-            caption={`${runtimeParam.min}-${runtimeParam.max}`}
+            caption={
+              runtimeParam.type === 'int'
+                ? `${runtimeParam.min}-${runtimeParam.max}`
+                : `${runtimeParam.min.toFixed(1)}-${runtimeParam.max.toFixed(
+                    1
+                  )}`
+            }
             id={id}
+            error={
+              Number.isNaN(value) ||
+              value < runtimeParam.min ||
+              value > runtimeParam.max
+                ? t(`value_out_of_range`, {
+                    min:
+                      runtimeParam.type === 'int'
+                        ? runtimeParam.min
+                        : runtimeParam.min.toFixed(1),
+                    max:
+                      runtimeParam.type === 'int'
+                        ? runtimeParam.max
+                        : runtimeParam.max.toFixed(1),
+                  })
+                : null
+            }
             onChange={e => {
               const clone = runTimeParametersOverrides.map((parameter, i) => {
                 if (i === index) {
