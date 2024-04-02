447a037b1bfd6947d13de3b1616c85c398774a13
Edward Cormany <edward.cormany@opentrons.com>
2024-04-02 18:27:44 -0400

    fix sphinx warning

--- api/docs/v2/conf.py
+++ api/docs/v2/conf.py
@@ -444,5 +444,6 @@ nitpick_ignore_regex = [
     ("py:class", r".*protocol_api\.deck.*"),
     ("py:class", r".*protocol_api\.config.*"),
     ("py:class", r".*opentrons_shared_data.*"),
+    ("py:class", r".*protocol_api._parameters.Parameters.*"),
     ("py:class", r'.*AbstractLabware|APIVersion|LabwareLike|LoadedCoreMap|ModuleTypes|NoneType|OffDeckType|ProtocolCore|WellCore'),  # laundry list of not fully qualified things
 ]
