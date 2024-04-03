838572ba7071681f78f686202fdc537dc1e372ff
Max Marrone <max@opentrons.com>
2024-04-03 12:05:40 -0400

    Add unit test for get_recovery_in_progress_for_command().

--- api/tests/opentrons/protocol_engine/state/test_command_view.py
+++ api/tests/opentrons/protocol_engine/state/test_command_view.py
@@ -243,6 +243,40 @@ def test_get_all_commands_final() -> None:
     assert subject.get_all_commands_final() is False
 
 
+def test_get_recovery_in_progress_for_command() -> None:
+    """It should return whether error recovery is in progress for the given command."""
+    # command-id is not a failed command:
+    subject = get_command_view()
+    assert not subject.get_recovery_in_progress_for_command("command-id")
+
+    # command-id is a failed command and we ARE recovering from it:
+    subject = get_command_view(
+        failed_command=CommandEntry(
+            create_failed_command(command_id="command-id"), index=0
+        ),
+        queue_status=QueueStatus.AWAITING_RECOVERY,
+    )
+    assert subject.get_recovery_in_progress_for_command("command-id")
+
+    # command-id is a failed command but we're not recovering from any command:
+    subject = get_command_view(
+        failed_command=CommandEntry(
+            create_failed_command(command_id="command-id"), index=0
+        ),
+        queue_status=QueueStatus.PAUSED,
+    )
+    assert not subject.get_recovery_in_progress_for_command("command-id")
+
+    # command-id is a failed command, but we're recovering from a different one:
+    subject = get_command_view(
+        failed_command=CommandEntry(
+            create_failed_command(command_id="different-command-id"), index=0
+        ),
+        queue_status=QueueStatus.AWAITING_RECOVERY,
+    )
+    assert not subject.get_recovery_in_progress_for_command("command-id")
+
+
 def test_raise_fatal_command_error() -> None:
     """It should raise the fatal command error."""
     completed_command = create_succeeded_command(command_id="command-id-1")
