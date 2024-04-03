ff844d3875e78c67e64a1a2682f83cceac86eb34
ncdiehl11 <ncdiehl11@gmail.com>
2024-04-03 12:41:05 -0400

    chore(app, api-client): remove mock RTP datafrom ProtocolDetails
    Remove mock RTP data from ProtocolDetails. Land on parameters tab for RTP-containing protocols.

--- app/src/organisms/ChooseRobotToRunProtocolSlideout/index.tsx
+++ app/src/organisms/ChooseRobotToRunProtocolSlideout/index.tsx
@@ -62,62 +62,8 @@ export function ChooseRobotToRunProtocolSlideoutComponent(
   )
 
   // TODO: (nd: 3/20/24) remove stubs and pull parameters from analysis
-  // const runTimeParameters =
-  //   storedProtocolData.mostRecentAnalysis?.runTimeParameters ?? []
-  const mockRunTimeParameters: RunTimeParameter[] = [
-    {
-      displayName: 'Dry Run',
-      value: false,
-      variableName: 'DRYRUN',
-      description: 'Is this a dry or wet run? Wet is true, dry is false',
-      type: 'bool',
-      default: false,
-    },
-    {
-      value: 4,
-      displayName: 'Columns of Samples',
-      variableName: 'COLUMNS',
-      description: 'How many columns do you want?',
-      type: 'int',
-      min: 1,
-      max: 14,
-      default: 4,
-    },
-    {
-      value: 6.5,
-      displayName: 'EtoH Volume',
-      variableName: 'ETOH_VOLUME',
-      description: '70% ethanol volume',
-      type: 'float',
-      suffix: 'mL',
-      min: 1.5,
-      max: 10.0,
-      default: 6.5,
-    },
-    {
-      value: 'none',
-      displayName: 'Default Module Offsets',
-      variableName: 'DEFAULT_OFFSETS',
-      description: 'default module offsets for temp, H-S, and none',
-      type: 'str',
-      choices: [
-        {
-          displayName: 'No offsets',
-          value: 'none',
-        },
-        {
-          displayName: 'temp offset',
-          value: '1',
-        },
-        {
-          displayName: 'heater-shaker offset',
-          value: '2',
-        },
-      ],
-      default: 'none',
-    },
-  ]
-  const runTimeParameters: RunTimeParameter[] = mockRunTimeParameters
+  const runTimeParameters =
+    storedProtocolData.mostRecentAnalysis?.runTimeParameters ?? []
   const [
     runTimeParametersOverrides,
     setRunTimeParametersOverrides,
