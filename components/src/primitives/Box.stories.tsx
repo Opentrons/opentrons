afbeaba4e74a319d4371d9bef4ccca4d7b15d441
koji <kk2796@nyu.edu>
2024-04-07 01:46:21 -0400

    refactor(components): update Box stories
    update Box stories
    
    close AUTH-294

--- components/src/primitives/Box.stories.tsx
+++ components/src/primitives/Box.stories.tsx
@@ -1,21 +1,23 @@
-import * as React from 'react'
 import { Box as BoxComponent } from './Box'
 
-import type { Story, Meta } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
-export default {
+const meta: Meta<typeof BoxComponent> = {
   title: 'Library/Atoms/Box',
-} as Meta
+  component: BoxComponent,
+}
 
-const Template: Story<React.ComponentProps<typeof BoxComponent>> = args => (
-  <BoxComponent {...args} />
-)
-export const Box = Template.bind({})
-Box.args = {
-  children:
-    'This is a simple box atom that accepts all primitive styling props.',
-  backgroundColor: 'grey',
-  border: '1px solid black',
-  padding: '1rem',
-  maxWidth: '20rem',
+export default meta
+
+type Story = StoryObj<typeof BoxComponent>
+
+export const Box: Story = {
+  args: {
+    children:
+      'This is a simple box atom that accepts all primitive styling props.',
+    backgroundColor: 'grey',
+    border: '1px solid black',
+    padding: '1rem',
+    maxWidth: '20rem',
+  },
 }
