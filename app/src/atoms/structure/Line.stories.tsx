e8c17d11213e40a6b89320574ea2ade05cf1d1cf
koji <kk2796@nyu.edu>
2024-04-07 01:08:09 -0400

    refactor(app): update Line stories
    update Line stories
    
    close AUTH-293

--- app/src/atoms/structure/Line.stories.tsx
+++ app/src/atoms/structure/Line.stories.tsx
@@ -9,49 +9,57 @@ import {
   TYPOGRAPHY,
 } from '@opentrons/components'
 
-import { Line } from './index'
-import type { Story, Meta } from '@storybook/react'
+import { Line as LineComponent } from './index'
+import type { Meta, StoryObj } from '@storybook/react'
 
-export default {
+const meta: Meta<typeof LineComponent> = {
   title: 'App/Atoms/Line',
-  component: Line,
-} as Meta
-
-const Template: Story<React.ComponentProps<typeof Line>> = args => (
-  <>
-    <Box paddingBottom={SPACING.spacing24}>
-      <Flex alignItems={ALIGN_CENTER} justifyContent={JUSTIFY_SPACE_BETWEEN}>
-        <Box marginRight={SPACING.spacing32}>
-          <Box marginBottom={SPACING.spacing8}>
-            <StyledText as="h3" fontWeight={TYPOGRAPHY.fontWeightSemiBold}>
-              {'About Calibration'}
-            </StyledText>
-          </Box>
-          <StyledText as="p" marginBottom={SPACING.spacing8}>
-            {'This section is about calibration.'}
-          </StyledText>
+  component: LineComponent,
+  decorators: [
+    Story => (
+      <>
+        <Box paddingBottom={SPACING.spacing24}>
+          <Flex
+            alignItems={ALIGN_CENTER}
+            justifyContent={JUSTIFY_SPACE_BETWEEN}
+          >
+            <Box marginRight={SPACING.spacing32}>
+              <Box marginBottom={SPACING.spacing8}>
+                <StyledText as="h3" fontWeight={TYPOGRAPHY.fontWeightSemiBold}>
+                  {'About Calibration'}
+                </StyledText>
+              </Box>
+              <StyledText as="p" marginBottom={SPACING.spacing8}>
+                {'This section is about calibration.'}
+              </StyledText>
+            </Box>
+          </Flex>
         </Box>
-      </Flex>
-    </Box>
-    <Line {...args} />
-    <Box paddingTop={SPACING.spacing24} paddingBottom={SPACING.spacing24}>
-      <Flex alignItems={ALIGN_CENTER} justifyContent={JUSTIFY_SPACE_BETWEEN}>
-        <Box marginRight={SPACING.spacing32}>
-          <Box marginBottom={SPACING.spacing8}>
-            <StyledText as="h3" fontWeight={TYPOGRAPHY.fontWeightSemiBold}>
-              {'Deck Calibration'}
-            </StyledText>
-          </Box>
-          <StyledText as="p" marginBottom={SPACING.spacing8}>
-            {'This section is for deck calibration.'}
-          </StyledText>
+        <Story />
+        <Box paddingTop={SPACING.spacing24} paddingBottom={SPACING.spacing24}>
+          <Flex
+            alignItems={ALIGN_CENTER}
+            justifyContent={JUSTIFY_SPACE_BETWEEN}
+          >
+            <Box marginRight={SPACING.spacing32}>
+              <Box marginBottom={SPACING.spacing8}>
+                <StyledText as="h3" fontWeight={TYPOGRAPHY.fontWeightSemiBold}>
+                  {'Deck Calibration'}
+                </StyledText>
+              </Box>
+              <StyledText as="p" marginBottom={SPACING.spacing8}>
+                {'This section is for deck calibration.'}
+              </StyledText>
+            </Box>
+          </Flex>
         </Box>
-      </Flex>
-    </Box>
-  </>
-)
-
-export const Primary = Template.bind({})
-Primary.args = {
-  marginY: SPACING.spacing8,
+      </>
+    ),
+  ],
 }
+
+export default meta
+
+type Story = StoryObj<typeof LineComponent>
+
+export const Line: Story = {}
