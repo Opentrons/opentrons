9cb56dacf712b04c569855aeaa2d4b7140904f71
koji <kk2796@nyu.edu>
2024-04-08 17:55:47 -0400

    add test utils

--- opentrons-ai-client/src/App.test.tsx
+++ opentrons-ai-client/src/App.test.tsx
@@ -1,29 +1,17 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { screen } from '@testing-library/react'
+import { describe, it } from 'vitest'
 
-import App from "./App";
+import { renderWithProviders } from './__testing-utils__'
 
-describe("App", () => {
-  it("should render text and image", () => {
-    render(<App />);
-    const headerText = screen.getByText("Vite + React");
-    expect(headerText).toBeInTheDocument();
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-    const secondText = screen.getByText(
-      "Click on the Vite and React logos to learn more",
-    );
-    expect(secondText).toBeInTheDocument();
-  });
+import App from './App'
 
-  it("when clicking the button, the app will count up", () => {
-    render(<App />);
-    const beforeCount = screen.getByText("count is 0");
-    expect(beforeCount).toBeInTheDocument();
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-    fireEvent.click(button);
-    const afterCount = screen.getByText("count is 1");
-    expect(afterCount).toBeInTheDocument();
-  });
-});
+const render = () => {
+  return renderWithProviders(<App />)
+}
+
+describe('App', () => {
+  it('should render text', () => {
+    render()
+    screen.getByText('Opentrons AI')
+  })
+})
