880a67c99c5635f2d40e93d2b770a0a618428332
koji <koji.kanao@opentrons.com>
2024-04-08 09:50:16 -0400

    Update app/src/atoms/SoftwareKeyboard/AlphanumericKeyboard/index.tsx
    Co-authored-by: Jamey Huffnagle <jamey.huffnagle@opentrons.com>

--- app/src/atoms/SoftwareKeyboard/AlphanumericKeyboard/index.tsx
+++ app/src/atoms/SoftwareKeyboard/AlphanumericKeyboard/index.tsx
@@ -5,7 +5,7 @@ import { alphanumericKeyboardLayout, customDisplay } from '../constants'
 import '../index.css'
 import './index.css'
 
-// Note (kk:04/05/2024) add debug to make debugging easy
+// TODO (kk:04/05/2024) add debug to make debugging easy
 interface AlphanumericKeyboardProps {
   onChange: (input: string) => void
   keyboardRef: React.MutableRefObject<any>
