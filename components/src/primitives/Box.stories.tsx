e7778552a99ae3dc694dd9d0f9db1b6cbfcc08df
koji <kk2796@nyu.edu>
2024-04-08 12:13:51 -0400

    address feedback

--- components/src/primitives/Box.stories.tsx
+++ components/src/primitives/Box.stories.tsx
@@ -1,3 +1,5 @@
+import { COLORS, BORDERS } from '../helix-design-system'
+import { SPACING } from '../ui-style-constants'
 import { Box as BoxComponent } from './Box'
 
 import type { Meta, StoryObj } from '@storybook/react'
@@ -15,9 +17,9 @@ export const Box: Story = {
   args: {
     children:
       'This is a simple box atom that accepts all primitive styling props.',
-    backgroundColor: 'grey',
-    border: '1px solid black',
-    padding: '1rem',
+    backgroundColor: COLORS.grey60,
+    border: `1px ${BORDERS.styleSolid} black`,
+    padding: SPACING.spacing16,
     maxWidth: '20rem',
   },
 }
