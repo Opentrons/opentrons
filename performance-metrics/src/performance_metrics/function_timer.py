a31345b260df6e4605d60ad4fa99e0a6fe24fdbc
Derek Maggio <derek.j.maggio@gmail.com>
2024-04-09 07:31:25 -0700

    Forgot to add begin time

--- performance-metrics/src/performance_metrics/function_timer.py
+++ performance-metrics/src/performance_metrics/function_timer.py
@@ -3,8 +3,8 @@
 The FunctionTimer class is intended to be used as a decorator to wrap functions and measure their execution times.
 """
 
-from time import perf_counter_ns
-from typing import Protocol, Callable, TypeVar
+from time import perf_counter_ns, clock_gettime_ns, CLOCK_REALTIME
+from typing import Iterator, Protocol, Callable, TypeVar, List, Tuple
 from typing_extensions import ParamSpec
 import inspect
 
@@ -12,29 +12,80 @@ P = ParamSpec("P")
 R = TypeVar("R")
 
 
-class StoreDuration(Protocol):
-    """Protocol for a callback function that stores the duration between two timestamps."""
+class CanStoreTimingResult(Protocol):
+    """A protocol for a function that can store the result of a timing operation."""
 
-    def __call__(self, start_time: int, end_time: int) -> None:
+    def store(
+        self,
+        function_start_time: int,
+        duration_measurement_start_time: int,
+        duration_measurement_end_time: int,
+    ) -> None:
         """Stores the duration of an operation.
 
         Args:
-            start_time (int): The start timestamp in nanoseconds.
-            end_time (int): The end timestamp in nanoseconds.
+            function_start_time: The time at which the function started executing.
+            duration_measurement_start_time: The time at which the duration measurement started.
+            duration_measurement_end_time: The time at which the duration measurement ended.
         """
         pass
+
+
+class TimingResultStore(CanStoreTimingResult):
+    """A class that stores the result of a timing operation."""
+
+    def __init__(self) -> None:
+        """Initializes the TimingResultStore with a storage method.
+
+        Args:
+            storage_method: A method that stores the result of a timing operation.
+        """
+        self._storage: List[Tuple[int, int, int]] = []
+
+    def __len__(self) -> int:
+        """Returns the number of stored timing results."""
+        return len(self._storage)
+
+    def __getitem__(self, index: int) -> Tuple[int, int, int]:
+        """Returns the timing result at the specified index."""
+        return self._storage[index]
+
+    def __iter__(self) -> Iterator[Tuple[int, int, int]]:
+        """Returns an iterator over the stored timing results."""
+        return iter(self._storage)
+
+    def store(
+        self,
+        function_start_time: int,
+        duration_measurement_start_time: int,
+        duration_measurement_end_time: int,
+    ) -> None:
+        """Stores the duration of an operation.
+
+        Args:
+            function_start_time: The time at which the function started executing.
+            duration_measurement_start_time: The time at which the duration measurement started.
+            duration_measurement_end_time: The time at which the duration measurement ended.
+        """
+        self._storage.append(
+            (
+                function_start_time,
+                duration_measurement_start_time,
+                duration_measurement_end_time,
+            )
+        )
 
 
 class FunctionTimer:
     """A class designed to measure and store the execution duration of functions, both synchronous and asynchronous."""
 
-    def __init__(self, store_duration: StoreDuration) -> None:
+    def __init__(self, can_store: CanStoreTimingResult) -> None:
         """Initializes the FunctionTimer with a specified storage mechanism for the execution duration.
 
         Args:
-            store_duration: A callback function that stores the execution duration.
+            can_store: A callback function that stores the execution duration.
         """
-        self._store_duration = store_duration
+        self._can_store = can_store
 
     def measure_duration(self, func: Callable[P, R]) -> Callable[P, R]:
         """Creates a wrapper around a given function to measure its execution duration.
@@ -52,13 +103,19 @@ class FunctionTimer:
 
             async def async_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
                 """An asynchronous wrapper function for measuring execution duration."""
-                start_time = perf_counter_ns()
+                function_start_time = clock_gettime_ns(CLOCK_REALTIME)
+                duration_measurement_start_time = perf_counter_ns()
                 try:
                     result: R = await func(*args, **kwargs)
                 except Exception as e:
                     raise e
                 finally:
-                    self._store_duration(start_time, perf_counter_ns())
+                    duration_measurement_end_time = perf_counter_ns()
+                    self._can_store.store(
+                        function_start_time,
+                        duration_measurement_start_time,
+                        duration_measurement_end_time,
+                    )
                 return result
 
             return async_wrapper  # type: ignore
@@ -66,13 +123,19 @@ class FunctionTimer:
 
             def sync_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
                 """A synchronous wrapper function for measuring execution duration."""
-                start_time = perf_counter_ns()
+                function_start_time = clock_gettime_ns(CLOCK_REALTIME)
+                duration_measurement_start_time = perf_counter_ns()
                 try:
                     result: R = func(*args, **kwargs)
                 except Exception as e:
                     raise e
                 finally:
-                    self._store_duration(start_time, perf_counter_ns())
+                    duration_measurement_end_time = perf_counter_ns()
+                    self._can_store.store(
+                        function_start_time,
+                        duration_measurement_start_time,
+                        duration_measurement_end_time,
+                    )
                 return result
 
             return sync_wrapper
--- performance-metrics/tests/performance_metrics/test_function_timer.py
+++ performance-metrics/tests/performance_metrics/test_function_timer.py
@@ -1,35 +1,30 @@
 """This module contains tests for the FunctionTimer class, focusing on its ability to accurately measure and record the execution times of synchronous and asynchronous functions, including when exceptions are raised."""
 
 import asyncio
-from typing import List
 import pytest
 from time import sleep
-from performance_metrics.function_timer import FunctionTimer, StoreDuration
+from performance_metrics.function_timer import (
+    FunctionTimer,
+    CanStoreTimingResult,
+    TimingResultStore,
+)
 
 
 @pytest.fixture
-def durations() -> List[int]:
+def timing_result_store() -> TimingResultStore:
     """Fixture that provides an empty list to store durations. This list is reset before each test."""
-    return []
+    return TimingResultStore()
 
 
 @pytest.fixture
-def store_duration_mock(durations: List[int]) -> StoreDuration:
-    """Provides a mock function for storing duration measurements. It appends durations to the provided list."""
-
-    def _mock(start_time: int, end_time: int) -> None:
-        durations.append(end_time - start_time)
-
-    return _mock
-
-
-@pytest.fixture
-def function_timer(store_duration_mock: StoreDuration) -> FunctionTimer:
+def function_timer(timing_result_store: CanStoreTimingResult) -> FunctionTimer:
     """Creates a FunctionTimer instance with a mock storage function for testing."""
-    return FunctionTimer(store_duration=store_duration_mock)
+    return FunctionTimer(can_store=timing_result_store)
 
 
-def test_sync_function(function_timer: FunctionTimer, durations: List[int]) -> None:
+def test_sync_function(
+    function_timer: FunctionTimer, timing_result_store: TimingResultStore
+) -> None:
     """Tests accurate measurement of a synchronous function's execution time."""
 
     @function_timer.measure_duration
@@ -37,13 +32,13 @@ def test_sync_function(function_timer: FunctionTimer, durations: List[int]) -> N
         sleep(0.01)
 
     sync_test()
-    assert len(durations) == 1
-    assert durations[0] > 0
+    assert len(timing_result_store) == 1
+    assert timing_result_store[0][1] < timing_result_store[0][2]
 
 
 @pytest.mark.asyncio
 async def test_async_function(
-    function_timer: FunctionTimer, durations: List[int]
+    function_timer: FunctionTimer, timing_result_store: TimingResultStore
 ) -> None:
     """Tests accurate measurement of an asynchronous function's execution time."""
 
@@ -52,12 +47,12 @@ async def test_async_function(
         await asyncio.sleep(0.01)
 
     await async_test()
-    assert len(durations) == 1
-    assert durations[0] > 0
+    assert len(timing_result_store) == 1
+    assert timing_result_store[0][1] < timing_result_store[0][2]
 
 
 def test_sync_function_exception(
-    function_timer: FunctionTimer, durations: List[int]
+    function_timer: FunctionTimer, timing_result_store: TimingResultStore
 ) -> None:
     """Tests duration measurement of a synchronous function that raises an exception."""
 
@@ -68,13 +63,13 @@ def test_sync_function_exception(
 
     with pytest.raises(ValueError):
         sync_test_exception()
-    assert len(durations) == 1
-    assert durations[0] > 0
+    assert len(timing_result_store) == 1
+    assert timing_result_store[0][1] < timing_result_store[0][2]
 
 
 @pytest.mark.asyncio
 async def test_async_function_exception(
-    function_timer: FunctionTimer, durations: List[int]
+    function_timer: FunctionTimer, timing_result_store: TimingResultStore
 ) -> None:
     """Tests duration measurement of an asynchronous function that raises an exception."""
 
@@ -85,5 +80,38 @@ async def test_async_function_exception(
 
     with pytest.raises(ValueError):
         await async_test_exception()
-    assert len(durations) == 1
-    assert durations[0] > 0
+    assert len(timing_result_store) == 1
+    assert timing_result_store[0][1] < timing_result_store[0][2]
+
+
+def test_sync_function_multiple_calls(
+    function_timer: FunctionTimer, timing_result_store: TimingResultStore
+) -> None:
+    """Tests duration measurement of multiple calls to a synchronous function."""
+
+    @function_timer.measure_duration
+    def sync_test_multiple_calls() -> None:
+        sleep(0.01)
+
+    sync_test_multiple_calls()
+    sync_test_multiple_calls()
+    assert len(timing_result_store) == 2
+    assert timing_result_store[0][0] < timing_result_store[1][0]
+    assert all(start < end for _, start, end in timing_result_store)
+
+
+@pytest.mark.asyncio
+async def test_async_function_multiple_calls(
+    function_timer: FunctionTimer, timing_result_store: TimingResultStore
+) -> None:
+    """Tests duration measurement of multiple calls to an asynchronous function."""
+
+    @function_timer.measure_duration
+    async def async_test_multiple_calls() -> None:
+        await asyncio.sleep(0.01)
+
+    await async_test_multiple_calls()
+    await async_test_multiple_calls()
+    assert len(timing_result_store) == 2
+    assert timing_result_store[0][0] < timing_result_store[1][0]
+    assert all(start < end for _, start, end in timing_result_store)
