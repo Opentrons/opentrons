77f5eb139d954d603d67185c02f56da88c33d2cb
Ryan howard <ryan.howard@opentrons.com>
2024-04-09 17:14:56 -0400

    fix old hw-testing scripts

--- hardware-testing/hardware_testing/gravimetric/config.py
+++ hardware-testing/hardware_testing/gravimetric/config.py
@@ -5,7 +5,7 @@ from typing_extensions import Final
 from enum import Enum
 from opentrons.config.types import LiquidProbeSettings, OutputOptions
 from opentrons.protocol_api.labware import Well
-
+from opentrons.hardware_control.types import InstrumentProbeType
 
 class ConfigType(Enum):
     """Substitute for Literal which isn't available until 3.8.0."""
@@ -197,7 +197,7 @@ def _get_liquid_probe_settings(
         aspirate_while_sensing=False,
         auto_zero_sensor=True,
         num_baseline_reads=10,
-        data_file="/data/testing_data/pressure.csv",
+        data_files={InstrumentProbeType.PRIMARY: "/data/testing_data/pressure.csv"},
     )
 
 
