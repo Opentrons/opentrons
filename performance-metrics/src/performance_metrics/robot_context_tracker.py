07f60ea85f415422d32a3bba9397b34c1a3ce64f
Derek Maggio <derek.j.maggio@gmail.com>
2024-04-12 07:55:42 -0700

    lost something in merge

--- performance-metrics/src/performance_metrics/robot_context_tracker.py
+++ performance-metrics/src/performance_metrics/robot_context_tracker.py
@@ -24,6 +24,7 @@ class RobotContextTracker:
     def __init__(self, storage_file_path: Path, should_track: bool = False) -> None:
         """Initializes the RobotContextTracker with an empty storage list."""
         self._storage: deque[RawContextData] = deque()
+        self._storage_file_path = storage_file_path
         self._should_track = should_track
 
     def track(self, state: RobotContextState) -> Callable:  # type: ignore
