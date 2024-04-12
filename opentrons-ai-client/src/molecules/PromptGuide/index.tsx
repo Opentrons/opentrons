b8a2e272d5160f9a6dfde1974064a9a99aa484aa
koji <kk2796@nyu.edu>
2024-04-12 16:25:13 -0400

    feat(opentrons-ai-client): add prompt guide component
    add prompt guide component
    
    close AUTH-341

---
+++ opentrons-ai-client/src/molecules/PromptGuide/index.tsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import { Trans, useTranslation } from 'react-i18next'
+import styled, { css } from 'styled-components'
+import {
+  Flex,
+  COLORS,
+  StyledText,
+  SPACING,
+  DIRECTION_COLUMN,
+  TYPOGRAPHY,
+  BORDERS,
+  Link,
+} from '@opentrons/components'
+
+const LABWARE_LIBRARY_URL = 'https://labware.opentrons.com/'
+
+export function PromptGuide(): JSX.Element {
+  const { t } = useTranslation('protocol_generator')
+
+  return (
+    <Flex
+      flexDirection={DIRECTION_COLUMN}
+      padding={SPACING.spacing32}
+      backgroundColor={COLORS.grey30}
+      borderRadius={BORDERS.borderRadius12}
+      gridGap={SPACING.spacing32}
+      width="58.125rem"
+    >
+      <StyledText css={HEADER_TEXT_STYLE}>
+        {t('what_typeof_protocol')}
+      </StyledText>
+      <StyledText css={BODY_TEXT_STYLE}>
+        {t('make_sure_your_prompt')}
+      </StyledText>
+      <Flex
+        css={BODY_TEXT_STYLE}
+        flexDirection={DIRECTION_COLUMN}
+        paddingLeft={SPACING.spacing32}
+      >
+        <ul>
+          <li>
+            <StyledText>{t('metadata')}</StyledText>
+            <ul css={NESTED_ITEM_STYLE}>
+              <li>
+                <StyledText>{t('application')}</StyledText>
+              </li>
+              <li>
+                <StyledText>{t('robot')}</StyledText>
+              </li>
+              <li>
+                <StyledText>{t('api')}</StyledText>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <StyledText>{t('ot2_pipettes')}</StyledText>
+          </li>
+          <li>
+            <StyledText>{t('modules')}</StyledText>
+          </li>
+          <li>
+            <StyledText>{t('well_allocations')}</StyledText>
+          </li>
+          <li>
+            <Trans
+              t={t}
+              i18nKey="tipracks_and_labware"
+              components={{
+                a: <ExternalLink external href={LABWARE_LIBRARY_URL} />,
+                span: <StyledText css={BODY_TEXT_STYLE} />,
+              }}
+            />
+          </li>
+          <li>
+            <StyledText>{t('commands')}</StyledText>
+          </li>
+        </ul>
+      </Flex>
+      <Trans
+        // css={BODY_TEXT_STYLE}
+        t={t}
+        i18nKey="what_if_you"
+        components={{
+          bold: <strong />,
+          span: <StyledText css={BODY_TEXT_STYLE} />,
+        }}
+      />
+    </Flex>
+  )
+}
+
+const HEADER_TEXT_STYLE = css`
+  font-size: ${TYPOGRAPHY.fontSize28};
+  line-height: ${TYPOGRAPHY.lineHeight36};
+  font-weight: ${TYPOGRAPHY.fontWeightSemiBold};
+`
+const BODY_TEXT_STYLE = css`
+  font-size: ${TYPOGRAPHY.fontSize20};
+  line-height: ${TYPOGRAPHY.lineHeight24};
+`
+const NESTED_ITEM_STYLE = css`
+  padding-left: ${SPACING.spacing16};
+  list-style-type: disc;
+`
+const ExternalLink = styled(Link)`
+  font-size: ${TYPOGRAPHY.fontSize20};
+  line-height: ${TYPOGRAPHY.lineHeight24};
+  color: ${COLORS.black90};
+  text-decoration: ${TYPOGRAPHY.textDecorationUnderline};
+`
