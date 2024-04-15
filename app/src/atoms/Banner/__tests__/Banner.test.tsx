dc093fb69683deb2cdbe088355e47a22a7b5c143
koji <koji.kanao@opentrons.com>
2024-04-15 15:07:18 -0400

    refactor(app): refactor banner component stories (#14894)
    * refactor(app): refactor banner component stories

--- app/src/atoms/Banner/__tests__/Banner.test.tsx
+++ app/src/atoms/Banner/__tests__/Banner.test.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
 import { describe, it, vi, expect, beforeEach } from 'vitest'
-import '@testing-library/jest-dom/vitest'
-import { fireEvent } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithProviders } from '../../../__testing-utils__'
 import { i18n } from '../../../i18n'
 import { Banner } from '..'
-import { renderWithProviders } from '../../../__testing-utils__'
 
 const render = (props: React.ComponentProps<typeof Banner>) => {
   return renderWithProviders(<Banner {...props} />, {
@@ -21,60 +20,67 @@ describe('Banner', () => {
       children: 'TITLE',
     }
   })
+
   it('renders success banner', () => {
-    const { getByText, getByLabelText } = render(props)
-    getByLabelText('icon_success')
-    getByText('TITLE')
+    render(props)
+    screen.getByLabelText('icon_success')
+    screen.getByText('TITLE')
   })
+
   it('renders success banner with exit button and when click dismisses banner', () => {
     props = {
       type: 'success',
       children: 'TITLE',
       onCloseClick: vi.fn(),
     }
-    const { getByText, getByLabelText } = render(props)
-    getByText('TITLE')
-    const btn = getByLabelText('close_icon')
+    render(props)
+    screen.getByText('TITLE')
+    const btn = screen.getByLabelText('close_icon')
     fireEvent.click(btn)
     expect(props.onCloseClick).toHaveBeenCalled()
   })
+
   it('renders warning banner', () => {
     props = {
       type: 'warning',
       children: 'TITLE',
     }
-    const { getByText, getByLabelText } = render(props)
-    getByLabelText('icon_warning')
-    getByText('TITLE')
+    render(props)
+    screen.getByLabelText('icon_warning')
+    screen.getByText('TITLE')
   })
+
   it('renders error banner', () => {
     props = {
       type: 'error',
       children: 'TITLE',
     }
-    const { getByText, getByLabelText } = render(props)
-    getByLabelText('icon_error')
-    getByText('TITLE')
+    render(props)
+    screen.getByLabelText('icon_error')
+    screen.getByText('TITLE')
   })
+
   it('renders updating banner', () => {
     props = {
       type: 'updating',
       children: 'TITLE',
     }
-    const { getByText, getByLabelText } = render(props)
-    getByLabelText('icon_updating')
-    getByText('TITLE')
+    render(props)
+    screen.getByLabelText('icon_updating')
+    screen.getByText('TITLE')
   })
+
   it('renders custom icon banner', () => {
     props = {
       type: 'warning',
       children: 'TITLE',
       icon: { name: 'ot-hot-to-touch' },
     }
-    const { getByText, getByLabelText } = render(props)
-    getByLabelText('icon_warning')
-    getByText('TITLE')
+    render(props)
+    screen.getByLabelText('icon_warning')
+    screen.getByText('TITLE')
   })
+
   it('renders custom close', () => {
     props = {
       type: 'warning',
@@ -82,8 +88,8 @@ describe('Banner', () => {
       closeButton: 'close button',
       onCloseClick: vi.fn(),
     }
-    const { getByText } = render(props)
-    const btn = getByText('close button')
+    render(props)
+    const btn = screen.getByText('close button')
     fireEvent.click(btn)
     expect(props.onCloseClick).toHaveBeenCalled()
   })
