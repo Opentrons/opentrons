717993b01af816096906a760123d036adf307813
Seth Foster <seth@opentrons.com>
2024-04-16 19:18:19 -0400

    fix(app): fix various install and version issues (#14926)
    Fixes various ongoing issues building versions into the system. This is
    a follow-on to #14844 (61b137132a3130e8cea170c7cf3d4c1931735942)
    
    - vite `define` config the way we do it does not hang the defined values
    off of props of `global` explicitly (or maybe us injecting
    `'globalThis'` into `global` breaks it) so use them as true globals,
    altering the way they're accessed and the way they're declared in the
    typings.
    - i guess you don't actually have to do type imports in the top level
    typings? removing the type import of the ipc bridge in the app-shell and
    app-shell-odd global.d.ts fixed that issue. don't know why
    - the ESM import for the script that updates the releases.json was wrong
    which breaks some update stuff
    
    ## Testing
    This is a bit annoying to test.
    
    You _must_ test this on a compiled app package. You _cannot_ test this
    on a dev build.
    On a compiled app package,
    - [x] the version should display in the settings tab of the app
    - [x] you shouldn't have warnings about `include` on undefined in your
    app logs
    - [ ] you should get robot update prompts when you use an
    internal-release build and connect to a robot running 1.3 or previous;
    you should get robot update prompts when you use a release build and
    connect to a robot running 7.2.1 or previous (note: couldn't test this
    in time but the rest of it works)
    - [x] the help menu should have a bugs url that works (the "report an
    issue" button; it should pop a browser tab)
    - [x] the help menu should say "View Opentrons App Logs" or "View
    Opentrons OT-3 App Logs" as the variant demands
    - [x] it should NOT say "View App Logs". that means the package name
    wasn't properly interpolated.
    
    I haven't dev-tested the second part because on my home setup making a
    full compiled app package is broken for some reason, and you can't
    actually run the node side in dev
    This once more,
    Closes EXEC-385 
    Closes RQA-2579

--- app-shell/typings/global.d.ts
+++ app-shell/typings/global.d.ts
@@ -1,8 +1,9 @@
 /* eslint-disable no-var */
 declare global {
-  var _PKG_VERSION_: string
-  var _PKG_PRODUCT_NAME_: string
-  var _PKG_BUGS_URL_: string
-  var _OPENTRONS_PROJECT_: string
   var APP_SHELL_REMOTE: { ipcRenderer: IpcRenderer; [key: string]: any }
 }
+
+declare const _PKG_VERSION_: string
+declare const _PKG_PRODUCT_NAME_: string
+declare const _PKG_BUGS_URL_: string
+declare const _OPENTRONS_PROJECT_: string
