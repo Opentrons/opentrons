edf12d9d234c07a252c8ff61c9f10c7f97e4ef11
koji <kk2796@nyu.edu>
2024-04-16 14:21:58 -0400

    add stories for ChatContainer component

--- opentrons-ai-client/src/organisms/ChatContainer/index.tsx
+++ opentrons-ai-client/src/organisms/ChatContainer/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import {
   COLORS,
   DIRECTION_COLUMN,
+  FLEX_MAX_CONTENT,
   Flex,
   SPACING,
   StyledText,
@@ -13,10 +14,18 @@ export function ChatContainer(): JSX.Element {
   const { t } = useTranslation('protocol_generator')
   const isDummyInitial = true
   return (
-    <Flex padding={SPACING.spacing40} backgroundColor={COLORS.grey10}>
+    <Flex
+      padding={SPACING.spacing40}
+      backgroundColor={COLORS.grey10}
+      width={FLEX_MAX_CONTENT}
+    >
       {/* This will be updated when input textbox and function are implemented */}
       {isDummyInitial ? (
-        <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing12}>
+        <Flex
+          flexDirection={DIRECTION_COLUMN}
+          gridGap={SPACING.spacing12}
+          width="100%"
+        >
           <StyledText>{t('opentronsai')}</StyledText>
           <PromptGuide />
         </Flex>
