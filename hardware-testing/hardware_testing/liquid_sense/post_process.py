1cec8306ab6d7485b0da4b9199530bc477d4ad65
Ryan howard <ryan.howard@opentrons.com>
2024-04-23 13:49:45 -0400

    add double pressure handling to final_report.csv output

--- hardware-testing/hardware_testing/liquid_sense/post_process.py
+++ hardware-testing/hardware_testing/liquid_sense/post_process.py
@@ -21,6 +21,25 @@ COL_TRIAL_CONVERSION = {
 }
 
 
+def _get_pressure_results(result_file: str) -> Tuple[float, float, float, List[float]]:
+    z_velocity: float = 0.0
+    p_velocity: float = 0.0
+    threshold: float = 0.0
+    pressures: List[float] = []
+    with open(result_file, newline="") as trial_csv:
+        trial_reader = csv.reader(trial_csv)
+        i = 0
+        for row in trial_reader:
+            if i == 1:
+                z_velocity = float(row[2])
+                p_velocity = float(row[3])
+                threshold = float(row[4])
+            if i > 1:
+                pressures.append(float(row[1]))
+            i += 1
+    return z_velocity, p_velocity, threshold, pressures
+
+
 def process_csv_directory(  # noqa: C901
     data_directory: str, tips: List[int], trials: int, make_graph: bool = False
 ) -> None:
@@ -29,15 +48,22 @@ def process_csv_directory(  # noqa: C901
     summary: str = [f for f in csv_files if "CSVReport" in f][0]
     final_report_file: str = f"{data_directory}/final_report.csv"
     # initialize our data structs
-    pressure_csvs = [f for f in csv_files if "pressure_sensor_data" in f]
-    pressure_results_files: Dict[int, List[str]] = {}
-    pressure_results: Dict[int, Dict[int, List[float]]] = {}
+    primary_pressure_csvs = [f for f in csv_files if "PRIMARY" in f]
+    secondary_pressure_csvs = [f for f in csv_files if "SECONDARY" in f]
+    primary_pressure_results_files: Dict[int, List[str]] = {}
+    secondary_pressure_results_files: Dict[int, List[str]] = {}
+    pressure_results: Dict[int, Dict[int, List[Tuple[float, float]]]] = {}
     results_settings: Dict[int, Dict[int, Tuple[float, float, float]]] = {}
     tip_offsets: Dict[int, List[float]] = {}
     p_offsets: Dict[int, List[float]] = {}
     meniscus_travel: float = 0
     for tip in tips:
-        pressure_results_files[tip] = [f for f in pressure_csvs if f"tip{tip}" in f]
+        primary_pressure_results_files[tip] = [
+            f for f in primary_pressure_csvs if f"tip{tip}" in f
+        ]
+        secondary_pressure_results_files[tip] = [
+            f for f in secondary_pressure_csvs if f"tip{tip}" in f
+        ]
         pressure_results[tip] = {}
         results_settings[tip] = {}
         tip_offsets[tip] = []
@@ -50,22 +76,35 @@ def process_csv_directory(  # noqa: C901
     # read in all of the pressure csvs into one big struct so we can process them
     for tip in tips:
         for trial in range(trials):
-            with open(
-                f"{data_directory}/{pressure_results_files[tip][trial]}", newline=""
-            ) as trial_csv:
-                trial_reader = csv.reader(trial_csv)
-                i = 0
-                for row in trial_reader:
-                    if i == 1:
-                        results_settings[tip][trial] = (
-                            float(row[2]),
-                            float(row[3]),
-                            float(row[4]),
-                        )
-                    if i > 1:
-                        pressure_results[tip][trial].append(float(row[1]))
-                    i += 1
-                max_results_len = max([i - 2, max_results_len])
+            z_velocity: float = 0.0
+            p_velocity: float = 0.0
+            threshold: float = 0.0
+            primary_pressures: List[float] = []
+            secondary_pressures: List[float] = []
+            if trial < len(primary_pressure_results_files[tip]):
+                (
+                    z_velocity,
+                    p_velocity,
+                    threshold,
+                    primary_pressures,
+                ) = _get_pressure_results(
+                    f"{data_directory}/{primary_pressure_results_files[tip][trial]}"
+                )
+            if trial < len(secondary_pressure_results_files[tip]):
+                (
+                    z_velocity,
+                    p_velocity,
+                    threshold,
+                    secondary_pressures,
+                ) = _get_pressure_results(
+                    f"{data_directory}/{secondary_pressure_results_files[tip][trial]}"
+                )
+            results_settings[tip][trial] = (z_velocity, p_velocity, threshold)
+            for i in range(max(len(primary_pressures), len(secondary_pressures))):
+                p = primary_pressures[i] if i < len(primary_pressures) else 0.0
+                s = secondary_pressures[i] if i < len(secondary_pressures) else 0.0
+                pressure_results[tip][trial].append((p, s))
+            max_results_len = max([len(pressure_results[tip][trial]), max_results_len])
     # start writing the final report csv
     with open(f"{data_directory}/{summary}", newline="") as summary_csv:
         summary_reader = csv.reader(summary_csv)
@@ -102,7 +141,12 @@ def process_csv_directory(  # noqa: C901
             pressure_header_row = ["time", ""]
             for i in range(trials):
                 pressure_header_row.extend(
-                    [f"pressure T{i+1}", f"z_travel T{i+1}", f"p_travel T{i+1}"]
+                    [
+                        f"primary pressure T{i+1}",
+                        f"secondary pressure T{i+1}",
+                        f"z_travel T{i+1}",
+                        f"p_travel T{i+1}",
+                    ]
                 )
 
             # we want to line up the z height's of each trial at time==0
@@ -153,7 +197,8 @@ def process_csv_directory(  # noqa: C901
                         pressure_row.append("")
                     for trial in range(trials):
                         if i < len(pressure_results[tip][trial]):
-                            pressure_row.append(f"{pressure_results[tip][trial][i]}")
+                            pressure_row.append(f"{pressure_results[tip][trial][i][0]}")
+                            pressure_row.append(f"{pressure_results[tip][trial][i][1]}")
                         else:
                             pressure_row.append("")
                         pressure_row.append(
