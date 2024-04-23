685417b3c2c9c05b41a71726c062ff0f9d5d2d56
koji <kk2796@nyu.edu>
2024-04-23 00:42:40 -0400

    remove i18n provider

--- opentrons-ai-client/src/organisms/PromptButton/PromptButton.stories.tsx
+++ opentrons-ai-client/src/organisms/PromptButton/PromptButton.stories.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { I18nextProvider } from 'react-i18next'
 import { Flex, SPACING } from '@opentrons/components'
-import { i18n } from '../../i18n'
 import { PromptProvider, promptContext } from './PromptProvider'
 import { PromptButton as PromptButtonComponent } from '.'
 
@@ -29,12 +27,10 @@ const meta: Meta<typeof PromptButtonComponent> = {
   decorators: [
     Story => {
       return (
-        <I18nextProvider i18n={i18n}>
-          <PromptProvider>
-            <Story />
-            <PromptDisplay />
-          </PromptProvider>
-        </I18nextProvider>
+        <PromptProvider>
+          <Story />
+          <PromptDisplay />
+        </PromptProvider>
       )
     },
   ],
