7beeb21ef691c321d9fbaa7646398a9177a1c069
Derek Maggio <derek.j.maggio@gmail.com>
2024-04-23 15:21:44 -0700

    formating and fix test

--- shared-data/python/tests/performance/test_module_builds.py
+++ shared-data/python/tests/performance/test_module_builds.py
@@ -1,10 +1,6 @@
 from pathlib import Path
-import pytest
-from opentrons_shared_data.performance.dev_types import MetricsMetadata
+from opentrons_shared_data.performance.dev_types import RobotContextState
+
 
 def test_metrics_metadata(tmp_path: Path) -> None:
-    MetricsMetadata(
-        name="test",
-        storage_dir=tmp_path,
-        headers=("a", "b")
-    )
+    RobotContextState.ANALYZING_PROTOCOL
