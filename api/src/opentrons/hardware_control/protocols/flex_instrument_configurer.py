3bc0790a96e2261b471d87899af635ba70b4adbc
caila-marashaj <caila.marashaj@opentrons.com>
2024-04-23 12:03:54 -0400

    typo fix

--- api/src/opentrons/hardware_control/protocols/flex_instrument_configurer.py
+++ api/src/opentrons/hardware_control/protocols/flex_instrument_configurer.py
@@ -46,7 +46,7 @@ class FlexInstrumentConfigurer(Protocol[MountArgType]):
         self,
         mount: MountArgType,
         expected: TipStateType,
-        follow_singular_sensor: Optional[InstrumentProbeType] = None,
+        ht_follow_singular_sensor: Optional[InstrumentProbeType] = None,
     ) -> None:
         """Check tip presence status and raise if it does not match `expected`."""
         ...
