a8832a5829edd1de333838dd072e761ec5617006
Brian Cooper <brian@opentrons.com>
2024-04-25 12:00:54 -0400

    fix staged changes removal and addition in the ODD deck configurator

--- app/src/pages/DeckConfiguration/index.tsx
+++ app/src/pages/DeckConfiguration/index.tsx
@@ -90,18 +90,18 @@ export function DeckConfigurationEditor(): JSX.Element {
       deckDef.cutoutFixtures.find(cf => cf.id === cutoutFixtureId)
         ?.fixtureGroup ?? {}
 
-    let newDeckConfig = deckConfig
+    let newDeckConfig = currentDeckConfig
     if (cutoutId in fixtureGroup) {
       const groupMap =
         fixtureGroup[cutoutId]?.find(group =>
           Object.entries(group).every(([cId, cfId]) =>
-            deckConfig.find(
+            currentDeckConfig.find(
               config =>
                 config.cutoutId === cId && config.cutoutFixtureId === cfId
             )
           )
         ) ?? {}
-      newDeckConfig = deckConfig.map(cutoutConfig =>
+      newDeckConfig = currentDeckConfig.map(cutoutConfig =>
         cutoutConfig.cutoutId in groupMap
           ? {
               ...cutoutConfig,
@@ -111,7 +111,7 @@ export function DeckConfigurationEditor(): JSX.Element {
           : cutoutConfig
       )
     } else {
-      newDeckConfig = deckConfig.map(cutoutConfig =>
+      newDeckConfig = currentDeckConfig.map(cutoutConfig =>
         cutoutConfig.cutoutId === cutoutId
           ? {
               ...cutoutConfig,
@@ -121,7 +121,7 @@ export function DeckConfigurationEditor(): JSX.Element {
           : cutoutConfig
       )
     }
-    updateDeckConfiguration(newDeckConfig)
+    setCurrentDeckConfig(newDeckConfig)
   }
 
   const handleClickConfirm = (): void => {
