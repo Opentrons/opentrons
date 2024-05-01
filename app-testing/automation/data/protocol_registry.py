4eac5a2954ac4ba8707838370c0e1a47cd52bf6c
Josh McVey <josh.mcvey@opentrons.com>
2024-05-01 10:03:07 -0400

    fix(test): snapshot renaming (#15038)
    # Cleanup snapshots 
    
    - Given the new protocol organization and naming
    - delete all the snapshots of the old naming `find -type f ! \( -name
    '*_S_*' -o -name '*_X_*' \) -exec rm {} +`
      - `make build-opentrons-analysis`  (default on edge)
      - ` make snapshot-test-update`
      - find old snapshots and delete

--- app-testing/automation/data/protocol_registry.py
+++ app-testing/automation/data/protocol_registry.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from typing import Optional
 
 from rich.console import Console
@@ -42,20 +43,41 @@ class ProtocolRegistry:
         return [getattr(self.protocols_with_overrides, prop) for prop in dir(self.protocols_with_overrides) if "__" not in prop]
 
 
+def all_stems() -> set[str]:
+    dir_path = Path(Path(__file__).resolve().parent.parent.parent, os.getenv("FILES_FOLDER", "files"), "protocols")
+    file_stems = {file.stem for file in dir_path.glob("*.py")}
+    return file_stems
+
+
 def main() -> None:
     console = Console()
     protocol_registry = ProtocolRegistry()
     console.print("protocols for APP_ANALYSIS_TEST_PROTOCOLS")
-    console.print(Panel('Formatted for .env APP_ANALYSIS_TEST_PROTOCOLS="'))
-    sorted_stems = sorted([p.file_stem for p in protocol_registry.all_defined_protocols()])
+    console.print(Panel("Formatted for .env"))
+    regular_stems = sorted([p.file_stem for p in protocol_registry.all_defined_protocols()])
     console.print('APP_ANALYSIS_TEST_PROTOCOLS="')
-    console.print(",\n".join(sorted_stems))
+    console.print(",\n".join(regular_stems))
     console.print('"')
-    console.print(Panel('Formatted for .env APP_ANALYSIS_TEST_PROTOCOLS_WITH_OVERRIDES="'))
+    override_stems = sorted([p.file_stem for p in protocol_registry.all_defined_protocols_with_overrides()])
     console.print('APP_ANALYSIS_TEST_PROTOCOLS_WITH_OVERRIDES="')
-    sorted_stems = sorted([p.file_stem for p in protocol_registry.all_defined_protocols_with_overrides()])
-    console.print(",\n".join(sorted_stems))
+    console.print(",\n".join(override_stems))
     console.print('"')
+
+    all_files = all_stems()
+    filtered_stems = {stem for stem in all_files if "overrides" not in stem.lower()}
+    found_override_stems = {stem for stem in all_files if "overrides" in stem.lower()}
+    # Finding and displaying differences
+    differences = filtered_stems - set(regular_stems)
+    if differences:
+        console.print(f"Stems in actual files not in mapping: {differences}")
+    else:
+        console.print("No differences between files and mapping.")
+
+    differences = found_override_stems - set(override_stems)
+    if differences:
+        console.print(f"Override Stems in actual files not in mapping: {differences}")
+    else:
+        console.print("No differences between actual override protocols and the mapping.")
 
 
 if __name__ == "__main__":
