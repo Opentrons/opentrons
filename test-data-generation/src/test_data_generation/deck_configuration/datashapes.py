60fc394dbf0a8fdee0670bf5dc4315ed2d23a6ca
Derek Maggio <derek.j.maggio@gmail.com>
2024-05-01 07:45:37 -0700

    feat: add Column

--- test-data-generation/src/test_data_generation/deck_configuration/datashapes.py
+++ test-data-generation/src/test_data_generation/deck_configuration/datashapes.py
@@ -5,9 +5,9 @@ import dataclasses
 import typing
 
 ColumnName = typing.Literal["1", "2", "3"]
-RowName = typing.Literal["A", "B", "C", "D"]
+RowName = typing.Literal["a", "b", "c", "d"]
 SlotName = typing.Literal[
-    "A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3", "D1", "D2", "D3"
+    "a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3", "d1", "d2", "d3"
 ]
 
 
@@ -41,33 +41,57 @@ class PossibleSlotContents(enum.Enum):
     def __str__(self) -> str:
         """Return a string representation of the slot content."""
         return f"{self.name.replace('_', ' ')}"
-
-    def is_a_module(self) -> bool:
-        """Return True if the slot contains a module."""
-        return self in [
+    
+    @classmethod
+    def all(cls) -> typing.List["PossibleSlotContents"]:
+        """Return all possible slot contents."""
+        return list(cls)
+    
+    @property
+    def modules(self) -> typing.List["PossibleSlotContents"]:
+        """Return the modules."""
+        return [
             PossibleSlotContents.THERMOCYCLER_MODULE,
             PossibleSlotContents.MAGNETIC_BLOCK_MODULE,
             PossibleSlotContents.TEMPERATURE_MODULE,
             PossibleSlotContents.HEATER_SHAKER_MODULE,
         ]
-
-    def is_a_staging_area(self) -> bool:
-        """Return True if the slot contains a staging area."""
-        return self in [
+    
+    @property
+    def staging_areas (self) -> typing.List["PossibleSlotContents"]:
+        """Return the staging areas."""
+        return [
             PossibleSlotContents.STAGING_AREA,
             PossibleSlotContents.STAGING_AREA_WITH_WASTE_CHUTE,
             PossibleSlotContents.STAGING_AREA_WITH_WASTE_CHUTE_NO_COVER,
             PossibleSlotContents.STAGING_AREA_WITH_MAGNETIC_BLOCK,
         ]
-
-    def is_a_waste_chute(self) -> bool:
-        """Return True if the slot contains a waste chute."""
-        return self in [
+    
+    @property
+    def waste_chutes(self) -> typing.List["PossibleSlotContents"]:
+        """Return the waste chutes."""
+        return [
             PossibleSlotContents.WASTE_CHUTE,
             PossibleSlotContents.WASTE_CHUTE_NO_COVER,
             PossibleSlotContents.STAGING_AREA_WITH_WASTE_CHUTE,
             PossibleSlotContents.STAGING_AREA_WITH_WASTE_CHUTE_NO_COVER,
         ]
+
+    def is_a_module(self) -> bool:
+        """Return True if the slot contains a module."""
+        return any([self is module for module in self.modules])
+    
+    def is_module_or_trash_bin(self) -> bool:
+        """Return True if the slot contains a module or trash bin."""
+        return self.is_a_module() or self is PossibleSlotContents.TRASH_BIN
+
+    def is_a_staging_area(self) -> bool:
+        """Return True if the slot contains a staging area."""
+        return any([self is staging_area for staging_area in self.staging_areas])
+
+    def is_a_waste_chute(self) -> bool:
+        """Return True if the slot contains a waste chute."""
+        return any([self is waste_chute for waste_chute in self.waste_chutes])
 
 
 @dataclasses.dataclass
@@ -97,6 +121,7 @@ class Slot:
         """Return the slot contents."""
         return f"{str(self.contents).center(self.contents.longest_string())}"
 
+
 @dataclasses.dataclass
 class Row:
     """A row of slots on a Flex."""
@@ -111,7 +136,7 @@ class Row:
         """Return a string representation of the row."""
         return f"{self.col1}{self.col2}{self.col3}"
 
-    def col_by_name(self, name: ColumnName) -> Slot:
+    def slot_by_col_number(self, name: ColumnName) -> Slot:
         """Return the slot by name."""
         return getattr(self, f"col{name}")  # type: ignore
 
@@ -123,6 +148,53 @@ class Row:
     def __len__(self) -> int:
         """Return the number of slots in the row."""
         return len(self.slots)
+    
+    def update_slot(self, slot: Slot) -> None:
+        """Update the slot in the row."""
+        setattr(self, f"col{slot.col}", slot)
+
+
+@dataclasses.dataclass
+class Column:
+    """A column of slots on a Flex."""
+
+    col: ColumnName
+
+    a: Slot
+    b: Slot
+    c: Slot
+    d: Slot
+
+    def __str__(self) -> str:
+        """Return a string representation of the column."""
+        return f"{self.a}{self.b}{self.c}{self.d}"
+
+    @property
+    def slots(self) -> typing.List[Slot]:
+        """Return the slots in the column."""
+        return [self.a, self.b, self.c, self.d]
+
+    def slot_by_row(self, name: RowName) -> Slot:
+        """Return the slot by name."""
+        return getattr(self, f"{name}")
+
+    def number_of(self, contents: PossibleSlotContents) -> int:
+        """Return the number of slots with the contents."""
+        return len([True for slot in self.slots if slot.contents is contents])
+
+    def slot_above(self, slot: Slot) -> typing.Optional[Slot]:
+        """Return the slot above the passed slot."""
+        index = self.slots.index(slot)
+        if index == 0:
+            return None
+        return self.slots[index - 1]
+
+    def slot_below(self, slot: Slot) -> typing.Optional[Slot]:
+        """Return the slot below the passed slot."""
+        index = self.slots.index(slot)
+        if index == 3:
+            return None
+        return self.slots[index + 1]
 
 
 @dataclasses.dataclass
@@ -163,7 +235,17 @@ class DeckConfiguration:
             for slot in self.slots
             for other_slot in other.slots
         )
-
+    
+    @classmethod
+    def from_cols(cls, col1: Column, col2: Column, col3: Column) -> "DeckConfiguration":
+        """Create a deck configuration from columns."""
+        return cls(
+            a=Row("a", col1.a, col2.a, col3.a),
+            b=Row("b", col1.b, col2.b, col3.b),
+            c=Row("c", col1.c, col2.c, col3.c),
+            d=Row("d", col1.d, col2.d, col3.d),
+        )
+    
     @property
     def rows(self) -> typing.List[Row]:
         """Return the rows of the deck."""
@@ -171,7 +253,7 @@ class DeckConfiguration:
 
     def row_by_name(self, name: RowName) -> Row:
         """Return the row by name."""
-        return getattr(self, name.lower())  # type: ignore
+        return getattr(self, name)  # type: ignore
 
     @property
     def slots(self) -> typing.List[Slot]:
@@ -183,11 +265,31 @@ class DeckConfiguration:
         row_index = self.rows.index(self.row_by_name(slot.row))
         if row_index == 0:
             return None
-        return self.rows[row_index - 1].col_by_name(slot.col)
+        return self.rows[row_index - 1].slot_by_col_number(slot.col)
 
     def slot_below(self, slot: Slot) -> typing.Optional[Slot]:
         """Return the slot below the passed slot."""
         row_index = self.rows.index(self.row_by_name(slot.row))
         if row_index == 3:
             return None
-        return self.rows[row_index + 1].col_by_name(slot.col)
+        return self.rows[row_index + 1].slot_by_col_number(slot.col)
+
+    def number_of(self, contents: PossibleSlotContents) -> int:
+        """Return the number of slots with the contents."""
+        return len([True for slot in self.slots if slot.contents is contents])
+
+    def override_with_column(self, column: Column) -> None:
+        """Override the deck configuration with the column."""
+        for row in self.rows:
+            new_value = column.slot_by_row(row.row)
+            row.update_slot(new_value)
+
+    def column_by_number(self, number: ColumnName) -> Column:
+        """Return the column by number."""
+        return Column(
+            col=number,
+            a=self.a.slot_by_col_number(number),
+            b=self.b.slot_by_col_number(number),
+            c=self.c.slot_by_col_number(number),
+            d=self.d.slot_by_col_number(number),
+        )
