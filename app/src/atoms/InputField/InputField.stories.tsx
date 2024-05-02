a167a251f4493f34f2c4618cb215eae78b363a3f
koji <kk2796@nyu.edu>
2024-05-02 19:01:50 -0400

    fix(app): update Inputfield stories
    update Inputfield stories
    
    clsoe RQA-2667

--- app/src/atoms/InputField/InputField.stories.tsx
+++ app/src/atoms/InputField/InputField.stories.tsx
@@ -1,32 +1,37 @@
-import * as React from 'react'
-import { InputField } from './index'
-import type { Story, Meta } from '@storybook/react'
+import { VIEWPORT } from '@opentrons/components'
+import { InputField as InputFieldComponent } from './index'
+import type { Meta, StoryObj } from '@storybook/react'
 
-export default {
+const meta: Meta<typeof InputFieldComponent> = {
+  // ToDo (kk05/02/2024) this should be in Library but at this moment there is the same name component in components
+  // The unification for this component will be done when the old component is retired completely.
   title: 'App/Atoms/InputField',
-  component: InputField,
-} as Meta
-
-const Template: Story<React.ComponentProps<typeof InputField>> = args => (
-  <InputField {...args} />
-)
-
-export const Primary = Template.bind({})
-Primary.args = {
-  value: 200,
-  units: 'rpm',
-  type: 'number',
-  caption: 'example caption',
-  max: 200,
-  min: 200,
+  component: InputFieldComponent,
+  parameters: VIEWPORT.touchScreenViewport,
 }
 
-export const ErrorMessage = Template.bind({})
-ErrorMessage.args = {
-  units: 'C',
-  type: 'number',
-  caption: 'example caption',
-  max: 10,
-  min: 10,
-  error: 'input does not equal 10',
+export default meta
+
+type Story = StoryObj<typeof InputFieldComponent>
+
+export const InputField: Story = {
+  args: {
+    value: 200,
+    units: 'rpm',
+    type: 'number',
+    caption: 'example caption',
+    max: 200,
+    min: 200,
+  },
+}
+
+export const InputFieldWithError: Story = {
+  args: {
+    units: 'C',
+    type: 'number',
+    caption: 'example caption',
+    max: 10,
+    min: 10,
+    error: 'input does not equal 10',
+  },
 }
