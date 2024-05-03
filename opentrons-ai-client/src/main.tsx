b317cd4d289e9101bbfa03ffbbe2535bc6e40b7e
koji <koji.kanao@opentrons.com>
2024-05-03 13:41:56 -0400

    feat(opentrons-ai-client): add jotai and custom hook for call api (#15029)
    * feat(opentrons-ai-client): add jotai and custom hook for call api

--- opentrons-ai-client/src/main.tsx
+++ opentrons-ai-client/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
 import { GlobalStyle } from './atoms/GlobalStyle'
-import { PromptProvider } from './organisms/PromptButton/PromptProvider'
 
 import { i18n } from './i18n'
 import { App } from './App'
@@ -13,9 +12,7 @@ if (rootElement != null) {
     <React.StrictMode>
       <GlobalStyle />
       <I18nextProvider i18n={i18n}>
-        <PromptProvider>
-          <App />
-        </PromptProvider>
+        <App />
       </I18nextProvider>
     </React.StrictMode>
   )
