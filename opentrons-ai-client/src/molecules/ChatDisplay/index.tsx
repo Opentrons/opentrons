d57e5640ee72fc2a57aad08e1d3ff7c926caded3
koji <kk2796@nyu.edu>
2024-05-07 17:52:57 -0400

    fix lint error

--- opentrons-ai-client/src/molecules/ChatDisplay/index.tsx
+++ opentrons-ai-client/src/molecules/ChatDisplay/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { useTranslation } from 'react-i18next'
 import Markdown from 'react-markdown'
 import {
