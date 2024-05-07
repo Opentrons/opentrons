c422fe80bafc3e8a0047a571836b05f4cc6f2908
koji <kk2796@nyu.edu>
2024-05-07 17:25:51 -0400

    feat(opentrons-ai-client): add code copy button
    add code copy button
    
    close AUTH-389

--- opentrons-ai-client/src/molecules/ChatDisplay/index.tsx
+++ opentrons-ai-client/src/molecules/ChatDisplay/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
-// import { css } from 'styled-components'
+import styled, { css } from 'styled-components'
 import { useTranslation } from 'react-i18next'
 import Markdown from 'react-markdown'
 import {
+  ALIGN_CENTER,
   BORDERS,
   COLORS,
   DIRECTION_COLUMN,
   Flex,
+  Icon,
+  JUSTIFY_CENTER,
+  POSITION_ABSOLUTE,
+  POSITION_RELATIVE,
+  PrimaryButton,
   SPACING,
   StyledText,
+  TYPOGRAPHY,
 } from '@opentrons/components'
 
 import type { ChatData } from '../../resources/types'
@@ -19,8 +26,17 @@ interface ChatDisplayProps {
 
 export function ChatDisplay({ chat }: ChatDisplayProps): JSX.Element {
   const { t } = useTranslation('protocol_generator')
+  const [isCopied, setIsCopied] = React.useState<boolean>(false)
   const { role, content } = chat
   const isUser = role === 'user'
+
+  const handleClickCopy = async (): Promise<void> => {
+    const lastCodeBlock = document.querySelectorAll('pre')[0]
+    const code = (lastCodeBlock.innerText || lastCodeBlock.textContent) ?? ''
+    await navigator.clipboard.writeText(code)
+    setIsCopied(true)
+  }
+
   return (
     <Flex
       flexDirection={DIRECTION_COLUMN}
@@ -38,9 +54,10 @@ export function ChatDisplay({ chat }: ChatDisplayProps): JSX.Element {
         width="100%"
         flexDirection={DIRECTION_COLUMN}
         gridGap={SPACING.spacing16}
+        position={POSITION_RELATIVE}
       >
         {/* ToDo (kk:05/02/2024) This part is waiting for Mel's design */}
-        {/* <Markdown
+        <Markdown
           components={{
             div: undefined,
             ul: UnnumberedListText,
@@ -48,45 +65,81 @@ export function ChatDisplay({ chat }: ChatDisplayProps): JSX.Element {
             li: ListItemText,
             p: ParagraphText,
             a: ExternalLink,
-            code: CodeText,
+            code: Code,
           }}
         >
           {content}
-        </Markdown> */}
-        <Markdown>{content}</Markdown>
+        </Markdown>
+        {role === 'assistant' ? (
+          <PrimaryButton
+            position={POSITION_ABSOLUTE}
+            right="2rem"
+            bottom="2rem"
+            borderRadius={BORDERS.borderRadiusFull}
+            onClick={handleClickCopy}
+          >
+            <Flex
+              padding={`${SPACING.spacing16} ${SPACING.spacing24}`}
+              gridGap={SPACING.spacing8}
+              alignItems={ALIGN_CENTER}
+              justifyContent={JUSTIFY_CENTER}
+            >
+              <Icon
+                size="2rem"
+                name={isCopied ? 'check' : 'copy-text'}
+                color={COLORS.white}
+              />
+              <StyledText
+                fontSize={TYPOGRAPHY.fontSize22}
+                lineHeight={TYPOGRAPHY.lineHeight28}
+                fontWeight={TYPOGRAPHY.fontWeightSemiBold}
+              >
+                {t('copy_code')}
+              </StyledText>
+            </Flex>
+          </PrimaryButton>
+        ) : null}
       </Flex>
     </Flex>
   )
 }
 
 // ToDo (kk:05/02/2024) This part is waiting for Mel's design
-// function ExternalLink(props: JSX.IntrinsicAttributes): JSX.Element {
-//   return <a {...props} target="_blank" rel="noopener noreferrer" />
-// }
+function ExternalLink(props: JSX.IntrinsicAttributes): JSX.Element {
+  return <a {...props} target="_blank" rel="noopener noreferrer" />
+}
 
-// function ParagraphText(props: JSX.IntrinsicAttributes): JSX.Element {
-//   return <StyledText {...props} as="p" />
-// }
+function ParagraphText(props: JSX.IntrinsicAttributes): JSX.Element {
+  return <StyledText {...props} as="p" />
+}
 
-// function HeaderText(props: JSX.IntrinsicAttributes): JSX.Element {
-//   return <StyledText {...props} as="h3" />
-// }
+function HeaderText(props: JSX.IntrinsicAttributes): JSX.Element {
+  return <StyledText {...props} as="h3" />
+}
 
-// function ListItemText(props: JSX.IntrinsicAttributes): JSX.Element {
-//   return <StyledText {...props} as="li" />
-// }
+function ListItemText(props: JSX.IntrinsicAttributes): JSX.Element {
+  return <StyledText {...props} as="li" />
+}
 
-// function UnnumberedListText(props: JSX.IntrinsicAttributes): JSX.Element {
-//   return <StyledText {...props} as="ul" />
-// }
+function UnnumberedListText(props: JSX.IntrinsicAttributes): JSX.Element {
+  return <StyledText {...props} as="ul" />
+}
 
-// const CODE_TEXT_STYLE = css`
-//   padding: ${SPACING.spacing16};
-//   font-family: monospace;
-//   color: ${COLORS.white};
-//   background-color: ${COLORS.black90};
-// `
+const CodeText = styled.code`
+  font-family: monospace;
+  color: ${COLORS.white};
+  background-color: ${COLORS.black90};
+`
 
-// function CodeText(props: JSX.IntrinsicAttributes): JSX.Element {
-//   return <StyledText {...props} as="p" css={CODE_TEXT_STYLE} />
-// }
+function Code(props: JSX.IntrinsicAttributes): JSX.Element {
+  return (
+    <Flex
+      backgroundColor={COLORS.black90}
+      padding={SPACING.spacing16}
+      borderRadius={BORDERS.borderRadius8}
+      marginBottom={SPACING.spacing32}
+    >
+      <CodeText {...props} />
+    </Flex>
+  )
+}
