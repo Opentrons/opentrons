ba3586cf419b75a56fecfa1861e97a3e611f5613
koji <koji.kanao@opentrons.com>
2024-05-07 11:45:05 -0400

    feat(opentrons-ai-client): add primary floating button (#15107)
    * feat(opentrons-ai-client): add primary floating button

---
+++ opentrons-ai-client/src/molecules/PrimaryFloatingButton/__tests__/PrimaryFloatingButton.test.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import { describe, it, beforeEach, expect } from 'vitest'
+import { fireEvent, screen } from '@testing-library/react'
+import { BORDERS, COLORS, TYPOGRAPHY, SPACING } from '@opentrons/components'
+import { renderWithProviders } from '../../../__testing-utils__'
+import { PrimaryFloatingButton } from '../index'
+
+const render = (props: React.ComponentProps<typeof PrimaryFloatingButton>) => {
+  return renderWithProviders(<PrimaryFloatingButton {...props} />)
+}
+
+describe('PrimaryFloatingButton', () => {
+  let props: React.ComponentProps<typeof PrimaryFloatingButton>
+  beforeEach(() => {
+    props = {
+      buttonText: 'primary floating button',
+      iconName: 'arrow-down',
+      disabled: false,
+    }
+  })
+  it('render primary floating button', () => {
+    render(props)
+    const button = screen.getByRole('button', {
+      name: 'primary floating button',
+    })
+    expect(button).toHaveStyle(`background-color: ${COLORS.blue50}`)
+    expect(button).toHaveStyle(
+      `padding: ${SPACING.spacing8} ${SPACING.spacing16}`
+    )
+    const buttonText = screen.getByText('primary floating button')
+    expect(buttonText).toHaveStyle(`font-size: ${TYPOGRAPHY.fontSizeP}`)
+    expect(buttonText).toHaveStyle(
+      `font-weight: ${TYPOGRAPHY.fontWeightSemiBold}`
+    )
+    expect(buttonText).toHaveStyle(`line-height: ${TYPOGRAPHY.lineHeight20}`)
+    expect(button).toHaveStyle(`border-radius: ${BORDERS.borderRadiusFull}`)
+    expect(button).toHaveStyle(
+      `text-transform: ${TYPOGRAPHY.textTransformNone}`
+    )
+    expect(button).toHaveStyle(`box-shadow: none`)
+    expect(button).toHaveStyle(`color: ${COLORS.white}`)
+  })
+
+  it('should render icon', () => {
+    render(props)
+    const icon = screen.getByTestId('PrimaryFloatingButton_Icon')
+    expect(icon).toHaveStyle(`width: 0.75rem`)
+    expect(icon).toHaveStyle(`height: 0.75rem`)
+    expect(icon).toHaveStyle(`color: ${COLORS.white}`)
+  })
+
+  it('renders primary button with text and disabled', () => {
+    props.disabled = true
+    render(props)
+    const button = screen.getByRole('button', {
+      name: 'primary floating button',
+    })
+    expect(button).toBeDisabled()
+    expect(button).toHaveStyle(`background-color: ${COLORS.grey30}`)
+    expect(button).toHaveStyle(`color: ${COLORS.grey40}`)
+  })
+
+  it('applies the correct states to the button - hover', () => {
+    render(props)
+    const button = screen.getByRole('button', {
+      name: 'primary floating button',
+    })
+    fireEvent.mouseOver(button)
+    expect(button).toHaveStyle(`background-color: ${COLORS.blue50}`)
+  })
+})
