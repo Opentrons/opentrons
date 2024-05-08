fc4b912abc659d9c330e0247cf6232462a701acd
tamarzanzouri <tamar.zanzouri@opentrons.com>
2024-05-08 15:43:36 -0400

    moved monkeypatch into builer test

--- api/tests/opentrons/protocol_runner/test_run_orchestrator_provider.py
+++ api/tests/opentrons/protocol_runner/test_run_orchestrator_provider.py
@@ -9,6 +9,7 @@ from opentrons.protocol_engine import (
     commands as pe_commands,
     CommandIntent,
 )
+from opentrons.protocol_engine.types import PostRunHardwareState
 from opentrons.hardware_control import API as HardwareAPI
 from opentrons.protocol_reader import JsonProtocolConfig, PythonProtocolConfig
 from opentrons.protocol_runner.run_orchestrator import RunOrchestrator
@@ -19,16 +20,6 @@ from opentrons.protocol_runner.protocol_runner import (
     LiveRunner,
     AnyRunner,
 )
-
-
-@pytest.fixture(autouse=True)
-def patch_mock_create_protocol_runner(
-    decoy: Decoy, monkeypatch: pytest.MonkeyPatch
-) -> None:
-    """Replace deck_conflict.check() with a mock."""
-    mock = decoy.mock(func=protocol_runner.create_protocol_runner)
-    monkeypatch.setattr(protocol_runner, "create_protocol_runner", mock)
-
 
 @pytest.fixture
 def mock_protocol_python_runner(decoy: Decoy) -> PythonAndLegacyRunner:
@@ -67,26 +58,38 @@ def mock_hardware_api(decoy: Decoy) -> HardwareAPI:
 
 
 @pytest.fixture
-def config(decoy: Decoy) -> HardwareAPI:
+def mock_runner(decoy: Decoy, mock_protocol_json_runner: JsonRunner) -> JsonRunner:
     """Get a mocked out HardwareAPI dependency."""
-    return JsonProtocolConfig(schema_version=7)
+    return mock_protocol_json_runner
 
 
 @pytest.mark.parametrize(
-    "config",
+    # "config",
+    # [
+    #     (PythonProtocolConfig(api_version=APIVersion.from_string("2.14"))),
+    #     JsonProtocolConfig(schema_version=7),
+    # ],
+    "mock_runner",
     [
-        (PythonProtocolConfig(api_version=APIVersion.from_string("2.14"))),
-        JsonProtocolConfig(schema_version=7),
+        (lazy_fixture("mock_protocol_json_runner")),
+        (lazy_fixture("mock_protocol_python_runner")),
     ],
 )
 @pytest.fixture
 def subject(
     mock_protocol_engine: ProtocolEngine,
     mock_hardware_api: HardwareAPI,
-    config: Union[JsonProtocolConfig, PythonProtocolConfig],
+    mock_runner: Union[JsonRunner, PythonAndLegacyRunner],
+    mock_fixit_runner: LiveRunner,
+    mock_setup_runner: LiveRunner
+    # config: Union[JsonProtocolConfig, PythonProtocolConfig],
 ) -> RunOrchestrator:
-    return RunOrchestrator.build_orchestrator(
-        protocol_engine=mock_protocol_engine, hardware_api=mock_hardware_api, protocol_config=config
+    return RunOrchestrator(
+        protocol_engine=mock_protocol_engine,
+        hardware_api=mock_hardware_api,
+        fixit_runner=mock_fixit_runner,
+        setup_runner=mock_setup_runner,
+        json_or_python_protocol_runner=mock_runner,
     )
 
 
@@ -106,6 +109,7 @@ def subject(
 )
 def test_build_run_orchestrator_provider(
     decoy: Decoy,
+    monkeypatch: pytest.MonkeyPatch,
     subject: RunOrchestrator,
     mock_protocol_engine: ProtocolEngine,
     mock_hardware_api: HardwareAPI,
@@ -114,30 +118,50 @@ def test_build_run_orchestrator_provider(
     mock_fixit_runner: LiveRunner,
     mock_protocol_runner: Optional[Union[PythonAndLegacyRunner, JsonRunner]],
 ) -> None:
+    mock_create_runner_func = decoy.mock(func=protocol_runner.create_protocol_runner)
+    monkeypatch.setattr(protocol_runner, "create_protocol_runner", mock_create_runner_func)
+
+    decoy.when(
+        mock_create_runner_func(
+            protocol_engine=mock_protocol_engine,
+            hardware_api=mock_hardware_api,
+            post_run_hardware_state=PostRunHardwareState.HOME_AND_STAY_ENGAGED,
+            drop_tips_after_run=True,
+        )
+    ).then_return(mock_setup_runner)
+
+    decoy.when(
+        mock_create_runner_func(
+            protocol_engine=mock_protocol_engine,
+            hardware_api=mock_hardware_api,
+            post_run_hardware_state=PostRunHardwareState.HOME_AND_STAY_ENGAGED,
+            drop_tips_after_run=True,
+        )
+    ).then_return(mock_fixit_runner)
+
+    decoy.when(
+        mock_create_runner_func(
+            protocol_config=input_protocol_config,
+            protocol_engine=mock_protocol_engine,
+            hardware_api=mock_hardware_api,
+            post_run_hardware_state=PostRunHardwareState.HOME_AND_STAY_ENGAGED,
+            drop_tips_after_run=True,
+        )
+    ).then_return(mock_fixit_runner)
+
     result = subject.build_orchestrator(
         protocol_engine=mock_protocol_engine,
         hardware_api=mock_hardware_api,
         protocol_config=input_protocol_config,
     )
 
-    print(result)
-    # decoy.when(
-    #     protocol_runner.create_protocol_runner(
-    #         protocol_engine=mock_protocol_engine, hardware_api=mock_hardware_api
-    #     )
-    # ).then_return(mock_setup_runner)
-    #
-    # decoy.when(
-    #     protocol_runner.create_protocol_runner(
-    #         protocol_engine=mock_protocol_engine, hardware_api=mock_hardware_api
-    #     )
-    # ).then_return(mock_fixit_runner)
-    #
-    # assert result == RunOrchestrator(
-    #     setup_runner=mock_setup_runner,
-    #     fixit_runner=mock_fixit_runner,
-    #     json_or_python_runner=mock_protocol_runner,
-    # )
+    assert result == RunOrchestrator(
+        protocol_engine=mock_protocol_engine,
+        hardware_api=mock_hardware_api,
+        setup_runner=mock_setup_runner,
+        fixit_runner=mock_fixit_runner,
+        json_or_python_protocol_runner=mock_protocol_runner,
+    )
 
 
 @pytest.mark.parametrize(
@@ -169,4 +193,5 @@ def test_add_command(
 
     assert result == command_to_queue
 
+    # not working!
     decoy.verify(runner.set_command_queued(command_to_queue))
