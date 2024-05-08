70e3d24aa60d82edf9313e2fd8d036f21786928b
Brian Cooper <brian@opentrons.com>
2024-05-08 16:33:02 -0400

    add border radius and background color

--- components/src/atoms/Checkbox/index.tsx
+++ components/src/atoms/Checkbox/index.tsx
@@ -3,18 +3,25 @@ import { COLORS, BORDERS } from '../../helix-design-system'
 import { Flex } from '../../primitives'
 import { Icon } from '../../icons'
 import { DIRECTION_ROW, JUSTIFY_SPACE_BETWEEN } from '../../styles'
+import { SPACING } from '../../ui-style-constants'
 import { StyledText } from '../StyledText'
 
 export interface CheckboxProps {
   /** checkbox is checked if value is true */
   isChecked: boolean
 }
-
 export function Checkbox(props: CheckboxProps): JSX.Element {
   const { isChecked } = props
-
+  const backgroundColor = isChecked ? COLORS.blue50 : COLORS.blue35
+  const color = isChecked ? COLORS.white : COLORS.black90
   return (
-    <Flex flexDirection={DIRECTION_ROW} justifyContent={JUSTIFY_SPACE_BETWEEN}>
+    <Flex
+      flexDirection={DIRECTION_ROW}
+      justifyContent={JUSTIFY_SPACE_BETWEEN}
+      padding={SPACING.spacing20}
+      borderRadius={BORDERS.borderRadius16}
+      backgroundColor={backgroundColor}
+      color={color}>
       <StyledText>
 
       </StyledText>
@@ -28,7 +35,7 @@ interface CheckProps {
 }
 function Check(props: CheckProps): JSX.Element {
   return props.isChecked ? (
-    <Icon name="ot-checkbox" size="1.75rem" />
+    <Icon name="ot-checkbox" size="1.75rem" color={COLORS.white} />
   ) : (
     <Flex
       size="1.75rem"
