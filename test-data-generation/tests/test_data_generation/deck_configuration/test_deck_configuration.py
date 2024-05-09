4481dc18425bd0e566713d34e01307c7d45475b1
Derek Maggio <derek.j.maggio@gmail.com>
2024-05-09 09:51:55 -0700

    chore: add running and evaluating analysis

--- test-data-generation/tests/test_data_generation/deck_configuration/test_deck_configuration.py
+++ test-data-generation/tests/test_data_generation/deck_configuration/test_deck_configuration.py
@@ -1,21 +1,34 @@
 """Tests to ensure that the deck configuration is generated correctly."""
 
 from pathlib import Path
-from hypothesis import given, settings, HealthCheck
-from test_data_generation.deck_configuration.datashapes import DeckConfiguration
+
+import pytest
+from hypothesis import HealthCheck, given, note, settings
+from tests.utils import has_errors, make_the_failed_protocol_pretty, run_analysis
+
+from test_data_generation.datashapes import DeckConfiguration
 from test_data_generation.deck_configuration.strategy.deck_configuration_strategies import (
     a_deck_configuration_with_invalid_fixture_in_col_2,
 )
 from test_data_generation.python_protocol_generation.python_protocol_generator import (
     PythonProtocolGenerator,
 )
+from test_data_generation.python_protocol_generation.strategy.python_protocol_generation_strategies import (
+    a_protocol_that_loads_invalid_stuff_into_a_staging_area_col_3,
+    a_protocol_that_loads_invalid_stuff_into_a_staging_area_col_4,
+)
 
 
 @given(deck_config=a_deck_configuration_with_invalid_fixture_in_col_2())
 @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
-def test_invalid_fixture_in_col_2(
+@pytest.mark.asyncio
+async def test_invalid_fixture_in_col_2(
     deck_config: DeckConfiguration, tmp_path: Path
 ) -> None:
     """I hypothesize, that any deck configuration that contains at least one, Heater-Shaker, Trash Bin, or Temperature module, in column 2 is invalid."""
     protocol_content = PythonProtocolGenerator(deck_config, "2.18").generate_protocol()
-    print(protocol_content)
+    analysis_response = await run_analysis(protocol_content, tmp_path)
+    note(str(deck_config))
+    assert has_errors(
+        analysis_response
+    ), "This deck configuration should cause analysis to fail."
