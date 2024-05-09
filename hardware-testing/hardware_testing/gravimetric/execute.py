410d267f9a61a8ec56208961a15b10dffcf98835
Ryan howard <ryan.howard@opentrons.com>
2024-05-09 10:55:21 -0400

    format

--- hardware-testing/hardware_testing/gravimetric/execute.py
+++ hardware-testing/hardware_testing/gravimetric/execute.py
@@ -576,7 +576,7 @@ def _get_liquid_height(
 
 def _multi_sense(
     resources: TestResources, cfg: config.GravimetricConfig, total_tips: int, well: Well
-):
+) -> float:
     height_sum: float = 0
     for h in range(_PROBES):
         tip = _next_tip_for_channel(cfg, resources, 0, total_tips)
@@ -591,12 +591,11 @@ def _multi_sense(
                 offset=_get_channel_offset(cfg, 0),
             )  # always trash calibration tips
     return height_sum / _PROBES
-    return
 
 
-def run(
+def run(  # noqa: C901
     cfg: config.GravimetricConfig, resources: TestResources, number_of_racks: int
-) -> None:  # noqa: C901
+) -> None:
     """Run."""
     global _PREV_TRIAL_GRAMS
     global _MEASUREMENTS
