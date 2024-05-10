7d4aa5a8f0e7465e6b1293205a785eef27d5d4f7
koji <kk2796@nyu.edu>
2024-05-10 15:07:42 -0400

    add useAuth0 hook

--- opentrons-ai-client/src/App.tsx
+++ opentrons-ai-client/src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react'
-import { DIRECTION_ROW, Flex } from '@opentrons/components'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useTranslation } from 'react-i18next'
+
+import {
+  DIRECTION_ROW,
+  Flex,
+  Link as LinkButton,
+  POSITION_ABSOLUTE,
+  POSITION_RELATIVE,
+  TYPOGRAPHY,
+} from '@opentrons/components'
 
 import { SidePanel } from './molecules/SidePanel'
 import { ChatContainer } from './organisms/ChatContainer'
 
+const LOGIN_PAGE_URL = 'https://auth-dev.opentrons.com/'
+
 export function App(): JSX.Element {
+  const { t } = useTranslation('protocol_generator')
+  const { isAuthenticated, logout } = useAuth0()
+
+  // if not authenticated redirect to login page
+  if (!isAuthenticated) {
+    window.location.href = LOGIN_PAGE_URL
+  }
+
   return (
-    <Flex flexDirection={DIRECTION_ROW}>
+    <Flex flexDirection={DIRECTION_ROW} position={POSITION_RELATIVE}>
+      {isAuthenticated ? (
+        // Note: this logout button is temporary
+        <Flex position={POSITION_ABSOLUTE} top="1rem" right="1rem">
+          <LinkButton
+            onClick={() => logout()}
+            textDecoration={TYPOGRAPHY.textDecorationUnderline}
+          >
+            {t('logout')}
+          </LinkButton>
+        </Flex>
+      ) : null}
       <SidePanel />
       <ChatContainer />
     </Flex>
