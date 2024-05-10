ab2df12284340b6fbc8f8579f58e088750040460
Seth Foster <seth@opentrons.com>
2024-05-10 16:16:57 -0400

    feat(app): add desktop modal for incompatible modules
    feat(app): IncompatibleModules on desktop
    
    Display a modal on the desktop app if incompatible modules are attached
    to the robot you're currently viewing. This modal allows interaction
    with the navbar but otherwise is displayed at any time that the app is
    viewing a specific robot (i.e. prepare for run, device details) and that
    robot has incompatible modules attached.

--- app/src/organisms/IncompatibleModule/IncompatibleModuleTakeover.tsx
+++ app/src/organisms/IncompatibleModule/IncompatibleModuleTakeover.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { createPortal } from 'react-dom'
 import { IncompatibleModuleODDModalBody } from './IncompatibleModuleODDModalBody'
+import { IncompatibleModuleDesktopModalBody } from './IncompatibleModuleDesktopModalBody'
 import { getTopPortalEl, getModalPortalEl } from '../../App/portal'
 import { useIncompatibleModulesAttached } from './hooks'
 
@@ -8,26 +9,32 @@ const POLL_INTERVAL_MS = 5000
 
 export interface IncompatibleModuleTakeoverProps {
   isOnDevice: boolean
+  robotName?: string
 }
 
 export function IncompatibleModuleTakeover({
   isOnDevice,
+  robotName,
 }: IncompatibleModuleTakeoverProps): JSX.Element {
   const incompatibleModules = useIncompatibleModulesAttached({
     refetchInterval: POLL_INTERVAL_MS,
   })
   return (
     <>
-      {incompatibleModules.length !== 0 ? (
-        isOnDevice ? (
-          createPortal(
-            <IncompatibleModuleODDModalBody modules={incompatibleModules} />,
-            getTopPortalEl()
-          )
-        ) : (
-          <></>
-        )
-      ) : null}
+      {incompatibleModules.length !== 0
+        ? isOnDevice
+          ? createPortal(
+              <IncompatibleModuleODDModalBody modules={incompatibleModules} />,
+              getTopPortalEl()
+            )
+          : createPortal(
+              <IncompatibleModuleDesktopModalBody
+                modules={incompatibleModules}
+                robotName={robotName ?? ''}
+              />,
+              getModalPortalEl()
+            )
+        : null}
     </>
   )
 }
--- app/src/organisms/IncompatibleModule/__tests__/IncompatibleModuleTakeover.test.tsx
+++ app/src/organisms/IncompatibleModule/__tests__/IncompatibleModuleTakeover.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { screen, findByText } from '@testing-library/react'
 import { describe, it, beforeEach, afterEach, expect, vi } from 'vitest'
 import { when } from 'vitest-when'
 import '@testing-library/jest-dom/vitest'
@@ -7,31 +7,50 @@ import { renderWithProviders } from '../../../__testing-utils__'
 import { i18n } from '../../../i18n'
 import { IncompatibleModuleTakeover } from '../IncompatibleModuleTakeover'
 import { IncompatibleModuleODDModalBody } from '../IncompatibleModuleODDModalBody'
+import { IncompatibleModuleDesktopModalBody } from '../IncompatibleModuleDesktopModalBody'
 import { useIncompatibleModulesAttached } from '../hooks'
 import type { AttachedModule } from '@opentrons/api-client'
-import { PortalRoot, MODAL_PORTAL_ID } from '../../../App/portal'
+import {
+  PortalRoot,
+  TopPortalRoot,
+  MODAL_PORTAL_ID,
+  TOP_PORTAL_ID,
+} from '../../../App/portal'
 import * as Fixtures from '../__fixtures__'
 
 vi.mock('../hooks')
 vi.mock('../IncompatibleModuleODDModalBody')
+vi.mock('../IncompatibleModuleDesktopModalBody')
 
 const getRenderer = (incompatibleModules: AttachedModule[]) => {
   when(useIncompatibleModulesAttached)
     .calledWith(expect.anything())
     .thenReturn(incompatibleModules)
   vi.mocked(IncompatibleModuleODDModalBody).mockReturnValue(
-    <div>TEST ELEMENT</div>
+    <div>TEST ELEMENT ODD</div>
+  )
+  vi.mocked(IncompatibleModuleDesktopModalBody).mockReturnValue(
+    <div>TEST ELEMENT DESKTOP</div>
   )
   return (props: React.ComponentProps<typeof IncompatibleModuleTakeover>) => {
-    return renderWithProviders(
+    const [rendered] = renderWithProviders(
       <>
         <PortalRoot />
+        <TopPortalRoot />
         <IncompatibleModuleTakeover {...(props as any)} />
       </>,
       {
         i18nInstance: i18n,
       }
-    )[0]
+    )
+    rendered.rerender(
+      <>
+        <PortalRoot />
+        <TopPortalRoot />
+        <IncompatibleModuleTakeover {...(props as any)} />
+      </>
+    )
+    return rendered
   }
 }
 
@@ -44,15 +63,32 @@ describe('IncompatibleModuleTakeover', () => {
   afterEach(() => {
     vi.restoreAllMocks()
   })
-
-  it('should render nothing when no incompatible modules are attached', () => {
-    getRenderer([])(props)
-    expect(screen.findByTestId(MODAL_PORTAL_ID)).resolves.toBeEmptyDOMElement()
+  ;['desktop', 'odd'].forEach(target => {
+    it(`should render nothing on ${target} when no incompatible modules are attached`, () => {
+      getRenderer([])({ ...props, isOnDevice: target === 'odd' })
+      expect(screen.findByTestId(TOP_PORTAL_ID)).resolves.toBeEmptyDOMElement()
+      expect(
+        screen.findByTestId(MODAL_PORTAL_ID)
+      ).resolves.toBeEmptyDOMElement()
+      expect(screen.queryByText(/TEST ELEMENT/)).toBeNull()
+    })
   })
 
-  it('should render the module body when incompatible modules are attached', async () => {
-    getRenderer(Fixtures.oneIncompatibleModule as any)(props)
+  it('should render the modal body on odd when incompatible modules are attached', async () => {
+    getRenderer(Fixtures.oneIncompatibleModule as any)({
+      ...props,
+      isOnDevice: true,
+    })
+    const container = await screen.findByTestId(TOP_PORTAL_ID)
+    await findByText(container, 'TEST ELEMENT ODD')
+  })
+
+  it('should render the modal body on desktop when incompatible modules are attached', async () => {
+    getRenderer(Fixtures.oneIncompatibleModule as any)({
+      ...props,
+      isOnDevice: false,
+    })
     const container = await screen.findByTestId(MODAL_PORTAL_ID)
-    await screen.findByText('TEST ELEMENT', {}, { container })
+    await findByText(container, 'TEST ELEMENT DESKTOP')
   })
 })
