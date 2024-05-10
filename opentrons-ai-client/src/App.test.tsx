5268be18e3e0b9a480d3f2a69df38204c4e0ab59
koji <kk2796@nyu.edu>
2024-05-10 15:24:31 -0400

    update test for useAuth0

--- opentrons-ai-client/src/App.test.tsx
+++ opentrons-ai-client/src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { screen } from '@testing-library/react'
 import { describe, it, vi, beforeEach } from 'vitest'
+import { useAuth0 } from '@auth0/auth0-react'
 
 import { renderWithProviders } from './__testing-utils__'
 import { SidePanel } from './molecules/SidePanel'
@@ -8,8 +9,11 @@ import { ChatContainer } from './organisms/ChatContainer'
 
 import { App } from './App'
 
+vi.mock('@auth0/auth0-react')
 vi.mock('./molecules/SidePanel')
 vi.mock('./organisms/ChatContainer')
+
+const mockLogout = vi.fn()
 
 const render = (): ReturnType<typeof renderWithProviders> => {
   return renderWithProviders(<App />)
@@ -17,6 +21,10 @@ const render = (): ReturnType<typeof renderWithProviders> => {
 
 describe('App', () => {
   beforeEach(() => {
+    vi.mocked(useAuth0).mockReturnValue({
+      isAuthenticated: true,
+      logout: mockLogout,
+    })
     vi.mocked(SidePanel).mockReturnValue(<div>mock SidePanel</div>)
     vi.mocked(ChatContainer).mockReturnValue(<div>mock ChatContainer</div>)
   })
