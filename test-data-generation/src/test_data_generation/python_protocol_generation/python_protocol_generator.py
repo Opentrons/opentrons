e8c8987b5ac2fa59ed2c6495edd0a0779dceb2f2
Derek Maggio <derek.j.maggio@gmail.com>
2024-05-10 13:45:26 -0700

    chore: use ProtocolConfiguration and add self._explicit_calls

--- test-data-generation/src/test_data_generation/python_protocol_generation/python_protocol_generator.py
+++ test-data-generation/src/test_data_generation/python_protocol_generation/python_protocol_generator.py
@@ -1,46 +1,40 @@
 """Module for generating Python protocol code from a deck configuration."""
 
 import ast
-import astor  # type: ignore
 import typing
-from .ast_helpers import CanGenerateAST
-from test_data_generation.deck_configuration.datashapes import (
-    DeckConfiguration,
-    PossibleSlotContents as PSC,
+
+import astor  # type: ignore
+
+from test_data_generation.datashapes import (
+    PipetteConfiguration,
+)
+from test_data_generation.datashapes import (
+    DeckConfigurationFixtures as DCF,
 )
 
-from .generation_phases.setup_phase import (
-    create_protocol_context_run_function,
-    import_protocol_context,
-    create_requirements_dict,
-)
-from .generation_phases.load_phase import (
-    create_deck_slot_load_statements,
-    create_pipette_load_statements,
-)
-from .generation_phases.call_phase import create_calls_to_loaded_entities
-from .util import PipetteConfiguration, PipetteNames
+from ..constants import PipetteNames
+from . import ast_helpers as ast_h
+from .generation_phases import setup_phase, load_phase, call_phase
+
+from .protocol_configuration import ProtocolConfiguration
 
 
 class PythonProtocolGenerator:
     """Class for generating Python protocol code from a deck configuration."""
 
-    def __init__(
-        self,
-        deck_configuration: DeckConfiguration,
-        api_version: str,
-    ) -> None:
+    def __init__(self, protocol_configuration: ProtocolConfiguration) -> None:
         """Initialize the PythonProtocolGenerator.
 
         Call boilerplate functions to set up the protocol.
         """
-        self._top_level_statements: typing.List[CanGenerateAST] = []
-        self._deck_configuration = deck_configuration
-
+        self._top_level_statements: typing.List[ast_h.CanGenerateAST] = []
+        self._protocol_configuration = protocol_configuration
         self._top_level_statements.extend(
             [
-                import_protocol_context(),
-                create_requirements_dict("OT-3", api_version),
+                setup_phase.import_protocol_context(),
+                setup_phase.create_requirements_dict(
+                    "OT-3", self._protocol_configuration.api_version
+                ),
             ]
         )
 
@@ -48,8 +42,8 @@ class PythonProtocolGenerator:
 
     def _choose_pipettes(self) -> PipetteConfiguration:
         """Choose the pipettes to use based on the deck configuration."""
-        if self._deck_configuration.d.col3.contents.is_one_of(
-            [PSC.WASTE_CHUTE_NO_COVER, PSC.STAGING_AREA_WITH_WASTE_CHUTE_NO_COVER]
+        if self._protocol_configuration.deck_configuration.d.col3.contents.is_one_of(
+            [DCF.WASTE_CHUTE_NO_COVER, DCF.STAGING_AREA_WITH_WASTE_CHUTE_NO_COVER]
         ):
             return PipetteConfiguration(
                 left=PipetteNames.NINETY_SIX_CHANNEL, right=None
@@ -59,29 +53,43 @@ class PythonProtocolGenerator:
                 left=PipetteNames.SINGLE_CHANNEL, right=PipetteNames.MULTI_CHANNEL
             )
 
+    def _generate_run_function_statements(
+        self,
+    ) -> typing.Sequence[ast.stmt]:
+        pipette_load_statements = load_phase.create_pipette_load_statements(
+            self._pipettes
+        )
+        deck_slot_load_statements = load_phase.create_deck_slot_load_statements(
+            self._protocol_configuration.deck_configuration.slots,
+            self._protocol_configuration.explicit_calls,
+            self._protocol_configuration.allow_overlapping_calls,
+        )
+        calls_to_loaded_entities = call_phase.create_calls_to_loaded_entities(
+            pipette_load_statements + deck_slot_load_statements
+        )
+
+        return [
+            statement.generate_ast()
+            for statement in pipette_load_statements
+            + deck_slot_load_statements
+            + calls_to_loaded_entities
+        ]
+
     def generate_protocol(self) -> str:
         """Generate the Python protocol code."""
         module = ast.Module(
             body=[statement.generate_ast() for statement in self._top_level_statements]
         )
-        run_function = create_protocol_context_run_function().generate_ast()
-        pipette_load_statements = create_pipette_load_statements(self._pipettes)
-        deck_slot_load_statements = create_deck_slot_load_statements(
-            self._deck_configuration.slots
-        )
+        run_function = setup_phase.create_protocol_context_run_function().generate_ast()
 
-        calls_to_loaded_entities = create_calls_to_loaded_entities(
-            pipette_load_statements + deck_slot_load_statements
-        )
+        run_function.body.extend(self._generate_run_function_statements())
 
-        statements_to_make = (
-            pipette_load_statements
-            + deck_slot_load_statements
-            + calls_to_loaded_entities
-        )
-
-        for statement in statements_to_make:
-            run_function.body.append(statement.generate_ast())
         module.body.append(run_function)
 
-        return str(astor.to_source(module))
+        if self._protocol_configuration.add_deck_configuration_comment:
+            deck_config_comment = (
+                self._protocol_configuration.deck_configuration.comment_string()
+            )
+        else:
+            deck_config_comment = ""
+        return deck_config_comment + str(astor.to_source(module))
