7eb4fe125aa34ce31bec8fd8dabdb4819804e8e7
Brian Cooper <brian@opentrons.com>
2024-05-14 14:29:13 -0400

    fix last sneaky type import in pd file data

--- protocol-designer/src/configureStore.ts
+++ protocol-designer/src/configureStore.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { trackEventMiddleware } from './analytics/middleware'
