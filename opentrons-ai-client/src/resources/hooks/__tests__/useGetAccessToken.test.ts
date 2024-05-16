0d2b5ffe29a0fcabc93347835b92419af41bbec4
koji <kk2796@nyu.edu>
2024-05-15 23:49:27 -0400

    add test for useGetAccessToken

---
+++ opentrons-ai-client/src/resources/hooks/__tests__/useGetAccessToken.test.ts
@@ -0,0 +1,48 @@
+import { describe, it, vi, expect } from 'vitest'
+import { renderHook } from '@testing-library/react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useGetAccessToken } from '../useGetAccessToken'
+import type { Mock } from 'vitest'
+
+vi.mock('@auth0/auth0-react')
+
+describe('useGetAccessToken', () => {
+  it('should return access token when getAccessTokenSilently is successful', async () => {
+    const mockGetAccessTokenSilently = vi
+      .fn()
+      .mockResolvedValue('mockAccessToken')
+    ;(useAuth0 as Mock).mockReturnValue({
+      getAccessTokenSilently: mockGetAccessTokenSilently,
+    })
+
+    const { result } = renderHook(() => useGetAccessToken())
+    const { getAccessToken } = result.current
+    const accessToken = getAccessToken()
+
+    expect(mockGetAccessTokenSilently).toHaveBeenCalledWith({
+      authorizationParams: {
+        audience: 'sandbox-ai-api',
+      },
+    })
+    expect(await accessToken).toBe('mockAccessToken')
+  })
+
+  it('should throw error when getAccessTokenSilently fails', async () => {
+    const mockGetAccessTokenSilently = vi
+      .fn()
+      .mockRejectedValue(new Error('mockError'))
+    ;(useAuth0 as Mock).mockReturnValue({
+      getAccessTokenSilently: mockGetAccessTokenSilently,
+    })
+    const { result } = renderHook(() => useGetAccessToken())
+    const { getAccessToken } = result.current
+    const accessToken = getAccessToken()
+
+    expect(mockGetAccessTokenSilently).toHaveBeenCalledWith({
+      authorizationParams: {
+        audience: 'sandbox-ai-api',
+      },
+    })
+    await expect(accessToken).rejects.toThrow('mockError')
+  })
+})
