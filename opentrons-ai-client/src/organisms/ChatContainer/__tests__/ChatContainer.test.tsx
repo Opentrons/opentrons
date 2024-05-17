8591123ac47e7f66dad86794f01cf8f2f879f075
koji <kk2796@nyu.edu>
2024-05-17 16:27:21 -0400

    fix(opentrons-ai-client): fix layout issues
    fix layout issues
    
    close AUTH-405

--- opentrons-ai-client/src/organisms/ChatContainer/__tests__/ChatContainer.test.tsx
+++ opentrons-ai-client/src/organisms/ChatContainer/__tests__/ChatContainer.test.tsx
@@ -4,11 +4,11 @@ import { describe, it, vi, beforeEach } from 'vitest'
 import { renderWithProviders } from '../../../__testing-utils__'
 import { i18n } from '../../../i18n'
 import { PromptGuide } from '../../../molecules/PromptGuide'
-import { InputPrompt } from '../../../molecules/InputPrompt'
+import { ChatFooter } from '../../../molecules/ChatFooter'
 import { ChatContainer } from '../index'
 
 vi.mock('../../../molecules/PromptGuide')
-vi.mock('../../../molecules/InputPrompt')
+vi.mock('../../../molecules/ChatFooter')
 
 const render = (): ReturnType<typeof renderWithProviders> => {
   return renderWithProviders(<ChatContainer />, {
@@ -19,17 +19,13 @@ const render = (): ReturnType<typeof renderWithProviders> => {
 describe('ChatContainer', () => {
   beforeEach(() => {
     vi.mocked(PromptGuide).mockReturnValue(<div>mock PromptGuide</div>)
-    vi.mocked(InputPrompt).mockReturnValue(<div>mock InputPrompt</div>)
+    vi.mocked(ChatFooter).mockReturnValue(<div>mock ChatFooter</div>)
   })
+
   it('should render prompt guide and text', () => {
     render()
     screen.getByText('OpentronsAI')
     screen.getByText('mock PromptGuide')
-    screen.getByText('mock InputPrompt')
-    screen.getByText(
-      'OpentronsAI can make mistakes. Review your protocol before running it on an Opentrons robot.'
-    )
+    screen.getByText('mock ChatFooter')
   })
-
-  // ToDo (kk:04/16/2024) Add more test cases
 })
