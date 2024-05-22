470169d72fb3630c1d6f4ce96e52bc16cbadd550
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2024-05-22 16:47:23 -0400

    update splash designs

--- app/src/organisms/ErrorRecoveryFlows/RunPausedSplash.tsx
+++ app/src/organisms/ErrorRecoveryFlows/RunPausedSplash.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { useTranslation } from 'react-i18next'
 
 import {
   Flex,
-  Btn,
   Icon,
   JUSTIFY_CENTER,
   ALIGN_CENTER,
@@ -15,9 +14,12 @@ import {
   TYPOGRAPHY,
   OVERFLOW_WRAP_BREAK_WORD,
   DISPLAY_FLEX,
+  JUSTIFY_SPACE_BETWEEN,
 } from '@opentrons/components'
 
 import type { FailedCommand } from './types'
+import { getErrorKind, useErrorMessage, useErrorName } from './utils'
+import { LargeButton } from '../../atoms/buttons'
 
 interface RunPausedSplashProps {
   onClick: () => void
@@ -29,19 +31,15 @@ export function RunPausedSplash({
   failedCommand,
 }: RunPausedSplashProps): JSX.Element {
   const { t } = useTranslation('error_recovery')
-
-  const errorType = failedCommand?.error?.errorType
-
-  let subText: string | null
-  switch (errorType) {
-    default:
-      subText = 'test' ?? null
-  }
+  const errorKind = getErrorKind(failedCommand?.error?.errorType)
+  const title = useErrorName(errorKind)
+  const subText = useErrorMessage(errorKind)
 
   // TODO(jh 05-22-24): The hardcoded Z-indexing is non-ideal but must be done to keep the splash page above
-  // several components in the RunningProtocol page. Investigate why these components have seemingly arbitrary zIndex values.
+  // several components in the RunningProtocol page. Investigate why these components have seemingly arbitrary zIndex values
+  // and devise a better solution to layering modals.
   return (
-    <Btn
+    <Flex
       display={DISPLAY_FLEX}
       height="100vh"
       width="100%"
@@ -49,22 +47,36 @@ export function RunPausedSplash({
       alignItems={ALIGN_CENTER}
       position={POSITION_ABSOLUTE}
       flexDirection={DIRECTION_COLUMN}
-      gridGap={SPACING.spacing40}
-      padding={SPACING.spacing120}
-      backgroundColor={COLORS.grey50}
-      onClick={onClick}
+      gridGap={SPACING.spacing60}
+      padding={SPACING.spacing40}
+      backgroundColor={COLORS.red50}
       zIndex={5}
     >
       <SplashFrame>
         <Flex gridGap={SPACING.spacing32} alignItems={ALIGN_CENTER}>
           <Icon name="ot-alert" size="4.5rem" color={COLORS.white} />
-          <SplashHeader>{t('run_paused')}</SplashHeader>
+          <SplashHeader>{title}</SplashHeader>
         </Flex>
         <Flex width="49rem" justifyContent={JUSTIFY_CENTER}>
           <SplashBody>{subText}</SplashBody>
         </Flex>
       </SplashFrame>
-    </Btn>
+      <Flex justifyContent={JUSTIFY_SPACE_BETWEEN} gridGap={SPACING.spacing16}>
+        <LargeButton
+          onClick={() => null}
+          buttonText={t('cancel_run')}
+          css={CANCEL_RUN_BTN_STYLE}
+          iconName={'remove'}
+          iconColorOverride={COLORS.red50}
+        />
+        <LargeButton
+          onClick={() => null}
+          buttonText={t('launch_recovery_mode')}
+          css={LAUNCH_RECOVERY_BTN_STYLE}
+          iconName={'recovery'}
+        />
+      </Flex>
+    </Flex>
   )
 }
 
@@ -96,4 +108,49 @@ const SplashFrame = styled(Flex)`
   justify-content: ${JUSTIFY_CENTER};
   align-items: ${ALIGN_CENTER};
   grid-gap: ${SPACING.spacing40};
+  padding: ${SPACING.spacing24};
+  padding-bottom: 0px;
+`
+
+const SHARED_BUTTON_STYLE = `
+width: 464px;
+height: 216px;
+`
+
+const LAUNCH_RECOVERY_BTN_STYLE = css`
+  ${SHARED_BUTTON_STYLE};
+  background-color: transparent;
+  border: 4px solid ${COLORS.white};
+
+  &:hover {
+    background-color: transparent;
+    border: 4px solid ${COLORS.white};
+  }
+  &:active {
+    background-color: transparent;
+    border: 4px solid ${COLORS.white};
+  }
+  &:focus-visible {
+    background-color: transparent;
+    border: 4px solid ${COLORS.white};
+  }
+`
+
+const CANCEL_RUN_BTN_STYLE = css`
+  ${SHARED_BUTTON_STYLE};
+  color: ${COLORS.red50};
+  background-color: ${COLORS.white};
+
+  &:hover {
+    color: ${COLORS.red50};
+    background-color: ${COLORS.white};
+  }
+  &:active {
+    color: ${COLORS.red50};
+    background-color: ${COLORS.white};
+  }
+  &:focus-visible {
+    color: ${COLORS.red50};
+    background-color: ${COLORS.white};
+  }
 `
--- app/src/organisms/ErrorRecoveryFlows/utils.ts
+++ app/src/organisms/ErrorRecoveryFlows/utils.ts
@@ -76,6 +76,7 @@ export function useErrorName(errorKind: ErrorKind): string {
 export function useErrorMessage(errorKind: ErrorKind): string {
   const { t } = useTranslation('error_recovery')
 
+  // TOME: Need a new utility function here to get the splash screen subtext.
   switch (errorKind) {
     default:
       return t('general_error_message')
