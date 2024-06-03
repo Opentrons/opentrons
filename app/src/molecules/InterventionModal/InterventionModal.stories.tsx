8fb0cf014133f6f2e1065d4d494d0465097b3455
koji <koji.kanao@opentrons.com>
2024-06-03 14:17:35 -0400

    fix(app): add DropdownMenu stories (#15283)
    * fix(app): add DropdownMenu stories

--- app/src/molecules/InterventionModal/InterventionModal.stories.tsx
+++ app/src/molecules/InterventionModal/InterventionModal.stories.tsx
@@ -1,31 +1,51 @@
 import * as React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import { StyledText } from '@opentrons/components'
+import { configReducer } from '../../redux/config/reducer'
 import { InterventionModal as InterventionModalComponent } from './'
-import type { Story, Meta } from '@storybook/react'
 
-export default {
+import type { Store } from 'redux'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const dummyConfig = {
+  config: {
+    isOnDevice: false,
+  },
+} as any
+const store: Store<any> = createStore(configReducer, dummyConfig)
+
+const meta: Meta<typeof InterventionModalComponent> = {
   title: 'App/Molecules/InterventionModal',
   component: InterventionModalComponent,
-} as Meta
+  decorators: [
+    Story => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
+}
+export default meta
 
-const Template: Story<
-  React.ComponentProps<typeof InterventionModalComponent>
-> = args => <InterventionModalComponent {...args} />
+type Story = StoryObj<typeof InterventionModalComponent>
 
-export const ErrorIntervention = Template.bind({})
-ErrorIntervention.args = {
-  robotName: 'Otie',
-  type: 'error',
-  heading: <StyledText as="h3">Oh no, an error!</StyledText>,
-  iconName: 'alert-circle',
-  children: <StyledText as="p">Heres some error content</StyledText>,
+export const ErrorIntervention: Story = {
+  args: {
+    type: 'error',
+    titleHeading: <StyledText as="h3">Oh no, an error!</StyledText>,
+    iconName: 'alert-circle',
+    children: <StyledText as="p">{"Here's some error content"}</StyledText>,
+  },
 }
 
-export const InterventionRequiredIntervention = Template.bind({})
-InterventionRequiredIntervention.args = {
-  robotName: 'Otie',
-  type: 'intervention-required',
-  heading: <StyledText as="h3">Looks like theres something to do</StyledText>,
-  children: <StyledText as="p">Youve got to intervene!</StyledText>,
+export const InterventionRequiredIntervention: Story = {
+  args: {
+    type: 'intervention-required',
+    titleHeading: (
+      <StyledText as="h3">Looks like theres something to do</StyledText>
+    ),
+    children: <StyledText as="p">{"You've got to intervene!"}</StyledText>,
+  },
 }
