1d34fafaa570fcbebb75368d09ae7b32b4a19dd2
Brian Cooper <brian@opentrons.com>
2024-06-04 17:12:31 -0400

    app shell odd start

--- app-shell-odd/src/config/index.ts
+++ app-shell-odd/src/config/index.ts
@@ -122,8 +122,8 @@ export function getOverrides(path?: string): unknown {
 
 export function getConfig<P extends keyof Config>(path: P): Config[P]
 export function getConfig(): Config
-export function getConfig(path?: any): any {
-  const result = store().get(path)
+export function getConfig(path?: string): any {
+  const result = path == null ? null : store().get(path)
   const over = getOverrides(path)
 
   if (over != null) {
