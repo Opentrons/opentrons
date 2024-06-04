2578195b09b70699fc728a2d9ab97cc367dbd62b
wweiye <275241708@qq.com>
2024-06-04 15:41:29 +0800

    fix fixture fail

--- hardware-testing/hardware_testing/production_qc/pipette_assembly_qc_ot3/__main__.py
+++ hardware-testing/hardware_testing/production_qc/pipette_assembly_qc_ot3/__main__.py
@@ -447,7 +447,10 @@ async def _move_to_fixture(api: OT3API, mount: OT3Mount) -> None:
     )
 
     #Z down 0.5
-    await api.move_rel(mount, Point(z=-0.6))
+    if "single" in pipptype[OT3Mount.LEFT]['name']:
+        await api.move_rel(mount, Point(z=-0.4))
+    elif "multi" in pipptype[OT3Mount.LEFT]['name']:
+        await api.move_rel(mount, Point(z=0.3))
     CALIBRATED_LABWARE_LOCATIONS.fixture = await api.gantry_position(mount)
 
 
