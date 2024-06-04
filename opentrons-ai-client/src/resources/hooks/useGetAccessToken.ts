b480ebb664d9cd8583485d6c1eb35bda3cafc3d9
koji <kk2796@nyu.edu>
2024-06-04 13:56:10 -0400

    feat(opentrons-ai-client): add process env to ai-client for prod
    add process env to ai-client for prod
    
    close AUTH-

--- opentrons-ai-client/src/resources/hooks/useGetAccessToken.ts
+++ opentrons-ai-client/src/resources/hooks/useGetAccessToken.ts
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { AUTH0_AUDIENCE } from '../constants'
+import { PROD_AUTH0_AUDIENCE, STAGING_AUTH0_AUDIENCE } from '../constants'
 
 interface UseGetAccessTokenResult {
   getAccessToken: () => Promise<string>
@@ -7,12 +7,16 @@ interface UseGetAccessTokenResult {
 
 export const useGetAccessToken = (): UseGetAccessTokenResult => {
   const { getAccessTokenSilently } = useAuth0()
+  const auth0Audience =
+    process.env.NODE_ENV === 'production'
+      ? PROD_AUTH0_AUDIENCE
+      : STAGING_AUTH0_AUDIENCE
 
   const getAccessToken = async (): Promise<string> => {
     try {
       const accessToken = await getAccessTokenSilently({
         authorizationParams: {
-          audience: AUTH0_AUDIENCE,
+          audience: auth0Audience,
         },
       })
       return accessToken
