1c8ce2974e185321af4305134775901025463c51
Derek Maggio <derek.j.maggio@gmail.com>
2024-06-07 06:28:08 -0700

    chore: expose externally used deps

--- performance-metrics/src/performance_metrics/__init__.py
+++ performance-metrics/src/performance_metrics/__init__.py
@@ -1,5 +1,7 @@
 """Opentrons performance metrics library."""
 
 from .robot_context_tracker import RobotContextTracker
+from .data_definitions import RobotContextState
+from .types import SupportsTracking
 
-__all__ = ["RobotContextTracker"]
+__all__ = ["RobotContextTracker", "RobotContextState", "SupportsTracking"]
