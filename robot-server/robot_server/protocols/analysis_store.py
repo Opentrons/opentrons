6fb8adf1dbdf493bda559cf9b863edbe09a91948
Sanniti Pimpley <sanni-t@users.noreply.github.com>
2024-06-10 12:20:47 -0400

    feat(robot-server): show run time parameters in pending analysis (#15229)
    Closes AUTH-282
    
    # Overview
    
    Follow-up to #15217 
    
    This PR changes a *pending analysis'* `AnalysisSummary` to include the
    run time parameters (with overrides).
    In order to do this, we will now be parsing the RTPs before analysis
    response is created. This is a very quick process and shouldn't cause
    any perceptible delays in the response of neither `POST /protocols` nor
    `POST /runs/.../analyses` endpoints, as long as recommended python
    protocol guidelines are followed.
    
    # Test Plan
    
    - [ ] Upload a protocol with run time parameters, verify that the
    response contains run time parameter definitions along with the values
    specified in the request
    - [ ] Send a `POST` request on `/protocols/{protocolId}/analyses`
    endpoint with different RTP values, or by specifying the
    `forceReanalysis` flag, and verify that the newly pending analysis also
    contains (updated) RTPs
    - [ ] Verify that pending summaries in `GET` endpoints also contain RTPs
    - [ ] Verify that summaries of completed analyses don't expose RTPs,
    while RTPs are available in the full analyses.
    
    # Changelog
    
    - Main change is the introduction of `AnalysesManager`. The analyses
    manager is responsible for creating a protocol analyzer per analysis,
    extracting the run time parameters from the runner and starting the
    analysis.
    - To facilitate this extraction of run time parameters, the
    `ProtocolAnalyzer` has been divided into two functions- one that loads
    the runner and other that runs the runner.
    - Updated the router to use `AnalysesManager` for starting analyses
    instead of directly interacting with `ProtocolAnalyzer`.
    
    # Review requests
    
    - Does the new architecture of `AnalysesManager` & updated
    `ProtocolAnalyzer` makes sense.
    - This PR doesn't expose the run time parameters in analysis summaries
    of completed analyses because of two reasons- (1) as opposed to the RTPs
    of a pending analysis, the completed analysis isn't guaranteed to be
    cached in memory. So we would need to read the completed analysis from
    database, de-serialize it and then return it. This would be made easier
    when we start storing RTPs in their own database table, but we're not
    there yet. (2) I'm not sure if it is needed. A completed analysis
    already contains the RTPs.
    Would like to get others' opinions on this.
    - Usual code review
    
    # Risk assessment
    
    - Medium. It changes a core part of protocol analysis but the end effect
    is a small change to the API. As long as tests are passing, it shouldn't
    cause any problems.
    - Small risk of analysis behavior change for badly written protocols as
    mentioned in
    [this](https://github.com/Opentrons/opentrons/pull/15229#discussion_r1619075927)
    comment.

--- robot-server/robot_server/protocols/analysis_store.py
+++ robot-server/robot_server/protocols/analysis_store.py
@@ -1,16 +1,13 @@
 """Protocol analysis storage."""
 from __future__ import annotations
 
-
+import sqlalchemy
 from logging import getLogger
 from typing import Dict, List, Optional
-
-from opentrons.protocol_engine.types import RunTimeParameter
 from typing_extensions import Final
+
 from opentrons_shared_data.robot.dev_types import RobotType
-
-import sqlalchemy
-
+from opentrons.protocol_engine.types import RunTimeParameter, RunTimeParamValuesType
 from opentrons.protocol_engine import (
     Command,
     ErrorOccurrence,
@@ -19,8 +16,6 @@ from opentrons.protocol_engine import (
     LoadedModule,
     Liquid,
 )
-from opentrons.protocol_engine.types import RunTimeParamValuesType
-
 from .analysis_models import (
     AnalysisSummary,
     ProtocolAnalysis,
@@ -116,7 +111,11 @@ class AnalysisStore:
             current_analyzer_version=_CURRENT_ANALYZER_VERSION,
         )
 
-    def add_pending(self, protocol_id: str, analysis_id: str) -> AnalysisSummary:
+    def add_pending(
+        self,
+        protocol_id: str,
+        analysis_id: str,
+    ) -> PendingAnalysis:
         """Add a new pending analysis to the store.
 
         Args:
@@ -129,10 +128,10 @@ class AnalysisStore:
         Returns:
             A summary of the just-added analysis.
         """
-        new_pending_analysis = self._pending_store.add(
-            protocol_id=protocol_id, analysis_id=analysis_id
+        return self._pending_store.add(
+            protocol_id=protocol_id,
+            analysis_id=analysis_id,
         )
-        return _summarize_pending(pending_analysis=new_pending_analysis)
 
     async def update(
         self,
@@ -243,6 +242,9 @@ class AnalysisStore:
         completed_analysis_ids = self._completed_store.get_ids_by_protocol(
             protocol_id=protocol_id
         )
+        # TODO (spp, 2024-06-05): populate runTimeParameters in the completed analysis summaries once
+        #  we start saving RTPs to their own table. Currently, fetching RTPs from a
+        #  completed analysis requires de-serializing the full analysis resource.
         completed_analysis_summaries = [
             AnalysisSummary.construct(id=analysis_id, status=AnalysisStatus.COMPLETED)
             for analysis_id in completed_analysis_ids
@@ -316,6 +318,11 @@ class AnalysisStore:
         parameters in the analysis use default values.
         """
         if analysis_summary.status == AnalysisStatus.PENDING:
+            # TODO: extract defaults and values from pending analysis now that they're available
+            #   If the pending analysis RTPs match the current RTPs, do nothing(?).
+            #   If the pending analysis RTPs DO NOT match the current RTPs, raise the
+            #   AnalysisIsPending error. Eventually, we might allow either canceling the
+            #   pending analysis or starting another analysis when there's already a pending one.
             raise AnalysisIsPendingError(analysis_summary.id)
 
         rtp_values_and_defaults_in_last_analysis = (
@@ -371,13 +378,19 @@ class _PendingAnalysisStore:
         self._analysis_ids_by_protocol_id: Dict[str, str] = {}
         self._protocol_ids_by_analysis_id: Dict[str, str] = {}
 
-    def add(self, protocol_id: str, analysis_id: str) -> PendingAnalysis:
+    def add(
+        self,
+        protocol_id: str,
+        analysis_id: str,
+    ) -> PendingAnalysis:
         """Add a new pending analysis and associate it with the given protocol."""
         assert (
             protocol_id not in self._analysis_ids_by_protocol_id
         ), "Protocol must not already have a pending analysis."
 
-        new_pending_analysis = PendingAnalysis.construct(id=analysis_id)
+        new_pending_analysis = PendingAnalysis.construct(
+            id=analysis_id,
+        )
 
         self._analyses_by_id[analysis_id] = new_pending_analysis
         self._analysis_ids_by_protocol_id[protocol_id] = analysis_id
