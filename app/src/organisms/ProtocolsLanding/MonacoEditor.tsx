cfdd3a4f067279098eabd9a39b9cc87e93f6b1c4
koji <kojikoji@skiff.com>
2024-06-11 19:51:11 -0400

    chore: remove workaround from workflows
    remove workaround from workflows

---
+++ app/src/organisms/ProtocolsLanding/MonacoEditor.tsx
@@ -0,0 +1,29 @@
+import { VFC, useRef, useState, useEffect } from 'react';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import styles from './Editor.module.css';
+
+interface MonacoEditorProps {
+  code: string
+}
+
+export function MonacoEditor({code }:MonacoEditorProps ):JSX.Element {
+	const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+	const monacoEl = useRef(null);
+
+	useEffect(() => {
+		if (monacoEl) {
+			setEditor((editor) => {
+				if (editor) return editor;
+
+				return monaco.editor.create(monacoEl.current!, {
+					value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+					language: 'typescript'
+				});
+			});
+		}
+
+		return () => editor?.dispose();
+	}, [monacoEl.current]);
+
+	return <div className={styles.Editor} ref={monacoEl}></div>;
+};
