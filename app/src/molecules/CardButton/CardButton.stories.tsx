712d692c24239cafec31d0369291698e9a1ecc23
koji <kojikoji@skiff.com>
2024-06-11 20:38:21 -0400

    fix format via prettier command

--- app/src/molecules/CardButton/CardButton.stories.tsx
+++ app/src/molecules/CardButton/CardButton.stories.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react'
-import { Flex, ICON_DATA_BY_NAME, SPACING, VIEWPORT } from '@opentrons/components'
+import {
+  Flex,
+  ICON_DATA_BY_NAME,
+  SPACING,
+  VIEWPORT,
+} from '@opentrons/components'
 import { CardButton as CardButtonComponent } from './index'
 
 import type { Meta, StoryObj } from '@storybook/react'
@@ -18,9 +23,9 @@ const meta: Meta<typeof CardButtonComponent> = {
   },
   decorators: [
     Story => (
-    <Flex marginTop={SPACING.spacing16} width="15.375rem" height="17rem">
-      <Story/>
-    </Flex>
+      <Flex marginTop={SPACING.spacing16} width="15.375rem" height="17rem">
+        <Story />
+      </Flex>
     ),
   ],
 }
@@ -29,11 +34,11 @@ export default meta
 type Story = StoryObj<typeof CardButtonComponent>
 
 export const CardButton: Story = {
-args:{
-  title: 'Header',
-  iconName: 'wifi',
-  description: 'Subtext.',
-  destinationPath: '/app-molecules-cardbutton--primary',
-  disabled: false,
-}
+  args: {
+    title: 'Header',
+    iconName: 'wifi',
+    description: 'Subtext.',
+    destinationPath: '/app-molecules-cardbutton--primary',
+    disabled: false,
+  },
 }
