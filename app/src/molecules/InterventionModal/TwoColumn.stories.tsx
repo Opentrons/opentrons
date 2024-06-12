b6ca6e8e52bd319d44d9901b6e36a6d0b40a3e87
Seth Foster <seth@opentrons.com>
2024-06-12 17:37:56 -0400

    use some nice standin nodes

--- app/src/molecules/InterventionModal/TwoColumn.stories.tsx
+++ app/src/molecules/InterventionModal/TwoColumn.stories.tsx
@@ -2,7 +2,13 @@ import * as React from 'react'
 
 import SuccessIcon from '../../assets/images/icon_success.png'
 
-import { StyledText, Flex, DIRECTION_COLUMN, Box } from '@opentrons/components'
+import {
+  StyledText,
+  Flex,
+  DIRECTION_COLUMN,
+  Box,
+  BORDERS,
+} from '@opentrons/components'
 import { InlineNotification } from '../../atoms/InlineNotification'
 
 import { TwoColumn as TwoColumnComponent } from './'
@@ -10,13 +16,102 @@ import { TwoColumn as TwoColumnComponent } from './'
 import type { Meta, StoryObj } from '@storybook/react'
 
 interface StorybookArgs {
-  freeText: string
-  imageUrl: string[]
-  notificationHeader: string
-  notificationBody: string
-  notificationType: 'alert' | 'error' | 'neutral'
+  leftStandIn: boolean
+  rightStandIn: boolean
+  leftImageUrl?: string[]
+  rightImageUrl?: string[]
+  leftNotificationHeading?: string
+  leftNotificationMessage?: string
+  leftNotificationType: 'alert' | 'error' | 'neutral'
+  rightNotificationHeading?: string
+  rightNotificationMessage?: string
+  rightNotificationType: 'alert' | 'error' | 'neutral'
+  leftText?: string
+  rightText?: string
   containerWidth: number
   containerHeight: number
+}
+
+function StandInContent(): JSX.Element {
+  return (
+    <Box
+      border={'4px dashed #A864FFFF'}
+      borderRadius={BORDERS.borderRadius8}
+      width="207px"
+      height="104px"
+      backgroundColor="#A864FF0A"
+    />
+  )
+}
+
+interface NotificationProps {
+  heading?: string
+  message?: string
+  type?: string
+}
+function Notification({
+  heading,
+  message,
+  type,
+}: NotificationProps): JSX.Element | null {
+  const hasComponent =
+    (heading != null && heading.length > 0) ||
+    (message != null && message.length > 0)
+  return hasComponent ? (
+    <InlineNotification heading={heading} message={message} type={type} />
+  ) : null
+}
+
+interface TextProps {
+  text?: string
+}
+function Text({ text }: TextProps): JSX.Element | null {
+  const hasComponent = text != null && text.length > 0
+  return hasComponent ? <StyledText>{text}</StyledText> : null
+}
+
+interface ImageProps {
+  imageUrl?: string[]
+}
+function Image({ imageUrl }: ImageProps): JSX.Element | null {
+  const hasComponent =
+    imageUrl != null && imageUrl.length > 0 && imageUrl[0].length > 0
+  return hasComponent ? (
+    <img src={new URL(imageUrl[0], import.meta.url).href} width={'100%'} />
+  ) : null
+}
+
+interface SectionBodyProps {
+  text?: string
+  imageUrl?: string[]
+  notificationHeading?: string
+  notificationMessage?: string
+  notificationType: string
+}
+function SectionBody({
+  text,
+  imageUrl,
+  notificationHeading,
+  notificationMessage,
+  notificationType,
+}: SectionBodyProps): JSX.Element | null {
+  return (
+    <Flex flexDirection={DIRECTION_COLUMN}>
+      <Notification
+        heading={notificationHeading}
+        message={notificationMessage}
+        type={notificationType}
+      />
+      <Text text={text} />
+      <Image imageUrl={imageUrl} />
+    </Flex>
+  )
+}
+
+function SectionBodyOrStandIn(
+  props: SectionBodyProps & { standIn: boolean }
+): JSX.Element {
+  return props.standIn ? <StandInContent /> : <SectionBody {...props} />
 }
 
 const meta: Meta<React.ComponentProps<TwoColumnComponent> & StorybookArgs> = {
@@ -29,54 +124,123 @@ const meta: Meta<React.ComponentProps<TwoColumnComponent> & StorybookArgs> = {
       borderWidth={'8px'}
     >
       <TwoColumnComponent>
-        <Flex flexDirection={DIRECTION_COLUMN}>
-          <StyledText>{args.freeText}</StyledText>
-          <InlineNotification
-            type={args.notificationType}
-            heading={args.notificationHeader}
-            message={args.notificationBody}
-          />
-        </Flex>
-        <img
-          src={new URL(args.imageUrl[0], import.meta.url).href}
-          width={'100%'}
+        <SectionBodyOrStandIn
+          standIn={args.leftStandIn}
+          text={args.leftText}
+          imageUrl={args.leftImageUrl}
+          notificationHeading={args.leftNotificationHeading}
+          notificationMessage={args.leftNotificationMessage}
+          notificationType={args.leftNotificationType}
+        />
+        <SectionBodyOrStandIn
+          standIn={args.rightStandIn}
+          text={args.rightText}
+          imageUrl={args.rightImageUrl}
+          notificationHeading={args.rightNotificationHeading}
+          notificationMessage={args.rightNotificationmessage}
+          notificationType={args.rightNotificationType}
         />
       </TwoColumnComponent>
     </Box>
   ),
   argTypes: {
+    leftStandIn: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: true,
+    },
+    rightStandIn: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: true,
+    },
     containerHeight: {
       control: {
         type: 'number',
       },
-      default: 432,
+      defaultValue: 104,
     },
     containerWidth: {
       control: {
         type: 'number',
       },
-      default: 960,
+      defaultValue: 454,
     },
-    freeText: {
+    leftText: {
       control: {
         type: 'text',
       },
+      defaultValue: undefined,
+      if: { arg: 'leftStandIn', truthy: false },
     },
-    imageUrl: {
+    rightText: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: undefined,
+      if: { arg: 'rightStandIn', truthy: false },
+    },
+    leftImageUrl: {
       control: {
         type: 'file',
         accept: '.png',
       },
+      defaultValue: undefined,
+      if: { arg: 'leftStandIn', truthy: false },
     },
-    notificationHeader: {
+    rightImageUrl: {
+      control: {
+        type: 'file',
+        accept: '.png',
+      },
+      defaultValue: undefined,
+      if: { arg: 'rightStandIn', truthy: false },
+    },
+    leftNotificationHeading: {
       control: {
         type: 'text',
       },
+      defaultValue: undefined,
+      if: { arg: 'leftStandIn', truthy: false },
     },
-    notificationBody: {
+    leftNotificationMessage: {
       control: {
         type: 'text',
       },
+      defaultValue: undefined,
+      if: { arg: 'leftStandIn', truthy: false },
+    },
+    leftNotificationType: {
+      control: {
+        type: 'select',
+      },
+      options: ['alert', 'error', 'neutral', undefined],
+      defaultValue: undefined,
+      if: { arg: 'leftStandIn', truthy: false },
+    },
+    rightNotificationHeading: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: undefined,
+      if: { arg: 'rightStandIn', truthy: false },
+    },
+    rightNotificationMessage: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: undefined,
+      if: { arg: 'rightStandIn', truthy: false },
+    },
+    rightNotificationType: {
+      control: {
+        type: 'select',
+      },
+      options: ['alert', 'error', 'neutral', undefined],
+      defaultValue: undefined,
+      if: { arg: 'rightStandIn', truthy: false },
     },
   },
 }
@@ -84,14 +248,37 @@ export default meta
 
 type Story = StoryObj<typeof TwoColumnComponent>
 
+export const TwoColumnWithStandins: Story = {
+  args: {
+    leftStandIn: true,
+    rightStandIn: true,
+    leftText: undefined,
+    rightText: undefined,
+    leftImageUrl: undefined,
+    rightImageUrl: undefined,
+    leftNotificationHeading: undefined,
+    leftNotificationMessage: undefined,
+    leftNotificationType: 'neutral',
+    rightNotificationHeading: undefined,
+    rightNotificationMessage: undefined,
+    rightNotificationType: 'alert',
+  },
+}
+
 export const ExampleTwoColumn: Story = {
   args: {
-    freeText:
+    leftStandIn: false,
+    rightStandIn: false,
+    leftText:
       'Hello here is some text you can type a lot with even multiple lines',
-    notificationHeader: 'Oh no',
-    notificationBody: 'Theres a notification',
-    notificationType: 'alert',
-    imageUrl: [SuccessIcon],
+    leftNotificationHeading: 'Oh no',
+    leftNotificationMessage: 'Theres a notification',
+    leftNotificationType: 'alert',
+    leftImageUrl: undefined,
+    rightImageUrl: [SuccessIcon],
+    rightNotificationHeading: undefined,
+    rightNotificationMessage: undefined,
+    rightNotificationType: undefined,
     containerWidth: 960,
     containerHeight: 432,
   },
