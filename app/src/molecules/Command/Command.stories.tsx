743d8e1b45d13e82c43f1ebb2bbbcd517dfb754f
Seth Foster <seth@opentrons.com>
2024-06-12 15:56:34 -0400

    stories for command

--- app/src/molecules/Command/Command.stories.tsx
+++ app/src/molecules/Command/Command.stories.tsx
@@ -1,16 +1,84 @@
+import * as React from 'react'
 import { Command as CommandComponent } from '.'
+import type { CommandState } from './Command'
+import * as Fixtures from './__fixtures__'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
-const meta: Meta<typeof CommandComponent> = {
+interface StorybookArgs {
+  commandIndex: number
+  aligned: 'left' | 'center'
+  kind: 'odd' | 'desktop'
+  state: CommandState
+}
+
+function Wrapper(props: StorybookArgs): JSX.Element {
+  const command = Fixtures.mockDoItAllTextData.commands[props.commandIndex]
+  return (
+    <CommandComponent
+      command={command}
+      commandTextData={Fixtures.mockDoItAllTextData}
+      robotType='OT-3 Standard'
+      state={props.state}
+      aligned={props.aligned}
+    />)
+}
+
+const meta: Meta<StorybookArgs> = {
   title: 'App/Molecules/Command/Command',
-  component: CommandComponent,
+  component: Wrapper,
+  argTypes: {
+    commandIndex: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: Fixtures.mockDoItAllTextData.commands.length - 1
+      },
+      defaultValue: 0
+    },
+    aligned: {
+      control: {
+        type: 'select',
+      },
+      options: ['left', 'center'],
+      defaultValue: 'left'
+    },
+    kind: {
+      control: {
+        type: 'select',
+      },
+      options: ['odd', 'desktop'],
+      defaultValue: 'odd',
+    },
+    state: {
+      control: {
+        type: 'select',
+      },
+      options: ['current', 'failed', 'future', 'loading'],
+      defaultValue: 'current'
+    },
+  }
 }
 
 export default meta
 
-type Story = StoryObj<typeof CommandComponent>
+type Story = StoryObj<typeof Wrapper>
 
-export const Command: Story = {
-  args: {},
+export const LeftCurrentPauseCommand: Story = {
+  args: {commandIndex: 55, aligned: 'left', kind: 'odd', state: 'current'},
+}
+export const LeftFuturePauseCommand: Story = {
+  args: {commandIndex: 55, aligned: 'left', kind: 'odd', state: 'future'},
+}
+export const LeftFailedPauseCommand: Story = {
+  args: {commandIndex: 55, aligned: 'left', kind: 'odd', state: 'failed'},
+}
+export const CenterCurrentPauseCommand: Story = {
+  args: {commandIndex: 55, aligned: 'center', kind: 'odd', state: 'current'},
+}
+export const CenterFuturePauseCommand: Story = {
+  args: {commandIndex: 55, aligned: 'center', kind: 'odd', state: 'future'}
+}
+export const CenterFailedPauseCommand: Story = {
+  args: {commandIndex: 55, aligned: 'center', kind: 'odd', state: 'failed'}
 }
--- app/src/molecules/Command/Command.tsx
+++ app/src/molecules/Command/Command.tsx
@@ -1,49 +1,133 @@
 import * as React from 'react'
 import {
   Flex,
-  JUSTIFY_SPACE_AROUND,
+  JUSTIFY_CENTER,
   JUSTIFY_FLEX_START,
   ALIGN_CENTER,
+  COLORS,
+  BORDERS,
+  TYPOGRAPHY,
+  SPACING
 } from '@opentrons/components'
 import type { RobotType, RunTimeCommand } from '@opentrons/shared-data'
-import { CommandText } from './CommandText'
+import { CommandText, CommandIcon } from '.'
 import type { CommandTextData } from './types'
+import { Skeleton } from '../../atoms/Skeleton'
+import type {StyleProps} from '@opentrons/components'
 
-export type CommandState = 'current' | 'failed' | 'future' | 'loading'
+export type CommandState = NonSkeletonCommandState | 'loading'
+type NonSkeletonCommandState = 'current' | 'failed' | 'future'
 
-export interface CommandProps {
-  state: CommandState
-  aligned: 'left' | 'center'
-  command: RunTimeCommand
-  commandTextData: CommandTextData
+interface FundamentalProps {
   robotType: RobotType
   isOnDevice?: boolean
+  aligned: 'left' | 'center'
 }
 
+interface SkeletonCommandProps extends FundamentalProps {
+  state: 'loading'
+  command: any
+  commandTextData: any
+}
+
+interface NonSkeletonCommandProps extends FundamentalProps {
+  state: NonSkeletonCommandState
+  command: RunTimeCommand
+  commandTextData: CommandTextData
+}
+
+export type CommandProps = SkeletonCommandProps | NonSkeletonCommandProps
+
 export function Command(props: CommandProps): JSX.Element {
-  return props.aligned === 'left' ? (
-    <LeftAlignedCommand {...props} />
+  return props.state === 'loading' ? (
+    <Skeleton width='100%' height='2rem' backgroundSize='47rem'/>
   ) : (
-    <CenteredCommand {...props} />
-  )
+    props.aligned === 'left' ? (
+      <LeftAlignedCommand {...props} />
+    ) : (
+      <CenteredCommand {...props} />
+  ))
+}
+
+const UNIVERSAL_CONTAINER_STYLES = {
+  borderRadius: BORDERS.borderRadius8,
+  gap: SPACING.spacing12,
+} as const
+
+const UNIVERSAL_ICON_STYLES = {
+  size: SPACING.spacing32
+} as const
+
+const UNIVERSAL_TEXT_STYLES = {
+  fontSize: TYPOGRAPHY.fontSize22,
+  fontWeight: TYPOGRAPHY.fontWeightRegular,
+} as const
+
+const PROPS_BY_STATE: Record<NonSkeletonState, {container: StyleProps, icon: StyleProps, text: StyleProps}> = {
+  current: {
+    container: {
+      backgroundColor: COLORS.blue35,
+      ...UNIVERSAL_CONTAINER_STYLES
+    },
+    icon: {
+      ...UNIVERSAL_ICON_STYLES
+    },
+    text: {
+      ...UNIVERSAL_TEXT_STYLES
+    }
+  },
+  failed: {
+    container: {
+      backgroundColor: COLORS.red35,
+      ...UNIVERSAL_CONTAINER_STYLES
+    },
+    icon: {
+      ...UNIVERSAL_ICON_STYLES
+    },
+    text: {
+      ...UNIVERSAL_TEXT_STYLES
+    }
+  },
+  future: {
+    container: {
+      backgroundColor: COLORS.grey35,
+      ...UNIVERSAL_CONTAINER_STYLES
+    },
+    icon: {
+      ...UNIVERSAL_ICON_STYLES
+    },
+    font: {
+      ...UNIVERSAL_TEXT_STYLES
+    }
+  },
 }
 
 export function CenteredCommand(
-  props: Omit<CommandProps, 'aligned'>
+  props: Omit<NonSkeletonCommandProps, 'aligned'>
 ): JSX.Element {
   return (
-    <Flex justifyContent={JUSTIFY_SPACE_AROUND} alignContent={ALIGN_CENTER}>
-      <CommandText {...props} />
+    <Flex
+      justifyContent={JUSTIFY_CENTER}
+      alignItems={ALIGN_CENTER}
+      {...PROPS_BY_STATE[props.state].container}
+    >
+      <CommandIcon command={props.command} {...PROPS_BY_STATE[props.state].icon} />
+      <CommandText {...props} {...PROPS_BY_STATE[props.state].text} />
     </Flex>
   )
 }
 
 export function LeftAlignedCommand(
-  props: Omit<CommandProps, 'aligned'>
+  props: Omit<NonSkeletonCommandProps, 'aligned'>
 ): JSX.Element {
   return (
-    <Flex justifyContent={JUSTIFY_FLEX_START} alignContent={ALIGN_CENTER}>
-      <CommandText {...props} />
+    <Flex
+      justifyContent={JUSTIFY_FLEX_START}
+      alignItems={ALIGN_CENTER}
+      {...PROPS_BY_STATE[props.state].container}
+    >
+      <CommandIcon command={props.command} {...PROPS_BY_STATE[props.state].icon}/>
+      <CommandText {...props} {...PROPS_BY_STATE[props.state].text}/>
     </Flex>
   )
 }
