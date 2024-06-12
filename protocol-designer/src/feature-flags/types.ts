2447ddb4ce7f06e9a44c7a2ffe59f4695740363e
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2024-06-12 11:19:30 -0400

    hide abs reader behind a feature flag in pd

--- protocol-designer/src/feature-flags/types.ts
+++ protocol-designer/src/feature-flags/types.ts
@@ -36,5 +36,9 @@ export const userFacingFlags: FlagTypes[] = [
   'OT_PD_DISABLE_MODULE_RESTRICTIONS',
   'OT_PD_ALLOW_ALL_TIPRACKS',
 ]
-export const allFlags: FlagTypes[] = [...userFacingFlags, 'PRERELEASE_MODE', 'OT_PD_ENABLE_ABSORBANCE_READER']
+export const allFlags: FlagTypes[] = [
+  ...userFacingFlags,
+  'PRERELEASE_MODE',
+  'OT_PD_ENABLE_ABSORBANCE_READER',
+]
 export type Flags = Partial<Record<FlagTypes, boolean | null | undefined>>
