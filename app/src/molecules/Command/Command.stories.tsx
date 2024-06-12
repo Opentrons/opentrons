86ad16c4490664b58ace0f13cf67bc8188d2c439
Seth Foster <seth@opentrons.com>
2024-06-12 12:00:31 -0400

    fixup command stories

--- app/src/molecules/Command/Command.stories.tsx
+++ app/src/molecules/Command/Command.stories.tsx
@@ -1,16 +1,16 @@
-import { ProtocolCommand as ProtocolCommandComponent } from '.'
+import { Command as CommandComponent } from '.'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
-const meta: Meta<typeof ProtocolCommandComponent> = {
-  title: 'App/Atoms/ProtocolCommand',
-  component: ProtocolCommandComponent,
+const meta: Meta<typeof CommandComponent> = {
+  title: 'App/Molecules/Command/Command',
+  component: CommandComponent,
 }
 
 export default meta
 
-type Story = StoryObj<typeof ProtocolCommandComponent>
+type Story = StoryObj<typeof CommandComponent>
 
-export const ProtocolCommand: Story = {
+export const Command: Story = {
   args: {},
 }
