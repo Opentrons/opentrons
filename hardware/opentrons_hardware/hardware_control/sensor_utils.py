fac12f854694336a63cab220d579e6554daad5ab
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2024-06-12 10:50:50 -0400

    refactor(hardware): change function definition and variable names for improved formatting
    Refactor detectLiquidOrBlockage to did_tip_hit_liquid among other minor formatting changes
    
    re EXEC-517

--- hardware/opentrons_hardware/hardware_control/sensor_utils.py
+++ hardware/opentrons_hardware/hardware_control/sensor_utils.py
@@ -6,22 +6,20 @@ from opentrons_shared_data.errors.exceptions import (
 import numpy as np
 
 
-def detectLiquidOrBlockage(
-        pressureReadings: list[float],
-        liquidSolidThreshold: float
-) -> None:
-    dx = 1
-    #take derivative of pressureReadings with respect to dx
-    pressureDerivatives = np.gradient(pressureReadings[-5:], dx)
-    endDerivative = pressureDerivatives[-1]
-    if endDerivative > liquidSolidThreshold:
-        #Hit Bottom of Well(solid)
+def did_tip_hit_liquid(
+        pressure_readings: list[float],
+        liquid_solid_threshold: float
+) -> bool:
+    pressure_difference = np.gradient(pressure_readings[-5:], 1)
+    end_difference = pressure_difference[-1]
+    if end_difference > liquid_solid_threshold:
+        #Hit Bottom of Well
         raise TipHitWellBottomError(
                 "Tip hit the bottom of the well.",
                 {
-                    "Pressure change detected: " + str(endDerivative)
+                    "Pressure change detected: " + str(end_difference)
                 },
             )
     else:
         #Hit Liquid
-        return None
+        return True
