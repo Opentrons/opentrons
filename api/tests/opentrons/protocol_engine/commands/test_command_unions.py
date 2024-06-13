e33ea63d10ad0394a07d531de204de098cb402a3
Max Marrone <max@opentrons.com>
2024-06-13 15:16:03 -0400

    Add one more smoke test.

--- api/tests/opentrons/protocol_engine/commands/test_command_unions.py
+++ api/tests/opentrons/protocol_engine/commands/test_command_unions.py
@@ -1,7 +1,23 @@
 """Unit tests for the `command_unions` module."""
 
 from opentrons.protocol_engine.commands import command_unions
+from opentrons.protocol_engine.commands import (
+    AspirateCreate,
+    AspirateParams,
+    CustomCreate,
+    CustomParams,
+)
 from opentrons.util.get_union_elements import get_union_elements
+
+
+def test_create_types_by_params_type() -> None:
+    """CREATE_TYPES_BY_PARAMS_TYPE should map *Params classes to *Create classes.
+
+    We won't test every possible pairing here--just enough to make sure it's basically
+    working.
+    """
+    assert command_unions.CREATE_TYPES_BY_PARAMS_TYPE[AspirateParams] is AspirateCreate
+    assert command_unions.CREATE_TYPES_BY_PARAMS_TYPE[CustomParams] is CustomCreate
 
 
 def test_create_types_by_params_type_is_exhaustive() -> None:
