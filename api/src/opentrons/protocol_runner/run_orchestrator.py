27a03bea93650e0b7944e809c4fe011d2a51fb52
tamarzanzouri <tamar.zanzouri@opentrons.com>
2024-06-14 14:54:01 -0400

    extract generator to seperate class

--- api/src/opentrons/protocol_runner/run_orchestrator.py
+++ api/src/opentrons/protocol_runner/run_orchestrator.py
@@ -335,23 +335,3 @@ class RunOrchestrator:
     def get_deck_type(self) -> DeckType:
         """Get engine deck type."""
         return self._protocol_engine.state_view.config.deck_type
-
-    async def command_generator(self) -> AsyncGenerator[str, None]:
-        while True:
-            try:
-                command_id = await self._protocol_engine._state_store.wait_for(
-                    condition=self._protocol_engine.state_view.commands.get_next_to_execute
-                )
-                # Assert for type hinting. This is valid because the wait_for() above
-                # only returns when the value is truthy.
-                assert command_id is not None
-                yield command_id
-            except RunStoppedError:
-                # There are no more commands that we should execute, either because the run has
-                # completed on its own, or because a client requested it to stop.
-                break
-
-    async def get_next_command_to_execute(self) -> None:
-        gen = self.command_generator()
-        async for command in gen:
-            print(command)
