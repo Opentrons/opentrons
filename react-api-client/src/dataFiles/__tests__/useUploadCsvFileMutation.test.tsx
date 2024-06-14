154001c2295b02ea55a3172b09c902e114a6f53e
koji <kk2796@nyu.edu>
2024-06-14 18:21:29 -0400

    add a test case for useUploadCsvFileMutation

--- react-api-client/src/dataFiles/__tests__/useUploadCsvFileMutation.test.tsx
+++ react-api-client/src/dataFiles/__tests__/useUploadCsvFileMutation.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { describe, it, expect, beforeEach, vi } from 'vitest'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { renderHook, waitFor } from '@testing-library/react'
+import { act, renderHook, waitFor } from '@testing-library/react'
 import { uploadCsvFile } from '@opentrons/api-client'
 import { useHost } from '../../api'
 import { useUploadCsvFileMutation } from '../useUploadCsvFileMutation'
@@ -17,6 +17,13 @@ vi.mock('../../api/useHost')
 
 const HOST_CONFIG: HostConfig = { hostname: 'localhost' }
 const mockFilePath = 'media/mock-usb-drive/mock.csv'
+const mockUploadResponse = {
+  data: {
+    id: '1',
+    createdAt: '2024-06-07T19:19:56.268029+00:00',
+    name: 'rtp_mock_file.csv',
+  },
+} as UploadedCsvFileResponse
 
 describe('useUploadCsvFileMutation', () => {
   let wrapper: React.FunctionComponent<{ children: React.ReactNode }>
@@ -58,6 +65,23 @@ describe('useUploadCsvFileMutation', () => {
     result.current.uploadCsvFile(mockFilePath)
     await waitFor(() => {
       expect(result.current.data).toBeUndefined()
+    })
+  })
+
+  it('should return data when calling uploadCsvFile successfully', async () => {
+    vi.mocked(useHost).mockReturnValue(HOST_CONFIG)
+    vi.mocked(uploadCsvFile).mockResolvedValue({
+      data: mockUploadResponse,
+    } as Response<UploadedCsvFileResponse>)
+
+    const { result } = renderHook(
+      () => useUploadCsvFileMutation(mockFilePath),
+      { wrapper }
+    )
+    act(() => result.current.uploadCsvFile(mockFilePath))
+
+    await waitFor(() => {
+      expect(result.current.data).toEqual(mockUploadResponse)
     })
   })
 })
