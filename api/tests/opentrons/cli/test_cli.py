de5b66f20c8468cc03bed56e313309ec1f2b894a
Max Marrone <max@opentrons.com>
2024-06-14 14:48:08 -0400

    refactor(api): Emit proper `comment` commands from legacy protocols (#15426)

--- api/tests/opentrons/cli/test_cli.py
+++ api/tests/opentrons/cli/test_cli.py
@@ -152,15 +152,7 @@ def test_analysis_deck_definition(
         "commands"
     ]
 
-    # ["params"]["message"] for Protocol Engine (PAPIv≥2.14),
-    # ["params"]["legacyCommandText"] for the legacy backend (PAPIv≤2.13).
-    # Eventually the legacy backend should change to match Protocol Engine.
-    comment_message = (
-        comment_command["params"]["message"]
-        if "message" in comment_command["params"]
-        else comment_command["params"]["legacyCommandText"]
-    )
-    assert comment_message == expected_point
+    assert comment_command["params"]["message"] == expected_point
 
 
 # TODO(mm, 2023-08-12): We can remove this test when we remove special handling for these
