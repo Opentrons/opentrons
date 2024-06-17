29d7d79a75f4d616eeb5ecabb36bbfbf479ee8d7
Brayan Almonte <vegano1@users.noreply.github.com>
2024-06-17 13:53:19 -0400

    feat(hardware): added safety relay active state to HepaUVState and safety_relay_inactive error (#15311)

--- hardware/opentrons_hardware/hardware_control/hepa_uv_settings.py
+++ hardware/opentrons_hardware/hardware_control/hepa_uv_settings.py
@@ -1,4 +1,5 @@
 """Utilities for controlling the hepa/uv extension module."""
+
 import logging
 import asyncio
 from typing import Optional
@@ -46,6 +47,7 @@ class HepaUVState:
     uv_duration_s: int
     remaining_time_s: int
     uv_current_ma: int
+    safety_relay_active: bool
 
 
 async def set_hepa_fan_state(
@@ -136,6 +138,7 @@ async def get_hepa_uv_state(can_messenger: CanMessenger) -> Optional[HepaUVState
                 uv_duration_s=int(message.payload.uv_duration_s.value),
                 remaining_time_s=int(message.payload.remaining_time_s.value),
                 uv_current_ma=int(message.payload.uv_current_ma.value),
+                safety_relay_active=bool(message.payload.safety_relay_active.value),
             )
 
     def _filter(arb_id: ArbitrationId) -> bool:
