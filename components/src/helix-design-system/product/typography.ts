a276f4c55013e0dc30e20ad0795135974636b7c4
Seth Foster <seth@opentrons.com>
2024-06-18 18:10:34 -0400

    feat(components): add helix product typography
    - Adds the new sizes for type in the helix product system
    - Adds a dummy component under "Design Tokens" in storybook so you can
      view all the text
    
    Closes EXEC-566
    
    Make the storybook display all the stories

---
+++ components/src/helix-design-system/product/typography.ts
@@ -0,0 +1,141 @@
+/**
+ * Note: these values are only currently correct for desktop and should not be used on ODD.
+ */
+
+// Valid for most typography styles
+export const fontFamily = 'Public Sans'
+
+export const fontWeightRegular = '400'
+export const fontWeightSemiBold = '600'
+export const fontWeightBold = '700'
+
+// Display styles
+const fontFamilyDisplay = fontFamily
+const fontSizeDisplay = '2.4375rem' // 39px
+const lineHeightDisplay = '3rem' // 48px
+
+// Display-Bold
+export const fontSizeDisplayBold = fontSizeDisplay
+export const lineHeightDisplayBold = lineHeightDisplay
+export const fontFamilyDisplayBold = fontFamilyDisplay
+export const fontWeightDisplayBold = fontWeightBold
+export const fontStyleDisplayBold = `${fontWeightDisplayBold} ${fontSizeDisplayBold}/${lineHeightDisplayBold} ${fontFamilyDisplayBold}`
+
+// Heading-Large
+const fontSizeHeadingLarge = '1.625rem' // 26px
+const lineHeightHeadingLarge = '2rem' // 32px
+const fontFamilyHeadingLarge = fontFamily
+
+// Heading-Large-Regular
+export const fontFamilyHeadingLargeRegular = fontFamilyHeadingLarge
+export const fontSizeHeadingLargeRegular = fontSizeHeadingLarge
+export const lineHeightHeadingLargeRegular = lineHeightHeadingLarge
+export const fontWeightHeadingLargeRegular = fontWeightRegular
+export const fontStyleHeadingLargeRegular = `${fontWeightHeadingLargeRegular} ${fontSizeHeadingLargeRegular}/${lineHeightHeadingLargeRegular} ${fontFamilyHeadingLargeRegular}`
+
+// Heading-Large-Bold
+export const fontFamilyHeadingLargeBold = fontFamilyHeadingLarge
+export const fontSizeHeadingLargeBold = fontSizeHeadingLarge
+export const lineHeightHeadingLargeBold = lineHeightHeadingLarge
+export const fontWeightHeadingLargeBold = fontWeightBold
+export const fontStyleHeadingLargeBold = `${fontWeightHeadingLargeBold} ${fontSizeHeadingLargeBold}/${lineHeightHeadingLargeBold} ${fontFamilyHeadingLargeBold}`
+
+// Heading-Medium
+const fontSizeHeadingMedium = '1.4375rem' // 23px
+const lineHeightHeadingMedium = '1.75rem' // 28px
+const fontFamilyHeadingMedium = fontFamily
+
+// Heading-Medium-Regular
+export const fontSizeHeadingMediumMedium = fontSizeHeadingMedium
+export const lineHeightHeadingMediumMedium = lineHeightHeadingMedium
+export const fontFamilyHeadingMediumMedium = fontFamilyHeadingMedium
+export const fontWeightHeadingMediumMedium = fontWeightSemiBold
+export const fontStyleHeadingMediumMedium = `${fontWeightHeadingMediumMedium} ${fontSizeHeadingMediumMedium}/${lineHeightHeadingMediumMedium} ${fontFamilyHeadingMediumMedium}`
+
+// Heading-Small
+const fontSizeHeadingSmall = '1.125rem' // 18px
+const lineHeightHeadingSmall = '1.5rem' // 24px
+const fontFamilyHeadingSmall = fontFamily
+
+// Heading-Small-Regular
+export const fontSizeHeadingSmallRegular = fontSizeHeadingSmall
+export const lineHeightHeadingSmallRegular = lineHeightHeadingSmall
+export const fontFamilyHeadingSmallRegular = fontFamilyHeadingSmall
+export const fontWeightHeadingSmallRegular = '600'
+export const fontStyleHeadingSmallRegular = `${fontWeightHeadingSmallRegular} ${fontSizeHeadingSmallRegular}/${lineHeightHeadingSmallRegular} ${fontFamilyHeadingSmallRegular}`
+
+// Heading-Small-Bold
+export const fontSizeHeadingSmallBold = fontSizeHeadingSmall
+export const lineHeightHeadingSmallBold = lineHeightHeadingSmall
+export const fontFamilyHeadingSmallBold = fontFamilyHeadingSmall
+export const fontWeightHeadingSmallBold = fontWeightBold
+export const fontStyleHeadingSmallBold = `${fontWeightHeadingSmallBold} ${fontSizeHeadingSmallBold}/${lineHeightHeadingSmallBold} ${fontFamilyHeadingSmallBold}`
+
+// Body-Large
+const fontSizeBodyLarge = '1rem' // 16px
+const lineHeightBodyLarge = '1.5rem' // 24px
+const fontFamilyBodyLarge = fontFamily
+
+// Body-Large-Medium
+export const fontSizeBodyLargeMedium = fontSizeBodyLarge
+export const lineHeightBodyLargeMedium = lineHeightBodyLarge
+export const fontFamilyBodyLargeMedium = fontFamilyBodyLarge
+export const fontWeightBodyLargeMedium = fontWeightSemiBold
+export const fontStyleBodyLargeMedium = `${fontWeightBodyLargeMedium} ${fontSizeBodyLargeMedium}/${lineHeightBodyLargeMedium} ${fontFamilyBodyLargeMedium}`
+
+// Body-Large-Regular
+export const fontSizeBodyLargeRegular = fontSizeBodyLarge
+export const lineHeightBodyLargeRegular = lineHeightBodyLarge
+export const fontFamilyBodyLargeRegular = fontFamilyBodyLarge
+export const fontWeightBodyLargeRegular = fontWeightRegular
+export const fontStyleBodyLargeRegular = `${fontWeightBodyLargeRegular} ${fontSizeBodyLargeRegular}/${lineHeightBodyLargeRegular} ${fontFamilyBodyLargeRegular}`
+
+// Body-Default
+const fontSizeBodyDefault = '0.875rem' // 14px
+const lineHeightBodyDefault = '1.25rem' // 20px
+const fontFamilyBodyDefault = fontFamily
+
+// Body-Default-Medium
+export const fontSizeBodyDefaultMedium = fontSizeBodyDefault
+export const lineHeightBodyDefaultMedium = lineHeightBodyDefault
+export const fontFamilyBodyDefaultMedium = fontFamilyBodyDefault
+export const fontWeightBodyDefaultMedium = fontWeightSemiBold
+export const fontStyleBodyDefaultMedium = `${fontWeightBodyDefaultMedium} ${fontSizeBodyDefaultMedium}/${lineHeightBodyDefaultMedium} ${fontFamilyBodyDefaultMedium}`
+
+// Body-Default-Regular
+export const fontSizeBodyDefaultRegular = fontSizeBodyDefault
+export const lineHeightBodyDefaultRegular = lineHeightBodyDefault
+export const fontFamilyBodyDefaultRegular = fontFamilyBodyDefault
+export const fontWeightBodyDefaultRegular = fontWeightRegular
+export const fontStyleBodyDefaultRegular = `${fontWeightBodyDefaultRegular} ${fontSizeBodyDefaultRegular}/${lineHeightBodyDefaultRegular} ${fontFamilyBodyDefaultRegular}`
+
+// Caption
+const fontSizeCaption = '0.8125rem' // 13px
+const lineHeightCaption = '1rem' // 16px
+const fontFamilyCaption = 'Public Sans'
+
+// Caption-Medium
+export const fontSizeCaptionMedium = fontSizeCaption
+export const lineHeightCaptionMedium = lineHeightCaption
+export const fontFamilyCaptionMedium = fontFamilyCaption
+export const fontWeightCaptionMedium = '500'
+export const fontStyleCaptionMedium = `${fontWeightCaptionMedium} ${fontSizeCaptionMedium}/${lineHeightCaptionMedium} ${fontFamilyCaptionMedium}`
+
+// Caption-Regular
+export const fontSizeCaptionRegular = fontSizeCaption
+export const lineHeightCaptionRegular = lineHeightCaption
+export const fontFamilyCaptionRegular = fontFamilyCaption
+export const fontWeightCaptionRegular = '400'
+export const fontStyleCaptionRegular = `${fontWeightCaptionRegular} ${fontSizeCaptionRegular}/${lineHeightCaptionRegular} ${fontFamilyCaptionRegular}`
+
+// Code
+const fontSizeCode = '0.75rem' // 12px
+const lineHeightCode = '1.25rem' // 20px
+const fontFamilyCode = 'Reddit Mono'
+
+// Code-Regular
+export const fontSizeCodeRegular = fontSizeCode
+export const lineHeightCodeRegular = lineHeightCode
+export const fontFamilyCodeRegular = fontFamilyCode
+export const fontWeightCodeRegular = fontWeightRegular
+export const fontStyleCodeRegular = `${fontWeightCodeRegular} ${fontSizeCodeRegular}/${lineHeightCodeRegular} ${fontFamilyCodeRegular}`
