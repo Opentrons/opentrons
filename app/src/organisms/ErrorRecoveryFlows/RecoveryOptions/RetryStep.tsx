65bdbbe5cef0d1c85c3ddef4f2111ac99c245b9c
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2024-06-19 12:40:16 -0400

    update retry step route to designs

--- app/src/organisms/ErrorRecoveryFlows/RecoveryOptions/RetryStep.tsx
+++ app/src/organisms/ErrorRecoveryFlows/RecoveryOptions/RetryStep.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 
-import {
-  ALIGN_CENTER,
-  DIRECTION_COLUMN,
-  Flex,
-  Icon,
-  SPACING,
-  StyledText,
-} from '@opentrons/components'
+import { StyledText } from '@opentrons/components'
 
 import { RECOVERY_MAP } from '../constants'
-import { RecoveryFooterButtons, RecoverySingleColumnContent } from '../shared'
+import { TwoColTextAndFailedStepNextStep } from '../shared'
 import { SelectRecoveryOption } from './SelectRecoveryOption'
 
 import type { RecoveryContentProps } from '../types'
@@ -34,16 +27,13 @@ export function RetryStep(props: RecoveryContentProps): JSX.Element {
   return buildContent()
 }
 
-function RetryStepInfo({
-  isOnDevice,
-  routeUpdateActions,
-  recoveryCommands,
-}: RecoveryContentProps): JSX.Element | null {
+function RetryStepInfo(props: RecoveryContentProps): JSX.Element {
+  const { routeUpdateActions, recoveryCommands } = props
   const { ROBOT_RETRYING_STEP } = RECOVERY_MAP
   const { t } = useTranslation('error_recovery')
 
   const { retryFailedCommand, resumeRun } = recoveryCommands
-  const { goBackPrevStep, setRobotInMotion } = routeUpdateActions
+  const { setRobotInMotion } = routeUpdateActions
 
   const primaryBtnOnClick = (): Promise<void> => {
     return setRobotInMotion(true, ROBOT_RETRYING_STEP.ROUTE)
@@ -53,36 +43,25 @@ function RetryStepInfo({
       })
   }
 
-  if (isOnDevice) {
+  const buildBodyText = (): JSX.Element => {
     return (
-      <RecoverySingleColumnContent
-        gridGap={SPACING.spacing24}
-        alignItems={ALIGN_CENTER}
-      >
-        <Flex
-          flexDirection={DIRECTION_COLUMN}
-          alignItems={ALIGN_CENTER}
-          gridGap={SPACING.spacing24}
-          height="100%"
-          width="848px"
-        >
-          <Icon name="ot-alert" size="3.75rem" marginTop={SPACING.spacing24} />
-          <StyledText as="h3Bold">
-            {t('are_you_sure_you_want_to_resume')}
-          </StyledText>
-          <StyledText as="h4" textAlign={ALIGN_CENTER}>
-            {t('run_will_resume')}
-          </StyledText>
-        </Flex>
-        <RecoveryFooterButtons
-          isOnDevice={isOnDevice}
-          primaryBtnOnClick={primaryBtnOnClick}
-          secondaryBtnOnClick={goBackPrevStep}
-          primaryBtnTextOverride={t('confirm')}
-        />
-      </RecoverySingleColumnContent>
+      <Trans
+        t={t}
+        i18nKey="first_take_any_necessary_actions"
+        components={{
+          block: <StyledText as="p" />,
+        }}
+      />
     )
-  } else {
-    return null
   }
+
+  return (
+    <TwoColTextAndFailedStepNextStep
+      {...props}
+      leftColTitle={t('retry_step')}
+      leftColBodyText={buildBodyText()}
+      primaryBtnOnClick={primaryBtnOnClick}
+      primaryBtnCopy={t('retry_now')}
+    />
+  )
 }
