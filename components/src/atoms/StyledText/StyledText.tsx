34104fe66deac23ca9519dad91652d4753b20865
Seth Foster <seth@opentrons.com>
2024-06-20 13:45:36 -0400

    feat(components): StyledText styles for app/odd
    StyledText is a component that is only really useful if you're goign to
    do semantic styles. Semantic styles are only really useful if they mean
    the same thing in all the places they're used. This isn't really true
    right now, but we can at least have enumerated style tokens that you can
    select from when creating one.
    
    Now, which style token you use is separately selectable across ODD and
    non-ODD flows. You can still fully or partially override the CSS if you
    want, but the styles on desktop align with helix and the styles on ODD
    align with what's going on in ODD.

--- components/src/atoms/StyledText/StyledText.tsx
+++ components/src/atoms/StyledText/StyledText.tsx
@@ -1,95 +1,276 @@
 import styled, { css } from 'styled-components'
 import { Text } from '../../primitives'
 import { TYPOGRAPHY, RESPONSIVENESS } from '../../ui-style-constants'
+import { TYPOGRAPHY as HELIX_TYPOGRAPHY } from '../../helix-design-system/product'
 
 import type * as React from 'react'
 import type { FlattenSimpleInterpolation } from 'styled-components'
 
+const helixProductStyleMap = {
+  displayBold: {
+    as: 'h1',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleDisplayBold};
+      }
+    `,
+  },
+  headingLargeRegular: {
+    as: 'h2',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleHeadingLargeRegular};
+      }
+    `,
+  },
+  headingLargeBold: {
+    as: 'h2',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleHeadingLargeBold};
+      }
+    `,
+  },
+  headingMediumMedium: {
+    as: 'h3',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleHeadingMediumMedium};
+      }
+    `,
+  },
+  headingSmallRegular: {
+    as: 'h4',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleHeadingSmallRegular};
+      }
+    `,
+  },
+  headingSmallBold: {
+    as: 'h4',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleHeadingSmallBold};
+      }
+    `,
+  },
+  bodyLargeMedium: {
+    as: 'p',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleBodyLargeMedium};
+      }
+    `,
+  },
+  bodyLargeRegular: {
+    as: 'p',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleBodyLargeRegular};
+      }
+    `,
+  },
+  bodyDefaultMedium: {
+    as: 'p',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleBodyDefaultMedium};
+      }
+    `,
+  },
+  bodyDefaultRegular: {
+    as: 'p',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleBodyDefaultRegular};
+      }
+    `,
+  },
+  captionMedium: {
+    as: 'label',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleCaptionMedium};
+      }
+    `,
+  },
+  captionRegular: {
+    as: 'label',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleCaptionRegular};
+      }
+    `,
+  },
+  codeRegular: {
+    as: 'p',
+    style: css`
+      @media not (${RESPONSIVENESS.touchscreenMediaQuerySpecs}) {
+        font: ${HELIX_TYPOGRAPHY.fontStyleCodeRegular};
+      }
+    `,
+  },
+} as const
+
+const ODDStyleMap = {
+  level1Header: {
+    as: 'h1',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level1Header};
+      }
+    `,
+  },
+  level2HeaderRegular: {
+    as: 'h2',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level2HeaderRegular};
+      }
+    `,
+  },
+  level2HeaderSemiBold: {
+    as: 'h2',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level2HeaderSemiBold}
+      }
+    `,
+  },
+  level2HeaderBold: {
+    as: 'h2',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level2HeaderBold}
+      }
+    `,
+  },
+
+  level3HeaderRegular: {
+    as: 'h3',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level3HeaderRegular};
+      }
+    `,
+  },
+  level3HeaderSemiBold: {
+    as: 'h3',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level3HeaderSemiBold}
+      }
+    `,
+  },
+
+  level3HeaderBold: {
+    as: 'h3',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level3HeaderBold}
+      }
+    `,
+  },
+
+  level4HeaderRegular: {
+    as: 'h4',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level4HeaderRegular};
+      }
+    `,
+  },
+  level4HeaderBold: {
+    as: 'h4',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.level4HeaderBold}
+      }
+    `,
+  },
+
+  bodyTextRegular: {
+    as: 'p',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.bodyTextRegular}
+      }
+    `,
+  },
+  bodyTextSemiBold: {
+    as: 'p',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.bodyTextSemiBold}
+      }
+    `,
+  },
+  bodyTextBold: {
+    as: 'p',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.bodyTextBold}
+      }
+    `,
+  },
+  smallBodyTextRegular: {
+    as: 'label',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.smallBodyTextRegular}
+      }
+    `,
+  },
+
+  smallBodyTextSemiBold: {
+    as: 'label',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        font-size: ${TYPOGRAPHY.fontSize20};
+        line-height: ${TYPOGRAPHY.lineHeight24};
+      }
+    `,
+  },
+
+  smallBodyTextBold: {
+    as: 'label',
+    style: css`
+      @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+        ${TYPOGRAPHY.smallBodyTextBold}
+      }
+    `,
+  },
+} as const
+
 export interface Props extends React.ComponentProps<typeof Text> {
-  asDesktop?: string
-  asODD?: string
+  oddStyle?: ODDStyles
+  desktopStyle?: HelixStyles
   children?: React.ReactNode
 }
+export const ODD_STYLES = Object.keys(ODDStyleMap)
+export const HELIX_STYLES = Object.keys(helixProductStyleMap)
+export type HelixStyles = typeof HELIX_STYLES
+export type ODDStyles = typeof ODD_STYLES
 
-const styleMap: { [tag: string]: FlattenSimpleInterpolation } = {
-  h1: css`
-    ${TYPOGRAPHY.h1Default};
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.level1Header};
-    }
-  `,
-  h2: css`
-    ${TYPOGRAPHY.h2Regular}
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.level2HeaderRegular};
-    }
-  `,
-  h3: css`
-    ${TYPOGRAPHY.h3Regular}
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.level3HeaderRegular};
-    }
-  `,
-  h4: css`
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.level4HeaderRegular};
-    }
-  `,
-  h6: TYPOGRAPHY.h6Default,
-  p: css`
-    ${TYPOGRAPHY.pRegular}
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.bodyTextRegular}
-    }
-  `,
-  label: css`
-    ${TYPOGRAPHY.labelRegular}
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.smallBodyTextRegular}
-    }
-  `,
-  h2SemiBold: css`
-    ${TYPOGRAPHY.h2SemiBold}
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.level2HeaderSemiBold}
-    }
-  `,
-  h3SemiBold: css`
-    ${TYPOGRAPHY.h3SemiBold}
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.level3HeaderSemiBold}
-    }
-  `,
-  h4SemiBold: TYPOGRAPHY.level4HeaderSemiBold,
-  h6SemiBold: TYPOGRAPHY.h6SemiBold,
-  pSemiBold: css`
-    ${TYPOGRAPHY.pSemiBold}
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      ${TYPOGRAPHY.bodyTextSemiBold}
-    }
-  `,
-  labelSemiBold: css`
-    ${TYPOGRAPHY.labelSemiBold}
-    @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-      font-size: ${TYPOGRAPHY.fontSize20};
-      line-height: ${TYPOGRAPHY.lineHeight24};
-    }
-  `,
-  h2Bold: TYPOGRAPHY.level2HeaderBold,
-  h3Bold: TYPOGRAPHY.level3HeaderBold,
-  h4Bold: TYPOGRAPHY.level4HeaderBold,
-  pBold: TYPOGRAPHY.bodyTextBold,
-  labelBold: TYPOGRAPHY.smallBodyTextBold,
+function styleForDesktopName(
+  name?: keyof typeof helixProductStyleMap
+): FlattenSimpleInterpolation {
+  return name ? helixProductStyleMap[name].style : css``
 }
 
-export const StyledText: (props: Props) => JSX.Element = styled(Text)<Props>`
-  ${props => {
-    let fontWeight = ''
-    if (props.fontWeight === TYPOGRAPHY.fontWeightSemiBold) {
-      fontWeight = 'SemiBold'
-    } else if (props.fontWeight === TYPOGRAPHY.fontWeightBold) {
-      fontWeight = 'Bold'
-    }
-    return styleMap[`${props.as}${fontWeight}`]
-  }}
+function styleForODDName(
+  name?: keyof typeof ODDStyleMap
+): FlattenSimpleInterpolation {
+  return name ? ODDStyleMap[name].style : css``
+}
+
+const DesktopStyledText: (props: Props) => JSX.Element = styled(Text)<Props>`
+  ${props => styleForDesktopName(props.desktopStyle)}
+`
+
+export const StyledText: (props: Props) => JSX.Element = styled(
+  DesktopStyledText
+)<Props>`
+  ${props => styleForODDName(props.oddStyle)}
 `
