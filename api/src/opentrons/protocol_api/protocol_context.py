951c406283479d42ae2738fd1b7f0ee4dfb163e8
pmoegenburg <pbm53@cornell.edu>
2024-06-24 13:58:51 -0400

    added api version checking

--- api/src/opentrons/protocol_api/protocol_context.py
+++ api/src/opentrons/protocol_api/protocol_context.py
@@ -870,7 +870,7 @@ class ProtocolContext(CommandPublisher):
         mount: Union[Mount, str, None] = None,
         tip_racks: Optional[List[Labware]] = None,
         replace: bool = False,
-        liquid_presence_detection: bool = False,
+        liquid_presence_detection: Optional[bool] = None,
     ) -> InstrumentContext:
         """Load a specific instrument for use in the protocol.
 
@@ -898,7 +898,7 @@ class ProtocolContext(CommandPublisher):
                              control <advanced-control>` applications. You cannot
                              replace an instrument in the middle of a protocol being run
                              from the Opentrons App or touchscreen.
-        :param bool liquid_presence_detection: If ``True``, enable liquid presence detection for instrument.
+        :param bool liquid_presence_detection: If ``True``, enable liquid presence detection for instrument. Only available on Flex robots in API Version 2.19 and above.
         """
         instrument_name = validation.ensure_lowercase_name(instrument_name)
         checked_instrument_name = validation.ensure_pipette_name(instrument_name)
@@ -930,11 +930,22 @@ class ProtocolContext(CommandPublisher):
             f"Loading {checked_instrument_name} on {checked_mount.name.lower()} mount"
         )
 
-        instrument_core = self._core.load_instrument(
-            instrument_name=checked_instrument_name,
-            mount=checked_mount,
-            liquid_presence_detection=liquid_presence_detection,
-        )
+        if self._core.robot_type != "OT-3 Standard" or self._api_version < APIVersion(2, 19) and liquid_presence_detection != None:
+            raise APIVersionError(
+                "Liquid Presence Detection is only supported on Flex protocols in API Version 2.19 and above."
+            )
+        elif self._api_version >= APIVersion(2, 19) and liquid_presence_detection == None:
+            instrument_core = self._core.load_instrument(
+                instrument_name=checked_instrument_name,
+                mount=checked_mount,
+                liquid_presence_detection=False,
+            )
+        else:
+            instrument_core = self._core.load_instrument(
+                instrument_name=checked_instrument_name,
+                mount=checked_mount,
+                liquid_presence_detection=liquid_presence_detection,
+            )
 
         for tip_rack in tip_racks:
             instrument_support.validate_tiprack(
