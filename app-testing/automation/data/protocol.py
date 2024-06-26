351ce0f89d365af0295d5ea60a8a70c5ece7fb42
Derek Maggio <DerekMaggio@users.noreply.github.com>
2024-06-26 12:31:20 -0700

    test(app-testing): analysis snapshot improvements (#15522)
    the culmination of the 4 below PRs and then reverting all snapshot
    changes
    Shout out to @skowalski08 for her help
    
    ---------
    
    Co-authored-by: Sara Kowalski <53955458+skowalski08@users.noreply.github.com>

--- app-testing/automation/data/protocol.py
+++ app-testing/automation/data/protocol.py
@@ -7,8 +7,6 @@ from typing import Literal, Optional
 
 from pydantic import BaseModel, Field
 
-from automation.resources.robot_data import module_types
-
 GENERATED_PROTOCOLS_FOLDER = "generated_protocols"
 OVERRIDE_MONIKER = "_Override_"
 
@@ -19,16 +17,7 @@ class Protocol(BaseModel):
     file_stem: str = Field(description="file name not including extension")
     file_extension: Literal["json", "py"] = Field(description="file extension of the protocol")
     robot: Literal["OT2", "Flex"] = Field(description="the robot type which will appear in the robot field in the app")
-    app_error: bool = Field(description="will analysis with the app raise an error")
-    robot_error: bool = Field(description="will analysis with the robot raise an error")
-    app_analysis_error: Optional[str] = Field(description="the exact error shown in the app popout", default=None)
-    robot_analysis_error: Optional[str] = Field(description="the exact analysis error from the robot", default=None)
     custom_labware: Optional[list[str]] = Field(description="list of custom labware file stems", default=None)
-    instruments: Optional[list[str]] = Field(description="list of instruments that will show in the app", default=None)
-    modules: Optional[list[module_types]] = Field(description="list of modules that will show in the app", default=None)
-    description: Optional[str] = Field(description="Details about this protocol", default=None)
-    expected_test_failure: bool = Field(description="Is this test expected to fail", default=False)
-    expected_test_reason: Optional[str] = Field(description="Reason test is failing", default=None)
     override_variable_name: Optional[str] = Field(description="The variable name to override", default=None)
     override_value: Optional[str] = Field(description="The value of the override", default=None)
     from_override: bool = Field(description="Is this protocol generated from an override", default=False)
