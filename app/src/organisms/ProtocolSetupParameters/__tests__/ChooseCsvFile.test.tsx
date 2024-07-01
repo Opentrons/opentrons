5d574a5befb38c7f7d76ff14e8b9519d2d7d7865
koji <kk2796@nyu.edu>
2024-07-01 16:12:56 -0400

    fix tests

--- app/src/organisms/ProtocolSetupParameters/__tests__/ChooseCsvFile.test.tsx
+++ app/src/organisms/ProtocolSetupParameters/__tests__/ChooseCsvFile.test.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import { describe, it, beforeEach, vi, expect } from 'vitest'
 import { screen, fireEvent } from '@testing-library/react'
+import { when } from 'vitest-when'
+
+import { useAllCsvFilesQuery } from '@opentrons/react-api-client'
 
 import { i18n } from '../../../i18n'
 import { renderWithProviders } from '../../../__testing-utils__'
@@ -8,11 +11,11 @@ import { EmptyFile } from '../EmptyFile'
 import { mockConnectedRobot } from '../../../redux/discovery/__fixtures__'
 import { getLocalRobot } from '../../../redux/discovery'
 import { getShellUpdateDataFiles } from '../../../redux/shell'
-import { useAllCsvFilesQuery } from '@opentrons/react-api-client'
 import { ChooseCsvFile } from '../ChooseCsvFile'
 
 import type { CsvFileParameter } from '@opentrons/shared-data'
 
+vi.mock('@opentrons/react-api-client')
 vi.mock('../../../redux/discovery')
 vi.mock('../../../redux/shell')
 vi.mock('../EmptyFile')
@@ -22,24 +25,30 @@ const mockSetParameter = vi.fn()
 const mockParameter: CsvFileParameter = {} as any
 const mockSetFileInfo = vi.fn()
 const PROTOCOL_ID = 'fake_protocol_id'
-const mockUsbData = {
-  type: 'shell:SEND_FILE_PATHS',
-  payload: {
-    filePaths: [
-      '/media/mock-usb-drive/mock-file1.csv',
-      '/media/mock-usb-drive/mock-file2.csv',
-      '/media/mock-usb-drive/mock-file3.csv',
-    ],
-  },
-  meta: { shell: true },
-} as any
+const mockUsbData = [
+  '/media/mock-usb-drive/mock-file1.csv',
+  '/media/mock-usb-drive/mock-file2.csv',
+  '/media/mock-usb-drive/mock-file3.csv',
+]
 
-const mockEmptyUsbData = {
-  ...mockUsbData,
-  payload: {
-    filePaths: [],
+const mockDataOnRobot = {
+  data: {
+    data: {
+      files: [
+        {
+          id: '1',
+          createdAt: '2024-06-07T19:19:56.268029+00:00',
+          name: 'rtp_mock_file1.csv',
+        },
+        {
+          id: '2',
+          createdAt: '2024-06-17T19:19:56.268029+00:00',
+          name: 'rtp_mock_file2.csv',
+        },
+      ],
+    },
   },
-} as any
+}
 
 const render = (props: React.ComponentProps<typeof ChooseCsvFile>) => {
   return renderWithProviders(<ChooseCsvFile {...props} />, {
@@ -62,6 +71,9 @@ describe('ChooseCsvFile', () => {
     vi.mocked(EmptyFile).mockReturnValue(<div>mock EmptyFile</div>)
     vi.mocked(getShellUpdateDataFiles).mockReturnValue(mockUsbData)
     vi.mocked(useAllCsvFilesQuery)
+    when(useAllCsvFilesQuery)
+      .calledWith(PROTOCOL_ID)
+      .thenReturn([] as any)
   })
   it('should render text and buttons', () => {
     render(props)
@@ -69,6 +81,15 @@ describe('ChooseCsvFile', () => {
     screen.getByText('CSV files on robot')
     screen.getByText('CSV files on USB')
     screen.getByText('Confirm selection')
+  })
+
+  it('should render csv file names', () => {
+    when(useAllCsvFilesQuery)
+      .calledWith(PROTOCOL_ID)
+      .thenReturn(mockDataOnRobot as any)
+    render(props)
+    screen.getByText('rtp_mock_file1.csv')
+    screen.getByText('rtp_mock_file2.csv')
     screen.getByText('mock-file1.csv')
     screen.getByText('mock-file2.csv')
     screen.getByText('mock-file3.csv')
@@ -83,7 +104,7 @@ describe('ChooseCsvFile', () => {
   it.todo('should call a mock function when tapping a csv file')
 
   it('should render mock empty file component when there is no csv file', () => {
-    vi.mocked(getShellUpdateDataFiles).mockReturnValue(mockEmptyUsbData)
+    vi.mocked(getShellUpdateDataFiles).mockReturnValue([])
     render(props)
     expect(screen.getAllByText('mock EmptyFile').length).toBe(2)
   })
