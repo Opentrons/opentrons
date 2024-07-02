f87426883f3853df77b3e101a43fb634b036ed25
Nicholas Shiland <nicholas.shiland@opentrons.com>
2024-07-02 10:32:54 -0400

    changed format of json file, now treats each robot name as a different item, changed code to match

--- abr-testing/abr_testing/tools/IP_grabber.py
+++ abr-testing/abr_testing/tools/IP_grabber.py
@@ -14,6 +14,7 @@ def get_ips(storage_directory: str) -> None:
 
     DO some things idk
     """
+    robot = input("Robot: ")
     #setup IP sheet
     ip_json_file = os.path.join(storage_directory, "IP_N_Volumes.json")
     #try to create an array copying the contents of IP_N_Volumes
@@ -22,8 +23,10 @@ def get_ips(storage_directory: str) -> None:
     except FileNotFoundError:
         print(f"Add .json file with robot IPs to: {storage_directory}.")
         sys.exit()
-    ip_address_list = ip_file["ip_address_list"]
-    for ip in ip_address_list:
+    robot_info = ip_file[robot]
+    
+    
+    for ip in robot_info:
         print("haha")
         #do smth to grab ip ad
 
@@ -32,4 +35,17 @@ def get_ips(storage_directory: str) -> None:
 OKAY, so basically I need to make a JSON source file with the robot name, IP's, and corresponding volume changes and put it into the thing
 
 need to find out how to read from a json file properly
+
+{'DVT1ABR1': 'IP address' : 'volume displaced'}
+{'DVT1ABR2': 'IP address' : ''}
+{'DVT1ABR3': 'IP address' : ''}
+{'DVT1ABR4': 'IP address' : 'volume displaced'}
+{'DVT2ABR5': 'IP address' : ''}
+{'DVT2ABR6': 'IP address' : ''}
+{'PVT1ABR7': 'IP address' : 'volume displaced'}
+{'PVT1ABR8': 'IP address' : 'volume displaced'}
+{'PVT1ABR9': 'IP address' : 'volume displaced'}
+{'PVT1ABR10': 'IP address' : 'volume displaced'}
+{'PVT1ABR11': 'IP address' : 'volume displaced'}
+{'PVT1ABR12': 'IP address' : ''}
 """
