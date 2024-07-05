5761a2424f0f1b03f5dfdbaa9456975c4c6b746d
CaseyBatten <caseyhbatten@gmail.com>
2024-07-05 14:10:42 -0400

    gravimetrics

--- hardware-testing/hardware_testing/gravimetric/helpers.py
+++ hardware-testing/hardware_testing/gravimetric/helpers.py
@@ -445,7 +445,7 @@ def _load_pipette(
     #       so we need to decrease the pick-up current to work with 1 tip.
     if pipette.channels == 8 and not increment and not photometric:
         pipette._core.configure_nozzle_layout(
-            style=NozzleLayout.SINGLE, primary_nozzle="A1", front_right_nozzle="A1"
+            style=NozzleLayout.SINGLE, primary_nozzle="A1", front_right_nozzle="A1", back_left_nozzle="A1"
         )
         # override deck conflict checking cause we specially lay out our tipracks
         DeckConflit.check_safe_for_pipette_movement = (
