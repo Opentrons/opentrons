3e9cf19550469fdd02cbe553e20ed3646ab8b5a2
Seth Foster <seth@opentrons.com>
2024-07-05 11:42:09 -0400

    feat(app): add CommandIndex component
    This is a little component that will render a command index on only
    desktop. We may want this to work slightly differently but it seems like
    a good start. It's worth componentizing because there are already and
    probably will be more weird little details around the width when used in
    a stack of command objects to make them not all different lengths.

---
+++ app/src/molecules/Command/CommandIndex.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+
+import {
+  StyledText,
+  RESPONSIVENESS,
+  Flex,
+  ALIGN_CENTER,
+  JUSTIFY_FLEX_START,
+} from '@opentrons/components'
+
+export interface CommandIndexProps {
+  index: string
+  allowSpaceForNDigits?: number
+}
+
+export function CommandIndex({
+  index,
+  allowSpaceForNDigits,
+}: CommandIndexProps): JSX.Element {
+  return (
+    <Flex
+      justifyContent={JUSTIFY_FLEX_START}
+      alignItems={ALIGN_CENTER}
+      width="100%"
+      maxWidth={`${Math.max(allowSpaceForNDigits ?? 0, 3)}ch`}
+      css={`
+        @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
+          display: none;
+        }
+      `}
+    >
+      <StyledText desktopStyle="captionRegular"> {index} </StyledText>
+    </Flex>
+  )
+}
