1f134eebbd03c3b664067d3f8dee464262207b3c
jbleon95 <jeremybenleon@gmail.com>
2024-07-09 16:08:13 -0400

    don't automatically parse csv or read contents

--- api/src/opentrons/protocols/parameters/types.py
+++ api/src/opentrons/protocols/parameters/types.py
@@ -7,30 +7,8 @@ from .exceptions import RuntimeParameterRequired, ParameterValueError
 class CSVParameter:
     def __init__(self, csv_file: Optional[TextIO]) -> None:
         self._file = csv_file
-        self._rows = []
-        self._contents = ""
-        if self._file is not None:
-            self._file.seek(0)
-            self._contents = self._file.read()
-            self._file.seek(0)
-            try:
-                dialect = csv.Sniffer().sniff(self._file.read(1024))
-                self._file.seek(0)
-                # Weird spacing with commas can sometimes produce a false positive for a non-comma delimiter,
-                # so overwrite that and enforce comma as separator for automatic row detection.
-                reader = csv.reader(self._file, dialect, delimiter=",")
-            except UnicodeDecodeError:
-                raise ParameterValueError("Cannot parse provided CSV file.")
-            except csv.Error:
-                # If we can't sniff the dialect than create the reader with the default and
-                # handle any potential failures when parsing rows
-                reader = csv.reader(self._file)
-            try:
-                for row in reader:
-                    self._rows.append(row)
-            except (UnicodeDecodeError, csv.Error):
-                raise ParameterValueError("Cannot parse provided CSV file.")
-            self._file.seek(0)
+        self._rows: List[List[str]] = []
+        self._contents: Optional[str] = None
 
     @property
     def file(self) -> TextIO:
@@ -44,10 +22,9 @@ class CSVParameter:
     @property
     def contents(self) -> str:
         """Returns the full contents of the CSV file as a single string."""
-        if self._file is None:
-            raise RuntimeParameterRequired(
-                "CSV parameter needs to be set to a file for full analysis or run."
-            )
+        if self._contents is None:
+            self.file.seek(0)
+            self._contents = self.file.read()
         return self._contents
 
     def rows(self) -> List[List[str]]:
@@ -56,10 +33,25 @@ class CSVParameter:
         If there is a header for the CSV that will be the first row in the list (i.e. `.rows()[0]`).
         All elements will be represented as strings, even if they are numeric in nature.
         """
-        if self._file is None:
-            raise RuntimeParameterRequired(
-                "CSV parameter needs to be set to a file for full analysis or run."
-            )
+        if not self._rows:
+            try:
+                dialect = csv.Sniffer().sniff(self.file.read(1024))
+                self.file.seek(0)
+                # Weird spacing with commas can sometimes produce a false positive for a non-comma delimiter,
+                # so overwrite that and enforce comma as separator for automatic row detection.
+                reader = csv.reader(self.file, dialect, delimiter=",")
+            except UnicodeDecodeError:
+                raise ParameterValueError("Cannot parse provided CSV file.")
+            except csv.Error:
+                # If we can't sniff the dialect than create the reader with the default and
+                # handle any potential failures when parsing rows
+                reader = csv.reader(self.file)
+            try:
+                for row in reader:
+                    self._rows.append(row)
+            except (UnicodeDecodeError, csv.Error):
+                raise ParameterValueError("Cannot parse provided CSV file.")
+            self.file.seek(0)
         return self._rows
 
 
