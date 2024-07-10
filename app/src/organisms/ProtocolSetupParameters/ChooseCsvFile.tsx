0834e471a415ff8bc8bcb216aa80abd279df423f
shiyaochen <shiyaochen@admins-MacBook-Pro.local>
2024-07-10 10:16:42 -0400

    feat(app): redirect the screen to protocol setup parameter when pressed confirm
    re AUTH-521

--- app/src/organisms/ProtocolSetupParameters/ChooseCsvFile.tsx
+++ app/src/organisms/ProtocolSetupParameters/ChooseCsvFile.tsx
@@ -68,6 +68,8 @@ export function ChooseCsvFile({
     setChooseValueScreen(null)
   }
 
+
+
   return (
     <>
       <ChildNavigation
--- app/src/organisms/ProtocolSetupParameters/index.tsx
+++ app/src/organisms/ProtocolSetupParameters/index.tsx
@@ -232,6 +232,8 @@ export function ProtocolSetupParameters({
                 ? t('required')
                 : parameter.displayName
 
+            console.log("confirm: "+csvFileInfo)
+
             let setupStatus: 'ready' | 'not ready' | 'general' | 'inform' =
               'inform'
             if (
