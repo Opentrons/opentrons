f3f013b096e372d3dcda4c2ca6ba26f72acea01e
Seth Foster <seth@opentrons.com>
2024-07-11 11:45:14 -0400

    floormat

--- app/src/organisms/ErrorRecoveryFlows/shared/TwoColTextAndFailedStepNextStep.tsx
+++ app/src/organisms/ErrorRecoveryFlows/shared/TwoColTextAndFailedStepNextStep.tsx
@@ -50,23 +50,23 @@ export function TwoColTextAndFailedStepNextStep({
   const { t } = useTranslation('error_recovery')
   const nthStepAfter = (n: number): number | undefined =>
     stepCounts.currentStepNumber == null
-    ? undefined
-    : stepCounts.currentStepNumber + n
+      ? undefined
+      : stepCounts.currentStepNumber + n
   const nthCommand = (n: number): typeof failedCommand =>
     commandsAfterFailedCommand != null
-    ? n < commandsAfterFailedCommand.length
+      ? n < commandsAfterFailedCommand.length
         ? commandsAfterFailedCommand[n]
         : null
-        : null
+      : null
 
   const commandsAfter = [nthCommand(0), nthCommand(1)] as const
 
   const indexedCommandsAfter = [
     commandsAfter[0] != null
-    ? { command: commandsAfter[0], index: nthStepAfter(1) }
-    : null,
+      ? { command: commandsAfter[0], index: nthStepAfter(1) }
+      : null,
     commandsAfter[1] != null
-    ? { command: commandsAfter[1], index: nthStepAfter(2) }
+      ? { command: commandsAfter[1], index: nthStepAfter(2) }
       : null,
   ] as const
 
