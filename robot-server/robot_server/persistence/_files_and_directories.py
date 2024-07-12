fceb9f7c8ce820d26c7fda90ee31e100b44d24ec
Sanniti <sanniti@opentrons.com>
2024-07-12 12:14:39 -0400

    remove rtp col from analysis, add new rtp table

---
+++ robot-server/robot_server/persistence/_files_and_directories.py
@@ -0,0 +1,6 @@
+"""Files and directories in persistent storage."""
+from typing import Final
+
+DECK_CONFIGURATION_FILE: Final = "deck_configuration.json"
+PROTOCOLS_DIRECTORY: Final = "protocols"
+DB_FILE: Final = "robot_server.db"
