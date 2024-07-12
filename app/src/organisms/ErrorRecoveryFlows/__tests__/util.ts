68af2c6f030b8bd0e9f9c06286ea7213a635dfa4
Seth Foster <seth@opentrons.com>
2024-07-12 16:19:15 -0400

    format

--- app/src/organisms/ErrorRecoveryFlows/__tests__/util.ts
+++ app/src/organisms/ErrorRecoveryFlows/__tests__/util.ts
@@ -1,6 +1,6 @@
 import { screen, fireEvent } from '@testing-library/react'
 
 export function clickButtonLabeled(label: string): void {
-    const buttons = screen.getAllByRole('button', { name: label })
-    fireEvent.click(buttons[0])
+  const buttons = screen.getAllByRole('button', { name: label })
+  fireEvent.click(buttons[0])
 }
