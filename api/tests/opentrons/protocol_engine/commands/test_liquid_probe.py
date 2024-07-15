03a345f80dcac62e6ab2a505f01648482d4b7b6b
Max Marrone <max@opentrons.com>
2024-07-15 14:38:40 -0400

    Update liquidProbe/tryLiquidProbe unit tests.

--- api/tests/opentrons/protocol_engine/commands/test_liquid_probe.py
+++ api/tests/opentrons/protocol_engine/commands/test_liquid_probe.py
@@ -1,5 +1,6 @@
 """Test LiquidProbe commands."""
 from datetime import datetime
+from typing import Type, Union
 
 from opentrons.protocol_engine.errors.exceptions import (
     MustHomeError,
@@ -23,6 +24,8 @@ from opentrons.protocol_engine.commands.liquid_probe import (
     LiquidProbeParams,
     LiquidProbeResult,
     LiquidProbeImplementation,
+    TryLiquidProbeResult,
+    TryLiquidProbeImplementation,
 )
 from opentrons.protocol_engine.commands.command import DefinedErrorData, SuccessData
 
@@ -36,14 +39,51 @@ from opentrons.protocol_engine.resources.model_utils import ModelUtils
 from opentrons.protocol_engine.types import CurrentWell, LoadedPipette
 
 
+EitherImplementationType = Union[
+    Type[LiquidProbeImplementation], Type[TryLiquidProbeImplementation]
+]
+EitherImplementation = Union[LiquidProbeImplementation, TryLiquidProbeImplementation]
+EitherResultType = Union[Type[LiquidProbeResult], Type[TryLiquidProbeResult]]
+
+
+@pytest.fixture(
+    params=[
+        (LiquidProbeImplementation, LiquidProbeResult),
+        (TryLiquidProbeImplementation, TryLiquidProbeResult),
+    ]
+)
+def implementation_and_result_types(
+    request: pytest.FixtureRequest,
+) -> tuple[EitherImplementationType, EitherResultType]:
+    """Return an implementation class and the result class associated with it."""
+    return request.param  # type: ignore[no-any-return]
+
+
+@pytest.fixture
+def implementation_type(
+    implementation_and_result_types: tuple[EitherImplementationType, object]
+) -> EitherImplementationType:
+    """Return an implementation class. This is kept in sync with `result_type`."""
+    return implementation_and_result_types[0]
+
+
+@pytest.fixture
+def result_type(
+    implementation_and_result_types: tuple[object, EitherResultType]
+) -> EitherResultType:
+    """Return a result class. This is kept in sync with `implementation_type`."""
+    return implementation_and_result_types[1]
+
+
 @pytest.fixture
 def subject(
+    implementation_type: EitherImplementationType,
     movement: MovementHandler,
     pipetting: PipettingHandler,
     model_utils: ModelUtils,
-) -> LiquidProbeImplementation:
+) -> Union[LiquidProbeImplementation, TryLiquidProbeImplementation]:
     """Get the implementation subject."""
-    return LiquidProbeImplementation(
+    return implementation_type(
         pipetting=pipetting,
         movement=movement,
         model_utils=model_utils,
@@ -54,7 +94,8 @@ async def test_liquid_probe_implementation_no_prep(
     decoy: Decoy,
     movement: MovementHandler,
     pipetting: PipettingHandler,
-    subject: LiquidProbeImplementation,
+    subject: EitherImplementation,
+    result_type: EitherResultType,
 ) -> None:
     """A Liquid Probe should have an execution implementation without preparing to aspirate."""
     location = WellLocation(origin=WellOrigin.BOTTOM, offset=WellOffset(x=0, y=0, z=1))
@@ -89,8 +130,9 @@ async def test_liquid_probe_implementation_no_prep(
 
     result = await subject.execute(data)
 
+    assert type(result.public) is result_type  # Pydantic v1 only compares the fields.
     assert result == SuccessData(
-        public=LiquidProbeResult(z_position=15.0, position=DeckPoint(x=1, y=2, z=3)),
+        public=result_type(z_position=15.0, position=DeckPoint(x=1, y=2, z=3)),
         private=None,
     )
 
@@ -100,7 +142,8 @@ async def test_liquid_probe_implementation_with_prep(
     state_view: StateView,
     movement: MovementHandler,
     pipetting: PipettingHandler,
-    subject: LiquidProbeImplementation,
+    subject: EitherImplementation,
+    result_type: EitherResultType,
 ) -> None:
     """A Liquid Probe should have an execution implementation with preparing to aspirate."""
     location = WellLocation(origin=WellOrigin.TOP, offset=WellOffset(x=0, y=0, z=0))
@@ -140,19 +183,10 @@ async def test_liquid_probe_implementation_with_prep(
 
     result = await subject.execute(data)
 
+    assert type(result.public) is result_type  # Pydantic v1 only compares the fields.
     assert result == SuccessData(
-        public=LiquidProbeResult(z_position=15.0, position=DeckPoint(x=1, y=2, z=3)),
+        public=result_type(z_position=15.0, position=DeckPoint(x=1, y=2, z=3)),
         private=None,
-    )
-
-    decoy.verify(
-        await movement.move_to_well(
-            pipette_id="abc",
-            labware_id="123",
-            well_name="A3",
-            well_location=WellLocation(origin=WellOrigin.TOP),
-            current_well=current_well,
-        ),
     )
 
 
@@ -160,7 +194,7 @@ async def test_liquid_not_found_error(
     decoy: Decoy,
     movement: MovementHandler,
     pipetting: PipettingHandler,
-    subject: LiquidProbeImplementation,
+    subject: EitherImplementation,
     model_utils: ModelUtils,
 ) -> None:
     """It should return a liquid not found error if the hardware API indicates that."""
@@ -213,22 +247,31 @@ async def test_liquid_not_found_error(
 
     result = await subject.execute(data)
 
-    assert result == DefinedErrorData(
-        public=LiquidNotFoundError.construct(
-            id=error_id, createdAt=error_timestamp, wrappedErrors=[matchers.Anything()]
-        ),
-        private=LiquidNotFoundErrorInternalData(
-            position=DeckPoint(x=position.x, y=position.y, z=position.z)
-        ),
-    )
+    if isinstance(subject, LiquidProbeImplementation):
+        assert result == DefinedErrorData(
+            public=LiquidNotFoundError.construct(
+                id=error_id,
+                createdAt=error_timestamp,
+                wrappedErrors=[matchers.Anything()],
+            ),
+            private=LiquidNotFoundErrorInternalData(
+                position=DeckPoint(x=position.x, y=position.y, z=position.z)
+            ),
+        )
+    else:
+        assert result == SuccessData(
+            public=TryLiquidProbeResult(
+                z_position=None,
+                position=DeckPoint(x=position.x, y=position.y, z=position.z),
+            ),
+            private=None,
+        )
 
 
 async def test_liquid_probe_tip_checking(
     decoy: Decoy,
-    movement: MovementHandler,
     pipetting: PipettingHandler,
-    subject: LiquidProbeImplementation,
-    model_utils: ModelUtils,
+    subject: EitherImplementation,
 ) -> None:
     """It should return a TipNotAttached error if the hardware API indicates that."""
     pipette_id = "pipette-id"
@@ -256,10 +299,8 @@ async def test_liquid_probe_tip_checking(
 
 async def test_liquid_probe_volume_checking(
     decoy: Decoy,
-    movement: MovementHandler,
     pipetting: PipettingHandler,
-    subject: LiquidProbeImplementation,
-    model_utils: ModelUtils,
+    subject: EitherImplementation,
 ) -> None:
     """It should return a TipNotEmptyError if the hardware API indicates that."""
     pipette_id = "pipette-id"
@@ -285,9 +326,7 @@ async def test_liquid_probe_volume_checking(
 async def test_liquid_probe_location_checking(
     decoy: Decoy,
     movement: MovementHandler,
-    pipetting: PipettingHandler,
-    subject: LiquidProbeImplementation,
-    model_utils: ModelUtils,
+    subject: EitherImplementation,
 ) -> None:
     """It should return a PositionUnkownError if the hardware API indicates that."""
     pipette_id = "pipette-id"
