860df0dd00903922a41691831780125a31bd3ac9
rclarke0 <rhyann.clarke@opentrons.com>
2024-07-16 14:45:52 -0400

    feat(abr-testing): standardize lpc data

--- abr-testing/abr_testing/tools/abr_lpc.py
+++ abr-testing/abr_testing/tools/abr_lpc.py
@@ -3,37 +3,28 @@ import os
 import argparse
 from abr_testing.automation import google_sheets_tool
 import sys
+import pandas as pd
 
+def standardize_categorical_data(df: pd.DataFrame)-> pd.DataFrame:
+    """Replaces strings with numberical value."""
+    columns = ["Robot", "Labware Type", "Module", "Adapter", "Slot"]
+    for col in columns:
+        df = df.copy()
+        new_column_name = col + "ID"
+        df.loc[:,col] = df[col].replace("",None)
+        labels, unique_values = pd.factorize(df[col])
+        df.loc[:,new_column_name] = labels
+    return df
 
-def remove_duplicate_data() -> None:
+def remove_duplicate_data(df: pd.DataFrame) -> pd.DataFrame:
     """Determine unique sets of data."""
-    seen = set()
-    new_values = []
-    row_indices = []
-    headers = google_sheet_lpc.get_row(1)
-    sheet_data = google_sheet_lpc.get_all_data(headers)
-    for i, row in enumerate(sheet_data):
-        key = (
-            row["Robot"],
-            row["Software Version"],
-            row["Errors"],
-            row["Slot"],
-            row["Module"],
-            row["Adapter"],
-            row["X"],
-            row["Y"],
-            row["Z"],
-        )
-
-        if key not in seen:
-            seen.add(key)
-            new_values.append(row)
-        else:
-            row_indices.append(i)
-    if len(row_indices) > 0:
-        google_sheet_lpc.batch_delete_rows(row_indices)
-
-
+    len_before = len(df)
+    column_for_duplicate_removal =["Robot", "Software Version", "createdAt", "Errors", "Slot", "Labware Type", "X", "Y", "Z"]
+    df_no_duplicates = df.drop_duplicates(subset = column_for_duplicate_removal)
+    len_after = len(df_no_duplicates)
+    print(f"Length before: {len_before}. Length after: {len_after}")
+    return df_no_duplicates
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Read run logs on google drive.")
     parser.add_argument(
@@ -50,10 +41,22 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print(f"Add credentials.json file to: {storage_directory}.")
         sys.exit()
-
+    # Connect to LPC google sheet
     google_sheet_lpc = google_sheets_tool.google_sheet(credentials_path, "ABR-LPC", 0)
     headers = google_sheet_lpc.get_row(1)
-    print(len(google_sheet_lpc.get_all_data(headers)))
-    remove_duplicate_data()
-    num_of_rows = print(len(google_sheet_lpc.get_all_data(headers)))
-    # TODO: automate data analysis
+    all_lpc_data = google_sheet_lpc.get_all_data(headers)    
+    df_lpc_data = pd.DataFrame(all_lpc_data)
+    
+    # Connect to calibration sheet
+    google_sheet_calibration = google_sheets_tool.google_sheet(credentials_path, "ABR-Calibration-Data", 0)
+    headers = google_sheet_calibration.get_row(1)
+    all_calibration_data = google_sheet_calibration.get_all_data(headers)    
+    df_calibration_data = pd.DataFrame(all_calibration_data)
+    
+    # Clean up data
+    df_no_duplicates = remove_duplicate_data(df_lpc_data)
+    df_categorized = standardize_categorical_data(df_no_duplicates)
+    # Match up LPC and calibration dates
+    
+    # Determine correlation
+    
