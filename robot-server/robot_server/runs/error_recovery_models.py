d51fb915c63b51cec37408763a491e1000b4f407
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2024-07-18 14:29:06 -0400

    lint

--- robot-server/robot_server/runs/error_recovery_models.py
+++ robot-server/robot_server/runs/error_recovery_models.py
@@ -2,12 +2,17 @@
 from enum import Enum
 from pydantic import BaseModel, Field
 
-from robot_server.service.json_api import ResourceModel
-
 
 class valueMatchType(str, Enum):
+    """The type of the error recovery setting.
 
-    IGNORE_AND_CONTINUE = "ignore"
+    * `"ignore-and-continue"`: Ignore this error and future errors of the same type.
+    * `"fail-run"`: Errors of this type should fail the run.
+    * `"wait-for-recovery"`: Instances of this error should initiate a recover operation.
+
+    """
+
+    IGNORE_AND_CONTINUE = "ignore-and-continue"
     FAIL_RUN = "fail-run"
     WAIT_FOR_RECOVERY = "wait-for-recovery"
 
@@ -15,6 +20,11 @@ class valueMatchType(str, Enum):
 class ErrorRecoveryRule(BaseModel):
     """Request model for new error recovery rule creation."""
 
-    commandType: str
-    errorType: str
-    ifMatch: valueMatchType
+    commandType: str = Field(
+        ..., description="The command type that this rule applies to."
+    )
+    errorType: str = Field(..., description="The error type that this rule applies to.")
+    ifMatch: valueMatchType = Field(
+        ...,
+        description="The specific recovery setting that will be in use if the type parameters match.",
+    )
