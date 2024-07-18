f2b3788a9b5834707f20bc9289873631565cae81
koji <kojikoji@skiff.com>
2024-07-17 23:37:08 -0400

    update vitest and vitest plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,6 +14,10 @@ export default mergeConfig(
       allowOnly: true,
       exclude: [...configDefaults.exclude, '**/node_modules/**', '**/dist/**'],
       setupFiles: ['./setup-vitest.ts'],
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+      },
     },
     resolve: {
       alias: {
