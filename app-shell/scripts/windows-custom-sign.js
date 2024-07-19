aced4cf95a9d931997c1ad28a627ea57ee7b9de6
Seth Foster <seth@opentrons.com>
2024-07-19 15:45:35 -0400

    healthcheck and verify

--- app-shell/scripts/windows-custom-sign.js
+++ app-shell/scripts/windows-custom-sign.js
@@ -5,9 +5,9 @@
 const { execSync } = require('node:child_process')
 
 exports.default = async configuration => {
-  const cmd = `smctl sign --fingerprint="${
-    process.env.SM_CODE_SIGNING_CERT_SHA1_HASH
-  }" --input "${String(
+  const signCmd = `smctl sign --keypair-alias="${String(
+    process.env.SM_KEYPAIR_ALIAS
+  )}" --input "${String(
     configuration.path
   )}" --exit-non-zero-on-fail --failfast --verbose`
   console.log(cmd)
@@ -21,11 +21,36 @@ exports.default = async configuration => {
     console.log(`Sign stderr: ${stderr.toString()}`)
     console.log(`Sign code: ${process.code}`)
   } catch (err) {
-    console.error(
-      `Exception running sign: ${
-        err.status
-      }! Output: ${err.stdout.toString()} Error: ${err.stdout.toString()}`
-    )
+    console.error(`Exception running sign: ${err.status}!
+Process stdout:
+ ${err.stdout.toString()}
+-------------
+Process stderr:
+${err.stdout.toString()}
+-------------
+`)
+    throw err
+  }
+  const verifyCmd = `smctl sign verify --fingerprint-string="${String(
+    process.env.SM_CODE_SIGNING_CERT_SHA1_HASH
+  )}" --input="${String(configuration.path)}" --verbose`
+  console.log(verifyCmd)
+  try {
+    const verifyProcess = execSync(verifyCmd, { stdio: 'pipe' })
+    const stdout = process.stdout.read()
+    const stderr = process.stderr.read()
+    console.log(`Verify stdout: ${stdout}`)
+    console.log(`Verify stderr: ${stderr}`)
+  } catch (err) {
+    console.error(`
+Exception running verification: ${err.status}!
+Process stdout:
+ ${err.stdout.toString()}
+--------------
+Process stderr:
+ ${err.stderr.toString()}
+--------------
+`)
     throw err
   }
 }
