7eb8f04333264bac2342f9f7426ccec0738c84e6
Seth Foster <seth@opentrons.com>
2024-07-19 14:33:14 -0400

    add thumbprint and print more

--- app-shell/scripts/windows-custom-sign.js
+++ app-shell/scripts/windows-custom-sign.js
@@ -6,18 +6,22 @@ const { execSync } = require('node:child_process')
 
 exports.default = async configuration => {
   try {
-    execSync(
-      `smctl sign --fingerprint="${
-        process.env.SM_CODE_SIGNING_CERT_SHA1_HASH
-      }" --input "${String(configuration.path)}"`,
-      {
-        stdio: 'inherit',
-      }
-    )
-    console.log(
-      `Signed ${configuration.path} with ${process.env.SM_CODE_SIGNING_CERT_SHA1_HASH}`
-    )
-  } catch (error) {
-    console.error(`Signing ${configuration.path}: failed:`, error)
-  }
+    const cmd = `smctl sign --fingerprint="${
+      process.env.SM_CODE_SIGNING_CERT_SHA1_HASH
+    }" --input "${String(
+      configuration.path
+    )}" --exit-non-zero-on-fail --failfast --verbose`
+    console.log(cmd)
+    try {
+       const process = execSync(cmd, {
+         stdio: 'pipe',
+       })
+    } catch (err) {
+      console.log(`Exception running sign: ${err.code}`)
+    }
+    const stdout = process.stdout.read()
+    const stderr = process.stderr.read()
+    console.log(`Sign stdout: ${stdout.toString()}`)
+    console.log(`Sign stderr: ${stderr.toString()}`)
+    console.log(`Sign code: ${process.code}`)
 }
