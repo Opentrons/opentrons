bc1fa6403507275377b6cb771410c307ecc59e18
Seth Foster <seth@opentrons.com>
2024-07-19 14:44:06 -0400

    blind

--- app-shell/scripts/windows-custom-sign.js
+++ app-shell/scripts/windows-custom-sign.js
@@ -5,23 +5,22 @@
 const { execSync } = require('node:child_process')
 
 exports.default = async configuration => {
+  const cmd = `smctl sign --fingerprint="${
+    process.env.SM_CODE_SIGNING_CERT_SHA1_HASH
+  }" --input "${String(
+    configuration.path
+  )}" --exit-non-zero-on-fail --failfast --verbose`
+  console.log(cmd)
   try {
-    const cmd = `smctl sign --fingerprint="${
-      process.env.SM_CODE_SIGNING_CERT_SHA1_HASH
-    }" --input "${String(
-      configuration.path
-    )}" --exit-non-zero-on-fail --failfast --verbose`
-    console.log(cmd)
-    try {
-       const process = execSync(cmd, {
-         stdio: 'pipe',
-       })
-    } catch (err) {
-      console.log(`Exception running sign: ${err.code}`)
-    }
+    const process = execSync(cmd, {
+      stdio: 'pipe',
+    })
     const stdout = process.stdout.read()
     const stderr = process.stderr.read()
     console.log(`Sign stdout: ${stdout.toString()}`)
     console.log(`Sign stderr: ${stderr.toString()}`)
     console.log(`Sign code: ${process.code}`)
+  } catch (err) {
+    console.log(`Exception running sign: ${err.code}`)
+  }
 }
