b5f84b6eec4c835f180c146c604b5e1aae6ec089
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2024-07-19 11:05:42 -0400

    fix(cli): try to fix packaging imoprt
    Put packaging package in all pipfiles and hope that it fixes dependency issue
    
    BREAKING CHANGE: im not sure

--- scripts/python_build_utils.py
+++ scripts/python_build_utils.py
@@ -9,6 +9,7 @@ import json
 import os
 import subprocess
 import sys
+import packaging
 from collections import namedtuple
 
 
@@ -52,16 +53,6 @@ def get_version(package, project, extra_tag='', git_dir=None):
     return version
 
 def normalize_version(package, project, extra_tag='', git_dir=None):
-    # Pipenv requires setuptools >= 36.2.1. Since 36.2.1, setuptools changed
-    # the way they vendor dependencies, like the packaging module that
-    # provides the way to normalize version numbers for wheel file names. So
-    # we try all the possible ways to find it.
-    try:
-        # new way
-        from setuptools.extern import packaging
-    except ImportError:
-        # old way
-        from pkg_resources.extern import packaging
     vers_obj = packaging.version.Version(get_version(package, project, extra_tag, git_dir))
     return str(vers_obj)
 
