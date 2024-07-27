444307425436fe698106f4a62624c6ef3048608e
koji <kojikoji@skiff.com>
2024-07-26 23:57:18 -0400

    docs(labware-library): remove description about webpack
    remove the description about webpack since we switched from webpack to vite
    
    close AUTH-

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  stories: [
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../app/**/*.stories.@(js|jsx|ts|tsx)',
+    '../opentrons-ai-client/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-pseudo-states',
+  ],
+
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+
+  docs: {
+    autodocs: true,
+  },
+}
