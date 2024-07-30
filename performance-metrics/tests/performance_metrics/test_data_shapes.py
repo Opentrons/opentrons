79d91c51461b3bb2c496cf7e89ce3ef653be263c
Derek Maggio <derek.j.maggio@gmail.com>
2024-07-30 09:27:32 -0700

    add tests

---
+++ performance-metrics/tests/performance_metrics/test_data_shapes.py
@@ -0,0 +1,50 @@
+from performance_metrics._data_shapes import ProcessResourceUsageSnapshot
+
+def test_headers_ordering() -> None: 
+    assert ProcessResourceUsageSnapshot.headers() == (
+        "query_time",
+        "command",
+        "running_since",
+        "user_cpu_time",
+        "system_cpu_time",
+        "memory_percent",
+    )
+
+def test_csv_row_method_ordering() -> None:
+    expected = (
+        1,
+        "test",
+        2,
+        3,
+        4,
+        5,
+    )
+
+    assert ProcessResourceUsageSnapshot(
+        query_time=1,
+        command="test",
+        running_since=2,
+        user_cpu_time=3,
+        system_cpu_time=4,
+        memory_percent=5,
+    ).csv_row() == expected
+
+    assert ProcessResourceUsageSnapshot(
+        command="test",
+        query_time=1,
+        user_cpu_time=3,
+        system_cpu_time=4,
+        running_since=2,
+        memory_percent=5,
+    ).csv_row() == expected
+
+    assert ProcessResourceUsageSnapshot.from_csv_row(
+        (
+            1,
+            "test",
+            2,
+            3,
+            4,
+            5,
+        )
+    ).csv_row() == expected
