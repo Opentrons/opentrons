905258beb83728bb01f586c538332b8e7c22dc72
Seth Foster <seth@opentrons.com>
2024-07-30 14:09:50 -0400

    refactor(app): Add more host overrides

--- app/src/resources/runs/useCurrentRunId.ts
+++ app/src/resources/runs/useCurrentRunId.ts
@@ -3,11 +3,17 @@ import { useNotifyAllRunsQuery } from './useNotifyAllRunsQuery'
 import type { AxiosError } from 'axios'
 import type { UseAllRunsQueryOptions } from '@opentrons/react-api-client/src/runs/useAllRunsQuery'
 import type { QueryOptionsWithPolling } from '../useNotifyDataReady'
+import type { HostConfig } from '@opentrons/api-client'
 
 export function useCurrentRunId(
-  options: QueryOptionsWithPolling<UseAllRunsQueryOptions, AxiosError> = {}
+  options: QueryOptionsWithPolling<UseAllRunsQueryOptions, AxiosError> = {},
+  hostOverride?: HostConfig | null
 ): string | null {
-  const { data: allRuns } = useNotifyAllRunsQuery({ pageLength: 0 }, options)
+  const { data: allRuns } = useNotifyAllRunsQuery(
+    { pageLength: 0 },
+    options,
+    hostOverride
+  )
   const currentRunLink = allRuns?.links?.current ?? null
   return currentRunLink != null &&
     typeof currentRunLink !== 'string' &&
