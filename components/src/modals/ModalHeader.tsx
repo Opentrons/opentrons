11f525639b8f864035ffcb9ba3d694238eb46863
Jethary <jethary@opentrons.com>
2024-08-02 16:31:57 -0400

    fix prettier

--- components/src/modals/ModalHeader.tsx
+++ components/src/modals/ModalHeader.tsx
@@ -4,11 +4,7 @@ import { css } from 'styled-components'
 import { Icon } from '../icons'
 import { Box, Btn, Flex } from '../primitives'
 import { LegacyStyledText } from '../atoms'
-import {
-  ALIGN_CENTER,
-  JUSTIFY_CENTER,
-  JUSTIFY_SPACE_BETWEEN,
-} from '../styles'
+import { ALIGN_CENTER, JUSTIFY_CENTER, JUSTIFY_SPACE_BETWEEN } from '../styles'
 import { SPACING, TYPOGRAPHY } from '../ui-style-constants'
 import { COLORS } from '../helix-design-system'
 import type { IconProps } from '../icons'
