07bbc7cc409fdcc75f011110b04e9f914c75ed2c
Seth Foster <seth@opentrons.com>
2024-08-05 11:54:18 -0400

    one last little lint

--- app/src/organisms/Devices/ProtocolRun/SetupLiquids/__tests__/SetupLiquids.test.tsx
+++ app/src/organisms/Devices/ProtocolRun/SetupLiquids/__tests__/SetupLiquids.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { describe, it, beforeEach, vi, MockedFunction } from 'vitest'
+import { describe, it, beforeEach, vi } from 'vitest'
 import { screen, fireEvent } from '@testing-library/react'
 
 import { renderWithProviders } from '../../../../../__testing-utils__'
