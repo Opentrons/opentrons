cb0bdbfd61cfc9a2323492e3d24eb7f366c43df4
wweiye <275241708@qq.com>
2024-08-05 16:51:10 +0800

    add lifetime

--- hardware-testing/hardware_testing/production_qc/ninety_six_assembly_qc_ot3/test_plunger.py
+++ hardware-testing/hardware_testing/production_qc/ninety_six_assembly_qc_ot3/test_plunger.py
@@ -12,7 +12,7 @@ from hardware_testing.data.csv_report import (
 )
 from hardware_testing.opentrons_api import helpers_ot3
 from hardware_testing.opentrons_api.types import Axis, OT3Mount
-
+import time
 PLUNGER_MAX_SKIP_MM = 0.1
 SPEEDS_TO_TEST: List[float] = [5, 15, 22]
 CURRENTS_SPEEDS: Dict[float, List[float]] = {
@@ -74,52 +74,100 @@ async def run(api: OT3API, report: CSVReport, section: str) -> None:
     slot_5 = helpers_ot3.get_slot_calibration_square_position_ot3(5)
     home_pos = await api.gantry_position(OT3Mount.LEFT)
     await api.move_to(OT3Mount.LEFT, slot_5._replace(z=home_pos.z))
+    # runtime = input("请输入你需要运行的时间:(S)")
+    # T1=time.time()
+    # T2=time.time()
+    # TDIF = T2 - T1
+    # while TDIF <= runtime:
+    #     ui.print_info(f"运行时间:{TDIF}S")
+    #     current = 1.5
+    #     speed = 5.5
+    #     ui.print_header(f"CURRENT: {current}, SPEED: {speed}")
+    #     # HOME
+    #     print("homing...")
+    #     await api.home([ax])
+    #     print(f"lowering run-current to {current} amps")
+    #     await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(
+    #         api,
+    #         ax,
+    #         run_current=current,
+    #     )
+    #     await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(
+    #         api, ax, default_max_speed=speed
+    #     )
+    #     # MOVE DOWN
+    #     print(f"moving down {blow_out} mm at {speed} mm/sec")
+    #     await _save_result(_get_test_tag(current, speed, "down", "start"))
+    #     await helpers_ot3.move_plunger_absolute_ot3(
+    #         api, mount, blow_out, speed=speed, motor_current=current
+    #     )
+    #     down_passed = await _save_result(
+    #         _get_test_tag(current, speed, "down", "end")
+    #     )
+    #     # MOVE UP
+    #     print(f"moving up {blow_out} mm at {speed} mm/sec")
+    #     await _save_result(_get_test_tag(current, speed, "up", "start"))
+    #     await helpers_ot3.move_plunger_absolute_ot3(
+    #         api, mount, 0, speed=speed, motor_current=current
+    #     )
+    #     up_passed = await _save_result(_get_test_tag(current, speed, "up", "end"))
+    #     T2=time.time()
+    #     TDIF = T2 - T1
 
     # LOOP THROUGH CURRENTS + SPEEDS
+    
     currents = list(CURRENTS_SPEEDS.keys())
-    for current in sorted(currents, reverse=True):
-        speeds = CURRENTS_SPEEDS[current]
-        for speed in sorted(speeds, reverse=False):
-            ui.print_header(f"CURRENT: {current}, SPEED: {speed}")
-            # HOME
-            print("homing...")
-            await api.home([ax])
-            print(f"lowering run-current to {current} amps")
-            await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(
-                api,
-                ax,
-                run_current=current,
-            )
-            await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(
-                api, ax, default_max_speed=speed
-            )
-            # MOVE DOWN
-            print(f"moving down {blow_out} mm at {speed} mm/sec")
-            await _save_result(_get_test_tag(current, speed, "down", "start"))
-            await helpers_ot3.move_plunger_absolute_ot3(
-                api, mount, blow_out, speed=speed, motor_current=current
-            )
-            down_passed = await _save_result(
-                _get_test_tag(current, speed, "down", "end")
-            )
-            # MOVE UP
-            print(f"moving up {blow_out} mm at {speed} mm/sec")
-            await _save_result(_get_test_tag(current, speed, "up", "start"))
-            await helpers_ot3.move_plunger_absolute_ot3(
-                api, mount, 0, speed=speed, motor_current=current
-            )
-            up_passed = await _save_result(_get_test_tag(current, speed, "up", "end"))
-            # RESET CURRENTS AND HOME
-            print("homing...")
-            await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(
-                api, ax, run_current=default_current
-            )
-            await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(
-                api, ax, default_max_speed=default_speed
-            )
-            await api._backend.set_active_current({Axis.P_L: default_current})
-            await api.home([ax])
-            if not down_passed or not up_passed and not api.is_simulator:
-                print(f"current {current} failed")
-                print("skipping any remaining speeds at this current")
-                break
+    runtime = input("请输入你需要运行的时间:(S)")
+    T1=time.time()
+    T2=time.time()
+    TDIF = T2 - T1
+    while TDIF <= runtime:
+        ui.print_info(f"运行时间:{TDIF}S")
+        for current in sorted(currents, reverse=True):
+            speeds = CURRENTS_SPEEDS[current]
+            for speed in sorted(speeds, reverse=False):
+                ui.print_header(f"CURRENT: {current}, SPEED: {speed}")
+                # HOME
+                print("homing...")
+                await api.home([ax])
+                print(f"lowering run-current to {current} amps")
+                await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(
+                    api,
+                    ax,
+                    run_current=current,
+                )
+                await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(
+                    api, ax, default_max_speed=speed
+                )
+                # MOVE DOWN
+                print(f"moving down {blow_out} mm at {speed} mm/sec")
+                await _save_result(_get_test_tag(current, speed, "down", "start"))
+                await helpers_ot3.move_plunger_absolute_ot3(
+                    api, mount, blow_out, speed=speed, motor_current=current
+                )
+                down_passed = await _save_result(
+                    _get_test_tag(current, speed, "down", "end")
+                )
+                # MOVE UP
+                print(f"moving up {blow_out} mm at {speed} mm/sec")
+                await _save_result(_get_test_tag(current, speed, "up", "start"))
+                await helpers_ot3.move_plunger_absolute_ot3(
+                    api, mount, 0, speed=speed, motor_current=current
+                )
+                up_passed = await _save_result(_get_test_tag(current, speed, "up", "end"))
+                # RESET CURRENTS AND HOME
+                print("homing...")
+                await helpers_ot3.set_gantry_load_per_axis_current_settings_ot3(
+                    api, ax, run_current=default_current
+                )
+                await helpers_ot3.set_gantry_load_per_axis_motion_settings_ot3(
+                    api, ax, default_max_speed=default_speed
+                )
+                await api._backend.set_active_current({Axis.P_L: default_current})
+                await api.home([ax])
+                # if not down_passed or not up_passed and not api.is_simulator:
+                #     print(f"current {current} failed")
+                #     print("skipping any remaining speeds at this current")
+                #     break
+        T2=time.time()
+        TDIF = T2 - T1
