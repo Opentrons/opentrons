d43913932d88ee792011e5185251fb3d22c326cf
Seth Foster <seth@opentrons.com>
2024-08-09 16:24:52 -0400

    that's not a type

--- app-shell/src/usb.ts
+++ app-shell/src/usb.ts
@@ -18,7 +18,7 @@ import {
 } from './constants'
 
 import type { IpcMainInvokeEvent } from 'electron'
-import type { AxiosRequestConfig, isAxiosError } from 'axios'
+import { AxiosRequestConfig, isAxiosError } from 'axios'
 import type { IPCSafeFormData } from '@opentrons/app/src/redux/shell/types'
 import type { UsbDevice } from '@opentrons/app/src/redux/system-info/types'
 import type { PortInfo } from '@opentrons/usb-bridge/node-client'
