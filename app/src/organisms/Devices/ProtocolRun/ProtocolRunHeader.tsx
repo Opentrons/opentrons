d850730607ece5d3206d605dd9ed7b0b3a64fc87
tamarzanzouri <tamar.zanzouri@opentrons.com>
2024-08-09 12:52:43 -0400

    revert debug change

--- app/src/organisms/Devices/ProtocolRun/ProtocolRunHeader.tsx
+++ app/src/organisms/Devices/ProtocolRun/ProtocolRunHeader.tsx
@@ -661,8 +661,9 @@ function ActionButton(props: ActionButtonProps): JSX.Element {
   )
   const [showIsShakingModal, setShowIsShakingModal] = React.useState(false)
   const isSetupComplete =
-    // isCalibrationComplete &&
-    isModuleCalibrationComplete && missingModuleIds.length === 0
+    isCalibrationComplete &&
+    isModuleCalibrationComplete &&
+    missingModuleIds.length === 0
   const isRobotOnWrongVersionOfSoftware = ['upgrade', 'downgrade'].includes(
     useSelector((state: State) => {
       return getRobotUpdateDisplayInfo(state, robotName)
