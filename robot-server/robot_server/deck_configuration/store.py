f2cbe697407e54a39351acc3b42b7875e600bc52
Max Marrone <max@opentrons.com>
2024-08-15 11:11:13 -0400

    Add store.get_for_cli() side channel.

--- robot-server/robot_server/deck_configuration/store.py
+++ robot-server/robot_server/deck_configuration/store.py
@@ -109,6 +109,40 @@ class DeckConfigurationStore:  # noqa: D101
             return _storage_types_to_http_types(from_storage)
 
 
+async def get_for_cli(deck_type: DeckType, path: Path) -> bytes:
+    """Get the robot's current deck configuration.
+
+    This is a hack to support `opentrons.deck_configuration.cli`. This is supposed to
+    act like `DeckConfigurationStore.get()`, but with two differences:
+
+    1. You can call this without dealing with the notification system
+       (`DeckConfigurationPublisher`).
+    2. The return type is different. Whereas `DeckConfigurationStore.get()` returns
+       a JSON document to expose over HTTP, this returns the data in the on-disk format
+       defined by `opentrons.calibration_storage`.
+
+    This is unsafe to call while there is an active `DeckConfigurationStore`, and
+    should only be called by `opentrons.deck_configuration.cli`.
+    """
+    # This read bypasses the store's normal locking and is therefore unsafe in the face
+    # of concurrent access. That's "okay" because the CLI should't run at the same
+    # time as the server anyway.
+    from_storage = await _read(anyio.Path(path))
+    if from_storage is not None:
+        return serialize_deck_configuration(from_storage[0], from_storage[1])
+    else:
+        default_as_http_response = _get_default(deck_type)
+        default_as_http_request = models.DeckConfigurationRequest.construct(
+            cutoutFixtures=default_as_http_response.cutoutFixtures
+        )
+        storable_default = _http_types_to_storage_types(
+            default_as_http_request,
+            # This timestamp is arbitrary. The CLI's caller does not care about it.
+            datetime.fromtimestamp(0),
+        )
+        return serialize_deck_configuration(storable_default[0], storable_default[1])
+
+
 def _http_types_to_storage_types(
     http_val: models.DeckConfigurationRequest, last_modified_at: datetime
 ) -> _StorableDeckConfiguration:
