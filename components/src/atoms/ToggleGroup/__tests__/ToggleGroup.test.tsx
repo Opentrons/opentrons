d9d4df829f2d7670768287c5904b9edeafff1afa
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2024-08-15 12:29:46 -0400

    refactor(components): toggleGroup creation from useToggleGroup (#16008)
    closes AUTH-650

---
+++ components/src/atoms/ToggleGroup/__tests__/ToggleGroup.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { describe, it, expect, vi } from 'vitest'
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithProviders } from '../../../testing/utils'
+import { ToggleGroup } from '../index'
+
+const render = (props: React.ComponentProps<typeof ToggleGroup>) => {
+  return renderWithProviders(<ToggleGroup {...props} />)
+}
+
+describe('ToggleGroup', () => {
+  let props: React.ComponentProps<typeof ToggleGroup>
+
+  it('should render text and buttons', () => {
+    props = {
+      leftText: 'mock leftText',
+      rightText: 'mock rightText',
+      leftClick: vi.fn(),
+      rightClick: vi.fn(),
+      selectedValue: 'mock leftText',
+    }
+    render(props)
+    fireEvent.click(screen.getByText('mock leftText'))
+    expect(props.leftClick).toHaveBeenCalled()
+    fireEvent.click(screen.getByText('mock rightText'))
+    expect(props.rightClick).toHaveBeenCalled()
+  })
+})
