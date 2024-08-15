2e127edff323e0710881e7f7ee3da6134bb748e7
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2024-08-15 10:36:57 -0400

    fix(app): fix ODD "Run again" button growing when clicked

--- app/src/pages/RunSummary/index.tsx
+++ app/src/pages/RunSummary/index.tsx
@@ -291,7 +291,7 @@ export function RunSummary(): JSX.Element {
   }
 
   const RUN_AGAIN_SPINNER_TEXT = (
-    <Flex justifyContent={JUSTIFY_SPACE_BETWEEN} width="25.5rem">
+    <Flex justifyContent={JUSTIFY_SPACE_BETWEEN} width="16rem">
       {t('run_again')}
       <Icon
         name="ot-spinner"
@@ -399,9 +399,8 @@ export function RunSummary(): JSX.Element {
               </SummaryDatum>
             </Flex>
           </Flex>
-          <Flex alignSelf={ALIGN_STRETCH} gridGap={SPACING.spacing16}>
-            <LargeButton
-              flex="1"
+          <ButtonContainer>
+            <EqualWidthButton
               iconName="arrow-left"
               buttonType="secondary"
               onClick={() => {
@@ -412,10 +411,8 @@ export function RunSummary(): JSX.Element {
                   ? t('return_to_quick_transfer')
                   : t('return_to_dashboard')
               }
-              height="17rem"
             />
-            <LargeButton
-              flex="1"
+            <EqualWidthButton
               iconName="play-round-corners"
               onClick={() => {
                 handleRunAgain(aPipetteWithTip)
@@ -423,20 +420,17 @@ export function RunSummary(): JSX.Element {
               buttonText={
                 showRunAgainSpinner ? RUN_AGAIN_SPINNER_TEXT : t('run_again')
               }
-              height="17rem"
               css={showRunAgainSpinner ? RUN_AGAIN_CLICKED_STYLE : undefined}
             />
             {showErrorDetailsBtn ? (
-              <LargeButton
-                flex="1"
+              <EqualWidthButton
                 iconName="info"
                 buttonType="alert"
                 onClick={handleViewErrorDetails}
                 buttonText={t('view_error_details')}
-                height="17rem"
               />
             ) : null}
-          </Flex>
+          </ButtonContainer>
         </Flex>
       )}
     </Btn>
@@ -531,4 +525,15 @@ const RUN_AGAIN_CLICKED_STYLE = css`
   &:active {
     background-color: ${COLORS.blue60};
   }
+`
+
+const ButtonContainer = styled(Flex)`
+  align-self: stretch;
+  gap: ${SPACING.spacing16};
+`
+
+const EqualWidthButton = styled(LargeButton)`
+  flex: 1;
+  min-width: 0;
+  height: 17rem;
 `
