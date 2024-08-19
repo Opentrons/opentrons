a919f846cd0f539aadc8d84a80c270670322bf07
elyorcv <elyor.kodirov@opentrons.com>
2024-08-19 14:58:59 +0500

    add type [AUTH-630]

--- opentrons-ai-server/api/utils/create_index.py
+++ opentrons-ai-server/api/utils/create_index.py
@@ -1,17 +1,18 @@
 import os
 import os.path
 from pathlib import Path
-from typing import Any
+from typing import Any, Union
 
 from llama_index.core import Settings as llamasettings
 from llama_index.core import SimpleDirectoryReader, StorageContext, VectorStoreIndex, load_index_from_storage
+from llama_index.core.indices.base import BaseIndex
 from llama_index.embeddings.openai import OpenAIEmbedding
 
 ROOT_PATH: Path = Path(Path(__file__)).parent.parent.parent
 llamasettings.embed_model = OpenAIEmbedding(model_name="text-embedding-3-large", api_key=os.environ["OPENAI_API_KEY"])
 
 
-def create_index(data_path: str, data_file: str, index_name: str) -> Any:
+def create_index(data_path: str, data_file: str, index_name: str) -> Union[BaseIndex[Any], VectorStoreIndex]:
     """
     # creating index using llama-index
     data_path = str(ROOT_PATH / "api" / "data")
@@ -34,7 +35,7 @@ def create_index(data_path: str, data_file: str, index_name: str) -> Any:
     PERSIST_DIR = str(ROOT_PATH / "api" / "storage" / "index" / index_name)
     if not os.path.exists(PERSIST_DIR):
         # load the documents and create the index
-        documents: Any = SimpleDirectoryReader(data_path, [data_file]).load_data()
+        documents = SimpleDirectoryReader(data_path, [data_file]).load_data()
         index = VectorStoreIndex.from_documents(documents)
         # store it for later
         index.storage_context.persist(persist_dir=PERSIST_DIR)
