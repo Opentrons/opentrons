e605c66b71932ee0ac02bd7d68d39d4eb49c853d
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2024-08-21 15:27:38 -0400

    feat(protocol-designer): incompatible tips modal in wizard flow (#16081)
    closes AUTH-728

---
+++ protocol-designer/src/pages/CreateNewProtocolWizard/__tests__/SelectPipettes.test.tsx
@@ -0,0 +1,147 @@
+import * as React from 'react'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
+import '@testing-library/jest-dom/vitest'
+import { FLEX_ROBOT_TYPE, OT2_ROBOT_TYPE } from '@opentrons/shared-data'
+import { fireEvent, screen } from '@testing-library/react'
+import { i18n } from '../../../assets/localization'
+import { renderWithProviders } from '../../../__testing-utils__'
+import { getLabwareDefsByURI } from '../../../labware-defs/selectors'
+import { getAllowAllTipracks } from '../../../feature-flags/selectors'
+import { IncompatibleTipsModal } from '../../../organisms'
+import { createCustomTiprackDef } from '../../../labware-defs/actions'
+import { SelectPipettes } from '../SelectPipettes'
+import { getTiprackOptions } from '../utils'
+
+import type { WizardFormState, WizardTileProps } from '../types'
+
+vi.mock('../../../labware-defs/selectors')
+vi.mock('../../../feature-flags/selectors')
+vi.mock('../../../organisms')
+vi.mock('../../../labware-defs/actions')
+vi.mock('../utils')
+
+const render = (props: React.ComponentProps<typeof SelectPipettes>) => {
+  return renderWithProviders(<SelectPipettes {...props} />, {
+    i18nInstance: i18n,
+  })[0]
+}
+
+const values = {
+  additionalEquipment: [],
+  fields: {
+    name: '',
+    description: '',
+    organizationOrAuthor: '',
+    robotType: FLEX_ROBOT_TYPE,
+  },
+  pipettesByMount: { left: {}, right: {} },
+  modules: null,
+} as WizardFormState
+
+const mockWizardTileProps: Partial<WizardTileProps> = {
+  proceed: vi.fn(),
+  setValue: vi.fn(),
+  watch: vi.fn((name: keyof typeof values) => values[name]) as any,
+}
+
+describe('SelectPipettes', () => {
+  let props: React.ComponentProps<typeof SelectPipettes>
+
+  beforeEach(() => {
+    props = {
+      ...props,
+      goBack: vi.fn(),
+      ...mockWizardTileProps,
+    } as WizardTileProps
+    vi.mocked(IncompatibleTipsModal).mockReturnValue(
+      <div>mock incompatible tips modal</div>
+    )
+    vi.mocked(getLabwareDefsByURI).mockReturnValue({})
+    vi.mocked(getAllowAllTipracks).mockReturnValue(false)
+    vi.mocked(getTiprackOptions).mockReturnValue({
+      'opentrons/opentrons_flex_96_tiprack_200ul/1': '200uL Flex tipracks',
+      'opentrons/opentrons_flex_96_tiprack_1000ul/1': '1000uL Flex tipracks',
+    })
+  })
+
+  it('renders the first page of select pipettes for a Flex', () => {
+    render(props)
+    screen.getByText('Step 2')
+    screen.getByText('Add a pipette and tips')
+    screen.getByText(
+      'Pick your first pipette. If you need a second pipette, you can add it next.'
+    )
+    screen.getByText('Pipette type')
+    // select pip type
+    fireEvent.click(screen.getByRole('label', { name: '1-Channel' }))
+    screen.getByText('Pipette volume')
+    // select pip volume
+    fireEvent.click(screen.getByRole('label', { name: '1000 uL' }))
+    // select tip
+    screen.getByText('Add custom pipette tips')
+    screen.getByText('200uL Flex tipracks')
+    fireEvent.click(screen.getByText('1000uL Flex tipracks'))
+
+    screen.getByRole('button', { name: 'Confirm' })
+
+    // go back
+    fireEvent.click(screen.getByRole('button', { name: 'Go back' }))
+    expect(props.goBack).toHaveBeenCalled()
+
+    fireEvent.click(screen.getByRole('button', { name: 'Confirm' }))
+  })
+
+  it('renders the first page of select pipettes for an ot-2', () => {
+    vi.mocked(getTiprackOptions).mockReturnValue({
+      'opentrons/opentrons_96_tiprack_10ul/1': '10uL tipracks',
+      'opentrons/opentrons_96_tiprack_300ul/1': '300uL tipracks',
+    })
+
+    const values = {
+      additionalEquipment: [],
+      fields: {
+        name: '',
+        description: '',
+        organizationOrAuthor: '',
+        robotType: OT2_ROBOT_TYPE,
+      },
+      pipettesByMount: { left: {}, right: {} },
+      modules: null,
+    } as WizardFormState
+
+    props = {
+      ...props,
+      watch: vi.fn((name: keyof typeof values) => values[name]) as any,
+    }
+    render(props)
+    screen.getByText('Step 2')
+    screen.getByText('Add a pipette and tips')
+    screen.getByText(
+      'Pick your first pipette. If you need a second pipette, you can add it next.'
+    )
+    screen.getByText('Pipette type')
+    // select pip type
+    fireEvent.click(screen.getByRole('label', { name: '1-Channel' }))
+
+    screen.getByText('Pipette generation')
+    // select gen
+    fireEvent.click(screen.getByRole('label', { name: 'GEN2' }))
+
+    screen.getByText('Pipette volume')
+    // select pip volume
+    fireEvent.click(screen.getByRole('label', { name: '20 uL' }))
+    // select tip
+    screen.getByText('Add custom pipette tips')
+    screen.getByText('10uL tipracks')
+    fireEvent.click(screen.getByText('300uL tipracks'))
+    screen.getByText('Add custom pipette tips')
+
+    //  add custom pipette tips
+    fireEvent.change(screen.getByTestId('SelectPipettes_customTipInput'))
+    expect(vi.mocked(createCustomTiprackDef)).toHaveBeenCalled()
+
+    //  change all tip setting
+    fireEvent.click(screen.getByText('Show all tips'))
+    screen.getByText('mock incompatible tips modal')
+  })
+})
--- protocol-designer/src/pages/CreateNewProtocolWizard/utils.tsx
+++ protocol-designer/src/pages/CreateNewProtocolWizard/utils.tsx
@@ -2,11 +2,18 @@ import * as React from 'react'
 import {
   MAGNETIC_BLOCK_TYPE,
   THERMOCYCLER_MODULE_TYPE,
+  getLabwareDefURI,
+  getLabwareDisplayName,
+  getPipetteSpecsV2,
 } from '@opentrons/shared-data'
 import wasteChuteImage from '../../assets/images/waste_chute.png'
 import trashBinImage from '../../assets/images/flex_trash_bin.png'
 import stagingAreaImage from '../../assets/images/staging_area.png'
-
+import type {
+  LabwareDefByDefURI,
+  LabwareDefinition2,
+  PipetteName,
+} from '@opentrons/shared-data'
 import type { AdditionalEquipment, WizardFormState } from './types'
 
 const TOTAL_MODULE_SLOTS = 8
@@ -104,4 +111,54 @@ export function AdditionalEquipmentDiagram(props: EquipmentProps): JSX.Element {
       )
     }
   }
+}
+
+interface TiprackOptionsProps {
+  allLabware: LabwareDefByDefURI
+  allowAllTipracks: boolean
+  selectedPipetteName?: string | null
+}
+//  returns a hashmap of LabwareDefUri : displayName
+export function getTiprackOptions(
+  props: TiprackOptionsProps
+): Record<string, string> {
+  const { allLabware, allowAllTipracks, selectedPipetteName } = props
+
+  if (!allLabware) return {}
+
+  const pipetteSpecs = selectedPipetteName
+    ? getPipetteSpecsV2(selectedPipetteName as PipetteName)
+    : null
+
+  const defaultTipracks = pipetteSpecs?.liquids.default.defaultTipracks ?? []
+  const displayCategory = pipetteSpecs?.displayCategory ?? ''
+  const isFlexPipette =
+    displayCategory === 'FLEX' || selectedPipetteName === 'p1000_96'
+
+  const tiprackOptionsMap = Object.values(allLabware)
+    .filter(def => def.metadata.displayCategory === 'tipRack')
+    .filter(def => {
+      if (allowAllTipracks) {
+        return isFlexPipette
+          ? def.metadata.displayName.includes('Flex') ||
+              def.namespace === 'custom_beta'
+          : !def.metadata.displayName.includes('Flex') ||
+              def.namespace === 'custom_beta'
+      }
+      return (
+        defaultTipracks.includes(getLabwareDefURI(def)) ||
+        def.namespace === 'custom_beta'
+      )
+    })
+    .reduce((acc: Record<string, string>, def: LabwareDefinition2) => {
+      const displayName = getLabwareDisplayName(def)
+      const name =
+        def.parameters.loadName.includes('flex') && isFlexPipette
+          ? displayName.split('Opentrons Flex')[1]
+          : displayName
+      acc[getLabwareDefURI(def)] = name
+      return acc
+    }, {})
+
+  return tiprackOptionsMap
 }
