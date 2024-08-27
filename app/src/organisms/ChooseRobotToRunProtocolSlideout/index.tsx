f5e33179dd7c4fc16f3b376ad1c776a94750d8d1
koji <koji.kanao@opentrons.com>
2024-08-28 06:24:43 +0900

    fix(app): confirm values button shrinking issue (#16134)
    * fix(app): confirm values button shrinking issue

--- app/src/organisms/ChooseRobotToRunProtocolSlideout/index.tsx
+++ app/src/organisms/ChooseRobotToRunProtocolSlideout/index.tsx
@@ -5,15 +5,16 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 import {
-  Icon,
-  Flex,
+  ALIGN_CENTER,
   DIRECTION_COLUMN,
-  PrimaryButton,
   DIRECTION_ROW,
+  Flex,
+  Icon,
+  NO_WRAP,
+  PrimaryButton,
   SecondaryButton,
   SPACING,
   useHoverTooltip,
-  ALIGN_CENTER,
 } from '@opentrons/components'
 import { useUploadCsvFileMutation } from '@opentrons/react-api-client'
 
@@ -259,7 +260,11 @@ export function ChooseRobotToRunProtocolSlideoutComponent(
             </PrimaryButton>
           </>
         ) : (
-          <Flex gridGap={SPACING.spacing8} flexDirection={DIRECTION_ROW}>
+          <Flex
+            gridGap={SPACING.spacing8}
+            flexDirection={DIRECTION_ROW}
+            whiteSpace={NO_WRAP}
+          >
             <SecondaryButton
               onClick={() => {
                 setCurrentPage(1)
@@ -278,7 +283,6 @@ export function ChooseRobotToRunProtocolSlideoutComponent(
                 <Flex
                   gridGap={SPACING.spacing4}
                   alignItems={ALIGN_CENTER}
-                  whiteSpace="nowrap"
                   marginLeft={`-${SPACING.spacing4}`}
                 >
                   <Icon name="ot-spinner" spin size="1rem" />
