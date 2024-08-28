55f6bf1e5f67c77cf5334ccfb3a65637d8267b5c
koji <kk2796@nyu.edu>
2024-08-28 14:15:52 +0900

    feat(protocol-designerr): add Materials List Modal
    add Materials List Modal
    
    close AUTH-679

---
+++ protocol-designer/src/organisms/MaterialsListModal/MaterialsListModal.stories.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { I18nextProvider } from 'react-i18next'
+import { i18n } from '../../assets/localization'
+import { MaterialsListModal as MaterialsListModalComponent } from '.'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const mockHardware = [] as any[]
+const mockLabware = [] as any[]
+const mockLiquids = [] as any[]
+
+const meta: Meta<typeof MaterialsListModalComponent> = {
+  title: 'Protocol-Designer/Organisms/MaterialsListModal',
+  component: MaterialsListModalComponent,
+  decorators: [
+    Story => (
+      <I18nextProvider i18n={i18n}>
+        <Story />
+      </I18nextProvider>
+    ),
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<typeof MaterialsListModalComponent>
+
+export const MaterialsListModal: Story = {
+  args: {
+    hardware: mockHardware,
+    labware: mockLabware,
+    liquids: mockLiquids,
+  },
+}
---
+++ protocol-designer/src/organisms/MaterialsListModal/index.tsx
@@ -0,0 +1,98 @@
+import * as React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import {
+  Flex,
+  SPACING,
+  StyledText,
+  Modal,
+  DIRECTION_COLUMN,
+  DIRECTION_ROW,
+  ListItem,
+  ListItemDescriptor,
+} from '@opentrons/components'
+
+interface MaterialsListModalProps {
+  hardware: any[]
+  labware: any[]
+  liquids: any[]
+  closeModal: () => void
+}
+
+export function MaterialsListModal({
+  hardware,
+  labware,
+  liquids,
+  closeModal,
+}: MaterialsListModalProps): JSX.Element {
+  const { t } = useTranslation('protocol_overview')
+  return (
+    <Modal onClose={closeModal} closeOnOutsideClick title={t('materials_list')}>
+      <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing24}>
+        <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing8}>
+          <StyledText desktopStyle="bodyDefaultSemiBold">
+            {t('deck_hardware')}
+          </StyledText>
+          <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing4}>
+            {hardware.length > 0
+              ? hardware.map((hw, id) => (
+                  <ListItem type="noActive" key={`hardware${id}`}>
+                    <ListItemDescriptor
+                      type="default"
+                      description={hw}
+                      content={hw}
+                    />
+                  </ListItem>
+                ))
+              : null}
+          </Flex>
+        </Flex>
+
+        <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing8}>
+          <StyledText desktopStyle="bodyDefaultSemiBold">
+            {t('labware')}
+          </StyledText>
+          <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing4}>
+            {labware.length > 0
+              ? labware.map((lw, id) => (
+                  <ListItem type="noActive" key={`labware_${id}`}>
+                    <ListItemDescriptor
+                      type="default"
+                      description={lw}
+                      content={lw}
+                    />
+                  </ListItem>
+                ))
+              : null}
+          </Flex>
+        </Flex>
+
+        <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing8}>
+          <StyledText desktopStyle="bodyDefaultSemiBold">
+            {t('liquids')}
+          </StyledText>
+          <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing4}>
+            <Flex
+              flexDirection={DIRECTION_ROW}
+              gridGap={SPACING.spacing8}
+              paddingY={SPACING.spacing12}
+            ></Flex>
+            <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing4}>
+              {liquids.length > 0
+                ? liquids.map((liquid, id) => (
+                    <ListItem type="noActive" key={`liquid_${id}`}>
+                      <ListItemDescriptor
+                        type="default"
+                        description={liquid}
+                        content={liquid}
+                      />
+                    </ListItem>
+                  ))
+                : null}
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Modal>
+  )
+}
