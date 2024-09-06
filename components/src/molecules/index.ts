748bb450c02935d4736dd602b3f2774e4ed1004f
koji <kk2796@nyu.edu>
2024-09-06 10:51:38 +0900

    add export type

--- components/src/molecules/index.ts
+++ components/src/molecules/index.ts
@@ -1,7 +1,10 @@
 export * from './DeckInfoLabel'
+export * from './DeckLabel'
 export * from './DropdownMenu'
 export * from './InfoScreen'
 export * from './LiquidIcon'
 export * from './ParametersTable'
 export * from './Tabs'
-export * from './DeckLabel'
+
+// type
+export type * from './DeckLabel'
