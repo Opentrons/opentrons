5711dd96d02e4c511cd43c53f5c4daf1203c929e
CaseyBatten <caseyhbatten@gmail.com>
2024-09-10 16:47:54 -0400

    todo for docs consistency

--- app/src/molecules/Command/hooks/useCommandTextString/utils/getConfigureNozzleLayoutCommandText.ts
+++ app/src/molecules/Command/hooks/useCommandTextString/utils/getConfigureNozzleLayoutCommandText.ts
@@ -13,6 +13,7 @@ export function getConfigureNozzleLayoutCommandText({
     pip => pip.id === pipetteId
   )?.pipetteName
 
+  // TODO(cb, 2024-09-10): confirm these strings for copy consistency and add them to i18n
   const ConfigAmount = {
     SINGLE: 'single nozzle layout',
     COLUMN: 'column layout',
