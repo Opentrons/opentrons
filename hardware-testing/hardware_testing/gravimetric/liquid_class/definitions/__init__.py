b11f9b4c0d58f245e78221405ad6ef0b8ada34ff
andySigler <andrewsigler1@gmail.com>
2024-09-10 11:33:28 -0400

    fix circular imports

--- hardware-testing/hardware_testing/gravimetric/liquid_class/definitions/__init__.py
+++ hardware-testing/hardware_testing/gravimetric/liquid_class/definitions/__init__.py
@@ -3,111 +3,12 @@ from ..liquid_class_settings import *
 from . import water
 
 
-default = Liquid(
-    submerge=Submerge(
-        position=Position(
-            offset=Point(
-                x=0.0,
-                y=0.0,
-                z=2.0,
-            ),
-            ref=PositionRef.MENISCUS,
-        ),
-        speed=50.0,
-        delay=0.0,
-        lld=False,
-    ),
-    retract=Retract(
-        position=Position(
-            offset=Point(x=0.0, y=0.0, z=5.0),
-            ref=PositionRef.MENISCUS,
-        ),
-        speed=50.0,
-        delay=0.0,
-        air_gap=0.1,
-        blow_out=BlowOut(
-            enabled=True,
-            position=Position(
-                offset=Point(
-                    x=0.0,
-                    y=0.0,
-                    z=2.0,
-                ),
-                ref=PositionRef.MENISCUS,
-            ),
-            volume=20.0,
-        ),
-        touch_tip=TouchTip(
-            enabled=False,
-            position=Position(
-                offset=Point(
-                    x=0.0,
-                    y=0.0,
-                    z=-1.0,
-                ),
-                ref=PositionRef.WELL_TOP,
-            ),
-            speed=30.0,
-            mm_to_edge=1.0,
-        ),
-    ),
-    aspirate=Aspirate(
-        order=[],
-        position=Position(
-            offset=Point(
-                x=0.0,
-                y=0.0,
-                z=-1.5,
-            ),
-            ref=PositionRef.MENISCUS,
-        ),
-        flow_rate=30.0,
-        delay=0.5,
-        mix=Mix(
-            enabled=False,
-            count=0,
-            volume=0.0,
-        ),
-        distribute=Distribute(
-            enabled=False,
-            conditioning_volume=0.0,
-            disposal_volume=0.0,
-        ),
-    ),
-    dispense=Dispense(
-        order=[],
-        position=Position(
-            offset=Point(
-                x=0.0,
-                y=0.0,
-                z=-1.5,
-            ),
-            ref=PositionRef.MENISCUS,
-        ),
-        flow_rate=30.0,
-        delay=1.0,
-        mix=Mix(
-            enabled=False,
-            count=0,
-            volume=0.0,
-        ),
-        push_out=7.0,
-    ),
-)
-
-
 # FIXME: add glycerol, ethanol (at appropriate dilutions)
 _all_classes: Dict[str, Dict[str, Dict[str, Dict[str, Dict[int, Liquid]]]]] = {
     "water": {
-        "t50": {
-            "p50": {
-                "1ch": water.t50.p50.single_channel.VOLUMES,
-                "8ch": water.t50.p50.eight_channel.VOLUMES,
-            },
+        "t1000": {
             "p1000": {
-                "1ch": water.t50.p1000.single_channel.VOLUMES,
-                "8ch": water.t50.p1000.eight_channel.VOLUMES,
-                "96ch": water.t50.p1000.ninety_six_channel.VOLUMES,
+                "1ch": water.t1000.p1000.single_channel.default,
             },
         },
     },
