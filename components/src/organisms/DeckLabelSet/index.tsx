2c27b2824dcc1586d8aeebca77579b9c89dae248
Jethary <jethary@opentrons.com>
2024-09-10 14:43:04 -0400

    address feedback

--- components/src/organisms/DeckLabelSet/index.tsx
+++ components/src/organisms/DeckLabelSet/index.tsx
@@ -17,46 +17,53 @@ interface DeckLabelSetProps {
   height: number
 }
 
+const DeckLabelSetComponent = (
+  props: DeckLabelSetProps,
+  ref: React.ForwardedRef<HTMLDivElement>
+): JSX.Element => {
+  const { deckLabels, x, y, width, height } = props
+
+  return (
+    <RobotCoordsForeignDiv x={x} y={y}>
+      <StyledBox width={width} height={height} data-testid="DeckLabeSet" />
+      <LabelContainer ref={ref}>
+        {deckLabels.length > 0
+          ? deckLabels.map((deckLabel, index) => (
+              <DeckLabel
+                key={`DeckLabel_${index}`}
+                maxWidth={`calc(${width}px - ${SPACING.spacing16})`}
+                {...deckLabel}
+                isLast={deckLabels.length - 1 === index}
+              />
+            ))
+          : null}
+      </LabelContainer>
+    </RobotCoordsForeignDiv>
+  )
+}
+
 export const DeckLabelSet = React.forwardRef<HTMLDivElement, DeckLabelSetProps>(
-  function DeckLabelSet({ deckLabels, x, y, width, height }, ref): JSX.Element {
-    const StyledBox = styled(Box)`
-      border-radius: ${BORDERS.borderRadius4};
-      border: 1.5px solid ${COLORS.blue50};
-    `
-
-    const LabelContainer = styled.div`
-      padding-left: ${SPACING.spacing12};
-      & > *:not(:first-child):not(:last-child) {
-        border-bottom-right-radius: ${BORDERS.borderRadius4};
-        border-top-right-radius: ${BORDERS.borderRadius4};
-      }
-
-      & > *:first-child {
-        border-bottom-right-radius: ${BORDERS.borderRadius4};
-      }
-
-      & > *:last-child {
-        border-bottom-left-radius: ${BORDERS.borderRadius4};
-        border-bottom-right-radius: ${BORDERS.borderRadius4};
-      }
-    `
-
-    return (
-      <RobotCoordsForeignDiv x={x} y={y}>
-        <StyledBox width={width} height={height} data-testid="DeckLabeSet" />
-        <LabelContainer ref={ref}>
-          {deckLabels.length > 0
-            ? deckLabels.map((deckLabel, index) => (
-                <DeckLabel
-                  key={`DeckLabel_${index}`}
-                  maxWidth={`calc(${width}px - ${SPACING.spacing16})`}
-                  {...deckLabel}
-                  isLast={deckLabels.length - 1 === index}
-                />
-              ))
-            : null}
-        </LabelContainer>
-      </RobotCoordsForeignDiv>
-    )
-  }
+  DeckLabelSetComponent
 )
+
+const StyledBox = styled(Box)`
+  border-radius: ${BORDERS.borderRadius4};
+  border: 1.5px solid ${COLORS.blue50};
+`
+
+const LabelContainer = styled.div`
+  padding-left: ${SPACING.spacing12};
+  & > *:not(:first-child):not(:last-child) {
+    border-bottom-right-radius: ${BORDERS.borderRadius4};
+    border-top-right-radius: ${BORDERS.borderRadius4};
+  }
+
+  & > *:first-child {
+    border-bottom-right-radius: ${BORDERS.borderRadius4};
+  }
+
+  & > *:last-child {
+    border-bottom-left-radius: ${BORDERS.borderRadius4};
+    border-bottom-right-radius: ${BORDERS.borderRadius4};
+  }
+`
