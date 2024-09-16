826da0a110dbde4c160374c522a533d683a8e03c
Max Marrone <max@opentrons.com>
2024-09-16 15:43:01 -0400

    Clean up liquidProbe tests.
    * No need for two whole separate functions that only differ in the get_is_ready_to_aspirate() mock's return value, especially when the subject ignores that return value.
    * Remove a redundant verify().

--- api/tests/opentrons/protocol_engine/commands/test_liquid_probe.py
+++ api/tests/opentrons/protocol_engine/commands/test_liquid_probe.py
@@ -30,14 +30,12 @@ from opentrons.protocol_engine.commands.liquid_probe import (
 )
 from opentrons.protocol_engine.commands.command import DefinedErrorData, SuccessData
 
-from opentrons.protocol_engine.state.state import StateView
 
 from opentrons.protocol_engine.execution import (
     MovementHandler,
     PipettingHandler,
 )
 from opentrons.protocol_engine.resources.model_utils import ModelUtils
-from opentrons.protocol_engine.types import LoadedPipette
 
 
 EitherImplementationType = Union[
@@ -96,7 +94,7 @@ def subject(
     )
 
 
-async def test_liquid_probe_implementation_no_prep(
+async def test_liquid_probe_implementation(
     decoy: Decoy,
     movement: MovementHandler,
     pipetting: PipettingHandler,
@@ -104,7 +102,7 @@ async def test_liquid_probe_implementation_no_prep(
     params_type: EitherParamsType,
     result_type: EitherResultType,
 ) -> None:
-    """A Liquid Probe should have an execution implementation without preparing to aspirate."""
+    """It should move to the destination and do a liquid probe there."""
     location = WellLocation(origin=WellOrigin.BOTTOM, offset=WellOffset(x=0, y=0, z=1))
 
     data = params_type(
@@ -140,67 +138,6 @@ async def test_liquid_probe_implementation_no_prep(
     assert result == SuccessData(
         public=result_type(z_position=15.0, position=DeckPoint(x=1, y=2, z=3)),
         private=None,
-    )
-
-
-async def test_liquid_probe_implementation_with_prep(
-    decoy: Decoy,
-    state_view: StateView,
-    movement: MovementHandler,
-    pipetting: PipettingHandler,
-    subject: EitherImplementation,
-    params_type: EitherParamsType,
-    result_type: EitherResultType,
-) -> None:
-    """A Liquid Probe should have an execution implementation with preparing to aspirate."""
-    location = WellLocation(origin=WellOrigin.TOP, offset=WellOffset(x=0, y=0, z=2))
-
-    data = params_type(
-        pipetteId="abc",
-        labwareId="123",
-        wellName="A3",
-        wellLocation=location,
-    )
-
-    decoy.when(pipetting.get_is_ready_to_aspirate(pipette_id="abc")).then_return(False)
-
-    decoy.when(state_view.pipettes.get(pipette_id="abc")).then_return(
-        LoadedPipette.construct(  # type:ignore[call-arg]
-            mount=MountType.LEFT
-        )
-    )
-    decoy.when(
-        await movement.move_to_well(
-            pipette_id="abc", labware_id="123", well_name="A3", well_location=location
-        ),
-    ).then_return(Point(x=1, y=2, z=3))
-
-    decoy.when(
-        await pipetting.liquid_probe_in_place(
-            pipette_id="abc",
-            labware_id="123",
-            well_name="A3",
-            well_location=location,
-        ),
-    ).then_return(15.0)
-
-    result = await subject.execute(data)
-
-    assert type(result.public) is result_type  # Pydantic v1 only compares the fields.
-    assert result == SuccessData(
-        public=result_type(z_position=15.0, position=DeckPoint(x=1, y=2, z=3)),
-        private=None,
-    )
-
-    decoy.verify(
-        await movement.move_to_well(
-            pipette_id="abc",
-            labware_id="123",
-            well_name="A3",
-            well_location=WellLocation(
-                origin=WellOrigin.TOP, offset=WellOffset(x=0, y=0, z=2)
-            ),
-        ),
     )
 
 
