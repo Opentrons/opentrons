4064bdff21a17cea8d1659c125f1d17548fbc1ee
pmoegenburg <pbm53@cornell.edu>
2024-09-17 16:23:48 -0400

    feat(api): add meniscus wellOrigin enum (#16139)
    <!--
    Thanks for taking the time to open a Pull Request (PR)! Please make sure
    you've read the "Opening Pull Requests" section of our Contributing
    Guide:
    
    
    https://github.com/Opentrons/opentrons/blob/edge/CONTRIBUTING.md#opening-pull-requests
    
    GitHub provides robust markdown to format your PR. Links, diagrams,
    pictures, and videos along with text formatting make it possible to
    create a rich and informative PR. For more information on GitHub
    markdown, see:
    
    
    https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
    
    To ensure your code is reviewed quickly and thoroughly, please fill out
    the sections below to the best of your ability!
    -->
    
    # Overview
    
    <!--
    Describe your PR at a high level. State acceptance criteria and how this
    PR fits into other work. Link issues, PRs, and other relevant resources.
    -->
    
    This adds MENISCUS to the WellOrigin enums. This also adds methods that
    support using WellOrigin.MENISCUS. With this PR, Protocol Engine State
    GeometryView `get_well_position()` now accepts calls specifying
    WellOrigin.MENISCUS and pulls a well's liquid height (relative to well
    bottom) from Protocol Engine State WellView. Currently, a well's liquid
    height is populated only once a successful liquid probe has been done
    for that well.
    
    ## Test Plan and Hands on Testing
    
    <!--
    Describe your testing of the PR. Emphasize testing not reflected in the
    code. Attach protocols, logs, screenshots and any other assets that
    support your testing.
    -->
    
    ## Changelog
    
    <!--
    List changes introduced by this PR considering future developers and the
    end user. Give careful thought and clear documentation to breaking
    changes.
    -->
    
    ## Review requests
    
    <!--
    - What do you need from reviewers to feel confident this PR is ready to
    merge?
    - Ask questions.
    -->
    
    ## Risk assessment
    
    <!--
    - Indicate the level of attention this PR needs.
    - Provide context to guide reviewers.
    - Discuss trade-offs, coupling, and side effects.
    - Look for the possibility, even if you think it's small, that your
    change may affect some other part of the system.
    - For instance, changing return tip behavior may also change the
    behavior of labware calibration.
    - How do your unit tests and on hands on testing mitigate this PR's
    risks and the risk of future regressions?
    - Especially in high risk PRs, explain how you know your testing is
    enough.
    -->

--- api/src/opentrons/protocol_api/core/legacy/legacy_well_core.py
+++ api/src/opentrons/protocol_api/core/legacy/legacy_well_core.py
@@ -106,6 +106,10 @@ class LegacyWellCore(AbstractWellCore):
         """Get the coordinate of the well's center."""
         return self._geometry.center()
 
+    def get_meniscus(self, z_offset: float) -> Point:
+        """This will never be called because it was added in API 2.21."""
+        assert False, "get_meniscus only supported in API 2.21 & later"
+
     def load_liquid(
         self,
         liquid: Liquid,
