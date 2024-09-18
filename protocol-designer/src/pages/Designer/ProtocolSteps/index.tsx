1d30e93494446a103e948907e8630a61174f2f8c
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2024-09-18 15:07:28 -0400

    feat(protocol-designer): stepForm skeleton (#16284)
    closes AUTH-828

--- protocol-designer/src/pages/Designer/ProtocolSteps/index.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/index.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react'
 import { COLORS, Flex, POSITION_ABSOLUTE, SPACING } from '@opentrons/components'
-import { StepEditForm } from '../../../components/StepEditForm'
 import { DeckSetupContainer } from '../DeckSetup'
 import { TimelineToolbox } from './Timeline'
+import { StepForm } from './StepForm'
 
 export function ProtocolSteps(): JSX.Element {
   return (
     <>
-      {/* TODO: wire up the step edit form designs */}
       <Flex position={POSITION_ABSOLUTE} right="0" zIndex={10}>
-        <StepEditForm />
+        <StepForm />
       </Flex>
       <Flex padding={SPACING.spacing12} backgroundColor={COLORS.grey10}>
         <TimelineToolbox />
