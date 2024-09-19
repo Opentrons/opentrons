8cc59c989831b4aaf4a7b2f8dfa2cbe0671d8ed0
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2024-09-19 10:56:27 -0400

    feat(protocol-designer): add off-deck view when on protocol steps (#16293)
    closes AUTH-801

--- protocol-designer/src/pages/Designer/ProtocolSteps/__tests__/ProtocolSteps.test.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/__tests__/ProtocolSteps.test.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
-import { describe, it, vi } from 'vitest'
-import { screen } from '@testing-library/react'
+import { describe, it, vi, beforeEach, expect } from 'vitest'
+import '@testing-library/jest-dom/vitest'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithProviders } from '../../../../__testing-utils__'
+import { getUnsavedForm } from '../../../../step-forms/selectors'
 import { DeckSetupContainer } from '../../DeckSetup'
-import { TimelineToolbox } from '../Timeline'
+import { OffDeck } from '../../Offdeck'
 import { ProtocolSteps } from '..'
+import { TimelineToolbox } from '../Timeline'
 
+vi.mock('../../Offdeck')
+vi.mock('../../../../step-forms/selectors')
 vi.mock('../StepForm')
 vi.mock('../../DeckSetup')
 vi.mock('../Timeline')
@@ -14,13 +19,34 @@ const render = () => {
 }
 
 describe('ProtocolSteps', () => {
-  it('renders each component in ProtocolSteps', () => {
+  beforeEach(() => {
     vi.mocked(TimelineToolbox).mockReturnValue(<div>mock TimelineToolbox</div>)
     vi.mocked(DeckSetupContainer).mockReturnValue(
       <div>mock DeckSetupContainer</div>
     )
+    vi.mocked(OffDeck).mockReturnValue(<div>mock OffDeck</div>)
+    vi.mocked(getUnsavedForm).mockReturnValue(null)
+  })
+
+  it('renders each component in ProtocolSteps', () => {
     render()
     screen.getByText('mock TimelineToolbox')
     screen.getByText('mock DeckSetupContainer')
+  })
+
+  it('renders the toggle when formData is null', () => {
+    render()
+    screen.getByText('mock DeckSetupContainer')
+    fireEvent.click(screen.getByText('offDeck'))
+    screen.getByText('mock OffDeck')
+  })
+
+  it('renders no toggle when formData does not equal moveLabware type', () => {
+    vi.mocked(getUnsavedForm).mockReturnValue({
+      stepType: 'magnet',
+      id: 'mockId',
+    })
+    render()
+    expect(screen.queryByText('offDeck')).not.toBeInTheDocument()
   })
 })
