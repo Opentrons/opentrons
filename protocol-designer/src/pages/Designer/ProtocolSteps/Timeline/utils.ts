e61adecacc07484d87437d884de01a5b26b7587f
shiyaochen <shiyaochen@admins-mbp.mynetworksettings.com>
2024-09-20 17:24:54 -0400

    refactoring util function

--- protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/utils.ts
+++ protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/utils.ts
@@ -1,6 +1,5 @@
-export function capitalizeFirstLetterAfterNumber(title: string): string {
-  return title.replace(
+export const capitalizeFirstLetterAfterNumber = (title: string): string =>
+  title.replace(
     /(^[\d\W]*)([a-zA-Z])/,
     (match, prefix, firstLetter) => `${prefix}${firstLetter.toUpperCase()}`
   )
-}
