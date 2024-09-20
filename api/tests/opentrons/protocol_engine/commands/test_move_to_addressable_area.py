6ed95ed9e3e7062bbae10c5a8ed9dfdf03a1d3ca
Seth Foster <seth@opentrons.com>
2024-09-20 10:42:44 -0400

    fix(api): dont move gen1s all the way up
    Gen1 pipettes need more z margin than usual when moving around at
    maximum z for some reason.
    
    Closes RQA-3229

--- api/tests/opentrons/protocol_engine/commands/test_move_to_addressable_area.py
+++ api/tests/opentrons/protocol_engine/commands/test_move_to_addressable_area.py
@@ -1,11 +1,13 @@
 """Test move to addressable area commands."""
 from decoy import Decoy
+import pytest
 
-from opentrons.protocol_engine import DeckPoint, AddressableOffsetVector
+from opentrons_shared_data.pipette.types import PipetteNameType
+from opentrons.protocol_engine import DeckPoint, AddressableOffsetVector, LoadedPipette
 from opentrons.protocol_engine.execution import MovementHandler
 from opentrons.protocol_engine.state import update_types
 from opentrons.protocol_engine.state.state import StateView
-from opentrons.types import Point
+from opentrons.types import Point, MountType
 
 from opentrons.protocol_engine.commands.command import SuccessData
 from opentrons.protocol_engine.commands.move_to_addressable_area import (
@@ -15,10 +17,28 @@ from opentrons.protocol_engine.commands.move_to_addressable_area import (
 )
 
 
-async def test_move_to_addressable_area_implementation(
+@pytest.mark.parametrize(
+    "pipette_name",
+    (
+        pipette_name
+        for pipette_name in PipetteNameType
+        if pipette_name
+        not in (
+            PipetteNameType.P10_SINGLE,
+            PipetteNameType.P10_MULTI,
+            PipetteNameType.P50_MULTI,
+            PipetteNameType.P50_SINGLE,
+            PipetteNameType.P300_SINGLE,
+            PipetteNameType.P300_MULTI,
+            PipetteNameType.P1000_SINGLE,
+        )
+    ),
+)
+async def test_move_to_addressable_area_implementation_non_gen1(
     decoy: Decoy,
     state_view: StateView,
     movement: MovementHandler,
+    pipette_name: PipetteNameType,
 ) -> None:
     """A MoveToAddressableArea command should have an execution implementation."""
     subject = MoveToAddressableAreaImplementation(
@@ -35,6 +55,9 @@ async def test_move_to_addressable_area_implementation(
         stayAtHighestPossibleZ=True,
     )
 
+    decoy.when(state_view.pipettes.get("abc")).then_return(
+        LoadedPipette(id="abc", pipetteName=pipette_name, mount=MountType.LEFT)
+    )
     decoy.when(
         await movement.move_to_addressable_area(
             pipette_id="abc",
@@ -44,6 +67,64 @@ async def test_move_to_addressable_area_implementation(
             minimum_z_height=4.56,
             speed=7.89,
             stay_at_highest_possible_z=True,
+            highest_possible_z_extra_offset=None,
+        )
+    ).then_return(Point(x=9, y=8, z=7))
+
+    result = await subject.execute(data)
+
+    assert result == SuccessData(
+        public=MoveToAddressableAreaResult(position=DeckPoint(x=9, y=8, z=7)),
+        private=None,
+    )
+
+
+@pytest.mark.parametrize(
+    "pipette_name",
+    (
+        PipetteNameType.P10_SINGLE,
+        PipetteNameType.P10_MULTI,
+        PipetteNameType.P50_MULTI,
+        PipetteNameType.P50_SINGLE,
+        PipetteNameType.P300_SINGLE,
+        PipetteNameType.P300_MULTI,
+        PipetteNameType.P1000_SINGLE,
+    ),
+)
+async def test_move_to_addressable_area_implementation_with_gen1(
+    decoy: Decoy,
+    state_view: StateView,
+    movement: MovementHandler,
+    pipette_name: PipetteNameType,
+) -> None:
+    """A MoveToAddressableArea command should have an execution implementation."""
+    subject = MoveToAddressableAreaImplementation(
+        movement=movement, state_view=state_view
+    )
+
+    data = MoveToAddressableAreaParams(
+        pipetteId="abc",
+        addressableAreaName="123",
+        offset=AddressableOffsetVector(x=1, y=2, z=3),
+        forceDirect=True,
+        minimumZHeight=4.56,
+        speed=7.89,
+        stayAtHighestPossibleZ=True,
+    )
+
+    decoy.when(state_view.pipettes.get("abc")).then_return(
+        LoadedPipette(id="abc", pipetteName=pipette_name, mount=MountType.LEFT)
+    )
+    decoy.when(
+        await movement.move_to_addressable_area(
+            pipette_id="abc",
+            addressable_area_name="123",
+            offset=AddressableOffsetVector(x=1, y=2, z=3),
+            force_direct=True,
+            minimum_z_height=4.56,
+            speed=7.89,
+            stay_at_highest_possible_z=True,
+            highest_possible_z_extra_offset=5.0,
         )
     ).then_return(Point(x=9, y=8, z=7))
 
