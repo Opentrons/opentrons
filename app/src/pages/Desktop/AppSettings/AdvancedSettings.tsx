35a64f8b8f5e198f74d801536c0e9ba7e64bb95d
Brent Hagen <brent.hagen@opentrons.com>
2024-09-23 14:38:39 -0400

    add temp language toggle in app advanced settings

--- app/src/pages/Desktop/AppSettings/AdvancedSettings.tsx
+++ app/src/pages/Desktop/AppSettings/AdvancedSettings.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react'
-import { Box, SPACING } from '@opentrons/components'
+import { css } from 'styled-components'
+
+import {
+  Box,
+  DIRECTION_COLUMN,
+  Flex,
+  RadioGroup,
+  SPACING,
+  TYPOGRAPHY,
+} from '@opentrons/components'
+
 import { Divider } from '/app/atoms/structure'
+import { i18n } from '/app/i18n'
 import {
   ClearUnavailableRobots,
   EnableDevTools,
@@ -13,6 +24,7 @@ import {
   UpdatedChannel,
   AdditionalCustomLabwareSourceFolder,
 } from '/app/organisms/AdvancedSettings'
+import { useFeatureFlag } from '/app/redux/config'
 
 export function AdvancedSettings(): JSX.Element {
   return (
@@ -37,7 +49,36 @@ export function AdvancedSettings(): JSX.Element {
         <OT2AdvancedSettings />
         <Divider marginY={SPACING.spacing24} />
         <U2EInformation />
+        {/* TODO(bh, 2024-09-23): remove when localization setting designs implemented */}
+        <LocalizationSetting />
       </Box>
     </>
   )
+}
+
+function LocalizationSetting(): JSX.Element | null {
+  const enableLocalization = useFeatureFlag('enableLocalization')
+
+  return enableLocalization ? (
+    <>
+      <Divider marginY={SPACING.spacing24} />
+      <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing8}>
+        <RadioGroup
+          useBlueChecked
+          css={css`
+            ${TYPOGRAPHY.pRegular}
+            line-height: ${TYPOGRAPHY.lineHeight20};
+          `}
+          value={i18n.language}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            void i18n.changeLanguage(event.currentTarget.value)
+          }}
+          options={[
+            { name: 'EN', value: 'en' },
+            { name: 'CN', value: 'zh' },
+          ]}
+        />
+      </Flex>
+    </>
+  ) : null
 }
