680ee12f8408498c0d6ce39ad8b7a44eaff42d80
koji <koji.kanao@opentrons.com>
2024-10-01 10:38:00 -0400

    fix(components, protocol-designer): Fix text wrap issues in PD (#16385)
    * fix(components, protocol-designer): Fix text wrap issues in PD

--- components/src/atoms/ListItem/ListItemChildren/ListItemDescriptor.tsx
+++ components/src/atoms/ListItem/ListItemChildren/ListItemDescriptor.tsx
@@ -1,6 +1,6 @@
 import { Flex } from '../../../primitives'
 import {
-  ALIGN_CENTER,
+  ALIGN_FLEX_START,
   DIRECTION_ROW,
   FLEX_AUTO,
   JUSTIFY_SPACE_BETWEEN,
@@ -22,7 +22,7 @@ export const ListItemDescriptor = (
       flexDirection={DIRECTION_ROW}
       gridGap={SPACING.spacing8}
       width="100%"
-      alignItems={ALIGN_CENTER}
+      alignItems={ALIGN_FLEX_START}
       justifyContent={type === 'mini' ? JUSTIFY_SPACE_BETWEEN : 'none'}
       padding={
         type === 'mini'
@@ -36,7 +36,9 @@ export const ListItemDescriptor = (
       >
         {description}
       </Flex>
-      <Flex flex={type === 'default' && '1.95'}>{content}</Flex>
+      <Flex flex={type === 'default' && '1.95'} overflowWrap="anywhere">
+        {content}
+      </Flex>
     </Flex>
   )
 }
