5932ab54bc6133bff897e0a37692133927b0ac9b
Josh McVey <yersh13@gmail.com>
2024-10-02 19:57:48 -0500

    plugin to force chrome downloads folder setting

--- labware-library/cypress/plugins/index.js
+++ labware-library/cypress/plugins/index.js
@@ -16,8 +16,13 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    if (browser.name === 'chrome') {
+      launchOptions.preferences.default.download = {
+        default_directory: config['downloadsFolder'].replace(/\\/g, '\\\\'),
+      }
+      return launchOptions
+    }
+  })
 }
