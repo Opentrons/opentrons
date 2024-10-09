f64f9e3166b8f4a1c4949958c13fa5cba817f73f
tamarzanzouri <tamar.zanzouri@opentrons.com>
2024-10-09 12:10:27 -0400

    fixed import and export getFixedTrashLabwareDefinitionsByUri

--- shared-data/js/helpers/getFixedTrashLabwareDefinitionsByUri.ts
+++ shared-data/js/helpers/getFixedTrashLabwareDefinitionsByUri.ts
@@ -1,9 +1,6 @@
 import type {LabwareDefinition2} from '..'
-import fixedTrashUncasted from '.../labware/definitions/2/opentrons_1_trash_3200ml_fixed/1.json'
-
-export interface LabwareDefinitionsByUri {
-  [defURI: string]: LabwareDefinition2
-}
+import type {LabwareDefinitionsByUri} from './getLoadedLabwareDefinitionsByUri'
+import fixedTrashUncasted from '../../labware/definitions/2/opentrons_1_trash_3200ml_fixed/1.json'
 
 export function getFixedTrashLabwareDefinitionsByUri(
   labwareDefUri: string
