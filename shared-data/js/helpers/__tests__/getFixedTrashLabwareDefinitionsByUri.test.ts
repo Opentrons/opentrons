40c443bba47eb3d39a2568397fb702cdc7cec596
tamarzanzouri <tamar.zanzouri@opentrons.com>
2024-10-09 15:55:04 -0400

    added test for get fixed trash

---
+++ shared-data/js/helpers/__tests__/getFixedTrashLabwareDefinitionsByUri.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+import { getFixedTrashLabwareDefinitionsByUri } from '../index'
+import type { LabwareDefinition2 } from '../..'
+import fixedTrashUncasted from '../../../labware/definitions/2/opentrons_1_trash_3200ml_fixed/1.json'
+
+describe('getFixedTrashLabwareDefinitionsByUri', () => {
+  it(`should return the fixed trash labware defition`, () => {
+    expect(
+      getFixedTrashLabwareDefinitionsByUri(
+        'opentrons_1_trash_3200ml_fixed'
+      )
+    ).toEqual({ ['opentrons_1_trash_3200ml_fixed']: (fixedTrashUncasted as unknown) as LabwareDefinition2 })
+  })
+})
