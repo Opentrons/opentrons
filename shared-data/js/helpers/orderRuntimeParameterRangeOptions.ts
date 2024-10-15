f8b716a15ab2354dcc50cc1b688c5bfb3e568df0
ncdiehl11 <ndiehl@opentrons.com>
2024-10-15 17:17:36 -0400

    roll back unintended change

--- shared-data/js/helpers/orderRuntimeParameterRangeOptions.ts
+++ shared-data/js/helpers/orderRuntimeParameterRangeOptions.ts
@@ -22,12 +22,11 @@ export const orderRuntimeParameterRangeOptions = (
   choices: Choice[]
 ): string => {
   // when this function is called, the array length is always 2
-  console.log({ choices })
   if (choices.length > 2) {
     console.error(`expected to have length 2 but has length ${choices.length}`)
     return ''
   }
-  const displayNames = choices.map(choice => choice.displayName)
+  const displayNames = [choices[0].displayName, choices[1].displayName]
   if (isNumeric(displayNames[0])) {
     return displayNames
       .sort((a, b) => {
