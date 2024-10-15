d2df4bf640e17f101155b8d8c4b66e5679927369
smb2268 <sarah.breen@opentrons.com>
2024-10-15 17:00:14 -0400

    add reference wavelength to command text if it exits

--- app/src/molecules/Command/hooks/useCommandTextString/utils/getAbsorbanceReaderCommandText.ts
+++ app/src/molecules/Command/hooks/useCommandTextString/utils/getAbsorbanceReaderCommandText.ts
@@ -37,10 +37,17 @@ export const getAbsorbanceReaderCommandText = ({
     const wavelengths = command.params.sampleWavelengths.join(' nm, ') + ` nm`
     const mode =
       command.params.measureMode === 'multi' ? t('multiple') : t('single')
-    return t('absorbance_reader_initialize', {
+
+    return `${t('absorbance_reader_initialize', {
       mode,
       wavelengths,
-    })
+    })} ${
+      command.params.referenceWavelength != null
+        ? t('with_reference_of', {
+            wavelength: command.params.referenceWavelength,
+          })
+        : ''
+    }`
   }
   return t(KEYS_BY_COMMAND_TYPE[command.commandType])
 }
