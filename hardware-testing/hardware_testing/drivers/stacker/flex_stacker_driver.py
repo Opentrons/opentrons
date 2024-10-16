375847c4019f4bbf87abcd8bdc771d22ec52e786
Carlos <fernandez.carlos40@gmail.com>
2024-10-16 14:58:24 -0400

    reverting to old setting with new things to consider for.

--- hardware-testing/hardware_testing/drivers/stacker/flex_stacker_driver.py
+++ hardware-testing/hardware_testing/drivers/stacker/flex_stacker_driver.py
@@ -62,11 +62,11 @@ RETRACT_DIST_X = 1
 RETRACT_DIST_Z = 1
 HOME_SPEED = 20
 HOME_ACCELERATION = 100
-MOVE_ACCELERATION_X = 750 #1500
+MOVE_ACCELERATION_X = 1500
 MOVE_ACCELERATION_Z = 50
 MAX_SPEED_DISCONTINUITY_X = 20
 MAX_SPEED_DISCONTINUITY_Z = 20
-MOVE_SPEED_X = 200 #300
+MOVE_SPEED_X = 300
 MOVE_SPEED_UPZ = 200
 MOVE_SPEED_DOWNZ = 200
 LABWARE_CLEARANCE = 9
@@ -277,17 +277,17 @@ class FlexStacker():
     def move(self, axis: AXIS, distance: float, direction: DIR,
                 velocity: Optional[float] = None, acceleration: Optional[float] = None):
         if axis == AXIS.X:
-            max_speed_discontinuity = 5
+            self.max_speed_discontinuity = 80
             if velocity == None or acceleration == None:
                 velocity = self.move_speed_x
                 acceleration = self.move_acceleration_x
         elif axis == AXIS.Z:
-            max_speed_discontinuity = 5
+            self.max_speed_discontinuity = 10
             if velocity == None or acceleration == None:
                 velocity = self.move_speed_up_z
                 acceleration = self.move_acceleration_z
         else:
-            max_speed_discontinuity = 5
+            self.max_speed_discontinuity = 5
             if velocity == None or acceleration == None:
                 velocity = self.move_speed_up_z
                 acceleration = self.move_acceleration_z
@@ -303,7 +303,7 @@ class FlexStacker():
                                                     ).add_element(
                                                     f'A{acceleration}' #)
                                                     ).add_element(
-                                                    f'D{max_speed_discontinuity}')
+                                                    f'D{self.max_speed_discontinuity}')
 
         print(c)
         self.send_command(command=c, retries=DEFAULT_COMMAND_RETRIES)
