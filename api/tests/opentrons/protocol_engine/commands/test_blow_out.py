fb2386747be99f6907250a9147468a452bc3a5fb
tamarzanzouri <tamar.zanzouri@opentrons.com>
2024-10-16 14:55:52 -0400

    blow out

--- api/tests/opentrons/protocol_engine/commands/test_blow_out.py
+++ api/tests/opentrons/protocol_engine/commands/test_blow_out.py
@@ -1,6 +1,9 @@
 """Test blow-out command."""
-from decoy import Decoy
+from datetime import datetime
+from decoy import Decoy, matchers
 
+from opentrons.protocol_engine.commands.pipetting_common import OverpressureError
+from opentrons.protocol_engine.resources.model_utils import ModelUtils
 from opentrons.types import Point
 from opentrons.protocol_engine import WellLocation, WellOrigin, WellOffset, DeckPoint
 from opentrons.protocol_engine.state import update_types
@@ -10,28 +13,40 @@ from opentrons.protocol_engine.commands import (
     BlowOutImplementation,
     BlowOutParams,
 )
-from opentrons.protocol_engine.commands.command import SuccessData
+from opentrons.protocol_engine.commands.command import DefinedErrorData, SuccessData
 from opentrons.protocol_engine.execution import (
     MovementHandler,
     PipettingHandler,
 )
 from opentrons.hardware_control import HardwareControlAPI
+from opentrons_shared_data.errors.exceptions import PipetteOverpressureError
+import pytest
 
 
-async def test_blow_out_implementation(
-    decoy: Decoy,
+@pytest.fixture
+def subject(
     state_view: StateView,
     hardware_api: HardwareControlAPI,
     movement: MovementHandler,
+    model_utils: ModelUtils,
     pipetting: PipettingHandler,
-) -> None:
-    """Test BlowOut command execution."""
-    subject = BlowOutImplementation(
+) -> BlowOutImplementation:
+    return BlowOutImplementation(
         state_view=state_view,
         movement=movement,
         hardware_api=hardware_api,
         pipetting=pipetting,
+        model_utils=model_utils,
     )
+
+
+async def test_blow_out_implementation(
+    decoy: Decoy,
+    movement: MovementHandler,
+    pipetting: PipettingHandler,
+    subject: BlowOutImplementation,
+) -> None:
+    """Test BlowOut command execution."""
 
     location = WellLocation(origin=WellOrigin.BOTTOM, offset=WellOffset(x=0, y=0, z=1))
 
@@ -72,4 +87,58 @@ async def test_blow_out_implementation(
     decoy.verify(
         await pipetting.blow_out_in_place(pipette_id="pipette-id", flow_rate=1.234),
         times=1,
+    )
+
+
+async def test_overpressure_error(
+    decoy: Decoy,
+    pipetting: PipettingHandler,
+    subject: BlowOutImplementation,
+    model_utils: ModelUtils,
+    movement: MovementHandler,
+) -> None:
+    """It should return an overpressure error if the hardware API indicates that."""
+    pipette_id = "pipette-id"
+
+    error_id = "error-id"
+    error_timestamp = datetime(year=2020, month=1, day=2)
+
+    location = WellLocation(origin=WellOrigin.BOTTOM, offset=WellOffset(x=0, y=0, z=1))
+
+    data = BlowOutParams(
+        pipetteId="pipette-id",
+        labwareId="labware-id",
+        wellName="C6",
+        wellLocation=location,
+        flowRate=1.234,
+    )
+
+    decoy.when(pipetting.get_is_ready_to_aspirate(pipette_id=pipette_id)).then_return(
+        True
+    )
+
+    decoy.when(
+        await pipetting.blow_out_in_place(pipette_id="pipette-id", flow_rate=1.234)
+    ).then_raise(PipetteOverpressureError())
+
+    decoy.when(model_utils.generate_id()).then_return(error_id)
+    decoy.when(model_utils.get_timestamp()).then_return(error_timestamp)
+    decoy.when(
+        await movement.move_to_well(
+            pipette_id="pipette-id",
+            labware_id="labware-id",
+            well_name="C6",
+            well_location=location,
+        )
+    ).then_return(Point(x=1, y=2, z=3))
+
+    result = await subject.execute(data)
+
+    assert result == DefinedErrorData(
+        public=OverpressureError.construct(
+            id=error_id,
+            createdAt=error_timestamp,
+            wrappedErrors=[matchers.Anything()],
+            errorInfo={"retryLocation": (1, 2, 3)},
+        ),
     )
