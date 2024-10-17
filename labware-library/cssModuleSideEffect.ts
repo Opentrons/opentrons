4db06b6abaa49aa00331575f9e01a1925d9a9986
koji <kk2796@nyu.edu>
2024-10-17 10:13:32 -0400

    address feedback

--- labware-library/cssModuleSideEffect.ts
+++ labware-library/cssModuleSideEffect.ts
@@ -1,8 +1,16 @@
-export const cssModuleSideEffect = (): any => {
+import type { Plugin } from 'vite'
+
+/**
+ * Creates a Vite plugin that marks CSS modules as having side effects
+ *
+ * @returns {Plugin} The Vite plugin object.
+ */
+
+export const cssModuleSideEffect = (): Plugin => {
   return {
     name: 'css-module-side-effectful',
     enforce: 'post',
-    transform(_: any, id: any) {
+    transform(_: string, id: string) {
       if (id.includes('.module.')) {
         return {
           moduleSideEffects: 'no-treeshake', // or true, which also works with slightly better treeshake
