16b3e528755999c1688906aaa17f7631268a4af9
znaim <zair.naim@thoughtworks.com>
2024-10-21 12:18:09 -0400

    ran prettier

--- opentrons-ai-client/src/molecules/Header/__tests__/Header.test.tsx
+++ opentrons-ai-client/src/molecules/Header/__tests__/Header.test.tsx
@@ -5,20 +5,19 @@ import { describe, it } from 'vitest'
 import { screen } from '@testing-library/react'
 
 const render = (): ReturnType<typeof renderWithProviders> => {
-    return renderWithProviders(<Header />, {
-        i18nInstance: i18n,
-    })
+  return renderWithProviders(<Header />, {
+    i18nInstance: i18n,
+  })
 }
 
 describe('Header', () => {
-    it('should render Header component', () => {
-        render()
-        screen.getByText('Opentrons')
-    })
+  it('should render Header component', () => {
+    render()
+    screen.getByText('Opentrons')
+  })
 
-    it('should render log out button', () => {
-        render()
-        screen.getByText('Log out')
-    })
-
-});
+  it('should render log out button', () => {
+    render()
+    screen.getByText('Log out')
+  })
+})
