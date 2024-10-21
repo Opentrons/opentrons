cc2075f0069c76c6ef306b5d7d8cf4dd912076c4
znaim <zair.naim@thoughtworks.com>
2024-10-21 12:05:46 -0400

    ran prettier

--- opentrons-ai-client/src/molecules/Footer/Footer.stories.tsx
+++ opentrons-ai-client/src/molecules/Footer/Footer.stories.tsx
@@ -3,20 +3,18 @@ import { Footer } from '.'
 import { COLORS, Flex } from '@opentrons/components'
 
 const meta: Meta<typeof Footer> = {
-    title: 'AI/Molecules/Footer',
-    component: Footer,
-    decorators: [
-        Story => (
-            <Flex
-                backgroundColor={COLORS.grey10}>
-                <Story />
-            </Flex>
-        ),
-    ]
+  title: 'AI/Molecules/Footer',
+  component: Footer,
+  decorators: [
+    Story => (
+      <Flex backgroundColor={COLORS.grey10}>
+        <Story />
+      </Flex>
+    ),
+  ],
 }
 export default meta
 
 type Story = StoryObj<typeof Footer>
 
-export const FooterExample: Story = {
-}
+export const FooterExample: Story = {}
--- opentrons-ai-client/src/molecules/Footer/index.tsx
+++ opentrons-ai-client/src/molecules/Footer/index.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components'
 import {
-    ALIGN_CENTER,
-    COLORS,
-    Flex,
-    JUSTIFY_CENTER,
-    SPACING,
-    TYPOGRAPHY,
+  ALIGN_CENTER,
+  COLORS,
+  Flex,
+  JUSTIFY_CENTER,
+  SPACING,
+  TYPOGRAPHY,
 } from '@opentrons/components'
 import { useTranslation } from 'react-i18next'
 
 const NewLineText = styled.span`
   display: block;
-`;
+`
 
 const BlueLink = styled.a`
   color: ${COLORS.blue50};
@@ -20,7 +20,7 @@ const BlueLink = styled.a`
   &:hover {
     text-decoration: underline;
   }
-`;
+`
 
 const FooterText = styled.p`
   color: ${COLORS.grey60};
@@ -28,29 +28,47 @@ const FooterText = styled.p`
   line-height: ${TYPOGRAPHY.lineHeight16};
   text-align: ${TYPOGRAPHY.textAlignCenter};
   padding-bottom: ${SPACING.spacing24};
-`;
+`
 
 export function Footer(): JSX.Element {
-    const { t } = useTranslation('protocol_generator')
-    const privacyPolicyText = t('privacy_policy');
-    const [firstPart, privacyPolicy, and, EULA, copyright] = privacyPolicyText.split('\n');
+  const { t } = useTranslation('protocol_generator')
+  const privacyPolicyText = t('privacy_policy')
+  const [
+    firstPart,
+    privacyPolicy,
+    and,
+    EULA,
+    copyright,
+  ] = privacyPolicyText.split('\n')
 
-    return (
-        <Flex
-            width="100%"
-            height="88px"
-            backgroundColor={COLORS.grey10}
-            alignItems={ALIGN_CENTER}
-            justifyContent={JUSTIFY_CENTER}
-            paddingTop={SPACING.spacing24}
+  return (
+    <Flex
+      width="100%"
+      height="88px"
+      backgroundColor={COLORS.grey10}
+      alignItems={ALIGN_CENTER}
+      justifyContent={JUSTIFY_CENTER}
+      paddingTop={SPACING.spacing24}
+    >
+      <FooterText>
+        {firstPart}
+        <BlueLink
+          href="https://insights.opentrons.com/hubfs/Legal%20Documentation/Opentrons-Labworks-Privacy-Policy-5-4-23.docx-1.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-            <FooterText>
-                {firstPart}
-                <BlueLink href="https://insights.opentrons.com/hubfs/Legal%20Documentation/Opentrons-Labworks-Privacy-Policy-5-4-23.docx-1.pdf" target="_blank" rel="noopener noreferrer">{privacyPolicy}</BlueLink>
-                {and}
-                <BlueLink href="https://insights.opentrons.com/hubfs/Legal%20Documentation/Opentrons%20EULA%2020240710.pdf" target="_blank" rel="noopener noreferrer">{EULA}</BlueLink>
-                <NewLineText>{copyright}</NewLineText>
-            </FooterText>
-        </Flex>
-    )
+          {privacyPolicy}
+        </BlueLink>
+        {and}
+        <BlueLink
+          href="https://insights.opentrons.com/hubfs/Legal%20Documentation/Opentrons%20EULA%2020240710.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {EULA}
+        </BlueLink>
+        <NewLineText>{copyright}</NewLineText>
+      </FooterText>
+    </Flex>
+  )
 }
