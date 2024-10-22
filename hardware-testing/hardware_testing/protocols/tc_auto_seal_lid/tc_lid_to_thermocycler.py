07151ec93fa0b8514f63582cf941c3e85142d259
rclarke0 <rhyann.clarke@opentrons.com>
2024-10-22 14:49:54 -0400

    z test for lid

--- hardware-testing/hardware_testing/protocols/tc_auto_seal_lid/tc_lid_to_thermocycler.py
+++ hardware-testing/hardware_testing/protocols/tc_auto_seal_lid/tc_lid_to_thermocycler.py
@@ -21,12 +21,27 @@ def add_parameters(parameters: ParameterContext) -> None:
         display_name="Number of Stack Ups",
         choices=[
             {"display_name": "1", "value": 1},
-            {"display_name": "10", "value": 10},
+            {"display_name": "5", "value": 5},
             {"display_name": "20", "value": 20},
             {"display_name": "30", "value": 30},
             {"display_name": "40", "value": 40},
         ],
         default=20,
+    )
+    parameters.add_int(
+        variable_name="z_offset",
+        display_name="Z Offset",
+        choices=[
+            {"display_name": "-1", "value": -1},
+            {"display_name": "-0.5", "value": -0.5},
+            {"display_name": "0.5", "value": 0.5},
+            {"display_name": "1", "value": 1},
+            {"display_name": "1.5", "value": 1.5},
+            {"display_name": "2", "value": 2.5},
+            {"display_name": "2.5", "value": 2.5},
+            {"display_name": "3", "value": 3},
+        ],
+        default=-1,
     )
 
 
@@ -34,6 +49,7 @@ def run(protocol: ProtocolContext) -> None:
     """Runs protocol that moves lids and stacks them."""
     # Load Parameters
     iterations = protocol.params.num_of_stack_ups  # type: ignore[attr-defined]
+    z_offset = protocol.params.z_offset # type: ignore[attr-defined]
     # Thermocycler
     thermocycler: ThermocyclerContext = protocol.load_module(
         "thermocyclerModuleV2"
@@ -49,21 +65,22 @@ def run(protocol: ProtocolContext) -> None:
     for i in range(4):
         lids.append(lids[-1].load_labware("opentrons_tough_pcr_auto_sealing_lid"))
     lids.reverse()
-    top_lid = lids[0]
-    second_from_top = lids[1]
     
+    stack_locations = ["C2", "D2"]
     slot = 0
-    x_offset = [-1, 0, 1]
     for iteration in range(iterations - 1):
-        for offset in x_offset:
-            OFFSET_GUIDES = {
-                "drop": {"x": offset, "y": 0, "z": -1},
-            }
-            protocol.comment(f"Stack up {iteration}, x offset {offset}")
+        loc = 0
+        for lid in lids:
+            protocol.comment(f"Stack up {iteration}")
             # move lid to plate in thermocycler
-            protocol.move_labware(top_lid, plate_in_cycler, use_gripper=True, drop_offset=OFFSET_GUIDES["drop"])
+            protocol.move_labware(lid, plate_in_cycler, use_gripper=True)
             # move lid to deck slot
-            thermocycler.close_lid()
-            thermocycler.open_lid()
-            # move lid back to stack
-            protocol.move_labware(top_lid, second_from_top, use_gripper=True)
+            if loc == 0:
+                protocol.move_labware(lid, stack_locations[slot], use_gripper=True, drop_offset = {"x": 0, "y":0, "z": z_offset})
+                prev_moved_lid: Labware = lid
+            else:
+                protocol.move_labware(lid, prev_moved_lid, use_gripper=True)
+                prev_moved_lid = lid
+            loc += 1
+        slot = (slot + 1) % 2  # Switch between 0 and 1 to rotate stack locations
+        lids.reverse()
