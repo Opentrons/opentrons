7bc250406a9418a6f206fef7c4c9852ddc504aaa
connected-znaim <60662281+connected-znaim@users.noreply.github.com>
2024-10-23 13:13:43 -0400

    feat(opentrons-ai-client): Created the footer component with the privacy policy and EULA (#16535)
    <!--
    Thanks for taking the time to open a Pull Request (PR)! Please make sure
    you've read the "Opening Pull Requests" section of our Contributing
    Guide:
    
    
    https://github.com/Opentrons/opentrons/blob/edge/CONTRIBUTING.md#opening-pull-requests
    
    GitHub provides robust markdown to format your PR. Links, diagrams,
    pictures, and videos along with text formatting make it possible to
    create a rich and informative PR. For more information on GitHub
    markdown, see:
    
    
    https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
    
    To ensure your code is reviewed quickly and thoroughly, please fill out
    the sections below to the best of your ability!
    -->
    
    # Overview
    
    
    
    <!--
    Describe your PR at a high level. State acceptance criteria and how this
    PR fits into other work. Link issues, PRs, and other relevant resources.
    -->
    
    ## Test Plan and Hands on Testing
    
    Manually tested both links
    
    <!--
    Describe your testing of the PR. Emphasize testing not reflected in the
    code. Attach protocols, logs, screenshots and any other assets that
    support your testing.
    -->
    
    ## Changelog
    
    <!--
    List changes introduced by this PR considering future developers and the
    end user. Give careful thought and clear documentation to breaking
    changes.
    -->
    
    <img width="1247" alt="image"
    src="https://github.com/user-attachments/assets/8fc58368-da93-4e43-8c99-d549125bc242">
    
    The footer links to the following two urls: 
    
    Privacy policy:
    
    "https://insights.opentrons.com/hubfs/Legal%20Documentation/Opentrons-Labworks-Privacy-Policy-5-4-23.docx-1.pdf"
    
    EULA:
    
    "https://insights.opentrons.com/hubfs/Legal%20Documentation/Opentrons%20EULA%2020240710.pdf"
    ## Review requests
    
    <!--
    - What do you need from reviewers to feel confident this PR is ready to
    merge?
    - Ask questions.
    -->
    
    ## Risk assessment
    
    <!--
    - Indicate the level of attention this PR needs.
    - Provide context to guide reviewers.
    - Discuss trade-offs, coupling, and side effects.
    - Look for the possibility, even if you think it's small, that your
    change may affect some other part of the system.
    - For instance, changing return tip behavior may also change the
    behavior of labware calibration.
    - How do your unit tests and on hands on testing mitigate this PR's
    risks and the risk of future regressions?
    - Especially in high risk PRs, explain how you know your testing is
    enough.
    -->

---
+++ opentrons-ai-client/src/molecules/Footer/index.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+import {
+  ALIGN_CENTER,
+  COLORS,
+  Flex,
+  JUSTIFY_CENTER,
+  SPACING,
+  TYPOGRAPHY,
+} from '@opentrons/components'
+import { Trans, useTranslation } from 'react-i18next'
+
+const NewLineText = styled.span`
+  display: block;
+`
+
+const BlueLink = styled.a`
+  color: ${COLORS.blue50};
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const FooterText = styled.p`
+  color: ${COLORS.grey60};
+  font-size: ${TYPOGRAPHY.fontSizeH4};
+  line-height: ${TYPOGRAPHY.lineHeight16};
+  text-align: ${TYPOGRAPHY.textAlignCenter};
+  padding-bottom: ${SPACING.spacing24};
+`
+
+export function Footer(): JSX.Element {
+  const { t } = useTranslation('protocol_generator')
+
+  return (
+    <Flex
+      width="100%"
+      height="88px"
+      backgroundColor={COLORS.grey10}
+      alignItems={ALIGN_CENTER}
+      justifyContent={JUSTIFY_CENTER}
+      paddingTop={SPACING.spacing24}
+    >
+      <FooterText>
+        <Trans
+          i18nKey={t('privacy_policy')}
+          t={t}
+          components={{
+            privacyPolicyLink: (
+              <BlueLink
+                href="https://insights.opentrons.com/hubfs/Legal%20Documentation/Opentrons-Labworks-Privacy-Policy-5-4-23.docx-1.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              />
+            ),
+            EULALink: (
+              <BlueLink
+                href="https://insights.opentrons.com/hubfs/Legal%20Documentation/Opentrons%20EULA%2020240710.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              />
+            ),
+          }}
+        />
+        <NewLineText>{t('copyright')}</NewLineText>
+      </FooterText>
+    </Flex>
+  )
+}
