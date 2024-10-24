bc58fdd86359da0513266854cbb0becd246e2cb7
Brent Hagen <brent.hagen@opentrons.com>
2024-10-24 10:26:08 -0400

    feat(app): implement desktop app localization setting toggle
    adds the desktop app dropdown menu to choose language on the app settings general tab
    
    closes PLAT-507

--- app/src/pages/Desktop/AppSettings/AdvancedSettings.tsx
+++ app/src/pages/Desktop/AppSettings/AdvancedSettings.tsx
@@ -1,16 +1,4 @@
-import { useContext } from 'react'
-import { I18nContext } from 'react-i18next'
-import { useDispatch } from 'react-redux'
-import { css } from 'styled-components'
-
-import {
-  Box,
-  DIRECTION_COLUMN,
-  Flex,
-  RadioGroup,
-  SPACING,
-  TYPOGRAPHY,
-} from '@opentrons/components'
+import { Box, SPACING } from '@opentrons/components'
 
 import { Divider } from '/app/atoms/structure'
 import {
@@ -25,9 +13,6 @@ import {
   UpdatedChannel,
   AdditionalCustomLabwareSourceFolder,
 } from '/app/organisms/Desktop/AdvancedSettings'
-import { updateConfigValue, useFeatureFlag } from '/app/redux/config'
-
-import type { Dispatch } from '/app/redux/types'
 
 export function AdvancedSettings(): JSX.Element {
   return (
@@ -52,44 +37,7 @@ export function AdvancedSettings(): JSX.Element {
         <OT2AdvancedSettings />
         <Divider marginY={SPACING.spacing24} />
         <U2EInformation />
-        {/* TODO(bh, 2024-09-23): remove when localization setting designs implemented */}
-        <LocalizationSetting />
       </Box>
     </>
   )
-}
-
-function LocalizationSetting(): JSX.Element | null {
-  const enableLocalization = useFeatureFlag('enableLocalization')
-  const dispatch = useDispatch<Dispatch>()
-
-  const { i18n } = useContext(I18nContext)
-
-  return enableLocalization ? (
-    <>
-      <Divider marginY={SPACING.spacing24} />
-      <Flex flexDirection={DIRECTION_COLUMN} gridGap={SPACING.spacing8}>
-        <RadioGroup
-          useBlueChecked
-          css={css`
-            ${TYPOGRAPHY.pRegular}
-            line-height: ${TYPOGRAPHY.lineHeight20};
-          `}
-          value={i18n.language}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            dispatch(
-              updateConfigValue(
-                'language.appLanguage',
-                event.currentTarget.value
-              )
-            )
-          }}
-          options={[
-            { name: 'EN', value: 'en' },
-            { name: 'CN', value: 'zh' },
-          ]}
-        />
-      </Flex>
-    </>
-  ) : null
 }
