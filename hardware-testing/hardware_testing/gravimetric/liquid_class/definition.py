44ae68cf98a697781b7c3359d52109037d964b4e
Ryan Howard <ryan.howard@opentrons.com>
2025-01-14 16:29:02 -0500

    feat(shared-data, api, hardware-testing): add the p200_96 (#17105)
    <!--
    Thanks for taking the time to open a Pull Request (PR)! Please make sure
    you've read the "Opening Pull Requests" section of our Contributing
    Guide:
    
    https://github.com/Opentrons/opentrons/blob/edge/CONTRIBUTING.md#opening-pull-requests
    
    GitHub provides robust markdown to format your PR. Links, diagrams,
    pictures, and videos along with text formatting make it possible to
    create a rich and informative PR. For more information on GitHub
    markdown, see:
    
    https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
    
    To ensure your code is reviewed quickly and thoroughly, please fill out
    the sections below to the best of your ability!
    -->
    
    This pulls in the changes from several different hardware-testing
    branches/tags that have diverged in the factory and includes all the
    work for the p200_96 that has happened on those branches.
    
    <!--
    Describe your PR at a high level. State acceptance criteria and how this
    PR fits into other work. Link issues, PRs, and other relevant resources.
    -->
    
    <!--
    Describe your testing of the PR. Emphasize testing not reflected in the
    code. Attach protocols, logs, screenshots and any other assets that
    support your testing.
    -->
    
    <!--
    List changes introduced by this PR considering future developers and the
    end user. Give careful thought and clear documentation to breaking
    changes.
    -->
    
    <!--
    - What do you need from reviewers to feel confident this PR is ready to
    merge?
    - Ask questions.
    -->
    
    <!--
    - Indicate the level of attention this PR needs.
    - Provide context to guide reviewers.
    - Discuss trade-offs, coupling, and side effects.
    - Look for the possibility, even if you think it's small, that your
    change may affect some other part of the system.
    - For instance, changing return tip behavior may also change the
    behavior of labware calibration.
    - How do your unit tests and on hands on testing mitigate this PR's
    risks and the risk of future regressions?
    - Especially in high risk PRs, explain how you know your testing is
    enough.
    -->
    
    ---------
    
    Co-authored-by: Andiiiiiiyy <andy.hu@opentrons.com>

--- hardware-testing/hardware_testing/gravimetric/liquid_class/definition.py
+++ hardware-testing/hardware_testing/gravimetric/liquid_class/definition.py
@@ -27,6 +27,7 @@ class DispenseSettings(LiquidSettings):
     """Dispense Settings."""
 
     blow_out_submerged: float  # microliters
+    blow_out_flow_rate: float  # ul/s
 
 
 @dataclass
@@ -89,6 +90,9 @@ def interpolate(
             ),
             blow_out_submerged=_interp(
                 a.dispense.blow_out_submerged, b.dispense.blow_out_submerged
+            ),
+            blow_out_flow_rate=_interp(
+                a.dispense.blow_out_flow_rate, b.dispense.blow_out_flow_rate
             ),
         ),
     )
