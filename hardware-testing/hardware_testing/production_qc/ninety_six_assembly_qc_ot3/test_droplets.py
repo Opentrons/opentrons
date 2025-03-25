4ab41efcc7cef6a21874a79406f1760e1e9d2a1b
Andiiiiiiyy <andy.hu@opentrons.com>
2025-03-25 15:59:46 +0800

    1. fix worng SN 2. ignore negtive speed for jaw motor 3. add low volume test for droplets

--- hardware-testing/hardware_testing/production_qc/ninety_six_assembly_qc_ot3/test_droplets.py
+++ hardware-testing/hardware_testing/production_qc/ninety_six_assembly_qc_ot3/test_droplets.py
@@ -242,6 +242,21 @@ async def run(
         section, "droplets-96-tips", [duration, CSVResult.from_bool(droplets_result)]
     )
 
+            # TEST DROPLETS for 96 TIPS
+            ui.print_header("96 Tips: ASPIRATE and WAIT")
+            await _find_reservoir_pos()
+        assert reservoir_a1_actual
+        result, duration = await aspirate_and_wait(
+            api,
+            reservoir_a1_actual,
+            pipette=test_volume,
+            seconds=NUM_SECONDS_TO_WAIT,
+        )
+        droplets_result = droplets_result & result
+        await _drop_tip(api, trash_nominal, pipette)
+        await api.home_z(OT3Mount.LEFT)
+    report(section, "droplets-96-tips", [duration, CSVResult.from_bool(droplets_result)])
+
     # if not api.is_simulator:
     #     ui.get_user_ready(f"REMOVE 96 tip-rack from slot #{TIP_RACK_96_SLOT}")
     #     ui.get_user_ready(f"ADD partial tip-rack to slot #{TIP_RACK_PARTIAL_SLOT}")
