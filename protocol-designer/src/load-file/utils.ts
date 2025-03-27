e0176b8fb7482b4ea1807acf271489ec16dbac70
David Chau <david.chau@opentrons.com>
2025-03-27 11:53:20 -0400

    Add code highlighting for Demo Day.

--- protocol-designer/src/load-file/utils.ts
+++ protocol-designer/src/load-file/utils.ts
@@ -7,7 +7,15 @@ export const saveFile = (fileData: ProtocolFile, fileName: string): void => {
   saveAs(blob, fileName)
 }
 export const savePythonFile = (fileData: string, fileName: string): void => {
-  const blob = new Blob([fileData], { type: 'text/x-python;charset=UTF-8' })
+  const highlighted = `
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/python.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+<style>.reveal pre code, .hljs { background-color: unset; }</style>
+<pre><code class="language-python">${fileData}</code></pre>
+<script>hljs.highlightAll();</script>
+`.trimStart()
+  const blob = new Blob([highlighted], { type: 'text/html;charset=UTF-8' })
   // For now, show the generated Python in a new window instead of saving it to a file.
   // (A saved Python file wouldn't be runnable anyway until we finish this project.)
   window.open(URL.createObjectURL(blob), '_blank')
