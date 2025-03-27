9fca935d727da5c49fb2b71f283451a30e4f90d8
Rhyann Clarke <146747548+rclarke0@users.noreply.github.com>
2025-03-26 22:07:27 -0400

    fix (abr-testing): Remove instances where pipette was returning tips with liquid (#17818)
    <!--
    Thanks for taking the time to open a Pull Request (PR)! Please make sure
    you've read the "Opening Pull Requests" section of our Contributing
    Guide:
    
    
    https://github.com/Opentrons/opentrons/blob/edge/CONTRIBUTING.md#opening-pull-requests
    
    GitHub provides robust markdown to format your PR. Links, diagrams,
    pictures, and videos along with text formatting make it possible to
    create a rich and informative PR. For more information on GitHub
    markdown, see:
    
    
    https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
    
    To ensure your code is reviewed quickly and thoroughly, please fill out
    the sections below to the best of your ability!
    -->
    
    # Overview
    
    Removing instances where tips were returning with liquid in them
    
    ## Test Plan and Hands on Testing
    
    Ran protocols and checked tip rack bases upon completion to ensure they
    were empty.
    
    ## Changelog
    
    <!--
    List changes introduced by this PR considering future developers and the
    end user. Give careful thought and clear documentation to breaking
    changes.
    -->
    
    ## Review requests
    
    <!--
    - What do you need from reviewers to feel confident this PR is ready to
    merge?
    - Ask questions.
    -->
    
    ## Risk assessment
    
    <!--
    - Indicate the level of attention this PR needs.
    - Provide context to guide reviewers.
    - Discuss trade-offs, coupling, and side effects.
    - Look for the possibility, even if you think it's small, that your
    change may affect some other part of the system.
    - For instance, changing return tip behavior may also change the
    behavior of labware calibration.
    - How do your unit tests and on hands on testing mitigate this PR's
    risks and the risk of future regressions?
    - Especially in high risk PRs, explain how you know your testing is
    enough.
    -->

--- abr-testing/abr_testing/protocols/liquid_setups/All_Liquid_Set_ups.py
+++ abr-testing/abr_testing/protocols/liquid_setups/All_Liquid_Set_ups.py
@@ -221,7 +221,7 @@ def run(protocol: ProtocolContext) -> None:
             "nest_96_wellplate_2ml_deep", str(SLOTS["LABWARE"][0]), "Reservoir 1"
         )  # Reservoir
         sample_plate_2 = protocol.load_labware(
-            "thermoscientificnunc_96_wellplate_1300ul",
+            "nest_96_wellplate_2ml_deep",
             str(SLOTS["LABWARE"][1]),
             "Sample Plate 2",
         )  # Reservoir
