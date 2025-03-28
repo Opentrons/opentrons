b69770f78bc34f7f507fd249b0748c8fefea57ee
Sara Kowalski <sara.kowalski@opentrons.com>
2025-03-28 16:55:56 -0400

    fix lint and stuff

--- protocol-designer/cypress/support/SetupSteps.ts
+++ protocol-designer/cypress/support/SetupSteps.ts
@@ -75,7 +75,7 @@ export enum SetupLocators {
   LiquidNameInput = 'input[name="displayName"]',
   ModalShellArea = 'div[aria-label="ModalShell_ModalArea"]',
   SaveButton = 'button[type="submit"]',
-  LiquidsDropdown = 'div[tabindex="0"].sc-ksBlkl', // NOTE: locator
+  LiquidsDropdown = 'div[tabindex="0"].sc-ksBlkl', // NOTE: locator 
   Div = 'div',
   Button = 'button',
   TempdeckTempInput = 'input[name="targetTemperature"]',
@@ -327,7 +327,7 @@ export const SetupSteps = {
   }),
 
   /**
-   * Choose deck slot A2.
+   * Choose deck slot A2. Please
    */
   ChoseDeckSlotA2: (): StepThunk => ({
     call: () => {
