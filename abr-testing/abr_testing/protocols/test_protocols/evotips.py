1143a36c0ca96342b2fd2c3957f2107227d84bf9
Rhyann Clarke <146747548+rclarke0@users.noreply.github.com>
2025-04-01 15:06:10 -0400

    Update abr-testing/abr_testing/protocols/test_protocols/evotips.py
    Co-authored-by: Seth Foster <seth@opentrons.com>

--- abr-testing/abr_testing/protocols/test_protocols/evotips.py
+++ abr-testing/abr_testing/protocols/test_protocols/evotips.py
@@ -168,7 +168,6 @@ def run(protocol: ProtocolContext) -> None:
             new_location=evotips_adapter,
             use_gripper=True,
             pick_up_offset={"x": 0, "y": 0, "z": 0},
-            drop_offset={"x": 0, "y": 0, "z": 0},
         )
 
     protocol.delay(seconds=soak_seconds)
