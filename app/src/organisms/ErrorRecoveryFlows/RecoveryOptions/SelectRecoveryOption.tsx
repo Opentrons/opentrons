92d562b261f0be93e8949ecb61b911bcbf1ea07a
TamarZanzouri <zanzouritamar@gmail.com>
2025-04-01 21:24:04 -0400

    Update app/src/organisms/ErrorRecoveryFlows/RecoveryOptions/SelectRecoveryOption.tsx
    Co-authored-by: Jamey Huffnagle <jamey.huffnagle@opentrons.com>

--- app/src/organisms/ErrorRecoveryFlows/RecoveryOptions/SelectRecoveryOption.tsx
+++ app/src/organisms/ErrorRecoveryFlows/RecoveryOptions/SelectRecoveryOption.tsx
@@ -175,7 +175,7 @@ export function getRecoveryOptions(errorKind: ErrorKind): RecoveryRoute[] {
     case ERROR_KINDS.LABWARE_MISSING_IN_HOPPER:
       return LABWARE_MISSING_IN_HOPPER_OPTIONS
     case ERROR_KINDS.SHUTTE_MISSING:
-      return SHUTTE_MISSING_OPTIONS
+      return SHUTTLE_MISSING_OPTIONS
   }
 }
 
