e876e7b9bb316d798b0603b0fcc24281a911c806
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-04-02 13:57:28 -0400

    refactor(protocol-designer): labware-ingred reducers to not use load commands (#17944)

--- protocol-designer/src/labware-ingred/selectors.ts
+++ protocol-designer/src/labware-ingred/selectors.ts
@@ -77,10 +77,6 @@ const getLiquidSelectionOptions: Selector<
 const selectedAddLabwareSlot = (state: BaseState): DeckSlot | false =>
   rootSelector(state).modeLabwareSelection
 
-// TODO(mc, 2020-06-04): move SavedLabwareState to common location and import here
-const getSavedLabware = (state: BaseState): Record<string, boolean> =>
-  rootSelector(state).savedLabware
-
 const getSelectedLabwareId: Selector<
   RootSlice,
   SelectedContainerId
@@ -180,7 +176,6 @@ export const selectors = {
   getLiquidSelectionOptions,
   getLiquidGroupsOnDeck,
   getNextLiquidGroupId,
-  getSavedLabware,
   getSelectedLabwareId,
   getSelectedLiquidGroupState,
   getDrillDownLabwareId,
