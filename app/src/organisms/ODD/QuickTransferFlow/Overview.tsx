a86229be15ae75cc3b819439c1ef7db5b0cec83d
koji <kk2796@nyu.edu>
2025-04-03 17:27:28 -0400

    add ff to overview

--- app/src/organisms/ODD/QuickTransferFlow/Overview.tsx
+++ app/src/organisms/ODD/QuickTransferFlow/Overview.tsx
@@ -17,6 +17,7 @@ import {
 
 import { useToaster } from '/app/organisms/ToasterOven'
 import { CONSOLIDATE, DISTRIBUTE } from './constants'
+import { useFeatureFlag } from '/app/redux/config'
 
 import type { QuickTransferSummaryState } from './types'
 
@@ -28,6 +29,9 @@ export function Overview(props: OverviewProps): JSX.Element | null {
   const { state } = props
   const { t } = useTranslation(['quick_transfer', 'shared'])
   const { makeSnackbar } = useToaster()
+  const enableLiquidClassesForQT = useFeatureFlag(
+    'liquidClassesForQuickTransfer'
+  )
 
   let transferCopy = t('volume_per_well')
   if (state.transferType === CONSOLIDATE) {
@@ -39,63 +43,90 @@ export function Overview(props: OverviewProps): JSX.Element | null {
     makeSnackbar(t('create_new_to_edit') as string)
   }
 
-  const displayItems = [
-    {
-      option: t('pipette'),
-      value: state.pipette.displayName,
-    },
-    {
-      option: t('tip_rack'),
-      value: state.tipRack.metadata.displayName,
-    },
-    {
-      option: t('source_labware'),
-      value: state.source.metadata.displayName,
-    },
-    {
-      option: t('destination_labware'),
-      value:
-        state.destination === 'source'
-          ? state.source.metadata.displayName
-          : state.destination.metadata.displayName,
-    },
-    {
-      option: t('pipette_path'),
-      value: state.path,
-    },
-    {
-      option: t('tip_change_frequency'),
-      value: state.changeTip,
-    },
-    {
-      option: transferCopy,
-      value: `${state.volume}µL`,
-    },
-    {
-      option: t('tip_drop_location'),
-      value: t(
-        `${
-          state.dropTipLocation.cutoutFixtureId === TRASH_BIN_ADAPTER_FIXTURE
-            ? 'trashBin'
-            : 'wasteChute'
-        }_location`,
+  const displayItems = enableLiquidClassesForQT
+    ? [
         {
-          slotName:
-            FLEX_SINGLE_SLOT_BY_CUTOUT_ID[state.dropTipLocation.cutoutId],
-        }
-      ),
-    },
-    {
-      option: t('liquid_class'),
-      value: 'Dummy liquid class',
-    },
-  ]
+          option: t('pipette'),
+          value: state.pipette.displayName,
+        },
+        {
+          option: t('tip_rack'),
+          value: state.tipRack.metadata.displayName,
+        },
+        {
+          option: t('source_labware'),
+          value: state.source.metadata.displayName,
+        },
+        {
+          option: t('destination_labware'),
+          value:
+            state.destination === 'source'
+              ? state.source.metadata.displayName
+              : state.destination.metadata.displayName,
+        },
+        {
+          option: t('pipette_path'),
+          value: state.path,
+        },
+        {
+          option: t('tip_change_frequency'),
+          value: state.changeTip,
+        },
+        {
+          option: transferCopy,
+          value: `${state.volume}µL`,
+        },
+        {
+          option: t('tip_drop_location'),
+          value: t(
+            `${
+              state.dropTipLocation.cutoutFixtureId ===
+              TRASH_BIN_ADAPTER_FIXTURE
+                ? 'trashBin'
+                : 'wasteChute'
+            }_location`,
+            {
+              slotName:
+                FLEX_SINGLE_SLOT_BY_CUTOUT_ID[state.dropTipLocation.cutoutId],
+            }
+          ),
+        },
+        {
+          option: t('liquid_class'),
+          value: 'Dummy liquid class',
+        },
+      ]
+    : [
+        {
+          option: t('pipette'),
+          value: state.pipette.displayName,
+        },
+        {
+          option: t('tip_rack'),
+          value: state.tipRack.metadata.displayName,
+        },
+        {
+          option: t('source_labware'),
+          value: state.source.metadata.displayName,
+        },
+        {
+          option: t('destination_labware'),
+          value:
+            state.destination === 'source'
+              ? state.source.metadata.displayName
+              : state.destination.metadata.displayName,
+        },
+        {
+          option: transferCopy,
+          value: `${state.volume}µL`,
+        },
+      ]
 
   return (
     <Flex
       gridGap={SPACING.spacing8}
       flexDirection={DIRECTION_COLUMN}
-      marginTop="192px"
+      paddingTop="12rem"
     >
       {displayItems.map(displayItem => (
         <ListItem type="default" key={displayItem.option} onClick={onClick}>
