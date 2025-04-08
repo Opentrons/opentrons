24f5cc59f2889a9c9bdbb94ce7a9e784f96a5244
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-04-08 13:15:06 -0400

    feat(app, labware-library): add evotip definition assets (#18007)
    closes AUTH-1644

--- labware-library/src/components/labware-ui/labware-images.ts
+++ labware-library/src/components/labware-ui/labware-images.ts
@@ -477,4 +477,12 @@ export const labwareImages: Record<string, string[]> = {
   opentrons_flex_deck_riser: [
     new URL('../../images/opentrons_flex_deck_riser.png', import.meta.url).href,
   ],
+  evotip_flex_tall_adapter: [
+    new URL('../../images/opentrons_evotip_tall_adapter.png', import.meta.url)
+      .href,
+  ],
+  evotip_flex_short_adapter: [
+    new URL('../../images/opentrons_evotip_short_adapter.png', import.meta.url)
+      .href,
+  ],
 }
