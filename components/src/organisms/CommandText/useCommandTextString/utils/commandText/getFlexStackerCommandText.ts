823fbbc04a3ebc448fed0ae2e1f2e8b1fdf67388
tamarzanzouri <tamar.zanzouri@opentrons.com>
2025-04-08 20:51:28 -0400

    WIP implement getFlexStackerCommandText

--- components/src/organisms/CommandText/useCommandTextString/utils/commandText/getFlexStackerCommandText.ts
+++ components/src/organisms/CommandText/useCommandTextString/utils/commandText/getFlexStackerCommandText.ts
@@ -15,6 +15,16 @@ export type FlexStackerCommand =
   | FlexStackerFillRunTimeCommand
   | FlexStackerEmptyRunTimeCommand
 
+const KEYS_BY_COMMAND_TYPE: {
+  [commandType in FlexStackerCommand['commandType']]: string
+} = {
+  'flexStacker/retrieve': 'flex_stacker_retrieve',
+  'flexStacker/store': 'flex_stacker_store',
+  'flexStacker/setStoredLabware': 'flex_stacker_set_stored_labware',
+  'flexStacker/empty': 'flex_stacker_empty',
+  'flexStacker/fill': 'flex_stacker_set_stored_labware',
+}
+
 type HandledCommands = Extract<
   RunTimeCommand,
   { commandType: FlexStackerCommand }
@@ -25,8 +35,19 @@ type GetFlexStackerCommandText = HandlesCommands<HandledCommands>
 // hold until Casey's work is implemented?
 export const getFlexStackerCommandText = ({
   command,
+  allRunDefs,
+  commandTextData,
   t,
 }: // stackerCommand,
 GetFlexStackerCommandText): string => {
-  return ''
+  console.log('commandTextData:', commandTextData)
+  console.log('allRunDefs: ', allRunDefs)
+  console.log('command: ', command)
+  if (command.commandType === 'flexStacker/retrieve') {
+    return (
+      'flexStacker/retrieve' +
+      'Retrieve {command.result.primaryLabwareURI} from Flex Stacker to slotLocation'
+    )
+  }
+  return t(KEYS_BY_COMMAND_TYPE[command.commandType])
 }
