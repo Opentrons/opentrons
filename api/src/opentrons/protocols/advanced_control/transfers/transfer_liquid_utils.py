a356d0bcb62a1de2d78bebbcef5c79719c482613
caila-marashaj <caila.marashaj@opentrons.com>
2025-04-09 11:16:33 -0400

    left a todo in there

--- api/src/opentrons/protocols/advanced_control/transfers/transfer_liquid_utils.py
+++ api/src/opentrons/protocols/advanced_control/transfers/transfer_liquid_utils.py
@@ -60,7 +60,6 @@ def raise_if_location_inside_liquid(
         # liquid classes-based transfer to only when LPD is enabled or when liquids are
         # loaded in protocols using `load_liquid`. This can be quite restrictive
         # so we will not raise but just log a warning.
-        # todo: allow simulatedliquidprobe and dont raise this error
         logger.warning(
             f"Could not verify height of liquid in well {well_core.get_display_name()}, either"
             f" because the liquid in this well has not been probed or because"
