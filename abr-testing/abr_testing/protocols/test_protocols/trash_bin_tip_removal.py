0925838c85a0f60d8c501567800f45eb5fafbcc8
rclarke0 <rhyann.clarke@opentrons.com>
2025-04-11 13:19:15 -0400

    trash bin and waste chute protocol

--- abr-testing/abr_testing/protocols/test_protocols/trash_bin_tip_removal.py
+++ abr-testing/abr_testing/protocols/test_protocols/trash_bin_tip_removal.py
@@ -1,17 +1,18 @@
-"""Statically Clung Tips on 8ch Pipette Waste Chute Removal Method tested on 8.3.1."""
+"""Statically Clung Tips on 8ch Pipette Trash Bin Removal Method tested on 8.3.1."""
 from opentrons import types
-from opentrons.protocol_api import ParameterContext, ProtocolContext, SINGLE
 from opentrons.hardware_control.types import OT3Mount
-from opentrons.hardware_control import SyncHardwareAPI
 from opentrons.hardware_control.motion_utilities import target_position_from_plunger
+from opentrons.protocol_api import ParameterContext, ProtocolContext
+from opentrons.hardware_control import SyncHardwareAPI
 from typing import List
 
 # NOTE: Analyzing Protocol will raise a TipNotAttachedError. Ignore it and run on robot.
 
+
 metadata = {
-    "protocolName": "Waste Chute Tip Removal",
-    "author": "Rhyann Clarke",
-    "description": "Waste Chute Swipe Method for Statically Clung Tips on 8ch Pipette",
+    "protocolName": "Trash Bin Tip Removal",
+    "author": "Jon Klar",
+    "description": "Trash Bin Swipe Method for Statically Clung Tips on 8ch Pipette",
 }
 
 requirements = {"robotType": "OT-3", "apiLevel": "2.21"}
@@ -20,28 +21,42 @@ requirements = {"robotType": "OT-3", "apiLevel": "2.21"}
 def add_parameters(parameters: ParameterContext) -> None:
     """Parameters."""
     parameters.add_float(
+        variable_name="z_travel",
+        display_name="z distance to trash",
+        default=-44.0,
+        unit="mm",
+        minimum=-48.0,
+        maximum=0.0,
+        description="distance the pipette travels to reach the trash bin",
+    )
+
+    parameters.add_float(
+        variable_name="x_travel",
+        display_name="x distance to edge of trash",
+        default=-110.4,
+        unit="mm",
+        minimum=-115.0,
+        maximum=115.0,
+        description="distance the pipette travels to reach the edge of the trash bin",
+    )
+
+    parameters.add_float(
         variable_name="clip_offset",
         display_name="clip distance",
-        default=-5.0,
-        minimum=-15.0,
-        maximum=5.0,  # DEFAULT AND MOST TESTED CLIP_OFFSET IS -5.
+        default=-1.25,
+        minimum=-5.0,
+        maximum=5.0,
         unit="mm",
-        description="distance pipette travels to clip edge of waste chute. Tested Value is -5",
+        description="distance the pipette travels to clip the edge of the trash bin",
     )
 
     parameters.add_int(
-        variable_name="num_times",
-        display_name="number of pick ups",
-        default=96,
+        variable_name="tip_cols",
+        display_name="columns of tips",
+        default=12,
         minimum=1,
-        maximum=480,
-        description="number of pick ups",
-    )
-    parameters.add_bool(
-        variable_name="partial_tip",
-        display_name="Partial Tip",
-        default=True,
-        description="True means partial tip is on",
+        maximum=60,
+        description="12 correlates to 1 full tip rack.",
     )
     parameters.add_str(
         variable_name="pipette_type",
@@ -55,30 +70,36 @@ def add_parameters(parameters: ParameterContext) -> None:
 
 
 def run(protocol: ProtocolContext) -> None:
-    """Protocol to removing statically clung tips with waste chute."""
-    z_travel = -33
-    x_travel = -40
+    """Protocol to removing statically clung tips with trash bin."""
+    x_travel = protocol.params.x_travel  # type: ignore[attr-defined]
+    z_travel = protocol.params.z_travel  # type: ignore[attr-defined]
     clip_offset = protocol.params.clip_offset  # type: ignore[attr-defined]
-    num_times = protocol.params.num_times  # type: ignore[attr-defined]
-    partial_tip = protocol.params.partial_tip  # type: ignore[attr-defined]
+    cols_of_tips = protocol.params.tip_cols  # type: ignore[attr-defined]
     pipette_type = protocol.params.pipette_type  # type: ignore[attr-defined]
+
+    trash = protocol.load_trash_bin("A3")
 
     DECK_SLOTS: List[str] = ["D1", "B1", "D2", "B2", "B3"]
     tip_racks = [
         protocol.load_labware("opentrons_flex_96_filtertiprack_50ul", slot)
         for slot in DECK_SLOTS
     ]
-    pipette = protocol.load_instrument(pipette_type, "left")
-    if partial_tip:
-        pipette.configure_nozzle_layout(style=SINGLE, start="H1", tip_racks=tip_racks)
-    trash = protocol.load_waste_chute()
+    pipette = protocol.load_instrument(pipette_type, "left", tip_racks=tip_racks)
+
+    all_tips_by_col = []
+    loaded_cols = 0
+    for rack in tip_racks:
+        for col in rack.columns():
+            all_tips_by_col.append(col)
+            loaded_cols += 1
+            if loaded_cols == cols_of_tips:
+                break
 
     hwapi = protocol._core.get_hardware()
     hwapi._cache_instruments()
     hwapi._cache_current_position()
 
     mount = OT3Mount.LEFT
-
     drop_tip_pos = 91.5
     bottom_pos = 61.5
 
@@ -88,9 +109,10 @@ def run(protocol: ProtocolContext) -> None:
         api.remove_tip(mount)
         pipette._last_tip_picked_up_from = None
 
-    def clip_waste_chute_edge() -> None:
-        """Knock tips off with waste chute edge."""
-        hwapi.move_rel(mount, types.Point(x=x_travel + clip_offset))
+    def clip_trash_edge() -> None:
+        """Knock tips off with trash bin edge."""
+        hwapi.move_rel(mount, types.Point(x=x_travel))
+        hwapi.move_rel(mount, types.Point(x=clip_offset))
 
     def pipette_to_neutral_position(api: SyncHardwareAPI, mount: OT3Mount) -> None:
         """Move pipette back to neutral state."""
@@ -103,9 +125,9 @@ def run(protocol: ProtocolContext) -> None:
         target_pos = target_position_from_plunger(realmount, pos, current_pos)
         api._move(target_pos)
 
-    for col in list(range(num_times)):
-        pipette.pick_up_tip()
+    for col in all_tips_by_col:
+        pipette.pick_up_tip(col[0])
         pipette.move_to(trash.top(x=x_travel, z=z_travel))
         drop_tip_with_ejector_extended(hwapi, mount)
-        clip_waste_chute_edge()
+        clip_trash_edge()
         pipette_to_neutral_position(hwapi, mount)
