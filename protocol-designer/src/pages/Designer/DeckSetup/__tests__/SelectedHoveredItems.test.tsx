bb25eaf19b46ad76df03ab9655b465d1f5ec78a0
Jethary <jethary@opentrons.com>
2025-04-14 09:03:56 -0400

    fix(protocol-designer): get all labware defs instead of latest when zooming into slot
    closes AUTH-1730

--- protocol-designer/src/pages/Designer/DeckSetup/__tests__/SelectedHoveredItems.test.tsx
+++ protocol-designer/src/pages/Designer/DeckSetup/__tests__/SelectedHoveredItems.test.tsx
@@ -7,6 +7,7 @@ import {
   HEATERSHAKER_MODULE_V1,
   fixture24Tuberack,
   getDeckDefFromRobotType,
+  getAllLabwareDefs,
 } from '@opentrons/shared-data'
 import { Module } from '@opentrons/components'
 import { selectors } from '../../../../labware-ingred/selectors'
@@ -19,6 +20,7 @@ import { SelectedHoveredItems } from '../SelectedHoveredItems'
 
 import type { ComponentProps } from 'react'
 import type * as OpentronsComponents from '@opentrons/components'
+import type * as OpentronsSharedData from '@opentrons/shared-data'
 import type { LabwareDefinition2 } from '@opentrons/shared-data'
 
 vi.mock('../../../../file-data/selectors')
@@ -34,11 +36,19 @@ vi.mock('@opentrons/components', async importOriginal => {
     Module: vi.fn(),
   }
 })
+vi.mock('@opentrons/shared-data', async importOriginal => {
+  const actual = await importOriginal<typeof OpentronsSharedData>()
+  return {
+    ...actual,
+    getAllLabwareDefs: vi.fn(),
+  }
+})
 
 const render = (props: ComponentProps<typeof SelectedHoveredItems>) => {
   return renderWithProviders(<SelectedHoveredItems {...props} />)[0]
 }
 
+const mockAdapterURI = 'fixture/fixture_universal_flat_bottom_adapter/1'
 describe('SelectedHoveredItems', () => {
   let props: ComponentProps<typeof SelectedHoveredItems>
 
@@ -51,6 +61,14 @@ describe('SelectedHoveredItems', () => {
       hoveredFixture: null,
       slotPosition: [0, 0, 0],
     }
+    vi.mocked(getAllLabwareDefs).mockReturnValue({
+      [mockAdapterURI]: {
+        ...fixture24Tuberack,
+        metadata: {
+          displayName: 'Fixture Opentrons Universal Flat Heater-Shaker Adapter',
+        },
+      } as any,
+    })
     vi.mocked(getDesignerTab).mockReturnValue('startingDeck')
     vi.mocked(getInitialDeckSetup).mockReturnValue({
       modules: {},
@@ -60,7 +78,7 @@ describe('SelectedHoveredItems', () => {
         labware: {
           id: 'mockId',
           def: fixture24Tuberack as LabwareDefinition2,
-          labwareDefURI: 'fixture/fixture_universal_flat_bottom_adapter/1',
+          labwareDefURI: mockAdapterURI,
           slot: 'D3',
           pythonName: 'mockPythonName',
         },
@@ -85,7 +103,7 @@ describe('SelectedHoveredItems', () => {
   })
   it('renders a selected fixture with a selected labware', () => {
     vi.mocked(selectors.getZoomedInSlotInfo).mockReturnValue({
-      selectedLabwareDefUri: 'fixture/fixture_universal_flat_bottom_adapter/1',
+      selectedLabwareDefUri: mockAdapterURI,
       selectedNestedLabwareDefUri: null,
       selectedFixture: 'trashBin',
       selectedModuleModel: null,
@@ -112,7 +130,7 @@ describe('SelectedHoveredItems', () => {
   })
   it('renders a selected module and a selected labware', () => {
     vi.mocked(selectors.getZoomedInSlotInfo).mockReturnValue({
-      selectedLabwareDefUri: 'fixture/fixture_universal_flat_bottom_adapter/1',
+      selectedLabwareDefUri: mockAdapterURI,
       selectedNestedLabwareDefUri: null,
       selectedFixture: null,
       selectedModuleModel: HEATERSHAKER_MODULE_V1,
@@ -133,23 +151,22 @@ describe('SelectedHoveredItems', () => {
         labware: {
           id: 'mockId',
           def: fixture24Tuberack as LabwareDefinition2,
-          labwareDefURI: 'fixture/fixture_universal_flat_bottom_adapter/1',
+          labwareDefURI: mockAdapterURI,
           slot: 'D3',
           pythonName: 'mockPythonName',
         },
         labware2: {
           id: 'mockId2',
           def: fixture24Tuberack as LabwareDefinition2,
-          labwareDefURI: 'fixture/fixture_universal_flat_bottom_adapter/1',
+          labwareDefURI: mockAdapterURI,
           slot: 'mockId',
           pythonName: 'mockPythonName',
         },
       },
     })
     vi.mocked(selectors.getZoomedInSlotInfo).mockReturnValue({
-      selectedLabwareDefUri: 'fixture/fixture_universal_flat_bottom_adapter/1',
-      selectedNestedLabwareDefUri:
-        'fixture/fixture_universal_flat_bottom_adapter/1',
+      selectedLabwareDefUri: mockAdapterURI,
+      selectedNestedLabwareDefUri: mockAdapterURI,
       selectedFixture: 'trashBin',
       selectedModuleModel: null,
       selectedSlot: { slot: 'D3', cutout: 'cutoutD3' },
