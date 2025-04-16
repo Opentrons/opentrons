e27811efde754fac8d77561c5e2844177dbe0cce
Jethary <jethary@opentrons.com>
2025-04-16 14:55:47 -0400

    fix(protocol-designer): closes AUTH-1745
    closes AUTH-1745
    
    closes AUTH-1745

--- protocol-designer/src/load-file/migration/7_0_0.ts
+++ protocol-designer/src/load-file/migration/7_0_0.ts
@@ -3,11 +3,12 @@ import { uuid } from '../../utils'
 import { getOnlyLatestDefs } from '../../labware-defs'
 import { INITIAL_DECK_SETUP_STEP_ID } from '../../constants'
 import { getAdapterAndLabwareSplitInfo } from './utils/getAdapterAndLabwareSplitInfo'
-import type {
-  LabwareDefinition2,
-  LabwareDefinitionsByUri,
-  LoadLiquidCreateCommand,
-  ProtocolFileV6,
+import {
+  getAllLabwareDefs,
+  type LabwareDefinition2,
+  type LabwareDefinitionsByUri,
+  type LoadLiquidCreateCommand,
+  type ProtocolFileV6,
 } from '@opentrons/shared-data'
 import type {
   LoadPipetteCreateCommand,
@@ -62,7 +63,7 @@ export const migrateFile = (
     ].labwareLocationUpdate
   const ingredLocations = appData.designerApplication?.data?.ingredLocations
 
-  const allLatestDefs = getOnlyLatestDefs()
+  const allLabwareDefs = getAllLabwareDefs()
 
   const getIsAdapter = (labwareId: string): boolean => {
     const labwareEntity = labware[labwareId]
@@ -149,11 +150,11 @@ export const migrateFile = (
       const {
         parameters: adapterParameters,
         version: adapterVersion,
-      } = allLatestDefs[adapterUri]
+      } = allLabwareDefs[adapterUri]
       const {
         parameters: labwareParameters,
         version: labwareVersion,
-      } = allLatestDefs[labwareUri]
+      } = allLabwareDefs[labwareUri]
       const adapterId = mappedLabwareIds[command.params.labwareId].newAdapterId
 
       const loadAdapterCommand: LoadLabwareCreateCommand = {
@@ -196,8 +197,8 @@ export const migrateFile = (
     const loadName = labwareDefinition.parameters.loadName
     if (ADAPTER_LABWARE_COMBO_LOAD_NAMES.includes(loadName)) {
       const { adapterUri, labwareUri } = getAdapterAndLabwareSplitInfo(defId)
-      const adapterLabwareDef = allLatestDefs[adapterUri]
-      const labwareDef = allLatestDefs[labwareUri]
+      const adapterLabwareDef = allLabwareDefs[adapterUri]
+      const labwareDef = allLabwareDefs[labwareUri]
       acc[adapterUri] = adapterLabwareDef
       acc[labwareUri] = labwareDef
     } else {
