dc3521af06c01aefff16b70aaf0373d29741dee2
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-04-21 16:21:54 -0400

    refactor(app): Fix ignore error logic for "no liquid detected"

--- app/src/organisms/ErrorRecoveryFlows/RecoveryOptions/FillWellAndRetrySameTips.tsx
+++ app/src/organisms/ErrorRecoveryFlows/RecoveryOptions/FillWellAndRetrySameTips.tsx
@@ -1,6 +1,14 @@
+import { useTranslation, Trans } from 'react-i18next'
+
+import { LegacyStyledText } from '@opentrons/components'
+
 import { RECOVERY_MAP } from '../constants'
 import { CancelRun } from './CancelRun'
-import { FillWell, RetryWithSameTips } from '../shared'
+import {
+  FillWell,
+  RetryWithSameTips,
+  TwoColTextAndFailedStepNextStep,
+} from '../shared'
 import { SelectRecoveryOption } from './SelectRecoveryOption'
 
 import type { RecoveryContentProps } from '../types'
@@ -10,6 +18,7 @@ export function FillWellAndRetrySameTips(
 ): JSX.Element {
   const { recoveryMap } = props
   const { step, route } = recoveryMap
+  console.log('=>(FillWellAndRetrySameTips.tsx:21) step', step)
   const { MANUAL_FILL_AND_RETRY_SAME_TIPS, CANCEL_RUN } = RECOVERY_MAP
 
   const buildContent = (): JSX.Element => {
@@ -18,6 +27,8 @@ export function FillWellAndRetrySameTips(
         return <FillWell {...props} />
       case MANUAL_FILL_AND_RETRY_SAME_TIPS.STEPS.RETRY_SAME_TIPS:
         return <RetryWithSameTips {...props} />
+      case MANUAL_FILL_AND_RETRY_SAME_TIPS.STEPS.SKIP:
+        return <SkipToNextStep {...props} />
       case CANCEL_RUN.STEPS.CONFIRM_CANCEL:
         return <CancelRun {...props} />
       default:
@@ -29,4 +40,60 @@ export function FillWellAndRetrySameTips(
   }
 
   return buildContent()
+}
+
+export function SkipToNextStep(
+  props: RecoveryContentProps
+): JSX.Element | null {
+  const {
+    routeUpdateActions,
+    recoveryCommands,
+    currentRecoveryOptionUtils,
+  } = props
+  const {
+    handleMotionRouting,
+    goBackPrevStep,
+    proceedToRouteAndStep,
+  } = routeUpdateActions
+  const { selectedRecoveryOption } = currentRecoveryOptionUtils
+  const { skipFailedCommand } = recoveryCommands
+  const { ROBOT_SKIPPING_STEP, IGNORE_AND_SKIP } = RECOVERY_MAP
+  const { t } = useTranslation('error_recovery')
+
+  const secondaryBtnOnClick = (): void => {
+    if (selectedRecoveryOption === IGNORE_AND_SKIP.ROUTE) {
+      void proceedToRouteAndStep(IGNORE_AND_SKIP.ROUTE)
+    } else {
+      void goBackPrevStep()
+    }
+  }
+
+  const primaryBtnOnClick = (): Promise<void> => {
+    return handleMotionRouting(true, ROBOT_SKIPPING_STEP.ROUTE).then(() => {
+      skipFailedCommand()
+    })
+  }
+
+  const buildBodyText = (): JSX.Element => {
+    return (
+      <Trans
+        t={t}
+        i18nKey="robot_will_not_check_for_liquid"
+        components={{
+          block: <LegacyStyledText as="p" />,
+        }}
+      />
+    )
+  }
+
+  return (
+    <TwoColTextAndFailedStepNextStep
+      {...props}
+      leftColTitle={t('skip_to_next_step')}
+      leftColBodyText={buildBodyText()}
+      primaryBtnCopy={t('continue_run_now')}
+      primaryBtnOnClick={primaryBtnOnClick}
+      secondaryBtnOnClickOverride={secondaryBtnOnClick}
+    />
+  )
 }
--- app/src/organisms/ErrorRecoveryFlows/constants.ts
+++ app/src/organisms/ErrorRecoveryFlows/constants.ts
@@ -141,6 +141,7 @@ export const RECOVERY_MAP = {
     STEPS: {
       MANUAL_FILL: 'manual-fill',
       RETRY_SAME_TIPS: 'retry-same-tips',
+      SKIP: 'skip',
     },
   },
   MANUAL_FILL_AND_RETRY_NEW_TIPS: {
@@ -282,6 +283,7 @@ export const STEP_ORDER: StepOrder = {
   [MANUAL_FILL_AND_RETRY_SAME_TIPS.ROUTE]: [
     MANUAL_FILL_AND_RETRY_SAME_TIPS.STEPS.MANUAL_FILL,
     MANUAL_FILL_AND_RETRY_SAME_TIPS.STEPS.RETRY_SAME_TIPS,
+    MANUAL_FILL_AND_RETRY_SAME_TIPS.STEPS.SKIP,
   ],
   [MANUAL_FILL_AND_RETRY_NEW_TIPS.ROUTE]: [
     MANUAL_FILL_AND_RETRY_NEW_TIPS.STEPS.MANUAL_FILL,
@@ -404,6 +406,9 @@ export const RECOVERY_MAP_METADATA: RecoveryRouteStepMetadata = {
       allowDoorOpen: true,
     },
     [MANUAL_FILL_AND_RETRY_SAME_TIPS.STEPS.RETRY_SAME_TIPS]: {
+      allowDoorOpen: true,
+    },
+    [MANUAL_FILL_AND_RETRY_SAME_TIPS.STEPS.SKIP]: {
       allowDoorOpen: true,
     },
   },
