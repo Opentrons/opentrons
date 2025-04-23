9e1b0a705f96eaca3896068c3db463220810eb83
elyorcv <elyor.kodirov@opentrons.com>
2025-04-23 21:27:59 +0100

    fix import order

--- opentrons-ai-client/src/resources/utils/createProtocolUtils.tsx
+++ opentrons-ai-client/src/resources/utils/createProtocolUtils.tsx
@@ -18,10 +18,10 @@ import {
 import { getOnlyLatestDefs } from './labware'
 
 import type { UseFormWatch } from 'react-hook-form'
+import { PYTHON } from '../../organisms/ProtocolFormatSection'
 import type { PipetteName } from '@opentrons/shared-data'
 import type { CreateProtocolFormData } from '../../pages/CreateProtocol'
 import type { CreatePrompt } from '../types'
-import { PYTHON } from '../../organisms/ProtocolFormatSection'
 
 export function generatePromptPreviewProtocolFormatItems(
   watch: UseFormWatch<CreateProtocolFormData>,
