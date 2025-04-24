56146212dd60b969384e6413c3408a231e7b11b0
ncdiehl11 <ndiehl@opentrons.com>
2025-04-24 14:50:29 -0400

    style(protocol-designer): fix whitespace

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/StepTools/MoveLiquidTools/FirstStepMoveLiquidTools.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/StepTools/MoveLiquidTools/FirstStepMoveLiquidTools.tsx
@@ -1,6 +1,12 @@
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
-import { DIRECTION_COLUMN, Divider, Flex, SPACING, StyledText } from '@opentrons/components'
+import {
+  DIRECTION_COLUMN,
+  Divider,
+  Flex,
+  SPACING,
+  StyledText,
+} from '@opentrons/components'
 
 import {
   getEnablePartialTipSupport,
