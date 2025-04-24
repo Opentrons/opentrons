1ffd003b81840959041aa9dff4b566fe82d03f52
Jethary <jethary@opentrons.com>
2025-04-24 12:41:10 -0400

    fix(protocol-designer): cypress e2e migration tests migrated correctly to 8_5_0

--- protocol-designer/src/constants.ts
+++ protocol-designer/src/constants.ts
@@ -196,7 +196,6 @@ export const ABSORBANCE_READER_COLOR_BY_WAVELENGTH: Record<number, string> = {
 }
 
 export const OFFDECK: 'offDeck' = 'offDeck'
-export const GRIPPER_LOCATION: 'mounted' = 'mounted'
 
 export const PROTOCOL_DESIGNER_SOURCE: 'Protocol Designer' = 'Protocol Designer' // protocolSource for tracking analytics in the app
 
