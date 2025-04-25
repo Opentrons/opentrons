ceb983a4179022ce4b0c484179e4b29c39839181
Laura Cox <31892318+Laura-Danielle@users.noreply.github.com>
2025-04-25 19:58:40 +0300

    feat(api): RobotContext add opentrons_simulate outputs (#17040)
    # Overview
    
    Any movement related commands in the robot context should have run logs
    associated with them.
    
    ## Test Plan and Hands on Testing
    
    1. Check that run logs get generated for robot commands.
    
    ## Changelog
    
    - Add run logs to robot context commands
    
    ## Review requests
    
    Check that the run log messages make sense.
    
    ## Risk assessment
    
    Low. Adding run logs to un-used API.
    
    ---------
    
    Co-authored-by: Seth Foster <seth@opentrons.com>
    (cherry picked from commit 37824558eeb6ba6cc5a628067681bc2b9cc653af)

--- api/src/opentrons/protocol_engine/commands/command_unions.py
+++ api/src/opentrons/protocol_engine/commands/command_unions.py
@@ -498,8 +498,8 @@ Command = Annotated[
         robot.MoveTo,
         robot.MoveAxesRelative,
         robot.MoveAxesTo,
-        robot.openGripperJaw,
-        robot.closeGripperJaw,
+        robot.OpenGripperJaw,
+        robot.CloseGripperJaw,
     ],
     Field(discriminator="commandType"),
 ]
@@ -599,8 +599,8 @@ CommandParams = Union[
     robot.MoveAxesRelativeParams,
     robot.MoveAxesToParams,
     robot.MoveToParams,
-    robot.openGripperJawParams,
-    robot.closeGripperJawParams,
+    robot.OpenGripperJawParams,
+    robot.CloseGripperJawParams,
 ]
 
 CommandType = Union[
@@ -698,8 +698,8 @@ CommandType = Union[
     robot.MoveAxesRelativeCommandType,
     robot.MoveAxesToCommandType,
     robot.MoveToCommandType,
-    robot.openGripperJawCommandType,
-    robot.closeGripperJawCommandType,
+    robot.OpenGripperJawCommandType,
+    robot.CloseGripperJawCommandType,
 ]
 
 CommandCreate = Annotated[
@@ -798,8 +798,8 @@ CommandCreate = Annotated[
         robot.MoveAxesRelativeCreate,
         robot.MoveAxesToCreate,
         robot.MoveToCreate,
-        robot.openGripperJawCreate,
-        robot.closeGripperJawCreate,
+        robot.OpenGripperJawCreate,
+        robot.CloseGripperJawCreate,
     ],
     Field(discriminator="commandType"),
 ]
@@ -906,8 +906,8 @@ CommandResult = Union[
     robot.MoveAxesRelativeResult,
     robot.MoveAxesToResult,
     robot.MoveToResult,
-    robot.openGripperJawResult,
-    robot.closeGripperJawResult,
+    robot.OpenGripperJawResult,
+    robot.CloseGripperJawResult,
 ]
 
 
