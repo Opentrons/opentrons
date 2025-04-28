06c5233b0ed79ae93cac1b9b57cad6a5f49eaceb
Seth Foster <seth@opentrons.com>
2025-04-28 14:43:49 -0400

    format

--- shared-data/python/opentrons_shared_data/labware/__init__.py
+++ shared-data/python/opentrons_shared_data/labware/__init__.py
@@ -4,7 +4,7 @@ opentrons_shared_data.labware: types and functions for accessing labware defs
 
 from __future__ import annotations
 import json
-from typing import Any, Dict, NewType, TYPE_CHECKING, overload
+from typing import Any, Dict, NewType, TYPE_CHECKING, overload, Literal
 
 from .. import load_shared_data
 
@@ -17,13 +17,15 @@ Schema = NewType("Schema", Dict[str, Any])
 @overload
 def load_definition(
     loadname: str, version: int, schema: Literal[2] = 2
-) -> LabwareDefinition2: ...
+) -> LabwareDefinition2:
+    ...
 
 
 @overload
 def load_definition(
     loadname: str, version: int, schema: Literal[3]
-) -> LabwareDefinition3: ...
+) -> LabwareDefinition3:
+    ...
 
 
 def load_definition(loadname: str, version: int, schema: int = 2) -> LabwareDefinition:
