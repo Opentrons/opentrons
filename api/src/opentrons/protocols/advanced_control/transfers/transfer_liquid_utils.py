7429fae5e948465f46df93026dd553dea5ee3a6f
Sanniti Pimpley <sanni-t@users.noreply.github.com>
2025-04-28 14:06:47 -0400

    fix(api): meniscus relative positioning in LC based transfers (#18097)
    Closes AUTH-1653
    
    # Overview
    
    Meniscus relative positions for aspirate & dispense will now be relative
    to the position of meniscus of the liquid *after* the aspirate or
    dispense action.
    
    Updated the `absolute_point_from_position_reference_and_offset()`
    function to use `well.estimate_liquid_height_after_pipetting()` instead
    of `well.meniscus()`
    
    ## Risk assessment
    
    Low. Only affects meniscus-relative pipetting locations

--- api/src/opentrons/protocols/advanced_control/transfers/transfer_liquid_utils.py
+++ api/src/opentrons/protocols/advanced_control/transfers/transfer_liquid_utils.py
@@ -48,7 +48,7 @@ def raise_if_location_inside_liquid(
         liquid_height_from_bottom = well_core.current_liquid_height()
     except LiquidHeightUnknownError:
         liquid_height_from_bottom = None
-    if liquid_height_from_bottom is not None:
+    if isinstance(liquid_height_from_bottom, (int, float)):
         if liquid_height_from_bottom + well_core.get_bottom(0).z > location.point.z:
             raise RuntimeError(
                 f"{location_check_descriptors.location_type.capitalize()} location {location} is"
