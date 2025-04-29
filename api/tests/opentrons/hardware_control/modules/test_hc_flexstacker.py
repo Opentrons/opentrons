07130f8bb91c6b8f0728e53414747fd074288021
Brayan Almonte <vegano1@users.noreply.github.com>
2025-04-29 14:37:08 -0400

    feat(api): For the Flex Stacker only set run/ihold current if they are actually different (#18175)

--- api/tests/opentrons/hardware_control/modules/test_hc_flexstacker.py
+++ api/tests/opentrons/hardware_control/modules/test_hc_flexstacker.py
@@ -2,8 +2,23 @@ import asyncio
 import pytest
 import mock
 from typing import AsyncGenerator
+from opentrons.drivers.flex_stacker.driver import (
+    STACKER_MOTION_CONFIG,
+)
+from opentrons.drivers.flex_stacker.simulator import SimulatingDriver
+from opentrons.drivers.flex_stacker.types import (
+    Direction,
+    LimitSwitchStatus,
+    PlatformStatus,
+    StackerAxis,
+)
 from opentrons.hardware_control import modules, ExecutionManager
 from opentrons.drivers.rpi_drivers.types import USBPort
+from opentrons.hardware_control.modules.flex_stacker import (
+    SIMULATING_POLL_PERIOD,
+    FlexStackerReader,
+)
+from opentrons.hardware_control.poller import Poller
 
 
 @pytest.fixture
@@ -16,37 +31,113 @@ def usb_port() -> USBPort:
 
 
 @pytest.fixture
-async def simulating_module(
-    usb_port: USBPort,
-) -> AsyncGenerator[modules.AbstractModule, None]:
-    module = await modules.build(
-        port=usb_port.device_path,
-        usb_port=usb_port,
-        type=modules.ModuleType["FLEX_STACKER"],
-        simulating=True,
-        hw_control_loop=asyncio.get_running_loop(),
-        execution_manager=ExecutionManager(),
-    )
-    assert isinstance(module, modules.AbstractModule)
-    try:
-        yield module
-    finally:
-        await module.cleanup()
+def mock_driver() -> mock.AsyncMock:
+    return mock.AsyncMock(spec=SimulatingDriver)
 
 
 @pytest.fixture
-async def simulating_module_driver_patched(
-    simulating_module: modules.FlexStacker,
-) -> AsyncGenerator[modules.AbstractModule, None]:
-    driver_mock = mock.MagicMock()
-    with mock.patch.object(
-        simulating_module, "_driver", driver_mock
-    ), mock.patch.object(simulating_module._reader, "_driver", driver_mock):
-        yield simulating_module
+async def subject(
+    usb_port: USBPort,
+    mock_driver: mock.AsyncMock,
+) -> AsyncGenerator[modules.FlexStacker, None]:
+    """Test subject with mocked driver"""
+    reader = FlexStackerReader(driver=mock_driver)
+    poller = Poller(reader=reader, interval=SIMULATING_POLL_PERIOD)
+    stacker = modules.FlexStacker(
+        port="/dev/ot_module_sim_flexstacker0",
+        usb_port=usb_port,
+        driver=mock_driver,
+        reader=reader,
+        poller=poller,
+        device_info={
+            "serial": "dummySerialFS",
+            "model": "a1",
+            "version": "stacker-fw",
+        },
+        hw_control_loop=asyncio.get_running_loop(),
+        execution_manager=ExecutionManager(),
+    )
+    await poller.start()
+    try:
+        yield stacker
+    finally:
+        await stacker.cleanup()
 
 
-async def test_sim_state(simulating_module: modules.FlexStacker) -> None:
-    status = simulating_module.device_info
+async def test_sim_state(subject: modules.FlexStacker) -> None:
+    status = subject.device_info
     assert status["serial"] == "dummySerialFS"
     assert status["model"] == "a1"
     assert status["version"] == "stacker-fw"
+
+
+async def test_set_run_hold_current(
+    subject: modules.FlexStacker, mock_driver: mock.AsyncMock
+) -> None:
+    mock_driver.get_platform_status.side_effect = [
+        PlatformStatus(True, False),
+        PlatformStatus(False, True),
+    ]
+    mock_driver.get_limit_switches_status.side_effect = [
+        LimitSwitchStatus(False, True, False, False, False),
+        LimitSwitchStatus(True, True, False, False, False),
+    ]
+
+    # Test move_axis
+
+    # run and hold current are 0 by default
+    assert subject._reader.motion_params[StackerAxis.X].run_current == 0
+    assert subject._reader.motion_params[StackerAxis.X].hold_current == 0
+    default = STACKER_MOTION_CONFIG[StackerAxis.X]["move"]
+
+    # Call the move_axis function with default current
+    await subject.move_axis(StackerAxis.X, Direction.EXTEND, 44)
+    # set_run_current should be called and run_current recorded
+    mock_driver.set_run_current.assert_called_with(StackerAxis.X, default.run_current)
+    mock_driver.set_ihold_current.assert_called_with(
+        StackerAxis.X, default.hold_current
+    )
+    motion_params = subject._reader.motion_params[StackerAxis.X]
+    assert motion_params.run_current == default.run_current
+    assert motion_params.hold_current == default.hold_current
+    mock_driver.set_run_current.reset_mock()
+    mock_driver.set_ihold_current.reset_mock()
+
+    # Make sure set_run_current and set_ihold_current are not called again
+    await subject.move_axis(StackerAxis.X, Direction.EXTEND, 44)
+    mock_driver.set_run_current.assert_not_called()
+    mock_driver.set_ihold_current.assert_not_called()
+    motion_params = subject._reader.motion_params[StackerAxis.X]
+    assert motion_params.run_current == default.run_current
+    assert motion_params.hold_current == default.hold_current
+
+    # Test home_axis
+
+    # Reset the run/hold current recorded
+    default = STACKER_MOTION_CONFIG[StackerAxis.X]["home"]
+    subject._reader.motion_params[StackerAxis.X].run_current = 0
+    subject._reader.motion_params[StackerAxis.X].hold_current = 0
+
+    # Call the home_axis function with default current
+    await subject.home_axis(StackerAxis.X, Direction.EXTEND)
+    mock_driver.set_run_current.assert_called_with(StackerAxis.X, default.run_current)
+    mock_driver.set_ihold_current.assert_called_with(
+        StackerAxis.X, default.hold_current
+    )
+    motion_params = subject._reader.motion_params[StackerAxis.X]
+    assert motion_params.run_current == default.run_current
+    assert motion_params.hold_current == default.hold_current
+    mock_driver.set_run_current.reset_mock()
+    mock_driver.set_ihold_current.reset_mock()
+
+    # Make sure set_run_current and set_ihold_current are not called again
+    await subject.home_axis(StackerAxis.X, Direction.EXTEND, 44)
+    mock_driver.set_run_current.assert_not_called()
+    mock_driver.set_ihold_current.assert_not_called()
+
+    # The recorded run/hold current should stay the same
+    motion_params = subject._reader.motion_params[StackerAxis.X]
+    assert motion_params.run_current == default.run_current
+    assert motion_params.hold_current == default.hold_current
+    mock_driver.set_run_current.reset_mock()
+    mock_driver.set_ihold_current.reset_mock()
