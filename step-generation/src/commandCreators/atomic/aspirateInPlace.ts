33160ff1b38c9ff1e1d4fdab636b964ac21869a4
ncdiehl11 <ndiehl@opentrons.com>
2025-04-29 10:56:42 -0400

    feat(step-generation): add python to `aspirateInPlace`
    This PR adds Python code to the `aspirateInPlace` step-generation atomic command creator.

--- step-generation/src/commandCreators/atomic/aspirateInPlace.ts
+++ step-generation/src/commandCreators/atomic/aspirateInPlace.ts
@@ -1,4 +1,4 @@
-import { uuid } from '../../utils'
+import { indentPyLines, uuid } from '../../utils'
 
 import type { AspirateInPlaceParams } from '@opentrons/shared-data'
 import type { CommandCreator } from '../../types'
@@ -6,7 +6,7 @@ import type { CommandCreator } from '../../types'
 export const aspirateInPlace: CommandCreator<AspirateInPlaceParams> = (
   args,
   invariantContext,
-  prevRobotState
+  robotState
 ) => {
   const { pipetteId, volume, flowRate } = args
 
@@ -21,7 +21,23 @@ export const aspirateInPlace: CommandCreator<AspirateInPlaceParams> = (
       },
     },
   ]
+  console.log(pipetteId)
+  console.log(invariantContext.pipetteEntities)
+
+  const pipettePythonName =
+    invariantContext.pipetteEntities[pipetteId].pythonName
+  const pythonArgs = [
+    `volume=${volume}`,
+    // rate= is a ratio in the PAPI, and we have no good way to figure out what
+    // flowrate the PAPI has set the pipette to, so we just have to do a division:
+    `rate=${flowRate} / ${pipettePythonName}.flow_rate.aspirate`,
+  ]
+  const python = `${pipettePythonName}.aspirate(\n${indentPyLines(
+    pythonArgs.join(',\n')
+  )},\n)`
+
   return {
     commands,
+    python,
   }
 }
