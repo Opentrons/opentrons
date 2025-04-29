24eae7480ab8907bdfeede4bc5efabf5ea0360c2
Shlok Amin <shlokamin@me.com>
2025-04-29 15:06:06 -0400

    feat(opentrons-ai-client): add hidden feature flags (#18198)
    closes AUTH-1789

--- components/src/controls/index.ts
+++ components/src/controls/index.ts
@@ -3,7 +3,6 @@
 // belong in the please-refactor-these-to-use-primitives-and-styled-components
 // category). We should move them all.
 export * from './ToggleButton'
-export * from './LabeledToggle'
 export * from './LabeledButton'
 export * from './LabeledCheckbox'
 export * from './LabeledRadioGroup'
--- opentrons-ai-client/src/OpentronsAI.tsx
+++ opentrons-ai-client/src/OpentronsAI.tsx
@@ -19,7 +19,14 @@ import { Header } from './molecules/Header'
 import { HeaderWithMeter } from './molecules/HeaderWithMeter'
 import { Loading } from './molecules/Loading'
 import { OpentronsAIRoutes } from './OpentronsAIRoutes'
-import { headerWithMeterAtom, mixpanelAtom, tokenAtom } from './resources/atoms'
+import { FeatureFlagsModal } from './organisms/FeatureFlagsModal'
+import {
+  displayFeatureFlagsModalAtom,
+  featureFlagsAtom,
+  headerWithMeterAtom,
+  mixpanelAtom,
+  tokenAtom,
+} from './resources/atoms'
 import { CLIENT_MAX_WIDTH } from './resources/constants'
 import { useGetAccessToken } from './resources/hooks'
 import { useTrackEvent } from './resources/hooks/useTrackEvent'
@@ -30,6 +37,9 @@ export function OpentronsAI(): JSX.Element | null {
   const [{ displayHeaderWithMeter, progress }] = useAtom(headerWithMeterAtom)
   const [mixpanelState, setMixpanelState] = useAtom(mixpanelAtom)
   const { getAccessToken } = useGetAccessToken()
+  const [featureFlags, setFeatureFlags] = useAtom(featureFlagsAtom)
+  const [displayFeatureFlagsModal] = useAtom(displayFeatureFlagsModalAtom)
+
   const trackEvent = useTrackEvent()
 
   const fetchAccessToken = async (): Promise<void> => {
@@ -69,6 +79,10 @@ export function OpentronsAI(): JSX.Element | null {
     return null
   }
 
+  global.enablePrereleaseMode = () => {
+    setFeatureFlags({ enablePrereleaseMode: true })
+  }
+
   return (
     <Flex
       id="opentrons-ai"
@@ -76,6 +90,9 @@ export function OpentronsAI(): JSX.Element | null {
       height={'100vh'}
       flexDirection={DIRECTION_COLUMN}
     >
+      {displayFeatureFlagsModal && featureFlags.enablePrereleaseMode && (
+        <FeatureFlagsModal />
+      )}
       <StickyHeader>
         {displayHeaderWithMeter ? (
           <HeaderWithMeter progressPercentage={progress} />
