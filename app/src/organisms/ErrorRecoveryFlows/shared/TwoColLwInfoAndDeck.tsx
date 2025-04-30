f95692965d08d26eff8e3e28a509171877f22aef
TamarZanzouri <zanzouritamar@gmail.com>
2025-04-30 13:24:41 -0400

    Update app/src/organisms/ErrorRecoveryFlows/shared/TwoColLwInfoAndDeck.tsx

--- app/src/organisms/ErrorRecoveryFlows/shared/TwoColLwInfoAndDeck.tsx
+++ app/src/organisms/ErrorRecoveryFlows/shared/TwoColLwInfoAndDeck.tsx
@@ -145,7 +145,7 @@ export function TwoColLwInfoAndDeck(
         if (
           step === MANUAL_LOAD_IN_STACKER_AND_SKIP.STEPS.MANUAL_REPLACE ||
           step ===
-            HOPPER_MANUAL_LOAD_ON_SHUTTLE_AND_SKIP.STEPS.HOOPER_MANUAL_REPLACE
+            HOPPER_MANUAL_LOAD_ON_SHUTTLE_AND_SKIP.STEPS.HOPPER_MANUAL_REPLACE
         ) {
           return null
         } else {
