d50923b45122900a6f1e0fbf64e9b393fd326250
TamarZanzouri <zanzouritamar@gmail.com>
2025-04-30 13:21:57 -0400

    Update app/src/organisms/ErrorRecoveryFlows/shared/LeftColumnLabwareInfo.tsx

--- app/src/organisms/ErrorRecoveryFlows/shared/LeftColumnLabwareInfo.tsx
+++ app/src/organisms/ErrorRecoveryFlows/shared/LeftColumnLabwareInfo.tsx
@@ -77,7 +77,7 @@ export function LeftColumnLabwareInfo({
             },
           }
         case MANUAL_LOAD_IN_STACKER_AND_SKIP.STEPS.MANUAL_REPLACE:
-        case HOPPER_MANUAL_LOAD_ON_SHUTTLE_AND_SKIP.STEPS.HOOPER_MANUAL_REPLACE:
+        case HOPPER_MANUAL_LOAD_ON_SHUTTLE_AND_SKIP.STEPS.HOPPER_MANUAL_REPLACE:
         case MANUAL_LOAD_ON_SHUTTLE_AND_SKIP.STEPS.CONFIRM_RETRY:
           return {
             labwareName: failedLabwareNames.name ?? '',
