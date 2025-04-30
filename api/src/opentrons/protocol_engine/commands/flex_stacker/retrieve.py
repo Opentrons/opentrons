31c31de6001f33f32bd3610a5637b81e54582380
tamarzanzouri <tamar.zanzouri@opentrons.com>
2025-04-30 11:09:33 -0400

    revert retieve testing change

--- api/src/opentrons/protocol_engine/commands/flex_stacker/retrieve.py
+++ api/src/opentrons/protocol_engine/commands/flex_stacker/retrieve.py
@@ -201,11 +201,6 @@ class RetrieveImpl(AbstractCommandImpl[RetrieveParams, _ExecuteReturn]):
 
     async def execute(self, params: RetrieveParams) -> _ExecuteReturn:
         """Execute the labware retrieval command."""
-        try:
-            raise FlexStackerShuttleLabwareError(serial="123", labware_expected=False, shuttle_state="")
-        except FlexStackerShuttleLabwareError as e:
-            return self.handle_recoverable_error(e, '123',
-            update_types.StateUpdate())
         stacker_state = self._state_view.modules.get_flex_stacker_substate(
             params.moduleId
         )
