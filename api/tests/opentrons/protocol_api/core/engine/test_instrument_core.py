b4a158f6012577533d2ac7785e84a3bfc20d8f6d
Max Marrone <max@opentrons.com>
2025-04-30 14:01:40 -0400

    fix(api): Do not hang if there is an error in `pickUpTip` planning (#18207)
    ## Overview
    
    This closes RQA-4058 (protocol analysis hanging if `pickUpTip` is given
    a bad labware definition), and resolves some duplication we noticed a
    little while ago
    (https://github.com/Opentrons/opentrons/pull/16469#discussion_r1797230213).
    
    ## Details
    
    When a `pickUpTip` command completes, `TipStore` marks the picked-up
    tips as "used." Before this PR, that worked by `PickUpTipImplementation`
    passing along just the single "target" tip, like A1; `TipStore` then
    internally expanded that into the full list of actually-picked-up tips,
    like A1+B1+⋯+H1, for an 8-channel pickup.
    
    The choice to do the expansion inside `TipStore` caused two problems:
    
    1. Error handling. If it happens inside `TipStore`, and something goes
    wrong and raises an exception, then we've raised an exception from
    inside an `opentrons.protocol_engine` *state update.* We are not in a
    good position to handle that: it was an early architectural of
    `opentrons.protocol_engine` that all the real work ought to happen in
    command implementations, and that state updates ought to be dead-simple
    and always successful.
    
    (We can and should do something about that premise, but that's beyond
    the scope of this PR.)
    
    This is what caused RQA-4058.
    
    2. Duplication. For `TipStore` to automatically expand the target well
    into the full list of actually-picked-up tips, it needed to know details
    about the pipette's current configuration. That was already part of
    `PipetteStore`'s job;`TipStore` was duplicating it.
    
    This PR tries to solve both of those by having `PickUpTipImplementation`
    give `TipStore` a full precomputed list of tips.
    `PickUpTipImplementation` produces that precomputed list with help from
    new functions in `PipetteView` and `TipView`. Pipette concerns like
    nozzle maps are expunged from `TipStore`. Overall, there's no new logic
    here—just shifting things around and changing what's running where.
    
    (cherry picked from commit 0855bd1e316ed4165ceba00021a0697d73082b52)

--- api/tests/opentrons/protocol_api/core/engine/test_instrument_core.py
+++ api/tests/opentrons/protocol_api/core/engine/test_instrument_core.py
@@ -463,9 +463,7 @@ def test_drop_tip_no_location(
         labware_id="labware-id",
         engine_client=mock_engine_client,
     )
-    decoy.when(
-        mock_engine_client.state.tips.get_pipette_channels("abc123")
-    ).then_return(8)
+    decoy.when(mock_engine_client.state.pipettes.get_channels("abc123")).then_return(8)
 
     subject.drop_tip(location=None, well_core=well_core, home_after=True)
 
@@ -517,9 +515,7 @@ def test_drop_tip_with_location(
     ).then_return(
         (WellLocation(origin=WellOrigin.TOP, offset=WellOffset(x=3, y=2, z=1)), False)
     )
-    decoy.when(
-        mock_engine_client.state.tips.get_pipette_channels("abc123")
-    ).then_return(8)
+    decoy.when(mock_engine_client.state.pipettes.get_channels("abc123")).then_return(8)
     decoy.when(mock_engine_client.state.labware.is_tiprack("labware-id")).then_return(
         True
     )
@@ -599,9 +595,7 @@ def test_drop_tip_in_waste_chute(
     waste_chute = decoy.mock(cls=WasteChute)
 
     decoy.when(waste_chute.offset).then_return(DisposalOffset(x=4, y=5, z=6))
-    decoy.when(
-        mock_engine_client.state.tips.get_pipette_channels("abc123")
-    ).then_return(96)
+    decoy.when(mock_engine_client.state.pipettes.get_channels("abc123")).then_return(96)
 
     subject.drop_tip_in_disposal_location(
         waste_chute, home_after=True, alternate_tip_drop=True
@@ -1306,9 +1300,7 @@ def test_get_channels(
 ) -> None:
     """It should get the pipette's number of channels."""
     decoy.when(
-        mock_engine_client.state.tips.get_pipette_channels(
-            pipette_id=subject.pipette_id
-        )
+        mock_engine_client.state.pipettes.get_channels(pipette_id=subject.pipette_id)
     ).then_return(42)
     assert subject.get_channels() == 42
 
@@ -1639,7 +1631,7 @@ def test_is_tip_tracking_available(
 ) -> None:
     """It should return whether tip tracking is available based on nozzle configuration."""
     decoy.when(
-        mock_engine_client.state.tips.get_pipette_channels(subject.pipette_id)
+        mock_engine_client.state.pipettes.get_channels(subject.pipette_id)
     ).then_return(pipette_channels)
     decoy.when(
         mock_engine_client.state.pipettes.get_nozzle_layout_type(subject.pipette_id)
