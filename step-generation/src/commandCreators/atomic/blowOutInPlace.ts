de159a5573ce0599feaea4219ac6bf5ebb3c7625
David Chau <david.chau@opentrons.com>
2025-05-02 15:09:37 -0400

    generate Python for blowOutInPlace command

--- step-generation/src/commandCreators/atomic/blowOutInPlace.ts
+++ step-generation/src/commandCreators/atomic/blowOutInPlace.ts
@@ -9,6 +9,9 @@ export const blowOutInPlace: CommandCreator<BlowoutInPlaceParams> = (
   prevRobotState
 ) => {
   const { pipetteId, flowRate } = args
+  const { pipetteEntities } = invariantContext
+  const pipette = pipetteEntities[pipetteId]
+  const pipettePythonName = pipette.pythonName
   const commands = [
     {
       commandType: 'blowOutInPlace' as const,
@@ -21,5 +24,9 @@ export const blowOutInPlace: CommandCreator<BlowoutInPlaceParams> = (
   ]
   return {
     commands,
+    python:
+      // blow_out() blows out in place if no location= is specified
+      `${pipettePythonName}.flow_rate.blow_out = ${flowRate}\n` +
+      `${pipettePythonName}.blow_out()`,
   }
 }
