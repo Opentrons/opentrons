4a7df2d71058eaeb0c5ebe64385a827e807d345d
elyorcv <elyor.kodirov@opentrons.com>
2025-05-02 10:51:31 +0100

    fix preciew steps font size issue

--- opentrons-ai-client/src/molecules/PromptPreviewSection/index.tsx
+++ opentrons-ai-client/src/molecules/PromptPreviewSection/index.tsx
@@ -77,6 +77,7 @@ const StepTag = styled.div`
   border-radius: ${BORDERS.borderRadius4};
   padding: ${SPACING.spacing2} ${SPACING.spacing8};
   width: 100%;
+  font-size: 0.87rem;
 `
 
 // Component to handle multiline steps
