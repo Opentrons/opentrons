a07673a0618ed6f0d94c7f852d21e2188281621c
elyorcv <elyor.kodirov@opentrons.com>
2025-05-06 12:03:37 +0100

    fix(opentrons-ai-client): fixed the modules design to match with Figma

--- opentrons-ai-client/src/molecules/ControlledEmptySelectorButtonGroup/index.tsx
+++ opentrons-ai-client/src/molecules/ControlledEmptySelectorButtonGroup/index.tsx
@@ -1,10 +1,17 @@
 import { Controller, useFormContext } from 'react-hook-form'
+import styled from 'styled-components'
 
 import { EmptySelectorButton, Flex, SPACING, WRAP } from '@opentrons/components'
 
 import { MODULES_FIELD_NAME } from '../../organisms/ModulesSection'
 
 import type { DisplayModules } from '../../organisms/ModulesSection'
+
+const ButtonWrapper = styled.div`
+  display: inline-block;
+  flex-grow: 0;
+  flex-shrink: 1;
+`
 
 export function ControlledEmptySelectorButtonGroup({
   modules,
@@ -20,20 +27,26 @@ export function ControlledEmptySelectorButtonGroup({
       name={MODULES_FIELD_NAME}
       render={({ field }) => {
         return (
-          <Flex flexWrap={WRAP} gap={SPACING.spacing8}>
+          <Flex
+            flexWrap={WRAP}
+            gap={SPACING.spacing8}
+            justifyContent="flex-start"
+          >
             {modules.map(module => (
-              <EmptySelectorButton
-                key={module.type}
-                iconName="plus"
-                onClick={() => {
-                  if (modulesWatch.some(m => m.type === module.type)) {
-                    return
-                  }
-                  field.onChange([...modulesWatch, module])
-                }}
-                text={module.name}
-                textAlignment="left"
-              />
+              <ButtonWrapper key={module.type}>
+                <EmptySelectorButton
+                  key={module.type}
+                  iconName="plus"
+                  onClick={() => {
+                    if (modulesWatch.some(m => m.type === module.type)) {
+                      return
+                    }
+                    field.onChange([...modulesWatch, module])
+                  }}
+                  text={module.name}
+                  textAlignment="left"
+                />
+              </ButtonWrapper>
             ))}
           </Flex>
         )
