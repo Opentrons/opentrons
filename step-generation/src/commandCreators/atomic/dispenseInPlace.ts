7fc291a1e964c103fe7bb53a3b38beffb3bbab12
David Chau <46395074+ddcc4@users.noreply.github.com>
2025-05-06 13:59:34 -0400

    feat(step-generation) generate Python for `dispenseInPlace` CommandCreator (#18269)
    # Overview
    
    I noticed that we didn't generate Python for `dispenseInPlace`. We will
    very likely need it for the new PD transfer step generator, and it's
    easy enough to implement it, so this PR emits the Python code for
    `dispenseInPlace`. AUTH-1094
    
    ## Test Plan and Hands on Testing
    
    Updated unit test.
    
    ## Risk assessment
    
    Low.

--- step-generation/src/commandCreators/atomic/dispenseInPlace.ts
+++ step-generation/src/commandCreators/atomic/dispenseInPlace.ts
@@ -1,4 +1,4 @@
-import { uuid } from '../../utils'
+import { indentPyLines, uuid } from '../../utils'
 
 import type { DispenseInPlaceParams } from '@opentrons/shared-data'
 import type { CommandCreator } from '../../types'
@@ -22,7 +22,22 @@ export const dispenseInPlace: CommandCreator<DispenseInPlaceParams> = (
       },
     },
   ]
+
+  const pipettePythonName =
+    invariantContext.pipetteEntities[pipetteId].pythonName
+  const pythonArgs = [
+    `volume=${volume}`,
+    // rate= is a ratio in the PAPI, and we have no good way to figure out what
+    // flowrate the PAPI has set the pipette to, so we just have to emit a division:
+    `rate=${flowRate} / ${pipettePythonName}.flow_rate.dispense`,
+    ...(pushOut != null ? [`push_out=${pushOut}`] : []),
+  ]
+  const python = `${pipettePythonName}.dispense(\n${indentPyLines(
+    pythonArgs.join(',\n')
+  )},\n)`
+
   return {
     commands,
+    python,
   }
 }
