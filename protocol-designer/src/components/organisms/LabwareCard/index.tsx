6a10512e4264e7a51d53684aa24355188c266e02
Jethary <jethary@opentrons.com>
2025-05-07 10:52:35 -0400

    create components

--- protocol-designer/src/components/organisms/LabwareCard/index.tsx
+++ protocol-designer/src/components/organisms/LabwareCard/index.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 import {
   ALIGN_START,
+  Box,
   Btn,
   COLORS,
   DIRECTION_COLUMN,
@@ -18,7 +20,9 @@ import {
 } from '@opentrons/components'
 
 import { openIngredientSelector } from '../../../labware-ingred/actions'
+import { getLabwareNicknamesById } from '../../../ui/labware/selectors'
 import { LINK_BUTTON_STYLE } from '../../atoms'
+import { LabwareCardOverflowMenu } from '../LabwareCardOverflowMenu'
 
 import type { LabwareOnDeck } from '../../../step-forms'
 import type { ThunkDispatch } from '../../../types'
@@ -36,61 +40,71 @@ export function LabwareCard(props: LabwareCardProps): JSX.Element {
   const dispatch = useDispatch<ThunkDispatch<any>>()
   const { t } = useTranslation('starting_deck_state')
   const { def } = labware
-  const displayName = def.metadata.displayName
+  const nickNames = useSelector(getLabwareNicknamesById)
+  const displayName = nickNames[labware.id]
   const isAdapterOrTiprack =
     def.allowedRoles?.includes('adapter') || def.parameters.isTiprack
-
+  const [showOverflowMenu, setShowOverflowMenu] = useState<boolean>(false)
   return (
-    <ListItem type="default" backgroundColor={COLORS.grey30}>
-      <Flex
-        flexDirection={DIRECTION_COLUMN}
-        gridGap={SPACING.spacing16}
-        alignItems={ALIGN_START}
-        padding={SPACING.spacing16}
-        position={POSITION_RELATIVE}
-        width="100%"
-      >
-        <Flex
-          position={POSITION_ABSOLUTE}
-          top={SPACING.spacing4}
-          right={SPACING.spacing4}
-        >
-          <OverflowBtn
-            data-testid="LabwareCard_overflowBtn"
-            onClick={() => {
-              console.log('TODO: wire up')
-            }}
-          />
-        </Flex>
+    <Box position={POSITION_RELATIVE}>
+      {showOverflowMenu ? (
+        <LabwareCardOverflowMenu
+          setShowOverflowMenu={setShowOverflowMenu}
+          //  TODO: refactor when we know how stacking labware works for this overflow menu
+          labwareIds={[labware.id]}
+        />
+      ) : null}
+      <ListItem type="default" backgroundColor={COLORS.grey30}>
         <Flex
           flexDirection={DIRECTION_COLUMN}
-          gridGap={SPACING.spacing4}
-          maxWidth="95%"
+          gridGap={SPACING.spacing16}
+          alignItems={ALIGN_START}
+          padding={SPACING.spacing16}
+          position={POSITION_RELATIVE}
+          width="100%"
         >
-          <StyledText desktopStyle="bodyDefaultSemiBold">
-            {displayName}
-          </StyledText>
-          {lidDisplayName != null ? (
-            <StyledText desktopStyle="captionRegular" color={COLORS.grey60}>
-              {lidDisplayName}
+          <Flex
+            position={POSITION_ABSOLUTE}
+            top={SPACING.spacing4}
+            right={SPACING.spacing4}
+          >
+            <OverflowBtn
+              data-testid="LabwareCard_overflowBtn"
+              onClick={() => {
+                setShowOverflowMenu(true)
+              }}
+            />
+          </Flex>
+          <Flex
+            flexDirection={DIRECTION_COLUMN}
+            gridGap={SPACING.spacing4}
+            maxWidth="95%"
+          >
+            <StyledText desktopStyle="bodyDefaultSemiBold">
+              {displayName}
             </StyledText>
+            {lidDisplayName != null ? (
+              <StyledText desktopStyle="captionRegular" color={COLORS.grey60}>
+                {lidDisplayName}
+              </StyledText>
+            ) : null}
+          </Flex>
+          {!isAdapterOrTiprack ? (
+            <Btn
+              textDecoration={TYPOGRAPHY.textDecorationUnderline}
+              css={LINK_BUTTON_STYLE}
+              onClick={() => {
+                dispatch(openIngredientSelector(labware.id))
+                navigate('/liquids')
+              }}
+            >
+              <StyledText desktopStyle="captionRegular">
+                {t('add_liquid')}
+              </StyledText>
+            </Btn>
           ) : null}
         </Flex>
-        {!isAdapterOrTiprack ? (
-          <Btn
-            textDecoration={TYPOGRAPHY.textDecorationUnderline}
-            css={LINK_BUTTON_STYLE}
-            onClick={() => {
-              dispatch(openIngredientSelector(labware.id))
-              navigate('/liquids')
-            }}
-          >
-            <StyledText desktopStyle="captionRegular">
-              {t('add_liquid')}
-            </StyledText>
-          </Btn>
-        ) : null}
-      </Flex>
-    </ListItem>
+      </ListItem>
+    </Box>
   )
 }
---
+++ protocol-designer/src/components/organisms/LabwareCardOverflowMenu/index.tsx
@@ -0,0 +1,163 @@
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import { css } from 'styled-components'
+
+import {
+  BORDERS,
+  COLORS,
+  CURSOR_POINTER,
+  DIRECTION_COLUMN,
+  Divider,
+  Flex,
+  MenuItem,
+  NO_WRAP,
+  POSITION_ABSOLUTE,
+  StyledText,
+  useOnClickOutside,
+} from '@opentrons/components'
+
+import {
+  ConfirmDeleteEntityInUseModal,
+  EditNickNameModal,
+} from '../../../components/organisms'
+import { deleteContainer } from '../../../labware-ingred/actions'
+import { getIsLabwareOnSlotInUse } from '../../../pages/Designer/DeckSetup/utils'
+import { getSavedStepForms } from '../../../step-forms/selectors'
+import { getDeckSetupForActiveItem } from '../../../top-selectors/labware-locations'
+
+import type { Dispatch, MouseEvent, SetStateAction } from 'react'
+import type { ThunkDispatch } from '../../../types'
+
+interface LabwareCardOverflowMenuProps {
+  labwareIds: string[]
+  setShowOverflowMenu: Dispatch<SetStateAction<boolean>>
+}
+export function LabwareCardOverflowMenu(
+  props: LabwareCardOverflowMenuProps
+): JSX.Element | null {
+  const { labwareIds, setShowOverflowMenu } = props
+  const { t } = useTranslation('starting_deck_state')
+  const savedSteps = useSelector(getSavedStepForms)
+  const deckSetup = useSelector(getDeckSetupForActiveItem)
+  const { labware: deckSetupLabware } = deckSetup
+  const dispatch = useDispatch<ThunkDispatch<any>>()
+  const topLabwareId = labwareIds[0]
+  const disAllowNickname =
+    labwareIds.length > 1 ||
+    deckSetupLabware[topLabwareId].def.allowedRoles?.includes('adapter') ||
+    deckSetupLabware[topLabwareId].def.parameters.isTiprack ||
+    deckSetupLabware[topLabwareId].def.parameters.quirks?.includes(
+      'tiprackAdapterFor96Channel'
+    )
+
+  const [
+    showDeleteEntityInUseModal,
+    setShowDeleteEntityInUseModal,
+  ] = useState<boolean>(false)
+  const [showNickNameModal, setShowNickNameModal] = useState<boolean>(false)
+  const overflowWrapperRef = useOnClickOutside<HTMLDivElement>({
+    onClickOutside: () => {
+      if (!showNickNameModal && !showDeleteEntityInUseModal) {
+        setShowOverflowMenu(false)
+      }
+    },
+  })
+
+  const isLabwareOnSlotInUse =
+    topLabwareId != null
+      ? getIsLabwareOnSlotInUse(savedSteps, deckSetupLabware[topLabwareId])
+      : false
+
+  const handleClear = (): void => {
+    labwareIds.forEach(id => {
+      dispatch(deleteContainer({ labwareId: deckSetupLabware[id].id }))
+    })
+  }
+
+  const handleConfirmDeleteEntityInUseModal = (): void => {
+    handleClear()
+    setShowOverflowMenu(false)
+    setShowDeleteEntityInUseModal(false)
+  }
+
+  const handleClearLabware = (e: MouseEvent): void => {
+    if (isLabwareOnSlotInUse) {
+      setShowDeleteEntityInUseModal(true)
+      e.preventDefault()
+      e.stopPropagation()
+    } else {
+      handleClear()
+      setShowOverflowMenu(false)
+    }
+  }
+
+  return (
+    <>
+      {isLabwareOnSlotInUse && showDeleteEntityInUseModal ? (
+        <ConfirmDeleteEntityInUseModal
+          onConfirm={handleConfirmDeleteEntityInUseModal}
+          onClose={() => {
+            setShowDeleteEntityInUseModal(false)
+          }}
+        />
+      ) : null}
+      {showNickNameModal ? (
+        <EditNickNameModal
+          labwareId={labwareIds[0]}
+          onClose={() => {
+            setShowNickNameModal(false)
+            setShowOverflowMenu(false)
+          }}
+        />
+      ) : null}
+      <Flex
+        whiteSpace={NO_WRAP}
+        ref={overflowWrapperRef}
+        position={POSITION_ABSOLUTE}
+        top={40}
+        zIndex={2}
+        right={4}
+        borderRadius={BORDERS.borderRadius8}
+        boxShadow="0px 1px 3px rgba(0, 0, 0, 0.2)"
+        backgroundColor={COLORS.white}
+        flexDirection={DIRECTION_COLUMN}
+        onClick={(e: MouseEvent) => {
+          e.preventDefault()
+          e.stopPropagation()
+        }}
+      >
+        {!disAllowNickname ? (
+          <MenuItem
+            css={CURSOR_STYLE}
+            onClick={(e: MouseEvent) => {
+              setShowNickNameModal(true)
+              e.preventDefault()
+              e.stopPropagation()
+            }}
+          >
+            <StyledText desktopStyle="bodyDefaultRegular">
+              {t('rename_lab')}
+            </StyledText>
+          </MenuItem>
+        ) : null}
+
+        <Divider marginY="0" />
+        <MenuItem
+          css={CURSOR_STYLE}
+          onClick={(e: MouseEvent) => {
+            handleClearLabware(e)
+          }}
+        >
+          <StyledText desktopStyle="bodyDefaultRegular">
+            {t('delete')}
+          </StyledText>
+        </MenuItem>
+      </Flex>
+    </>
+  )
+}
+
+const CURSOR_STYLE = css`
+  cursor: ${CURSOR_POINTER};
+`
