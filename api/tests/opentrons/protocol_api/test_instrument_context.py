715044d0b4f3cd2a73a9b67d951a1445febc55a7
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-05-07 13:50:10 -0400

    feat(api): add rate and flow_rate args to air_gap (#18252)
    closes AUTH-1628

--- api/tests/opentrons/protocol_api/test_instrument_context.py
+++ api/tests/opentrons/protocol_api/test_instrument_context.py
@@ -1845,6 +1845,84 @@ def test_air_gap_uses_air_gap(
     decoy.verify(mock_instrument_core.air_gap_in_place(10, 11))
 
 
+@pytest.mark.parametrize("api_version", versions_at_or_above(APIVersion(2, 24)))
+def test_air_gap_has_rate(
+    decoy: Decoy,
+    mock_instrument_core: InstrumentCore,
+    mock_protocol_core: ProtocolCore,
+    subject: InstrumentContext,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """It should use its own rate param."""
+    mock_well = decoy.mock(cls=Well)
+    top_location = Location(point=Point(9, 9, 14), labware=mock_well)
+    last_location = Location(point=Point(9, 9, 9), labware=mock_well)
+    mock_move_to = decoy.mock(func=subject.move_to)
+    monkeypatch.setattr(subject, "move_to", mock_move_to)
+
+    decoy.when(mock_instrument_core.has_tip()).then_return(True)
+    decoy.when(mock_protocol_core.get_last_location()).then_return(last_location)
+    decoy.when(mock_well.top(z=5.0)).then_return(top_location)
+    decoy.when(mock_instrument_core.get_aspirate_flow_rate()).then_return(100)
+
+    subject.air_gap(volume=10, height=5, rate=1.2)
+
+    decoy.verify(mock_move_to(top_location, publish=False))
+    decoy.verify(mock_instrument_core.prepare_to_aspirate())
+    decoy.verify(
+        mock_instrument_core.air_gap_in_place(10, 120)
+    )  # 120 is from the flow_rate calculated from the rate * aspirate_flow_rate param
+
+
+@pytest.mark.parametrize("api_version", versions_at_or_above(APIVersion(2, 24)))
+def test_air_gap_has_flow_rate(
+    decoy: Decoy,
+    mock_instrument_core: InstrumentCore,
+    mock_protocol_core: ProtocolCore,
+    subject: InstrumentContext,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """It should use its own flow_rate param."""
+    mock_well = decoy.mock(cls=Well)
+    top_location = Location(point=Point(9, 9, 14), labware=mock_well)
+    last_location = Location(point=Point(9, 9, 9), labware=mock_well)
+    mock_move_to = decoy.mock(func=subject.move_to)
+    monkeypatch.setattr(subject, "move_to", mock_move_to)
+
+    decoy.when(mock_instrument_core.has_tip()).then_return(True)
+    decoy.when(mock_protocol_core.get_last_location()).then_return(last_location)
+    decoy.when(mock_well.top(z=5.0)).then_return(top_location)
+
+    subject.air_gap(volume=10, height=5, flow_rate=100)
+
+    decoy.verify(mock_move_to(top_location, publish=False))
+    decoy.verify(mock_instrument_core.prepare_to_aspirate())
+    decoy.verify(
+        mock_instrument_core.air_gap_in_place(10, 100)
+    )  # 100 is the flow_rate param
+
+
+def test_air_gap_has_flow_rate_and_rate(
+    decoy: Decoy,
+    mock_instrument_core: InstrumentCore,
+    mock_protocol_core: ProtocolCore,
+    subject: InstrumentContext,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """It raise an error when rate and flow_rate are specified."""
+    mock_well = decoy.mock(cls=Well)
+    top_location = Location(point=Point(9, 9, 14), labware=mock_well)
+    last_location = Location(point=Point(9, 9, 9), labware=mock_well)
+    mock_move_to = decoy.mock(func=subject.move_to)
+    monkeypatch.setattr(subject, "move_to", mock_move_to)
+
+    decoy.when(mock_instrument_core.has_tip()).then_return(True)
+    decoy.when(mock_protocol_core.get_last_location()).then_return(last_location)
+    decoy.when(mock_well.top(z=5.0)).then_return(top_location)
+    with pytest.raises(ValueError, match="Cannot define both flow_rate and rate."):
+        subject.air_gap(volume=10, height=5, flow_rate=100, rate=5.0)
+
+
 @pytest.mark.parametrize("robot_type", ["OT-2 Standard", "OT-3 Standard"])
 def test_transfer_liquid_raises_for_invalid_locations(
     decoy: Decoy,
