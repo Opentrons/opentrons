870e7e0b9ff4d69ce8c755ed7948427afd83aa4b
koji <kk2796@nyu.edu>
2025-05-07 19:33:37 -0400

    fix test errors

--- components/src/molecules/DropdownMenu/index.tsx
+++ components/src/molecules/DropdownMenu/index.tsx
@@ -341,7 +341,6 @@ export function DropdownMenu(props: DropdownMenuProps): JSX.Element {
                   border="none"
                   onMouseEnter={() => onEnter?.(option.value)}
                   onMouseLeave={onExit}
-                  role="option"
                 >
                   <Flex
                     gridGap={SPACING.spacing8}
