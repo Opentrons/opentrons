f451c410d9b5590fb3fc34511130c6bc762e6da0
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-05-09 12:53:34 -0400

    refactor(app): fix LPC type error (#18303)

--- app/src/organisms/LabwarePositionCheck/hooks/useLPCCommands/useHandleConfirmLwModulePlacement.ts
+++ app/src/organisms/LabwarePositionCheck/hooks/useLPCCommands/useHandleConfirmLwModulePlacement.ts
@@ -6,10 +6,10 @@ import {
 
 import type { VectorOffset } from '@opentrons/api-client'
 import type {
-  Coordinates,
   CreateCommand,
   LabwareLocation,
   MoveLabwareCreateCommand,
+  Vector3D,
 } from '@opentrons/shared-data'
 import type { OffsetLocationDetails } from '/app/redux/protocol-runs'
 import type { UseLPCCommandWithChainRunChildProps } from './types'
@@ -26,12 +26,12 @@ export interface UseHandleConfirmPlacementResult {
     offsetLocationDetails: OffsetLocationDetails,
     pipetteId: string,
     initialVectorOffset?: VectorOffset | null
-  ) => Promise<Coordinates>
+  ) => Promise<Vector3D>
   handleMoveToInitialOffsetPosition: (
     offsetLocationDetails: OffsetLocationDetails,
     pipetteId: string,
     initialVectorOffset: VectorOffset | null
-  ) => Promise<Coordinates>
+  ) => Promise<Vector3D>
 }
 
 export function useHandleConfirmLwModulePlacement({
@@ -43,7 +43,7 @@ export function useHandleConfirmLwModulePlacement({
     offsetLocationDetails: OffsetLocationDetails,
     pipetteId: string,
     initialVectorOffset?: VectorOffset | null
-  ): Promise<Coordinates> => {
+  ): Promise<Vector3D> => {
     const confirmCommands: CreateCommand[] = [
       ...buildMoveLabwareCommand(offsetLocationDetails),
       ...moduleInitDuringLPCCommands(analysis),
@@ -79,7 +79,7 @@ export function useHandleConfirmLwModulePlacement({
     offsetLocationDetails: OffsetLocationDetails,
     pipetteId: string,
     initialVectorOffset: VectorOffset | null
-  ): Promise<Coordinates> => {
+  ): Promise<Vector3D> => {
     const moveCommands: CreateCommand[] = [
       ...moveToWellCommands(
         offsetLocationDetails,
