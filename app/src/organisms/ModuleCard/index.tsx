3a0667d5d54b03bfd5dcb5cc5c48ff0174392a0d
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2025-05-09 13:09:26 -0400

    fixup! add a constant

--- app/src/organisms/ModuleCard/index.tsx
+++ app/src/organisms/ModuleCard/index.tsx
@@ -26,7 +26,6 @@ import {
 } from '@opentrons/components'
 import {
   ABSORBANCE_READER_TYPE,
-  FLEX,
   FLEX_STACKER_MODULE_TYPE,
   getModuleDisplayName,
   HEATERSHAKER_MODULE_TYPE,
@@ -359,10 +358,10 @@ export const ModuleCard = (props: ModuleCardProps): JSX.Element | null => {
               />
             )}
             {attachPipetteRequired != null &&
-            calibratePipetteRequired != null &&
-            updatePipetteFWRequired != null &&
-            requireModuleCalibration &&
-            !isPending ? (
+              calibratePipetteRequired != null &&
+              updatePipetteFWRequired != null &&
+              requireModuleCalibration &&
+              !isPending ? (
               <UpdateBanner
                 robotName={robotName}
                 updateType="calibration"
@@ -377,9 +376,9 @@ export const ModuleCard = (props: ModuleCardProps): JSX.Element | null => {
             ) : null}
             {/* Calibration performs firmware updates, so only show calibration if both true. */}
             {!requireModuleCalibration &&
-            module.hasAvailableUpdate &&
-            showFWBanner &&
-            !isPending ? (
+              module.hasAvailableUpdate &&
+              showFWBanner &&
+              !isPending ? (
               <UpdateBanner
                 robotName={robotName}
                 updateType="firmware"
@@ -433,17 +432,17 @@ export const ModuleCard = (props: ModuleCardProps): JSX.Element | null => {
                   data-testid={`module_card_usb_port_${module.serialNumber}`}
                 >
                   {module.moduleType !== THERMOCYCLER_MODULE_TYPE &&
-                  slotName != null
+                    slotName != null
                     ? t('deck_slot', { slot: slotName }) + ' - '
                     : null}
                   {module?.usbPort !== null
                     ? t('usb_port', {
-                        port: module?.usbPort?.port,
-                        hubPort:
-                          module?.usbPort?.hubPort != null
-                            ? `.${module.usbPort.hubPort}`
-                            : '',
-                      })
+                      port: module?.usbPort?.port,
+                      hubPort:
+                        module?.usbPort?.hubPort != null
+                          ? `.${module.usbPort.hubPort}`
+                          : '',
+                    })
                     : t('usb_port_not_connected')}
                 </StyledText>
                 <Flex
