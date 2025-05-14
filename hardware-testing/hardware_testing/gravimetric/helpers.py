f0c3ccc5ca754a070075c4c8153e487ef5747b52
Seth Foster <seth@opentrons.com>
2025-05-14 12:30:49 -0400

    fix(hardware-testing): handle non-Location locs (#18345)
    This wasn't handling the return values of this method properly, now it
    is.
    
    (cherry picked from commit 921901475fcb6cdea02bbe32e28fa8646f2cc1df)

--- hardware-testing/hardware_testing/gravimetric/helpers.py
+++ hardware-testing/hardware_testing/gravimetric/helpers.py
@@ -1,4 +1,5 @@
 """Opentrons helper methods."""
+
 import asyncio
 from random import random, randint
 from types import MethodType
@@ -380,7 +381,7 @@ def _drop_tip(
             pipette.drop_tip(home_after=False)
     if minimum_z_height > 0:
         cur_location = pipette._get_last_location_by_api_version()
-        if cur_location is not None:
+        if isinstance(cur_location, Location):
             pipette.move_to(cur_location.move(Point(0, 0, minimum_z_height)))
 
 
