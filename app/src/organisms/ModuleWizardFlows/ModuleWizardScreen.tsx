341818bf4ceed82082a31e59f783371f65748f3d
smb2268 <sarah.breen@opentrons.com>
2025-05-16 13:45:45 -0400

    small refactors

--- app/src/organisms/ModuleWizardFlows/ModuleWizardScreen.tsx
+++ app/src/organisms/ModuleWizardFlows/ModuleWizardScreen.tsx
@@ -36,6 +36,7 @@ export const ModuleWizardScreen = (
       currentStep={currentStepIndex}
       totalSteps={totalStepCount}
       onExit={isRobotMoving ? undefined : handleCleanUpAndClose}
+      hideStepText
     />
   )
 
