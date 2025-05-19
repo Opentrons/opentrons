c20760e61e9308a3a9b2694f03d77e0ba82d6abe
koji <kk2796@nyu.edu>
2025-05-19 16:23:06 -0400

    refactor(components): refactor snackbar component and stories
    refactor snackbar component and stories
    
    close RUXD-2305

--- components/src/atoms/Snackbar/Snackbar.stories.tsx
+++ components/src/atoms/Snackbar/Snackbar.stories.tsx
@@ -1,69 +1,96 @@
 import * as React from 'react'
 
+import { Snackbar as SnackbarComponent } from '.'
+import { Flex, STYLE_PROPS } from '../../primitives'
 import {
   ALIGN_CENTER,
   DIRECTION_COLUMN,
   DIRECTION_ROW,
-  Flex,
   JUSTIFY_CENTER,
-  LegacyStyledText,
-  PrimaryButton,
-  SPACING,
-  VIEWPORT,
-} from '@opentrons/components'
+} from '../../styles'
+import { SPACING, VIEWPORT } from '../../ui-style-constants'
+import { PrimaryButton } from '../buttons'
+import { StyledText } from '../StyledText'
 
-import { Snackbar } from './index'
+import type { Meta, StoryObj } from '@storybook/react'
 
-import type { Meta, Story } from '@storybook/react'
-
-export default {
+const meta: Meta<typeof SnackbarComponent> = {
   title: 'Helix/Atoms/Snackbar',
-  component: Snackbar,
+  component: SnackbarComponent,
   parameters: VIEWPORT.touchScreenViewport,
-} as Meta
-
-const DefaultTemplate: Story<React.ComponentProps<typeof Snackbar>> = args => {
-  const [isShowSnackbar, setIsShowSnackbar] = React.useState<boolean>(false)
-
-  const handleClick = (): void => {
-    setIsShowSnackbar(true)
-  }
-
-  return (
-    <>
-      <Flex flexDirection={DIRECTION_ROW} marginY={SPACING.spacing16}>
-        <PrimaryButton onClick={handleClick}>Click me</PrimaryButton>
-        <Flex flexDirection={DIRECTION_COLUMN} marginLeft={SPACING.spacing8}>
-          <LegacyStyledText as="p">
-            When clicking the button, the Snackbar shows up in the bottom.
-          </LegacyStyledText>
-          <LegacyStyledText as="p">
-            By default the Snackbar will disappear after 4 seconds.
-          </LegacyStyledText>
-        </Flex>
-      </Flex>
-      {isShowSnackbar && (
-        <Flex
-          alignItems={ALIGN_CENTER}
-          justifyContent={JUSTIFY_CENTER}
-          width="100%"
-          position="absolute"
-          bottom={SPACING.spacing40}
-          zIndex={1000}
-        >
-          <Snackbar
-            {...args}
-            onClose={() => {
-              setIsShowSnackbar(false)
-            }}
-          />
-        </Flex>
-      )}
-    </>
-  )
+  argTypes: {
+    // Disable all StyleProps
+    ...Object.fromEntries(
+      [...STYLE_PROPS, 'as', 'ref', 'theme', 'forwardedAs'].map(prop => [
+        prop,
+        { table: { disable: true } },
+      ])
+    ),
+    message: {
+      control: 'text',
+    },
+    duration: {
+      control: 'number',
+    },
+  },
 }
 
-export const SnackbarComponent = DefaultTemplate.bind({})
-SnackbarComponent.args = {
-  message: 'Short and sweet message',
+const SnackbarTemplate: Story = {
+  render: args => {
+    const [isShowSnackbar, setIsShowSnackbar] = React.useState<boolean>(false)
+
+    const handleClick = (): void => {
+      setIsShowSnackbar(true)
+    }
+
+    return (
+      <>
+        <Flex flexDirection={DIRECTION_ROW} marginY={SPACING.spacing16}>
+          <PrimaryButton onClick={handleClick}>Click me</PrimaryButton>
+          <Flex flexDirection={DIRECTION_COLUMN} marginLeft={SPACING.spacing8}>
+            <StyledText
+              desktopStyle="bodyDefaultRegular"
+              oddStyle="level4HeaderRegular"
+            >
+              When clicking the button, the Snackbar shows up in the bottom.
+            </StyledText>
+            <StyledText
+              desktopStyle="bodyDefaultRegular"
+              oddStyle="level4HeaderRegular"
+            >
+              By default the Snackbar will disappear after 4 seconds.
+            </StyledText>
+          </Flex>
+        </Flex>
+        {isShowSnackbar && (
+          <Flex
+            alignItems={ALIGN_CENTER}
+            justifyContent={JUSTIFY_CENTER}
+            width="100%"
+            position="absolute"
+            bottom={SPACING.spacing40}
+            zIndex={1000}
+          >
+            <SnackbarComponent
+              {...args}
+              onClose={() => {
+                setIsShowSnackbar(false)
+              }}
+            />
+          </Flex>
+        )}
+      </>
+    )
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof SnackbarComponent>
+
+export const Snackbar: Story = {
+  ...SnackbarTemplate,
+  args: {
+    message: 'Short and sweet message',
+    duration: 4000,
+  },
 }
--- components/src/atoms/Snackbar/index.tsx
+++ components/src/atoms/Snackbar/index.tsx
@@ -16,14 +16,49 @@ export interface SnackbarProps extends StyleProps {
 }
 
 const SNACKBAR_ANIMATION_DURATION = 500
-
 const ODD_ANIMATION_OPTIMIZATIONS = `
   backface-visibility: hidden;
   perspective: 1000;
   will-change: opacity;
   `
 
+export function Snackbar(props: SnackbarProps): JSX.Element {
+  const { message, onClose, duration = 4000, ...styleProps } = props
+  const [isClosed, setIsClosed] = useState<boolean>(false)
+
+  const animationStyle = isClosed ? CLOSE_STYLE : OPEN_STYLE
+
+  setTimeout(() => {
+    setIsClosed(true)
+    setTimeout(() => {
+      onClose?.()
+    }, SNACKBAR_ANIMATION_DURATION - 50)
+  }, duration)
+
+  return (
+    <Flex css={animationStyle} data-testid="Snackbar" {...styleProps}>
+      <StyledText
+        oddStyle="bodyTextSemiBold"
+        desktopStyle="bodyDefaultSemiBold"
+      >
+        {message}
+      </StyledText>
+    </Flex>
+  )
+}
+
+const COMMON_STYLE = css`
+  align-items: ${ALIGN_CENTER};
+  border-radius: ${BORDERS.borderRadius8};
+  box-shadow: ${BORDERS.shadowSmall};
+  background-color: ${COLORS.black90};
+  max-width: max-content;
+  padding: ${SPACING.spacing20} ${SPACING.spacing24};
+  color: ${COLORS.white};
+`
+
 const OPEN_STYLE = css`
+  ${COMMON_STYLE}
   animation-duration: ${SNACKBAR_ANIMATION_DURATION}ms;
   animation-name: fadein;
   overflow: hidden;
@@ -40,6 +75,7 @@ const OPEN_STYLE = css`
 `
 
 const CLOSE_STYLE = css`
+  ${COMMON_STYLE}
   animation-duration: ${SNACKBAR_ANIMATION_DURATION}ms;
   animation-name: fadeout;
   overflow: hidden;
@@ -54,39 +90,3 @@ const CLOSE_STYLE = css`
     }
   }
 `
-
-export function Snackbar(props: SnackbarProps): JSX.Element {
-  const { message, onClose, duration = 4000, ...styleProps } = props
-  const [isClosed, setIsClosed] = useState<boolean>(false)
-
-  const animationStyle = isClosed ? CLOSE_STYLE : OPEN_STYLE
-
-  setTimeout(() => {
-    setIsClosed(true)
-    setTimeout(() => {
-      onClose?.()
-    }, SNACKBAR_ANIMATION_DURATION - 50)
-  }, duration)
-
-  return (
-    <Flex
-      css={animationStyle}
-      alignItems={ALIGN_CENTER}
-      borderRadius={BORDERS.borderRadius8}
-      boxShadow={BORDERS.shadowSmall}
-      backgroundColor={COLORS.black90}
-      maxWidth="max-content"
-      padding={`${SPACING.spacing20} ${SPACING.spacing24}`}
-      data-testid="Snackbar"
-      color={COLORS.white}
-      {...styleProps}
-    >
-      <StyledText
-        oddStyle="bodyTextSemiBold"
-        desktopStyle="bodyDefaultSemiBold"
-      >
-        {message}
-      </StyledText>
-    </Flex>
-  )
-}
