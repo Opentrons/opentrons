7a39c35c3ae46c5236c8628cbb07245b507a9a84
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-05-19 08:22:10 -0400

    refactor(protocol-designer): modifications to LabwareCard and EditNic… (#18356)
    …knameModal
    
    closes AUTH-1833

--- protocol-designer/src/components/organisms/LabwareCard/__tests__/LabwareCard.test.tsx
+++ protocol-designer/src/components/organisms/LabwareCard/__tests__/LabwareCard.test.tsx
@@ -6,8 +6,10 @@ import { fixture96Plate } from '@opentrons/shared-data'
 import { renderWithProviders } from '../../../../__testing-utils__'
 import { i18n } from '../../../../assets/localization'
 import { openIngredientSelector } from '../../../../labware-ingred/actions'
+import * as wellContentsSelectors from '../../../../top-selectors/well-contents'
 import { getLabwareNicknamesById } from '../../../../ui/labware/selectors'
 import { LabwareCardOverflowMenu } from '../../LabwareCardOverflowMenu'
+import { getLiquidIdsOnLabware } from '../../utils'
 import { LabwareCard } from '../index'
 
 import type { ComponentProps } from 'react'
@@ -19,6 +21,8 @@ const mockNavigate = vi.fn()
 vi.mock('../../../../labware-ingred/actions')
 vi.mock('../../LabwareCardOverflowMenu')
 vi.mock('../../../../ui/labware/selectors')
+vi.mock('../../../../top-selectors/well-contents')
+vi.mock('../../utils')
 vi.mock('react-router-dom', async importOriginal => {
   const actual = await importOriginal<NavigateFunction>()
   return {
@@ -53,16 +57,32 @@ describe('LabwareCard', () => {
     vi.mocked(getLabwareNicknamesById).mockReturnValue({
       labwareId: 'mock NickName',
     })
+    vi.mocked(
+      wellContentsSelectors.getAllWellContentsForActiveItem
+    ).mockReturnValue(null)
+    vi.mocked(getLiquidIdsOnLabware).mockReturnValue([])
   })
 
   it('renders a labware card with the liquids button and overflow menu', () => {
     render(props)
     screen.getByText('mock NickName')
+    screen.getByText('ANSI 96 Standard Microplate')
     screen.getByText('mock lid')
+    screen.getByText('No liquids added')
     fireEvent.click(screen.getByText('Add liquid'))
     expect(mockNavigate).toHaveBeenCalledWith('/liquids')
     expect(vi.mocked(openIngredientSelector)).toHaveBeenCalled()
     fireEvent.click(screen.getByTestId('LabwareCard_overflowBtn'))
     screen.getByText('mock LabwareCardOverflowMenu')
+  })
+  it('renders a labware card with 2 liquids added', () => {
+    vi.mocked(getLiquidIdsOnLabware).mockReturnValue(['0', '1'])
+    render(props)
+    screen.getByText('2 liquids')
+  })
+  it('renders a labware card with 1 liquid added', () => {
+    vi.mocked(getLiquidIdsOnLabware).mockReturnValue(['0'])
+    render(props)
+    screen.getByText('1 liquid')
   })
 })
