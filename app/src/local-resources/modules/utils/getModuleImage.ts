7b387a4763530f8d020baa6a9a9938e52dbfbff6
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2025-05-19 15:54:01 -0400

    fixup! add flex stacker image to app assets

--- app/src/local-resources/modules/utils/getModuleImage.ts
+++ app/src/local-resources/modules/utils/getModuleImage.ts
@@ -1,3 +1,4 @@
+import flexStackerModule from '/app/assets/images/flex_stacker_no_labware.png'
 import heaterShakerModule from '/app/assets/images/heater_shaker_module_transparent.png'
 import magneticBlockGen1 from '/app/assets/images/magnetic_block_gen_1.png'
 import magneticBlockGen1HighRes from '/app/assets/images/magnetic_block_gen_1@3x.png'
@@ -10,7 +11,6 @@ import absorbanceReader from '/app/assets/images/opentrons_plate_reader.png'
 import temperatureModule from '/app/assets/images/temp_deck_gen_2_transparent.png'
 import thermoModuleGen1 from '/app/assets/images/thermocycler_closed.png'
 import thermoModuleGen2 from '/app/assets/images/thermocycler_gen_2_closed.png'
-import flexStackerModule from '/app/assets/images/flex_stacker_no_labware.png'
 
 import type { ModuleModel } from '@opentrons/shared-data'
 
