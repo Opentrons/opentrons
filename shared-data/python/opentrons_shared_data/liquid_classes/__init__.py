06dddbfa969002afc951c5fc00fd22dfa24b4663
jbleon95 <jeremybenleon@gmail.com>
2025-05-20 12:19:10 -0400

    fix lint error

--- shared-data/python/opentrons_shared_data/liquid_classes/__init__.py
+++ shared-data/python/opentrons_shared_data/liquid_classes/__init__.py
@@ -11,14 +11,18 @@ class LiquidClassDefinitionDoesNotExist(Exception):
     """Specified liquid class definition does not exist."""
 
 
-def load_definition(name: str, version, schema_version: int = DEFAULT_SCHEMA_VERSION) -> LiquidClassSchemaV1:
+def load_definition(
+    name: str, version: int, schema_version: int = DEFAULT_SCHEMA_VERSION
+) -> LiquidClassSchemaV1:
     """Load the specified liquid class' definition as a LiquidClassSchemaV1 object.
 
     Note: this is an expensive operation and should be called sparingly.
     """
     try:
         return LiquidClassSchemaV1.model_validate_json(
-            load_shared_data(f"liquid-class/definitions/{schema_version}/{name}/{version}.json")
+            load_shared_data(
+                f"liquid-class/definitions/{schema_version}/{name}/{version}.json"
+            )
         )
     except FileNotFoundError:
         raise LiquidClassDefinitionDoesNotExist(
