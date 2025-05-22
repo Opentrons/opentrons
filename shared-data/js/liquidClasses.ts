83bf4549fd3b294a4dee17fe3d3eee17c52fe5ee
Jeremy Leon <jeremy.leon@opentrons.com>
2025-05-22 16:57:03 -0400

    feat(shared-data): add in-schema versions to liquid class schema and definitions (#18401)
    Adds a version field to the liquid class schema and the three existing liquid class definitions (plus test fixtures).
    
    (cherry picked from commit 44b5be297cdf197ab17f382fc7de0aa1eb078ec1)

--- shared-data/js/liquidClasses.ts
+++ shared-data/js/liquidClasses.ts
@@ -1,6 +1,7 @@
-import ethanol80V1Uncasted from '../liquid-class/definitions/1/ethanol_80.json'
-import glycerol50V1Uncasted from '../liquid-class/definitions/1/glycerol_50.json'
-import waterV1Uncasted from '../liquid-class/definitions/1/water.json'
+import ethanol80V1Uncasted from '../liquid-class/definitions/1/ethanol_80/1.json'
+import glycerol50V1Uncasted from '../liquid-class/definitions/1/glycerol_50/1.json'
+import waterV1Uncasted from '../liquid-class/definitions/1/water/1.json'
+
 import type { LiquidClass } from '.'
 
 const ethanol80V1 = ethanol80V1Uncasted as LiquidClass
