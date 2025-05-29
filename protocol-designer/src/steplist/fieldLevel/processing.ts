81a1ab91e715e555da7bc1f9fe01aaa06db1ad37
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-05-29 15:35:50 -0400

    feat(protocol-designer): create labware quantity modal (#18440)
    closes AUTH-1907

--- protocol-designer/src/steplist/fieldLevel/processing.ts
+++ protocol-designer/src/steplist/fieldLevel/processing.ts
@@ -12,6 +12,13 @@ export const maskToInteger = (rawValue: unknown): string => {
       : String(rawValue)
   return rawNumericValue
 }
+export const maskToPositiveInteger = (rawValue: unknown): string => {
+  const rawNumericValue =
+    typeof rawValue === 'string'
+      ? rawValue.replace(/\D/g, '')
+      : String(rawValue)
+  return rawNumericValue
+}
 export const maskToTime = (rawValue: unknown): string => {
   if (rawValue == null) {
     return ''
