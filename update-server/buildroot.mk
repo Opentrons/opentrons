6623c6466f9955b51e4fceb381432aec16e16d75
Seth Foster <seth@opentrons.com>
2025-05-30 16:30:15 -0400

    buildroot: better pathing for our makescript

--- update-server/buildroot.mk
+++ update-server/buildroot.mk
@@ -3,7 +3,7 @@
 # python-opentrons-update-server
 #
 ################################################################################
-include ../scripts/python.mk
+include $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/scripts/python.mk
 
 define OTUPDATESERVER_CALL_PBU
 	$(shell python $(BR2_EXTERNAL_OPENTRONS_MONOREPO_PATH)/scripts/python_build_utils.py update-server $(or $(OPENTRONS_PROJECT),robot-stack) $(1))
