ff8d37bb41d482aa74f96e7186ea9e088c031a2e
Max Marrone <max@opentrons.com>
2025-05-30 12:06:57 -0400

    Add unit tests and work around a bizarre React quirk.

--- components/src/atoms/AnimationVideo/index.tsx
+++ components/src/atoms/AnimationVideo/index.tsx
@@ -17,10 +17,13 @@ export function AnimationVideo(props: AnimationVideoProps): JSX.Element {
     <video
       autoPlay
       loop
-      // Our animations don't have audio in the first place, but we apparently need to
-      // pass `muted` anyway. It prevents the browser from trying to play audio on
-      // connected Bluetooth headphones, which interrupts audio coming from other
-      // devices.
+      // Our animations don't have audio, but we apparently need to pass `muted` anyway.
+      // It prevents the browser from trying to play audio on connected Bluetooth
+      // headphones, which would interrupt audio coming from other devices.
+      //
+      // Bcause of some React nonsense, the rendered DOM node will not have a `muted`
+      // attribute, but its `muted` property will be true, which is good enough.
+      // https://github.com/facebook/react/issues/10389
       muted
       controls={false}
       {...props}
