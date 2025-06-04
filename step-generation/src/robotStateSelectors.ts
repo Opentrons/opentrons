9a5319a6f9dc1af6b392f53ec05891b454e5f341
Jethary <jethary@opentrons.com>
2025-06-04 12:42:35 -0400

    remove change

--- step-generation/src/robotStateSelectors.ts
+++ step-generation/src/robotStateSelectors.ts
@@ -138,6 +138,7 @@ export function getNextTiprack(
   const filteredSortedTipRackIdsFor96Channel = sortedTipracksIds.filter(
     tiprackId => {
       const tipRackLocation = robotState.labware[tiprackId].stack[1]
+
       const adapterEntity = invariantContext.labwareEntities[tipRackLocation]
       const has96TiprackAdapterId =
         adapterEntity?.def.parameters.loadName ===
