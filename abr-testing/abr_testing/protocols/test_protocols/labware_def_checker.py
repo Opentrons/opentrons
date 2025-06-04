5ee8affdb81b1f6b546dc6ffefbb67562ce01543
Hovan Ngo <hovan.ngo@opentrons.com>
2025-06-04 17:28:54 -0400

    added pictures

--- abr-testing/abr_testing/protocols/test_protocols/labware_def_checker.py
+++ abr-testing/abr_testing/protocols/test_protocols/labware_def_checker.py
@@ -1,12 +1,7 @@
 """Test labware definition ."""
-from opentrons.protocol_api import (
-    ProtocolContext,
-    ParameterContext,
-    SINGLE,
-    ALL,
-    Well,
-)
-from typing import List, Dict
+from opentrons.protocol_api import ProtocolContext, ParameterContext
+from opentrons.protocol_api.module_contexts import HeaterShakerContext
+
 
 metadata = {
     "protocolName": "labware definition checker ",
@@ -15,6 +10,7 @@ metadata = {
 }
 
 requirements = {"robotType": "Flex", "apiLevel": "2.23"}
+
 
 def add_parameters(parameters: ParameterContext) -> None:
     """Parameters."""
@@ -48,23 +44,24 @@ def add_parameters(parameters: ParameterContext) -> None:
 
 
 def run(protocol: ProtocolContext) -> None:
+    """Protocol."""
     labware_type = protocol.params.labware_type  # type: ignore[attr-defined]
     heater_shaker_enabled = protocol.params.heater_shaker  # type: ignore[attr-defined]
 
-    #Lid loading 
+    # Lid loading
     if labware_type == "corning_96_wellplate_360ul_lid":
-        lid = protocol.load_lid_stack(labware_type, "C3", 1)
+        protocol.load_lid_stack(labware_type, "C3", 1)
         labware = protocol.load_labware("ibidi_96_square_well_plate_300ul", "D3")
         for _ in range(3):
             protocol.move_lid("C3", labware, use_gripper=True)
-            protocol.move_lid(labware, "C3")
-    
-    else: 
+            protocol.move_lid(labware, "C3", use_gripper=True)
+    else:
         labware = protocol.load_labware(labware_type, "D3")
-
         # load pipette and tip rack
         tip_rack = protocol.load_labware("opentrons_flex_96_tiprack_200ul", "D2")
-        pipette = protocol.load_instrument("flex_8channel_1000", "left", tip_racks=[tip_rack])
+        pipette = protocol.load_instrument(
+            "flex_8channel_1000", "left", tip_racks=[tip_rack]
+        )
 
         # pick up tip
         pipette.pick_up_tip()
@@ -77,7 +74,9 @@ def run(protocol: ProtocolContext) -> None:
 
         # Optional heater shaker use
         if heater_shaker_enabled:
-            hsh = protocol.load_module("heaterShakerModuleV1", "D1")
+            hsh: HeaterShakerContext = protocol.load_module(
+                "heaterShakerModuleV1", "D1"
+            )  # type: ignore[assignment]
             hsh.close_labware_latch()
             adapter = hsh.load_adapter("opentrons_universal_flat_adapter")
             hsh.open_labware_latch()
@@ -86,9 +85,5 @@ def run(protocol: ProtocolContext) -> None:
 
             pipette.move_to(labware["A6"].top(z=1))
             protocol.pause("Check top height with shim")
-            pipette.move_to(labware["A6"].bottom(z=1))
-            protocol.pause("Check bottom height with shim")
 
         pipette.return_tip()
-
-    
