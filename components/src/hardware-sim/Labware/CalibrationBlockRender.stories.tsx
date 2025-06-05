1dc30d3de6aeefc487d481aae495bb12da3c5bad
Max Marrone <max@opentrons.com>
2025-06-05 19:26:55 -0400

    Format.

--- components/src/hardware-sim/Labware/CalibrationBlockRender.stories.tsx
+++ components/src/hardware-sim/Labware/CalibrationBlockRender.stories.tsx
@@ -1,47 +1,48 @@
 import { getAllDefinitions, getSchema2Dimensions } from '@opentrons/shared-data'
+
 import { RobotWorkSpace } from '../Deck'
 import { CalibrationBlockRender } from './CalibrationBlockRender'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
-const LOAD_NAMES = ["opentrons_calibrationblock_short_side_left", "opentrons_calibrationblock_short_side_right"]
+const LOAD_NAMES = [
+  'opentrons_calibrationblock_short_side_left',
+  'opentrons_calibrationblock_short_side_right',
+]
 const DEFS_BY_URI = Object.fromEntries(
-  Object.entries(
-    getAllDefinitions()
-  ).filter(
-    ([uri, def]) => LOAD_NAMES.includes(def.parameters.loadName)
+  Object.entries(getAllDefinitions()).filter(([uri, def]) =>
+    LOAD_NAMES.includes(def.parameters.loadName)
   )
 )
-
 
 const meta: Meta<typeof CalibrationBlockRender> = {
   component: CalibrationBlockRender,
   decorators: [
     (Story, context) => {
-      const {labwareDef} = context.args
+      const { labwareDef } = context.args
       // todo(mm, 2025-06-05): Update viewBox to account for labware schema 3.
-      const {xDimension, yDimension} = getSchema2Dimensions(labwareDef)
+      const { xDimension, yDimension } = getSchema2Dimensions(labwareDef)
       const viewBox = `0 0 ${xDimension} ${yDimension}`
 
-      return <RobotWorkSpace viewBox={viewBox}>
-        {() => <Story />}
-      </RobotWorkSpace>
-    }
-  ]
+      return (
+        <RobotWorkSpace viewBox={viewBox}>{() => <Story />}</RobotWorkSpace>
+      )
+    },
+  ],
 }
 
-export default meta;
+export default meta
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
-    labwareDef: Object.keys(DEFS_BY_URI)[0]
+    labwareDef: Object.keys(DEFS_BY_URI)[0],
   },
   argTypes: {
     labwareDef: {
       options: Object.keys(DEFS_BY_URI),
       defaultValue: Object.keys(DEFS_BY_URI)[0],
-      mapping: DEFS_BY_URI
-    }
-  }
+      mapping: DEFS_BY_URI,
+    },
+  },
 }
