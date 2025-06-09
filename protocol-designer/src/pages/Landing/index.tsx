cfe71868f4909ba4b674048952dc472c4c923ef4
koji <kk2796@nyu.edu>
2025-06-09 19:20:17 -0400

    add a button to test sentry

--- protocol-designer/src/pages/Landing/index.tsx
+++ protocol-designer/src/pages/Landing/index.tsx
@@ -160,14 +160,6 @@ export function Landing(): JSX.Element {
           <BasicButton onClick={handleImportClick} underLine>
             {t('import_existing_protocol')}
           </BasicButton>
-          <button
-            onClick={() => {
-              throw new Error('This is your first error!')
-            }}
-          >
-            Break the world
-          </button>
-          ;
           <StyledInput
             type="file"
             onChange={loadFile}
@@ -175,6 +167,13 @@ export function Landing(): JSX.Element {
             aria-label={t('import')}
           />
         </StyledLabel>
+        <button
+          onClick={() => {
+            throw new Error('This is your first error!')
+          }}
+        >
+          Break the world
+        </button>
       </Flex>
       <EndUserAgreementFooter />
     </>
