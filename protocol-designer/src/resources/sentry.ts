05a1f1b7f6cc7c8fe4242a9175ac26231b459500
koji <kk2796@nyu.edu>
2025-06-10 15:44:11 -0400

    update the comment to kick the CI workflow

--- protocol-designer/src/resources/sentry.ts
+++ protocol-designer/src/resources/sentry.ts
@@ -13,7 +13,7 @@ let isSentryInitialized = false
 
 // Note (kk: 06/09/2025) at this moment, we are not using a dev DSN
 // because we are not using Sentry in development. If we decide to use it
-// in the future, we can add a dev DSN here.
+// in the future, we can add a dev DSN here if needed.
 const sentryDsn = getIsProduction()
   ? process.env.OT_PD_SENTRY_DSN
   : process.env.OT_PD_SENTRY_DEV_DSN
