899f0e1ce00c8cc37d2174333b336ac724e7f550
Max Marrone <max@opentrons.com>
2025-06-09 21:24:49 -0400

    Delete unnecessary top-level describe().

--- shared-data/js/helpers/__tests__/labwareSchemaShims.test.ts
+++ shared-data/js/helpers/__tests__/labwareSchemaShims.test.ts
@@ -4,49 +4,47 @@ import { getSchema2Dimensions } from '../..'
 
 import type { LabwareDefinition2, LabwareDefinition3 } from '../..'
 
-describe('labwareSchemaShims', () => {
-  describe('getSchema2Dimensions()', () => {
-    it('should handle schema 2 definitions', () => {
-      const definition: Partial<LabwareDefinition2> = {
-        schemaVersion: 2,
-        dimensions: {
-          xDimension: 12.34,
-          yDimension: 56.78,
-          zDimension: 90.12,
-        },
-      }
-      const result = getSchema2Dimensions(definition as LabwareDefinition2)
-      expect(result).toStrictEqual({
+describe('getSchema2Dimensions()', () => {
+  it('should handle schema 2 definitions', () => {
+    const definition: Partial<LabwareDefinition2> = {
+      schemaVersion: 2,
+      dimensions: {
         xDimension: 12.34,
         yDimension: 56.78,
         zDimension: 90.12,
-      })
+      },
+    }
+    const result = getSchema2Dimensions(definition as LabwareDefinition2)
+    expect(result).toStrictEqual({
+      xDimension: 12.34,
+      yDimension: 56.78,
+      zDimension: 90.12,
     })
-    it('should handle schema 3 definitions', () => {
-      const definition: Partial<LabwareDefinition3> = {
-        schemaVersion: 3,
-        extents: {
-          total: {
-            backLeftBottom: {
-              x: -0.1,
-              y: 2.0,
-              z: -0.3,
-            },
-            frontRightTop: {
-              x: 1.0,
-              y: -0.2,
-              z: 3.0,
-            },
+  })
+  it('should handle schema 3 definitions', () => {
+    const definition: Partial<LabwareDefinition3> = {
+      schemaVersion: 3,
+      extents: {
+        total: {
+          backLeftBottom: {
+            x: -0.1,
+            y: 2.0,
+            z: -0.3,
           },
-          footprint: {} as any,
+          frontRightTop: {
+            x: 1.0,
+            y: -0.2,
+            z: 3.0,
+          },
         },
-      }
-      const result = getSchema2Dimensions(definition as LabwareDefinition3)
-      expect(result).toStrictEqual({
-        xDimension: 1.1,
-        yDimension: 2.2,
-        zDimension: 3.3,
-      })
+        footprint: {} as any,
+      },
+    }
+    const result = getSchema2Dimensions(definition as LabwareDefinition3)
+    expect(result).toStrictEqual({
+      xDimension: 1.1,
+      yDimension: 2.2,
+      zDimension: 3.3,
     })
   })
 })
