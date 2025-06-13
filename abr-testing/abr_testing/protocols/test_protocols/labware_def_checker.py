22273b15081f5261eec2fd67254faeb2d6278dd1
Hovan Ngo <hovan.ngo@opentrons.com>
2025-06-13 15:11:13 -0400

    minor labware def change

--- abr-testing/abr_testing/protocols/test_protocols/labware_def_checker.py
+++ abr-testing/abr_testing/protocols/test_protocols/labware_def_checker.py
@@ -32,7 +32,7 @@ def add_parameters(parameters: ParameterContext) -> None:
                 "value": "nest_24_reservoir_10.4ml"
             },
         ],
-        default="axygen_96_wellplate_500ul",
+        default="nest_8_reservoir_22ml"
     )
     parameters.add_bool(
         variable_name="heater_shaker",
@@ -57,18 +57,18 @@ def run(protocol: ProtocolContext) -> None:
     else:
         labware = protocol.load_labware(labware_type, "D3")
         # load pipette and tip rack
-        tip_rack = protocol.load_labware("opentrons_flex_96_tiprack_200ul", "D2")
+        tip_rack = protocol.load_labware("opentrons_flex_96_tiprack_50ul", "D2")
         pipette = protocol.load_instrument(
-            "flex_8channel_1000", "left", tip_racks=[tip_rack]
+            "flex_1channel_50", "left", tip_racks=[tip_rack]
         )
 
         # pick up tip
         pipette.pick_up_tip()
 
         # move tip to positions
-        pipette.move_to(labware["A6"].top(z=1))
+        pipette.move_to(labware["A1"].top(z=1))
         protocol.pause("Check top height with shim")
-        pipette.move_to(labware["A6"].bottom(z=1))
+        pipette.move_to(labware["A1"].bottom(z=1))
         protocol.pause("Check bottom height with shim")
 
         # Optional heater shaker use
@@ -82,7 +82,7 @@ def run(protocol: ProtocolContext) -> None:
             protocol.move_labware(labware, adapter, use_gripper=True)
             hsh.close_labware_latch()
 
-            pipette.move_to(labware["A6"].top(z=1))
+            pipette.move_to(labware["A1"].top(z=1))
             protocol.pause("Check top height with shim")
 
         pipette.return_tip()
