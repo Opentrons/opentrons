315df6632fb603a5668f7e965eeced8954e9bb98
tamarzanzouri <tamar.zanzouri@opentrons.com>
2025-06-14 22:00:59 -0400

    varios small fixes

--- components/src/organisms/FixtureOption/index.tsx
+++ components/src/organisms/FixtureOption/index.tsx
@@ -1,18 +1,8 @@
-import styled from 'styled-components'
-
 import { TertiaryButton } from '../../atoms/buttons'
 import { ListItem } from '../../atoms/ListItem/index'
 import { StyledText } from '../../atoms/StyledText'
-import { BORDERS, COLORS } from '../../helix-design-system'
-import { Btn } from '../../primitives'
-import {
-  ALIGN_CENTER,
-  CURSOR_DEFAULT,
-  DIRECTION_ROW,
-  DISPLAY_FLEX,
-  JUSTIFY_SPACE_BETWEEN,
-} from '../../styles'
-import { SPACING, TYPOGRAPHY } from '../../ui-style-constants'
+import { ALIGN_CENTER, JUSTIFY_SPACE_BETWEEN } from '../../styles'
+import { SPACING } from '../../ui-style-constants'
 
 import type { MouseEventHandler } from 'react'
 
@@ -23,21 +13,11 @@ interface FixtureOptionProps {
   isOnDevice: boolean
 }
 export function FixtureOption(props: FixtureOptionProps): JSX.Element {
-  const { onClickHandler, optionName, buttonText, isOnDevice } = props
-  return isOnDevice ? (
-    <FixtureButtonODD onClick={onClickHandler}>
-      <StyledText
-        oddStyle="bodyTextRegular"
-        fontWeight={TYPOGRAPHY.fontWeightSemiBold}
-      >
-        {optionName}
-      </StyledText>
-      <StyledText oddStyle="bodyTextRegular">{buttonText}</StyledText>
-    </FixtureButtonODD>
-  ) : (
+  const { onClickHandler, optionName, buttonText } = props
+  return (
     <ListItem
       type="default"
-      padding="16px"
+      padding={SPACING.spacing16 + ' ' + SPACING.spacing24}
       alignItems={ALIGN_CENTER}
       justifyContent={JUSTIFY_SPACE_BETWEEN}
     >
@@ -52,40 +32,3 @@ export function FixtureOption(props: FixtureOptionProps): JSX.Element {
     </ListItem>
   )
 }
-
-export const FixtureButtonODD = styled(Btn)`
-  display: ${DISPLAY_FLEX};
-  background-color: ${COLORS.grey35};
-  justify-content: ${JUSTIFY_SPACE_BETWEEN};
-  flex-direction: ${DIRECTION_ROW};
-  align-items: ${ALIGN_CENTER};
-  cursor: ${CURSOR_DEFAULT};
-  border-radius: ${BORDERS.borderRadius16};
-  box-shadow: none;
-  padding: ${SPACING.spacing16} ${SPACING.spacing24};
-
-  &:focus {
-    background-color: ${COLORS.grey40};
-    box-shadow: none;
-  }
-
-  &:hover {
-    border: none;
-    box-shadow: none;
-    background-color: ${COLORS.grey35};
-  }
-
-  &:focus-visible {
-    box-shadow: 0 0 0 ${SPACING.spacing4} ${COLORS.blue50};
-    background-color: ${COLORS.grey35};
-  }
-
-  &:active {
-    background-color: ${COLORS.grey40};
-  }
-
-  &:disabled {
-    background-color: ${COLORS.grey35};
-    color: ${COLORS.grey50};
-  }
-`
