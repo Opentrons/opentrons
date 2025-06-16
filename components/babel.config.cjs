36b1d80aa9ed99d99531d39bae9734ef0867e26f
koji <kk2796@nyu.edu>
2025-06-16 17:59:27 -0400

    update babel settings to use styled-components + emotion

--- components/babel.config.cjs
+++ components/babel.config.cjs
@@ -1,24 +1,47 @@
+// 'use strict'
+
+// module.exports = {
+//   env: {
+//     // Note(isk: 3/2/20): Must have babel-plugin-styled-components in each env,
+//     // see here for further details: s https://styled-components.com/docs/tooling#babel-plugin
+//     production: {
+//       plugins: [
+//         // 'babel-plugin-styled-components',
+//         'babel-plugin-unassert',
+//         '@emotion/babel-plugin',
+//       ],
+//     },
+//     development: {
+//       plugins: ['@emotion/babel-plugin'],
+//     },
+//     test: {
+//       plugins: [
+//         // NOTE(mc, 2020-05-08): disable ssr, displayName to fix toHaveStyleRule
+//         // https://github.com/styled-components/jest-styled-components/issues/294
+//         // ['babel-plugin-styled-components', { ssr: false, displayName: false }],
+//         ['@emotion/babel-plugin', { ssr: false, displayName: false }],
+//       ],
+//     },
+//   },
+// }
+
 'use strict'
 
 module.exports = {
   env: {
-    // Note(isk: 3/2/20): Must have babel-plugin-styled-components in each env,
-    // see here for further details: s https://styled-components.com/docs/tooling#babel-plugin
     production: {
       plugins: [
-        // 'babel-plugin-styled-components',
+        'babel-plugin-styled-components',
         'babel-plugin-unassert',
         '@emotion/babel-plugin',
       ],
     },
     development: {
-      plugins: ['@emotion/babel-plugin'],
+      plugins: ['babel-plugin-styled-components', '@emotion/babel-plugin'],
     },
     test: {
       plugins: [
-        // NOTE(mc, 2020-05-08): disable ssr, displayName to fix toHaveStyleRule
-        // https://github.com/styled-components/jest-styled-components/issues/294
-        // ['babel-plugin-styled-components', { ssr: false, displayName: false }],
+        ['babel-plugin-styled-components', { ssr: false, displayName: false }],
         ['@emotion/babel-plugin', { ssr: false, displayName: false }],
       ],
     },
