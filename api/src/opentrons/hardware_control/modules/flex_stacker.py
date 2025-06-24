99bc23640232759641c2e2d60056aff436e45e51
CaseyBatten <caseyhbatten@gmail.com>
2025-06-24 11:51:12 -0400

    fix(api): Stacker LED ensure identify state is cleanly exited (#18654)
    Covers RQA-4271
    Ensures we reset the stacker identify state regardless of what happens.

--- api/src/opentrons/hardware_control/modules/flex_stacker.py
+++ api/src/opentrons/hardware_control/modules/flex_stacker.py
@@ -679,6 +679,10 @@ class FlexStacker(mod_abc.AbstractModule):
     def set_stacker_identify(self, state: bool) -> None:
         self._should_identify = state
 
+    def cleanup_persistent(self) -> None:
+        """Reset persistent data on the module that should not exist outside of a run."""
+        self.set_stacker_identify(False)
+
 
 class FlexStackerReader(Reader):
     error: Optional[str]
