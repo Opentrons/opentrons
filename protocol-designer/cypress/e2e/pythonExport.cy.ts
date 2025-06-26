e5ab2d5f6f61c97a8e836d3880818e627ddc32cc
Sara Kowalski <sara.kowalski@opentrons.com>
2025-06-26 16:44:16 -0400

    brute force version of python export

--- protocol-designer/cypress/e2e/pythonExport.cy.ts
+++ protocol-designer/cypress/e2e/pythonExport.cy.ts
@@ -3,26 +3,27 @@ import { verifyImportProtocolPage } from '../support/Import'
 // import { SetupSteps } from '../support/SetupSteps'
 // import { StepBuilder } from '../support/StepBuilder'
 import { getExportTestFile, ExportTestFilePath } from '../support/TestFiles'
-// import { UniversalSteps } from '../support/UniversalSteps'
+import { UniversalSteps } from '../support/UniversalSteps'
 
 describe('Import a json and export as a python', () => {
   const exportTestFilePaths = Object.values(ExportTestFilePath) as ExportTestFilePath[];
+  
   beforeEach(() => {
     cy.visit('/')
     cy.closeAnalyticsModal()
     cy.closeReleaseNotesModal()
   })
-  
-  it('should verify the working function of taking in a json file and exporting as a python file', () => {
-      for (let exportProtocol of exportTestFilePaths) {
+
+  for (let exportProtocol of exportTestFilePaths) {
+    it('should verify the working function of taking in a json file and exporting as a python file', () => {
           const file = getExportTestFile(exportProtocol)
           cy.importProtocol(file.path)
-          cy.contains('Import').click({force:true})
+          cy.get( 'div[aria-label="ModalShell_ModalArea"]')
+            .contains('Import').click({force:true})
           verifyImportProtocolPage(file)
           cy.contains('Export protocol').click()
-        //   cy.visit('/')
-      };
-    // const steps = new StepBuilder()
-    // steps.execute()
-  })
+          // const steps = new StepBuilder()
+          // steps.execute()
+        })
+    };
 })
