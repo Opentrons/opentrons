e835573e745ba4ba675075ccf41eacdbe70aca84
David Chau <46395074+ddcc4@users.noreply.github.com>
2025-06-30 11:24:40 -0400

    fix(shared-data): revert changes to LC displayNames (#18774)
    # Overview
    
    In PR #18711 (AUTH-2003), we changed the liquid class display names to:
    `Aqueous (Deionized water)` / `Volatile (80% ethanol)` / `Viscous (50% glycerol)`.
    
    But we've decided we want to change the display names back. So:
    - displayName: `Aqueous`, description `Deionized water`, loadname: `water`
    - displayName: `Volatile`, description `80% ethanol`, loadname: `ethanol_80`
    - displayName: `Viscous`, description `50% glycerol`, loadname: `glycerol_50`
    
    https://opentrons.slack.com/archives/C07JJA1AJFQ/p1751036532344239?thread_ts=1750692306.637929&cid=C07JJA1AJFQ
    
    This PR reverts #18711 and #18738, and was generated by `git revert`
    with no manual changes:
    ```
    git revert ec54caf 968ddc8
    ```
    
    ## Test Plan and Hands on Testing
    
    Running CI tests.
    
    ## Review requests
    
    This is what we want now, right?
    
    ## Risk assessment
    
    Low, but we're cutting it close.

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/StepTools/MoveLiquidTools/__tests__/LiquidClassesStepTools.test.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/StepTools/MoveLiquidTools/__tests__/LiquidClassesStepTools.test.tsx
@@ -1,9 +1,10 @@
+import { describe, it, vi, beforeEach, expect } from 'vitest'
 import { fireEvent, screen } from '@testing-library/react'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { renderWithProviders } from '../../../../../../../__testing-utils__'
 import { i18n } from '../../../../../../../assets/localization'
+import { renderWithProviders } from '../../../../../../../__testing-utils__'
 import { getLiquidEntities } from '../../../../../../../step-forms/selectors'
 import { LiquidClassesStepTools } from '../LiquidClassesStepTools'
+
 import type { ComponentProps } from 'react'
 
 vi.mock('../../../../../../../step-forms/selectors')
@@ -38,17 +39,15 @@ describe('LiquidClassesStepMoveLiquidTools', () => {
     render(props)
     screen.getByText('Apply liquid class settings for this transfer')
     screen.getByText("Don't use a liquid class")
-    screen.getByText('Aqueous (Deionized water)')
+    screen.getByText('Aqueous')
     screen.getByText('Deionized water')
-    screen.getByText('Viscous (50% glycerol)')
+    screen.getByText('Viscous')
     screen.getByText('50% glycerol')
-    screen.getByText('Volatile (80% ethanol)')
+    screen.getByText('Volatile')
     screen.getByText('80% ethanol')
 
     fireEvent.click(
-      screen.getByRole('label', {
-        name: 'Aqueous (Deionized water) Deionized water',
-      })
+      screen.getByRole('label', { name: 'Aqueous Deionized water' })
     )
     expect(props.propsForFields.liquidClass.updateValue).toHaveBeenCalled()
   })
