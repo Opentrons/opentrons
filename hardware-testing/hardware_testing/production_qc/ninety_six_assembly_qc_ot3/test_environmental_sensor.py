e495db3504947eeab718b4c037b9dc8c84575bea
Andiiiiiiyy <andy.hu@opentrons.com>
2025-07-03 13:29:58 +0800

    fix environment result shows None in csv report

--- hardware-testing/hardware_testing/production_qc/ninety_six_assembly_qc_ot3/test_environmental_sensor.py
+++ hardware-testing/hardware_testing/production_qc/ninety_six_assembly_qc_ot3/test_environmental_sensor.py
@@ -1,6 +1,6 @@
 """Test Environmental Sensor."""
 from asyncio import sleep
-from typing import List, Union, Literal
+from typing import List, Union
 
 from opentrons.hardware_control.ot3api import OT3API
 
@@ -11,6 +11,7 @@ from hardware_testing.opentrons_api import helpers_ot3
 from hardware_testing.opentrons_api.types import OT3Mount
 from hardware_testing.data.csv_report import (
     CSVReport,
+    CSVResult,
     CSVLine,
     CSVLineRepeating,
 )
@@ -18,12 +19,14 @@ from hardware_testing.data.csv_report import (
 
 NUM_SAMPLES = 10
 INTER_SAMPLE_DELAY_SECONDS = 0.25
+TEMPERATURE_THRESHOLD = [10, 40]
+HUMIDITY_THRESHOLD = [10, 90]
 
 
 def build_csv_lines() -> List[Union[CSVLine, CSVLineRepeating]]:
     """Build CSV Lines."""
     return [
-        CSVLine(f"environment-{sensor_id.name}-celsius-humidity", [float, float])
+        CSVLine(f"environment-{sensor_id.name}-celsius-humidity", [float, float, CSVResult])
         for sensor_id in [SensorId.S0, SensorId.S1]
     ]
 
@@ -33,9 +36,7 @@ def _remove_outliers_and_average(values: List[float]) -> float:
     return sum(no_outliers) / len(no_outliers)
 
 
-async def run(
-    api: OT3API, report: CSVReport, section: str, pipette: Literal[200, 1000]
-) -> None:
+async def run(api: OT3API, report: CSVReport, section: str, pipette) -> None:
     """Run."""
     await api.home_z(OT3Mount.LEFT)
     slot_5 = helpers_ot3.get_slot_calibration_square_position_ot3(5)
@@ -45,6 +46,7 @@ async def run(
         ui.print_header(sensor_id.name.upper())
         celsius_samples = []
         humidity_samples = []
+        air_params = True
         print(f"averaging {NUM_SAMPLES} samples:")
         print("\tc\th")
         for _ in range(NUM_SAMPLES):
@@ -62,8 +64,10 @@ async def run(
         humidity = _remove_outliers_and_average(humidity_samples)
         print(f"[{sensor_id.name}] Celsius = {round(celsius, 2)} degrees")
         print(f"[{sensor_id.name}] Humidity = {round(humidity, 2)} percent")
+        air_params = air_params & True if TEMPERATURE_THRESHOLD[0] <= celsius <= TEMPERATURE_THRESHOLD[1] and HUMIDITY_THRESHOLD[0] <= humidity <= HUMIDITY_THRESHOLD[1] else False
+    
         report(
             section,
             f"environment-{sensor_id.name}-celsius-humidity",
-            [celsius, humidity],
+            [celsius, humidity, CSVResult.from_bool(air_params)]
         )
