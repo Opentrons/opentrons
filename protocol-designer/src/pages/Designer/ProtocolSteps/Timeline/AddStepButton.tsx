daefea3d4d751a66c91bf2626957c38c304e95a5
Jethary <jethary@opentrons.com>
2025-07-09 11:07:01 -0400

    add css import

--- protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/AddStepButton.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/AddStepButton.tsx
@@ -3,6 +3,7 @@ import { createPortal } from 'react-dom'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import last from 'lodash/last'
+import { css } from 'styled-components'
 
 import {
   ALIGN_CENTER,
