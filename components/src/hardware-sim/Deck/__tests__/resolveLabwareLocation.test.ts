22b151d6115d63eb5cd88191dcc1d89ac01f26c4
Max Marrone <max@opentrons.com>
2025-07-11 14:12:39 -0400

    Add unit tests for resolveLabwareLocation().

---
+++ components/src/hardware-sim/Deck/__tests__/resolveLabwareLocation.test.ts
@@ -0,0 +1,132 @@
+import { expect, it } from 'vitest'
+
+import {
+  getAllDefinitions,
+  getDeckDefinitions,
+  getModuleDef,
+} from '@opentrons/shared-data'
+
+import type { ModuleModel } from '@opentrons/shared-data'
+
+import { resolveLabwareLocation } from '../resolveLabwareLocation'
+
+it('should resolve a labware location', () => {
+  const labwareAUri = 'opentrons/nest_12_reservoir_15ml/1'
+  const labwareBUri = "opentrons/agilent_1_reservoir_290ml/1"
+  const labwareCUri = 'opentrons/opentrons_96_pcr_adapter/1'
+  const labwareADefinition = getAllDefinitions()[labwareAUri]
+  const labwareBDefinition = getAllDefinitions()[labwareBUri]
+  const labwareCDefinition = getAllDefinitions()[labwareCUri]
+
+  const moduleModel: ModuleModel = "temperatureModuleV2"
+  const moduleDefinition = getModuleDef(moduleModel)
+
+  const deckDefinition = getDeckDefinitions().ot3_standard
+
+  const slotId = "C2"
+
+  const result = resolveLabwareLocation({
+    targetLabwareDef: labwareADefinition,
+    targetLabwareLocation: {
+      labwareId: 'labware-b-id',
+    },
+    otherLoadedLabware: [
+      {
+        id: 'labware-b-id',
+        definitionUri: labwareBUri,
+        location: {
+          labwareId: 'labware-c-id',
+        },
+        loadName: '',
+      },
+      {
+        id: 'labware-c-id',
+        definitionUri: labwareCUri,
+        location: {
+          moduleId: "module-id"
+        },
+        loadName: '',
+      },
+    ],
+    deckDef: deckDefinition,
+    otherLabwareDefinitions: [
+      labwareBDefinition, labwareCDefinition
+    ],
+    loadedModules: [{
+      id: "module-id",
+      model: moduleModel,
+      location: {
+        slotName: slotId
+      },
+      serialNumber: ""
+    }],
+  })
+
+  expect(result).toStrictEqual({
+    deckDefinition,
+    slotId,
+    moduleDefinition,
+    labwareDefinitionsBottomToTop: [
+      labwareCDefinition,
+      labwareBDefinition,
+      labwareADefinition
+    ]
+  } satisfies typeof result)
+})
+
+it('should return offDeck if the labware is off-deck', () => {
+  const labwareAUri = 'opentrons/nest_12_reservoir_15ml/1'
+  const labwareBUri = "opentrons/agilent_1_reservoir_290ml/1"
+  const labwareADefinition = getAllDefinitions()[labwareAUri]
+  const labwareBDefinition = getAllDefinitions()[labwareBUri]
+
+  const deckDefinition = getDeckDefinitions().ot3_standard
+
+  const result = resolveLabwareLocation({
+    targetLabwareDef: labwareADefinition,
+    targetLabwareLocation: {
+      labwareId: 'labware-b-id',
+    },
+    otherLoadedLabware: [
+      {
+        id: 'labware-b-id',
+        definitionUri: labwareBUri,
+        location: "offDeck",
+        loadName: '',
+      },
+    ],
+    deckDef: deckDefinition,
+    otherLabwareDefinitions: [labwareBDefinition],
+    loadedModules: [],
+  })
+
+  expect(result).toStrictEqual("offDeck" satisfies typeof result)
+})
+
+it('should return error if something is missing from the input definitions', () => {
+  const labwareAUri = 'opentrons/nest_12_reservoir_15ml/1'
+  const labwareBUri = "opentrons/agilent_1_reservoir_290ml/1"
+  const labwareADefinition = getAllDefinitions()[labwareAUri]
+
+  const deckDefinition = getDeckDefinitions().ot3_standard
+
+  const result = resolveLabwareLocation({
+    targetLabwareDef: labwareADefinition,
+    targetLabwareLocation: {
+      labwareId: 'labware-b-id',
+    },
+    otherLoadedLabware: [
+      {
+        id: 'labware-b-id',
+        definitionUri: labwareBUri,
+        location: "offDeck",
+        loadName: '',
+      },
+    ],
+    deckDef: deckDefinition,
+    otherLabwareDefinitions: [], // Missing labware B definition.
+    loadedModules: [],
+  })
+
+  expect(result).toStrictEqual("error" satisfies typeof result)
+})
