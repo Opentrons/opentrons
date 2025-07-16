b96c04ae152b47e11e343e11ef17e4b71c2c831f
koji <kk2796@nyu.edu>
2025-07-16 13:20:08 -0400

    update path

--- labware-library/src/index.tsx
+++ labware-library/src/index.tsx
@@ -5,7 +5,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import { App } from './components/App'
 import { LabwareCreator } from './labware-creator'
 
-import './styles.global.module.css'
+import './styles.global.css'
 
 export * from './labware-creator'
 
