17fcdab88829ae7c41bc7f2f8644739c10533e47
Jethary <jethary@opentrons.com>
2025-07-16 13:51:01 -0400

    fix import

--- shared-data/js/helpers/getWellNamePerMultiTip.ts
+++ shared-data/js/helpers/getWellNamePerMultiTip.ts
@@ -1,7 +1,7 @@
 import range from 'lodash/range'
 
-import { getLabwareHasQuirk } from '././getLabwareHasQuirk'
 import { get96Channel384WellPlateWells } from './get96Channel384WellPlateWells'
+import { getLabwareHasQuirk } from './getLabwareHasQuirk'
 import { orderWells } from './orderWells'
 import { sortWells } from './sortWells'
 
