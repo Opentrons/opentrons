2eb4bb4fbe654dfb437d78cb6a62fd627aec8399
koji <kk2796@nyu.edu>
2025-07-17 19:15:10 -0400

    fix(components): fix Module storybook
    fix Module storybook

--- components/src/hardware-sim/Module/Module.stories.tsx
+++ components/src/hardware-sim/Module/Module.stories.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react'
+
 import {
   fixture96Plate,
   getModuleDef,
@@ -29,6 +31,17 @@ const moduleModels: ModuleModel[] = [
   MAGNETIC_BLOCK_V1,
 ]
 
+const moduleModelOptions = {
+  'Temperature Module V2': TEMPERATURE_MODULE_V2,
+  'Temperature Module V1': TEMPERATURE_MODULE_V1,
+  'Thermocycler Module V1': THERMOCYCLER_MODULE_V1,
+  'Thermocycler Module V2': THERMOCYCLER_MODULE_V2,
+  'Magnetic Module V1': MAGNETIC_MODULE_V1,
+  'Magnetic Module V2': MAGNETIC_MODULE_V2,
+  'Heater-Shaker Module V1': HEATERSHAKER_MODULE_V1,
+  'Magnetic Block V1': MAGNETIC_BLOCK_V1,
+}
+
 export default {
   title: 'Library/Molecules/Simulation/Module',
 } as Meta
@@ -39,53 +52,65 @@ const Template: Story<{
   hasLabware: boolean
   innerProps: {}
 }> = args => {
+  // Add null check and default to first module model if undefined
+  const moduleModel = args.model || moduleModels[0]
+
   return (
     <RobotCoordinateSpace height="100vh" width="100vw" viewBox="0 -50 200 320">
       <ModuleComponent
-        def={getModuleDef(args.model)}
+        def={getModuleDef(moduleModel)}
         x={0}
         y={0}
         innerProps={args.innerProps}
         orientation={args.orientation}
+        targetSlotId={null}
+        targetDeckId={null}
       >
         {args.hasLabware ? (
-          <LabwareRender definition={fixture96Plate as LabwareDefinition} />
+          <LabwareRender
+            definition={fixture96Plate as LabwareDefinition}
+            positioningMode="passThrough"
+          />
         ) : null}
       </ModuleComponent>
     </RobotCoordinateSpace>
   )
 }
+
 export const Module = Template.bind({})
+Module.args = {
+  model: TEMPERATURE_MODULE_V2,
+  orientation: 'left',
+  hasLabware: false,
+  innerProps: {
+    lidMotorState: 'open',
+    blockTargetTemp: 90,
+    targetTemperature: 4,
+    targetTemp: 40,
+  },
+}
+
 Module.argTypes = {
   model: {
     control: {
       type: 'select',
-      options: moduleModels,
     },
-    defaultValue: moduleModels[0],
+    options: moduleModelOptions,
   },
   orientation: {
     control: {
       type: 'select',
-      options: ['left', 'right'],
     },
-    defaultValue: 'left',
+    options: ['left', 'right'],
   },
   hasLabware: {
     control: {
       type: 'boolean',
     },
-    defaultValue: false,
   },
   innerProps: {
     control: {
       type: 'object',
-    },
-    defaultValue: {
-      lidMotorState: 'open',
-      blockTargetTemp: 90,
-      targetTemperature: 4,
-      targetTemp: 40,
     },
   },
 }
