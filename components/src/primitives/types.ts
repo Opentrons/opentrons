3809321d6db7f1f342decd47ca3c143c316d2efd
koji <kk2796@nyu.edu>
2025-07-21 13:04:21 -0400

    feat(components): remove styled-components from StyleProps
    remove styled-components from StyleProps
    
    clsoe AUTH-2132

--- components/src/primitives/types.ts
+++ components/src/primitives/types.ts
@@ -1,6 +1,6 @@
 // known style types
-import type { CSSProperties, StyledComponent } from 'styled-components'
-import type { ComponentType } from 'react'
+import type { StyledComponent } from 'styled-components'
+import type { ComponentType, CSSProperties } from 'react'
 
 export interface ColorProps {
   color?: string
