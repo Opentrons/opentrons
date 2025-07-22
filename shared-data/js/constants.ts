a5e1e5d87e7f12373dfd1984502bcada2e6ba56a
TamarZanzouri <zanzouritamar@gmail.com>
2025-07-22 15:49:39 -0400

    Update shared-data/js/constants.ts
    Co-authored-by: Alise Au <20424172+ahiuchingau@users.noreply.github.com>

--- shared-data/js/constants.ts
+++ shared-data/js/constants.ts
@@ -735,7 +735,7 @@ export const COMBO_FIXTURES: CutoutFixtureIdsWithFakes[] = [
   STAGING_AREA_RIGHT_SLOT_FIXTURE,
 ]
 
-export const DEFAULT_AA_FOR_WASTE_CHUTE = '1ChannelWasteChute'
+export const DEFAULT_AA_FOR_WASTE_CHUTE = ONE_CHANNEL_WASTE_CHUTE_ADDRESSABLE_AREA
 
 export const LOW_VOLUME_PIPETTES = ['p50_single_flex', 'p50_multi_flex']
 
