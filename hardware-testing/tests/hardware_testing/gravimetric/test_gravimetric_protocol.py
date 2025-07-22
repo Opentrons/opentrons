765678779a5ca84bbd97d10880491f3cfd336026
Ryan Howard <ryan.howard@opentrons.com>
2025-07-21 21:40:10 -0400

    fix the csv by correctly creating the extra tests.

--- hardware-testing/tests/hardware_testing/gravimetric/test_gravimetric_protocol.py
+++ hardware-testing/tests/hardware_testing/gravimetric/test_gravimetric_protocol.py
@@ -116,11 +116,11 @@ def test_gravimetric_test_protocol_has_max_api(pipette: str) -> None:
         # ["1ch1000_extra.csv"],
         ["1ch50.csv"],
         # ["1ch50_extra.csv"],
-        ["96ch1000.csv"],
+        # ["96ch1000.csv"],
         # ["96ch200.csv"], #Needs LC to complete
         # ["8ch1000.csv"],
         # ["8ch1000_extra.csv"],
-        ["8ch50.csv"],
+        # ["8ch50.csv"],
         # ["8ch50_extra.csv"],
     ],
 )
