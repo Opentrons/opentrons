448328629c94022e05d03b27edd53cd0fd1091a7
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2025-07-23 16:36:49 -0400

    getModuleUSBPort util

--- app/src/local-resources/modules/utils/index.ts
+++ app/src/local-resources/modules/utils/index.ts
@@ -1,2 +1,20 @@
+import { FLEX_STACKER_MODULE_TYPE } from '@opentrons/shared-data'
+import type { AttachedModule } from '/app/redux/modules/types'
+
 export * from './getModuleImage'
 export * from './getModulePrepCommands'
+
+
+export function getModuleUSBPort(
+  module: AttachedModule
+): string {
+  if (module.usbPort === null) {
+    return ''
+  }
+  const { port , hubPort } = module.usbPort
+    if (module.moduleType === FLEX_STACKER_MODULE_TYPE) {
+      return hubPort !== undefined ? `S-${hubPort}` : ''
+    }
+  return `USB-${port}${hubPort !== undefined ? '.' + hubPort : ''}`
+}
+
