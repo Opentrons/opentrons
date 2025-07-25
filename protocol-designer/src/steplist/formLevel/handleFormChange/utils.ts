3649ab2fe9e19dcf4a44e104261bda08fe61e3e5
ncdiehl11 <ndiehl@opentrons.com>
2025-07-25 15:31:10 -0400

    fix(protocol-designer): populate blowout flow rate, remove disposal volume warning
    This PR ensures blowout flow rate is populated for "don't use a liquid class" transfers. It also removes a legacy warning for disposal volume below recommended value (liquid classes specify values below the 5ul minimum frequently).
    
    Closes RQA-4425

--- protocol-designer/src/steplist/formLevel/handleFormChange/utils.ts
+++ protocol-designer/src/steplist/formLevel/handleFormChange/utils.ts
@@ -631,6 +631,8 @@ const getNoLiquidClassValuesMoveLiquid = (args: {
       dispense_retract_delay_seconds:
         allOT2Defaults.dispense_retract_delay_seconds,
       ...dipsosalFields,
+      blowout_flowRate:
+        matchingTipLiquidSpecs?.defaultBlowOutFlowRate.default ?? null,
     }
     return {
       ...(liquidHandlingAction === 'all' || liquidHandlingAction === 'aspirate'
@@ -732,6 +734,11 @@ const getNoLiquidClassValuesMoveLiquid = (args: {
     'dispense',
     dispenseMaxUiFlowRate
   )
+
+  const blowoutFlowRateFields = {
+    blowout_flowRate: dispense.retract.blowout.params?.flowRate ?? null,
+  }
+
   const pushOutVolume =
     linearInterpolate(
       volume,
@@ -780,6 +787,7 @@ const getNoLiquidClassValuesMoveLiquid = (args: {
     ...dispenseFlowRateFields,
     ...dispenseOffsetFields,
     ...dispensePositionReferenceFields,
+    ...blowoutFlowRateFields,
     dispense_mmFromBottom: DEFAULT_MM_OFFSET_FROM_BOTTOM,
     dispense_submerge_mmFromBottom: SAFE_MOVE_TO_WELL_OFFSET_FROM_TOP_MM,
     dispense_submerge_position_reference: POSITION_REFERENCE_TOP,
