716295167b7ad2079f79b1fd5bca96f1a58affb9
koji <kk2796@nyu.edu>
2025-08-01 11:39:41 -0400

    address feedback

--- app/src/organisms/ODD/QuickTransferFlow/Overview.tsx
+++ app/src/organisms/ODD/QuickTransferFlow/Overview.tsx
@@ -42,12 +42,12 @@ export function Overview(props: OverviewProps): JSX.Element | null {
     makeSnackbar(t('create_new_to_edit') as string)
   }
 
-  let pathCopy = t('single')
-  if (state.path === 'multiAspirate') {
-    pathCopy = t('consolidate')
-  } else if (state.path === 'multiDispense') {
-    pathCopy = t('distribute')
+  const pathCopyMap = {
+    multiAspirate: t('consolidate'),
+    multiDispense: t('distribute'),
+    single: t('single'),
   }
+  const pathCopy = pathCopyMap[state.path]
 
   const displayItems = [
     {
