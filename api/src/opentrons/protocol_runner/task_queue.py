0bd73596343d15df22a8f27eaf62a6b6ef0c9c59
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-08-01 17:02:39 -0400

    refactor(api): ensure task queue cleanup

--- api/src/opentrons/protocol_runner/task_queue.py
+++ api/src/opentrons/protocol_runner/task_queue.py
@@ -1,5 +1,6 @@
 """Asynchronous task queue to accomplish a protocol run."""
 import asyncio
+import gc
 import logging
 from typing import Any, Awaitable, Callable, Optional, ParamSpec, Concatenate
 
@@ -85,6 +86,11 @@ class TaskQueue:
         except Exception as e:
             log.exception("Exception raised by protocol")
             error = e
+        finally:
+            self._run_func = None
 
-        if self._cleanup_func is not None:
-            await self._cleanup_func(error)
+        try:
+            if self._cleanup_func is not None:
+                await self._cleanup_func(error)
+        finally:
+            self._cleanup_func = None
