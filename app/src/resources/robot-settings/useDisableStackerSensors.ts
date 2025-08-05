5bd946300c23c84f8d04207c14197b895054f004
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2025-08-05 16:02:56 -0400

    add to AdvancedSettings page instead of as a feature flag

---
+++ app/src/resources/robot-settings/useDisableStackerSensors.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import {
+  fetchSettings,
+  getRobotSettings,
+  updateSetting,
+} from '/app/redux/robot-settings'
+
+import type { RobotSettings } from '/app/redux/robot-settings/types'
+import type { Dispatch, State } from '/app/redux/types'
+
+// not releveant to the OT-2, this controls the front LED lights on the Flex
+export function useDisableStackerSensors(
+  robotName: string
+): {
+  sensorsDisabled: boolean
+  toggleSensors: () => void
+} {
+  const [sensorDisabledCache, setSensorsDisabledCache] = useState<boolean>(
+    false
+  )
+
+  const dispatch = useDispatch<Dispatch>()
+
+  const sensorsDisabledFromSettings =
+    useSelector<State, RobotSettings>((state: State) =>
+      getRobotSettings(state, robotName)
+    ).find(setting => setting.id === 'disableFlexStackerLabwareDetection')
+      ?.value === true
+
+  useEffect(() => {
+    setSensorsDisabledCache(sensorsDisabledFromSettings)
+  }, [sensorsDisabledFromSettings])
+
+  useEffect(() => {
+    dispatch(fetchSettings(robotName))
+  }, [dispatch, robotName])
+
+  const toggleSensors = (): void => {
+    dispatch(
+      updateSetting(
+        robotName,
+        'disableFlexStackerLabwareDetection',
+        sensorDisabledCache
+      )
+    )
+    setSensorsDisabledCache(!sensorDisabledCache)
+  }
+
+  return { sensorsDisabled: sensorDisabledCache, toggleSensors }
+}
