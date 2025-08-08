a34ef6fcf72142a0ec0ab1002288d5a0ba2c0e9f
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2025-08-08 15:09:55 -0400

    rename route and fix make sure we set handleMotionRouting as false

--- app/src/organisms/ErrorRecoveryFlows/shared/StackerReengageLatch.tsx
+++ app/src/organisms/ErrorRecoveryFlows/shared/StackerReengageLatch.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next'
 import ReengageLatch from '/app/assets/images/flex_stacker_reengage_latch.png'
 import { DescriptionContent, TwoColumn } from '/app/molecules/InterventionModal'
 
-import { RECOVERY_MAP } from '../constants'
 import { RecoverySingleColumnContentWrapper } from './RecoveryContentWrapper'
 import { RecoveryFooterButtons } from './RecoveryFooterButtons'
 
@@ -16,17 +15,12 @@ export function StackerReengageLatch(props: RecoveryContentProps): JSX.Element {
   const {
     proceedNextStep,
     goBackPrevStep,
-    handleMotionRouting,
   } = routeUpdateActions
   const { closeLabwareLatch } = recoveryCommands
-  const { ROBOT_IN_MOTION } = RECOVERY_MAP
 
   const primaryOnClick = (): void => {
-    void handleMotionRouting(true, ROBOT_IN_MOTION.ROUTE).then(() => {
-      void closeLabwareLatch().then(() => {
-        void proceedNextStep()
-      })
-    })
+    void closeLabwareLatch()
+      .then(() => { void proceedNextStep() })
   }
 
   return (
