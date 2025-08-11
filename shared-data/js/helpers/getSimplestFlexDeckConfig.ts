b8787e083052a5244f1056bbe4a87967230169d0
smb2268 <sarah.breen@opentrons.com>
2025-08-10 22:01:40 -0400

    Fix for waste chute + stacker combo fixtures getting overridden

--- shared-data/js/helpers/getSimplestFlexDeckConfig.ts
+++ shared-data/js/helpers/getSimplestFlexDeckConfig.ts
@@ -1,9 +1,6 @@
 import { getAddressableAreasInProtocol, getDeckDefFromRobotType } from '.'
-import { FLEX_ROBOT_TYPE } from '../constants'
-import {
-  getAddressableAreaFromSlotId,
-  getMainNonComboFixtureId,
-} from '../fixtures'
+import { COMBINATION_FIXTURES, FLEX_ROBOT_TYPE } from '../constants'
+import { getAddressableAreaFromSlotId } from '../fixtures'
 
 import type { AddressableAreaName, CutoutFixtureId, CutoutId } from '../../deck'
 import type { ProtocolAnalysisOutput } from '../../protocol'
@@ -42,24 +39,27 @@ export const FLEX_SIMPLEST_DECK_CONFIG_PROTOCOL_SPEC: CutoutConfigProtocolSpec[]
 export function getSimplestDeckConfigForProtocol(
   protocolAnalysis: CompletedProtocolAnalysis | ProtocolAnalysisOutput | null
 ): CutoutConfigProtocolSpec[] {
-  // TODO(BC, 2023-11-06): abstract out the robot type
   const deckDef = getDeckDefFromRobotType(FLEX_ROBOT_TYPE)
 
   const addressableAreas =
     protocolAnalysis != null
       ? getAddressableAreasInProtocol(protocolAnalysis, deckDef)
       : []
+  // iterates through the list of required addressable areas for the protocol
   const simplestDeckConfig = addressableAreas.reduce<
     CutoutConfigProtocolSpec[]
   >((acc, addressableArea) => {
+    // finds all cutout fixtures that provide this addressable area
     const cutoutFixturesForAddressableArea = getCutoutFixturesForAddressableAreas(
       [addressableArea],
       deckDef.cutoutFixtures
     )
+    // grabs the cutout id for the addressable area
     const cutoutIdForAddressableArea = getCutoutIdForAddressableArea(
       addressableArea,
       cutoutFixturesForAddressableArea
     )
+    // grabs all possible cutout fixtures for that cutout id
     const cutoutFixturesForCutoutId =
       cutoutIdForAddressableArea != null
         ? getCutoutFixturesForCutoutId(
@@ -67,6 +67,8 @@ export function getSimplestDeckConfigForProtocol(
             deckDef.cutoutFixtures
           )
         : null
+
+    // finds what is currently in that specific cutout
     const existingCutoutConfig = acc.find(
       cutoutConfig => cutoutConfig.cutoutId === cutoutIdForAddressableArea
     )
@@ -76,32 +78,33 @@ export function getSimplestDeckConfigForProtocol(
       cutoutFixturesForCutoutId != null &&
       cutoutIdForAddressableArea != null
     ) {
-      const indexOfExistingFixture = cutoutFixturesForCutoutId.findIndex(
-        ({ id }) => id === existingCutoutConfig.cutoutFixtureId
-      )
+      // finds what index in out accumulated deck config object that cutout is at
+      // so that we can see if we've already added a fixture for a different
+      // addressable area located at the same cutout. this would mean we need to change it
+      // to be a combination fixture
       const accIndex = acc.findIndex(
         ({ cutoutId }) => cutoutId === cutoutIdForAddressableArea
       )
+      // what addressable areas we've already looped through and added to this cutout
       const previousRequiredAAs = acc[accIndex]?.requiredAddressableAreas
+
+      // what addressable area we're currently looking at and need to incorporate
       const allNextRequiredAddressableAreas =
         previousRequiredAAs != null &&
         previousRequiredAAs.includes(addressableArea)
           ? previousRequiredAAs
           : [...previousRequiredAAs, addressableArea]
 
+      // check for the next compatible fixture for the new, longer list of addressable areas
       const nextCompatibleCutoutFixture = getSimplestFixtureForAddressableAreas(
         cutoutIdForAddressableArea,
         allNextRequiredAddressableAreas,
         cutoutFixturesForCutoutId
       )
-      const indexOfCurrentFixture = cutoutFixturesForCutoutId.findIndex(
-        ({ id }) => id === nextCompatibleCutoutFixture?.id
-      )
 
-      if (
-        nextCompatibleCutoutFixture != null &&
-        indexOfCurrentFixture > indexOfExistingFixture
-      ) {
+      // this logic swaps out the newly found cutoutfixture id with the existing one
+      // that was added for the last addressable area we referenced
+      if (nextCompatibleCutoutFixture != null) {
         return [
           ...acc.slice(0, accIndex),
           {
@@ -187,20 +190,14 @@ export function getSimplestFixtureForAddressableAreas(
     cutoutId,
     allCutoutFixtures
   )
+
   const nextCompatibleCutoutFixtures = getCutoutFixturesForAddressableAreas(
     requiredAddressableAreas,
     cutoutFixturesForCutoutId
   )
-  if (nextCompatibleCutoutFixtures.length > 1) {
-    const mainFixture = getMainNonComboFixtureId(
-      nextCompatibleCutoutFixtures.map(cf => cf.id),
-      requiredAddressableAreas,
-      cutoutId
-    )
-    return (
-      nextCompatibleCutoutFixtures.find(cf => cf.id === mainFixture) ?? null
-    )
-  } else {
-    return nextCompatibleCutoutFixtures?.[0] ?? null
-  }
+  const nonComboFixture = nextCompatibleCutoutFixtures.find(
+    fixture => !COMBINATION_FIXTURES.includes(fixture.id)
+  )
+
+  return nonComboFixture ?? nextCompatibleCutoutFixtures?.[0] ?? null
 }
