466ae0971c83efb6be07a676213b804c7220f3f7
Tamar Zanzouri <zanzouritamar@gmail.com>
2025-08-11 14:42:23 -0400

    return deck config

--- app/src/organisms/Desktop/Devices/ProtocolRun/SetupLabware/SetupLabwareMap.tsx
+++ app/src/organisms/Desktop/Devices/ProtocolRun/SetupLabware/SetupLabwareMap.tsx
@@ -52,7 +52,7 @@ export function SetupLabwareMap({
   const [hoverLabwareId, setHoverLabwareId] = useState<string | null>(null)
 
   const deckConfig = useMemo(() => {
-      getSimplestDeckConfigForProtocol(protocolAnalysis)
+    return getSimplestDeckConfigForProtocol(protocolAnalysis)
   }, [protocolAnalysis])
 
   const startingDeck = useMemo(
