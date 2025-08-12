8d80f98b2bca6cd6cefdcbd66468e71eb8ad7047
koji <baxin1919@gmail.com>
2025-08-12 10:20:43 -0400

    refactor(components, app): remove unsed buttons (#19194)
    * refactor(components, app): remove unsed buttons

--- components/src/primitives/Btn.stories.tsx
+++ components/src/primitives/Btn.stories.tsx
@@ -1,10 +1,7 @@
 import { Box } from './Box'
 import {
-  LightSecondaryBtn,
-  NewAlertSecondaryBtn,
   NewPrimaryBtn,
   NewSecondaryBtn,
-  PrimaryBtn,
   SecondaryBtn,
   TertiaryBtn,
 } from './Btn'
@@ -16,15 +13,6 @@ export default {
   title: 'Library/Atoms/Btn',
   argTypes: { onClick: { action: 'clicked' } },
 } as Meta
-
-const PrimaryTemplate: Story<
-  React.ComponentProps<typeof PrimaryBtn>
-> = args => <PrimaryBtn {...args} />
-export const Primary = PrimaryTemplate.bind({})
-Primary.args = {
-  children: 'Confirm and Proceed',
-  title: 'primary button title',
-}
 
 const SecondaryTemplate: Story<
   React.ComponentProps<typeof SecondaryBtn>
@@ -53,42 +41,17 @@ AppSecondary.args = {
   title: 'app secondary button title',
 }
 
-const AppAlertSecondaryTemplate: Story<
-  React.ComponentProps<typeof NewAlertSecondaryBtn>
-> = args => <NewAlertSecondaryBtn {...args} />
-export const AppAlertSecondary = AppAlertSecondaryTemplate.bind({})
-AppAlertSecondary.args = {
-  children: 'Cancel',
-  title: 'app alert secondary button title',
-}
-
-const LightSecondaryTemplate: Story<
-  React.ComponentProps<typeof LightSecondaryBtn>
-> = args => <LightSecondaryBtn {...args} />
-export const LightSecondary = LightSecondaryTemplate.bind({})
-LightSecondary.decorators = [
-  Story => (
-    <Box backgroundColor="black" size="20rem" padding="2rem">
-      <Story />
-    </Box>
-  ),
-]
-LightSecondary.args = {
-  children: 'Cancel',
-  title: 'secondary button title',
-}
-
 const TertiaryTemplate: Story<
   React.ComponentProps<typeof TertiaryBtn>
 > = args => <TertiaryBtn {...args} />
-TertiaryTemplate.decorators = [
+export const Tertiary = TertiaryTemplate.bind({})
+Tertiary.decorators = [
   Story => (
-    <Box backgroundColor="black" size="20rem" padding="2rem">
+    <Box backgroundColor="black" size="20rem" padding="2rem" id="hello">
       <Story />
     </Box>
   ),
 ]
-export const Tertiary = TertiaryTemplate.bind({})
 Tertiary.args = {
   children: 'Perform Side-Effect',
   title: 'tertiary button title',
--- components/src/primitives/Btn.tsx
+++ components/src/primitives/Btn.tsx
@@ -67,36 +67,6 @@ export const Btn: StyledComponent<
 `
 
 /**
- * Primary button variant
- *
- * @component
- */
-export const PrimaryBtn = styled(Btn)`
-  ${BUTTON_VARIANT_STYLE}
-  background-color: ${Styles.C_DARK_GRAY};
-  color: ${Styles.C_WHITE};
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.2);
-
-  &:hover,
-  &:focus {
-    background-color: ${Styles.C_BLACK};
-  }
-
-  &:active {
-    background-color: ${Styles.C_MED_DARK_GRAY};
-  }
-
-  &:disabled,
-  &.disabled {
-    background-color: ${Styles.C_LIGHT_GRAY};
-    color: ${Styles.C_MED_GRAY};
-    box-shadow: none;
-  }
-
-  ${styleProps}
-`
-
-/**
  * Secondary button variant
  *
  * @component
@@ -130,7 +100,10 @@ export const SecondaryBtn = styled(Btn)`
  *
  * @component
  */
-export const NewPrimaryBtn = styled(PrimaryBtn)`
+export const NewPrimaryBtn = styled(Btn)`
+  ${BUTTON_VARIANT_STYLE}
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.2);
+
   background-color: ${Styles.C_BLUE};
   color: ${Styles.C_WHITE};
 
@@ -192,33 +165,14 @@ export const NewSecondaryBtn = styled(SecondaryBtn)`
 `
 
 /**
- * Red secondary button variant used in app
+ * Tertiary button variant
  *
  * @component
  */
-export const NewAlertSecondaryBtn = styled(NewSecondaryBtn)`
-  color: ${Styles.C_ERROR_DARK};
-
-  &:hover,
-  &:focus {
-    color: ${Styles.C_ERROR_DARK};
-  }
-
-  &:active {
-    color: ${Styles.C_ERROR_LIGHT};
-  }
-
-  ${styleProps}
-`
-
-/**
- * Light secondary button variant
- *
- * @component
- */
-export const LightSecondaryBtn = styled(SecondaryBtn)`
+export const TertiaryBtn = styled(SecondaryBtn)`
   background-color: ${Styles.C_TRANSPARENT};
   color: ${Styles.C_WHITE};
+  border-width: 0;
 
   &:hover,
   &:focus {
@@ -236,13 +190,4 @@ export const LightSecondaryBtn = styled(SecondaryBtn)`
   }
 
   ${styleProps}
-`
-
-/**
- * Tertiary button variant
- *
- * @component
- */
-export const TertiaryBtn = styled(LightSecondaryBtn)`
-  border-width: 0;
 `
