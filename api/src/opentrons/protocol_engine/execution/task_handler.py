c5c0db23b6be7e6715aa5cb77d3dbe847e3a5ea8
rclarke0 <rhyann.clarke@opentrons.com>
2025-08-18 13:39:22 -0400

    feat(api): Add tests for concurrent commands
    Creates tests to check if tasks have been scheduled or canceled by synchronization functions, added to task_handler, in the expected order.

--- api/src/opentrons/protocol_engine/execution/task_handler.py
+++ api/src/opentrons/protocol_engine/execution/task_handler.py
@@ -1,18 +1,28 @@
 """Task handling."""
 
-
+from __future__ import annotations
 import logging
-from typing import Coroutine
+from typing import Protocol, AsyncIterator
 from ..state.state import StateStore
 from ..resources import ModelUtils
 from ..types import Task
 import asyncio
+import contextlib
+
 
 log = logging.getLogger(__name__)
 
 
+class TaskFunction(Protocol):
+    """The function run inside a task protocol."""
+
+    async def __call__(self, task_handler: TaskHandler) -> None:
+        """The function called inside a task."""
+        ...
+
+
 class TaskHandler:
-    """Implementation logic for fask concurrency."""
+    """Implementation logic for fast concurrency."""
 
     _state_store: StateStore
     _model_utils: ModelUtils
@@ -24,12 +34,12 @@ class TaskHandler:
         self._state_store = state_store
         self._model_utils = model_utils if model_utils is not None else ModelUtils()
 
-    async def create_task(
-        self, task_code: Coroutine[None, None, None], id: str | None = None
-    ) -> Task:
+    async def create_task(self, task_code: TaskFunction, id: str | None = None) -> Task:
         """Create a task and immediately schedules it."""
         task_id = self._model_utils.ensure_id(id)
-        asyncio_task = asyncio.create_task(task_code, name=f"engine-task-{task_id}")
+        asyncio_task = asyncio.create_task(
+            task_code(task_handler=self), name=f"engine-task-{task_id}"
+        )
         return Task(
             id=task_id,
             createdAt=self._model_utils.get_timestamp(),
@@ -37,3 +47,23 @@ class TaskHandler:
             finishedAt=None,
             error=None,
         )
+
+    @contextlib.asynccontextmanager
+    async def synchronize_cancel_latest(self, id: str) -> AsyncIterator[None]:
+        """Cancel current task."""
+        yield
+
+    @contextlib.asynccontextmanager
+    async def synchronize_cancel_previous(self, id: str) -> AsyncIterator[None]:
+        """Cancel previous run."""
+        yield
+
+    @contextlib.asynccontextmanager
+    async def synchronize_sequential(self, id: str) -> AsyncIterator[None]:
+        """Run tasks one after the other."""
+        yield
+
+    @contextlib.asynccontextmanager
+    async def synchronize_concurrent(self, id: str) -> AsyncIterator[None]:
+        """Run a list of tasks at the same time."""
+        yield
--- api/tests/opentrons/protocol_engine/execution/test_task_handler.py
+++ api/tests/opentrons/protocol_engine/execution/test_task_handler.py
@@ -37,12 +37,12 @@ async def test_create_task(
     """Create a task and run it."""
     task_ran = asyncio.Event()
 
-    async def _task() -> None:
+    async def _task(task_handler: TaskHandler) -> None:
         task_ran.set()
 
     created_timestamp = datetime.now()
     decoy.when(model_utils.get_timestamp()).then_return(created_timestamp)
-    task = await subject.create_task(_task())
+    task = await subject.create_task(_task)
     await asyncio.wait_for(task_ran.wait(), timeout=0.25)
     await task.asyncioTask
     assert task.createdAt == created_timestamp
@@ -52,8 +52,12 @@ async def test_uses_passed_id(
     subject: TaskHandler, decoy: Decoy, model_utils: ModelUtils
 ) -> None:
     """Should use provided id."""
+
+    async def _task(task_handler: TaskHandler) -> None:
+        await asyncio.sleep(0)
+
     decoy.when(model_utils.ensure_id("testid1")).then_return("checked testid1")
-    task = await subject.create_task(asyncio.sleep(0), id="testid1")
+    task = await subject.create_task(_task, id="testid1")
     assert task.id == "checked testid1"
 
 
@@ -61,6 +65,160 @@ async def test_generates_id(
     subject: TaskHandler, decoy: Decoy, model_utils: ModelUtils
 ) -> None:
     """It should generate an id if no id is provided."""
+
+    async def _task(task_handler: TaskHandler) -> None:
+        await asyncio.sleep(0)
+
     decoy.when(model_utils.ensure_id(None)).then_return("testid2")
-    task = await subject.create_task(asyncio.sleep(0))
+    task = await subject.create_task(_task)
     assert task.id == "testid2"
+
+
+async def test_synchronization_cancel_latest(subject: TaskHandler) -> None:
+    """Test cancel_lastest synchronization."""
+    task1_started = asyncio.Event()
+    task2_canceled = asyncio.Event()
+
+    async def task_1_method(task_handler: TaskHandler) -> None:
+        """First run method that will proceed."""
+        async with task_handler.synchronize_cancel_latest("test"):
+            task1_started.set()
+            await task2_canceled.wait()
+
+    async def task_2_method(task_handler: TaskHandler) -> None:
+        """Second run method that will get canceled."""
+        await task1_started.wait()
+        try:
+            async with task_handler.synchronize_cancel_latest("test"):
+                await asyncio.sleep(0)
+        except asyncio.CancelledError:
+            task2_canceled.set()
+            raise
+
+    task1 = await subject.create_task(task_1_method)
+    task2 = await subject.create_task(task_2_method)
+    await asyncio.wait_for(
+        asyncio.gather(task1.asyncioTask, task2.asyncioTask, return_exceptions=True),
+        timeout=0.25,
+    )
+    assert task1.asyncioTask.done()
+    assert task1.asyncioTask.exception() is None
+    assert task2.asyncioTask.done()
+    assert isinstance(task2.asyncioTask.exception(), asyncio.CancelledError)
+
+
+async def test_synchronization_cancel_previous(subject: TaskHandler) -> None:
+    """Test cancel_previous synchronization."""
+    task1_started = asyncio.Event()
+    task1_canceled = asyncio.Event()
+
+    async def task_1_method(task_handler: TaskHandler) -> None:
+        """First run method that will get canceled."""
+        try:
+            async with task_handler.synchronize_cancel_previous("test"):
+                task1_started.set()
+                await task1_canceled.wait()
+        except asyncio.CancelledError:
+            task1_canceled.set()
+            raise
+
+    async def task_2_method(task_handler: TaskHandler) -> None:
+        """Second run method will finish."""
+        await task1_started.wait()
+        async with task_handler.synchronize_cancel_previous("test"):
+            task1_canceled.set()
+            await asyncio.sleep(0)
+
+    task1 = await subject.create_task(task_1_method)
+    task2 = await subject.create_task(task_2_method)
+    await asyncio.wait_for(
+        asyncio.gather(task1.asyncioTask, task2.asyncioTask, return_exceptions=True),
+        timeout=0.25,
+    )
+    assert task2.asyncioTask.done()
+    assert task2.asyncioTask.exception() is None
+    assert task1.asyncioTask.done()
+    assert isinstance(task1.asyncioTask.exception(), asyncio.CancelledError)
+
+
+async def test_synchronization_sequential(subject: TaskHandler) -> None:
+    """Test sequential synchronization."""
+    task_queue: "asyncio.Queue[str]" = asyncio.Queue()
+    task1_started = asyncio.Event()
+    task2_started = asyncio.Event()
+
+    async def task_1_method(task_handler: TaskHandler) -> None:
+        """First task will finish first."""
+        async with task_handler.synchronize_sequential("test"):
+            task_queue.put_nowait("task1started")
+            task1_started.set()
+            await task2_started.wait()
+            task_queue.put_nowait("task1finishedwaiting")
+        task_queue.put_nowait("task1finished")
+
+    async def task_3_method() -> None:
+        task2_started.set()
+
+    async def task_2_method(task_handler: TaskHandler) -> None:
+        """Second task will finish second."""
+        await task1_started.wait()
+        synchronizer = task_handler.synchronize_sequential("test")
+        await asyncio.gather(synchronizer.__aenter__(), task_3_method())
+        task_queue.put_nowait("task2started")
+        await synchronizer.__aexit__(None, None, None)
+
+    task1 = await subject.create_task(task_1_method)
+    task2 = await subject.create_task(task_2_method)
+    await asyncio.wait((task1.asyncioTask, task2.asyncioTask), timeout=0.25)
+    assert task1.asyncioTask.done()
+    assert task2.asyncioTask.done()
+    events: list[str] = []
+    while not task_queue.empty():
+        events.append(task_queue.get_nowait())
+    assert events == [
+        "task1started",
+        "task1finishedwaiting",
+        "task2started",
+        "task1finished",
+    ]
+
+
+async def test_synchronize_concurrent(subject: TaskHandler) -> None:
+    """Test concurrent synchronization."""
+    task_queue: "asyncio.Queue[str]" = asyncio.Queue()
+    task1_started = asyncio.Event()
+    task2_started = asyncio.Event()
+
+    async def task_1_method(task_handler: TaskHandler) -> None:
+        """Task 1 starts and doesn't finish until task 2 has started."""
+        async with task_handler.synchronize_concurrent("test"):
+            task_queue.put_nowait("task1started")
+            task1_started.set()
+            await task2_started.wait()
+        task_queue.put_nowait("task1finished")
+
+    async def task_2_method(task_handler: TaskHandler) -> None:
+        """Task 2 starts concurrently with task 1."""
+        await task1_started.wait()
+        async with task_handler.synchronize_concurrent("test"):
+            task_queue.put_nowait("task2started")
+            task2_started.set()
+        task_queue.put_nowait("task2finished")
+
+    task1 = await subject.create_task(task_1_method)
+    task2 = await subject.create_task(task_2_method)
+    await asyncio.wait((task1.asyncioTask, task2.asyncioTask), timeout=0.25)
+    assert task1.asyncioTask.done()
+    assert task2.asyncioTask.done()
+    events: list[str] = []
+    while not task_queue.empty():
+        events.append(task_queue.get_nowait())
+    assert set(events) == {
+        "task1started",
+        "task2started",
+        "task1finished",
+        "task2finished",
+    }
+    assert max(events.index("task1started"), events.index("task2started")) < min(
+        events.index("task1finished"), events.index("task2finished")
+    )
