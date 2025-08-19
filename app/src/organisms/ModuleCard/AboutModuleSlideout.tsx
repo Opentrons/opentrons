cff4b01f1358395d04e27e182ed89b0b626ebd3d
smb2268 <sarah.breen@opentrons.com>
2025-08-19 16:10:45 -0400

    fix(app): RQA-4546

--- app/src/organisms/ModuleCard/AboutModuleSlideout.tsx
+++ app/src/organisms/ModuleCard/AboutModuleSlideout.tsx
@@ -84,7 +84,7 @@ export const AboutModuleSlideout = (
               setShowBanner(false)
             }}
           >
-            {t('firmware_update_available')}
+            {`${t('firmware_update_available')}.`}
             <Btn
               textAlign={ALIGN_START}
               paddingLeft={SPACING.spacing4}
