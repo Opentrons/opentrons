9ffc11be0df93532570c9c6d59f1ccbc290cfc93
Max Marrone <max@opentrons.com>
2025-08-20 18:32:55 -0400

    Lint + format.

--- components/src/molecules/Checkpoints/CheckpointStepContainer.tsx
+++ components/src/molecules/Checkpoints/CheckpointStepContainer.tsx
@@ -1,15 +1,18 @@
+import { StepContainer } from '../../molecules/StepContainer'
 import spacingStyles from './commonspacing.module.css'
 
-import { StepContainer } from '../../molecules/StepContainer'
-import { type ComponentProps } from 'react'
-
+import type { ComponentProps } from 'react'
 
 /**
  * A `StepContainer` nested within a `Checkpoint`.
  * This wrapper component provides `Checkpoint`-specific spacing.
  */
-export function CheckpointStepContainer(props: ComponentProps<typeof StepContainer>): JSX.Element {
-  return <div className={spacingStyles.step_container}>
-    <StepContainer {...props} />
-  </div>
+export function CheckpointStepContainer(
+  props: ComponentProps<typeof StepContainer>
+): JSX.Element {
+  return (
+    <div className={spacingStyles.step_container}>
+      <StepContainer {...props} />
+    </div>
+  )
 }
