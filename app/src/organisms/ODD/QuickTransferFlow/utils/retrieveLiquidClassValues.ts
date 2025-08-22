b272ef00c25d0084b71c6eaba0c826119271f920
koji <kk2796@nyu.edu>
2025-08-22 17:17:42 -0400

    address feedback

--- app/src/organisms/ODD/QuickTransferFlow/utils/retrieveLiquidClassValues.ts
+++ app/src/organisms/ODD/QuickTransferFlow/utils/retrieveLiquidClassValues.ts
@@ -90,7 +90,7 @@ const getNoLiquidClassValues = (
 
   const aspirateFlowRateFields = getFlowRateFields(
     volume,
-    aspirate?.flowRateByVolume,
+    aspirate.flowRateByVolume,
     'aspirate'
   )
   const dispenseFlowRateFields = getFlowRateFields(
