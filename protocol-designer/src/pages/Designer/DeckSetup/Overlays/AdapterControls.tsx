331bb12132939c68f555adc948212f9bf9c725ee
koji <kk2796@nyu.edu>
2025-08-22 20:54:10 -0400

    address feedback

--- protocol-designer/src/pages/Designer/DeckSetup/Overlays/AdapterControls.tsx
+++ protocol-designer/src/pages/Designer/DeckSetup/Overlays/AdapterControls.tsx
@@ -123,6 +123,9 @@ export const AdapterControls = (
     return null
   }
   const draggedDef = draggedItem?.labwareOnDeck?.def
+
+  // when dragging labware over a slot many times quickly
+  // labwareOnDeck could be null/undefined and cause the white screen
   const isCustomLabware =
     draggedItem.labwareOnDeck != null
       ? getLabwareIsCustom(customLabwareDefs, draggedItem.labwareOnDeck)
--- protocol-designer/src/pages/Designer/DeckSetup/Overlays/SlotControls.tsx
+++ protocol-designer/src/pages/Designer/DeckSetup/Overlays/SlotControls.tsx
@@ -141,6 +141,8 @@ export const SlotControls = (props: SlotControlsProps): JSX.Element | null => {
 
   const draggedDef = draggedItem?.labwareOnDeck?.def
 
+  // when dragging labware over a slot many times quickly
+  // labwareOnDeck could be null/undefined and cause the white screen
   const isCustomLabware =
     draggedItem?.labwareOnDeck != null
       ? getLabwareIsCustom(customLabwareDefs, draggedItem?.labwareOnDeck)
