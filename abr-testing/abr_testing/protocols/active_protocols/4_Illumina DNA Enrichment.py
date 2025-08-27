a3620030eafe980e949cfb36d82c2dab8c9b1243
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-08-27 14:51:17 -0400

    fix(abr-testing): only probe when parameter says so

--- abr-testing/abr_testing/protocols/active_protocols/4_Illumina DNA Enrichment.py
+++ abr-testing/abr_testing/protocols/active_protocols/4_Illumina DNA Enrichment.py
@@ -1077,7 +1077,8 @@ def run(protocol: ProtocolContext) -> None:
             Liquid_trash_well_2,
         ]
         protocol.move_lid(reagent_plate, lid, use_gripper=True)
-        helpers.find_liquid_height_of_all_wells(protocol, p50, liquids_to_probe_at_end)
+        if probe_liquid_height_bool:
+            helpers.find_liquid_height_of_all_wells(protocol, p50, liquids_to_probe_at_end)
         if deactivate_modules_bool:
             helpers.deactivate_modules(protocol)
         if not protocol.is_simulating():
