0afa70cdf820b370dc4248fffc0d8311582242bf
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-08-27 14:50:17 -0400

    fix(abr-testing): remove extra load lid command

--- abr-testing/abr_testing/protocols/active_protocols/4_Illumina DNA Enrichment.py
+++ abr-testing/abr_testing/protocols/active_protocols/4_Illumina DNA Enrichment.py
@@ -112,7 +112,6 @@ def run(protocol: ProtocolContext) -> None:
     reagent_plate, temp_adapter = helpers.load_temp_adapter_and_labware(
         "opentrons_96_wellplate_200ul_pcr_full_skirt", temp_block, "Reagent Plate"
     )
-    lid = protocol.load_lid_stack("opentrons_tough_universal_lid", "B4", 2)
     # ========== SECOND ROW ==========
     MAG_PLATE_SLOT: MagneticBlockContext = protocol.load_module(
         helpers.mag_str, "C1"
