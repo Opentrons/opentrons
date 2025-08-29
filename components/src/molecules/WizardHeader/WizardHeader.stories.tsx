e76447135e94a0248acedffaa12e072d4fa66b3e
koji <baxin1919@gmail.com>
2025-08-29 14:13:26 -0400

    update css module and stories for WizardHeader

--- components/src/molecules/WizardHeader/WizardHeader.stories.tsx
+++ components/src/molecules/WizardHeader/WizardHeader.stories.tsx
@@ -1,35 +1,46 @@
-import { WizardHeader } from './index'
+import { WizardHeader as WizardHeaderComponent } from './index'
 
-import type { Meta, Story } from '@storybook/react'
-import type * as React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
 
-export default {
+const meta: Meta<typeof WizardHeaderComponent> = {
   title: 'Helix/Molecules/WizardHeader',
-  component: WizardHeader,
-} as Meta
-
-const Template: Story<React.ComponentProps<typeof WizardHeader>> = args => (
-  <WizardHeader {...args} />
-)
-
-export const Primary = Template.bind({})
-Primary.args = {
-  totalSteps: 5,
-  currentStep: 2,
-  title: 'Tip Length Calibration',
+  component: WizardHeaderComponent,
 }
 
-export const CurrentStepZero = Template.bind({})
-CurrentStepZero.args = {
-  totalSteps: 5,
-  currentStep: 0,
-  title: 'Tip Length Calibration',
+export default meta
+
+type Story = StoryObj<typeof WizardHeaderComponent>
+
+export const WizardHeader: Story = {
+  args: {
+    totalSteps: 5,
+    currentStep: 2,
+    title: 'Tip Length Calibration',
+  },
 }
 
-export const ErrorState = Template.bind({})
-ErrorState.args = {
-  totalSteps: 5,
-  currentStep: 1,
-  isErrorState: true,
-  title: 'Tip Length Calibration',
+export const WizardHeaderCurrentStepZero: Story = {
+  args: {
+    totalSteps: 5,
+    currentStep: 0,
+    title: 'Tip Length Calibration',
+  },
+}
+
+export const WizardHeaderNoExit: Story = {
+  args: {
+    totalSteps: 5,
+    currentStep: 1,
+    onExit: null,
+    title: 'Tip Length Calibration',
+  },
+}
+
+export const WizardHeaderHideStepText: Story = {
+  args: {
+    totalSteps: 5,
+    currentStep: 1,
+    hideStepText: true,
+    title: 'Tip Length Calibration',
+  },
 }
