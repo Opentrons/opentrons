8989142db27da46eaf3c269b994775758bd41b4a
Caila Marashaj <98041399+caila-marashaj@users.noreply.github.com>
2025-09-02 17:03:38 -0400

    feat(api): tip positioning for reservoir bottoms for schema 2 (#19317)

--- api/tests/opentrons/protocol_engine/commands/test_move_to_well.py
+++ api/tests/opentrons/protocol_engine/commands/test_move_to_well.py
@@ -71,6 +71,7 @@ async def test_move_to_well_implementation(
             speed=7.89,
             current_well=None,
             operation_volume=None,
+            offset_pipette_for_reservoir_subwells=False,
         )
     ).then_return(Point(x=9, y=8, z=7))
 
@@ -137,6 +138,7 @@ async def test_move_to_well_stall_defined_error(
             speed=7.89,
             current_well=None,
             operation_volume=None,
+            offset_pipette_for_reservoir_subwells=False,
         )
     ).then_raise(StallOrCollisionDetectedError())
     decoy.when(mock_model_utils.generate_id()).then_return(error_id)
