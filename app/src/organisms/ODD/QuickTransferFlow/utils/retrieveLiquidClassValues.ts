c4168398d94fff7251027c411bfadbec3426a3b8
koji <baxin1919@gmail.com>
2025-09-04 16:35:48 -0400

    fix(app): fix the function to retrieve Liquid Class values
    update the function with PD's implementation to fix the function to retrieve Liquid Class values
    
    close RQA-4624

--- app/src/organisms/ODD/QuickTransferFlow/utils/retrieveLiquidClassValues.ts
+++ app/src/organisms/ODD/QuickTransferFlow/utils/retrieveLiquidClassValues.ts
@@ -15,9 +15,11 @@ import {
   SOURCE_WELL_BLOWOUT_DESTINATION,
 } from '@opentrons/step-generation'
 
+import { getFlowRateFields } from './getFlowRaiteFields'
+import { getMatchingTipLiquidSpecsFromSpec } from './getMatchingTipLiquidSpecsFromSpec'
+import { getMaxUiFlowRate } from './getMaxUiFlowRate'
 import { getTransferPlanAndReferenceVolumes } from './getTransferPlanAndReferenceVolumes'
 
-import type { LiquidHandlingPropertyByVolume } from '@opentrons/shared-data'
 import type { BlowOutLocation, QuickTransferSummaryState } from '../types'
 
 export const retrieveLiquidClassValues = (
@@ -71,13 +73,13 @@ const getNoLiquidClassValues = (
   liquidHandlingAction: 'aspirate' | 'dispense' | 'all'
 ): QuickTransferSummaryState => {
   const { tipRack, path, volume, pipette } = state
-  const tiprackDefinition = getLabwareDefURI(tipRack)
+  const tiprackUri = getLabwareDefURI(tipRack)
   const referenceLiquidClass = getAllLiquidClassDefs()[WATER_LIQUID_CLASS_NAME]
   const liquidClassValuesForPipette = referenceLiquidClass.byPipette.find(
     ({ pipetteModel }) => convertedPipetteName === pipetteModel
   )
   const liquidClassValuesForTip = liquidClassValuesForPipette?.byTipType.find(
-    tipObject => tipObject.tiprack === tiprackDefinition
+    tipObject => tipObject.tiprack === tiprackUri
   )
   if (liquidClassValuesForTip == null) {
     return state
@@ -87,17 +89,6 @@ const getNoLiquidClassValues = (
     multiDispense != null && path === 'multiDispense'
       ? multiDispense
       : singleDispense
-
-  const aspirateFlowRateFields = getFlowRateFields(
-    volume,
-    aspirate.flowRateByVolume,
-    'aspirate'
-  )
-  const dispenseFlowRateFields = getFlowRateFields(
-    volume,
-    dispense.flowRateByVolume,
-    'dispense'
-  )
 
   const pushOutVolume = linearInterpolate(
     volume,
@@ -128,7 +119,54 @@ const getNoLiquidClassValues = (
     disposalByVolume,
   }).referenceVolumes
 
-  const { airGap, conditioning } = byVolumeLookup
+  const { airGap, conditioning, correction, flowRate } = byVolumeLookup
+
+  const aspirateCorrectionVolume = linearInterpolate(
+    correction.aspirate,
+    aspirate.correctionByVolume as Array<[number, number]>
+  )
+  const dispenseCorrectionVolume = linearInterpolate(
+    correction.dispense,
+    dispense.correctionByVolume as Array<[number, number]>
+  )
+
+  const matchingTipLiquidSpecs = getMatchingTipLiquidSpecsFromSpec(
+    pipette,
+    volume,
+    tiprackUri as string
+  )
+
+  const aspirateMaxUiFlowRate = getMaxUiFlowRate({
+    targetVolume: flowRate.aspirate,
+    channels: pipette.channels,
+    tipLiquidSpecs: matchingTipLiquidSpecs,
+    flowRateType: 'aspirate',
+    correctionVolume: aspirateCorrectionVolume ?? 0,
+    shaftULperMM: pipette.shaftULperMM,
+  })
+
+  const dispenseMaxUiFlowRate = getMaxUiFlowRate({
+    targetVolume: flowRate.dispense,
+    channels: pipette.channels,
+    tipLiquidSpecs: matchingTipLiquidSpecs,
+    flowRateType: 'dispense',
+    correctionVolume: dispenseCorrectionVolume ?? 0,
+    shaftULperMM: pipette.shaftULperMM,
+  })
+
+  const aspirateFlowRateFields = getFlowRateFields(
+    volume,
+    aspirate.flowRateByVolume,
+    'aspirate',
+    aspirateMaxUiFlowRate
+  )
+
+  const dispenseFlowRateFields = getFlowRateFields(
+    volume,
+    dispense.flowRateByVolume,
+    'dispense',
+    dispenseMaxUiFlowRate
+  )
 
   const aspirateState = {
     aspirateFlowRate: aspirateFlowRateFields.aspirate_flowRate ?? 0,
@@ -155,7 +193,7 @@ const getNoLiquidClassValues = (
       ? undefined
       : aspirate.retract.touchTip.params?.zOffset,
     touchTipAspirateSpeed: aspirate.retract.touchTip.params?.speed,
-    airGapAspirate: airGap.aspirate,
+    airGapAspirate: aspirate.retract.airGapByVolume[0][1] ?? 0,
     conditionAspirate: conditioning ?? 0,
   }
 
@@ -173,7 +211,11 @@ const getNoLiquidClassValues = (
           delayDuration: dispense.delay.params?.duration ?? 0,
         },
     pushOutDispense: {
-      volume: pushOutVolume ?? 0,
+      volume:
+        linearInterpolate(
+          volume,
+          singleDispense?.pushOutByVolume as Array<[number, number]>
+        ) ?? 0,
     },
     retractDispense: {
       speed: dispense.retract.speed,
@@ -191,7 +233,7 @@ const getNoLiquidClassValues = (
       ? undefined
       : dispense.retract.touchTip.params?.zOffset,
     touchTipDispenseSpeed: dispense.retract.touchTip.params?.speed,
-    airGapDispense: airGap.dispense,
+    airGapDispense: dispense.retract.airGapByVolume[0][1] ?? 0,
     disposalVolumeDispenseSettings: {
       volume: pipette.liquids.default.minVolume,
       blowOutLocation:
@@ -220,20 +262,6 @@ const getNoLiquidClassValues = (
       ...state,
       ...dispenseState,
     }
-  }
-}
-
-const getFlowRateFields = (
-  volume: number,
-  flowRateByVolume: LiquidHandlingPropertyByVolume,
-  liquidHandlingAction: 'aspirate' | 'dispense' | 'all'
-): Record<string, number | null> => {
-  const interpolatedFlowRate = linearInterpolate(
-    volume,
-    flowRateByVolume as Array<[number, number]>
-  )
-  return {
-    [`${liquidHandlingAction}_flowRate`]: interpolatedFlowRate,
   }
 }
 
@@ -277,21 +305,26 @@ const getLiquidClassValues = (
 
   const { aspirate, singleDispense, multiDispense } = tipTypeSettings ?? {}
 
+  const {
+    flowRateByVolume: aspirateFlowRateByVolume,
+    aspiratePosition,
+    preWet,
+    mix: aspirateMix,
+    delay: aspirateDelay,
+  } = aspirate ?? {}
+
   const dispense =
     multiDispense != null && path === 'multiDispense'
       ? multiDispense
       : singleDispense
 
-  const aspirateFlowRateFields = getFlowRateFields(
-    volume,
-    aspirate?.flowRateByVolume ?? [],
-    'aspirate'
-  )
-  const dispenseFlowRateFields = getFlowRateFields(
-    volume,
-    dispense?.flowRateByVolume ?? [],
-    'dispense'
-  )
+  const {
+    flowRateByVolume: dispenseFlowRateByVolume,
+    dispensePosition,
+    delay: dispenseDelay,
+  } = dispense ?? {}
+
+  const { pushOutByVolume } = singleDispense ?? {}
 
   const {
     conditioningByVolume: rawConditioningByVolume = [],
@@ -317,7 +350,60 @@ const getLiquidClassValues = (
     disposalByVolume,
   }).referenceVolumes
 
+  const { flowRate } = byVolumeLookup
+  const tiprackUri = getLabwareDefURI(tipRack)
+
+  const matchingTipLiquidSpecs = getMatchingTipLiquidSpecsFromSpec(
+    pipetteSpecs,
+    volume,
+    tiprackUri
+  )
+
+  const aspirateCorrectionVolume = linearInterpolate(
+    flowRate.aspirate,
+    aspirate?.correctionByVolume as Array<[number, number]>
+  )
+  const dispenseCorrectionVolume = linearInterpolate(
+    flowRate.dispense,
+    dispense?.correctionByVolume as Array<[number, number]>
+  )
+
+  const aspirateMaxUiFlowRate = getMaxUiFlowRate({
+    targetVolume: flowRate.aspirate,
+    channels: pipetteSpecs.channels,
+    tipLiquidSpecs: matchingTipLiquidSpecs,
+    flowRateType: 'aspirate',
+    correctionVolume: aspirateCorrectionVolume ?? 0,
+    shaftULperMM: pipetteSpecs.shaftULperMM,
+  })
+
+  const dispenseMaxUiFlowRate = getMaxUiFlowRate({
+    targetVolume: flowRate.dispense,
+    channels: pipetteSpecs.channels,
+    tipLiquidSpecs: matchingTipLiquidSpecs,
+    flowRateType: 'dispense',
+    correctionVolume: dispenseCorrectionVolume ?? 0,
+    shaftULperMM: pipetteSpecs.shaftULperMM,
+  })
+
+  const aspirateFlowRateFields = getFlowRateFields(
+    volume,
+    aspirate?.flowRateByVolume ?? [],
+    'aspirate',
+    aspirateMaxUiFlowRate
+  )
+
+  const dispenseFlowRateFields = getFlowRateFields(
+    volume,
+    dispense?.flowRateByVolume ?? [],
+    'dispense',
+    dispenseMaxUiFlowRate
+  )
+
   const { pushOut, airGap, conditioning, disposal } = byVolumeLookup
+
+  console.log('pushOut', pushOut)
+  console.log('airGap', airGap)
 
   const aspirateState = {
     aspirateFlowRate: aspirateFlowRateFields.aspirate_flowRate ?? 0,
@@ -327,7 +413,7 @@ const getLiquidClassValues = (
       positionFromBottom: aspirate?.submerge.startPosition.offset.z ?? 0,
       delayDuration: aspirate?.submerge.delay.params?.duration ?? 0,
     },
-    preWetTip: aspirate?.preWet ?? false,
+    preWetTip: preWet ?? false,
     mixOnAspirate:
       aspirate?.mix.enable === false
         ? undefined
@@ -354,7 +440,7 @@ const getLiquidClassValues = (
       aspirate?.retract.touchTip.enable === false
         ? undefined
         : aspirate?.retract.touchTip.params?.speed,
-    airGapAspirate: airGap.aspirate,
+    airGapAspirate: aspirate?.retract.airGapByVolume[0][1] ?? 0,
     conditionAspirate: conditioning ?? 0,
   }
 
@@ -380,7 +466,11 @@ const getLiquidClassValues = (
             repetitions: singleDispense?.mix?.params?.repetitions ?? 0,
           },
     pushOutDispense: {
-      volume: pushOut,
+      volume:
+        linearInterpolate(
+          volume,
+          singleDispense?.pushOutByVolume as Array<[number, number]>
+        ) ?? 0,
     },
     retractDispense: {
       speed: dispense?.retract.speed ?? 0,
@@ -405,7 +495,7 @@ const getLiquidClassValues = (
       dispense?.retract.touchTip.enable === false
         ? undefined
         : dispense?.retract.touchTip.params?.speed,
-    airGapDispense: airGap.dispense,
+    airGapDispense: dispense?.retract.airGapByVolume[0][1] ?? 0,
     disposalVolumeDispenseSettings: {
       volume: disposal ?? 0,
       blowOutLocation:
