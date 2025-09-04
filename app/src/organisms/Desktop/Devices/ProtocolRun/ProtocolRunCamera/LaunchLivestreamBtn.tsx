a0b25a4b480039619cfdd12f489f59892d108b10
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-09-04 14:49:32 -0400

    feat(app): add ProtocolRunCamera to camera tab

---
+++ app/src/organisms/Desktop/Devices/ProtocolRun/ProtocolRunCamera/LaunchLivestreamBtn.tsx
@@ -0,0 +1,32 @@
+import { useTranslation } from 'react-i18next'
+import { useDispatch } from 'react-redux'
+
+import { Icon, SecondaryButton } from '@opentrons/components'
+import { useHost } from '@opentrons/react-api-client'
+
+import { cameraStreamOpenAction } from '/app/redux/shell'
+
+import styles from './runcamera.module.css'
+
+export function LaunchLivestreamBtn(): JSX.Element {
+  const { t } = useTranslation('run_details')
+  const dispatch = useDispatch()
+  const host = useHost()
+  const isLaunchCameraEnabled =
+    host?.robotName != null && host?.hostname != null
+
+  const handleOpenCameraStream = (): void => {
+    dispatch(cameraStreamOpenAction(host?.hostname ?? 'UNKNOWN'))
+  }
+
+  return (
+    <SecondaryButton
+      className={styles.launch_camera_btn}
+      aria-disabled={!isLaunchCameraEnabled}
+      onClick={handleOpenCameraStream}
+    >
+      {t('live_camera_view')}
+      <Icon className={styles.launch_icon_style} name="open-in-new" />
+    </SecondaryButton>
+  )
+}
