31c2b988bd36f233afc9059b98812cff2cc8e11a
koji <baxin1919@gmail.com>
2025-09-04 16:49:06 -0400

    remove unused lines

--- app/src/organisms/ODD/QuickTransferFlow/utils/getFlowRaiteFields.ts
+++ app/src/organisms/ODD/QuickTransferFlow/utils/getFlowRaiteFields.ts
@@ -4,21 +4,6 @@ import { linearInterpolate } from '@opentrons/shared-data'
 
 import type { LiquidHandlingPropertyByVolume } from '@opentrons/shared-data'
 
-// export const getFlowRateFields = (
-//   volume: number,
-//   flowRateByVolume: LiquidHandlingPropertyByVolume,
-//   liquidHandlingAction: 'aspirate' | 'dispense' | 'all'
-//   hardwareMaximum: number | null = null
-// ): Record<string, number | null> => {
-//   const interpolatedFlowRate = linearInterpolate(
-//     volume,
-//     flowRateByVolume as Array<[number, number]>
-//   )
-//   return {
-//     [`${liquidHandlingAction}_flowRate`]: interpolatedFlowRate,
-//   }
-// }
-
 export const getFlowRateFields = (
   volume: number,
   flowRateByVolume: LiquidHandlingPropertyByVolume,
