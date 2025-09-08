a304d1544b9f7c4bbf45f948c3dd790fb43df9c4
Max Marrone <max@opentrons.com>
2025-09-08 15:37:06 -0400

    Delete StepDragPreview.

--- protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/DraggableSteps.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/DraggableSteps.tsx
@@ -154,48 +154,6 @@ export function DraggableSteps(props: DraggableStepsProps): JSX.Element | null {
           sidebarWidth={sidebarWidth}
         />
       ))}
-      <StepDragPreview sidebarWidth={sidebarWidth} />
-    </Flex>
-  )
-}
-
-interface StepDragPreviewProps {
-  sidebarWidth: number
-}
-
-function StepDragPreview({
-  sidebarWidth,
-}: StepDragPreviewProps): JSX.Element | null {
-  const [{ isDragging, itemType, item, currentOffset }] = useDrag(() => ({
-    type: DND_TYPES.STEP_ITEM,
-    collect: (monitor: DragLayerMonitor) => ({
-      currentOffset: monitor.getSourceClientOffset(),
-      isDragging: monitor.isDragging(),
-      itemType: monitor.getItemType(),
-      item: monitor.getItem() as { stepId: StepIdType },
-    }),
-  }))
-
-  const savedStepForms = useSelector(stepFormSelectors.getSavedStepForms)
-  const savedForm = item && savedStepForms[item.stepId]
-  const { stepType, stepName } = savedForm || {}
-
-  if (
-    itemType !== DND_TYPES.STEP_ITEM ||
-    !isDragging ||
-    stepType == null ||
-    currentOffset == null
-  ) {
-    return null
-  }
-
-  return (
-    <Flex cursor="grabbing" backgroundColor={COLORS.transparent}>
-      <ConnectedStepContainer
-        iconName={stepIconsByType[stepType]}
-        title={stepName || ''}
-        sidebarWidth={sidebarWidth}
-      />
     </Flex>
   )
 }
