b664e8359414f5bc9de74c8ef71deea9e4acfd19
Max Marrone <max@opentrons.com>
2025-09-09 15:28:35 -0400

    Fix merge mistake.

--- protocol-designer/cypress/e2e/thermocycler.cy.ts
+++ protocol-designer/cypress/e2e/thermocycler.cy.ts
@@ -22,7 +22,7 @@ describe('Redesigned Thermocycler Set Up Steps - Happy Path', () => {
   })
 
   it('It should verify the working function of thermocycler set up', () => {
-    const se = new StepBuilder()
+    const se = new StepExecutor()
     se.execute(
       TimelineSteps.SelectItemMenuOption(1, 'Thermocycler', 'Edit step')
     )
