f003f4907480314bcd1967e1a0c97e4df83b43f1
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-09-11 12:54:25 -0400

    fix(api): run format and correct docs reference

--- api/docs/v2/new_protocol_api.rst
+++ api/docs/v2/new_protocol_api.rst
@@ -16,7 +16,7 @@ Protocols
    :members:
    :exclude-members: location_cache, cleanup, clear_commands
 
-.. autoclass:: opentrons.protocol_api.Tasks
+.. autoclass:: opentrons.protocol_api.tasks
    :members:
 
 Instruments
--- api/src/opentrons/protocol_api/core/legacy_simulator/tasks.py
+++ api/src/opentrons/protocol_api/core/legacy_simulator/tasks.py
@@ -8,12 +8,12 @@ class LegacyTaskCore(AbstractTaskCore):
 
     def get_created_at_timestamp(self) -> datetime:
         raise NotImplementedError("Legacy protocols do not implement tasks.")
-    
+
     def is_done(self) -> bool:
         raise NotImplementedError("Legacy protocols do not implement tasks.")
-    
+
     def is_started(self) -> bool:
         raise NotImplementedError("Legacy protocols do not implement tasks.")
-    
+
     def get_finished_at_timestamp(self) -> datetime | None:
         raise NotImplementedError("Legacy protocols do not implement tasks.")
