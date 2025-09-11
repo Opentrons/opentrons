0a2107e83832316f011c8398ca4ad4545f4058bc
Tamar Zanzouri <zanzouritamar@gmail.com>
2025-09-11 11:48:53 -0400

    infrastructure for new flow

--- app/src/organisms/ErrorRecoveryFlows/hooks/useRecoveryOptionCopy.tsx
+++ app/src/organisms/ErrorRecoveryFlows/hooks/useRecoveryOptionCopy.tsx
@@ -62,8 +62,10 @@ export function useRecoveryOptionCopy(): (
       case RECOVERY_MAP.STACKER_SHUTTLE_MISSING_RETRY.ROUTE:
         return t('load_labware_shuttle_and_retry_step')
       case RECOVERY_MAP.STACKER_SHUTTLE_EMPTY_SKIP.ROUTE:
+      case RECOVERY_MAP.STACKER_SHUTTLE_EMPTY_STORE_SKIP.ROUTE:
         return t('manually_load_labware_into_shuttle_and_skip')
       case RECOVERY_MAP.STACKER_SHUTTLE_EMPTY_RETRY.ROUTE:
+      case RECOVERY_MAP.STACKER_SHUTTLE_EMPTY_STORE_RETRY.ROUTE:
         return t('replace_labware_in_stacker_and_retry')
       default:
         return 'Unknown action'
