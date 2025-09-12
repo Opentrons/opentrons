f9357e0efa85d06bb7d47ad2329c17df3cd69fc6
Jethary <jethary@opentrons.com>
2025-09-12 09:52:03 -0700

    remove console.log

--- app/src/organisms/ODD/QuickTransferFlow/utils/createQuickTransferFile.ts
+++ app/src/organisms/ODD/QuickTransferFlow/utils/createQuickTransferFile.ts
@@ -154,7 +154,6 @@ export function createQuickTransferFile(
     robotStateTimeline.timeline,
     timelineFrame => timelineFrame.commands
   )
-  console.log('nonLoadCommands', nonLoadCommands)
   const commands: CreateCommand[] = [
     loadPipetteCommand,
     ...loadAdapterCommands,
