b5bf98740e995e0e1189713bab453254ca5d026c
Rhyann Clarke <146747548+rclarke0@users.noreply.github.com>
2025-09-15 09:46:39 -0400

    fix(api): clearer docstrings in Task class (#19542)
    Task class is included properly in the API reference and that its method
    and property docstrings are up to par.
    
    closes RTC-836

--- api/src/opentrons/protocol_api/core/tasks.py
+++ api/src/opentrons/protocol_api/core/tasks.py
@@ -10,15 +10,18 @@ class AbstractTaskCore(ABC):
 
     @abstractmethod
     def is_done(self) -> bool:
-        """Return whether the task is done."""
+        """Returns ``True`` if the task is done."""
         ...
 
     @abstractmethod
     def is_started(self) -> bool:
-        """Return whether the task has started."""
+        """Returns ``True`` if the task has started."""
         ...
 
     @abstractmethod
     def get_finished_at_timestamp(self) -> datetime | None:
-        """Get the finishedAt timestamp of the task, or None if not finished."""
+        """The timestamp of the when the task finished.
+
+        Returns ``None`` if the task hasn't finished yet.
+        """
         ...
