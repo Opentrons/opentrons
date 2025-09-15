cd9f470a79bd70b7a83d6f6992e8b9c0b681e50f
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-09-15 16:29:17 -0400

    refactor(abr-testing): Update protocols to use new error function

--- abr-testing/abr_testing/protocols/active_protocols/13_Stacker_Labware_Stamping_Test.py
+++ abr-testing/abr_testing/protocols/active_protocols/13_Stacker_Labware_Stamping_Test.py
@@ -220,5 +220,7 @@ def run(ctx: ProtocolContext) -> None:
         )
     except Exception as e:
         if not ctx.is_simulating():
-            slack_bot.send_error_message(metadata["protocolName"], str(e))
+            helpers.send_slack_error_message_with_log(
+                slack_bot, metadata["protocolName"], str(e)
+            )
         raise (e)
