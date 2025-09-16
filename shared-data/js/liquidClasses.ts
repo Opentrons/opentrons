c95180fc23fee435193d144455f1b3ec50ba8a9a
Jethary <jethary@opentrons.com>
2025-09-16 11:09:28 -0700

    fix const

--- shared-data/js/liquidClasses.ts
+++ shared-data/js/liquidClasses.ts
@@ -4,8 +4,8 @@ import waterV2Uncasted from '../liquid-class/definitions/1/water/2.json'
 
 import type { LiquidClass } from '.'
 
-const ethanol80V1 = ethanol80V1Uncasted as LiquidClass
-const glycerol50V1 = glycerol50V1Uncasted as LiquidClass
+const ethanol80V2 = ethanol80V1Uncasted as LiquidClass
+const glycerol50V2 = glycerol50V1Uncasted as LiquidClass
 const waterV2 = waterV2Uncasted as LiquidClass
 
 export const WATER_LIQUID_CLASS_NAME_V2 = 'waterV2'
@@ -13,7 +13,7 @@ export const NONE_LIQUID_CLASS_NAME = 'none'
 export const GLYCEROL_LIQUID_CLASS_NAME_V2 = 'glycerol50V2'
 export const ETHANOL_LIQUID_CLASS_NAME_V2 = 'ethanol80V2'
 
-const defs = { waterV2, glycerol50V1, ethanol80V1 }
+const defs = { waterV2, glycerol50V2, ethanol80V2 }
 
 //  returns all liquid class defs but their latest version only
 //  NOTE: we should refactor this util though to get the latest versions of all the definitions types
