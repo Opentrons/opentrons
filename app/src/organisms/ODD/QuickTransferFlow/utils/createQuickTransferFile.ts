27690053862b28db47833db6a7102a766be29bd9
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-09-19 10:28:19 -0700

    feat(app): update QT version to 2.0.0 to include py generation (#19610)

--- app/src/organisms/ODD/QuickTransferFlow/utils/createQuickTransferFile.ts
+++ app/src/organisms/ODD/QuickTransferFlow/utils/createQuickTransferFile.ts
@@ -280,9 +280,8 @@ export function createQuickTransferPythonFile(
       protocolName ?? `Quick Transfer ${quickTransferState.volume}µL`,
     description: `This quick transfer moves liquids from a ${sourceLabwareName} to a ${destinationLabwareName}`,
     source: 'Quick Transfer',
-    //  TODO: increase version for when we export python
     //  see QuickTransferFlow/README.md for versioning details
-    version: '1.2.0',
+    version: '2.0.0',
     category: null,
     subcategory: null,
     tags: [],
