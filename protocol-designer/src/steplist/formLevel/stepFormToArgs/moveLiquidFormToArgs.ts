91ad0ea7f0f257efcd67a6525bb5c9ca7247cf3a
ncdiehl11 <ndiehl@opentrons.com>
2025-09-22 15:34:29 -0400

    fix(protocol-designer): fix logic for null and 0 zOffset values
    Correctly handle null form values for aspirate/dispense _mmFromBottom fields in moveLiquid form and mix_mmFromBottom field in mix form.

--- protocol-designer/src/steplist/formLevel/stepFormToArgs/moveLiquidFormToArgs.ts
+++ protocol-designer/src/steplist/formLevel/stepFormToArgs/moveLiquidFormToArgs.ts
@@ -353,11 +353,15 @@ export const moveLiquidFormToArgs = (
     nozzles,
     aspirateXOffset: aspirate_x_position ?? 0,
     aspirateYOffset: aspirate_y_position ?? 0,
-    aspirateZOffset: hydratedFormData.aspirate_mmFromBottom ?? 0,
+    aspirateZOffset:
+      hydratedFormData.aspirate_mmFromBottom ??
+      DEFAULT_MM_TOUCH_TIP_OFFSET_FROM_TOP,
     aspiratePositionReference: hydratedFormData.aspirate_position_reference,
     dispenseXOffset: dispense_x_position ?? 0,
     dispenseYOffset: dispense_y_position ?? 0,
-    dispenseZOffset: hydratedFormData.dispense_mmFromBottom ?? 0,
+    dispenseZOffset:
+      hydratedFormData.dispense_mmFromBottom ??
+      DEFAULT_MM_TOUCH_TIP_OFFSET_FROM_TOP,
     dispensePositionReference: hydratedFormData.dispense_position_reference,
     aspirateSubmergeSpeed: hydratedFormData.aspirate_submerge_speed ?? null,
     aspirateSubmergeXOffset: hydratedFormData.aspirate_submerge_x_position ?? 0,
