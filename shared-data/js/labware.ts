ad98df83c8ef425c2af1fd0aafcd9151820f6466
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-09-23 14:20:29 -0400

    refactor(shared-data): add path-browserify to get labware image paths

--- shared-data/js/labware.ts
+++ shared-data/js/labware.ts
@@ -1,3 +1,4 @@
+import { basename, extname } from "path-browserify"
 import fixture12Trough from '../labware/fixtures/2/fixture_12_trough.json'
 import fixture24Tuberack from '../labware/fixtures/2/fixture_24_tuberack.json'
 import fixture96Plate from '../labware/fixtures/2/fixture_96_plate.json'
@@ -112,8 +113,9 @@ function getAllImages(): Record<string, string> {
   })
   const imageKeyToUrl: Record<string, string> = {}
   for (const imgPath in imageModules) {
-    const filename = imgPath.split(/[/\\]/).pop() || ""
-    const base = filename.replace(/\.(png|jpe?g)$/i, '')
+    const filename = basename(imgPath)
+    const ext = extname(filename) 
+    const base = basename(filename, ext)
     const varName = base.replace(/\./g, '_').replace(/-/g, '_')
     imageKeyToUrl[varName] = imageModules[imgPath] as string
   }
