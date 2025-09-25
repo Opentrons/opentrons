6d22aaf4c3dd82e165efe5cd3f42679ef9fa5bd2
koji <baxin1919@gmail.com>
2025-09-25 15:15:59 -0400

    feat(app): add visualization button to protocol setup (#19600)
    * feat(app): add visualization button to protocol setup

--- app/src/organisms/Desktop/ProtocolDetails/index.tsx
+++ app/src/organisms/Desktop/ProtocolDetails/index.tsx
@@ -383,7 +383,7 @@ export function ProtocolDetails(
   }
 
   const handleClickTimeline = (): void => {
-    navigate(`/protocols/${protocolKey}/preview`)
+    navigate(`/protocols/${protocolKey}/visualization`)
   }
 
   const UNKNOWN_ATTACHMENT_ERROR = `${protocolDisplayName} protocol uses
@@ -514,13 +514,12 @@ export function ProtocolDetails(
                   </LegacyStyledText>
                 </Flex>
                 <Flex gridGap={SPACING.spacing4} justifySelf={JUSTIFY_END}>
-                  {/* TODO: add OT-2 support */}
-                  {enableProtocolTimeline && robotType === FLEX_ROBOT_TYPE ? (
+                  {enableProtocolTimeline ? (
                     <SecondaryButton
                       onClick={handleClickTimeline}
                       cursor={CURSOR_POINTER}
                     >
-                      Preview
+                      {t('visualize')}
                     </SecondaryButton>
                   ) : null}
                   <PrimaryButton
