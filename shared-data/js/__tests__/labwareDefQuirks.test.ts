4618b0030b92a387799d01fdb4d1004faa69703c
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-09-29 17:25:16 -0400

    correct quirk placement

--- shared-data/js/__tests__/labwareDefQuirks.test.ts
+++ shared-data/js/__tests__/labwareDefQuirks.test.ts
@@ -15,6 +15,7 @@ const EXPECTED_VALID_QUIRKS = [
   'tiprackAdapterFor96Channel',
   'stackingMaxFive',
   'stackingOnly',
+  'noLabwarePositionCheck'
 ]
 
 describe('check quirks for all labware defs', () => {
