a8190b22479b202efc967f3577793d59210b3c12
koji <baxin1919@gmail.com>
2025-09-30 18:36:25 -0400

    fix lint errors

--- components/src/molecules/TimelineScrubber/TimelineScrubber.stories.tsx
+++ components/src/molecules/TimelineScrubber/TimelineScrubber.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { action } from '@storybook/addon-actions'
 
 import { TimelineScrubber as TimelineScrubberComponent } from './index'
