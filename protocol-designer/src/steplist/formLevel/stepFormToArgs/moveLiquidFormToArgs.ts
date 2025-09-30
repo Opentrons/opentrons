157c260fdaba089333939f2305eca69f335e9d9b
Jethary <jethary@opentrons.com>
2025-09-30 08:14:52 -0700

    add to release notes

--- protocol-designer/src/steplist/formLevel/stepFormToArgs/moveLiquidFormToArgs.ts
+++ protocol-designer/src/steplist/formLevel/stepFormToArgs/moveLiquidFormToArgs.ts
@@ -183,7 +183,6 @@ export const moveLiquidFormToArgs = (
     hydratedFormData.aspirate_wellOrder_first,
     hydratedFormData.aspirate_wellOrder_second
   )
-  console.log('form to args tiprack', tipRack)
 
   const isDispensingIntoDisposalLocation =
     'name' in destLabware &&
