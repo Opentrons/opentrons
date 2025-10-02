0d02e2d2694780e9d72a73a46623f0fbc110f177
Hovan Ngo <hovan.ngo@opentrons.com>
2025-10-02 11:10:49 -0400

    added corning falcon 384 lid definition. Added back some labware def keys because ithey didn't work without them

--- abr-testing/abr_testing/protocols/test_protocols/labware_def_checker.py
+++ abr-testing/abr_testing/protocols/test_protocols/labware_def_checker.py
@@ -20,8 +20,8 @@ def add_parameters(parameters: ParameterContext) -> None:
         description="Type of Labware",
         choices=[
             {
-                "display_name": "Axygen 500 ul Well Plate",
-                "value": "axygen_96_wellplate_500ul",
+                "display_name": "falcon 384 lid",
+                "value": "corning_falcon_384_wellplate_130ul_flat_lid",
             },
             {
                 "display_name": "Corn96Wellplate 360 ul Lid",
@@ -37,7 +37,7 @@ def add_parameters(parameters: ParameterContext) -> None:
                 "value": "ibidi_96_square_well_plate_300ul_lid",
             },
         ],
-        default="ibidi_96_square_well_plate_300ul_lid",
+        default="corning_falcon_384_wellplate_130ul_flat_lid",
     )
     parameters.add_bool(
         variable_name="heater_shaker",
@@ -52,48 +52,50 @@ def run(protocol: ProtocolContext) -> None:
     labware_type = protocol.params.labware_type  # type: ignore[attr-defined]
     heater_shaker_enabled = protocol.params.heater_shaker  # type: ignore[attr-defined]
 
+    tip_rack = protocol.load_labware("opentrons_flex_96_tiprack_50ul", "D2")
+    pipette = protocol.load_instrument(
+        "flex_1channel_50", "left", tip_racks=[tip_rack]
+    )
+    deck_riser = protocol.load_adapter("opentrons_flex_deck_riser", "B3")
+
+    lid = protocol.load_lid_stack(labware_type, "C3", 1)
+    labware = protocol.load_labware("corning_falcon_384_wellplate_130ul_flat", "D3")
+
+
     # Lid loading
-    if labware_type == "ibidi_96_square_well_plate_300ul_lid":
-        protocol.load_lid_stack(labware_type, "C3", 1)
-        labware = protocol.load_labware("ibidi_96_square_well_plate_300ul", "D3")
-        for _ in range(3):
-            protocol.move_lid("C3", labware, use_gripper=True)
-            protocol.move_lid(labware, "C3", use_gripper=True)
-    elif labware_type == "corning_96_wellplate_360ul_lid":
-        protocol.load_lid_stack(labware_type, "C3", 1)
-        labware = protocol.load_labware("corning_96_wellplate_360ul_flat", "D3")
-        for _ in range(3):
-            protocol.move_lid("C3", labware, use_gripper=True)
-            protocol.move_lid(labware, "C3", use_gripper=True)
-    else:
-        labware = protocol.load_labware(labware_type, "D3")
-        # load pipette and tip rack
-        tip_rack = protocol.load_labware("opentrons_flex_96_tiprack_200ul", "D2")
-        pipette = protocol.load_instrument(
-            "flex_8channel_1000", "left", tip_racks=[tip_rack]
-        )
+    #if labware_type == "corning_falcon_384_wellplate_130ul_flat_lid":
+    #    for _ in range(2):
+    #        protocol.move_lid("C3", labware, use_gripper=True)
+    #        protocol.move_lid(labware, deck_riser, use_gripper=True)
+    #        protocol.move_lid(deck_riser, "C3", use_gripper=True)
+    #else:
+    #    labware = protocol.load_labware(labware_type, "D3")
 
-        # pick up tip
-        pipette.pick_up_tip()
 
-        # move tip to positions
-        pipette.move_to(labware["A6"].top(z=1))
-        protocol.pause("Check top height with shim")
-        pipette.move_to(labware["A6"].bottom(z=1))
-        protocol.pause("Check bottom height with shim")
+    # pick up tip
+    pipette.pick_up_tip()
+
+    # move tip to positions
+    pipette.move_to(labware["A6"].top(z=1))
+    protocol.pause("Check top height with shim")
+    pipette.move_to(labware["A6"].bottom(z=1))
+    protocol.pause("Check bottom height with shim")
 
         # Optional heater shaker use
-        if heater_shaker_enabled:
-            hsh: HeaterShakerContext = protocol.load_module(
-                "heaterShakerModuleV1", "D1"
-            )  # type: ignore[assignment]
-            hsh.close_labware_latch()
-            adapter = hsh.load_adapter("opentrons_universal_flat_adapter")
-            hsh.open_labware_latch()
-            protocol.move_labware(labware, adapter, use_gripper=True)
-            hsh.close_labware_latch()
 
-            pipette.move_to(labware["A6"].top(z=1))
-            protocol.pause("Check top height with shim")
-
+    if heater_shaker_enabled:
+        hsh: HeaterShakerContext = protocol.load_module(
+            "heaterShakerModuleV1", "D1"
+        )  # type: ignore[assignment]
+        hsh.close_labware_latch()
+        adapter = hsh.load_adapter("opentrons_universal_flat_adapter")
+        hsh.open_labware_latch()
+        protocol.move_labware(labware, adapter, use_gripper=True)
+        hsh.close_labware_latch()
+        pipette.move_to(labware["A1"].top(z=1))
+        protocol.pause("Check top height with shim on heater shaker")
+        protocol.move_lid("C3", labware, use_gripper=True)
+        protocol.move_lid(labware, deck_riser, use_gripper=True)
+        protocol.move_lid(deck_riser, labware, use_gripper=True)
         pipette.return_tip()
+        hsh.close_labware_latch()
