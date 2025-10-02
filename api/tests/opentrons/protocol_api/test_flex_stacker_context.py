df3f9539b2bc2fa848d22253afa1aa627cd5e72a
Max Marrone <max@opentrons.com>
2025-10-02 17:46:12 -0400

    Three test.

--- api/tests/opentrons/protocol_api/test_flex_stacker_context.py
+++ api/tests/opentrons/protocol_api/test_flex_stacker_context.py
@@ -17,7 +17,7 @@ from opentrons.protocol_api.core.common import (
 )
 from opentrons.protocol_api.core.core_map import LoadedCoreMap
 
-from . import versions_at_or_above, versions_between
+from . import versions_at_or_above, versions_below, versions_between
 
 
 @pytest.fixture
@@ -175,7 +175,7 @@ def test_set_stored_labware(
         low_inclusive_bound=APIVersion(2, 25), high_exclusive_bound=APIVersion(2, 26)
     ),
 )
-def test_set_stored_labware_namespace_version_param_minimum_version(
+def test_set_stored_labware_namespace_version_params_minimum_version(
     subject: FlexStackerContext,
 ) -> None:
     """{adapter,lid}_{namespace,version} params were unavailable before version 2.26."""
@@ -223,6 +223,48 @@ def test_set_stored_labware_namespace_version_param_minimum_version(
             stacking_offset_z=1.0,
             lid_version=123,
         )
+
+
+@pytest.mark.parametrize(
+    "api_version",
+    versions_between(
+        low_inclusive_bound=APIVersion(2, 25), high_exclusive_bound=APIVersion(2, 26)
+    ),
+)
+def test_set_stored_labware_namespace_version_params_legacy_behavior(
+    decoy: Decoy,
+    mock_core: FlexStackerCore,
+    subject: FlexStackerContext,
+) -> None:
+    """Test legacy (version <2.26) behavior when the {adapter,lid}_{namespace,version} params were omitted.
+
+    The adapter and lid namespace and version were taken from the main labware's namespace and version.
+    """
+    subject.set_stored_labware(
+        load_name=sentinel.main_load_name,
+        namespace=sentinel.main_namespace,
+        version=sentinel.main_version,
+        adapter=sentinel.adapter_load_name,
+        lid=sentinel.lid_load_name,
+        count=sentinel.count,
+        stacking_offset_z=sentinel.stacking_offset_z,
+    )
+
+    decoy.verify(
+        mock_core.set_stored_labware(
+            main_load_name=sentinel.main_load_name,
+            main_namespace=sentinel.main_namespace,
+            main_version=sentinel.main_version,
+            lid_load_name=sentinel.lid_load_name,
+            lid_namespace=sentinel.main_namespace,
+            lid_version=sentinel.main_version,
+            adapter_load_name=sentinel.adapter_load_name,
+            adapter_namespace=sentinel.main_namespace,
+            adapter_version=sentinel.main_version,
+            count=sentinel.count,
+            stacking_offset_z=sentinel.stacking_offset_z,
+        )
+    )
 
 
 @pytest.mark.parametrize(
