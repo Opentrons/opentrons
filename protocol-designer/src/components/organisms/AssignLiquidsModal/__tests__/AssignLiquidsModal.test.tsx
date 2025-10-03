7259e92f7b39b933ac31aac14df80dedbfb5f48d
Tamar Zanzouri <zanzouritamar@gmail.com>
2025-10-03 16:24:58 -0400

    mocking first selector

--- protocol-designer/src/components/organisms/AssignLiquidsModal/__tests__/AssignLiquidsModal.test.tsx
+++ protocol-designer/src/components/organisms/AssignLiquidsModal/__tests__/AssignLiquidsModal.test.tsx
@@ -1,17 +1,18 @@
 import { screen } from '@testing-library/react'
-import { beforeEach, describe, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 
-import { renderWithProviders } from '/protocol-designer/__testing-utils__'
-import { i18n } from '/protocol-designer/assets/localization'
+import { renderWithProviders } from '/app/__testing-utils__'
+import { i18n } from '/app/i18n'
 
-import { AssignLiquidsModal } from '../..'
+import { selectors as labwareIngredSelectors } from '/protocol-designer/labware-ingred/selectors'
 
+import type { Mock } from 'vitest'
 import type { ComponentProps } from 'react'
-import { useSelector } from 'react-redux'
-import { fixture96Plate, LabwareDefinition2 } from '@opentrons/shared-data'
+import { AssignLiquidsModal } from '..'
 import { getInitialDeckSetup } from '/protocol-designer/step-forms/selectors'
+import { fixture96Plate, LabwareDefinition2 } from '@opentrons/shared-data'
+import { useSelector } from 'react-redux'
 
-vi.mock('/protocol-designer/step-forms/selectors')
 vi.mock('react-redux', async () => {
     const actual = await vi.importActual('react-redux')
     return {
@@ -19,79 +20,94 @@ vi.mock('react-redux', async () => {
       useSelector: vi.fn(),
     }
   })
+vi.mock('/protocol-designer/labware-ingred/selectors', async (importOriginal) => {
+    const actual = await importOriginal()
+    return {
+      ...actual,
+      getSelectedLabwareId: vi.fn()
+    }
+  })
+vi.mock('/protocol-designer/step-forms/selectors', async (importOriginal) => {
+    const actual = await importOriginal()
+    return {
+      ...actual,
+      getInitialDeckSetup: vi.fn()
+    }
+  })
+// vi.mock('/protocol-designer/top-selectors/labware-locations', async (importOriginal) => {
+//     const actual = await importOriginal()
+//     return {
+//       ...actual,
+//       getDeckSetupForActiveItem: vi.fn()
+//     }
+//   })
 
-const render = (props: ComponentProps<typeof AssignLiquidsModal>) => {
+const render = (
+  props: ComponentProps<typeof AssignLiquidsModal>,
+) => {
+
   return renderWithProviders(<AssignLiquidsModal {...props} />, {
     i18nInstance: i18n,
-  })
+  })[0]
 }
-
-const mockAppState = {
-    labwareInvariantProperties:
-      {
-        mockLabwareId: {
-        stack: ['mockLabwareId'],
-        id: 'mockLabwareId',
-        labwareDefURI: 'mockUri',
-        def: fixture96Plate as LabwareDefinition2,
-        pythonName: 'mockPythonName',
-      },
-    },
-    containers: {
-      mockLabwareId: {
-        stack: ['mockLabwareId'],
-        id: 'mockLabwareId',
-        labwareDefURI: 'mockUri',
-        def: fixture96Plate as LabwareDefinition2,
-        pythonName: 'mockPythonName',
-      },
-    },
-    // ... other parts of your state
-  };
 
 describe('AssignLiquidsModal', () => {
   let props: ComponentProps<typeof AssignLiquidsModal>
+  let mockShowLiquidOverflowMenu: Mock
+  let mockSetDefineLiquidModal: Mock
+
   beforeEach(() => {
+    mockShowLiquidOverflowMenu = vi.fn()
+    mockSetDefineLiquidModal = vi.fn()
+
+    vi.fn(
+        labwareIngredSelectors.getSelectedLabwareId
+      ).mockImplementation(() => 'mockLabwareId')
+
+    vi.fn(
+      getInitialDeckSetup
+    ).mockImplementation(() => {
+        return {
+          labware: {
+            mockLabwareId: {
+              stack: ['mockLabwareId'],
+              id: 'mockLabwareId',
+              labwareDefURI: 'mockUri',
+              def: fixture96Plate as LabwareDefinition2,
+              pythonName: 'mockPythonName',
+            },
+          },
+        }
+    })
     props = {
-      showLiquidOverflowMenu: vi.fn(),
-      setDefineLiquidModal: vi.fn(),
-    }
-    
-    vi.mocked(useSelector).mockImplementation(selector => selector(mockAppState));
-    // vi.mocked(useSelector).mockReturnValue({
-    //     labware: {
-    //       mockLabwareId: {
-    //         stack: ['mockLabwareId'],
-    //         id: 'mockLabwareId',
-    //         labwareDefURI: 'mockUri',
-    //         def: fixture96Plate as LabwareDefinition2,
-    //         pythonName: 'mockPythonName',
-    //     },    
-    //   },
-    //   labwareId: 'mockLabwareId',
-    //   selectedLabware: 'mockLabwareId',
-    //   pipettes: {},
-    //   modules: {},
-    //   additionalEquipmentOnDeck: {},
-    // })
-    // vi.mocked(getInitialDeckSetup).mockReturnValue({
-    //     labware: {
-    //       mockLabwareId: {
-    //         stack: ['mockLabwareId'],
-    //         id: 'mockLabwareId',
-    //         labwareDefURI: 'mockUri',
-    //         def: fixture96Plate as LabwareDefinition2,
-    //         pythonName: 'mockPythonName',
-    //       },
-    //     },
-    //     pipettes: {},
-    //     additionalEquipmentOnDeck: {},
-    //     modules: {},
-    //   })
+        showLiquidOverflowMenu: vi.fn(),
+        setDefineLiquidModal: vi.fn(),
+      }
+      vi.fn(useSelector).mockReturnValue({
+        labwareInvariantProperties: {
+          mockLabwareId: {
+            stack: ['mockLabwareId'],
+            id: 'mockLabwareId',
+            labwareDefURI: 'mockUri',
+            def: fixture96Plate as LabwareDefinition2,
+          },
+        },
+        pipettes: {},
+        modules: {},
+        additionalEquipmentOnDeck: {},
+      });
   })
 
-  it('should render text', () => {
+  it('loads the modal', () => {
     render(props)
-    screen.getByText('mock AssignLiquidsModal')
+
+    const header = screen.getByTestId('header-prop')
+    expect(header).toHaveTextContent('Assign Liquids')
+
+    const primaryButton = screen.getByTestId('primary-button')
+    expect(primaryButton).toHaveAttribute('data-button-text', 'Continue')
+
+    const secondaryButton = screen.getByTestId('secondary-button')
+    expect(secondaryButton).toHaveAttribute('data-text', 'Exit')
   })
 })
