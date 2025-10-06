e3c1e3e10a3cca4034281c36e23ba9e0b73700c5
Ryan Howard <ryan.howard@opentrons.com>
2025-10-06 15:44:24 -0400

    add power to the tc graphing script.

--- hardware-testing/hardware_testing/scripts/monitor_thermocycler.py
+++ hardware-testing/hardware_testing/scripts/monitor_thermocycler.py
@@ -48,6 +48,19 @@ class DebugPlateTemperature:
     back_center_adc: float
 
 
+@dataclass
+class DebugPower:
+    """All the data returned by get power debug gcode message."""
+
+    left: float
+    center: float
+    right: float
+    heater: float
+    fan: float
+    tach1: float
+    tach2: float
+
+
 def parse_temperature_response_debug(temp_str: str) -> DebugPlateTemperature:
     """Parse debug resposne example below.
 
@@ -89,6 +102,23 @@ def parse_temperature_response_debug(temp_str: str) -> DebugPlateTemperature:
     )
 
 
+def parse_power_response_debug(pwr_str: str) -> DebugPower:
+    """Parse debug resposne example below.
+
+    "M103.D L:%0.2f C:%0.2f R:%0.2f H:%0.2f F:%0.2f T1:%3.2f T2:%3.2f OK",
+    """
+    values = parse_key_values(pwr_str)
+    left = parse_number(values["L"], TC_GCODE_ROUNDING_PRECISION)
+    center = parse_number(values["C"], TC_GCODE_ROUNDING_PRECISION)
+    right = parse_number(values["R"], TC_GCODE_ROUNDING_PRECISION)
+    heater = parse_number(values["H"], TC_GCODE_ROUNDING_PRECISION)
+    fan = parse_number(values["F"], TC_GCODE_ROUNDING_PRECISION)
+    tach1 = parse_number(values["T1"], TC_GCODE_ROUNDING_PRECISION)
+    tach2 = parse_number(values["T2"], TC_GCODE_ROUNDING_PRECISION)
+
+    return DebugPower(left, right, center, heater, fan, tach1, tach2)
+
+
 async def _get_temperature_debug(
     connection: ThermocyclerDriverV2,
 ) -> DebugPlateTemperature:
@@ -99,6 +129,16 @@ async def _get_temperature_debug(
     return parse_temperature_response_debug(response)
 
 
+async def _get_power_debug(
+    connection: ThermocyclerDriverV2,
+) -> DebugPower:
+    c = CommandBuilder(terminator=TC_COMMAND_TERMINATOR).add_gcode(gcode="M103.D")
+    response = await connection._connection.send_command(
+        command=c, retries=DEFAULT_COMMAND_RETRIES
+    )
+    return parse_power_response_debug(response)
+
+
 async def _loop_til_temp(
     connection: ThermocyclerDriverV2, set_temp: float, ramp_rate: Optional[float]
 ) -> None:
@@ -107,11 +147,15 @@ async def _loop_til_temp(
     await connection.set_plate_temperature(set_temp, ramp_rate=ramp_rate)
     start = time.time()
     status.update(await connection.get_plate_temperature())
-    debug_info: List[Tuple[DebugPlateTemperature, float]] = []
+    temp_debug_info: List[Tuple[DebugPlateTemperature, float]] = []
+    power_debug_info: List[Tuple[DebugPower, float]] = []
     time_to_hit = 0.0
-    plt.suptitle("Temperature.")
-    ax = plt.gca()
-    ax.set_ylim(BLOCK_TARGET_MIN - 5, BLOCK_TARGET_MAX + 5)
+    max_displacement = start_temp.current
+    fig, axs = plt.subplots(2)
+    temp_ax = axs[0]
+    power_ax = axs[1]
+    temp_ax.set_title("Temperature")
+    power_ax.set_title("Power")
     if ramp_rate is not None:
         theoretical_time = abs(start_temp.current - set_temp) / ramp_rate
     else:
@@ -120,14 +164,20 @@ async def _loop_til_temp(
         status.status is TemperatureStatus.COOLING
         or status.status is TemperatureStatus.HEATING
     ):
-        debug_info.append((await _get_temperature_debug(connection), time.time()))
+        time_this = time.time()
+        temp_debug_info.append((await _get_temperature_debug(connection), time_this))
+        power_debug_info.append((await _get_power_debug(connection), time_this))
         next_temp = await connection.get_plate_temperature()
         print(f"target {set_temp} current {next_temp.current}")
         if abs(set_temp - next_temp.current) < 1 and time_to_hit == 0.0:
             print("Hit it")
             time_to_hit = time.time() - start
+        if status.status is TemperatureStatus.COOLING:
+            max_displacement = min(max_displacement, next_temp.current)
+        else:
+            max_displacement = max(max_displacement, next_temp.current)
         status.update(next_temp)
-        times = [d[1] - start for d in debug_info]
+        times = [d[1] - start for d in temp_debug_info]
         ramp_factor = (
             -1 if start_temp.current > set_temp and ramp_rate is not None else 1
         )
@@ -139,28 +189,49 @@ async def _loop_til_temp(
             if t <= theoretical_time
         ]
         target = [set_temp for _ in range(len(times))]
-        # hs = [d[0].heatsink for d in debug_info]
-        fr = [d[0].front_right for d in debug_info]
-        fl = [d[0].front_left for d in debug_info]
-        fc = [d[0].front_center for d in debug_info]
-        br = [d[0].back_right for d in debug_info]
-        bl = [d[0].back_left for d in debug_info]
-        bc = [d[0].back_center for d in debug_info]
+        # hs = [d[0].heatsink for d in temp_debug_info]
+        fr = [d[0].front_right for d in temp_debug_info]
+        fl = [d[0].front_left for d in temp_debug_info]
+        fc = [d[0].front_center for d in temp_debug_info]
+        br = [d[0].back_right for d in temp_debug_info]
+        bl = [d[0].back_left for d in temp_debug_info]
+        bc = [d[0].back_center for d in temp_debug_info]
 
-        plt.plot(times, target, linestyle=":")
+        left = [d[0].left for d in power_debug_info]
+        center = [d[0].center for d in power_debug_info]
+        right = [d[0].right for d in power_debug_info]
+        # heater = [d[0].heater for d in power_debug_info]
+        # fan = [d[0].fan for d in power_debug_info]
+        # tach1 = [d[0].tach1 for d in power_debug_info]
+        # tach2 = [d[0].tach2 for d in power_debug_info]
+
+        temp_ax.clear()
+        power_ax.clear()
+
+        temp_ax.set_ylim(BLOCK_TARGET_MIN - 5, BLOCK_TARGET_MAX + 5)
+        power_ax.set_ylim(-1, 1)
+
+        temp_ax.plot(times, target, linestyle=":")
         # plt.plot(times, hs)
-        plt.plot(times, fr)
-        plt.plot(times, fl)
-        plt.plot(times, fc)
-        plt.plot(times, bl)
-        plt.plot(times, br)
-        plt.plot(times, bc)
+        temp_ax.plot(times, fr, label="Front right")
+        temp_ax.plot(times, fl, label="Front left")
+        temp_ax.plot(times, fc, label="Front center")
+        temp_ax.plot(times, bl, label="back left")
+        temp_ax.plot(times, br, label="back right")
+        temp_ax.plot(times, bc, label="back center")
+
+        power_ax.plot(times, left, label="Left")
+        power_ax.plot(times, center, label="Center")
+        power_ax.plot(times, right, label="Right")
+
         abbrv_times = times[: len(ideal)]
-        plt.plot(abbrv_times, ideal, linestyle="--")
+        temp_ax.plot(abbrv_times, ideal, linestyle="--")
+        temp_ax.legend()
+        power_ax.legend()
         plt.pause(0.05)
 
     print(
-        f"Start temp {start_temp.current} time to hit it {time_to_hit} effective ramp rate {abs((start_temp.current - set_temp) / time_to_hit)}"
+        f"Start temp {start_temp.current} time to hit it {time_to_hit} effective ramp rate {abs((start_temp.current - set_temp) / time_to_hit)} overshoot {abs(max_displacement - set_temp)}"
     )
     plt.show()
 
