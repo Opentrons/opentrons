afb87e48c3bd293e8d28c3f146f2476c3f129fc5
ncdiehl11 <ndiehl@opentrons.com>
2025-10-07 12:51:39 -0400

    refactor TiprackSelectHover styling to css modules

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/TiprackSelectHover.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/TiprackSelectHover.tsx
@@ -1,15 +1,8 @@
-import {
-  ALIGN_CENTER,
-  BORDERS,
-  COLORS,
-  CURSOR_POINTER,
-  Flex,
-  JUSTIFY_CENTER,
-  RobotCoordsForeignDiv,
-  SPACING,
-} from '@opentrons/components'
+import { RobotCoordsForeignDiv } from '@opentrons/components'
 
-import { DECK_CONTROLS_STYLE } from '/protocol-designer/pages/Designer/DeckSetup/constants'
+import { DECK_CONTROLS_STYLE_BASE } from '/protocol-designer/pages/Designer/DeckSetup/constants'
+
+import styles from './tipselectionwizard.module.css'
 
 import type { Dispatch, SetStateAction } from 'react'
 import type { CoordinateTuple } from '@opentrons/shared-data'
@@ -25,25 +18,13 @@ interface TiprackSelectHoverProps {
 export function TiprackSelectHover(
   props: TiprackSelectHoverProps
 ): JSX.Element {
-  const slotFill = (
-    <Flex
-      alignItems={ALIGN_CENTER}
-      backgroundColor={`${COLORS.black90}cc`}
-      borderRadius={BORDERS.borderRadius4}
-      color={COLORS.white}
-      gridGap={SPACING.spacing8}
-      justifyContent={JUSTIFY_CENTER}
-      width="100%"
-      height="100%"
-    />
-  )
+  const slotFill = <div className={styles.slot_fill} />
 
   const { slotPosition, setHover, labware, onClick } = props
   const { def } = labware
   const { xDimension, yDimension } = def.dimensions
   const { cornerOffsetFromSlot } = def
   const { x: xOffset, y: yOffset } = cornerOffsetFromSlot
-  const opacity = 0
   return (
     <RobotCoordsForeignDiv
       {...{
@@ -52,13 +33,7 @@ export function TiprackSelectHover(
         width: xDimension,
         height: yDimension,
       }}
-      innerDivProps={{
-        opacity: opacity,
-        ...DECK_CONTROLS_STYLE,
-        zIndex: 20,
-        cursor: CURSOR_POINTER,
-        backgroundColor: 'black',
-      }}
+      innerDivProps={DECK_CONTROLS_STYLE_BASE}
       innerDivEvents={{
         onMouseEnter: () => {
           setHover(labware.id)
