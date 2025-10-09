373294dfc3415d4991120b6c6798340b7fbe3023
Josh McVey <josh.mcvey@opentrons.com>
2025-10-09 08:34:39 -0500

    fix(scripts/static-deploy): update CF id for docs

--- scripts/static-deploy/deploy_config.py
+++ scripts/static-deploy/deploy_config.py
@@ -152,13 +152,13 @@ def get_deploy_config() -> DeployConfig:
         docs=ApplicationConfig(
             name="docs",
             s3_bucket="opentrons.staging.docs",
-            cloudfront_id="E8IWASMDOWHYP",
+            cloudfront_id="E2DBE0K9VT8YB9",
             url="https://staging.docs.opentrons.com/",
         ),
         mkdocs=ApplicationConfig(
             name="mkdocs",
             s3_bucket="opentrons.staging.docs",
-            cloudfront_id="E8IWASMDOWHYP",
+            cloudfront_id="E2DBE0K9VT8YB9",
             url="https://staging.docs.opentrons.com/",
         ),
     )
@@ -180,13 +180,13 @@ def get_deploy_config() -> DeployConfig:
         docs=ApplicationConfig(
             name="docs",
             s3_bucket="opentrons.production.docs",
-            cloudfront_id="E16BZZXDTINN0S",
+            cloudfront_id="E2PSPUXND1RQWG",
             url="https://docs.opentrons.com/",
         ),
         mkdocs=ApplicationConfig(
             name="mkdocs",
             s3_bucket="opentrons.production.docs",
-            cloudfront_id="E16BZZXDTINN0S",
+            cloudfront_id="E2PSPUXND1RQWG",
             url="https://docs.opentrons.com/",
         ),
     )
