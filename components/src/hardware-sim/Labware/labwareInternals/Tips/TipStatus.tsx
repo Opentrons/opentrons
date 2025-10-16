32b2731b49538042971eb98a51af43eba36f91f3
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2025-10-16 10:45:26 -0400

    feat(components, protocol-designer): tip selection pt. 2 (#19792)
    This PR continues the implementation of tip selection functionality in
    Protocol Designer. It enhances how tip states are visually represented
    and selected within the UI, expanding from static rendering to
    interactive tip selection and highlighting.

--- components/src/hardware-sim/Labware/labwareInternals/Tips/TipStatus.tsx
+++ components/src/hardware-sim/Labware/labwareInternals/Tips/TipStatus.tsx
@@ -1,3 +1,11 @@
+import {
+  INACCESSIBLE,
+  NEW,
+  NO,
+  SELECTED,
+  SELECTED_USED,
+  USED,
+} from './constants'
 import { InaccessibleTip } from './InaccessibleTip'
 import { NewTip } from './NewTip'
 import { NoTip } from './NoTip'
@@ -9,18 +17,21 @@ import type { TipType } from '../types'
 export function TipStatus(props: {
   type: TipType
   size?: string
+  text?: string
 }): JSX.Element {
-  const { type, size } = props
+  const { type, size, text } = props
   switch (type) {
-    case 'new':
+    case NEW:
       return <NewTip size={size} />
-    case 'used':
+    case USED:
       return <UsedTip size={size} />
-    case 'selected':
-      return <SelectedTip size={size} />
-    case 'no':
+    case SELECTED:
+      return <SelectedTip size={size} textInsideTip={text} />
+    case NO:
       return <NoTip size={size} />
-    case 'inaccessible':
+    case INACCESSIBLE:
       return <InaccessibleTip size={size} />
+    case SELECTED_USED:
+      return <SelectedTip size={size} textInsideTip={text} isUsed />
   }
 }
