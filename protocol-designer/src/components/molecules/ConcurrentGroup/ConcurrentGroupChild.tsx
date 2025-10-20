8bfbe9e995289ab2825d5f1cd937f36f5b1add59
Max Marrone <max@opentrons.com>
2025-10-20 11:20:34 -0400

    refactor: Let ConcurrentGroupStepContainer wrap around arbitrary children.
    We need this because `StepContainer`s and `ConcurrentGroupCheckpoint`s can't necessarily be the direct children. For technical reasons, the drag-n-drop code often wants to wrap them in divs and such.

---
+++ protocol-designer/src/components/molecules/ConcurrentGroup/ConcurrentGroupChild.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx'
+
+import styles from './childrencommon.module.css'
+
+import type { PropsWithChildren } from 'react'
+
+interface ConcurrentGroupChildProps {
+  type: 'step' | 'checkpoint'
+}
+
+/**
+ * This wrapper component implements spacing between child elements of a
+ * `<ConcurrentGroup>`.
+ *
+ * Example:
+ *
+ * ```
+ * <ConcurrentGroup>
+ *   <ConcurrentGroupChild type="checkpoint">
+ *     <ConcurrentGroupCheckpoint />
+ *   </ConcurrentGroupChild>
+ *   <ConcurrentGroupChild type="step">
+ *     <StepContainer />
+ *   </ConcurrentGroupChild>
+ * </ConcurrentGroup>
+ * ```
+ */
+export function ConcurrentGroupChild(
+  props: PropsWithChildren<ConcurrentGroupChildProps>
+): JSX.Element {
+  const { type, children } = props
+  return (
+    <li
+      className={clsx(
+        type === 'step' && styles.step_container_spacing,
+        type === 'checkpoint' && styles.checkpoint_spacing,
+        styles.no_li_bullet
+      )}
+    >
+      {children}
+    </li>
+  )
+}
--- protocol-designer/src/components/molecules/ConcurrentGroup/__tests__/ConcurrentGroupStepContainer.test.tsx
+++ protocol-designer/src/components/molecules/ConcurrentGroup/__tests__/ConcurrentGroupStepContainer.test.tsx
@@ -4,30 +4,20 @@ import { describe, expect, it } from 'vitest'
 import { renderWithProviders } from '/protocol-designer/__testing-utils__'
 import { i18n } from '/protocol-designer/assets/localization'
 
-import { ConcurrentGroupStepContainer } from '../ConcurrentGroupStepContainer'
+import { ConcurrentGroupChild } from '../ConcurrentGroupChild'
 
 import type { ComponentProps } from 'react'
 
-const render = (props: ComponentProps<typeof ConcurrentGroupStepContainer>) => {
-  return renderWithProviders(<ConcurrentGroupStepContainer {...props} />, {
+const render = (props: ComponentProps<typeof ConcurrentGroupChild>) => {
+  return renderWithProviders(<ConcurrentGroupChild {...props} />, {
     i18nInstance: i18n,
   })
 }
 
-describe('ConcurrentGroupStepContainer', () => {
-  it('renders a StepContainer inside a list item', () => {
-    render({
-      stepNumber: null,
-      text: 'Test step text',
-      iconName: 'transfer',
-      type: 'default',
-      size: 'iconAndText',
-      active: false,
-      error: false,
-      hover: false,
-      semiTransparent: false,
-      cursor: 'default',
-    })
+describe('ConcurrentGroupChild', () => {
+  it('renders a child component inside a list item', () => {
+    const child = <p>Test step text</p>
+    render({ type: 'step', children: child })
 
     const listItem = screen.getByRole('listitem')
     expect(within(listItem).getByText('Test step text')).toBeInTheDocument()
