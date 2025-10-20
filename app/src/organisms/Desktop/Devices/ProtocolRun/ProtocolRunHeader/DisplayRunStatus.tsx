08a0cfdf813762703349ce80659f8a6d855129ca
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-10-20 14:00:21 -0400

    fix(app): correct variable typing

--- app/src/organisms/Desktop/Devices/ProtocolRun/ProtocolRunHeader/DisplayRunStatus.tsx
+++ app/src/organisms/Desktop/Devices/ProtocolRun/ProtocolRunHeader/DisplayRunStatus.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { RUN_STATUS_AWAITING_RECOVERY } from '@opentrons/api-client'
 import { ALIGN_CENTER, Chip, Flex } from '@opentrons/components'
 
+import type { TFunction } from 'i18next'
 import type { RunStatus } from '@opentrons/api-client'
 import type { ChipType, IconName } from '@opentrons/components'
 
@@ -13,7 +14,7 @@ interface DisplayRunStatusProps {
 // Styles the run status chip.
 const getRunStatusChip = (
   runStatus: RunStatus | null,
-  t: any
+  t: TFunction
 ): [ChipType, boolean, boolean, string, IconName?] => {
   let text: string
   if (runStatus === RUN_STATUS_AWAITING_RECOVERY) {
@@ -44,7 +45,7 @@ export function DisplayRunStatus(props: DisplayRunStatusProps): JSX.Element {
   const { t } = useTranslation('run_details')
   const [chipType, icon, pulse, statusText, iconName] = getRunStatusChip(
     props.runStatus,
-    t
+    t as TFunction
   )
   return (
     <Flex alignItems={ALIGN_CENTER}>
