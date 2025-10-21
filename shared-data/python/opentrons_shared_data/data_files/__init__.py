3836274dff734596cc349b6d9dc1f17eef72fd88
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-10-21 14:16:11 -0400

    fix(api, robot-server, shared-data): inject run metadata into `FileProvider` (#19851)
    Works towards EXEC-1978
    
    When FileProvider is instantiated, it captures a bound method reference to FileProviderExecutor's callback. Any subsequent state changes to the FileProviderExecutor instance won't be reflected when the callback executes, since the method binding happened at instantiation time. To fix, let's just keep the run-related state on the FileProvider, since the updated instance of FileProvider is injected into PE on each run.

--- shared-data/python/opentrons_shared_data/data_files/__init__.py
+++ shared-data/python/opentrons_shared_data/data_files/__init__.py
@@ -8,6 +8,7 @@ from .types import (
     MimeType,
     IODataFileInfo,
     DataFileSource,
+    RunFileNameMetadata,
 )
 
 
@@ -19,4 +20,5 @@ __all__ = [
     "CmdDataFileInfo",
     "MimeType",
     "DataFileSource",
+    "RunFileNameMetadata",
 ]
