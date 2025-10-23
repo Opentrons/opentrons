2102fb0a5dc285d21c20c845672d4d9d96017ada
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2025-10-23 13:00:52 -0400

    feat(protocol-designer): create and wire up tip selection for OT-2 (#19885)
    This PR adds PipetteShadow components and correct deck svg for OT-2
    tiprack and tip selection. It also fixes a step navigation issue for
    OT-2 moveLiquid and mix tools
    
    Note that there is a bug in the OT-2 single-channel shared pipe type
    definitions such that their SVGs have 0 height and 0 width. This will be
    addressed separately.

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/SelectTips.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/SelectTips.tsx
@@ -23,13 +23,14 @@ import {
 import { EMPTY, getSlotInLocationStack } from '@opentrons/step-generation'
 
 import { LabwareOnDeck } from '/protocol-designer/components/organisms'
+import { getRobotType } from '/protocol-designer/file-data/selectors'
 import { getRobotStateAtActiveItem } from '/protocol-designer/top-selectors/labware-locations'
 import { getLabwareNicknamesById } from '/protocol-designer/ui/labware/selectors'
 
 import { BaseDeckTipSelection } from './BaseDeckTipSelection'
 import { TIP_STATE_TO_TIP_TYPE } from './constants'
 import { DeckOverlay } from './DeckOverlay'
-import { PipetteShadow } from './PipetteShadows/PipetteFlexShadow'
+import { PipetteShadow } from './PipetteShadows/PipetteShadow'
 import { TipLegend } from './TipLegend'
 import styles from './tipselectionwizard.module.css'
 import {
@@ -62,6 +63,7 @@ export function SelectTips(
 ): JSX.Element {
   const { t } = useTranslation('tip_selection')
   const labwareNicknamesById = useSelector(getLabwareNicknamesById)
+  const robotType = useSelector(getRobotType)
   const [hoveredWell, setHoveredWell] = useState<string | null>(null)
   const leaveTimeoutRef = useRef<NodeJS.Timeout | null>(null)
   const currentHoveredWellRef = useRef<string | null>(null)
@@ -267,6 +269,7 @@ export function SelectTips(
         />
         {hoveredWell != null ? (
           <PipetteShadow
+            robotType={robotType}
             pipetteSpec={pipetteSpecs}
             slotPosition={slotPosition}
             hoveredWell={hoveredWell}
