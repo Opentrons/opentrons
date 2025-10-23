7bafd9b12467663a57f30cca48c4c843888a1ef6
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2025-10-23 08:44:13 -0400

    feat(protocol-designer): add logic for tiprack availability (#19867)
    This PR refactors TipSelectionWizard components and hooks, and adds
    logic to the effect of ensuring the TipSelectionModal cannot be opened
    if there are no valid manual tip selections on the deck at the time of
    the step's creation.

---
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/hooks/useMemoizedTipAccessibilityByTiprackIdByWellName.ts
@@ -0,0 +1,106 @@
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+
+import {
+  EMPTY,
+  getIsSafePickupWithinTiprack,
+  getIsSafePipetteMovement,
+} from '@opentrons/step-generation'
+
+import { OFFDECK } from '/protocol-designer/constants'
+import { getInvariantContext } from '/protocol-designer/step-forms/selectors'
+import { getRobotStateAtActiveItem } from '/protocol-designer/top-selectors/labware-locations'
+
+import type {
+  NozzleConfigurationStyle,
+  PipetteV2Specs,
+} from '@opentrons/shared-data'
+
+/**
+ * Returns a record of tip accessibility status by  tiprack id and well name.
+ * Example return:
+ * {
+ *   'tiprack1Id': {
+ *     'A1': true,
+ *     'A2': false,
+ *   },
+ *   'tiprack2Id': {
+ *     'A1': true,
+ *     'A2': false,
+ *   },
+ */
+export const useMemoizedTipAccessibilityByTiprackIdByWellName = (args: {
+  nozzles: NozzleConfigurationStyle
+  pipetteSpecs: PipetteV2Specs
+  selectedTips: string[][]
+  primaryNozzle: string
+  pipetteId: string
+  tiprackUri: string
+}): Record<string, Record<string, boolean>> => {
+  const {
+    nozzles,
+    pipetteSpecs,
+    selectedTips,
+    primaryNozzle,
+    pipetteId,
+    tiprackUri,
+  } = args
+  const robotState = useSelector(getRobotStateAtActiveItem)
+  const invariantContext = useSelector(getInvariantContext)
+  const { labwareEntities } = invariantContext
+
+  return useMemo(() => {
+    if (robotState == null) {
+      return {}
+    }
+    return Object.entries(robotState.labware).reduce<
+      Record<string, Record<string, boolean>>
+    >((acc, [id, { stack }]) => {
+      const { def, labwareDefURI } = labwareEntities[id]
+      const isMatchingTiprackOnDeck =
+        !stack.includes(OFFDECK) &&
+        def.parameters.isTiprack &&
+        labwareDefURI === tiprackUri
+      if (!isMatchingTiprackOnDeck) {
+        return acc
+      }
+      const tipState = robotState?.tipState.tipracks[id] ?? null
+      if (tipState == null) {
+        return acc
+      }
+      return {
+        ...acc,
+        [id]: Object.keys(def.wells).reduce(
+          (acc, wellName) => ({
+            ...acc,
+            [wellName]:
+              // check accessibility of tips in tiprack
+              getIsSafePickupWithinTiprack({
+                tipState,
+                primaryNozzle,
+                channels: pipetteSpecs.channels,
+                nozzleConfiguration: nozzles,
+                wellName,
+                tiprackDef: def,
+                tipsToIgnore: selectedTips.flat(),
+              }) &&
+              // check if pipette movement is safe relative to surrounding labware and pipette bounds
+              getIsSafePipetteMovement({
+                robotState,
+                invariantContext,
+                pipetteId,
+                labwareId: id,
+                wellLocationOffset: { x: 0, y: 0 },
+                wellTargetName: wellName,
+                primaryNozzle,
+                nozzleConfiguration: nozzles,
+              }) &&
+              // check if tip is not empty
+              tipState[wellName] !== EMPTY,
+          }),
+          {}
+        ),
+      }
+    }, {})
+  }, [selectedTips])
+}
