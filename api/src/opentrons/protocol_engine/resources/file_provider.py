321a7b0844764e0b0e9d61d4de6733b64452d47e
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-10-24 12:34:26 -0400

    feat(api, robot-server): Error on disk space violations (#19909)
    Closes EXEC-1963
    
    Whenever a protocol command attempts to generate a data file, we should check if:
    
    The overall disk space is sufficient.
    If it's an image generating command, that the images directory disk space is sufficient.
    If either of these conditions are violated, we should throw a StorageLimitReachedError.
    
    Note that we deprecate the hard file limit error we were using before. This is true for the absorbance reader file output as well - now we exclusively check disk space.

--- api/src/opentrons/protocol_engine/resources/file_provider.py
+++ api/src/opentrons/protocol_engine/resources/file_provider.py
@@ -11,10 +11,6 @@ from opentrons_shared_data.data_files import (
     RunFileNameMetadata,
 )
 
-from ..errors import StorageLimitReachedError
-
-MAXIMUM_FILE_LIMIT = 400
-
 
 @dataclass(frozen=True)
 class FileNameCmdMetadata:
@@ -162,7 +158,6 @@ class FileProvider:
         data_files_write_file_cb: Optional[
             Callable[[FileData], Awaitable[DataFileInfo]]
         ] = None,
-        data_files_filecount: Optional[Callable[[], Awaitable[int]]] = None,
     ) -> None:
         """Initialize the interface callbacks of the File Provider for data file handling within the Protocol Engine.
 
@@ -171,7 +166,6 @@ class FileProvider:
             data_files_filecount: Callback to check the amount of data files already present in the data files directory.
         """
         self._data_files_write_file_cb = data_files_write_file_cb
-        self._data_files_filecount = data_files_filecount
         self._run_metadata: RunFileNameMetadata | None = None
 
     async def write_file(
@@ -180,22 +174,23 @@ class FileProvider:
         mime_type: MimeType,
         command_metadata: CommandFileNameMetadata,
     ) -> DataFileInfo:
-        """Writes arbitrary data to a file in the Data Files directory. Returns the `DataFileInfo` of the file created."""
-        if self._data_files_filecount is not None:
-            file_count = await self._data_files_filecount()
-            if file_count >= MAXIMUM_FILE_LIMIT:
-                raise StorageLimitReachedError(
-                    f"Not enough space to store file. Maximum file limit of {MAXIMUM_FILE_LIMIT} reached."
-                )
-            if self._data_files_write_file_cb is not None:
-                assert self._run_metadata is not None
-                file_data = FileData.build(
-                    data=data,
-                    mime_type=mime_type,
-                    command_metadata=command_metadata,
-                    run_metadata=self._run_metadata,
-                )
-                return await self._data_files_write_file_cb(file_data)
+        """Writes arbitrary data to a file in the Data Files directory.
+
+        Returns the `DataFileInfo` of the file created.
+
+        Raises:
+            Note that the callback may raise a StorageLimitReachedError.
+        """
+        if self._data_files_write_file_cb is not None:
+            assert self._run_metadata is not None
+            file_data = FileData.build(
+                data=data,
+                mime_type=mime_type,
+                command_metadata=command_metadata,
+                run_metadata=self._run_metadata,
+            )
+
+            return await self._data_files_write_file_cb(file_data)
         # If we are in an analysis or simulation state, return an empty `DataFileInfo`
         return DataFileInfo(
             id="",
