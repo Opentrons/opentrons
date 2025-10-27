17da2a77bd1b14a182c9a3b565600b08a3476568
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-10-27 17:07:56 -0400

    feat(shared-data, components): Add `captureImage` command text (#19938)
    Closes EXEC-2010

---
+++ shared-data/command/types/devices.ts
@@ -0,0 +1,32 @@
+// export type SystemDevicesRunTimeCommand =
+
+import type { CommonCommandCreateInfo, CommonCommandRunTimeInfo } from './index'
+
+export type RobotDevicesRunTimeCommand = CaptureImageRunTimeCommand
+
+export type RobotDevicesCreateCommand = CaptureImageCreateCommand
+
+type Width = number
+type Height = number
+
+export interface CaptureImageParams {
+  homeBefore?: boolean
+  fileName?: string
+  resolution?: [Width, Height]
+  zoom?: number
+  contrast?: number
+  brightness?: number
+  saturation?: number
+}
+
+export interface CaptureImageCreateCommand extends CommonCommandCreateInfo {
+  commandType: 'captureImage'
+  params: CaptureImageParams
+}
+
+export interface CaptureImageRunTimeCommand
+  extends CommonCommandRunTimeInfo,
+    CaptureImageCreateCommand {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  result?: {} // Returns an empty object currently.
+}
