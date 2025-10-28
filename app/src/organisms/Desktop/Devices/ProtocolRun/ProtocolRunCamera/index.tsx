79fff93f2584a6871886f549b8cfc58c41444a03
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-10-27 21:56:41 -0400

    refactor(app): update gallery header livestream/chip states

--- app/src/organisms/Desktop/Devices/ProtocolRun/ProtocolRunCamera/index.tsx
+++ app/src/organisms/Desktop/Devices/ProtocolRun/ProtocolRunCamera/index.tsx
@@ -3,15 +3,25 @@ import { useTranslation } from 'react-i18next'
 import { Chip, Flex, SPACING, StyledText } from '@opentrons/components'
 
 import { Divider } from '/app/atoms/structure'
+import { isTerminalRunStatus } from '/app/organisms/Desktop/Devices/ProtocolRun/ProtocolRunHeader/utils'
 import { useCameraUsageSettings } from '/app/organisms/Desktop/Devices/RobotSettings/RobotSettingsCamera/hooks/useCameraUsageSettings'
 
 import { ImageGalleryContainer } from './ImageGalleryContainer'
 import { LaunchLivestreamBtn } from './LaunchLivestreamBtn'
 import styles from './runcamera.module.css'
 
-export function ProtocolRunCamera(): JSX.Element {
+import type { RunStatus } from '@opentrons/api-client'
+
+export interface ProtocolRunCameraProps {
+  runStatus: RunStatus | null
+}
+
+export function ProtocolRunCamera({
+  runStatus,
+}: ProtocolRunCameraProps): JSX.Element {
   const { t } = useTranslation('run_details')
   const { isCameraEnabled } = useCameraUsageSettings()
+
   return (
     <div className={styles.content_container}>
       <div className={styles.header_container}>
@@ -20,14 +30,18 @@ export function ProtocolRunCamera(): JSX.Element {
             <StyledText desktopStyle="bodyLargeSemiBold">
               {t('camera')}
             </StyledText>
-            <Chip
-              text={isCameraEnabled ? t('enabled') : t('Disabled')}
-              type={isCameraEnabled ? 'success' : 'neutral'}
-              iconName="connection-status"
-            />
+            {!isTerminalRunStatus(runStatus) && (
+              <Chip
+                text={isCameraEnabled ? t('enabled') : t('disabled')}
+                type={isCameraEnabled ? 'success' : 'neutral'}
+                iconName="connection-status"
+              />
+            )}
           </Flex>
         </div>
-        {isCameraEnabled ? <LaunchLivestreamBtn /> : null}
+        {!isTerminalRunStatus(runStatus) && isCameraEnabled ? (
+          <LaunchLivestreamBtn />
+        ) : null}
       </div>
       <Divider width="100%" />
       <ImageGalleryContainer />
