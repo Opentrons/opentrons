2fc3344dd4aef7e5d8aecdbbf7ab6a6af2fa4c47
Max Marrone <max@opentrons.com>
2025-10-28 15:56:45 -0400

    test: Fill in some concurrentStepGroupUtils tests.

--- protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/__tests__/concurrentStepGroupUtils.test.ts
+++ protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/__tests__/concurrentStepGroupUtils.test.ts
@@ -18,7 +18,7 @@ describe('convertFlatStepArrayToHierarchy() and convertStepHierarchyToFlatArray(
   test.each([
     { label: 'empty', flat: [], hierarchy: { topLevelItems: [] } },
     {
-      label: 'only top-level',
+      label: 'only top-level steps, no groups',
       flat: [
         {
           id: 'a',
@@ -142,12 +142,166 @@ describe('convertFlatStepArrayToHierarchy() and convertStepHierarchyToFlatArray(
     const flatResult = convertStepHierarchyToArray(hierarchyResult)
     expect(flatResult).toStrictEqual(flat.map(element => element.id))
   })
+
+  it('should no-op if enableConcurrentModuleActions is false', () => {
+    const input: FormData[] = [
+      {
+        id: 'a',
+        stepType: 'comment',
+      },
+      {
+        id: 'b',
+        stepType: 'thermocycler',
+        thermocyclerFormType: THERMOCYCLER_PROFILE,
+      },
+      {
+        id: 'c',
+        stepType: 'comment',
+      },
+      {
+        id: 'd',
+        stepType: 'comment',
+      },
+      {
+        id: 'e',
+        stepType: 'pause',
+        pauseAction: PAUSE_UNTIL_TC_PROFILE_COMPLETE,
+      },
+      {
+        id: 'f',
+        stepType: 'comment',
+      },
+    ]
+    const result = convertStepArrayToHierarchy(input, false)
+    const expectedResult: typeof result = {
+      topLevelItems: input.map(step => ({
+        type: 'standaloneStep',
+        stepId: step.id,
+      })),
+    }
+    expect(result).toStrictEqual(expectedResult)
+  })
 })
 
 describe('computeStepMove()', () => {
   it('can move a step before another step', () => {
-    // TODO: Implement this test.
+    const originalHierarchy: StepHierarchy = {
+      topLevelItems: [
+        {
+          type: 'standaloneStep',
+          stepId: 'a',
+        },
+        {
+          type: 'standaloneStep',
+          stepId: 'b',
+        },
+        {
+          type: 'standaloneStep',
+          stepId: 'c',
+        },
+      ],
+    }
+    const result = computeStepMove(originalHierarchy, {
+      moveType: 'insertBeforeDestinationStep',
+      movedStepId: 'b',
+      destinationStepId: 'a',
+    })
+    const expectedResult: typeof result = {
+      isMoveAllowed: true,
+      stepsAfterMove: {
+        topLevelItems: [
+          {
+            type: 'standaloneStep',
+            stepId: 'b',
+          },
+          {
+            type: 'standaloneStep',
+            stepId: 'a',
+          },
+          {
+            type: 'standaloneStep',
+            stepId: 'c',
+          },
+        ],
+      },
+    }
+    expect(result).toStrictEqual(expectedResult)
   })
+
+  it("can move a step that's inside a group to before another step that's inside a different group", () => {
+    const originalHierarchy: StepHierarchy = {
+      topLevelItems: [
+        {
+          type: 'standaloneStep',
+          stepId: 'a',
+        },
+        {
+          type: 'thermocyclerProfileGroup',
+          thermocyclerProfileStepId: 'b',
+          concurrentSteps: [
+            { type: 'standaloneStep', stepId: 'c' },
+            { type: 'standaloneStep', stepId: 'd' },
+          ],
+          waitForThermocyclerProfileStepId: 'e',
+        },
+        {
+          type: 'standaloneStep',
+          stepId: 'f',
+        },
+        {
+          type: 'thermocyclerProfileGroup',
+          thermocyclerProfileStepId: 'g',
+          concurrentSteps: [{ type: 'standaloneStep', stepId: 'h' }],
+          waitForThermocyclerProfileStepId: 'i',
+        },
+        {
+          type: 'standaloneStep',
+          stepId: 'j',
+        },
+      ],
+    }
+    const result = computeStepMove(originalHierarchy, {
+      moveType: 'insertBeforeDestinationStep',
+      movedStepId: 'd',
+      destinationStepId: 'h',
+    })
+    const expectedResult: typeof result = {
+      isMoveAllowed: true,
+      stepsAfterMove: {
+        topLevelItems: [
+          {
+            type: 'standaloneStep',
+            stepId: 'a',
+          },
+          {
+            type: 'thermocyclerProfileGroup',
+            thermocyclerProfileStepId: 'b',
+            concurrentSteps: [{ type: 'standaloneStep', stepId: 'c' }],
+            waitForThermocyclerProfileStepId: 'e',
+          },
+          {
+            type: 'standaloneStep',
+            stepId: 'f',
+          },
+          {
+            type: 'thermocyclerProfileGroup',
+            thermocyclerProfileStepId: 'g',
+            concurrentSteps: [
+              { type: 'standaloneStep', stepId: 'd' },
+              { type: 'standaloneStep', stepId: 'h' },
+            ],
+            waitForThermocyclerProfileStepId: 'i',
+          },
+          {
+            type: 'standaloneStep',
+            stepId: 'j',
+          },
+        ],
+      },
+    }
+    expect(result).toStrictEqual(expectedResult)
+  })
+
   it('can move a step to the end of a Thermocycler group', () => {
     const originalHierarchy: StepHierarchy = {
       topLevelItems: [
@@ -191,10 +345,95 @@ describe('computeStepMove()', () => {
     }
     expect(result).toStrictEqual(expectedResult)
   })
+
   it('can move an entire Thermocycler group', () => {
-    // TODO: Implement this test.
+    const originalHierarchy: StepHierarchy = {
+      topLevelItems: [
+        {
+          type: 'standaloneStep',
+          stepId: 'a',
+        },
+        {
+          type: 'thermocyclerProfileGroup',
+          thermocyclerProfileStepId: 'b',
+          concurrentSteps: [{ type: 'standaloneStep', stepId: 'c' }],
+          waitForThermocyclerProfileStepId: 'd',
+        },
+        {
+          type: 'standaloneStep',
+          stepId: 'e',
+        },
+      ],
+    }
+    const result = computeStepMove(originalHierarchy, {
+      moveType: 'insertBeforeDestinationStep',
+      movedStepId: 'b',
+      destinationStepId: 'a',
+    })
+    const expectedResult: typeof result = {
+      isMoveAllowed: true,
+      stepsAfterMove: {
+        topLevelItems: [
+          {
+            type: 'thermocyclerProfileGroup',
+            thermocyclerProfileStepId: 'b',
+            concurrentSteps: [{ type: 'standaloneStep', stepId: 'c' }],
+            waitForThermocyclerProfileStepId: 'd',
+          },
+          {
+            type: 'standaloneStep',
+            stepId: 'a',
+          },
+          {
+            type: 'standaloneStep',
+            stepId: 'e',
+          },
+        ],
+      },
+    }
+    expect(result).toStrictEqual(expectedResult)
   })
+
   it('allows moving a step to itself, as a no-op', () => {
-    // TODO: Implement this test.
+    const originalHierarchy: StepHierarchy = {
+      topLevelItems: [
+        {
+          type: 'standaloneStep',
+          stepId: 'a',
+        },
+        {
+          type: 'thermocyclerProfileGroup',
+          thermocyclerProfileStepId: 'b',
+          concurrentSteps: [{ type: 'standaloneStep', stepId: 'c' }],
+          waitForThermocyclerProfileStepId: 'd',
+        },
+        {
+          type: 'standaloneStep',
+          stepId: 'e',
+        },
+      ],
+    }
+
+    for (const stepId of ['a', 'b', 'c', /* skip d */ 'e']) {
+      const result = computeStepMove(originalHierarchy, {
+        moveType: 'insertBeforeDestinationStep',
+        movedStepId: stepId,
+        destinationStepId: stepId,
+      })
+      expect(result).toStrictEqual({
+        isMoveAllowed: true,
+        stepsAfterMove: originalHierarchy,
+      } satisfies typeof result)
+    }
+
+    const result = computeStepMove(originalHierarchy, {
+      moveType: 'insertAsLastStepOfGroup',
+      movedStepId: 'c', // c is already the last step of its group.
+      destinationGroupRootStepId: 'b',
+    })
+    expect(result).toStrictEqual({
+      isMoveAllowed: true,
+      stepsAfterMove: originalHierarchy,
+    } satisfies typeof result)
   })
 })
