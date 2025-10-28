7942bf14e3c730c8e91a7824877583fbddf62622
CaseyBatten <caseyhbatten@gmail.com>
2025-10-28 11:14:58 -0400

    camera settings endpoint cleanup

--- robot-server/robot_server/runs/router/camera_router.py
+++ robot-server/robot_server/runs/router/camera_router.py
@@ -20,6 +20,8 @@ from ..run_orchestrator_store import RunOrchestratorStore
 from ..dependencies import get_run_orchestrator_store
 from .base_router import RunNotFound, RunStopped, RunNotIdle, get_run_data_from_url
 
+from robot_server.service.legacy.models.settings import CameraEnable
+
 log = logging.getLogger(__name__)
 camera_router = LightRouter()
 
@@ -42,13 +44,13 @@ camera_router = LightRouter()
         status.HTTP_409_CONFLICT: {"model": ErrorBody[Union[RunStopped, RunNotIdle]]},
     },
 )
-async def add_labware_offset(
-    request_body: RequestModel[CameraSettings],
+async def add_camera_settings(
+    request_body: RequestModel[CameraEnable],
     run_orchestrator_store: Annotated[
         RunOrchestratorStore, Depends(get_run_orchestrator_store)
     ],
     run: Annotated[Run, Depends(get_run_data_from_url)],
-) -> PydanticResponse[SimpleBody[CameraSettings]]:
+) -> PydanticResponse[SimpleBody[CameraEnable]]:
     """Add unique camera settings to a run to be used in place of the global camera settings.
 
     Args:
@@ -60,13 +62,18 @@ async def add_labware_offset(
         raise RunStopped(detail=f"Run {run.id} is not the current run").as_error(
             status.HTTP_409_CONFLICT
         )
+    
+    camera_settings = CameraSettings(
+        camera_enabled=request_body.data.cameraEnabled if request_body.data.cameraEnabled is not None else False,
+        live_stream_enabled=request_body.data.liveStreamEnabled if request_body.data.liveStreamEnabled is not None else False,
+        error_recovery_enabled=request_body.data.errorRecoveryCameraEnabled if request_body.data.errorRecoveryCameraEnabled is not None else False,
 
-    response_data = run_orchestrator_store.add_camera_enablement_settings(
-        request_body.data
     )
+
+    response_data = run_orchestrator_store.add_camera_enablement_settings(camera_settings)
     log.info(f'Added unique camera settings "{request_body.data}" to run "{run.id}".')
 
     return await PydanticResponse.create(
-        content=SimpleBody.model_construct(data=response_data),
+        content=SimpleBody.model_construct(data=CameraEnable(cameraEnabled=response_data.camera_enabled, liveStreamEnabled=response_data.live_stream_enabled, errorRecoveryCameraEnabled=response_data.error_recovery_enabled)),
         status_code=status.HTTP_201_CREATED,
     )
