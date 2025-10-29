a21dc0cc48fda0094d1dff27bb9eae5756ef6dff
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-10-29 09:57:12 -0400

    feat(app): add "live video" chip to livestream

--- app/src/pages/Desktop/LivestreamViewer/index.tsx
+++ app/src/pages/Desktop/LivestreamViewer/index.tsx
@@ -1,6 +1,7 @@
-import { useLayoutEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSearchParams } from 'react-router-dom'
+
+import { Chip } from '@opentrons/components'
 
 import { useHlsVideo } from '/app/pages/Desktop/LivestreamViewer/hooks/useHlsVideo'
 import {
@@ -31,7 +32,26 @@ export function LivestreamViewer(): JSX.Element {
             infoScreenType != null ? styles.video_inactive : styles.video_active
           }
         />
+        {infoScreenType == null && (
+          <div className={styles.chip_overlay}>
+            <LiveVideoChip />
+          </div>
+        )}
       </div>
     </div>
+  )
+}
+
+function LiveVideoChip(): JSX.Element {
+  const { t } = useTranslation('run_details')
+
+  return (
+    <Chip
+      type="success"
+      text={t('live_video')}
+      chipSize="small"
+      hasIcon={true}
+      iconName="connection-status"
+    />
   )
 }
