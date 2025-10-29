113c0cc3a3b36a7bc8b14a3f5ef5cebdb64ec2f7
Max Marrone <max@opentrons.com>
2025-10-29 14:35:03 -0400

    Lint.

--- protocol-designer/src/components/molecules/ConcurrentGroup/__tests__/ConcurrentGroupCheckpoint.test.tsx
+++ protocol-designer/src/components/molecules/ConcurrentGroup/__tests__/ConcurrentGroupCheckpoint.test.tsx
@@ -1,4 +1,4 @@
-import { screen, within } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 
 import { renderWithProviders } from '/protocol-designer/__testing-utils__'
