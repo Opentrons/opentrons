71216d1a1d9aa306e8b1495c039c2d64390d2d04
koji <baxin1919@gmail.com>
2025-10-31 15:30:08 -0400

    remove properties from ignore list

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -80,8 +80,6 @@ module.exports = {
           'lost-unit',
           'lost-utility',
           'lost-waffle',
-          'container-name',
-          'container-type',
         ],
       },
     ],
