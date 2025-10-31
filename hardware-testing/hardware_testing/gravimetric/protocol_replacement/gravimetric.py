93d2cbc7e1c4e45b3c854e89a480c3523c6665d8
Ryan Howard <ryan.howard@opentrons.com>
2025-10-31 12:22:11 -0400

    upgrade the protocol error output.

--- hardware-testing/hardware_testing/gravimetric/protocol_replacement/gravimetric.py
+++ hardware-testing/hardware_testing/gravimetric/protocol_replacement/gravimetric.py
@@ -8,6 +8,7 @@ from time import time
 import importlib
 import copy
 import json
+import traceback
 
 from opentrons.protocol_api import (
     ProtocolContext,
@@ -1481,6 +1482,7 @@ def run(ctx: ProtocolContext) -> None:
         _run(ctx, fixture_settings)
     except Exception as e:
         print_error(f"error during run {e}")
+        traceback.print_exception(e)
     finally:
         if fixture_settings.recorder is not None:
             print_info("ending recording")
