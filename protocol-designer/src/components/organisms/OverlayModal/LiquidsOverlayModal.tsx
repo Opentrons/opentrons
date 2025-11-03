a87a16e4aafeb2ebe387f18718be4a27c102f6fb
TamarZanzouri <zanzouritamar@gmail.com>
2025-11-03 15:45:03 -0500

    dont show overlay if cleared liquids

--- protocol-designer/src/components/organisms/OverlayModal/LiquidsOverlayModal.tsx
+++ protocol-designer/src/components/organisms/OverlayModal/LiquidsOverlayModal.tsx
@@ -37,6 +37,7 @@ export function LiquidLayoutOverlayModalContainer({
               wells: wellContentsId,
             })
           )
+          showLiquidOverflowMenu(false)
         },
       }}
       secondaryButtonProps={{
