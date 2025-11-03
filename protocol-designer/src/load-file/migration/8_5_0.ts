76aa505abfa56563aa10219f2c55401ca5fb6027
David Chau <david.chau@opentrons.com>
2025-11-03 14:21:08 -0500

    Prettier

--- protocol-designer/src/load-file/migration/8_5_0.ts
+++ protocol-designer/src/load-file/migration/8_5_0.ts
@@ -89,7 +89,10 @@ const getClippedFlowRateForMoveLiquid = (args: {
     ?.byTipType.find(byTipType => byTipType.tiprack === tiprack)
   // TODO: this won't find the definition for a custom tiprack
   const tiprackDef = getAllDefinitions()[tiprack]
-  console.assert(tiprackDef != null, `could not find labware definition for ${tiprack}`)
+  console.assert(
+    tiprackDef != null,
+    `could not find labware definition for ${tiprack}`
+  )
   let correctionVolume: number = 0
   if (tipLiquidSpecs != null && flowRateType !== 'blowout') {
     const liquidClassLookup =
