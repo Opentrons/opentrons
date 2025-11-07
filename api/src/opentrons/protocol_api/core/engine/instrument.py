6bf060df40cd3f647525a3038138d9d483aad45c
Ryan Howard <ryan.howard@opentrons.com>
2025-11-07 11:49:14 -0500

    remove restriction of meniscus at start.

--- api/src/opentrons/protocol_api/core/engine/instrument.py
+++ api/src/opentrons/protocol_api/core/engine/instrument.py
@@ -229,8 +229,6 @@ class InstrumentCore(AbstractInstrument[WellCore, LabwareCore]):
             in_place: whether this is a in-place command.
             meniscus_tracking: Optional data about where to aspirate from.
         """
-        if meniscus_tracking == MeniscusTrackingTarget.START and end_location is None:
-            raise ValueError("Cannot aspirate at the starting liquid height.")
         final_location = location
         if well_core is None:
             if not in_place:
