02991537e489de7d61955a5fa2ee87a28fc0219b
Edward Cormany <edward.cormany@opentrons.com>
2025-11-07 15:02:57 -0500

    griffe labels working for properties too (thx Seth)

--- docs/v2/griffe_version_required.py
+++ docs/v2/griffe_version_required.py
@@ -1,11 +1,13 @@
 """
 Griffe extension to support a custom @requires_version decorator.
 """
+import ast
 import griffe
 import re
 
 class RequiresVersionExtension(griffe.Extension):
     def on_function_instance(self, *, func, **kwargs):
+        print(f"{func} {getattr(func, 'decorators', [])}")
         for decorator in getattr(func, "decorators", []):
             callable_path = getattr(decorator, "callable_path", None)
             if callable_path == "opentrons.protocols.api_support.util.requires_version":
@@ -15,3 +17,12 @@ class RequiresVersionExtension(griffe.Extension):
                     major, minor = match.groups()
                     version_text = f"v{major}.{minor}"
                     func.labels.add(version_text)
+    def on_attribute_instance(self, *, node, attr, **kwargs):
+        print(f"Node: {node} {attr} {getattr(node, 'decorator_list', [])}")
+        for decorator in getattr(node, "decorator_list", []):
+            if isinstance(decorator, ast.Call):
+                print(f"Node {attr}: {decorator.func.id} {[arg.value for arg in decorator.args]} {decorator.keywords}")
+                if "requires_version" in decorator.func.id:
+                    major, minor = [arg.value for arg in decorator.args]
+                    version_text = f"v{major}.{minor}"
+                    attr.labels.add(version_text)
