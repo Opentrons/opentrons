6b392d40729a5031d55382f0dcfb17e29b522d76
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2025-11-10 17:30:04 -0500

    feat(protocol-designer, shared-data): refine OT-2 8-channel shadow and offset (#20106)
    This PR adds formerly missing bounding box points for OT-2 GEN2
    8-channel pipettes (P20, P300, and P1000). Also, it adds a custom
    pipette shadow label offset for the specific case of OT-2 8-channel
    pipettes, with a right label placement, to account for the irregular
    shadow shape. Lastly, it removes erroneous well offsets for pipette
    shadow placement (the well position (x, y) itself points to the center
    of the well).

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/PipetteShadows/PipetteShadow.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/PipetteShadows/PipetteShadow.tsx
@@ -79,12 +79,29 @@ export function PipetteShadow(props: {
   const labelRef = useRef<HTMLDivElement>(null)
   const [labelWidth, setLabelWidth] = useState(0)
   const [labelHeight, setLabelHeight] = useState(0)
+
+  const labelText = (() => {
+    if (!hasPickupsRemaining && !isHoveredWellSelected) {
+      return t('all_pickups_selected')
+    }
+    if (!isAccessible && inaccessibleReason != null) {
+      return t(`tip_inaccessible.${inaccessibleReason}`)
+    }
+    if (isAccessible) {
+      return isHoveredWellSelected
+        ? t('tip_accessible.deselect')
+        : t('tip_accessible.select')
+    }
+    console.error('No label text found')
+    return ''
+  })()
+
   useEffect(() => {
     if (labelRef.current) {
       setLabelWidth(labelRef.current.offsetWidth)
       setLabelHeight(labelRef.current.offsetHeight)
     }
-  }, [hoveredWell])
+  }, [hoveredWell, labelText])
 
   const { x: xOffset, y: yOffset } = getHoveredOffsetFromWell({
     selectedTiprackId,
@@ -127,28 +144,15 @@ export function PipetteShadow(props: {
     channels,
   })
   const ShadowComponent = SHADOW_BY_ROBOT_TYPE_AND_CHANNELS[robotType][channels]
+  const isOt2EightChannel = robotType === OT2_ROBOT_TYPE && channels === 8
   const { x: labelOffsetX, y: labelOffsetY } = getLabelOffsetByPlacement({
     labelPlacement,
     labelWidth,
     labelHeight,
     shadowWidth: width,
     shadowHeight: height,
+    isOt2EightChannel,
   })
-  const labelText = (() => {
-    if (!hasPickupsRemaining && !isHoveredWellSelected) {
-      return t('all_pickups_selected')
-    }
-    if (!isAccessible && inaccessibleReason != null) {
-      return t(`tip_inaccessible.${inaccessibleReason}`)
-    }
-    if (isAccessible) {
-      return isHoveredWellSelected
-        ? t('tip_accessible.deselect')
-        : t('tip_accessible.select')
-    }
-    console.error('No label text found')
-    return ''
-  })()
 
   return (
     <g className={styles.shadow_overlay}>
