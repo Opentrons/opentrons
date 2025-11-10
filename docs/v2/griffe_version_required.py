ab0227000ff66361c9e142f9331e1280a121eaac
Edward Cormany <edward.cormany@opentrons.com>
2025-11-10 09:26:37 -0500

    remove print debugging in griffe script

--- docs/v2/griffe_version_required.py
+++ docs/v2/griffe_version_required.py
@@ -7,7 +7,6 @@ import re
 
 class RequiresVersionExtension(griffe.Extension):
     def on_function_instance(self, *, func, **kwargs):
-        print(f"{func} {getattr(func, 'decorators', [])}")
         for decorator in getattr(func, "decorators", []):
             callable_path = getattr(decorator, "callable_path", None)
             if callable_path == "opentrons.protocols.api_support.util.requires_version":
@@ -18,10 +17,8 @@ class RequiresVersionExtension(griffe.Extension):
                     version_text = f"v{major}.{minor}"
                     func.labels.add(version_text)
     def on_attribute_instance(self, *, node, attr, **kwargs):
-        print(f"Node: {node} {attr} {getattr(node, 'decorator_list', [])}")
         for decorator in getattr(node, "decorator_list", []):
             if isinstance(decorator, ast.Call):
-                print(f"Node {attr}: {decorator.func.id} {[arg.value for arg in decorator.args]} {decorator.keywords}")
                 if "requires_version" in decorator.func.id:
                     major, minor = [arg.value for arg in decorator.args]
                     version_text = f"v{major}.{minor}"
