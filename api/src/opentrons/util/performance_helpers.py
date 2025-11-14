8c8abf50dab05d9e58318419740e4a818ef9c1af
jesse.johnson <jesse.johnson@neochromosome.com>
2025-11-14 15:36:16 -0800

    mypy errors

--- api/src/opentrons/util/performance_helpers.py
+++ api/src/opentrons/util/performance_helpers.py
@@ -124,7 +124,7 @@ def _track_a_function(
         **kwargs: _UnderlyingFunctionParameters.kwargs
     ) -> _UnderlyingFunctionReturn:
         try:
-            return typing.cast(_UnderlyingFunctionReturn, wrapped(*args, **kwargs))
+            return wrapped(*args, **kwargs)
         finally:
             # TODO: derek maggio (06-18-2024): After investigation, it appears on startup
             # that the first call to tracker.store() will not actually store the data.
