f401f0d3cc468f28f6889976c05a53c6a8ad0ef7
jesse.johnson <jesse.johnson@neochromosome.com>
2025-11-17 10:01:37 -0800

    squashed commits
    more squashed commits

--- api/src/opentrons/util/performance_helpers.py
+++ api/src/opentrons/util/performance_helpers.py
@@ -74,7 +74,7 @@ def _handle_package_import() -> typing.Type["SupportsTracking"]:
     try:
         from performance_metrics import RobotActivityTracker
 
-        return typing.cast(typing.Type["SupportsTracking"], RobotActivityTracker)
+        return RobotActivityTracker
     except ImportError:
         return _StubbedTracker
 
@@ -116,12 +116,7 @@ def _track_a_function(
         The decorated function.
     """
     tracker: SupportsTracking = _get_robot_activity_tracker()
-    wrapped: _UnderlyingFunction[
-        _UnderlyingFunctionParameters, _UnderlyingFunctionReturn
-    ] = typing.cast(
-        _UnderlyingFunction[_UnderlyingFunctionParameters, _UnderlyingFunctionReturn],
-        tracker.track(state=state_name)(func),
-    )
+    wrapped = tracker.track(state=state_name)(func)
 
     @functools.wraps(func)
     def wrapper(
