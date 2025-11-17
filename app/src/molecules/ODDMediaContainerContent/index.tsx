6599c958e867614f80ba77ff16a96ba6e2a7005e
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-11-17 09:54:08 -0500

    fix prettier

--- app/src/molecules/ODDMediaContainerContent/index.tsx
+++ app/src/molecules/ODDMediaContainerContent/index.tsx
@@ -37,12 +37,13 @@ export function ODDMediaContainerContent(
         <div className={styles.list_item_container}>
           <div className={styles.list_item_content_container}>
             <div>
-              {isLoading ? (<Skeleton width="100%" height="100%" backgroundSize="47rem" />):
-               <StyledText oddStyle="bodyTextSemiBold">
-                {leftPrimaryText}
-              </StyledText>
-              }
-             
+              {isLoading ? (
+                <Skeleton width="100%" height="100%" backgroundSize="47rem" />
+              ) : (
+                <StyledText oddStyle="bodyTextSemiBold">
+                  {leftPrimaryText}
+                </StyledText>
+              )}
             </div>
             <div className={styles.list_item_step}>
               {!isLoading && isCurrentCmdError && (
