dae775ce32376493e0202437f6762e32950441f2
Max Marrone <max@opentrons.com>
2025-11-19 11:50:40 -0500

    feat(protocol-designer): Correctly duplicate steps when there are concurrent Thermocycler profiles (#20161)

--- protocol-designer/src/steplist/utils/getStepsToSelect.ts
+++ protocol-designer/src/steplist/utils/getStepsToSelect.ts
@@ -40,3 +40,22 @@ export function getStepToSelectAfterDeletion(
 
   return nextSelectableId ?? prevSelectableId ?? null
 }
+
+/**
+ * When the user duplicates steps, this chooses which steps we should automatically
+ * select after the duplication.
+ *
+ * @param stepHierarchyAfterDuplication: The steps after the duplication operation.
+ * @param newStepIds: The IDs of the steps that the duplication operation added.
+ * @return The steps we should select, in timeline order.
+ */
+export function getStepsToSelectAfterDuplication(
+  stepHierarchyAfterDuplication: StepHierarchy,
+  newStepIds: Set<StepIdType>
+): StepIdType[] {
+  const flatStepIds = convertStepHierarchyToArray(stepHierarchyAfterDuplication)
+  const visibilities = getStepVisibilities(stepHierarchyAfterDuplication)
+  return flatStepIds.filter(
+    stepId => visibilities[stepId].isVisibleToUser && newStepIds.has(stepId)
+  )
+}
