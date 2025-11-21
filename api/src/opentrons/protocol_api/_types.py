449f59d55139ec74ca214aecfa6dd0cfc5aa6570
Edward Cormany <edward.cormany@opentrons.com>
2025-11-21 10:38:31 -0500

    none more black

--- api/src/opentrons/protocol_api/_types.py
+++ api/src/opentrons/protocol_api/_types.py
@@ -23,6 +23,7 @@ See [The Off-Deck Location][the-off-deck-location] for details on using `OFF_DEC
 """
 WASTE_CHUTE: Final = OffDeckType.WASTE_CHUTE
 
+
 class PlungerPositionTypes(enum.Enum):
     PLUNGER_TOP = "top"
     PLUNGER_BOTTOM = "bottom"
