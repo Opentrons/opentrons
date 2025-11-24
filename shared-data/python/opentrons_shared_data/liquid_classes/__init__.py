e8463d65ba8c1c26e853fd95af6dac44d8860d08
Ryan Howard <ryan.howard@opentrons.com>
2025-11-24 14:38:55 -0500

    correctly differnciate LiquidClass types

--- shared-data/python/opentrons_shared_data/liquid_classes/__init__.py
+++ shared-data/python/opentrons_shared_data/liquid_classes/__init__.py
@@ -1,8 +1,8 @@
 """Types and functions for accessing liquid class definitions."""
 from pathlib import Path
 from .. import load_shared_data, get_shared_data_root
-from .liquid_class_definition import LiquidClassSchemaV1
-
+from .liquid_class_definition import LiquidClassSchemaV1, LiquidClassSchemaV2
+from typing import Union
 
 DEFAULT_SCHEMA_VERSION = 1
 DEFAULT_LC_VERSION = 1
@@ -16,13 +16,19 @@ def load_definition(
     name: str,
     version: int = DEFAULT_LC_VERSION,
     schema_version: int = DEFAULT_SCHEMA_VERSION,
-) -> LiquidClassSchemaV1:
+) -> Union[LiquidClassSchemaV2, LiquidClassSchemaV1]:
     """Load the specified liquid class' definition as a LiquidClassSchemaV1 object.
 
     Note: this is an expensive operation and should be called sparingly.
     """
+    LC_TYPE: type[LiquidClassSchemaV2 | LiquidClassSchemaV1]
+    match schema_version:
+        case 1:
+            LC_TYPE = LiquidClassSchemaV1
+        case 2:
+            LC_TYPE = LiquidClassSchemaV2
     try:
-        return LiquidClassSchemaV1.model_validate_json(
+        return LC_TYPE.model_validate_json(
             load_shared_data(
                 f"liquid-class/definitions/{schema_version}/{name}/{version}.json"
             )
