e691fe4037cbc5ce84b0b4e731be0161295ee4f8
Max Marrone <max@opentrons.com>
2025-11-24 17:44:03 -0500

    Preserve the old top-level vite.config.mjs for Storybook.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -20,7 +20,15 @@ const config: StorybookConfig = {
 
   framework: {
     name: '@storybook/react-vite',
-    options: {},
+    options: {
+      builder: {
+        // Storybook would normally find the Vite config automatically.
+        // That doesn't work for us because we have one monorepo-wide Storybook
+        // installation, while each project has its own local Vite config.
+        // So we treat Storybook as its own Vite project with its own config.
+        viteConfigPath: '.storybook/vite.config.mjs',
+      },
+    },
   },
 
   docs: {
