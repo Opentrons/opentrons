254ed5678e4b72ab0ff4f48460eedec0b959cda7
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2025-12-01 14:26:45 -0500

    fix(protocol-designer): make volume field title copy dynamic (#20261)
    Correctly reflect volume field title for moveLiquid volume field
    depending on path according to designs. The pattern is as follows:
    
    - single path -> "Volume per well"
    - distribute path -> "Dispense volume per well"
    - consolidate path -> "Aspirate volume per well"

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/VolumeField.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/VolumeField.tsx
@@ -2,17 +2,24 @@ import { useTranslation } from 'react-i18next'
 
 import { InputStepFormField } from '/protocol-designer/components/molecules'
 
+import type { PathOption } from '/protocol-designer/form-types'
 import type { FieldProps } from '../types'
 
-export function VolumeField(props: FieldProps): JSX.Element {
+interface VolumeFieldProps {
+  fieldProps: FieldProps
+  path?: PathOption
+}
+
+export function VolumeField(props: VolumeFieldProps): JSX.Element {
   const { t } = useTranslation(['protocol_steps', 'application'])
+  const { fieldProps, path = 'single' } = props
 
   return (
     <InputStepFormField
-      title={t('volume_per_well')}
+      title={t(`volume_per_well.${path}`)}
       units={t('application:units.microliter')}
       showTooltip={false}
-      {...props}
+      {...fieldProps}
     />
   )
 }
