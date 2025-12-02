c904731c03c6a35565248dfaf1c27451454b08d1
Jethary <jethary@opentrons.com>
2025-12-02 08:48:48 -0800

    refactor(step-generation): create HOPPER_STACKER_LOCATION const and fix retrieve python
    closes EXEC-2802

--- step-generation/src/commandCreators/atomic/flexStackerRetrieve.ts
+++ step-generation/src/commandCreators/atomic/flexStackerRetrieve.ts
@@ -1,13 +1,22 @@
-import { uuid } from '../../utils'
+import { getLargestStackInSlot, uuid } from '../../utils'
 
 import type { FlexStackerRetrieveCreateCommand } from '@opentrons/shared-data'
 import type { CommandCreator } from '../../types'
 
 export const flexStackerRetrieve: CommandCreator<
   FlexStackerRetrieveCreateCommand['params']
-> = (args, invariantContext) => {
+> = (args, invariantContext, robotState) => {
   const { moduleId } = args
-  const pythonName = invariantContext.moduleEntities[moduleId].pythonName
+  const { modules, labware } = robotState
+  const { moduleEntities, labwareEntities } = invariantContext
+  const modulePythonName = moduleEntities[moduleId].pythonName
+  const moduleLocation = modules[moduleId].slot
+  const largestStackInSlot = getLargestStackInSlot(labware, moduleLocation)
+  // -4 to account for slot, Hopper const, and moduleId that occurs before it
+  const labwareIdOnModule = largestStackInSlot[largestStackInSlot.length - 4]
+  const labwarePythonName = labwareEntities[labwareIdOnModule]?.pythonName
+  //  TODO: add error creator if there is no labware in the stack
+
   return {
     commands: [
       {
@@ -18,6 +27,6 @@ export const flexStackerRetrieve: CommandCreator<
         },
       },
     ],
-    python: `${pythonName}.retrieve()`,
+    python: `${labwarePythonName} = ${modulePythonName}.retrieve()`,
   }
 }
