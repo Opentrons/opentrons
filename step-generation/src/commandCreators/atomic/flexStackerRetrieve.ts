efc894c5080b437a8863bad9e1d97baf7ea34a26
Jethary <jethary@opentrons.com>
2025-12-02 09:28:47 -0800

    make getLabwareIdOnHopper

--- step-generation/src/commandCreators/atomic/flexStackerRetrieve.ts
+++ step-generation/src/commandCreators/atomic/flexStackerRetrieve.ts
@@ -1,4 +1,4 @@
-import { getLargestStackInSlot, uuid } from '../../utils'
+import { getLabwareIdOnHopper, uuid } from '../../utils'
 
 import type { FlexStackerRetrieveCreateCommand } from '@opentrons/shared-data'
 import type { CommandCreator } from '../../types'
@@ -11,9 +11,7 @@ export const flexStackerRetrieve: CommandCreator<
   const { moduleEntities, labwareEntities } = invariantContext
   const modulePythonName = moduleEntities[moduleId].pythonName
   const moduleLocation = modules[moduleId].slot
-  const largestStackInSlot = getLargestStackInSlot(labware, moduleLocation)
-  // -4 to account for slot, Hopper const, and moduleId that occurs before it
-  const labwareIdOnModule = largestStackInSlot[largestStackInSlot.length - 4]
+  const labwareIdOnModule = getLabwareIdOnHopper(labware, moduleLocation)
   const labwarePythonName = labwareEntities[labwareIdOnModule]?.pythonName
   //  TODO: add error creator if there is no labware in the stack
 
