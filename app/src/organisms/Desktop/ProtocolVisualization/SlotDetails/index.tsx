620f04efd8f8650bed121afc2ad2a76b5fc96ec7
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-12-03 08:01:38 -0800

    feat(app): module state in PV spotlight view (#20263)
    closes AUTh-2536

--- app/src/organisms/Desktop/ProtocolVisualization/SlotDetails/index.tsx
+++ app/src/organisms/Desktop/ProtocolVisualization/SlotDetails/index.tsx
@@ -1,12 +1,20 @@
-import { Divider, RobotInfoLabel, StyledText } from '@opentrons/components'
-import { getModuleDeckLabel } from '@opentrons/shared-data'
+import { useTranslation } from 'react-i18next'
+
+import {
+  Divider,
+  MODULE_ICON_NAME_BY_TYPE,
+  RobotInfoLabel,
+  StyledText,
+} from '@opentrons/components'
+import { getIsTiprack } from '@opentrons/shared-data'
 import { getFullStackFromLabwares } from '@opentrons/step-generation'
 
 import { SlotDetailsEmptyState } from '/app/molecules/SlotDetailsEmptyState'
 
 import { LabwareSlotContainer } from '../LabwareSlotContainer'
-import { ModuleSlotDetails } from '../ModuleSlotDetails'
-import { TrashSlotDetails } from '../TrashSlotDetails'
+import { ModuleContainer } from '../ModuleContainer'
+import { TipDisposalContainer } from '../TipDisposalContainer'
+import { TipPickupContainer } from '../TipPickupContainer'
 import styles from './slotdetails.module.css'
 
 import type {
@@ -36,46 +44,53 @@ export function SlotDetails(props: SlotDetailsProps): JSX.Element {
     pipetteEntities,
   } = invariantContext
   const { commands } = analysis
+  const { t } = useTranslation('protocol_visualization')
   const stackOfLabwareOnSlot = getFullStackFromLabwares(labware, slotId)
   const moduleOnSlot = Object.entries(modules).find(
     ([id, module]) => module.slot === slotId
   )
   const topMostLabwareOnSlot =
     stackOfLabwareOnSlot?.length > 1 ? stackOfLabwareOnSlot[0] : null
+  const isTopmostLabwareATiprack =
+    topMostLabwareOnSlot != null &&
+    getIsTiprack(labwareEntities[topMostLabwareOnSlot].def)
   const isTrashOnSlot =
     Object.values(trashBinEntities).some(
       trash => trash.location.split('cutout')[1] === slotId
     ) ||
     Object.values(wasteChuteEntities).some(
       trash => trash.location.split('cutout')[1] === slotId
-    )
+    ) ||
+    slotId === 'fixedTrash'
+
   return (
     <div className={styles.slot_container}>
       <div className={styles.slot_details}>
         <div className={styles.command_step_header}>
           <div className={styles.slot_detail_header}>
-            <StyledText desktopStyle="bodyLargeSemiBold">Slot</StyledText>
+            <StyledText desktopStyle="bodyLargeSemiBold">
+              {t('slot')}
+            </StyledText>
             <RobotInfoLabel
-              deckLabel={
-                moduleOnSlot != null
-                  ? getModuleDeckLabel(
-                      moduleEntities[moduleOnSlot[0]].type,
-                      slotId
-                    )
-                  : slotId
-              }
+              deckLabel={slotId === 'fixedTrash' ? t('fixedTrash') : slotId}
             />
+            {moduleOnSlot != null ? (
+              <RobotInfoLabel
+                iconName={
+                  MODULE_ICON_NAME_BY_TYPE[moduleEntities[moduleOnSlot[0]].type]
+                }
+              />
+            ) : null}
           </div>
         </div>
         <Divider />
-        {moduleOnSlot != null ? (
-          <ModuleSlotDetails
-            moduleId={moduleOnSlot[0]}
-            moduleEntities={moduleEntities}
-            moduleRobotState={modules}
+        {topMostLabwareOnSlot != null && isTopmostLabwareATiprack ? (
+          <TipPickupContainer
+            tiprackEntity={labwareEntities[topMostLabwareOnSlot]}
+            robotState={robotState}
           />
         ) : null}
-        {topMostLabwareOnSlot != null ? (
+        {topMostLabwareOnSlot != null && !isTopmostLabwareATiprack ? (
           <LabwareSlotContainer
             topLabwareOnSlotId={topMostLabwareOnSlot}
             labwareEntities={labwareEntities}
@@ -88,7 +103,14 @@ export function SlotDetails(props: SlotDetailsProps): JSX.Element {
           />
         ) : null}
         {isTrashOnSlot ? (
-          <TrashSlotDetails trashBinEntities={trashBinEntities} />
+          <TipDisposalContainer robotState={robotState} />
+        ) : null}
+        {moduleOnSlot != null ? (
+          <ModuleContainer
+            moduleId={moduleOnSlot[0]}
+            moduleEntities={moduleEntities}
+            moduleRobotState={modules}
+          />
         ) : null}
         {moduleOnSlot == null &&
         topMostLabwareOnSlot == null &&
