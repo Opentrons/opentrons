75128507224a974d29a14a0072411062a70a45c6
Max Marrone <max@opentrons.com>
2025-12-09 13:46:09 -0500

    fix(app): Always render labware centered over their underlying "slots" (#20285)

--- components/src/hardware-sim/alignment/AlignToModuleChildSlot.tsx
+++ components/src/hardware-sim/alignment/AlignToModuleChildSlot.tsx
@@ -3,7 +3,7 @@ import { getModuleParentOriginToChildSlotOrigin } from '@opentrons/shared-data'
 import type { PropsWithChildren } from 'react'
 import type { ModuleDefinition } from '@opentrons/shared-data'
 
-export type AlignControlToModuleProps = PropsWithChildren<{
+export type AlignToModuleChildSlotProps = PropsWithChildren<{
   /** Should match the `deckId` passed to the parent `<Module>`. */
   deckId: string | null
   /** Should match the `slotId` passed to the parent `<Module>`. */
@@ -16,25 +16,18 @@ export type AlignControlToModuleProps = PropsWithChildren<{
  * Applies an SVG transform to children so that their SVG origin is at the front-left
  * (-x, -y) corner of the "slot" atop a module.
  *
- * This is deprecated if the children are labware, because some modules do not really
- * have "slots," and in labware schema 3 we stopped pretending they do. This component
- * cannot correctly deal with labware schema 3. Use `<AlignLabwareToModule>` instead,
- * because it has a more sophisticated understanding of all the ways that labware and
- * modules can physically fit together.
- *
- * This component is intended for UI controls like an "add labware" button when you
- * hover over a module's labware area. We have historically written such components
- * to assume the "slot front-left" behavior described above.
+ * See `getModuleParentOriginToChildSlotOrigin()` for nuances of what "the 'slot' atop
+ * the module" means for things like the Thermocycler and Heater-Shaker.
  *
  * @example
  * <Module ...>
- *   <AlignControlToModule ...>
+ *   <AlignToModuleChildSlot ...>
  *     <SomeUIControl ... />
- *   </AlignLabwareToModule>
+ *   </AlignToModuleChildSlot>
  * </Module>
  */
-export function AlignControlToModule(
-  props: AlignControlToModuleProps
+export function AlignToModuleChildSlot(
+  props: AlignToModuleChildSlotProps
 ): JSX.Element {
   const { deckId, slotId, moduleDefinition, children } = props
   const { x, y } = getModuleParentOriginToChildSlotOrigin(
