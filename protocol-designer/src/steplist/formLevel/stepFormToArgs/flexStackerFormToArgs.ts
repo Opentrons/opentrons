daadb45d0ff0ed22dc47edf8e5ebc8bc89226a69
Rhyann Clarke <146747548+rclarke0@users.noreply.github.com>
2025-12-10 11:29:59 -0500

    feat(protocol-designer, step-generation): wire flexStacker formToArgs (#20334)
    # Overview
    
    This PR wires up the stacker step form fields to step generation by
    transforming forms to arguments and creates commands for all possible
    stacker steps
    
    ## Test Plan and Hands on Testing
    
    - wrote unit tests, will need to be smoke tested later
    
    ## Changelog
    
    - added flex stacker command types for step generation
    - connected commands to form

---
+++ protocol-designer/src/steplist/formLevel/stepFormToArgs/flexStackerFormToArgs.ts
@@ -0,0 +1,43 @@
+import type { FlexStackerArgs } from '@opentrons/step-generation'
+import type { HydratedFlexStackerFormData } from '../../../form-types'
+import type { GetCastFormData } from '../../fieldLevel'
+
+export const flexStackerFormToArgs = (
+  castFormData: GetCastFormData<HydratedFlexStackerFormData>
+): FlexStackerArgs | null => {
+  const {
+    fillLabwareUri,
+    flexStackerFormType,
+    interventionMessage,
+    moduleId,
+    fillQuantity,
+  } = castFormData
+  switch (flexStackerFormType) {
+    case 'empty':
+      return {
+        moduleId: moduleId!,
+        commandCreatorFnName: 'flexStackerEmpty',
+        interventionMessage: interventionMessage,
+      }
+    case 'fill':
+      return {
+        moduleId: moduleId!,
+        commandCreatorFnName: 'flexStackerFillItems',
+        fillLabwareUri: fillLabwareUri,
+        fillQuantity: fillQuantity,
+      }
+    case 'retrieve':
+      return {
+        moduleId: moduleId!,
+        commandCreatorFnName: 'flexStackerRetrieve',
+      }
+    case 'store':
+      return {
+        moduleId: moduleId!,
+        commandCreatorFnName: 'flexStackerStore',
+      }
+
+    default:
+      return null
+  }
+}
---
+++ protocol-designer/src/steplist/formLevel/stepFormToArgs/test/flexStackerFormToArgs.test.ts
@@ -0,0 +1,77 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { flexStackerFormToArgs } from '../flexStackerFormToArgs'
+
+import type {
+  FlexStackerFormType,
+  HydratedFlexStackerFormData,
+} from '/protocol-designer/form-types'
+import type { GetCastFormData } from '/protocol-designer/steplist/fieldLevel'
+
+describe('flexStackerFormToArgs', () => {
+  let baseFormData: GetCastFormData<HydratedFlexStackerFormData>
+  beforeEach(() => {
+    baseFormData = {
+      stepType: 'flexStacker',
+      id: 'stacker-id',
+      fillLabwareUri: null,
+      fillQuantity: null,
+      flexStackerFormType: null,
+      interventionMessage: null,
+      moduleId: 'moduleId',
+      stepName: 'step name',
+      stepDetails: 'stacker fill step',
+      stepNumber: 1,
+    }
+  })
+  it('returns flex stacker empty command creator', () => {
+    const formData = {
+      ...baseFormData,
+      flexStackerFormType: 'empty' as FlexStackerFormType,
+      interventionMessage: 'empty message',
+    }
+    const expected = {
+      moduleId: 'moduleId',
+      commandCreatorFnName: 'flexStackerEmpty',
+      interventionMessage: 'empty message',
+    }
+    expect(flexStackerFormToArgs(formData)).toEqual(expected)
+  })
+  it('returns flex stacker fill command creator', () => {
+    const formData = {
+      ...baseFormData,
+      flexStackerFormType: 'fill' as FlexStackerFormType,
+      fillLabwareUri: 'labware',
+      fillQuantity: 1,
+    }
+    const expected = {
+      moduleId: 'moduleId',
+      commandCreatorFnName: 'flexStackerFillItems',
+      fillLabwareUri: 'labware',
+      fillQuantity: 1,
+    }
+    expect(flexStackerFormToArgs(formData)).toEqual(expected)
+  })
+  it('returns flex stacker retrieve command creator', () => {
+    const formData = {
+      ...baseFormData,
+      flexStackerFormType: 'retrieve' as FlexStackerFormType,
+    }
+    const expected = {
+      moduleId: 'moduleId',
+      commandCreatorFnName: 'flexStackerRetrieve',
+    }
+    expect(flexStackerFormToArgs(formData)).toEqual(expected)
+  })
+  it('returns flex stacker store command creator', () => {
+    const formData = {
+      ...baseFormData,
+      flexStackerFormType: 'store' as FlexStackerFormType,
+    }
+    const expected = {
+      moduleId: 'moduleId',
+      commandCreatorFnName: 'flexStackerStore',
+    }
+    expect(flexStackerFormToArgs(formData)).toEqual(expected)
+  })
+})
