e7e3c0be04b3b7176385e95fd1a0259ae6147f77
Sara Kowalski <sara.kowalski@opentrons.com>
2025-12-10 14:17:48 -0500

    create pd page for moving labware

---
+++ e2e-testing/automation/pd_pages/move_labware.py
@@ -0,0 +1,10 @@
+"Move Labware with/withut Gripper"
+
+from playwright.sync_api import Page, expect
+
+from .base_page import BasePage
+
+class MnaualMove(BasePage):
+
+
+class GripperMove(BasePage):
