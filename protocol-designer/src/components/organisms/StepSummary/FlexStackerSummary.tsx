363ff01aef60ac1750baa73eefe4359a4aff2ec4
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-12-10 09:36:24 -0500

    use flex stacker types

--- protocol-designer/src/components/organisms/StepSummary/FlexStackerSummary.tsx
+++ protocol-designer/src/components/organisms/StepSummary/FlexStackerSummary.tsx
@@ -1,3 +1,9 @@
+import {
+  FLEX_STACKER_EMPTY,
+  FLEX_STACKER_FILL,
+  FLEX_STACKER_RETRIEVE,
+  FLEX_STACKER_STORE,
+} from '../../../constants'
 import { StyledTrans } from './StyledTrans'
 
 import type { LabwareEntities } from '@opentrons/step-generation'
@@ -19,35 +25,37 @@ export function FlexStackerSummary(
   let stepSummaryContent: JSX.Element | null = null
 
   switch (flexStackerFormType) {
-    case 'empty': {
+    case FLEX_STACKER_EMPTY: {
       stepSummaryContent = (
-        <StyledTrans i18nKey="protocol_steps:flex_stacker.empty" />
+        <StyledTrans
+          i18nKey={`protocol_steps:flex_stacker.${FLEX_STACKER_EMPTY}`}
+        />
       )
       break
     }
-    case 'fill': {
+    case FLEX_STACKER_FILL: {
       stepSummaryContent = (
         <StyledTrans
-          i18nKey="protocol_steps:flex_stacker.fill"
+          i18nKey={`protocol_steps:flex_stacker.${FLEX_STACKER_FILL}`}
           tagText={labwareName}
           tagText2={fillQuantity}
         />
       )
       break
     }
-    case 'retrieve': {
+    case FLEX_STACKER_RETRIEVE: {
       stepSummaryContent = (
         <StyledTrans
-          i18nKey="protocol_steps:flex_stacker.retrieve"
+          i18nKey={`protocol_steps:flex_stacker.${FLEX_STACKER_RETRIEVE}`}
           tagText={labwareName}
         />
       )
       break
     }
-    case 'store': {
+    case FLEX_STACKER_STORE: {
       stepSummaryContent = (
         <StyledTrans
-          i18nKey="protocol_steps:flex_stacker.store"
+          i18nKey={`protocol_steps:flex_stacker.${FLEX_STACKER_STORE}`}
           tagText={labwareName}
         />
       )
