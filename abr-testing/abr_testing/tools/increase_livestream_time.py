726ae12e3b6135c656bc85a6d8315b3c8a7438f2
Rhyann Clarke <rhyann.clarke@opentrons.com>
2025-12-11 14:09:13 -0500

    feat(abr-testing): script to increase livestream time

---
+++ abr-testing/abr_testing/tools/increase_livestream_time.py
@@ -0,0 +1,29 @@
+"""Increase minimum duration on the livestream video tool."""
+from .make_push import get_robot_ips
+import subprocess
+
+
+def edit_livestream_length(ip: str, time: str) -> None:
+    """SSH into robot and increase livestream time."""
+    ssh_command = f"""
+    ssh root@{ip} '
+        mount -o remount,rw / &&
+        sed -i "96s/.*/            hls_playlist_length {time}s;/" /etc/nginx/nginx.conf &&
+        systemctl daemon-reload &&
+        systemctl restart nginx
+    '
+    """
+    try:
+        subprocess.run(ssh_command, shell=True, check=True)
+        print(f"Successfully updated livestream length on {ip}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to update {ip}: {e}")
+
+
+if __name__ == "__main__":
+    robot_to_push = get_robot_ips()
+    livestream_time = input(
+        "Enter desired livestream length in seconds (e.g., 30): "
+    ).strip()
+    for robot_ip in robot_to_push:
+        edit_livestream_length(robot_ip, livestream_time)
