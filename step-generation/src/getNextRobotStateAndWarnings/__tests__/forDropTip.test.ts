2c32716ea8d8a8e97b26963f75a89eeaf980ab3f
Max Marrone <max@opentrons.com>
2025-12-12 15:26:24 -0500

    refactor(step-generation): Split up tests into local subdirectories (#20365)
    ## Overview
    
    step-generation's source is organized into a bunch of subdirectories,
    like `/utils/`, `/commandCreators/atomic/`,
    `/commandCreators/compound/`, etc., but its unit tests were all living
    together in one flat top-level directory. With a bunch of
    similarly-named test files, that was getting difficult to follow.
    
    As discussed in
    https://github.com/Opentrons/opentrons/pull/20349#discussion_r2610861235,
    this splits the monolithic `__tests__` directory into a bunch of
    distributed "local" `__test__` directories, following the pattern
    elsewhere in our codebase.
    
    ## Test Plan and Hands on Testing
    
    * [x] Compared the test count before and after to make sure I didn't
    accidentally remove anything.

---
+++ step-generation/src/getNextRobotStateAndWarnings/__tests__/forDropTip.test.ts
@@ -0,0 +1,103 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { makeImmutableStateUpdater } from '../../__utils__'
+import {
+  DEFAULT_PIPETTE,
+  getInitialRobotStateStandard,
+  makeContext,
+} from '../../fixtures'
+import { forDropTip as _forDropTip } from '../forDropTip'
+
+import type { InvariantContext, RobotState } from '../../types'
+
+const forDropTip = makeImmutableStateUpdater(_forDropTip)
+
+const TIPRACK_ID = 'tiprack1Id'
+
+describe('dropTip', () => {
+  let invariantContext: InvariantContext
+  let prevRobotState: RobotState
+  beforeEach(() => {
+    invariantContext = makeContext()
+    prevRobotState = getInitialRobotStateStandard(invariantContext)
+  })
+
+  describe('replaceTip: single channel', () => {
+    it('drop tip if there is a tip', () => {
+      prevRobotState = {
+        ...prevRobotState,
+        tipState: {
+          pipettes: {
+            p300SingleId: {
+              hasTip: true,
+              tiprackURI: 'tiprackId',
+            },
+            p300MultiId: {
+              hasTip: true,
+              tiprackURI: 'tiprackId',
+            },
+          },
+          tipracks: {
+            tiprack1Id: {},
+          } as any,
+        },
+      }
+      const params = {
+        pipetteId: DEFAULT_PIPETTE,
+        labwareId: TIPRACK_ID,
+        wellName: 'A1',
+      }
+      const result = forDropTip(params, invariantContext, prevRobotState)
+      expect(result.robotState.tipState.pipettes).toEqual({
+        p300SingleId: {
+          hasTip: false,
+          tiprackURI: null,
+        },
+        p300MultiId: {
+          hasTip: true,
+          tiprackURI: 'tiprackId',
+        },
+      })
+    })
+    // TODO: IL 2019-11-20
+    it.todo('no tip on pipette')
+  })
+  describe('Multi-channel dropTip', () => {
+    it('drop tip when there are tips', () => {
+      prevRobotState = {
+        ...prevRobotState,
+        tipState: {
+          pipettes: {
+            p300SingleId: {
+              hasTip: true,
+              tiprackURI: 'tiprackId',
+            },
+            p300MultiId: {
+              hasTip: true,
+              tiprackURI: 'tiprackId',
+            },
+          },
+          tipracks: {
+            [TIPRACK_ID]: {},
+          } as any,
+        },
+      }
+      const params = {
+        pipetteId: 'p300MultiId',
+        labwareId: TIPRACK_ID,
+        wellName: 'A1',
+      }
+      const result = forDropTip(params, invariantContext, prevRobotState)
+      expect(result.robotState.tipState.pipettes).toEqual({
+        p300SingleId: {
+          hasTip: true,
+          tiprackURI: 'tiprackId',
+        },
+        p300MultiId: {
+          hasTip: false,
+          tiprackURI: null,
+        },
+      })
+    })
+  })
+})
