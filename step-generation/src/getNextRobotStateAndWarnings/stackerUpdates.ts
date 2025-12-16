cba8634e7271843e8f2a9b63adda404e53da8680
Jethary <jethary@opentrons.com>
2025-12-16 06:45:29 -0800

    continuous stack

--- step-generation/src/getNextRobotStateAndWarnings/stackerUpdates.ts
+++ step-generation/src/getNextRobotStateAndWarnings/stackerUpdates.ts
@@ -183,19 +183,24 @@ export const forFlexStackerStore = (
       moduleState.labwareOnShuttle = null
 
       // build trivial stacks for stored labware group elements
+      let previousTopLabwareId: string | null = null
+
       for (const labwarePoolKey of BOTTOM_UP_LABWARE_POOL_KEYS) {
         const labwareId =
           labwareOnShuttle[
             labwarePoolKey as keyof FlexStackerStoredLabwareGroup
           ]
-        if (labwareId != null) {
-          robotState.labware[labwareId].stack = [
-            labwareId,
-            HOPPER_STACKER_LOCATION,
-            moduleId,
-            moduleSlot,
-          ]
-        }
+
+        if (labwareId == null) continue
+
+        const baseStack =
+          previousTopLabwareId != null
+            ? robotState.labware[previousTopLabwareId].stack
+            : [HOPPER_STACKER_LOCATION, moduleId, moduleSlot]
+
+        robotState.labware[labwareId].stack = [labwareId, ...baseStack]
+
+        previousTopLabwareId = labwareId
       }
     }
   }
