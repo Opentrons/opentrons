f046ee912539094e5bddc3793e86001037d2b432
koji <baxin1919@gmail.com>
2025-12-17 09:58:48 -0500

    fix(app): fix pipette container chip
    fix pipette container chip
    
    close RQA-4982

--- components/src/atoms/Chip/index.tsx
+++ components/src/atoms/Chip/index.tsx
@@ -102,7 +102,7 @@ export function Chip(props: ChipProps): JSX.Element {
         return '0.75rem'
     }
   }
-  const smallSize = getIconSize(iconName)
+  const smallSize = getIconSize(icon)
 
   return (
     <Flex
