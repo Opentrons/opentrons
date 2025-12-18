a75c63949b92c87e030c293d608647cbfdfb9567
koji <baxin1919@gmail.com>
2025-12-18 13:14:26 -0500

    fix a couple of pd bugs

--- protocol-designer/src/components/organisms/DefineLiquidsModal/index.tsx
+++ protocol-designer/src/components/organisms/DefineLiquidsModal/index.tsx
@@ -43,6 +43,7 @@ import { LiquidClassDropdown } from './LiquidClassDropdown'
 import { LiquidColorPicker } from './LiquidColorPicker'
 
 import type { ThunkDispatch } from 'redux-thunk'
+import type { MouseEvent } from 'react'
 import type { Ingredient } from '@opentrons/step-generation'
 import type { BaseState } from '/protocol-designer/types'
 
@@ -271,7 +272,8 @@ export function DefineLiquidsModal(
                   </StyledText>
 
                   <LiquidIcon
-                    onClick={() => {
+                    onClick={(e: MouseEvent<HTMLDivElement>) => {
+                      e.preventDefault()
                       setShowColorPicker(prev => !prev)
                     }}
                     color={color}
