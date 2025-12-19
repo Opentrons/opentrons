8750ac8ec6f835fe99b23aafccaedbc42fa0bd11
alexjoel42 <alexcopperman1@gmail.com>
2025-12-19 10:47:38 -0500

    updated snapshots

--- analyses-snapshot-testing/tests/custom_json_snapshot_extension.py
+++ analyses-snapshot-testing/tests/custom_json_snapshot_extension.py
@@ -40,7 +40,7 @@ class CustomJSONSnapshotExtension(JSONSnapshotExtension):
             "stackLabwareId",
             "lid_id",
             "taskId",
-            "taskIds",
+            "task_ids",
         ]
         self.timestamp_keys_to_replace = [
             "createdAt",
