eb8a2f32d733ded8f4bc45aa6ef71040d15a7e72
Seth Foster <seth@opentrons.com>
2025-12-24 10:53:02 -0500

    chore: switch mainline python projects to ruff
    Ruff ( https://docs.astral.sh/ruff ) is a modern code checking tool that
    combines many formatting and style/correctness linting tools and is
    really fast. It has a lot of options for things that have previously
    been flake8 plugins or separate tools, and it's configured in
    pyproject.toml instead of a separate file.
    
    It also has a lot of options for things we've wanted forever but haven't
    found a good tool for yet - like import sorting!
    
    Its formatting codestyle philosophy is very similar to black - not a lot
    of options, which is good - and so is its formatting output... but its
    formatting output is not _identical_ to black's, and so there is a big
    old diff here that is 99% automatic and 1% "that type: ignore or noqa is
    in the wrong place now".

--- shared-data/Makefile-python.mk
+++ shared-data/Makefile-python.mk
@@ -96,12 +96,11 @@ sdist: $(py_sources) $(json_sources)
 .PHONY: lint
 lint: $(py_sources)
 	$(python) -m mypy python/opentrons_shared_data python_tests tools
-	$(python) -m black --check python/opentrons_shared_data python_tests tools
-	$(python) -m flake8 python/opentrons_shared_data python_tests tools
+	$(ruff) check python/opentrons_shared_data python_tests tools
 
 .PHONY: format
 format:
-	$(python) -m black python/opentrons_shared_data python_tests tools
+	$(ruff) format python/opentrons_shared_data python_tests tools
 
 .PHONY: push-no-restart
 push-no-restart: wheel
