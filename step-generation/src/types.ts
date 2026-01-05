f4bc5ef6a44d1a321db2cbbc6538ca77da3fdee5
Jethary <jethary@opentrons.com>
2026-01-05 12:26:16 -0800

    weee

--- step-generation/src/types.ts
+++ step-generation/src/types.ts
@@ -725,7 +725,7 @@ export interface FlexStackerFillItemsArgs extends CommonArgs {
   moduleId: string
   commandCreatorFnName: 'flexStackerFillItems'
   fillLabwareUri: string | null
-  fill: string[]
+  fillLabwareIds: string[]
   interventionMessage: string | null
 }
 export interface FlexStackerStoreArgs extends CommonArgs {
