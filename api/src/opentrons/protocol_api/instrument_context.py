901a2757fe85101c6c2497ee74f2a0102e801fea
David Chau <david.chau@opentrons.com>
2026-01-12 16:47:00 -0500

    Make lint happy

--- api/src/opentrons/protocol_api/instrument_context.py
+++ api/src/opentrons/protocol_api/instrument_context.py
@@ -1676,7 +1676,7 @@ class InstrumentContext(publisher.CommandPublisher):
         return self
 
     @requires_version(2, 0)
-    def drop_tip(
+    def drop_tip(  # noqa: C901
         self,
         location: Optional[
             Union[
