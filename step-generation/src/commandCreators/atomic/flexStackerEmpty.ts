74c42093b5dadaf40a697e1e44939271381a389d
ncdiehl11 <ndiehl@opentrons.com>
2026-01-12 16:45:40 -0500

    fix(step-generation): remove gripper check for flex stacker empty
    Remove irrelevant check for gripper in Flex stacker empty step generation command creator

--- step-generation/src/commandCreators/atomic/flexStackerEmpty.ts
+++ step-generation/src/commandCreators/atomic/flexStackerEmpty.ts
@@ -8,18 +8,14 @@ import type { CommandCreator, CommandCreatorError } from '../../types'
 export const flexStackerEmpty: CommandCreator<
   FlexStackerEmptyCreateCommand['params']
 > = (args, invariantContext, prevRobotState) => {
-  const { gripperEntities, moduleEntities } = invariantContext
+  const { moduleEntities } = invariantContext
   const flexStackerState = flexStackerStateGetter(prevRobotState, args.moduleId)
-  const hasGripperEntity = Object.keys(gripperEntities).length > 0
 
   const errors: CommandCreatorError[] = []
   if (args.moduleId == null || flexStackerState == null) {
     errors.push(errorCreators.missingModuleError())
   }
 
-  if (!hasGripperEntity) {
-    errors.push(errorCreators.flexStackerNoGripper())
-  }
   if (errors.length > 0) {
     return { errors }
   }
