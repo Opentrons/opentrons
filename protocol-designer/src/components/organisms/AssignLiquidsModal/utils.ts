a87c6b36958c60d1b2cdc4bc29d09c5aeef37e54
ncdiehl11 <ndiehl@opentrons.com>
2026-01-14 15:48:24 -0500

    format

--- protocol-designer/src/components/organisms/AssignLiquidsModal/utils.ts
+++ protocol-designer/src/components/organisms/AssignLiquidsModal/utils.ts
@@ -1,24 +1,34 @@
-import type { ModuleOnDeck } from "/protocol-designer/step-forms";
-import { FLEX_STACKER_MODULE_TYPE, LabwareDefinition } from "@opentrons/shared-data";
-import { FlexStackerModuleState } from "@opentrons/step-generation";
+import { FLEX_STACKER_MODULE_TYPE } from '@opentrons/shared-data'
 
-export const getStackerModuleStateFromSlot = (args: { slot: string; modules: Record<string, ModuleOnDeck> }): FlexStackerModuleState | null => {
-    const { slot, modules } = args
-    const stackerModule =
-      Object.values(modules).find(
-        ({ slot: moduleSlot, moduleState }) =>
-          moduleSlot === slot && moduleState.type === FLEX_STACKER_MODULE_TYPE
-      ) ?? null
-    return stackerModule != null && stackerModule.moduleState.type === FLEX_STACKER_MODULE_TYPE ? stackerModule.moduleState : null
-  }
-  
-  export const getTopDownPrimaryLabwareInHopper = (args: { slot: string; modules: Record<string, ModuleOnDeck> }): string[] => {
-    const { slot, modules } = args
-    const stackerModuleState = getStackerModuleStateFromSlot({ slot, modules })
-    const bottomUpLabwareGroups =
-      stackerModuleState?.labwareInHopper
-      ?? []
-    return bottomUpLabwareGroups.map(group => group.primaryLabwareId).reverse()
-  }
-  
-  export const getStackLimitFromDef = (labwareDef: LabwareDefinition): number => labwareDef.stackLimit ?? 0
+import type { LabwareDefinition } from '@opentrons/shared-data'
+import type { FlexStackerModuleState } from '@opentrons/step-generation'
+import type { ModuleOnDeck } from '/protocol-designer/step-forms'
+
+export const getStackerModuleStateFromSlot = (args: {
+  slot: string
+  modules: Record<string, ModuleOnDeck>
+}): FlexStackerModuleState | null => {
+  const { slot, modules } = args
+  const stackerModule =
+    Object.values(modules).find(
+      ({ slot: moduleSlot, moduleState }) =>
+        moduleSlot === slot && moduleState.type === FLEX_STACKER_MODULE_TYPE
+    ) ?? null
+  return stackerModule != null &&
+    stackerModule.moduleState.type === FLEX_STACKER_MODULE_TYPE
+    ? stackerModule.moduleState
+    : null
+}
+
+export const getTopDownPrimaryLabwareInHopper = (args: {
+  slot: string
+  modules: Record<string, ModuleOnDeck>
+}): string[] => {
+  const { slot, modules } = args
+  const stackerModuleState = getStackerModuleStateFromSlot({ slot, modules })
+  const bottomUpLabwareGroups = stackerModuleState?.labwareInHopper ?? []
+  return bottomUpLabwareGroups.map(group => group.primaryLabwareId).reverse()
+}
+
+export const getStackLimitFromDef = (labwareDef: LabwareDefinition): number =>
+  labwareDef.stackLimit ?? 0
