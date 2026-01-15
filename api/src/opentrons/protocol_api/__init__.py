d01722f5ff7bdf6b2bdff4b966cd098985afb5ed
vegano1 <brayan.almonte@gmail.com>
2026-01-15 16:43:59 -0500

    save

--- api/src/opentrons/protocol_api/__init__.py
+++ api/src/opentrons/protocol_api/__init__.py
@@ -5,3 +5,106 @@ control an OT-2 or Flex robot.
 
 """
 
+from opentrons.protocols.api_support.definitions import (
+    MAX_SUPPORTED_VERSION,
+    MIN_SUPPORTED_VERSION,
+    MIN_SUPPORTED_VERSION_FOR_FLEX,
+)
+from opentrons.protocols.parameters.exceptions import (
+    RuntimeParameterRequired as RuntimeParameterRequiredError,
+)
+from opentrons.protocols.parameters.csv_parameter_interface import CSVParameter
+from .tasks import Task
+from .protocol_context import ProtocolContext
+from .deck import Deck
+from .robot_context import RobotContext
+from .instrument_context import InstrumentContext
+from .labware import Labware, Well
+from .module_contexts import (
+    ModuleContext,
+    ThermocyclerContext,
+    MagneticModuleContext,
+    TemperatureModuleContext,
+    HeaterShakerContext,
+    MagneticBlockContext,
+    AbsorbanceReaderContext,
+    FlexStackerContext,
+)
+from .disposal_locations import TrashBin, WasteChute
+from ._liquid import Liquid, LiquidClass
+from ._types import (
+    OffDeckType,
+    OFF_DECK,
+    PLUNGER_BLOWOUT,
+    PLUNGER_TOP,
+    PLUNGER_BOTTOM,
+    PLUNGER_DROPTIP,
+    ASPIRATE_ACTION,
+    DISPENSE_ACTION,
+    BLOWOUT_ACTION,
+)
+from ._nozzle_layout import (
+    COLUMN,
+    PARTIAL_COLUMN,
+    SINGLE,
+    ROW,
+    ALL,
+)
+from ._parameters import Parameters
+from ._parameter_context import ParameterContext
+
+from .create_protocol_context import (
+    create_protocol_context,
+    ProtocolEngineCoreRequiredError,
+)
+
+__all__ = [
+    "MAX_SUPPORTED_VERSION",
+    "MIN_SUPPORTED_VERSION",
+    "MIN_SUPPORTED_VERSION_FOR_FLEX",
+    "ProtocolContext",
+    "Deck",
+    "ModuleContext",
+    "RobotContext",
+    "InstrumentContext",
+    "TemperatureModuleContext",
+    "MagneticModuleContext",
+    "ThermocyclerContext",
+    "HeaterShakerContext",
+    "MagneticBlockContext",
+    "AbsorbanceReaderContext",
+    "FlexStackerContext",
+    "VacuumModuleContext",
+    "ParameterContext",
+    "Labware",
+    "TrashBin",
+    "WasteChute",
+    "Well",
+    "Liquid",
+    "LiquidClass",
+    "Parameters",
+    # Partial Tip types
+    "COLUMN",
+    "PARTIAL_COLUMN",
+    "SINGLE",
+    "ROW",
+    "ALL",
+    # Deck location types
+    "OffDeckType",
+    "OFF_DECK",
+    # Pipette plunger types
+    "PLUNGER_BLOWOUT",
+    "PLUNGER_TOP",
+    "PLUNGER_BOTTOM",
+    "PLUNGER_DROPTIP",
+    "ASPIRATE_ACTION",
+    "DISPENSE_ACTION",
+    "BLOWOUT_ACTION",
+    "RuntimeParameterRequiredError",
+    "CSVParameter",
+    # Concurrent task types
+    "Task",
+    # For internal Opentrons use only:
+    "create_protocol_context",
+    "ProtocolEngineCoreRequiredError",
+]
