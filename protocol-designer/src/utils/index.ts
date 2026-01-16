41b58d3c8142c2b66525dfd49524036f409fe074
David Chau <david.chau@opentrons.com>
2026-01-16 15:14:16 -0500

    Update comment

--- protocol-designer/src/utils/index.ts
+++ protocol-designer/src/utils/index.ts
@@ -305,6 +305,7 @@ export const getDefaultBlowoutFlowRate = (
   const liquidsObject = isInLowVolumeMode
     ? liquids.lowVolumeDefault
     : liquids.default
+  // if the tiprack is not in the pipette's supportedTips, we'll just return null:
   return liquidsObject.supportedTips[
     `t${tiprackDef.wells.A1.totalLiquidVolume}`
   ]?.defaultBlowOutFlowRate.default
