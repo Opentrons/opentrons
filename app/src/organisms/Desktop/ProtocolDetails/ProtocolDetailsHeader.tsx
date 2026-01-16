b9b11d8ea0b67342c8a9f0f4ce978e29fd99d848
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2026-01-16 12:33:00 -0800

    feat(app): keep track of numberOfAtomicCommands in PV mixpanel (#20613)
    closes AUTH-2706

--- app/src/organisms/Desktop/ProtocolDetails/ProtocolDetailsHeader.tsx
+++ app/src/organisms/Desktop/ProtocolDetails/ProtocolDetailsHeader.tsx
@@ -78,7 +78,7 @@ export function ProtocolDetailsHeader({
   const navigate = useNavigate()
   const trackEvent = useTrackEvent()
   const [isReadMore, setIsReadMore] = useState(true)
-
+  const numberOfAtomicCommands = mostRecentAnalysis?.commands.length ?? 0
   const protocolDescription = mostRecentAnalysis?.metadata.description ?? ''
   const slicedDescription = protocolDescription.slice(0, MAX_DESCRIPTION_LENGTH)
   const isDescriptionTruncated =
@@ -135,7 +135,10 @@ export function ProtocolDetailsHeader({
   const handleClickTimeline = (): void => {
     trackEvent({
       name: ANALYTICS_LAUNCH_PROTOCOL_VISUALIZATION,
-      properties: { sourceLocation: 'protocol details header' },
+      properties: {
+        sourceLocation: 'protocol details header',
+        numberOfAtomicCommands,
+      },
     })
     navigate(`/protocols/${protocolKey}/visualization`)
   }
