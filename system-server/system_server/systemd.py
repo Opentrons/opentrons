28b399531d1f16fc5def4bc586066f0cbe285a0d
Max Marrone <max@opentrons.com>
2026-01-20 13:25:49 -0500

    Fix a wrong comment in system-server.
    This was copy-pasted from update-server. update-server, not system-server, sets the robot's hostname.

--- system-server/system_server/systemd.py
+++ system-server/system_server/systemd.py
@@ -17,12 +17,6 @@ try:
             "SYSLOG_IDENTIFIER": topic_name,
         }
 
-    # By using sd_notify
-    # (https://www.freedesktop.org/software/systemd/man/sd_notify.html)
-    # and type=notify in the unit file, we can prevent systemd from starting
-    # dependent services until we actually say we're ready. By calling this
-    # after we change the hostname, we make anything with an After= on us
-    # be guaranteed to see the correct hostname
     def notify_up() -> None:
         """Notify systemd that the service is up."""
         systemd.daemon.notify("READY=1")
