65cbed4df1a16f8e307ceb787717a4a2247e04a3
Max Marrone <max@opentrons.com>
2026-01-21 10:49:06 -0500

    Rip out broken logging config, for now.

--- auth-server/auth_server/__main__.py
+++ auth-server/auth_server/__main__.py
@@ -3,11 +3,8 @@
 import argparse
 import logging
 from dataclasses import dataclass
-from typing import Literal, TypedDict
 
 import uvicorn
-
-from . import systemd
 
 _log = logging.getLogger(__name__)
 
@@ -65,11 +62,6 @@ def _parse_args() -> _ParsedArgs:
 
 if __name__ == "__main__":
     args = _parse_args()
-
-    # todo(mm, 2026-01-20): This configure_logging() call is copied from system-server,
-    # but both here and there, it seems to immediately get clobbered by uvicorn.run()
-    # setting its own logging config.
-    systemd.configure_logging(level=args.log_level)
 
     if args.uds is not None:
         _log.info(f"Starting auth server on {args.uds}.")
