7f4826bc8563b77f6997d73ebc509768a8afafdb
Sara Kowalski <sara.kowalski@opentrons.com>
2026-01-22 14:16:58 -0500

    start flex stacker PD playwright

---
+++ e2e-testing/automation/pd_pages/flex_stacker.py
@@ -0,0 +1,13 @@
+import re
+from typing import Literal
+
+from playwright.sync_api import Page
+
+from .base_page import BasePage
+
+
+class PlateReaderPage(BasePage):
+    """Main transfer page for configuring transfer steps."""
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
---
+++ e2e-testing/tests/pd/test_pd_flex_stacker.py
@@ -0,0 +1,32 @@
+import pytest
+from playwright.sync_api import Page
+from automation.pd_pages.create_protocol_wizard import CreateProtocolWizard
+
+
+
+@pytest.mark.pdE2E
+@pytest.mark.slow
+def test_flex_stacker(page: Page, base_url: str) -> None:
+    create_protocol = CreateProtocolWizard(page)
+
+#before each maybe? test.beforeEach(async ({ page }) => {}
+    ##Creating a new protocol
+    ##Test adding 4 Stackers, with a waste chute
+#before each maybe? test.beforeEach(async ({ page }) => {}
+    ##Creating a new protocol
+    ##Test adding 4 Stackers, without a waste chute
+#before each maybe? test.beforeEach(async ({ page }) => {}
+    ##Creating a new protocol
+    ##Test adding 3 stackers and a trashbin
+#before each maybe? test.beforeEach(async ({ page }) => {}
+    ##Creating a new protocol
+    ##Test adding 3 stackers and an absorbance reader
+#before each maybe? test.beforeEach(async ({ page }) => {}
+    ##Creating a new protocol
+    ##Test adding 2 stackers and an absorbnace reader and a expansion slot
+    ##Add labware to stacker
+        ##select labware, check off tip rack lid, quantity
+        ##replace labware is visible (app eyes)
+    ##Add step for stacker
+        ##Select Stacker by location
+    ##check to make surethat the stacker is highlighted
