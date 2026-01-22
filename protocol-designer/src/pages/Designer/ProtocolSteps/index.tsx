cabe0b132c453eff0d98b7064467376f8d540c11
Rhyann Clarke <rhyann.clarke@opentrons.com>
2026-01-22 17:00:29 -0500

    fix(protocol-designer): export button always at bottom

--- protocol-designer/src/pages/Designer/ProtocolSteps/index.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/index.tsx
@@ -263,7 +263,7 @@ export function ProtocolSteps({
                 justifyContent={JUSTIFY_END}
                 position={POSITION_ABSOLUTE}
                 right={SPACING.spacing24}
-                zIndex={showDefineLiquidModal ? 1 : 1000}
+                zIndex={0}
               >
                 <ExportButton onClick={handleExportClick} />
               </Flex>
