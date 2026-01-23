45df5d1bd78d42debc57b814d2401bb44461ff3b
Sara Kowalski <sara.kowalski@opentrons.com>
2026-01-23 17:01:10 -0500

    fix redundant use of confirm in function, and fixed test associated

--- e2e-testing/tests/pd/test_pd_plate_reader.py
+++ e2e-testing/tests/pd/test_pd_plate_reader.py
@@ -21,6 +21,7 @@ def test_flex_absorbance_reader_setup(page: Page, base_url: str) -> None:
     # Configure deck hardware
     ## Snapshot: Validate Absorbance Plate Reader module configuration option
     plate_reader_page.configure_module("B3", "Absorbance Plate Reader Module GEN1")
+    plate_reader_page.button_selection("Confirm")
     create_protocol.name_protocol("test")
     plate_reader_page.button_selection("Confirm")
 
