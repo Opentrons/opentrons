f3530d1b5077760a08ecfb0f35ea0ee98f20b130
Sara Kowalski <sara.kowalski@opentrons.com>
2026-01-23 14:56:59 -0500

    testing for configuring 4 stackers with waste chute

--- e2e-testing/tests/pd/test_pd_flex_stacker.py
+++ e2e-testing/tests/pd/test_pd_flex_stacker.py
@@ -1,15 +1,28 @@
 import pytest
 from playwright.sync_api import Page
-from automation.pd_pages.create_protocol_wizard import CreateProtocolWizard
-from utility import create_new_protocol
+from utility import create_new_protocol, create_new_protocol_from_landing_page
+from automation.pd_pages.flex_stacker import FlexStackerPage
+from automation.pd_pages.protocol_editor_page import ProtocolEditorPage
 
 
 
 @pytest.mark.pdE2E
 @pytest.mark.slow
 def test_flex_stacker(page: Page, base_url: str) -> None:
-    
+    flex_stacker_page = FlexStackerPage(page)
+    protocol_editor = ProtocolEditorPage(page)
+
+
+
+    create_new_protocol_from_landing_page(page)
+    flex_stacker_page.configure_stacker("A4")
+    flex_stacker_page.configure_stacker("B4")
+    flex_stacker_page.configure_stacker("C4")
+    flex_stacker_page.configure_stacker("D4")
+    flex_stacker_page.start_new_create_protocol()
     create_new_protocol(page)
+    print("Configured 4 Flex Stackers with waste chute")
+    page.wait_for_timeout(10000)
 #before each maybe? test.beforeEach(async ({ page }) => {}
     ##Creating a new protocol
     ##Test adding 4 Stackers, with a waste chute
