080fa0b1128d3d5bb2be2e4ebbb09a86c3ce4271
Sara Kowalski <sara.kowalski@opentrons.com>
2026-01-26 11:48:50 -0500

    removed redundant functions found elsewhere in automation folder

--- e2e-testing/automation/pd_pages/flex_stacker.py
+++ e2e-testing/automation/pd_pages/flex_stacker.py
@@ -12,21 +12,6 @@ class FlexStackerPage(BasePage):
     def __init__(self, page: Page) -> None:
         super().__init__(page)
 
-    def configure_stacker(self, slot: Literal["A4", "B4", "C4", "D4"]) -> None:
-        if slot == "A4":
-            self.page.get_by_test_id("fakeA4").click()
-        elif slot == "B4":
-            self.page.get_by_test_id("fakeB4").click()
-        elif slot == "C4":
-            self.page.get_by_test_id("fakeC4").click()
-        elif slot == "D4":
-            self.page.get_by_test_id("fakeD4").click()
-        self.page.get_by_test_id("Modules").click()
-        self.page.get_by_test_id("Flex Stacker Module GEN1").click()
-  
-    def start_new_create_protocol(self) -> None:
-        self.page.on("dialog", lambda dialog: dialog.accept())
-        self.page.get_by_test_id("basic_button_Create new").click()
 
 
 
