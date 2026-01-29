14581be4269237f3a6bf7338d85116925bc11ef3
Ryan Howard <ryan.howard@opentrons.com>
2026-01-29 16:17:00 -0500

    add a barcode simulator.

--- api/src/opentrons/drivers/barcode_scanner/__init__.py
+++ api/src/opentrons/drivers/barcode_scanner/__init__.py
@@ -1,9 +1,11 @@
 from .abstract import AbstractBarcodeScannerDriver
 from .types import BarcodeModuleInfo, LEDProfile, SoundProfile
+from .simulator import BarcodeSimulatorDriver
 
 __all__ = [
     "AbstractBarcodeScannerDriver",
     "SoundProfile",
     "LEDProfile",
     "BarcodeModuleInfo",
+    "BarcodeSimulatorDriver",
 ]
