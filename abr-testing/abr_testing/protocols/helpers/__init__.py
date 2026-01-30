71e2769cebb0288fc6a0c98c4cb777bfd5664ba5
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-01-30 14:59:53 -0500

    'I'm the worker' explaination improved and simplified

--- abr-testing/abr_testing/protocols/helpers/__init__.py
+++ abr-testing/abr_testing/protocols/helpers/__init__.py
@@ -3,15 +3,8 @@ import os
 import subprocess
 
 """
-Pattern used:
-
-1. Package is imported
-2. Subprocess with infinite loop is started ("I'M THE WORKER not set yet)
-    a) set environment varialbe to "I'M THE WORKER"
-3. Subprocess initializes background_helpers
-    a) checks if "I'M THE WORKER" during initialization
-        i) False: continue running subprocess
-        ii) True: break out to prevent recursive explosion
+Checks if it is inside a "worker" process. Prevents that code block from accidentally getting run 
+from inside its sub process
 """
 if not os.environ.get("I'M THE WORKER"):
     # gets this package's directory
