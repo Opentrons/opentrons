45af8ef42967873423e688455e7bdea979c3d98f
koji <baxin1919@gmail.com>
2026-01-30 16:44:11 -0500

    fix(app,app-shell): close secondary window automatically when app is crashing
    close secondary window automatically when app is crashing
    
    close RQA-5118

---
+++ app/src/App/SecondaryWindowAppFallback.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+
+import { useSentryReport } from '/app/App/hooks'
+import { remote } from '/app/redux/shell/remote'
+
+import type { FallbackProps } from 'react-error-boundary'
+
+declare global {
+  interface Window {
+    secondaryApi?: {
+      closeSelf?: () => void | Promise<void>
+    }
+  }
+}
+
+export function SecondaryWindowAppFallback({
+  error,
+}: FallbackProps): JSX.Element | null {
+  useSentryReport(error)
+
+  useEffect(() => {
+    // close the Window
+    remote.ipcRenderer.send('secondary-window:close-self')
+  }, [])
+
+  return null
+}
