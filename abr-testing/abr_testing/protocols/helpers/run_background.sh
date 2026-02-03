16af8f48133edaaef162ac6c8ddb01504a60accf
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-02-03 10:16:50 -0500

    Add check for background process failure before writing to lockfile

--- abr-testing/abr_testing/protocols/helpers/run_background.sh
+++ abr-testing/abr_testing/protocols/helpers/run_background.sh
@@ -17,7 +17,16 @@ fi
 # check what already exists and kill it
 pkill -f "background_helpers"
 
-python3 -c "from background_helpers import detect_robot_status; detect_robot_status('$IP_ADDRESS')" &
 
-# Push the Python process ID to the Lockfile
-echo $! > "$LOCKFILE"
+
+(python3 -c "from background_helpers import detect_robot_status; detect_robot_status('$IP_ADDRESS')" &)
+sleep 0.5
+processID=$!
+
+if kill -0 "$processID" 2>/dev/null; then
+  # Push the Python process ID to the Lockfile
+  echo "$processID" > "$LOCKFILE"
+  echo "background process successfully launched"
+else
+  echo "background process failed to launch"
+fi
