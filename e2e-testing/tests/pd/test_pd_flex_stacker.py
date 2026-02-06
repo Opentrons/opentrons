49e72322d1e1823b4ed9015fcdbfea710b19efe9
Sara Kowalski <sara.kowalski@opentrons.com>
2026-02-06 01:22:27 -0500

    e2e test for stacker behavior

--- e2e-testing/tests/pd/test_pd_flex_stacker.py
+++ e2e-testing/tests/pd/test_pd_flex_stacker.py
@@ -1,3 +1,8 @@
+"""Test Protocol Designer Flex Stacker behavior in a protocol
+
+E2E test to cover Flex stacker commands in a protocol.
+"""
+
 import pytest
 from playwright.sync_api import Page
 
@@ -10,7 +15,7 @@ from automation.pd_pages.tc_step_form_page import ThermocyclerStepPage
 from automation.pd_pages.timeline import Timeline
 from automation.pd_pages.transfer_form import TransferPage
 from eyes import Eyes
-from utility import create_new_protocol_flow, create_new_protocol_from_landing_page, start_new_create_protocol
+from utility import create_new_protocol_from_landing_page
 
 
 @pytest.mark.pdE2E
@@ -24,77 +29,8 @@ def test_flex_stacker(page: Page, eyes: Eyes | None) -> None:
     thermocycler_page = ThermocyclerStepPage(page)
     transfer_page = TransferPage(page)
 
-    ## Create new Flex protocol from Landing Page, 4 stackers and waste chute and magnetic block
+    # ## Create new Flex protocol from Landing Page, 4 stackers and waste chute and magnetic block
     create_new_protocol_from_landing_page(pipette="8-Channel", gripper=True, tc=True, waste_chute=True, page=page)
-    deck_config_page.select_slot("A4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("A3")
-    deck_config_page.select_module("Magnetic Block GEN1")
-    deck_config_page.select_slot("B4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("C4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("D4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    # applitools eyes?
-    print("\n✓ Configured 4 Flex Stackers with waste chute and magnetic block")
-
-    ## Create new Flex protocol from Create New button,  and configuring 4 stackers without waste chute
-    start_new_create_protocol(page)
-    create_new_protocol_flow(pipette="8-Channel", gripper=True, tc=True, waste_chute=False, page=page)
-    deck_config_page.remove_fixture("Trash bin")
-    deck_config_page.select_slot("A4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.remove_fixture("Stacker")
-    deck_config_page.select_slot("A4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("B4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("C4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("D4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    print("✓ Configured 4 Flex Stackers without waste chute or trash bin")
-
-    ##Creating a new protocol and adding 3 stackers and a trash bin
-    start_new_create_protocol(page)
-    create_new_protocol_flow(pipette="8-Channel", gripper=True, tc=True, waste_chute=True, page=page)
-    deck_config_page.select_slot("B4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("C4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("D4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    print("✓ Configured 3 Flex Stackers with a trash bin")
-
-    ##Creating a new protocol and adding 3 stackers and an absorbance reader
-    start_new_create_protocol(page)
-    create_new_protocol_flow(pipette="8-Channel", gripper=True, tc=True, waste_chute=True, page=page)
-    deck_config_page.select_slot("A4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    plate_reader_page.configure_module("B3", "Absorbance Plate Reader Module GEN1")
-    deck_config_page.select_slot("C4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("D4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    print("✓ Configured 3 Flex Stackers with waste chute a plate reader")
-
-    ##Test adding 2 stackers and an absorbance reader and an trash bin
-    start_new_create_protocol(page)
-    create_new_protocol_flow(pipette="8-Channel", gripper=True, tc=True, waste_chute=True, page=page)
-    deck_config_page.select_slot("A3")
-    deck_config_page.select_fixture("Trash bin")
-    deck_config_page.select_slot("B3")
-    deck_config_page.select_module("Absorbance Plate Reader Module GEN1")
-    deck_config_page.select_slot("C4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    deck_config_page.select_slot("D4")
-    deck_config_page.select_module("Flex Stacker Module GEN1")
-    print("✓ Configured 2 Flex Stackers with Trash bin and a plate reader")
-
-    ##Test adding 2 stackers and all available modules and fixtures
-    start_new_create_protocol(page)
-    create_new_protocol_flow(pipette="8-Channel", gripper=True, tc=True, waste_chute=True, page=page)
     deck_config_page.select_slot("A4")
     deck_config_page.select_fixture("Staging Area Slot")
     deck_config_page.select_slot("A3")
