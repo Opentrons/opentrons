2153700bd8ccd7a01e4f3660055a7d7fb1bb9542
TamarZanzouri <zanzouritamar@gmail.com>
2026-02-13 12:24:00 -0500

    verify password

--- auth-server/auth_server/oauth2/backend.py
+++ auth-server/auth_server/oauth2/backend.py
@@ -9,9 +9,8 @@ import oauthlib.common
 import oauthlib.oauth2
 import pydantic
 
-from server_utils.auth.scopes import Scope, UnrecognizedScopeError, serialize_scopes
-
-from auth_server.users.store import User, get, password_hash
+from auth_server.users.scopes import Scope
+from auth_server.users.store import TEST_USERS, User, password_hash
 
 _log = logging.getLogger(__name__)
 
@@ -164,7 +163,9 @@ class _RequestValidator(oauthlib.oauth2.RequestValidator):
     ) -> bool:
         """Check if some user credentials are valid to log in, and if so, return that user."""
         for user in TEST_USERS:
-            if user.username == username and user.hashed_password == password:
+            if user.username == username and password_hash.verify(
+                password, user.hashed_password
+            ):
                 # Set `.user` per the oauthlib docs.
                 request.user = user  # type: ignore[attr-defined]
                 return True
