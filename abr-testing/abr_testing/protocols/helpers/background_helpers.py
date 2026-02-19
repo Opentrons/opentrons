f38730929fa7707347260b54110ff228dbba6d60
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-02-19 16:42:21 -0500

    change polling interval

--- abr-testing/abr_testing/protocols/helpers/background_helpers.py
+++ abr-testing/abr_testing/protocols/helpers/background_helpers.py
@@ -25,7 +25,7 @@ def detect_robot_status(ip: str) -> None:
 
     # Process will be constantly running
     while True:
-        time.sleep(300)
+        time.sleep(30)
 
         # Reset running_robot and completed_robot information to prevent possible data corruption
         running_robots: List[str] = []
