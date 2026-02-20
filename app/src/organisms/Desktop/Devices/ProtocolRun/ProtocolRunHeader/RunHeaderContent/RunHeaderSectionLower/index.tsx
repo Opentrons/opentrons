32e883898fa47bab5e50419edcc495da18859b8a
Seth Foster <seth@opentrons.com>
2026-02-20 09:49:26 -0500

    feat(app): enable ot2 protocol visualization (#12)
    Highly advanced.
    
    Closes EXEC-2340

--- app/src/organisms/Desktop/Devices/ProtocolRun/ProtocolRunHeader/RunHeaderContent/RunHeaderSectionLower/index.tsx
+++ app/src/organisms/Desktop/Devices/ProtocolRun/ProtocolRunHeader/RunHeaderContent/RunHeaderSectionLower/index.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom'
 
 import { RUN_STATUS_IDLE } from '@opentrons/api-client'
 import { SecondaryButton } from '@opentrons/components'
-import { FLEX_ROBOT_TYPE } from '@opentrons/shared-data'
 
 import { useToaster } from '/app/organisms/ToasterOven'
 import {
@@ -45,7 +44,7 @@ export function RunHeaderSectionLower({
   const completedAtTimestamp =
     completedAt != null ? formatTimestamp(completedAt) : EMPTY_TIMESTAMP
   const createdAtTimestamp = useRunCreatedAtTimestamp(runId)
-  const { protocolKey, robotType } = useProtocolDetailsForRun(runId)
+  const { protocolKey } = useProtocolDetailsForRun(runId)
   const { makeSnackbar } = useToaster()
 
   const storedProtocolAnalysis = useStoredProtocolAnalysis(runId)
@@ -87,7 +86,7 @@ export function RunHeaderSectionLower({
         <LabeledValue label={t('protocol_start')} value={startedAtTimestamp} />
         <LabeledValue label={t('protocol_end')} value={completedAtTimestamp} />
       </div>
-      {runStatus === RUN_STATUS_IDLE && robotType === FLEX_ROBOT_TYPE ? (
+      {runStatus === RUN_STATUS_IDLE ? (
         <div className={styles.button_container}>
           <SecondaryButton onClick={handleVisualizeClick}>
             {t('visualize')}
