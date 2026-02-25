6cc2e6250f6ce1c47f09fef9025f253a02f721e4
TamarZanzouri <zanzouritamar@gmail.com>
2026-02-25 13:11:47 -0500

    revert auth changes

--- server-utils/tests/auth/test_scopes.py
+++ server-utils/tests/auth/test_scopes.py
@@ -25,10 +25,7 @@ def test_no_invalid_characters_in_scope(scope: Scope) -> None:
 def test_parse() -> None:
     assert parse_scopes("") == set()
 
-    assert parse_scopes("runs.write users.write") == {
-        Scope.RUNS_WRITE,
-        Scope.USERS_WRITE,
-    }
+    assert parse_scopes("runs.write users.write") == {Scope.RUNS_WRITE, Scope.USERS_WRITE}
 
     with pytest.raises(UnrecognizedScopeError) as exception:
         parse_scopes("these are not valid scopes")
@@ -39,10 +36,8 @@ def test_serialize() -> None:
     assert serialize_scopes(set()) == ""
 
     assert (
-        serialize_scopes({Scope.RUNS_WRITE, Scope.USERS_WRITE})
-        == "runs.write users.write"
+        serialize_scopes({Scope.RUNS_WRITE, Scope.USERS_WRITE}) == "runs.write users.write"
     )
     assert (
-        serialize_scopes({Scope.USERS_WRITE, Scope.RUNS_WRITE})
-        == "runs.write users.write"
+        serialize_scopes({Scope.USERS_WRITE, Scope.RUNS_WRITE}) == "runs.write users.write"
     )
