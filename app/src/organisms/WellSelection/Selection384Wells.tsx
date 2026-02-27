a9e61405458d783d82958e2876711d7edeed56a2
koji <baxin1919@gmail.com>
2026-02-27 15:35:29 -0500

    refactor(components, protocol-designer, app): refactor Checkbox to replace Btn with input
    refactor Checkbox component to replace Btn with input type"checkbox" to support more accessibility
    and migrate from styled-component to css modules
    
    close AUTH-2779

--- app/src/organisms/WellSelection/Selection384Wells.tsx
+++ app/src/organisms/WellSelection/Selection384Wells.tsx
@@ -281,7 +281,7 @@ function StartingWell({
             key={well}
             isChecked={startingWellState[well]}
             labelText={well}
-            onClick={() => {
+            onChange={() => {
               if (channels === 96) {
                 if (startingWellState[well]) {
                   deselectWells([well])
