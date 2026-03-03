a08c5753f0968d12826b95f271ba27eef446c8ee
Max Marrone <max@opentrons.com>
2026-03-03 12:05:34 -0500

    Edit eslintrc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     'react/display-name': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
-      'warn',
+      'error',
       {
         additionalHooks: '(useDrag|useDrop)',
       },
