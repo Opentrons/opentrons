fa130bb066fc1a0c09cd40a1fbbd210d049ca8be
TamarZanzouri <zanzouritamar@gmail.com>
2026-03-05 15:36:23 -0500

    update migration to migrate from alembic

--- auth-server/auth_server/persistence/_migrations/up_to_v01.py
+++ auth-server/auth_server/persistence/_migrations/up_to_v01.py
@@ -7,10 +7,15 @@ from server_utils.persistence.folder_migrator import Migration
 from auth_server.persistence.database import create_schema, sql_engine_ctx
 from auth_server.persistence.file_and_directory_names import DB_FILE
 
+from alembic.config import Config
+from alembic import command
 
-class MigrationUpTo1(Migration):  # noqa: D101
+from auth_server.persistence.file_and_directory_names import DB_FILE
+
+
+class MigrationUpTo1(Migration):
     def migrate(self, source_dir: Path, dest_dir: Path) -> None:
-        """Create a fresh database with the v1 schema."""
         dest_db_file = dest_dir / DB_FILE
-        with sql_engine_ctx(dest_db_file) as engine:
-            create_schema(engine)
+        alembic_cfg = Config("alembic.ini")
+        alembic_cfg.set_main_option("sqlalchemy.url", f"sqlite:///{dest_db_file}")
+        command.upgrade(alembic_cfg, "head")
