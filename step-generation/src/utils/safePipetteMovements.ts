4113726f6e6eac750dda3985702c2fe0ff5c5ed2
Rhyann <rhyannclarke6@gmail.com>
2026-03-06 14:18:37 -0500

    fix(protocol-designer, step-generation): include waste chute in highest z calculation

--- step-generation/src/utils/safePipetteMovements.ts
+++ step-generation/src/utils/safePipetteMovements.ts
@@ -12,6 +12,7 @@ import {
   getPositionFromSlotId,
   getRobotDefFromRobotType,
   H1_NOZZLE,
+  NINETY_SIX_CHANNEL_WASTE_CHUTE_ADDRESSABLE_AREA,
   OT2_ROBOT_TYPE,
   PARTIAL_COLUMN,
   PARTIAL_NOZZLE_MAP,
@@ -140,8 +141,22 @@ const getModuleHeightFromDeckDefinition = (
   return getModuleDef(moduleModel).dimensions.bareOverallHeight
 }
 
+const getWasteChuteHeightFromDeckDefinition = (
+  robotType: RobotType
+): number => {
+  if (robotType === FLEX_ROBOT_TYPE) {
+    const deckDef = getDeckDefFromRobotType(robotType)
+    const wasteChute = Object.values(deckDef.locations.addressableAreas).find(
+      addressableArea =>
+        addressableArea.id === NINETY_SIX_CHANNEL_WASTE_CHUTE_ADDRESSABLE_AREA
+    )
+    return wasteChute?.offsetFromCutoutFixture[2] ?? 0
+  }
+  return 0
+}
+
 //  check the highest Z-point of all items stacked given a deck slot (including modules,
-//  adapters, and modules on adapters)
+//  adapters, waste chute, and modules on adapters)
 const getHighestZInSlot = (
   robotState: RobotState,
   invariantContext: InvariantContext,
@@ -149,14 +164,18 @@ const getHighestZInSlot = (
   robotType: RobotType
 ): number => {
   const { modules, labware } = robotState
-  const { moduleEntities, labwareEntities } = invariantContext
+  const { moduleEntities, labwareEntities, wasteChuteEntities } =
+    invariantContext
 
   let totalHeight: number = 0
   const largestLabwareStack = getFullStackFromLabwares(labware, slotId)
   const moduleInSlot = Object.keys(modules).find(
     moduleId => modules[moduleId].slot === slotId
   )
-
+  // if slot has waste chute
+  if (wasteChuteEntities && slotId === 'D3') {
+    totalHeight += getWasteChuteHeightFromDeckDefinition(robotType)
+  }
   //  if slot has labware, includes labware, adapters, and module
   if (largestLabwareStack.length > 0) {
     largestLabwareStack.forEach(item => {
