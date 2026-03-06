c9968a50fbfb7e1559b765eed52d4201094c7d52
Max Marrone <max@opentrons.com>
2026-03-06 12:23:43 -0500

    Format.

--- app-shell/src/robot-update/release-files.ts
+++ app-shell/src/robot-update/release-files.ts
@@ -164,11 +164,9 @@ export function cleanupReleaseFiles(
 ): Promise<unknown> {
   return readdir(downloadsDir, { withFileTypes: true })
     .then(files => {
-      return (
-        files
-          .filter(f => f.isDirectory() && f.name !== currentRelease)
-          .map(f => path.join(downloadsDir, f.name))
-      )
+      return files
+        .filter(f => f.isDirectory() && f.name !== currentRelease)
+        .map(f => path.join(downloadsDir, f.name))
     })
     .then(removals => Promise.all(removals.map(f => remove(f))))
 }
