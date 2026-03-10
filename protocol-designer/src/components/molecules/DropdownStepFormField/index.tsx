60a4bd61a66675a845edc991d7234b45dca2acea
ncdiehl11 <ndiehl@opentrons.com>
2026-03-10 10:08:37 -0400

    remove default StyledText color

--- protocol-designer/src/components/molecules/DropdownStepFormField/index.tsx
+++ protocol-designer/src/components/molecules/DropdownStepFormField/index.tsx
@@ -131,9 +131,7 @@ export function DropdownStepFormField(
           flexDirection={DIRECTION_COLUMN}
           width="100%"
         >
-          <StyledText desktopStyle="bodyDefaultSemiBold" color={COLORS.black90}>
-            {title}
-          </StyledText>
+          <StyledText desktopStyle="bodyDefaultSemiBold">{title}</StyledText>
           <ListItem type="default">
             <Flex
               gridGap={SPACING.spacing8}
