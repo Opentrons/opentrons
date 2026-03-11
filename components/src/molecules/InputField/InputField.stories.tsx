8374a812c2898d6a8c28ffd0ca36858a1cba450d
koji <baxin1919@gmail.com>
2026-03-11 16:06:43 -0400

    fix format of tsx

--- components/src/molecules/InputField/InputField.stories.tsx
+++ components/src/molecules/InputField/InputField.stories.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 
 import { DIRECTION_COLUMN } from '../../styles'
 import { SPACING, VIEWPORT } from '../../ui-style-constants'
-import { InputField as InputFieldComponent } from './index'
 import { TouchInputField as TouchInputFieldComponent } from '../TouchInputField/TouchInputField'
+import { InputField as InputFieldComponent } from './index'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
