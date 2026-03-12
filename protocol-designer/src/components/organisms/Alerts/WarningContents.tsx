9a863331b5592a417546fbeb78a80e1e7c2051c3
Rhyann <rhyannclarke6@gmail.com>
2026-03-12 16:40:41 -0400

    simplify warningcontents

--- protocol-designer/src/components/organisms/Alerts/WarningContents.tsx
+++ protocol-designer/src/components/organisms/Alerts/WarningContents.tsx
@@ -6,32 +6,10 @@ interface WarningContentsProps {
   warningType: string
   level: AlertLevel
 }
-export function WarningContents(
-  props: WarningContentsProps
-): JSX.Element | null {
+export function WarningContents(props: WarningContentsProps): JSX.Element {
   const { warningType, level } = props
   const { t } = useTranslation('alert')
-
-  if (level === 'timeline') {
-    switch (warningType) {
-      case 'ASPIRATE_FROM_PRISTINE_WELL':
-        return (
-          <>
-            {t(`timeline.warning.${warningType}.body`, {
-              defaultValue: '',
-            })}
-          </>
-        )
-      default:
-        return t(`timeline.warning.${warningType}.body`, {
-          defaultValue: '',
-        })
-    }
-  } else if (props.level === 'form') {
-    return t(`form.warning.${warningType}.body`, {
-      defaultValue: '',
-    })
-  } else {
-    return null
-  }
+  return t(`${level}.warning.${warningType}.body`, {
+    defaultValue: '',
+  })
 }
