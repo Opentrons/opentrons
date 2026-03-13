19a68da2a362f9f729acd6d714cab0b8866b0a03
Max Marrone <max@opentrons.com>
2026-03-13 09:57:03 -0400

    test(python): Auto-select ports through pytest-asyncio (#21030)
    This tries to resolve a source of flakiness in our Python integration
    tests.
    
    The integration tests in auth-server, key-server, robot-server, and
    system-server run instances of the server on localhost on a random port.
    The random port occasionally collides with something else, causing a
    spurious test failure.
    
    This PR changes things to use pytest-asyncio's unused_tcp_port fixture instead.
    The fixture works by [acquiring a random port from the OS and immediately releasing it,
    relying on the fact that it will probably be a long time before the OS
    reuses it. This is a hack, but it's probably a more battle-tested hack?
    
    The right solution is either:
    
    * Use Unix sockets instead of TCP sockets. But it looks annoying to get
    Tavern to work with those.
    * Get the servers to internally bind to an OS-chosen port, and then
    output that port over a Unix socket. But this feels a bit Rube Goldberg,
    and it also looks kind of sketchy to implement with Uvicorn.

--- system-server/tests/dev_server.py
+++ system-server/tests/dev_server.py
@@ -10,9 +10,9 @@ from types import TracebackType
 
 class DevServer:
     def __init__(
-        self, port: int | None = None, persistence_directory: Path | None = None
+        self, port: int, persistence_directory: Path | None = None
     ) -> None:
-        """Initialize a dev server."""
+        """Initialize a dev server with the given port."""
         self.server_temp_directory: str = tempfile.mkdtemp()
         self.persistence_directory: Path = (
             persistence_directory
