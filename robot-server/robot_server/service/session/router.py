42eed774d4b0a882068973f0f4a4da41483b7c4a
Max Marrone <max@opentrons.com>
2026-03-16 17:20:50 -0400

    Add access control to OT-2 calibration endpoints.
    This is probably academic, since these endpoints are OT-2-specific, and access control is always disabled on the OT-2. Still, we might as well try to keep up good habits.

--- robot-server/robot_server/service/session/router.py
+++ robot-server/robot_server/service/session/router.py
@@ -4,6 +4,8 @@ from typing import Annotated, Optional
 from fastapi import APIRouter, Depends, Query
 from starlette import status as http_status_codes
 
+from server_utils.auth.resource_server.fastapi_dependencies import require_scopes
+from server_utils.auth.scopes import Scope
 from server_utils.fastapi_utils.models.json_api import ResourceLink
 from server_utils.fastapi_utils.models.json_api.resource_links import (
     ResourceLinkKey,
@@ -58,6 +60,7 @@ def get_session(manager: SessionManager, session_id: IdentifierType) -> BaseSess
     deprecated=True,
     response_model=SessionResponse,
     status_code=http_status_codes.HTTP_201_CREATED,
+    dependencies=[Depends(require_scopes(Scope.ROBOT_CONTROL_WRITE))],
 )
 async def create_session_handler(
     create_request: SessionCreateRequest,
@@ -86,6 +89,7 @@ async def create_session_handler(
     ),
     deprecated=True,
     response_model=SessionResponse,
+    dependencies=[Depends(require_scopes(Scope.ROBOT_CONTROL_WRITE))],
 )
 async def delete_session_handler(
     sessionId: IdentifierType,
@@ -155,6 +159,7 @@ async def get_sessions_handler(
     ),
     deprecated=True,
     response_model=CommandResponse,
+    dependencies=[Depends(require_scopes(Scope.ROBOT_CONTROL_WRITE))],
 )
 async def session_command_execute_handler(
     sessionId: IdentifierType,
