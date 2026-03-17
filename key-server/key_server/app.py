9b6f745bf8c6d32aae0341bf549415f192ceb43b
Seth Foster <seth@opentrons.com>
2026-03-17 10:41:13 -0400

    async exit stack

--- key-server/key_server/app.py
+++ key-server/key_server/app.py
@@ -1,7 +1,7 @@
 """The server's ASGI app object."""
 
 import logging
-from contextlib import asynccontextmanager
+from contextlib import AsyncExitStack, asynccontextmanager
 from typing import AsyncGenerator
 
 from fastapi import FastAPI
@@ -19,21 +19,20 @@ from key_server.settings.store import SettingsStore, install_settings_store
 
 @asynccontextmanager
 async def _lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    try:
-        configure_logging(logging.INFO)
-    except BaseException as be:
-        # needs to be a print because logging doesn't work yet!
-        print(f"Logging configuration failed: {be}")  # noqa: T201
-    settings_store = SettingsStore()
-    install_settings_store(app.state, settings_store)
-    secure_volume_manager = build_secure_volume_manager(settings_store)
-    install_secure_volume_manager(app.state, secure_volume_manager)
-    await secure_volume_manager.mount()
-    systemd_utils.notify_up()
-    try:
+    async with AsyncExitStack() as exit_stack:
+        try:
+            configure_logging(logging.INFO)
+        except Exception as be:
+            # needs to be a print because logging doesn't work yet!
+            print(f"Logging configuration failed: {be}")  # noqa: T201
+        settings_store = SettingsStore()
+        install_settings_store(app.state, settings_store)
+        secure_volume_manager = build_secure_volume_manager(settings_store)
+        install_secure_volume_manager(app.state, secure_volume_manager)
+        await secure_volume_manager.mount()
+        exit_stack.push_async_callback(secure_volume_manager.unmount)
+        systemd_utils.notify_up()
         yield
-    finally:
-        await secure_volume_manager.unmount()
 
 
 app = FastAPI(
