Opentrons/opentrons

commit 93ae78bd238fce138a062f749b9e366aed89dcd4

fix(app): ensure wellName is a part of labwareDef safegaurd in PV (#20896)
closes RQA-5221

app/src/organisms/Desktop/ProtocolVisualization/DeckView/LabwareOnDeck.tsx
@@ -46,12 +46,11 @@ export function LabwareOnDeck(props: LabwareOnDeckProps): JSX.Element {
     pipetteTemporalProperties != null
       ? pipetteTemporalProperties[1].wellName
       : null
-  const wellGroup: WellGroup | null =
-    activeWellName != null
-      ? {
-          [activeWellName]: null,
-        }
-      : null
+
+  const wellName = activeWellName ?? 'A1'
+
+  const wellGroup: WellGroup =
+    labwareDef.wells[wellName] != null ? { [wellName]: null } : {}
 
   const allWellContentsForActiveItem = getAllWellContentsAtFrame(
     liquidState,

app/src/organisms/Desktop/ProtocolVisualization/SecondWindow/LabwareSlot/index.tsx
@@ -128,13 +128,13 @@ export function LabwareSlot(props: LabwareSlotContainerProps): JSX.Element {
       ? pipetteTemporalProperties[1].wellName
       : null
   const wellGroup: WellGroup | null =
-    activeWellName != null
+    activeWellName != null && labwareDef.wells[activeWellName] != null
       ? {
           [activeWellName]: null,
         }
       : null
   const hoveredWellGroup: WellGroup | null =
-    hoveredWellName != null
+    hoveredWellName != null && labwareDef.wells[hoveredWellName] != null
       ? {
           [hoveredWellName]: null,
         }

commit 1c98d88f2e1ac901e19cb0576fe4366734fe8f73

fix(app): highlight correct well in protocol visualization (#21046)
A regression introduced in #20896 was causing the viz deck map to
highlight every A1 well of every labware on every step.

This small fix corrects the logic so only active wells are highlighted,
and A1 does not become a "default" value for highlighting.

app/src/organisms/Desktop/ProtocolVisualization/DeckView/LabwareOnDeck.tsx
@@ -47,10 +47,10 @@ export function LabwareOnDeck(props: LabwareOnDeckProps): JSX.Element {
       ? pipetteTemporalProperties[1].wellName
       : null
 
-  const wellName = activeWellName ?? 'A1'
-
   const wellGroup: WellGroup =
-    labwareDef.wells[wellName] != null ? { [wellName]: null } : {}
+    activeWellName != null && labwareDef.wells[activeWellName] != null
+      ? { [activeWellName]: null }
+      : {}
 
   const allWellContentsForActiveItem = getAllWellContentsAtFrame(
     liquidState,