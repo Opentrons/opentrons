62b2d83d84e3152b893433ad14235b83442b1b5b
Brayan Almonte <vegano1@users.noreply.github.com>
2026-03-20 17:55:40 -0400

    feat(api, robot-server, shared-data, app, components, protocol-designer): Add ability to  use load_module on the vacuum module + Remove Millipore references. (#21076)

--- app/src/organisms/ModuleCard/ModuleSetupModal.tsx
+++ app/src/organisms/ModuleCard/ModuleSetupModal.tsx
@@ -26,7 +26,7 @@ import { getTopPortalEl } from '/app/App/portal'
 import absorbanceReaderManualQRCode from '/app/assets/images/absorbance_reader_instruction_manual_code.png'
 import helpCenterQRCode from '/app/assets/images/module_instruction_code.png'
 import stackerInstallationQRCode from '/app/assets/images/stacker_installation_qr.png'
-import vacuumModuleMilliporeManualQRCode from '/app/assets/images/vacuum_setup_instructions_qr.png'
+import vacuumModuleManualQRCode from '/app/assets/images/vacuum_setup_instructions_qr.png'
 
 import type { ModuleModel } from '@opentrons/shared-data'
 
@@ -35,8 +35,8 @@ const ABSORBANCE_READER_MANUAL_URL =
   'https://insights.opentrons.com/hubfs/Absorbance%20Plate%20Reader%20Instruction%20Manual.pdf'
 const FLEX_STACKER_INSTALL_DOCS_URL =
   'https://docs.opentrons.com/stacker/installation/'
-// TODO: add vacuum module millipore manual url
-const VACUUM_MODULE_MILLIPORE_MANUAL_URL = 'https://docs.opentrons.com/modules/'
+// TODO: add vacuum module manual url
+const VACUUM_MODULE_MANUAL_URL = 'https://docs.opentrons.com/modules/'
 
 interface ModuleSetupModalProps {
   close: () => void
@@ -80,7 +80,7 @@ export const ModuleSetupModal = (props: ModuleSetupModalProps): JSX.Element => {
       case FLEX_STACKER_MODULE_TYPE:
         return FLEX_STACKER_INSTALL_DOCS_URL
       case VACUUM_MODULE_TYPE:
-        return VACUUM_MODULE_MILLIPORE_MANUAL_URL
+        return VACUUM_MODULE_MANUAL_URL
       default:
         return MODULE_SETUP_URL
     }
@@ -93,7 +93,7 @@ export const ModuleSetupModal = (props: ModuleSetupModalProps): JSX.Element => {
       case FLEX_STACKER_MODULE_TYPE:
         return stackerInstallationQRCode
       case VACUUM_MODULE_TYPE:
-        return vacuumModuleMilliporeManualQRCode
+        return vacuumModuleManualQRCode
       default:
         return helpCenterQRCode
     }
