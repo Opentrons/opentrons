88436bd99d416d873314d0c46759bd01bdf1bc56
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2026-03-20 12:57:00 -0400

    refactor(api): delete redundant LC PAPI 2.27 version

--- api/src/opentrons/protocol_api/core/engine/_default_liquid_class_versions.py
+++ api/src/opentrons/protocol_api/core/engine/_default_liquid_class_versions.py
@@ -32,11 +32,6 @@ DEFAULT_LIQUID_CLASS_VERSIONS: DefaultLiquidClassVersions = {
         "glycerol_50": 2,
         "water": 2,
     },
-    APIVersion(2, 27): {
-        "ethanol_80": 2,
-        "glycerol_50": 2,
-        "water": 2,
-    },
     APIVersion(2, 28): {
         "ethanol_80": 3,
         "glycerol_50": 3,
