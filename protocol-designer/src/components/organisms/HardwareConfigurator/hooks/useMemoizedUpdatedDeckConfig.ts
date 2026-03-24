01213c121fc63d624722cae9ab90df70205793cd
ncdiehl11 <ndiehl@opentrons.com>
2026-03-24 17:29:41 -0400

    fix(protocol-designer): fix hardware configuration update bug
    This PR refactors the useEffect logic in PD's HardwareConfigurator component to correctly live update. The error stemmed from the useEffect's dependency array being incorrectly empty (only running on mount). I move the buesiness logic for the updated config to a custom hook with memoization and populate the useEffect (that dispatches the update) with modules and fixtures.

---
+++ protocol-designer/src/components/organisms/HardwareConfigurator/hooks/useMemoizedUpdatedDeckConfig.ts
@@ -0,0 +1,107 @@
+import { useMemo } from 'react'
+
+import {
+  FLEX_ROBOT_TYPE,
+  getAddedMissingThermocyclerFixtures,
+  getCutoutFixtureIdsForModuleModel,
+  getCutoutIdFromAddressableArea,
+  getDeckDefFromRobotType,
+  getEmptyDeckConfiguration,
+  replaceFixtureToFakeFixtureAndTransformCutoutFixturesToAA,
+  THERMOCYCLER_MODULE_TYPE,
+} from '@opentrons/shared-data'
+
+import { mergeToComboFixtures } from '../utils'
+
+import type {
+  AddressableAreaNamesWithFakes,
+  CutoutConfig,
+  CutoutConfigMap,
+  CutoutId,
+  DeckConfiguration,
+} from '@opentrons/shared-data'
+import type { FormModule, FormModules } from '/protocol-designer/step-forms'
+import type { Fixtures } from '../../types'
+import type {
+  InitialDeckStateModules,
+  ModuleExtended,
+} from '../AddFixtureModal'
+
+export function useMemoizedUpdatedDeckConfig(
+  modules: FormModules | InitialDeckStateModules,
+  fixtures: Fixtures
+): DeckConfiguration {
+  return useMemo((): DeckConfiguration => {
+    const deckDef = getDeckDefFromRobotType(FLEX_ROBOT_TYPE)
+    const emptyDeckConfiguration = getEmptyDeckConfiguration(deckDef)
+    const deckConfigWithAA =
+      replaceFixtureToFakeFixtureAndTransformCutoutFixturesToAA(
+        emptyDeckConfiguration
+      )
+    const simpleDeckConfig: DeckConfiguration = emptyDeckConfiguration.filter(
+      ({ cutoutId }) => {
+        const hasModule = Object.values(modules).some(
+          module =>
+            getCutoutIdFromAddressableArea(module.slot as string, deckDef) ===
+            cutoutId
+        )
+        //  since we are adding cutoutA1 in moduleConfig if
+        //  there is a TC
+        const hasTCAndCutoutA1 =
+          Object.values(modules).some(
+            module => module.type === THERMOCYCLER_MODULE_TYPE
+          ) && cutoutId === 'cutoutA1'
+        const hasFixture = Object.values(fixtures).some(
+          fixture => fixture.cutoutId === cutoutId
+        )
+        return !hasModule && !hasFixture && !hasTCAndCutoutA1
+      }
+    )
+
+    const moduleConfig: CutoutConfigMap[] = Object.values(modules).flatMap(
+      (module: FormModule | ModuleExtended): CutoutConfigMap[] => {
+        const fixtureModule = getCutoutFixtureIdsForModuleModel(module.model)[0]
+        const cutoutId = getCutoutIdFromAddressableArea(module.slot, deckDef)!
+        const matchingDeckConfigEntry = deckConfigWithAA.find(
+          config =>
+            config.cutoutId === cutoutId &&
+            config.cutoutFixtureId === fixtureModule
+        )
+        const defaultModuleConfig: CutoutConfigMap = {
+          cutoutId,
+          cutoutFixtureId: fixtureModule,
+          addressableAreaId:
+            matchingDeckConfigEntry?.addressableAreaId ??
+            (module.slot as AddressableAreaNamesWithFakes),
+        }
+        // getAddedMissingThermocyclerFixtures returns input array + any missing TC fixtures
+        return getAddedMissingThermocyclerFixtures(
+          [defaultModuleConfig],
+          deckDef
+        )
+      }
+    )
+    const additionalEquipmentConfig: DeckConfiguration = Object.values(
+      fixtures
+    ).map(
+      (ae): CutoutConfig => ({
+        cutoutId: ae.cutoutId as CutoutId,
+        cutoutFixtureId: ae.cutoutFixtureId,
+      })
+    )
+
+    // Merge modules and fixtures into combo fixtures where applicable
+    const {
+      comboFixtures,
+      remainingModuleConfig,
+      remainingAdditionalEquipmentConfig,
+    } = mergeToComboFixtures(moduleConfig, additionalEquipmentConfig)
+
+    return [
+      ...simpleDeckConfig,
+      ...remainingModuleConfig,
+      ...remainingAdditionalEquipmentConfig,
+      ...comboFixtures,
+    ] as DeckConfiguration
+  }, [modules, fixtures])
+}
