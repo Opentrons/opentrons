bee911b5c15250f5fb6b57ba7c28b9ac4bbf2031
Ryan Howard <ryan.howard@opentrons.com>
2026-03-24 17:46:57 -0400

    add a test for the mixed module/peripheral logic.

--- api/tests/opentrons/hardware_control/test_module_control.py
+++ api/tests/opentrons/hardware_control/test_module_control.py
@@ -9,6 +9,7 @@ from opentrons.drivers.rpi_drivers.interfaces import USBDriverInterface
 from opentrons.drivers.rpi_drivers.types import USBPort
 from opentrons.hardware_control import API as HardwareAPI
 from opentrons.hardware_control import types
+from opentrons.hardware_control.abstract_device import AbstractDevice
 from opentrons.hardware_control.module_control import AttachedModulesControl
 from opentrons.hardware_control.modules import AbstractModule
 from opentrons.hardware_control.modules.types import (
@@ -16,6 +17,8 @@ from opentrons.hardware_control.modules.types import (
     ModuleType,
     SimulatingModuleAtPort,
 )
+from opentrons.hardware_control.peripherals import AbstractPeripheral
+from opentrons.hardware_control.peripherals.types import PeripheralType
 
 
 @pytest.fixture()
@@ -31,7 +34,7 @@ def usb_bus(decoy: Decoy) -> USBDriverInterface:
 
 
 @pytest.fixture()
-def build_device(decoy: Decoy) -> Callable[..., Awaitable[AbstractModule]]:
+def build_device(decoy: Decoy) -> Callable[..., Awaitable[AbstractDevice]]:
     """Get a mocked out AttachedModuleControl.build_device.
 
     !!! warning
@@ -42,7 +45,7 @@ def build_device(decoy: Decoy) -> Callable[..., Awaitable[AbstractModule]]:
         are too brittle and of questionable value.
     """
     return cast(
-        Callable[..., Awaitable[AbstractModule]],
+        Callable[..., Awaitable[AbstractDevice]],
         decoy.mock(name="build_device", is_async=True),
     )
 
@@ -56,7 +59,7 @@ def event_callback(decoy: Decoy) -> Callable[[types.HardwareEvent], None]:
 def subject(
     hardware_api: HardwareAPI,
     usb_bus: USBDriverInterface,
-    build_device: Callable[..., Awaitable[AbstractModule]],
+    build_device: Callable[..., Awaitable[AbstractDevice]],
     event_callback: Callable[[types.HardwareEvent], None],
 ) -> AttachedModulesControl:
     modules_control = AttachedModulesControl(
@@ -68,6 +71,60 @@ def subject(
     # https://github.com/testdouble/contributing-tests/wiki/Partial-Mock
     modules_control.build_device = build_device  # type: ignore[assignment]
     return modules_control
+
+
+async def test_register_mixed_devices(
+    decoy: Decoy,
+    usb_bus: USBDriverInterface,
+    build_device: Callable[..., Awaitable[AbstractDevice]],
+    hardware_api: HardwareAPI,
+    subject: AttachedModulesControl,
+) -> None:
+    """It should register attached modules."""
+    actual_ports = [
+        ModuleAtPort(
+            port="/dev/foo1",
+            name="tempdeck",
+            usb_port=USBPort(name="baz1", port_number=0),
+        ),
+        ModuleAtPort(
+            port="/dev/foo2",
+            name="barcodescanner",
+            usb_port=USBPort(name="baz2", port_number=1),
+        ),
+    ]
+
+    module = decoy.mock(cls=AbstractModule)
+    peripheral = decoy.mock(cls=AbstractPeripheral)
+    decoy.when(module.usb_port).then_return(USBPort(name="baz1", port_number=0))
+    decoy.when(peripheral.usb_port).then_return(USBPort(name="baz2", port_number=1))
+
+    decoy.when(usb_bus.match_virtual_ports(actual_ports)).then_return(actual_ports)
+    decoy.when(
+        await build_device(
+            port="/dev/foo1",
+            usb_port=USBPort(name="baz1", port_number=0),
+            type=ModuleType.TEMPERATURE,
+            sim_serial_number=None,
+            sim_model=None,
+        )
+    ).then_return(module)
+    decoy.when(
+        await build_device(
+            port="/dev/foo2",
+            usb_port=USBPort(name="baz2", port_number=1),
+            type=PeripheralType.BARCODE_SCANNER,
+            sim_serial_number=None,
+            sim_model=None,
+        )
+    ).then_return(peripheral)
+
+    await subject.register_devices(new_devices_at_ports=actual_ports)
+    modules = subject.available_modules
+    peripherals = subject.available_peripherals
+
+    assert modules == [module]
+    assert peripherals == [peripheral]
 
 
 @pytest.mark.parametrize(
@@ -89,7 +146,7 @@ def subject(
 async def test_register_modules(
     decoy: Decoy,
     usb_bus: USBDriverInterface,
-    build_device: Callable[..., Awaitable[AbstractModule]],
+    build_device: Callable[..., Awaitable[AbstractDevice]],
     hardware_api: HardwareAPI,
     subject: AttachedModulesControl,
     module_at_port_input: Union[List[ModuleAtPort], List[SimulatingModuleAtPort]],
@@ -128,7 +185,7 @@ async def test_register_modules(
 async def test_register_modules_sort(
     decoy: Decoy,
     usb_bus: USBDriverInterface,
-    build_device: Callable[..., Awaitable[AbstractModule]],
+    build_device: Callable[..., Awaitable[AbstractDevice]],
     hardware_api: HardwareAPI,
     subject: AttachedModulesControl,
 ) -> None:
