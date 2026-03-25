f3016729c9ac8c90a8ebdf2df30b6ea56c8a0426
koji <baxin1919@gmail.com>
2026-03-25 15:16:06 -0400

    fix e2e test errors

--- app/src/atoms/structure/Divider.tsx
+++ app/src/atoms/structure/Divider.tsx
@@ -8,7 +8,7 @@ export function Divider(props: Props): JSX.Element {
   const { marginY } = props
   return (
     <Box
-      borderBottom={`1px solid ${String(COLORS.grey30)}`}
+      borderBottom={`1px solid ${COLORS.grey30}`}
       marginY={marginY ?? SPACING.spacing4}
       {...props}
       data-testid="divider"
