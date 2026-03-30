630662ddc3e8b42d81ce42c2144b175a323273f3
TamarZanzouri <zanzouritamar@gmail.com>
2026-03-30 12:52:15 -0400

    unuserd condition

--- auth-server/auth_server/settings/router.py
+++ auth-server/auth_server/settings/router.py
@@ -43,10 +43,7 @@ async def get_access_control_settings(  # noqa: D103
     settings_store: Annotated[SettingsStore, fastapi.Depends(get_settings_store)],
 ) -> SimpleBody[AccessControlResponseData]:
     accessControlEnabled = settings_store.get_access_control_settings()
-    return SimpleBody.model_construct(
-        data=accessControlEnabled
-        or AccessControlResponseData(accessControlEnabled=False)
-    )
+    return SimpleBody.model_construct(data=accessControlEnabled)
 
 
 @router.patch(
