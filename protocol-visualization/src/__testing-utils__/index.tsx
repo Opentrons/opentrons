59c047a32fecf82b7f779631a00f0c05e32c95a9
Sanniti <sanniti@opentrons.com>
2026-04-02 13:57:08 -0400

    some fixes, added tests

---
+++ protocol-visualization/src/__testing-utils__/index.tsx
@@ -0,0 +1,11 @@
+import { I18nextProvider, initReactI18next } from 'react-i18next'
+import { render } from '@testing-library/react'
+import i18n from 'i18next'
+
+// minimal i18n instance - returns key as value
+const testI18n = i18n.createInstance()
+testI18n.use(initReactI18next).init({ lng: 'en', resources: {} })
+
+export function renderWithProviders(ui: React.ReactElement) {
+  return render(<I18nextProvider i18n={testI18n}>{ui}</I18nextProvider>)
+}
--- protocol-visualization/src/types.ts
+++ protocol-visualization/src/types.ts
@@ -2,15 +2,15 @@ import type { RunTimeCommand } from '@opentrons/shared-data'
 import type { CommandAnnotationV2 } from '@opentrons/shared-data/commandAnnotation/types'
 
 interface ParentNode {
-    annotationId: string
-    subCommands: LeafNode[]
-    isHighLighted: boolean
-    annotation?: CommandAnnotationV2
+  annotationId: string
+  subCommands: LeafNode[]
+  isHighLighted: boolean
+  annotation?: CommandAnnotationV2
 }
 
 export interface LeafNode {
-    command: RunTimeCommand
-    isHighlighted: boolean
+  command: RunTimeCommand
+  isHighlighted: boolean
 }
 
 export type GroupedCommands = Array<LeafNode | ParentNode>
