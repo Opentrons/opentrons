7bd276728940ef127fdfade0c58bedb73ef62170
Caila Marashaj <98041399+caila-marashaj@users.noreply.github.com>
2026-04-03 12:35:29 -0400

    feat(api): add StopVacuum command (#21111)

--- api/src/opentrons/hardware_control/modules/vacuum_module.py
+++ api/src/opentrons/hardware_control/modules/vacuum_module.py
@@ -200,9 +200,9 @@ class VacuumModule(mod_abc.AbstractModule):
 
     @property
     def live_data(self) -> LiveData:
-        # TODO: FIX THIS
         data: VacuumModuleData = {
             "errorDetails": self._reader.error,
+            "pumpEngaged": self._reader.pump_state.pump_running,
         }
         return {"status": self.status.value, "data": data}
 
