9976df79eeb0835d3f39c91839a8a2f9f83a91b2
Rhyann Clarke <146747548+rclarke0@users.noreply.github.com>
2026-04-07 16:31:07 -0400

    fix(protocol-designer): Add state when mouse moves out of well on WellSelector (#21188)
    # Overview
    
    Adds handleLeaveWell mouse event to set hovered wells to null and
    therefore remove the shadow.
    
    ## Test Plan and Hands on Testing
    
    - smoke tested:
    
    
    https://github.com/user-attachments/assets/2e331b2c-8a32-44ae-95df-647ac3a22ba9
    
    ## Changelog
    
    - added handleLeaveWell mouse event
    - added useRef state to keep track of currrently tracked well to avoid
    flickery mouse behavior
    
    ## Review requests
    
    - timing of hover removal? is 120 seconds too fast?
    
    ## Risk assessment
    low
    
    Closes RQA-5298-b

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/NozzleAndWellSelectionModal/WellSelector.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/NozzleAndWellSelectionModal/WellSelector.tsx
@@ -132,8 +132,15 @@ export function WellSelector(props: WellSelectorProps): JSX.Element {
     useState<string[][]>(getSelectedWells())
 
   const [hoveredWells, setHoveredWells] = useState<string[] | null>(null)
+  const currentHoveredWellRef = useRef<string[] | null>(null)
+
   useEffect(
     () => {
+      if (leaveTimeoutRef.current) {
+        clearTimeout(leaveTimeoutRef.current)
+        leaveTimeoutRef.current = null
+      }
+      currentHoveredWellRef.current = null
       setSelectedWells(getSelectedWells())
       setHoveredWells(null)
     },
@@ -141,6 +148,19 @@ export function WellSelector(props: WellSelectorProps): JSX.Element {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [stepType]
   )
+
+  const handleLeaveWell = (_: WellMouseEvent): void => {
+    if (leaveTimeoutRef.current) {
+      clearTimeout(leaveTimeoutRef.current)
+    }
+    leaveTimeoutRef.current = setTimeout(() => {
+      if (currentHoveredWellRef.current === hoveredWells) {
+        setHoveredWells(null)
+        currentHoveredWellRef.current = null
+      }
+      leaveTimeoutRef.current = null
+    }, 120)
+  }
 
   const handleHoverWell = (e: WellMouseEvent): void => {
     if (leaveTimeoutRef.current) {
@@ -157,6 +177,7 @@ export function WellSelector(props: WellSelectorProps): JSX.Element {
     )
 
     setHoveredWells(hovered)
+    currentHoveredWellRef.current = hovered
   }
 
   const allWellsWithStatus = useMemo(
@@ -419,6 +440,7 @@ export function WellSelector(props: WellSelectorProps): JSX.Element {
           y={slotPosition[1]}
           handleClickWell={handleClickWell}
           onMouseEnterWell={handleHoverWell}
+          onMouseLeaveWell={handleLeaveWell}
           selectedTipsByIndex={allWellsWithStatus}
           statusByWellName={allWellsWithState}
           fill={COLORS.white}
