b64c85d90dec00af2fd957ecfa9e859201d2c076
Koji Kanao <baxin1919@gmail.com>
2026-04-10 10:49:22 -0400

    feat(app) add a new inline notification for a flex protocol (#33)
    * feat(app) add a new inline notification for a flex protocol

--- app/src/organisms/Desktop/ProtocolsLanding/ProtocolCard.tsx
+++ app/src/organisms/Desktop/ProtocolsLanding/ProtocolCard.tsx
@@ -12,19 +12,20 @@ import {
   DIRECTION_COLUMN,
   Flex,
   Icon,
+  InlineNotification,
   JUSTIFY_FLEX_END,
   LegacyStyledText,
   ModuleIcon,
   OVERFLOW_WRAP_ANYWHERE,
   POSITION_ABSOLUTE,
   ProtocolDeck,
-  SIZE_3,
   SPACING,
   StyledText,
   TYPOGRAPHY,
   WRAP,
 } from '@opentrons/components'
 import {
+  FLEX_ROBOT_TYPE,
   getGripperDisplayName,
   getModuleType,
   getPipetteNameSpecs,
@@ -140,6 +141,8 @@ function AnalysisInfo(props: AnalysisInfoProps): JSX.Element {
     mostRecentAnalysis != null ? mostRecentAnalysis.commands : []
   )
 
+  const isFlex = mostRecentAnalysis?.robotType === FLEX_ROBOT_TYPE
+
   const requiredModuleTypes = requiredModuleModels.map(getModuleType)
 
   const hasPeripherals =
@@ -159,20 +162,10 @@ function AnalysisInfo(props: AnalysisInfoProps): JSX.Element {
         {
           {
             missing: (
-              <Icon
-                name="ot-spinner"
-                color={COLORS.grey60}
-                spin
-                size={SIZE_3}
-              />
+              <Icon name="ot-spinner" color={COLORS.grey60} spin size="4rem" />
             ),
             loading: (
-              <Icon
-                name="ot-spinner"
-                color={COLORS.grey60}
-                spin
-                size={SIZE_3}
-              />
+              <Icon name="ot-spinner" color={COLORS.grey60} spin size="4rem" />
             ),
             error: (
               <Box
@@ -196,10 +189,14 @@ function AnalysisInfo(props: AnalysisInfoProps): JSX.Element {
               />
             ),
             complete:
-              mostRecentAnalysis != null ? (
+              mostRecentAnalysis != null && !isFlex ? (
                 <ProtocolDeck protocolAnalysis={mostRecentAnalysis} />
               ) : (
-                <Box size="6rem" backgroundColor={COLORS.grey30} />
+                <Box
+                  size="6rem"
+                  backgroundColor={COLORS.grey30}
+                  borderRadius={BORDERS.borderRadius8}
+                />
               ),
           }[analysisStatus]
         }
@@ -222,6 +219,16 @@ function AnalysisInfo(props: AnalysisInfoProps): JSX.Element {
           ) : null}
           {analysisStatus === 'stale' ? (
             <ProtocolAnalysisStale protocolKey={protocolKey} />
+          ) : null}
+          {isFlex === true ? (
+            <Box paddingRight={SPACING.spacing24}>
+              <InlineNotification
+                type="alert"
+                heading={t('branded:flex_protocol_detected')}
+                message={t('branded:flex_protocol_detected_description')}
+                linkText={t('branded:get_the_app')}
+              />
+            </Box>
           ) : null}
           <Flex paddingRight={SPACING.spacing24}>
             <LegacyStyledText
