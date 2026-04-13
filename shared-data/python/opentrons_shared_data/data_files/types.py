2372cf055ff7ec701d58818b208a36f36434a17f
CaseyBatten <caseyhbatten@gmail.com>
2026-04-13 09:45:25 -0400

    remove old comment

--- shared-data/python/opentrons_shared_data/data_files/types.py
+++ shared-data/python/opentrons_shared_data/data_files/types.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
 from typing import Optional, Union
+
 from pydantic import BaseModel, ConfigDict
 
 from ..util import StrEnum
@@ -33,7 +34,6 @@ class DataFileSource(Enum):
     GENERATED = "generated"
 
 
-#@dataclass(frozen=True)
 class DataFileInfo(BaseModel):
     """Metadata about a data file."""
 
