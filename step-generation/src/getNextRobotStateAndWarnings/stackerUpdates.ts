6fc29d2ec25b5fa72e6940d37c33aa39a7a4e5c8
ncdiehl11 <ndiehl@opentrons.com>
2026-04-14 13:08:10 -0400

    refactor logic for stacker shuttle addressable area in enrichment and stacker updates

--- step-generation/src/getNextRobotStateAndWarnings/stackerUpdates.ts
+++ step-generation/src/getNextRobotStateAndWarnings/stackerUpdates.ts
@@ -1,11 +1,4 @@
-import {
-  FLEX_CUTOUT_BY_SLOT_ID,
-  FLEX_ROBOT_TYPE,
-  FLEX_STACKER_ADDRESSABLE_AREAS,
-  FLEX_STACKER_V1_FIXTURE,
-  getDeckDefFromRobotType,
-  SYSTEM_LOCATION,
-} from '@opentrons/shared-data'
+import { SYSTEM_LOCATION } from '@opentrons/shared-data'
 
 import {
   BOTTOM_UP_LABWARE_POOL_KEYS,
@@ -15,7 +8,6 @@ import { flexStackerStateGetter } from '../robotStateSelectors'
 import { getFlexStackerShuttleAddressableArea } from '../utils/misc'
 
 import type {
-  AddressableAreaName,
   FlexStackerEmptyCreateCommand,
   FlexStackerFillItemsParams,
   FlexStackerFillParams,
@@ -677,24 +669,4 @@ export const forFlexStackerStore = (
       }
     }
   }
-}
-
-const _getStackerShuttleAA = (
-  moduleSlot: string
-): AddressableAreaName | null => {
-  // stacker only compatible with Flex
-  const deckDef = getDeckDefFromRobotType(FLEX_ROBOT_TYPE)
-
-  // Flex stacker slots are currently set in column 4, so we need to transform the slot name to the correct cutout id
-  const transformedSlot = `${moduleSlot[0]}3`
-  const cutuoutId = FLEX_CUTOUT_BY_SLOT_ID[transformedSlot]
-  const flexStackerAAs = deckDef.cutoutFixtures.find(
-    ({ id }) => id === FLEX_STACKER_V1_FIXTURE
-  )?.providesAddressableAreas[cutuoutId]
-
-  // pick off the shuttle AA that is exposed by the stacker
-  return (
-    FLEX_STACKER_ADDRESSABLE_AREAS.find(aa => flexStackerAAs?.includes(aa)) ??
-    null
-  )
 }
