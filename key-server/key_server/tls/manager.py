fd113e0ac275aee22465913a99e5bb29593c4d6c
Seth Foster <seth@opentrons.com>
2026-04-15 14:39:19 -0400

    feat(key-server): add robot IP and hostname to TLS certs (#21266)
    TLS certificates provide a guarantee of server identity by including the
    server's domain and (optionally) IP in fields of the certificate that
    are protected by the cert's signature. We need that for our TLS certs,
    but that means we need to get the IP address and hostname of the robot
    in the key server.
    
    This is a bit annoying because those things are known by other system
    software and by other opentrons software (the robot server controls the
    IP addresses since it calls `nmcli`, and the update server controls the
    hostname since it calls `hostnamectl`). We don't want to rely on other
    Opentrons software, so we need to communicate with the system software.
    In other places we do that via command line access; here, we do it via
    d-bus.
    
    D-Bus is nice because it's very well documented - services are
    established on d-bus in the first place by xml files that also serve as
    documentation - and because there's no text parsing involved. It's not
    nice because the python-dbus bindings are really bad and we can't use
    half the functionality of d-bus, like event notifications, without using
    some other mainloop than asyncio. But even if we have to poll, it's a
    lot better than dealing with asyncio subprocess.
    
    Closes EXEC-2476

--- key-server/key_server/tls/manager.py
+++ key-server/key_server/tls/manager.py
@@ -2,13 +2,13 @@
 
 import asyncio
 import logging
-import socket
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from typing import Final, Literal, Self, Type
 
 from .ca_manager import TLSCAManager
 from .ee_manager import TLSEEManager
+from .robot_details.interface import RobotDetails
 
 LOG = logging.getLogger(__name__)
 
@@ -18,11 +18,18 @@ class TLSManager:
 
     EXPIRY_CHECKER_POLL_PERIOD: Final = timedelta(hours=7)
 
-    def __init__(self, ca_manager: TLSCAManager, ee_manager: TLSEEManager) -> None:
+    def __init__(
+        self,
+        ca_manager: TLSCAManager,
+        ee_manager: TLSEEManager,
+        robot_details: RobotDetails,
+    ) -> None:
         """Build the TLSManager."""
         self._ca_manager = ca_manager
         self._ee_manager = ee_manager
         self._expiry_task: "asyncio.Task[None] | None" = None
+        self._robot_details_task: "asyncio.Task[None] | None" = None
+        self._robot_details = robot_details
 
     @classmethod
     async def create(
@@ -33,31 +40,46 @@ class TLSManager:
         terminator_reload: Literal["systemd-nginx", "dev-none"],
     ) -> Self:
         """Create a TLS manager, taking several useful setup steps."""
-        hostname = socket.gethostname()
+        if terminator_reload == "systemd-nginx":
+            # dynamic import to make it so this can still be run on dev machines where python-dbus is not present
+            from .robot_details.machine import RobotDetailsMachine
+
+            robot_details: RobotDetails = RobotDetailsMachine()
+        else:
+            from .robot_details.dev import RobotDetailsDev
+
+            robot_details = RobotDetailsDev()
+
+        robot_hostname, robot_ips = await robot_details.get_details()
         ca_manager = TLSCAManager(key_dir=ca_key_dir, ca_cert_dir=ca_cert_dir)
         ee_manager = TLSEEManager(
             key_dir=tls_ee_dir,
             ee_cert_dir=tls_ee_dir,
-            robot_hostname=hostname,
+            robot_hostname=robot_hostname or "unknown",
             # note: we are an early-init system. NetworkManager may not be up by this time, and while
             # we could use a subprocess to ifconfig -a, we may not have DHCP resolution by this time.
             # the easiest way is to start with no IP addresses and then to let the system that configures
             # IP addresses tell us when they start to exist.
-            robot_ips=[],
+            robot_ips=robot_ips,
             rotate_fn=(
                 TLSEEManager.rotate_cert_nginx
                 if terminator_reload == "systemd-nginx"
                 else TLSEEManager.rotate_cert_none
             ),
         )
-        obj = cls(ca_manager=ca_manager, ee_manager=ee_manager)
+
+        obj = cls(
+            ca_manager=ca_manager, ee_manager=ee_manager, robot_details=robot_details
+        )
         await obj.refresh_ee()
         await obj.schedule_expiry_task(cls.EXPIRY_CHECKER_POLL_PERIOD)
+        await obj.schedule_robot_details_task()
         return obj
 
     async def teardown(self) -> None:
         """Tear down the TLS manager at the end of a session."""
         await self.cancel_expiry_task()
+        await self.cancel_robot_details_task()
         self._ee_manager.remove_cert("shutting down")
 
     async def refresh_ee(self) -> None:
@@ -70,6 +92,12 @@ class TLSManager:
         """True if the expiry task is running properly. Mostly used internally and for testing."""
         return self._expiry_task is not None and not self._expiry_task.done()
 
+    def robot_details_task_running(self) -> bool:
+        """True if the robot details task is running properly. Mostly used internally and for testing."""
+        return (
+            self._robot_details_task is not None and not self._robot_details_task.done()
+        )
+
     async def cancel_expiry_task(self) -> None:
         """Cancel a running expiry task. Mostly used for testing; callers should prefer teardown()."""
         if not self._expiry_task:
@@ -80,18 +108,44 @@ class TLSManager:
                 await self._expiry_task
             except asyncio.CancelledError:
                 pass
-        self._expiry_task = None
+            finally:
+                self._expiry_task = None
+        else:
+            self._expiry_task = None
+
+    async def cancel_robot_details_task(self) -> None:
+        """Cancel a running robot details task. Mostly used for testing; callers should prefer teardown()."""
+        if not self._robot_details_task:
+            return
+        if not self._robot_details_task.done():
+            self._robot_details_task.cancel()
+            try:
+                await self._robot_details_task
+            except asyncio.CancelledError:
+                pass
+            finally:
+                self._robot_details_task = None
+        else:
+            self._robot_details_task = None
 
     async def schedule_expiry_task(self, poll_time: timedelta) -> None:
         """Create and run the expiry monitoring task. Mostly used for testing; callers should prefer create()."""
         await self.cancel_expiry_task()
         self._expiry_task = asyncio.create_task(self._expiry_task_outer(poll_time))
 
+    async def schedule_robot_details_task(self) -> None:
+        """Create and run the robot details monitoring task. Mostly used for testing; callers should prefer create()."""
+        await self.cancel_robot_details_task()
+        self._robot_details_task = asyncio.create_task(self._robot_details_task_outer())
+
     async def _expiry_task_outer(self, poll_time: timedelta) -> None:
         try:
             await self._expiry_task_inner(poll_time)
         except asyncio.CancelledError:
             LOG.info("Stopping expiry task")
+        except BaseException:
+            LOG.exception("Expiry task failed")
+            raise
 
     async def _expiry_task_inner(self, poll_time: timedelta) -> None:
         while True:
@@ -105,3 +159,23 @@ class TLSManager:
             if not self._ee_manager.ready(now + poll_time):
                 await self.refresh_ee()
             await asyncio.sleep(poll_time.total_seconds())
+
+    async def _robot_details_task_inner(self) -> None:
+        async for (
+            new_hostname,
+            new_ips,
+        ) in self._robot_details.yield_details_on_change():
+            LOG.info(f"Robot details changed to hostname={new_hostname} ips={new_ips}")
+            if not self._ee_manager.set_robot_details(
+                datetime.now(timezone.utc), new_hostname, new_ips
+            ):
+                await self.refresh_ee()
+
+    async def _robot_details_task_outer(self) -> None:
+        try:
+            await self._robot_details_task_inner()
+        except asyncio.CancelledError:
+            LOG.info("Stopping robot details task")
+        except BaseException:
+            LOG.exception("Robot details task failed")
+            raise
