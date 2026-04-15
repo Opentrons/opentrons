a26006539793da1fd746031901547390b73a724c
TamarZanzouri <zanzouritamar@gmail.com>
2026-04-15 09:46:04 -0400

    added module.css

--- app/src/pages/ODD/Login/index.tsx
+++ app/src/pages/ODD/Login/index.tsx
@@ -1,38 +1,26 @@
-import { useState } from 'react'
-import { createPortal } from 'react-dom'
-import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router-dom'
-
 import {
   Btn,
-  Flex,
+  InputField,
 } from '@opentrons/components'
 import { ChildNavigation } from '/app/organisms/ODD/ChildNavigation'
 
+import styles from './login.module.css'
+
 export function Login(): JSX.Element {
-  // const { t, i18n } = useTranslation([
-  //   'protocol_setup',
-  //   'devices_landing',
-  //   'shared',
-  // ])
-  // const navigate = useNavigate()
-  // const [showSetupInstructionsModal, setShowSetupInstructionsModal] =
-  //   useState<boolean>(false)
-
-  // const handleClickConfirm = (): void => {
-  //   navigate(-1)
-  // }
-
   return (
-    <Flex>
-      <ChildNavigation
-        header='Login'
-        buttonText='Login'
-        onClickButton={() => {}}
-      />
-      <h4>username</h4>
-      <input type="text" />
-      <Btn onClick={() => {}}>Login</Btn>
-    </Flex>
+    <>
+      <div className={styles.nav_container}>
+        <ChildNavigation
+          header="Login"
+          buttonText="Login"
+          onClickButton={() => {}}
+        />
+      </div>
+      <div className={styles.form_container}>
+        <h4>username</h4>
+        <InputField type="text" size="medium" />
+        <Btn onClick={() => {}}>Login</Btn>
+      </div>
+    </>
   )
 }
