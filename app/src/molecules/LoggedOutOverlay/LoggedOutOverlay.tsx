ce7f8bc00f6e8758ae32f02a375b75d953ec093e
Max Marrone <max@opentrons.com>
2026-04-16 12:43:21 -0400

    Add some half-assed aria roles to the overlay.
    This doesn't implement proper overlay accessibility--it needs to support the keyboard better. We don't currently have a pattern for that in the codebase. Our other modals have the same issues with the keyboard.

--- app/src/molecules/LoggedOutOverlay/LoggedOutOverlay.tsx
+++ app/src/molecules/LoggedOutOverlay/LoggedOutOverlay.tsx
@@ -14,8 +14,19 @@ export interface LoggedOutOverlayProps {
 export function LoggedOutOverlay(props: LoggedOutOverlayProps): JSX.Element {
   const { onClick } = props
 
+  // todo(mm, 2026-04-16): Handle keyboard interaction:
+  // - Allow Enter/Spacebar/whatever to dismiss the overlay just like tapping does.
+  //   (Maybe put the icon in a <button> for focusability.)
+  // - Trap focus so the user can't use the keyboard to navigate underneath the overlay.
+
   return (
-    <div className={styles.overlay} onClick={onClick}>
+    <div
+      className={styles.overlay}
+      onClick={onClick}
+      role="dialog"
+      aria-modal="true"
+      aria-label="Logged out"
+    >
       <div className={styles.acm_badge}>
         <Icon name="acm" className={styles.acm_icon} />
       </div>
