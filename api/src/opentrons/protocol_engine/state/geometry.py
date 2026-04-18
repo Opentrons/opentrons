fb69dbd5b5eac2516e541a1b6fa765828fa55d49
vegano1 <brayan.almonte@gmail.com>
2026-04-18 15:54:18 -0400

    add containment_utils.py and move is_fully_contained function to it + unit tests

--- api/src/opentrons/protocol_engine/state/geometry.py
+++ api/src/opentrons/protocol_engine/state/geometry.py
@@ -17,7 +17,6 @@ from opentrons_shared_data.errors.exceptions import (
 )
 from opentrons_shared_data.labware.constants import WELL_NAME_PATTERN
 from opentrons_shared_data.labware.labware_definition import (
-    ContainedSpace,
     InnerWellGeometry,
     LabwareDefinition,
     LabwareDefinition2,
@@ -936,41 +935,6 @@ class GeometryView:
                 "Provided location is not linked to an addressable area on the deck.",
                 details={"terminal-location": repr(current_loc)},
             )
-
-    def _is_fully_contained(
-        self, resident_def: LabwareDefinition, boundary_space: ContainedSpace
-    ) -> bool:
-        """Validates that the resident fits entirely within the boundary_space box.
-
-        All coordinates are relative to the parent's (0,0,0).
-        """
-        # TODO: Add LabwareSchemaV3 support
-        if isinstance(resident_def, LabwareDefinition3):
-            return True
-        # Resident Extents (Standard labware starts at 0,0,0 relative to parent)
-        res_min_x, res_max_x = 0.0, resident_def.dimensions.xDimension
-        res_min_y, res_max_y = 0.0, resident_def.dimensions.yDimension
-        res_min_z, res_max_z = 0.0, resident_def.dimensions.zDimension
-
-        # Boundary Extents (The 'hole' defined in the JSON)
-        bound_min_x = boundary_space.origin.x
-        bound_max_x = bound_min_x + boundary_space.dimensions.xDimension
-
-        bound_min_y = boundary_space.origin.y
-        bound_max_y = bound_min_y + boundary_space.dimensions.yDimension
-
-        bound_min_z = boundary_space.origin.z
-        bound_max_z = bound_min_z + boundary_space.dimensions.zDimension
-
-        # Logical Check: Resident must be bounded by the boundary planes
-        return (
-            res_min_x >= bound_min_x
-            and res_max_x <= bound_max_x
-            and res_min_y >= bound_min_y
-            and res_max_y <= bound_max_y
-            and res_min_z >= bound_min_z
-            and res_max_z <= bound_max_z
-        )
 
     def ensure_location_not_occupied(
         self,
