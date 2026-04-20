d04f838f1465f02318fb8e068da2998c25142306
Max Marrone <max@opentrons.com>
2026-04-20 13:01:05 -0400

    Delete hooks.ts, for now.

--- app/src/redux/robot-auth/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux'
-
-import { getRobotToken } from './slice'
-
-import type { State } from '/app/redux/types'
-
-export function useRobotToken(robotName: string | null): string | null {
-  return useSelector((state: State) => getRobotToken(state, robotName))
-}
