ad7f09e187b352a679ddc7c545828a11e2c22249
CaseyBatten <caseyhbatten@gmail.com>
2026-04-21 14:55:59 -0400

    prototype server side validator

--- api/src/opentrons/util/pyro/pyro_synchronous_adapter.py
+++ api/src/opentrons/util/pyro/pyro_synchronous_adapter.py
@@ -14,6 +14,7 @@ from opentrons.util.pyro.pyro_serialization import (
     TypedDictWrapper,
     UnhashableDictWrapper,
 )
+from opentrons.util.pyro.pyro_client_async_adapter import AsyncClientPyroObject, AsyncPyroFunctionWrapper
 
 T = TypeVar("T")
 P = ParamSpec("P")
@@ -382,6 +383,40 @@ def get_pyro_attributes_with_proxy_result(self: Any) -> list[str]:
     result: list[str] = self._pyro_attributes_with_proxy_results
     return result
 
+# Validators
+# CASEY NOTE: This won't be called on non-pyro behavior functions. Maybe those need to be checked too.
+def _validated_parameters(*args: P.args, **kwargs: P.kwargs) -> tuple[tuple, dict]:
+        def _validations(arg: Any) -> Any:
+            # NOTE: Extend this as further validations are needed
+            arg = _validate_inbound_proxy(arg)
+            return arg
+
+        validated_args = tuple()  # type: ignore
+        for arg in args:
+            # Validate each non-keyword argument and reconstruct the argument tuple
+            validated_args = (*validated_args, _validations(arg))
+        # Validate each keyword argument and reconstruct the kwargs dictionary
+        kwargs = {key: _validations(kwargs[key]) for key in kwargs.keys()}
+        return (validated_args, kwargs)
+
+def _validate_inbound_proxy(arg: Any) -> Any:
+    """Handle an argument which is a remote Proxy that may have been forwarded through multiple processes."""
+    if isinstance(arg, pyro.Proxy):
+        try:
+            if arg.is_callable:
+                arg = AsyncPyroFunctionWrapper(proxy=arg)
+        except AttributeError:
+            try:
+                iter(arg)
+                validated_arg = []
+                for r in arg:
+                    validated_arg.append(AsyncClientPyroObject(r))
+                arg = validated_arg
+            except AttributeError:
+                arg = AsyncClientPyroObject(arg)
+    return arg
+
+# CASEY NOTE: put some kind of generic "validator wrapper" here? is there even a usecase where non-pso decorated functions get these? maybe create() for protocol engine
 
 ### Specialty Functions for use with the `pyro_behavior` decorator ###
 
@@ -401,6 +436,8 @@ def convert_result_to_proxy(  # noqa: C901
     @functools.wraps(attr)
     def wrapper(self: Any, *args: P.args, **kwargs: P.kwargs) -> Any:  # noqa: C901
         # Of note, the wrapper passes self to terminate the self instance passed by the PSO
+        args, kwargs = _validated_parameters(*args, **kwargs)
+        print(f"HERE WITH: {args} --- {kwargs}")
         if inspect.iscoroutinefunction(attr):
             sync_func = synchronous(attr)
             bound_method = MethodType(sync_func, core_obj)
@@ -458,6 +495,7 @@ def convert_result_to_wrapped_dict(  # noqa: C901
     @functools.wraps(attr)
     def wrapper(self: Any, *args: P.args, **kwargs: P.kwargs) -> Any:
         # Of note, the wrapper passes self to terminate the self instance passed by the PSO
+        args, kwargs = _validated_parameters(*args, **kwargs)
         if inspect.iscoroutinefunction(attr):
             sync_func = synchronous(attr)
             bound_method = MethodType(sync_func, core_obj)
@@ -523,6 +561,7 @@ def convert_type_to_instance(
 
     @functools.wraps(attr)
     def wrapper(self: Any, *args: P.args, **kwargs: P.kwargs) -> Any:
+        args, kwargs = _validated_parameters(*args, **kwargs)
         if inspect.iscoroutinefunction(attr):
             sync_func = synchronous(attr)
             result = sync_func(self, *args, **kwargs)
@@ -555,6 +594,7 @@ def convert_result_to_wrapped_typed_dict(
 
     @functools.wraps(attr)
     def wrapper(self: Any, *args: P.args, **kwargs: P.kwargs) -> Any:
+        args, kwargs = _validated_parameters(*args, **kwargs)
         if inspect.iscoroutinefunction(attr):
             sync_func = synchronous(attr)
             bound_method = MethodType(sync_func, core_obj)
