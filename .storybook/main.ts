1275e0e2276ea31765b4fd218a1cdc56716f7d44
koji <baxin1919@gmail.com>
2026-04-25 18:32:41 -0400

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,10 @@
 // This file has been automatically migrated to valid ESM format by Storybook.
-import { createRequire } from "node:module";
+import { createRequire } from 'node:module'
 import { dirname, join } from 'path'
 
 import type { StorybookConfig } from '@storybook/react-vite'
 
-const require = createRequire(import.meta.url);
+const require = createRequire(import.meta.url)
 
 const config: StorybookConfig = {
   stories: [
@@ -24,7 +24,6 @@ const config: StorybookConfig = {
     getAbsolutePath('storybook-addon-pseudo-states'),
     getAbsolutePath('@storybook/addon-docs'),
   ],
-
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {
