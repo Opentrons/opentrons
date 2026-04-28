e6edec9568eedccd7c16a68f28fefbf7feb86671
Rhyann <rhyannclarke6@gmail.com>
2026-04-28 17:05:15 -0400

    change this only for OT2

--- step-generation/src/utils/safePipetteMovements.ts
+++ step-generation/src/utils/safePipetteMovements.ts
@@ -350,11 +350,11 @@ export const getIsSafePipetteMovement = (args: {
   const robotType = isFlexPipette ? FLEX_ROBOT_TYPE : OT2_ROBOT_TYPE
 
   const deckDefinition = getDeckDefFromRobotType(robotType)
-  //  early exit if labwareId is a trashBin or wasteChute or if no well name is provided or if 1ch pipette
+  //  early exit if labwareId is a trashBin or wasteChute or if no well name is provided or if 1ch OT-2 pipette
   if (
     labwareEntities[labwareId] == null ||
     wellTargetName == null ||
-    channels === 1
+    (channels === 1 && robotType === OT2_ROBOT_TYPE)
   ) {
     return true
   }
