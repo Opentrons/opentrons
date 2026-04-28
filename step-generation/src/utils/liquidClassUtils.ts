b3c71ab44a2269099e595af893940a687971fbff
Rhyann <rhyannclarke6@gmail.com>
2026-04-28 11:09:28 -0400

    add constant

--- step-generation/src/utils/liquidClassUtils.ts
+++ step-generation/src/utils/liquidClassUtils.ts
@@ -1,4 +1,7 @@
-import { getAllLiquidClassDefs } from '@opentrons/shared-data'
+import {
+  getAllLiquidClassDefs,
+  POSITION_REFERENCE_TOP,
+} from '@opentrons/shared-data'
 
 import {
   DEST_WELL_BLOWOUT_DESTINATION,
@@ -282,7 +285,7 @@ const getBlowoutWellPosition = (
       y: args.blowoutYPosition ?? 0,
       z: args.blowoutOffsetFromTopMm ?? 1,
     },
-    position_reference: args.blowoutPositionReference ?? 'well-top',
+    position_reference: args.blowoutPositionReference ?? POSITION_REFERENCE_TOP,
   }
 }
 
