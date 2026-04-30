7184a0db6d2eb8a644ebfa63a5d54ff85f86a328
koji <baxin1919@gmail.com>
2026-04-29 22:57:27 -0400

    fix lint error

--- app/src/organisms/ODD/ProtocolSetup/ProtocolSetupParameters/ViewOnlyParameters.tsx
+++ app/src/organisms/ODD/ProtocolSetup/ProtocolSetupParameters/ViewOnlyParameters.tsx
@@ -1,10 +1,6 @@
 import { useTranslation } from 'react-i18next'
 
-import {
-  Chip,
-  COLORS,
-  StyledText,
-} from '@opentrons/components'
+import { Chip, COLORS, StyledText } from '@opentrons/components'
 import {
   formatRunTimeParameterValue,
   sortRuntimeParameters,
