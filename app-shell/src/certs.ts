500cf9a1bc4a70b317dc72cae1f2addb1ea5cc70
Seth Foster <seth@opentrons.com>
2026-05-01 10:10:22 -0400

    add ttl checking

--- app-shell/src/certs.ts
+++ app-shell/src/certs.ts
@@ -10,6 +10,14 @@ const promisifiedPBKDF = promisify(pbkdf2)
 
 const knownCertificates = new Map<string, X509Certificate>()
 
+// certificates encrypted more than 1 minute ago and less than 10 years ago (so that things don't break
+// if the robot's time isn't synced) will fail. also if it's from the future by too much something odd is
+// going on and it's better to avoid things
+const CERT_DECRYPTION_TTL_RECENT_S = 60
+const CERT_DECRYPTION_TTL_NOT_RECENT_S = 10 * 365 * 24 * 60 * 60
+const CERT_DECRYPTION_FORWARD_CLOCK_SKEW_NEAR_S = 24 * 60 * 60
+const CERT_DECRYPTION_FORWARD_CLOCK_SKEW_FAR_S = 10 * 365 * 24 * 60 * 60
+
 function addCertificateToMap(certificate: X509Certificate): void {
   const sha256Fingerprint = certificate.fingerprint256
   knownCertificates.set(sha256Fingerprint, certificate)
@@ -71,9 +79,33 @@ export async function decryptFromOTDetails(
   // the token is a binary packed struct base64 encoded, so decode and unpack the parts
   // we need
   const decodedToken = Buffer.from(token, 'base64url')
+  const version = decodedToken.subarray(0, 1)
+  const time = decodedToken.subarray(1, 9)
   const iv = decodedToken.subarray(9, 25)
   const ciphertext = decodedToken.subarray(25, -32)
   const hmac = decodedToken.subarray(-32)
+  if (version[0] !== 128) {
+    throw new Error('Bad encryption version')
+  }
+  const timeVal = time.readBigInt64BE()
+  if (timeVal > Number.MAX_SAFE_INTEGER / 1000) {
+    throw new Error('Time too far in the future')
+  }
+  const tokenTime = new Date(Number(timeVal) * 1000).valueOf()
+  const now = Date.now()
+  const timeDiff = (now - tokenTime) / 1000
+  if (
+    timeDiff > CERT_DECRYPTION_TTL_RECENT_S &&
+    timeDiff < CERT_DECRYPTION_TTL_NOT_RECENT_S
+  ) {
+    throw new Error('Encrypted certificate is too old')
+  }
+  if (
+    timeDiff < -CERT_DECRYPTION_FORWARD_CLOCK_SKEW_NEAR_S &&
+    timeDiff > -CERT_DECRYPTION_FORWARD_CLOCK_SKEW_FAR_S
+  ) {
+    throw new Error('Encrypted certificate is from the future')
+  }
   // we can decrypt the message with the crypt key
   const decryptedData = await subtle.decrypt(
     {
