63725eb4833c1eda2df32c223c619a1ad702b164
jbleon95 <jeremybenleon@gmail.com>
2026-05-04 15:30:12 -0400

    rename run orchestrator to run coordinator

--- robot-server/tests/runs/test_run_orchestrator_store.py
+++ robot-server/tests/runs/test_run_orchestrator_store.py
@@ -32,7 +32,7 @@ from opentrons_shared_data.robot.types import RobotType
 from robot_server.protocols.protocol_models import ProtocolKind
 from robot_server.protocols.protocol_store import ProtocolResource
 from robot_server.runs.run_orchestrator_store import (
-    NoRunOrchestrator,
+    NoRunCoordinator,
     RunConflictError,
     RunOrchestratorStore,
     handle_hardware_event,
@@ -113,8 +113,8 @@ async def test_create_engine(decoy: Decoy, subject: RunOrchestratorStore) -> Non
 
     assert subject.current_run_id == "run-id"
     assert isinstance(result, StateSummary)
-    assert subject._run_orchestrator is not None
-    assert isinstance(subject._run_orchestrator, RunOrchestrator)
+    assert subject._run_coordinator is not None
+    assert isinstance(subject._run_coordinator, RunOrchestrator)
 
 
 @pytest.mark.parametrize("robot_type", ["OT-2 Standard", "OT-3 Standard"])
@@ -149,7 +149,7 @@ async def test_create_engine_uses_robot_type(
         notify_publishers=mock_notify_publishers,
     )
 
-    assert subject._run_orchestrator is not None
+    assert subject._run_coordinator is not None
 
 
 async def test_create_engine_with_labware_offsets(
@@ -259,8 +259,8 @@ async def test_clear_engine(subject: RunOrchestratorStore) -> None:
         camera_provider=CameraProvider(),
         notify_publishers=mock_notify_publishers,
     )
-    assert subject._run_orchestrator is not None
-    engine = subject._run_orchestrator._protocol_engine  # type: ignore[union-attr]
+    assert subject._run_coordinator is not None
+    engine = subject._run_coordinator._protocol_engine  # type: ignore[union-attr]
     engine.state_view.state.commands.command_history._queued_command_ids.add("1231")
     result = await subject.clear()
     assert (
@@ -270,8 +270,8 @@ async def test_clear_engine(subject: RunOrchestratorStore) -> None:
     assert subject.current_run_id is None
     assert isinstance(result, RunResult)
 
-    with pytest.raises(NoRunOrchestrator):
-        subject.run_orchestrator
+    with pytest.raises(NoRunCoordinator):
+        subject.run_coordinator
 
 
 async def test_clear_engine_not_stopped_or_idle(subject: RunOrchestratorStore) -> None:
@@ -288,8 +288,8 @@ async def test_clear_engine_not_stopped_or_idle(subject: RunOrchestratorStore) -
         camera_provider=CameraProvider(),
         notify_publishers=mock_notify_publishers,
     )
-    assert subject._run_orchestrator is not None
-    subject._run_orchestrator.play(deck_configuration=[])
+    assert subject._run_coordinator is not None
+    subject._run_coordinator.play(deck_configuration=[])
     with pytest.raises(RunConflictError):
         await subject.clear()
 
@@ -308,13 +308,13 @@ async def test_clear_idle_engine(subject: RunOrchestratorStore) -> None:
         camera_provider=CameraProvider(),
         notify_publishers=mock_notify_publishers,
     )
-    assert subject._run_orchestrator is not None
+    assert subject._run_coordinator is not None
 
     await subject.clear()
 
     # TODO: test engine finish is called
-    with pytest.raises(NoRunOrchestrator):
-        subject.run_orchestrator
+    with pytest.raises(NoRunCoordinator):
+        subject.run_coordinator
 
 
 async def test_get_default_orchestrator_idempotent(
@@ -420,7 +420,7 @@ async def test_estop_callback(
 ) -> None:
     """The callback should stop an active engine."""
     run_orchestrator_store = decoy.mock(cls=RunOrchestratorStore)
-    decoy.when(run_orchestrator_store.run_orchestrator).then_return(
+    decoy.when(run_orchestrator_store.run_coordinator).then_return(
         decoy.mock(cls=RunOrchestrator)
     )
 
@@ -433,9 +433,9 @@ async def test_estop_callback(
 
     decoy.when(run_orchestrator_store.current_run_id).then_return(None)
     await handle_hardware_event(run_orchestrator_store, disengage_event)
-    assert run_orchestrator_store.run_orchestrator is not None
+    assert run_orchestrator_store.run_coordinator is not None
     decoy.verify(
-        run_orchestrator_store.run_orchestrator.estop(),
+        run_orchestrator_store.run_coordinator.estop(),
         ignore_extra_args=True,
         times=0,
     )
@@ -447,10 +447,10 @@ async def test_estop_callback(
 
     decoy.when(run_orchestrator_store.current_run_id).then_return("fake-run-id")
     await handle_hardware_event(run_orchestrator_store, engage_event)
-    assert run_orchestrator_store._run_orchestrator is not None
+    assert run_orchestrator_store._run_coordinator is not None
     decoy.verify(
-        run_orchestrator_store.run_orchestrator.estop(),
-        await run_orchestrator_store.run_orchestrator.finish(
+        run_orchestrator_store.run_coordinator.estop(),
+        await run_orchestrator_store.run_coordinator.finish(
             error=matchers.IsA(EStopActivatedError)
         ),
         times=1,
@@ -460,7 +460,7 @@ async def test_estop_callback(
 async def test_async_module_callback_noops_with_no_engine(decoy: Decoy) -> None:
     """It should noop without a run."""
     run_orchestrator_store = decoy.mock(cls=RunOrchestratorStore)
-    decoy.when(run_orchestrator_store.run_orchestrator).then_return(
+    decoy.when(run_orchestrator_store.run_coordinator).then_return(
         decoy.mock(cls=RunOrchestrator)
     )
 
@@ -474,9 +474,9 @@ async def test_async_module_callback_noops_with_no_engine(decoy: Decoy) -> None:
 
     decoy.when(run_orchestrator_store.current_run_id).then_return(None)
     await handle_hardware_event(run_orchestrator_store, error_event)
-    assert run_orchestrator_store.run_orchestrator is not None
+    assert run_orchestrator_store.run_coordinator is not None
     decoy.verify(
-        await run_orchestrator_store.run_orchestrator.asynchronous_module_error(
+        await run_orchestrator_store.run_coordinator.asynchronous_module_error(
             module_model=matchers.Anything(), module_serial=matchers.Anything()
         ),
         times=0,
@@ -491,7 +491,7 @@ async def test_async_module_callback_noops_with_no_engine(decoy: Decoy) -> None:
 async def test_async_module_callback_noops_if_engine_says_no(decoy: Decoy) -> None:
     """It shouldn't finish if the engine doesn't want it to."""
     run_orchestrator_store = decoy.mock(cls=RunOrchestratorStore)
-    decoy.when(run_orchestrator_store.run_orchestrator).then_return(
+    decoy.when(run_orchestrator_store.run_coordinator).then_return(
         decoy.mock(cls=RunOrchestrator)
     )
 
@@ -505,15 +505,15 @@ async def test_async_module_callback_noops_if_engine_says_no(decoy: Decoy) -> No
 
     decoy.when(run_orchestrator_store.current_run_id).then_return("fake-run-id")
     decoy.when(
-        await run_orchestrator_store.run_orchestrator.asynchronous_module_error(
+        await run_orchestrator_store.run_coordinator.asynchronous_module_error(
             module_model=TemperatureModuleModel.TEMPERATURE_V2,
             module_serial="some-serial",
         )
     ).then_return(False)
     await handle_hardware_event(run_orchestrator_store, error_event)
-    assert run_orchestrator_store._run_orchestrator is not None
+    assert run_orchestrator_store._run_coordinator is not None
     decoy.verify(
-        await run_orchestrator_store.run_orchestrator.finish(error=None),
+        await run_orchestrator_store.run_coordinator.finish(error=None),
         ignore_extra_args=True,
         times=0,
     )
@@ -522,7 +522,7 @@ async def test_async_module_callback_noops_if_engine_says_no(decoy: Decoy) -> No
 async def test_async_module_callback_finishes_if_engine_says_so(decoy: Decoy) -> None:
     """It should finish with the error if the engine says it should."""
     run_orchestrator_store = decoy.mock(cls=RunOrchestratorStore)
-    decoy.when(run_orchestrator_store.run_orchestrator).then_return(
+    decoy.when(run_orchestrator_store.run_coordinator).then_return(
         decoy.mock(cls=RunOrchestrator)
     )
 
@@ -536,13 +536,13 @@ async def test_async_module_callback_finishes_if_engine_says_so(decoy: Decoy) ->
     decoy.when(run_orchestrator_store.current_run_id).then_return("fake-run-id")
 
     decoy.when(
-        await run_orchestrator_store.run_orchestrator.asynchronous_module_error(
+        await run_orchestrator_store.run_coordinator.asynchronous_module_error(
             module_model=TemperatureModuleModel.TEMPERATURE_V2,
             module_serial="some-serial",
         )
     ).then_return(True)
     await handle_hardware_event(run_orchestrator_store, error_event)
-    assert run_orchestrator_store._run_orchestrator is not None
+    assert run_orchestrator_store._run_coordinator is not None
     decoy.verify(
-        await run_orchestrator_store.run_orchestrator.finish(error=exc),
+        await run_orchestrator_store.run_coordinator.finish(error=exc),
     )
