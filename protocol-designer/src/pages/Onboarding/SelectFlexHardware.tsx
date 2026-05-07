6ffa4c476287817e146ccd8b5a7a81a50b3c2c75
koji <baxin1919@gmail.com>
2026-05-07 17:21:44 -0400

    feat(protocol-designer): remove ff and tweak the onboarding flow
    remove ff and tweak the onboarding flow
    
    close EXEC-2621 and EXEC-2628

--- protocol-designer/src/pages/Onboarding/SelectFlexHardware.tsx
+++ protocol-designer/src/pages/Onboarding/SelectFlexHardware.tsx
@@ -1,14 +1,12 @@
 import { useTranslation } from 'react-i18next'
 
-import { FLEX_ROBOT_TYPE } from '@opentrons/shared-data'
-
 import { HandleEnter } from '../../components/atoms'
 import { HardwareConfigurator } from '../../components/organisms/HardwareConfigurator'
 import { WizardBody } from './WizardBody'
 
 import type { WizardTileProps } from './types'
 
-export function SelectHardware(props: WizardTileProps): JSX.Element | null {
+export function SelectFlexHardware(props: WizardTileProps): JSX.Element | null {
   const { goBack, proceed, watch, setValue } = props
   const fixtures = watch('fixtures')
   const modules = watch('modules')
@@ -22,7 +20,6 @@ export function SelectHardware(props: WizardTileProps): JSX.Element | null {
   return (
     <HandleEnter onEnter={handleProceed}>
       <WizardBody
-        robotType={FLEX_ROBOT_TYPE}
         stepNumber={2}
         subStepNumber={5}
         header={t('configure_deck_hardware')}
