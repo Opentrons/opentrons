35c34ab29f892ee6498acefbed7f92b44715654e
Max Marrone <max@opentrons.com>
2026-05-08 14:00:56 -0400

    Try a different way.

--- react-api-client/src/api/ApiHostProvider.tsx
+++ react-api-client/src/api/ApiHostProvider.tsx
@@ -1,35 +1,25 @@
 import * as React from 'react'
 
-import type { AxiosRequestConfig } from 'axios'
-import type { HostConfig, ResponsePromise } from '@opentrons/api-client'
+import type { HostConfig } from '@opentrons/api-client'
 
 export const ApiHostContext = React.createContext<HostConfig | null>(null)
 
-export interface ApiHostProviderProps {
-  hostname: string | null
-  port?: number | null
-  requestor?: <ResData>(config: AxiosRequestConfig) => ResponsePromise<ResData>
-  robotName?: string | null
-  token?: string
+export type ApiHostProviderProps = Omit<HostConfig, 'hostname'> & {
+  hostname: HostConfig['hostname'] | null
+} & {
   children?: React.ReactNode
 }
 
 export function ApiHostProvider(props: ApiHostProviderProps): JSX.Element {
-  const {
-    hostname,
-    port = null,
-    requestor,
-    robotName = null,
-    token,
-    children,
-  } = props
+  const { hostname, port, secure, requestor, robotName, token, children } =
+    props
 
   const hostConfig = React.useMemo<HostConfig | null>(
     () =>
       hostname !== null
-        ? { hostname, port, requestor, robotName, token }
+        ? { hostname, port, secure, requestor, robotName, token }
         : null,
-    [hostname, port, requestor, robotName, token]
+    [hostname, port, secure, requestor, robotName, token]
   )
 
   return (
