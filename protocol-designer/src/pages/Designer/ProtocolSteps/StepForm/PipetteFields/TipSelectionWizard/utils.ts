a56c58a24d83221930b7116c99c69e911ac7b3b8
Nick Diehl <47604184+ncdiehl11@users.noreply.github.com>
2026-05-13 15:07:25 -0400

    feat(protocol-designer): always allow entering tip selector modal (#21485)
    Since it can be unclear why tip selection may cause collisions, product
    has specified to allow the user to enter the tip selector modal for a
    visual guide to tiprack and tip accessibility. We should allow the user
    to enter the modal even if there are no possible tipracks + tips that
    are able to accommodate the mix or transfer plan. Similarly, we should
    allow selecting "bad" tipracks with a meaningful error or warning inline
    notification.
    
    Closes RQA-5381

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/utils.ts
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/TipSelectionWizard/utils.ts
@@ -185,17 +185,10 @@ export function getIsTiprackSelectable(args: {
   pipetteSpecs: PipetteV2Specs
   nozzles: NozzleConfigurationStyle
   labwareEntities: LabwareEntities
-  validTiprackIds: string[]
 }): boolean {
   // TODO: check if tiprack is on stacker. Will bottom of stack still be slot?
-  const {
-    labware,
-    formTiprackUri,
-    pipetteSpecs,
-    nozzles,
-    labwareEntities,
-    validTiprackIds,
-  } = args
+  const { labware, formTiprackUri, pipetteSpecs, nozzles, labwareEntities } =
+    args
   const { channels } = pipetteSpecs
   const { def, labwareDefURI, stack } = labware
   const isPickupCompatibleWithPossibleAdapter =
@@ -210,9 +203,76 @@ export function getIsTiprackSelectable(args: {
     getIsTiprack(def) &&
     labwareDefURI === formTiprackUri &&
     !COLUMN_4_SLOTS.includes(slot) &&
-    isPickupCompatibleWithPossibleAdapter &&
-    validTiprackIds.includes(labware.id)
+    isPickupCompatibleWithPossibleAdapter
   )
+}
+
+interface GetIsTiprackSelectableAndValidArgs {
+  labware: LabwareOnDeck
+  formTiprackUri: string
+  pipetteSpecs: PipetteV2Specs
+  nozzles: NozzleConfigurationStyle
+  labwareEntities: LabwareEntities
+  validTiprackIds: string[]
+}
+
+export const getIsTiprackSelectableAndValid = (
+  args: GetIsTiprackSelectableAndValidArgs
+): boolean => {
+  const {
+    labware,
+    formTiprackUri,
+    pipetteSpecs,
+    nozzles,
+    labwareEntities,
+    validTiprackIds,
+  } = args
+  const isSelectable = getIsTiprackSelectable({
+    labware,
+    formTiprackUri,
+    pipetteSpecs,
+    nozzles,
+    labwareEntities,
+  })
+  return isSelectable && validTiprackIds.includes(labware.id)
+}
+
+export const getAreAnyMatchingTipracksSelectable = (args: {
+  allLabware: LabwareOnDeck[]
+  formTiprackUri: string
+  pipetteSpecs: PipetteV2Specs
+  nozzles: NozzleConfigurationStyle
+  labwareEntities: LabwareEntities
+  validTiprackIds: string[]
+}): boolean => {
+  const {
+    allLabware,
+    formTiprackUri,
+    pipetteSpecs,
+    nozzles,
+    labwareEntities,
+    validTiprackIds,
+  } = args
+  const { channels } = pipetteSpecs
+  return validTiprackIds.some(id => {
+    const labware = allLabware.find(l => l.id === id)
+    if (labware == null) {
+      return false
+    }
+    const { def, labwareDefURI, stack } = labware
+    const isPickupCompatibleWithPossibleAdapter =
+      getIsPickupCompatibleWithPossibleAdapter(
+        stack,
+        labwareEntities,
+        nozzles,
+        channels
+      )
+    return (
+      getIsTiprack(def) &&
+      labwareDefURI === formTiprackUri &&
+      isPickupCompatibleWithPossibleAdapter
+    )
+  })
 }
 
 export const getAllWellsInColumn = (
