3c93b4a1db159541d79dbfa94d543d23778f624f
Seth Foster <seth@opentrons.com>
2026-05-14 17:01:44 -0400

    feat(app): display ODD password modal when you need it from desktop (#21486)
    We want the view password modal on the ODD to pop up automatically when
    you enter the import-certificate UI on the desktop app.
    
    The way we do things like that is to add stuff to `/clientData`, a robot
    server endpoint that lets clients put some data in server storage to be
    read by other clients - the server doesn't care what it is. We can do
    that with unique values per key request invocation, and then we can get
    whatever behavior we want with when the modal on the ODD should open or
    close.
    
    This PR implements it so that:
    - Opening the import-certificate modal on the desktop causes the
    view-password modal to open on the ODD, if it wasn't already open
    - Regardless of why the view-password modal on the ODD was open, if you
    close the view-password modal on the ODD with its UI it will close
    - When you close the import-certificate modal on the desktop, if the
    only reason the view-password modal on the ODD was open was because of
    the import-certificate modal, it will close
    - If you open the view-password modal on the ODD, then open the
    import-certificate modal on the desktop, then close the
    import-certificate modal on the desktop, the view-password modal will
    stay open
    
    Note that you can replace "on the desktop" with "on any number of
    desktop clients" and all the behavior should be the same.
    
    Also note that unfortunately we don't really have atomic dictionary key
    append in client data so the modal can get left open incorrectly
    sometimes. Open to ideas for fixing this.
    
    Closes EXEC-2632

---
+++ app/src/organisms/ODD/RobotSettingsDashboard/RobotEncryptionKey/RobotEncryptionKeyModal.tsx
@@ -0,0 +1,82 @@
+import { useTranslation } from 'react-i18next'
+import NiceModal, { useModal } from '@ebay/nice-modal-react'
+
+import { StyledText } from '@opentrons/components'
+import {
+  ApiHostProvider,
+  useCACertPasswordQuery,
+} from '@opentrons/react-api-client'
+
+import { SmallButton } from '/app/atoms/buttons'
+import { OddModal } from '/app/molecules/OddModal'
+import { appShellInternalApiRequestor } from '/app/redux/shell/remote'
+import { useUpdateClientDataEncryptionKeys } from '/app/resources/client_data/encryptionKeys'
+
+import styles from './robot_encryption_key_modal.module.css'
+
+const BACKUP_REFETCH_TIME_MS = 1000
+
+export function refetchTimeForPassword(now: Date, validUntil: Date): number {
+  return Math.max(validUntil.getTime() - now.getTime(), 1)
+}
+
+function RobotEncryptionKeyModalElement({
+  clearClientData,
+}: {
+  clearClientData: () => void
+}): JSX.Element {
+  const { i18n, t } = useTranslation(['device_settings', 'shared', 'branded'])
+  const modal = useModal()
+
+  const close = (): void => {
+    modal.remove()
+    clearClientData()
+  }
+  const { password } = useCACertPasswordQuery({
+    refetchInterval: query =>
+      !!query
+        ? refetchTimeForPassword(
+            new Date(),
+            new Date(query.data.valid_until_utc)
+          )
+        : BACKUP_REFETCH_TIME_MS,
+  }).data?.data ?? { password: '' }
+  const header = {
+    title: t('device_settings:robot_encryption_key'),
+    onClick: close,
+  }
+
+  return (
+    <OddModal
+      header={header}
+      className={styles.modal_container}
+      modalSize="small"
+    >
+      <div className={styles.modal_content}>
+        <div className={styles.password_container}>
+          <StyledText oddStyle="bodyTextRegular">{password}</StyledText>
+        </div>
+        <StyledText oddStyle="bodyTextRegular">
+          {t('branded:enter_this_key_into_the_app')}
+        </StyledText>
+      </div>
+      <SmallButton
+        flex="1"
+        buttonText={i18n.format(t('shared:ok'), 'capitalize')}
+        onClick={close}
+      />
+    </OddModal>
+  )
+}
+
+export const RobotEncryptionKeyModal = NiceModal.create((): JSX.Element => {
+  const { clearClientData } = useUpdateClientDataEncryptionKeys()
+  return (
+    <ApiHostProvider
+      hostname={_ODD_IP_ ?? 'localhost'}
+      requestor={appShellInternalApiRequestor}
+    >
+      <RobotEncryptionKeyModalElement clearClientData={clearClientData} />
+    </ApiHostProvider>
+  )
+})
