d0e1d819eccd5310c31bb8fdb05f9a11c98eabe0
koji <baxin1919@gmail.com>
2026-05-15 10:37:52 -0400

    fix(app): fix float number handling for an external keyboard (#21497)
    * fix(app): fix float number handling for an external keyboard

--- app/src/organisms/ODD/ProtocolSetup/ProtocolSetupParameters/__tests__/ChooseNumber.test.tsx
+++ app/src/organisms/ODD/ProtocolSetup/ProtocolSetupParameters/__tests__/ChooseNumber.test.tsx
@@ -103,4 +103,15 @@ describe('ChooseNumber', () => {
     fireEvent.click(screen.getAllByRole('button')[0])
     expect(mockMakeSnackbar).toHaveBeenCalledWith('Value must be in range')
   })
+
+  it('should delete external keyboard input with the numerical keyboard', () => {
+    props = { ...props, parameter: mockFloatNumberParameterData }
+    render(props)
+    const input = screen.getByLabelText('EtoH Volume')
+
+    fireEvent.change(input, { target: { value: '12' } })
+    fireEvent.click(screen.getByRole('button', { name: 'del' }))
+
+    expect(input).toHaveValue('1')
+  })
 })
