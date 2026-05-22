a2339e7fd3c40266313b0b66e2d2840aadf2bd5e
Josh McVey <josh.mcvey@opentrons.com>
2026-05-22 16:36:23 -0500

    fix(app-shell): update ot2 manifest url (#87)

--- app-shell/src/robot-update/constants.ts
+++ app-shell/src/robot-update/constants.ts
@@ -9,12 +9,12 @@ import type { UpdateManifestUrls } from './types'
 const OPENTRONS_PROJECT: string = _OPENTRONS_PROJECT_
 
 const UPDATE_MANIFEST_URLS_RELEASE = {
-  ot2: 'https://builds.opentrons.com/ot2-br/releases.json',
+  ot2: 'https://ot2.builds.opentrons.com/ot2-br/releases.json',
   flex: 'https://builds.opentrons.com/ot3-oe/releases.json',
 }
 
 const UPDATE_MANIFEST_URLS_INTERNAL_RELEASE = {
-  ot2: 'https://ot3-development.builds.opentrons.com/ot2-br/releases.json',
+  ot2: 'https://ot2-development.builds.opentrons.com/ot2-br/releases.json',
   flex: 'https://ot3-development.builds.opentrons.com/ot3-oe/releases.json',
 }
 
