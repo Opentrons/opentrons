56a0c285799219a81b689cb1c87e329a937f652e
CaseyBatten <caseyhbatten@gmail.com>
2026-05-27 16:25:10 -0400

    feat(robot-server): Implement Pyro compatible door watcher callback for Default Run Orchestrator (#21602)
    Allows the operation of a default run even when subprocess mode is enabled, enabling things like drop tip wizard.

--- robot-server/robot_server/service/pyro_utils/pyro_resource.py
+++ robot-server/robot_server/service/pyro_utils/pyro_resource.py
@@ -176,6 +176,19 @@ class RobotServerPyroResource:
                 "Cannot provider a maintenance run door watcher from the RobotServerPyroResource without a MaintenanceRunOrchestratorStore."
             )
 
+    @pyro_behavior(specialty_func=convert_result_to_proxy, apply_local=False)
+    def get_default_run_orchestrator_door_watcher_callback(
+        self,
+    ) -> HardwareEventHandler:
+        """Get a door watcher callback from a non-subprocess Default Run Orchestrator."""
+        orchestrator_store = self._run_orchestrator_store
+        if orchestrator_store is not None:
+            return orchestrator_store.default_run_orchestrator_door_watcher_callback_route_for_proxy
+        else:
+            raise RuntimeError(
+                "Cannot provider a default run orchestrator door watcher from the RobotServerPyroResource without a RunOrchestratorStore."
+            )
+
     async def get_deck_configuration(self) -> DeckConfigurationType:
         """Provide the current recognized DeckConfiguration of the robot server."""
 
