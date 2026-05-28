8396bdf87a4115430e7dd83a10bccf345b080d9d
ncdiehl11 <ncdiehl11@gmail.com>
2026-05-28 13:41:52 -0400

    remove logs

--- step-generation/src/commandCreators/compound/replaceTip.ts
+++ step-generation/src/commandCreators/compound/replaceTip.ts
@@ -90,10 +90,6 @@ export const replaceTip: CommandCreator<ReplaceTipArgs> = (
       primaryNozzle,
       nozzles
     )
-    console.log(nextTiprackResult)
-    console.log(
-      prevRobotState.labware[nextTiprackResult.nextTiprack?.tiprackId ?? '']
-    )
 
     nextTiprack = nextTiprackResult.nextTiprack
     const tipracks = nextTiprackResult.tipracks
