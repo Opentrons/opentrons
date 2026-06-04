2a771dcbc06750976fd21d9278cbc4df0088a12a
alexjoel42 <alexcopperman1@gmail.com>
2026-06-04 11:03:43 -0400

    fix(step-generation): fix return tip logic for high volume transfers in PD (#100)
    RESC-582

--- step-generation/src/commandCreators/compound/transfer.ts
+++ step-generation/src/commandCreators/compound/transfer.ts
@@ -1302,8 +1302,7 @@ export const transfer: CommandCreator<TransferArgs> = (
               break
           }
           const returnTipCommands: CurriedCommandCreator[] =
-            isReturnTip &&
-            (pairIdx === sourceDestPairs.length - 1 || changeTip === 'always')
+            isReturnTip && (isUltimateSubtransfer || changeTip === 'always')
               ? [
                   curryWithoutPython(dropTip, {
                     pipette,
