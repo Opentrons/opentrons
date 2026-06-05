b419820789b117d9337df8e26b849dbba3a46b38
Max Marrone <max@opentrons.com>
2026-06-05 16:49:06 -0400

    Delete Redux state for lights.

--- app/src/redux/robot-controls/constants.ts
+++ app/src/redux/robot-controls/constants.ts
@@ -17,31 +17,12 @@ export const ATTACH_TIP: 'attachTip' = 'attachTip'
 
 // http paths
 
-export const LIGHTS_PATH: '/robot/lights' = '/robot/lights'
 export const HOME_PATH: '/robot/home' = '/robot/home'
 export const POSITIONS_PATH: '/robot/positions' = '/robot/positions'
 export const MOVE_PATH: '/robot/move' = '/robot/move'
 export const DISENGAGE_MOTORS_PATH: '/motors/disengage' = '/motors/disengage'
 
 // action type strings
-
-export const FETCH_LIGHTS: 'robotControls:FETCH_LIGHTS' =
-  'robotControls:FETCH_LIGHTS'
-
-export const FETCH_LIGHTS_SUCCESS: 'robotControls:FETCH_LIGHTS_SUCCESS' =
-  'robotControls:FETCH_LIGHTS_SUCCESS'
-
-export const FETCH_LIGHTS_FAILURE: 'robotControls:FETCH_LIGHTS_FAILURE' =
-  'robotControls:FETCH_LIGHTS_FAILURE'
-
-export const UPDATE_LIGHTS: 'robotControls:UPDATE_LIGHTS' =
-  'robotControls:UPDATE_LIGHTS'
-
-export const UPDATE_LIGHTS_SUCCESS: 'robotControls:UPDATE_LIGHTS_SUCCESS' =
-  'robotControls:UPDATE_LIGHTS_SUCCESS'
-
-export const UPDATE_LIGHTS_FAILURE: 'robotControls:UPDATE_LIGHTS_FAILURE' =
-  'robotControls:UPDATE_LIGHTS_FAILURE'
 
 export const HOME: 'robotControls:HOME' = 'robotControls:HOME'
 
