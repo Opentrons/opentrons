82572ea921c55424774d5a6318a7143cc977df40
TamarZanzouri <zanzouritamar@gmail.com>
2026-06-08 10:39:25 -0400

    linting

--- app/src/resources/auth/hooks/__tests__/useOAuth2PasswordLogin.test.ts
+++ app/src/resources/auth/hooks/__tests__/useOAuth2PasswordLogin.test.ts
@@ -1,13 +1,14 @@
 import { act, renderHook, waitFor } from '@testing-library/react'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 
-import { getSelf, OAUTH2_CLIENT_ID } from '@opentrons/api-client'
+import { OAUTH2_CLIENT_ID } from '@opentrons/api-client'
 
 import { useOAuth2PasswordLogin } from '../useOAuth2PasswordLogin'
 
 import type {
   AuthUser,
   AuthUserResponse,
+  getSelf,
   OAuth2TokenResponse,
   Response,
 } from '@opentrons/api-client'
