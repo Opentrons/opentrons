ebd63c11d0a896a59e58a0a035d925884a5ff5f6
ncdiehl11 <ndiehl@opentrons.com>
2026-06-08 14:36:39 -0400

    feat(app): generalize identifyModule live command for any module
    On the frontend, we should not gate sending the `identifyModule` live command (wrapped in the then
    `useSendIdentifyStacker`, now `useSendIdentifyModule` hook) by module type. The backend shold accept
    this command for any module. Also, here I render the pulsing lights notification in the
    `SelectLocation` module wizard flow screen if the module is a vacuum.a

--- app/src/organisms/ModuleWizardFlows/useModuleSetupWizard.ts
+++ app/src/organisms/ModuleWizardFlows/useModuleSetupWizard.ts
@@ -14,7 +14,7 @@ import {
 import { useCreateTargetedMaintenanceRunMutation } from '/app/resources/runs'
 
 import { ACTIONS } from './constants'
-import { useSendIdentifyStacker } from './hooks'
+import { useSendIdentifyModule } from './hooks'
 import { moduleSetupWizardReducer } from './moduleSetupWizardReducer'
 
 import type { SetStateAction } from 'react'
@@ -68,7 +68,7 @@ export function useModuleSetupWizard(
 ): UseModuleSetupWizardResult {
   const { closeFlow, attachedModuleOnLaunch, onComplete } = params
   const isOnDevice = useSelector(getIsOnDevice)
-  const sendIdentifyStacker = useSendIdentifyStacker()
+  const sendIdentifyModule = useSendIdentifyModule()
   const [state, dispatch] = useReducer(moduleSetupWizardReducer, {
     currentStepIndex: 0,
     currentStep: null,
@@ -142,7 +142,7 @@ export function useModuleSetupWizard(
 
   const handleCleanUpAndClose = (): void => {
     setIsExiting(true)
-    if (attachedModule != null) sendIdentifyStacker(attachedModule, false)
+    if (attachedModule != null) sendIdentifyModule(attachedModule, false)
     if (maintenanceRunId == null) {
       console.log(
         'closing module setup wizard: no maintenance run, not deleting'
