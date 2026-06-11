973200ec0dd08ec4c148ffe7da6bd27acfd82c24
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-06-11 17:16:43 -0400

    idk what I did, whatever

--- include/eeprom/core/book_accessor.hpp
+++ include/eeprom/core/book_accessor.hpp
@@ -8,6 +8,9 @@
 #include <cstring>
 #include <unordered_map>
 
+// #pragma GCC push_options
+// #pragma GCC optimize("O0")
+
 #include "accessor.hpp"
 #include "addresses.hpp"
 #include "common/core/bit_utils.hpp"
@@ -720,3 +723,4 @@ class BookAccessor
 
 }  // namespace book_accessor
 }  // namespace eeprom
+// #pragma GCC pop_options
