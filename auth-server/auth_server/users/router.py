7abe994b096d4e6d9e56c9896f2ff2a1f2f5b4d9
TamarZanzouri <zanzouritamar@gmail.com>
2026-06-11 14:37:46 -0400

    linting

--- auth-server/auth_server/users/router.py
+++ auth-server/auth_server/users/router.py
@@ -250,9 +250,7 @@ async def update_self(
     request_body: RequestModel[UpdateSelf],
     authorization_details: Annotated[
         RequireScopesResult,
-        fastapi.Depends(
-            require_scopes(Scope.USERS_WRITE_SELF_PASSWORD, Scope.USERS_WRITE_SELF)
-        ),
+        fastapi.Depends(require_scopes(Scope.USERS_WRITE_SELF_PASSWORD)),
     ],
     user_data_manager: Annotated[
         UserDataManager, fastapi.Depends(get_user_data_manager)
