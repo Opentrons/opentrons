3abdd858ff9425826ed62ed451e9205d8477320a
ncdiehl11 <ndiehl@opentrons.com>
2026-06-15 12:25:28 -0400

    fix(protocol-designer, step-generation): fix vacuum profile Python emission
    Fixes Python emission for vacuum profile commands. Namely, updates `profile` arg to correct `steps`
    arg and adds explicit `"enable_pump"` entry to all profile steps. Also, fixes a typing bug to
    convert `null` vent after values to `false`

--- step-generation/src/types.ts
+++ step-generation/src/types.ts
@@ -145,7 +145,7 @@ export interface ProfileBlockActivity {
 
 export interface VacuumPumpProfileActivity {
   type: 'profile'
-  profileElements: VacuumRunProfileParams['profile']
+  profileElements: VacuumRunProfileParams['steps']
   taskId: string
   ventAfter: boolean
 }
@@ -892,14 +892,14 @@ export type VacuumPumpArgs =
 export interface VacuumStartRunProfileArgs extends CommonArgs {
   moduleId: string
   commandCreatorFnName: 'vacuumStartRunProfile'
-  profile: VacuumRunProfileParams['profile']
+  profile: VacuumRunProfileParams['steps']
   ventAfter: boolean
 }
 
 export interface VacuumCloseVentStartProfileArgs extends CommonArgs {
   moduleId: string
   commandCreatorFnName: 'vacuumCloseVentStartProfile'
-  profile: VacuumRunProfileParams['profile']
+  profile: VacuumRunProfileParams['steps']
   ventAfter: boolean
 }
 
