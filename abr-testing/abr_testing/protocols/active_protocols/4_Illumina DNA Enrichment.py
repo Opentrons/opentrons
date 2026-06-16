687d1f3bf4d558e0c5ecc9eb7cb0552c28c6d1cf
alexjoel42 <alex.copperman@opentrons.com>
2026-06-16 11:11:57 -0400

    Update 4_Illumina DNA Enrichment.py

--- abr-testing/abr_testing/protocols/active_protocols/4_Illumina DNA Enrichment.py
+++ abr-testing/abr_testing/protocols/active_protocols/4_Illumina DNA Enrichment.py
@@ -22,7 +22,7 @@ from opentrons.hardware_control.modules.types import ThermocyclerStep
 
 
 metadata = {
-    "protocolName": "Illumina DNA Enrichment v4 with TC Auto Sealing Lid",
+    "protocolName": "ABR OFF Illumina DNA Enrichment v4 with TC Auto Sealing Lid",
     "author": "Opentrons <protocols@opentrons.com>",
     "source": "Protocol Library",
 }
