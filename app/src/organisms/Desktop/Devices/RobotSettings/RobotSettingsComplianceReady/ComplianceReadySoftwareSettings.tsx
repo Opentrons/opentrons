458c9b20b7337a57ef91d88fb82bdae253c8b3af
TamarZanzouri <zanzouritamar@gmail.com>
2026-06-17 16:40:44 -0400

    added a toggle input setting

--- app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsComplianceReady/ComplianceReadySoftwareSettings.tsx
+++ app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsComplianceReady/ComplianceReadySoftwareSettings.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 
 import { Divider, StyledText } from '@opentrons/components'
 
+import { ToggleButton } from '/app/atoms/buttons'
 import { Accordion } from '/app/molecules/Accordion'
 import { InputSetting } from '/app/molecules/InputSetting'
 
@@ -20,6 +21,40 @@ export function ComplianceReadySoftwareSettings({
   const { t } = useTranslation('access_control')
   const [loginAttempts, setLoginAttempts] = useState('')
   const [autoLogoutMinutes, setAutoLogoutMinutes] = useState('')
+  const [requirePasswordChange, setRequirePasswordChange] = useState(false)
+  const [passwordChangeDays, setPasswordChangeDays] = useState('')
+
+  const requirePasswordChangeSetting = (
+    <div className={styles.toggle_setting}>
+      <div className={styles.toggle_row}>
+        <StyledText
+          desktopStyle="bodyDefaultRegular"
+          className={styles.toggle_label}
+        >
+          {t('desktop_require_password_change_after_time')}
+        </StyledText>
+        <div className={styles.toggle_button}>
+          <ToggleButton
+            label={t('desktop_require_password_change_after_time')}
+            toggledOn={requirePasswordChange}
+            onClick={() => {
+              setRequirePasswordChange(current => !current)
+            }}
+          />
+        </div>
+      </div>
+      {requirePasswordChange ? (
+        <InputSetting
+          label={t('desktop_length_of_time')}
+          value={passwordChangeDays}
+          units={t('desktop_days')}
+          onChange={event => {
+            setPasswordChangeDays(event.target.value)
+          }}
+        />
+      ) : null}
+    </div>
+  )
 
   const children = (
     <div className={styles.content}>
@@ -28,13 +63,17 @@ export function ComplianceReadySoftwareSettings({
       </StyledText>
       <div className={styles.field_list}>
         <InputSetting
-          label={t('desktop_maximum_login_attempts_before_account_deactivation')}
+          label={t(
+            'desktop_maximum_login_attempts_before_account_deactivation'
+          )}
           value={loginAttempts}
           units={t('desktop_logins')}
           onChange={event => {
             setLoginAttempts(event.target.value)
           }}
         />
+        <Divider />
+        {requirePasswordChangeSetting}
         <Divider />
         <InputSetting
           label={t('desktop_auto_logout_inactivity_length')}
