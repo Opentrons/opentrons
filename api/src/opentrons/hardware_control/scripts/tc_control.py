f38a343f67434e3367cbbfbcb7b853a7769e73e7
caila-marashaj <caila.marashaj@opentrons.com>
2026-06-23 04:54:15 -0400

    so far

--- api/src/opentrons/hardware_control/scripts/tc_control.py
+++ api/src/opentrons/hardware_control/scripts/tc_control.py
@@ -1,98 +1,115 @@
 import asyncio
+import argparse
 import subprocess
+import sys
 from typing import Any
-
 from serial import Serial  # type: ignore[import-untyped]
 
-_READ_ALL = "readall"
-_READ_LINE = "read"
-_DONE = "done"
-_MOVE_SEAL = "ms"
-_MOVE_LID = "ml"
+from opentrons.drivers.vacuum_module.types import GCODE as VM_GCODES
+from opentrons.drivers.thermocycler.types import GCODE as TC_GCODES
+from opentrons.drivers.heater_shaker.types import GCODE as HS_GCODES
+from opentrons.drivers.mag_deck.types import GCODE as MD_GCODES
+from opentrons.drivers.flex_stacker.types import GCODE as FS_GCODES
+from opentrons.drivers.temp_deck.types import GCODE as TD_GCODES
 
-gcode_shortcuts = {
-    "status": "M119",
-    _MOVE_SEAL: "M241.D",  # move seal motor
-    _MOVE_LID: "M240.D",  # move lid stepper motor
-    "ol": "M126",  # open lid
-    "cl": "M127",  # close lid
-    "sw": "M901.D",  # status of all switches
-    "lt": "M141.D",  # get lid temperature
-    "pt": "M105.D",  # get plate temperature
+COMMANDS = {
+    "readall" : lambda dev: dev.readlines().decode(),
+    "read" : lambda dev: dev.readline().decode(),
 }
 
+# the modules as they appear in /dev/ mapped to their respective GCODES
+MODULE_NAMES = {
+    "thermocycler": TC_GCODES,
+    "vacuum": VM_GCODES,
+    "heatershaker": HS_GCODES,
+    "magdeck": MD_GCODES,
+    "flexstacker": FS_GCODES,
+    "tempdeck": TD_GCODES,
+}
 
-async def message_read(dev: Serial) -> Any:
+MODULE_NAME_INDEX = 0
+SERIAL_OBJ_INDEX = 1
+
+# returns format "GET_DEVICE_INFO" : "M115"
+def get_commands_for_module(module_name: str) -> Dict[str, str]: 
+    # take the 'ot_module_' and the number off the ends of the name
+    module_kind = module_name.split('_')[-1][:-1]
+    module_gcodes = MODULE_NAMES[module_kind]
+    return {_cmd.value : _cmd.name for _cmd in list(module_gcodes)}
+
+async def _message_read(dev: Serial) -> Any:
     response = dev.readline().decode()
     while not response:
         await asyncio.sleep(1)
         response = dev.readline().decode()
     return response
 
-
 async def message_return(dev: Serial) -> Any:
     try:
-        response = await asyncio.wait_for(message_read(dev), timeout=20)
-        return response
+        response = await asyncio.wait_for(_message_read(dev), timeout=20)
+        return response 
     except asyncio.exceptions.TimeoutError:
         print("response timed out.")
         return ""
 
+async def comms_loop(modules_dict: Dict[int, Tuple[str, Serial]]) -> None:
+    # get the module to talk to 
+    print("modules:")
+    for index, name_pair in modules_dict.items():
+        # should be vacuum0 / flexstacker1 / etc.
+        name = name_pair[MODULE_NAME_INDEX].split('_')[-1]
+        print(f"\t{index} : {name}")
 
-async def handle_gcode_shortcut(dev: Serial, command: str) -> None:
-    # handle debugging commands that require followup
-    if command == _MOVE_SEAL:
-        distance = input("enter distance in steps => ")
-        dev.write(
-            f"{gcode_shortcuts[command]} {distance}\n".encode()
-        )  # (+) -> retract, (-) -> engage
+    which_mod = input("\n enter module >>> ")
+    if which_mod == "quit": 
+        sys.exit(0) 
+    try:
+        serial_line = modules_dict[int(which_mod)][SERIAL_OBJ_INDEX]
+        module_name = modules_dict[int(which_mod)][MODULE_NAME_INDEX]
+    except KeyError:
+        print(f"Invalid module number.")
+        return
+    
+    module_gcodes = get_commands_for_module(module_name)
+    print("commands:")
+    for gcode, cmd_name in module_gcodes.items():
+        print(f"{cmd_name} : {gcode}")
+   # get user input for gcode command 
+
+
+   # old
+    try:
+        dev.write(f"{command}\n".encode())
         print(await message_return(dev))
-    elif command == _MOVE_LID:
-        distance = input(
-            "enter angular distance in degrees => "
-        )  # (+) -> open, (-) -> close
-        dev.write(f"{gcode_shortcuts[command]} {distance}\n".encode())
-        print(await message_return(dev))
-    # everything else
-    else:
-        dev.write(f"{gcode_shortcuts[command]}\n".encode())
-        print(await message_return(dev))
+    except TypeError:
+        print("Invalid input.")
 
 
-async def comms_loop(dev: Serial) -> bool:
-    _exit = False
-    command = input("\n>>> ")
-    if command == _READ_ALL:
-        print(dev.readlines())
-    elif command == _READ_LINE:
-        print(dev.readline())
-    elif command == _DONE:
-        _exit = True
-    elif command in gcode_shortcuts:
-        await handle_gcode_shortcut(dev, command)
-    else:
-        try:
-            dev.write(f"{command}\n".encode())
-            print(await message_return(dev))
-        except TypeError:
-            print("Invalid input.")
-    return _exit
-
-
-async def _main() -> None:
-    tc_name = (
-        subprocess.check_output(["find", "/dev/", "-name", "*thermocycler*"])
+async def main() -> None:
+    modules_found = (
+        subprocess.run(["find", "/dev/", "-name", "{ot_module}*"])
         .decode()
         .strip()
     )
-    if not tc_name:
-        print("Thermocycler not found. Exiting.")
+    if not modules_found:
+        print("No modules found. Exiting.")
         return
-    dev = Serial(f"{tc_name}", 9600, timeout=2)
-    _exit = False
-    while not _exit:
-        _exit = await comms_loop(dev)
+
+    modules_dict = {}
+    for module_index in range(len(modules_found)):
+        module_name = modules_found[module_index]
+        # {
+        #   0 : (ot_module_vacuum0, <serial_obj>),
+        #   1 : (ot_module_vacuum1, <serial_obj>),
+        #   2 : (ot_module_thermocycler0, <serial_obj>),
+        # }
+        name_serial_obj = module_mame, Serial(f"{module_name}", 9600, timeout=2)
+        modules_dict[module_index] = name_serial_obj
+
+    while True:
+        await comms_loop(modules_dict)
 
 
-if __name__ == "__main__":
-    asyncio.run(_main())
+if __name__ == "__main__":    
+    module_key = MODULE_NAMES[args.module_kind][0]
+    asyncio.run(main(module_key=module_key))
