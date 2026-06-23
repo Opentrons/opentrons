b4a8bad2801a1c6814d5c962393565e55452bf95
Seth Foster <seth@opentrons.com>
2026-06-23 13:02:10 -0400

    feat(shared-data): add audit logging errors
    These are used for a couple different categories of failure in the audit
    logging system

--- shared-data/python/opentrons_shared_data/errors/codes.py
+++ shared-data/python/opentrons_shared_data/errors/codes.py
@@ -43,6 +43,7 @@ class ErrorCodes(Enum):
     INTERNAL_MESSAGE_FORMAT_ERROR = _code_from_dict_entry("1006")
     CANBUS_CONFIGURATION_ERROR = _code_from_dict_entry("1007")
     CANBUS_BUS_ERROR = _code_from_dict_entry("1008")
+    KEY_STORAGE_UNAVAILABLE = _code_from_dict_entry("1009")
     ROBOTICS_CONTROL_ERROR = _code_from_dict_entry("2000")
     MOTION_FAILED = _code_from_dict_entry("2001")
     HOMING_FAILED = _code_from_dict_entry("2002")
@@ -101,6 +102,7 @@ class ErrorCodes(Enum):
     INCORRECT_API_VERSION = _code_from_dict_entry("4011")
     LABWARE_LOCATING_FEATURE_ERROR = _code_from_dict_entry("4012")
     CAMERA_ERROR = _code_from_dict_entry("4013")
+    AUDIT_LOGGING_ERROR = _code_from_dict_entry("4014")
 
     @classmethod
     @lru_cache(25)
