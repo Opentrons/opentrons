73b179a9951d6fba04c0f3b7096fa5930e4f20a0
TamarZanzouri <zanzouritamar@gmail.com>
2026-06-29 16:16:41 -0400

    linting

--- api-client/src/index.ts
+++ api-client/src/index.ts
@@ -1,8 +1,8 @@
 // api client entry point
 export * from './audit'
-export * from './accessControl/settings/getRobotServerAccessControlSettings'
-export * from './accessControl/settings/patchRobotServerAccessControlSettings'
-export * from './accessControl/settings/types'
+export * from './robotServer/settings/getRobotServerAccessControlSettings'
+export * from './robotServer/settings/patchRobotServerAccessControlSettings'
+export * from './robotServer/settings/types'
 export * from './auth'
 export * from './calibration'
 export * from './client_data'
