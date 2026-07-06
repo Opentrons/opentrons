50641aceb01b31dea4a83888b69e714e45e15cb9
ncdiehl11 <ndiehl@opentrons.com>
2026-07-06 14:21:08 -0400

    conditionally render compliance ready software files dependent on access control enablement

--- app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsFileManager/__tests__/RobotSettingsFileManager.test.tsx
+++ app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsFileManager/__tests__/RobotSettingsFileManager.test.tsx
@@ -1,14 +1,19 @@
 import { screen } from '@testing-library/react'
-import { beforeEach, describe, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+
+import { useAccessControlEnabledQuery } from '@opentrons/react-api-client'
 
 import { renderWithProviders } from '/app/__testing-utils__'
 
+import { ComplianceReadySoftwareFiles } from '../ComplianceReadySoftwareFiles'
 import { RobotSettingsFileManager } from '../index'
 import { ProtocolRunRecords } from '../ProtocolRunRecords'
 import { RobotStorage } from '../RobotStorage'
 
+vi.mock('@opentrons/react-api-client')
 vi.mock('../RobotStorage')
 vi.mock('../ProtocolRunRecords')
+vi.mock('../ComplianceReadySoftwareFiles')
 
 const render = () => renderWithProviders(<RobotSettingsFileManager />)
 
@@ -19,11 +24,32 @@ describe('RobotSettingsFileManager', () => {
     vi.mocked(ProtocolRunRecords).mockReturnValue(
       <div>mock protocol run records</div>
     )
+    vi.mocked(ComplianceReadySoftwareFiles).mockReturnValue(
+      <div>mock compliance ready software files</div>
+    )
+    vi.mocked(useAccessControlEnabledQuery).mockReturnValue({
+      data: { data: { accessControlEnabled: true } },
+    } as any)
   })
 
   it('renders robot storage', () => {
     render()
     screen.getByText('mock robot storage')
+  })
+
+  it('renders compliance ready software files if access control is enabled', () => {
+    render()
+    screen.getByText('mock compliance ready software files')
+  })
+
+  it(' renders compliance ready software files if access control is not enabled', () => {
+    vi.mocked(useAccessControlEnabledQuery).mockReturnValue({
+      data: { data: { accessControlEnabled: false } },
+    } as any)
+    render()
+    expect(
+      screen.queryByText('mock compliance ready software files')
+    ).not.toBeInTheDocument()
   })
 
   it('renders protocol run records', () => {
--- app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsFileManager/index.tsx
+++ app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsFileManager/index.tsx
@@ -1,13 +1,19 @@
+import { useAccessControlEnabledQuery } from '@opentrons/react-api-client'
+
 import { ComplianceReadySoftwareFiles } from './ComplianceReadySoftwareFiles'
 import { ProtocolRunRecords } from './ProtocolRunRecords'
 import styles from './robotsettingsfilemanager.module.css'
 import { RobotStorage } from './RobotStorage'
 
 export function RobotSettingsFileManager(): JSX.Element {
+  const { data: accessControlData } = useAccessControlEnabledQuery()
+  const isComplianceReady =
+    accessControlData?.data?.accessControlEnabled ?? false
+
   return (
     <div className={styles.container}>
       <RobotStorage />
-      <ComplianceReadySoftwareFiles />
+      {isComplianceReady ? <ComplianceReadySoftwareFiles /> : null}
       <ProtocolRunRecords />
     </div>
   )
