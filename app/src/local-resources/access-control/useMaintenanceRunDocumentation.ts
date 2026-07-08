e0921671c3ac1c04dc0dda9e2f6cfc15cd383928
Janie Jaffe <35556896+janiejaffe@users.noreply.github.com>
2026-07-08 17:23:55 -0400

    fix lpc doc popup

--- app/src/local-resources/access-control/useMaintenanceRunDocumentation.ts
+++ app/src/local-resources/access-control/useMaintenanceRunDocumentation.ts
@@ -16,6 +16,7 @@ import type {
  * @param maintenanceRunName: The name of the maintenance run, to be displayed in the initial documentation modal.
  * @param onCancelStart: Optional callback when the user backs out of the initial documentation modal.
  * @param initialDocstate: An optional documentation state - to be used in flows like attaching where prompting needs to occur before the first mutation.
+ * @param promptEnabled: When false, defer the command documentation prompt until set to true. Defaults to true.
  *
  * @returns commandDocState: DocumentationState to be passed to the creation hook and the maintenance run commands.
  * @returns deletionDocState: DocumentationState to be passed to the deletion hook.
@@ -25,7 +26,8 @@ import type {
 export const useMaintenanceRunDocumentation = (
   maintenanceRunName: DocumentedAction,
   onCancelStart?: () => void,
-  initialDocstate?: DocumentationState
+  initialDocstate?: DocumentationState,
+  promptEnabled: boolean = true
 ): {
   commandDocState: DocumentationState
   deletionDocState: DocumentationState
@@ -39,7 +41,8 @@ export const useMaintenanceRunDocumentation = (
   const commandDocState = usePromptForDocumentation(
     [maintenanceRunName],
     onCancelStart,
-    initialDocstate
+    initialDocstate,
+    promptEnabled
   )
   const deletionDocState = useDocumentationState()
 
