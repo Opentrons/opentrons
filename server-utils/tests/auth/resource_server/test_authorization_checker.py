5bd9d7b0e659006482859e77f3580d32382e5d72
Seth Foster <seth@opentrons.com>
2026-07-08 09:48:20 -0400

    feat: flip logs to audit s-u tree
    The reason settings now live in audit server and of course the actual
    audit logging lives in audit server. While we're not ready to actually
    send logs to audit server - we need some more work to redefine the
    interface for how the robot server interacts with it - we can remove the
    code that was checking the wrong invariants and formatting things the
    wrong way and sending messages to the wrong place and replacing it with
    something that still does half that stuff but will put it in the right
    place eventually.

--- server-utils/tests/auth/resource_server/test_authorization_checker.py
+++ server-utils/tests/auth/resource_server/test_authorization_checker.py
@@ -5,8 +5,6 @@ from server_utils.auth.resource_server.auth_server import (
     AuthSettingsResponse,
     AuthSettingsResponseData,
     Client,
-    RequireReasonForInteractionSettingsResponse,
-    RequireReasonForInteractionSettingsResponseData,
     TokenIntrospectionResponse,
 )
 from server_utils.auth.resource_server.authorization_checker import (
@@ -28,12 +26,6 @@ def mock_client(decoy: Decoy) -> Client:
 
 
 class TestAlwaysAllowedAuthorizationChecker:
-    async def test_get_require_reason_for_interaction_settings(self) -> None:
-        subject = AlwaysAllowedAuthorizationChecker()
-        settings = await subject.get_require_reason_for_interaction_settings()
-        assert settings.data.requireReasonForInteraction is False
-        assert await subject.is_reason_for_interaction_required() is False
-
     async def test_check(self) -> None:
         subject = AlwaysAllowedAuthorizationChecker()
         assert (
@@ -53,46 +45,6 @@ class TestAuthServerAuthorizationChecker:
     def mock_client(self, decoy: Decoy) -> Client:
         """Return a mock in the shape of a client."""
         return decoy.mock(cls=Client)
-
-    async def test_get_require_reason_for_interaction_settings(
-        self, mock_client: Client, decoy: Decoy
-    ) -> None:
-        subject = AuthServerAuthorizationChecker(mock_client)
-        expected = RequireReasonForInteractionSettingsResponse(
-            data=RequireReasonForInteractionSettingsResponseData(
-                requireReasonForInteraction=True
-            )
-        )
-        decoy.when(
-            await mock_client.get_require_reason_for_interaction_settings()
-        ).then_return(expected)
-        decoy.when(await mock_client.get_auth_settings()).then_return(
-            AuthSettingsResponse(
-                data=AuthSettingsResponseData(accessControlEnabled=True)
-            )
-        )
-        assert await subject.get_require_reason_for_interaction_settings() == expected
-        assert await subject.is_reason_for_interaction_required() is True
-
-    async def test_get_require_reason_disabled_when_access_control_off(
-        self, mock_client: Client, decoy: Decoy
-    ) -> None:
-        subject = AuthServerAuthorizationChecker(mock_client)
-        decoy.when(
-            await mock_client.get_require_reason_for_interaction_settings()
-        ).then_return(
-            RequireReasonForInteractionSettingsResponse(
-                data=RequireReasonForInteractionSettingsResponseData(
-                    requireReasonForInteraction=True
-                )
-            )
-        )
-        decoy.when(await mock_client.get_auth_settings()).then_return(
-            AuthSettingsResponse(
-                data=AuthSettingsResponseData(accessControlEnabled=False)
-            )
-        )
-        assert await subject.is_reason_for_interaction_required() is False
 
     async def test_check_given_no_token(
         self, mock_client: Client, decoy: Decoy
