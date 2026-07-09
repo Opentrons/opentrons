0d0dda79e98193773aca8c6dac67927813e7a4bf
Seth Foster <seth@opentrons.com>
2026-07-09 10:54:24 -0400

    feat(react-api-client): add audit settings
    The audit server now hosts the reason for interaction required and
    minimum length settings; add data bindings.

--- api-client/src/audit/index.ts
+++ api-client/src/audit/index.ts
@@ -1,4 +1,5 @@
 export { getLogPeriodSummaries } from './getLogPeriodSummaries'
 export { getLogPeriodRaw } from './getLogPeriodRaw'
 export { deleteLogPeriod } from './deleteLogPeriod'
+export * from './settings'
 export * from './types'
