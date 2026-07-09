03645c83eb44cfdce88d94cb5cf02ffa5b63b083
koji <baxin1919@gmail.com>
2026-07-08 20:04:45 -0400

    fix lint errors

--- app/src/organisms/ODD/ProtocolSetup/ProtocolSetupLabware/index.tsx
+++ app/src/organisms/ODD/ProtocolSetup/ProtocolSetupLabware/index.tsx
@@ -124,7 +124,10 @@ export function ProtocolSetupLabware({
   const moduleQuery = useModulesQuery({
     refetchInterval: MODULE_REFETCH_INTERVAL_MS,
   })
-  const attachedModules = moduleQuery?.data?.data ?? []
+  const attachedModules = useMemo(
+    () => moduleQuery?.data?.data ?? [],
+    [moduleQuery?.data?.data]
+  )
   const protocolModulesInfo = useMemo(
     () =>
       mostRecentAnalysis != null
