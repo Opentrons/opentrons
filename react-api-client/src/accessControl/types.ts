fb229521ae476b3c06c421aa35c7af8b8a17dd9f
Janie Jaffe <35556896+janiejaffe@users.noreply.github.com>
2026-07-16 15:49:55 -0400

    document update deck config

--- react-api-client/src/accessControl/types.ts
+++ react-api-client/src/accessControl/types.ts
@@ -153,6 +153,7 @@ type AuditLogAction =
   | 'shutdown_robot'
   | 'update_robot_name'
   | 'pause_run'
+  | 'update_deck_configuration'
 
 /**
  * Type used for DocumentedActions - keys and info to enable correct rendering of actions in the 'list actions' popup in the Documentation Required Modal.
