d25f49676146077ffc59d98fbc906b7ebf874043
Janie Jaffe <35556896+janiejaffe@users.noreply.github.com>
2026-07-17 11:27:38 -0400

    feat(app,odd): document update deck configuration (#21964)
    # Overview
    
    
    https://github.com/user-attachments/assets/6dc18bc3-3ea0-49b3-ab9f-fd2f43995a91
    
    ## Test Plan and Hands on Testing
    
    1. Updating the deck configuration in the desktop app or on the ODD
    should prompt for documentation
    2. During module setup, updating the deck configuration should not
    prompt for documentation, but should appear in the list of actions to
    document at the end of the flow.
    
    ## Risk assessment
    
    Low

--- react-api-client/src/accessControl/types.ts
+++ react-api-client/src/accessControl/types.ts
@@ -157,6 +157,7 @@ type AuditLogAction =
   | 'update_camera'
   | 'update_camera_settings_for_run'
   | 'pause_run'
+  | 'update_deck_configuration'
 
 /**
  * Type used for DocumentedActions - keys and info to enable correct rendering of actions in the 'list actions' popup in the Documentation Required Modal.
