8b99c27f4f71fc3a6377746e39ac7b6b877c73bf
CaseyBatten <caseyhbatten@gmail.com>
2026-07-29 17:26:50 -0400

    remove proxy conversion weakref overload
    This was causing a failure when attempting to generate a list of proxies for the ot3api, `r.__weakref__()` is not callable for proxy modules now that we aren't going through the thread manager directly. This is generally "a bad approach" and we shouldn't do it.

--- api/src/opentrons/util/pyro/pyro_synchronous_adapter.py
+++ api/src/opentrons/util/pyro/pyro_synchronous_adapter.py
@@ -630,11 +630,6 @@ def convert_result_to_proxy(  # noqa: C901
         try:
             proxy_list = []
             for r in result:
-                if hasattr(r, "__weakref__"):
-                    # If the resulting object contains a weakref then utilize the inner instance
-                    # Example - CallBridger wrapping an AbstractModule
-                    r = r.__weakref__()
-
                 pyro_synchronous_obj = utility.find_PSO(r)
                 if pyro_synchronous_obj is None:
                     if not hasattr(r, "_loop"):
