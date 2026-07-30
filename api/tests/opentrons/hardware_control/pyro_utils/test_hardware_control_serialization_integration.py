dc20e8c9e8e43721b97486208d498036b05503f3
CaseyBatten <caseyhbatten@gmail.com>
2026-07-30 17:18:09 -0400

    update the integration test to have a full suite of mock params

--- api/tests/opentrons/hardware_control/pyro_utils/test_hardware_control_serialization_integration.py
+++ api/tests/opentrons/hardware_control/pyro_utils/test_hardware_control_serialization_integration.py
@@ -5,37 +5,39 @@ import inspect
 import socket
 import threading
 from dataclasses import is_dataclass
-from typing import Any, get_args, get_type_hints, Union, Dict
-from pathlib import Path
-
 from datetime import datetime
-
-import opentrons.hardware_control.types as hw_types
-import opentrons.hardware_control.dev_types as dev_types
-import opentrons.hardware_control.modules.mod_abc as mod_abc
-import opentrons.hardware_control.modules.types as module_types
-import opentrons.hardware_control.peripherals.types as peripheral_types
-import opentrons_shared_data.pipette.types as pipette_types
-import opentrons.types as ot_types
-import opentrons.config.types as config_types
-from opentrons_shared_data.pipette import (
-    load_data as load_pipette_data,
-)
-from opentrons.hardware_control.ot3_calibration import OT3Transforms
-from opentrons.hardware_control.robot_calibration import DeckCalibration
-import opentrons.calibration_storage.types as calibration_types
-from opentrons.config import gripper_config as gc
-import opentrons.drivers.types as driver_types
-import opentrons.drivers.vacuum_module.types as vac_types
+from pathlib import Path
+from typing import Any, Dict, Union, get_args, get_type_hints
 
 import pytest
 from decoy import Decoy
 from Pyro5 import api as pyro
 from Pyro5 import nameserver
 
+import opentrons_shared_data.pipette.types as pipette_types
+from opentrons_shared_data.errors.exceptions import VacuumModuleWasteFullError
+from opentrons_shared_data.gripper import GripperModel
+from opentrons_shared_data.pipette import (
+    load_data as load_pipette_data,
+)
 from opentrons_shared_data.robot.types import RobotTypeEnum
 
+import opentrons.calibration_storage.types as calibration_types
+import opentrons.config.types as config_types
+import opentrons.drivers.flex_stacker.types as stacker_types
+import opentrons.drivers.rpi_drivers.types as rpi_types
+import opentrons.drivers.types as driver_types
+import opentrons.drivers.vacuum_module.types as vac_types
+import opentrons.hardware_control.dev_types as dev_types
+import opentrons.hardware_control.instruments.ot3.instrument_calibration as instr_calibration_types
+import opentrons.hardware_control.modules.mod_abc as mod_abc
+import opentrons.hardware_control.modules.module_calibration as mod_cal
+import opentrons.hardware_control.modules.types as module_types
+import opentrons.hardware_control.peripherals.types as peripheral_types
+import opentrons.hardware_control.types as hw_types
+import opentrons.types as ot_types
 from opentrons.config import feature_flags
+from opentrons.config import gripper_config as gc
 from opentrons.config.types import RobotConfig
 from opentrons.drivers.heater_shaker.simulator import SimulatingDriver
 from opentrons.drivers.thermocycler.simulator import (
@@ -59,17 +61,18 @@ from opentrons.hardware_control.modules.types import (
     MagneticBlockModel,
     SimulatingModule,
 )
+from opentrons.hardware_control.ot3_calibration import OT3Transforms
 from opentrons.hardware_control.ot3api import OT3API
 from opentrons.hardware_control.poller import Poller
 from opentrons.hardware_control.pyro_utils.serpent_type_registry import (
     HARDWARE_CLASS_PACKAGES,
     register_hardware_types,
 )
+from opentrons.hardware_control.robot_calibration import DeckCalibration
 from opentrons.hardware_control.types import DoorState
 from opentrons.util.pyro.pyro_client_async_adapter import AsyncClientPyroObject
 from opentrons.util.pyro.pyro_daemon_utility import create_pyro_daemon
 from opentrons.util.pyro.pyro_serialization import find_opentrons_classes_in_packages
-from opentrons_shared_data.gripper import GripperModel
 
 TEST_PYRO_TIMEOUT = 5
 
@@ -489,6 +492,7 @@ async def test_module_registration_coverage(
 
     assert set(class_cover_list) == set(hardware_registry_class_list)
 
+
 CLASS_TYPE_MOCK_TABLE: Dict[type, Any] = {
     hw_types.Axis: hw_types.Axis.X,
     hw_types.OT3Mount: hw_types.OT3Mount.LEFT,
@@ -496,7 +500,9 @@ CLASS_TYPE_MOCK_TABLE: Dict[type, Any] = {
     hw_types.CriticalPoint: hw_types.CriticalPoint.MOUNT,
     ot_types.Point: ot_types.Point(1, 2, 3),
     hw_types.InstrumentProbeType: hw_types.InstrumentProbeType.PRIMARY,
-    dev_types.AttachedGripper: dev_types.AttachedGripper(config=gc.load(GripperModel.v1), id="test"),
+    dev_types.AttachedGripper: dev_types.AttachedGripper(
+        config=gc.load(GripperModel.v1), id="test"
+    ),
     dev_types.AttachedPipette: dev_types.AttachedPipette(
         config=load_pipette_data.load_definition(
             pipette_types.PipetteModelType("p1000"),
@@ -511,15 +517,15 @@ CLASS_TYPE_MOCK_TABLE: Dict[type, Any] = {
     hw_types.TipScrapeType: hw_types.TipScrapeType.LEFT_ONE_COL,
     hw_types.TipStateType: hw_types.TipStateType.ABSENT,
     Union[
-            module_types.MagneticModuleModel,
-            module_types.TemperatureModuleModel,
-            module_types.ThermocyclerModuleModel,
-            module_types.HeaterShakerModuleModel,
-            module_types.MagneticBlockModel,
-            module_types.AbsorbanceReaderModel,
-            module_types.FlexStackerModuleModel,
-            module_types.VacuumModuleModel,
-        ]: module_types.ThermocyclerModuleModel.THERMOCYCLER_V2,
+        module_types.MagneticModuleModel,
+        module_types.TemperatureModuleModel,
+        module_types.ThermocyclerModuleModel,
+        module_types.HeaterShakerModuleModel,
+        module_types.MagneticBlockModel,
+        module_types.AbsorbanceReaderModel,
+        module_types.FlexStackerModuleModel,
+        module_types.VacuumModuleModel,
+    ]: module_types.ThermocyclerModuleModel.THERMOCYCLER_V2,
     peripheral_types.BarcodeScannerModel: peripheral_types.BarcodeScannerModel.BARCODE_SCANNER_V1,
     hw_types.GripperProbe: hw_types.GripperProbe.FRONT,
     hw_types.InstrumentProbeType: None,
@@ -529,27 +535,27 @@ CLASS_TYPE_MOCK_TABLE: Dict[type, Any] = {
         prep_distance_mm=1.0,
         max_overrun_distance_mm=2.0,
         speed_mm_per_s=3.0,
-        sensor_threshold_pf=5.0
+        sensor_threshold_pf=5.0,
     ),
     hw_types.CriticalPoint: hw_types.CriticalPoint.MOUNT,
     OT3Transforms: OT3Transforms(
         deck_calibration=DeckCalibration(
-            attitude= [[0.0, 1.0]],
-            source= calibration_types.SourceType.default,
-            status= calibration_types.CalibrationStatus(
+            attitude=[[0.0, 1.0]],
+            source=calibration_types.SourceType.default,
+            status=calibration_types.CalibrationStatus(
                 markedBad=False,
                 source=calibration_types.SourceType.default,
                 markedAt=datetime.now(),
             ),
-            belt_attitude = None,
-            last_modified = datetime.now(),
-            pipette_calibrated_with = None,
-            tiprack = None,
+            belt_attitude=None,
+            last_modified=datetime.now(),
+            pipette_calibrated_with=None,
+            tiprack=None,
         ),
-        carriage_offset=ot_types.Point(1,1,1),
-        left_mount_offset=ot_types.Point(1,1,1),
-        right_mount_offset=ot_types.Point(1,1,1),
-        gripper_mount_offset=ot_types.Point(1,1,1),
+        carriage_offset=ot_types.Point(1, 1, 1),
+        left_mount_offset=ot_types.Point(1, 1, 1),
+        right_mount_offset=ot_types.Point(1, 1, 1),
+        gripper_mount_offset=ot_types.Point(1, 1, 1),
     ),
     config_types.LiquidProbeSettings: config_types.LiquidProbeSettings(
         mount_speed=1.0,
@@ -563,31 +569,231 @@ CLASS_TYPE_MOCK_TABLE: Dict[type, Any] = {
         sample_time_sec=7.0,
     ),
     hw_types.PipetteSensorId: hw_types.PipetteSensorId.S1,
-    hw_types.PipetteSensorData: hw_types.PipetteSensorData(sensor_type=hw_types.PipetteSensorType.capacitive, _as_int=0x01, _as_float=0x01),
+    hw_types.PipetteSensorData: hw_types.PipetteSensorData(
+        sensor_type=hw_types.PipetteSensorType.capacitive, _as_int=0x01, _as_float=0x01
+    ),
     hw_types.MotionChecks: hw_types.MotionChecks.HIGH,
-    driver_types.ABSMeasurementConfig: driver_types.ABSMeasurementConfig(measure_mode=driver_types.ABSMeasurementMode.SINGLE, sample_wavelengths=[100, 200], reference_wavelength=100),
-    module_types.BundledFirmware: module_types.BundledFirmware(version="v1", path=Path("coolpath")),
-    vac_types.PumpState: vac_types.PumpState(target_rpm=0.1, current_rpm=0.2, target_pwm=1.5, current_pwm=2.5, pump_running=False, manual_control=True),
-    hw_types.ModuleConnectedNotification: hw_types.ModuleConnectedNotification(module_serial="123", name="coolmod", port="456", event=hw_types.HardwareEventType.MODULE_CONNECTED)
+    driver_types.ABSMeasurementConfig: driver_types.ABSMeasurementConfig(
+        measure_mode=driver_types.ABSMeasurementMode.SINGLE,
+        sample_wavelengths=[100, 200],
+        reference_wavelength=100,
+    ),
+    module_types.BundledFirmware: module_types.BundledFirmware(
+        version="v1", path=Path("coolpath")
+    ),
+    vac_types.PumpState: vac_types.PumpState(
+        target_rpm=0.1,
+        current_rpm=0.2,
+        target_pwm=1.5,
+        current_pwm=2.5,
+        pump_running=False,
+        manual_control=True,
+    ),
+    rpi_types.USBPort: rpi_types.USBPort(
+        name="USB",
+        port_number=10,
+        port_group=rpi_types.PortGroup.MAIN,
+        hub=True,
+        hub_port=11,
+        device_path="cooldevpath",
+    ),
+    hw_types.HepaUVState: hw_types.HepaUVState(
+        light_on=True, uv_duration_s=100, remaining_time_s=60
+    ),
+    hw_types.HepaFanState: hw_types.HepaFanState(fan_on=True, duty_cycle=10),
+    hw_types.EstopStateNotification: hw_types.EstopStateNotification(
+        event=hw_types.HardwareEventType.ESTOP_CHANGE,
+        old_state=hw_types.EstopState.DISENGAGED,
+        new_state=hw_types.EstopState.PHYSICALLY_ENGAGED,
+    ),
+    hw_types.DoorStateNotification: hw_types.DoorStateNotification(
+        event=hw_types.HardwareEventType.DOOR_SWITCH_CHANGE,
+        new_state=DoorState.CLOSED,
+        module_serial=None,
+    ),
+    hw_types.ModuleConnectedNotification: hw_types.ModuleConnectedNotification(
+        module_serial="123",
+        name="coolmod",
+        port="456",
+        event=hw_types.HardwareEventType.MODULE_CONNECTED,
+    ),
+    hw_types.ModuleDisconnectedNotification: hw_types.ModuleDisconnectedNotification(
+        event=hw_types.HardwareEventType.MODULE_DISCONNECTED,
+        module_model=module_types.ThermocyclerModuleModel,
+        port="123",
+        module_serial="ABCDF4",
+    ),
+    hw_types.SubsystemConnectionNotification: hw_types.SubsystemConnectionNotification(
+        event=hw_types.HardwareEventType.SUBSYSTEM_CONNECTION
+    ),
+    hw_types.AsynchronousModuleErrorNotification: hw_types.AsynchronousModuleErrorNotification(
+        exception=VacuumModuleWasteFullError(
+            serial="1234ABC",
+            mode="coolmode",
+            target=10.1,
+            current=11.3,
+            message="coolmsg",
+            detail=None,  # CASEY TODO
+            wrapping=None,  # CASEY TODO
+        ),
+        module_serial="1234ABC",
+        module_model=module_types.VacuumModuleModel,
+        port="1",
+        event=hw_types.HardwareEventType.ASYNCHRONOUS_MODULE_ERROR,
+    ),
+    hw_types.ErrorMessageNotification: hw_types.ErrorMessageNotification(
+        event=hw_types.HardwareEventType.ERROR_MESSAGE, message="AGGA"
+    ),
+    hw_types.StatusBarUpdateEvent: hw_types.StatusBarUpdateEvent(
+        state=hw_types.StatusBarState.SOFTWARE_ERROR, enabled=True
+    ),
+    hw_types.EstopOverallStatus: hw_types.EstopOverallStatus(
+        state=hw_types.EstopState.DISENGAGED,
+        left_physical_state=hw_types.EstopPhysicalStatus.ENGAGED,
+        right_physical_state=hw_types.EstopPhysicalStatus.DISENGAGED,
+    ),
+    instr_calibration_types.GripperCalibrationOffset: instr_calibration_types.GripperCalibrationOffset(
+        status=instr_calibration_types.CalibrationStatus(
+            markedBad=False,
+            source=instr_calibration_types.SourceType.factory,
+            markedAt=datetime.now(),
+        ),
+        offset=ot_types.Point(0.1, 0.2, 0.3),
+        source=instr_calibration_types.SourceType.calibration_check,
+        last_modified=datetime.now(),
+    ),
+    instr_calibration_types.PipetteOffsetSummary: instr_calibration_types.PipetteOffsetSummary(
+        offset=ot_types.Point(0.1, 0.2, 0.3),
+        source=instr_calibration_types.SourceType.calibration_check,
+        status=instr_calibration_types.CalibrationStatus(
+            markedBad=False,
+            source=instr_calibration_types.SourceType.factory,
+            markedAt=datetime.now(),
+        ),
+        last_modified=datetime.now(),
+        reasonability_check_failures=[
+            instr_calibration_types.ReasonabilityCheckFailure(
+                kind="inconsistent-pipette-offset",
+                offsets={ot_types.Mount.RIGHT: ot_types.Point(0.1, 0.2, 0.3)},
+                limit=5.1,
+            )
+        ],
+    ),
+    mod_cal.ModuleCalibrationOffset: mod_cal.ModuleCalibrationOffset(
+        offset=ot_types.Point(0.1, 0.2, 0.3),
+        module_id="id",
+        module=module_types.ThermocyclerModuleModel,
+        source=mod_cal.SourceType.factory,
+        status=mod_cal.CalibrationStatus(
+            markedBad=False,
+            source=mod_cal.SourceType.calibration_check,
+            markedAt=datetime.now(),
+        ),
+        mount=hw_types.OT3Mount.LEFT,
+        instrument_id="123",
+        last_modified=datetime.now(),
+        slot="D1",
+    ),
+    vac_types.VacuumState: vac_types.VacuumState(
+        target_gauge_pressure=1.1,
+        current_gauge_pressure=2.2,
+        pressure_abs_a=3.3,
+        pressure_abs_b=4.4,
+        pressure_atm=5.5,
+        vacuum_enabled=True,
+        vacuum_duration=100,
+        vent_state=vac_types.VentState.OPENED,
+    ),
+    stacker_types.TOFMeasurementResult: stacker_types.TOFMeasurementResult(
+        sensor=stacker_types.TOFSensor.X,
+        kind=stacker_types.MeasurementKind.HISTOGRAM,
+        bins={1: [1.0, 2.1]},
+    ),
+    hw_types.HardwareFeatureFlags: hw_types.HardwareFeatureFlags(False, True, True, True)
 }
 
- # ERROR COLLECTION LISTS
+# ERROR COLLECTION LISTS
 
 
-
-def test_serialization_validation() -> None:
+async def test_serialization_validation() -> None:
     """Test to check if types registered in the hardware class package properly serialize and deserialize."""
 
+    class DataTester:
+        """Class for testing data serializaiton over pyro"""
+
+        @pyro.expose
+        def data_in_data_out(self, value: Any) -> Any:
+            return value
+
+    tester = DataTester()
+
+    name_server_ready = threading.Event()
+    await _setup_namerserver(name_server_ready=name_server_ready)
+
+    def _ot3api_pyro_daemon() -> None:
+        # Wait for the nameserver to be ready so locate_ns can succeed.
+        name_server_ready.wait(timeout=TEST_PYRO_TIMEOUT)
+        register_hardware_types()
+        with pyro.Daemon() as daemon:  # type: ignore
+            # Create a guaranteed synchronous adapted alias to the resource
+            daemon.register(tester)
+            try:
+                with pyro.locate_ns() as ns:
+                    # Register our objects URI with the system nameserver
+                    try:
+                        ns.register("TEST_PROXY", daemon.uriFor(tester))
+
+                        # Maintain a request loop to handle requests on our resource instance from remote processes
+                        daemon.requestLoop()
+                    finally:
+                        ns.remove(name="TEST_PROXY")
+            except Exception as e:
+                raise ValueError("Test Nameserver not found.")
+            finally:
+                daemon.close()
+
+    ot3api_thread = threading.Thread(target=_ot3api_pyro_daemon, daemon=True)
+    ot3api_thread.start()
+
+    ns = pyro.locate_ns()
+    retries_counter = 0
+    uri = None
+    while retries_counter <= 10:
+        # Wait and try again, the resource isnt registered yet
+        try:
+            uri = ns.lookup("TEST_PROXY")
+            break
+        except Exception:
+            await asyncio.sleep(0.01)
+            retries_counter += 1
+
+    # Stop waiting for the nameserver, will fail on pyro.resolve (something is wrong with nameserver and/or daemon)
+    if uri is None:
+        raise TimeoutError("TEST FAILURE ON PYRO NAMESERVER IN SERIALIZER TEST SETUP.")
+
+    uri = pyro.resolve(uri="PYRONAME:TEST_PROXY")
+    tester_proxy = pyro.Proxy(uri)  # type: ignore
+
+
+    # Get the hardware registry
     hardware_registry_class_list = find_opentrons_classes_in_packages(
-            HARDWARE_CLASS_PACKAGES
-        )
+        HARDWARE_CLASS_PACKAGES
+    )
     hardware_registry_class_list = list(set(hardware_registry_class_list))
 
+    # Validate each class can be sent over pyro and come back deserialized in the expected format
     for clazz in hardware_registry_class_list:
         try:
-            serializable_dict = clazz.to_pyro_dict(CLASS_TYPE_MOCK_TABLE[clazz])
+            mock_data = CLASS_TYPE_MOCK_TABLE[clazz]
         except KeyError as e:
             raise KeyError(f"{e} - Mock data missing for type {clazz}")
 
-        deserialized_output = clazz.from_pyro_dict("dummy_classname", serializable_dict)
-        assert deserialized_output == CLASS_TYPE_MOCK_TABLE[clazz]
+        try:
+            deserialized_output = tester_proxy.data_in_data_out(mock_data)
+        except Exception as e:
+            print(f"Failed serialization on: {mock_data} - {e}")
+        try:
+            assert deserialized_output == mock_data
+        except:
+            print("failed assert")
+    raise ValueError("done")
