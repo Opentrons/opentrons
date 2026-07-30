0db4d5b4d88cacfe5f4c78bebd095ceaa304c334
Seth Foster <seth@opentrons.com>
2026-07-30 10:54:25 -0400

    after all, why not have an openapi

--- update-server/otupdate/openembedded/__init__.py
+++ update-server/otupdate/openembedded/__init__.py
@@ -4,6 +4,8 @@ import logging
 from typing import Any, Mapping, Optional
 
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_redoc_html
+from fastapi.responses import HTMLResponse
 from starlette.types import ASGIApp, Receive, Scope, Send
 
 from server_utils.auth.resource_server.authentication_checker import (
@@ -15,6 +17,7 @@ from server_utils.auth.resource_server.fastapi import (
     install_authentication_checker,
 )
 
+from otupdate._version import version as package_version
 from otupdate.common import (
     config,
     constants,
@@ -33,6 +36,8 @@ from otupdate.openembedded.update_actions import (
 )
 
 OE_BUILTIN_VERSION_FILE = "/etc/VERSION.json"
+
+_REDOC_CDN_URL = "https://cdn.jsdelivr.net/npm/redoc@2/bundles/redoc.standalone.js"
 
 
 LOG = logging.getLogger(__name__)
@@ -82,10 +87,10 @@ async def get_app(
             "The HTTP API that updates the robot's software and controls its"
             " power state, name, and authorized ssh keys."
         ),
-        # This server predates our use of OpenAPI, and its clients don't read a
-        # spec, so don't serve one or the docs pages that go with it.
-        openapi_url=None,
+        version=package_version,
+        openapi_url="/server/openapi.json",
         docs_url=None,
+        # redoc_url is replaced by our own /redoc router, below.
         redoc_url=None,
     )
 
@@ -109,6 +114,24 @@ async def get_app(
     app.include_router(update.router)
     app.include_router(ssh_key_management.router)
     app.include_router(name_management.router)
+
+    # This is a workaround for a broken /redoc page in versions of FastAPI <0.115.3.
+    # The page loads Redoc from a CDN, and the problem is that the default CDN URL
+    # uses a fragile version tag.
+    # https://github.com/Redocly/redoc/issues/2743
+    # https://github.com/fastapi/fastapi/pull/9700
+    @app.get("/server/redoc", include_in_schema=False)
+    async def redoc_html() -> HTMLResponse:  # noqa: D103
+        if app.openapi_url is None:
+            raise RuntimeError(
+                "Couldn't get OpenAPI URL from FastAPI."
+                + " This is probably some kind of misconfiguration."
+            )
+        return get_redoc_html(
+            openapi_url=app.openapi_url,
+            title=app.title,
+            redoc_js_url=_REDOC_CDN_URL,
+        )
 
     LOG.info(
         "Setup: "
