97069bf2ba251ccd2d5253ef4636773b2846435c
Max Marrone <max@opentrons.com>
2026-07-30 13:55:01 -0400

    Remove th

--- app/src/redux/config/schema-types.ts
+++ app/src/redux/config/schema-types.ts
@@ -17,7 +17,6 @@ export type DevInternalFlag =
   | 'reactQueryDevtools'
   | 'reactScan'
   | 'quickTransferProtocolContentsLog'
-  | 'accessControlMode'
   | 'robotSearchBar'
   | 'showGitDetails'
 
