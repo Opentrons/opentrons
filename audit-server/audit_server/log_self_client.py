b9ce894c56efd12a9e86a6d6691d8624f0110165
Seth Foster <seth@opentrons.com>
2026-07-31 16:26:23 -0400

    feat(audit-server): handle reason settings
    The audit server needs some extra help setting things up because it
    needs its little dummy audit client that reads from internal
    dependencies. Also while we're at it, strengthen the tests a bit.

--- audit-server/audit_server/log_self_client.py
+++ audit-server/audit_server/log_self_client.py
@@ -4,6 +4,9 @@ import datetime
 from typing import override
 
 from server_utils.audit.audit_server import (
+    AuditSettingsResponseData as SUSettingsData,
+)
+from server_utils.audit.audit_server import (
     Client as SUClient,
 )
 from server_utils.audit.audit_server import (
@@ -15,14 +18,18 @@ from server_utils.audit.audit_server import (
 
 from .log_ingest.models import AuditLogMessage
 from audit_server.log_storage.log_data_manager import LogDataManager
+from audit_server.settings.store import SettingsStore
 
 
 class LocalClient(SUClient):
     """An 'audit client' that provides direct access to the log data manager."""
 
-    def __init__(self, log_data_manager: LogDataManager) -> None:
+    def __init__(
+        self, log_data_manager: LogDataManager, settings_store: SettingsStore
+    ) -> None:
         """Build the LocalClient and provide the log data manager."""
         self._log_data_manager = log_data_manager
+        self._settings_store = settings_store
 
     @override
     async def submit_log_message(self, message: SUSubmitData) -> SUSuccessData:
@@ -39,3 +46,11 @@ class LocalClient(SUClient):
         message_str = to_log.model_dump_json(indent=None)
         await self._log_data_manager.store_log(message_str)
         return SUSuccessData(loggedAt=ingest_time)
+
+    @override
+    async def get_settings(self) -> SUSettingsData:
+        settings = self._settings_store.get_settings()
+        return SUSettingsData(
+            requireReasonForInteraction=settings.requireReasonForInteraction,
+            minLengthOfReasonForInteraction=settings.minLengthOfReasonForInteraction,
+        )
