5c08434d35438f3562fdb7b603c9a8d2b2bd9631
Seth Foster <seth@opentrons.com>
2026-08-03 16:36:56 -0400

    fix(server-utils): fix pydantic parse
    This is allowed to be missing

--- server-utils/server_utils/audit/audit_server.py
+++ server-utils/server_utils/audit/audit_server.py
@@ -184,7 +184,7 @@ class AuditSettingsResponseData(_StrictBaseModel):
     """Audit settings payload."""
 
     requireReasonForInteraction: bool
-    minLengthOfReasonForInteraction: int | None
+    minLengthOfReasonForInteraction: int | None = None
 
 
 class AuditSettingsResponseBody(_StrictBaseModel):
