b2f67c369cce8249a2090ccd4b014a6230b5e6a9
Janie Jaffe <35556896+janiejaffe@users.noreply.github.com>
2026-08-04 17:03:13 -0400

    add logperiod id to rundata + rundata to dismiss reponse

--- api-client/src/runs/dismissCurrentRun.ts
+++ api-client/src/runs/dismissCurrentRun.ts
@@ -1,14 +1,15 @@
 import { PATCH, request } from '../request'
 
 import type { ResponsePromise } from '../request'
-import type { EmptyResponse, HostConfig } from '../types'
+import type { HostConfig } from '../types'
+import type { RunData } from './types'
 
 export function dismissCurrentRun(
   config: HostConfig,
   runId: string,
   userNotes?: string
-): ResponsePromise<EmptyResponse> {
-  return request<EmptyResponse, { data: { current: false } }>(
+): ResponsePromise<RunData> {
+  return request<RunData, { data: { current: false } }>(
     PATCH,
     `/runs/${runId}`,
     config,
