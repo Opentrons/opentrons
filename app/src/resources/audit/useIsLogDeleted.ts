fda311c907e2c103eed5ccef9eb2db68f34fdb6e
Janie Jaffe <35556896+janiejaffe@users.noreply.github.com>
2026-08-05 14:21:01 -0400

    hopefully dismiss ODD modal when desktop says its good

--- app/src/resources/audit/useIsLogDeleted.ts
+++ app/src/resources/audit/useIsLogDeleted.ts
@@ -2,19 +2,26 @@ import { useMemo } from 'react'
 
 import { useLogPeriodDetailsQuery } from '@opentrons/react-api-client'
 
+import { useClientDataLogDeletion } from '../client_data/audit/useClientDataLogDeletion'
+
 export function useIsLogDeleted(logPeriodId: string): {
   isLoading: boolean
   isDeleted: boolean
+  isError: boolean
 } {
   const { isLoading, error } = useLogPeriodDetailsQuery(logPeriodId)
 
+  const logDeletionStatus = useClientDataLogDeletion(logPeriodId)
+
+  const isError = logDeletionStatus === 'failed'
+
   const isDeleted = useMemo(() => {
-    if (error?.response?.status === 404) {
+    if (error?.response?.status === 404 || logDeletionStatus === 'completed') {
       return true
     }
 
     return false
-  }, [error])
+  }, [error?.response?.status, logDeletionStatus])
 
-  return { isLoading, isDeleted }
+  return { isLoading, isDeleted, isError }
 }
