40c7f3a20d96876acbd252b7a422b737b240d151
jbleon95 <jeremybenleon@gmail.com>
2026-08-07 09:25:50 -0400

    use accurate filename and download active run log

--- audit-server/audit_server/log_export/router.py
+++ audit-server/audit_server/log_export/router.py
@@ -22,7 +22,12 @@ from server_utils.persistence.persistence_directory import (
     ensure_persistence_temp_directory,
 )
 from server_utils.robot.fastapi import get_robot_client
-from server_utils.robot.robot_server import Client as RobotServerClient
+from server_utils.robot.robot_server import (
+    ActiveCurrentRunNotFoundError,
+)
+from server_utils.robot.robot_server import (
+    Client as RobotServerClient,
+)
 
 from audit_server.log_storage.dependency import get_log_data_manager
 from audit_server.log_storage.log_data_manager import LogDataManager
@@ -63,7 +68,11 @@ async def get_log_periods(
     responses={
         fastapi.status.HTTP_404_NOT_FOUND: {
             "description": "No log period could be found for the period ID."
-        }
+        },
+        fastapi.status.HTTP_409_CONFLICT: {
+            "description": "When requesting current log period, no current run"
+            " could be found when one is running."
+        },
     },
 )
 async def download_log_period(
@@ -79,7 +88,8 @@ async def download_log_period(
 ) -> FileResponse:
     """Download a zipped verifiable audit log period."""
     try:
-        periods = log_data_manager.get_period_entries(period_id=periodId)
+        period_entries = log_data_manager.get_period_entries(period_id=periodId)
+        period_details = log_data_manager.get_log_period_details(period_id=periodId)
     except NoPeriodById as exc:
         raise fastapi.HTTPException(
             status_code=fastapi.status.HTTP_404_NOT_FOUND,
@@ -93,6 +103,18 @@ async def download_log_period(
 
     signing_key = await key_client.get_key_and_hash()
     robot_info = await robot_server_client.get_name_and_serial()
+    if period_details.endedAt is None:
+        try:
+            current_run_log_response = await robot_server_client.get_current_run_log()
+        except ActiveCurrentRunNotFoundError as exc:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_409_CONFLICT,
+                detail="Could not find run log information for active run.",
+            ) from exc
+
+        serialized_log = current_run_log_response.serialized_log
+    else:
+        serialized_log = None
 
     signed_robot_identity = await key_client.sign_message(
         SignMessageData(
@@ -114,12 +136,14 @@ async def download_log_period(
 
     zip_file_path = Path(temp_dir.name) / "log_period.zip"
     with zipfile.ZipFile(zip_file_path, mode="w") as zh:
-        zh.writestr("log_period.json", periods.user_log.model_dump_json())
+        zh.writestr("log_period.json", period_entries.user_log.model_dump_json())
         zh.writestr("signing_key.pem", signing_key.publicKey)
         zh.writestr("robot_identity.json", signed_robot_identity.model_dump_json())
-        for robot_log in periods.robot_log_entries:
+        for robot_log in period_entries.robot_log_entries:
             robot_log_path = Path(robot_log.file_path)
             zh.write(robot_log_path, arcname=robot_log_path.name)
+        if serialized_log is not None:
+            zh.writestr(serialized_log.filename, serialized_log.serialized_json)
 
     def cleanup_files() -> None:
         zip_file_path.unlink()
