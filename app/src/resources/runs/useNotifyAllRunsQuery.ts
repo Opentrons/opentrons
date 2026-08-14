213d24308feb894d2c5a04d6a71bb64344684cbd
Janie Jaffe <35556896+janiejaffe@users.noreply.github.com>
2026-08-14 16:49:22 -0400

    refetch after paint and not in render

--- app/src/resources/runs/useNotifyAllRunsQuery.ts
+++ app/src/resources/runs/useNotifyAllRunsQuery.ts
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 import { useAllRunsQuery } from '@opentrons/react-api-client'
 
 import { useNotifyDataReady } from '../useNotifyDataReady'
@@ -31,9 +33,14 @@ export function useNotifyAllRunsQuery(
     hostOverride
   )
 
-  if (shouldRefetch) {
-    void httpQueryResult.refetch()
-  }
+  useEffect(() => {
+    if (shouldRefetch) {
+      void httpQueryResult.refetch()
+    }
+
+    // refetch is stable, the result object is not
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shouldRefetch])
 
   return httpQueryResult
 }
