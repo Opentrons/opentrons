5d0a7b6885be5cba0fa6330e3071de0a7e36179d
ncdiehl11 <ndiehl@opentrons.com>
2026-08-14 11:23:07 -0400

    fix(protocol-designer): render vacuum module dock label
    Special cases vacuum module dock label in ModuleLabel and SelectedItems. Also, fixes utility that
    inadvertently set the vacuum module position far to the right.
    
    Closes RQA-5867

--- protocol-designer/src/pages/Designer/utils.ts
+++ protocol-designer/src/pages/Designer/utils.ts
@@ -111,14 +111,7 @@ const _getAdjustedSlot = (
   }
   return slot
 }
-const _getOffsetFromSlot = (
-  slot: DeckSlot,
-  isSlotAVacuumDock: boolean,
-  isSlotAHopper: boolean
-): number => {
-  if (isSlotAVacuumDock) {
-    return VACUUM_DOCK_LABWARE_X_OFFSET
-  }
+const _getOffsetFromSlot = (slot: DeckSlot, isSlotAHopper: boolean): number => {
   if (isSlotAHopper) {
     return HOPPER_LABWARE_X_OFFSET
   }
@@ -152,7 +145,7 @@ export const getSlotInformation = (
       ? getPositionFromSlotId(
           adjustedSlot,
           deckDef,
-          _getOffsetFromSlot(slot, isSlotAVacuumDock, isSlotAHopper)
+          _getOffsetFromSlot(slot, isSlotAHopper)
         )
       : null
   const createdModuleForSlot = Object.values(deckSetupModules).find(
