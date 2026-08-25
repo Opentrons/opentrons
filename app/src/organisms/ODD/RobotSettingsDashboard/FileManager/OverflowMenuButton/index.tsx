c0abd5ec75932936b72db3684e063d788ccfa3c8
ncdiehl11 <ndiehl@opentrons.com>
2026-08-25 10:43:14 -0400

    translate overflow button aria label

--- app/src/organisms/ODD/RobotSettingsDashboard/FileManager/OverflowMenuButton/index.tsx
+++ app/src/organisms/ODD/RobotSettingsDashboard/FileManager/OverflowMenuButton/index.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next'
+
 import { COLORS, Icon } from '@opentrons/components'
 
 import styles from './overflowmenubutton.module.css'
@@ -10,12 +12,13 @@ interface OverflowMenuButtonProps {
 
 export function OverflowMenuButton(props: OverflowMenuButtonProps): ReactNode {
   const { onClick } = props
+  const { t } = useTranslation('device_details')
 
   return (
     <button
       type="button"
       className={styles.overflow_button}
-      aria-label="overflow menu button"
+      aria-label={t('overflow_menu_button')}
       onClick={onClick}
     >
       <Icon name="overflow-btn-touchscreen" color={COLORS.grey60} />
