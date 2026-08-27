608559f281c9ed2e20970c3e3e35d191cb77440e
Seth Foster <seth@opentrons.com>
2026-08-27 11:06:09 -0400

    dont use robot server logging
    If you try to use robot server logging for this then you don't actually
    get any logging because, well, that's for the robot server and it
    doesn't properly configure the api logging.

--- robot-server/robot_server/runs/run_process_entry_point.py
+++ robot-server/robot_server/runs/run_process_entry_point.py
@@ -2,24 +2,21 @@
 
 import argparse
 import asyncio
+import logging
 import threading
 from typing import Any
 
 from opentrons.protocol_engine import DeckType
+from opentrons.util.logging_config import log_init
 from opentrons.util.pyro.pyro_daemon_utility import create_pyro_daemon
 
 from robot_server.runs.run_process import DirectedRunProcess, register_all_needed_types
-from robot_server.service.logging import initialize_logging
-
-import logging
 
 log = logging.getLogger(__name__)
 
 
 def initialize_run_process(process_name: str) -> threading.Thread:
     """Build an instance of the DirectedRunProcess, create a thread to run it, and return a pyro daemon thread."""
-
-    initialize_logging()
     log.info(f"run process {process_name} begins")
 
     def _start_and_run_process(process: DirectedRunProcess) -> None:
@@ -71,6 +68,17 @@ if __name__ == "__main__":
     parser.add_argument(
         "--pyroname", required=True, help="The name of the pyro daemon."
     )
-    pyro_thread = initialize_run_process(parser.parse_args().pyroname)
+    parser.add_argument(
+        "--loglevel",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "NONE"],
+        required=False,
+        help="Logging level",
+        default="NONE",
+    )
+    args = parser.parse_args()
+    if args.loglevel != "NONE":
+        log_init(args.loglevel.upper())
+    pyro_thread = initialize_run_process(args.pyroname)
+
     pyro_thread.start()
     pyro_thread.join()
