fae16f335ee57f155f580ee2b5644a90dcfd6fd9
TamarZanzouri <zanzouritamar@gmail.com>
2026-08-31 13:09:35 -0400

    fix(app, auth-server): max login attempts behavior (#22368)
    Closes RQA-5911
    
    Adds a top boundary for max login attempts, when blur out reset state of input field if no documentation supplied.

--- app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsComplianceReady/ComplianceReadySoftwareSettings/index.tsx
+++ app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsComplianceReady/ComplianceReadySoftwareSettings/index.tsx
@@ -20,7 +20,9 @@ import {
   getAuthInputPatch,
   getFieldValuesFromSettings,
   isValidLogoutIdleTime,
+  isValidMaxNumberOfLoginAttempts,
   isValidPasswordComplexityMinimumLength,
+  MAX_NUMBER_OF_LOGIN_ATTEMPTS,
   MAX_PASSWORD_COMPLEXITY_MINIMUM_LENGTH,
 } from './complianceReadySettingsHelper'
 import {
@@ -85,9 +87,9 @@ export function ComplianceReadySoftwareSettings({
   const auditSettingsQuery = useAuditSettingsQuery()
   const robotServerAccessControlSettingsQuery =
     useGetRobotServerAccessControlSettingsQuery()
-  const { mutate: patchAuthSettings } =
+  const { mutateAsync: patchAuthSettings } =
     useAuthSettingsMutation(documentationState)
-  const { mutate: patchAuditSettings } =
+  const { mutateAsync: patchAuditSettings } =
     useAuditSettingsMutation(documentationState)
   const { mutate: patchRobotServerAccessControlSettings } =
     usePatchRobotServerAccessControlSettingsMutation(documentationState)
@@ -106,23 +108,23 @@ export function ComplianceReadySoftwareSettings({
     ]
   )
 
-  const handleAuthSettingInputBlur = (
+  const handleAuthSettingInputBlur = async (
     id: AuthSettingFieldId,
     value: string
-  ): void => {
+  ): Promise<void> => {
     const authPatch = getAuthInputPatch(id, value, fieldValues)
     if (authPatch != null) {
-      patchAuthSettings(authPatch)
+      await patchAuthSettings(authPatch)
     }
   }
 
-  const handleAuditSettingInputBlur = (
+  const handleAuditSettingInputBlur = async (
     id: AuditServerSettingFieldId,
     value: string
-  ): void => {
+  ): Promise<void> => {
     const auditPatch = getAuditInputPatch(id, value, fieldValues)
     if (auditPatch != null) {
-      patchAuditSettings(auditPatch)
+      await patchAuditSettings(auditPatch)
     }
   }
 
@@ -194,15 +196,23 @@ export function ComplianceReadySoftwareSettings({
           isLastSection={false}
         >
           <InputSetting
-            key={String(fieldValues.maxNumberOfLoginAttempts)}
             label={t(
               'desktop_maximum_login_attempts_before_account_deactivation'
             )}
             value={String(fieldValues.maxNumberOfLoginAttempts)}
             units={t('desktop_logins')}
-            onBlur={value => {
+            min={1}
+            max={MAX_NUMBER_OF_LOGIN_ATTEMPTS}
+            validate={value =>
+              isValidMaxNumberOfLoginAttempts(value)
+                ? null
+                : t('desktop_maximum_login_attempts_invalid', {
+                    max: MAX_NUMBER_OF_LOGIN_ATTEMPTS,
+                  })
+            }
+            onBlur={value =>
               handleAuthSettingInputBlur('maxNumberOfLoginAttempts', value)
-            }}
+            }
           />
           <Divider />
           <ComplianceReadyToggleField
@@ -214,13 +224,12 @@ export function ComplianceReadySoftwareSettings({
             }}
           >
             <InputSetting
-              key={String(fieldValues.passwordResetTime)}
               label={t('desktop_length_of_time')}
               value={String(fieldValues.passwordResetTime)}
               units={t('desktop_days')}
-              onBlur={value => {
+              onBlur={value =>
                 handleAuthSettingInputBlur('passwordResetTime', value)
-              }}
+              }
             />
           </ComplianceReadyToggleField>
           <Divider />
@@ -243,7 +252,6 @@ export function ComplianceReadySoftwareSettings({
               }}
             />
             <InputSetting
-              key={String(fieldValues.passwordComplexityMinimumLength)}
               label={t('desktop_minimum_password_length')}
               value={String(fieldValues.passwordComplexityMinimumLength)}
               units={t('desktop_characters')}
@@ -256,17 +264,16 @@ export function ComplianceReadySoftwareSettings({
                       max: MAX_PASSWORD_COMPLEXITY_MINIMUM_LENGTH,
                     })
               }
-              onBlur={value => {
+              onBlur={value =>
                 handleAuthSettingInputBlur(
                   'passwordComplexityMinimumLength',
                   value
                 )
-              }}
+              }
             />
           </ComplianceReadyToggleField>
           <Divider />
           <InputSetting
-            key={String(fieldValues.idleLogout)}
             label={t('desktop_auto_logout_inactivity_length')}
             value={String(fieldValues.idleLogout)}
             units={t('desktop_minutes')}
@@ -275,9 +282,7 @@ export function ComplianceReadySoftwareSettings({
                 ? null
                 : t('desktop_idle_logout_must_be_greater_than_zero')
             }
-            onBlur={value => {
-              handleAuthSettingInputBlur('idleLogout', value)
-            }}
+            onBlur={value => handleAuthSettingInputBlur('idleLogout', value)}
           />
         </ComplianceReadySettingsSection>
 
@@ -335,18 +340,17 @@ export function ComplianceReadySoftwareSettings({
             }}
           >
             <InputSetting
-              key={String(fieldValues.minLengthOfReasonForInteraction)}
               label={t(
                 'desktop_minimum_length_for_documentation_for_robot_actions'
               )}
               value={String(fieldValues.minLengthOfReasonForInteraction)}
               units={t('desktop_characters')}
-              onBlur={value => {
+              onBlur={value =>
                 handleAuditSettingInputBlur(
                   'minLengthOfReasonForInteraction',
                   value
                 )
-              }}
+              }
             />
           </ComplianceReadyToggleField>
           <Divider />
