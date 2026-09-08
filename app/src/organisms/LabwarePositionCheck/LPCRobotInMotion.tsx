e18c329ff46125b2b14c4316123346addc7af186
TamarZanzouri <zanzouritamar@gmail.com>
2026-09-08 13:46:41 -0400

    removed deprecated compoments and styled text

--- app/src/organisms/LabwarePositionCheck/LPCRobotInMotion.tsx
+++ app/src/organisms/LabwarePositionCheck/LPCRobotInMotion.tsx
@@ -1,22 +1,19 @@
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
-import styled, { css } from 'styled-components'
+import { css } from 'styled-components'
 
 import {
-  ALIGN_CENTER,
-  COLORS,
   DIRECTION_COLUMN,
-  Flex,
   Icon,
-  JUSTIFY_CENTER,
-  LegacyStyledText,
   RESPONSIVENESS,
   SPACING,
-  TYPOGRAPHY,
+  StyledText,
 } from '@opentrons/components'
 
 import { LPCContentContainer } from '/app/organisms/LabwarePositionCheck/LPCContentContainer'
 import { getIsOnDevice } from '/app/redux/config'
+
+import styles from './lpcrobotinmotion.module.css'
 
 import type { LPCWizardContentProps } from '/app/organisms/LabwarePositionCheck/types'
 
@@ -39,42 +36,25 @@ export function LPCRobotInMotion(props: RobotMotionLoaderProps): JSX.Element {
       desktopHeaderBtnCopy="NO_COPY"
       desktopFooterBtnCopy="NO_COPY"
     >
-      <Flex css={CONTAINER_STYLE}>
-        <Icon name="ot-spinner" css={SPINNER_STYLE} spin />
-        {header != null ? <LoadingText>{header}</LoadingText> : null}
-        {body != null ? (
-          <LegacyStyledText forwardedAs="p">{body}</LegacyStyledText>
+      <div className={styles.container}>
+        <Icon name="ot-spinner" spin className={styles.spinner} />
+        {header != null ? (
+          <StyledText oddStyle="level3HeaderBold" desktopStyle="headingSmallBold">
+            {header}
+          </StyledText>
         ) : null}
-      </Flex>
+        {body != null ? (
+          <StyledText
+            oddStyle="level4HeaderRegular"
+            desktopStyle="bodyDefaultRegular"
+          >
+            {body}
+          </StyledText>
+        ) : null}
+      </div>
     </LPCContentContainer>
   )
 }
-
-const LoadingText = styled.h1`
-  ${TYPOGRAPHY.h1Default}
-
-  p {
-    text-transform: lowercase;
-  }
-
-  p::first-letter {
-    text-transform: uppercase;
-  }
-
-  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-    ${TYPOGRAPHY.level3HeaderBold}
-  }
-`
-
-const CONTAINER_STYLE = css`
-  padding: ${SPACING.spacing40};
-  height: 100%;
-  width: 100%;
-  flex-direction: ${DIRECTION_COLUMN};
-  justify-content: ${JUSTIFY_CENTER};
-  align-items: ${ALIGN_CENTER};
-  grid-gap: ${SPACING.spacing24};
-`
 
 // The design system makes a padding exception for this view.
 const CHILDREN_CONTAINER_STYLE = css`
@@ -85,17 +65,5 @@ const CHILDREN_CONTAINER_STYLE = css`
   @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
     padding: 0 ${SPACING.spacing60} ${SPACING.spacing40} ${SPACING.spacing60};
     gap: ${SPACING.spacing40};
-  }
-`
-
-const SPINNER_STYLE = css`
-  width: 5rem;
-  height: 5rem;
-  color: ${COLORS.grey60};
-
-  @media ${RESPONSIVENESS.touchscreenMediaQuerySpecs} {
-    width: 8rem;
-    height: 8rem;
-    color: ${COLORS.grey50};
   }
 `
