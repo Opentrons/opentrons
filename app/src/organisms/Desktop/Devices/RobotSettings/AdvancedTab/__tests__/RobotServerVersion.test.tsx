5f1179a52b1f3da389a44125ee580ffc183870bb
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2026-09-16 10:01:59 -0400

    lint

--- app/src/organisms/Desktop/Devices/RobotSettings/AdvancedTab/__tests__/RobotServerVersion.test.tsx
+++ app/src/organisms/Desktop/Devices/RobotSettings/AdvancedTab/__tests__/RobotServerVersion.test.tsx
@@ -15,9 +15,8 @@ import { handleUpdateBuildroot } from '../../UpdateBuildroot'
 import { RobotServerVersion } from '../RobotServerVersion'
 
 vi.mock('/app/redux-resources/robots')
-vi.mock('/app/redux/robot-update/selectors', async importOriginal => {
-  const actual =
-    await importOriginal<typeof import('/app/redux/robot-update/selectors')>()
+vi.mock('/app/redux/robot-update/selectors', async () => {
+  const actual = await vi.importActual('/app/redux/robot-update/selectors')
   return {
     ...actual,
     getRobotUpdateDisplayInfo: vi.fn(),
