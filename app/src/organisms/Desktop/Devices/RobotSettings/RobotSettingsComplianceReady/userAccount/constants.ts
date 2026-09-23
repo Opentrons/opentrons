9ee179d5b46582969224f628e69cf1c20bdaec13
Janie Jaffe <35556896+janiejaffe@users.noreply.github.com>
2026-09-23 10:21:15 -0400

    lint

--- app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsComplianceReady/userAccount/constants.ts
+++ app/src/organisms/Desktop/Devices/RobotSettings/RobotSettingsComplianceReady/userAccount/constants.ts
@@ -1,7 +1,5 @@
 import type { AuthUserAccountType } from '@opentrons/api-client'
 
-
-
 export const ADD_USER_WIZARD_TOTAL_STEPS = 2
 export const ADD_USER_WIZARD_CREATE_ACCOUNT_STEP = 0
 export const ADD_USER_WIZARD_ONE_TIME_PASSWORD_STEP = 2
